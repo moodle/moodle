6bfdcdef6bee6b66415a9af50f8d5337db53edb4
martin <martin>
2001-11-25 15:50:17 +0000

    Setting it back to default status.

--- config.php
+++ config.php
@@ -14,15 +14,15 @@
 $CFG->dbtype    = "mysql";       // eg mysql, postgres, oracle, access etc 
 $CFG->dbhost    = "localhost";   // eg localhost 
 $CFG->dbname    = "moodle";      // eg moodle
-$CFG->dbuser    = "moodle";
-$CFG->dbpass    = "moodle";
+$CFG->dbuser    = "username";
+$CFG->dbpass    = "password";
 
 
 // Next you need to tell Moodle where it is, and where it can save files.
 
-$CFG->wwwroot   = "http://server.dougiamas.net/moodle";
-$CFG->dirroot   = "/web/server/moodle";
-$CFG->dataroot  = "/web/moodledata";    // Web-server writeable
+$CFG->wwwroot   = "http://example.com/moodle";
+$CFG->dirroot   = "/web/moodle";
+$CFG->dataroot  = "/home/moodledata";    // Web-server writeable
 
 
 // Choose a theme from the "themes" folder.  Default theme is "standard".
@@ -33,7 +33,7 @@ $CFG->theme     = "standard";
 // Give the full name (eg mail.example.com) of an SMTP server that the 
 // web server machine has access to (to send mail).  Default: "localhost".
 
-$CFG->smtphost  = "dougiamas.com";
+$CFG->smtphost  = "mail.example.com";
 
 
 // You should not need to change anything below this line
