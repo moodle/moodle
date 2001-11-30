f32f0c749c3aa7e825f505bca64ec842349a09a0
martin <martin>
2001-11-30 15:42:43 +0000

    Added copyright notices and cleaned up the documentation

--- config.php
+++ config.php
@@ -6,12 +6,33 @@
 // This file should be located in the top-level directory.
 //
 ///////////////////////////////////////////////////////////////////////////
-
+// 
+// NOTICE OF COPYRIGHT
+//
+// Moodle - Modular Object-Oriented Dynamic Learning Environment
+//          http://moodle.com
+// 
+// Copyright (C) 2001  Martin Dougiamas  http://dougiamas.com
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details:
+// 
+//          http://www.gnu.org/copyleft/gpl.html
+// 
+////////////////////////////////////////////////////////////////////////////
+//
 // Site configuration variables are all stored in the CFG object.
 
-// First, we need to configure the database where all Moodle data 
+// First, you need to configure the database where all Moodle data 
 // will be stored.  This database must already have been created
-// and a username/password created to access it.   See INSTALL doc.
+// and a username/password created to access it.   See doc/INSTALL.
 
 $CFG->dbtype    = "mysql";       // eg mysql, postgres, oracle, access etc 
 $CFG->dbhost    = "localhost";   // eg localhost 
@@ -20,11 +41,17 @@ $CFG->dbuser    = "username";
 $CFG->dbpass    = "password";
 
 
-// Next you need to tell Moodle where it is, and where it can save files.
+// Next you need to tell Moodle where it is located
 
 $CFG->wwwroot   = "http://example.com/moodle";
 $CFG->dirroot   = "/web/moodle";
-$CFG->dataroot  = "/home/moodledata";    // Web-server writeable
+
+
+// And where it can save files.  This directory should be writeable
+// by the web server user (usually 'nobody' or 'apache'), but it should 
+// not be accessible directly via the web.
+
+$CFG->dataroot  = "/home/moodledata";
 
 
 // Choose a theme from the "themes" folder.  Default theme is "standard".
@@ -33,12 +60,13 @@ $CFG->theme     = "standard";
 
 
 // Give the full name (eg mail.example.com) of an SMTP server that the 
-// web server machine has access to (to send mail).  Default: "localhost".
+// web server machine has access to (to send mail).
 
 $CFG->smtphost  = "mail.example.com";
 
 
-// You should not need to change anything below this line
+// You should not need to change anything else. To continue setting up 
+// Moodle, use your web browser to go to the moodle/admin web page.
 ///////////////////////////////////////////////////////////////////////////
 
 $CFG->libdir    = "$CFG->dirroot/lib";
