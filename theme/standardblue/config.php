bb31c3db9d519140be0dd09bd61e6dd4b0448841
martin <martin>
2002-02-26 03:50:53 +0000

    New blue theme

---
+++ theme/standardblue/config.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$
+
+$THEME->body         = "#C5EFFF";  // Main page color
+$THEME->cellheading  = "#8BCDE8";  // Standard headings of big tables
+$THEME->cellheading2 = "#B5EAFF";  // Highlight headings of tables
+$THEME->cellcontent  = "#FFFFFF";  // For areas with text
+$THEME->cellcontent2 = "#FFD991";  // Alternate colour
+$THEME->borders      = "#555555";  // Table borders
+
+?>
