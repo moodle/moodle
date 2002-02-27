6beb3343c3ce3358bda360271de2ca3eec0884fa
martin <martin>
2002-02-27 10:09:02 +0000

    Slight color changes

--- theme/standardblue/config.php
+++ theme/standardblue/config.php
@@ -1,8 +1,9 @@
 <?PHP // $Id$
 
 $THEME->body         = "#C5EFFF";  // Main page color
-$THEME->cellheading  = "#8BCDE8";  // Standard headings of big tables
-$THEME->cellheading2 = "#B5EAFF";  // Highlight headings of tables
+$THEME->cellheading  = "#9ADAF4";  // Standard headings of big tables
+$THEME->cellheading2 = "#73C1E1";  // Highlight headings of tables
+
 $THEME->cellcontent  = "#FFFFFF";  // For areas with text
 $THEME->cellcontent2 = "#FFD991";  // Alternate colour
 $THEME->borders      = "#555555";  // Table borders
