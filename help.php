229b6580e1026dda0e4a2e5fdb64b2f6f18bdf46
martin <martin>
2002-03-11 03:18:13 +0000

    First pass at a simple help system

---
+++ help.php
@@ -0,0 +1,23 @@
+<?PHP
+  /// help.php - prints a very simple page and includes a
+  ///            page content or a string from elsewhere
+  ///            Usually this will appear in a popup 
+  ///            See help() in lib/moodlelib.php
+
+  include("config.php");
+
+  if (ereg("\\.\\.", $file)) {
+      error("Error: Filenames can not contain \"..\"");
+  }
+
+
+?>
+<HTML>
+<HEAD>
+<LINK rel="stylesheet" href="<?=$CFG->wwwroot?>/theme/<?=$CFG->theme?>/styles.css">
+</HEAD>
+<BODY BGCOLOR="<?=$THEME->body ?>">
+<? include("lang/$CFG->lang/$file"); ?>
+</BODY>
+</HTML>
+
