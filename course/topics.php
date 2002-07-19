97b3785424c89f04c4be249d8c8021174a9bec5a
martin <martin>
2002-07-19 12:08:30 +0000

    Make 'Edit profile" link blink, as a reminder, if not used yet

--- course/topics.php
+++ course/topics.php
@@ -49,7 +49,11 @@
     print_simple_box("People", $align="CENTER", $width="100%", $color="$THEME->cellheading");
     $moddata[]="<A HREF=\"../user/index.php?id=$course->id\">List of all people</A>";
     $modicon[]="<IMG SRC=\"../user/users.gif\" HEIGHT=16 WIDTH=16 ALT=\"List of everyone\">";
-    $moddata[]="<A HREF=\"../user/view.php?id=$USER->id&course=$course->id\">Edit my profile</A>";
+    if ($USER->description) {
+        $moddata[]="<A HREF=\"../user/view.php?id=$USER->id&course=$course->id\">Edit my profile</A>";
+    } else {
+        $moddata[]="<A HREF=\"../user/view.php?id=$USER->id&course=$course->id\"><BLINK>Edit my profile</BLINK></A>";
+    }
     $modicon[]="<IMG SRC=\"../user/user.gif\" HEIGHT=16 WIDTH=16 ALT=\"Me\">";
     print_side_block("", $moddata, "", $modicon);
 
