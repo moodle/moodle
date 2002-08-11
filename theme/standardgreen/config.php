63e99961d985859404f436b238875892857c46c9
martin <martin>
2002-08-11 06:11:05 +0000

    New coloured themes (green and red) just to add some options

---
+++ theme/standardgreen/config.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$
+
+$THEME->body         = "#B4EFC1";  // Main page color
+$THEME->cellheading  = "#85E49A";  // Standard headings of big tables
+$THEME->cellheading2 = "#3DD35F";  // Highlight headings of tables
+
+$THEME->cellcontent  = "#FFFFFF";  // For areas with text
+$THEME->cellcontent2 = "#85E49A";  // Alternate colour
+$THEME->borders      = "#555555";  // Table borders
+
+?>
