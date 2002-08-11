e4c84e5dc40b7c556a8b356add28335acde91a44
martin <martin>
2002-08-11 06:15:39 +0000

    Minor tweaks to colors

--- theme/standardred/config.php
+++ theme/standardred/config.php
@@ -1,11 +1,11 @@
 <?PHP // $Id$
 
-$THEME->body         = "#EFD6D7";  // Main page color
-$THEME->cellheading  = "#E9C5C6";  // Standard headings of big tables
-$THEME->cellheading2 = "#E4B6B7";  // Highlight headings of tables
+$THEME->body         = "#F5E7E7";  // Main page color
+$THEME->cellheading  = "#ECD2D2";  // Standard headings of big tables
+$THEME->cellheading2 = "#E9C5C6";  // Highlight headings of tables
 
 $THEME->cellcontent  = "#FFFFFF";  // For areas with text
-$THEME->cellcontent2 = "#E4B6B7";  // Alternate colour
+$THEME->cellcontent2 = "#ECD2D2";  // Alternate colour
 $THEME->borders      = "#552021";  // Table borders
 
 ?>
