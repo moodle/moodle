0a40b985837622fb9f89ae91872a51f8ea9c9d6d
martin <martin>
2002-08-12 14:59:56 +0000

    More translating

--- lang/en/survey.php
+++ lang/en/survey.php
@@ -5,4 +5,7 @@ $string[modulename] = "Survey";
 $string[modulenameplural] = "Surveys";
 #------------------------------------------------------------
 
+$string[surveyname] = "Survey name";
+$string[surveytype] = "Survey type";
+
 ?>
