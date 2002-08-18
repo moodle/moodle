ecac31f1ff9bc1396335d3c1dd09fb5f8d903434
martin <martin>
2002-08-18 09:43:39 +0000

    new strings

--- lang/en/choice.php
+++ lang/en/choice.php
@@ -12,6 +12,7 @@ $string[choicename] = "Choice name";
 $string[choicetext] = "Choice text";
 $string[notanswered] = "Not answered yet";
 $string[responses] = "Responses";
+$string[responsesto] = "Responses to \$a";
 $string[savemychoice] = "Save my choice";
 $string[viewallresponses] = "View all responses";
 ?>
