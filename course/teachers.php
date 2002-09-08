09442e50676d6eb33b72fcf0d6d3b15d081f0ca4
martin <martin>
2002-09-08 05:15:52 +0000

    Added documentation to the teachers page.

--- course/teachers.php
+++ course/teachers.php
@@ -47,7 +47,7 @@
 
 /// Otherwise fill and print the form.
 
-	print_header($streditcoursesettings, "$course->fullname", 
+	print_header("$course->shortname: $course->teachers", "$course->fullname", 
                  "<A HREF=\"$CFG->wwwroot/course/view.php?id=$course->id\">$course->shortname</A> 
                   -> $course->teachers");
 
@@ -59,18 +59,25 @@
 
     echo "<FORM ACTION=teachers.php METHOD=post>";
     foreach ($teachers as $teacher) {
-        $table->head  = array (get_string("name"), get_string("order"), get_string("role"));
-        $table->align = array ("LEFT", "CENTER", "CENTER");
+        $table->head  = array ("", get_string("name"), get_string("order"), get_string("role"));
+        $table->align = array ("RIGHT", "LEFT", "CENTER", "CENTER");
+        $table->size  = array ("35", "", "", "");
 
         $picture = print_user_picture($teacher->id, $course->id, $teacher->picture, false, true);
 
-        $table->data[] = array ("$picture $teacher->firstname $teacher->lastname",
+        if (!$teacher->role) {
+            $teacher->role = $course->teacher;
+        }
+
+        $table->data[] = array ($picture, "$teacher->firstname $teacher->lastname",
                                 "<INPUT TYPE=text NAME=\"a$teacher->id\" VALUE=\"$teacher->authority\" SIZE=2>",
                                 "<INPUT TYPE=text NAME=\"r$teacher->id\" VALUE=\"$teacher->role\" SIZE=30>");
     }
     print_table($table);
     echo "<INPUT TYPE=hidden NAME=id VALUE=\"$course->id\">";
-    echo "<CENTER><BR><INPUT TYPE=submit VALUE=\"".get_string("savechanges")."\"></CENTER>";
+    echo "<CENTER><BR><INPUT TYPE=submit VALUE=\"".get_string("savechanges")."\"> ";
+    helpbutton("teachers", $course->teachers);
+    echo "</CENTER>";
     echo "</FORM>";
 
     print_footer($course);
