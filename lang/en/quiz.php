8e7ad7283b6afaaa4de8e2c32c6cef05c1ba8c42
moodler <moodler>
2002-10-06 17:20:54 +0000

    New words

--- lang/en/quiz.php
+++ lang/en/quiz.php
@@ -6,6 +6,7 @@ $string['modulenameplural'] = "Quizzes";
 #------------------------------------------------------------
 
 $string['alwaysavailable'] = "Always available";
+$string['answer'] = "Answer";
 $string['attempt'] = "Attempt \$a";
 $string['attemptfirst'] = "First attempt";
 $string['attemptlast'] = "Last attempt";
@@ -13,9 +14,13 @@ $string['attempts'] = "Attempts";
 $string['attemptsallowed'] = "Attempts allowed";
 $string['attemptsunlimited'] = "Unlimited attempts";
 $string['daysavailable'] = "Days available";
+$string['false'] = "False";
 $string['gradeaverage'] = "Average grade";
 $string['gradehighest'] = "Highest grade";
 $string['grademethod'] = "Grading method";
 $string['introduction'] = "Introduction";
+$string['marks'] = "Marks";
+$string['savemyanswers'] = "Save my answers";
 $string['showfeedback'] = "After answering, show feedback?";
 $string['showcorrectanswer'] = "After answering, show correct answers?";
+$string['true'] = "True";
