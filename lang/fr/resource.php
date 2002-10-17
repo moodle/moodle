92674bf8bbca00970ba94616fc16354433848acd
moodler <moodler>
2002-10-17 15:14:54 +0000

    Changes due to new module 'resource'

--- lang/fr/resource.php
+++ lang/fr/resource.php
@@ -2,8 +2,8 @@
       // reading.php - created with Moodle 1.0.5 (2002092700)
 
 
-$string['addreading'] = "Ajouter une lecture";
-$string['editingareading'] = "Modifier une lecture";
+$string['addresource'] = "Ajouter une lecture";
+$string['editingaresource'] = "Modifier une lecture";
 $string['example'] = "Exemple";
 $string['examplereference'] = "Tobin, K. & Tippins, D (1993) Constructivism as a Referent for Teaching and Learning. In: K. Tobin (Ed) The Practice of Constructivism in Science Education, pp 3-21, Lawrence-Erlbaum, Hillsdale, NJ.";
 $string['exampleurl'] = "http://www.exemple.fr/dossiers/fichier.html";
@@ -17,12 +17,12 @@ $string['note'] = "Note";
 $string['notefile'] = "Pour envoyer plus de fichiers dans le cours (afin qu'ils apparaissent dans cette liste), utiliser le
 <A HREF=\$a>Gestionnaire de fichiers</A>.";
 $string['notypechosen'] = "Vous devez choisir un type. Utiliser le bouton « Retour » de votre navigateur pour recommencer.";
-$string['readingtype'] = "Type de ressource";
-$string['readingtype1'] = "Référence";
-$string['readingtype2'] = "Page Web";
-$string['readingtype3'] = "Fichier envoyé";
-$string['readingtype4'] = "Texte";
-$string['readingtype5'] = "Lien vers une page Web";
-$string['readingtype6'] = "Texte HTML";
+$string['resourcetype'] = "Type de ressource";
+$string['resourcetype1'] = "Référence";
+$string['resourcetype2'] = "Page Web";
+$string['resourcetype3'] = "Fichier envoyé";
+$string['resourcetype4'] = "Texte";
+$string['resourcetype5'] = "Lien vers une page Web";
+$string['resourcetype6'] = "Texte HTML";
 
 ?>
