b0e999e6461544e09fadb6487b811469ef762d10
moodler <moodler>
2002-10-17 15:32:52 +0000

    Changes related to new module 'resource'

--- lang/it/resource.php
+++ lang/it/resource.php
@@ -2,8 +2,8 @@
       // reading.php - created with Moodle 1.0.5 beta 1 (2002092500)
 
 
-$string['addreading'] = "Aggiungi una lettura";
-$string['editingareading'] = "Edita una lettura";
+$string['addresource'] = "Aggiungi una lettura";
+$string['editingaresource'] = "Edita una lettura";
 $string['example'] = "Esempio";
 $string['examplereference'] = "Tobin, K. & Tippins, D (1993) Constructivism as a Referent for Teaching and Learning. In: K. Tobin (Ed) The Practice of Constructivism in Science Education, pp 3-21, Lawrence-Erlbaum, Hillsdale, NJ.";
 $string['exampleurl'] = "http://www.example.com/somedirectory/somefile.html";
@@ -12,17 +12,17 @@ $string['fulltext'] = "Testo integrale";
 $string['htmlfragment'] = "Codice html";
 $string['modulename'] = "Lettura";
 $string['modulenameplural'] = "Letture";
-$string['neverread'] = "Mai letto";
+$string['neverseen'] = "Mai letto";
 $string['note'] = "Nota";
 $string['notefile'] = "Per trasferire altri files nell'archivio del corso (come quelli che compaiono in questa lista) usa il
 <A HREF=\$a >File Manager</A>.";
 $string['notypechosen'] = "È necessario che tu scelga un tipo. Usa il pulsante indietro per ritornare alla pagina precedente e riprovare.";
-$string['readingtype'] = "Tipo di risorsa";
-$string['readingtype1'] = "Riferimento";
-$string['readingtype2'] = "Pagina web";
-$string['readingtype3'] = "File trasferito sul server";
-$string['readingtype4'] = "Puro testo";
-$string['readingtype5'] = "Collegamento web";
-$string['readingtype6'] = "Testo HTML";
+$string['resourcetype'] = "Tipo di risorsa";
+$string['resourcetype1'] = "Riferimento";
+$string['resourcetype2'] = "Pagina web";
+$string['resourcetype3'] = "File trasferito sul server";
+$string['resourcetype4'] = "Puro testo";
+$string['resourcetype5'] = "Collegamento web";
+$string['resourcetype6'] = "Testo HTML";
 
 ?>
--- lang/pt_br/resource.php
+++ lang/pt_br/resource.php
@@ -6,24 +6,24 @@ $string[modulename] = "Leitura";
 $string[modulenameplural] = "Leituras";
 #------------------------------------------------------------
 
-$string[addreading] = "Adicionar leitura";
-$string[editingareading] = "Editar leitura";
+$string[addresource] = "Adicionar leitura";
+$string[editingaresource] = "Editar leitura";
 $string[example] = "Exemplo";
 $string[examplereference] = "Tobin, K. & Tippins, D (1993) Construtivismo como um Referent por Ensinar e Aprender. Em: K. Tobin (Ed) A Prática de Construtivismo em Educação de Ciência, pp 3-21, Lawrence-Erlbaum, Hillsdale, NJ.";
 $string[exampleurl] = "http://www.example.com/somedirectory/somefile.html";
 $string[filename] = "Nome do arquivo";
 $string[fulltext] = "Texto completo";
-$string[neverread] = "Nunca leia";
+$string[neverseen] = "Nunca leia";
 $string[note] = "Nota";
 $string[notefile] = "Para upload mais arquivos no curso (assim eles aparecem nesta lista) use o 
 <A HREF=\$a >Manipulação de arquivo</A>.";
 $string[notypechosen] = "Você precisa escolher um tipo.  Use seu botão de parte de trás para voltar e tentar novamente.";
-$string[readingtype] = "Tipo de recurso";
-$string[readingtype1] = "Referencia";
-$string[readingtype2] = "Página da Web";
-$string[readingtype3] = "Arquivo enviado";
-$string[readingtype4] = "Texto claro";
-$string[readingtype5] = "Endereço da Web";
+$string[resourcetype] = "Tipo de recurso";
+$string[resourcetype1] = "Referencia";
+$string[resourcetype2] = "Página da Web";
+$string[resourcetype3] = "Arquivo enviado";
+$string[resourcetype4] = "Texto claro";
+$string[resourcetype5] = "Endereço da Web";
 $string[htmlfragment] = "Fragmento HTML";
-$string[readingtype6] = "Texto HTML";
+$string[resourcetype6] = "Texto HTML";
 ?>
