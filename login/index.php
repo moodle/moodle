910d1731af12eb837760bc791bcb6b88c740ade5
moodler <moodler>
2002-11-13 02:11:50 +0000

    Guest language ALWAYS the same as the current site language

--- login/index.php
+++ login/index.php
@@ -38,6 +38,9 @@
             $USER = $user;
             $USER->loggedin = true;
             $USER->site = $CFG->wwwroot;   // for added security
+            if ($USER->username = "guest") {
+                $USER->lang = $CFG->lang;  // Guest language always same as site
+            }
             save_session("USER");
     
             if (!update_user_in_db()) {
