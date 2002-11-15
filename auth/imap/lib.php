d17bdf2b3735361f505490c8af4e45bf42508465
moodler <moodler>
2002-11-15 08:59:30 +0000

    More options and faster (use IPs always)

--- auth/imap/lib.php
+++ auth/imap/lib.php
@@ -4,9 +4,9 @@
 // This code is completely untested so far - I'm just jotting down ideas ...
 // Looks like it should work though ...
 
-$CFG->auth_imaphost   = "localhost";
-$CFG->auth_imapport   = "143";     // 143, 993, 100, 119
-$CFG->auth_imaptype   = "imap";    // imap, imapssl, pop3, nntp
+$CFG->auth_imaphost   = "127.0.0.1";  // Should be IP number
+$CFG->auth_imaptype   = "imap";       // imap, imapssl, imapcert, pop3, pop3cert, nntp
+$CFG->auth_imapport   = "143";        // 143, 993, 100, 119
 
 
 function auth_user_login ($username, $password) {
@@ -22,15 +22,21 @@ function auth_user_login ($username, $password) {
         case "imapssl":
             $host = "{$CFG->auth_imaphost:$CFG->auth_imapport/imap/ssl}INBOX";
         break;
+        case "imapcert":
+            $host = "{$CFG->auth_imaphost:$CFG->auth_imapport/imap/ssl/novalidate-cert}INBOX";
+        break;
         case "pop3":
             $host = "{$CFG->auth_imaphost:$CFG->auth_imapport/pop3}INBOX";
         break;
+        case "pop3cert":
+            $host = "{$CFG->auth_imaphost:$CFG->auth_imapport/pop3/ssl/novalidate-cert}INBOX";
+        break;
         case "nntp":
-            $host = "{$CFG->auth_imaphost:$CFG->auth_imapport/nntp}comp.test";
+            $host = "{$CFG->auth_imaphost:$CFG->auth_imapport/nntp}";
         break;
     }
 
-    if ($connection = imap_open($host, $username, $password)) {
+    if ($connection = imap_open($host, $username, $password, OP_HALFOPEN)) {
         imap_close($connection);
         return true;
 
