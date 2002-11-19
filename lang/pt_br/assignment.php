3c1cb3178b427b08a305f845951ebdbd38dd28e4
moodler <moodler>
2002-11-19 15:52:25 +0000

    Checking in new updates from Fabricio

--- lang/pt_br/assignment.php
+++ lang/pt_br/assignment.php
@@ -1,44 +1,40 @@
-<?PHP // $Id$
-# Traduzido por Fabricio Valadares - Três Corações - MG - Brasil
-# Webdesigner da Universidade Vale do Rio Verde
-#------------------------------------------------------------
-$string[modulename] = "Tarefa";
-$string[modulenameplural] = "Tarefas";
-#------------------------------------------------------------
+<?PHP // $Id$ 
+      // assignment.php - created with Moodle 1.0.6.3 (2002111401)
 
 
-$string[assignmentdetails] = "Detalhes de tarefa";
-$string[assignmentname] = "Nome de tarefa";
-$string[assignmenttype] = "Tipo de tarefa";
-$string[description] = "Descrição";
-$string[duedate] = "Data devida";
-$string[early] = "\$a cedo";
-$string[failedupdatefeedback] = "Não atualizou realimentação de submissão para usuário \$a";
-$string[feedbackupdated] = "Realimentação de submissões atualizou para \$a pessoas";
-$string[late] = "\$a tarde";
-$string[maximumgrade] = "Grau de máximo";
-$string[maximumsize] = "Tamanho máximo";
-$string[notsubmittedyet] = "Ainda não submetido";
-$string[overwritewarning] = "Advertindo: enviado SUBSTITUIRÃO seu envio atual novamente";
-$string[submissionfeedback] = "Realimentação de submissão";
-$string[submissions] = "Submissões";
-$string[submitted] = "Enviado";
-$string[typeuploadsingle] = "Enviar um único arquivo, preço entre 0 - 100 marcas";
-$string[uploadedfiles] = "Arquivo enviado";
-$string[viewsubmissions] = "Ver \$a tarefas enviadas";
-$string[yoursubmission] = "Sua submissão";
-$string[newsubmissions] = "Tarefas enviadas";
-$string[submitassignment] = "Submeta sua tarefa usando esta forma";
-$string[uploadbadname] = "Os caráteres estranhos contidos este nome de arquivo e não podiam enviados";
-$string[uploaderror] = "Um erro aconteceu ao conservar o arquivo no usuário";
-$string[uploadfailnoupdate] = "O arquivo enviado está OK mas não pode atualizar sua submissão!  ";
-$string[uploadfiletoobig] = "Desculpe, mas esse arquivo é demasiado grande (o limite é \$a bytes)";
-$string[uploadnofilefound] = "Nenhum arquivo foi encontrado - você selecionou um para enviar?";
-$string[uploadnotregistered] = "'\$a' enviado está OK mas a submissão não registou!  ";
-$string[uploadsuccess] = "Enviado '\$a' com sucesso!";
+$string['assignmentdetails'] = "Detalhes da tarefa";
+$string['assignmentname'] = "Nome da tarefa";
+$string['assignmenttype'] = "Tipo de tarefa";
+$string['description'] = "Descrição";
+$string['duedate'] = "Data devida";
+$string['early'] = "\$a cedo";
+$string['failedupdatefeedback'] = "Não atualizou a realimentação de submissão para usuário \$a";
+$string['feedbackupdated'] = "A realimentação de submissões atualizou para \$a pessoas";
+$string['late'] = "\$a tarde";
+$string['maximumgrade'] = "Grau de máximo";
+$string['maximumsize'] = "Tamanho máximo";
+$string['modulename'] = "Tarefa";
+$string['modulenameplural'] = "Tarefas";
+$string['newsubmissions'] = "Tarefas enviadas";
+$string['notsubmittedyet'] = "Ainda não enviado";
+$string['overwritewarning'] = "Cuidado: enviando novamente SUBSTITUIRÃO seu envio atual.";
+$string['submissionfeedback'] = "Realimentação de envio";
+$string['submissions'] = "Enviados";
+$string['submitassignment'] = "Envie sua tarefa usando esta forma";
+$string['submitted'] = "Enviado";
 $string['typeoffline'] = "Atividade desativada";
+$string['typeuploadsingle'] = "Enviar um único arquivo, com grau entre 0 - 100 créditos";
+$string['uploadbadname'] = "Os caráteres estranhos contidos neste nome de arquivo e não pode ser enviado";
+$string['uploadedfiles'] = "Arquivo enviado";
+$string['uploaderror'] = "Um erro aconteceu ao conservar o arquivo no usuário";
+$string['uploadfailnoupdate'] = "O arquivo enviado está OK mas não pode atualizar sua submissão!  ";
+$string['uploadfiletoobig'] = "Desculpe, mas esse arquivo é muito grande (o limite é de \$a bytes)";
+$string['uploadnofilefound'] = "Nenhum arquivo foi encontrado - você selecionou algum para enviar?";
+$string['uploadnotregistered'] = "'\$a' enviado está OK mas a submissão não registou!  ";
+$string['uploadsuccess'] = "Enviado '\$a' com sucesso!";
 $string['viewfeedback'] = "Classes e gabarito da atribuição da vista";
-
+$string['viewsubmissions'] = "Ver \$a tarefas enviadas";
+$string['yoursubmission'] = "Seu envio";
+$string['allowresubmit'] = "Enviar novamente reservado";
 
 ?>
-
--- lang/pt_br/journal.php
+++ lang/pt_br/journal.php
@@ -1,30 +1,27 @@
-<?PHP // $Id$
-# Traduzido por Fabricio Valadares - Três Corações - MG - Brasil
-# Webdesigner da Universidade Vale do Rio Verde
-#------------------------------------------------------------
-$string[modulename] = "Diário";
-$string[modulenameplural] = "Diários";
-#------------------------------------------------------------
+<?PHP // $Id$ 
+      // journal.php - created with Moodle 1.0.6.3 (2002111401)
 
 
-$string[alwaysopen] = "Sempre aberto";
-$string[blankentry] = "Entrada em branco";
-$string[daysavailable] = "Dias disponíveis";
-$string[editingends] = "Fins de período editando";
-$string[editingended] = "Editando período terminou";
-$string[journalname] = "Nome de diário";
-$string[journalquestion] = "Pergunta de diário";
-$string[journalrating1] = "Não satisfatório";
-$string[journalrating2] = "Satisfatório";
-$string[journalrating3] = "Excelente";
-$string[noentry] = "Nenhuma entrada";
-$string[notopenuntil] = "Este diário não estará aberto até";
-$string[notstarted] = "Você não começou este diário contudo";
-$string[noratinggiven] = "Nenhuma avaliação dada";
-$string[overallrating] = "Avaliação global";
-$string[rate] = "Taxa";
-$string[startoredit] = "Começa ou edita minha entrada de diário";
-$string[viewallentries] = "Veja todas as entradas de diário";
-$string[newjournalentries] = "Entradas de diário novas";
+$string['alwaysopen'] = "Sempre aberto";
+$string['blankentry'] = "Entrada em branco";
+$string['daysavailable'] = "Dias disponíveis";
+$string['editingended'] = "O periodo de edição terminou";
+$string['editingends'] = "Editando extremidades do período";
+$string['journalname'] = "Nome de diário";
+$string['journalquestion'] = "Pergunta de diário";
+$string['journalrating1'] = "Não satisfatório";
+$string['journalrating2'] = "Satisfatório";
+$string['journalrating3'] = "Excelente";
+$string['modulename'] = "Diário";
+$string['modulenameplural'] = "Diários";
+$string['newjournalentries'] = "Novas entradas de diário";
+$string['noentry'] = "Nenhuma entrada";
+$string['noratinggiven'] = "Nenhuma avaliação dada";
+$string['notopenuntil'] = "Este diário não estará aberto até";
+$string['notstarted'] = "Você não começou este diário ainda";
+$string['overallrating'] = "Avaliação global";
+$string['rate'] = "Taxa";
+$string['startoredit'] = "Comece ou edite minha entrada de diário";
+$string['viewallentries'] = "Veja todas as entradas do diário";
 
 ?>
