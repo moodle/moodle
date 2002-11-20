1e77d2035c94869b3dbcc19c89f48502dd99ae25
moodler <moodler>
2002-11-20 15:17:03 +0000

    Update version slightly

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 // database to determine whether upgrades should
 // be performed (see lib/db/*.php)
 
-$version = 2002111900;   // The current version is a date (YYYYMMDDXX)
+$version = 2002112001;   // The current version is a date (YYYYMMDDXX)
 
 $release = "1.0.6.4 beta";  // User-friendly version number
 
