66fe2bb26271a0791e2670c23167fe4b042112d8
moodler <moodler>
2002-11-24 15:30:01 +0000

    Updated versionm, ready for 1.0.6.4

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 // database to determine whether upgrades should
 // be performed (see lib/db/*.php)
 
-$version = 2002112201;   // The current version is a date (YYYYMMDDXX)
+$version = 2002112400;   // The current version is a date (YYYYMMDDXX)
 
-$release = "1.0.6.4 bug-hunting";  // User-friendly version number
+$release = "1.0.6.4";  // User-friendly version number
 
