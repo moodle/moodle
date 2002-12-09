bf1969570e5f154e56fbac49722c0103fbb2d078
moodler <moodler>
2002-12-09 16:21:02 +0000

    Fixed blue a little

--- theme/standardblue/config.php
+++ theme/standardblue/config.php
@@ -4,7 +4,7 @@ $THEME->body         = "#C5EFFF";  // Main page color
 $THEME->cellheading  = "#9ADAF4";  // Standard headings of big tables
 $THEME->cellheading2 = "#73C1E1";  // Highlight headings of tables
 $THEME->cellcontent  = "#FFFFFF";  // For areas with text
-$THEME->cellcontent2 = "#B7E4F6";  // Alternate colour
+$THEME->cellcontent2 = "#B3EAFF";  // Alternate colour
 $THEME->borders      = "#555555";  // Table borders
 $THEME->highlight    = "#AAFFAA";  // Highlighted text (eg after a search)
 
