f1d2a92c853f80220c6138584da2e041433a93e1
mits <mits>
2002-12-10 16:23:50 +0000

    Save with Moodle.

--- lang/ja/choice.php
+++ lang/ja/choice.php
@@ -1,1 +1,17 @@
-<?PHP // $Id$       // choice.php - created with Moodle 1.0.6.4 (2002112400)$string['answered'] = "回答済み";$string['choice'] = "選択肢 \$a";$string['choicename'] = "選択名";$string['choicetext'] = "選択文";$string['modulename'] = "選択";$string['modulenameplural'] = "選択";$string['notanswered'] = "未回答";$string['responses'] = "回答";$string['responsesto'] = "\$a へ回答する";$string['savemychoice'] = "回答を保存する";$string['viewallresponses'] = "\$a 件の回答を見る";?>
+<?PHP // $Id$ 
+      // choice.php - created with Moodle 1.0.6.4 (2002112400)
+
+
+$string['answered'] = "回答済み";
+$string['choice'] = "選択肢 \$a";
+$string['choicename'] = "選択名";
+$string['choicetext'] = "選択文";
+$string['modulename'] = "選択";
+$string['modulenameplural'] = "選択";
+$string['notanswered'] = "未回答";
+$string['responses'] = "回答";
+$string['responsesto'] = "\$a へ回答する";
+$string['savemychoice'] = "回答を保存する";
+$string['viewallresponses'] = "\$a 件の回答を見る";
+
+?>
--- lang/ja/resource.php
+++ lang/ja/resource.php
@@ -1,1 +1,27 @@
-<?PHP // $Id$       // resource.php - created with Moodle 1.0.6.4 (2002112400)$string['addresource'] = "資料を追加する";$string['editingaresource'] = "資料を編集する";$string['example'] = "例";$string['examplereference'] = "Tobin, K. & Tippins, D (1993) Constructivism as a Referent for Teaching and Learning. In: K. Tobin (Ed) The Practice of Constructivism in Science Education, pp 3-21, Lawrence-Erlbaum, Hillsdale, NJ.";$string['exampleurl'] = "http://www.example.com/somedirectory/somefile.html";$string['filename'] = "ファイル名";$string['fulltext'] = "フルテキスト";$string['htmlfragment'] = "HTMLフラグメント";$string['modulename'] = "資料";$string['modulenameplural'] = "資料";$string['neverseen'] = "未確認";$string['note'] = "ノート";$string['notefile'] = "更にファイルをアップロード(このリストに表示されます)する場合、<A HREF=\$a >File Manager</A> を利用してください。";$string['notypechosen'] = "タイプを選択してください。「戻る」ボタンを押して再度入力してください。";$string['resourcetype'] = "資料のタイプ";$string['resourcetype1'] = "参考文献";$string['resourcetype2'] = "ウェブページ";$string['resourcetype3'] = "ファイル";$string['resourcetype4'] = "プレインテキスト";$string['resourcetype5'] = "リンク";$string['resourcetype6'] = "HTMLテキスト";?>
+<?PHP // $Id$ 
+      // resource.php - created with Moodle 1.0.6.4 (2002112400)
+
+
+$string['addresource'] = "資料を追加する";
+$string['editingaresource'] = "資料を編集する";
+$string['example'] = "例";
+$string['examplereference'] = "Tobin, K. & Tippins, D (1993) Constructivism as a Referent for Teaching and Learning. In: K. Tobin (Ed) The Practice of Constructivism in Science Education, pp 3-21, Lawrence-Erlbaum, Hillsdale, NJ.";
+$string['exampleurl'] = "http://www.example.com/somedirectory/somefile.html";
+$string['filename'] = "ファイル名";
+$string['fulltext'] = "フルテキスト";
+$string['htmlfragment'] = "HTMLフラグメント";
+$string['modulename'] = "資料";
+$string['modulenameplural'] = "資料";
+$string['neverseen'] = "未確認";
+$string['note'] = "ノート";
+$string['notefile'] = "更にファイルをアップロード(このリストに表示されます)する場合、<A HREF=\$a >File Manager</A> を利用してください。";
+$string['notypechosen'] = "タイプを選択してください。「戻る」ボタンを押して再度入力してください。";
+$string['resourcetype'] = "資料のタイプ";
+$string['resourcetype1'] = "参考文献";
+$string['resourcetype2'] = "ウェブページ";
+$string['resourcetype3'] = "ファイル";
+$string['resourcetype4'] = "プレインテキスト";
+$string['resourcetype5'] = "リンク";
+$string['resourcetype6'] = "HTMLテキスト";
+
+?>
