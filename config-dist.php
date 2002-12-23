4e042a3ee932a65062a624cb57ae9e493502f5bd
moodler <moodler>
2002-12-23 14:44:32 +0000

    Clearer directions.

--- config-dist.php
+++ config-dist.php
@@ -12,7 +12,7 @@
 // Moodle - Modular Object-Oriented Dynamic Learning Environment         //
 //          http://moodle.com                                            //
 //                                                                       //
-// Copyright (C) 2001-2002  Martin Dougiamas  http://dougiamas.com       //
+// Copyright (C) 2001-2003  Martin Dougiamas  http://dougiamas.com       //
 //                                                                       //
 // This program is free software; you can redistribute it and/or modify  //
 // it under the terms of the GNU General Public License as published by  //
@@ -29,47 +29,61 @@
 ///////////////////////////////////////////////////////////////////////////
 
 
-///////////////////////////////////////////////////////////////////////////
+//=========================================================================
+// 1. DATABASE SETUP
+//=========================================================================
 // First, you need to configure the database where all Moodle data       //
 // will be stored.  This database must already have been created         //
-// and a username/password created to access it.  If you specify mysql   //
-// then Moodle can set up all your tables for you.  If you try to use    //
-// a different database you will need to set up all your tables by hand  //
-// which could be a big job.    See doc/install.html                     //
+// and a username/password created to access it.                         //
+//                                                                       //
+//   mysql      - the prefix is optional, but useful when installing     //
+//                into databases that already contain tables.            //
+//   postgres7  - the prefix is REQUIRED, regardless of whether the      //
+//                database is empty of not.                              //
 
-$CFG->dbtype    = "mysql";     // mysql or postgres7 
-$CFG->dbhost    = "localhost"; // eg localhost 
-$CFG->dbname    = "moodletest";    // eg moodle
-$CFG->dbuser    = "username";
-$CFG->dbpass    = "password";
+$CFG->dbtype    = "mysql";       // mysql or postgres7 (for now)
+$CFG->dbhost    = "localhost";   // eg localhost or db.isp.com 
+$CFG->dbname    = "moodle";      // database name, eg moodle
+$CFG->dbuser    = "username";    // your database username
+$CFG->dbpass    = "password";    // your database password
 
-$CFG->prefix    = "mdl_";      // Prefix value to use for all table names
+$CFG->prefix    = "mdl_";        // Prefix to use for all table names
 
 
-///////////////////////////////////////////////////////////////////////////
+//=========================================================================
+// 2. WEB SITE LOCATION
+//=========================================================================
 // Now you need to tell Moodle where it is located. Specify the full
 // web address where moodle has been installed (without trailing slash)
 
 $CFG->wwwroot   = "http://example.com/moodle";
 
 
-///////////////////////////////////////////////////////////////////////////
+//=========================================================================
+// 3. SERVER FILES LOCATION
+//=========================================================================
 // Next, specify the full OS directory path to this same location
 // For Windows this might be something like "C:\apache\htdocs\moodle"
 
 $CFG->dirroot   = "/web/moodle";
 
 
-///////////////////////////////////////////////////////////////////////////
+//=========================================================================
+// 4. DATA FILES LOCATION
+//=========================================================================
 // Now you need a place where Moodle can save uploaded files.  This
 // directory should be writeable by the web server user (usually 'nobody'
 // or 'apache'), but it should not be accessible directly via the web.
-// On Windows systems you might specify something like "C:\moodledata"
+// - On hosting systems you might need to make sure that your "group" has
+//   no permissions at all, but that "others" have full permissions.
+// - On Windows systems you might specify something like "C:\moodledata"
 
 $CFG->dataroot  = "/home/moodledata";
 
 
-///////////////////////////////////////////////////////////////////////////
+//=========================================================================
+// 5. TROUBLESHOOTING  (most people can just ignore this setting)
+//=========================================================================
 // A very small percentage of servers have a bug which causes HTTP_REFERER
 // not to work.  The symptoms of this are that you fill out the configure
 // form during Moodle setup but when hit save you find yourself on the 
@@ -79,11 +93,14 @@ $CFG->dataroot  = "/home/moodledata";
 $CFG->buggy_referer = false;
 
 
-///////////////////////////////////////////////////////////////////////////
-// To continue the setup, use your web browser to go to your Moodle page //
-///////////////////////////////////////////////////////////////////////////
+//=========================================================================
+// 6. ALL DONE!  To continue your setup, visit your Moodle web page.
+//=========================================================================
+
 
 $CFG->libdir    = "$CFG->dirroot/lib";   // Do not change this
 require("$CFG->libdir/setup.php");       // Do not change this
 
+// MAKE SURE WHEN YOU EDIT THIS FILE THAT THERE ARE NO SPACES, BLANK LINES, 
+// RETURNS, OR ANYTHING ELSE AFTER THE TWO CHARACTERS ON THE NEXT LINE.
 ?>
