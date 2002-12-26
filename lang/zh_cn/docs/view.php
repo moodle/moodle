212f17dd08a3e3f0e3a9b2af723df23640a573c5
coolbit <coolbit>
2002-12-26 17:01:55 +0000

    new docs

---
+++ lang/zh_cn/docs/view.php
@@ -0,0 +1,37 @@
+<?PHP  // $Id$
+
+    require("../config.php");
+
+    optional_variable($id);      // course context
+    require_variable($file);    // file in this directory to view
+
+    $file = clean_filename($file);
+
+    if (!file_exists($file)) {
+        error("404 - File not found");
+    }
+
+
+    if ($id) {
+        if (! $course = get_record("course", "id", $id)) {
+            error("Course is misconfigured");
+        }
+        $strhelp = get_string("help");
+        print_header("$course->shortname: $strhelp", "$course->fullname", 
+                     "<A HREF=\"../course/view.php?id=$course->id\">$course->shortname</A> -> $strhelp");
+    } else {
+        if (! $site = get_site()) {
+            error("Site is misconfigured");
+        }
+        $strdocumentation = get_string("documentation");
+        print_header("$site->shortname: $strhelp", "$site->fullname", 
+                     "<A HREF=\"view.php?file=contents.html\">$strdocumentation</A>");
+        
+    }
+    
+    echo "<BLOCKQUOTE>";
+    include($file);
+    echo "</BLOCKQUOTE>";
+
+?>
+
