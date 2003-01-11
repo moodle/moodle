6b72b9a911f74312266b910f7574cb2d69c5d64d
moodler <moodler>
2003-01-11 13:58:17 +0000

    A small standalone test program to test session support

---
+++ lib/session-test.php
@@ -0,0 +1,22 @@
+<?PHP
+/// This is a tiny standalone diagnostic script to test that sessions 
+/// are working correctly on a given server.  
+///
+/// Just run it from a browser.   The first time you run it will 
+/// set a new variable, and after that it will try to find it again.
+/// The random number is just to prevent browser caching.
+
+session_start();
+
+if (!isset($_SESSION["test"])) {   // First time you call it.
+    echo "<P>No session found - starting a session now.";
+    $_SESSION["test"] = "welcome back!";
+
+} else {                           // Subsequent times you call it
+    echo "<P>Session found (".$_SESSION["test"].")!";
+    echo "<P>Sessions are working correctly</P>";
+}
+
+echo "<P><A HREF=\"session-test.php?random=".rand(1,1000000)."\">Reload this page</A></P>"
+
+?>
