69ae05837c0c855829d051f4d5f011771d003f06
mits <mits>
2003-01-19 12:00:06 +0000

    New translation

--- lang/ja/docs/files.php
+++ lang/ja/docs/files.php
@@ -1,18 +1,16 @@
 <?PHP // $Id$
       // Names of the documentation files
 
-$string['intro.html'] = "Introduction";
-$string['background.html'] = "Background";
-$string['features.html'] = "Features";
-$string['release.html'] = "Release Notes";
-$string['install.html'] = "Installation";
-$string['upgrade.html'] = "Upgrading";
-$string['teacher.html'] = "Teacher Manual";
-$string['developer.html'] = "Developer Manual";
-$string['cvs.html'] = "Using CVS";
-$string['future.html'] = "Future";
-$string['credits.html'] = "Credits";
-$string['licence.html'] = "License";
-
-
+$string['intro.html'] = "イントロダクション";
+$string['background.html'] = "背景";
+$string['features.html'] = "特徴";
+$string['release.html'] = "リリースノート";
+$string['install.html'] = "インストール";
+$string['upgrade.html'] = "アップグレード";
+$string['teacher.html'] = "教師用マニュアル";
+$string['developer.html'] = "開発者用マニュアル";
+$string['cvs.html'] = "CVSの利用";
+$string['future.html'] = "将来";
+$string['credits.html'] = "クレジット";
+$string['licence.html'] = "ライセンス";
 ?>
