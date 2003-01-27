1b27d92ad45384bcc8d10ddfdc6873f76a041753
moodler <moodler>
2003-01-27 02:50:34 +0000

    Old version of this file ... Ray will replace it shortly

---
+++ lang/en/pgassignment.php
@@ -0,0 +1,113 @@
+<?PHP // $Id$ 
+      // pmassignment.php - created by RAK (2002122000)
+
+
+
+$string['absent'] = "Absent";
+$string['acceptedgrades'] = "Accepted grades (\$a)";
+$string['acceptassessment'] = "Are you happy to accept this assessment?";
+$string['addamendassessmentelements'] = "Add/Amend Assessment Elements";
+$string['afterdeadline'] = "\$a after deadline";
+$string['allocatedon'] = "Allocated on";
+$string['allocatesubmissions'] = "Allocate submissions to students";
+$string['allocationsincludegoodandbad'] = "Allocations to each student will include at least one good and one poor assignment";
+$string['allowsubmissions'] = "Allow submissions from students";
+$string['amendassignmentelements'] = "Amend assignment elements";
+$string['analyseassignment'] = "Analyse assignment";
+$string['anonymous'] = "Do peer grading anonymously";
+$string['assignmentawaitinggrading'] = "Assignment awaiting grading (Deadline is \$a)";
+$string['assignmenthasbeengraded'] = "Assignment has been graded";
+$string['awaitingregrading'] = "Awaiting re-grading (on iteration \$a)";
+$string['beforedeadline'] = "\$a before deadline";
+$string['checkallocation'] = "Checking allocation of submissions...";
+$string['deadlineforpeergradingreached'] = "Deadline for peer grading reached";
+$string['deadlineis'] = "Deadline is \$a";
+$string['displayofgrades'] = "When to Display Grades";
+$string['displaygradefromteacher'] = "Display Grade from Teacher";
+$string['doallocation'] = "Do you want to continue with the allocation of assignments to students?";
+
+$string['dontshowgrades'] = "Only show grades after comments have been agreed";
+$string['editingassessmentelements'] = "Editing Assessment Elements";
+$string['element'] = "Assessment Element";
+$string['elementweight'] = "Element Weight";
+$string['feedbackgoeshere'] = "Feedback goes here";
+$string['generalcomment'] = "General comment";
+$string['graded'] = "Grading and feedback by other Students";
+$string['gradedawaitingacceptance'] = "Graded, awaiting acceptance (on iteration \$a)";
+$string['gradedawaitingreply'] = "Graded, awaiting reply (\$a)";
+$string['gradedawaitingreplydeadline'] = "Graded, awaiting reply (Deadline is \$a)";
+$string['gradedon'] = "Graded on \$a";
+$string['gradethiswork'] = "Grade this work";
+$string['gradedby'] = "Graded by \$a";
+$string['gradeddate'] = "Grade work by";
+$string['gradethissubmission'] = "Grade this submission";
+$string['gradingaccepted'] = "Grading accepted after \$a interations";
+$string['gradingacceptedon'] = "Grading accepted on";
+$string['gradingby'] = "Grading by \$a";
+$string['gradinggiventothissubmission'] = "Grading given to this submission";
+$string['gradingdetails'] = "Grading details";
+$string['gradingrepliedtoon'] = "Grading replied to on";
+$string['includeself'] = "Include self in list of graders";
+$string['managingpeerassignment'] = "Managing a Peer Graded Assignment";
+$string['modulename'] = "Peer Graded Assignment";
+$string['modulenameplural'] = "Peer Graded Assignments";
+
+$string['notavailable'] = "This assignment is in preparation, it is not yet available.";
+$string['notenoughmarked'] = "There are not enough marked assignments to ensure that allocations include at least one
+good and one poor assignment";
+
+$string['noteonassessmentelements'] = 
+	"Note that in a peer graded assignment grading is broken down into a number of 'Assessment Elements'.<BR>
+	This makes the grading easier and more consistent. As teacher you must add these Elements before making<BR>
+	the assignment available to students. This is done by clicking on the peer graded assignment and choosing the<BR>
+	first option in the &quot;Managing a Peer Graded Assignment&quot menu.";
+
+$string['notgraded'] = "No grading, only feedback by other Students";
+$string['notyetgraded'] = "Not yet graded";
+$string['notyetassessedby'] = "Not yet assessed by \$a";
+$string['notyetsubmitted'] = "Not yet submitted";
+$string['numberofassessmentelements'] = "Number of Assessment Elements";
+$string['numberofassessors'] = "Number of (Student) assessors for each piece of work";
+$string['numberofstudents'] = "Number of students enrolled on this course: \$a";
+$string['numberofsubmissions'] = "Number of assignments submitted: \$a";
+$string['numbermarked'] = "Number of assignments graded by the Teacher: \$a";
+$string['numberofallocations'] = "Number of assignments to be assessed by each student: \$a";
+$string['numberofallocationsreduced'] = "Number of assignments to be assessed by each student reduced to: \$a";
+$string['peerassessmentsdisabled'] = "Peer assessments disabled";
+$string['peerassessmentsalreadydisabled'] = "Peer assessments already disabled";
+$string['phase'] = "Phase";
+$string['present'] = "Present";
+$string['regradethissubmission'] = "Reassess this submission";
+$string['reply'] = "Reply";
+$string['returntosubmissionpage'] = "Return to submission page";
+$string['savemyassessment'] = "Save my assessment";
+$string['savemyreply'] = "Save my reply";
+$string['scaleyes'] = "2 point Yes/No scale";
+$string['scalepresent'] = "2 point Present/Absent scale";
+$string['scalecorrect'] = "2 point Correct/Incorrect scale";
+$string['scalegood3'] = "3 point Good/Poor scale";
+$string['scaleexcellent4'] = "4 point Excellent/Very Poor scale";
+$string['scaleexcellent5'] = "5 point Excellent/Very Poor scale";
+$string['scaleexcellent7'] = "7 point Excellent/Very Poor scale";
+$string['scale10'] = "Score out of 10";
+$string['scale20'] = "Score out of 20";
+$string['scale100'] = "Score out of 100";
+$string['showgrades'] = "Always show grades with comments";
+$string['showgrading'] = "Show grading";
+$string['showstatus'] = "Show status of the peer grading";
+$string['specimengradingform'] = "Specimen Grading Form";
+$string['submittedby'] = "Submitted by \$a";
+$string['submissiondate'] = "Submit work by";
+$string['submissionsalreadyallowed'] = "Submissions already allowed";
+$string['submissionsawaitinggrading'] = "Submissions awaiting grading (\$a)";
+$string['submissionsawaitingregrading'] = "Submissions awaiting re-grading (\$a)";
+$string['thecurrentgradeis'] = "The current grade is";
+$string['thegradegiventothissubmissionis'] = "The Grade given to this submission is";
+$string['thegradeis'] = "The Grade is";
+$string['typeofscale'] = "Type of scale";
+$string['warningonamendingelements'] = "WARNING: There are submitted assessments. <BR>Do NOT change the number
+ of elements, the scale types or the element weights.";
+$string['workbeingassessed'] = "Work being assessed by you";
+$string['yourworkbeingassessed'] = "Your work being assessed by other";
+
+?>
