98a792faaf70a517c50719ad22aedaaff8f59c08
compuproggy <compuproggy>
2003-05-09 05:58:22 +0000

    *** empty log message ***

--- lang/de/docs/files.php
+++ lang/de/docs/files.php
@@ -1,19 +1,20 @@
 <?PHP // $Id$
       // Names of the documentation files
 
-$string['intro.html'] = "Introduction";
-$string['background.html'] = "Background";
+$string['intro.html'] = "Einführung";
+$string['background.html'] = "Hintergrund";
 $string['features.html'] = "Features";
 $string['release.html'] = "Release Notes";
 $string['install.html'] = "Installation";
-$string['upgrade.html'] = "Upgrading";
+$string['upgrade.html'] = "Upgrade";
 $string['faq.html'] = "FAQ";
-$string['teacher.html'] = "Teacher Manual";
-$string['developer.html'] = "Developer Manual";
-$string['cvs.html'] = "Using CVS";
-$string['future.html'] = "Future";
-$string['credits.html'] = "Credits";
-$string['licence.html'] = "License";
+$string['teacher.html'] = "Lehrer-Anleitung";
+$string['developer.html'] = "Entwickler-Anleitung";
+$string['cvs.html'] = "Nutzung von CVS";
+$string['future.html'] = "Zukunft";
+$string['credits.html'] = "Danksagungen";
+$string['licence.html'] = "Licenz";
 
 
 ?>
+
