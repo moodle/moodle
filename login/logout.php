eab79dc13deae43187c399aa5dbe628b60f48aad
moodler <moodler>
2003-05-17 04:45:52 +0000

    Hopefully this will fix the logout problems - it now specifically checks for PHP 4.3.0 or later, and also does the unset anyway.
    Sessions are not consistent in PHP versions.

--- login/logout.php
+++ login/logout.php
@@ -3,14 +3,15 @@
 
     require_once("../config.php");
 
-    if (ini_get_bool("register_globals")) {
-        // This method is to try to avoid silly warnings from PHP 4.3.0
-        session_unregister("SESSION");
+    if (ini_get_bool("register_globals") and check_php_version("4.3.0")) {
+        // This method is just to try to avoid silly warnings from PHP 4.3.0
         session_unregister("USER");
-    } else {
-        unset($_SESSION['USER']);
-        unset($_SESSION['SESSION']);
+        session_unregister("SESSION");
     }
+
+    unset($_SESSION['USER']);
+    unset($_SESSION['SESSION']);
+
     redirect($CFG->wwwroot);
 
 ?>
