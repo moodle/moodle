3c382acc280d03335150e929ef384ba7d058c6b4
mits <mits>
2003-05-24 11:51:01 +0000

    New translation

---
+++ lang/ja/workshop.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // workshop.php - created with Moodle 1.0.9 Beta 1 + (2003052300)
+
+
+$string['delete'] = "削除";
+$string['description'] = "詳細";
+$string['edit'] = "編集";
+$string['editacomment'] = "コメントの編集";
+$string['element'] = "要素";
+$string['maximumsize'] = "最大サイズ";
+$string['modulename'] = "ワークショップ";
+$string['modulenameplural'] = "ワークショップ";
+$string['reply'] = "返事";
+$string['title'] = "題名";
+
+?>
