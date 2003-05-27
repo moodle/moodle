576fd161a0714be35e7256c24fa34b6b4afc211b
mits <mits>
2003-05-27 14:57:05 +0000

    Mod

--- lang/ja/docs/files.php
+++ lang/ja/docs/files.php
@@ -6,6 +6,7 @@ $string['background.html'] = "背景";
 $string['features.html'] = "特徴";
 $string['release.html'] = "リリースノート";
 $string['install.html'] = "インストール";
+$string['faq.html'] = "インストール FAQ";
 $string['upgrade.html'] = "アップグレード";
 $string['teacher.html'] = "教師用マニュアル";
 $string['developer.html'] = "開発者用マニュアル";
@@ -13,4 +14,6 @@ $string['cvs.html'] = "CVSの利用";
 $string['future.html'] = "将来";
 $string['credits.html'] = "クレジット";
 $string['licence.html'] = "ライセンス";
+
+
 ?>
