02fa0aa3b0d1201cae06ab7fca05bc20a3010beb
stronk7 <stronk7>
2003-06-10 15:43:58 +0000

    Up a bit

--- backup/backup_version.php
+++ backup/backup_version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2003060100;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2003061000;   // The current version is a date (YYYYMMDDXX)
 
-$backup_release = "0.5.0 alpha";  // User-friendly version number
+$backup_release = "0.5.5 alpha";  // User-friendly version number
