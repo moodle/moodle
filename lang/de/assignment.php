097a5aff67cb641be496d40f766cde7bb03e6b0f
compuproggy <compuproggy>
2003-06-12 06:07:42 +0000

    New German Strings added

--- lang/de/assignment.php
+++ lang/de/assignment.php
@@ -1,7 +1,17 @@
-<?php
+<?PHP // $Id$ 
+      // assignment.php - created with Moodle 1.0.8 dev (2002122301)
+
 
 $string['allowresubmit'] = "Erneutes Einreichen erlauben";
 $string['assignmentdetails'] = "Aufgabendetails";
+$string['assignmentmail'] = "\$a->teacher hat einen Kommentar zu Ihrer eingereichten Aufgabe '\$a->assignment' abgegeben.
+
+Sie können diesen angehängt an Ihre eingereichte Aufgabe sehen:
+
+\$a->url";
+$string['assignmentmailhtml'] = "\$a->teacher hat einen Kommentar zu Ihrer eingereichten Aufgabe </i>'<br /><br />
+Sie können diesen angehängt an Ihre
+<a href=\\\"\$a->url\\\">eingereichte Aufgabe</a> sehen:.";
 $string['assignmentname'] = "Aufgabenname";
 $string['assignmenttype'] = "Aufgabentyp";
 $string['description'] = "Beschreibung ";
@@ -31,11 +41,10 @@ $string['uploaderror'] = "Beim Hochladen der Datei trat ein Fehler auf";
 $string['uploadfailnoupdate'] = "Die Datei wurde korrekt hochgeladen, aber Ihr Eintrag kann nicht aktualisiert werden!";
 $string['uploadfiletoobig'] = "Entschuldigung, aber diese Datei ist zu groß (Die Begrenzung ist \$a Bytes)";
 $string['uploadnofilefound'] = "Es wurde keine Datei gefunden. Sind Sie sicher, dass Sie eine für das Hochladen ausgewählt haben?";
-$string['uploadnotregistered'] = "\'\$a\' wurde korrekt hochgeladen, aber der Eintrag wurde nicht registriert!";
-$string['uploadsuccess'] = "\'\$a\' wurde erfolgreich hochgeladen";
+$string['uploadnotregistered'] = "'\$a' wurde korrekt hochgeladen, aber der Eintrag wurde nicht registriert!";
+$string['uploadsuccess'] = "'\$a' wurde erfolgreich hochgeladen";
 $string['viewfeedback'] = "Aufgabenbewertung und Rückmeldung anzeigen ";
 $string['viewsubmissions'] = "Zeige \$a eingereichte Aufgaben ";
 $string['yoursubmission'] = "Ihre eingereichten Aufgaben";
-
 
 ?>
