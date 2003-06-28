546431e9b8427b5b257976dc5fb66371a25b3803
stronk7 <stronk7>
2003-06-28 23:24:51 +0000

    Now categories and questions are recovered completely (STEP1) Go up a bit!!    :-)

--- backup/backup_version.php
+++ backup/backup_version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2003062400;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2003062900;   // The current version is a date (YYYYMMDDXX)
 
-$backup_release = "0.6.3 alpha";  // User-friendly version number
+$backup_release = "0.6.4 alpha";  // User-friendly version number
