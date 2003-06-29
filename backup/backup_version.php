4f93ac2254539b10e530f48ef1c5e6e82e001445
stronk7 <stronk7>
2003-06-29 23:03:49 +0000

    Update version to 0.7 alpha Changes is backup to avoid duplicate tag names !! Broken backup/restore compatibility with previous versions

--- backup/backup_version.php
+++ backup/backup_version.php
@@ -5,6 +5,7 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2003062900;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2003063003;   // The current version is a date (YYYYMMDDXX)
 
-$backup_release = "0.6.4 alpha";  // User-friendly version number
+$backup_release = "0.7.0 alpha <font color=red>(Previous backup compatibility broken !!)</font><p align=center>
+                   <a href=\"http://moodle.org/bugs/bug.php?op=show&bugid=84\">See Bug 84</a>";  // User-friendly version number
