1bcb0d8700b2f19076810bb6889408756fd60e15
moodler <moodler>
2003-07-07 09:46:00 +0000

    Strings

--- lang/en/chat.php
+++ lang/en/chat.php
@@ -8,6 +8,7 @@ $string['modulenameplural'] = "Chats";
 $string['chatintro'] = "Introduction text";
 $string['chatname'] = "Name of this chat room";
 $string['enterchat'] = "Click here to enter the chat";
+$string['idle'] = "Idle";
 $string['messageenter'] = "\$a has just entered this chat";
 $string['savemessages'] = "Number of messages to save";
 $string['strftimemessage'] = "%%H:%%M";
