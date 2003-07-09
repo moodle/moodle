e0f96309a4f4082756ef4867f5bccb7978ea0df9
compuproggy <compuproggy>
2003-07-09 06:32:50 +0000

    *** empty log message ***

---
+++ lang/de/chat.php
@@ -0,0 +1,17 @@
+<?PHP // $Id$
+
+#------------------------------------------------------------
+$string['modulename'] = "Chat";
+$string['modulenameplural'] = "Chats";
+#------------------------------------------------------------
+
+$string['chatintro'] = "Introduction text";
+$string['chatname'] = "Name of this chat room";
+$string['enterchat'] = "Click here to enter the chat";
+$string['idle'] = "Idle";
+$string['messageenter'] = "\$a has just entered this chat";
+$string['messageexit'] = "\$a has left this chat";
+$string['savemessages'] = "Number of messages to save";
+$string['strftimemessage'] = "%%H:%%M";
+
+?>
