0fa09c7a49ded39bb792055a27ee357722b5bbfb
giro123 <giro123>
2003-07-15 18:17:52 +0000

    en 1.5

---
+++ lang/es/chat.php
@@ -0,0 +1,20 @@
+<?PHP // $Id$ 
+      // chat.php - created with Moodle 1.0.9 Beta 1 + (2003051700)
+
+
+$string['beep'] = "beep";
+$string['chatintro'] = "Texto introductorio";
+$string['chatname'] = "Nombre de la sala ";
+$string['currentchats'] = "Salas activas";
+$string['enterchat'] = "Entrar a la sala";
+$string['idle'] = "inactivo";
+$string['messagebeepseveryone'] = "\$a beeps a alguien";
+$string['messagebeepsyou'] = "\$a le acaba de enviar un beep";
+$string['messageenter'] = "\$a entró a la sala";
+$string['messageexit'] = "\$a salió de la sala";
+$string['modulename'] = "Chat";
+$string['modulenameplural'] = "Chats";
+$string['savemessages'] = "Número de mensajes a guardar";
+$string['strftimemessage'] = "%%H:%%M";
+
+?>
