e61611898a75693bf69fa10956459122e4364a94
giro123 <giro123>
2003-07-27 05:28:31 +0000

    *** empty log message ***

--- lang/es/chat.php
+++ lang/es/chat.php
@@ -5,8 +5,13 @@
 $string['beep'] = "beep";
 $string['chatintro'] = "Texto introductorio";
 $string['chatname'] = "Nombre de la sala ";
+$string['chatreport'] = "Sesiones";
+$string['chattime'] = "Próxima cita";
 $string['currentchats'] = "Salas activas";
+$string['currentusers'] = "usuarios ";
+$string['donotusechattime'] = "No publicar tiempo de chat";
 $string['enterchat'] = "Entrar a la sala";
+$string['errornousers'] = "¡No puedo encontrar usuarios!";
 $string['idle'] = "inactivo";
 $string['messagebeepseveryone'] = "\$a beeps a alguien";
 $string['messagebeepsyou'] = "\$a le acaba de enviar un beep";
@@ -14,7 +19,18 @@ $string['messageenter'] = "\$a entró a la sala";
 $string['messageexit'] = "\$a salió de la sala";
 $string['modulename'] = "Chat";
 $string['modulenameplural'] = "Chats";
+$string['neverdeletemessages'] = "Nunca borrar mensajes";
+$string['nextsession'] = "Próxima sesión agendada";
+$string['nomessages'] = "Aún no hay mensajes";
+$string['repeatdaily'] = "A la misma hora todos los días";
+$string['repeatnone'] = "Sin repeticiones, sólo la fecha especificada";
+$string['repeattimes'] = "Repetir sesiones";
+$string['repeatweekly'] = "A la misma hora todos los días";
 $string['savemessages'] = "Número de mensajes a guardar";
+$string['seesession'] = "Ver la sesión";
+$string['sessions'] = "Sesiones";
 $string['strftimemessage'] = "%%H:%%M";
+$string['studentseereports'] = "Todos pueden ver las sesioens pasadas";
+$string['viewreport'] = "Ver las sesiones anteriores";
 
 ?>
