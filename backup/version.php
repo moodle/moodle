2c54aa080b254bf4191750692481f35d3b7c8a47
stronk7 <stronk7>
2003-07-31 16:55:11 +0000

    Copied from old backup_version.php (to be more consistent with the rest of the modules)

---
+++ backup/version.php
@@ -0,0 +1,10 @@
+<?PHP  //$Id$
+// This file defines the current version of the
+// backup/restore code that is being used.  This can be
+// compared against the values stored in the 
+// database (backup_version) to determine whether upgrades should
+// be performed (see db/backup_*.php)
+
+$backup_version = 2003073100;   // The current version is a date (YYYYMMDDXX)
+
+$backup_release = "0.8.4 alpha";  // User-friendly version number
