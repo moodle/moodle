41586fa0ca725e2d5525cb8714ee983a6c0abd2b
moodler <moodler>
2003-08-26 07:00:20 +0000

    Bump - looks pretty good now

--- version.php
+++ version.php
@@ -5,9 +5,9 @@
 // database to determine whether upgrades should
 // be performed (see lib/db/*.php)
 
-$version = 2003082300;   // The current version is a date (YYYYMMDDXX)
+$version = 2003082600;   // The current version is a date (YYYYMMDDXX)
 
-$release = "1.1 Beta +";   // User-friendly version number
+$release = "1.1 Beta ++";   // User-friendly version number
 
 
 ?>
