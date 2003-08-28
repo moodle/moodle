04319b3c1c48f33200598a7729df56112619f56a
moodler <moodler>
2003-08-28 16:19:18 +0000

    Not quite ready, but anyone trying the Beta at this stage will probably not need to upgrade.

--- version.php
+++ version.php
@@ -7,7 +7,7 @@
 
 $version = 2003082800;   // The current version is a date (YYYYMMDDXX)
 
-$release = "1.1 -";   // User-friendly version number
+$release = "1.1";   // User-friendly version number
 
 
 ?>
