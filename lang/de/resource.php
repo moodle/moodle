88cc4a04007fe6516865dd7b4dfc9cfbe859f32b
danielfetzer <danielfetzer>
2003-08-29 14:50:10 +0000

    *** empty log message ***

--- lang/de/resource.php
+++ lang/de/resource.php
@@ -1,12 +1,16 @@
-<?PHP // $Id$ 
+<?PHP // $Id$
       // resource.php - created with Moodle 1.0.8 dev (2002122301)
 
 
 $string['addresource'] = "Literatur hinzufügen";
+$string['configframesize'] = "Wenn eine Webseite oder eine hochgeladene Datei in einem Frame angezeigt wird, so gibt dieser Wert die Größe des Top-Frames, der die Navigation beinhaltet, in Pixeln an.";
+
 $string['editingaresource'] = "Literatur bearbeiten";
 $string['example'] = "Beispiel";
 $string['examplereference'] = "Stefan Münz,
+
 SelfHTML
+
 ";
 $string['exampleurl'] = "http://selfhtml.teamone.de";
 $string['filename'] = "Dateiname";
@@ -28,7 +32,8 @@ $string['newwidth'] = "Standard Fensterbreite (in Pixeln)";
 $string['newwindow'] = "Neues Fenster";
 $string['newwindowopen'] = "Diese Literatur in einem neuen Popup-Fenster anzeigen";
 $string['note'] = "Anmerkung";
-$string['notefile'] = "Um mehr als eine Datei für diesen Kurs hochzuladen (damit dieses in der Liste erscheinen) benutzen Sie bitte den 
+$string['notefile'] = "Um mehr als eine Datei für diesen Kurs hochzuladen (damit dieses in der Liste erscheinen) benutzen Sie bitte den
+
 <A HREF=\$a >Datei-Manager</A>.";
 $string['notypechosen'] = "Sie müssen einen Typ auswählen. Verwenden Sie den Zurück-Knopf und probieren Sie es erneut";
 $string['resourcetype'] = "Typ der Quelle";
