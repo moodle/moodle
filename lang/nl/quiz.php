afb0e2758361422d0ebd5f3540e3e2aa954ba115
koenr <koenr>
2003-09-07 19:37:57 +0000

    leraars replaced with leraren

--- lang/nl/quiz.php
+++ lang/nl/quiz.php
@@ -1,161 +1,328 @@
 <?PHP // $Id$ 
+
       // quiz.php - created with Moodle 1.1 Beta (2003082001)
 
 
+
+
+
 $string['acceptederror'] = "Geaccepteerde fout";
-$string['addingquestions'] = "Aan deze kant van de pagina kun je je vragenlijst beheren. De vragen worden bewaard per categorie om je te helpen ze te ordenen. Ze kunnen gebruikt worden in elke test van je vak of zelfs in andere vakken als je ze wil 'publiceren'.
-<br /><br />Pas wanneer je een categorie gekozen of gemaakt hebt, kun je vragen maken of bewerken.
+
+$string['addingquestions'] = "Aan deze kant van de pagina kun je je vragenlijst beheren. De vragen worden bewaard per categorie om je te helpen ze te ordenen. Ze kunnen gebruikt worden in elke test van je vak of zelfs in andere vakken als je ze wil 'publiceren'.
+
+
+<br /><br />Pas wanneer je een categorie gekozen of gemaakt hebt, kun je vragen maken of bewerken.
+
+
 Je kunt gelijk welke van die vragen selecteren om ze toe te voegen bij je test aan de andere kant van deze pagina.";
+
 $string['addquestions'] = "Voeg vragen toe";
+
 $string['addquestionstoquiz'] = "Voeg vragen aan de huidige test toe";
+
 $string['addselectedtoquiz'] = "Voeg het geselecteerde toe aan de test";
+
 $string['allowreview'] = "Sta nalezen toe";
+
 $string['alreadysubmitted'] = "Waarschijnlijk heb je deze poging al ingestuurd";
+
 $string['alwaysavailable'] = "Altijd beschikbaar ";
+
 $string['answer'] = "Antwoord";
+
 $string['answerhowmany'] = "Eén of meerdere antwoorden";
+
 $string['answersingleno'] = "Meerdere antwoorden toegestaan";
+
 $string['answersingleyes'] = "Slechts één antwoord";
+
 $string['answerswithacceptederrormarginmustbenumeric'] = "Antwoorden met een geaccepteerde fout moeten numeriek zijn";
+
 $string['attempt'] = "Poging \$a";
+
 $string['attemptfirst'] = "Eerste poging";
+
 $string['attemptlast'] = "Laatste poging";
+
 $string['attemptquiznow'] = "Probeer de test nu";
+
 $string['attempts'] = "Pogingen";
+
 $string['attemptsallowed'] = "Toegestane pogingen ";
+
 $string['attemptsunlimited'] = "Onbeperkt aantal pogingen";
+
 $string['backtoquiz'] = "Terug naar het bewerken/wijzigen van de test";
+
 $string['bestgrade'] = "Hoogste cijfer";
+
 $string['blackboard'] = "Blackboard";
+
 $string['caseno'] = "Nee, hoofd/kleine letters zijn niet belangrijk";
+
 $string['casesensitive'] = "Gevoeligheid voor hoofd/kleine letters";
+
 $string['caseyes'] = "Ja, hoofd/kleine letters moeten exact hetzelfde zijn";
+
 $string['categories'] = "Categorieën  ";
+
 $string['category'] = "Categorie";
+
 $string['categoryinfo'] = "Info over de categorie";
+
 $string['categorymove'] = "De categorie '\$a->name' bevat \$a->count vragen. Kies alsjeblieft een categorie om deze vragen naar te verplaatsen";
+
 $string['categorymoveto'] = "Verplaats ze naar deze categorie";
+
 $string['choice'] = "Keuzemogelijkheid";
+
 $string['choices'] = "Mogelijke keuzes";
+
 $string['correctanswer'] = "Het goede antwoord";
+
 $string['correctanswers'] = "Goede antwoorden";
+
 $string['countdown'] = "Aftellen";
+
 $string['countdownfinished'] = "De test gaat dicht. Je moet nu je antwoorden insturen.";
+
 $string['countdowntenminutes'] = "De test gaat over tien minuten dicht.";
+
 $string['createmultiple'] = "Maak meerdere vragen";
+
 $string['createnewquestion'] = "Maak een nieuwe vraag";
+
 $string['custom'] = "Aangepast formaat";
+
 $string['daysavailable'] = "Dagen beschikbaar";
+
 $string['default'] = "Standaard";
+
 $string['defaultgrade'] = "Standaard beoordeling vraag";
+
 $string['defaultinfo'] = "De standaardcategorie voor vragen.";
+
 $string['deletequestioncheck'] = "Weet je heel zeker dat je '\$a' wilt verwijderen?";
+
 $string['description'] = "Beschrijving";
+
 $string['eachattemptbuildsonthelast'] = "Elke poging bouwt voort op de vorige";
+
 $string['editcategories'] = "Bewerk de categorieën ";
+
 $string['editingdescription'] = "Een beschrijving bewerken";
+
 $string['editingmatch'] = "Een paren vraag bewerken";
+
 $string['editingmultianswer'] = "Ingebedde antwoorden (Cloze) bewerken";
+
 $string['editingmultichoice'] = "Een multiple choice vraag bewerken";
+
 $string['editingnumerical'] = "Een numerieke vraag bewerken";
+
 $string['editingquestion'] = "Een vraag bewerken";
+
 $string['editingquiz'] = "Bewerk test";
+
 $string['editingrandom'] = "Een willekeurige vraag bewerken";
+
 $string['editingrandomsamatch'] = "Een willekeurige vraag met kort antwoord bewerken";
+
 $string['editingshortanswer'] = "Een vraag met kort antwoord bewerken";
+
 $string['editingtruefalse'] = "Een waar/onwaar vraag bewerken";
+
 $string['false'] = "Onwaar";
+
 $string['feedback'] = "Feedback";
+
 $string['fileformat'] = "Bestandsformaat";
+
 $string['filloutoneanswer'] = "Je moet minstens één mogelijk antwoord invullen. Lege antwoorden zullen niet worden gebruikt.";
+
 $string['filloutthreequestions'] = "Je moet minstens drie vragen invullen. Leeg gelaten vragen zullen niet worden gebruikt.";
+
 $string['fillouttwochoices'] = "Je moet minstens twee keuzemogelijkheden invullen. Lege keuzemogelijkheden zullen niet worden gebruikt.";
-$string['fractionsaddwrong'] = "De positieve cijfers die je hebt gekozen tellen niet op tot 100%%
-<BR>In plaats daarvan tellen ze op tot \$a%%
+
+$string['fractionsaddwrong'] = "De positieve cijfers die je hebt gekozen tellen niet op tot 100%%
+
+
+<BR>In plaats daarvan tellen ze op tot \$a%%
+
+
 <BR>Wil je terug gaan en deze vraag verbeteren?";
-$string['fractionsnomax'] = "Eén van de antwoorden moet 100%% zijn, zodat het mogelijk is
-<BR>om een vol punt voor deze vraag te krijgen.
+
+$string['fractionsnomax'] = "Eén van de antwoorden moet 100%% zijn, zodat het mogelijk is
+
+
+<BR>om een vol punt voor deze vraag te krijgen.
+
+
 <BR>Wil je terug gaan en deze vraag verbeteren?";
+
 $string['gradeaverage'] = "Gemiddelde cijfer";
+
 $string['gradehighest'] = "Hoogste cijfer";
+
 $string['grademethod'] = "Beoordelingsmethode";
+
 $string['grades'] = "Beoordelingen";
+
 $string['guestsno'] = "Sorry, gasten kunnen testen niet bekijken noch een poging wagen";
+
 $string['imagedisplay'] = "Weer te geven plaatje";
+
 $string['importquestions'] = "Importeer vragen uit een bestand";
+
 $string['introduction'] = "Inleiding";
+
 $string['marks'] = "Punten";
+
 $string['match'] = "Koppelen";
+
 $string['matchanswer'] = "Gepaarde antwoord";
+
 $string['missingcorrectanswer'] = "Het goede antwoord moet worden aangegeven";
+
 $string['missingname'] = "Naam voor de vraag ontbreekt";
+
 $string['missingquestiontext'] = "Tekst voor de vraag ontbreekt";
-$string['missingword'] = "Woordopmaak ontbreekt";
+
+$string['missingword'] = "Gatentekst";
+
 $string['modulename'] = "Test";
+
 $string['modulenameplural'] = "Testen";
+
 $string['multianswer'] = "Ingebedde antwoorden (Cloze)";
+
 $string['multichoice'] = "Multiple choice";
+
 $string['noanswers'] = "Er zijn geen antwoorden geselecteerd!";
+
 $string['noattempts'] = "Er zijn geen pogingen geweest om deze test te maken";
+
 $string['nomoreattempts'] = "Een nieuwe poging is niet meer toegestaan";
+
 $string['noquestions'] = "Er zijn nog geen vragen toegevoegd";
+
 $string['noreview'] = "Het is niet toegestaan om de antwoorden na te lezen en te verbeteren";
+
 $string['noreviewuntil'] = "Het is niet toegestaan om deze test na te lezen tot \$a";
+
 $string['notenoughsubquestions'] = "Er zijn onvoldoende deelvragen gedefinieerd!<br>Wil je teruggaan en deze vraag herstellen?";
+
 $string['numerical'] = "Numeriek";
+
 $string['publish'] = "Publiceer  ";
+
 $string['qti'] = "IMS QTI opmaak";
+
 $string['question'] = "Vraag";
+
 $string['questioninuse'] = "De vraag '\$a' is op dit moment al in gebruik:";
+
 $string['questionname'] = "Naam vraag";
+
 $string['questions'] = "Vragen";
+
 $string['quizavailable'] = "De test is beschikbaar tot: \$a";
+
 $string['quizclose'] = "Sluit de test";
+
 $string['quizclosed'] = "Deze test  wordt op \$a gesloten";
+
 $string['quizcloses'] = "Test gaat dicht";
+
 $string['quiznotavailable'] = "De test zal nog niet beschikbaar zijn tot :  \$a  ";
+
 $string['quizopen'] = "Open de test ";
+
 $string['random'] = "Willekeurige set";
+
 $string['randomcreate'] = "Maak willekeurige vragen aan";
+
 $string['randomsamatch'] = "Willekeurig korte antwoorden paren";
+
 $string['randomsamatchcreate'] = "Maak willekeurige korte antwoorden paren vragen aan";
+
 $string['randomsamatchintro'] = "Selecteer voor elk van de volgende vragen een passend antwoord (als paar) uit het menu.";
+
 $string['randomsamatchnumber'] = "Aan te selecteren vragen";
+
 $string['readytosend'] = "Je staat op het punt om de test in te sturen zodat hij beoordeeld kan worden. Weet je zeker dat je door wilt gaan?";
+
 $string['recentlyaddedquestion'] = "Recent toegevoegde vraag!";
+
 $string['regrade'] = "Herbereken alle pogingen";
+
 $string['regradecomplete'] = "Alle pogingen zijn herberekend";
+
 $string['regradecount'] = "\$a->changed van de \$a->attempt cijfers zijn gewijzigd  ";
+
 $string['rename'] = "Hernoem";
+
 $string['report'] = "Rapporten";
+
 $string['reportoverview'] = "Overzicht";
+
 $string['reportregrade'] = "Herwaarderen van de pogingen";
+
 $string['reportsimplestat'] = "Eenvoudige statistieken";
+
 $string['review'] = "Lees na";
+
 $string['save'] = "Bewaar";
+
 $string['savegrades'] = "Bewaar cijfers";
+
 $string['savemyanswers'] = "Bewaar mijn antwoorden";
+
 $string['savequiz'] = "Bewaar deze hele test";
+
 $string['score'] = "Ruwe score";
+
 $string['select'] = "Selecteer";
+
 $string['selectall'] = "Selecteer alle";
+
 $string['selectcategoryabove'] = "Selecteer één van de bovenstaande categorieën";
+
 $string['shortanswer'] = "Kort antwoord";
+
 $string['show'] = "Laat zien";
+
 $string['showcorrectanswer'] = "In de feedback, de juiste antwoorden laten zien?";
+
 $string['showfeedback'] = "Feedback laten zien na het beantwoorden?";
+
 $string['shuffleanswers'] = "Mix de antwoorden";
+
 $string['shufflequestions'] = "Mix de vragen";
+
 $string['time'] = "Tijd  ";
+
 $string['timecompleted'] = "Voltooid";
+
 $string['timetaken'] = "Gebruikte tijd";
+
 $string['toomanyrandom'] = "Er zijn meer willekeurige vragen vereist, dan er in deze categorie zitten! (\$a)";
+
 $string['true'] = "Waar";
+
 $string['truefalse'] = "Waar/Onwaar";
+
 $string['type'] = "Soort";
+
 $string['viewallanswers'] = "Bekijk \$a voltooide testen";
+
 $string['viewallreports'] = "Bekijk overzicht van \$a pogingen";
+
 $string['webct'] = "WebCT opmaak";
+
 $string['yourfinalgradeis'] = "Je eindcijfer voor deze test is \$a";
 
+
+
 ?>
+
