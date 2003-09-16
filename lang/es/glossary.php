20e62af383f1c18dae2075e4d973e2d876cec58e
moodler <moodler>
2003-09-16 03:06:10 +0000

    Spanish language files for new Glossary module

---
+++ lang/es/glossary.php
@@ -0,0 +1,40 @@
+<?PHP // $Id$ 
+      // glossary.php - created with Moodle 1.1 Beta (2003082001)
+
+$string['addentry'] = "Agregar definición";
+$string['allentries'] = "TODOS";
+$string['allowduplicatedentries'] = "Permitir definiciones duplicadas?";
+$string['areyousuredelete'] = "Está seguro que desea eliminar esta definición?";
+$string['canstudentpost'] = "Los estudiantes podrán agregar definiciones?";
+$string['concept'] = "Concepto";
+$string['concepts'] = "Conceptos";
+$string['definition'] = "Definición";
+$string['definitions'] = "Definiciones";
+$string['deleteentry'] = "Eliminar definición";
+$string['displayformat'] = "Formato de visualización";
+$string['displayformatwouser'] = "Amplio, sin datos del autor.";
+$string['displayformatwuser'] = "Amplio, con datos del autor";
+$string['displayformatsimple'] = "Sencillo, estilo diccionario";
+$string['dontallowduplicates'] = "No, no permitir duplicados.";
+$string['editentry'] = "Editar definición";
+$string['entryalreadyexist'] = "Esta entrada ya existe.";
+$string['entrydeleted'] = "Definición eliminada.";
+$string['entryexported'] = "Concepto exportado exitosamente.";
+$string['exporttomainglossary'] = "Exportar al glosario principal.";
+$string['isthisthemainglossary'] = "Es este el glosario principal del curso?";
+$string['itisthemainglossary'] = "Si, este es el principal.";
+$string['itisntthemainglossary'] = "No, este no el principal.";
+$string['modulename'] = "Glosario";
+$string['modulenameplural'] = "Glosarios";
+$string['newglossaryentries'] = "Nuevos conceptos de glosario:";
+$string['noentries'] = "No existen definiciones en esta sección";
+$string['noentry'] = "No existe ninguna definición.";
+$string['selectletter'] = "Seleccione la letra para la cual desea ver las entradas del glosario.<p><b>Especial</b> le mostrará todos los términos que comienzan con carácteres no alfabeticos<br><b>TODOS</b> Mostrará todas las entradas en una sóla página.";
+$string['searchconcept'] = "Buscar concepto:";
+$string['searchindefinition'] = "Buscar en concepto Y en definición?";
+$string['special'] = "Especial";
+$string['studentcanpost'] = "Si, los estudiantes podrán agregar definiciones";
+$string['studentcantpost'] = "No, los estudiantes no podrán agregar definiciones";
+$string['writtenby'] = "Por";
+$string['yesallowduplicates'] = "Si, permitir duplicados.";
+?>
