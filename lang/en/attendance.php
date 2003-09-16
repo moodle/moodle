48769c6e2d71faaf20c9c593348e91e0e2686e9b
moodler <moodler>
2003-09-16 05:32:16 +0000

    Language file for new Attendance module, by Russell Jungwirth

---
+++ lang/en/attendance.php
@@ -0,0 +1,39 @@
+<?PHP 
+
+#------------------------------------------------------------
+$string['modulename'] = "Attendance";
+$string['allmodulename'] = "All Attendance Rolls";
+$string['modulenameplural'] = "Attendance Rolls";
+#------------------------------------------------------------
+
+
+$string['takeroll'] = "Take roll on this day?";
+$string['for'] = "for";
+$string['dayofroll'] = "Date to take roll for";
+$string['notes'] = "Exceptional information";
+$string['notesfor'] = "Exceptional information for";
+$string['hoursinclass'] = "Number of Hours in a Class Period";
+$string['presentshort'] = "P";
+$string['tardyshort'] = "T";
+$string['absentshort'] = "A";
+$string['presentlong'] = "Present";
+$string['tardylong'] = "Tardy";
+$string['absentlong'] = "Absent";
+$string['dynamicsection'] = "Determine week for attendance based on this date";
+$string['defaultdynamicsection'] = "Whether to move attendance rolls to the correct week by default";
+$string['defaulthoursinclass'] = "How many hours there should be in a default attendance roll";
+$string['tardiesforabsence'] = "How many tardies should count as one absence";
+$string['hoursinfullreport'] = "The maximum number of hours of attendance that should be displayed on a single page of a report";
+$string['previous'] = "Previous";
+$string['pages'] = "Pages";
+$string['of'] = "of";
+$string['viewall'] = "View All Attendance Rolls";
+$string['viewone'] = "View All Pages of Report";
+$string['viewtable'] = "View Report as One Table";
+$string['viewmulti'] = "View Report as Multiple Pages";
+$string['noviews'] = "Sorry, there is no data for your account here";
+
+
+
+
+?>
