88208963382278487b3bccbca71f733004f436e9
moodler <moodler>
2003-09-16 03:04:32 +0000

    Language files for Glossary module - help files in Spanish still!

---
+++ lang/en/glossary.php
@@ -0,0 +1,40 @@
+<?PHP // $Id$ 
+      // glossary.php - created with Moodle 1.1 Beta (2003082001)
+
+$string['addentry'] = "Add entry";
+$string['allentries'] = "ALL";
+$string['allowduplicatedentries'] = "Are duplicated entries allowed?";
+$string['areyousuredelete'] = "Are you sure you want to delete this entry?";
+$string['canstudentpost'] = "Can the students add entries?";
+$string['concept'] = "Concept";
+$string['concepts'] = "Concepts";
+$string['definition'] = "Definition";
+$string['definitions'] = "Definitions";
+$string['deleteentry'] = "Delete entry";
+$string['displayformat'] = "Display format?";
+$string['displayformatwouser'] = "Full with author.";
+$string['displayformatwuser']  = "Full without author.";
+$string['displayformatsimple'] = "Simple, dictionary style";
+$string['dontallowduplicates'] = "No, don't allow duplicates.";
+$string['editentry'] = "Edit entry";
+$string['entryalreadyexist'] = "Entry already exist.";
+$string['entrydeleted'] = "Entry deleted.";
+$string['entryexported'] = "Entry succesfully exported.";
+$string['exporttomainglossary'] = "Export to main glossary";
+$string['isthisthemainglossary'] = "Is this the main glossary of this course?";
+$string['itisthemainglossary'] = "Yes, it is.";
+$string['itisntthemainglossary'] = "No, it is not.";
+$string['modulename'] = "Glossary";
+$string['modulenameplural'] = "Glossaries";
+$string['newglossaryentries'] = "New glossary entries:";
+$string['noentries'] = "No entries found in this section";
+$string['noentry'] = "No entry found.";
+$string['selectletter'] = "Select the letter you want to look for.<p><b>Special</b> will show all concepts that does not begin with a letter.<br><b>ALL</b> will show all entries in one page.";
+$string['searchconcept'] = "Search concept:";
+$string['searchindefinition'] = "Search in concept AND definitions?";
+$string['special'] = "Special";
+$string['studentcanpost'] = "Yes, the students can add entries";
+$string['studentcantpost'] = "No, the students can't add entries";
+$string['writtenby'] = "By";
+$string['yesallowduplicates'] = "Yes, allow duplicates.";
+?>
