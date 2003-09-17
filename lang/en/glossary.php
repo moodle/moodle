cd564e5dfc601eb4b7ebb7c5e10e4ce46b30c1aa
willcast <willcast>
2003-09-17 01:56:56 +0000

    - Testing new permissions...

--- lang/en/glossary.php
+++ lang/en/glossary.php
@@ -1,38 +1,76 @@
 <?PHP // $Id$ 
+
       // glossary.php - created with Moodle 1.1 Beta (2003082001)
 
+
+
 $string['addentry'] = "Add entry";
+
 $string['allentries'] = "ALL";
-$string['allowduplicatedentries'] = "Are duplicated entries allowed?";
+
+$string['allowduplicatedentries'] = "Duplicated entries allowed";
+
 $string['alphabet'] = "A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z";
+
 $string['areyousuredelete'] = "Are you sure you want to delete this entry?";
+
 $string['attachment'] = "Attachment";
+
 $string['studentcanpost'] = "Students can add entries";
+
 $string['concept'] = "Concept";
+
 $string['concepts'] = "Concepts";
+
 $string['definition'] = "Definition";
+
 $string['definitions'] = "Definitions";
+
 $string['deleteentry'] = "Delete entry";
+
 $string['displayformat'] = "Display format?";
+
 $string['displayformatdefault'] = "Simple, dictionary style";
+
 $string['displayformat1'] = "Full with author.";
+
 $string['displayformat2']  = "Full without author.";
+
 $string['editentry'] = "Edit entry";
+
 $string['entryalreadyexist'] = "Entry already exist.";
+
 $string['entrydeleted'] = "Entry deleted.";
+
 $string['entryexported'] = "Entry succesfully exported.";
+
 $string['exporttomainglossary'] = "Export to main glossary";
+
 $string['glossaryformat'] = "Glossary format";
+
 $string['mainglossary'] = "Main glossary";
+
 $string['modulename'] = "Glossary";
+
 $string['modulenameplural'] = "Glossaries";
+
 $string['newglossaryentries'] = "New glossary entries:";
+
 $string['noentries'] = "No entries found in this section";
+
 $string['noentry'] = "No entry found.";
-$string['searchconcept'] = "Search concept:";
+
+$string['searchconcept'] = "Search:";
+
 $string['searchindefinition'] = "Search in concept AND definitions?";
+
 $string['secundaryglossary'] = "Secundary glossary";
+
 $string['selectletter'] = "Select the letter you want to look for.<p><b>Special</b> will show all concepts that does not begin with a letter.<br><b>ALL</b> will show all entries in one page.";
+
 $string['special'] = "Special";
+
 $string['writtenby'] = "By";
+
 ?>
+
