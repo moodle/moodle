8bc37becbc99601ecbe62814df8579bd7e1e79e8
jungwirr <jungwirr>
2003-09-17 14:40:38 +0000

    just some more strings for new functionality.

--- lang/en/attendance.php
+++ lang/en/attendance.php
@@ -4,6 +4,7 @@
 $string['modulename'] = "Attendance";
 $string['allmodulename'] = "All Attendance Rolls";
 $string['modulenameplural'] = "Attendance Rolls";
+$string['addmultiple'] = "Add Multiple Attendance Rolls";
 #------------------------------------------------------------
 
 
@@ -13,7 +14,7 @@ $string['dayofroll'] = "Date to take roll for";
 $string['notes'] = "Exceptional information";
 $string['notesfor'] = "Exceptional information for";
 $string['hoursinclass'] = "Number of Hours in a Class Period";
-$string['presentshort'] = "P";
+$string['presentshort'] = "X";
 $string['tardyshort'] = "T";
 $string['absentshort'] = "A";
 $string['presentlong'] = "Present";
@@ -22,7 +23,7 @@ $string['absentlong'] = "Absent";
 $string['dynamicsection'] = "Determine week for attendance based on this date";
 $string['defaultdynamicsection'] = "Whether to move attendance rolls to the correct week by default";
 $string['defaulthoursinclass'] = "How many hours there should be in a default attendance roll";
-$string['tardiesforabsence'] = "How many tardies should count as one absence";
+$string['tardiesperabsence'] = "How many tardies should count as one absence";
 $string['hoursinfullreport'] = "The maximum number of hours of attendance that should be displayed on a single page of a report";
 $string['previous'] = "Previous";
 $string['pages'] = "Pages";
@@ -32,8 +33,10 @@ $string['viewone'] = "View All Pages of Report";
 $string['viewtable'] = "View Report as One Table";
 $string['viewmulti'] = "View Report as Multiple Pages";
 $string['noviews'] = "Sorry, there is no data for your account here";
-
-
-
+$string['norolls'] = "There are no attendance rolls in this course";
+$string['startmulti'] = "Date for first attendance roll";
+$string['endmulti'] = "Date for last attendance roll";
+$string['choosedays'] = "Days of each week to take attendance";
+$string['dynamicsectionmulti'] = "Place rolls in their weekly section automatically";
 
 ?>
