eb53588cdb838d0d15926606736bae7d168aeef1
giro123 <giro123>
2003-09-17 16:50:28 +0000

    *** empty log message ***

---
+++ lang/es/attendance.php
@@ -0,0 +1,39 @@
+<?PHP 
+
+#------------------------------------------------------------
+$string['modulename'] = "Asistencia";
+$string['allmodulename'] = "Todos los registros de asistencia";
+$string['modulenameplural'] = "Registros de asistencia";
+#------------------------------------------------------------
+
+
+$string['takeroll'] = "¿Tomar registro de ese día?";
+$string['for'] = "para";
+$string['dayofroll'] = "Fecha para tomar el registro";
+$string['notes'] = "Información excepcional";
+$string['notesfor'] = "Información excepcional para";
+$string['hoursinclass'] = "Número de horas en un período de clase";
+$string['presentshort'] = "P";
+$string['tardyshort'] = "R";
+$string['absentshort'] = "A";
+$string['presentlong'] = "Presente";
+$string['tardylong'] = "Retrasado";
+$string['absentlong'] = "Ausente";
+$string['dynamicsection'] = "Determinar la semana para asistencia basándose en esta fecha";
+$string['defaultdynamicsection'] = "Trasladar los registros de asistencia a la semana correcta por defecto";
+$string['defaulthoursinclass'] = "Cuántas horas debería haber en un registro de asistencia por defecto";
+$string['tardiesforabsence'] = "Cuántos retrasos cuentan como una ausencia";
+$string['hoursinfullreport'] = "El número máximo de horas de asistencia que debería mostrarse en una sola página de un informe";
+$string['previous'] = "Anterior";
+$string['pages'] = "Páginas";
+$string['of'] = "de";
+$string['viewall'] = "¿Ver todos los registros de asistencia?";
+$string['viewone'] = "Ver todas las páginas del informe";
+$string['viewtable'] = "Ver informe como una tabla";
+$string['viewmulti'] = "Ver informe en múltiples páginas";
+$string['noviews'] = "Lo sentimos, no hay datos aquí para su cuenta";
+
+
+
+
+?>
