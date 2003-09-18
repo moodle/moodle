1edaf3f6f5b34659e718b167bc56ccfeac867732
willcast <willcast>
2003-09-18 15:49:52 +0000

    - When guest or students search entries, now they are correctly displayed.

--- lang/en/glossary.php
+++ lang/en/glossary.php
@@ -1,75 +1,44 @@
 <?PHP // $Id$ 
-
       // glossary.php - created with Moodle 1.1 Beta (2003082001)
 
-
-
 $string['addentry'] = "Add entry";
-
 $string['allentries'] = "ALL";
-
 $string['allowduplicatedentries'] = "Duplicated entries allowed";
-
 $string['alphabet'] = "A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z";
-
 $string['areyousuredelete'] = "Are you sure you want to delete this entry?";
-
 $string['attachment'] = "Attachment";
-
-$string['studentcanpost'] = "Students can add entries";
-
+$string['categoryview'] = "Vista por Categoría";
 $string['concept'] = "Concept";
-
 $string['concepts'] = "Concepts";
-
 $string['definition'] = "Definition";
-
 $string['definitions'] = "Definitions";
-
 $string['deleteentry'] = "Delete entry";
-
-$string['displayformat'] = "Display format?";
-
+$string['displayformat'] = "Display format";
 $string['displayformatdefault'] = "Simple, dictionary style";
-
-$string['displayformat1'] = "Full with author.";
-
-$string['displayformat2']  = "Full without author.";
-
+$string['displayformat1'] = "Full without author.";
+$string['displayformat2']  = "Full with author.";
 $string['editentry'] = "Edit entry";
-
 $string['entryalreadyexist'] = "Entry already exist.";
-
 $string['entrydeleted'] = "Entry deleted.";
-
 $string['entryexported'] = "Entry succesfully exported.";
-
 $string['exporttomainglossary'] = "Export to main glossary";
-
 $string['glossaryformat'] = "Glossary format";
-
 $string['mainglossary'] = "Main glossary";
-
 $string['modulename'] = "Glossary";
-
 $string['modulenameplural'] = "Glossaries";
-
 $string['newglossaryentries'] = "New glossary entries:";
-
 $string['noentries'] = "No entries found in this section";
-
 $string['noentry'] = "No entry found.";
-
 $string['searchconcept'] = "Search:";
-
 $string['searchindefinition'] = "Search in concept AND definitions?";
-
 $string['secundaryglossary'] = "Secundary glossary";
-
 $string['selectletter'] = "Select the letter you want to look for.<p><b>Special</b> will show all concepts that does not begin with a letter.<br><b>ALL</b> will show all entries in one page.";
-
+$string['showspecial'] = "Show 'Special' link";
+$string['showalphabet'] = "Show alphabet";
+$string['showall'] = "Show 'ALL' link";
 $string['special'] = "Special";
-
+$string['standardview'] = "Vista Normal";
+$string['studentcanpost'] = "Students can add entries";
 $string['writtenby'] = "By";
 
 ?>
