5afc5272f518f81d093b1e4fc272211da7f1370c
mits <mits>
2003-09-22 16:12:55 +0000

    Some new translations added.

--- lang/ja/attendance.php
+++ lang/ja/attendance.php
@@ -1,24 +1,33 @@
 <?PHP // $Id$ 
-      // attendance.php - created with Moodle 1.2 development (2003091600)
+      // attendance.php - created with Moodle 1.2 development (2003091800)
 
 
 $string['absentlong'] = "欠席";
 $string['absentshort'] = "欠";
 $string['allmodulename'] = "全出席簿";
+$string['friday'] = "金";
 $string['modulename'] = "出席";
 $string['modulenameplural'] = "出席簿";
+$string['monday'] = "月";
 $string['notes'] = "備考";
 $string['notesfor'] = "備考";
 $string['noviews'] = "申し訳ございません、あなたのデータはありません";
 $string['pages'] = "ページ";
 $string['presentlong'] = "出席";
 $string['presentshort'] = "出";
-$string['tardiesforabsence'] = "何回の遅刻を1回の欠席と見なすか";
+$string['saturday'] = "土";
+$string['sunday'] = "日";
 $string['tardylong'] = "遅刻";
 $string['tardyshort'] = "遅";
+$string['thursday'] = "木";
+$string['tuesday'] = "火";
 $string['viewall'] = "全ての出席簿を表示する";
 $string['viewmulti'] = "レポートを複数ページに表示する";
 $string['viewone'] = "レポートの全ページを表示する";
+$string['viewsection'] = "このセクションの全出席簿を表示する";
 $string['viewtable'] = "レポートを一つのテーブルに表示する";
+$string['viewweek'] = "カレンダー週の全出席簿を表示する";
+$string['wednesday'] = "水";
+$string['weekmodulename'] = "先週の全出席簿";
 
 ?>
--- lang/ja/glossary.php
+++ lang/ja/glossary.php
@@ -1,42 +1,49 @@
 <?PHP // $Id$ 
-      // glossary.php - created with Moodle 1.2 development (2003091600)
+      // glossary.php - created with Moodle 1.2 development (2003091800)
 
 
 $string['addentry'] = "エントリーの追加";
+$string['allcategories'] = "全てのカテゴリ";
 $string['allentries'] = "全て";
 $string['allowduplicatedentries'] = "エントリーの重複を許可しますか?";
+$string['alphabet'] = "A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z";
 $string['areyousuredelete'] = "本当にこのエントリーを削除しても宜しいですか?";
-$string['canstudentpost'] = "学生はエントリーを追加出来ますか?";
+$string['attachment'] = "添付";
+$string['back'] = "戻る";
+$string['categories'] = "カテゴリ";
+$string['category'] = "カテゴリ";
+$string['categorydeleted'] = "カテゴリが削除されました";
 $string['concept'] = "コンセプト";
 $string['concepts'] = "コンセプト";
 $string['definition'] = "定義";
 $string['definitions'] = "定義";
 $string['deleteentry'] = "エントリーの削除";
 $string['displayformat'] = "フォーマットを表示しますか?";
-$string['displayformatsimple'] = "シンプル、辞書スタイル";
-$string['displayformatwouser'] = "フル、著者有り";
-$string['displayformatwuser'] = "フル、著者無し";
-$string['dontallowduplicates'] = "いいえ、重複を許可しません";
+$string['editcategories'] = "カテゴリの編集";
 $string['editentry'] = "エントリーの編集";
+$string['entries'] = "エントリー";
+$string['entrieswithoutcategory'] = "カテゴリ無しのエントリー";
+$string['entry'] = "エントリー";
 $string['entryalreadyexist'] = "エントリーが既に登録されています。";
 $string['entrydeleted'] = "エントリーが削除されました。";
 $string['entryexported'] = "エントリーが正常にエクスポートされました。";
 $string['exporttomainglossary'] = "メイングロッサリーのエクスポート";
-$string['isthisthemainglossary'] = "これはこのコースのメイングロッサリーですか?";
-$string['itisntthemainglossary'] = "いいえ、違います。";
-$string['itisthemainglossary'] = "はい、そうです。";
+$string['glossarytype'] = "グロッサリータイプ";
+$string['mainglossary'] = "メイングロッサリー";
 $string['modulename'] = "グロッサリー";
 $string['modulenameplural'] = "グロッサリー";
 $string['newglossaryentries'] = "新規グロッサリーエントリー";
+$string['nocategorized'] = "カテゴリがありません";
 $string['noentries'] = "このセクションにはエントリーがありません";
 $string['noentry'] = "エントリーが見つかりませんでした。";
 $string['searchconcept'] = "コンセプトの検索:";
 $string['searchindefinition'] = "コンセプトと定義で検索しますか?";
 $string['selectletter'] = "探したい文字を選択してください。<p><b>特別</b> は1つのの言葉で始まらない全てのコンセプトを表示します。<br><b>全て</b>は1ページに全てのエントリーを表示します。";
+$string['showall'] = "全てのリンクを表示";
+$string['showalphabet'] = "アルファベットを表示";
+$string['showspecial'] = "特別リンクを表示";
 $string['special'] = "特別";
 $string['studentcanpost'] = "はい、学生はエントリーを追加できます";
-$string['studentcantpost'] = "いいえ、学生はエントリーを追加できません";
 $string['writtenby'] = "作成";
-$string['yesallowduplicates'] = "はい、重複を許可します";
 
 ?>
