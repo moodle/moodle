861a1b96bc41e5eb4f312ef8fdf13e8ce91a4440
les_kopari <les_kopari>
2003-09-24 23:09:49 +0000

    Initial add for Oracle...does nothing.

---
+++ mod/journal/db/oci8po.php
@@ -0,0 +1,12 @@
+<?PHP
+
+function glossary_upgrade($oldversion) {
+/// This function does anything necessary to upgrade 
+/// older versions to match current functionality 
+
+    global $CFG;
+    return true;
+}
+
+?>
+
