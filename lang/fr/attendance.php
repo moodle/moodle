f26f1e9491e78a0a4033c77ee1cb4e8ec1af4733
martignoni <martignoni>
2003-10-06 14:46:39 +0000

    New tranlated files added to repository

---
+++ lang/fr/attendance.php
@@ -0,0 +1,39 @@
+<?PHP 
+
+#------------------------------------------------------------
+$string['modulename'] = "Présences";
+$string['allmodulename'] = "Toutes les listes de présence";
+$string['modulenameplural'] = "Listes de présence";
+#------------------------------------------------------------
+
+
+$string['takeroll'] = "Contrôle des présences de ce jour ?";
+$string['for'] = "pour";
+$string['dayofroll'] = "Date du contrôle des présences";
+$string['notes'] = "Information exceptionnelle";
+$string['notesfor'] = "Information exceptionnelle pour";
+$string['hoursinclass'] = "Nombre d'heures dans cette période de classe";
+$string['presentshort'] = "P";
+$string['tardyshort'] = "R";
+$string['absentshort'] = "A";
+$string['presentlong'] = "Présent";
+$string['tardylong'] = "En retard";
+$string['absentlong'] = "Absent";
+$string['dynamicsection'] = "La présence à cette date détermine la présence hebdomadaire";
+$string['defaultdynamicsection'] = "Whether to move attendance rolls to the correct week by default";
+$string['defaulthoursinclass'] = "Nombres d'heures par défaut dans une liste de présence";
+$string['tardiesforabsence'] = "Nombre de retards comptant pour une absence";
+$string['hoursinfullreport'] = "The maximum number of hours of attendance that should be displayed on a single page of a report";
+$string['previous'] = "Précédente";
+$string['pages'] = "Pages";
+$string['of'] = "sur";
+$string['viewall'] = "Afficher toutes les listes de présence";
+$string['viewone'] = "Afficher toutes les pages du rapport";
+$string['viewtable'] = "Afficher le rapport dans un tableau";
+$string['viewmulti'] = "Afficher le rapport sur plusieurs pages";
+$string['noviews'] = "Sorry, there is no data for your account here";
+
+
+
+
+?>
