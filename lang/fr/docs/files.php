3c99a66aa90127a6171e20d724f9dbb903cc961c
martignoni <martignoni>
2003-10-08 16:48:00 +0000

    New translated files

---
+++ lang/fr/docs/files.php
@@ -0,0 +1,32 @@
+<?php // $Id$
+      // Names of the documentation files
+      // Files with a definition "-" are treated as spacers
+      // French translation 2003/10/08 13:22 Mina
+
+$string['-about'] = "Ë propos de Moodle";
+$string['intro.html'] = "Introduction";
+$string['background.html'] = "Contexte";
+$string['philosophy.html'] = "Philosophie";
+$string['licence.html'] = "Licence";
+$string['features.html'] = "FonctionnalitŽs";
+$string['release.html'] = "NouveautŽs";
+$string['future.html'] = "Futur";
+$string['credits.html'] = "Remerciements";
+
+$string['-installation'] = "Administration";
+$string['install.html'] = "Installation";
+$string['faq.html'] = "FAQ d'installation";
+$string['installamp.html'] = "Apache, MySQL, PHP";
+$string['upgrade.html'] = "Mise ˆ jour";
+
+$string['-usage'] = "Utilisation de Moodle";
+$string['teacher.html'] = "Manuel de l'enseignant";
+$string['other.html'] = "Autre documentation";
+
+$string['-development'] = "DŽveloppement";
+$string['developer.html'] = "Manuel du dŽveloppeur";
+$string['coding.html'] = "Guide se programmation";
+$string['cvs.html'] = "Utilisation de CVS";
+
+
+?>
