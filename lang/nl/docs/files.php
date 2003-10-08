76e0168130ae90f15ad2c353bf05ee286fd11a8d
koenr <koenr>
2003-10-08 21:18:10 +0000

    translated by Koen Roggemans

---
+++ lang/nl/docs/files.php
@@ -0,0 +1,31 @@
+<?php // $Id$
+      // Names of the documentation files
+      // Files with a definition "-" are treated as spacers
+
+$string['-about'] = "Over Moodle";
+$string['intro.html'] = "Inleiding";
+$string['background.html'] = "Achtergrond";
+$string['philosophy.html'] = "Filosofie";
+$string['licence.html'] = "Licentie";
+$string['features.html'] = "Mogelijkheden";
+$string['release.html'] = "Release Notes";
+$string['future.html'] = "Toekomst";
+$string['credits.html'] = "Kredieten";
+
+$string['-installation'] = "Beheer";
+$string['install.html'] = "Installatie";
+$string['faq.html'] = "Installatie FAQ";
+$string['installamp.html'] = "Apache, MySQL, PHP";
+$string['upgrade.html'] = "Upgraden";
+
+$string['-usage'] = "Moodle Gebruiken";
+$string['teacher.html'] = "Lerarenhandleiding";
+$string['other.html'] = "Andere documenten";
+
+$string['-development'] = "Ontwikkeling";
+$string['developer.html'] = "Ontwikkelaarshandleiding";
+$string['coding.html'] = "Code Gids";
+$string['cvs.html'] = "CVS Gebruiken";
+
+
+?>
