a8d28cb932ed079a060f2497773305dc1cc55618
koenr <koenr>
2003-10-08 18:50:08 +0000

    translated by koen roggemans

---
+++ lang/nl/label.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // label.php - created with Moodle 1.2 development (2003092900)
+
+
+$string['labeltext'] = "Label tekst";
+$string['modulename'] = "Label";
+$string['modulenameplural'] = "Labels";
+
+?>
