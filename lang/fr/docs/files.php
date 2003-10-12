1ee767efc1f47b397ebdc756a4166e5af3173af0
martignoni <martignoni>
2003-10-12 20:55:11 +0000

    Encoding corrected

--- lang/fr/docs/files.php
+++ lang/fr/docs/files.php
@@ -1,15 +1,15 @@
 <?php // $Id$
       // Names of the documentation files
       // Files with a definition "-" are treated as spacers
-      // French translation 2003/10/08 13:22 Mina
+      // French translation 2003/10/12 22:54 Mina
 
-$string['-about'] = "Ë propos de Moodle";
+$string['-about'] = "À propos de Moodle";
 $string['intro.html'] = "Introduction";
 $string['background.html'] = "Contexte";
 $string['philosophy.html'] = "Philosophie";
 $string['licence.html'] = "Licence";
-$string['features.html'] = "FonctionnalitŽs";
-$string['release.html'] = "NouveautŽs";
+$string['features.html'] = "Fonctionnalités";
+$string['release.html'] = "Nouveautés";
 $string['future.html'] = "Futur";
 $string['credits.html'] = "Remerciements";
 
@@ -17,14 +17,14 @@ $string['-installation'] = "Administration";
 $string['install.html'] = "Installation";
 $string['faq.html'] = "FAQ d'installation";
 $string['installamp.html'] = "Apache, MySQL, PHP";
-$string['upgrade.html'] = "Mise ˆ jour";
+$string['upgrade.html'] = "Mise à jour";
 
 $string['-usage'] = "Utilisation de Moodle";
 $string['teacher.html'] = "Manuel de l'enseignant";
 $string['other.html'] = "Autre documentation";
 
-$string['-development'] = "DŽveloppement";
-$string['developer.html'] = "Manuel du dŽveloppeur";
+$string['-development'] = "Développement";
+$string['developer.html'] = "Manuel du développeur";
 $string['coding.html'] = "Guide se programmation";
 $string['cvs.html'] = "Utilisation de CVS";
 
