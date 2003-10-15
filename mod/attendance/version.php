7386d3d88670fb753070aa59a0532eebc7a9142a
jungwirr <jungwirr>
2003-10-15 14:34:50 +0000

    fixed some foreach problems with if tests

--- mod/attendance/version.php
+++ mod/attendance/version.php
@@ -5,7 +5,7 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2003092500;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2003092501;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 3600;           // Period for cron to check this module (secs)
 
 ?>
