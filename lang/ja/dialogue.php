09c35884465cf70ec765eb3e819d30f1b9b98b82
mits <mits>
2003-10-16 20:07:40 +0000

    New translation and plished some.

---
+++ lang/ja/dialogue.php
@@ -0,0 +1,35 @@
+<?PHP // $Id$ 
+      // dialogue.php - created with Moodle 1.2 development (2003101200)
+
+
+$string['addmynewentries'] = "新しいエントリーを追加";
+$string['addmynewentry'] = "新しいエントリーを追加";
+$string['addsubject'] = "題名を追加";
+$string['allowmultiple'] = "1人で1つ以上のダイアログの作成を許可する";
+$string['allowstudentdialogues'] = "学生 - 学生のダイアログを許可する";
+$string['close'] = "閉じる";
+$string['closed'] = "閉鎖中";
+$string['deleteafter'] = "閉じられたダイアログを削除する(○日後)";
+$string['dialogueclosed'] = "ダイアログが閉じられました";
+$string['dialogueintro'] = "ダイアログ説明";
+$string['dialoguename'] = "ダイアログ名";
+$string['dialogueopened'] = "\$a により開かれたダイアログ";
+$string['everybody'] = "全員";
+$string['furtherinformation'] = "詳細";
+$string['lastentry'] = "最新エントリ";
+$string['modulename'] = "ダイアログ";
+$string['modulenameplural'] = "ダイアログ";
+$string['namehascloseddialogue'] = "\$a がダイアログを閉じました";
+$string['newdialogueentries'] = "新しいダイアログエントリ";
+$string['newentry'] = "新しいエントリ";
+$string['open'] = "開く";
+$string['opendialogue'] = "ダイアログを開く";
+$string['pane0'] = "ダイアログを開く";
+$string['status'] = "ステータス";
+$string['studenttostudent'] = "学生 - 学生";
+$string['subject'] = "題名";
+$string['subjectadded'] = "題名が追加されました";
+$string['teachertostudent'] = "教師 - 学生";
+$string['typeofdialogue'] = "ダイアログタイプ";
+
+?>
