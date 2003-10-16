3e6525ba88656b2db96617f565861e3d9bfc6fb9
moodler <moodler>
2003-10-16 09:33:27 +0000

    A very simple filter to censor "bad" words.
    Also provides an example of how to write filters.

---
+++ filter/censor/censor.php
@@ -0,0 +1,37 @@
+<?php // $id$
+//////////////////////////////////////////////////////////////
+//  Censorship filtering
+// 
+//  This very simple example of a Text Filter will parse
+//  printed text, replacing words with other words
+//
+//  To activate this filter, add a line like this to your
+//  config.php:
+//
+//  $CFG->textfilter1 = 'filter/censor/censor.php';
+//
+//////////////////////////////////////////////////////////////
+
+/// These lines are important - the variable must match the name 
+/// of the actual function below
+
+    $textfilter_function = 'censor_filter';
+
+    if (function_exists($textfilter_function)) {
+        return;
+    }
+
+
+/// This is the filtering function itself.  It accepts the 
+/// courseid and the text to be filtered (in HTML form).
+
+function censor_filter($courseid, $text) {
+
+    static $search  = array('fuck', 'cunt', 'shit', 'wank', 'cock');
+    static $replace = array('f***', 'c***', 's***', 'w***', 'c***');
+
+    return str_ireplace($search, $replace, $text);
+}
+
+
+?>
