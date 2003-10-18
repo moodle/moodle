3dfa6ce23a8cf22d471e5c11daee5fc51be4fb42
willcast <willcast>
2003-10-18 20:37:40 +0000

    - Fixed another bug when showing "dynaentries" (it does not take in count the case sensitive setting)

--- mod/glossary/showentry.php
+++ mod/glossary/showentry.php
@@ -8,7 +8,8 @@
     print_header();
     $entries = get_records_sql("select e.* from {$CFG->prefix}glossary_entries e, {$CFG->prefix}glossary g".
                                   " where e.glossaryid = g.id and".
-                                      " ucase(concept) = '" . strtoupper(trim($concept)). "' and".
+                                      " (e.casesensitive = 1 and ucase(concept) = '" . strtoupper(trim($concept)). "' or".
+                                      " e.casesensitive = 0 and concept = '$concept') and".
                                       " g.course = $courseid and".
                                       " e.usedynalink = 1 and g.usedynalink = 1");
     
