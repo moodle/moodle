b67117d84877bd54765b2b07a41b7f54b6d740d4
moodler <moodler>
2003-10-22 07:47:18 +0000

    Some missing colours

--- theme/oceanblue/config.php
+++ theme/oceanblue/config.php
@@ -7,6 +7,8 @@ $THEME->cellcontent  = "#F0F0FF";  // For areas with text
 $THEME->cellcontent2 = "#CCCCFF";  // Alternate colour
 $THEME->borders      = "#AAAAFF";  // Table borders
 $THEME->highlight    = "#AAFFAA";  // Highlighted text (eg after a search)
+$THEME->hidden       = "#AAAAAA";  // To color things that are hidden
+$THEME->autolink     = "#DDDDDD";  // To color auto-generated links (eg glossary)
 
 $THEME->custompix    = false;      // If true, then this theme must have a "pix" 
                                    // subdirectory that contains copies of all 
