8cae411fa1246d3d2d743c29a27c2cc8df7e341c
giro123 <giro123>
2003-10-25 01:38:59 +0000

    *** empty log message ***

---
+++ lang/es/dialogue.php
@@ -0,0 +1,68 @@
+<?PHP // $Id$
+
+#------------------------------------------------------------
+$string['modulename'] = "Charla";
+$string['modulenameplural'] = "Charlas";
+#------------------------------------------------------------
+ 
+$string['addmynewentries'] = "Agregar mis nuevas entradas";
+$string['addmynewentry'] = "Agregar mi nueva entrada";
+$string['addsubject'] = "Agregar tema";
+$string['allowmultiple'] = "Permitir más de una charla con la misma persona";
+$string['allowstudentdialogues'] = "Permitir charlas entre estudiantes";
+$string['confirmclosure'] = "Está a punto de cerrar su charla con \$a. Las charlas ya cerradas no se pueden reabierir. Si cierra esta charla podrá consultarla pero ya no podrá intervenir en ella, y deberá iniciar una nueva charla con la misma persona para continuar la conversación. ¿Está seguro de querer cerrarla?";
+$string['close'] = "Cerrar";
+$string['closed'] = "Cerrada";
+$string['deleteafter'] = "Borrar charlas cerradas después de (Días)";
+$string['dialogueclosed'] = "Charla cerrada";
+$string['dialogueintro'] = "Introducción a la charla";
+$string['dialoguemail'] = "\$a->userfrom ha respondido a su intervención en la charla '\$a->dialogue'
+Puede verlo junto a su intervención en:
+    \$a->url";
+$string['dialoguemailhtml'] = "El usuario \$a->userfrom ha respondido a su intervención en la charla '<i>\$a->dialogue</i>'<br /><br />
+Puede verla adjunta <a href=\\\"\$a->url\\\">aquí</a>.";
+$string['dialoguename'] = "Nombre de la charla";
+$string['dialogueopened'] = "Charla abierta con \$a";
+$string['dialoguewith'] = "Charla con \$a";
+$string['furtherinformation'] = "Información complementaria";
+$string['of'] = "de";
+$string['everybody'] = "Todos";
+$string['open'] = "Abierto";
+$string['lastentry'] = "Ultima participación";
+$string['maildefault'] = "Correo electrónico por defecto";
+$string['namehascloseddialogue'] = "\$a ha cerrado la charla";
+$string['newdialogueentries'] = "Nuevas intervenciones en la charla";
+$string['newentry'] = "Nueva intervención";
+$string['noavailablepeople'] = "No hay nadie disponible para charlar.";
+$string['nopersonchosen'] = "Ninguna persona elegida";
+$string['nosubject'] = "Ningún tema elegido";
+$string['notextentered'] = "Ningún texto incluído";
+$string['notstarted'] = "Usted no ha iniciado una charla aún";
+$string['notyetseen'] = "Aún no ha sido leído";
+$string['numberofentries'] = "Número de intervenciones";
+$string['numberofentriesadded'] = "Número de intervenciones agregadas: \$a";
+$string['onwrote'] = "Acerca de \$a escribió";
+$string['onyouwrote'] = "Acerca de \$a usted escribió";
+$string['open'] = "Abierto";
+$string['openadialoguewith'] = "Charla abierta con";
+$string['opendialogue'] = "Iniciar charla";
+$string['pane0'] = "Iniciar una charla";
+$string['pane1'] = "\$a charlas esperan tu respuesta";
+$string['pane1one'] = "Una charla espera tu respuesta";
+$string['pane2'] = "\$a  charlas de esperando respuestas de otra persona";
+$string['pane2one'] = "1 charla esperando una respuesta de la otra persona";
+$string['pane3'] = "\$a charlas cerradas";
+$string['pane3one'] = "1 charla cerrada";
+$string['seen'] = "Visto hace \$a";
+$string['sendmailmessages'] = " Enviar correo electrónico con mis nuevas entradas";
+$string['status'] = "Estatus";
+$string['studenttostudent'] = "Estudiante a Estudiante";
+$string['subject'] = "Tema";
+$string['subjectadded'] = "Tema agregado";
+$string['teachertostudent'] = "Profesor a Estudiante";
+$string['typefirstentry'] = "Escriba la primera entrada aquí";
+$string['typefollowup'] = "Escriba la continuación aquí";
+$string['typeofdialogue'] = "Tipo de charla";
+$string['typereply'] = "Escriba la respuesta aquí";
+$string['viewallentries'] = " Ver entradas a charla de \$a";
+?>
