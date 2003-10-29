7ee3031e69bc4b2a564d79f2ca76453e614b25b4
koenr <koenr>
2003-10-29 12:28:41 +0000

    filling in missing string

--- lang/nl/resource.php
+++ lang/nl/resource.php
@@ -1,83 +1,43 @@
 <?PHP // $Id$ 
-
-      // resource.php - created with Moodle 1.1 Beta (2003082001)
-
-
-
+      // resource.php - created with Moodle 1.2 development (2003102900)
 
 
 $string['addresource'] = "Voeg bron toe";
-
+$string['chooseafile'] = "Kies of upload een bestand";
 $string['configframesize'] = "Als een webpagina of een geüpload bestand binnen een frame wordt weergegeven, dan is deze waarde de hoogte (in pixels) van het bovenste frame (dat de navigatie bevat).";
-
 $string['editingaresource'] = "Bewerk een bron";
-
 $string['example'] = "Voorbeeld";
-
 $string['examplereference'] = "Koers, Diane (1999) <I>Visuele leermethode Office 2000</I>, A.W. Bruna multimedia, ISBN 9022943046.";
-
 $string['exampleurl'] = "http://www.voorbeeld.nl/eenmap/eenbestand.html";
-
 $string['filename'] = "Bestandsnaam";
-
 $string['fulltext'] = "Volledige tekst";
-
 $string['htmlfragment'] = "HTML fragment";
-
 $string['modulename'] = "Bron";
-
 $string['modulenameplural'] = "Bronnen";
-
 $string['neverseen'] = "Nooit gezien";
-
 $string['newdirectories'] = "Toon de links naar de mappen";
-
 $string['newfullscreen'] = "Vul het volledige scherm";
-
 $string['newheight'] = "Standaard vensterhoogte (in pixels)";
-
 $string['newlocation'] = "Toon de locatiebalk";
-
 $string['newmenubar'] = "Toon de menubalk";
-
 $string['newresizable'] = "Sta het veranderen van de grootte van het venster toe";
-
 $string['newscrollbars'] = "Sta scrollen van het venster toe";
-
 $string['newstatus'] = "Toon de statusbalk";
-
 $string['newtoolbar'] = "Toon de werkbalk";
-
 $string['newwidth'] = "Standaard vensterbreedte (in pixels)";
-
 $string['newwindow'] = "Nieuw venster";
-
 $string['newwindowopen'] = "Toon deze bron in een popup venster";
-
 $string['note'] = "Aantekening";
-
 $string['notefile'] = "Gebruik <A HREF=\$a>Bestandsbeheer</A> om meer bestanden naar de cursus te uploaden (zodat ze in deze lijst verschijnen).";
-
 $string['notypechosen'] = "Je moet een soort kiezen. Gebruik de 'Back'-knop om terug te gaan en het opnieuw te proberen.";
-
 $string['resourcetype'] = "Soort bron";
-
 $string['resourcetype1'] = "Verwijzing";
-
 $string['resourcetype2'] = "Website (binnen deze site)";
-
 $string['resourcetype3'] = "Geüpload bestand";
-
 $string['resourcetype4'] = "Platte tekst";
-
 $string['resourcetype5'] = "Weblink (nieuw venster)";
-
 $string['resourcetype6'] = "HTML tekst";
-
 $string['resourcetype7'] = "Programma";
-
 $string['resourcetype8'] = "Wiki tekst";
-
-
 
 ?>
