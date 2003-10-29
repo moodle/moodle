4ea47efb055331efcdf5d593a7ba0612d69d4bcc
carlesbellver <carlesbellver>
2003-10-29 09:07:49 +0000

    *** empty log message ***

---
+++ lang/ca/glossary.php
@@ -0,0 +1,40 @@
+<?PHP // $Id$ 
+      // glossary.php - created with Moodle 1.1 Beta (2003082001)
+
+$string['addentry'] = "Afegeix definició";
+$string['allentries'] = "TOTS";
+$string['allowduplicatedentries'] = "Permeteu definicions duplicades?";
+$string['areyousuredelete'] = "Esteu segur que voleu eliminar aquesta definició?";
+$string['canstudentpost'] = "Els estudiants podran afegir definicions?";
+$string['concept'] = "Concepte";
+$string['concepts'] = "Conceptes";
+$string['definition'] = "Definició";
+$string['definitions'] = "Definicions";
+$string['deleteentry'] = "Elimina definició";
+$string['displayformat'] = "Format de visualització";
+$string['displayformatwouser'] = "Ample, sense dades de l'autor.";
+$string['displayformatwuser'] = "Ample, amb dades de l'autor.";
+$string['displayformatsimple'] = "Senzill, estil diccionari";
+$string['dontallowduplicates'] = "No, no permetre duplicats.";
+$string['editentry'] = "Edita definició";
+$string['entryalreadyexist'] = "Aquesta entrada ja existeix.";
+$string['entrydeleted'] = "Definició eliminada.";
+$string['entryexported'] = "Concepte exportat satisfactòriament.";
+$string['exporttomainglossary'] = "Exporta al glossari principal.";
+$string['isthisthemainglossary'] = "Aquest és el glossari principal del curs?";
+$string['itisthemainglossary'] = "Sí, aquest és el principal.";
+$string['itisntthemainglossary'] = "No, aquest no és el principal.";
+$string['modulename'] = "Glossari";
+$string['modulenameplural'] = "Glossaris";
+$string['newglossaryentries'] = "Nous conceptes de glossari:";
+$string['noentries'] = "No existeixen definicions en aquesta secció";
+$string['noentry'] = "No existeix cap definició.";
+$string['selectletter'] = "Seleccioneu la lletra per la qual voleu veure les entrades del glossari.<p><b>Especial</b> us mostrarà tots els termes que comencen amb caràcters no alfabètics<br><b>TOTS</b> us mostrarà totes les entrades en una sola pàgina.";
+$string['searchconcept'] = "Cerca concepte:";
+$string['searchindefinition'] = "Cerca en concepte I en definició?";
+$string['special'] = "Especial";
+$string['studentcanpost'] = "Sí, els estudiants podran afegir definicions";
+$string['studentcantpost'] = "No, els estudiants no podran afegir definicions";
+$string['writtenby'] = "Per";
+$string['yesallowduplicates'] = "Sí, permetre duplicats.";
+?>
