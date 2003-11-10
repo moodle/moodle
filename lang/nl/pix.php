1d7abb687c7cb6558a04befd3af8c11ad4bf7a53
koenr <koenr>
2003-11-10 00:01:21 +0000

    translated  by koen roggemans

---
+++ lang/nl/pix.php
@@ -0,0 +1,26 @@
+<?PHP // $Id$ 
+      // pix.php - created with Moodle 1.2 development (2003110400)
+
+
+$string['angry'] = "boos";
+$string['approve'] = "goedkeurend";
+$string['biggrin'] = "grijns";
+$string['blackeye'] = "blauw oog";
+$string['blush'] = "blozend";
+$string['clown'] = "clown";
+$string['cool'] = "cool";
+$string['dead'] = "dood";
+$string['evil'] = "boosaardig";
+$string['kiss'] = "kusjes";
+$string['mixed'] = "gemengd gevoel";
+$string['sad'] = "triest";
+$string['shy'] = "verlegen";
+$string['sleepy'] = "slaperig";
+$string['smiley'] = "glimlach";
+$string['surprise'] = "verrast";
+$string['thoughtful'] = "bedenking";
+$string['tongueout'] = "tong uit";
+$string['wideeyes'] = "ogen wijd";
+$string['wink'] = "knipoog";
+
+?>
