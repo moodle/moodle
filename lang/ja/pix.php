46fa359f8c228678900f522fce47b1ea3138bc27
mits <mits>
2003-11-11 01:22:47 +0000

    New langfile added.

---
+++ lang/ja/pix.php
@@ -0,0 +1,26 @@
+<?PHP // $Id$ 
+      // pix.php - created with Moodle 1.2 development (2003110400)
+
+
+$string['angry'] = "怒り";
+$string['approve'] = "賛成";
+$string['biggrin'] = "満面の笑顔";
+$string['blackeye'] = "眼帯";
+$string['blush'] = "赤面";
+$string['clown'] = "ピエロ";
+$string['cool'] = "クール";
+$string['dead'] = "ヘトヘト";
+$string['evil'] = "意地悪";
+$string['kiss'] = "キス";
+$string['mixed'] = "複雑な";
+$string['sad'] = "悲しい";
+$string['shy'] = "恥ずかしい";
+$string['sleepy'] = "眠たい";
+$string['smiley'] = "笑顔";
+$string['surprise'] = "驚き";
+$string['thoughtful'] = "思慮深い";
+$string['tongueout'] = "ベー";
+$string['wideeyes'] = "大きな目";
+$string['wink'] = "ウインク";
+
+?>
