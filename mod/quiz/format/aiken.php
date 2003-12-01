79edc7f9727e9acaa0835011284cf090b399d56b
moodler <moodler>
2003-12-01 12:21:49 +0000

    Allow A. B. C.  in addition to A) B) C)

--- mod/quiz/format/aiken.php
+++ mod/quiz/format/aiken.php
@@ -50,7 +50,7 @@ class quiz_file_format extends quiz_default_format {
                 //                This will show everyline when file is being processed
                 //                print("$nowline<br>");
                 $leader = substr(ltrim($nowline),0,2);
-                if (strpos(",A),B),C),D),E),F),G),H),I),J)",$leader)>0){
+                if (strpos(".A)B)C)D)E)F)G)H)I)J)A.B.C.D.E.F.G.H.I.J.",$leader)>0){
                     //trim off the label and space
                     $question->answer[] = substr($nowline,3);
                     $question->fraction[] = 0;
