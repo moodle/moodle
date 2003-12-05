07eb7fa7b88fe1679a2ad448b1c677b10049358e
moodler <moodler>
2003-12-05 03:22:34 +0000

    Fixes for POP3 and TLS (new option)

--- auth/pop3/lib.php
+++ auth/pop3/lib.php
@@ -9,6 +9,9 @@ function auth_user_login ($username, $password) {
 
     switch ($CFG->auth_pop3type) {
         case "pop3":
+            $host = "{".$CFG->auth_pop3host.":$CFG->auth_pop3port/pop3}INBOX";
+        break;
+        case "pop3notls":
             $host = "{".$CFG->auth_pop3host.":$CFG->auth_pop3port/pop3/notls}INBOX";
         break;
         case "pop3cert":
