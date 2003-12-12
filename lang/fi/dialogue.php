402377c375bbc33945e37db039f30d8d485a25ee
paca70 <paca70>
2003-12-12 05:24:32 +0000

    initial translation and helpfiles for dialogue module. Thanks to Janne Mikkonen.

---
+++ lang/fi/dialogue.php
@@ -0,0 +1,69 @@
+<?PHP // $Id$ 
+      // dialogue.php - created with Moodle 1.2 development (2003120700)
+
+
+$string['addmynewentries'] = 'Lisää viestini';
+$string['addmynewentry'] = 'Lisää viestini';
+$string['addsubject'] = 'Lisää aihe';
+$string['allowmultiple'] = 'Avoimet vuoropuhelut per/hlö.';
+$string['allowstudentdialogues'] = 'Salli vuoropuhelut opiskelijoiden välillä';
+$string['close'] = 'Sulje';
+$string['closed'] = 'Suljettu';
+$string['confirmclosure'] = 'Olet sulkemassa vuoropuhelua $a :n kanssa. Suljettuja vuoropuheluja ei voida uudelleenavata. Jos suljet tämän vuoropuhelun, voit katsella merkintöjä mutta et voi enää lisätä siihen mitään.
+
+Oletko varma, että haluat sulkea keskustelun?';
+$string['deleteafter'] = 'Poista suljetut (päivät)';
+$string['dialogueclosed'] = 'Vuoropuhelu suljettu';
+$string['dialogueintro'] = 'Kuvaus';
+$string['dialoguemail'] = '$a->userform on lisännyt viestin vuoropuheluunne \'$a->dialogue\'
+
+Näet lisätyt viestit osoitteessa $a->url';
+$string['dialoguemailhtml'] = '$a->userform on lisännyt viestin vuoropuheluunne \'<i>$a->dialogue</i>\'<br /><br />
+
+Näet lisätyt viestit osoitteessa <a href=\"$a->url\">vuoropuhelu</a>.';
+$string['dialoguename'] = 'Nimi';
+$string['dialogueopened'] = 'Vuoropuhelu aukaistu $a :lle';
+$string['dialoguewith'] = 'Keskustelija';
+$string['everybody'] = 'Kaikki';
+$string['furtherinformation'] = 'Lisätietoja';
+$string['lastentry'] = 'Viimeisin merkintä';
+$string['maildefault'] = 'Sähköpostioletus';
+$string['modulename'] = 'Vuoropuhelu';
+$string['modulenameplural'] = 'Vuoropuhelut';
+$string['namehascloseddialogue'] = '$a on sulkenut vuoropuhelun';
+$string['newdialogueentries'] = 'Uudet viestit';
+$string['newentry'] = 'Uusi viesti';
+$string['noavailablepeople'] = 'Ei henkilöitä, joiden kanssa vuoropuhelu voidaan aloittaa';
+$string['nopersonchosen'] = 'Henkilöä ei ole valittu!';
+$string['nosubject'] = 'Aihe puuttuu!!!';
+$string['notextentered'] = 'Teksti puttuu!!!';
+$string['notstarted'] = 'Et ole aloittanut tätä vuoropuhelua vielä';
+$string['notyetseen'] = 'Avaamatta';
+$string['numberofentries'] = 'Viestien määrä';
+$string['numberofentriesadded'] = '$a :n merkinnät';
+$string['onwrote'] = '$a kirjoitti';
+$string['onyouwrote'] = '$a kirjoitit';
+$string['open'] = 'Avaa';
+$string['openadialoguewith'] = 'Avaa vuoropuhelu';
+$string['opendialogue'] = 'Avaa vuoropuhelu';
+$string['pane0'] = 'Avaa vuoropuhelu';
+$string['pane1'] = '$a odottaa vastauksiasi';
+$string['pane1one'] = '1 odottaa vastaustasi';
+$string['pane2'] = '$a odottaa vastausta toiselta henkiöltä';
+$string['pane2one'] = '1 odottaa vastausta toiselta henkilöltä';
+$string['pane3'] = '$a suljetut vuoropuhelut';
+$string['pane3one'] = '1 suljettu vuoropuhelu';
+$string['seen'] = 'Avattu $a sitten';
+$string['sendmailmessages'] = 'Lähetä sähköpostiviesti';
+$string['status'] = 'Tila';
+$string['studenttostudent'] = 'Opiskelija opiskelijalle';
+$string['subject'] = 'Aihe';
+$string['subjectadded'] = 'Aihe lisätty';
+$string['teachertostudent'] = 'Opettaja opiskelijalle';
+$string['typefirstentry'] = 'Kirjoita ensinmäinen viesti tähän';
+$string['typefollowup'] = 'Kirjoita seuraavat tähän';
+$string['typeofdialogue'] = 'Vuoropuhelun tyyppi';
+$string['typereply'] = 'Kirjoita vastaus tähän';
+$string['viewallentries'] = 'Näytä $a vuoropuhelu viestit';
+
+?>
