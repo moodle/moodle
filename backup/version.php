b1b589b5ccf1c18d15ed26b3469cc6f686f98e65
stronk7 <stronk7>
2003-12-24 12:11:35 +0000

    Now admin/configuration/backup strings are included. Happy Christmas :-)

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2003121500;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2003122400;   // The current version is a date (YYYYMMDDXX)
 
 $backup_release = "1.2 development";  // User-friendly version number
