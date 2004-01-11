81cc156467177a571dc62e318a19de307f12321a
koenr <koenr>
2004-01-11 22:18:36 +0000

    translated by koen Roggemans

---
+++ lang/nl/error.php
@@ -0,0 +1,13 @@
+<?PHP // $Id$ 
+      // error.php - created with Moodle 1.2 development (2004010900)
+
+
+$string['erroronline'] = 'Fout op lijn $a';
+$string['fieldrequired'] = '\"$a\" is een vereist veld';
+$string['invalidfieldname'] = '\"$a\" is geen geldige veldnaam';
+$string['missingfield'] = 'Veld \"$a\" ontbreekt';
+$string['unknowncourse'] = 'Onbekend vak \"$a\"';
+$string['usernotaddederror'] = 'Gebruiker \"$a\" niet toegevoegd - onbekende fout';
+$string['usernotaddedregistered'] = 'Gebruiker \"$a\" niet toegevoegd - gebruiker bestaat al';
+
+?>
