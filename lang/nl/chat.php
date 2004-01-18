60904615c513b8d5e46be515391fca856925ced1
koenr <koenr>
2004-01-18 02:05:19 +0000

    missing strings

--- lang/nl/chat.php
+++ lang/nl/chat.php
@@ -1,41 +1,43 @@
 <?PHP // $Id$ 
-      // chat.php - created with Moodle 1.1 Beta + (2003082300)
+      // chat.php - created with Moodle 1.2 development (2004011700)
 
 
-$string['beep'] = "piep";
-$string['chatintro'] = "Inleidende tekst";
-$string['chatname'] = "Naam van deze chatroom";
-$string['chatreport'] = "Chat sessies";
-$string['chattime'] = "Volgende chat tijd";
-$string['configoldping'] = "Na hoeveel tijd wil je een gebruiker als weg beschouwen, nadat deze een tijd niets van zich heeft laten horen?";
-$string['configrefreshroom'] = "Hoe vaak moet de chatroom ververst worden? (in seconden). Als je deze waarde laag in stelt kan de chatroom sneller lijken te werken, maar het kan ook, als veel mensen aan het chatten zijn, een hogere belasting op de webserver veroorzaken.";
-$string['configrefreshuserlist'] = "Hoe vaak moet de lijst met gebruikers ververst worden? (in seconden)";
-$string['currentchats'] = "Actieve chat sessies";
-$string['currentusers'] = "Huidige gebruikers";
-$string['donotusechattime'] = "Publiceer geen chat tijden";
-$string['enterchat'] = "Klik hier om de chat nu binnen te gaan";
-$string['errornousers'] = "Kon geen gebruikers vinden!";
-$string['helpchatting'] = "Help met chatten";
-$string['idle'] = "Wachtend";
-$string['messagebeepseveryone'] = "\$a piept iedereen!";
-$string['messagebeepsyou'] = "\$a heeft je net gepiept!";
-$string['messageenter'] = "\$a is net deze chat binnen gekomen";
-$string['messageexit'] = "\$a heeft deze chat verlaten";
-$string['messages'] = "Berichten";
-$string['modulename'] = "Chat";
-$string['modulenameplural'] = "Chats";
-$string['neverdeletemessages'] = "Berichten nooit verwijderen";
-$string['nextsession'] = "Volgende geplande sessie";
-$string['nomessages'] = "Nog geen berichten";
-$string['repeatdaily'] = "Elke dag om dezelfde tijd";
-$string['repeatnone'] = "Geen herhalingen - alleen publiceren op de aangegeven tijd";
-$string['repeattimes'] = "Herhaal de sessies";
-$string['repeatweekly'] = "Elke week om dezelfde tijd";
-$string['savemessages'] = "Bewaar 'oude' chat sessies";
-$string['seesession'] = "Bekijk deze sessie";
-$string['sessions'] = "Chat sessies";
-$string['strftimemessage'] = "%%H:%%M";
-$string['studentseereports'] = "Iedereen kan 'oude' chat sessies bekijken";
-$string['viewreport'] = "Bekijk 'oude' chat sessies";
+$string['beep'] = 'dingdong';
+$string['chatintro'] = 'Inleidende tekst';
+$string['chatname'] = 'Naam van deze chatroom';
+$string['chatreport'] = 'Chat sessies';
+$string['chattime'] = 'Volgende chat tijd';
+$string['configoldping'] = 'Na hoeveel tijd wil je een gebruiker als weg beschouwen, nadat deze een tijd niets van zich heeft laten horen?';
+$string['configrefreshroom'] = 'Hoe vaak moet de chatroom ververst worden? (in seconden). Als je deze waarde laag in stelt kan de chatroom sneller lijken te werken, maar het kan ook, als veel mensen aan het chatten zijn, een hogere belasting op de webserver veroorzaken.';
+$string['configrefreshuserlist'] = 'Hoe vaak moet de lijst met gebruikers ververst worden? (in seconden)';
+$string['currentchats'] = 'Actieve chat sessies';
+$string['currentusers'] = 'Huidige gebruikers';
+$string['deletesession'] = 'Verwijder de sessie';
+$string['deletesessionsure'] = 'Ben je er zeker van dat je deze sessie wil verwijderen?';
+$string['donotusechattime'] = 'Publiceer geen chat tijden';
+$string['enterchat'] = 'Klik hier om de chat nu binnen te gaan';
+$string['errornousers'] = 'Kon geen gebruikers vinden!';
+$string['helpchatting'] = 'Help met chatten';
+$string['idle'] = 'Wachtend';
+$string['messagebeepseveryone'] = '$a piept iedereen!';
+$string['messagebeepsyou'] = '$a heeft je net gepiept!';
+$string['messageenter'] = '$a is net deze chat binnen gekomen';
+$string['messageexit'] = '$a heeft deze chat verlaten';
+$string['messages'] = 'Berichten';
+$string['modulename'] = 'Chat';
+$string['modulenameplural'] = 'Chats';
+$string['neverdeletemessages'] = 'Berichten nooit verwijderen';
+$string['nextsession'] = 'Volgende geplande sessie';
+$string['nomessages'] = 'Nog geen berichten';
+$string['repeatdaily'] = 'Elke dag om dezelfde tijd';
+$string['repeatnone'] = 'Geen herhalingen - alleen publiceren op de aangegeven tijd';
+$string['repeattimes'] = 'Herhaal de sessies';
+$string['repeatweekly'] = 'Elke week om dezelfde tijd';
+$string['savemessages'] = 'Bewaar \'oude\' chat sessies';
+$string['seesession'] = 'Bekijk deze sessie';
+$string['sessions'] = 'Chat sessies';
+$string['strftimemessage'] = '%%H:%%M';
+$string['studentseereports'] = 'Iedereen kan \'oude\' chat sessies bekijken';
+$string['viewreport'] = 'Bekijk \'oude\' chat sessies';
 
 ?>
