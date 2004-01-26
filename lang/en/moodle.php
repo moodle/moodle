fd6c9e1d3a92cc8b73ff7c19cff088c654191c4a
moodler <moodler>
2004-01-26 17:32:07 +0000

    Setting moodle.php back to default format (as created by admin/lang.php)

--- lang/en/moodle.php
+++ lang/en/moodle.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // moodle.php - created with Moodle 1.2 development (2003122600)
+      // moodle.php - created with Moodle 1.2 development (2004011700)
 
 
 $string['action'] = 'Action';
