006af3266467565adfb679cfcbc3708abc198632
moodler <moodler>
2004-01-31 04:27:08 +0000

    Missing string

--- lang/en/error.php
+++ lang/en/error.php
@@ -6,6 +6,7 @@ $string['erroronline'] = 'Error on line $a';
 $string['fieldrequired'] = '\"$a\" is a required field';
 $string['invalidfieldname'] = '\"$a\" is not a valid field name';
 $string['missingfield'] = 'Field \"$a\" is missing';
+$string['modulerequirementsnotmet'] = 'Module \"$a->modulename\" ($a->moduleversion) could not be installed.  It requires a newer version of Moodle (currently you are using $a->currentmoodle, you need $a->requiremoodle).';
 $string['restricteduser'] = 'Sorry, but you are currently restricted from doing that.';
 $string['unknowncourse'] = 'Unknown course named \"$a\"';
 $string['usernotaddederror'] = 'User \"$a\" not added - unknown error';
