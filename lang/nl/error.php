4de2f1d6ffdfdd7f87425e5ee1bd295a61bf25ee
koenr <koenr>
2004-01-31 08:37:04 +0000

    missing strings

--- lang/nl/error.php
+++ lang/nl/error.php
@@ -6,6 +6,7 @@ $string['erroronline'] = 'Fout op lijn $a';
 $string['fieldrequired'] = '\"$a\" is een vereist veld';
 $string['invalidfieldname'] = '\"$a\" is geen geldige veldnaam';
 $string['missingfield'] = 'Veld \"$a\" ontbreekt';
+$string['modulerequirementsnotmet'] = 'De module \"$a->modulename\" ($a->moduleversion) kon niet geïnstalleerd worden. Een nieuwere versie van Moodle is vereist (je gebruikt nu $a->currentmoodle en je hebt $a->requiremoodle nodig).';
 $string['restricteduser'] = 'Sorry, maar je hebt onvoldoende rechten om dat te doen.';
 $string['unknowncourse'] = 'Onbekend vak \"$a\"';
 $string['usernotaddederror'] = 'Gebruiker \"$a\" niet toegevoegd - onbekende fout';
