8ba911fc295c7ccceec111bfa3e6ff0e6ceffbe9
mits <mits>
2004-01-31 17:40:29 +0000

    New file translated.

---
+++ lang/ja/error.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // error.php - created with Moodle 1.2 development (2004013101)
+
+
+$string['erroronline'] = '$a 行にエラーがあります';
+$string['fieldrequired'] = '\"$a\" は必須項目です';
+$string['invalidfieldname'] = '\"$a\" は正しいフィールド名ではありません';
+$string['missingfield'] = '\"$a\" に入力してください';
+$string['modulerequirementsnotmet'] = 'モジュール \"$a->modulename\" ($a->moduleversion) をインストール出来ませんでした。このモジュールをインストールする為には新しいバージョンのMoodleをインストールする必要があります。(現在、あなたは $a->currentmoodle を使用しています。 $a->requiremoodle が必要です。)';
+$string['restricteduser'] = '申し訳ございません。現在、この処理を行うことが出来ません。';
+$string['unknowncourse'] = '不明なコース名 \"$a\"';
+$string['usernotaddederror'] = 'ユーザ \"$a\" は追加されませんでした - 原因不明';
+$string['usernotaddedregistered'] = 'ユーザ \"$a\" は追加されませんでした - 登録済み';
+
+?>
