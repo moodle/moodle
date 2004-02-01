114c82afe595873b0de9e5c783459fe2ab39ec07
moodler <moodler>
2004-02-01 07:27:40 +0000

    On second thoughts, no need to double up these logs

--- course/mod.php
+++ course/mod.php
@@ -56,9 +56,6 @@
                 if (is_string($return)) {
                     error($return, "view.php?id=$mod->course");
                 }
-                add_to_log($mod->course, "course", "update mod", 
-                           "../mod/$mod->modulename/view.php?id=$mod->coursemodule", 
-                           "$mod->modulename $mod->instance", $mod->coursemodule); 
                 add_to_log($mod->course, $mod->modulename, "update", 
                            "view.php?id=$mod->coursemodule", 
                            "$mod->instance", $mod->coursemodule); 
@@ -97,9 +94,6 @@
                 if (! set_field("course_modules", "section", $sectionid, "id", $mod->coursemodule)) {
                     error("Could not update the course module with the correct section");
                 }   
-                add_to_log($mod->course, "course", "add mod", 
-                           "../mod/$mod->modulename/view.php?id=$mod->coursemodule", 
-                           "$mod->modulename $mod->instance", $mod->coursemodule); 
                 add_to_log($mod->course, $mod->modulename, "add", 
                            "view.php?id=$mod->coursemodule", 
                            "$mod->instance", $mod->coursemodule); 
@@ -116,7 +110,7 @@
                 }
                 add_to_log($mod->course, "course", "delete mod", 
                            "view.php?id=$mod->course", 
-                           "$mod->modulename $mod->instance"); 
+                           "$mod->modulename $mod->instance", $mod->coursemodule); 
                 break;
             default:
                 error("No mode defined");
