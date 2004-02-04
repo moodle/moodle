941391a4c1468a7fa6f0f6b0a6f0e2231d4fd1db
koenr <koenr>
2004-02-04 09:35:43 +0000

    translating at Moodle speed :-)

--- lang/nl/assignment.php
+++ lang/nl/assignment.php
@@ -1,54 +1,55 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.2 development (2003102200)
+      // assignment.php - created with Moodle 1.2 development (2004020300)
 
 
-$string['allowresubmit'] = "Sta  meerdere malen insturen toe";
-$string['assignmentdetails'] = "De details van de opdracht";
-$string['assignmentmail'] = "\$a->teacher heeft feedback gepost voor je bijdrage aan de opdracht '\$a->assignment'
+$string['allowresubmit'] = 'Sta  meerdere malen insturen toe';
+$string['assignmentdetails'] = 'De details van de opdracht';
+$string['assignmentmail'] = '$a->teacher heeft feedback gepost voor je bijdrage aan de opdracht \'$a->assignment\'
 Het is toegevoegd aan je opdracht-bijdrage:
-\$a->url";
-$string['assignmentmailhtml'] = "\$a->teacher heeft feedback gepost voor je bijdrage aan de opdracht '<i>\$a->assignment</i>'<br /><br />
-Het is toegevoegd aan je <a href=\\\"\$a->url\\\">opdracht-bijdrage</a>.
-";
-$string['assignmentname'] = "Naam van de opdracht";
-$string['assignmenttype'] = "Soort opdracht";
-$string['configmaxbytes'] = "Standaard maximum grootte voor alle opdrachten op de site (afhankelijk van vak limieten en andere lokale instellingen)";
-$string['description'] = "Beschrijving  ";
-$string['duedate'] = "Uiterste inleverdatum";
-$string['duedateno'] = "Geen uiterste inleverdatum";
-$string['early'] = "\$a vroeg";
-$string['failedupdatefeedback'] = "Het updaten van de feedback op de ingestuurde opdracht van gebruiker \$a is niet gelukt";
-$string['feedback'] = "Feedback";
-$string['feedbackupdated'] = "De feedback op ingestuurde opdrachten is voor \$a mensen bijgewerkt";
-$string['late'] = "\$a laat";
-$string['maximumgrade'] = "Maximale cijfer";
-$string['maximumsize'] = "Maximale grootte  ";
-$string['modulename'] = "Opdracht";
-$string['modulenameplural'] = "Opdrachten";
-$string['newsubmissions'] = "Ingestuurde opdrachten";
-$string['notgradedyet'] = "Nog niet beoordeeld";
-$string['notsubmittedyet'] = "Nog niet ingestuurd";
-$string['overwritewarning'] = "Waarschuwing: als je opnieuw een opdracht instuurt OVERSCHRIJFT deze de huidige ingestuurde opdracht";
-$string['saveallfeedback'] = "Bewaar al mijn feedback";
-$string['submissionfeedback'] = "Feedback op de ingestuurde opdracht";
-$string['submissions'] = "Ingestuurde opdrachten";
-$string['submitassignment'] = "Stuur je opdracht in door dit formulier in te vullen";
-$string['submitted'] = "Ingestuurd";
-$string['typeoffline'] = "Offline activiteit";
-$string['typeuploadsingle'] = "Upload een bestand";
-$string['uploadbadname'] = "In deze bestandsnaam staan rare tekens, het bestand kon daardoor niet worden geüpload
-";
-$string['uploadedfiles'] = "Ingestuurde bestanden";
-$string['uploaderror'] = "Er is een fout geconstanteerd tijdens het opslaan van het bestand op de server";
-$string['uploadfailnoupdate'] = "Het bestand werd prima geüpload, maar je bijdrage kon niet worden bijgewerkt!
-";
-$string['uploadfiletoobig'] = "Helaas is dat bestand te groot (de toegestane grootte is \$a bytes)";
-$string['uploadnofilefound'] = "Er werd geen bestand gevonden - weet je zeker dat je er één geselecteerd had om te uploaden?";
-$string['uploadnotregistered'] = "'\$a' werd prima geüpload, maar je bijdrage werd niet verwerkt!
-";
-$string['uploadsuccess'] = "'\$a' met succes geüpload  ";
-$string['viewfeedback'] = "Bekijk de cijfers en feedback voor de opdrachten";
-$string['viewsubmissions'] = "Bekijk \$a ingestuurde opdrachten";
-$string['yoursubmission'] = "Jouw ingestuurde opdracht";
+$a->url';
+$string['assignmentmailhtml'] = '$a->teacher heeft feedback gepost voor je bijdrage aan de opdracht \'<i>$a->assignment</i>\'<br /><br />
+Het is toegevoegd aan je <a href=\"$a->url\">opdracht-bijdrage</a>.
+';
+$string['assignmentname'] = 'Naam van de opdracht';
+$string['assignmenttype'] = 'Soort opdracht';
+$string['configmaxbytes'] = 'Standaard maximum grootte voor alle opdrachten op de site (afhankelijk van vak limieten en andere lokale instellingen)';
+$string['description'] = 'Beschrijving  ';
+$string['duedate'] = 'Uiterste inleverdatum';
+$string['duedateno'] = 'Geen uiterste inleverdatum';
+$string['early'] = '$a vroeg';
+$string['existingfiledeleted'] = 'Het vorige bestand is verwijderd: $a';
+$string['failedupdatefeedback'] = 'Het updaten van de feedback op de ingestuurde opdracht van gebruiker $a is niet gelukt';
+$string['feedback'] = 'Feedback';
+$string['feedbackupdated'] = 'De feedback op ingestuurde opdrachten is voor $a mensen bijgewerkt';
+$string['late'] = '$a laat';
+$string['maximumgrade'] = 'Maximale cijfer';
+$string['maximumsize'] = 'Maximale grootte  ';
+$string['modulename'] = 'Opdracht';
+$string['modulenameplural'] = 'Opdrachten';
+$string['newsubmissions'] = 'Ingestuurde opdrachten';
+$string['notgradedyet'] = 'Nog niet beoordeeld';
+$string['notsubmittedyet'] = 'Nog niet ingestuurd';
+$string['overwritewarning'] = 'Waarschuwing: als je opnieuw een opdracht instuurt OVERSCHRIJFT deze de huidige ingestuurde opdracht';
+$string['saveallfeedback'] = 'Bewaar al mijn feedback';
+$string['submissionfeedback'] = 'Feedback op de ingestuurde opdracht';
+$string['submissions'] = 'Ingestuurde opdrachten';
+$string['submitassignment'] = 'Stuur je opdracht in door dit formulier in te vullen';
+$string['submitted'] = 'Ingestuurd';
+$string['typeoffline'] = 'Offline activiteit';
+$string['typeuploadsingle'] = 'Upload een bestand';
+$string['uploadbadname'] = 'In deze bestandsnaam staan rare tekens, het bestand kon daardoor niet worden geüpload
+';
+$string['uploadedfiles'] = 'Ingestuurde bestanden';
+$string['uploaderror'] = 'Er is een fout geconstanteerd tijdens het opslaan van het bestand op de server';
+$string['uploadfailnoupdate'] = 'Het bestand werd prima geüpload, maar je bijdrage kon niet worden bijgewerkt!
+';
+$string['uploadfiletoobig'] = 'Helaas is dat bestand te groot (de toegestane grootte is $a bytes)';
+$string['uploadnofilefound'] = 'Er werd geen bestand gevonden - weet je zeker dat je er één geselecteerd had om te uploaden?';
+$string['uploadnotregistered'] = '\'$a\' werd prima geüpload, maar je bijdrage werd niet verwerkt!
+';
+$string['uploadsuccess'] = '\'$a\' met succes geüpload  ';
+$string['viewfeedback'] = 'Bekijk de cijfers en feedback voor de opdrachten';
+$string['viewsubmissions'] = 'Bekijk $a ingestuurde opdrachten';
+$string['yoursubmission'] = 'Jouw ingestuurde opdracht';
 
 ?>
