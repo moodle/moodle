b5abf3e824aee50871893b93052bb8ff367327b8
moodler <moodler>
2004-02-04 06:16:59 +0000

    Fixed a Typo -  thanks Bruno Vernier

--- lib/datalib.php
+++ lib/datalib.php
@@ -1112,7 +1112,8 @@ function get_course_students($courseid, $sort="s.timeaccess", $dir="", $page=0, 
     }
 
     return get_records_sql("SELECT u.id, u.username, u.firstname, u.lastname, u.maildisplay, u.mailformat,
-                            u.email, u.city, u.country, u.lastlogin, u.picture, u.department, uuuuuu.lang, u.timezone, s.timeaccess as lastaccess
+                            u.email, u.city, u.country, u.lastlogin, u.picture, u.department, u.lang, 
+                            u.timezone, s.timeaccess as lastaccess
                             FROM {$CFG->prefix}user u, 
                                  {$CFG->prefix}user_students s $groupmembers
                             WHERE $select
