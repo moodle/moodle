af7d68413f2fc98cabffef2676362f62cd1758ee
moodler <moodler>
2004-02-05 05:00:52 +0000

    Update old quiz logs

--- mod/quiz/version.php
+++ mod/quiz/version.php
@@ -5,8 +5,8 @@
 //  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2004013101;   // The (date) version of this module
-$module->requires = 2004013101;  // Requires this Moodle version
+$module->version  = 2004020500;   // The (date) version of this module
+$module->requires = 2004013101;   // Requires this Moodle version
 $module->cron     = 0;            // How often should cron check this module (seconds)?
 
 ?>
