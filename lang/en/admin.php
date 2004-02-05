14076f3ad7ddcff901441fbbb3948742fa6c4779
moodler <moodler>
2004-02-05 09:54:57 +0000

    More new strings

--- lang/en/admin.php
+++ lang/en/admin.php
@@ -3,6 +3,8 @@
 
 
 $string['upgradelogs'] = 'For full functionality, your old logs need to be upgraded.  <a href=\"$a\">More information</a>';
+$string['upgradelogsinfo'] = 'Some changes have way some logs are stored.  To be able to view all of your old logs on a per-activity basis, your old logs need to be upgraded.  Depending on your site this can take a long time (eg several hours) and can be quite taxing on the database for large sites.  Once you start this process you should let it finish (by keeping the browser window open).  Don\'t worry - your site will work fine for other people while the logs are being upgraded.<br /><br />Do you want to upgrade your logs now?';
 
+$string['upgradinglogs'] = 'Upgrading logs';
 
 ?>
