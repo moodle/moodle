13bc710ffa195512125f8d1dc62a1c018d1f0a5a
moodler <moodler>
2004-02-05 09:10:23 +0000

    New file for admin-only strings.

---
+++ lang/en/admin.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$
+      // admin.php - created with Moodle 1.2 development (2003111400)
+
+
+$string['upgradelogs'] = 'For full functionality, your old logs need to be upgraded.  <a href=\"$a\">More information</a>';
+
+
+?>
