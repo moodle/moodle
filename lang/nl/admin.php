e1fa1667b36073df8b3c46e24408b38c9a583642
koenr <koenr>
2004-02-06 23:39:23 +0000

    translated by Koen Roggemans

---
+++ lang/nl/admin.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // admin.php - created with Moodle 1.2 development (2004020300)
+
+
+$string['upgradelogs'] = 'Om volledige functionaliteit te bekomen moeten je oude logbestanden geupgraded worden. <a href=\"$a\">Meer informatie</a>';
+$string['upgradelogsinfo'] = 'Sommige wijzigen hebben invloed op de manier waarom de logs worden opgeslagen. Om al je oude logs per activiteit te kunnen bekijken, moeten je oude logs geupdated worden. Afhankelijk van je site kan dit lang duren (zelfs enkele uren) en voor lange sites kan dat de databank zwaar belasten. Als je het proces start, dan zou je het moeten laten aflopen (door het browservenster open te laten). Maak je geen zorgen, terwijl je de logs upgrade, zal de site blijven werken voor je gebruikers.<br /><br />Wil je je logs nu upgraden?';
+$string['upgradinglogs'] = 'Bezig met upgraden van logs';
+
+?>
