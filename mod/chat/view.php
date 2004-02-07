1eddd0d8f55e0659228c5c7857afec366cadfac8
moodler <moodler>
2004-02-07 12:27:23 +0000

    Added timezone display to reduce confusion

--- mod/chat/view.php
+++ mod/chat/view.php
@@ -95,7 +95,14 @@
 
 
     if ($chat->chattime and $chat->schedule) {  // A chat is scheduled
-        echo "<p align=center>$strnextsession: ".userdate($chat->chattime)."</p>";
+        if (abs($USER->timezone) > 13) {
+            $timezone = get_string("serverlocaltime");
+        } else if ($USER->timezone < 0) {
+            $timezone = "GMT".$USER->timezone;
+        } else {
+            $timezone = "GMT+".$USER->timezone;
+        }
+        echo "<p align=\"center\">$strnextsession: ".userdate($chat->chattime)." ($timezone)</p>";
     } else {
         echo "<br />";
     }
