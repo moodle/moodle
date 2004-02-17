4c918350d6ef80408599e786e07cafbf703ff299
giro123 <giro123>
2004-02-17 16:34:12 +0000

    *** empty log message ***

---
+++ lang/es/lesson.php
@@ -0,0 +1,43 @@
+<?PHP // $Id$ 
+      // lesson.php - created with Moodle 1.2 development (2003111400)
+
+
+$string['addanewpage'] = "Agregar página";
+$string['addpagehere'] = "Agregar página aquí";
+$string['answer'] = "Respuesta";
+$string['attempt'] = "Intento: \$a";
+$string['attempts'] = "Intentos";
+$string['available'] = "Disponible desde";
+$string['canretake'] = "\$a puede retomar";
+$string['checknavigation'] = "Revisar navegación";
+$string['confirmdeletionofthispage'] = "Confirme borrar la página";
+$string['congratulations'] = "Felicitaciones, ha llegado al final de la lección";
+$string['continue'] = "Continuar";
+$string['deadline'] = "Fecha final";
+$string['deletingpage'] = "Borrando página: \$a";
+$string['displayofgrade'] = "Mostrar calificación (para estudiante)";
+$string['endoflesson'] = "Fin de la lección";
+$string['gradeis'] = "La calificación es \$a";
+$string['jumpto'] = "Ir a";
+$string['maximumnumberofanswers'] = "Número máximo de respuestas";
+$string['modulename'] = "Lección";
+$string['modulenameplural'] = "Lecciones";
+$string['movepagehere'] = "Mover la página aquí";
+$string['moving'] = "Moviendo página: \$a";
+$string['nextpage'] = "Siguiente página";
+$string['noanswer'] = "Sin respuesta";
+$string['noattemptrecordsfound'] = "No se encontraro registros de intentos. Sin calificación.";
+$string['numberofcorrectanswers'] = "Número de respuestas correctas: \$a";
+$string['numberofpagesviewed'] = "Número de páginas vistas: \$a";
+$string['outof'] = "Fuera de \$a";
+$string['page'] = "Página: \$a";
+$string['pagecontents'] = "Contenido de la página";
+$string['pagetitle'] = "Título de la página";
+$string['pleasecheckoneanswer'] = "Seleccione una respuesta";
+$string['response'] = "Respuesta";
+$string['sanitycheckfailed'] = "Sanity Check Failed: Este intento se ha borrado";
+$string['savepage'] = "Guardar página";
+$string['thispage'] = "Esta página";
+$string['youhaveseen'] = "Usted ya vio más de una página de esta lección.<br />¿Desea comenzar desde la última página que vio?";
+
+?>
