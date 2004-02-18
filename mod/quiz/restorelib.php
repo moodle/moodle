a99008f6e187c317bbec000f01152590cd2f52fd
moodler <moodler>
2004-02-18 02:28:47 +0000

    Restore logs consistently

--- mod/quiz/restorelib.php
+++ mod/quiz/restorelib.php
@@ -1609,7 +1609,7 @@
                     //Get the new_id of the attempt (to recode the url field)
                     $att = backup_getid($restore->backup_unique_code,"quiz_attempts",$attid);
                     if ($att) {
-                        $log->url = "review.php?q=".$mod->new_id."&attempt=".$att->new_id;
+                        $log->url = "review.php?id=".$log->cmid."&attempt=".$att->new_id;
                         $log->info = $mod->new_id;
                         $status = true;
                     }
@@ -1626,7 +1626,7 @@
                     //Get the new_id of the attempt (to recode the url field)
                     $att = backup_getid($restore->backup_unique_code,"quiz_attempts",$attid);
                     if ($att) {
-                        $log->url = "review.php?q=".$mod->new_id."&attempt=".$att->new_id;
+                        $log->url = "review.php?id=".$log->cmid."&attempt=".$att->new_id;
                         $log->info = $mod->new_id;
                         $status = true;
                     }
@@ -1643,7 +1643,7 @@
                     //Get the new_id of the attempt (to recode the url field)
                     $att = backup_getid($restore->backup_unique_code,"quiz_attempts",$attid);
                     if ($att) { 
-                        $log->url = "review.php?q=".$mod->new_id."&attempt=".$att->new_id;
+                        $log->url = "review.php?id=".$log->cmid."&attempt=".$att->new_id;
                         $log->info = $mod->new_id;
                         $status = true;
                     }
