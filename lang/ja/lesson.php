4a4d7c5ff4bcb21a9b8740ca8cae02804ea21c12
mits <mits>
2004-02-18 16:17:38 +0000

    step by step

---
+++ lang/ja/lesson.php
@@ -0,0 +1,43 @@
+<?PHP // $Id$ 
+      // lesson.php - created with Moodle 1.2 development (2004021700)
+
+
+$string['addanewpage'] = '新しいページの追加';
+$string['addpagehere'] = 'ここにページを追加する';
+$string['answer'] = '回答';
+$string['attempt'] = '挑戦: $a';
+$string['attempts'] = '挑戦';
+$string['available'] = '利用開始';
+$string['canretake'] = '$a の再挑戦';
+$string['checknavigation'] = 'ナビゲーションチェック';
+$string['confirmdeletionofthispage'] = 'ページ削除の確認';
+$string['congratulations'] = 'おめでとうございます - 最終レッスンに到達しました';
+$string['continue'] = '続く';
+$string['deadline'] = '利用終了';
+$string['deletingpage'] = 'ページの削除: $a';
+$string['displayofgrade'] = '評価を表示する(学生に対して)';
+$string['endoflesson'] = 'レッスン終了';
+$string['gradeis'] = '評価は $a です';
+$string['jumpto'] = '移動';
+$string['maximumnumberofanswers'] = '最大回答数';
+$string['modulename'] = 'レッスン';
+$string['modulenameplural'] = 'レッスン';
+$string['movepagehere'] = 'ページをここに移動する';
+$string['moving'] = 'ページ移動: $a';
+$string['nextpage'] = '次ページ';
+$string['noanswer'] = '回答がありません';
+$string['noattemptrecordsfound'] = '挑戦レコードが見つかりません: 評価無し';
+$string['numberofcorrectanswers'] = '正答数: $a';
+$string['numberofpagesviewed'] = 'ページ閲覧数: $a';
+$string['outof'] = '$a 中';
+$string['page'] = 'ページ: $a';
+$string['pagecontents'] = 'ページ内容';
+$string['pagetitle'] = 'ページタイトル';
+$string['pleasecheckoneanswer'] = '答えを確認してください';
+$string['response'] = '返答';
+$string['sanitycheckfailed'] = '正当性チェックが失敗しました: この挑戦は削除されました';
+$string['savepage'] = 'ページの保存';
+$string['thispage'] = 'このページ';
+$string['youhaveseen'] = 'あなたは既にこのレッスンを1ページ以上閲覧しました。<br />あなたが見た最後のページから始めますか?';
+
+?>
