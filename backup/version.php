c9ec4c55571b18920aecf847add1de66b5a5ecd3
stronk7 <stronk7>
2004-02-19 00:06:19 +0000

    Bump. More modules support logs-restore.

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2004021501;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2004021800;   // The current version is a date (YYYYMMDDXX)
 
 $backup_release = "1.2 development";  // User-friendly version number
