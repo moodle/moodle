9f182b104690425bf8b9acf78b5c00ed76db06ed
mits <mits>
2004-02-19 15:42:04 +0000

    translated few.

--- lang/ja/attendance.php
+++ lang/ja/attendance.php
@@ -1,33 +1,39 @@
 <?PHP // $Id$ 
-      // attendance.php - created with Moodle 1.2 development (2003091800)
+      // attendance.php - created with Moodle 1.2 development (2004021700)
 
 
-$string['absentlong'] = "欠席";
-$string['absentshort'] = "欠";
-$string['allmodulename'] = "全出席簿";
-$string['friday'] = "金";
-$string['modulename'] = "出席";
-$string['modulenameplural'] = "出席簿";
-$string['monday'] = "月";
-$string['notes'] = "備考";
-$string['notesfor'] = "備考";
-$string['noviews'] = "申し訳ございません、あなたのデータはありません";
-$string['pages'] = "ページ";
-$string['presentlong'] = "出席";
-$string['presentshort'] = "出";
-$string['saturday'] = "土";
-$string['sunday'] = "日";
-$string['tardylong'] = "遅刻";
-$string['tardyshort'] = "遅";
-$string['thursday'] = "木";
-$string['tuesday'] = "火";
-$string['viewall'] = "全ての出席簿を表示する";
-$string['viewmulti'] = "レポートを複数ページに表示する";
-$string['viewone'] = "レポートの全ページを表示する";
-$string['viewsection'] = "このセクションの全出席簿を表示する";
-$string['viewtable'] = "レポートを一つのテーブルに表示する";
-$string['viewweek'] = "カレンダー週の全出席簿を表示する";
-$string['wednesday'] = "水";
-$string['weekmodulename'] = "先週の全出席簿";
+$string['absentlong'] = '欠席';
+$string['absentshort'] = '欠';
+$string['allmodulename'] = '全出席簿';
+$string['auto'] = '自動';
+$string['downloadexcelfull'] = 'フル・エクセルシートをダウンロードする';
+$string['downloadexceltotals'] = 'サマリー・エクセルシートをダウンロードする';
+$string['downloadtextfull'] = 'フル・テキストレポートをダウンロードする';
+$string['downloadtexttotals'] = 'サマリー・テキストレポートをダウンロードする';
+$string['friday'] = '金';
+$string['modulename'] = '出席';
+$string['modulenameplural'] = '出席簿';
+$string['monday'] = '月';
+$string['notes'] = '備考';
+$string['notesfor'] = '備考';
+$string['noviews'] = '申し訳ございません、あなたのデータはありません';
+$string['pages'] = 'ページ';
+$string['presentlong'] = '出席';
+$string['presentshort'] = '出';
+$string['saturday'] = '土';
+$string['startafterend'] = '開始日がコース終了日以降に設定されています';
+$string['sunday'] = '日';
+$string['tardylong'] = '遅刻';
+$string['tardyshort'] = '遅';
+$string['thursday'] = '木';
+$string['tuesday'] = '火';
+$string['viewall'] = '全ての出席簿を表示する';
+$string['viewmulti'] = 'レポートを複数ページに表示する';
+$string['viewone'] = 'レポートの全ページを表示する';
+$string['viewsection'] = 'このセクションの全出席簿を表示する';
+$string['viewtable'] = 'レポートを一つのテーブルに表示する';
+$string['viewweek'] = 'カレンダー週の全出席簿を表示する';
+$string['wednesday'] = '水';
+$string['weekmodulename'] = '先週の全出席簿';
 
 ?>
