77885194e3d781a18f828caf081e705bf6720e9b
stronk7 <stronk7>
2004-02-20 20:08:14 +0000

    Bump to 1.2 alpha (to be synchronized) :-)

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2004021800;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2004022000;   // The current version is a date (YYYYMMDDXX)
 
-$backup_release = "1.2 development";  // User-friendly version number
+$backup_release = "1.2 alpha";  // User-friendly version number
