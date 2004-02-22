08df81a8e627dd11772f88b8dff625f995fa7554
moodler <moodler>
2004-02-22 00:20:14 +0000

    Renamed dynalink.php to filter.php for consistency with other filters

--- mod/glossary/db/mysql.php
+++ mod/glossary/db/mysql.php
@@ -242,6 +242,13 @@ function glossary_upgrade($oldversion) {
     if ($oldversion < 2003112701) {
         delete_records("glossary_alias","entryid",0);
     }
+
+    if ($oldversion < 2004022200) {
+        if (!empty($CFG->textfilters)) {
+            $CFG->textfilters = str_replace("dynalink.php", "filter.php", $CFG->textfilters);
+            set_config("textfilters", $CFG->textfilters);
+        }
+    }
     
   return true;
 }
--- mod/glossary/db/postgres7.php
+++ mod/glossary/db/postgres7.php
@@ -6,6 +6,12 @@ function glossary_upgrade($oldversion) {
 
     global $CFG;
 
+    if ($oldversion < 2004022200) {
+        if (!empty($CFG->textfilters)) {
+            $CFG->textfilters = str_replace("dynalink.php", "filter.php", $CFG->textfilters);
+            set_config("textfilters", $CFG->textfilters);
+        }
+    }
 
     return true;
 }
