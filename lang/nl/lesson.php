acd4298d685d15f40025f1ef038752647d791db3
koenr <koenr>
2004-02-24 22:13:34 +0000

    first translationattempt: please comment

---
+++ lang/nl/lesson.php
@@ -0,0 +1,43 @@
+<?PHP // $Id$ 
+      // lesson.php - created with Moodle 1.2 Beta (2004022400)
+
+
+$string['addanewpage'] = 'Voeg een nieuwe pagina toe';
+$string['addpagehere'] = 'Voeg de pagina hier in';
+$string['answer'] = 'Antwoord';
+$string['attempt'] = 'Poging: $a';
+$string['attempts'] = 'Pogingen';
+$string['available'] = 'Beschikbaar van';
+$string['canretake'] = '$a mag opnieuw';
+$string['checknavigation'] = 'Controleer de navigatie';
+$string['confirmdeletionofthispage'] = 'Bevestig het verwijderen van deze pagina';
+$string['congratulations'] = 'Proficiat - je hebt het einde van de les bereikt';
+$string['continue'] = 'Ga verder';
+$string['deadline'] = 'Deadline';
+$string['deletingpage'] = 'Pagina $a aan het verwijderen';
+$string['displayofgrade'] = 'Tonen van het cijfer (voor de leerling)';
+$string['endoflesson'] = 'Einde van de les';
+$string['gradeis'] = 'Cijfer is $a';
+$string['jumpto'] = 'Ga naar';
+$string['maximumnumberofanswers'] = 'Maximum aantal antwoorden';
+$string['modulename'] = 'Les';
+$string['modulenameplural'] = 'Lessen';
+$string['movepagehere'] = 'Verplaats de pagina naar hier';
+$string['moving'] = 'Pagina $a aan het verplaatsen';
+$string['nextpage'] = 'Volgende pagina';
+$string['noanswer'] = 'Er is niet geantwoord';
+$string['noattemptrecordsfound'] = 'Geen pogingen gevonden: geen cijfer gegeven.';
+$string['numberofcorrectanswers'] = 'Aantal juiste antwoorden: $a';
+$string['numberofpagesviewed'] = 'Aantal bekeken pagina\'s: $a';
+$string['outof'] = 'Van $a';
+$string['page'] = 'Pagina: $a';
+$string['pagecontents'] = 'Inhoud van de pagina';
+$string['pagetitle'] = 'Titel van de pagina';
+$string['pleasecheckoneanswer'] = 'Controleer aub één antwoord';
+$string['response'] = 'Antwoord';
+$string['sanitycheckfailed'] = 'Fouten gevonden: deze poging wordt verwijderd';
+$string['savepage'] = 'Bewaar pagina';
+$string['thispage'] = 'Deze pagina';
+$string['youhaveseen'] = 'Je hebt al meer dan één pagina van deze les bekeken.<br />Wil je beginnen bij de laatste pagina die je vorige keer bekeken hebt?';
+
+?>
