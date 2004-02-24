eed6ad1d1593b6d690c8a7c0bd3bacdedbd3bd32
moodler <moodler>
2004-02-24 07:50:18 +0000

    I'll be packaging this up in the next few hours

--- version.php
+++ version.php
@@ -7,7 +7,7 @@
 
 $version = 2004022400;   // The current version is a date (YYYYMMDDXX)
 
-$release = "1.2 nearly beta!";   // User-friendly version number
+$release = "1.2 Beta";   // User-friendly version number
 
 
 ?>
