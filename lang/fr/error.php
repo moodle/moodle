db763c3810246fffac432932bc5fe16eef6cd49d
martignoni <martignoni>
2004-02-25 21:08:37 +0000

    New strings added

--- lang/fr/error.php
+++ lang/fr/error.php
@@ -1,8 +1,11 @@
 <?PHP // $Id$ 
 
 
+$string['coursegroupunknown'] = 'Cours correspondant au groupe $a non indiqué';
 $string['erroronline'] = 'Erreur à la ligne $a';
 $string['fieldrequired'] = '« $a » est un champ obligatoire';
+$string['groupalready'] = 'L\'utilisateur appartient déjà au groupe $a';
+$string['groupunknown'] = 'Le groupe $a n\'est pas associé au groupe indiqué';
 $string['invalidfieldname'] = '« $a » n\'est pas un nom de champ valable';
 $string['missingfield'] = 'Le champ « $a » est manquant';
 $string['modulerequirementsnotmet'] = 'Le module « $a->modulename » ($a->moduleversion) n\'a pas pu être installé. Il requiert une version plus récente de Moodle (votre version actuelle est $a->currentmoodle, vous avez besoin de la version $a->requiremoodle).';
