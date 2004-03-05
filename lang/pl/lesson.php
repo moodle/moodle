85d9f6a4ff05fa1301965ae0fd422b12f9a8a814
adamtpaw <adamtpaw>
2004-03-05 14:08:00 +0000

    New files and updates Moodle v1.2

---
+++ lang/pl/lesson.php
@@ -0,0 +1,43 @@
+<?PHP // $Id$ 
+      // lesson.php - created with Moodle 1.2 Beta (2004022400)
+
+
+$string['addanewpage'] = 'Dodaj now± stronê';
+$string['addpagehere'] = 'Wstaw tutaj now± stronê';
+$string['answer'] = 'Odpowied¼';
+$string['attempt'] = 'Próba: $a';
+$string['attempts'] = 'Próby';
+$string['available'] = 'Dostêpne od';
+$string['canretake'] = 'Ponowne przegl±danie lekcji';
+$string['checknavigation'] = 'Sprawd¼ nawigacjê';
+$string['confirmdeletionofthispage'] = 'Potwierd¼ usuniêcie tej strony';
+$string['congratulations'] = 'Gratualcje - koniec lekcji';
+$string['continue'] = 'Kontynuuj';
+$string['deadline'] = 'Termin koñcowy';
+$string['deletingpage'] = 'Usuwanie strony: $a';
+$string['displayofgrade'] = 'Wy¶wietl ocenê studentowi';
+$string['endoflesson'] = 'Koniec lekcji';
+$string['gradeis'] = 'Ocena: $a';
+$string['jumpto'] = 'Przejd¼ do';
+$string['maximumnumberofanswers'] = 'Maksymalna liczba odpowiedzi';
+$string['modulename'] = 'Lekcja';
+$string['modulenameplural'] = 'Lekcje';
+$string['movepagehere'] = 'Przesuñ stronê tutaj';
+$string['moving'] = 'Przesuwanie strony: $a';
+$string['nextpage'] = 'Nastêpna strona';
+$string['noanswer'] = 'Brak odpowiedzi';
+$string['noattemptrecordsfound'] = 'Brak prób: nie przyznano oceny';
+$string['numberofcorrectanswers'] = 'Ilo¶æ poprawnych odpowiedzi: $a';
+$string['numberofpagesviewed'] = 'Ilo¶æ obejrzanych strony: $a';
+$string['outof'] = 'Z $a';
+$string['page'] = 'Strona: $a';
+$string['pagecontents'] = 'Spis tre¶ci';
+$string['pagetitle'] = 'Tytu³ strony';
+$string['pleasecheckoneanswer'] = 'Proszê sprawd¼ jedn± odpowied¼';
+$string['response'] = 'Informacja zwrotna';
+$string['sanitycheckfailed'] = 'Ta próba zosta³a usuniêta!';
+$string['savepage'] = 'Zapisz stronê';
+$string['thispage'] = 'Ta strona';
+$string['youhaveseen'] = 'Widzia³e¶ ju¿ wiêcej ni¿ jedn± stronê tej lekcji. <br/> Czy chcia³by¶ zacz±æ od ostatniej strony, któr± przegl±da³e¶?';
+
+?>
