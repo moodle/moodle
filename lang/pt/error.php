17050991cd69bb84f3590f2870977fc62d531a5c
villate <villate>
2004-03-10 13:40:21 +0000

    Upgrades for 1.2

---
+++ lang/pt/error.php
@@ -0,0 +1,18 @@
+<?PHP // $Id$ 
+      // error.php - created with Moodle 1.2 Beta (2004022400)
+
+
+$string['coursegroupunknown'] = 'Faltou especificar a disciplina correspondente ao grupo $a';
+$string['erroronline'] = 'Erro na linha $a';
+$string['fieldrequired'] = '\"$a\" é um campo de preenchimento obrigatório';
+$string['groupalready'] = 'O utilizador já pertence ao grupo $a';
+$string['groupunknown'] = 'O grupo $a não está associado à disciplina indicada';
+$string['invalidfieldname'] = '\"$a\" é um nome de campo inválido';
+$string['missingfield'] = 'O campo \"$a\" não existe';
+$string['modulerequirementsnotmet'] = 'O módulo \"$a->modulename\" ($a->moduleversion) não pode ser instalado. Para a sua instalação é necessária uma versão mais recente do Moodle (actualmente está a usar a versão $a->currentmoodle e precisa da versão $a->requiremoodle).';
+$string['restricteduser'] = 'Lamentamos, mas na sua conta actual \"$a\" não lhe permite fazer isso.';
+$string['unknowncourse'] = 'Disciplina designada de \"$a\" é desconhecida';
+$string['usernotaddederror'] = 'O utilizador \"$a\" não foi adicionado - erro desconhecido';
+$string['usernotaddedregistered'] = 'O utilizador \"$a\" não foi adicionado - já se encontra registado';
+
+?>
