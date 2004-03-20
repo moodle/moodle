47b9d485449d4bb469d60e9aa24709e30697d594
martignoni <martignoni>
2004-03-20 17:14:35 +0000

    Added "notavailable" string

--- lang/fr/error.php
+++ lang/fr/error.php
@@ -9,6 +9,7 @@ $string['groupunknown'] = 'Le groupe $a n\'est pas associé au groupe indiqué';
 $string['invalidfieldname'] = '« $a » n\'est pas un nom de champ valable';
 $string['missingfield'] = 'Le champ « $a » est manquant';
 $string['modulerequirementsnotmet'] = 'Le module « $a->modulename » ($a->moduleversion) n\'a pas pu être installé. Il requiert une version plus récente de Moodle (votre version actuelle est $a->currentmoodle, vous avez besoin de la version $a->requiremoodle).';
+$string['notavailable'] = 'Ceci n\'est actuellement pas disponible';
 $string['restricteduser'] = 'Désolé, votre compte utilisateur « $a » ne vous permet pas d\'effectuer cette opération.';
 $string['unknowncourse'] = 'Le cours « $a » est inconnu';
 $string['usernotaddederror'] = 'L\'utilisateur « $a » n\'a pas été ajouté - erreur inconnue';
