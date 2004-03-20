d2c84650ab9faffad3aa163b0b8db2c25294dcf9
koenr <koenr>
2004-03-20 18:34:54 +0000

    missing string

--- lang/nl/error.php
+++ lang/nl/error.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // error.php - created with Moodle 1.2 Beta (2004022400)
+      // error.php - created with Moodle 1.2 (2004032000)
 
 
 $string['coursegroupunknown'] = 'Het vak dat met groep $a overeenkomt is niet gespecifieerd';
@@ -10,6 +10,7 @@ $string['groupunknown'] = 'Groep $a is niet geassocieerd met de gespecifieerde g
 $string['invalidfieldname'] = '\"$a\" is geen geldige veldnaam';
 $string['missingfield'] = 'Veld \"$a\" ontbreekt';
 $string['modulerequirementsnotmet'] = 'De module \"$a->modulename\" ($a->moduleversion) kon niet geïnstalleerd worden. Een nieuwere versie van Moodle is vereist (je gebruikt nu $a->currentmoodle en je hebt $a->requiremoodle nodig).';
+$string['notavailable'] = 'Dat is nu niet beschikbaar';
 $string['restricteduser'] = 'Sorry, maar je hebt onvoldoende rechten om dat te doen.';
 $string['unknowncourse'] = 'Onbekend vak \"$a\"';
 $string['usernotaddederror'] = 'Gebruiker \"$a\" niet toegevoegd - onbekende fout';
