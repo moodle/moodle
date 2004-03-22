2147cbda7ec6589382d1af259730826549f01980
stronk7 <stronk7>
2004-03-22 19:42:46 +0000

    preparing for micro-release in a day or two. Bump to 1.2.1

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2004032100;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2004032300;   // The current version is a date (YYYYMMDDXX)
 
-$backup_release = "1.2 +";  // User-friendly version number
+$backup_release = "1.2.1";  // User-friendly version number
