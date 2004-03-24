de861fe3cb4978633f73ed80c50ce873c9629b5f
adamtpaw <adamtpaw>
2004-03-24 08:55:42 +0000

    Few corrections and removing of not translated files.

--- lang/pl/error.php
+++ lang/pl/error.php
@@ -1,9 +1,12 @@
 <?PHP // $Id$ 
-      // error.php - created with Moodle 1.2 development (2004020300)
+      // error.php - created with Moodle 1.2 Beta (2004022400)
 
 
+$string['coursegroupunknown'] = 'Nie okre¶lono kursu dla grupy $a';
 $string['erroronline'] = 'B³±d na lini $a.';
 $string['fieldrequired'] = '\"$a\" nie mo¿e byæ puste.';
+$string['groupalready'] = 'U¿ytkownik ju¿ przynale¿y do grupy $a';
+$string['groupunknown'] = 'Grupa $a nie jest przypisana do tego kursu';
 $string['invalidfieldname'] = '\"$a\" nie jest dozwolon± nazw±.';
 $string['missingfield'] = 'Brakuje pola \"$a\" .';
 $string['modulerequirementsnotmet'] = 'Modu³ \"$a->modulename\" ($a->moduleversion) nie zosta³ poprawnie zainstalowany. Zainstaluj nowsz± wersjê Moodle! Aktualnie u¿ywasz wersji $a->currentmoodle, a potrzebujesz $a->requiremoodle).';
