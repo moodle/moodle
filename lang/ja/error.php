87d10572a56ed5f77910b1f9f60649e8fa7aca74
mits <mits>
2004-03-26 12:50:33 +0000

    New translation

--- lang/ja/error.php
+++ lang/ja/error.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // error.php - created with Moodle 1.2 Beta (2004022400)
+      // error.php - created with Moodle 1.2.1 (2004032500)
 
 
 $string['coursegroupunknown'] = 'コースに関連するグループ $a が指定されていません';
@@ -10,6 +10,7 @@ $string['groupunknown'] = 'グループ $a は指定されたコースに関連していません';
 $string['invalidfieldname'] = '\"$a\" は正しいフィールド名ではありません';
 $string['missingfield'] = '\"$a\" に入力してください';
 $string['modulerequirementsnotmet'] = 'モジュール \"$a->modulename\" ($a->moduleversion) をインストール出来ませんでした。このモジュールをインストールする為には新しいバージョンのMoodleをインストールする必要があります。(現在、あなたは $a->currentmoodle を使用しています。 $a->requiremoodle が必要です。)';
+$string['notavailable'] = '現在利用出来ません';
 $string['restricteduser'] = '申し訳ございません。現在、この処理を行うことが出来ません。';
 $string['unknowncourse'] = '不明なコース名 \"$a\"';
 $string['usernotaddederror'] = 'ユーザ \"$a\" は追加されませんでした - 原因不明';
