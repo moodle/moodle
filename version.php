69e8e9848fb73811983a96b568b0dfda62c4fe58
moodler <moodler>
2004-03-27 17:58:07 +0000

    Phew, finally finished upgrading all my customers!  :-)

--- version.php
+++ version.php
@@ -5,9 +5,9 @@
 // database to determine whether upgrades should
 // be performed (see lib/db/*.php)
 
-$version = 2004032500;   // The current version is a date (YYYYMMDDXX)
+$version = 2004032800;   // The current version is a date (YYYYMMDDXX)
 
-$release = "1.2.1";   // User-friendly version number
+$release = "1.3 development";   // User-friendly version number
 
 
 ?>
