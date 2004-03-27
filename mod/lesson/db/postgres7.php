e2a9adcc57367f552c1ad869b70ff4ec0b06f826
rkingdon <rkingdon>
2004-03-27 05:01:22 +0000

    Version 2 and 3 updates.

--- mod/lesson/db/postgres7.php
+++ mod/lesson/db/postgres7.php
@@ -6,6 +6,36 @@ function lesson_upgrade($oldversion) {
 
     global $CFG;
 
+    if ($oldversion < 2004021600) {
+
+       delete_records("log_display", "module", "lesson");
+
+       modify_database ("", "INSERT INTO prefix_log_display VALUES ('lesson', 'start', 'lesson', 'name');");
+       modify_database ("", "INSERT INTO prefix_log_display VALUES ('lesson', 'end', 'lesson', 'name');");
+       modify_database ("", "INSERT INTO prefix_log_display VALUES ('lesson', 'view', 'lesson_pages', 'title');");
+
+    }
+
+    if ($oldversion < 2004022200) {
+
+		table_column("lesson", "", "maxattempts", "INTEGER", "4", "UNSIGNED", "0", "NOT NULL", "maxanswers");
+		table_column("lesson", "", "nextpagedefault", "INTEGER", "4", "UNSIGNED", "0", "NOT NULL", "maxattempts");
+		table_column("lesson", "", "maxpages", "INTEGER", "4", "UNSIGNED", "0", "NOT NULL", "nextpagedefault");
+		table_column("lesson_pages", "", "qtype", "INTEGER", "4", "UNSIGNED", "0", "NOT NULL", "lessonid");
+		table_column("lesson_pages", "", "qoption", "INTEGER", "4", "UNSIGNED", "0", "NOT NULL", "qtype");
+		table_column("lesson_answers", "", "grade", "INTEGER", "4", "UNSIGNED", "0", "NOT NULL", "jumpto");
+
+    }
+
+    if ($oldversion < 2004032000) {           // Upgrade some old beta lessons
+		execute_sql(" UPDATE `{$CFG->prefix}lesson_pages` SET qtype = 3 WHERE qtype = 0");
+    }
+    
+    if ($oldversion < 2004032400) {
+		table_column("lesson", "", "usegrademax", "INTEGER", "4", "UNSIGNED", "0", "NOT NULL", "grade");
+		table_column("lesson", "", "minquestions", "INTEGER", "4", "UNSIGNED", "0", "NOT NULL", "nextpagedefault");
+    }
+    
     return true;
 }
 
