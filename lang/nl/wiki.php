f69be8f5a873cbb5e4ffe30961ac860294998cde
koenr <koenr>
2004-04-04 21:44:36 +0000

    initial translation for wiki development version

---
+++ lang/nl/wiki.php
@@ -0,0 +1,33 @@
+<?PHP // $Id$ 
+      // wiki.php - created with Moodle 1.3 development (2004032800)
+
+
+$string['FileDownload'] = 'Bestand downloaden';
+$string['MostOftenChangedPages'] = 'Meest gewijzigde pagina\'s';
+$string['MostVisitedPages'] = 'Meest bezochte pagina\'s';
+$string['NewestPages'] = 'Nieuwste pagina\'s';
+$string['OrphanedPages'] = 'Ongelinkte pagina\'s';
+$string['PageIndex'] = 'Indexpagina';
+$string['UpdatedPages'] = 'Vernieuwde pagina\'s';
+$string['WantedPages'] = 'Gevraagde pagina\'s';
+$string['allowstudentstowiki'] = 'Leerlingen mogen meewerken aan de wiki';
+$string['attachments'] = 'Bijlage';
+$string['backlinks'] = 'Verwijzende pagina\'s';
+$string['editthispage'] = 'Bewerk deze pagina';
+$string['ewikiacceptbinary'] = 'Laat binaire bestanden toe';
+$string['ewikiallowsafehtml'] = 'Laat veilige HTML toe';
+$string['ewikiprinttitle'] = 'Zet de titel op elke Wikipagina';
+$string['formattingrules'] = 'Opmaakregels';
+$string['initialcontent'] = 'Wiki inhoud bij start';
+$string['modulename'] = 'Wiki';
+$string['modulenameplural'] = 'Wikis';
+$string['pageactions'] = 'Pagina activiteiten';
+$string['pageinfo'] = 'Pagina informatie';
+$string['searchwiki'] = 'Zoeken in Wiki';
+$string['wikidefaultpagename'] = 'StartPagina';
+$string['wikilinks'] = 'Wiki links';
+$string['wikiname'] = 'Naam pagina';
+$string['wikistartederror'] = 'Er zit al inhoud in de Wiki - de gevraagde wijziging kan niet meer';
+$string['wikitype'] = 'Type';
+
+?>
