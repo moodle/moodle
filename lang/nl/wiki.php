ebc80c53a01409e79d64c3d24992c197fb898acc
koenr <koenr>
2004-04-07 20:09:53 +0000

    filling in missing strings (and it works now :-)  )

--- lang/nl/wiki.php
+++ lang/nl/wiki.php
@@ -2,6 +2,7 @@
       // wiki.php - created with Moodle 1.3 development (2004040500)
 
 
+$string['00empty'] = 'Lege Wiki';
 $string['FileDownload'] = 'Bestand downloaden';
 $string['MostOftenChangedPages'] = 'Meest gewijzigde pagina\'s';
 $string['MostVisitedPages'] = 'Meest bezochte pagina\'s';
@@ -14,15 +15,19 @@ $string['attachments'] = 'Bijlage';
 $string['backlinks'] = 'Verwijzende pagina\'s';
 $string['editthispage'] = 'Bewerk deze pagina';
 $string['ewikiacceptbinary'] = 'Laat binaire bestanden toe';
-$string['ewikiallowsafehtml'] = 'Laat veilige HTML toe';
+$string['ewikidefault'] = 'ErfurtWiki Standaard';
 $string['ewikiprinttitle'] = 'Zet de titel op elke Wikipagina';
 $string['formattingrules'] = 'Opmaakregels';
+$string['htmlmode'] = 'HTML modus';
+$string['htmlonly'] = 'Uitsluitend HTML';
 $string['initialcontent'] = 'Wiki inhoud bij start';
 $string['modulename'] = 'Wiki';
 $string['modulenameplural'] = 'Wikis';
+$string['nohtml'] = 'Geen HTML';
 $string['otherwikis'] = 'Andere Wikis';
 $string['pageactions'] = 'Pagina activiteiten';
 $string['pageinfo'] = 'Pagina informatie';
+$string['safehtml'] = 'Veilige HTML';
 $string['searchwiki'] = 'Zoeken in Wiki';
 $string['wikidefaultpagename'] = 'StartPagina';
 $string['wikilinks'] = 'Wiki links';
