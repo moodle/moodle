1af376eca43df877db6dfed5e266e5a48bd6af89
fiedorow <fiedorow>
2004-04-15 15:41:06 +0000

    Fix for bug on Windows and Mac servers while generating quiz reports

--- lib/excel/Workbook.php
+++ lib/excel/Workbook.php
@@ -32,7 +32,8 @@
 *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-require_once('Format.php');
+//require_once('Format.php');
+require_once("$CFG->libdir/excel/Format.php");
 require_once('OLEwriter.php');
 require_once('BIFFwriter.php');
 
