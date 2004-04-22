d192fce49338ff9e72d2a6b805f511a4299bca0f
carlesbellver <carlesbellver>
2004-04-22 12:00:41 +0000

    *** empty log message ***

---
+++ lang/ca/block_online_users.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // block_online_users.php - created with Moodle 1.3 development (2004041800)
+
+
+$string['blockname'] = 'Usuaris en línia';
+$string['configtimetosee'] = 'Nombre de minuts per considerar que un usuari està en línia';
+$string['periodnminutes'] = 'darrers $a minuts';
+
+?>
