89ace597518b737e158cbbedd635a18f90e8990f
defacer <defacer>
2004-04-28 15:21:38 +0000

    Fixed my own stupidities...

--- lang/el/fonts/lang_decode.php
+++ lang/el/fonts/lang_decode.php
@@ -1,7 +1,6 @@
 <?php
 
 function lang_decode($s) {
-/*
     $len = strlen($s);
     $out = '';
     for($i=0; $i < $len; $i++) {
@@ -9,8 +8,6 @@ function lang_decode($s) {
         $out .= $ch > 128 && $ch < 256 ? '&#'.(720 + $ch).';' : chr($ch);
     }
     return $out;
-*/
-    return iconv('ISO-8859-7', 'UTF-8', $s);
 }
 
 ?>
