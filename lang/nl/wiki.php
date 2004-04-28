cd2d90e3bf6b860008f1294001792bc336f784a3
koenr <koenr>
2004-04-28 10:55:14 +0000

    filling in missing strings...

--- lang/nl/wiki.php
+++ lang/nl/wiki.php
@@ -1,35 +1,38 @@
 <?PHP // $Id$ 
-      // wiki.php - created with Moodle 1.3 development (2004040500)
+      // wiki.php - created with Moodle 1.3 development (2004042703)
 
 
 $string['00empty'] = 'Lege Wiki';
-$string['FileDownload'] = 'Bestand downloaden';
-$string['MostOftenChangedPages'] = 'Meest gewijzigde pagina\'s';
-$string['MostVisitedPages'] = 'Meest bezochte pagina\'s';
-$string['NewestPages'] = 'Nieuwste pagina\'s';
-$string['OrphanedPages'] = 'Ongelinkte pagina\'s';
-$string['PageIndex'] = 'Indexpagina';
-$string['UpdatedPages'] = 'Vernieuwde pagina\'s';
-$string['WantedPages'] = 'Gevraagde pagina\'s';
+$string['action'] = '- - Acties - -';
 $string['attachments'] = 'Bijlage';
 $string['backlinks'] = 'Verwijzende pagina\'s';
+$string['choosewikilinks'] = '- - Kies Wiki Links - -';
 $string['editthispage'] = 'Bewerk deze pagina';
 $string['ewikiacceptbinary'] = 'Laat binaire bestanden toe';
 $string['ewikidefault'] = 'ErfurtWiki Standaard';
 $string['ewikiprinttitle'] = 'Zet de titel op elke Wikipagina';
+$string['filedownload'] = 'Bestand downloaden';
 $string['formattingrules'] = 'Opmaakregels';
 $string['htmlmode'] = 'HTML modus';
 $string['htmlonly'] = 'Uitsluitend HTML';
 $string['initialcontent'] = 'Wiki inhoud bij start';
 $string['modulename'] = 'Wiki';
 $string['modulenameplural'] = 'Wikis';
+$string['mostoftenchangedpages'] = 'Meest gewijzigde pagina\'s';
+$string['mostvisitedpages'] = 'Meest bezochte pagina\'s';
+$string['newestpages'] = 'Nieuwste pagina\'s';
 $string['nohtml'] = 'Geen HTML';
 $string['nowikicreated'] = 'Je kunt hier geen wiki maken en er is nog geen gemaakt om te bekijken.';
+$string['orphanedpages'] = 'Ongelinkte pagina\'s';
 $string['otherwikis'] = 'Andere Wikis';
 $string['pageactions'] = 'Pagina activiteiten';
+$string['pageindex'] = 'Index pagina';
 $string['pageinfo'] = 'Pagina informatie';
 $string['safehtml'] = 'Veilige HTML';
 $string['searchwiki'] = 'Zoeken in Wiki';
+$string['updatedpages'] = 'Gewijzigde pagina\'s';
+$string['viewpage'] = 'Bekijk pagina';
+$string['wantedpages'] = 'Te maken pagina\'s';
 $string['wikidefaultpagename'] = 'StartPagina';
 $string['wikilinks'] = 'Wiki links';
 $string['wikiname'] = 'Naam pagina';
