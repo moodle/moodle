47a2aa48daa8f42978073688f120d0505b7cd261
defacer <defacer>
2004-04-29 10:29:48 +0000

    Fix for bug #1280: the "showrecent" course setting has to go.
    Bump, to force db upgrade.

--- version.php
+++ version.php
@@ -1,11 +1,11 @@
 <?PHP  //$Id$
 // This file defines the current version of the
 // Moodle code that is being used.  This can be
-// compared against the values stored in the 
+// compared against the values stored in the
 // database to determine whether upgrades should
 // be performed (see lib/db/*.php)
 
-$version = 2004042703;   // The current version is a date (YYYYMMDDXX)
+$version = 2004042900;   // The current version is a date (YYYYMMDDXX)
 
 $release = "1.3 development";   // User-friendly version number
 
