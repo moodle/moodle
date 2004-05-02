47bb59fbf28dd08b2539bd91172f0f0270a70ce0
moodler <moodler>
2004-05-02 15:32:21 +0000

    Indicating dependency

--- mod/resource/version.php
+++ mod/resource/version.php
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 $module->version  = 2004013101;
-$module->requires = 2004013101;  // Requires this Moodle version
+$module->requires = 2004050200;  // Requires this Moodle version
 $module->cron     = 0;
 
 ?>
