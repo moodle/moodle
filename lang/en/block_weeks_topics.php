aff97c0b2e80c05300af742d06f6d68d538f7aaa
gustav_delius <gustav_delius>
2004-05-06 19:16:46 +0000

    a new block providing links to weeks/topics, see http://moodle.org/mod/forum/discuss.php?d=7648

---
+++ lang/en/block_weeks_topics.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+
+$string['blockname'] = 'Weeks/Topics';
+$string['topics'] = 'Topics';
+$string['jumptocurrenttopic'] = 'Jump to current topic';
+$string['jumptocurrentweek'] = 'Jump to current week';
+$string['weeks'] = 'Weeks';
+?>
