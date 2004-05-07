8a0878918c3e2cce793fe6d6e77c08a0713668db
martignoni <martignoni>
2004-05-07 07:24:09 +0000

    New file translated

---
+++ lang/fr/block_section_links.php
@@ -0,0 +1,9 @@
+<?php // $Id$ 
+
+$string['blockname'] = 'Semaines/Thèmes';
+$string['topics'] = 'Thèmes';
+$string['jumptocurrenttopic'] = 'Thème actuel';
+$string['jumptocurrentweek'] = 'Semaine actuelle';
+$string['weeks'] = 'Semaines';
+
+?>
