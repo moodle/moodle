1cbb892849d47d8ea53015a1f8716bd77190be58
moodler <moodler>
2004-05-10 13:20:37 +0000

    Time to draw a line in the mud

--- version.php
+++ version.php
@@ -5,9 +5,9 @@
 // database to determine whether upgrades should
 // be performed (see lib/db/*.php)
 
-$version = 2004050500;   // The current version is a date (YYYYMMDDXX)
+$version = 2004051000;   // The current version is a date (YYYYMMDDXX)
 
-$release = "1.3 development";   // User-friendly version number
+$release = "1.3 Beta";   // User-friendly version number
 
 
 ?>
