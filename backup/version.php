ba484b210886d8cbc5212b045cc613d6a1eadeca
stronk7 <stronk7>
2004-05-19 23:08:03 +0000

    Now day-names are showed properly always. Bug 1422. Using gmt functions to avoid TZ displacements... (http://moodle.org/bugs/bug.php?op=show&bugid=1422) And BUMP version.

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2004051300;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2004051600;   // The current version is a date (YYYYMMDDXX)
 
 $backup_release = "1.3 Beta +";  // User-friendly version number
