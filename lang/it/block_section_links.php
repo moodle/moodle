9d5287f29f713f7725b84d0d0e29d290158ceb04
bobopinna <bobopinna>
2004-05-20 11:31:54 +0000

    First translation

---
+++ lang/it/block_section_links.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // block_section_links.php - created with Moodle 1.3 Beta + (2004051300)
+
+
+$string['blockname'] = 'Collegamenti sezione';
+$string['jumptocurrenttopic'] = 'Salta all\'argomento corrente';
+$string['jumptocurrentweek'] = 'Salta alla settimana corrente';
+$string['topics'] = 'Argomenti';
+$string['weeks'] = 'Settimane';
+
+?>
