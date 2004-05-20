4db1fd85e03fb5780b2efd7caf2011f4475d307a
moodler <moodler>
2004-05-20 04:31:01 +0000

    Metal theme from A. Chavan

---
+++ theme/metal/README.txt
@@ -0,0 +1,14 @@
+MOODLE THEME: METAL
+
+To install the theme, copy the "metal" directory to moodle's "theme" directory.
+
+This theme is designed by A. Chavan (chavan at users.sourceforge.net).
+
+It is based on moodle's "standardwhite" theme. 
+It uses the background image from, and is inspired by:
+http://www.alexking.org/software/wordpress/styles/sample.php?wpstyle=metaldreams
+
+It has been tested with Mozilla. The header appears slightly different
+on Safari (MacOSX) and MSIE6(WindowsXP).
+
+$Lastupdate: Wed May 19 13:05:59 2004 chavan$
---
+++ theme/metal/config.php
@@ -0,0 +1,17 @@
+<?PHP // $Id$
+
+$THEME->body         = "#FFFFFF";  // Main page color
+$THEME->cellheading  = "#E8E8E8";  // Standard headings of big tables
+$THEME->cellheading2 = "#AAAAAA";  // Highlight headings of tables
+$THEME->cellcontent  = "#FFFFFF";  // For areas with text
+$THEME->cellcontent2 = "#EFEFEF";  // Alternate colour
+$THEME->borders      = "#999999";  // Table borders
+$THEME->highlight    = "#AAFFAA";  // Highlighted text (eg after a search)
+$THEME->hidden       = "#AAAAAA";  // To color things that are hidden
+$THEME->autolink     = "#EEEEEE";  // To color auto-generated links (eg glossary)
+
+$THEME->custompix    = false;      // If true, then this theme must have a "pix" 
+                                   // subdirectory that contains copies of all 
+                                   // files from the moodle/pix directory
+                                   // See "cordoroyblue" for an up-to-date example.
+?>
