30f649342ce45f127c309d13e1e81ff612c8baf8
stronk7 <stronk7>
2004-05-20 08:22:15 +0000

    Bump to 1.3 Final. Ready... (only bug 1420 ) remains in process...

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2004051600;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2004052000;   // The current version is a date (YYYYMMDDXX)
 
-$backup_release = "1.3 Beta +";  // User-friendly version number
+$backup_release = "1.3";  // User-friendly version number
