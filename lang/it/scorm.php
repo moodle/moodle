5acb92ac9fb7069db5502f25ee11f9af62d6b9bb
moodler <moodler>
2004-05-20 09:55:52 +0000

    Italian translation for SCORM module

---
+++ lang/it/scorm.php
@@ -0,0 +1,36 @@
+<?PHP // $Id$ 
+      // scorm.php - created with Moodle 1.2.1 (2004032500)
+
+
+$string['autocontinue'] = 'Continuazione automatica';
+$string['browse'] = 'Visita';
+$string['browsed'] = 'Navigato';
+$string['browsemode'] = 'Modalità di visita corso';
+$string['chooseapacket'] = 'Scegli o aggiorna un pacchetto SCORM';
+$string['completed'] = 'Completato';
+$string['coursepacket'] = 'Pacchetto del corso';
+$string['coursestruct'] = 'Struttura del corso';
+$string['datadir'] = 'Errore Filesystem: non è possibile creare la directory dei dati';
+$string['entercourse'] = 'Segui corso SCORM';
+$string['failed'] = 'Fallito';
+$string['incomplete'] = 'Incompleto';
+$string['mode'] = 'Modalità';
+$string['modulename'] = 'Scorm';
+$string['modulenameplural'] = 'Scorms';
+$string['next'] = 'Continua';
+$string['nomanifest'] = 'Manifest non trovato';
+$string['noreports'] = 'Non ci sono tentativi da visualizzare';
+$string['normal'] = 'Normale';
+$string['notattempted'] = 'Non provato';
+$string['packagedir'] = 'Errore Filesystem: non è possibile creare la directory del pacchetto SCORM';
+$string['passed'] = 'Superato';
+$string['prev'] = 'Precedente';
+$string['regular'] = 'Manifest corretto';
+$string['report'] = 'Report';
+$string['review'] = 'Rivedi';
+$string['trackingloose'] = 'ATTENZIONE: I dati di tracciamento esistenti saranno cancellati!';
+$string['validateascorm'] = 'Controllo pacchetto scorm';
+$string['validation'] = 'Risultato controllo';
+$string['viewallreports'] = 'Vedi lo stato dei $a tentativi';
+
+?>
