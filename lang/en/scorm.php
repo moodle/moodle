929f4875ab6f118bca878e33d6766bcbe2d82949
moodler <moodler>
2004-05-20 09:55:01 +0000

    Adding SCORM module

---
+++ lang/en/scorm.php
@@ -0,0 +1,36 @@
+<?PHP // $Id$
+
+#------------------------------------------------------------
+$string['modulename'] = 'Scorm';
+$string['modulenameplural'] = 'Scorms';
+#------------------------------------------------------------
+
+$string['autocontinue'] = 'Auto-Continue';
+$string['browse'] = 'Browse';
+$string['browsed'] = 'Browsed';
+$string['browsemode'] = 'Browse Mode';
+$string['chooseapacket'] = 'Choose or update a SCORM package';
+$string['completed'] = 'Completed';
+$string['coursestruct'] = 'Course structure';
+$string['coursepacket'] = 'Course package';
+$string['datadir'] = 'Filesystem Error: Can\'t create course data directory';
+$string['entercourse'] = 'Enter SCORM course';
+$string['failed'] = 'Failed';
+$string['incomplete'] = 'Incomplete';
+$string['next'] = 'Continue';
+$string['nomanifest'] = 'Manifest not found';
+$string['noreports'] = 'No report to display';
+$string['normal'] = 'Normal';
+$string['notattempted'] = 'Not attempted';
+$string['mode'] = 'Mode';
+$string['packagedir'] = 'Filesystem Error: Can\'t create package directory';
+$string['passed'] = 'Passed';
+$string['prev'] = 'Previous';
+$string['regular'] = 'Regular Manifest';
+$string['report'] = 'Report';
+$string['review'] = 'Review';
+$string['trackingloose'] = 'WARNING: The tracking data of this SCORM package will be lost!';
+$string['validation'] = 'Validation result';
+$string['validateascorm'] = 'Validate a SCORM package';
+$string['viewallreports'] = 'View reports for $a attempts';
+?>
