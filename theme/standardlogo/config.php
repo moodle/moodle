f96831ee67615e4d93936c3c871b7f73877a91eb
moodler <moodler>
2004-05-23 06:38:48 +0000

    Now the same colours as standard

--- theme/standardlogo/config.php
+++ theme/standardlogo/config.php
@@ -2,13 +2,13 @@
 
 $THEME->body         = "#FFEECE";  // Main page color
 $THEME->cellheading  = "#FFD991";  // Standard headings of big tables
-$THEME->cellheading2 = "#FFB62D";  // Highlight headings of tables
+$THEME->cellheading2 = "#FFC85F";  // Highlight headings of tables
 $THEME->cellcontent  = "#FFFFFF";  // For areas with text
-$THEME->cellcontent2 = "#FFD991";  // Alternate colour
-$THEME->borders      = "#FFB62D";  // Table borders
+$THEME->cellcontent2 = "#FEE6B9";  // Alternate colour
+$THEME->borders      = "#FFC85F";  // Table borders
 $THEME->highlight    = "#AAFFAA";  // Highlighted text (eg after a search)
 $THEME->hidden       = "#AAAAAA";  // To color things that are hidden
-$THEME->autolink     = "#EEEEEE";  // To color auto-generated links (eg glossary)
+$THEME->autolink     = "#DDDDDD";  // To color auto-generated links (eg glossary)
 
 $THEME->custompix    = false;      // If true, then this theme must have a "pix" 
                                    // subdirectory that contains copies of all 
