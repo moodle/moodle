f93956e508c0b2501b257aefad6d0a653b91e34f
koenr <koenr>
2004-05-24 06:14:20 +0000

    initial translation by Koen Roggemans

---
+++ lang/nl/scorm.php
@@ -0,0 +1,37 @@
+<?PHP // $Id$ 
+      // scorm.php - created with Moodle 1.3 Nearly Ready (2004052000)
+
+
+$string['autocontinue'] = 'Ga automatisch verder';
+$string['browse'] = 'Bladeren';
+$string['browsed'] = 'Gebladerd';
+$string['browsemode'] = 'Blader modus';
+$string['chooseapacket'] = 'Kies of update een SCORM pakket';
+$string['completed'] = 'Volledig';
+$string['coursepacket'] = 'Cursuspakket';
+$string['coursestruct'] = 'cursusstructuur';
+$string['datadir'] = 'Fout van het bestandssysteem: de gegevensfolder van de cursus kan niet gemaakt worden';
+$string['entercourse'] = 'Voeg een SCORM cursus toe';
+$string['failed'] = 'Mislukt';
+$string['incomplete'] = 'Onvolledig';
+$string['mode'] = 'Modus';
+$string['modulename'] = 'Scorm';
+$string['modulenameplural'] = 'Scoms';
+$string['next'] = 'Ga verder';
+$string['nomanifest'] = 'Manifest niet gevonden';
+$string['noreports'] = 'Er is geen rapport om te tonen';
+$string['normal'] = 'Normaal';
+$string['notattempted'] = 'Niet geprobeerd';
+$string['packagedir'] = 'Fout van het bestandssysteem: kan geen map voor het pakket maken';
+$string['passed'] = 'Geslaagd';
+$string['prev'] = 'Vorige';
+$string['regular'] = 'Normaal manifest';
+$string['report'] = 'Rapport';
+$string['review'] = 'Herzien';
+$string['score'] = 'Score';
+$string['trackingloose'] = 'WAARSCHUWING: de gegevens voor opvolging van dit SCORM-pakket zullen verloren gaan';
+$string['validateascorm'] = 'Bevestig een SCORM-pakket';
+$string['validation'] = 'Bevestigingsresultaat';
+$string['viewallreports'] = 'Bekijk de rapporten voor $a pogingen';
+
+?>
