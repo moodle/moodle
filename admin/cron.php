70c8fa117cac204e38e5a417777d2b51b069f7a8
moodler <moodler>
2004-05-26 14:14:43 +0000

    RSS requires backup/lib.php currently ... Eloy, see bug 1467  :-)

--- admin/cron.php
+++ admin/cron.php
@@ -149,6 +149,7 @@
 
     if (!empty($CFG->enablerssfeeds)) {  //Defined in admin/variables page
         if (file_exists("$CFG->dirroot/rss/rsslib.php")) {
+            include_once("$CFG->dirroot/backup/lib.php");    // Unfortunately this is required
             include_once("$CFG->dirroot/rss/rsslib.php");
             echo "Running rssfeeds if required...\n";
             flush();
