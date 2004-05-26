ee13686d51077e58245548b1170a143c3c2f82f4
barias <barias>
2004-05-26 22:25:16 +0000

    Latest updates

--- lang/es/dialogue.php
+++ lang/es/dialogue.php
@@ -1,39 +1,39 @@
 <?PHP // $Id$ 
-      // dialogue.php - created with Moodle 1.3 development (2004042600)
+      // dialogue.php - created with Moodle 1.3 (2004052500)
 
 
 $string['addmynewentries'] = 'Agregar mis nuevas entradas';
 $string['addmynewentry'] = 'Agregar mi nueva entrada';
 $string['addsubject'] = 'Agregar tema';
-$string['allowmultiple'] = 'Permitir más de una charla con la misma persona';
-$string['allowstudentdialogues'] = 'Permitir charlas entre estudiantes';
+$string['allowmultiple'] = 'Permitir más de un diálogo con la misma persona';
+$string['allowstudentdialogues'] = 'Permitir diálogos entre estudiantes';
 $string['close'] = 'Cerrar';
-$string['closed'] = 'Cerrada';
-$string['confirmclosure'] = 'Está a punto de cerrar su charla con $a. Las charlas ya cerradas no se pueden volver a abrir. Si cierra esta charla podrá consultarla pero ya no podrá intervenir en ella, y deberá iniciar una nueva charla con la misma persona para continuar la conversación. ¿Está seguro de que desea cerrarla?';
-$string['deleteafter'] = 'Borrar charlas cerradas después de (Días)';
-$string['dialogueclosed'] = 'Charla cerrada';
-$string['dialogueintro'] = 'Introducción a la charla';
-$string['dialoguemail'] = '$a->userfrom ha respondido a su intervención en la charla \'$a->dialogue\'
+$string['closed'] = 'Cerrado';
+$string['confirmclosure'] = 'Está a punto de cerrar su diálogo con $a. Los diálogos ya cerradosno se pueden volver a abrir. Si cierra este diálogo podrá consultarlo pero ya no podrá intervenir en él, y deberá iniciar un nuev diálogo con la misma persona para continuar la conversación. ¿Está seguro de que desea cerrarlo?';
+$string['deleteafter'] = 'Borrar diálogos cerrados después de (Días)';
+$string['dialogueclosed'] = 'Diálogo cerrado';
+$string['dialogueintro'] = 'Introducción al diálogo';
+$string['dialoguemail'] = '$a->userfrom ha respondido a su intervención en el diálogo \'$a->dialogue\'
 Puede verlo junto a su intervención en:
     $a->url';
-$string['dialoguemailhtml'] = 'El usuario $a->userfrom ha respondido a su intervención en la charla \'<i>$a->dialogue</i>\'<br /><br />
-Puede verla adjunta <a href=\"$a->url\">aquí</a>.';
-$string['dialoguename'] = 'Nombre de la charla';
-$string['dialogueopened'] = 'Charla abierta con $a';
-$string['dialoguewith'] = 'Charla con $a';
+$string['dialoguemailhtml'] = 'El usuario $a->userfrom ha respondido a su intervención en el diálogo \'<i>$a->dialogue</i>\'<br /><br />
+Puede verlo adjunto <a href=\"$a->url\">aquí</a>.';
+$string['dialoguename'] = 'Nombre del diálogo';
+$string['dialogueopened'] = 'Diálogo abierto con $a';
+$string['dialoguewith'] = 'Diálogo con $a';
 $string['everybody'] = 'Todos';
 $string['furtherinformation'] = 'Información complementaria';
 $string['lastentry'] = 'Ultima participación';
 $string['maildefault'] = 'Correo electrónico por defecto';
-$string['modulename'] = 'Charla';
-$string['modulenameplural'] = 'Charlas';
-$string['namehascloseddialogue'] = '$a ha cerrado la charla';
-$string['newdialogueentries'] = 'Nuevas intervenciones en la charla';
+$string['modulename'] = 'Diálogo';
+$string['modulenameplural'] = 'Diálogos';
+$string['namehascloseddialogue'] = '$a ha cerrado el diálogo';
+$string['newdialogueentries'] = 'Nuevas intervenciones en el diálogo';
 $string['newentry'] = 'Nueva intervención';
-$string['noavailablepeople'] = 'No hay nadie disponible para charlar.';
-$string['nopersonchosen'] = 'Ninguna persona elegida';
-$string['nosubject'] = 'Ningún tema elegido';
-$string['notextentered'] = 'Ningún texto incluído';
+$string['noavailablepeople'] = 'No hay nadie disponible para dialogar.';
+$string['nopersonchosen'] = 'No se ha elegido interlocutor';
+$string['nosubject'] = 'No se ha elegido tema';
+$string['notextentered'] = 'No se ha incluído texto';
 $string['notstarted'] = 'Usted no ha iniciado una charla aún';
 $string['notyetseen'] = 'Aún no ha sido leído';
 $string['numberofentries'] = 'Número de intervenciones';
@@ -42,18 +42,18 @@ $string['of'] = 'de';
 $string['onwrote'] = 'Acerca de $a escribió';
 $string['onyouwrote'] = 'Acerca de $a usted escribió';
 $string['open'] = 'Abierto';
-$string['openadialoguewith'] = 'Charla abierta con';
-$string['opendialogue'] = 'Iniciar charla';
-$string['opendialogueentries'] = 'Abrir entradas de la charla';
-$string['pane0'] = 'Iniciar una charla';
-$string['pane1'] = '$a charlas esperan su respuesta';
-$string['pane1one'] = 'Una charla espera su respuesta';
-$string['pane2'] = '$a charlas esperando respuestas de la otra persona';
-$string['pane2one'] = '1 charla esperando una respuesta de la otra persona';
-$string['pane3'] = '$a charlas cerradas';
-$string['pane3one'] = '1 charla cerrada';
+$string['openadialoguewith'] = 'Diálogo abierto con';
+$string['opendialogue'] = 'Iniciar diálogo';
+$string['opendialogueentries'] = 'Abrir entradas del diálogo';
+$string['pane0'] = 'Iniciar un diálogo';
+$string['pane1'] = '$a diálogos esperan su respuesta';
+$string['pane1one'] = 'Un diálogo espera su respuesta';
+$string['pane2'] = '$a diálogos esperando respuestas de la otra persona';
+$string['pane2one'] = '1 diálogo esperando una respuesta de la otra persona';
+$string['pane3'] = '$a diálogos cerrados';
+$string['pane3one'] = '1 diálogo cerrado';
 $string['seen'] = 'Visto hace $a';
-$string['sendmailmessages'] = ' Enviar correos electrónicos con mis nuevas entradas';
+$string['sendmailmessages'] = 'Enviar correos electrónicos con mis nuevas entradas';
 $string['status'] = 'Estatus';
 $string['studenttostudent'] = 'Estudiante a Estudiante';
 $string['subject'] = 'Tema';
@@ -61,8 +61,8 @@ $string['subjectadded'] = 'Tema agregado';
 $string['teachertostudent'] = 'Profesor a Estudiante';
 $string['typefirstentry'] = 'Escriba la primera entrada aquí';
 $string['typefollowup'] = 'Escriba la continuación aquí';
-$string['typeofdialogue'] = 'Tipo de charla';
+$string['typeofdialogue'] = 'Tipo de diálogo';
 $string['typereply'] = 'Escriba la respuesta aquí';
-$string['viewallentries'] = ' Ver entradas a charla de $a';
+$string['viewallentries'] = ' Ver entradas a diálogo de $a';
 
 ?>
--- lang/es/resource.php
+++ lang/es/resource.php
@@ -2,7 +2,7 @@
       // resource.php - created with Moodle 1.3 (2004052500)
 
 
-$string['addresource'] = 'Agregar material';
+$string['addresource'] = 'Agregar recurso';
 $string['chooseafile'] = 'Elija o suba un archivo';
 $string['configdefaulturl'] = 'Este valor se usa como pre-relleno cuando se crea un nuevo recurso basado en URL.';
 $string['configfilterexternalpages'] = 'Activar esta variable ocasionará que todos los recursos externos (páginas web, archivos HTML cargados) se procesen según los filtros definidos en el sitio (como el glosario, los enlaces automáticos, etc.). Esto puede hacer que el rendimiento del sistema disminuya considerablemente --úselo con cuidado, sólo si es necesario.';
@@ -20,7 +20,7 @@ $string['configpopupwidth'] = '¿Qué ancho deben tener por defecto las ventanas \
 $string['configwebsearch'] = 'Cuando agregue una URL como página wueb o como enlace, esta ubicación se ofrece como un sitio para que el usuario busque la URL que desea.';
 $string['directlink'] = 'Enlace directo a este archivo';
 $string['directoryinfo'] = 'Se mostrarán todos los archivos en el directorio elegido.';
-$string['editingaresource'] = 'Editar material';
+$string['editingaresource'] = 'Editar recurso';
 $string['example'] = 'Ejemplo';
 $string['examplereference'] = 'Tobin, K. & Tippins, D (1993) Constructivism as a Referent for Teaching and Learning. En: K. Tobin (Ed.). The Practice of Constructivism in Science Education, pp 3-21, Lawrence-Erlbaum, Hillsdale, NJ.';
 $string['exampleurl'] = 'http://www.ejemplo.com/directorio/archivo.html';
@@ -32,8 +32,8 @@ $string['filtername'] = 'Auto-enlace de recursos';
 $string['fulltext'] = 'Texto completo';
 $string['htmlfragment'] = 'Fragmento HTML';
 $string['maindirectory'] = 'Directorio principal de archivos';
-$string['modulename'] = 'Material';
-$string['modulenameplural'] = 'Materiales';
+$string['modulename'] = 'Recurso';
+$string['modulenameplural'] = 'Recursos';
 $string['neverseen'] = 'Nunca visto';
 $string['newdirectories'] = 'Mostrar los enlaces del directorio';
 $string['newfullscreen'] = 'Llenar la pantalla';
@@ -46,14 +46,14 @@ $string['newstatus'] = 'Mostrar la barra de estado';
 $string['newtoolbar'] = 'Mostrar la barra de herramientas';
 $string['newwidth'] = 'Ancho de la  ventana (en píxeles)';
 $string['newwindow'] = 'Nueva ventana';
-$string['newwindowopen'] = 'Mostrar el material en una nueva ventana emergente (\"popup\")';
+$string['newwindowopen'] = 'Mostrar el recurso en una nueva ventana emergente (\"popup\")';
 $string['note'] = 'Nota';
 $string['notefile'] = 'Para subir más de un archivo (y se puedan ver en la lista) utilice el 
 <A HREF=$a >Administrador de archivos</A>.';
 $string['notypechosen'] = 'Necesita elegir un texto. Regrese e inténtelo de nuevo.';
 $string['popupresource'] = 'Este recurso debe aparecer en una ventana ';
 $string['popupresourcelink'] = 'Si no, haga clic aquí: $a';
-$string['resourcetype'] = 'Tipo de material';
+$string['resourcetype'] = 'Tipo de recurso';
 $string['resourcetype1'] = 'Referencia';
 $string['resourcetype2'] = 'Página web (se verá dentro)';
 $string['resourcetype3'] = 'Archivo cargado';
