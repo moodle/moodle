49cab2670914f6998dd81eab51f6a6e99b09087c
martignoni <martignoni>
2004-05-30 21:16:42 +0000

    New file added

---
+++ lang/fr/block_course_list.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+
+$string['blockname'] = 'Liste des cours';
+$string['allcourses'] = 'L\'administrateur voit tous les cours';
+$string['configadminview'] = '';
+$string['owncourses'] = 'L\'administrateur ne voit que ses propres cours';
+
+?>
