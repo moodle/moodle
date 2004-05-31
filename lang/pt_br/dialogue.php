558a1ede02be8aaf04bd0e57c60a1b4e8c5fc682
moodler <moodler>
2004-05-31 03:14:05 +0000

    translated to pt_br by athanazio may-2004

---
+++ lang/pt_br/dialogue.php
@@ -0,0 +1,70 @@
+<?PHP // $Id$ 
+      // dialogue.php - created with Moodle 1.2 development (2003111400)
+      // translated to pt_br by athanazio may-2004
+
+
+$string['addmynewentries'] = 'Adicione novos itens';
+$string['addmynewentry'] = 'Adicionar meu novo item';
+$string['addsubject'] = 'Adicionar assunto';
+$string['allowmultiple'] = 'Permitir mais de um diálogo com a mesma pessoa';
+$string['allowstudentdialogues'] = 'Permitir diálogos Aluno-Aluno';
+$string['close'] = 'Encerrar';
+$string['closed'] = 'Encerrado';
+$string['confirmclosure'] = 'Você esta prestes a encerrar um diálogo com $a. Diálogos encerrados não podem ser reabertos. Se você fechar este diálogo poderá velo mas não edita-lo, e terá de iniciar outro diálogo para continuar &quot;falando&quot; com esta pessoa.<br/><br/>Tem certeza que quer encerrar este Diálogo ?';
+$string['deleteafter'] = 'Remove Diálogos fechados após (Dias)';
+$string['dialogueclosed'] = 'Diálogo Encerrado';
+$string['dialogueintro'] = 'Introdução do Diálogo';
+$string['dialoguemail'] = '$a->userfrom enviou um novo item no seu diálogo para \'$a->dialogue\' 
+
+Você pode encontrar adicionado ao seu item de diálogo 
+$a->url';
+
+$string['dialoguemailhtml'] = '$a->userfrom enviou um novo item no seu diálogo para \'<i>$a->dialogue</i>\' <br><br>Você pode encontrar adicionado ao seu item de diálogo <a href="$a->url">dialogue</a>.';
+$string['dialoguename'] = 'Nome do diálogo';
+$string['dialogueopened'] = 'Diálogo aberto com $a';
+$string['dialoguewith'] = 'Diálogo com $a';
+$string['everybody'] = 'Todos';
+$string['furtherinformation'] = 'Informações adicionais';
+$string['lastentry'] = 'Último item';
+$string['maildefault'] = 'Email padrão';
+$string['modulename'] = 'Diálogo';
+$string['modulenameplural'] = 'Diálogos';
+$string['namehascloseddialogue'] = '$a encerrou o diálogo';
+$string['newdialogueentries'] = 'Novos itens de diálogo';
+$string['newentry'] = 'Novo item';
+$string['noavailablepeople'] = 'Não há ninguém disponível para dialogar.';
+$string['nopersonchosen'] = 'Nenhuma pessoa escolhida';
+$string['nosubject'] = 'Nenhum assunto informado';
+$string['notextentered'] = 'Nenhum texto informado';
+$string['notstarted'] = 'Você ainda não iniciou este diálogo';
+$string['notyetseen'] = 'Ainda não visto';
+$string['numberofentries'] = 'Número de itens';
+$string['numberofentriesadded'] = 'número de itens adicionados: $a';
+$string['of'] = 'de';
+$string['onwrote'] = 'Em $a escreveu';
+$string['onyouwrote'] = 'Em $a você escreveu';
+$string['open'] = 'Iniciado';
+$string['openadialoguewith'] = 'Iniciar um diálogo com ';
+$string['opendialogue'] = 'Diálogo iniciado';
+$string['opendialogueentries'] = 'Itens de diálogos abertos';
+$string['pane0'] = 'Inicie um diálogo';
+$string['pane1'] = '$a Diálogos esperando respostas suas';
+$string['pane1one'] = '1 Diálogo esperando respostas sua';
+$string['pane2'] = '$a Diálogos esperando respostas de outra pessoa';
+$string['pane2one'] = '1 Diálogo esperando respostas de outra pessoa';
+$string['pane3'] = '$a Diálogos encerrados';
+$string['pane3one'] = '1 Dialogo encerrado';
+$string['seen'] = 'Viu $a atrás';
+$string['sendmailmessages'] = 'Envie email sobre meus novos itens';
+$string['status'] = 'Status';
+$string['studenttostudent'] = 'Estudante para Estudante';
+$string['subject'] = 'Assunto';
+$string['subjectadded'] = 'Assunto adicionado';
+$string['teachertostudent'] = 'Facilitador para Estudante';
+$string['typefirstentry'] = 'Digite seu primeiro item aqui';
+$string['typefollowup'] = 'Digite um encaminhamento aqui';
+$string['typeofdialogue'] = 'Tipo de Diálogo';
+$string['typereply'] = 'Digite um resposta aqui';
+$string['viewallentries'] = 'Veja $a Itens de diálogo';
+
+?>
