fb870aca6fb43d63db66db4b7f57872b40c42790
barias <barias>
2004-06-02 22:48:48 +0000

    Latest updates

---
+++ lang/es/hotpot.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // quiz.php - created with Moodle 1.2 development (2003111400)
+//translators:  You might want to leave the first two items 'as is' in English
+$string['modulename'] = 'Hot Potatoes Quiz';
+$string['modulenameplural'] = 'Hot Potatoes Quizzes';
+$string['strattemptlabel'] = 'Intentos: Puntuación, Tiempo (Duración), [Penalizacones]';
+$string['deleteall'] = 'Eliminar todo';
+$string['clearall'] = 'Limpiar todo';
+$string['really'] = '¿Realmente desea eliminar todos los resultados de este examen?';
+
+?>
