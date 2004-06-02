4d1efb421e99151e3c782a8ac4290ac5dddca216
koenr <koenr>
2004-06-02 21:01:10 +0000

    initial translation by Koen Roggemans

---
+++ lang/nl/activitynames.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // activitynames.php - created with Moodle 1.3 - (2004052400)
+
+
+$string['filtername'] = 'Automatisch linken van activiteiten';
+
+?>
---
+++ lang/nl/block_course_list.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // block_course_list.php - created with Moodle 1.4 development (2004060100)
+
+
+$string['allcourses'] = 'Beheerder ziet alle vakken';
+$string['blockname'] = 'Vakkenlijst';
+$string['configadminview'] = ' ';
+$string['owncourses'] = 'Beheerder ziet eigen vakken';
+
+?>
