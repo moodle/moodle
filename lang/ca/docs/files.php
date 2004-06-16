bfd08796f01f6ffafe8d055c4c871b685c35c735
carlesbellver <carlesbellver>
2004-06-16 11:13:38 +0000

    *** empty log message ***

---
+++ lang/ca/docs/files.php
@@ -0,0 +1,31 @@
+<?php // $Id$
+      // Names of the documentation files
+      // Files with a definition "-" are treated as spacers
+
+$string['-about'] = "Sobre Moodle";
+$string['intro.html'] = "Introducció";
+$string['background.html'] = "Rerefons";
+$string['philosophy.html'] = "Filosofia";
+$string['licence.html'] = "Llicència";
+$string['features.html'] = "Característiques";
+$string['release.html'] = "Notes de llançament";
+$string['future.html'] = "Futur";
+$string['credits.html'] = "Crèdits";
+
+$string['-installation'] = "Administració";
+$string['install.html'] = "Instal·lació";
+$string['faq.html'] = "PMF d'instal·lació";
+$string['installamp.html'] = "Apache, MySQL, PHP";
+$string['upgrade.html'] = "Actualització";
+
+$string['-usage'] = "Ús de Moodle";
+$string['teacher.html'] = "Manual del professorat";
+$string['other.html'] = "Altres documents";
+
+$string['-development'] = "Desenvolupament";
+$string['developer.html'] = "Manual de desenvolupament";
+$string['coding.html'] = "Guia de codificació";
+$string['cvs.html'] = "Ús del CVS";
+
+
+?>
