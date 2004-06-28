6d2c5179d17ee47fc06d03b57a0976cbdfac0699
moodler <moodler>
2004-06-28 05:35:23 +0000

    New files

---
+++ lang/pt_br/block_course_list.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$ 
+      // block_course_list.php - created with Moodle 1.3 Stable (001) (2004052500)
+
+
+$string['allcourses'] = 'Administrador vê todos os cursos';
+$string['blockname'] = 'Lista de Cursos';
+$string['owncourses'] = 'Administrador vê apenas os seus cursos';
+
+?>
---
+++ lang/pt_br/scorm.php
@@ -0,0 +1,37 @@
+<?PHP // $Id$ 
+      // scorm.php - created with Moodle 1.3 Stable (001) (2004052500)
+
+
+$string['autocontinue'] = 'Continuação Automática';
+$string['browse'] = 'Visitar';
+$string['browsed'] = 'Visto';
+$string['browsemode'] = 'Modalidade di visita ao curso';
+$string['chooseapacket'] = 'Escolher ou atualizar um pacote SCORM';
+$string['completed'] = 'Completado';
+$string['coursepacket'] = 'Pacote do curso';
+$string['coursestruct'] = 'Estrutura do curso';
+$string['datadir'] = 'Erro no filesystem:não é possível criar um diretório para os dados do curso';
+$string['entercourse'] = 'Iniciar';
+$string['failed'] = 'Falhou';
+$string['incomplete'] = 'Incompleto';
+$string['mode'] = 'Modalidade';
+$string['modulename'] = 'Scorm';
+$string['modulenameplural'] = 'Scorms';
+$string['next'] = 'Próxima';
+$string['nomanifest'] = 'O manifesto não foi encontrado';
+$string['noreports'] = 'Nenhum relatório disponível';
+$string['normal'] = 'Normal';
+$string['notattempted'] = 'Nenhuma tentativa';
+$string['packagedir'] = 'Erro no filesystem: não é possível criar um diretório para o pacote';
+$string['passed'] = 'Completado';
+$string['prev'] = 'Anterior';
+$string['regular'] = 'Manifesto correto';
+$string['report'] = 'Relatório';
+$string['review'] = 'Rever';
+$string['score'] = 'Resultado';
+$string['trackingloose'] = 'Atenção: os dados relativos ao tracking deste pacote SCORM serão cancelados!';
+$string['validateascorm'] = 'Validar pacote SCORM';
+$string['validation'] = 'Resultado da validação';
+$string['viewallreports'] = 'Ver relatório de $a tentativas';
+
+?>
