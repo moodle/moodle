4a6cfa32d964ab842afdd53bef1502b93a267614
moodler <moodler>
2004-06-28 16:25:14 +0000

    Updated requirements for quiz module

--- mod/quiz/version.php
+++ mod/quiz/version.php
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 $module->version  = 2004060400;   // The (date) version of this module
-$module->requires = 2004052501;   // Requires this Moodle version
+$module->requires = 2004060200;   // Requires this Moodle version
 $module->cron     = 0;            // How often should cron check this module (seconds)?
 
 ?>
