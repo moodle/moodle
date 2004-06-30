26d0dfa02e89c9d3fd19f2e453b25d32f2c943f0
villate <villate>
2004-06-30 20:21:04 +0000

    First translation.

---
+++ lang/pt/docs/files.php
@@ -0,0 +1,31 @@
+<?php // $Id$
+      // Names of the documentation files
+      // Files with a definition "-" are treated as spacers
+
+$string['-about'] = "Sobre o Moodle";
+$string['intro.html'] = "Introdução";
+$string['background.html'] = "História";
+$string['philosophy.html'] = "Filosofia";
+$string['licence.html'] = "Licença";
+$string['features.html'] = "Características";
+$string['release.html'] = "Notas de lançamento";
+$string['future.html'] = "Futuro";
+$string['credits.html'] = "Autoria";
+
+$string['-installation'] = "Administração";
+$string['install.html'] = "Instalação";
+$string['faq.html'] = "FAQ da Instalção";
+$string['installamp.html'] = "Apache, MySQL, PHP";
+$string['upgrade.html'] = "Actualizações";
+
+$string['-usage'] = "Uso do Moodle";
+$string['teacher.html'] = "Manual do Professor";
+$string['other.html'] = "Outros Documentos";
+
+$string['-development'] = "Desenvolvimento";
+$string['developer.html'] = "Manual do programador";
+$string['coding.html'] = "Guia de programação";
+$string['cvs.html'] = "Uso doCVS";
+
+
+?>
