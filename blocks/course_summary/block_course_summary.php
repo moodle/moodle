9b88d1a07c31ebb44c23e32552cbb4460888494d
gustav_delius <gustav_delius>
2004-06-30 12:41:05 +0000

    added editing icon in course summary block

--- blocks/course_summary/block_course_summary.php
+++ blocks/course_summary/block_course_summary.php
@@ -13,6 +13,7 @@ class CourseBlock_course_summary extends MoodleBlock {
     }
 
     function get_content() {
+        global $CFG, $THEME;
 
         if($this->content !== NULL) {
             return $this->content;
@@ -20,6 +21,15 @@ class CourseBlock_course_summary extends MoodleBlock {
 
         $this->content = New stdClass;
         $this->content->text = format_text($this->course->summary, FORMAT_HTML);
+        if (isediting($this->course->id)) {
+            $path = $CFG->wwwroot.'/course';
+            if (empty($THEME->custompix)) {
+                 $pixpath = $path.'/../pix';
+            } else {
+                 $pixpath = $path.'/../theme/'.$CFG->theme.'/pix';
+            }
+            $this->content->text .= "<div align=\"right\"><a href=\"$CFG->wwwroot/admin/site.php\"><img src=\"$pixpath/t/edit.gif\" /></a></div>";
+        }
         $this->content->footer = '';
 
         return $this->content;
