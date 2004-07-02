ea2f17571e3e5da5b24444e2cac95a77bce60090
barias <barias>
2004-07-02 19:21:05 +0000

    Latest updates

---
+++ lang/es/book.php
@@ -0,0 +1,29 @@
+<?PHP // $Id$ 
+      // book.php - created with Moodle 1.4 development (2004062600)
+
+
+$string['addafter'] = 'Agregar un nuevo capítulo';
+$string['chapterscount'] = 'Capítulos';
+$string['chaptertitle'] = 'Título del capítulo';
+$string['confchapterdelete'] = '¿Realmente desea eliminar este capítulo?';
+$string['content'] = 'Contenido';
+$string['faq'] = 'FAQ Libro';
+$string['modulename'] = 'Libro';
+$string['modulenameplural'] = 'Libros';
+$string['navnext'] = 'Siguiente';
+$string['navprev'] = 'Anterior';
+$string['numbering'] = 'Número del capítulo';
+$string['numbering0'] = 'No';
+$string['numbering1'] = 'Numérico';
+$string['numbering2'] = 'Viñetas';
+$string['numbering3'] = 'Sangría';
+$string['printbook'] = 'Imprimir el libro completo';
+$string['printchapter'] = 'Imprimir este capítulo';
+$string['printdate'] = 'Fecha';
+$string['printedby'] = 'Imprimido por';
+$string['subchapter'] = 'Sección';
+$string['toc'] = 'Tabla de contenidos';
+$string['tocwidth'] = 'Seleciona la anchura de la tabla de contenidos de todos los libros.';
+$string['top'] = 'arriba';
+
+?>
