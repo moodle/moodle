a82b352f0b6bffadc9899ffdc35c6332385d11a3
martignoni <martignoni>
2004-07-03 18:26:18 +0000

    New file translated

---
+++ lang/fr/dialogue.php
@@ -0,0 +1,67 @@
+<?php // $Id$ 
+
+$string['addmynewentries'] = 'Ajouter mes nouvelles contributions';
+$string['addmynewentry'] = 'Ajouter ma nouvelle contribution';
+$string['addsubject'] = 'Ajouter un sujet';
+$string['allowmultiple'] = 'Autoriser plus d\'un dialogue avec la même personne';
+$string['allowstudentdialogues'] = 'Autoriser des dialogues entre étudiants';
+$string['close'] = 'Terminer';
+$string['closed'] = 'Terminé';
+$string['confirmclosure'] = 'Vous êtes sur le point de terminer un dialogue avec $a. Les dialogues terminés ne peuvent être réouverts. Une fois terminé, vous pourrez consulter ce dialogue, mais ne pourrez plus y ajouter de contributions.<br /><br />Voulez-vous vraiment terminer ce dialogue ?';
+$string['deleteafter'] = 'Supprimer les dialogues terminés après (jours)';
+$string['dialogueclosed'] = 'Dialogue terminé';
+$string['dialogueintro'] = 'Introduction';
+$string['dialoguemail'] = '$a->userfrom a écrit une nouvelle contribution dans votre dialogue « $a->dialogue ».
+
+Vous pouvez lire cette contribution à la suite de la vôtre :
+
+    $a->url';
+$string['dialoguemailhtml'] = '$a->userfrom a écrit une nouvelle contribution dans votre dialogue «&nbsp;<i>$a->dialogue</i>&nbsp;»<br /><br />Vous pouvez lire cette contribution à la suite de <a href=\"$a->url\">la vôtre</a>.';
+$string['dialoguename'] = 'Nom du dialogue';
+$string['dialogueopened'] = 'Dialogue ouvert avec $a';
+$string['dialoguewith'] = 'Dialogue avec $a';
+$string['everybody'] = 'Tout le monde';
+$string['furtherinformation'] = 'Informations générales';
+$string['lastentry'] = 'Dernière contribution';
+$string['maildefault'] = 'Envoyer les courriels par défaut';
+$string['modulename'] = 'Dialogue';
+$string['modulenameplural'] = 'Dialogues';
+$string['namehascloseddialogue'] = '$a a terminé le dialogue';
+$string['newdialogueentries'] = 'Nouvelles contributions au dialogue';
+$string['newentry'] = 'Nouvelle contribution';
+$string['noavailablepeople'] = 'Il n\'y a personne avec qui dialoguer.';
+$string['nopersonchosen'] = 'Aucun utilisateur choisi';
+$string['nosubject'] = 'Aucun sujet donné';
+$string['notextentered'] = 'Aucun texte saisi';
+$string['notstarted'] = 'Vous n\'avez pas encore commencé ce dialogue';
+$string['notyetseen'] = 'Pas encore consulté';
+$string['numberofentries'] = 'Nombre de contributions';
+$string['numberofentriesadded'] = 'Nombre de contributions ajoutées : $a';
+$string['of'] = 'sur';
+$string['onwrote'] = '$a a écrit';
+$string['onyouwrote'] = '$a, vous avez écrit';
+$string['open'] = 'Ouvert';
+$string['openadialoguewith'] = 'Ouvrir un dialogue avec';
+$string['opendialogue'] = 'Ouvrir le dialogue';
+$string['opendialogueentries'] = 'Contributions aux dialogues ouverts';
+$string['pane0'] = 'Ouvrir un dialogue';
+$string['pane1'] = '$a dialogues attendent une réponse de votre part';
+$string['pane1one'] = 'Un dialogue attend une réponse de votre part';
+$string['pane2'] = '$a dialogues attendent une réponse d\'un autre participant';
+$string['pane2one'] = 'Un dialogue attend une réponse d\'un autre participant';
+$string['pane3'] = '$a dialogues terminés';
+$string['pane3one'] = 'Un dialogue terminé';
+$string['seen'] = 'Consulté il y a $a';
+$string['sendmailmessages'] = 'Envoyer les courriels sur mes nouvelles contributions';
+$string['status'] = 'Statut';
+$string['studenttostudent'] = 'Étudiant <-> Étudiant';
+$string['subject'] = 'Sujet';
+$string['subjectadded'] = 'Sujet ajouté';
+$string['teachertostudent'] = 'Enseignant <-> Étudiant';
+$string['typefirstentry'] = 'Tapez ici la première contribution';
+$string['typefollowup'] = 'Tapez ici la suite';
+$string['typeofdialogue'] = 'Type de dialogue';
+$string['typereply'] = 'Tapez ici votre réponse';
+$string['viewallentries'] = 'Afficher $a contributions';
+
+?>
