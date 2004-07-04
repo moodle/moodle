31cf1ffae1690163604836d29f5785941e5809ad
stronk7 <stronk7>
2004-07-04 23:41:49 +0000

    Actualizado a la version en actual.

--- lang/es/docs/files.php
+++ lang/es/docs/files.php
@@ -1,20 +1,29 @@
 <?PHP // $Id$
       // Names of the documentation files
 
+$string['-about'] = "Acerca de Moodle";
 $string['intro.html'] = "Introducción";
 $string['background.html'] = "Antecedentes";
+$string['philosophy.html'] = "Filosofía";
+$string['licence.html'] = "Licencia";
 $string['features.html'] = "Características";
 $string['release.html'] = "Notas sobre versiones";
-$string['install.html'] = "Instalación";
-$string['upgrade.html'] = "Actualización";
-$string['faq.html'] = "Preguntas frecuentes";
-$string['installamp.html'] = "Apache, MySQL, PHP";
-$string['teacher.html'] = "Manual del Profesor";
-$string['developer.html'] = "Manual del Desarrollador";
-$string['cvs.html'] = "Cómo usar el CVS";
 $string['future.html'] = "Futuro";
 $string['credits.html'] = "Créditos";
-$string['licence.html'] = "Licencia";
-$string['other.html'] = "Documentación adicional";
+
+$string['-installation'] = "Administración";
+$string['install.html'] = "Instalación";
+$string['faq.html'] = "Preguntas frecuentes";
+$string['installamp.html'] = "Apache, MySQL, PHP";
+$string['upgrade.html'] = "Actualización";
+
+$string['-usage'] = "Utilizando Moodle";
+$string['teacher.html'] = "Manual del profesor";
+$string['other.html'] = "Otros documentos";
+
+$string['-development'] = "Desarrollo";
+$string['developer.html'] = "Manual del desarrollador";
+$string['coding.html'] = "Manual de estilo del código";
+$string['cvs.html'] = "Utilizando el CVS";
 
 ?>
