c399e90ff2e6dbebaaf91d2c52580db8ed899351
mits <mits>
2004-07-08 16:37:18 +0000

    Polished.

--- lang/ja/algebra.php
+++ lang/ja/algebra.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // algebra.php - created with Moodle 1.2 nearly beta! (2004022400)
+      // algebra.php - created with Moodle 1.4 development (2004070800)
 
 
 $string['filtername'] = '代数表記法';
