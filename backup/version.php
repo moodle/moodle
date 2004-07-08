68b59d7256fc7b60cda223ff6b2ff7c6792753ca
stronk7 <stronk7>
2004-07-08 06:05:34 +0000

    Now only restored wikis (posts and resources) are re-encoded when restoring to a existing course. Bug 1571. (http://moodle.org/bugs/bug.php?op=show&bugid=1571)
    Merged from MOODLE_13_STABLE

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2004062600;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2004070800;   // The current version is a date (YYYYMMDDXX)
 
 $backup_release = "1.4 development";  // User-friendly version number
