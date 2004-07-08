c288e6d6ee69bf79c3237521333bc4ffade054b0
moodler <moodler>
2004-07-08 07:53:50 +0000

    Update requirement for address_in_subnet function in moodlelib.php

--- mod/quiz/version.php
+++ mod/quiz/version.php
@@ -5,8 +5,8 @@
 //  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2004070700;   // The (date) version of this module
-$module->requires = 2004060200;   // Requires this Moodle version
+$module->version  = 2004070800;   // The (date) version of this module
+$module->requires = 2004070800;   // Requires this Moodle version
 $module->cron     = 0;            // How often should cron check this module (seconds)?
 
 ?>
