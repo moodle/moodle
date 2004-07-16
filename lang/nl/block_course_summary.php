af1cb63f721c29e4c751f066df5374ce574356fc
moodler <moodler>
2004-07-16 16:16:08 +0000

    Updates in Dutch from dev

--- lang/nl/block_course_summary.php
+++ lang/nl/block_course_summary.php
@@ -1,7 +1,8 @@
 <?PHP // $Id$ 
-      // block_course_summary.php - created with Moodle 1.3 development (2004041800)
+      // block_course_summary.php - created with Moodle 1.4 development (2004062600)
 
 
 $string['blockname'] = 'Vak samenvatting';
+$string['siteinfo'] = 'Site informatie';
 
 ?>
