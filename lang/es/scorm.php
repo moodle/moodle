55a7a484097fe3cd29ef7820eb7334c8b135bd21
barias <barias>
2004-07-16 19:56:16 +0000

    Latest updates

--- lang/es/scorm.php
+++ lang/es/scorm.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // scorm.php - created with Moodle 1.3 (2004052500)
+      // scorm.php - created with Moodle 1.4 development (2004070800)
 
 
 $string['autocontinue'] = 'Continuación automática';
@@ -8,6 +8,13 @@ $string['browsed'] = 'Navegado';
 $string['browsemode'] = 'Modo Navegación';
 $string['chooseapacket'] = 'Elegir o actualizar un paquete SCORM';
 $string['completed'] = 'Completado';
+$string['configframesize'] = 'Este valor es el tamaño (en píxels) del marco superior (i.e., el que contiene la navegación) cuando se ejecuta un paquete scorm.';
+$string['configpopup'] = 'Cuando se añade un nuevo paquete scorm que es capaz de mostrarse en una ventana emergente, ¿debería esta opción estar habilitada por defecto?';
+$string['configpopupheight'] = '¿Cuál debería ser la altura por defecto de las nuevas ventanas emergentes?';
+$string['configpopupresizable'] = '¿Debería poderse modificar por defecto el tamaño de las ventanas emergentes?';
+$string['configpopupscrollbars'] = '¿Deberían las ventanas emergentes poderse desplazar por defecto?';
+$string['configpopupstatus'] = '¿Deberían las ventanas emergentes mostrar por defecto la barra de estado?';
+$string['configpopupwidth'] = '¿Cuál debería ser la anchura por defecto de las nuevas ventanas emergentes?';
 $string['coursepacket'] = 'Paquete de curso';
 $string['coursestruct'] = 'Estructura de curso';
 $string['datadir'] = 'Error del Sistema: No se puede crear un directorio de datos del curso';
@@ -17,6 +24,13 @@ $string['incomplete'] = 'Incompleto';
 $string['mode'] = 'Modo';
 $string['modulename'] = 'Scorm';
 $string['modulenameplural'] = 'Scorms';
+$string['newheight'] = 'Altura (en píxels) por defecto de la ventana';
+$string['newresizable'] = 'Permitir modificar el tamaño de la ventana';
+$string['newscrollbars'] = 'Permitir el desplazamiento de la ventana';
+$string['newstatus'] = 'Mostrar la barra de estado';
+$string['newwidth'] = 'Anchura (en píxels) por defecto de la ventana';
+$string['newwindow'] = 'Nueva ventana';
+$string['newwindowopen'] = 'Mostrar este paquete scorm en una nueva ventana emergente';
 $string['next'] = 'Continuar';
 $string['nomanifest'] = 'Manifiesto no encontrado';
 $string['noreports'] = 'No hay informes que mostrar';
@@ -28,6 +42,7 @@ $string['prev'] = 'Anterior';
 $string['regular'] = 'Manifiesto Regular';
 $string['report'] = 'Informe';
 $string['review'] = 'Revisión';
+$string['scoes'] = 'Scoes';
 $string['score'] = 'Puntuación';
 $string['trackingloose'] = 'ADVERTENCIA: ¡Los datos de rastreo de este paquete SCORM se perderán!';
 $string['validateascorm'] = 'Validar un paquete SCORM';
