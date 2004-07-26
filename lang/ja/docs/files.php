de1f7842c5f9512097de5729bd2180cfd64ea64a
mits <mits>
2004-07-26 01:57:57 +0000

    Polished.

--- lang/ja/docs/files.php
+++ lang/ja/docs/files.php
@@ -1,19 +1,31 @@
-<?PHP // $Id$
+<?php // $Id$
       // Names of the documentation files
+      // Files with a definition "-" are treated as spacers
 
+$string['-about'] = "Moodleについて";
 $string['intro.html'] = "イントロダクション";
 $string['background.html'] = "背景";
+$string['philosophy.html'] = "哲学";
+$string['licence.html'] = "ライセンス";
 $string['features.html'] = "特徴";
 $string['release.html'] = "リリースノート";
-$string['install.html'] = "インストール";
-$string['faq.html'] = "インストール FAQ";
-$string['upgrade.html'] = "アップグレード";
-$string['teacher.html'] = "教師用マニュアル";
-$string['developer.html'] = "開発者用マニュアル";
-$string['cvs.html'] = "CVSの利用";
 $string['future.html'] = "将来";
 $string['credits.html'] = "クレジット";
-$string['licence.html'] = "ライセンス";
+
+$string['-installation'] = "管理";
+$string['install.html'] = "インストール";
+$string['faq.html'] = "インストール FAQ";
+$string['installamp.html'] = "アパッチ、MySQL、PHP";
+$string['upgrade.html'] = "アップグレード";
+
+$string['-usage'] = "Moodleの利用";
+$string['teacher.html'] = "教師マニュアル";
+$string['other.html'] = "その他文書";
+
+$string['-development'] = "開発";
+$string['developer.html'] = "開発者マニュアル";
+$string['coding.html'] = "コーディングガイド";
+$string['cvs.html'] = "CVSの使用";
 
 
 ?>
