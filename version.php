812a8400fb3373a9fb013728d0e7bf6323496acb
moodler <moodler>
2004-07-27 15:57:34 +0000

    Aiming for a 1.3.4 soon

--- version.php
+++ version.php
@@ -7,6 +7,6 @@
 
 $version = 2004052503;   // The current version is a date (YYYYMMDDXX)
 
-$release = "1.3.3";      // User-friendly version number
+$release = "1.3.3 +";      // User-friendly version number
 
 ?>
