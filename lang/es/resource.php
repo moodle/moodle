0b72fbdaa222be9fad6654a55c8591b42803e072
moodler <moodler>
2004-07-30 03:27:37 +0000

    Shortened some long lines .. resourcetypefile is still too long ...

--- lang/es/resource.php
+++ lang/es/resource.php
@@ -62,10 +62,10 @@ $string['popupresourcelink'] = 'Si no, haga clic aquí: $a';
 $string['resourcetype'] = 'Tipo de recurso';
 $string['resourcetypedirectory'] = 'Mostrar un directorio';
 $string['resourcetypefile'] = 'Enlazar con un archivo o un sitio web';
-$string['resourcetypehtml'] = 'Componer una nueva página web';
+$string['resourcetypehtml'] = 'Componer una página web';
 $string['resourcetypelabel'] = 'Insertar una etiqueta';
 $string['resourcetypereference'] = 'Enviar una referencia';
-$string['resourcetypetext'] = 'Componer una nueva página de texto';
+$string['resourcetypetext'] = 'Componer una página de texto';
 $string['searchweb'] = 'Buscar una página web';
 $string['variablename'] = 'Nombre de la variable';
 
