a06d368555ac13bae6791b911b095acded2f8ef2
mits <mits>
2004-08-18 17:47:02 +0000

    translated some.

--- lang/ja/install.php
+++ lang/ja/install.php
@@ -2,19 +2,38 @@
       // install.php - created with Moodle 1.4 alpha (2004081500)
 
 
+$string['PHPversion'] = 'PHPバージョン';
+$string['PHPversionerror'] = 'PHPバージョンは4.1.0以上をお使いください';
 $string['admindirerror'] = '設定されたadminディレクトリが間違っています。';
 $string['admindirname'] = 'Adminディレクトリ';
 $string['chooselanguage'] = '言語を選択してください。';
+$string['compatibilitysettings'] = 'PHP設定を確認しています ...';
+$string['configfilenotwritten'] = 'インストールスクリプトは、自動的にあなたの設定を反映したconfig.phpファイルを作成することが出来ませんでした。下記のコードをconfig.phpという名称のファイルにコピーして、Moodleのルートディレクトリに入れてください。';
 $string['configfilewritten'] = 'config.phpが正常に作成されました。';
 $string['configurationcomplete'] = '設定が完了しました。';
 $string['database'] = 'データベース';
 $string['dataroot'] = 'データ';
 $string['datarooterror'] = '「データ」設定が間違っています。';
+$string['dbconnectionerror'] = 'データベース接続エラー。データベース設定を確認してください。';
+$string['dbcreationerror'] = 'データベース作成エラー。設定で提示された名称のデータベースを作成出来ませんでした。';
 $string['dbhost'] = 'ホストサーバ';
 $string['dbpass'] = 'パスワード';
 $string['dbprefix'] = 'テーブル接頭辞';
 $string['dbtype'] = 'タイプ';
 $string['dirroot'] = 'ディレクトリ';
+$string['gdversion'] = 'GDバージョン';
+$string['gdversionerror'] = 'イメージの処理及び作成を行うにはGDライブラリが必要です。';
+$string['gdversionhelp'] = 'イメージの処理及び作成を行うにはGDライブラリが必要です。';
+$string['installation'] = 'インストール';
+$string['memorylimit'] = 'メモリー制限';
+$string['phpversionhelp'] = 'MoodleではPHPバージョン4.1.0以上が必要です。現在、バージョン $a が動作しています。';
+$string['safemode'] = 'セーフモード';
+$string['safemodeerror'] = 'セーフモードがONの場合、Moodleはファイルを正常に処理できません。';
+$string['safemodehelp'] = 'セーフモードがONの場合、Moodleはファイルを正常に処理できません。';
+$string['sessionautostart'] = 'セッション自動スタート';
+$string['sessionsavepath'] = 'セッション保存パス';
+$string['sessionsavepatherror'] = 'サーバがセッションをサポートしていません。';
+$string['sessionsavepathhelp'] = 'Moodleはセッションのサポートを必要とします。';
 $string['wwwroot'] = 'WWW';
 $string['wwwrooterror'] = '「WWW」設定が間違っています。';
 
