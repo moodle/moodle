09869fc6c8be7e9add46535202810ed32725dded
martignoni <martignoni>
2004-08-18 11:18:17 +0000

    New string 'file not found' and other tweaks

--- lang/fr/error.php
+++ lang/fr/error.php
@@ -3,16 +3,17 @@
 $string['coursegroupunknown'] = 'Cours correspondant au groupe $a non indiqué';
 $string['erroronline'] = 'Erreur à la ligne $a';
 $string['fieldrequired'] = '« $a » est un champ obligatoire';
+$string['filenotfound'] = 'Le fichier demandé n\'a pas été trouvé';
 $string['groupalready'] = 'L\'utilisateur appartient déjà au groupe $a';
 $string['groupunknown'] = 'Le groupe $a n\'est pas associé au groupe indiqué';
 $string['invalidfieldname'] = '« $a » n\'est pas un nom de champ valable';
 $string['missingfield'] = 'Le champ « $a » est manquant';
 $string['modulerequirementsnotmet'] = 'Le module « $a->modulename » ($a->moduleversion) n\'a pas pu être installé. Il requiert une version plus récente de Moodle (votre version actuelle est $a->currentmoodle, vous avez besoin de la version $a->requiremoodle).';
 $string['notavailable'] = 'Ceci n\'est actuellement pas disponible';
-$string['restricteduser'] = 'Désolé, votre compte utilisateur « $a » ne vous permet pas d\'effectuer cette opération.';
+$string['restricteduser'] = 'Votre compte utilisateur « $a » ne vous permet pas d\'effectuer cette opération.';
 $string['unknowncourse'] = 'Le cours « $a » est inconnu';
-$string['usernotaddederror'] = 'L\'utilisateur « $a » n\'a pas été ajouté - erreur inconnue';
-$string['usernotaddedregistered'] = 'L\'utilisateur « $a » n\'a pas été ajouté - il est déjà référencé';
+$string['usernotaddederror'] = 'L\'utilisateur « $a » n\'a pas été ajouté : erreur inconnue';
+$string['usernotaddedregistered'] = 'L\'utilisateur « $a » n\'a pas été ajouté ; il est déjà référencé';
 $string['usernotavailable'] = 'Vous n\'avez pas l\'autorisation de consulter les coordonnées de cet utilisateur.';
 
 ?>
