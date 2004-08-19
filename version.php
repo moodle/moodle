2dd899c85a9caf6dcc26bbaceb54875e37b160f9
moodler <moodler>
2004-08-19 17:15:24 +0000

    Bump

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 // database to determine whether upgrades should
 // be performed (see lib/db/*.php)
 
-$version = 2004081500;   // The current version is a date (YYYYMMDDXX)
+$version = 2004081900;   // The current version is a date (YYYYMMDDXX)
 
 $release = "1.4 alpha";   // User-friendly version number
 
