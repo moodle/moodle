ed1354d31e29880d95e98d51184761094e47d683
koenr <koenr>
2004-08-22 10:25:55 +0000

    typo

--- lang/nl/enrol_internal.php
+++ lang/nl/enrol_internal.php
@@ -4,7 +4,7 @@
 
 $string['description'] = 'Dit is de standaard manier van aanmelden. Er zijn twee belangrijke manieren waarop een leerling aangemeld kan worden in een vak.
 <ul>
-<li>Een leraar of een beheerder kan de leerlingen met de hand aanmelder door de link te gebruiken in het beheermenu van het vak.</li>
+<li>Een leraar of een beheerder kan de leerlingen met de hand aanmelden door de link te gebruiken in het beheermenu van het vak.</li>
 <li>Er kan een wachtwoord ingegeven worden voor een vak, de \"vaksleutel\". Wie deze sleutel kent, kan zichzelf aanmelden in een vak.</li>
 </ul>';
 $string['enrolname'] = 'Interne aanmelding';
