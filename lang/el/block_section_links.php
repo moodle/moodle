7a89b30fac001b0fa55a2d03732adc9e367db599
defacer <defacer>
2004-08-24 12:27:20 +0000

    I may not be as productive a translator as Koen, but the ? lines in my CVS updates were approaching the third page boundary! ;-)

---
+++ lang/el/block_section_links.php
@@ -0,0 +1,9 @@
+<?PHP // $Id$
+
+$string['blockname'] = 'Ενότητες';
+$string['topics'] = 'Ενότητες';
+$string['jumptocurrenttopic'] = 'Τρέχουσα ενότητα';
+$string['jumptocurrentweek'] = 'Τρέχουσα εβδομάδα';
+$string['weeks'] = 'Εβδομάδες';
+?>
+
