785d24f0f6bd98bdaad6addd66b4c75f6d620bf5
moodler <moodler>
2004-08-25 13:37:38 +0000

    FIx for recent activity when sockets being used

--- mod/chat/lib.php
+++ mod/chat/lib.php
@@ -160,13 +160,14 @@ function chat_print_recent_activity($course, $isteacher, $timestart) {
 
     $timeold = time() - $CFG->chat_old_ping;
 
+    $lastpingsearch = ($CFG->chat_method == 'sockets') ? "": "AND cu.lastping > '$timeold'";
+
     if (!$chatusers = get_records_sql("SELECT u.id, cu.chatid, u.firstname, u.lastname
                                         FROM {$CFG->prefix}chat_users as cu,
                                              {$CFG->prefix}chat as ch,
                                              {$CFG->prefix}user as u
                                        WHERE cu.userid = u.id
-                                         AND cu.chatid = ch.id
-                                         AND cu.lastping > '$timeold'
+                                         AND cu.chatid = ch.id $lastpingsearch
                                          AND ch.course = '$course->id'
                                        ORDER BY cu.chatid ASC") ) {
         return false;
