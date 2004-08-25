56c9574d13ad1c6293c2e74ec491584ca7b5acc2
mits <mits>
2004-08-25 23:44:40 +0000

    Corrected miss-encoding(from shift_jis to EUC-JP)

--- lang/ja/block_social_activities.php
+++ lang/ja/block_social_activities.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // block_social_activities.php - created with Moodle 1.4 development (2004072901)
+      // block_social_activities.php - created with Moodle 1.5 development (2004082300)
 
 
 $string['blockname'] = '社会活動';
--- lang/ja/tex.php
+++ lang/ja/tex.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // tex.php - created with Moodle 1.2 nearly beta! (2004022400)
+      // tex.php - created with Moodle 1.5 development (2004082300)
 
 
 $string['filtername'] = 'TexÉ½µ­Ë¡';
