069a098f642f065e06013204a2cd17ea48547b16
koenr <koenr>
2004-08-27 18:49:59 +0000

    filling in missing strings

--- lang/nl/chat.php
+++ lang/nl/chat.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // chat.php - created with Moodle 1.5 development (2004082300)
+      // chat.php - created with Moodle 1.5 development (2004082600)
 
 
 $string['beep'] = 'Beep';
@@ -8,7 +8,7 @@ $string['chatname'] = 'Naam van deze chatroom';
 $string['chatreport'] = 'Chatsessies';
 $string['chattime'] = 'Volgende chat-tijd';
 $string['configmethod'] = 'De gewone chatmethode laat de clients regelmatig contact opnemen met de server voor updates. Deze methode vraagt geen configuratie en werkt op alle servers, maar kan een zware belasting op de server veroorzaken wanneer er veel chatters tegelijk online zijn. Om een serverdaemon te kunnen gebruiken heb je  shell-toegang tot Unix nodig, maar je krijgt er een snelle, schaalbare chatomgeving mee.';
-$string['configoldping'] = 'Na hoeveel tijd zonder activiteit wil je een gebruiker als weg uit de chat beschouwen?';
+$string['configoldping'] = 'Na hoeveel tijd zonder activiteit wil je een gebruiker als weg uit de chat beschouwen (in seconden)? Dit is slechts een maximumlimiet. Verbroken verbindingen worden gewoonlijk heel vlug gedetecteerd. Kleinere waarden zullen je server zwaarder belasten. Als je de normale methode gebruikt, zet deze instelling dan <strong>nooit</strong> lager dan 2* chat_refresh_room';
 $string['configrefreshroom'] = 'Hoe vaak moet de chatroom ververst worden? (in seconden). Als je deze waarde laag in stelt kan de chatroom sneller lijken te werken, maar het kan ook, als veel mensen aan het chatten zijn, een hogere belasting op de webserver veroorzaken.';
 $string['configrefreshuserlist'] = 'Hoe vaak moet de lijst met gebruikers ververst worden? (in seconden)';
 $string['configserverhost'] = 'De naam van de computer waarop de server daemon geïnstalleerd is';
@@ -22,6 +22,10 @@ $string['deletesessionsure'] = 'Ben je er zeker van dat je deze sessie wil verwi
 $string['donotusechattime'] = 'Publiceer geen chat-tijden';
 $string['enterchat'] = 'Klik hier om de chat nu binnen te gaan';
 $string['errornousers'] = 'Kon geen gebruikers vinden!';
+$string['explaingeneralconfig'] = 'Deze instellingen worden <strong>altijd</strong> gebruikt';
+$string['explainmethoddaemon'] = 'Deze instellingen worden <strong>alleen maar</strong> gebruikt als je de \"Chat server daemon\" gekozen hebt bij chat_method';
+$string['explainmethodnormal'] = 'Deze instellingen worden <strong>alleen maar</strong> gebrukt als je \"Normale methode\" gekozen hebt bij chat_method';
+$string['generalconfig'] = 'Algemene instellingen';
 $string['helpchatting'] = 'Help met chatten';
 $string['idle'] = 'Wachtend';
 $string['messagebeepseveryone'] = '$a piept iedereen!';
