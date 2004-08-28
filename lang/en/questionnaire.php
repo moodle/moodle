5a76a21f2fb8c0e20fc8edff766c2e928ce51282
moodler <moodler>
2004-08-28 07:35:59 +0000

    Keep Questionnaire language files in main pack

---
+++ lang/en/questionnaire.php
@@ -0,0 +1,14 @@
+<?php
+$string['modulename'] = 'Questionnaire';
+$string['modulenameplural'] = 'Questionnaires';
+$string['qmanage'] = 'Manage Surveys';
+$string['qmanagetitle'] = 'Survey Management phpESP';
+$string['qtype'] = "Type";
+$string['respondenttype'] = "Respondent Type";
+$string['questionnaireid'] = "Survey";
+$string['notavail'] = "That questionnaire is not available yet. Try again later.";
+$string['mustcomplete'] = "<b>You must complete this questionnaire <i>now</i> to record your results. "
+						 ."You will not be allowed to complete it at another time</b><br><br>\n";
+$string['alreadyfilled'] = "You have already filled out this questionnaire for us. Thank you.";
+
+?>
