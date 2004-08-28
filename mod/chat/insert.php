223b5ee6199fa12d2e9b9e768c1552b265458dc5
moodler <moodler>
2004-08-28 07:57:57 +0000

    Merged changes from trunk

--- mod/chat/insert.php
+++ mod/chat/insert.php
@@ -27,7 +27,7 @@
 
 /// Clean up the message
 
-    $chat_message = clean_text($chat_message, FORMAT_MOODLE);  // Strip bad tags
+    $chat_message = addslashes(clean_text(stripslashes($chat_message), FORMAT_MOODLE));  // Strip bad tags
 
 /// Add the message to the database
 
