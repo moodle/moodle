efc2f1aa4c413052f5cd2a9edc63d29482820e1a
koenr <koenr>
2004-08-29 19:03:36 +0000

    translation provided by Jean-Pierre Pawlak

---
+++ lang/nl/hotpot.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // hotpot.php - created with Moodle 1.5 development (2004082600)
+
+
+$string['clearall'] = 'Wis keuzes';
+$string['deleteall'] = 'Wis alles';
+$string['modulename'] = 'Hot Potatoes test';
+$string['modulenameplural'] = 'Hot Potatoes testen';
+$string['really'] = 'Wil je echt alle resultaten van deze test wissen?';
+$string['strattemptlabel'] = 'Pogingen: Score, Datum Tijd (Duur), [strafpunten]';
+
+?>
