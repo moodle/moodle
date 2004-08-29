420b3d092c4a680f74a72d9c5123ae6c312c13d2
stronk7 <stronk7>
2004-08-29 10:14:56 +0000

    Backup is ready for 1.4! Bump.

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2004080702;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2004083100;   // The current version is a date (YYYYMMDDXX)
 
-$backup_release = "1.4 beta";  // User-friendly version number
+$backup_release = "1.4";  // User-friendly version number
