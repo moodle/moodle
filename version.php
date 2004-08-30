5ff4353744cc6a556b84a22a2ddecf9813bdfd22
moodler <moodler>
2004-08-30 03:40:56 +0000

    Bump to cater for SITEID

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 // database to determine whether upgrades should
 // be performed (see lib/db/*.php)
 
-$version = 2004082900;   // The current version is a date (YYYYMMDDXX)
+$version = 2004083000;   // The current version is a date (YYYYMMDDXX)
 
 $release = "1.5 development";   // User-friendly version number
 
