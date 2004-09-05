5930c3000555b7c595cf6267d5899c88531f416f
martignoni <martignoni>
2004-09-05 08:55:13 +0000

    Correct encoding!

--- lang/fr/admin.php
+++ lang/fr/admin.php
@@ -1,13 +1,13 @@
 <?php // $Id$
 
-$string['cachetext'] = 'DurÈe de vie du cache texte';
-$string['filteruploadedfiles'] = 'Filtrer les fichiers dÈposÈs';
-$string['upgradelogs'] = 'Pour profiter de toutes les fonctionnalitÈs, les historiques (logs) doivent Ítre mis ý jour. <a href=\"$a\">Plus d\'informations</a>';
-$string['upgradelogsinfo'] = 'Des changements ont eu lieu quant ý la faÁon dont les historiques sont conservÈs. Afin de pouvoir profiter de l\'affichage des historiques par activitÈ, ceux-ci doivent Ítre mis ý jour. Selon la taille de votre site, cette opÈration peut durer assez longtemps (c\'est-ý-dire plusieurs heures) et peut ralentir les grosses bases de donnÈes. Une fois l\'opÈration dÈmarrÈe, vous devez la laisser se terminer (en laissant ouverte la fenÍtre du navigateur). N\'ayez crainte, votre site continuera ý trËs bien fonctionner pour les autres utilisateurs durant la mise ý jour des historiques.<br /><br />Voulez-vous mettre ý jour maintenant vos fichiers d\'historique&nbsp;?';
+$string['cachetext'] = 'Durée de vie du cache texte';
+$string['filteruploadedfiles'] = 'Filtrer les fichiers déposés';
+$string['upgradelogs'] = 'Pour profiter de toutes les fonctionnalités, les historiques (logs) doivent être mis à jour. <a href=\"$a\">Plus d\'informations</a>';
+$string['upgradelogsinfo'] = 'Des changements ont eu lieu quant à la façon dont les historiques sont conservés. Afin de pouvoir profiter de l\'affichage des historiques par activité, ceux-ci doivent être mis à jour. Selon la taille de votre site, cette opération peut durer assez longtemps (c\'est-à-dire plusieurs heures) et peut ralentir les grosses bases de données. Une fois l\'opération démarrée, vous devez la laisser se terminer (en laissant ouverte la fenêtre du navigateur). N\'ayez crainte, votre site continuera à très bien fonctionner pour les autres utilisateurs durant la mise à jour des historiques.<br /><br />Voulez-vous mettre à jour maintenant vos fichiers d\'historique&nbsp;?';
 $string['upgradesure'] = 'Certains fichiers de votre Moodle ont été modifiés, et vous êtes sur le point<br />de lancer la procédure automatique de mise à jour de votre serveur vers cette version&nbsp;:
   	 <p><b>$a</b></p>
   	 <p>Une fois cette mise à jour effectuée, il sera impossible de revenir en arrière&nbsp;!</p>
   	 <p>Voulez-vous vraiment mettre à jour ce Moodle vers cette version&nbsp;?</p>';
-$string['upgradinglogs'] = 'Mise ý jour des fichiers d\'historique (logs)';
+$string['upgradinglogs'] = 'Mise à jour des fichiers d\'historique (logs)';
 
 ?>
