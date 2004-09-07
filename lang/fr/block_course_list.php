8158c4fdccb62446e741cceaa5ff8f27b599806b
martignoni <martignoni>
2004-09-07 19:45:33 +0000

    Completion of string

--- lang/fr/block_course_list.php
+++ lang/fr/block_course_list.php
@@ -2,7 +2,7 @@
 
 $string['blockname'] = 'Liste des cours';
 $string['allcourses'] = 'L\'administrateur voit tous les cours';
-$string['configadminview'] = '';
+$string['configadminview'] = 'Configuration de l\'affichage de l\'administrateur';
 $string['owncourses'] = 'L\'administrateur ne voit que ses propres cours';
 
 ?>
