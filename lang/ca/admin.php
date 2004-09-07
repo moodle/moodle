415ce9bd4519308e56620e353198dd5f9607d452
carlesbellver <carlesbellver>
2004-09-07 10:51:05 +0000

    *** empty log message ***

--- lang/ca/admin.php
+++ lang/ca/admin.php
@@ -8,7 +8,7 @@ $string['upgradelogs'] = 'Per a disposar de totes les funcionalitats, els vostre
 $string['upgradelogsinfo'] = 'S\'han introduït alguns canvis en la manera d\'emmagatzemar els registres. Per tal de poder veure tots els vostres registres vells per activitat, els vostres registres vells s\'han d\'actualitzar. Depenent del vostre servidor això pot trigar una bona estona (unes quantes hores) i en una instal·lació gran pot carregar una mica la base de dades. Una vegada hàgeu engegat aquest procés haureu de deixar que acabi (mantenint la finestra del navegador oberta). No us amoïneu: el vostre lloc seguirà actiu per als usuaris mentre els registres s\'actualitzen. <br /><br />Voleu actualitzar els registres ara?';
 $string['upgradesure'] = 'Els vostres fitxers de Moodle han canviat i esteu a punt d\'actualitzar automàticament el servidor a aquesta versió:
 <p><b>$a</b></p>
-<p>En acabat de fer això no podreu tornar enrere.</p> 
+<p>Després de fer això no podreu tornar enrere.</p> 
 <p>Esteu segur que voleu actualitzar aquest servidor a aquesta versió?</p>';
 $string['upgradinglogs'] = 'S\'estan actualitzant els registres';
 
