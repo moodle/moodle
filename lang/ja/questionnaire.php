1d59bcdf6f16ead6cbd9da369888244fbb1476b3
mits <mits>
2004-09-12 13:32:53 +0000

    New translated files added. Now another translator in Japanese moodle!!

--- lang/ja/questionnaire.php
+++ lang/ja/questionnaire.php
@@ -2,13 +2,16 @@
       // questionnaire.php - created with Moodle 1.5 unstable development (2004091100)
 
 
-$string['alreadyfilled'] = 'あなたは既にこの質問に回答しています。ありがとうございます。';
-$string['modulename'] = '質問';
-$string['modulenameplural'] = '質問';
+$string['alreadyfilled'] = 'あなたは既にこのアンケートに回答しています。ありがとうございます。';
+$string['modulename'] = 'アンケート';
+$string['modulenameplural'] = 'アンケート';
+$string['mustcomplete'] = '<b>アンケート結果を保存する為には<i>今</i>このアンケートを完了させる必要があります。別の機会に回答することは出来ません。</b><br><br>';
 $string['notavail'] = 'この質問は利用できません。後でお試しください。';
 $string['qmanage'] = '調査の管理';
+$string['qmanagetitle'] = 'Survey Management phpESP';
 $string['qtype'] = 'タイプ';
 $string['questionnaireid'] = '調査';
+$string['respondenttype'] = '記名の有無';
 $string['thischarset'] = 'EUC-JP';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Japanese';
