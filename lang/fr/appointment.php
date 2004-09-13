378f858b49d86cb6fc2a2ab920c4d477b83e252f
martignoni <martignoni>
2004-09-13 12:45:48 +0000

    New file added

---
+++ lang/fr/appointment.php
@@ -0,0 +1,45 @@
+<?php // $Id$ 
+
+// Temporary translation, to be re-worked when module in stable versionof Moodle
+$string['allowresubmit'] = 'Permettre la répétition';
+$string['appointmentdetails'] = 'Détails du rendez-vous';
+$string['appointmentname'] = 'Nom du rendez-vous';
+$string['appointmentlocation'] = 'Lieu du rendez-vous';
+$string['description'] = 'Description';
+$string['date'] = 'Date du rendez-vous';
+$string['timestart'] = 'Début du rendez-vous';
+$string['timeend'] = 'Fin du rendez-vous';
+$string['early'] = '$a en avance';
+$string['failedupdatefeedback'] = 'Échec de la mise à jour du feedback pour l\'utilisateur $a';
+$string['feedback'] = 'Feedback';
+$string['feedbackupdated'] = 'Feedbacks mis à jour pour $a personnes';
+$string['late'] = '$a en retard';
+$string['maximumgrade'] = 'Note ';
+$string['maximumsize'] = 'Taille maximale';
+$string['modulename'] = 'Rendez-vous';
+$string['modulenameplural'] = 'Rendez-vous';
+$string['newsubmissions'] = 'Rendez-vous envoyés';
+$string['notgradedyet'] = 'Pas encore de note';
+$string['notsubmittedyet'] = 'Pas encore envoyé'; // remis ?
+$string['overwritewarning'] = 'Attention ! Ce nouveau téléchargement remplacera votre fichier déjà remis';
+$string['saveallfeedback'] = 'Enregistrer tous mes feedbacks';
+$string['submissionfeedback'] = 'Mise à jour des feedbacks';
+$string['submissions'] = 'Rendez-vous envoyés';
+$string['submitappointment'] = 'Envoyez votre rendez-vous à l\'aide de ce formulaire';
+$string['submitted'] = 'Envoyé';
+$string['typeoffline'] = 'Activité hors ligne';
+$string['typeuploadsingle'] = 'Déposer un fichier';
+$string['uploadbadname'] = 'Ce nom de fichier contient d\'étranges caractères. Il ne peut être déposé';
+$string['uploadedfiles'] = 'fichiers déposés';
+$string['uploaderror'] = 'Une erreur est survenue lors de l\'enregistrement du fichier sur le serveur';
+$string['uploadfailnoupdate'] = 'Le fichier a été correctement déposé, mais il n\'a pas été possible de mettre à jour votre devoir !';
+$string['uploadfiletoobig'] = 'Désolé, la taille de ce fichier est trop grande (taille maximale: $a octets)';
+$string['uploadnofilefound'] = 'Aucun fichier n\'a été trouvé, êtes-vous certain d\'en avoir choisi un à déposer ?';
+$string['uploadnotregistered'] = '« $a » a été correctement déposé, mais votre devoir n\'a pu être enregistré !';
+$string['uploadsuccess'] = '« $a » a été correctement déposé';
+$string['viewfeedback'] = 'Afficher les notes et feedbacks des rendez-vous';
+$string['viewsubmissions'] = 'Afficher les $a rendez-vous envoyés';
+$string['yoursubmission'] = 'Votre devoir';
+$string['yoursubmission'] = 'Votre rendez-vous';
+
+?>
