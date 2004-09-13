6cf88a6d7db9a44799cb402eb139d15af92a40b7
mits <mits>
2004-09-13 09:21:58 +0000

    Polished.

--- lang/ja/choice.php
+++ lang/ja/choice.php
@@ -2,26 +2,26 @@
       // choice.php - created with Moodle 1.5 unstable development (2004091100)
 
 
-$string['allowupdate'] = '選択の更新を許可する';
+$string['allowupdate'] = '投票の更新を許可する';
 $string['answered'] = '回答済み';
 $string['choice'] = '選択肢 $a';
 $string['choiceclose'] = '終了日時';
-$string['choicename'] = '選択名';
+$string['choicename'] = '投票名';
 $string['choiceopen'] = '開始日時';
-$string['choicetext'] = '選択文';
-$string['havetologin'] = '選択を実行する前にログインしてください。';
-$string['modulename'] = '選択';
-$string['modulenameplural'] = '選択';
+$string['choicetext'] = '説明文';
+$string['havetologin'] = '投票を実行する前にログインしてください。';
+$string['modulename'] = '投票';
+$string['modulenameplural'] = '投票';
 $string['mustchooseone'] = '保存する前に答えを選択する必要があります。保存処理は行われませんでした。';
 $string['notanswered'] = '未回答';
 $string['notopenyet'] = '申し訳ございません。この活動は $a まで利用できません。';
 $string['privacy'] = 'プライバシー';
 $string['publish'] = '結果の公開';
 $string['publishafteranswer'] = '回答の後に学生に結果を表示する';
-$string['publishafterclose'] = '選択が終了した後のみに学生に結果を表示する';
+$string['publishafterclose'] = '投票が終了した後のみに学生に結果を表示する';
 $string['publishalways'] = '常に学生に結果を表示する';
 $string['publishanonymous'] = '結果を匿名で公開する、学生の氏名を表示しない';
-$string['publishnames'] = '結果を全て公開する、学生の氏名と選択を表示する';
+$string['publishnames'] = '結果を全て公開する、学生の氏名と投票内容を表示する';
 $string['publishnot'] = '学生に結果を公開しない';
 $string['responses'] = '回答';
 $string['responsesto'] = '$a へ回答する';
@@ -30,7 +30,7 @@ $string['showunanswered'] = '未回答欄を表示する';
 $string['thischarset'] = 'EUC-JP';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Japanese';
-$string['timerestrict'] = '回答期間の制限';
+$string['timerestrict'] = '回答期限';
 $string['viewallresponses'] = '回答を表示($a 件)';
 
 ?>
