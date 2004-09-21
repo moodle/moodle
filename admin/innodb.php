0f5706a3f39d684212345549bdfec05a42f8602a
moodler <moodler>
2004-09-21 08:40:18 +0000

    Working but unpolished script to convert all MySQL tables to InnoDB format
    Not linked, just call it directly

---
+++ admin/innodb.php
@@ -0,0 +1,29 @@
+<?php
+
+   include('config.php');
+
+   require_login();
+
+   if (!isadmin) {
+       error('Admin only');
+   }
+
+   print_header("Convert all tables from MYISAM to InnoDB", "Convert all tables from MYISAM to InnoDB", 
+                "Convert all tables from MYISAM to InnoDB");
+
+
+   if ($confirm) {
+
+       print_heading("Please be patient and wait for this to complete...");
+
+       if ($tables = $db->MetaTables()) {
+           $db->debug = true;
+           foreach ($tables as $table) {
+               execute_sql("ALTER TABLE $table TYPE=INNODB; ");
+           }
+       }
+   } else {
+       notice("Are you sure you want convert all your tables to the InnoDB format?", "innodb.php?confirm=yes", "index.php");
+   }
+
+?>
