0a307f230c2dc629dd904573806f5368b5d970d7
paca70 <paca70>
2004-09-21 13:20:41 +0000

    Updates

--- lang/fi/admin.php
+++ lang/fi/admin.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // admin.php - created with Moodle 1.2 Beta +++ (2004031300)
+      // admin.php - created with Moodle 1.5 unstable development (2004092000)
 
 
 $string['cachetext'] = 'Tekstiv‰limuistin kesto';
@@ -8,6 +8,10 @@ $string['upgradelogs'] = 'Tapahtumalokien sis‰llˆt pit‰‰ p‰ivitt‰‰ uuteen muotoo
 $string['upgradelogsinfo'] = 'Tapahtumalokien tallennuksessa on tapahtunut muutoksia. Jotta voisit tarkastella lokeja uudella tavalla, vanhat lokimerkinn‰t pit‰‰ p‰ivitt‰‰ uuteen muotoon. T‰m‰ tapahtuu automaattisesti eik‰ h‰iritse sivustosi muuta k‰yttˆ‰. Riippuen sivustosi koosta t‰h‰n saattaa kuitenkin menn‰ aikaa jopa useita tunteja. Kun aloitat p‰ivityksen, pit‰‰ se antaan menn‰ loppuun asti keskeytt‰m‰tt‰.
 <br /><br />
 Haluatko p‰ivitt‰‰ lokit nyt?';
+$string['upgradesure'] = 'Moodlen asennustiedostot ovat muuttuneet, j‰rjestelm‰ tullaan p‰ivitt‰m‰‰n uuteen versioon automaattisesti. Uusi version on:
+ <p><b>$a</b></p>
+<p>Kun aloitat p‰ivitt‰misen et voi en‰‰ palata takaisin aiempaan versioon.</p>
+<p>Oletko varma ett‰ haluat p‰ivitt‰‰ palvelimen?</p>';
 $string['upgradinglogs'] = 'P‰ivitet‰‰n tapahtumalokeja';
 
 ?>
--- lang/fi/auth.php
+++ lang/fi/auth.php
@@ -1,7 +1,9 @@
 <?PHP // $Id$ 
-      // auth.php - created with Moodle 1.3 development (2004050200)
+      // auth.php - created with Moodle 1.5 unstable development (2004092000)
 
 
+$string['auth_common_settings'] = 'Yleiset asetukset';
+$string['auth_data_mapping'] = 'Tietojen yhdistäminen';
 $string['auth_dbdescription'] = 'Tämä moduli tarkistaa ulkoisen tietokannan taulusta käyttäjätunnuksen ja salasanan. Jos käyttäjätunnus on uusi, myös muita tietoja voidaan kopioda Moodleen.';
 $string['auth_dbextrafields'] = 'Nämä kentät ovat valinnaisia. Voit asettaa Moodlen hakemaan valmiiksi joitakin käyttäjätietoja <b>ulkoisesta tietokannasta</b>.<p>Jos jätät nämä kentät tyhjiksi, käytetään oletusasetusarvoja.</p> Käyttäjä voi joka tapauksessa muuttaa omia henkilötietojaan myöhemmin.';
 $string['auth_dbfieldpass'] = 'Salasanakentännimi';
@@ -14,10 +16,13 @@ $string['auth_dbtable'] = 'Tietokannan taulun nimi';
 $string['auth_dbtitle'] = 'Käytä ulkoista tietokantaa';
 $string['auth_dbtype'] = 'Tietokannan tyyppi (Katso <A HREF=../lib/adodb/readme.htm#drivers>ADOdb dokumentoinnista</A> yksityiskohdat)';
 $string['auth_dbuser'] = 'Käyttäjätunnus tietokantaan lukuoikeuksin';
+$string['auth_editlock'] = 'Lukitse arvo';
+$string['auth_editlock_expl'] = '<p><b>Lukitse arvo:</b> Estää tiedon muokaamisen Moodlen sisältä. </p>';
 $string['auth_emaildescription'] = 'Sähköpostivarmistus on oletusarvoinen tunnistusmetodi käyttäjälle.
 Kun käyttäjä luo itselleen tunnuksen, lähetetään varmistusviesti
 käyttäjälle. Viesti sisältää linkin, minkä avulla käyttäjä voi aktivoida tunnuksensa.';
 $string['auth_emailtitle'] = 'Käytä sähköpostivarmistusta';
+$string['auth_fccreators'] = 'Tämän ryhmän(ryhmien) jäsenet saavat luoda uusia kursseja. Eroittelu useat ryhmänimet \';\'-merkillä. Nimet on oltava täysin samoin kuin FirstClass palvelimella.';
 $string['auth_imapdescription'] = 'Tämä tapa käyttää IMAP-palvelinta käyttäjätunnuksen ja salasanan tarkistamiseen.';
 $string['auth_imaphost'] = 'IMAP-palvelimen osoite. Käytä IP-numeroa, älä domainnimeä.';
 $string['auth_imapport'] = 'IMAP-palvelimen portti, yleensä 143 tai 993.';
@@ -25,14 +30,18 @@ $string['auth_imaptitle'] = 'Käytä IMAP-palvelinta';
 $string['auth_imaptype'] = 'IMAP-palvelimen tyyppi. Katso ohjeesta (yllä) lisätietoja.';
 $string['auth_ldap_bind_dn'] = 'Jos haluat käyttää välityskäyttäjää yhteyden muodostamiseen, määritä se tähän. Esim. \'cn=ldapuser,ou=public,o=org\'';
 $string['auth_ldap_bind_pw'] = 'Salasana välityskäyttäjälle.';
+$string['auth_ldap_bind_settings'] = 'Sidosasetukset';
 $string['auth_ldap_contexts'] = 'Lista konteksteista, missä käyttäjät sijaitsevat. Erota kontekstit toisistaan \';\'-merkillä. Esim: \'ou=users,o=org; ou=others,o=org\'';
 $string['auth_ldap_create_context'] = 'Jos luodaan sähköpostiviestillä tunnuksensa varmentaneet käyttäjät automaattisesti ldap-hakemistoon, määritä tässä konteksti, minne käyttäjät luodaan. On hyvä käyttää jotain eritystä kontekstia, jotta vältyt tietoturvariskeiltä. Tätä kontekstia ei tarvitse erikseen lisätä yllä olevaan muuttujaan.';
 $string['auth_ldap_creators'] = 'Lista ryhmistä, minkä jäsenet voivat luoda uusia kursseja Moodleen. Erota useat ryhmät toisistaan \';\'-merkillä. Esimerkiksi \'cn=teachers,ou=staff,o=myorg;\'';
 $string['auth_ldap_host_url'] = 'Määritä LDAP-palvelin URL-muodossa. Esim. \'ldap://ldap.myorg.com/\' tai \'ldaps://ldap.myorg.com/\' ';
+$string['auth_ldap_login_settings'] = 'Kirjaantumisasetukset';
 $string['auth_ldap_memberattribute'] = 'Määritä käyttäjän ryhmäjäsenyysattribuutti. Yleensä \'member\' tai \'groupMembership\' ';
-$string['auth_ldap_search_sub'] = 'Aseta arvo &lt;&gt; 0, jos haluat hakea käyttäjiä myös alikonteksteista.';
+$string['auth_ldap_search_sub'] = 'Aseta arvo <> 0, jos haluat hakea käyttäjiä myös alikonteksteista.';
+$string['auth_ldap_server_settings'] = 'LDAP palvelimen asetukset';
 $string['auth_ldap_update_userinfo'] = 'Päivitä käyttäjätiedot LDAP:ista Moodleen (etunimi, sukunimi, osoite..). Katso <a href=\"/auth/ldap/attr_mappings.php\">/auth/ldap/attr_mappings.php</a> tarkempia määrittelytietoja.';
 $string['auth_ldap_user_attribute'] = 'Attribuutti käyttäjänimille. Yleensä \'cn\'.';
+$string['auth_ldap_user_settings'] = 'Käyttäjien etsintä';
 $string['auth_ldap_version'] = 'Palvelimella käytettävä LDAP protokolla versio';
 $string['auth_ldapdescription'] = 'Tämä tapa tarjoaa käyttäjätunnistuksen LDAP-palvelimelta. Jos salasana ja tunnus täsmäävät, Moodle luo uuden käyttäjän  tietokantaansa. Jos olet valinnut \'auth_ldap_update_userinfo\'-option, myös käyttäjätiedot kopioidaan LDAP:sta Moodleen.
 
@@ -55,6 +64,8 @@ $string['auth_pop3host'] = 'POP3-palvelimen osoite. Käytä IP-numeroa, älä domain
 $string['auth_pop3port'] = 'POP3-palvelimen portti (yleensä 110 )';
 $string['auth_pop3title'] = 'Käytä POP3-palvelinta';
 $string['auth_pop3type'] = 'Palvelimen tyyppi. Jos käytätte salattua yhteyttä, valitse pop3cert.';
+$string['auth_updatelocal'] = 'Päivitä sisäinen arvo';
+$string['auth_updateremote'] = 'Päivitä ulkoinen arvo';
 $string['auth_user_create'] = 'Käyttäjän luonti';
 $string['auth_user_creation'] = 'Käyttäjät voivat itse luoda tunnuksensa. Käyttäjätiedot tarkistetaan sähköpostin avulla. Jos aktivoit tämän vaihtoehdon, muista myös määritellä autentikointi-modulin muut tähän liittyvät asetukset.';
 $string['auth_usernameexists'] = 'Käyttäjätunnus on jo käytössä. Valitse joku toinen.';
@@ -63,6 +74,7 @@ $string['authinstructions'] = 'Tähän voi kirjoittaa ohjeet opiskelijoille, mitä 
 $string['changepassword'] = 'Salasananvaihto-URL';
 $string['changepasswordhelp'] = 'Tässä osoitteessa käyttäjät voivat vaihtaa unohtamansa salasanan. Käyttäjille tämä näkyy painikkeena kirjautumissivulla ja heidän käyttäjätietosivullaan.';
 $string['chooseauthmethod'] = 'Valitse käyttäjäntunnistusmetodi: ';
+$string['forcechangepassword'] = 'Pakoita salasanan vaihto';
 $string['guestloginbutton'] = 'Kirjaudu vieraana-painike';
 $string['instructions'] = 'Ohjeet';
 $string['md5'] = 'MD5-salaus';
