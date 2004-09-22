28a64f953b60bfd6aad5a61383cd5e6067d0d464
mits <mits>
2004-09-22 19:09:20 +0000

    Polished

--- lang/ja/block_social_activities.php
+++ lang/ja/block_social_activities.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // block_social_activities.php - created with Moodle 1.5 unstable development (2004091100)
+      // block_social_activities.php - created with Moodle 1.5 unstable development (2004092000)
 
 
 $string['blockname'] = '社会活動';
