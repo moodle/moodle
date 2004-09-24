4723e40941374dff60fc9f05b343a00682bfe429
dhawes <dhawes>
2004-09-24 23:18:17 +0000

    Double quote strings converted to single quote literals  - inspired by missing escapes on quote characters inside echo strings found by ottenhoff and noted in comment to bug 1981

--- mod/quiz/multiple.php
+++ mod/quiz/multiple.php
@@ -1,31 +1,31 @@
 <?php // $Id$
       // A quick way to add lots of questions to a category (and a quiz)
 
-    require_once("../../config.php");
-    require_once("lib.php");
+    require_once('../../config.php');
+    require_once('lib.php');
 
     require_variable($category);
 
     // This script can only be called while editing a quiz
 
     if (!isset($SESSION->modform)) {
-        error("You have used this page incorrectly!");
+        error('You have used this page incorrectly!');
     } else {
         $modform = $SESSION->modform;
     }
 
-    if (! $category = get_record("quiz_categories", "id", $category)) {
-        error("Course ID is incorrect");
+    if (! $category = get_record('quiz_categories', 'id', $category)) {
+        error('Course ID is incorrect');
     }
 
-    if (! $course = get_record("course", "id", $category->course)) {
-        error("Course ID is incorrect");
+    if (! $course = get_record('course', 'id', $category->course)) {
+        error('Course ID is incorrect');
     }
 
     require_login($course->id);
 
     if (!isteacher($course->id)) {
-        error("Only teachers can use this page!");
+        error('Only teachers can use this page!');
     }
 
 
@@ -34,7 +34,7 @@
 
     if ($form = data_submitted()) {
         if ($form->randomcreate > 0) {
-            $existing = count_records("quiz_questions", "qtype", RANDOM, "category", $category->id);
+            $existing = count_records('quiz_questions', 'qtype', RANDOM, 'category', $category->id);
             $randomcreate = $form->randomcreate - $existing;
 
             if ($randomcreate > 0) {
@@ -42,21 +42,21 @@
 
                 $question->qtype = RANDOM;
                 $question->category = $category->id;
-                $question->name = get_string("random", "quiz")." (".$category->name.")";
-                $question->questiontext = "---";
-                $question->image = "";
+                $question->name = get_string('random', 'quiz') .' ('. $category->name .')';
+                $question->questiontext = '---';
+                $question->image = '';
                 $question->defaultgrade = $form->randomgrade;
                 for ($i=0; $i<$randomcreate; $i++) {
                     $question->stamp = make_unique_id_code();  // Set the unique code (not to be changed)
-                    if (!$newquestionids[] = insert_record("quiz_questions", $question)) {
-                        error("Could not insert new random question!");
+                    if (!$newquestionids[] = insert_record('quiz_questions', $question)) {
+                        error('Could not insert new random question!');
                     }
                 }
 
                 // Add them to the quiz if necessary
                 if (!empty($form->addquestionstoquiz)) {
                     if (!empty($modform->questions)) {
-                        $questionids = explode(",", $modform->questions);
+                        $questionids = explode(',', $modform->questions);
                         foreach ($questionids as $questionid) {
                             foreach ($newquestionids as $key => $newquestionid) {
                                 if ($newquestionid == $questionid) {
@@ -75,12 +75,12 @@
                     }
 
                     $newquestionids = array_merge($questionids, $newquestionids);
-                    $modform->questions = implode(",", $newquestionids);
+                    $modform->questions = implode(',', $newquestionids);
                     $SESSION->modform = $modform;
                 }
             }
         }
-        redirect("edit.php");
+        redirect('edit.php');
     }
 
 
@@ -88,27 +88,27 @@
 
 /// Print headings
 
-    $strquestions = get_string("questions", "quiz");
-    $strpublish = get_string("publish", "quiz");
-    $strdelete = get_string("delete");
-    $straction = get_string("action");
-    $stradd = get_string("add");
-    $strcancel = get_string("cancel");
-    $strsavechanges = get_string("savechanges");
-    $strbacktoquiz = get_string("backtoquiz", "quiz");
+    $strquestions = get_string('questions', 'quiz');
+    $strpublish = get_string('publish', 'quiz');
+    $strdelete = get_string('delete');
+    $straction = get_string('action');
+    $stradd = get_string('add');
+    $strcancel = get_string('cancel');
+    $strsavechanges = get_string('savechanges');
+    $strbacktoquiz = get_string('backtoquiz', 'quiz');
     $strquizzes = get_string('modulenameplural', 'quiz');
-    $streditingquiz = get_string(isset($SESSION->modform->instance) ? "editingquiz" : "editquestions", "quiz");
-    $strcreatemultiple = get_string("createmultiple", "quiz");
+    $streditingquiz = get_string(isset($SESSION->modform->instance) ? 'editingquiz' : 'editquestions', 'quiz');
+    $strcreatemultiple = get_string('createmultiple', 'quiz');
 
-    print_header_simple("$strcreatemultiple", "$strcreatemultiple",
+    print_header_simple($strcreatemultiple, $strcreatemultiple,
                  "<a href=\"$CFG->wwwroot/mod/quiz/index.php?id=$course->id\">$strquizzes</a>".
                  " -> <a href=\"edit.php\">$streditingquiz</a> -> $strcreatemultiple");
 
 
-    print_heading_with_help($strcreatemultiple, "createmultiple", "quiz");
+    print_heading_with_help($strcreatemultiple, 'createmultiple', 'quiz');
 
     if (!$categories = quiz_get_category_menu($course->id, true)) {
-        error("No categories!");
+        error('No categories!');
     }
 
     for ($i=1;$i<=100; $i++) {
@@ -118,43 +118,43 @@
         $gradecount[$i] = $i;
     }
     $options = array();
-    $options[0] = get_string("no");
-    $options[1] = get_string("yes");
+    $options[0] = get_string('no');
+    $options[1] = get_string('yes');
 
-    print_simple_box_start("center", "", "$THEME->cellheading");
-    echo "<form method=\"POST\" action=\"multiple.php\">";
-    echo "<table cellpadding="5">";
-    echo "<tr><td align="right">";
-    print_string("category", "quiz");
-    echo ":</td><td>";
+    print_simple_box_start('center', '', $THEME->cellheading);
+    echo '<form method="POST" action="multiple.php">';
+    echo '<table cellpadding="5">';
+    echo '<tr><td align="right">';
+    print_string('category', 'quiz');
+    echo ':</td><td>';
     // choose_from_menu($categories, "category", "$category->id", "");
     quiz_category_select_menu($course->id, true, true, $category->id );
-    echo "</tr>";
+    echo '</tr>';
 
-    echo "<tr><td align="right">";
-    print_string("randomcreate", "quiz");
-    echo ":</td><td>";
-    choose_from_menu($randomcount, "randomcreate", "10", "");
-    echo "</tr>";
+    echo '<tr><td align="right">';
+    print_string('randomcreate', 'quiz');
+    echo ':</td><td>';
+    choose_from_menu($randomcount, 'randomcreate', '10', '');
+    echo '</tr>';
 
-    echo "<tr><td align="right">";
-    print_string("defaultgrade", "quiz");
-    echo ":</td><td>";
-    choose_from_menu($gradecount, "randomgrade", "1", "");
-    echo "</tr>";
+    echo '<tr><td align="right">';
+    print_string('defaultgrade', 'quiz');
+    echo ':</td><td>';
+    choose_from_menu($gradecount, 'randomgrade', '1', '');
+    echo '</tr>';
 
-    echo "<tr><td align="right">";
-    print_string("addquestionstoquiz", "quiz");
-    echo ":</td><td>";
-    choose_from_menu($options, "addquestionstoquiz", "1", "");
-    echo "</tr>";
+    echo '<tr><td align="right">';
+    print_string('addquestionstoquiz', 'quiz');
+    echo ':</td><td>';
+    choose_from_menu($options, 'addquestionstoquiz', '1', '');
+    echo '</tr>';
 
-    echo "<tr><td>&nbsp;</td><td>";
-    echo " <input type="hidden" name="category" value=\"$category->id\" />";
-    echo " <input type="submit" name="save" value=\"$strcreatemultiple\" />";
-    echo "</td></tr>";
-    echo "</table>";
-    echo "</form>";
+    echo '<tr><td>&nbsp;</td><td>';
+    echo ' <input type="hidden" name="category" value="'. $category->id .'" />';
+    echo ' <input type="submit" name="save" value="'. $strcreatemultiple .'" />';
+    echo '</td></tr>';
+    echo '</table>';
+    echo '</form>';
     print_simple_box_end();
 
     print_footer();
