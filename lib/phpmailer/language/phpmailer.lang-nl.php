b986e3b007c9d158fe3b6a0aca4baa88b813ced4
moodler <moodler>
2004-09-25 16:01:32 +0000

    Upgraded to phpmailer 1.72 - none of the old hacks necessary any more!

---
+++ lib/phpmailer/language/phpmailer.lang-nl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PHPMailer language file.
+ * Dutch Version
+ */
+
+$PHPMAILER_LANG = array();
+
+$PHPMAILER_LANG["provide_address"] = 'U moet op zijn minst één ontvanger ' .
+                                     'opgeven';
+$PHPMAILER_LANG["mailer_not_supported"] = ' e-mail service wordt niet ondersteund.';
+$PHPMAILER_LANG["execute"] = 'Kan niet worden uitgevoerd: ';
+$PHPMAILER_LANG["instantiate"] = 'Kan mail functie niet op gang brengen.';
+$PHPMAILER_LANG["authenticate"] = 'SMTP fout: Ongeldige gebruikersnaam of wachtwoord.';
+$PHPMAILER_LANG["from_failed"] = 'De volgende afzenders zijn ongeldig: ';
+$PHPMAILER_LANG["recipients_failed"] = 'SMTP Fout: Kon email niet verzend ' .
+                                       'naar de volgende ontvangers : ';
+$PHPMAILER_LANG["data_not_accepted"] = 'SMTP Fout: Data niet geaccepteerd.';
+$PHPMAILER_LANG["connect_host"] = 'SMTP Fout: Kan geen verbinding maken met de SMTP server.';
+$PHPMAILER_LANG["file_access"] = 'Bijlage kon niet worden geopend: ';
+$PHPMAILER_LANG["file_open"] = 'Bijlage kon niet worden geopend: ';
+$PHPMAILER_LANG["encoding"] = 'Onbekende codering: ';
+?>
