c24313bc8c03a71c211f7b324a040abceefe92f1
moodler <moodler>
2004-09-27 13:51:33 +0000

    Little typo

--- version.php
+++ version.php
@@ -6,9 +6,9 @@
 // This is compared against the values stored in the database to determine 
 // whether upgrades should be performed (see lib/db/*.php)
 
-   $version = 2004083110;  // YYYMMDD   = date of first major branch release 1.4
-                           //        X  = point release version 1,2,3 etc
-                           //         Y = increments between point releases
+   $version = 2004083110;  // YYYYMMDD   = date of first major branch release 1.4
+                           //         X  = point release version 1,2,3 etc
+                           //          Y = increments between point releases
 
    $release = '1.4.1+';    // Human-friendly version name
 
