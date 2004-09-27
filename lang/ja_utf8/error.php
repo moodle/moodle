52390e5f601e6645e4146a7753c783c3fea292a5
mits <mits>
2004-09-27 16:14:09 +0000

    polished and translated some.

--- lang/ja_utf8/error.php
+++ lang/ja_utf8/error.php
@@ -1,24 +1,25 @@
 <?PHP // $Id$ 
-      // error.php - created with Moodle 1.5 unstable development (2004092000)
+      // error.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004092700)
 
 
-$string['coursegroupunknown'] = 'コースに関連するグループ $a が指定されていません';
-$string['erroronline'] = '$a 行にエラーがあります';
-$string['fieldrequired'] = '\"$a\" は必須項目です';
+$string['coursegroupunknown'] = 'コースに関連するグループ $a が指定されていません。';
+$string['erroronline'] = '$a 行にエラーがあります。';
+$string['fieldrequired'] = '「 $a 」 は必須項目です。';
 $string['filenotfound'] = '申し訳ございません。ファイルが見つかりませんでした。';
 $string['groupalready'] = 'ユーザは既にグループ $a に属しています';
-$string['groupunknown'] = 'グループ $a は指定されたコースに関連していません';
-$string['invalidfieldname'] = '\"$a\" は正しいフィールド名ではありません';
-$string['missingfield'] = '\"$a\" に入力してください';
-$string['modulerequirementsnotmet'] = 'モジュール \"$a->modulename\" ($a->moduleversion) をインストールできませんでした。このモジュールをインストールするためには新しいバージョンのMoodleをインストールする必要があります。(現在、あなたは $a->currentmoodle を使用しています。 $a->requiremoodle が必要です。)';
+$string['groupunknown'] = 'グループ $a は指定されたコースに関連していません。';
+$string['invalidfieldname'] = '「 $a 」 は正しいフィールド名ではありません。';
+$string['missingfield'] = '「 $a 」 を入力してください。';
+$string['modulerequirementsnotmet'] = 'モジュール「 $a->modulename 」 ($a->moduleversion) をインストールできませんでした。このモジュールをインストールするためには新しいバージョンのMoodleをインストールする必要があります。(現在、あなたは $a->currentmoodle を使用しています。 $a->requiremoodle が必要です。)';
 $string['notavailable'] = '現在利用できません';
-$string['restricteduser'] = '申し訳ございません。現在、この処理を行うことができません。';
+$string['restricteduser'] = '申し訳ございません。あなたのアカウント「 $a 」では、この処理を行うことができません。';
+$string['sessionipnomatch'] = '申し訳ございません。あなたのIPアドレスは、最初のログインから変更されているようです。このセキュリティー機能は、あなたがサイトにログインしている間に、クラッカーから個人情報を盗むことを防いでいます。通常のユーザは、このメッセージを見ることはありません - 詳細はシステム管理者にお問合せください。';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'Japanese';
-$string['unknowncourse'] = '不明なコース名 \"$a\"';
-$string['usernotaddederror'] = 'ユーザ \"$a\" は追加されませんでした - 原因不明';
-$string['usernotaddedregistered'] = 'ユーザ \"$a\" は追加されませんでした - 登録済み';
+$string['unknowncourse'] = '不明なコース名 「 $a 」';
+$string['usernotaddederror'] = 'ユーザ 「 $a 」 は追加されませんでした - 原因不明のエラー';
+$string['usernotaddedregistered'] = 'ユーザ 「 $a 」 は追加されませんでした - 登録済み';
 $string['usernotavailable'] = 'このユーザの詳細は閲覧できません。';
 
 ?>
