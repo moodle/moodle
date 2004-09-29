822a10631986afdecac32ecc432e44753215cffb
dhawes <dhawes>
2004-09-29 18:19:39 +0000

    Added a file level phpdoc docblock tag. Also converted double quote strings to single quote literals.

--- file.php
+++ file.php
@@ -1,9 +1,21 @@
-<?PHP // $Id$
-      // This function fetches files from the data directory
-      // Syntax:   file.php/courseid/dir/.../dir/filename.ext
+<?php
 
-    require_once("config.php");
-    require_once("files/mimetypes.php");
+    /**
+     * file.php - Used to fetch file from the data directory
+     *
+     * This script file fetches files from the data directory (dataroot)<br>
+     * Syntax:   file.php/courseid/dir/.../dir/filename.ext
+     *
+     * @uses $CFG
+     * @uses FORMAT_HTML
+     * @uses FORMAT_MOODLE
+     * @author Martin Dougiamas
+     * @version $Id$
+     * @package moodlecore
+     */
+     
+    require_once('config.php');
+    require_once('files/mimetypes.php');
 
     if (empty($CFG->filelifetime)) {
         $CFG->filelifetime = 86400;     /// Seconds for files to remain in caches
@@ -12,28 +24,28 @@
     if (isset($file)) {     // workaround for situations where / syntax doesn't work
         $pathinfo = $file;
     } else {
-        $pathinfo = get_slash_arguments("file.php");
+        $pathinfo = get_slash_arguments('file.php');
     }
 
     if (!$pathinfo) {
-        error("No file parameters!");
+        error('No file parameters!');
     }
 
     $pathinfo = urldecode($pathinfo);
 
     if (! $args = parse_slash_arguments($pathinfo)) {
-        error("No valid arguments supplied");
+        error('No valid arguments supplied');
     }
 
     $numargs = count($args);
     if ($numargs < 2 or empty($args[1])) {
-        error("No valid arguments supplied");
+        error('No valid arguments supplied');
     }
 
     $courseid = (integer)$args[0];
 
-    if (!$course = get_record("course", "id", $courseid)) {  // Course ID must be specified
-        error("Invalid course ID");
+    if (!$course = get_record('course', 'id', $courseid)) {  // Course ID must be specified
+        error('Invalid course ID');
     }
 
     if ($course->category) {
@@ -42,57 +54,57 @@
         require_login();
     }
 
-    $pathname = "$CFG->dataroot$pathinfo";
-    if ($pathargs = explode("?",$pathname)) {
+    $pathname = $CFG->dataroot . $pathinfo;
+    if ($pathargs = explode('?', $pathname)) {
         $pathname = $pathargs[0];            // Only keep what's before the '?'
     }
     $filename = $args[$numargs-1];
-    if ($fileargs = explode("?",$filename)) {
+    if ($fileargs = explode('?', $filename)) {
         $filename = $fileargs[0];            // Only keep what's before the '?'
     }
 
     if (file_exists($pathname)) {
         $lastmodified = filemtime($pathname);
-        $mimetype = mimeinfo("type", $filename);
+        $mimetype = mimeinfo('type', $filename);
 
-        header("Last-Modified: " . gmdate("D, d M Y H:i:s", $lastmodified) . " GMT");
-        header("Expires: " . gmdate("D, d M Y H:i:s", time() + $CFG->filelifetime) . " GMT");
-        header("Cache-control: max_age = $CFG->filelifetime");
-        header("Pragma: ");
-        header("Content-disposition: inline; filename=$filename");
+        header('Last-Modified: ' . gmdate("D, d M Y H:i:s", $lastmodified) . ' GMT');
+        header('Expires: ' . gmdate("D, d M Y H:i:s", time() + $CFG->filelifetime) . ' GMT');
+        header('Cache-control: max_age = '. $CFG->filelifetime);
+        header('Pragma: ');
+        header('Content-disposition: inline; filename='. $filename);
 
 
         if (empty($CFG->filteruploadedfiles)) {
-            header("Content-length: ".filesize($pathname));
-            header("Content-type: $mimetype");
+            header('Content-length: '. filesize($pathname));
+            header('Content-type: '. $mimetype);
             readfile($pathname);
 
         } else {     /// Try and put the file through filters
-            if ($mimetype == "text/html") {
+            if ($mimetype == 'text/html') {
                 $options->noclean = true;
                 $output = format_text(implode('', file($pathname)), FORMAT_HTML, $options, $courseid);
 
-                header("Content-length: ".strlen($output));
-                header("Content-type: text/html");
+                header('Content-length: '. strlen($output));
+                header('Content-type: text/html');
                 echo $output;
     
-            } else if ($mimetype == "text/plain") {
+            } else if ($mimetype == 'text/plain') {
                 $options->newlines = false;
                 $options->noclean = true;
-                $output = '<pre>'.format_text(implode('', file($pathname)), FORMAT_MOODLE, $options, $courseid).'</pre>';
-                header("Content-length: ".strlen($output));
-                header("Content-type: text/html");
+                $output = '<pre>'. format_text(implode('', file($pathname)), FORMAT_MOODLE, $options, $courseid) .'</pre>';
+                header('Content-length: '. strlen($output));
+                header('Content-type: text/html');
                 echo $output;
     
             } else {    /// Just send it out raw
-                header("Content-length: ".filesize($pathname));
-                header("Content-type: $mimetype");
+                header('Content-length: '. filesize($pathname));
+                header('Content-type: '. $mimetype);
                 readfile($pathname);
             }
         }
     } else {
-        header("HTTP/1.0 404 not found");
-        error(get_string("filenotfound", "error"), "$CFG->wwwroot/course/view.php?id=$courseid");
+        header('HTTP/1.0 404 not found');
+        error(get_string('filenotfound', 'error'), $CFG->wwwroot .'/course/view.php?id='. $courseid);
     }
 
     exit;
--- help.php
+++ help.php
@@ -1,50 +1,60 @@
-<?PHP /// $Id$
-      /// help.php - prints a very simple page and includes a
-      ///            page content or a string from elsewhere
-      ///            Usually this will appear in a popup 
-      ///            See helpbutton() in lib/moodlelib.php
+<?php
 
-    require_once("config.php");
+    /**
+     * help.php - Displays help page.
+     *
+     * Prints a very simple page and includes
+     * page content or a string from elsewhere.
+     * Usually this will appear in a popup 
+     * See {@link helpbutton()} in {@link lib/moodlelib.php}
+     *
+     * @author Martin Dougiamas
+     * @version $Id$
+     * @package moodlecore
+     */
+ 
 
-    optional_variable($file, "");
-    optional_variable($text, "No text to display");
-    optional_variable($module, "moodle");
+    require_once('config.php');
+
+    optional_variable($file, '');
+    optional_variable($text, 'No text to display');
+    optional_variable($module, 'moodle');
 
     print_header();
 
-    if (detect_munged_arguments("$module/$file")) {
-        error("Filenames contain illegal characters!");
+    if (detect_munged_arguments($module .'/'. $file)) {
+        error('Filenames contain illegal characters!');
     }
 
-    print_simple_box_start("center", "96%");
+    print_simple_box_start('center', '96%');
 
     $helpfound = false;
-    $langs = array(current_language(), get_string("parentlanguage"), "en");  // Fallback
+    $langs = array(current_language(), get_string('parentlanguage'), 'en');  // Fallback
 
     if (!empty($file)) {
         foreach ($langs as $lang) {
             if (empty($lang)) {
                 continue;
             }
-            if ($module == "moodle") {
-                $filepath = "$CFG->dirroot/lang/$lang/help/$file";
+            if ($module == 'moodle') {
+                $filepath = $CFG->dirroot .'/lang/'. $lang .'/help/'. $file;
             } else {
-                $filepath = "$CFG->dirroot/lang/$lang/help/$module/$file";
+                $filepath = $CFG->dirroot .'/lang/'. $lang .'/help/'. $module .'/'. $file;
             }
   
-            if (file_exists("$filepath")) {
+            if (file_exists($filepath)) {
                 $helpfound = true;
-                include("$filepath");   // The actual helpfile
+                include($filepath);   // The actual helpfile
 
-                if ($module == "moodle" and ($file == "index.html" or $file == "mods.html")) {
+                if ($module == 'moodle' and ($file == 'index.html' or $file == 'mods.html')) {
                     // include file for each module
 
-                    if (!$modules = get_records("modules", "visible", 1)) {
-                        error("No modules found!!");        // Should never happen
+                    if (!$modules = get_records('modules', 'visible', 1)) {
+                        error('No modules found!!');        // Should never happen
                     }
 
                     foreach ($modules as $mod) {
-                        $strmodulename = get_string("modulename", "$mod->name");
+                        $strmodulename = get_string('modulename', $mod->name);
                         $modulebyname[$strmodulename] = $mod;
                     }
                     ksort($modulebyname);
@@ -54,19 +64,19 @@
                             if (empty($lang)) {
                                 continue;
                             }
-                            $filepath = "$CFG->dirroot/lang/$lang/help/$mod->name/$file";
+                            $filepath = $CFG->dirroot .'/lang/'. $lang .'/help/'. $mod->name .'/'. $file;
 
-                            if (file_exists("$filepath")) {
+                            if (file_exists($filepath)) {
                                 echo '<hr size="1" />';
-                                include("$filepath");   // The actual helpfile
+                                include($filepath);   // The actual helpfile
                                 break;
                             }
                         }
                     }
                 }
 
-                if ($module == "moodle" and ($file == "resource/types.html")) {  // RESOURCES
-                    require_once("$CFG->dirroot/mod/resource/lib.php");
+                if ($module == 'moodle' and ($file == 'resource/types.html')) {  // RESOURCES
+                    require_once($CFG->dirroot .'/mod/resource/lib.php');
                     $typelist = resource_get_resource_types();
                     $typelist['label'] = get_string('resourcetypelabel', 'resource');
 
@@ -75,10 +85,10 @@
                             if (empty($lang)) {
                                 continue;
                             }
-                            $filepath = "$CFG->dirroot/lang/$lang/help/resource/type/$type.html";
-                            if (file_exists("$filepath")) {
+                            $filepath = $CFG->dirroot .'/lang/'. $lang .'/help/resource/type/'. $type .'.html';
+                            if (file_exists($filepath)) {
                                 echo '<hr size="1" />';
-                                include("$filepath");   // The actual helpfile
+                                include($filepath);   // The actual helpfile
                                 break;
                             }
                         }
@@ -88,9 +98,9 @@
             }
         }
     } else {
-        echo "<p>";
+        echo '<p>';
         echo clean_text($text);
-        echo "</p>";
+        echo '</p>';
         $helpfound = true;
     }
 
@@ -98,13 +108,12 @@
 
     if (!$helpfound) {
         $file = clean_text($file);  // Keep it clean!
-        notify("Help file '$file' could not be found!");
+        notify('Help file "'. $file .'" could not be found!');
     }
 
     close_window_button();
 
-    echo "<center><p><a href=\"help.php?file=index.html\">".get_string("helpindex")."</a><p></center>";
+    echo '<center><p><a href="help.php?file=index.html">'. get_string('helpindex') .'</a><p></center>';
 ?>
 </body>
 </html>
-
