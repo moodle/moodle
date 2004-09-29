2750feef8248635258d81adc13f7eb7ff6f165b5
moodler <moodler>
2004-09-29 13:47:16 +0000

    A script that will jump to a given URL.  Very simple.

---
+++ course/jumpto.php
@@ -0,0 +1,18 @@
+<?php  // $Id$
+       
+/*
+ * Jumps to a given URL.  Mostly used for accessibility.
+ *
+ */
+
+require('../config.php');
+
+$jump = parameter('jump');
+
+if ($jump) {
+    redirect(urldecode($jump));
+}
+
+redirect($_SERVER['HTTP_REFERER']);
+
+?>
