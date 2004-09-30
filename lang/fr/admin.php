cda8a9ae4b7ca4569aa5dbdc6cba4abfedd6c9fa
martignoni <martignoni>
2004-09-30 07:04:52 +0000

    Changed everywhere <b> with <strong>

--- lang/fr/admin.php
+++ lang/fr/admin.php
@@ -5,7 +5,7 @@ $string['filteruploadedfiles'] = 'Filtrer les fichiers déposés';
 $string['upgradelogs'] = 'Pour profiter de toutes les fonctionnalités, les historiques (logs) doivent être mis à jour. <a href=\"$a\">Plus d\'informations</a>';
 $string['upgradelogsinfo'] = 'Récemment, des changements ont eu lieu dans la façon de conserver les historiques. Afin de pouvoir profiter de l\'affichage des anciens historiques par activité, ceux-ci doivent être mis à jour. Selon la taille de votre site, cette opération peut durer assez longtemps (c\'est-à-dire plusieurs heures) et peut ralentir les bases de données des sites complexes et très actifs. Une fois l\'opération démarrée, vous devez la laisser se terminer (en laissant ouverte la fenêtre du navigateur). N\'ayez crainte, votre site continuera à très bien fonctionner pour les autres utilisateurs durant la mise à jour des historiques.<br /><br />Voulez-vous mettre à jour maintenant vos historique&nbsp;?';
 $string['upgradesure'] = 'Certains fichiers de votre Moodle ont été modifiés, et vous êtes sur le point<br />de lancer la procédure automatique de mise à jour de votre serveur vers cette version&nbsp;:
-  	 <p><b>$a</b></p>
+  	 <p><strong>$a</strong></p>
   	 <p>Une fois cette mise à jour effectuée, il sera impossible de revenir en arrière&nbsp;!</p>
   	 <p>Voulez-vous vraiment mettre à jour ce Moodle vers cette version&nbsp;?</p>';
 $string['upgradinglogs'] = 'Mise à jour des fichiers d\'historique (logs)';
