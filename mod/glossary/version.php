af796dc7741cebb1fa40fb3916c522d1c67c63f7
stronk7 <stronk7>
2004-10-01 08:36:36 +0000

    New version syntax.

--- mod/glossary/version.php
+++ mod/glossary/version.php
@@ -5,10 +5,12 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2004080900;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2004080920;  // YYYYMMDD   = current module version
+                                 //         X  = point release version 1,2,3 etc
+                                 //          Y = increments between point releases
 $module->requires = 2004082300;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
-$release = "1.4 development";   // User-friendly version number
+$release = "1.4.2";   // User-friendly version number
 
 ?>
