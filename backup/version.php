8259642b1bd7d30a32ce67d8088669c88e65ac69
stronk7 <stronk7>
2004-10-01 09:23:29 +0000

    Changed to new 'point release' schema in STABLE.

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,8 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2004083102;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2004083120;  // YYYYMMDD   = date of first major branch release 1.4
+                               //         X  = point release version 1,2,3 etc
+                               //          Y = increments between point releases
 
 $backup_release = "1.4.2";  // User-friendly version number
