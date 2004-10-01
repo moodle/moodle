c286325360b14469ca462ad9b7bb88db48bffcb7
stronk7 <stronk7>
2004-10-01 09:02:27 +0000

    Undo las changes. Point releases are only for STABLE branches.

--- mod/glossary/version.php
+++ mod/glossary/version.php
@@ -5,9 +5,7 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2004091700;  // YYYYMMDD   = current module version
-                                 //         X  = point release version 1,2,3 etc
-                                 //          Y = increments between point releases
+$module->version  = 2004091700;
 $module->requires = 2004091700;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
