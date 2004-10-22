f3133619f0746bacd20c517c37d5807e95d21739
paca70 <paca70>
2004-10-22 14:56:49 +0000

    Upgrades for protgresql.

--- mod/lesson/db/postgres7.sql
+++ mod/lesson/db/postgres7.sql
@@ -1,55 +1,65 @@
-# This file contains a complete database schema for all the 
-# tables used by the mlesson module, written in SQL
-
-# It may also contain INSERT statements for particular data 
-# that may be used, especially new entries in the table log_display
-
 CREATE TABLE prefix_lesson (
   id SERIAL8 PRIMARY KEY,
   course INT8  NOT NULL default '0',
   name varchar(255) NOT NULL default '',
+  practice INT  NOT NULL DEFAULT '0',
+  usepassword INT  NOT NULL DEFAULT '0',
+  password VARCHAR(32) NOT NULL default '',
   grade INT NOT NULL default '0',
+  custom INT  NOT NULL DEFAULT '0',
+  ongoing INT  NOT NULL DEFAULT '0',
   usemaxgrade INT NOT NULL default '0',
   maxanswers INT  NOT NULL default '4',
-  maxattempts INT NOT NULL default '0',
-  nextpagedefault INT NOT NULL default '0',
-  maxpages INT NOT NULL default '0',
+  maxattempts INT  NOT NULL default '5',
+  review INT  NOT NULL DEFAULT '0',
+  nextpagedefault INT  NOT NULL default '0',
+  minquestions INT  NOT NULL default '0',
+  maxpages INT  NOT NULL default '0',
+  timed INT  NOT NULL DEFAULT '0',
+  maxtime INT8  NOT NULL DEFAULT '0',
   retake INT  NOT NULL default '1',
+  tree INT  NOT NULL DEFAULT '0',
+  slideshow INT  NOT NULL DEFAULT '0',
+  width INT8  NOT NULL DEFAULT '640',
+  height INT8  NOT NULL DEFAULT '480',
+  bgcolor VARCHAR(7) NOT NULL DEFAULT '#FFFFFF',
+  displayleft INT  NOT NULL DEFAULT '0',
+  highscores INT  NOT NULL DEFAULT '0',
+  maxhighscores INT8  NOT NULL DEFAULT '0',
   available INT8  NOT NULL default '0',
   deadline INT8  NOT NULL default '0',
   timemodified INT8  NOT NULL default '0'
 );
-# --------------------------------------------------------
 
 CREATE TABLE prefix_lesson_pages (
   id SERIAL8 PRIMARY KEY,
   lessonid INT8  NOT NULL default '0',
   prevpageid INT8  NOT NULL default '0',
   nextpageid INT8  NOT NULL default '0',
-  qtype INT8  NOT NULL default '0',
-  qoption INT8  NOT NULL default '0',
+  qtype INT  NOT NULL default '0',
+  qoption INT  NOT NULL default '0',
+  layout INT  NOT NULL DEFAULT '1',
+  display INT  NOT NULL DEFAULT '1',
   timecreated INT8  NOT NULL default '0',
   timemodified INT8  NOT NULL default '0',
   title varchar(255) NOT NULL default '',
   contents text NOT NULL default ''
-); 
-# COMMENT='Defines lesson_pages';
-# --------------------------------------------------------
+) ;
 
 CREATE TABLE prefix_lesson_answers (
   id SERIAL8 PRIMARY KEY,
   lessonid INT8  NOT NULL default '0',
   pageid INT8  NOT NULL default '0',
-  jumpto INT8 NOT NULL default '0',
-  grade INT8  NOT NULL default '0',
-  flags INT8  NOT NULL default '0',
+  jumpto int8 NOT NULL default '0',
+  grade INT  NOT NULL default '0',
+  score INT8 NOT NULL DEFAULT '0',
+  flags INT  NOT NULL default '0',
   timecreated INT8  NOT NULL default '0',
   timemodified INT8  NOT NULL default '0',
   answer text NOT NULL default '',
   response text NOT NULL default ''
-);
-# COMMENT='Defines lesson_answers';
-# --------------------------------------------------------
+) ;
+CREATE INDEX prefix_lesson_answers_pageid_idx ON prefix_lesson_answers (pageid);
 
 CREATE TABLE prefix_lesson_attempts (
   id SERIAL8 PRIMARY KEY,
@@ -60,9 +70,8 @@ CREATE TABLE prefix_lesson_attempts (
   retry INT  NOT NULL default '0',
   correct INT8  NOT NULL default '0',
   timeseen INT8  NOT NULL default '0'
-); 
-#COMMENT='Defines lesson_attempts';
-# --------------------------------------------------------
+) ;
+CREATE INDEX prefix_lesson_attempts_userid_idx ON prefix_lesson_attempts (userid);
 
 CREATE TABLE prefix_lesson_grades (
   id SERIAL8 PRIMARY KEY,
@@ -71,9 +80,77 @@ CREATE TABLE prefix_lesson_grades (
   grade INT  NOT NULL default '0',
   late INT  NOT NULL default '0',
   completed INT8  NOT NULL default '0'
-);
-# COMMENT='Defines lesson_grades';
-# --------------------------------------------------------
+) ;
+
+CREATE TABLE prefix_lesson_default 
+		( id SERIAL8 PRIMARY KEY,
+		  course INT8  NOT NULL default '0',
+		  practice INT  NOT NULL default '0',
+		  password varchar(32) NOT NULL default '',
+		  usepassword INT  NOT NULL default '0',
+		  grade INT NOT NULL default '0',
+		  custom INT  NOT NULL default '0',
+		  ongoing INT  NOT NULL default '0',
+		  usemaxgrade INT  NOT NULL default '0',
+		  maxanswers INT  NOT NULL default '4',
+		  maxattempts INT  NOT NULL default '5',
+		  review INT  NOT NULL default '0',
+		  nextpagedefault INT  NOT NULL default '0',
+		  minquestions INT  NOT NULL default '0',
+		  maxpages INT  NOT NULL default '0',
+		  timed INT  NOT NULL default '0',
+		  maxtime INT8  NOT NULL default '0',
+		  retake INT  NOT NULL default '1',
+		  tree INT  NOT NULL default '0',
+		  slideshow INT  NOT NULL default '0',
+		  width INT8  NOT NULL default '640',
+		  height INT8  NOT NULL default '480',
+		  bgcolor varchar(7) default '#FFFFFF',
+		  displayleft INT  NOT NULL default '0',
+		  highscores INT  NOT NULL default '0',
+		  maxhighscores INT8 NOT NULL default '0'
+		) ;
+
+CREATE TABLE prefix_lesson_timer
+	( id SERIAL8 PRIMARY KEY,
+  	  lessonid INT8  not null,
+	  userid INT8  not null,
+	  starttime INT8  not null,
+  	  lessontime INT8  not null
+	);
+
+CREATE TABLE prefix_lesson_branch
+	( id SERIAL8 PRIMARY KEY,
+	  lessonid INT8  not null,
+	  userid INT8  not null,
+	  pageid INT8  not null,
+	  retry INT8  not null,
+	  flag  INT  not null,
+	  timeseen INT8  not null
+	);
+
+CREATE TABLE prefix_lesson_essay
+	( id SERIAL8 PRIMARY KEY,
+	  lessonid INT8  not null,
+	  userid INT8  not null,
+	  pageid INT8  not null,
+	  answerid INT8  not null,
+	  try INT8  not null,
+	  answer text not null,
+	  graded INT  not null default 0,
+	  score INT8  not null default 0,
+	  response text not null,
+	  sent INT  not null default 0,
+	  timesubmitted INT8  not null
+	);
+
+CREATE TABLE prefix_lesson_high_scores
+	( id SERIAL8 PRIMARY KEY,
+	  lessonid INT8  not null,
+	  userid INT8  not null,
+	  gradeid INT8  not null,
+	  nickname varchar(5) not null
+	);
 
 
 INSERT INTO prefix_log_display VALUES ('lesson', 'start', 'lesson', 'name');
