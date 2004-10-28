3293a5c0001203c1672d27781806975262c6d2a7
scop22 <scop22>
2004-10-28 13:06:31 +0000

    Major update of existing files in Korean Language Pack; all fully encoded in euc-kr

--- lang/ko/journal.php
+++ lang/ko/journal.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // journal.php - created with Moodle 1.2 development (2004010800)
+      // journal.php - created with Moodle 1.4.2 nearly (2004083121)
 
 
 $string['alwaysopen'] = '항상 공개';
@@ -9,14 +9,17 @@ $string['editingended'] = '편집 기간 완료';
 $string['editingends'] = '편집 기간 완료';
 $string['entries'] = '기입';
 $string['feedbackupdated'] = ' 기입을 위해 개정된 정보(의견)';
-$string['journalmail'] = '교사가 우리의 저널에 몇가지 의견을 기록해왔다. 
-';
+$string['journalmail'] = '교사가 우리의 저널에 몇가지 의견을 기록해왔다. 
+당신은 당신의 저널에 첨가된것을 볼 수 있습니다.';
+$string['journalmailhtml'] = '교사가 우리의 저널에 몇가지 의견을 기록해왔다. 
+당신은 당신의 저널에 첨가된것을 볼 수 있습니다.';
 $string['journalname'] = '저널 명칭';
 $string['journalquestion'] = '저널 의문사항';
 $string['journalrating1'] = '불충분하다,만족스럽지않다';
 $string['journalrating2'] = '만족스럽다';
 $string['journalrating3'] = '우수하다,뛰어나다';
-$string['modulenameplural'] = 'Journals';
+$string['modulename'] = '저널';
+$string['modulenameplural'] = '저널';
 $string['newjournalentries'] = '새저널기재';
 $string['noentry'] = '기재안함';
 $string['noratinggiven'] = '평가미완료(평가를 하지 않았다';
@@ -26,6 +29,8 @@ $string['overallrating'] = '전체적인 평점(평점)';
 $string['rate'] = '평가하다, 등급';
 $string['saveallfeedback'] = '나의 모든 의견을 저장해라(기억해라)';
 $string['startoredit'] = '나의 저널기재를 시작 또는 편집합니다.';
+$string['thischarset'] = 'euc-kr';
+$string['thisdirection'] = 'ltr';
 $string['viewallentries'] = '저널을 다시 보다';
 
 ?>
--- lang/ko/label.php
+++ lang/ko/label.php
@@ -1,9 +1,11 @@
 <?PHP // $Id$ 
-      // label.php - created with Moodle 1.2 development (2004012700)
+      // label.php - created with Moodle 1.4.2 nearly (2004083121)
 
 
 $string['labeltext'] = '라벨 텍스트';
 $string['modulename'] = '라벨';
 $string['modulenameplural'] = '라벨들';
+$string['thischarset'] = 'euc-kr';
+$string['thisdirection'] = 'ltr';
 
 ?>
