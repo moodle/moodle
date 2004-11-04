54b5310062788c300f6a58ce1ff31498957fe901
moodler <moodler>
2004-11-04 06:28:17 +0000

    Addslashes to pathinfo to prevent SQL injections

--- lib/weblib.php
+++ lib/weblib.php
@@ -553,7 +553,7 @@ function get_slash_arguments($file="file.php") {
     $pathinfo = explode($file, $string);
 
     if (!empty($pathinfo[1])) {
-        return $pathinfo[1];
+        return addslashes($pathinfo[1]);
     } else {
         return false;
     }
