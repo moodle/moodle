ff61707012fc6d90610f5a59820256c8b7200d6b
mits <mits>
2004-11-08 05:29:50 +0000

    translated some.

--- lang/ja/block_course_summary.php
+++ lang/ja/block_course_summary.php
@@ -1,8 +1,8 @@
 <?php // $Id$ 
-      // block_course_summary.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004101900)
+      // block_course_summary.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004110200)
 
 
-$string['coursesummary'] = 'コース概要';
+$string['blockname'] = 'コース概要';
 $string['pagedescription'] = 'コース/サイト説明';
 $string['thischarset'] = 'EUC-JP';
 $string['thisdirection'] = 'ltr';
