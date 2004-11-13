c4fb19504670a00627fedccbb280e0425012f95a
kaipe <kaipe>
2004-11-13 18:51:47 +0000

    Fixed a typo that was introduced with revision 1.4 - I believe that revision must have been developed with some sort of script...

--- mod/quiz/questiontypes/match/editquestion.php
+++ mod/quiz/questiontypes/match/editquestion.php
@@ -7,7 +7,7 @@
         }
     }
     if (empty($subquestions) and empty($subanswers)) {
-        for ($i=0; $i<quiz_MAX_NUMBER_ANSWERS; $i++) {
+        for ($i=0 ; $i < QUIZ_MAX_NUMBER_ANSWERS ; $i++) {
             $subquestions[] = "";   // Make question slots, default as blank
             $subanswers[] = "";     // Make answer slots, default as blank
         }
