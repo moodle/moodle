62067fe3aea2a2fa8d754c166d3e4337810210be
stronk7 <stronk7>
2004-11-16 23:12:21 +0000

    Header is out and "Site:" is in.
    Partially solved Bug 1837
    (http://moodle.org/bugs/bug.php?op=show&bugid=1837)
    
    Merged from MOODLE_14_STABLE

--- mod/glossary/print.php
+++ mod/glossary/print.php
@@ -33,7 +33,7 @@
         $entriesbypage = $CFG->glossary_entbypage;
     }
 
-    print_header_simple(strip_tags("$glossary->name"));
+    print_header();
 
     if ($CFG->forcelogin) {
         require_login();
@@ -144,7 +144,7 @@
 
     $site = get_record("course","id",1);
     echo '<p align="right"><font size="-1">' . userdate(time()) . '</font></p>';
-    echo '<strong>' . $site->fullname . '</strong><br />';
+    echo get_string("site") . ': <strong>' . $site->fullname . '</strong><br />';
     echo get_string("course") . ': <strong>' . $course->fullname . ' ('. $course->shortname . ')</strong><br />';
     echo get_string("modulename","glossary") . ': <strong>' . $glossary->name . '</strong><p>';
     if ( $allentries ) {
