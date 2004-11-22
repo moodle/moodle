eaeafbce15797f66a943ce3032dc3c360bbd48c2
moodler <moodler>
2004-11-22 14:41:19 +0000

    Fixed bug 2210 - some sesskey variables were missed in this block making moving of activities in here non-functional - doh!

--- blocks/social_activities/block_social_activities.php
+++ blocks/social_activities/block_social_activities.php
@@ -78,7 +78,7 @@ class CourseBlock_social_activities extends MoodleBlock {
                         if ($mod->id == $USER->activitycopy) {
                             continue;
                         }
-                        $this->content->items[] = '<a title="'.$strmovefull.'" href="'.$CFG->wwwroot.'/course/mod.php?moveto='.$mod->id.'">'.
+                        $this->content->items[] = '<a title="'.$strmovefull.'" href="'.$CFG->wwwroot.'/course/mod.php?moveto='.$mod->id.'&amp;sesskey='.$USER->sesskey.'">'.
                             '<img height="16" width="80" src="'.$CFG->pixpath.'/movehere.gif" alt="'.$strmovehere.'" border="0"></a>';
                         $this->content->icons[] = '';
                     }
@@ -111,7 +111,7 @@ class CourseBlock_social_activities extends MoodleBlock {
         }
 
         if ($ismoving) {
-            $this->content->items[] = '<a title="'.$strmovefull.'" href="'.$CFG->wwwroot.'/course/mod.php?movetosection='.$section->id.'">'.
+            $this->content->items[] = '<a title="'.$strmovefull.'" href="'.$CFG->wwwroot.'/course/mod.php?movetosection='.$section->id.'&amp;sesskey='.$USER->sesskey.'">'.
                                       '<img height="16" width="80" src="'.$CFG->pixpath.'/movehere.gif" alt="'.$strmovehere.'" border="0"></a>';
             $this->content->icons[] = '';
         }
