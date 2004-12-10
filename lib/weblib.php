5debee2de4661e77629daa7c39253647c8fb535b
moodler <moodler>
2004-12-10 06:20:54 +0000

    Added latest knowledge on caching techniques to HEAD

--- lib/weblib.php
+++ lib/weblib.php
@@ -1565,16 +1565,21 @@ function print_header ($title='', $heading='', $navigation='', $focus='', $meta=
         $direction = ' dir="ltr"';
     }
 
-    if (!$cache) {   // Do everything we can to prevent clients and proxies caching
-        @header('Expires: Mon, 20 Aug 1969 09:23:00 GMT');
-        @header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+    if ($cache) {  // Allow caching on "back" (but not on normal clicks)
+        @header('Cache-Control: private, pre-check=0, post-check=0, max-age=0');
+        @header('Pragma: no-cache');
+        @header('Expires: ');          
+    } else {       // Do everything we can to always prevent clients and proxies caching
         @header('Cache-Control: no-store, no-cache, must-revalidate');
         @header('Cache-Control: post-check=0, pre-check=0', false);
         @header('Pragma: no-cache');
+        @header('Expires: Mon, 20 Aug 1969 09:23:00 GMT');
+        @header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
-        $meta .= "\n".'<meta http-equiv="pragma" content="no-cache" />';
-        $meta .= "\n".'<meta http-equiv="expires" content="0" />';
+        $meta .= "\n<meta http-equiv=\"pragma\" content=\"no-cache\" />";
+        $meta .= "\n<meta http-equiv=\"expires\" content=\"0\" />";
     }
+    @header('Accept-Ranges: none');
 
     if ($usexml) {       // Added by Gustav Delius / Mad Alex for MathML output
                          // Modified by Julian Sedding
