93c7d6dae0feaed058b035e59255fb2b8539a65a
dhawes <dhawes>
2004-12-14 18:12:09 +0000

    first add of new rss client block. please take a moment to test its features and provide feedback (now added to the correct folder moodle/lang/en)

---
+++ lang/en/block_rss_client.php
@@ -0,0 +1,36 @@
+<?php // $Id$ 
+
+$string['block_rss_feeds_title'] = 'Remote RSS Feeds';
+$string['block_rss_feeds_add_edit'] = 'Add/Edit Feeds';
+$string['block_rss_remote_news_feed'] = 'Remote News Feed';
+$string['block_rss_feed_updated'] = 'News feed updated';
+$string['block_rss_feed_added'] = 'News feed added';
+$string['block_rss_feed_deleted'] = 'News feed deleted';
+
+$string['block_rss_num_entries'] = 'Number of rss links to show per block.';
+$string['block_rss_submitters'] = 'Who can define new rss feeds? Feeds are made available for any member of your site to add to their own pages.';
+$string['block_rss_choose_feed'] = 'Choose an feed to display in this block:';
+$string['block_rss_no_feeds'] = 'There are no RSS feeds defined for this site.';
+$string['block_rss_edit_news_feeds'] = 'Edit news feeds';
+$string['block_rss_shownumentries'] = 'Max number entries to show per block.';
+$string['block_rss_display_description'] = 'Display each link\'s description?';
+
+$string['block_rss_could_not_find_feed'] = 'Could not find feed with id';
+$string['block_rss_feed'] = 'Feed';
+$string['block_rss_find_more_feeds'] = 'Find more rss feeds';
+
+$string['block_rss_delete_feed_confirm'] = 'Are you sure you want to delete this feed?';
+$string['block_rss_timeout'] = 'block_rss_timeout';
+$string['block_rss_timeout_desc'] = 'Time in minutes for an RSS feed to live in cache.';
+$string['block_rss_add_headline_block'] = 'Add RSS headline block';
+$string['block_rss_pick_feed'] = 'Pick a news feed';
+$string['block_rss_feeds'] = 'News Feeds';
+$string['block_rss_feed'] = 'News Feed';
+$string['block_rss_edit_feeds'] = 'Edit, subscribe or unsubsribe from RSS/Atom news feeds';
+$string['block_rss_see_all_feeds'] = 'See all feeds';
+$string['block_rss_edit_rss_block'] = 'Edit RSS Headline Block';
+
+$string['block_rss_add_new'] = 'Add New';
+$string['block_rss_word_title'] = 'Title';
+
+?>
