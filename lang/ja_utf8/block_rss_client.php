35cf1d37e472b59cf5a1ea148f674a70fcaa1e17
mits <mits>
2004-12-15 04:32:41 +0000

    New translated file added.

---
+++ lang/ja_utf8/block_rss_client.php
@@ -0,0 +1,33 @@
+<?php // $Id$ 
+      // block_rss_client.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004121400)
+
+
+$string['block_rss_add_headline_block'] = 'RSSヘッドラインブロックの追加';
+$string['block_rss_add_new'] = 'RSSの追加';
+$string['block_rss_choose_feed'] = 'ブロックに表示するフィードを設定してください:';
+$string['block_rss_could_not_find_feed'] = '指定されたIDのフィードが見つかりませんでした。';
+$string['block_rss_delete_feed_confirm'] = '本当にこのフィードを削除してもよろしいですか?';
+$string['block_rss_display_description'] = '各リンクの説明を表示しますか?';
+$string['block_rss_edit_feeds'] = 'RSS/Atomニュースフィードの購読登録、解除の編集';
+$string['block_rss_edit_news_feeds'] = 'ニュースフィードの編集';
+$string['block_rss_edit_rss_block'] = 'RSSヘッドラインブロックの編集';
+$string['block_rss_feed'] = 'ニュースフィード';
+$string['block_rss_feed_added'] = 'ニュースフィードが追加されました。';
+$string['block_rss_feed_deleted'] = 'ニュースフィードが削除されました。';
+$string['block_rss_feed_updated'] = 'ニュースフィードが更新されました。';
+$string['block_rss_feeds'] = 'ニュースフィード';
+$string['block_rss_feeds_add_edit'] = 'フィードの追加/編集';
+$string['block_rss_feeds_title'] = 'リモートRSSフィード';
+$string['block_rss_find_more_feeds'] = '更にRSSフィードを探す';
+$string['block_rss_no_feeds'] = 'このサイトで設定されたRSSフィードはありません。';
+$string['block_rss_num_entries'] = 'ブロックあたりのRSSリンク表示数';
+$string['block_rss_pick_feed'] = 'ニュースフィードを取得';
+$string['block_rss_remote_news_feed'] = 'リモートニュースフィード';
+$string['block_rss_see_all_feeds'] = '全てのフィードを表示';
+$string['block_rss_shownumentries'] = 'ブロックあたりの最大エントリ表示数';
+$string['block_rss_submitters'] = '誰が新しいRSSフィードを設定できますか? あなたのサイトの全てのメンバーが自分のページにフィードを追加することができます。';
+$string['block_rss_timeout'] = 'タイムアウト';
+$string['block_rss_timeout_desc'] = 'RSSフィードのキャッシュ保存期間 ( 分 )';
+$string['block_rss_word_title'] = 'タイトル';
+
+?>
