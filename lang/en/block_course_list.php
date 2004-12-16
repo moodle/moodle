ec997189660c34e3503f9adb48a0be2ba2da2469
moodler <moodler>
2004-12-16 07:32:37 +0000

    Filled out a missing string that caused people some consternation in the forums

--- lang/en/block_course_list.php
+++ lang/en/block_course_list.php
@@ -2,7 +2,7 @@
 
 $string['blockname'] = 'Course List';
 $string['allcourses'] = 'Admin user sees all courses';
-$string['configadminview'] = '';
+$string['configadminview'] = 'What should the admin see in the course list block?';
 $string['owncourses'] = 'Admin user sees own courses';
 
 ?>
