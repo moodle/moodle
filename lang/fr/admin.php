48e49f93879af3758c70cd6d5e291a079a31746e
martignoni <martignoni>
2004-12-19 13:12:54 +0000

    New strings for calendar

--- lang/fr/admin.php
+++ lang/fr/admin.php
@@ -1,10 +1,35 @@
 <?php // $Id$
 
+$string['adminseesallevents'] = 'Les administrateurs voient tous les événements';
+$string['adminseesownevents'] = 'Les administrateurs sont comme tous les autres utilisateurs';
 $string['blockinstances'] = 'Instances';
 $string['blockmultiple'] = 'Multiple';
+$string['calendarsettings'] = 'Calendrier';
 $string['change'] = 'changer';
 $string['cachetext'] = 'Durée de vie du cache texte';
+$string['confirmation'] = 'Confirmation';
+$string['confirmdeletedst'] = 'La suppression du réglage <strong>$a</strong> désactivera immédiatement l\'heure d\'été pour tous les utilisateurs de ce réglage. Cela risque de changer leur perception du temps sans avertissement. Voulez-vous vraiment continuer&nbsp;?';
+$string['confirmdeletedstdefault'] = '<strong>Le réglage $a est actuellement utilisé par tous les utilisateurs de ce site&nbsp;!</strong> Sa suppression changera leur perception du temps sans avertissement. Voulez-vous vraiment continuer&nbsp;?';
+$string['dstisapreference'] = 'Chaque utilisateur peut choisir son propre réglage';
+$string['dstisforcedto'] = 'Imposer à tous les utilisateurs le réglage';
+$string['dstpresets'] = 'Réglages heure d\'été';
+$string['emptydstlist'] = 'Il n\'y a actuellement aucun réglage d\'heure d\été défini. Vous pouvez en ajouter un en cliquant sur le bouton Ajouter.';
+$string['editingdstpreset'] = 'Modification d\'un réglage d\'heure d\'été';
+$string['errordstpresetactivateearlier'] = 'Le mois d\'activation doit précéder le mois de désactivation';
+$string['errordstpresetnameempty'] = 'Le nonm du réglage ne peut pas être vide';
+$string['errordstpresetnameexists'] = 'Un autre réglage de même nom existe déjà';
 $string['filteruploadedfiles'] = 'Filtrer les fichiers déposés';
+$string['helpadminseesall'] = 'Les administrateurs voient-ils tous les événements du calendrier ou seulement leurs propres événements&nbsp;?';
+$string['helpcalendarsettings'] = 'Configuration des divers aspects du calendrier de Moodle';
+$string['helpdstforusers'] = 'Chacun peut-il choisir son propre réglage de l\'heure d\'été&nbsp;?';
+$string['helpmanagedstpresets'] = 'Cliquer sur ce bouton pour ajouter, modifier ou supprimer les réglages d\'heure d\'été disponibles sur ce site.';
+$string['helpstartofweek'] = 'Quel est le premier jour de la semaine dans le calendrier&nbsp;?';
+$string['helpupcominglookahead'] = 'Par défaut, combien de jours dans le futur le calendrier considère-t-il les événements à venir&nbsp;?';
+$string['helpupcomingmaxevents'] = 'Par défaut, quel est le nombre maximum d\'événements à venir affichés&nbsp;?';
+$string['helpweekenddays'] = 'Quel jours sont-ils traités comme jours du «&nbsp;weekend&nbsp;» et affichés d\'une couleur différente&nbsp;?';
+$string['managedstpresets'] = 'Gestion des réglages d\'heure d\'été';
+$string['nodstpresetsexist'] = 'Le support de l\'heure d\'été est désactivé pour tous les utilisateurs, car aucun réglage n\'est défini. Vous pouvez définir des réglages en cliquant sur le bouton ci-dessous.';
+$string['therewereerrors'] = 'Des erreurs sont présentes dans vos données';
 $string['upgradelogs'] = 'Pour profiter de toutes les fonctionnalités, les historiques (logs) doivent être mis à jour. <a href=\"$a\">Plus d\'informations</a>';
 $string['upgradelogsinfo'] = 'Récemment, des changements ont eu lieu dans la façon de conserver les historiques. Afin de pouvoir profiter de l\'affichage des anciens historiques par activité, ceux-ci doivent être mis à jour. Selon la taille de votre site, cette opération peut durer assez longtemps (c\'est-à-dire plusieurs heures) et peut ralentir les bases de données des sites complexes et très actifs. Une fois l\'opération démarrée, vous devez la laisser se terminer (en laissant ouverte la fenêtre du navigateur). N\'ayez crainte, votre site continuera à très bien fonctionner pour les autres utilisateurs durant la mise à jour des historiques.<br /><br />Voulez-vous mettre à jour maintenant vos historique&nbsp;?';
 $string['upgradesure'] = 'Certains fichiers de votre Moodle ont été modifiés, et vous êtes sur le point<br />de lancer la procédure automatique de mise à jour de votre serveur vers cette version&nbsp;:<br /><br />
