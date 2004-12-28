f7ea3a0b1050303d9638c45581ecdc22b7de0318
moodler <moodler>
2004-12-28 13:45:04 +0000

    New strings for Messaging facility

---
+++ lang/en/message.php
@@ -0,0 +1,43 @@
+<?php  // $Id$
+
+$string['addcontact'] = 'Add contact';
+$string['ago'] = '$a ago';
+$string['allmine'] = 'All messages to me or from me';
+$string['allstudents'] = 'All messages between students in course';
+$string['allusers'] = 'All messages from all users';
+$string['blockcontact'] = 'Block contact';
+$string['discussion'] = 'Discussion';
+$string['emptysearchstring'] = 'You must search for something';
+$string['includeblockedusers'] = 'Include blocked users';
+$string['incomingcontacts'] = 'Incoming Contacts ($a)';
+$string['keywords'] = 'Keywords';
+$string['keywordssearchresults'] = 'Search results: $a messages found';
+$string['messages'] = 'Messages';
+$string['messaging'] = 'Messaging';
+$string['mycontacts'] = 'My Contacts';
+$string['newsearch'] = 'New search';
+$string['nosearchresults'] = 'There were no results from your search';
+$string['offlinecontacts'] = 'Offline Contacts ($a)';
+$string['onlinecontacts'] = 'Online Contacts ($a)';
+$string['onlyfromme'] = 'Only messages from me';
+$string['onlymycourses'] = 'Only in my courses';
+$string['onlytome'] = 'Only messages to me';
+$string['removecontact'] = 'Remove contact';
+$string['searchforperson'] = 'Search for a person';
+$string['searchmessages'] = 'Search messages';
+$string['sendmessage'] = 'Send message';
+$string['sendmessageto'] = 'Send message to $a';
+$string['unblockcontact'] = 'Unblock contact';
+$string['unreadmessages'] = '$a unread messages';
+$string['userssearchresults'] = 'Search results: $a users found';
+
+$string['savemysettings'] = 'Save my settings';
+$string['settingssaved'] = 'Your settings have been saved';
+$string['showmessagewindow'] = 'Automatically show Message window when I get new messages (your browser needs to be set so that it doesn\'t block popups on this site)';
+$string['beepnewmessage'] = 'Beep when a new message comes in';
+$string['maxmessages'] = 'Maximum number of messages to show in the discussion history';
+$string['deletemessagesdays'] = 'Number of days before old messages are automatically deleted';
+$string['emailmessages'] = 'Email messages when I am offline';
+$string['timenosee'] = 'Minutes since I was last seen online';
+
+?>
