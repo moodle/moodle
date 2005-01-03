860a2ac91299b1caf14a49713e4009f057eba514
koenr <koenr>
2005-01-03 16:16:28 +0000

    filling in missing strings

--- lang/nl/block_rss_client.php
+++ lang/nl/block_rss_client.php
@@ -1,10 +1,11 @@
 <?PHP // $Id$ 
-      // block_rss_client.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004121600)
+      // block_rss_client.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005010100)
 
 
 $string['block_rss_add_headline_block'] = 'Voeg RSS-hoofdpuntenblok toe';
 $string['block_rss_add_new'] = 'Voeg nieuw blok toe';
 $string['block_rss_choose_feed'] = 'Kies een RSS-feed om in dit blok te tonen';
+$string['block_rss_client_num_entries'] = 'Aantal te tonen RSS-links per blok.';
 $string['block_rss_could_not_find_feed'] = 'Kon geen RSS-feed vinden met dit id';
 $string['block_rss_delete_feed_confirm'] = 'Ben je er zeker van dat je deze RSS-feed wil verwijderen?';
 $string['block_rss_display_description'] = 'De beschrijving van elke link tonen?';
@@ -20,7 +21,6 @@ $string['block_rss_feeds_add_edit'] = 'Voeg toe/Bewerk RSS-feeds';
 $string['block_rss_feeds_title'] = 'RSS-feeds';
 $string['block_rss_find_more_feeds'] = 'Zoek meer RSS-feeds';
 $string['block_rss_no_feeds'] = 'Er zijn geen RSS-feeds gedefiniëerd voor deze site';
-$string['block_rss_num_entries'] = 'Aantal te tonen RSS-links per blok.';
 $string['block_rss_pick_feed'] = 'Kies een RSS nieuws-feed';
 $string['block_rss_remote_news_feed'] = 'Nieuws-feed';
 $string['block_rss_see_all_feeds'] = 'Bekijk alle RSS-feeds';
@@ -28,6 +28,5 @@ $string['block_rss_shownumentries'] = 'Maximum aantal te tonen items per blok';
 $string['block_rss_submitters'] = 'Wie kan nieuwe RSS-fees definiëren? De feeds staan ter beschikking voor elk lid van je site, zodat zij die kunnen toevoegen aan hun eigen pagina\'s';
 $string['block_rss_timeout'] = 'block_rss_timeout';
 $string['block_rss_timeout_desc'] = 'Tijd in minuten die een RSS-feed in cache blijft.';
-$string['block_rss_word_title'] = 'Titel';
 
 ?>
