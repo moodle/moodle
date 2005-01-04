c50e95d4b30f605b3b53d9a0261a8b301a3f473e
martignoni <martignoni>
2005-01-04 09:39:56 +0000

    Corrected file encoding

--- lang/fr/block_rss_client.php
+++ lang/fr/block_rss_client.php
@@ -3,16 +3,16 @@
 $string['block_rss_feeds_title'] = 'Fils RSS distants';
 $string['block_rss_feeds_add_edit'] = 'Ajouter/modifier des fils RSS';
 $string['block_rss_remote_news_feed'] = 'Fils de nouvelles RSS distants';
-$string['block_rss_feed_updated'] = 'Fil RSS mis ˆ jour';
-$string['block_rss_feed_added'] = 'Fil RSS ajoutŽ';
-$string['block_rss_feed_deleted'] = 'Fil RSS supprimŽ';
+$string['block_rss_feed_updated'] = 'Fil RSS mis à jour';
+$string['block_rss_feed_added'] = 'Fil RSS ajouté';
+$string['block_rss_feed_deleted'] = 'Fil RSS supprimé';
 
-$string['block_rss_client_num_entries'] = 'Nombre d\'articles RSS ˆ afficher par bloc.';
-$string['block_rss_submitters'] = 'Qui peut dŽfinir de nouveaux fils RSS&nbsp;? Les fils RSS sont mis ˆ la disposition des membres de votre site, pour leur permettre de les ajouter ˆ leurs propres pages.';
-$string['block_rss_choose_feed'] = 'Choisir les fils RSS que vous dŽsirez rendre disponibles dans ce bloc&nbsp;:';
-$string['block_rss_no_feeds'] = 'Il n\'y a pas de fil RSS dŽfini sur ce site.';
+$string['block_rss_client_num_entries'] = 'Nombre d\'articles RSS à afficher par bloc.';
+$string['block_rss_submitters'] = 'Qui peut définir de nouveaux fils RSS&nbsp;? Les fils RSS sont mis à la disposition des membres de votre site, pour leur permettre de les ajouter à leurs propres pages.';
+$string['block_rss_choose_feed'] = 'Choisir les fils RSS que vous désirez rendre disponibles dans ce bloc&nbsp;:';
+$string['block_rss_no_feeds'] = 'Il n\'y a pas de fil RSS défini sur ce site.';
 $string['block_rss_edit_news_feeds'] = 'Modifier les fils de nouvelles RSS';
-$string['block_rss_shownumentries'] = 'Nombre maximum d\'articles ˆ afficher par bloc.';
+$string['block_rss_shownumentries'] = 'Nombre maximum d\'articles à afficher par bloc.';
 $string['block_rss_display_description'] = 'Afficher la description de chaque lien&nbsp;?';
 
 $string['block_rss_could_not_find_feed'] = 'Impossible de trouver le fil RSS d\'identifiant';
@@ -21,12 +21,12 @@ $string['block_rss_find_more_feeds'] = 'Rechercher d\'autres fils RSS';
 
 $string['block_rss_delete_feed_confirm'] = 'Voulez-vous vraiment supprimer ce fil RSS&nbsp;?';
 $string['block_rss_timeout'] = 'block_rss_timeout';
-$string['block_rss_timeout_desc'] = 'DurŽe de vie en cache d\'un fil RSS (en minutes).';
+$string['block_rss_timeout_desc'] = 'Durée de vie en cache d\'un fil RSS (en minutes).';
 $string['block_rss_add_headline_block'] = 'Ajouter un bloc manchette RSS';
 $string['block_rss_pick_feed'] = 'Choisir un fil RSS';
 $string['block_rss_feeds'] = 'Fils RSS';
 $string['block_rss_feed'] = 'Fil RSS';
-$string['block_rss_edit_feeds'] = 'Modifier, s\'abonner ou se dŽsabonner d\'un fil RSS/';
+$string['block_rss_edit_feeds'] = 'Modifier, s\'abonner ou se désabonner d\'un fil RSS/';
 $string['block_rss_see_all_feeds'] = 'Afficher tous les fils RSS';
 $string['block_rss_edit_rss_block'] = 'Modifier le bloc manchette RSS';
 
