21e9e621950158c3880b2e27d24cf20e9312aa10
ralf-bonn <ralf-bonn>
2005-01-09 19:32:22 +0000

    *** empty log message ***

---
+++ lang/de/block_rss_client.php
@@ -0,0 +1,33 @@
+<?PHP // $Id$ 
+      // block_rss_client.php - created with Moodle 1.4.3 (2004083130)
+
+
+$string['block_rss_add_headline_block'] = 'RSS Überschriften Block hinzufügen';
+$string['block_rss_add_new'] = 'Neu hinzufügen';
+$string['block_rss_choose_feed'] = 'Einen Feed auswählen, der in diesem Block gezeigt werden soll';
+$string['block_rss_could_not_find_feed'] = 'Feed mit id konnte nicht gefunden werden';
+$string['block_rss_delete_feed_confirm'] = 'Sind Sie sicher, dass Sie diesen Feed löschen wollen?';
+$string['block_rss_display_description'] = 'Soll jede Linkbeschreibung angezeigt werden?';
+$string['block_rss_edit_feeds'] = 'Eintragen, löschen oder bearbeiten von RSS/Atom Newsfeed';
+$string['block_rss_edit_news_feeds'] = 'Newsfeed bearbeiten';
+$string['block_rss_edit_rss_block'] = 'RSS Überschriftenblock bearbeiten';
+$string['block_rss_feed'] = 'Newsfeed';
+$string['block_rss_feed_added'] = 'Newsfeed hinzugefügt';
+$string['block_rss_feed_deleted'] = 'Neswfeed gelöscht';
+$string['block_rss_feed_updated'] = 'Newsfeed aktualisiert';
+$string['block_rss_feeds'] = 'Newsfeeds';
+$string['block_rss_feeds_add_edit'] = 'Hinzufügen/bearbeiten der Feeds';
+$string['block_rss_feeds_title'] = 'Remote RSS Feeds';
+$string['block_rss_find_more_feeds'] = 'Weitere RSS Feeds finden';
+$string['block_rss_no_feeds'] = 'Es sind keine RSS Feeds für diese Seite festgelegt';
+$string['block_rss_num_entries'] = 'Anzahl der RSS Links, die pro Block angezeigt werden sollen';
+$string['block_rss_pick_feed'] = 'Neuen Newsfeed auswählen';
+$string['block_rss_remote_news_feed'] = 'Remote Newsfeed';
+$string['block_rss_see_all_feeds'] = 'Alle Feeds ansehen';
+$string['block_rss_shownumentries'] = 'Höchstzahl der Einträge, die im Block gezeigt werden';
+$string['block_rss_submitters'] = 'Wer kann neue RSS Feeds festlegen? Feeds stehen allen Nutzern der Seite zur Verfügung und können von Ihnen für ihre eigenen Seiten genutzt werden.';
+$string['block_rss_timeout'] = 'block_rss_timeout';
+$string['block_rss_timeout_desc'] = 'Zeit (Minuten), die ein RSS Feed im Cache verbleibt';
+$string['block_rss_word_title'] = 'Titel';
+
+?>
---
+++ lang/de/rssfeed.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // rssfeed.php - created with Moodle 1.4.3 (2004083130)
+
+
+$string['blockname'] = 'Zusammengeführte Feeds';
+$string['configrssfile'] = 'Geben Sie einen kurzen Seitennamen ein, gefolgt von einer  RSS Url (inkl. http://) in einer neuen Zeile<br/> und wiederholen Sie dies, wenn sie mehrere Einträge vornehmen wollen ';
+$string['extratext'] = '<a href=\"http://www.webthings.nl/nieuwsbrief\">Link zu Newsletter</a>';
+$string['maxitems'] = 'Maximale Zahl von Einträgen, die angezeigt werden soll';
+$string['nofeed'] = 'Keine Feeds verfügbar';
+$string['refresh'] = 'Zeit (Stunden), bevor nach Updates gesucht werden soll';
+
+?>
