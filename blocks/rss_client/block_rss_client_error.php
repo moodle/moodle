7530140579002b4bceed5b4b22fb3a5d2a842bfc
dhawes <dhawes>
2005-01-19 23:55:25 +0000

    I couldn't find a way to do this so I added this file. When an RSS feed fails to load and debug is on the rendered page could get quite ugly. I now capture the error into a string and simply show the user a 'load feed error' link pointing to this new error page. This new error page simply displays the full error message with more detail about why that particular feed failed to load (from file or url, etc)

---
+++ blocks/rss_client/block_rss_client_error.php
@@ -0,0 +1,15 @@
+<?php //$Id$
+
+    require_once('../../config.php');
+    global $USER, $CFG;
+    
+    require_variable($error);
+    
+    print_header(get_string('error'), 
+                 get_string('error'), 
+                 get_string('error') );
+
+    print urldecode($error);
+
+    print_footer();
+?>
