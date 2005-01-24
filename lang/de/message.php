c65f162fc26fcbc4e67b90bf2a00c854c4d2d166
ralf-bonn <ralf-bonn>
2005-01-24 15:40:54 +0000

    First german translation

---
+++ lang/de/message.php
@@ -0,0 +1,61 @@
+<?PHP // $Id$ 
+      // message.php - created with Moodle 1.4.3 (2004083130)
+
+
+$string['addcontact'] = 'Kontakt hinzufügen';
+$string['ago'] = '$a beendet';
+$string['allmine'] = 'Alle Mitteilungen an mich oder von mir';
+$string['allstudents'] = 'Alle Mitteilungen unter den Teilnehmer/innen';
+$string['allusers'] = 'Alle Mitteilungen (Teilnehmer/innen und Trainer/innen)';
+$string['beepnewmessage'] = 'Akustisches Signal beim Eingang neuer Nachrichten';
+$string['blockcontact'] = 'Kontakt sperren';
+$string['blockedmessages'] = '$a Mitteilung(en) an/von gesperrten Nutzer/innen';
+$string['contacts'] = 'Kontakte';
+$string['context'] = 'Kontext';
+$string['deletemessagesdays'] = 'Dauer (in Tagen) bis alte Mitteilungen automatisch gelöscht werden';
+$string['discussion'] = 'Diskussion';
+$string['emailmessages'] = 'Mitteilungen per E-Mail versenden, wenn ich offline bin';
+$string['emailtagline'] = 'Diese E-Mail ist eine Kopie der Mitteilungen, die Sie an \"$a\" geschickt haben';
+$string['emptysearchstring'] = 'Sie müssen einen Suchbegriff eingeben';
+$string['formorethan'] = 'Für mehr als';
+$string['includeblockedusers'] = 'Gesperrte Nutzer/innen einschließen';
+$string['incomingcontacts'] = 'Eingehende Kontakte ($a)';
+$string['keywords'] = 'Schlüsselbegriffe';
+$string['keywordssearchresults'] = 'Suchergebnis: $a Mitteilungenn gefunden';
+$string['maxmessages'] = 'Höchstzahl der Mitteilungen, die im Rückblick angezeigt werden';
+$string['message'] = 'Mitteilung';
+$string['messagehistory'] = 'Mitteilungen (Rückblick)';
+$string['messages'] = 'Mitteilungen';
+$string['messaging'] = 'Mitteilungen';
+$string['mycontacts'] = 'Meine Kontakte';
+$string['newsearch'] = 'Neue Suche';
+$string['nomessages'] = 'Es liegen keine neuen Mitteilungen vor';
+$string['nomessagesfound'] = 'Es wurden keine Mittelungenn gefunden';
+$string['nosearchresults'] = 'Es liegen keine Ergebnisse für Ihre Suche vor';
+$string['offline'] = 'Offline';
+$string['offlinecontacts'] = 'Offline Kontakte ($a)';
+$string['online'] = 'Online';
+$string['onlinecontacts'] = 'Online Kontakte ($a)';
+$string['onlyfromme'] = 'Nur Mitteilungen von mir';
+$string['onlymycourses'] = 'Nur in meinen Kursen';
+$string['onlytome'] = 'Nur Mitteilungen an mich';
+$string['pagerefreshes'] = 'Diese Seite wird automatisch nach $a Sekunden aktualisiert';
+$string['removecontact'] = 'Kontakt entfernen';
+$string['savemysettings'] = 'Meine Einstellungen speichern';
+$string['search'] = 'Suche';
+$string['searchforperson'] = 'Suche nach einer Person';
+$string['searchmessages'] = 'Suche nach Mitteilungen';
+$string['sendmessage'] = 'Mitteilungen senden';
+$string['sendmessageto'] = 'Mitteilungen an $a versenden';
+$string['settings'] = 'Einstellungen';
+$string['settingssaved'] = 'Ihre Einstellungen wurden gespeichert';
+$string['showmessagewindow'] = 'Mitteilungsfenster automatisch öffnen wenn eine neue Mitteilung ankommt (Pop-Up-Fenster dürfen für diese Funktion nicht geblockt werden)';
+$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%I:%%M %%p';
+$string['timenosee'] = 'Minute seit ich zuletzt online war';
+$string['timesent'] = 'Sendezeitpunkt';
+$string['unblockcontact'] = 'Kontakt aktivieren';
+$string['unreadmessages'] = '$a ungelesene Mitteilungen';
+$string['userisblockingyou'] = 'Diese/r Teilnehmer/innen hat Mitteilungen von Ihnen gesperrt';
+$string['userssearchresults'] = 'Suchergebniss(e): $a Nutzer/innen gefunden';
+
+?>
