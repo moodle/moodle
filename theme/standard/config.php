3e92eca303de3b96c7b13893f94643b7d4489de9
moodler <moodler>
2005-01-25 13:20:44 +0000

    I've set the old THEME variables to bright green to help identify where hard-coded colours need to be removed

--- theme/standard/config.php
+++ theme/standard/config.php
@@ -1,14 +1,17 @@
 <?PHP // $Id$
 
-$THEME->body         = "#FFEECE";  // Main page color
-$THEME->cellheading  = "#FFD991";  // Standard headings of big tables
-$THEME->cellheading2 = "#FFC85F";  // Highlight headings of tables
-$THEME->cellcontent  = "#FFFFFF";  // For areas with text
-$THEME->cellcontent2 = "#FEE6B9";  // Alternate colour
-$THEME->borders      = "#FFC85F";  // Table borders
-$THEME->highlight    = "#AAFFAA";  // Highlighted text (eg after a search)
-$THEME->hidden       = "#AAAAAA";  // To color things that are hidden
-$THEME->autolink     = "#DDDDDD";  // To color auto-generated links (eg glossary)
+// These colours are not used anymore, so I've set them to 
+// bright red to help identify where they should be removed
+
+$THEME->body         = "#22FF22";  // Main page color
+$THEME->cellheading  = "#22FF22";  // Standard headings of big tables
+$THEME->cellheading2 = "#22FF22";  // Highlight headings of tables
+$THEME->cellcontent  = "#22FF22";  // For areas with text
+$THEME->cellcontent2 = "#22FF22";  // Alternate colour
+$THEME->borders      = "#22FF22";  // Table borders
+$THEME->highlight    = "#22FF22";  // Highlighted text (eg after a search)
+$THEME->hidden       = "#22FF22";  // To color things that are hidden
+$THEME->autolink     = "#22FF22";  // To color auto-generated links (eg glossary)
 
 $THEME->custompix    = false;      // If true, then this theme must have a "pix" 
                                    // subdirectory that contains copies of all 
