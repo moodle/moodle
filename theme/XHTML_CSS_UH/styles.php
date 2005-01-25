f254d4d99130c64aa3a15d83e8bf56ef97019cb6
urs_hunkler <urs_hunkler>
2005-01-25 15:21:02 +0000

    Changed header.html, footer.html and the CSS files to work with the new DIV framework. Thanks Martin for making it being writing from weblib.php ;-)

--- theme/XHTML_CSS_UH/styles.php
+++ theme/XHTML_CSS_UH/styles.php
@@ -11,7 +11,7 @@
     $themeurl = style_sheet_setup(filemtime("styles.php"), 300, $themename);
 
     include ("./styles_layout.css");
-    include ("./styles_typography.css");
+    include ("./styles_font.css");
     include ("./styles_color.css");
-
+    // include ("./styles_block.css");
 ?>
