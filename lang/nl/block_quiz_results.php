ef86247e18817a25225b26c91f372cae7a0fea46
koenr <koenr>
2005-01-26 15:08:38 +0000

    missing strings

---
+++ lang/nl/block_quiz_results.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // block_quiz_results.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005012501)
+
+
+$string['bestgrades'] = 'De $a hoogste cijfers';
+$string['config_format_absolute'] = 'Absolute cijfers';
+$string['config_format_percentage'] = 'Percentages';
+$string['config_grade_format'] = 'Toon cijfers als:';
+$string['config_select_quiz'] = 'Van welke test wil je dat dit blok de resultaten toont?';
+$string['config_show_best'] = 'Hoeveel van de hoogste cijfers moeten getoond worden (0 om uit te schakelen)?';
+$string['config_show_worst'] = 'Hoeveel van de laagste cijfers moeten getoond worden (0 om uit te schakelen)?';
+$string['config_use_groups'] = 'Toon groepen in plaats van leerlingen (alleen als de test groepen ondersteund)?';
+$string['formaltitle'] = 'Testresultaten';
+$string['worstgrades'] = 'De $a laagste cijfers';
+
+?>
