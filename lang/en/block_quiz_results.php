95ceba88f28fe09d3f331bc38dfe9c0fbb3fc573
defacer <defacer>
2005-01-26 00:29:42 +0000

    Initial checkin of language for the quiz_results block.

---
+++ lang/en/block_quiz_results.php
@@ -0,0 +1,15 @@
+<?php // $Id$
+
+$string['bestgrades'] = 'The $a highest grades:';
+$string['worstgrades'] = 'The $a lowest grades:';
+$string['formaltitle'] = 'Quiz Results';
+$string['config_select_quiz'] = 'Which quiz should this block display results from?';
+$string['config_show_best'] = 'How many of the highest grades should be shown (0 to disable)?';
+$string['config_show_worst'] = 'How many of the lowest grades should be shown (0 to disable)?';
+$string['config_use_groups'] = 'Show groups instead of students (only if the quiz supports groups)?';
+$string['config_grade_format'] = 'Display grades as:';
+$string['config_format_percentage'] = 'Percentages';
+$string['config_format_fraction'] = 'Fractions';
+$string['config_format_absolute'] = 'Absolute numbers';
+
+?>
