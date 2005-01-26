ba05c3aeeb78c7c1034c0e44932242a1d39bf1b3
defacer <defacer>
2005-01-26 22:23:39 +0000

    More language strings for the quiz_results block. I think that will be all.

--- lang/en/block_quiz_results.php
+++ lang/en/block_quiz_results.php
@@ -1,7 +1,9 @@
 <?php // $Id$
 
 $string['bestgrades'] = 'The $a highest grades:';
+$string['bestgroupgrades'] = 'The $a groups with the highest average:';
 $string['worstgrades'] = 'The $a lowest grades:';
+$string['worstgroupgrades'] = 'The $a groups with the lowest average:';
 $string['formaltitle'] = 'Quiz Results';
 $string['config_select_quiz'] = 'Which quiz should this block display results from?';
 $string['config_show_best'] = 'How many of the highest grades should be shown (0 to disable)?';
@@ -11,5 +13,8 @@ $string['config_grade_format'] = 'Display grades as:';
 $string['config_format_percentage'] = 'Percentages';
 $string['config_format_fraction'] = 'Fractions';
 $string['config_format_absolute'] = 'Absolute numbers';
+$string['error_emptyquizid'] = 'There is an error right now with this block: you need to select which quiz it should display results from.';
+$string['error_emptyquizrecord'] = 'There is an error right now with this block: the selected quiz does not seem to exist in the database.';
+$string['error_nogroupsexist'] = 'There is an error right now with this block: it is set to display grades in group mode, but the course has no defined groups.';
 
 ?>
