7e30a666530060e6d4dc974a3321f5f81427d770
koenr <koenr>
2005-01-26 11:34:38 +0000

    changed boodschap to bericht: shorter and more neutral

--- lang/nl/message.php
+++ lang/nl/message.php
@@ -1,61 +1,61 @@
 <?PHP // $Id$ 
-      // message.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005011000)
+      // message.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005012501)
 
 
 $string['addcontact'] = 'Voeg een contactpersoon toe';
 $string['ago'] = '$a geleden';
-$string['allmine'] = 'Alle boodschappen aan mij of van mij';
-$string['allstudents'] = 'Alle boodschappen tussen leerlingen binnen een vak';
-$string['allusers'] = 'Alle boodschappen van alle gebruikers';
-$string['beepnewmessage'] = 'Geluidssignaal als er een nieuwe boodschap binnenkomt';
+$string['allmine'] = 'Alle berichten aan mij of van mij';
+$string['allstudents'] = 'Alle berichten tussen leerlingen binnen een vak';
+$string['allusers'] = 'Alle berichten van alle gebruikers';
+$string['beepnewmessage'] = 'Geluidssignaal als er een nieuw bericht binnenkomt';
 $string['blockcontact'] = 'Blokkeer contactpersoon';
-$string['blockedmessages'] = '$a boodschap(pen) van/aan geblokkeerde gebruikers';
+$string['blockedmessages'] = '$a bericht(en) van/aan geblokkeerde gebruikers';
 $string['contacts'] = 'Contactpersonen';
-$string['context'] = 'contekst';
+$string['context'] = 'context';
 $string['deletemessagesdays'] = 'Aantal dagen voor oude boodschappen automatisch verwijderd worden';
 $string['discussion'] = 'Discussie';
-$string['emailmessages'] = 'E-mail boodschappen als ik off line ben';
-$string['emailtagline'] = 'Deze e-mail is een kopie van een boodschap die naar jouw gestuurd is op \"$a\"';
+$string['emailmessages'] = 'E-mail berichten als ik off line ben';
+$string['emailtagline'] = 'Deze e-mail is een kopie van een bericht dat naar jouw gestuurd is op \"$a\"';
 $string['emptysearchstring'] = 'Je moet iets zoeken';
 $string['formorethan'] = 'Langer dan';
 $string['includeblockedusers'] = 'Geblokkeerde bezoekers inbegrepen';
 $string['incomingcontacts'] = 'Binnenkomende contactpersonen ($a)';
 $string['keywords'] = 'Sleutelwoorden';
-$string['keywordssearchresults'] = 'Zoekresultaten: $a boodschappen gevonden';
-$string['maxmessages'] = 'Maximum aantal boodschappen dat in de discussiegeschiedenis getoond mag worden';
-$string['message'] = 'Boodschap';
-$string['messagehistory'] = 'Geschiedenis van de boodschap';
-$string['messages'] = 'Boodschappen';
-$string['messaging'] = 'Boodschappen sturen';
+$string['keywordssearchresults'] = 'Zoekresultaten: $a berichten gevonden';
+$string['maxmessages'] = 'Maximum aantal berichten dat in de discussiegeschiedenis getoond mag worden';
+$string['message'] = 'Bericht';
+$string['messagehistory'] = 'Geschiedenis van het bericht';
+$string['messages'] = 'Berichten';
+$string['messaging'] = 'Berichten sturen';
 $string['mycontacts'] = 'Mijn contactpersonen';
 $string['newsearch'] = 'Nieuwe zoekopdracht';
-$string['nomessages'] = 'Geen wachtende boodschappen';
-$string['nomessagesfound'] = 'Geen boodschappen gevonden';
+$string['nomessages'] = 'Geen wachtende berichten';
+$string['nomessagesfound'] = 'Geen berichten gevonden';
 $string['nosearchresults'] = 'Je zoekopdracht gaf geen resultaten';
 $string['offline'] = 'Off line';
 $string['offlinecontacts'] = 'Off line contactpersonenen ($a)';
 $string['online'] = 'On line';
 $string['onlinecontacts'] = 'On line contactpersonen ($a)';
-$string['onlyfromme'] = 'Enkel boodschappen van mij';
+$string['onlyfromme'] = 'Enkel mijn berichten';
 $string['onlymycourses'] = 'Enkel in mijn vakken';
-$string['onlytome'] = 'Enkel boodschappen aan mij';
+$string['onlytome'] = 'Enkel berichten aan mij';
 $string['pagerefreshes'] = 'Deze pagina ververst automatisch om de $a seconden';
 $string['removecontact'] = 'Verwijder contactpersoon';
 $string['savemysettings'] = 'Bewaar mijn instellingen';
 $string['search'] = 'Zoek';
 $string['searchforperson'] = 'Zoek een persoon';
-$string['searchmessages'] = 'Zoek  boodschappen';
-$string['sendmessage'] = 'Stuur boodschap';
-$string['sendmessageto'] = 'Stuur boodschap aan $a';
+$string['searchmessages'] = 'Zoek  berichten';
+$string['sendmessage'] = 'Stuur bericht';
+$string['sendmessageto'] = 'Stuur bericht aan $a';
 $string['settings'] = 'Instellingen';
 $string['settingssaved'] = 'Jouw instellingen werden bewaard';
-$string['showmessagewindow'] = 'Toon automatisch het boodschapvenster wanneer ik nieuwe boodschappen krijg (je browser moet popups toelaten voor deze site)';
+$string['showmessagewindow'] = 'Toon automatisch het berichtvenster wanneer ik nieuwe berichten krijg (je browser moet popups toelaten voor deze site)';
 $string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%I:%%M %%p';
 $string['timenosee'] = 'Minuten sinds ik de laatste keer online gezien ben';
 $string['timesent'] = 'Tijd verstuurd';
 $string['unblockcontact'] = 'deblokkeer deze contactpersoon';
-$string['unreadmessages'] = '$a ongelezen boodschappen';
-$string['userisblockingyou'] = 'Deze gebruiker heeft je geblokkeerd om boodschappen naar hem te sturen';
+$string['unreadmessages'] = '$a ongelezen berichten';
+$string['userisblockingyou'] = 'Deze gebruiker heeft je geblokkeerd om berichten naar hem te sturen';
 $string['userssearchresults'] = 'Zoekresultaten: $a gebruikers gevonden';
 
 ?>
