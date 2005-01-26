93d882dfe9ca3d84d60f6f5a5644618c257790ea
koenr <koenr>
2005-01-26 14:50:58 +0000

    missing string

--- lang/nl/block_course_summary.php
+++ lang/nl/block_course_summary.php
@@ -1,11 +1,8 @@
 <?PHP // $Id$ 
-      // block_course_summary.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004111000)
+      // block_course_summary.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005012501)
 
 
-$string['blockname'] = 'Samenvatting van het vak';
+$string['coursesummary'] = 'Vakbeschrijving';
 $string['pagedescription'] = 'Vak- / sitebeschrijving';
-$string['thischarset'] = 'iso-8859-1';
-$string['thisdirection'] = 'ltr';
-$string['thislanguage'] = 'Nederlands';
 
 ?>
