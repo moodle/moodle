d175f50f9785aa9671ffca9bc499f25927ee9b52
mits <mits>
2005-01-27 08:25:07 +0000

    polished.

--- lang/ja_utf8/block_quiz_results.php
+++ lang/ja_utf8/block_quiz_results.php
@@ -2,7 +2,7 @@
       // block_quiz_results.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005012501)
 
 
-$string['bestgrades'] = '$a の最高評点:';
+$string['bestgrades'] = 'ベスト $a:';
 $string['config_format_absolute'] = '絶対数';
 $string['config_format_fraction'] = '分数';
 $string['config_format_percentage'] = 'パーセンテージ';
@@ -12,6 +12,6 @@ $string['config_show_best'] = '何件の最高評点を表示しますか (0 は
 $string['config_show_worst'] = '何件の最低評点を表示しますか (0 は非表示 ) ?';
 $string['config_use_groups'] = '学生の代わりにグループを表示する ( 小テストがグループをサポートする場合のみ )?';
 $string['formaltitle'] = '小テストの結果';
-$string['worstgrades'] = '$a の最低評点:';
+$string['worstgrades'] = 'ワースト $a';
 
 ?>
