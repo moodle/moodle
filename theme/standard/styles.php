6fde77de243225885b19d9fc3b5c28895df31e37
moodler <moodler>
2005-01-30 18:17:44 +0000

    Modifications to allow parent theme, and some genral cleanups

--- theme/standard/styles.php
+++ theme/standard/styles.php
@@ -4,40 +4,53 @@
 /// up any necessary variables, and lets us include raw CSS files.
 /// The output of this script should be a completely standard CSS file.
 
-    if (!isset($themename)) {
-        $themename = NULL;
-    }
+/// There should be no need to modify this file!!  Use config.php instead.
 
     $nomoodlecookie = true;
     require_once("../../config.php");
 
-/// Following lines are just for standard theme/styles.php
+    $lastmodified = 0;
+    $lifetime = 600;
+
+/// The following lines are only for standard/theme/styles.php
+
     if (!isset($THEME->standardsheets) or $THEME->standardsheets === true) { // Use all the sheets we have
-        $subsheets = array('styles_layout', 'styles_fonts', 'styles_color', 'styles_moz');
+        $THEME->sheets = array('styles_layout', 'styles_fonts', 'styles_color', 'styles_moz');
     } else if (empty($THEME->standardsheets)) {                              // We can stop right now!
         exit;
     } else {                                                                 // Use the provided subset only
-        $subsheets = $THEME->standardsheets;
+        $THEME->sheets = $THEME->standardsheets;
     }
 
-/// Normal themes will just use a line like this instead of the above.
-/// $subsheets = array('styles_layout', 'styles_fonts', 'styles_color', 'styles_moz');
+/// If we are a parent theme, then check for parent definitions
 
-/// There should be no need to touch the following
-
-    $lastmodified = filemtime('styles.php');
-
-    foreach ($subsheets as $subsheet) {
-        $lastmodifiedsub = filemtime($subsheet.'.css');
-        if ($lastmodifiedsub > $lastmodified) {
-            $lastmodified = $lastmodifiedsub;
+    if (isset($parent)) {
+        if (!isset($THEME->parentsheets) or $THEME->parentsheets === true) {     // Use all the sheets we have
+            $THEME->sheets = array('styles_layout', 'styles_fonts', 'styles_color', 'styles_moz');
+        } else if (empty($THEME->parentsheets)) {                                // We can stop right now!
+            exit;
+        } else {                                                                 // Use the provided subset only
+            $THEME->sheets = $THEME->parentsheets;
         }
     }
 
-    $themeurl = style_sheet_setup($lastmodifiedsub, 600, $themename);
+/// Work out the last modified date for this theme
 
-    foreach ($subsheets as $subsheet) {
-        include_once($subsheet.'.css');
+    foreach ($THEME->sheets as $sheet) {
+        $sheetmodified = filemtime($sheet.'.css');
+        if ($sheetmodified > $lastmodified) {
+            $lastmodified = $sheetmodified;
+        }
+    }
+
+/// Print out the entire style sheet
+
+    style_sheet_setup($lastmodified, $lifetime);
+
+    foreach ($THEME->sheets as $sheet) {
+        echo "/***** $sheet.css start *****/\n\n";
+        include_once($sheet.'.css');
+        echo "\n\n/***** $sheet.css end *****/\n\n";
     }
 
 ?>
