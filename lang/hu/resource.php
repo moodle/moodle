68cbb1f093f2976220a3e36eaf0372fd6650dedb
koenr <koenr>
2005-01-30 22:33:19 +0000

    missed a serious error

--- lang/hu/resource.php
+++ lang/hu/resource.php
@@ -1,40 +1,39 @@
 <?PHP // $Id$ 
-      // resource.php - created with Moodle 1.1.1 (2003091111)
+      // resource.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005012800)
 
 
-$string['addresource'] = "Tananyag hozzáadása";
-$string['editingaresource'] = "Tananyag szerkesztése";
-$string['example'] = "Példa";
-$string['examplereference'] = "Tobin, K. & Tippins, D (1993) Constructivism as a Referent for Teaching and Learning. In: K. Tobin (Ed) The Practice of Constructivism in Science Education, pp 3-21, Lawrence-Erlbaum, Hillsdale, NJ.";
-$string['exampleurl'] = "http://www.pelda.com/konyvtar/egyfajl.html";
-$string['filename'] = "Fájlnév";
-$string['fulltext'] = "Szöveg";
-$string['htmlfragment'] = "HTML részlet";
-$string['modulename'] = "Tananyag";
-$string['modulenameplural'] = "Tananyagok";
-$string['neverseen'] = "Ismeretlen";
-$string['newfullscreen'] = "Teljes képernyõs";
-$string['newheight'] = "Alapértelmezett ablakmagasság (pixel)";
-$string['newmenubar'] = "Menüsor megjelenítése";
-$string['newresizable'] = "Ablak átméretezésének engedélyezése";
-$string['newscrollbars'] = "Ablak gördítésének engedélyezése";
-$string['newstatus'] = "Státuszsor megmutatása";
-$string['newtoolbar'] = "Eszköztár megmutatása";
-$string['newwidth'] = "Alapértelmezett ablakszélesség (pixelben)";
-$string['newwindow'] = "Új ablak";
-$string['newwindowopen'] = "Ez a ttananyag új ablakban jelenjen meg";
-$string['note'] = "Megjegyzés";
-$string['notefile'] = "Egy vagy több fájl feltöltéséhez (ezután jelenik meg a listán) használja a 
-<A HREF=\$a >Fájl Menedzsert</A>.";
-$string['notypechosen'] = "Típust kell választania. A Vissza gombbal menjen az elõzõ oldalra és próbálja újból.";
-$string['resourcetype'] = "Tananyag típusa";
-$string['resourcetype1'] = "Hivatkozás";
-$string['resourcetype2'] = "Web oldal";
-$string['resourcetype3'] = "Feltöltött fájl";
-$string['resourcetype4'] = "Egyszerû szöveg";
-$string['resourcetype5'] = "Web hivatkozás";
-$string['resourcetype6'] = "HTML szöveg";
-$string['resourcetype7'] = "Program";
-$string['resourcetype8'] = "Wiki szöveg";
+$string['addresource'] = 'Tananyag hozzáadása';
+$string['editingaresource'] = 'Tananyag szerkesztése';
+$string['example'] = 'Példa';
+$string['exampleurl'] = 'http://www.pelda.com/konyvtar/egyfajl.html';
+$string['filename'] = 'Fájlnév';
+$string['fulltext'] = 'Szöveg';
+$string['htmlfragment'] = 'HTML részlet';
+$string['modulename'] = 'Tananyag';
+$string['modulenameplural'] = 'Tananyagok';
+$string['neverseen'] = 'Ismeretlen';
+$string['newfullscreen'] = 'Teljes képernyõs';
+$string['newheight'] = 'Alapértelmezett ablakmagasság (pixel)';
+$string['newmenubar'] = 'Menüsor megjelenítése';
+$string['newresizable'] = 'Ablak átméretezésének engedélyezése';
+$string['newscrollbars'] = 'Ablak gördítésének engedélyezése';
+$string['newstatus'] = 'Státuszsor megmutatása';
+$string['newtoolbar'] = 'Eszköztár megmutatása';
+$string['newwidth'] = 'Alapértelmezett ablakszélesség (pixelben)';
+$string['newwindow'] = 'Új ablak';
+$string['newwindowopen'] = 'Ez a ttananyag új ablakban jelenjen meg';
+$string['note'] = 'Megjegyzés';
+$string['notefile'] = 'Egy vagy több fájl feltöltéséhez (ezután jelenik meg a listán) használja a 
+<a href=\"$a\">Fájl Menedzsert</a>.';
+$string['notypechosen'] = 'Típust kell választania. A Vissza gombbal menjen az elõzõ oldalra és próbálja újból.';
+$string['resourcetype'] = 'Tananyag típusa';
+$string['resourcetype1'] = 'Hivatkozás';
+$string['resourcetype2'] = 'Web oldal';
+$string['resourcetype3'] = 'Feltöltött fájl';
+$string['resourcetype4'] = 'Egyszerû szöveg';
+$string['resourcetype5'] = 'Web hivatkozás';
+$string['resourcetype6'] = 'HTML szöveg';
+$string['resourcetype7'] = 'Program';
+$string['resourcetype8'] = 'Wiki szöveg';
 
 ?>
