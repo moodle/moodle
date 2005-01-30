3b765890f3de35ea19787565fb20c045c9abc0ed
moodler <moodler>
2005-01-30 18:21:54 +0000

    Updated to latest styles.php and config.php

--- theme/orangewhite/styles.php
+++ theme/orangewhite/styles.php
@@ -4,28 +4,43 @@
 /// up any necessary variables, and lets us include raw CSS files.
 /// The output of this script should be a completely standard CSS file.
 
-/// THERE IS USUALLY NO NEED TO EDIT THIS FILE!  See config.php
-
-    if (!isset($themename)) {
-        $themename = NULL;
-    }
+/// There should be no need to modify this file!!  Use config.php instead.
 
     $nomoodlecookie = true;
     require_once("../../config.php");
 
-    $lastmodified = filemtime('styles.php');
+    $lastmodified = 0;
+    $lifetime = 600;
 
-    foreach ($THEME->subsheets as $subsheet) {
-        $lastmodifiedsub = filemtime($subsheet.'.css');
-        if ($lastmodifiedsub > $lastmodified) {
-            $lastmodified = $lastmodifiedsub;
+/// If we are a parent theme, then check for parent definitions
+
+    if (isset($parent)) {
+        if (!isset($THEME->parentsheets) or $THEME->parentsheets === true) {     // Use all the sheets we have
+            $THEME->sheets = array('styles_layout', 'styles_fonts', 'styles_color', 'styles_moz');
+        } else if (empty($THEME->parentsheets)) {                                // We can stop right now!
+            exit;
+        } else {                                                                 // Use the provided subset only
+            $THEME->sheets = $THEME->parentsheets;
         }
     }
 
-    $themeurl = style_sheet_setup($lastmodifiedsub, 600, $themename);
+/// Work out the last modified date for this theme
 
-    foreach ($THEME->subsheets as $subsheet) {
-        include_once($subsheet.'.css');
+    foreach ($THEME->sheets as $sheet) {
+        $sheetmodified = filemtime($sheet.'.css');
+        if ($sheetmodified > $lastmodified) {
+            $lastmodified = $sheetmodified;
+        }
+    }
+
+/// Print out the entire style sheet
+
+    style_sheet_setup($lastmodified, $lifetime);
+
+    foreach ($THEME->sheets as $sheet) {
+        echo "/***** $sheet.css start *****/\n\n";
+        include_once($sheet.'.css');
+        echo "\n\n/***** $sheet.css end *****/\n\n";
     }
 
 ?>
