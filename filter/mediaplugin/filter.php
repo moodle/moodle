a2df2a1ac05111fa35ee464307d541750d0cf718
moodler <moodler>
2005-02-01 03:24:25 +0000

    Updates from Charlie Williams and "wild girl"s fantastic flash friend
    MP3 player now has a slider with loading information, and is slightly less high
    (15px instead of 18px) so it fits better inline.
    
    Source included.

--- filter/mediaplugin/filter.php
+++ filter/mediaplugin/filter.php
@@ -21,14 +21,14 @@ function mediaplugin_filter($courseid, $text) {
     if (empty($CFG->filter_mediaplugin_ignore_mp3)) {
         $search = '/<a(.*?)href=\"([^<]+)\.mp3\"([^>]*)>(.*?)<\/a>/i';
 
-        $replace  = '\\0&nbsp;<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"';
+        $replace  = '\\0&nbsp;<object class="mediaplugin mp3" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"';
         $replace .= ' codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" ';
-        $replace .= ' width="35" height="18" id="mp3player">';
+        $replace .= ' width="90" height="15" id="mp3player">';
         $replace .= " <param name=\"movie\" value=\"$CFG->wwwroot/filter/mediaplugin/mp3player.swf?src=\\2.mp3\" />";
         $replace .= ' <param name="quality" value="high" />';
         $replace .= ' <param name="bgcolor" value="#333333" />';
         $replace .= " <embed src=\"$CFG->wwwroot/filter/mediaplugin/mp3player.swf?src=\\2.mp3\" ";
-        $replace .= "  quality=\"high\" bgcolor=\"#333333\" width=\"35\" height=\"18\" name=\"mp3player\" ";
+        $replace .= "  quality=\"high\" bgcolor=\"#333333\" width=\"90\" height=\"15\" name=\"mp3player\" ";
         $replace .= ' type="application/x-shockwave-flash" ';
         $replace .= ' pluginspage="http://www.macromedia.com/go/getflashplayer">';
         $replace .= '</embed>';
@@ -40,7 +40,7 @@ function mediaplugin_filter($courseid, $text) {
     if (empty($CFG->filter_mediaplugin_ignore_swf)) {
         $search = '/<a(.*?)href=\"([^<]+)\.swf\"([^>]*)>(.*?)<\/a>/i';
 
-        $replace  = '\\0<p class="mediaplugin"><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"';
+        $replace  = '\\0<p class="mediaplugin swf"><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"';
         $replace .= ' codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" ';
         $replace .= ' width="400" height="300" id="mp3player">';
         $replace .= " <param name=\"movie\" value=\"\\2.swf\" />";
@@ -58,7 +58,7 @@ function mediaplugin_filter($courseid, $text) {
     if (empty($CFG->filter_mediaplugin_ignore_mov)) {
         $search = '/<a(.*?)href=\"([^<]+)\.mov\"([^>]*)>(.*?)<\/a>/i';
 
-        $replace  = '\\0<p class="mediaplugin"><object classid="CLSID:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B"';
+        $replace  = '\\0<p class="mediaplugin mov"><object classid="CLSID:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B"';
         $replace .= '        codebase="http://www.apple.com/qtactivex/qtplugin.cab" ';
         $replace .= '        height="300" width="400"';
         $replace .= '        id="quicktime" type="application/x-oleobject">';
@@ -80,7 +80,7 @@ function mediaplugin_filter($courseid, $text) {
     if (empty($CFG->filter_mediaplugin_ignore_wmv)) {
         $search = '/<a(.*?)href=\"([^<]+)\.wmv\"([^>]*)>(.*?)<\/a>/i';
 
-        $replace  = '\\0<p class="mediaplugin"><object classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95"';
+        $replace  = '\\0<p class="mediaplugin wmv"><object classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95"';
         $replace .= ' codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701" ';
         $replace .= ' standby="Loading Microsoft® Windows® Media Player components..." ';
         $replace .= ' id="msplayer" type="application/x-oleobject">';
@@ -107,7 +107,7 @@ function mediaplugin_filter($courseid, $text) {
     if (empty($CFG->filter_mediaplugin_ignore_mpg)) {
         $search = '/<a(.*?)href=\"([^<]+)\.(mpe?g)\"([^>]*)>(.*?)<\/a>/i';
 
-        $replace = '\\0<p class="mediaplugin"><object width="240" height="180">';
+        $replace = '\\0<p class="mediaplugin mpg"><object width="240" height="180">';
         $replace .= '<param name="src" value="\\2.\\3" />';
         $replace .= '<param name="controller" value="true" />';
         $replace .= '<param name="autoplay" value="false" />';
@@ -120,7 +120,7 @@ function mediaplugin_filter($courseid, $text) {
     if (empty($CFG->filter_mediaplugin_ignore_avi)) {
         $search = '/<a(.*?)href=\"([^<]+)\.avi\"([^>]*)>(.*?)<\/a>/i';
 
-        $replace = '\\0<p class="mediaplugin"><object width="240" height="180">';
+        $replace = '\\0<p class="mediaplugin avi"><object width="240" height="180">';
         $replace .= '<param name="src" value="\\2.avi" />';
         $replace .= '<param name="controller" value="true" />';
         $replace .= '<param name="autoplay" value="false" />';
