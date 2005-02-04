ea6c76e0f2d9c9b287168c0c1d7ceaf07bf03ecf
michaelpenne <michaelpenne>
2005-02-04 19:35:43 +0000

    removed a lot of styles because left menu and slideshow work in a table instead of styles.  This helps with compatibility with more browsers

--- mod/lesson/styles.php
+++ mod/lesson/styles.php
@@ -18,44 +18,12 @@ img {
 	color: <?PHP echo $THEME->body?>; 
 }
 
-.leftmenu { <?php //in view.php in lesson mod ?>
-	width:140px;
-	height:530px;
-	margin-top:-18px;
-	white-space:nowrap;
-}
-
-.leftmenu1 { <?php //in lesson.php in lesson mod ?>
-	background: no-repeat url(<?PHP echo "$themeurl"?>/leftmenu.jpg); 
-	width:140px;
-	height:530px;
-	margin-top:-6px;
-
-	white-space:nowrap;
-}
-
-.lmlinks {
-	width:140px;
-	height:400px;
-	overflow:auto;
-	<?php //white-space:nowrap; ?>
-	padding-left:7px;
-	padding-top:4px;
-	
-}
-
-
 .leftmenu a:link, .leftmenu a:active, .leftmenu a:visited, .leftmenu1 a:link, .leftmenu1 a:active, .leftmenu1 a:visited{
-font-size:.7em; 
+	font-size:.7em; 
 }
 
-.lmMainlinks a:link, .lmMainlinks a:active, .lmMainlinks a:visited{
-font-size:.9em; 
-}
-
-.slidepos {
-padding-left:153px; margin-top:-480px;
- font-family:Arial;
+.main a:link, .main a:active, .main a:visited{
+	font-size:.9em; 
 }
 
 .viewpos {
