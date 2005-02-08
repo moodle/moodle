43eb257412c77537b5d7fecb99c99564cda4e60c
borinsky2407 <borinsky2407>
2005-02-08 20:51:09 +0000

    adapted from the de-version, should xhtml-compatible

---
+++ lang/de_du/activitynames.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // activitynames.php - created with Moodle 1.3.1 + (2004052501)
+
+$string['parentlanguage'] = 'de';
+$string['thischarset'] = 'ISO-8859-1';
+$string['thisdirection'] = 'ltr';
+$string['thislanguage'] = 'ISO-8859-1';
+$string['filtername'] = 'Automatisches Verlinken der Bezeichnungen von Aktivitäten';
+
+?>
