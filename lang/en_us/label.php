d6af49274b0f69bf7a9bac4ff7a7baeca805be24
gustav_delius <gustav_delius>
2005-02-13 19:14:14 +0000

    Taken out duplicates and added a lot more enrollment strings, see bug 2541

--- lang/en_us/label.php
+++
@@ -1,9 +0,0 @@
-<?PHP // $Id$ 
-      // label.php - created with Moodle 1.2 development (2003120700)
-
-
-$string['labeltext'] = 'Label text';
-$string['modulename'] = 'Label';
-$string['modulenameplural'] = 'Labels';
-
-?>
