6cba878db85723e76d096cc79681c0867df92781
moodler <moodler>
2005-02-14 16:02:32 +0000

    Bring weekly into line with topics

--- course/format/weeks/format.php
+++ course/format/weeks/format.php
@@ -28,7 +28,7 @@
     }
 
     if ($course->newsitems) {
-        $news = forum_get_course_forum($course->id, "news");
+        $news = forum_get_course_forum($course->id, 'news');
     }
 
     $streditsummary  = get_string('editsummary');
@@ -42,10 +42,10 @@
 
     if ($editing) {
         $strstudents = moodle_strtolower($course->students);
-        $strweekhide = get_string("weekhide", "", $strstudents);
-        $strweekshow = get_string("weekshow", "", $strstudents);
-        $strmoveup = get_string("moveup");
-        $strmovedown = get_string("movedown");
+        $strweekhide = get_string('weekhide', '', $strstudents);
+        $strweekshow = get_string('weekshow', '', $strstudents);
+        $strmoveup   = get_string('moveup');
+        $strmovedown = get_string('movedown');
     }
 
 
@@ -54,7 +54,7 @@
 
 /// The left column ...
 
-    if(blocks_have_content($pageblocks[BLOCK_POS_LEFT]) || $editing) {
+    if (blocks_have_content($pageblocks[BLOCK_POS_LEFT]) || $editing) {
         echo '<td style="width: '.$preferred_width_left.'px;" id="left-column">';
         blocks_print_group($PAGE, $pageblocks[BLOCK_POS_LEFT]);
         echo '</td>';
@@ -64,23 +64,19 @@
     echo '<td id="middle-column">';
 
     print_heading_block(get_string('weeklyoutline'), 'outline');
-    print_spacer(8, 1, true);
 
-    echo "<table class=\"weeklyoutline\" border=\"0\" cellpadding=\"8\" cellspacing=\"0\" width=\"100%\">";
+    echo '<table class="weeks" width="100%">';
 
 /// If currently moving a file then show the current clipboard
     if (ismoving($course->id)) {
-        $stractivityclipboard = strip_tags(get_string("activityclipboard", "", addslashes($USER->activitycopyname)));
-        $strcancel= get_string("cancel");
-        echo "<tr>";
-        echo "<td colspan=\"3\" valign=\"top\" class=\"weeklyoutlineclip\" width=\"100%\">";
-        echo "<div><font size=\"2\">";
-        echo "$stractivityclipboard&nbsp;&nbsp;(<a href=\"mod.php?cancelcopy=true&amp;sesskey=$USER->sesskey\">$strcancel</a>)";
-        echo "</font></div>";
-        echo "</td>";
-        echo "</tr>";
-        echo "<tr><td colspan=\"3\"><img src=\"../pix/spacer.gif\" width=\"1\" height=\"1\" alt=\"\" /></td></tr>";
-    }
+        $stractivityclipboard = strip_tags(get_string('activityclipboard', '', addslashes($USER->activitycopyname)));
+        $strcancel= get_string('cancel');
+        echo '<tr class="clipboard">';
+        echo '<td colspan="3">';
+        echo $stractivityclipboard.'&nbsp;&nbsp;(<a href="mod.php?cancelcopy=true&amp;sesskey='.$USER->sesskey.'">'.$strcancel.'</a>)';
+        echo '</td>';
+        echo '</tr>';
+    }       
 
 /// Print Section 0 with general activities
 
@@ -88,17 +84,17 @@
     $thissection = $sections[$section];
 
     if ($thissection->summary or $thissection->sequence or isediting($course->id)) {
-        echo "<tr>";
-        echo "<td nowrap=\"nowrap\" class=\"weeklyoutlineside\" valign=\"top\" width=\"20\">&nbsp;</td>";
-        echo "<td valign=\"top\" class=\"weeklyoutlinecontent\" width=\"100%\">";
+        echo '<tr id="section-0" class="section">';
+        echo '<td class="left side">&nbsp;</td>';
+        echo '<td class="content">';
 
         $summaryformatoptions->noclean = true;
         echo format_text($thissection->summary, FORMAT_HTML, $summaryformatoptions);
 
         if (isediting($course->id)) {
-            echo " <a title=\"$streditsummary\" ".
-                 " href=\"editsection.php?id=$thissection->id\"><img height=\"11\" width=\"11\" src=\"$CFG->pixpath/t/edit.gif\" ".
-                 " border=\"0\" alt=\"$streditsummary\" /></a><br /><br />";
+            echo '<a title="'.$streditsummary.'" '.
+                 ' href="editsection.php?id='.$thissection->id.'"><img src="'.$CFG->pixpath.'/t/edit.gif" '.
+                 ' height="11" width="11" border="0" alt="'.$streditsummary.'" /></a><br /><br />';
         }
 
         print_section($course, $thissection, $mods, $modnamesused);
@@ -107,14 +103,17 @@
             print_section_add_menus($course, $section, $modnames);
         }
 
-        echo "</td>";
-        echo "<td nowrap=\"nowrap\" class=\"weeklyoutlineside\" valign=\"top\" align=\"center\" width=\"10\">";
-        echo "&nbsp;</td></tr>";
-        echo "<tr><td colspan=\"3\"><img src=\"../pix/spacer.gif\" width=\"1\" height=\"1\" alt=\"\" /></td></tr>";
+        echo '</td>';
+        echo '<td class="right side">&nbsp;</td>';
+        echo '</tr>';
+        echo '<tr class="section"><td colspan="3" class="spacer"></td></tr>';
+
     }
 
 
-/// Now all the weekly sections
+/// Now all the normal modules by week
+/// Everything below uses "section" terminology - each "section" is a week.
+
     $timenow = time();
     $weekdate = $course->startdate;    // this should be 0:00 Monday of that week
     $weekdate += 7200;                 // Add two hours to avoid possible DST problems
@@ -123,7 +122,7 @@
     $weekofseconds = 604800;
     $course->enddate = $course->startdate + ($weekofseconds * $course->numsections);
 
-    $strftimedateshort = " ".get_string("strftimedateshort");
+    $strftimedateshort = ' '.get_string('strftimedateshort');
 
     while ($weekdate < $course->enddate) {
 
@@ -138,10 +137,10 @@
             unset($thissection);
             $thissection->course = $course->id;   // Create a new week structure
             $thissection->section = $section;
-            $thissection->summary = "";
+            $thissection->summary = '';
             $thissection->visible = 1;
-            if (!$thissection->id = insert_record("course_sections", $thissection)) {
-                notify("Error inserting new week!");
+            if (!$thissection->id = insert_record('course_sections', $thissection)) {
+                notify('Error inserting new week!');
             }
         }
 
@@ -149,7 +148,7 @@
 
         if (!empty($displaysection) and $displaysection != $section) {  // Check this week is visible
             if ($showsection) {
-                $sectionmenu["week=$section"] = s("$strweek $section |     $weekday - $endweekday");
+                $sectionmenu['week='.$section] = s("$strweek $section |     $weekday - $endweekday");
             }
             $section++;
             $weekdate = $nextweekdate;
@@ -161,39 +160,31 @@
             $currentweek = (($weekdate <= $timenow) && ($timenow < $nextweekdate));
 
             if (!$thissection->visible) {
-                $colorsides = "class=\"weeklyoutlinesidehidden\"";
-                $colormain  = "class=\"weeklyoutlinecontenthidden\"";
+                $sectionstyle = ' hidden';
             } else if ($currentweek) {
-                $colorsides = "class=\"weeklyoutlinesidehighlight\"";
-                $colormain  = "class=\"weeklyoutlinecontenthighlight\"";
+                $sectionstyle = ' current';
             } else {
-                $colorsides = "class=\"weeklyoutlineside\"";
-                $colormain  = "class=\"weeklyoutlinecontent\"";
+                $sectionstyle = '';
             }
 
-            echo "<tr>";
-            echo "<td nowrap=\"nowrap\" $colorsides valign=\"top\" width=\"20\">";
-            echo "<div align=\"center\"><font size=\"3\"><a name=\"$section\">$section</a></font></div>";
-            echo "</td>";
+            echo '<tr id="section-'.$section.'" class="section'.$sectionstyle.'">';
+            echo '<td class="left side">';
+            echo '<a name="'.$section.'">'.$section.'</a>';
+            echo '</td>';
 
-            echo "<td valign=\"top\" $colormain width=\"100%\">";
-
+            echo '<td class="content">';
             if (!isteacher($course->id) and !$thissection->visible) {   // Hidden for students
-                echo "<div class=\"weeklydatetext\">$weekday - $endweekday ";
-                echo "(".get_string("notavailable").")";
-                echo "</div>";
-                echo "</td>";
+                echo '<div class="weekdates">'.$weekday.' - '.$endweekday.' ('.get_string('notavailable').')</div>';
 
             } else {
-
-                echo "<div class=\"weeklydatetext\">$weekday - $endweekday</div>";
+                echo '<div class="weekdates">'.$weekday.' - '.$endweekday.'</div>';
 
                 $summaryformatoptions->noclean = true;
                 echo format_text($thissection->summary, FORMAT_HTML, $summaryformatoptions);
 
                 if (isediting($course->id)) {
-                    echo " <a title=\"$streditsummary\" href=\"editsection.php?id=$thissection->id\">".
-                         "<img src=\"$CFG->pixpath/t/edit.gif\" height=\"11\" width=\"11\" border=\"0\" /></a><br /><br />";
+                    echo ' <a title="'.$streditsummary.'" href="editsection.php?id='.$thissection->id.'">'.
+                         '<img src="'.$CFG->pixpath.'/t/edit.gif" border="0" height="11" width="11" alt="" /></a><br /><br />';
                 }
 
                 print_section($course, $thissection, $mods, $modnamesused);
@@ -201,65 +192,59 @@
                 if (isediting($course->id)) {
                     print_section_add_menus($course, $section, $modnames);
                 }
-
-                echo "</td>";
             }
-            echo "<td nowrap=\"nowrap\" $colorsides valign=\"top\" align=\"center\" width=\"10\">";
-            echo "<font size=\"1\">";
+            echo '</td>';
+
+            echo '<td class="right side">';
 
             if ($displaysection == $section) {
-                echo "<a href=\"view.php?id=$course->id&amp;week=all\" title=\"$strshowallweeks\">".
-                     "<img src=\"$CFG->pixpath/i/all.gif\" height=\"25\" width=\"16\" border=\"0\" alt=\"\" /></a><br />";
+                echo '<a href="view.php?id='.$course->id.'&amp;week=all#'.$section.'" title="'.$strshowallweeks.'">'.
+                     '<img src="'.$CFG->pixpath.'/i/all.gif" height="25" width="16" border="0" /></a><br />';
             } else {
                 $strshowonlyweek = get_string("showonlyweek", "", $section);
-                echo "<a href=\"view.php?id=$course->id&amp;week=$section\" title=\"$strshowonlyweek\">".
-                     "<img src=\"$CFG->pixpath/i/one.gif\" height=\"16\" width=\"16\" border=\"0\" alt=\"\" /></a><br />";
+                echo '<a href="view.php?id='.$course->id.'&amp;week='.$section.'" title="'.$strshowonlyweek.'">'.
+                     '<img src="'.$CFG->pixpath.'/i/one.gif" height="16" width="16" border="0" alt="" /></a><br />';
             }
 
             if (isediting($course->id)) {
                 if ($thissection->visible) {        // Show the hide/show eye
-                    echo "<a href=\"view.php?id=$course->id&amp;hide=$section&amp;sesskey=$USER->sesskey\" title=\"$strweekhide\">".
-                         "<img src=\"$CFG->pixpath/i/hide.gif\" vspace=\"3\" height=\"16\" width=\"16\" border=\"0\" alt=\"\" /></a><br />";
+                    echo '<a href="view.php?id='.$course->id.'&amp;hide='.$section.'&amp;sesskey='.$USER->sesskey.'#'.$section.'" title="'.$strtopichide.'">'.
+                         '<img src="'.$CFG->pixpath.'/i/hide.gif" vspace="3" height="16" width="16" border="0" alt="" /></a><br />';
                 } else {
-                    echo "<a href=\"view.php?id=$course->id&amp;show=$section&amp;sesskey=$USER->sesskey\" title=\"$strweekshow\">".
-                         "<img src=\"$CFG->pixpath/i/show.gif\" vspace=\"3\" height=\"16\" width=\"16\" border=\"0\" alt=\"\" /></a><br />";
+                    echo '<a href="view.php?id='.$course->id.'&amp;show='.$section.'&amp;sesskey='.$USER->sesskey.'#'.$section.'" title="'.$strtopichide.'">'.
+                         '<img src="'.$CFG->pixpath.'/i/show.gif" vspace="3" height="16" width="16" border="0" alt="" /></a><br />';
                 }
-
                 if ($section > 1) {                       // Add a arrow to move section up
-                    echo "<a href=\"view.php?id=$course->id&amp;section=$section&amp;move=-1&amp;sesskey=$USER->sesskey\" title=\"$strmoveup\">".
-                         "<img src=\"$CFG->pixpath/t/up.gif\" vspace=\"3\" height=\"11\" width=\"11\" border=\"0\" alt=\"\" /></a><br />";
+                    echo '<a href="view.php?id='.$course->id.'&amp;section='.$section.'&amp;move=-1&amp;sesskey='.$USER->sesskey.'" title="'.$strmoveup.'">'.
+                         '<img src="'.$CFG->pixpath.'/t/up.gif" vspace="3" height="11" width="11" border="0" alt="" /></a><br />';
                 }
 
                 if ($section < $course->numsections) {    // Add a arrow to move section down
-                    echo "<a href=\"view.php?id=$course->id&amp;section=$section&amp;move=1&amp;sesskey=$USER->sesskey\" title=\"$strmovedown\">".
-                         "<img src=\"$CFG->pixpath/t/down.gif\" vspace=\"3\" height=\"11\" width=\"11\" border=\"0\" alt=\"\" /></a><br />";
+                    echo '<a href="view.php?id='.$course->id.'&amp;section='.$section.'&amp;move=1&amp;sesskey='.$USER->sesskey.'" title="'.$strmovedown.'">'.
+                         '<img src="'.$CFG->pixpath.'/t/down.gif" vspace="3" height="11" width="11" border="0" alt="" /></a><br />';
                 }
-
             }
 
-            echo "</font>";
-            echo "</td>";
-            echo "</tr>";
-            echo "<tr><td colspan=\"3\"><img src=\"../pix/spacer.gif\" width=\"1\" height=\"1\" alt=\"\"></td></tr>";
+            echo '</td></tr>';
+            echo '<tr class="section"><td colspan="3" class="spacer"></td></tr>';
         }
 
         $section++;
         $weekdate = $nextweekdate;
     }
-    echo "</table>";
+    echo '</table>';
 
     if (!empty($sectionmenu)) {
-        echo "<center>";
-        echo popup_form("$CFG->wwwroot/course/view.php?id=$course->id&", $sectionmenu,
-                   "sectionmenu", "", get_string("jumpto"), "", "", true);
-        echo "</center>";
+        echo '<div align="center" class="jumpmenu">';
+        echo popup_form($CFG->wwwroot.'/course/view.php?id='.$course->id.'&', $sectionmenu,
+                   'sectionmenu', '', get_string('jumpto'), '', '', true);
+        echo '</div>';
     }
 
-
-    echo "</td>";
+    echo '</td>';
 
     // The right column
-    if(blocks_have_content($pageblocks[BLOCK_POS_RIGHT]) || $editing) {
+    if (blocks_have_content($pageblocks[BLOCK_POS_RIGHT]) || $editing) {
         echo '<td style="width: '.$preferred_width_right.'px;" id="right-column">';
         blocks_print_group($PAGE, $pageblocks[BLOCK_POS_RIGHT]);
         if ($editing) {
@@ -268,6 +253,6 @@
         echo '</td>';
     }
 
-    echo "</tr>\n";
-    echo "</table>\n";
+    echo '</tr></table>';
+
 ?>
