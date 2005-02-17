8c1f8a308fa58773ae5a004c66940dd755eacfea
mits <mits>
2005-02-17 05:25:31 +0000

    polished.

--- lang/ja_utf8/algebra.php
+++ lang/ja_utf8/algebra.php
@@ -1,10 +1,7 @@
-<?php // $Id$ 
-      // algebra.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+<?PHP // $Id$ 
+      // algebra.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005021601)
 
 
 $string['filtername'] = '代数表記法';
-$string['thischarset'] = 'UTF-8';
-$string['thisdirection'] = 'ltr';
-$string['thislanguage'] = 'Japanese';
 
 ?>
