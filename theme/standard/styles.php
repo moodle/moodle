205baeb56a657481c6f78f0951e57bba712439e2
moodler <moodler>
2005-02-25 11:24:55 +0000

    This development temporary hack got checked in by accident

--- theme/standard/styles.php
+++ theme/standard/styles.php
@@ -7,7 +7,8 @@
 /// THERE SHOULD BE NO NEED TO MODIFY THIS FILE!!  USE CONFIG.PHP INSTEAD.
 
 
-    $lifetime  = 0;                                   // Seconds to cache this stylesheet
+    $lifetime  = 600;                                   // Seconds to cache this stylesheet
+    $nomoodlecookie = true;                             // Cookies prevent caching, so don't use them
     require_once("../../config.php");                   // Load up the Moodle libraries
     $themename = basename(dirname(__FILE__));           // Name of the folder we are in
     $forceconfig = optional_param('forceconfig', '', PARAM_FILE);   // Get config from this theme
