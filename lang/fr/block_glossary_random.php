f1eaa9727fd60334f7ec493a35377904f7b24ee3
martignoni <martignoni>
2005-02-26 16:45:38 +0000

    New file added

---
+++ lang/fr/block_glossary_random.php
@@ -0,0 +1,24 @@
+<?php // $Id$
+
+$string['aboutfooter'] = 'Ce bloc hérite certains réglages du glossaire auquel il est associé. Seul l\'un des textes suivants sera affichés, suivant les réglages du glossaire';
+$string['addentry'] = 'Ajouter vos citations préférées&nbsp;!';
+$string['askaddentry'] = 'Si les étudiants sont autorisés à ajouter leurs propres articles dans le glossaire, un lien vers le glossaire est affiché avec le texte suivant&nbsp;';
+$string['askinvisible'] = 'Si vous désirez que le glossaire soit caché pour les étudiants, aucun lien n\'est affiché, mais seulement le texte suivant&nbsp;'; 
+$string['askviewglossary'] = 'Si les étudiants ne sont autorisés à voir le glossaire que dans son intégralité, un lien est affiché avec le texte suivant&nbsp;'; 
+$string['blockname'] = 'Citation du jour';
+$string['change_configuration'] = 'Réglages du bloc';
+$string['invisible'] = '(suite)'; 
+$string['lastmodified'] = 'modifié en dernier';
+$string['nextone'] = 'suivant';
+$string['notyetconfigured'] = 'Veuillez ajouter au moins un glossaire à ce cours avant d\'effectuer la configuration de ce bloc.'; 
+$string['random'] = 'aléatoire';
+$string['refresh'] = 'jours avant de choisir un nouvel article';
+$string['select_glossary'] = 'Choisir les articles de ce glossaire';
+$string['title'] = 'Titre';
+$string['type'] = 'Comment un nouvel article est choisi';
+$string['typehelp'] = 'L\'option <b>suivant</b> est utile si vous utilisez le bloc pour afficher des informations (liens, citations, astuces, etc.) aux utilisateurs de façon journalière ou hebdomadaire. Même si vous avez ajouté trois nouveaux articles au glossaire, le bloc choisira le prochain uniquement. Il est ainsi possible d\'avoir un certain stock d\'articles.<br />L\'option <b>modifié en dernier</b> affiche systématiquement le dernier article modifié.';
+$string['viewglossary'] = 'Plus de citations...'; 
+$string['configureblock'] = 'Modifier la configuration de ce bloc';
+$string['intro'] = 'Assurez-vous qu\'au moins un glossaire avec au moins un article a été ajouté à ce cours. Vous pourrez ensuite modifier les réglages suivants';
+
+?>
