73011e828778ceb138877009fab16cb1fb9e9415
martignoni <martignoni>
2005-02-28 10:02:45 +0000

    Modifications to cope with last version

--- lang/fr/block_glossary_random.php
+++ lang/fr/block_glossary_random.php
@@ -1,24 +1,26 @@
 <?php // $Id$
 
-$string['aboutfooter'] = 'Ce bloc hérite certains réglages du glossaire auquel il est associé. Seul l\'un des textes suivants sera affichés, suivant les réglages du glossaire';
+$string['aboutfooter'] = 'Ce bloc hérite certains réglages du glossaire auquel il est associé. Seul l\'un des textes suivants sera affiché, selon les réglages du glossaire';
 $string['addentry'] = 'Ajouter vos citations préférées&nbsp;!';
 $string['askaddentry'] = 'Si les étudiants sont autorisés à ajouter leurs propres articles dans le glossaire, un lien vers le glossaire est affiché avec le texte suivant&nbsp;';
-$string['askinvisible'] = 'Si vous désirez que le glossaire soit caché pour les étudiants, aucun lien n\'est affiché, mais seulement le texte suivant&nbsp;'; 
+$string['askinvisible'] = 'Si vous désirez que le glossaire reste caché pour les étudiants, aucun lien n\'est affiché, mais seulement le texte suivant&nbsp;'; 
 $string['askviewglossary'] = 'Si les étudiants ne sont autorisés à voir le glossaire que dans son intégralité, un lien est affiché avec le texte suivant&nbsp;'; 
-$string['blockname'] = 'Citation du jour';
+$string['blockname'] = 'Article aléatoire d\un glossaire';
 $string['change_configuration'] = 'Réglages du bloc';
+$string['configureblock'] = 'Modifier la configuration de ce bloc';
+$string['intro'] = 'Assurez-vous que vous disposez dans ce cours d\'au moins un glossaire comprenant au moins un article. Vous pourrez alors modifier les réglages ci-dessous';
 $string['invisible'] = '(suite)'; 
-$string['lastmodified'] = 'modifié en dernier';
-$string['nextone'] = 'suivant';
-$string['notyetconfigured'] = 'Veuillez ajouter au moins un glossaire à ce cours avant d\'effectuer la configuration de ce bloc.'; 
-$string['random'] = 'aléatoire';
-$string['refresh'] = 'jours avant de choisir un nouvel article';
-$string['select_glossary'] = 'Choisir les articles de ce glossaire';
+$string['lastmodified'] = 'Dernier article modifié';
+$string['nextone'] = 'Article suivant';
+$string['noentriesyet'] = 'Il n\'y a pas encore d\'article dans le glossaire choisi.'; 
+$string['notyetconfigured'] = 'Veuillez configurer ce bloc en cliquant sur l\'icône d\'édition.'; 
+$string['notyetglossary'] = 'Vous devez disposer d\'un glossaire à sélectionner.'; 
+$string['random'] = 'Article aléatoire';
+$string['refresh'] = 'Jours avant le choix d\'un nouvel article';
+$string['select_glossary'] = 'Choisir les articles dans ce glossaire';
 $string['title'] = 'Titre';
 $string['type'] = 'Comment un nouvel article est choisi';
-$string['typehelp'] = 'L\'option <b>suivant</b> est utile si vous utilisez le bloc pour afficher des informations (liens, citations, astuces, etc.) aux utilisateurs de façon journalière ou hebdomadaire. Même si vous avez ajouté trois nouveaux articles au glossaire, le bloc choisira le prochain uniquement. Il est ainsi possible d\'avoir un certain stock d\'articles.<br />L\'option <b>modifié en dernier</b> affiche systématiquement le dernier article modifié.';
+$string['typehelp'] = 'L\'option <b>Dernier article modifié</b> affiche systématiquement le dernier article modifié, tandis que l\'option <b>Article aléatoire</b> affiche à chaque fois un nouvel article. L\'option <b>Article suivant</b> passe en revue les différents articles du glossaire dans l\'ordre. Cette dernière option est particulièrement utile si un nombre de jours est aussi splécifié, vous permettant par exemple de mettre en place pour vos visiteurs une «&nbsp;Citation de la semaine&nbsp;» ou une «&nbsp;Astuce du jour&nbsp;»';
 $string['viewglossary'] = 'Plus de citations...'; 
-$string['configureblock'] = 'Modifier la configuration de ce bloc';
-$string['intro'] = 'Assurez-vous qu\'au moins un glossaire avec au moins un article a été ajouté à ce cours. Vous pourrez ensuite modifier les réglages suivants';
 
 ?>
