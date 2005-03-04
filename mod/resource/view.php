3e88d524adf3a9dea2a48223d899e203d68d3819
moodler <moodler>
2005-03-04 10:13:42 +0000

    New faster filters!

--- mod/resource/view.php
+++ mod/resource/view.php
@@ -3,22 +3,36 @@
     require_once("../../config.php");
     require_once("lib.php");
  
-    require_variable($id);    // Course Module ID
+    $id = optional_param('id', 0, PARAM_INT);    // Course Module ID
+    $r  = optional_param('r', 0, PARAM_INT);  // Resource
 
     if (!empty($CFG->forcelogin)) {
         require_login();
     }
 
-    if (! $cm = get_record("course_modules", "id", $id)) {
-        error("Course Module ID was incorrect");
+    if ($r) {  // Two ways to specify the resource
+        if (! $resource = get_record('resource', 'id', $r)) {
+            error('Resource ID was incorrect');
+        }
+
+        if (! $cm = get_coursemodule_from_instance('resource', $resource->id, $resource->course)) {
+            error('Course Module ID was incorrect');
+        }
+
+    } else if ($id) {
+        if (! $cm = get_record('course_modules', 'id', $id)) {
+            error('Course Module ID was incorrect');
+        }
+
+        if (! $resource = get_record('resource', 'id', $cm->instance)) {
+            error('Resource ID was incorrect');
+        }
+    } else {
+        error('No valid parameters!!');
     }
 
-    if (! $resource = get_record("resource", "id", $cm->instance)) {
-        error("Resource ID was incorrect");
-    }
-
-    require ("$CFG->dirroot/mod/resource/type/$resource->type/resource.class.php");
-    $resourceclass = "resource_$resource->type";
+    require ($CFG->dirroot.'/mod/resource/type/'.$resource->type.'/resource.class.php');
+    $resourceclass = 'resource_'.$resource->type;
     $resourceinstance = new $resourceclass($cm->id);
 
     $resourceinstance->display();
