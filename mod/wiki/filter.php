b7ac9c8c75958f73b5bd7276e5be52878d8af479
moodler <moodler>
2005-03-08 07:47:10 +0000

    Fixed a bug that was using all versions of each page in the filter process. Now only one instance of each page is used. (SE)

--- mod/wiki/filter.php
+++ mod/wiki/filter.php
@@ -34,16 +34,19 @@
 
 //              Walk through each entry and get the pages.
                 foreach ($wiki_entries as $wiki_entry) {
-                    if ($wiki_pages = get_records('wiki_pages', 'wiki', $wiki_entry->id)) {
-
+                    if ($wiki_pages = get_records('wiki_pages', 'wiki', $wiki_entry->id, 'pagename, version DESC')) {
 //                      Walk through each page and filter.
+                        $wikientries = array();
                         foreach ($wiki_pages as $wiki_page) {
-                            $startlink = '<a class="autolink" title="Wiki" href="'
+                            if (!in_array($wiki_page->pagename, $wikientries)) {
+                                $startlink = '<a class="autolink" title="Wiki" href="'
                                         .$CFG->wwwroot.'/mod/wiki/view.php?wid='.$wiki->id
                                         .'&amp;userid='.$wiki_entry->userid
                                         .'&amp;groupid='.$wiki_entry->groupid
                                         .'&amp;page='.$wiki_page->pagename.'">';
-                            $linkarray[] = new filterobject($wiki_page->pagename, $startlink, '</a>', false, true);
+                                $linkarray[] = new filterobject($wiki_page->pagename, $startlink, '</a>', false, true);
+                                $wikientries[] = $wiki_page->pagename;
+                            }
                         }
                     }
                 }
