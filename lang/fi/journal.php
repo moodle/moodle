6142f65fce484c8014e4cc1c21c515793ebc36ed
paca70 <paca70>
2005-03-09 19:53:15 +0000

    Lyhyttehtävä > Päiväkirja > on nyt muistio saapi nähdä kauanko.

--- lang/fi/journal.php
+++ lang/fi/journal.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // journal.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004112900)
+      // journal.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005030501)
 
 
 $string['alwaysopen'] = 'Aina avoinna';
@@ -9,32 +9,32 @@ $string['editingended'] = 'Muokkausaika on loppunut';
 $string['editingends'] = 'Muokkausaika loppuu';
 $string['entries'] = 'Osat';
 $string['feedbackupdated'] = 'Palaute päivitetty $a :lle';
-$string['journalmail'] = '$a->teacher on antanut sinulle palautetta tehtävästä \'$a->journal\'
+$string['journalmail'] = '$a->teacher on antanut sinulle palautetta muistiosta \'$a->journal\'
 
 Voit nähdä sen osoitteessa:
 
 $a->url';
-$string['journalmailhtml'] = '$a->teacher on antanut sinulle palautetta päiväkirjasta \'<i>$a->journal</i>\'<br /><br />
+$string['journalmailhtml'] = '$a->teacher on antanut sinulle palautetta muistiosta \'<i>$a->journal</i>\'<br /><br />
 
 Voit nähdä sen lisättynä
 
-<a href=\"$a->url\">päiväkirjaasi</a>.';
-$string['journalname'] = 'Päiväkirjan nimi';
-$string['journalquestion'] = 'Päiväkirjan kysymys';
+<a href=\"$a->url\">muistioosi</a>.';
+$string['journalname'] = 'Muistion nimi';
+$string['journalquestion'] = 'Muistion kysymys';
 $string['journalrating1'] = 'Välttävä';
 $string['journalrating2'] = 'Tyydyttävä';
 $string['journalrating3'] = 'Kiitettävä';
-$string['modulename'] = 'Päiväkirja';
-$string['modulenameplural'] = 'Päiväkirjat';
-$string['newjournalentries'] = 'Uudet päiväkirjat';
+$string['modulename'] = 'Muistio';
+$string['modulenameplural'] = 'Muistiot';
+$string['newjournalentries'] = 'Uudet muistiot';
 $string['noentry'] = 'Ei palautettu';
 $string['noratinggiven'] = 'Ei arvioitu';
-$string['notopenuntil'] = 'Tämä päiväkirja on täytettävissä vasta';
-$string['notstarted'] = 'Et ole aloittanut tätä päiväkirjaa vielä';
+$string['notopenuntil'] = 'Tämä muistio on täytettävissä vasta';
+$string['notstarted'] = 'Et ole aloittanut tätä muistiota vielä';
 $string['overallrating'] = 'Yleinen arviointi';
 $string['rate'] = 'Arviointi';
 $string['saveallfeedback'] = 'Tallenna kaikki palaute';
-$string['startoredit'] = 'Aloita tai muokkaa päiväkirjaa';
-$string['viewallentries'] = 'Katso kaikkia $a päiväkirjaa';
+$string['startoredit'] = 'Aloita tai muokkaa muistiota';
+$string['viewallentries'] = 'Katso kaikkia $a muistioita';
 
 ?>
