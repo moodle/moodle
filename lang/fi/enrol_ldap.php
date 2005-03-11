9935523903cea69c8e2454e75894b6e54ca3b50f
paca70 <paca70>
2005-03-11 18:52:50 +0000

    Updates. Based on work from Mio Taalas.

---
+++ lang/fi/enrol_ldap.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // enrol_ldap.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004112900)
+
+
+$string['enrol_ldap_server_settings'] = 'LDAP-palvelimen asetukset';
+$string['enrolname'] = 'LDAP';
+
+?>
