e4fa95c28732d528212a0b1e0ec9d97f8008f051
stronk7 <stronk7>
2005-03-13 00:14:47 +0000

    Bump! Workshops are done.

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2005030600;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2005031200;   // The current version is a date (YYYYMMDDXX)
 
 $backup_release = "1.5 development";  // User-friendly version number
