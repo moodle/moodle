f8c330c02387f3aa66e7d07ad503a2b1b832453d
phoenixfr <phoenixfr>
2005-03-14 12:19:17 +0000

    *** empty log message ***

--- auth/cas/forbidden.php
+++ auth/cas/forbidden.php
@@ -2,7 +2,7 @@
 // version $Id$
 // Page for forbidden access from CAS
     require_once("../../config.php");
-    $errormsg = get_string("auth_cas_invalidcaslogin");
+    $errormsg = get_string("auth_cas_invalidcaslogin", "auth");
     print_header("$site->fullname: $loginsite", "$site->fullname", $loginsite,
     $focus, "", true, "<div align=right>$langmenu</div>");
     include("forbidden.html");
