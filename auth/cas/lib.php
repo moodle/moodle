5888f3912c49819ad1df409c460ff849fc949a8c
phoenixfr <phoenixfr>
2005-03-14 13:01:22 +0000

    add of cas functions for login module

--- auth/cas/lib.php
+++ auth/cas/lib.php
@@ -12,6 +12,9 @@ And use the /auth/cas/index_form.html and /auth/cas/caslogin.php.
 This module is using the LDAP Module so you need the /auth/ldap directory.
 */
 
+require_once($CFG->dirroot.'/config.php');
+include_once($CFG->dirroot.'/lib/cas/CAS.php');
+$cas_validate=false;
 define("AUTH_METHOD", 'cas');
 require_once($CFG->dirroot.'/auth/cas/commonlib.php');
 
@@ -67,6 +70,88 @@ function auth_user_login ($username, $password) {
         error("LDAP part of CAS-module cannot connect to server: $CFG->ldap_host_url");
     }
     return false;
+}
+
+/**
+ * authenticates user against CAS from screen login
+ * the user doesn't have a CAS Ticket yet.
+ *
+ * Returns an object user if the username and password work
+ * and nothing if they don't
+ *
+ * @param string  $username
+ * @param string  $password
+ *
+*/
+function cas_authenticate_user_login ($username, $password) {
+
+   global $CFG;
+   $cas_validate=true;
+
+   phpCAS::client($CFG->cas_version,$CFG->cas_hostname,(Integer)$CFG->cas_port,$CFG->cas_baseuri);
+   phpCAS::setLang($CFG->cas_language);
+   if (!phpCAS::isAuthenticated()){
+      phpCAS::authenticateIfNeeded();
+   }
+   if ($CFG->cas_create_user=="0"){
+      if (get_user_info_from_db("username", phpCAS::getUser())){
+         $user = authenticate_user_login(phpCAS::getUser(), 'cas');
+      }else{
+         //login as guest if CAS but not Moodle and not automatic creation
+         if ($CFG->guestloginbutton){
+             $user = authenticate_user_login('guest', 'guest');
+         }else{
+             $user = authenticate_user_login(phpCAS::getUser(), 'cas');
+         }
+      }
+   }else{
+      $user = authenticate_user_login(phpCAS::getUser(), 'cas');
+   }
+   return $user;
+}
+
+/**
+ * authenticates user against CAS when first call of Moodle
+ * if already in CAS (cookie with the CAS ticket), don't have to log again (SSO)
+ *
+ * Returns an object user if the username and password work
+ * and nothing if they don't
+ *
+ * @param object $user
+ *
+*/
+function cas_automatic_authenticate ($user="") {
+   global $CFG;
+   if (!$cas_validate){
+        $cas_validate=true;
+        phpCAS::client($CFG->cas_version,$CFG->cas_hostname,(Integer)$CFG->cas_port,$CFG->cas_baseuri);
+        phpCAS::setLang($CFG->cas_language);
+        if (!phpCAS::isAuthenticated() && !$CFG->guestloginbutton){
+           phpCAS::authenticateIfNeeded();
+        }
+        if (phpCAS::isAuthenticated()){
+           if ($CFG->cas_create_user=="0"){
+              if (get_user_info_from_db("username", phpCAS::getUser())){
+                 $user = authenticate_user_login(phpCAS::getUser(), 'cas');
+              }else{
+                 //login as guest if CAS but not Moodle and not automatic creation
+                 if ($CFG->guestloginbutton){
+                     $user = authenticate_user_login('guest', 'guest');
+                 }else{
+                     $user = authenticate_user_login(phpCAS::getUser(), 'cas');
+                 }
+              }
+           }else{
+              $user = authenticate_user_login(phpCAS::getUser(), 'cas');
+           }
+           return $user;
+        }else{
+           return;
+        }
+
+   }else{
+      return $user;
+   }
 }
 
 ?>
