c661aa947b51432068f61b60b7b24342f3cfbcee
stronk7 <stronk7>
2005-03-15 16:27:25 +0000

    Argh!
    Just deleting some files sent here by error!! (one wrong CVS dir
    was the cause) Sorry!!

--- blocks/recent_activity/lang/es/block_latest_activities.php
+++
@@ -1,6 +0,0 @@
-<?php // $Id$
-
-$string['blockname'] = 'Últimas Actividades';
-$string['lastnactivities'] = 'últimas $a actividades';
-$string['numberofactivities'] = 'Número de actividades a mostrar.';
-?>
