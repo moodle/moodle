f724fd8fd6a96d7eac06dec24ebe99121395e7b9
stronk7 <stronk7>
2005-03-15 16:19:23 +0000

    Simple block that shows the latest n visited activities by the user.
    Not fully tested but seems to work. Enjoy!

---
+++ blocks/recent_activity/lang/en/block_latest_activities.php
@@ -0,0 +1,6 @@
+<?php // $Id$
+
+$string['blockname'] = 'Latest Activities';
+$string['lastnactivities'] = 'last $a activities';
+$string['numberofactivities'] = 'Number of activities to be shown.';
+?>
---
+++ blocks/recent_activity/lang/es/block_latest_activities.php
@@ -0,0 +1,6 @@
+<?php // $Id$
+
+$string['blockname'] = 'Últimas Actividades';
+$string['lastnactivities'] = 'últimas $a actividades';
+$string['numberofactivities'] = 'Número de actividades a mostrar.';
+?>
