3e03850949b08dd952f8aedecf2558974079234d
mits <mits>
2005-03-18 13:14:03 +0000

    translated some.

--- lang/ja_utf8/error.php
+++ lang/ja_utf8/error.php
@@ -1,7 +1,8 @@
-<?php // $Id$ 
-      // error.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005021000)
+<?PHP // $Id$ 
+      // error.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005031000)
 
 
+$string['adminprimarynoedit'] = '主Adminは他から編集することはできません。';
 $string['confirmsesskeybad'] = '申し訳ございません、あなたのセッションキーは、このアクションを実行するための確認をできませんでした。このセキュリティ機能は、あなたのユーザ名で重要な機能が不測または悪意により実行されることを防ぎます。本当にこの機能を実行したいか確認してください。';
 $string['coursegroupunknown'] = 'コースに関連するグループ $a が指定されていません。';
 $string['erroronline'] = '$a 行にエラーがあります。';
@@ -13,6 +14,7 @@ $string['invalidfieldname'] = '「 $a 」 は正しいフィールド名では�
 $string['missingfield'] = '「 $a 」 を入力してください。';
 $string['modulerequirementsnotmet'] = 'モジュール「 $a->modulename 」 ($a->moduleversion) をインストールできませんでした。このモジュールをインストールするためには新しいバージョンのMoodleをインストールする必要があります。(現在、あなたは $a->currentmoodle を使用しています。 $a->requiremoodle が必要です。)';
 $string['notavailable'] = '現在利用できません';
+$string['onlyeditown'] = '他の人の情報は編集できません。';
 $string['processingstops'] = 'プロセスが停止しました。残りのレコードは無視されます。';
 $string['restricteduser'] = '申し訳ございません。あなたのアカウント「 $a 」では、この処理を行うことができません。';
 $string['sessionipnomatch'] = '申し訳ございません。あなたのIPアドレスは、最初のログインから変更されているようです。このセキュリティー機能は、あなたがサイトにログインしている間に、クラッカーから個人情報を盗むことを防いでいます。通常のユーザは、このメッセージを見ることはありません - 詳細はシステム管理者にお問合せください。';
