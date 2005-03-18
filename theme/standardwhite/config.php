86eb085d249eb709bdeb53488f32c872d43bae4c
moodler <moodler>
2005-03-18 14:46:58 +0000

    Standard white now looks like standard used to look.  It's exactly the same but it has the white gradients in the headers and navbar

--- theme/standardwhite/config.php
+++ theme/standardwhite/config.php
@@ -1,17 +1,68 @@
 <?PHP // $Id$
 
-$THEME->body         = "#FFFFFF";  // Main page color
-$THEME->cellheading  = "#E8E8E8";  // Standard headings of big tables
-$THEME->cellheading2 = "#AAAAAA";  // Highlight headings of tables
-$THEME->cellcontent  = "#FFFFFF";  // For areas with text
-$THEME->cellcontent2 = "#EFEFEF";  // Alternate colour
-$THEME->borders      = "#999999";  // Table borders
-$THEME->highlight    = "#AAFFAA";  // Highlighted text (eg after a search)
-$THEME->hidden       = "#AAAAAA";  // To color things that are hidden
-$THEME->autolink     = "#EEEEEE";  // To color auto-generated links (eg glossary)
+////////////////////////////////////////////////////////////////////////////////
+/// This file contains a few configuration variables that control 
+/// how Moodle uses this theme.
+////////////////////////////////////////////////////////////////////////////////
 
-$THEME->custompix    = false;      // If true, then this theme must have a "pix" 
-                                   // subdirectory that contains copies of all 
-                                   // files from the moodle/pix directory
-                                   // See "cordoroyblue" for an up-to-date example.
+
+$THEME->sheets = array('gradients');
+
+/// This variable is an array containing the names of all the 
+/// stylesheet files you want included in this theme, and in what order
+////////////////////////////////////////////////////////////////////////////////
+
+
+$THEME->standardsheets = true;  
+
+/// This variable can be set to an array containing
+/// filenames from the *STANDARD* theme.  If the 
+/// array exists, it will be used to choose the 
+/// files to include in the standard style sheet.
+/// When false, then no files are used.
+/// When true or NON-EXISTENT, then ALL standard files are used.
+/// This parameter can be used, for example, to prevent 
+/// having to override too many classes.
+/// Note that the trailing .css should not be included
+/// eg $THEME->standardsheets = array('styles_layout', 'styles_fonts', 
+///                                   'styles_color', 'styles_moz');
+////////////////////////////////////////////////////////////////////////////////
+
+
+$THEME->parent = '';  
+
+/// This variable can be set to the name of a parent theme
+/// which you want to have included before the current theme.
+/// This can make it easy to make modifications to another 
+/// theme without having to actually change the files
+/// If this variable is empty or false then a parent theme 
+/// is not used.
+////////////////////////////////////////////////////////////////////////////////
+
+
+$THEME->parentsheets = false;  
+
+/// This variable can be set to an array containing
+/// filenames from a chosen *PARENT* theme.  If the 
+/// array exists, it will be used to choose the 
+/// files to include in the standard style sheet.
+/// When false, then no files are used.
+/// When true or NON-EXISTENT, then ALL standard files are used.
+/// This parameter can be used, for example, to prevent 
+/// having to override too many classes.
+/// Note that the trailing .css should not be included
+/// eg $THEME->standardsheets = array('styles_layout', 'styles_fonts', 
+///                                   'styles_color', 'styles_moz');
+////////////////////////////////////////////////////////////////////////////////
+
+
+$THEME->custompix = false;
+
+/// If true, then this theme must have a "pix" 
+/// subdirectory that contains copies of all 
+/// files from the moodle/pix directory, plus a
+/// "pix/mod" directory containing all the icons 
+/// for all the activity modules.
+////////////////////////////////////////////////////////////////////////////////
+
 ?>
