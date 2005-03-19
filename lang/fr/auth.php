ffdb40800b40d2ca9afe28684d5c621e00e40ff9
martignoni <martignoni>
2005-03-19 09:57:18 +0000

    Minor change

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -80,7 +80,7 @@ $string['auth_nntptitle'] = 'Utiliser un serveur NNTP';
 $string['auth_nonedescription'] = 'Les utilisateurs peuvent s\'enregistrer et créer des comptes valides immédiatement sans aucune authentification ni confirmation par courriel. Attention ! Lors de l\'utilisation de cette méthode, bien penser à toutes les implications (problèmes d\'administration et sécurité).';
 $string['auth_nonetitle'] = 'Pas d\'authentification';
 $string['auth_pamdescription'] = 'Cette méthode utilise PAM pour accéder aux noms d\'utilisateurs natifs sur ce serveur. Pour utiliser ce module, vous devrez installer le logiciel <a href=\"http://www.math.ohio-state.edu/~ccunning/pam_auth/\" target=\"_blank\">PHP4 PAM Authentication</a>.';
-$string['auth_pamtitle'] = 'PAM (Pluggable Authentication Modules = Modules d\'authentification installables)';
+$string['auth_pamtitle'] = 'PAM (Modules d\'authentification installables)';
 $string['auth_passwordwillexpire'] = 'Votre mot de passe arrivera à échéance dans $a jours. Voulez-vous changer votre mot de passe maintenant ?';
 $string['auth_passwordisexpired'] = 'Votre mot de passe est échu. Voulez-vous le changer maintenant ?';
 $string['auth_pop3description'] = 'Cette méthode utilise un serveur POP3 pour vérifier qu\'un nom d\'utilisateur et son mot de passe sont valides.';
