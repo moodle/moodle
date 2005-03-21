0d1af1b309fcd5f58eeeab5c08bcb72f9d6363c8
scop22 <scop22>
2005-03-21 14:17:11 +0000

    New strings translated in several files

--- lang/ko/algebra.php
+++ lang/ko/algebra.php
@@ -1,9 +1,7 @@
 <?PHP // $Id$ 
-      // algebra.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004101900)
+      // algebra.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005021000)
 
 
 $string['filtername'] = '대수 표기';
-$string['thischarset'] = 'euc-kr';
-$string['thislanguage'] = '&#54620;&#44397;&#50612;';
 
 ?>
