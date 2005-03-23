24357c78474eb5a64f9c8abc5bd36b8513928e34
ethem <ethem>
2005-03-23 19:18:49 +0000

    New strings from Ethem Evlice (ethem DOT evlice DOT com) XHTML compliant

---
+++ lang/tr/error.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // error.php - created with Moodle 1.4.4 (2004083140)
+
+
+$string['confirmsesskeybad'] = 'Üzgünüz, oturum anahtarýnýz bu eylemi gerçekleþtirmek için onaylanamadý. Bu güvenlik özelliði sizin adýnýza yapýlacak olan önemli iþlevlerin yanlýþlýkla ya da kasten kullanýlmasýný engellemek içindir. Lütfen bu iþlevi gerçekten yapmak istediðinizden emin olun.';
+$string['coursegroupunknown'] = 'Grup $a ile ilgili kurs belirtilmemiþ';
+$string['erroronline'] = '$a. satýrda hata var';
+$string['fieldrequired'] = '\"$a\" doldurulmasý gerekli bir alandýr';
+$string['filenotfound'] = 'Üzgünüz, aranan dosya bulunamadý';
+$string['groupalready'] = 'Kullanýcý $a grubuna zaten kayýtlý';
+$string['groupunknown'] = '$a grubu belirtilen kursla iliþkili deðildir';
+$string['invalidfieldname'] = '\"$a\" geçerli bir alan adý deðildir';
+$string['missingfield'] = '\"$a\" alaný eksiktir';
+$string['modulerequirementsnotmet'] = '\"$a->modulename\" ($a->moduleversion) modülü kurulamadý. Yeni moodle sürümü gerekmektedir (þu anda kullandýðýnýz: $a->currentmoodle, gereken: $a->requiremoodle).';
+$string['notavailable'] = 'Þu anda eriþilemez';
+$string['processingstops'] = 'Ýþlem burada sona erer. Kalan kayýtlar yoksayýlýr.';
+$string['restricteduser'] = 'Üzgünüz, ancak þu anki \"$a\" hesabýnýz bunu yapmak için sýnýrlandýrýlmýþtýr.';
+$string['sessionipnomatch'] = 'Üzgünüz, IP numaranýz ilk girdiðinizden farklýdýr. Bu güvenlik özelliði siteye girdiðinizde crackerlarýn sizin kimlik bilgilerinizi çalmalarýný engellemek içindir. Normal kullanýcýlarýn bu mesajý görmemeleri gerekir - lütfen yardým için site yöneticisine baþvurun.';
+$string['unknowncourse'] = '\"$a\" adlý kurs bilinmiyor';
+$string['usernotaddederror'] = '\"$a\" kullanýcýsý eklenmedi - bilinmeyen hata';
+$string['usernotaddedregistered'] = '\"$a\" kullanýcý eklenmedi - zaten kayýtlý';
+$string['usernotavailable'] = 'Bu kullanýcýnýn ayrýntýlarý sizin eriþiminize kapalý';
+
+?>
---
+++ lang/tr/install.php
@@ -0,0 +1,20 @@
+<?PHP // $Id$ 
+      // install.php - created with Moodle 1.4.4 (2004083140)
+
+
+$string['caution'] = 'Dikkat';
+$string['chooselanguage'] = 'Bir dil seçin';
+$string['compatibilitysettings'] = 'PHP ayarlarýnýz kontrol ediliyor...';
+$string['configurationcomplete'] = 'Yapýlandýrma tamamlandý';
+$string['database'] = 'Veritabaný';
+$string['dbprefix'] = 'Tablo öneki';
+$string['dbtype'] = 'Tipi';
+$string['download'] = 'Ýndir';
+$string['gdversion'] = 'GD sürümü';
+$string['installation'] = 'Kurulum';
+$string['memorylimit'] = 'Bellek Limiti';
+$string['phpversion'] = 'PHP sürümü';
+$string['phpversionerror'] = 'PHP sürümü en az 4.1.0 olmalý';
+$string['sessionautostart'] = 'Oturum Otomatik Baþlama';
+
+?>
