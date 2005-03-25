d45fd4ddbd96ee491548d5334a9a4fc0facb44e6
moodler <moodler>
2005-03-25 17:15:02 +0000

    Removed styles_moz.css from default (it doesn't validate and prevents validation in fact)

--- lib/weblib.php
+++ lib/weblib.php
@@ -1840,7 +1840,7 @@ function style_sheet_setup($lastmodified=0, $lifetime=300, $themename='', $force
     header('Pragma: ');
     header('Content-type: text/css');  // Correct MIME type
 
-    $DEFAULT_SHEET_LIST = array('styles_layout', 'styles_fonts', 'styles_color', 'styles_moz');
+    $DEFAULT_SHEET_LIST = array('styles_layout', 'styles_fonts', 'styles_color');
 
     if (empty($themename)) {
         $themename = current_theme();  // So we have something.  Normally not needed.
