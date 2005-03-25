c4183f7a043cc49b6a83f5a04354a21651451789
moodler <moodler>
2005-03-25 16:21:33 +0000

    Styles moved to block

---
+++ blocks/quiz_results/styles.php
@@ -0,0 +1,26 @@
+.block_quiz_results {
+    text-align: center;
+}
+
+.block_quiz_results table.grades .number, 
+.block_quiz_results table.grades .grade {
+    text-align: right;
+    width: 10%;
+}
+
+.block_quiz_results table.grades {
+    text-align: left;
+    width: 100%;
+}
+
+.block_quiz_results table.grades caption {
+    margin: 1em 0px 0px 0px;
+    border-bottom: 1px solid;
+    font-weight: bold;
+}
+
+.block_quiz_results h1 {
+    margin: 4px;
+    font-weight: bold;
+    font-size: 1.1em;
+}
