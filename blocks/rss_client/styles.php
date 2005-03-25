1d856b0321e4a1784ef5e04d5aa2bc0e2f9a8790
moodler <moodler>
2005-03-25 16:10:17 +0000

    New styles for RSS client block

---
+++ blocks/rss_client/styles.php
@@ -0,0 +1,13 @@
+.rssclientlink {
+    border-top:1px solid;
+    border-top-color:#DDDDDD;
+    padding-bottom:5px;
+    font-size:0.7em;
+}
+
+.rssclientdescription {
+    color:#555555;
+    font-size:0.7em;
+    padding-left:10px;
+    padding-bottom:8px;
+}
