230de5bea677e84ef33eee9812d8ef0e7fbfebc3
ethem <ethem>
2005-03-29 14:10:49 +0000

    New strings from Ethem Evlice

---
+++ lang/tr/editor.php
@@ -0,0 +1,24 @@
+<?PHP // $Id$ 
+      // editor.php - created with Moodle 1.4.4 + (2004083140)
+
+
+$string['about'] = 'Bu editor hakkýnda';
+$string['address'] = 'Adres';
+$string['alertnoselectedtext'] = 'Öncelikle yazý seçmelisiniz!';
+$string['alignment'] = 'Hizalama';
+$string['alternatetext'] = 'Görünecek yazý';
+$string['bold'] = 'Kalýn';
+$string['browse'] = 'Gözat';
+$string['cancel'] = 'Ýptal';
+$string['choosechar'] = 'Karakter Seç';
+$string['close'] = 'Kapat';
+$string['underline'] = 'Altýçizgili';
+$string['undo'] = 'Son iþlemi geri al';
+$string['unorderedlist'] = 'Namarasýz Liste';
+$string['upload'] = 'Yükle';
+$string['vertical'] = 'Dikey';
+$string['width'] = 'Geniþlik';
+$string['wordclean'] = 'World HTML Temizle';
+$string['zip'] = 'Zip';
+
+?>
---
+++ lang/tr/questionnaire.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // questionnaire.php - created with Moodle 1.4.4 + (2004083140)
+
+
+$string['alreadyfilled'] = 'Bo soru formunu bizim için zaten doldurdunuz. Teþekkür ederiz.';
+$string['modulename'] = 'Soru Formu';
+$string['modulenameplural'] = 'Soru Formlarý';
+$string['mustcomplete'] = '<b>Bu soru formunu sonuçlarýnýzýn kaydedilmesi için <i>þimdi</i> tamamlamanýz gerekiyor. Baþka bir zaman bu soru formunu dolduramazsýnýz.</b><br /><br />';
+$string['notavail'] = 'Bu soru formuna þu anda eriþemezsiniz. Daha sonra tekrar deneyiniz.';
+$string['qmanage'] = 'Anketleri Yönet';
+$string['qmanagetitle'] = 'phpESP Anket Yönetimi';
+$string['qtype'] = 'Tip';
+$string['questionnaireid'] = 'Anket';
+$string['respondenttype'] = 'Cevap verme tipi';
+
+?>
