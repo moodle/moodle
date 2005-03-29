557b7c92c6e532c0f97b3e885a5be5f849137fcb
ethem <ethem>
2005-03-29 08:49:43 +0000

    New strings from Ethem Evlice

---
+++ lang/tr/pix.php
@@ -0,0 +1,26 @@
+<?PHP // $Id$ 
+      // pix.php - created with Moodle 1.4.4 + (2004083140)
+
+
+$string['angry'] = 'kýzgýn';
+$string['approve'] = 'onaylamýþ';
+$string['biggrin'] = 'sýrýtýyor';
+$string['blackeye'] = 'siyah göz';
+$string['blush'] = 'utanmýþ';
+$string['clown'] = 'soytarý';
+$string['cool'] = 'havalý';
+$string['dead'] = 'ölmüþ';
+$string['evil'] = 'cinlik düþünüyor';
+$string['kiss'] = 'öpüyor';
+$string['mixed'] = 'kafasý karýþmýþ';
+$string['sad'] = 'üzgün';
+$string['shy'] = 'çekingen';
+$string['sleepy'] = 'uyukluyor';
+$string['smiley'] = 'gülüyor';
+$string['surprise'] = 'þaþýrmýþ';
+$string['thoughtful'] = 'düþünüyor';
+$string['tongueout'] = 'dilini çýkarýyor';
+$string['wideeyes'] = 'gözleri açýlmýþ';
+$string['wink'] = 'göz kýrpýyor';
+
+?>
