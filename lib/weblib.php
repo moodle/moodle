5485e65387683ee2e556eaa37c6c240dbd7517fb
moodler <moodler>
2005-04-01 09:25:45 +0000

    Remove a notice

--- lib/weblib.php
+++ lib/weblib.php
@@ -1838,7 +1838,7 @@ function print_footer($course=NULL, $usercourse=NULL) {
 
     include ($CFG->themedir.$CFG->theme.'/footer.html');
 
-    if ($CFG->perfdebug  && function_exists('memory_get_usage')) {
+    if (!empty($CFG->perfdebug) && function_exists('memory_get_usage')) {
         error_log("Page $_SERVER[REQUEST_URI] completed. Memory allocated: "
                   . memory_get_usage());
     }
