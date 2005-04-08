71e677aacbbc02daa75a485f928c9156b0ddfe5e
moodler <moodler>
2005-04-08 06:07:30 +0000

    Cleaned up the settings screen

--- lang/en/block_glossary_random.php
+++ lang/en/block_glossary_random.php
@@ -1,24 +1,23 @@
 <?php  // $Id$
-$string['aboutfooter'] = 'This block inherits some settings from the glossary it is associated with. Only one of the following texts is displayed depending on the settings of the glossary';
 $string['addentry'] = 'Add your own favorite quotes!';
-$string['askaddentry'] = 'If students are allowed to add their own entries to the glossary, a link to the glossary is shown with the following text';
-$string['askinvisible'] = 'If you want to keep the original glossary hidden from students, no link is shown. The following text is shown instead.'; 
-$string['askviewglossary'] = 'If students are only allowed to see the entire content of the glossary, a link to the glossary is shown with the following text'; 
+$string['askaddentry'] = 'When users can add entries to the glossary, show a link with this text';
+$string['askinvisible'] = 'When users cannot edit or view the glossary, show this text (without link)'; 
+$string['askviewglossary'] = 'When users can view the glossary but not add entries, show a link with this text'; 
 $string['blockname'] = 'Random Glossary Entry';
-$string['change_configuration'] = 'Change settings for block';
-$string['configureblock'] = 'Change settings for this block';
 $string['intro'] = 'Make sure you have at least one glossary with at least one entry added to this course. Then you can adjust the following settings';
 $string['invisible'] = '(to be continued)'; 
 $string['lastmodified'] = 'Last modified entry';
 $string['nextone'] = 'Next entry';
 $string['noentriesyet'] = 'There are no entries yet in the chosen glossary.'; 
 $string['notyetconfigured'] = 'Please configure this block using the edit icon.'; 
-$string['notyetglossary'] = 'You need to have at least one Glossary to choose.'; 
+$string['notyetglossary'] = 'You need to have at least one glossary to choose.'; 
 $string['random'] = 'Random entry';
 $string['refresh'] = 'Days before a new entry is chosen';
 $string['select_glossary'] = 'Take entries from this glossary';
+$string['showconcept'] = 'Show glossary concept';
 $string['title'] = 'Title';
-$string['typehelp'] = '<b>Last modified entry</b> will always display the entry that was last modified, and <b>Random entry</b> will choose a new one at random every time.  The option <b>Next entry</b> will cycle through the entries in order.  This last option is especially useful when a number of days is also chosen, allowing you to make a \"Quote of the week\" or a \"Tip of the day\" that everyone sees.';
+$string['typehelp'] = '<b>Last modified entry</b> will always display the entry that was last modified, and <b>Random entry</b> will choose a new one at random every time.  The option <b>Next entry</b> will cycle through the entries in order.  This last option is especially useful when a number of days is also chosen, allowing you to make a <i>Quote of the week</i> or a <i>Tip of the day</i> that everyone sees.';
 $string['type'] = 'How a new entry is chosen';
 $string['viewglossary'] = 'More quotes...'; 
+$string['whichfooter'] = 'You can display links to actions of the glossary this block is associated with. The block will only display links to actions which are enabled for that glossary.'; 
 ?>
