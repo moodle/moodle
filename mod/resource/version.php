fa33cfe99e05502c2eeedfcc37c255b0863e5b7a
thepurpleblob <thepurpleblob>
2005-04-11 12:33:47 +0000

    Version 2005041100. Update converts wiki-like resources to Markdown.

--- mod/resource/version.php
+++ mod/resource/version.php
@@ -5,7 +5,7 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2005021600;
+$module->version  = 2005041100;
 $module->requires = 2005021600;  // Requires this Moodle version
 $module->cron     = 0;
 
