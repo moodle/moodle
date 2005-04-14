cb525e6ef01e8d3e80afd68f7a9f6ea826f5de45
moodler <moodler>
2005-04-14 03:04:45 +0000

    Sorting the file  :-)

--- lang/en/choice.php
+++ lang/en/choice.php
@@ -1,7 +1,8 @@
-<?php // $Id$ 
-      // choice.php - created with Moodle 1.2 development (2003111400)
+<?PHP // $Id$ 
+      // choice.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005041101)
 
 
+$string['addmorechoices'] = 'Add more choices';
 $string['allowupdate'] = 'Allow choice to be updated';
 $string['answered'] = 'Answered';
 $string['choice'] = 'Choice';
@@ -9,12 +10,19 @@ $string['choiceclose'] = 'Until';
 $string['choicename'] = 'Choice name';
 $string['choiceopen'] = 'Open';
 $string['choicetext'] = 'Choice text';
+$string['displayhorizontal'] = 'Display horizontally';
+$string['displaymode'] = 'Display Mode';
+$string['displayvertical'] = 'Display vertically';
+$string['full'] = '(Full)';
 $string['havetologin'] = 'You have to log in before you can submit your choice';
+$string['limit'] = 'Limit';
+$string['limitanswers'] = 'Limit the number of responses allowed';
 $string['modulename'] = 'Choice';
 $string['modulenameplural'] = 'Choices';
 $string['mustchooseone'] = 'You must choose an answer before saving.  Nothing was saved.';
 $string['notanswered'] = 'Not answered yet';
 $string['notopenyet'] = 'Sorry, this activity is not available until $a';
+$string['privacy'] = 'Privacy of results';
 $string['publish'] = 'Publish results';
 $string['publishafteranswer'] = 'Show results to a student after they answer';
 $string['publishafterclose'] = 'Show results to students only after the choice is closed';
@@ -22,21 +30,14 @@ $string['publishalways'] = 'Always show results to students';
 $string['publishanonymous'] = 'Publish anonymous results, do not show student names';
 $string['publishnames'] = 'Publish full results, showing names and their choices';
 $string['publishnot'] = 'Do not publish results to students';
-$string['privacy'] = 'Privacy of results';
 $string['responses'] = 'Responses';
 $string['responsesto'] = 'Responses to $a';
-$string['timerestrict'] = 'Restrict answering to this time period';
 $string['savemychoice'] = 'Save my choice';
 $string['showunanswered'] = 'Show column for unanswered';
-$string['viewallresponses'] = 'View $a responses';
-$string['addmorechoices'] = 'Add more choices';
-$string['displaymode'] = 'Display Mode';
-$string['displayhorizontal'] = 'Display horizontally';
-$string['displayvertical'] = 'Display vertically';
-$string['limitanswers'] = 'Limit the number of responses allowed';
-$string['limit'] = 'Limit';
-$string['full'] = '(Full)';
-$string['taken'] = 'Taken';
-$string['spacesleft'] = 'spaces available';
 $string['spaceleft'] = 'space available';
+$string['spacesleft'] = 'spaces available';
+$string['taken'] = 'Taken';
+$string['timerestrict'] = 'Restrict answering to this time period';
+$string['viewallresponses'] = 'View $a responses';
+
 ?>
