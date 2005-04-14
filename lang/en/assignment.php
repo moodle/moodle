36a3ed4ce10f98789a3e8674ab3ae2e6f0908b4b
moodler <moodler>
2005-04-14 13:25:21 +0000

    Some new strings from the refactoring ...

--- lang/en/assignment.php
+++ lang/en/assignment.php
@@ -15,6 +15,7 @@ assignment submission for \'<i>$a->assignment</i>\'<br /><br />
 You can see it appended to your <a href=\"$a->url\">assignment submission</a>.';
 $string['assignmentname'] = 'Assignment name';
 $string['assignmenttype'] = 'Assignment type';
+$string['availabledate'] = 'Available from';
 $string['configmaxbytes'] = 'Default maximum assignment size for all assignments on the site (subject to course limits and other local settings)';
 $string['description'] = 'Description';
 $string['duedate'] = 'Due date';
@@ -51,6 +52,7 @@ $string['submissions'] = 'Submissions';
 $string['submitassignment'] = 'Submit your assignment using this form';
 $string['submitted'] = 'Submitted';
 $string['typeoffline'] = 'Offline activity';
+$string['typeonline'] = 'Online activity';
 $string['typeuploadsingle'] = 'Upload a single file';
 $string['uploadbadname'] = 'This filename contained strange characters and couldn\'t be uploaded';
 $string['uploadedfiles'] = 'uploaded files';
