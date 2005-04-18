d151b49606714f52361cf2b0a4fcee4fc20657b9
gniske <gniske>
2005-04-18 11:47:52 +0000

    Created new language file

---
+++ lang/da/block_glossary_random.php
@@ -0,0 +1,28 @@
+<?PHP // $Id$ 
+      // block_glossary_random.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005033100)
+
+
+$string['aboutfooter'] = 'Denne blok arver nogle indstillinger fra den ordbog den er associeret med. Kun een af de følgene tekster vises afhængig af indstillingen i ordbogen.';
+$string['addentry'] = 'Tilføj dine favoritcitater!';
+$string['askaddentry'] = 'Hvis elever må tilføje deres egne indlæg til ordbagen så vil et link til ordbogen blive vist i den følgene tekst.';
+$string['askinvisible'] = 'Hvis du ønsker at beholde den originale ordbog skjult for studerende vil der ikke blive vist noget link. I stedet vil den følgene tekst blive vist.';
+$string['askviewglossary'] = 'Hvis studerende må se hele indholdet af ordbogen vil  et link blive vist med den følgene tekst.';
+$string['blockname'] = 'Tilfældig tordbogs indlæg';
+$string['change_configuration'] = 'Ret indstilling for blokken';
+$string['configureblock'] = 'Ret indstillinger for denne blok';
+$string['intro'] = 'Vær sikker på at du har mindst en ordbog med mindst et indlæg i tilføjet kurset. Når det er i orden kan du rette i de følgene indstillinger.';
+$string['invisible'] = '(fortsættes)';
+$string['lastmodified'] = 'Sidst rettede indlæg';
+$string['nextone'] = 'Næste indlæg';
+$string['noentriesyet'] = 'Der er endnu ikke nogen indlæg i den valgte ordbog';
+$string['notyetconfigured'] = 'Ret venlist i blokken via ret ikonet.';
+$string['notyetglossary'] = 'Der skal være mindst eet ordbogsindlæg for at vælge.';
+$string['random'] = 'Tilfældigt indlæg';
+$string['refresh'] = 'Antal dage før et nyt indlæg vælges.';
+$string['select_glossary'] = 'Tag indlæg fra denne ordbog';
+$string['title'] = 'Titel';
+$string['type'] = 'Hvordan et nyt indlæg bliver valgt';
+$string['typehelp'] = '<b>Sidst rettede indlæg</b> vil altid vise det sidst rettede indlæg og <b>Tilfældigt indlæg</b> vil vælge et tilfældigt indlæg hver gang. Valgmuligheden <b>Næste indlæg</b> vil altid tage det næste indlæg og starte forfra når slutningen er nået. Den sidste mulighed er især god hvis man også vælger et antal dage, hvilket giver muligheden for at lave et \"Ugens gruk\" eller \"Dagens Tip\" som alle ser.';
+$string['viewglossary'] = 'Flere citater...';
+
+?>
