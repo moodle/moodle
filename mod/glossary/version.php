0615bcfc119c983214038f022fae0110e1ae9350
stronk7 <stronk7>
2005-04-19 00:16:58 +0000

    Changed the wrong line. Solved.

--- mod/glossary/version.php
+++ mod/glossary/version.php
@@ -5,8 +5,8 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2005041101;
-$module->requires = 2005041900;  // Requires this Moodle version
+$module->version  = 2005041900;
+$module->requires = 2005031000;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
 $release = "1.5 development";   // User-friendly version number
