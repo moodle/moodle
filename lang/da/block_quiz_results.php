ad7fa8ed3481882f8b294e6dee2afec5592e1084
gniske <gniske>
2005-04-19 07:35:45 +0000

    Addet missing strings

---
+++ lang/da/block_quiz_results.php
@@ -0,0 +1,28 @@
+<?PHP // $Id$ 
+      // block_quiz_results.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005033100)
+
+
+$string['bestgrade'] = 'Højeste vudering:';
+$string['bestgrades'] = 'Den $a højeste vurdering:';
+$string['bestgroupgrade'] = 'Gruppen med det højeste gennemsnit:';
+$string['bestgroupgrades'] = 'De $a grupper med det højeste gennemsnit:';
+$string['config_format_absolute'] = 'Heltal';
+$string['config_format_fraction'] = 'Kommatal';
+$string['config_format_percentage'] = 'Procenter';
+$string['config_grade_format'] = 'Vis vurderinger som:';
+$string['config_no_quizzes_in_course'] = 'Dette kursus indeholder ikke nogle quizaktiviteter. Du bliver nødt til at tilføje mindst een før du kan bruge denne blok.';
+$string['config_select_quiz'] = 'Hvilken quiz skal denne blok vise resultater fra?';
+$string['config_show_best'] = 'Hvor mange af de højeste vurderinger skal vises (0 for at deaktivere)';
+$string['config_show_worst'] = 'Hvor mange af de laveste vurderinger skal vises (0 for at deaktivere)';
+$string['config_use_groups'] = 'Vis grupper i stedet for studerende (hvis quizzen understøtter grupper)';
+$string['configuredtoshownothing'] = 'Denne bloks konfiguration tillader på nuværende tidspunkt ikke at vise nogle resultater. Den skal måske omkonfigureres eller skjules.';
+$string['error_emptyquizid'] = 'Der er et problem med denne blok: Du bliver nødt til at vælge hvilken quiz den skal vise resultater fra.';
+$string['error_emptyquizrecord'] = 'Der er et problem med denne blok: Den valgte quiz ser ikke ud til at eksistere i databasen.';
+$string['error_nogroupsexist'] = 'Der er et problem med denne blok: Blokken er sat til at vise vurderinger på gruppebasis, men der er ikke defineret nogen grupper i kurset.';
+$string['formaltitle'] = 'Quiz resultater';
+$string['worstgrade'] = 'Den laveste vurdering';
+$string['worstgrades'] = 'De $a laveste vurderinger:';
+$string['worstgroupgrade'] = 'Gruppe med den laveste vurdering:';
+$string['worstgroupgrades'] = 'De $a grupper med de laveste vurderinger:';
+
+?>
