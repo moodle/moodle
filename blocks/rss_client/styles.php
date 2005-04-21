fade18149ce7309607bf6f0e35820fd1ba0be2c6
moodler <moodler>
2005-04-21 17:45:31 +0000

    Font tinkering

--- blocks/rss_client/styles.php
+++ blocks/rss_client/styles.php
@@ -2,12 +2,12 @@
     border-top:1px solid;
     border-top-color:#DDDDDD;
     padding-bottom:5px;
-    font-size:0.7em;
+    font-size:0.82em;
 }
 
 .block_rss_client .description {
     color:#555555;
-    font-size:0.7em;
+    font-size:0.78em;
     padding-left:10px;
     padding-bottom:8px;
 }
