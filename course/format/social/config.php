71de24539ac8c3026b821efdb9e75ec62806211a
defacer <defacer>
2005-04-21 13:25:43 +0000

    Removing one block from social courses defaults, too overcrowded. Fixing also line endings.

--- course/format/social/config.php
+++ course/format/social/config.php
@@ -1,13 +1,12 @@
-<?php
+<?php //$Id$
 //
 // Optional course format configuration file
 //
-// This file contains any specific configuration settings for the 
-// format.
+// This file contains any specific configuration settings for the
+// social format.
 //
 // The default blocks layout for this course format:
-    $format['defaultblocks'] = 'participants,search_forums,calendar_month,'.
-                               'calendar_upcoming,social_activities,'.
-                               'recent_activity,admin,course_list';
-//
+    $format['defaultblocks'] = 'participants,search_forums,calendar_upcoming,'.
+                               'social_activities,recent_activity,admin,course_list';
+
 ?>
