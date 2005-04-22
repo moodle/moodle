b9c1ffbe9f6a7c8778a669c38d6040edc9f1da21
dovix <dovix>
2005-04-22 11:03:43 +0000

    *** empty log message ***

--- lang/he/admin.php
+++ lang/he/admin.php
@@ -1,13 +1,36 @@
 <?PHP // $Id$ 
-      // admin.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004100800)
+      // admin.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005041101)
 
 
+$string['adminseesallevents'] = 'Administrators see all events';
+$string['adminseesownevents'] = 'Administrators are just like other users';
+$string['blockinstances'] = 'Instances';
+$string['blockmultiple'] = 'מרובים';
 $string['cachetext'] = 'משך התקפות של טקסט במטמון';
+$string['calendarsettings'] = 'לוח שנה';
+$string['change'] = 'שינוי';
+$string['configsectioninterface'] = 'ממשק';
+$string['configsectionmail'] = 'דואר';
+$string['configsectionmaintenance'] = 'תחזוקה';
+$string['configsectionmisc'] = 'שונות';
+$string['configsectionoperatingsystem'] = 'מערכת הפעלה';
+$string['configsectionpermissions'] = 'הרשאות';
+$string['configsectionsecurity'] = 'אבטחה';
+$string['configsectionuser'] = 'משתמש';
+$string['configvariables'] = 'משתנים';
+$string['confirmation'] = 'אישור';
+$string['edithelpdocs'] = 'ערוך מסמכי עזרה';
+$string['editstrings'] = 'ערוך מחרוזות';
+$string['filterall'] = 'סנן את כל המחרוזות';
 $string['filteruploadedfiles'] = 'סנן את הקבצים המועלים';
-$string['parentlanguage'] = 'en';
-$string['thischarset'] = 'ISO-8859-I';
-$string['thisdirection'] = 'rtl';
-$string['thislanguage'] = '&#1506;&#1489;&#1512;&#1497;&#1514;';
+$string['helpcalendarsettings'] = 'הגדר מאפיינים שונים הקשורים ללוח השנה ותאריך/שעה';
+$string['helpsitemaintenance'] = 'לשדרוגים ועבודות תחזוקה אחרות';
+$string['optionalmaintenancemessage'] = 'הודעת תחזוקה אופציונלית';
+$string['sitemaintenance'] = 'אתר זה עובר כרגע פעולות תחזוקה ואינו זמין';
+$string['sitemaintenancemode'] = 'מצב תחזוקה';
+$string['sitemaintenanceoff'] = 'מצב תחזוקה בוטל, וכעת האתר פועל שוב כרגיך';
+$string['sitemaintenanceon'] = 'מצב תחזוקה מופעל כרגע באתר (רק מנהלים יכולים להתחבר לאתר)';
+$string['sitemaintenancewarning'] = 'מצב תחזוקה מופעל כרגע באתר (רק מנהלים יכולים להתחבר לאתר). להחזרת האתר למצבו הרגיל, <a href=\"maintenance.php\">בטל את מצב התחזוקה</a>.';
 $string['upgradelogs'] = 'להפעלת מלוא היכולות, הדוחות הישנים שלך צריכים להתעדכן. <a href=\"$a\"> מידע נוסף</a>';
 $string['upgradelogsinfo'] = 'לאחרונה נעשו מספר שינויים לאופן שבו נשמרים הדוחות. על מנת לאפשר צפיה בדוחות הישנים בחלוקה לפי פעילות, יש לשדרג את הדוחות הישנים שלך. כתלות באתר, פעולה זו יכולה לקחת זמן רב (לדוגמא מספר שעות) ולצרוך הרבה משאבים מבסיס הנתונים של אתרים גדולים. מרגע שהתחלת תהליך זה, רצוי לתת לו לסיים (על ידי השארת חלון הדפדפן פתוח). אל תדאג - אתרך יהיה זמין למבקרים אחרים בזמן שהדוחות מתעדכנים.<br /><br /> האם אתה רוצה לעדכן את דוחותיך עכשיו?';
 $string['upgradesure'] = 'קבצי ה-Moodle הישנים שלך שונו, וכעת תוכנת השרת שלך תעודכן באופן אוטומטי לגרסה זו:
--- lang/he/block_course_summary.php
+++ lang/he/block_course_summary.php
@@ -1,12 +1,8 @@
 <?PHP // $Id$ 
-      // block_course_summary.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004100800)
+      // block_course_summary.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005041101)
 
 
-$string['blockname'] = 'סיכום קורס';
-$string['parentlanguage'] = 'en';
-$string['siteinfo'] = 'מידע אודות האתר';
-$string['thischarset'] = 'ISO-8859-8-I';
-$string['thisdirection'] = 'rtl';
-$string['thislanguage'] = '&#1506;&#1489;&#1512;&#1497;&#1514;';
+$string['coursesummary'] = 'אודות הקורס ';
+$string['pagedescription'] = 'אודות האתר';
 
 ?>
