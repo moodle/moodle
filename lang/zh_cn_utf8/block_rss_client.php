358fd55825a2307b0448f470c3fa83694bba3b24
lonecatz <lonecatz>
2005-04-23 19:30:51 +0000

    new translated file

---
+++ lang/zh_cn_utf8/block_rss_client.php
@@ -0,0 +1,35 @@
+<?PHP // $Id$ 
+      // block_rss_client.php - created with Moodle 1.5 ALPHA (2005042300)
+
+
+$string['block_rss_add_feed'] = '添加新种子链接：';
+$string['block_rss_add_new'] = '添加新的';
+$string['block_rss_choose_feed_label'] = '显示您要显示在这个版块中的种子：';
+$string['block_rss_client_channel_link'] = '源站点.';
+$string['block_rss_client_num_entries'] = '缺省情况下每个种子显示多少条信息。';
+$string['block_rss_client_show_channel_link_label'] = '是否显示原站链接(频道链接)?(如果在新闻种子中并未提供链接，则不会显示)';
+$string['block_rss_client_show_image_label'] = '如果可能，则显示频道的图片：';
+$string['block_rss_could_not_find_feed'] = '无法找到以如下id标识的种子';
+$string['block_rss_custom_title_label'] = '自定义题目(如果留空则使用种子内的标题)';
+$string['block_rss_delete_feed_confirm'] = '您确定要删除这个种子么?';
+$string['block_rss_display_description_label'] = '显示每个链接的描述?';
+$string['block_rss_edit_feeds'] = '修改、订阅或取消订阅RSS/Atom新闻种子';
+$string['block_rss_edit_news_feeds'] = '修改新闻种子';
+$string['block_rss_feed'] = '新闻种子';
+$string['block_rss_feed_added'] = '新闻种子已添加';
+$string['block_rss_feed_deleted'] = '新闻种子已删除';
+$string['block_rss_feed_updated'] = '新闻种子已更新';
+$string['block_rss_feeds'] = '新闻种子';
+$string['block_rss_feeds_add_edit'] = '添加/修改种子';
+$string['block_rss_feeds_title'] = '远程RSS种子';
+$string['block_rss_find_more_feeds'] = '查找更多RSS种子';
+$string['block_rss_no_feeds'] = '此站尚未添加RSS种子。';
+$string['block_rss_pick_feed'] = '选择一个新闻种子';
+$string['block_rss_remote_news_feed'] = '远程新闻种子';
+$string['block_rss_shownumentries_label'] = '每个版块中最多显示几条。';
+$string['block_rss_submitters'] = '允许谁定义新的RSS种子？已定义的种子可以在站点的任何页面显示。';
+$string['block_rss_timeout_desc'] = '缓冲RSS中种子的时间(分钟)';
+$string['block_rss_update_feed'] = '更新新闻种子链接：';
+$string['validate_feed'] = '验证种子';
+
+?>
