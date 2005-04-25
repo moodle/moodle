d490aac421276b34f6611b19f0c9da6188c2794c
lonecatz <lonecatz>
2005-04-25 05:52:58 +0000

    first translation

---
+++ lang/zh_cn_utf8/enrol_internal.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // enrol_internal.php - created with Moodle 1.5 ALPHA (2005042300)
+
+
+$string['description'] = '这是选课的缺省方式，学生要想参与某一课程有两种方法：
+<ul>
+<li>教师或管理员可以手工将其加入到课程中，相关链接在课程管理目录中。</li>
+<li>课程可以定一个密码作为选课密码，任何知道选课密码的人都可以参与课程。</li>
+</ul>';
+$string['enrolname'] = '内部选课';
+
+?>
