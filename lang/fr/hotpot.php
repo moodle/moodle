82dfc00e516ace546cae7fa2f6ef791416c3c967
martignoni <martignoni>
2005-04-25 19:58:47 +0000

    Hotpot strings

--- lang/fr/hotpot.php
+++ lang/fr/hotpot.php
@@ -1,11 +1,76 @@
 <?php // $Id$ 
 
-//translators:  You might want to leave the first two items 'as is' in English
 $string['modulename'] = 'Test Hot Potatoes';
 $string['modulenameplural'] = 'Tests Hot Potatoes';
-$string['strattemptlabel'] = 'Tentatives : score, date heure (durée), [pénalités]';
-$string['deleteall'] = 'Tout supprimer';
-$string['clearall'] = 'Tout effacer';
-$string['really'] = 'Voulez-vous vraiment supprimer tous les résultats de ce test ?';
+
+// for mod.html
+$string['outputformat'] = 'Format d\'exportation';
+$string['outputformat_best'] = 'meilleur';
+$string['outputformat_v6_plus'] = 'v6+';
+$string['outputformat_v6'] = 'v6';
+$string['outputformat_v5'] = 'v5';
+$string['outputformat_v4'] = 'v4';
+$string['outputformat_v3'] = 'v3';
+$string['outputformat_flash'] = 'Flash';
+$string['outputformat_mobile'] = 'mobile';
+  	 
+$string['navigation'] = 'Navigation';
+$string['navigation_bar'] = 'Barre de navigation Moodle';
+$string['navigation_frame'] = 'Cadre de navigation Moodle';
+$string['navigation_iframe'] = '&lt;IFRAME&gt; inclus';
+$string['navigation_buttons'] = 'Boutons des tests Hot Potatoes';
+$string['navigation_give_up'] = 'Bouton «&nbsp;Abandonner&nbsp;» uniquement';
+$string['navigation_none'] = 'Aucune';
+  	 
+$string['giveup'] = 'Abandonner';
+$string['location'] = 'Emplacement du fichier';
+$string['shownextquiz'] = 'Afficher test suivant&nbsp;?';
+$string['forceplugins'] = 'Imposer les plugins de médias';
+  	 
+$string['resultssaved'] = 'Les résultats du test ont été enregistrés';
+  	 
+  	 // for edit.php and show.php
+$string['filetype'] = 'Type de fichier';
+$string['quiztype'] = 'Type de test';
+$string['showxmlsource'] = 'Afficher la source XML';
+$string['showxmltree'] = 'Afficher l\'arbre XML';
+$string['showhtmlsource'] = 'Afficher la source HTML';
+$string['enterafilename'] = 'Veuillez saisir un nom de fichier';
+  	 
+  	 // lib.php
+$string['noactivity'] = 'Aucune activité';
+$string['inprogress'] = 'en cours';
+$string['abandoned'] = 'abandonné';
+  	 
+$string['correct'] = 'correct';
+$string['ignored'] = 'ignoré';
+$string['wrong'] = 'faux';
+$string['score'] = 'score';
+$string['weighting'] = 'pondération';
+$string['hints'] = 'conseils';
+$string['clues'] = 'indices';
+$string['checks'] = 'vérifications';
+$string['penalties'] = 'pénalités';
+  	 
+  	 // report.php
+$string['deletedattempts'] = '$a tentatives ont été supprimées';
+$string['deletedresponses'] = '$a réponses ont été supprimées';
+$string['selectattempt'] = 'Veuillez choisir une tentative';
+  	 
+$string['thiscourse'] = 'ce cours';
+$string['allmycourses'] = 'tous mes cours';
+$string['bestattempt'] = 'meilleure tentative';
+$string['allattempts'] = 'toutes les tentatives';
+  	 
+  	 // report/*/report.php
+$string['average'] = 'Moyenne';
+$string['questionshort'] = 'Q-$a';
+  	 
+  	 // report/overview/report.php
+$string['deleteabandoned'] = 'Supprimer les abandons';
+$string['deleteabandonedcheck'] = 'Voulez-vous vraiment supprimer les $a tentatives abandonées&nbsp;?';
+  	 
+  	 // review.php
+$string['noresponses'] = 'Aucune information n\'a été trouvée sur les questions et les réponses individuelles.';
 
 ?>
