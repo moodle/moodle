cc536aa18a4a3a8352a881fae1197a93585bf537
dlnsk <dlnsk>
2005-04-25 10:53:10 +0000

    Initial translation of Book module

---
+++ lang/ru/book.php
@@ -0,0 +1,42 @@
+<?PHP // $Id$ 
+      // book.php - created with Moodle 1.4.4 (2004083140)
+
+
+$string['addafter'] = 'Добавить главу';
+$string['chapterscount'] = 'Количество глав';
+$string['chaptertitle'] = 'Название главы';
+$string['confchapterdelete'] = 'Вы действительно хотите удалить эту главу?';
+$string['confchapterdeleteall'] = 'Вы действительно хотите удалить эту главу и все ее подглавы?';
+$string['content'] = 'Содержимое';
+$string['customtitles'] = 'Без заголовков';
+$string['disableprinting'] = 'Запретить вид \"для печати\"';
+$string['doimport'] = 'Импортировать';
+$string['editingchapter'] = 'Редактировать главу';
+$string['faq'] = 'ЧАвО по модулю';
+$string['fileordir'] = 'Файл или папка';
+$string['import'] = 'Импорт';
+$string['importinfo'] = 'Вы можете импортировать один HTML файл или целую папку.<br />Главы будут отсортированы по алфавиту в соответствии с именами файлов.<br />Названия глав берутся из тега TITLE.';
+$string['importing'] = 'Импортирование';
+$string['importingchapters'] = 'Импортирование глав в книгу';
+$string['maindirectory'] = 'Основная папка';
+$string['modulename'] = 'Книга';
+$string['modulenameplural'] = 'Книги';
+$string['navexit'] = 'Покинуть Книгу';
+$string['navnext'] = 'Следующая';
+$string['navprev'] = 'Предыдущая';
+$string['numbering'] = 'Нумерация глав';
+$string['numbering0'] = 'Нет';
+$string['numbering1'] = 'Номера';
+$string['numbering2'] = 'Маркеры';
+$string['numbering3'] = 'Сдвиг';
+$string['printbook'] = 'Печатать всю книгу';
+$string['printchapter'] = 'Печатать только эту главу';
+$string['printdate'] = 'Дата';
+$string['printedby'] = 'Напечатана';
+$string['relinking'] = 'Преобразование ссылок';
+$string['subchapter'] = 'Подглава';
+$string['toc'] = 'Содержание';
+$string['tocwidth'] = 'Выберите ширину Содержания для всех книг';
+$string['top'] = 'наверх';
+
+?>
