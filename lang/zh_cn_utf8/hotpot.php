f1586644805debeafb0379f14395749b756e495b
lonecatz <lonecatz>
2005-04-25 02:58:49 +0000

    first translation

---
+++ lang/zh_cn_utf8/hotpot.php
@@ -0,0 +1,12 @@
+<?PHP // $Id$ 
+      // hotpot.php - created with Moodle 1.5 ALPHA (2005042300)
+
+
+$string['clearall'] = '清空全部';
+$string['deleteall'] = '删除全部';
+$string['modulename'] = 'Hot Potatoes测验';
+$string['modulenameplural'] = 'Hot Potatoes测验';
+$string['really'] = '您确定要删除此测验的所有结果么？';
+$string['strattemptlabel'] = '试答: 成绩, 耗时, [惩罚]';
+
+?>
