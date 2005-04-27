047fce98375f71546453c985c72216f79c3cb512
gniske <gniske>
2005-04-27 22:28:58 +0000

    Created new language file

---
+++ lang/da/message.php
@@ -0,0 +1,63 @@
+<?PHP // $Id$ 
+      // message.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005033100)
+
+
+$string['addcontact'] = 'Tilføj kontakt';
+$string['ago'] = '$a siden';
+$string['allmine'] = 'Alle beskeder til mig eller fra mig';
+$string['allstudents'] = 'Alle beskeder mellem elerver i kurset';
+$string['allusers'] = 'Alle beskeder fra alle brugere';
+$string['backupmessageshelp'] = 'Hvis aktiveret vil beskeder blive inkluderet i automatiske SITE backups';
+$string['beepnewmessage'] = 'Bip når der kommer en ny besked';
+$string['blockcontact'] = 'Bloker kontakt';
+$string['blockedmessages'] = '$a besked(er) til/fra blokerede brugere';
+$string['contacts'] = 'Kontekter';
+$string['context'] = 'sammenhæng';
+$string['deletemessagesdays'] = 'Antal dage før gamle beskeder automatisk bliver slettet';
+$string['discussion'] = 'Diskussion';
+$string['emailmessages'] = 'Send beskeder som emails hvis jeg er offline';
+$string['emailtagline'] = 'Denne email er en kopi af en besked sendt til \"$a\"';
+$string['emptysearchstring'] = 'Du må søge efter noget';
+$string['formorethan'] = 'For mere end';
+$string['includeblockedusers'] = 'Inkluder blokerede brugere';
+$string['incomingcontacts'] = 'Indkomne kontakter ($a)';
+$string['keywords'] = 'Nøgleord';
+$string['keywordssearchresults'] = 'Søge resultat: $a beskeder fundet';
+$string['maxmessages'] = 'Maksimalt antal beskeder der vises i diskussionshistoriken';
+$string['message'] = 'Besked';
+$string['messagehistory'] = 'Besked historik';
+$string['messages'] = 'Beskeder';
+$string['messaging'] = 'Beskedsystem';
+$string['mycontacts'] = 'Mine kontakter';
+$string['newsearch'] = 'Ny søgning';
+$string['nomessages'] = 'Ingen ventene beskeder';
+$string['nomessagesfound'] = 'Ingen beskeder fundet';
+$string['nosearchresults'] = 'Der var ingen beskeder der opfyldte søgekriteriet';
+$string['offline'] = 'Offline';
+$string['offlinecontacts'] = 'Offline kontakter ($a)';
+$string['online'] = 'Online';
+$string['onlinecontacts'] = 'Online kontakter ($a)';
+$string['onlyfromme'] = 'Kun beskeder fra mig';
+$string['onlymycourses'] = 'Kun i mit kursus';
+$string['onlytome'] = 'Kun beskeder til mig';
+$string['pagerefreshes'] = 'Denne side opdateres hvert $a sekundt';
+$string['readmessages'] = '$a læste beskeder';
+$string['removecontact'] = 'Fjern kontakt';
+$string['savemysettings'] = 'Gem mine indstillinger';
+$string['search'] = 'Søg';
+$string['searchforperson'] = 'Søg efter en person';
+$string['searchmessages'] = 'Søg efter beskeder';
+$string['sendmessage'] = 'Send besked';
+$string['sendmessageto'] = 'Send besked til $a';
+$string['settings'] = 'Indstillinger';
+$string['settingssaved'] = 'Dine indstillinger er gemt';
+$string['showmessagewindow'] = 'Vis automatisk nye beskeder i et popupvindue. (det kræver at din browser ikke bloker for popupvinduer for dette site)';
+$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%I:%%M %%p';
+$string['timenosee'] = 'Antal minutter siden du sidst var online på sitet';
+$string['timesent'] = 'Tid sendt';
+$string['unblockcontact'] = 'Fjern blokering for kontakt';
+$string['unreadmessages'] = '$a ulæste beskeder';
+$string['userisblockingyou'] = 'Denne bruger har blokeret for beskeder til dem.';
+$string['userssearchresults'] = 'Søge resultat: $a brugere fundet';
+
+?>
