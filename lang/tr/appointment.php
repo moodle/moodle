56cf1027817a6222f861d58b9434f1f21099527f
ethem <ethem>
2005-04-27 10:12:20 +0000

    I love XHTML compliant unless a file contain XHTML tag :)

--- lang/tr/appointment.php
+++ lang/tr/appointment.php
@@ -2,8 +2,44 @@
       // appointment.php - created with Moodle 1.5 ALPHA (2005042300)
 
 
+$string['allowresubmit'] = 'Tekrar göndermeye izin ver';
+$string['appointmentdetails'] = 'Görüþme ayrýntýlarý';
+$string['appointmentlocation'] = 'Görüþme yeri';
+$string['appointmentname'] = 'Görüþme adý';
+$string['date'] = 'Görüþme tarihi';
+$string['description'] = 'Açýklama';
+$string['early'] = '$a erken';
+$string['failedupdatefeedback'] = '$a kullanýcýsý için gönderi geribildirimi güncellnirken hata oluþtu';
 $string['feedback'] = 'Geribildirim';
-$string['modulename'] = 'Randevu';
-$string['modulenameplural'] = 'Randevular';
+$string['feedbackupdated'] = '$a kiþi için gönderi geribildirimleri güncellendi';
+$string['late'] = '$a geç';
+$string['maximumgrade'] = 'En fazla not';
+$string['maximumsize'] = 'En fazla boyut';
+$string['modulename'] = 'Görüþme';
+$string['modulenameplural'] = 'Görüþmeler';
+$string['newsubmissions'] = 'Görüþmeler iletildi';
+$string['notgradedyet'] = 'Henüz notlandýrýlmadý';
+$string['notsubmittedyet'] = 'Henüz gönderilmedi';
+$string['overwritewarning'] = 'Uyarý: tekrar yükleme þu anki gönderinizi DEÐÝÞTÝRECEK';
+$string['saveallfeedback'] = 'Tüm geribildirimlerimi kaydet';
+$string['submissionfeedback'] = 'Geribildirim gönderimi';
+$string['submissions'] = 'Gönderiler';
+$string['submitappointment'] = 'Görüþmenizi bu formu kullanarak ayarlayýn';
+$string['submitted'] = 'Ýletildi';
+$string['timeend'] = 'Görüþme sonu';
+$string['timestart'] = 'Görüþme baþlangýcý';
+$string['typeoffline'] = 'Çevrimdýþý etkinlik';
+$string['typeuploadsingle'] = 'Bir dosya yükle';
+$string['uploadbadname'] = 'Dosya adý geçersiz karakterler içerdiði için yüklenemedi';
+$string['uploadedfiles'] = 'yüklenmiþ dosyalar';
+$string['uploaderror'] = 'Sunucuya dosya yüklenirken bir hata oluþtu';
+$string['uploadfailnoupdate'] = '\'$a\' yüklendi ancak iletiniz güncellenemedi';
+$string['uploadfiletoobig'] = 'Üzgünüz, bu dosya çok büyük (sýnýr $a byte\'dýr)';
+$string['uploadnofilefound'] = 'Dosya bulunamadý - yüklemek için bir dosya seçtiðinizden emin misiniz?';
+$string['uploadnotregistered'] = '\'$a\' yüklendi ancak iletiniz kayýt yapýlmadý!';
+$string['uploadsuccess'] = '\'$a\' baþarýyla gönderildi';
+$string['viewfeedback'] = 'Görüþme notlarýna ve geribildirimlerine bak';
+$string['viewsubmissions'] = '$a iletilmiþ görüþmeye bak';
+$string['yoursubmission'] = 'Gönderiniz';
 
 ?>
