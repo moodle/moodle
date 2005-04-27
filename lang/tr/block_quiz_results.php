b2095ac8a2d154af89170b20e5b77c5cb17dd462
ethem <ethem>
2005-04-27 17:06:32 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com)

--- lang/tr/block_quiz_results.php
+++ lang/tr/block_quiz_results.php
@@ -4,7 +4,25 @@
 
 $string['bestgrade'] = 'En yüksek not:';
 $string['bestgrades'] = 'En yüksek $a notlarý:';
+$string['bestgroupgrade'] = 'En yüksek ortalamalý grup';
+$string['bestgroupgrades'] = 'En yüksek ortalamalý $a grup';
+$string['config_format_absolute'] = 'Tam sayý';
+$string['config_format_fraction'] = 'Kesirli';
+$string['config_format_percentage'] = 'Yüzdelik';
+$string['config_grade_format'] = 'Not görünümü';
+$string['config_no_quizzes_in_course'] = 'Bu kursa ait hiç sýnav etkinliði yok. Bu bloðun doðru olarak görülmesi için en az bir tane sýnav eklemelisiniz.';
+$string['config_select_quiz'] = 'Hangi sýnavýn sonuçlarý bu blokta gösterilecek';
+$string['config_show_best'] = 'Kaç tane en yüksek not görünecek (0: etkin deðil)';
+$string['config_show_worst'] = 'Kaç tane en düþük not görünecek (0: etkin deðil)';
+$string['config_use_groups'] = 'Öðrenciler yerine gruplarý göster (sadece sýnavýn desteði varsa)';
+$string['configuredtoshownothing'] = 'Bu bloðun ayarlarý þu anda hiç bir sýnav sonucunun gösterilmesine izin vermiyor. Bu bloðu yapýlandýrabilir veya gizleyebilirsiniz.';
+$string['error_emptyquizid'] = 'Þu anda bu blokta bir hata var: Hangi sýnav sonuçlarýnýn bu blokta görünmesini istediðinizi seçmeniz gerekiyor.';
+$string['error_emptyquizrecord'] = 'Þu anda bu blokta bir hata var: Seçilen sýnav þu anda veritabanýnda yok.';
+$string['error_nogroupsexist'] = 'Þu anda bu blokta bir hata var: Notlarý grup modunda gösterilmek için ayarlanýþ, ancak hiç bir grup tanýmlanmamýþ.';
 $string['formaltitle'] = 'Sýnav Sonuçlarý';
 $string['worstgrade'] = 'En düþük not:';
+$string['worstgrades'] = 'En düþük $a not:';
+$string['worstgroupgrade'] = 'En düþük ortalamalý grup';
+$string['worstgroupgrades'] = 'En düþük ortalamalý $a grup';
 
 ?>
