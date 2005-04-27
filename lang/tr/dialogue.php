e7532d1d0f607a9b77aa0430b09cc3c83f4ff20d
ethem <ethem>
2005-04-27 10:53:14 +0000

    This is XHTML compliant because it has a <br /> tag :) (not <br/>)

--- lang/tr/dialogue.php
+++ lang/tr/dialogue.php
@@ -2,8 +2,72 @@
       // dialogue.php - created with Moodle 1.5 ALPHA (2005042300)
 
 
-$string['lastentry'] = 'Son Girdi';
+$string['addmynewentries'] = 'Yeni Kayýtlarý ekle';
+$string['addmynewentry'] = 'Yeni Kaydý ekle';
+$string['addsubject'] = 'Konu Ekle';
+$string['allowmultiple'] = 'Ayný kiþiyle birden fazla Söyleþiye izin ver';
+$string['allowstudentdialogues'] = 'Öðrenciden Öðrenciye Söyleþilere izin ver';
+$string['close'] = 'Kapat';
+$string['closed'] = 'Kapandý';
+$string['confirmclosure'] = '$a ile söyleþiyi kapatmak üzeresiniz. Kapatýlan söyleþiler tekrar açýlamaz. Bu söyleþiyi kapatýrsanýz görebilir, fakat yeni söz ekleyemezsiniz. Ayrýca bu kiþiyle yeniden \"konuþmak\" için baþka bir söyleþi baþlatmak zorundasýnýz..<br /><br /> Bu söyleþiyi kapatmak istediðinizden emin misiniz?';
+$string['deleteafter'] = 'Kapanmýþ Söyleþileri (Gün) sonra sil';
+$string['dialogueclosed'] = 'Söyleþi kapatýldý';
+$string['dialogueintro'] = 'Söyleþi Tanýtýmý';
+$string['dialoguemail'] = '$a->userfrom kullanýcýsý, \'$a->dialogue\' isimli söyleþinize yeni bir söz ekledi.
+
+Bunu söyleþi kaydýna eklenmiþ olarak görebilirsiniz:
+
+$a->url';
+$string['dialoguemailhtml'] = '$a->userfrom kullanýcýsý, \'<i>$a->dialogue</i>\'
+isimli söyleþinize yeni bir söz ekledi.<br /><br />
+
+Bunu <a href=\"$a->url\">söyleþi</a> kaydýna eklenmiþ olarak görebilirsiniz.';
+$string['dialoguename'] = 'Söyleþi adý';
+$string['dialogueopened'] = '$a ile söyleþi açýldý';
+$string['dialoguewith'] = '$a ile Söyleþi';
+$string['everybody'] = 'Herkes';
+$string['furtherinformation'] = 'Daha Fazla Bilgi';
+$string['lastentry'] = 'Son Kayýt';
+$string['maildefault'] = 'Mail Varsayýlaný';
+$string['mailnotification'] = 'Mail Onayý';
 $string['modulename'] = 'Söyleþi';
 $string['modulenameplural'] = 'Söyleþiler';
+$string['namehascloseddialogue'] = '$a söyleþiyi kapattý';
+$string['newdialogueentries'] = 'Yeni söyleþi kayýtlarý';
+$string['newentry'] = 'Yeni Kayýt';
+$string['noavailablepeople'] = 'Söyleþi yapýlabilecek hiç kimse yok';
+$string['nopersonchosen'] = 'Kiþi Seçilmedi';
+$string['nosubject'] = 'Konu Girilmedi';
+$string['notextentered'] = 'Yazý Girilmedi';
+$string['notstarted'] = 'Henüz bu söyleþiye baþlamadýnýz';
+$string['notyetseen'] = 'Henüz görülmedi';
+$string['numberofentries'] = 'Kayýt sayýsý';
+$string['numberofentriesadded'] = 'Eklenen kayýt sayýsý: $a';
+$string['of'] = 'nýn';
+$string['onwrote'] = '$a \'da yazýldý';
+$string['onyouwrote'] = '$a \'da yazdýnýz';
+$string['open'] = 'Açýk';
+$string['openadialoguewith'] = 'Bununla bir söyleþi aç';
+$string['opendialogue'] = 'Söyleþi Aç';
+$string['opendialogueentries'] = 'Açýk söyleþi kayýtlarý';
+$string['pane0'] = 'Bir söyleþi aç';
+$string['pane1'] = '$a söyleþi sizden Cevap bekliyor';
+$string['pane1one'] = '1 söyleþi sizden Cevap bekliyor';
+$string['pane2'] = '$a söyleþi diðer kiþilerden Cevap bekliyor';
+$string['pane2one'] = '1 söyleþi diðer kiþilerden Cevap bekliyor';
+$string['pane3'] = '$a Kapalý Söyleþi';
+$string['pane3one'] = '1 Kapalý Söyleþi';
+$string['seen'] = '$a önce görüldü';
+$string['sendmailmessages'] = 'Yeni kayýtlarým hakkýnda Email Mesajlarý Gönder';
+$string['status'] = 'Durum';
+$string['studenttostudent'] = 'Öðrenciden Öðrenciye';
+$string['subject'] = 'Konu';
+$string['subjectadded'] = 'Konu Eklendi';
+$string['teachertostudent'] = 'Eðitimciden Öðrenciye';
+$string['typefirstentry'] = 'Ýlk kaydý buraya yaz';
+$string['typefollowup'] = 'Takip edeni buraya yaz';
+$string['typeofdialogue'] = 'Söyleþi Tipi';
+$string['typereply'] = 'Yanýtý buraya yazýnýz';
+$string['viewallentries'] = '$a Söyleþi kaydýna bak';
 
 ?>
