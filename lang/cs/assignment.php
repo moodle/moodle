02eb3f861c0c44122a6650b13836e6196d2aeb64
mudrd8mz <mudrd8mz>
2005-04-28 21:31:49 +0000

    First of 1.5 updates - to be continued asap

--- lang/cs/assignment.php
+++ lang/cs/assignment.php
@@ -1,21 +1,40 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.2 Beta (2004022400)
+      // assignment.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005042100)
 
 
 $string['allowresubmit'] = 'Umo¾nit znovuodevzdání';
 $string['assignmentdetails'] = 'Podrobnosti';
-$string['assignmentmail'] = '$a->teacher zaslal komentar k vasemu odevzdanemu ukolu \'$a->assignment\'.  Naleznete jej pripojeny k vasemu ukolu na adrese: $a->url';
+$string['assignmentmail'] = '$a->teacher zaslal komentar k vasemu odevzdanemu ukolu \'$a->assignment\'.
+
+Naleznete jej pripojeny k vasemu ukolu na adrese:
+$a->url';
 $string['assignmentmailhtml'] = '$a->teacher zaslal komentáø k va¹emu odevzdanému úkolu \'<i>$a->assignment</i>\'<br /><br />. Naleznete jej pøipojený k <a href=\"$a->url\">va¹emu úkolu</a>.';
 $string['assignmentname'] = 'Název úkolu';
 $string['assignmenttype'] = 'Typ úkolu';
-$string['configmaxbytes'] = 'Implicitní maximální velikost odevzdávaného úkolu (mù¾e být upravena nastavením kurzu a dal¹ími lokálními promìnnými)';
+$string['availabledate'] = 'Dostupné od';
+$string['comment'] = 'Komentáø';
+$string['commentinline'] = 'Komentovat v textu';
+$string['configmaxbytes'] = 'Výchozí maximální velikost odevzdávaného úkolu (mù¾e být upravena nastavením kurzu a dal¹ími lokálními promìnnými)';
 $string['description'] = 'Popis';
 $string['duedate'] = 'Termín odevzdání';
 $string['duedateno'] = 'Netermínováno';
 $string['early'] = '$a vèas';
+$string['editmysubmission'] = 'Upravit odevzdaný úkol';
+$string['emailteachermail'] = '$a->username upravil(a) svuj odevzdany ukol \'$a->assignment\'
+
+Aktualizovane reseni je dostupne na:
+
+$a->url';
+$string['emailteachermailhtml'] = '$a->username upravil(a) svùj odevzdaný úkol <em>\'$a->assignment\'</em><br /><br />
+
+Aktualizované øe¹ení je <a href=\"$a->url\">dostupné zde</a>.
+
+';
+$string['emailteachers'] = 'Upoyoròovat uèitele emailem';
 $string['existingfiledeleted'] = 'Stávající soubor byl smazán: $a';
 $string['failedupdatefeedback'] = 'Selhala aktualizace odpovìdi pro u¾ivatele $a';
 $string['feedback'] = 'Odpovìï';
+$string['feedbackfromteacher'] = 'Odpovìï od: $a';
 $string['feedbackupdated'] = 'Aktualizovány odpovìdi pro $a u¾ivatelù';
 $string['late'] = '$a pozdì';
 $string['maximumgrade'] = 'Maximum bodù';
