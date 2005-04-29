84dfdbe6901c030c3789714808a2a3b253061fe3
ethem <ethem>
2005-04-29 15:05:09 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com)

--- lang/tr/chat.php
+++ lang/tr/chat.php
@@ -1,42 +1,55 @@
 <?PHP // $Id$ 
-      // chat.php - created with Moodle 1.5 ALPHA (2005042300)
+      // chat.php - created with Moodle 1.5 ALPHA (2005042400)
 
 
 $string['beep'] = 'biip';
-$string['chatintro'] = 'Giriþ metni';
+$string['chatintro'] = 'Tanýtým metni';
 $string['chatname'] = 'Sohbet odasýnýn adý';
 $string['chatreport'] = 'Sohbet oturumlarý';
 $string['chattime'] = 'Sohbet saati';
+$string['configmethod'] = 'Normal sohbet yöntemi, güncellemeler için devamlý olarak sunuya baðlanan istemcilerle ilgilidir. Hiç bir yapýlandýrma gerektirmez ve her yerde çalýþýr. Ancak sohbet eden kiþi çoðalýrsa sunucuya aðýr yük getirir. Sohbet sunucusu daemonu kullanýmý Unix\'e shell eriþimi gerektirir ve ayný zamanda sohbet için daha uygun bir ortam saðlar.';
+$string['configoldping'] = 'Bir kullanýcýnýn baðlantýsýnýn kesildiði algýlandýðýnda en fazla ne kadar süre geçecek(saniye olarak)? Bu sadece üst sýnýrdýr ve kesintiler zaten çok hýzlý þekilde algýlanýr. Düþük deðerler sunucunuza daha fazla talep olmasýna sebep olur. Normal yöntemi kullanýyorsanýz, bunu <strong>asla</strong> 2 * chat_refresh_room \'dan daha düþük ayarlamayýn.';
+$string['configrefreshroom'] = 'Sohbet odasý kaç saniyede bir yenilenecek? (saniye olarak). Bu ayarý düþük tutarsanýz sohbet odasý daha hýzlý yenilenir. Ancak sunucuda çok fazla kullanýcý sohbet ediyorsa sunucunuza aðýr yük getirebilir. ';
+$string['configrefreshuserlist'] = 'Kullanýcý listesi kaç saniyede bir yenilenecek? (saniye olarak)';
+$string['configserverhost'] = 'Daemonun çalýþtýðý bilgisayarýn alan adý';
+$string['configserverip'] = 'Yukarýdaki alan adýnýn IP numarasý';
+$string['configservermax'] = 'Ýzin verilen max istemci sayýsý';
+$string['configserverport'] = 'Daemon için sunucuda kullanýlacak port';
 $string['currentchats'] = 'Aktif sohbet oturumlarý';
 $string['currentusers'] = 'Aktif kullanýcýlar';
 $string['deletesession'] = 'Bu oturumu sil';
 $string['deletesessionsure'] = 'Bu oturumu silmek istediðinizden emin misiniz?';
-$string['enterchat'] = 'Sohbete katýl';
-$string['errornousers'] = 'Hiç kullanýcý bulunamadý';
+$string['donotusechattime'] = 'Zaman sýnýrý olmaksýzýn her zaman AÇIK';
+$string['enterchat'] = 'Þimdi sohbete katýl';
+$string['errornousers'] = 'Hiç kullanýcý bulunamadý!';
+$string['explaingeneralconfig'] = 'Bu ayarlar <strong>her zaman</strong> etkili olacak';
+$string['explainmethoddaemon'] = 'Bu ayarlar, chat_method\'dan <strong>sadece</strong> \"Sohbet sunucusu daemonu\"nu seçerseniz etkili olacak';
+$string['explainmethodnormal'] = 'Bu ayarlar, chat_method\'dan <strong>sadece</strong> \"Normal yöntem\"i seçerseniz etkili olacak';
 $string['generalconfig'] = 'Genel ayarlar';
 $string['helpchatting'] = 'Sohbet yardýmý';
-$string['idle'] = 'Meþgul';
-$string['messagebeepseveryone'] = '$a herkesi uyarýyor';
-$string['messagebeepsyou'] = '$a sizi uyardý!';
+$string['idle'] = 'Boþ geçen';
+$string['messagebeepseveryone'] = '$a herkese sesleniyor!';
+$string['messagebeepsyou'] = '$a size sesleniyor!';
 $string['messageenter'] = '$a odaya girdi';
 $string['messageexit'] = '$a odadan ayrýldý';
 $string['messages'] = 'Mesajlar';
+$string['methoddaemon'] = 'Sohbet sunucusu daemonu';
 $string['methodnormal'] = 'Normal yöntem';
 $string['modulename'] = 'Sohbet';
 $string['modulenameplural'] = 'Sohbetler';
-$string['neverdeletemessages'] = 'Asla mesajlarý silme';
+$string['neverdeletemessages'] = 'Mesajlarý asla silme';
 $string['nextsession'] = 'Sonraki oturum';
 $string['noguests'] = 'Sohbet odasý konuklara açýk deðildir';
 $string['nomessages'] = 'Henüz mesaj yok';
-$string['repeatdaily'] = 'Hergün ayný saatte';
-$string['repeatnone'] = 'Tekrar yok - sadece belirtilen saatte';
-$string['repeattimes'] = 'Oturum saatleri tekrarý';
-$string['repeatweekly'] = 'Her hafta ayný saatte';
-$string['savemessages'] = 'Geçmiþ oturumlarý saklama';
-$string['seesession'] = 'Bu oturumu gör';
+$string['repeatdaily'] = 'Her gün ayný saatte';
+$string['repeatnone'] = 'Tekrar yok - sadece BELÝRTÝLEN vakitte';
+$string['repeattimes'] = 'Oturum tekrarlarý';
+$string['repeatweekly'] = 'Her hafta ayný vakitte';
+$string['savemessages'] = 'Geçmiþ oturumlarý kaydet';
+$string['seesession'] = 'Bu oturuma gözat';
 $string['sessions'] = 'Sohbet oturumlarý';
 $string['strftimemessage'] = '%%H:%%M';
-$string['studentseereports'] = 'Geçmiþ oturumlarý herkes görebilir';
-$string['viewreport'] = 'Geçmiþ sohbet oturumlarýna bak';
+$string['studentseereports'] = 'Geçmiþ oturumlara herkes gözatabilir';
+$string['viewreport'] = 'Geçmiþ sohbet oturumlarýna gözat';
 
 ?>
