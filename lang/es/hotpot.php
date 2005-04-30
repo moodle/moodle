0e1ef357050ff40828d4fd6fbf5fef5d40b9866a
barias <barias>
2005-04-30 13:12:33 +0000

    Sorry... I hadn't seen your prior translation...

--- lang/es/hotpot.php
+++ lang/es/hotpot.php
@@ -1,12 +1,78 @@
 <?PHP // $Id$ 
-      // hotpot.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005042100)
+      // quiz.php - created with Moodle 1.2 development (2003111400)
 
-
-$string['clearall'] = 'Limpiar todo';
-$string['deleteall'] = 'Eliminar todo';
+//translators:  You might want to leave the first two items 'as is' in English
 $string['modulename'] = 'Hot Potatoes Quiz';
 $string['modulenameplural'] = 'Hot Potatoes Quizzes';
-$string['really'] = '¿Realmente desea eliminar todos los resultados de este cuestionario?';
-$string['strattemptlabel'] = 'Intentos: Puntuación, Hora (Duración), [Penalizaciones]';
+
+// for mod.html
+$string['outputformat'] = 'Formato de salida';
+$string['outputformat_best'] = 'el mejor posible';
+$string['outputformat_v6_plus'] = 'v6+';
+$string['outputformat_v6'] = 'v6';
+$string['outputformat_v5'] = 'v5';
+$string['outputformat_v4'] = 'v4';
+$string['outputformat_v3'] = 'v3';
+$string['outputformat_flash'] = 'Flash';
+$string['outputformat_mobile'] = 'móvil';
+
+$string['navigation'] = 'Navegación';
+$string['navigation_bar'] = 'Barra de navegación de Moodle';
+$string['navigation_frame'] = 'Marco de navegación de Moodle';
+$string['navigation_iframe'] = 'Embebido &lt;IFRAME&gt;';
+$string['navigation_buttons'] = 'Botones de Hot Potatoes';
+$string['navigation_give_up'] = 'Solamente un botón de &quot;Abandonar&quot; ';
+$string['navigation_none'] = 'Ninguno';
+
+$string['giveup'] = 'Abandonar';
+$string['location'] = 'Localización del archivo';
+$string['shownextquiz'] = '¿Mostrar siguiente ejercicio?';
+$string['forceplugins'] = "Forzar complementos multimedia";
+
+$string['resultssaved'] = 'Se guardaron los resultados del ejercicio';
+
+// for edit.php and show.php
+$string['filetype'] = 'Tipo de archivo';
+$string['quiztype'] = 'Tipo de ejercicio';
+$string['showxmlsource'] = 'Mostrar funete XML';
+$string['showxmltree'] = 'Mostrar árbol XML';
+$string['showhtmlsource'] = 'Mostrar fuente HTML';
+$string['enterafilename'] = 'Por, favor introduzca un nombre de archivo';
+
+// lib.php
+$string['noactivity'] = 'No hay actividad';
+$string['inprogress'] = 'en progreso';
+$string['abandoned'] = 'abandonado';
+
+$string['correct'] = 'correcto';
+$string['ignored'] = 'ignorado';
+$string['wrong'] = 'incorrecto';
+$string['score'] = 'puntuación';
+$string['weighting'] = 'valor en la calificación';
+$string['hints'] = 'ayudas';
+$string['clues'] = 'pistas';
+$string['checks'] = 'comprobaciones';
+$string['penalties'] = 'penalizaciones';
+
+// report.php
+$string['deletedattempts'] = '$a intentos fueron eliminados';
+$string['deletedresponses'] = '$a respuestas fueron eliminadas';
+$string['selectattempt'] = 'Por favor, seleccione uno de los intentos';
+
+$string['thiscourse'] = 'este curso';
+$string['allmycourses'] = 'todos mis cursos';
+$string['bestattempt'] = 'el mejor intento';
+$string['allattempts'] = 'todos los intentos';
+
+// report/*/report.php
+$string['average'] = 'Porcentaje';
+$string['questionshort'] = 'Q-$a';
+
+// report/overview/report.php
+$string['deleteabandoned'] = 'Borrar abandonado';
+$string['deleteabandonedcheck'] = '¿Está seguro de querer borrar todos los intentos abandonados?';
+
+// review.php
+$string['noresponses'] = 'No se ha encontrado información sobre preguntas individuales y respuestas.';
 
 ?>
