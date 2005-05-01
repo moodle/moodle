08c8105d8dd8bce1ef3537f2cb6bbafa82b2ea02
paca70 <paca70>
2005-05-01 16:04:31 +0000

    Updates

---
+++ lang/fi/message.php
@@ -0,0 +1,65 @@
+<?PHP // $Id$ 
+      // message.php - created with Moodle 1.5 ALPHA (2005042400)
+
+
+$string['addcontact'] = 'Lisää kontakti';
+$string['addsomecontacts'] = 'Käytä <a href=\"$a\">hakutoimintoa</a> lähettääksesi viestin jollekin, tai lisätäksesi heidät tälle sivulle. ';
+$string['addsomecontactsincoming'] = 'Nämä viestit ovat henkilöiltä jotka eivät ole kontakteissasi. Lisätäksesi heidät kontakteihin, klikkaa \"Lisää kontakti\" ikonia nimen perässä.';
+$string['ago'] = '$a sitten';
+$string['allmine'] = 'Kaikki viestit minulta tai minulle';
+$string['allstudents'] = 'Kaikki viestit kurssin oppilaiden kesken';
+$string['allusers'] = 'Kaikki viestit kaikilta';
+$string['backupmessageshelp'] = 'Määrittää varmuuskopioidaanko viestit sivuston varmuuskopioinnin yhteydessä.';
+$string['beepnewmessage'] = 'Äänimerkki viestin saapuessa';
+$string['blockcontact'] = 'Estä kontakti';
+$string['blockedmessages'] = '$a viesti(ä) estetyiltä käyttäjiltä/kayttäjille';
+$string['blocknoncontacts'] = 'Estä kaikki vietit jos lähettäjä ei ole listallani.';
+$string['contactlistempty'] = 'Kontaktilistasi on tyhjä.';
+$string['contacts'] = 'Kontaktit';
+$string['deletemessagesdays'] = 'Tätä vanhemmat viestit poistetaan automaattisesti.';
+$string['discussion'] = 'Keskustelu';
+$string['emailmessages'] = 'Lähetä viesit sähköpostilla jos olen poissa sivultolta';
+$string['emailtagline'] = 'Tämä on kopio vististä sivustolla \"$a\"';
+$string['emptysearchstring'] = 'Sinun pitää määritellä jokin hakuehto';
+$string['formorethan'] = 'Useampi kuin';
+$string['includeblockedusers'] = 'sisällytä estetyt käyttäjät';
+$string['incomingcontacts'] = 'Tulevat kontaktit';
+$string['keywords'] = 'Avainsanat';
+$string['keywordssearchresults'] = 'Hakutulos: $a viestiä löytyi.';
+$string['maxmessages'] = 'Viestihistorian pituus';
+$string['message'] = 'Viesti';
+$string['messagehistory'] = 'Viestihistoria';
+$string['messages'] = 'Viestit';
+$string['messaging'] = 'Viestintä';
+$string['mycontacts'] = 'Kontaktini';
+$string['newsearch'] = 'Uusi haku';
+$string['nomessages'] = 'Ei odottavia viestejä';
+$string['nomessagesfound'] = 'Viestejä ei löytynyt';
+$string['nosearchresults'] = 'Haku ei tuttanut tuolosta';
+$string['offline'] = 'Offline';
+$string['offlinecontacts'] = 'Offline kontaktit';
+$string['online'] = 'Online';
+$string['onlinecontacts'] = 'Online kontaktit';
+$string['onlyfromme'] = 'Vain viestit minulta';
+$string['onlymycourses'] = 'Vain kursseillani';
+$string['onlytome'] = 'Vain viestit minulle';
+$string['pagerefreshes'] = 'Tämä sivu päivittyy automaattisesti $a sekunnin välein';
+$string['readmessages'] = '$a lue viestejä';
+$string['removecontact'] = 'Poista kontakti';
+$string['savemysettings'] = 'Tallenna asetukseni';
+$string['search'] = 'Haku';
+$string['searchforperson'] = 'Hae henkilöä';
+$string['searchmessages'] = 'Hae viestiä';
+$string['sendmessage'] = 'Lähetä viesti';
+$string['sendmessageto'] = 'Lähetä viesti $a:lle';
+$string['settings'] = 'Asetukset';
+$string['settingssaved'] = 'Asetuksesi on tallennettu';
+$string['showmessagewindow'] = 'Näytä viesti-ikkuna automaatisesti kun saan uuden viestin.';
+$string['timesent'] = 'Lähetysaika';
+$string['unblockcontact'] = 'Salli kontakti';
+$string['unreadmessages'] = '$a uutta viestiä';
+$string['userisblockingyou'] = 'Käyttäjä ei ota vastaan viestejä sinulta.';
+$string['userisblockingyounoncontact'] = 'Käyttäjä ottaa vastaan viestejä vain omilta kontakteiltaan ja sinä et ole listalla.';
+$string['userssearchresults'] = 'Hakutulos: $a käyttäjää löydetty';
+
+?>
