6bd20d73358241774c706a88d64f39dd96ae1ba0
moodler <moodler>
2005-05-02 16:02:09 +0000

    More tweaking.  Now it displays especially nice in Tiger.  ;-)

--- lib/rsslib.php
+++ lib/rsslib.php
@@ -209,7 +209,7 @@ function rss_add_items($items) {
             $result .= rss_full_tag('pubDate',3,false,date('r',$item->pubdate));
             //Include the author if exists 
             if (isset($item->author)) {
-                $item->description = get_string('byname','',$item->author).'. <p>'.$item->description.'</p>';
+                $item->description = get_string('byname','',$item->author).'. &nbsp;<p>'.$item->description.'</p>';
             }
             $result .= rss_full_tag('description',3,false,$item->description);
             $result .= rss_end_tag('item',2,true);
