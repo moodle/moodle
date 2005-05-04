caebd27db6f252de2a86ac30eb14b5fd73083266
mits <mits>
2005-05-04 08:15:10 +0000

    translated one string for enrol_paypal.php. and polished hankaku/zenkaku in Japanese, meaning is same.

--- lang/ja/enrol_paypal.php
+++ lang/ja/enrol_paypal.php
@@ -1,13 +1,11 @@
-<?php // $Id$ 
-      // enrol_paypal.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+<?PHP // $Id$ 
+      // enrol_paypal.php - created with Moodle 1.5 ALPHA (2005043000)
 
 
 $string['business'] = 'Paypalビジネスアカウントのメールアドレス';
+$string['costorkey'] = '下記の登録方法から1つを選択してください。';
 $string['description'] = 'Paypalモジュールでは有料のコースを設定することができます。コースの費用がゼロの場合、学生は支払いを求められません。サイト全体に適用されるデフォルトの費用をここで設定することも、個々に設定することもできます。コース費用は、サイト全体の費用に優先されます。';
 $string['enrolname'] = 'Paypal';
 $string['sendpaymentbutton'] = 'Paypalで支払う';
-$string['thischarset'] = 'EUC-JP';
-$string['thisdirection'] = 'ltr';
-$string['thislanguage'] = 'Japanese';
 
 ?>
