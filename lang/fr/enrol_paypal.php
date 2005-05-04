5fc0f044b32e9471d3ea709f2819cc015ba606ca
martignoni <martignoni>
2005-05-04 08:47:47 +0000

    When using the Paypal module, you can now also use enrolment keys

--- lang/fr/enrol_paypal.php
+++ lang/fr/enrol_paypal.php
@@ -1,8 +1,9 @@
 <?php // $Id$ 
 
-$string['enrolname'] = 'Inscription via Paypal';
-$string['description'] = 'Le module Paypal permet de mettre en place des cours payant. Si le coût d\'un cours est nul, les étudiants peuvent s\'y inscrire sans payer. Un coût défini globalement, que vous fixez ici, est le coût par défaut pour tous les cours du site. Chaque cours peut ensuite avoir un coût spécifique fixé individuellement. S\'il est défini, le coût spécifique d\'un cours remplace le coût par défaut.';
 $string['business'] = 'L\'adresse de courriel de votre compte Paypal';
+$string['costorkey'] = 'Veuillez choisir l\'une des méthodes d\'inscription suivantes.';
+$string['description'] = 'Le module Paypal permet de mettre en place des cours payant. Si le coût d\'un cours est nul, les étudiants peuvent s\'y inscrire sans payer. Un coût défini globalement, que vous fixez ici, est le coût par défaut pour tous les cours du site. Chaque cours peut ensuite avoir un coût spécifique fixé individuellement. S\'il est défini, le coût spécifique d\'un cours remplace le coût par défaut.';
+$string['enrolname'] = 'Inscription via Paypal';
 $string['sendpaymentbutton'] = 'Envoyer un paiement avec Paypal';
 
 ?>
