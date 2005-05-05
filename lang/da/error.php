cb1eafa490c16f820cb708836badb5e8a46ebb6b
gniske <gniske>
2005-05-05 16:07:12 +0000

    Fixed spelling errors

--- lang/da/error.php
+++ lang/da/error.php
@@ -3,7 +3,7 @@
 
 
 $string['adminprimarynoedit'] = 'Den primære admin kan ikke rettet af andre';
-$string['confirmsesskeybad'] = 'Desværre, men din session key kunne ikke godkendes i denne forbindelse. Denne sikkerhedskontrol forhindre  utilsigtet eller uautoriseret tilgang til vigtige funktioner for din profil. Er du sikker på at du virkelig vil udføre denne funktion.';
+$string['confirmsesskeybad'] = 'Desværre, men din session key kunne ikke godkendes i denne forbindelse. Denne sikkerhedskontrol forhindre utilsigtet eller uautoriseret tilgang til vigtige funktioner for din profil. Er du sikker på at du virkelig vil udføre denne funktion.';
 $string['coursegroupunknown'] = 'Kursuset der hører sammen med gruppe $a er ikke specificeret';
 $string['erroronline'] = 'Fejl på linje $a';
 $string['fieldrequired'] = '\"$a\" er et påkrævet felt';
@@ -15,9 +15,9 @@ $string['missingfield'] = 'Felt \"a\" mangler';
 $string['modulerequirementsnotmet'] = 'Modul \"$a->modulename\" ($a->moduleversion) kunne ikke installeres. Det kræver en nyere version af moodle (du bruger i øjenblikket $a->currentmoodle, du skal bruge $a->requiremoodle).';
 $string['notavailable'] = 'Dette er ikke tilgængeligt lige nu';
 $string['onlyeditown'] = 'Du kan kun rette i din egen information';
-$string['processingstops'] = 'Behandling stopper her. Efterfølgene poster ignoreres.';
+$string['processingstops'] = 'Behandling stopper her. Efterfølgende poster ignoreres.';
 $string['restricteduser'] = 'Desværre, men din nuværende profil \"$a\" tillader ikke at gøre det.';
-$string['sessionipnomatch'] = 'Desværre, dit IPnummer ser ud til at være ændret siden du loggede ind. Denne sikkerhedsfunktion bør beskytte mod at andre overtager din identitet på sitet mens du er logget på sitet. Brugere bør normalt ikke få denne fejl. Hvis du ikke er klar over hvad det skyldes så prøv at kontakte sitets administrator.';
+$string['sessionipnomatch'] = 'Desværre, dit IP-nummer ser ud til at være ændret siden du loggede ind. Denne sikkerhedsfunktion bør beskytte mod at andre overtager din identitet på sitet mens du er logget på sitet. Brugere bør normalt ikke få denne fejl. Hvis du ikke er klar over hvad det skyldes så prøv at kontakte sitets administrator.';
 $string['unknowncourse'] = 'Ukendt kursus med navnet \"$a\"';
 $string['usernotaddederror'] = 'Bruger \"$a\" ikke tilføjet - ukendt fejl!';
 $string['usernotaddedregistered'] = 'Bruger \"$a\" ikke tilføjet - registreret i forvejen.';
