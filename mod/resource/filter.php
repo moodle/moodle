9f7fa650a5a8c5254586b1c87b014ad5a0498abf
stronk7 <stronk7>
2005-05-06 18:15:21 +0000

    Now resource autolinking filter is optimised too,  so all the autolinking filters are using latest improvements. Only Bug 3138 remains open and is important. I've its solution at home and I'll post it in some hours. Just testing it against PHP4 and PHP5. (http://moodle.org/bugs/bug.php?op=show&bugid=3138)

--- mod/resource/filter.php
+++ mod/resource/filter.php
@@ -7,24 +7,36 @@
 
         global $CFG;
 
-    /// The resources are sorted from long to short so longer ones can be linked first.
+        static $nothingtodo;
+        static $resourcelist;
 
-        if ($resources = get_records('resource', 'course', $courseid, 'CHAR_LENGTH(name) DESC', 'id,name')) {
+        if (!empty($nothingtodo)) {   // We've been here in this page already
+            return $text;
+        }
 
-            $links = array();
+    /// Create a list of all the resources to search for.  It may be cached already.
+
+        if (empty($resourcelist)) {
+
+        /// The resources are sorted from long to short so longer ones can be linked first.
+
+            if (!$resources = get_records('resource', 'course', $courseid, 'CHAR_LENGTH(name) DESC', 'id,name')) {
+                $nothingtodo = true;
+                return $text;
+            }
+
+            $resourcelist = array();
 
             foreach ($resources as $resource) {
                 $currentname = trim($resource->name);
-                $links[] = new filterobject($currentname,
+                $resourcelist[] = new filterobject($currentname,
                         '<a class="resource autolink" title="'.strip_tags($currentname).'" href="'.
                          $CFG->wwwroot.'/mod/resource/view.php?r='.$resource->id.'">', 
                          '</a>', false, true);
             }
 
-            $text = filter_phrases($text, $links);  // Look for all these links in the text
         }
-
-        return $text;
+        return  filter_phrases($text, $resourcelist);  // Look for all these links in the text
     }
 
 ?>
