d386fabb05f5fca442cd85b0ce4ad1e738abf009
moodler <moodler>
2005-05-07 13:33:36 +0000

    Releasing 1.5 very shortly

--- version.php
+++ version.php
@@ -6,10 +6,10 @@
 // This is compared against the values stored in the database to determine 
 // whether upgrades should be performed (see lib/db/*.php)
 
-   $version = 2004083140;  // YYYYMMDD   = date of first major branch release 1.4
+   $version = 2004083150;  // YYYYMMDD   = date of first major branch release 1.4
                            //         X  = point release version 1,2,3 etc
                            //          Y = increments between point releases
 
-   $release = '1.4.4 +';     // Human-friendly version name
+   $release = '1.4.5';     // Human-friendly version name
 
 ?>
