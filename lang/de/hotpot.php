1d30c89ed02cef71f736c804f4661937149106ad
ralf-bonn <ralf-bonn>
2005-05-08 17:22:24 +0000

    *** empty log message ***

--- lang/de/hotpot.php
+++ lang/de/hotpot.php
@@ -1,16 +1,78 @@
 <?PHP // $Id$ 
-      // hotpot.php - created with Moodle 1.4.1+ (2004083110)
+      // quiz.php - created with Moodle 1.2 development (2003111400)
 
+//translators:  You might want to leave the first two items 'as is' in English
+$string['modulename'] = 'Hot Potatoes Test';
+$string['modulenameplural'] = 'Hot Potatoes Tests';
 
-$string['clearall'] = 'Zurückstellen';
-$string['deleteall'] = 'Alle löschen';
-$string['modulename'] = 'Hot Potatoe Test';
-$string['modulenameplural'] = 'Hot Potatoe Tests';
-$string['parentlanguage'] = 'en';
-$string['really'] = 'Wollen Sie wirklich alle Ergebnisses dieses Tests löschen?';
-$string['strattemptlabel'] = 'Versuche: Punkte, Datum, Zeitdauer';
-$string['thischarset'] = 'ISO-8859-1';
-$string['thisdirection'] = 'ltr';
-$string['thislanguage'] = 'ISO-8859-1';
+// for mod.html
+$string['outputformat'] = 'Ausgabeformat';
+$string['outputformat_best'] = 'beste';
+$string['outputformat_v6_plus'] = 'v6+';
+$string['outputformat_v6'] = 'v6';
+$string['outputformat_v5'] = 'v5';
+$string['outputformat_v4'] = 'v4';
+$string['outputformat_v3'] = 'v3';
+$string['outputformat_flash'] = 'Flash';
+$string['outputformat_mobile'] = 'mobile';
+
+$string['navigation'] = 'Navigation';
+$string['navigation_bar'] = 'Moodle Navigationsbalken';
+$string['navigation_frame'] = 'Moodle Navigationrahmen';
+$string['navigation_iframe'] = 'Embedded &lt;IFRAME&gt;';
+$string['navigation_buttons'] = 'Hot Potatoes Test-Schaltfläche';
+$string['navigation_give_up'] = 'Nur eine &quot;Abgeben&quot; Schaltfläche';
+$string['navigation_none'] = 'Keine';
+
+$string['giveup'] = 'Abgeben';
+$string['location'] = 'Dateistandort';
+$string['shownextquiz'] = 'Den nächsten Test anzeigen';
+$string['forceplugins'] = "Benutze Media-Plugins";
+
+$string['resultssaved'] = 'Testergebnisse werden gespeichert';
+
+// for edit.php and show.php
+$string['filetype'] = 'Dateityp';
+$string['quiztype'] = 'Testart';
+$string['showxmlsource'] = 'XML-Quelltext';
+$string['showxmltree'] = 'XML-Verzeichnisbaum';
+$string['showhtmlsource'] = 'HTML-Quelltext';
+$string['enterafilename'] = 'Bitte den Dateinamen eingeben';
+
+// lib.php
+$string['noactivity'] = 'Keine Aktivität';
+$string['inprogress'] = 'in Bearbeitung';
+$string['abandoned'] = 'nichts';
+
+$string['correct'] = 'Richtig';
+$string['ignored'] = 'Nicht berücksichtigt';
+$string['wrong'] = 'Falsch';
+$string['score'] = 'Punktzahl';
+$string['weighting'] = 'Gewichtung';
+$string['hints'] = 'Hinweise';
+$string['clues'] = 'Lösungen';
+$string['checks'] = 'Überprüfungen';
+$string['penalties'] = 'Strafen';
+
+// report.php
+$string['deletedattempts'] = '$a Versuche wurden gelöscht';
+$string['deletedresponses'] = '$a Antworten wurden gelöscht';
+$string['selectattempt'] = 'Bitte einen Versuch auswählen';
+
+$string['thiscourse'] = 'Diesen Kurs';
+$string['allmycourses'] = 'Alle meine Kurse';
+$string['bestattempt'] = 'Den besten Versuch';
+$string['allattempts'] = 'Alle Versuche';
+
+// report/*/report.php
+$string['average'] = 'Durchschnitt';
+$string['questionshort'] = 'Q-$a';
+
+// report/overview/report.php
+$string['deleteabandoned'] = 'Lösche Versuche';
+$string['deleteabandonedcheck'] = 'Möchten Sie wirklich alle $a Versuche löschen?';
+
+// review.php
+$string['noresponses'] = 'Es wurden keine Angaben zu Fragen oder Antworten gefunden.';
 
 ?>
