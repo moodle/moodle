9413bb375e99d751848dcaa110f0ba50922bdd9c
koenr <koenr>
2005-05-08 14:36:15 +0000

    removed some garbage

--- lang/nl/censor.php
+++ lang/nl/censor.php
@@ -1,10 +1,7 @@
 <?PHP // $Id$ 
-      // censor.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+      // censor.php - created with Moodle 1.5 ALPHA (2005050600)
 
 
 $string['filtername'] = 'Woordcensuur';
-$string['thischarset'] = 'iso-8859-1';
-$string['thisdirection'] = 'ltr';
-$string['thislanguage'] = 'Nederlands';
 
 ?>
