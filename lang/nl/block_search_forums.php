d6f993a3edbecb9b1423fc3b6fa78257fa3dded1
koenr <koenr>
2005-05-08 21:29:56 +0000

    new file

---
+++ lang/nl/block_search_forums.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // block_search_forums.php - created with Moodle 1.5 ALPHA (2005050600)
+
+
+$string['advancedsearch'] = 'Geavanceerd zoeken';
+$string['blocktitle'] = 'Zoeken in forums';
+
+?>
