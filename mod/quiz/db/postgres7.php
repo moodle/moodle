124dabc799f9109e0cf77b953482d5195ed4b804
mjollnir_ <mjollnir_>
2005-05-08 21:28:37 +0000

    Updating postgres.php to match mysql.php with max-min/2 rather than min+max/2

--- mod/quiz/db/postgres7.php
+++ mod/quiz/db/postgres7.php
@@ -498,7 +498,7 @@ function quiz_upgrade($oldversion) {
       
         /// Use tolerance instead of min and max in numerical question type
         table_column('quiz_numerical', '', 'tolerance', 'varchar', '255', '', '0.0', 'not null', 'question');
-        execute_sql("UPDATE {$CFG->prefix}quiz_numerical SET tolerance = (min::text::real+max::text::real)/2");
+        execute_sql("UPDATE {$CFG->prefix}quiz_numerical SET tolerance = (max::text::real-min::text::real)/2");
         modify_database('', 'ALTER TABLE prefix_quiz_numerical DROP COLUMN min'); // Replaced by tolerance
         modify_database('', 'ALTER TABLE prefix_quiz_numerical DROP COLUMN max'); // Replaced by tolerance
       
