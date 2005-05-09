f3c3028c0b7f4665fe22fdbb2947466c20648133
moodler <moodler>
2005-05-09 18:28:33 +0000

    Language file for Authroize.net plugin

---
+++ lang/en/enrol_authorize.php
@@ -0,0 +1,19 @@
+<?PHP // $Id$ 
+      // enrol_authorize.php - created with Moodle 1.5 ALPHA (2005042400)
+
+
+$string['anlogin'] = 'Authorize.net: Login name';
+$string['anpassword'] = 'Authorize.net: Password (not required)';
+$string['antestmode'] = 'Authorize.net: Test transactions';
+$string['antrankey'] = 'Authorize.net: Transaction Key';
+$string['ccexpire'] = 'Expiry Date';
+$string['ccno'] = 'Credit Card Number';
+$string['ccvv'] = 'CV2';
+$string['ccvvhelp'] = 'Look at the back of card (last 3 digits)';
+$string['description'] = 'The Authorize.net module allows you to set up paid courses via CC providers.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
+$string['enrolname'] = 'Authorize.net Credit Card Gateway';
+$string['nameoncard'] = 'Name on card';
+$string['sendpaymentbutton'] = 'Send Payment';
+$string['zipcode'] = 'Zip code';
+
+?>
