4fd5abaa744438806f280b95315198acc370f8aa
koenr <koenr>
2005-05-10 22:38:13 +0000

    New strings/helpfiles for questionnaire. As I recall right, they could be added to the lang folder instead of to the contrib ??

--- lang/nl/questionnaire.php
+++ lang/nl/questionnaire.php
@@ -1,19 +1,51 @@
 <?PHP // $Id$ 
-      // questionnaire.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+      // questionnaire.php - created with Moodle 1.5 ALPHA (2005050600)
 
 
 $string['alreadyfilled'] = 'Deze enquête heb je voor ons al ingevuld. Bedankt!';
+$string['closed'] = 'De enquête is gesloten op $a. Bedankt.';
+$string['closedate'] = 'Gebruik een einddatum';
+$string['createcontent'] = 'Maak nieuwe inhoud';
+$string['createnew'] = 'Maak nieuw';
+$string['guestsno'] = 'Sorry, gasten kunnen enquêtes niet invullen';
+$string['inuse'] = 'In gebruik';
 $string['modulename'] = 'Enquête';
 $string['modulenameplural'] = 'Enquêtes';
-$string['mustcomplete'] = '<b>Je moet deze enquête <i>nu</i> verder afwerken om je resultaten te kunnen opslaan. Je mag ze niet verder afwerken op een ander moment.</b><br /><br />';
+$string['mustcomplete'] = '<b>Je moet deze enquête <i>nu</i> verder afwerken om je resultaten te kunnen opslaan. Je mag ze niet verder afwerken op een ander moment.</b><br><br>';
+$string['noneinuse'] = 'Er zijn nog geen vragen';
+$string['noresponses'] = 'Geen antwoorden';
 $string['notavail'] = 'Deze enquête is nog niet beschikbaar. Probeer later nog eens.';
+$string['noteligible'] = 'Het is voor jou niet toegelaten om deze enquête in te vullen';
+$string['notopen'] = 'Deze enquête zal pas openen op $a.';
+$string['notused'] = 'Niet gebruikt';
+$string['opendate'] = 'Gebruik een startdatum';
+$string['private'] = 'Privé';
+$string['public'] = 'Publiek';
 $string['qmanage'] = 'Beheer onderzoeken';
 $string['qmanagetitle'] = 'Onderzoeksbeheer phpESP';
 $string['qtype'] = 'Type';
-$string['questionnaireid'] = 'Onderzoek';
-$string['respondenttype'] = 'Type ondervraagde';
-$string['thischarset'] = 'iso-8859-1';
-$string['thisdirection'] = 'ltr';
-$string['thislanguage'] = 'Nederlands';
+$string['qtypeonce'] = 'Slechts één keer insturen toegelaten';
+$string['qtypeunlimited'] = 'Meerdere keren insturen toegelaten';
+$string['questionnaireid'] = 'Enquête-vragen';
+$string['realm'] = 'Enquête-type';
+$string['respondenteligible'] = 'Respondenten toegelaten';
+$string['respondenteligibleall'] = 'alle';
+$string['respondenteligiblestudents'] = 'enkel leerlingen';
+$string['respondenteligibleteachers'] = 'enkel leraren';
+$string['respondents'] = 'Respondenten';
+$string['respondenttype'] = 'Respondent-type';
+$string['respondenttypeanonymous'] = 'anoniem';
+$string['respondenttypefullname'] = 'Volledige naam';
+$string['resume'] = 'Enquête bewaren en onderbreken';
+$string['selectexisting'] = 'of kies een bestaande';
+$string['submitsurvey'] = 'Stuur enquête in';
+$string['template'] = 'Sjabloon';
+$string['templatenotviewable'] = 'Enquêtesjablonen zijn niet zichtbaar.';
+$string['type'] = 'Type';
+$string['unknown'] = 'Onbekend';
+$string['usepublic'] = 'Gebruik publiek';
+$string['usetemplate'] = 'Kopiëer bestaande';
+$string['usingsurvey'] = '$a aan het gebruiken.';
+$string['viewresponses'] = 'Bekijk $a antwoorden';
 
 ?>
