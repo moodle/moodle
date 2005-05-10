74528955bd6ee48bba96de2f7e7fd476b02cc3b9
moodler <moodler>
2005-05-10 05:39:32 +0000

    Make the RSS blocks wider (210)

--- blocks/rss_client/block_rss_client.php
+++ blocks/rss_client/block_rss_client.php
@@ -11,6 +11,10 @@ class block_rss_client extends block_base {
         $this->version = 2004112000;
     }
 
+    function preferred_width() {
+        return 210;
+    }
+
     function specialization() {
         // After the block has been loaded we customize the block's title display
         if (!empty($this->config) && !empty($this->config->title)) {
