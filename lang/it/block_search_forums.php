25709e92e349f99009fe747b512e93c57f59a669
bobopinna <bobopinna>
2005-05-13 15:32:41 +0000

    First translation

---
+++ lang/it/block_search_forums.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // block_search_forums.php - created with Moodle 1.5 ALPHA (2005050600)
+
+
+$string['advancedsearch'] = 'Ricerca avanzata';
+$string['blocktitle'] = 'Ricerca nei forum';
+
+?>
