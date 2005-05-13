fa4465f2bf43b583410aa13a53bd41e4c8c41139
ethem <ethem>
2005-05-13 12:04:53 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) XHTML compliant

--- lang/tr/enrol_authorize.php
+++ lang/tr/enrol_authorize.php
@@ -4,12 +4,16 @@
 
 $string['anlogin'] = 'Authorize.net: Kullanýcý adý';
 $string['anpassword'] = 'Authorize.net: Þifre (gerekli deðil)';
+$string['anreferer'] = 'Gerekliyse geliþ yeri URL\'sini belirtin. Bu, web isteðinde \"Referer: URL\" baþlýðýný gönderir';
 $string['antestmode'] = 'Authorize.net: Ýþlemleri test et';
 $string['antrankey'] = 'Authorize.net: Ýþlem Anahtarý (Transaction Key)';
 $string['ccexpire'] = 'Geçerlilik Tarihi';
+$string['ccinvalid'] = 'Geçersiz kart numarasý';
 $string['ccno'] = 'Kredi Kartý No';
+$string['cctype'] = 'Kredi Kartý Tipi';
 $string['ccvv'] = 'CV2';
 $string['ccvvhelp'] = 'Kartýn arkasýna bakýnýz (son 3 rakam)';
+$string['description'] = 'Authorize.net modülü Kredi Kartý saðlayýcýlarýyla ücretli kurslar ayarlamýnýza olanak verir. Bir kursun ücreti sýfýr ise öðrencilere ödeme yapmalarý için bir istekte bulunulmaz. Sitenin genelinde ayarlayacaðýnýz varsayýlan bir deðer vardýr ve her bir dersin ücretini tek tek ayarlayabilirsiniz. Kurs ücreti ayarlanýrsa site genelindeki ücret yoksayýlýr.';
 $string['enrolname'] = 'Authorize.net Kredi Kartý Saðlayýcýsý';
 $string['nameoncard'] = 'Kart üzerindeki isim';
 $string['sendpaymentbutton'] = 'Ödemeyi Yap';
