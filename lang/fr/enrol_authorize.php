22524f5318e4646eb0e4ba64fb324db67d41eba1
martignoni <martignoni>
2005-05-13 19:14:00 +0000

    New strings for authorize.net plugin.

--- lang/fr/enrol_authorize.php
+++ lang/fr/enrol_authorize.php
@@ -2,11 +2,14 @@
 
 $string['anlogin'] = 'Authorize.net&nbsp;: nom d\'utilisateur';
 $string['anpassword'] = 'Authorize.net&nbsp;: mot de passe (facultatif)';
+$string['anreferer'] = 'Fixer un URL référenceur au besoin. Ceci enverra une entête «&nbsp;Referer: URL&nbsp;» dans la requête web';
 $string['antestmode'] = 'Authorize.net&nbsp;: tester les transactions';
 $string['antrankey'] = 'Authorize.net&nbsp;: clef de transaction';
 $string['ccexpire'] = 'Date d\'échéance';
+$string['ccinvalid'] = 'Numéro de carte non valable';
 $string['ccno'] = 'Numéro de carte de crédit';
-$string['ccvv'] = 'CV2';
+$string['cctype'] = 'Type de carte de crédit';
+$string['ccvv'] = 'Code vérification';
 $string['ccvvhelp'] = 'Au verso de votre carte (les 3 derniers chiffres)';
 $string['description'] = 'Le module Authorize.net permet de mettre en place des cours payant. Si le coût d\'un cours est nul, les étudiants peuvent s\'y inscrire sans payer. Un coût défini globalement, que vous fixez ici, est le coût par défaut pour tous les cours du site. Chaque cours peut ensuite avoir un coût spécifique fixé individuellement. S\'il est défini, le coût spécifique d\'un cours remplace le coût par défaut.';
 $string['enrolname'] = 'Paiement par carte de crédit Authorize.net';
