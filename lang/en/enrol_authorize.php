b817ed450337178e5b4fbe4fd5491e52f5374bc2
ethem <ethem>
2005-05-13 09:42:00 +0000

    new strings for authorize.net plugin.

--- lang/en/enrol_authorize.php
+++ lang/en/enrol_authorize.php
@@ -4,10 +4,13 @@
 
 $string['anlogin'] = 'Authorize.net: Login name';
 $string['anpassword'] = 'Authorize.net: Password (not required)';
+$string['anreferer'] = 'Set up referer URL if required. This sends Rererer: URL header in web request';
 $string['antestmode'] = 'Authorize.net: Test transactions';
 $string['antrankey'] = 'Authorize.net: Transaction Key';
 $string['ccexpire'] = 'Expiry Date';
+$string['ccinvalid'] = 'Invalid card number';
 $string['ccno'] = 'Credit Card Number';
+$string['cctype'] = 'Credit Card Type';
 $string['ccvv'] = 'CV2';
 $string['ccvvhelp'] = 'Look at the back of card (last 3 digits)';
 $string['description'] = 'The Authorize.net module allows you to set up paid courses via CC providers.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
