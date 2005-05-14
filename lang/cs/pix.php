236081c39215924a7d15ad80c3350f78e89fc9ef
miksik <miksik>
2005-05-14 22:07:12 +0000

    Some subtle changes for 1.5

--- lang/cs/pix.php
+++ lang/cs/pix.php
@@ -1,25 +1,23 @@
-<?PHP // $Id$ 
-      // pix.php - created with Moodle 1.2 development (2003111400)
-
+<?PHP
 
 $string['angry'] = 'zlobit se';
 $string['approve'] = 'souhlasit';
 $string['biggrin'] = 'zubit se';
 $string['blackeye'] = 'monokl';
-$string['blush'] = 'èervenání';
+$string['blush'] = 'èervenat se';
 $string['clown'] = 'klaun';
 $string['cool'] = 'v pohodì';
 $string['dead'] = 'bez ¾ivota';
 $string['evil'] = '¹kodolibost';
-$string['kiss'] = 'pusa';
+$string['kiss'] = 'posílám pusu';
 $string['mixed'] = 'smí¹ený pocit';
-$string['sad'] = 'mrzutost';
+$string['sad'] = 'smutek';
 $string['shy'] = 'plachost';
 $string['sleepy'] = 'ospalost';
-$string['smiley'] = 'usmívat se';
+$string['smiley'] = 'úsmìv';
 $string['surprise'] = 'pøekvapení';
 $string['thoughtful'] = 'zamy¹lený';
-$string['tongueout'] = 'vyplazený jazyk';
+$string['tongueout'] = 'vypláznout jazyk';
 $string['wideeyes'] = 'kulit oèi';
 $string['wink'] = 'mrknout';
 
