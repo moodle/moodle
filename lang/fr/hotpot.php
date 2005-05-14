973c07eb3179164cd008572435472fbb9ede4854
martignoni <martignoni>
2005-05-14 17:09:02 +0000

    Adding help files for hotpot module

--- lang/fr/hotpot.php
+++ lang/fr/hotpot.php
@@ -4,20 +4,20 @@ $string['modulename'] = 'Test Hot Potatoes';
 $string['modulenameplural'] = 'Tests Hot Potatoes';
 
 // for mod.html
-$string['outputformat'] = 'Format d\'exportation';
-$string['outputformat_best'] = 'meilleur';
+$string['outputformat'] = 'Format d\'affichage';
+$string['outputformat_best'] = 'Meilleur';
 $string['outputformat_v6_plus'] = 'v6+';
 $string['outputformat_v6'] = 'v6';
 $string['outputformat_v5'] = 'v5';
 $string['outputformat_v4'] = 'v4';
 $string['outputformat_v3'] = 'v3';
 $string['outputformat_flash'] = 'Flash';
-$string['outputformat_mobile'] = 'mobile';
+$string['outputformat_mobile'] = 'Mobile';
   	 
 $string['navigation'] = 'Navigation';
 $string['navigation_bar'] = 'Barre de navigation Moodle';
 $string['navigation_frame'] = 'Cadre de navigation Moodle';
-$string['navigation_iframe'] = '&lt;IFRAME&gt; inclus';
+$string['navigation_iframe'] = 'Inclus dans un &lt;IFRAME&gt;';
 $string['navigation_buttons'] = 'Boutons des tests Hot Potatoes';
 $string['navigation_give_up'] = 'Bouton «&nbsp;Abandonner&nbsp;» uniquement';
 $string['navigation_none'] = 'Aucune';
@@ -25,7 +25,7 @@ $string['navigation_none'] = 'Aucune';
 $string['giveup'] = 'Abandonner';
 $string['location'] = 'Emplacement du fichier';
 $string['shownextquiz'] = 'Afficher test suivant';
-$string['forceplugins'] = 'Imposer les plugins de médias';
+$string['forceplugins'] = 'Lecteurs multimédias compatibles';
   	 
 $string['resultssaved'] = 'Les résultats du test ont été enregistrés';
   	 
@@ -39,8 +39,8 @@ $string['enterafilename'] = 'Veuillez saisir un nom de fichier';
   	 
   	 // lib.php
 $string['noactivity'] = 'Aucune activité';
-$string['inprogress'] = 'en cours';
-$string['abandoned'] = 'abandonné';
+$string['inprogress'] = 'En cours';
+$string['abandoned'] = 'Abandonné';
   	 
 $string['correct'] = 'Correct';
 $string['ignored'] = 'Ignoré';
@@ -57,10 +57,10 @@ $string['deletedattempts'] = '$a tentatives ont été supprimées';
 $string['deletedresponses'] = '$a réponses ont été supprimées';
 $string['selectattempt'] = 'Veuillez choisir une tentative';
   	 
-$string['thiscourse'] = 'ce cours';
-$string['allmycourses'] = 'tous mes cours';
-$string['bestattempt'] = 'meilleure tentative';
-$string['allattempts'] = 'toutes les tentatives';
+$string['thiscourse'] = 'Ce cours';
+$string['allmycourses'] = 'Tous mes cours';
+$string['bestattempt'] = 'Meilleure tentative';
+$string['allattempts'] = 'Toutes les tentatives';
   	 
   	 // report/*/report.php
 $string['average'] = 'Moyenne';
