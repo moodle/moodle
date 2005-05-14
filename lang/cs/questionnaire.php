400af450c36377acc1c700f4342d8a4683a63ecc
miksik <miksik>
2005-05-14 21:47:40 +0000

    Translation of the latest language file from contrib

--- lang/cs/questionnaire.php
+++ lang/cs/questionnaire.php
@@ -1,16 +1,50 @@
-<?PHP // $Id$ 
-      // questionnaire.php - created with Moodle 1.4 (2004083100)
+<?PHP
 
-
-$string['alreadyfilled'] = 'Ji¾ jste nám vyplnili tento dotazník. Dìkujeme.';
+$string['alreadyfilled'] = 'Tento dotazník jste ji¾ vyplnili. Dìkujeme.';
+$string['closed'] = 'Tento dotazník byl ji¾ uzavøen (datum uzavøení: $a). Dìkujeme.';
+$string['closedate'] = 'Pou¾ít datum pro uzavøení';
+$string['createcontent'] = 'Definovat obsah dotazníku';
+$string['createnew'] = 'Vytvoøit nový';
+$string['guestsno'] = 'Litujeme, hosté nemohou vyplòovat dotazníky.';
+$string['inuse'] = 'Pou¾ívá se';
 $string['modulename'] = 'Dotazník';
 $string['modulenameplural'] = 'Dotazníky';
-$string['mustcomplete'] = '<b>Chcete-li vyplnit tento dotazník, musíte tak uèinit <i>nyní</i>. Pozdìji ji¾ nebudou moci být va¹e odpovìdi zaznamenány.</b><br /><br />';
-$string['notavail'] = 'Tento dotazník dosud není dostupný. Zkuste to, prosím, pozdìji.';
+$string['mustcomplete'] = "<b>Mají-li být va¹e odpovìdi zaznamenány, musíte dokonèit vyplòování dotazníku <i>nyní</i>. "
+                         ."Pozdìj¹í dokonèení dotazníku není mo¾né.</b><br /><br />";
+$string['noneinuse'] = '®ádný obsah není k dispozici.';
+$string['noresponses'] = '®ádné odpovìdi';
+$string['notavail'] = 'Tento dotazník je¹tì nebyl zpøístupnìn. Zkuste to prosím pozdìji.';
+$string['noteligible'] = 'Pro vyplnìní tohoto dotazníku namáte potøebná oprávnìní.';
+$string['notopen'] = 'Tento dotazník dosud nebyl zpøístupnìn. Datum zpøístupnìní dotazníku: $a.';
+$string['notused'] = 'Nepou¾ívá se';
+$string['opendate'] = 'Pou¾ít datum pro zpøístupnìní';
+$string['private'] = 'soukromý';
+$string['public'] = 'veøejný';
 $string['qmanage'] = 'Správa dotazníkù';
 $string['qmanagetitle'] = 'Správa dotazníku phpESP';
-$string['qtype'] = 'Typ';
-$string['questionnaireid'] = 'Dotaznik';
-$string['respondenttype'] = 'Typ respondenta';
+$string['qtype'] = 'Zpùsob vyplnìní';
+$string['qtypeonce'] = 'vyplnit jednou';
+$string['qtypeunlimited'] = 'vyplnit opakovanì';
+$string['questionnaireid'] = 'Obsah dotazníku';
+$string['realm']= 'Typ dotazníku';
+$string['respondenteligible'] = 'Oprávnìní respondenti';
+$string['respondenteligibleall'] = 'studenti i uèitelé';
+$string['respondenteligiblestudents'] = 'pouze studenti';
+$string['respondenteligibleteachers'] = 'pouze uèitelé';
+$string['respondents'] = 'Respondentî';
+$string['respondenttype'] = 'Identifikace respondentù';
+$string['respondenttypeanonymous'] = 'anonymní';
+$string['respondenttypefullname'] = 'celé jméno';
+$string['resume'] = 'Povolit pøedbì¾nì ulo¾it odpovìdi';
+$string['selectexisting'] = 'nebo vybrat existující';
+$string['submitsurvey'] = 'Odeslat dotazník';
+$string['template'] = '¹ablona';
+$string['templatenotviewable'] = '©ablony dotazníku nelze prohlí¾et.';
+$string['type'] = 'Typ';
+$string['unknown'] = 'Neznámý';
+$string['usepublic'] = 'Pou¾ít veøejný';
+$string['usetemplate'] = 'Zkopírovat existující';
+$string['usingsurvey'] = 'pou¾ívá se $a.';
+$string['viewresponses'] = 'Prohlédnout odpovìdi ($a)';
 
 ?>
