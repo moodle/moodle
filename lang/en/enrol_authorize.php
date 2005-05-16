7228053025ec001b08c2cc1e44cc23546cb8d48d
ethem <ethem>
2005-05-16 07:22:44 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com)

--- lang/en/enrol_authorize.php
+++ lang/en/enrol_authorize.php
@@ -1,10 +1,10 @@
 <?PHP // $Id$ 
-      // enrol_authorize.php - created with Moodle 1.5 ALPHA (2005042400)
+      // enrol_authorize.php - created with Moodle 1.5 ALPHA (2005043000)
 
 
 $string['anlogin'] = 'Authorize.net: Login name';
 $string['anpassword'] = 'Authorize.net: Password (not required)';
-$string['anreferer'] = 'Set up referer URL if required. This sends Rererer: URL header in web request';
+$string['anreferer'] = 'Type here URL referer if you set up this in your authorize.net account. This will send a heading \"Referer: URL\" in the web request.';
 $string['antestmode'] = 'Authorize.net: Test transactions';
 $string['antrankey'] = 'Authorize.net: Transaction Key';
 $string['ccexpire'] = 'Expiry Date';
@@ -15,8 +15,8 @@ $string['ccvv'] = 'CV2';
 $string['ccvvhelp'] = 'Look at the back of card (last 3 digits)';
 $string['description'] = 'The Authorize.net module allows you to set up paid courses via CC providers.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
 $string['enrolname'] = 'Authorize.net Credit Card Gateway';
-$string['nameoncard'] = 'Name on card';
+$string['nameoncard'] = 'Name on Card';
 $string['sendpaymentbutton'] = 'Send Payment';
-$string['zipcode'] = 'Zip code';
+$string['zipcode'] = 'Zip Code';
 
 ?>
