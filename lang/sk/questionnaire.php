d9e722a94f57d0a02e215651d768b7e524d6c5f1
havran <havran>
2005-05-16 12:05:20 +0000

    new strings from Martina Majorova & Juraj Chlebec

---
+++ lang/sk/questionnaire.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // questionnaire.php - created with Moodle 1.5 ALPHA (2005051500)
+
+
+$string['alreadyfilled'] = 'U¾ ste nám vyplnili tento dotazník. Ïakujeme.';
+$string['modulename'] = 'Dotazník';
+$string['modulenameplural'] = 'Dotazníky';
+$string['mustcomplete'] = '<b>Ak chcete vyplni» tento dotazník, musíte tak urobi» <i>teraz</i>. Neskôr u¾ nebude mo¾né Va¹e odpovede zaznamena».</b><br /><br />';
+$string['notavail'] = 'Tento dotazník dosiaµ nie je dostupný. Skúste to, prosím, neskôr.';
+$string['qmanage'] = 'Správa dotazníkov';
+$string['qmanagetitle'] = 'Správa dotazníka phpESP';
+$string['qtype'] = 'Typ';
+$string['questionnaireid'] = 'Dotazník';
+$string['respondenttype'] = 'Typ respondenta';
+
+?>
