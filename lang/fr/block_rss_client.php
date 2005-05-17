d541556836a99c7463bc0d8c19179d23392f2d71
martignoni <martignoni>
2005-05-17 21:35:57 +0000

    Sorting and cleanup

--- lang/fr/block_rss_client.php
+++ lang/fr/block_rss_client.php
@@ -1,45 +1,40 @@
 <?php // $Id$ 
 
-$string['feedstitle'] = 'Fils RSS distants';
-$string['feedsaddedit'] = 'Ajouter/modifier des fils RSS';
-$string['remotenewsfeed'] = 'Fils de nouvelles RSS distants';
-$string['feedupdated'] = 'Fil RSS mis à jour';
-$string['feedadded'] = 'Fil RSS ajouté';
-$string['updatefeed'] = 'Modifier l\'URL d\'un fil URL : ';
 $string['addfeed'] = 'Ajouter l\'URL d\'un fil URL : ';
-$string['feeddeleted'] = 'Fil RSS supprimé';
-
-$string['clientnumentries'] = 'Nombre par défaut d\'articles RSS à afficher par bloc.';
-$string['submitters'] = 'Qui aura l\'autorisation de définir de nouveaux fils RSS&nbsp;? Les fils RSS définis sont disponibles sur toutes les pages de votre site.';
+$string['addheadlineblock'] = 'Ajouter un bloc manchette RSS';
+$string['addnew'] = 'Ajouter nouveau';
 $string['choosefeedlabel'] = 'Choisir les fils RSS que vous désirez rendre disponibles dans ce bloc&nbsp;:';
+$string['clientchannellink'] = 'Site d\'origine...';
+$string['clientnumentries'] = 'Nombre par défaut d\'articles RSS à afficher par bloc.';
 $string['clientshowchannellinklabel'] = 'Un lien vers le site original doit-il être  affiché&nbsp;? (Si aucun lien n\'est fourni dans le fil, aucun lien ne sera affiché)&nbsp;:';
 $string['clientshowimagelabel'] = 'Afficher l\'image du fil si disponible&nbsp;:';
-$string['clientchannellink'] = 'Site d\'origine...';
-$string['customtitlelabel'] = 'Titre personnalisé (laisser vide pour utiliser le titre fourni dans le fil)&nbsp;:';
-$string['validatefeed'] = 'Valider le fil';
-$string['nofeeds'] = 'Il n\'y a pas de fil RSS défini sur ce site.';
-$string['editnewsfeeds'] = 'Modifier les fils de nouvelles RSS';
-$string['shownumentrieslabel'] = 'Nombre maximum d\'articles à afficher par bloc.';
-$string['displaydescriptionlabel'] = 'Afficher la description de chaque lien&nbsp;?';
-
+$string['configblock'] = 'Configurer ce bloc';
 $string['couldnotfindfeed'] = 'Impossible de trouver le fil RSS d\'identifiant';
-$string['feed'] = 'Fil RSS';
-$string['findmorefeeds'] = 'Rechercher d\'autres fils RSS';
-
+$string['customtitlelabel'] = 'Titre personnalisé (laisser vide pour utiliser le titre fourni dans le fil)&nbsp;:';
 $string['deletefeedconfirm'] = 'Voulez-vous vraiment supprimer ce fil RSS&nbsp;?';
+$string['displaydescriptionlabel'] = 'Afficher la description de chaque lien&nbsp;?';
+$string['editfeeds'] = 'Modifier, s\'abonner ou se désabonner d\'un fil RSS/';
+$string['editnewsfeeds'] = 'Modifier les fils de nouvelles RSS';
+$string['editrssblock'] = 'Modifier le bloc manchette RSS';
+$string['feed'] = 'Fil RSS';
+$string['feedadded'] = 'Fil RSS ajouté';
+$string['feeddeleted'] = 'Fil RSS supprimé';
+$string['feeds'] = 'Fils RSS';
+$string['feedsaddedit'] = 'Ajouter/modifier des fils RSS';
+$string['feedsconfigurenewinstance'] = 'Cliquer ici pour afficher dans ce bloc des fils RSS';
+$string['feedstitle'] = 'Fils RSS distants';
+$string['feedupdated'] = 'Fil RSS mis à jour';
+$string['findmorefeeds'] = 'Rechercher d\'autres fils RSS';
+$string['managefeeds'] = 'Gérer tous mes fils RSS';
+$string['nofeeds'] = 'Il n\'y a pas de fil RSS défini sur ce site.';
+$string['pickfeed'] = 'Choisir un fil RSS';
+$string['remotenewsfeed'] = 'Fils de nouvelles RSS distants';
+$string['seeallfeeds'] = 'Afficher tous les fils RSS';
+$string['shownumentrieslabel'] = 'Nombre maximum d\'articles à afficher par bloc.';
+$string['submitters'] = 'Qui aura l\'autorisation de définir de nouveaux fils RSS&nbsp;? Les fils RSS définis sont disponibles sur toutes les pages de votre site.';
 $string['timeout'] = 'blockrsstimeout';
 $string['timeoutdesc'] = 'Durée de vie en cache d\'un fil RSS (en minutes).';
-$string['addheadlineblock'] = 'Ajouter un bloc manchette RSS';
-$string['pickfeed'] = 'Choisir un fil RSS';
-$string['feeds'] = 'Fils RSS';
-$string['feed'] = 'Fil RSS';
-$string['editfeeds'] = 'Modifier, s\'abonner ou se désabonner d\'un fil RSS/';
-$string['seeallfeeds'] = 'Afficher tous les fils RSS';
-$string['editrssblock'] = 'Modifier le bloc manchette RSS';
-
-$string['addnew'] = 'Ajouter nouveau';
-$string['managefeeds'] = 'Gérer tous mes fils RSS';
-$string['configblock'] = 'Configurer ce bloc';
-$string['feedsconfigurenewinstance'] = 'Cliquer ici pour afficher dans ce bloc des fils RSS';
+$string['updatefeed'] = 'Modifier l\'URL d\'un fil URL : ';
+$string['validatefeed'] = 'Valider le fil';
 
 ?>
