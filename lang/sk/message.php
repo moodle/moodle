d6008cd6c1a4982505394d079f0f3ae8c72092b5
havran <havran>
2005-05-17 12:01:58 +0000

    new strings from Juraj Chlebec

---
+++ lang/sk/message.php
@@ -0,0 +1,68 @@
+<?PHP // $Id$ 
+      // message.php - created with Moodle 1.5 ALPHA (2005051500)
+
+
+$string['addcontact'] = 'Prida» kontakt';
+$string['addsomecontacts'] = 'Pre poslanie správy niekomu, alebo pre pridanie odkazu naòho na túto stránku pou¾i <a href=\"$a\">zálo¾ku vyhµadávanie</a>.';
+$string['addsomecontactsincoming'] = 'Tieto správy sú od µudí ktorých nemá¹ v zozname kontaktov. Pokiaµ si ich chce¹ prida» medzi svoje kontakty, klikni na ikonu &Prida» kontakt& za ich menom.';
+$string['ago'] = '$a predtým';
+$string['allmine'] = 'V¹etky správy pre mòa alebo odomòa';
+$string['allstudents'] = 'V¹etky správy medzi ¹tudentami v kurze';
+$string['allusers'] = 'V¹etky správy od v¹etkých u¾ívateµov';
+$string['backupmessageshelp'] = 'Pokiaµ je toto zapnuté, správa bude zahrnutá do automatického zálohovania web stránok.';
+$string['beepnewmessage'] = 'Pípni keï prijde nová správa (pre prehratie zvuku mô¾e potrebova» zásuvný modul do prehliadaèa, ako napríklad Quicktime)';
+$string['blockcontact'] = 'Zablokova» kontakt';
+$string['blockedmessages'] = '$a správa(y) pre/od blokovaných u¾ívateµov';
+$string['blocknoncontacts'] = 'Zablokuj v¹etky nové správy od µudí ktorý nie sú v mojom zozname kontaktov';
+$string['contactlistempty'] = 'Tvoj zoznam kontaktov je práve prázdny';
+$string['contacts'] = 'Kontakty';
+$string['context'] = 'obsah';
+$string['deletemessagesdays'] = 'Poèet dní po ktorých budú staré správy automaticky vymazané';
+$string['discussion'] = 'Diskusie';
+$string['emailmessages'] = 'Pokiaµ nie som prihlásený, posla» mi správy e-mailom';
+$string['emailtagline'] = 'Tento e-mail je kópia správy ktorá ti bola poslaná na \"$a\"';
+$string['emptysearchstring'] = 'Musí¹ nieèo hµada»';
+$string['formorethan'] = 'Pre viac ako';
+$string['includeblockedusers'] = 'Zahrnú» blokovaných pou¾ívateµov';
+$string['incomingcontacts'] = 'Prichádzajúce kontakty ($a)';
+$string['keywords'] = 'Kµúèové slová';
+$string['keywordssearchresults'] = 'Výsledok hµadania: $a nájdených správ';
+$string['maxmessages'] = 'Maximálny poèet správ ktoré budú zobrazované v histórii diskusií';
+$string['message'] = 'Správa';
+$string['messagehistory'] = 'História správ';
+$string['messages'] = 'Správy';
+$string['messaging'] = 'Posielanie správ';
+$string['mycontacts'] = 'Moje kontakty';
+$string['newsearch'] = 'Nové hµadanie';
+$string['nomessages'] = 'Neèakajú ¾iadne správy';
+$string['nomessagesfound'] = 'Neboli nájdené ¾iadne správy';
+$string['nosearchresults'] = 'Nepodarilo sa nájs» ¾iadne výsledky';
+$string['offline'] = 'Nepripojený';
+$string['offlinecontacts'] = 'Nepripojené kontakty ($a)';
+$string['online'] = 'Pripojený';
+$string['onlinecontacts'] = 'Pripojené kontakty ($a)';
+$string['onlyfromme'] = 'Iba správy odomòa';
+$string['onlymycourses'] = 'Iba v mojich kurzoch';
+$string['onlytome'] = 'Iba správy pre mòa';
+$string['pagerefreshes'] = 'Táto stránka sa obnoví automaticky ka¾dých $a sekúnd';
+$string['readmessages'] = '$a èíta správy';
+$string['removecontact'] = 'Odstráni» kontakt';
+$string['savemysettings'] = 'Ulo¾ moje nastavenia';
+$string['search'] = 'Hµadaj';
+$string['searchforperson'] = 'Hµadaj osobu';
+$string['searchmessages'] = 'Hµadaj správu';
+$string['sendmessage'] = 'Po¹li správu';
+$string['sendmessageto'] = 'Po¹li správu pre $a';
+$string['settings'] = 'Nastavenia';
+$string['settingssaved'] = 'Tvoje nastavenia boli ulo¾ené';
+$string['showmessagewindow'] = 'Automaticky zobraz okno správ keï dostanem novú správu (tvoj prehliadaè je potrebné nastavi» tak aby neblokoval vyskakujúce okná na tejto stránke)';
+$string['strftimedaydatetime'] = '%%A, %%d %%B %%Y, %%I:%%M %%p';
+$string['timenosee'] = 'Poèet minút odkedy som bol naposledy pripojený';
+$string['timesent'] = 'Èas poslania';
+$string['unblockcontact'] = 'Odblokuj kontakt';
+$string['unreadmessages'] = '$a neèítaných správ';
+$string['userisblockingyou'] = 'Tento u¾ívateµ má nastavené blokovanie správ od teba';
+$string['userisblockingyounoncontact'] = 'Tento u¾ívateµ prijíma správy iba od µudí, ktorých má zapísaných vo svojom zozname kontaktov a ty sa v òom nenachádza¹. ';
+$string['userssearchresults'] = 'Výsledok hµadania: $a nájdených u¾ívateµov';
+
+?>
