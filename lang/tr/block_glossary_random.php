5b6b0b490e4ba2923acb487f314ae767ffbb9163
ethem <ethem>
2005-05-18 12:16:05 +0000

    New strings from Ethem Evlice for block_glossary_random (XHTML compliant)

--- lang/tr/block_glossary_random.php
+++ lang/tr/block_glossary_random.php
@@ -1,12 +1,27 @@
 <?PHP // $Id$ 
-      // block_glossary_random.php - created with Moodle 1.5 ALPHA (2005042300)
+      // block_glossary_random.php - created with Moodle 1.5 ALPHA (2005051500)
 
 
-$string['blockname'] = 'Rasgele Terimler';
-$string['invisible'] = '(devam edecek)';
+$string['addentry'] = 'Favori sözcüklerinizi ekleyin!';
+$string['askaddentry'] = 'Öðrenciler sözlüðe sözcük ekleyebiliyorlarsa, bu yazý ile bir link göster';
+$string['askinvisible'] = 'Öðrenciler sözlüðü düzenleyemiyor veya göremiyorsa bu yazýyý göster (linksiz)';
+$string['askviewglossary'] = 'Öðrenciler sözlüðü görebiliyor ancak kayýt ekleyemiyorlarsa bu yazý ile bir link göster';
+$string['blockname'] = 'Rasgele Sözcük';
+$string['intro'] = 'Bu kursa en az bir kayýtla bir sözlük eklendiðinden emin olun. Sonra aþaðýdaki ayarlarý yapabilirsiniz';
+$string['invisible'] = '(devamý var)';
 $string['lastmodified'] = 'Son düzenlenen kayýt';
 $string['nextone'] = 'Sonraki kayýt';
+$string['noentriesyet'] = 'Seçilen sözlükte henüz hiç bir sözcük yok.';
+$string['notyetconfigured'] = 'Düzenle ikonu kullanarak bu bloðu düzenleyin.';
+$string['notyetglossary'] = 'En az bir sözlük seçmeniz gerekiyor.';
 $string['random'] = 'Rasgele kayýt';
+$string['refresh'] = 'Gün içinde eklenenlerden seç';
+$string['select_glossary'] = 'Kayýtlarý bu sözlükten al';
+$string['showconcept'] = 'Herbir kayýt için kavramý (baþlýðý) göster';
 $string['title'] = 'Baþlýk';
+$string['type'] = 'Yeni kayýt nasýl seçilecek';
+$string['typehelp'] = '<b>Son düzenlenen kayýt</b> her zaman son güncellenen kayýtlarý gösterirken <b>Rasgele kayýt</b> her seferinde farklý bir tane seçer. <b>Sonraki kayýt</b> seçeneði ise sözcükleri sýrasýna göre gösterir. Son seçenek son X gün içinde eklenenlerden sözcüðü seçtiði için özellikle daha kullanýþlýdýr. Herkesin görebileceði <i>Haftanýn sözü</i> veya <i>Günün ipucu</i> gibi seçeneklere olanak sunar.';
+$string['viewglossary'] = 'Daha fazla sözcük...';
+$string['whichfooter'] = 'Bu bloðun iliþkili olduðu sözlükle ilgili iþlemler için link ayarý yapabilirsiniz. Bu blok sadece bu sözlükte bu linkleri gösterecektir.';
 
 ?>
