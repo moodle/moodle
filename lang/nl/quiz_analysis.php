be808502ac9c139c31e8faf9cf875847e43e66b6
koenr <koenr>
2005-05-18 21:44:50 +0000

    new file - not understood everything yet

---
+++ lang/nl/quiz_analysis.php
@@ -0,0 +1,27 @@
+<?PHP // $Id$ 
+      // quiz_analysis.php - created with Moodle 1.5 ALPHA (2005051500)
+
+
+$string['analysisoptions'] = 'Analyse-opties';
+$string['analysistitle'] = 'Item analysetabel';
+$string['attemptsall'] = 'Alle pogingen';
+$string['attemptselection'] = 'Te analyseren pogingen per gebruiker';
+$string['attemptsfirst'] = 'eerste poging';
+$string['attemptshighest'] = 'hoogste cijfer';
+$string['attemptslast'] = 'laatste poging';
+$string['downloadooo'] = 'Download in OpenOffice formaat';
+$string['lowmarkslimit'] = 'Analyseer niet als de score lager is dan:';
+$string['pagesize'] = 'Vragen per pagina';
+$string['qcounttitle'] = 'V. aantal';
+$string['qidtitle'] = 'V#';
+$string['qnametitle'] = 'Vraag naam';
+$string['qtexttitle'] = 'Vraag tekst';
+$string['qtypetitle'] = 'V. type';
+$string['quizreportdir'] = 'test_rapport';
+$string['rcounttitle'] = 'A.<br />aantallen';
+$string['reportanalysis'] = 'Item analyse rapport';
+$string['responsestitle'] = 'Antwoordtekst';
+$string['rpercenttitle'] = 'A.%%';
+$string['stddevtitle'] = 'SD';
+
+?>
