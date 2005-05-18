c877e32fa20875c7827a45a4072383066881a46a
gustav_delius <gustav_delius>
2005-05-18 19:53:44 +0000

    language file for the item analysis report for the quiz module developed by Enrique

---
+++ lang/en/quiz_analysis.php
@@ -0,0 +1,34 @@
+<?php // $Id$
+      // quiz.php - created with Moodle 1.4 development (2004080300)
+
+$string['analysisoptions'] = 'Analysis options';
+$string['attemptselection'] = 'Attempts to analyze per user ';
+$string['lowmarkslimit'] = "Don't analyze if score is less than: ";
+$string['attemptsall'] = 'all attempts';
+$string['attemptshighest'] = 'highest grade';
+$string['attemptsfirst'] = 'first attempt';
+$string['attemptslast'] = 'last attempt';
+$string['pagesize'] = 'Questions per page: ';
+$string['analysistitle'] = 'Item Analysis Table';
+$string['reportanalysis'] = 'Item Analysis Report';
+$string['downloadooo'] = 'Download in OpenOffice format';
+
+$string['qidtitle'] = 'Q#';
+$string['qnametitle'] = 'Question name';
+$string['qtexttitle'] = 'Question text';
+$string['qcounttitle'] = 'Q. count';
+$string['qtypetitle'] = 'Q. type';
+$string['responsestitle'] = "Answer's text";
+$string['rfractiontitle'] = 'parcial<br>credit';
+$string['rcounttitle'] = 'R.<br>Counts';
+$string['rpercenttitle'] = 'R.%%';
+$string['facilitytitle'] = '%% Correct<br>Facility';
+$string['stddevtitle'] = 'SD';
+$string['dicsindextitle'] = 'Disc.<br/>Index';
+$string['disccoefftitle'] = 'Disc.<br/>Coeff.';
+
+$string['quizreportdir'] = 'quiz_reports';
+
+
+
+?>
