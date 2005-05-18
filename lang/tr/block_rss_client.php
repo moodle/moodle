84f868036f176a3a1a930bd834878d3d9ce3e4b2
ethem <ethem>
2005-05-18 12:57:56 +0000

    New strings from Ethem Evlice for block_rss_client. <XHTML compliant/>

--- lang/tr/block_rss_client.php
+++ lang/tr/block_rss_client.php
@@ -1,7 +1,41 @@
 <?PHP // $Id$ 
-      // client.php - created with Moodle 1.5 ALPHA (2005042300)
+      // block_rss_client.php - created with Moodle 1.5 ALPHA (2005051500)
 
 
+$string['addfeed'] = 'Haber içeriði URL:';
+$string['addheadlineblock'] = 'RSS baþlýk bloðu ekle';
 $string['addnew'] = 'Yeni Ekle';
+$string['choosefeedlabel'] = 'Bu blokta hangi içeriklerin etkin olacaðýný seçin:';
+$string['clientchannellink'] = 'Kaynak site...';
+$string['clientnumentries'] = 'Her bir içerikte görünen varsayýlan kayýt sayýsý';
+$string['clientshowchannellinklabel'] = 'Orijinal siteye baðlantý (kanal linki) görünecek mi?';
+$string['clientshowimagelabel'] = 'Varsa kanal resmini göster:';
+$string['configblock'] = 'Bu bloðu yapýlandýr';
+$string['couldnotfindfeed'] = 'Bu id ile bir içerik bulunamadý';
+$string['customtitlelabel'] = 'Kiþisel baþlýk (Ýçerik tarafýndan saðlanmasýný istiyorsanýz boþ býrakýn):';
+$string['deletefeedconfirm'] = 'Bu içeriði silmek istediðinizden emin misiniz?';
+$string['displaydescriptionlabel'] = 'Her bir linkin açýklamasýný göster';
+$string['editfeeds'] = 'RSS/Atom haber içeriklerini düzenle/abone ol/aboneliði kaldýr';
+$string['editnewsfeeds'] = 'Haber içeriklerini düzenle';
+$string['editrssblock'] = 'RSS baþlýk bloðunu düzenle';
+$string['feed'] = 'Haber Ýçeriði';
+$string['feedadded'] = 'Haber içeriði eklendi';
+$string['feeddeleted'] = 'Haber içeriði silindi';
+$string['feeds'] = 'Haber Ýçerikleri';
+$string['feedsaddedit'] = 'Ýçerik Ekle/Düzenle';
+$string['feedsconfigurenewinstance'] = 'Bu bloðun RSS içerikleri göstermesi için yapýlandýrmanýz gerekiyor.';
+$string['feedstitle'] = 'Haber Ýçerikleri';
+$string['feedupdated'] = 'Haber içeriði güncellendi';
+$string['findmorefeeds'] = 'Daha fazla rss içeriði bul';
+$string['managefeeds'] = 'Tüm içeriklerimi yönet';
+$string['nofeeds'] = 'Bu site için tanýmlanmýþ hiç RSS içeriði yok';
+$string['pickfeed'] = 'Bir haber içeriði seç';
+$string['remotenewsfeed'] = 'Haber Ýçerikleri';
+$string['seeallfeeds'] = 'Tüm içeriklere bak';
+$string['shownumentrieslabel'] = 'Her bir blokta görünen max kayýt sayýsý';
+$string['submitters'] = 'Yeni rss içeriklerini eklemeye kimler yetkili? Eklenen içerikler sitenizin herhangi bir yerinde olabilir.';
+$string['timeoutdesc'] = 'Bir RSS içeriðinin ara bellekte dakika cinsinden kalma süresi.';
+$string['updatefeed'] = 'Haber içeriði URL\'sini güncelle:';
+$string['validatefeed'] = 'Ýçeriði denetle';
 
 ?>
