79d1644127b3c2df92d0b1a5db343ec94f3cb411
ethem <ethem>
2005-05-19 08:15:56 +0000

    be annotator.

---
+++ lang/tr/quiz_analysis.php
@@ -0,0 +1,27 @@
+<?PHP // $Id$ 
+      // quiz_analysis.php - created with Moodle 1.5 ALPHA (2005051500)
+
+
+$string['analysisoptions'] = 'Analiz seçenekleri';
+$string['analysistitle'] = 'Öge Analiz Tablosu';
+$string['attemptsall'] = 'Tüm uygulamalar';
+$string['attemptselection'] = 'Her bir kullanýcý için hangi uygulamalar analiz yapýlacak:';
+$string['attemptsfirst'] = 'Ýlk uygulama';
+$string['attemptshighest'] = 'En yüksek not';
+$string['attemptslast'] = 'Son uygulama';
+$string['dicsindextitle'] = 'Ayýrýcý<br />Index';
+$string['disccoefftitle'] = 'Ayýrýcý<br />Katsayý';
+$string['downloadooo'] = 'OpenOffice biçiminde indir';
+$string['lowmarkslimit'] = 'Puanlar bundan azsa analiz yapma:';
+$string['pagesize'] = 'Her sayfadaki soru';
+$string['qcounttitle'] = 'S. sayýsý';
+$string['qidtitle'] = 'S#';
+$string['qnametitle'] = 'Soru adý';
+$string['qtexttitle'] = 'Soru metni';
+$string['qtypetitle'] = 'S. tipi';
+$string['quizreportdir'] = 'sinav_sonuclari';
+$string['reportanalysis'] = 'Öge Analiz Raporu';
+$string['responsestitle'] = 'Cevap metni';
+$string['stddevtitle'] = 'SS';
+
+?>
