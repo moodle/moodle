478ba270789706ea6446311ae2987a884a4e87b9
martignoni <martignoni>
2005-05-19 09:35:51 +0000

    New file added

---
+++ lang/fr/quiz_analysis.php
@@ -0,0 +1,31 @@
+<?php // $Id$
+
+$string['analysisoptions'] = 'Options d\'analyse';
+$string['attemptselection'] = 'Tentatives à analyser pour l\'utilisateur ';
+$string['lowmarkslimit'] = 'Ne pas analyser si le score est inférieur à ';
+$string['attemptsall'] = 'Toutes tentatives';
+$string['attemptshighest'] = 'Meilleure note';
+$string['attemptsfirst'] = 'Première tentative';
+$string['attemptslast'] = 'Dernière tentative';
+$string['pagesize'] = 'Questions par page&nbsp;: ';
+$string['analysistitle'] = 'Table d\'analyse éléments';
+$string['reportanalysis'] = 'Rapport d\'analyse éléments';
+$string['downloadooo'] = 'Télécharger en format OpenOffice';
+
+$string['qidtitle'] = 'Q#';
+$string['qnametitle'] = 'Nom question';
+$string['qtexttitle'] = 'Texte question';
+$string['qcounttitle'] = 'Nombre Q.';
+$string['qtypetitle'] = 'Type Q.';
+$string['responsestitle'] = 'Texte réponse';
+$string['rfractiontitle'] = 'Points<br />partiels';
+$string['rcounttitle'] = 'Nombre<br />Réponse';
+$string['rpercenttitle'] = 'R. %%';
+$string['facilitytitle'] = '%% Correct<br />Facility';
+$string['stddevtitle'] = 'Éc. Type';
+$string['dicsindextitle'] = 'Index<br />disc.';
+$string['disccoefftitle'] = 'Coeff.<br />disc.';
+
+$string['quizreportdir'] = 'rapports_tests';
+
+?>
