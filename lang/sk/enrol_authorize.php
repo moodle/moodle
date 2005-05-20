02d10c615597d12cbf4a687cd47dc02a3c5f5ff4
havran <havran>
2005-05-20 14:22:16 +0000

    new strings from Juraj Chlebec

---
+++ lang/sk/enrol_authorize.php
@@ -0,0 +1,23 @@
+<?PHP // $Id$ 
+      // enrol_authorize.php - created with Moodle 1.5 ALPHA (2005051500)
+
+
+$string['anlogin'] = 'Authorize.net: Prihlasovacie meno';
+$string['anpassword'] = 'Authorize.net: Heslo (nie je po¾adované)';
+$string['anreferer'] = 'Sem vlo¾te URL referer, pokiaµ ho máte nastavený vo va¹om úète na authorize.net. Do HTTP po¾iadavku bude zaèlenená hlavièka &quot;Referer: URL&quot;.
+';
+$string['antestmode'] = 'Authorize.net: Testovacia transakcia';
+$string['antrankey'] = 'Authorize.net: Transakèný kµúè';
+$string['ccexpire'] = 'Dátum zániku platnosti';
+$string['ccinvalid'] = 'Neplatné èíslo karty';
+$string['ccno'] = 'Èíslo kreditnej karty';
+$string['cctype'] = 'Typ kreditnej karty';
+$string['ccvv'] = 'CV2';
+$string['ccvvhelp'] = 'Pozrite sa na zadnú stranu karty (posledné 3 èísla)';
+$string['description'] = 'Modul Authorize.net umo¾òuje pou¾íva» platený prístup ku kurzom cez poskytovateµov kreditných kariet. Pokiaµ je cena niektorého kurzu nulová, nebude od ¹tudentov vy¾adovaná platba za vstup. Cenu je mo¾né nastavi» globálne pre celý web a tie¾ pre ka¾dý kurz osobitne. Pokiaµ je nastavená cena za kurz, globálne nastavenie sa neberie do úvahy.';
+$string['enrolname'] = 'Authorize.net: Brána pre kreditné karty';
+$string['nameoncard'] = 'Meno karty';
+$string['sendpaymentbutton'] = 'Posla» platbu';
+$string['zipcode'] = 'Smerovacie èíslo';
+
+?>
