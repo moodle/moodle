40f9642897ce93134ec7fe30972d1ad49373fff6
barias <barias>
2005-05-22 22:11:01 +0000

    Latest updates in spanish translation

--- lang/es/block_rss_client.php
+++ lang/es/block_rss_client.php
@@ -1,37 +1,42 @@
-<?php // $Id$ 
-      // client.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+<?PHP // $Id$ 
+      // block_rss_client.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005042100)
 
-$string['addfeed'] = 'Agregar un canal URL de noticias:';
-$string['addheadlineblock'] = 'Añadir bloque de cabecera RSS';
+
+$string['addfeed'] = 'Añadir una URL de canal de noticias:';
+$string['addheadlineblock'] = 'Añadir un bloque de cabecera RSS';
 $string['addnew'] = 'Añadir nuevo';
-$string['choosefeedlabel'] = 'Elija los canales que desea estén disponibles en este bloque:';
+$string['choosefeedlabel'] = 'Seleccione los canales que desea estén disponibles en este bloque:';
 $string['clientchannellink'] = 'Sitio fuente...';
-$string['clientnumentries'] = 'Número de enlaces rss a mostrar por bloque.';
-$string['clientshowchannellinklabel'] = '¿Se mostrará un enlace al sitio original? (Observe que si no se suministra un enlace en el canal de noticias no se mostrará ningún enlace):';
+$string['clientnumentries'] = 'El número de entradas por defecto a mostrar por canal.';
+$string['clientshowchannellinklabel'] = '¿Deberá mostrarse un enlace al sitio original (vínculo de canal)? (Observe que si no se suministra un enlace en el canal de noticias, no se mostrará ninguno):';
 $string['clientshowimagelabel'] = 'Mostrar imagen del canal si está disponible:';
-$string['couldnotfindfeed'] = 'No se pudo encontrar un canal con id';
-$string['customtitlelabel'] = 'Título personalizado (dejar en blanco para utilizar el título suministrado por el canal):';
+$string['configblock'] = 'Configurar este bloque';
+$string['couldnotfindfeed'] = 'No se pudo encontrar un canal con esa id';
+$string['customtitlelabel'] = 'Título personalizado (déjelo en blanco para usar el título suministrado por el canal):';
 $string['deletefeedconfirm'] = '¿Está seguro de que desea eliminar este canal?';
-$string['displaydescriptionlabel'] = '¿Mostrar la descripción de cada enlace?';
-$string['editfeeds'] = 'Editar, suscribirse o darse de baja de canales de noticias RSS/Atom';
-$string['editnewsfeeds'] = 'Editar nuevos canales';
+$string['displaydescriptionlabel'] = '¿Mostrar descripción de cada enlace?';
+$string['editfeeds'] = 'Editar, suscribir o dar de baja de canales de noticias RSS/Atom';
+$string['editnewsfeeds'] = 'Editar canales de noticias';
 $string['editrssblock'] = 'Editar bloque de cabecera RSS';
-$string['feed'] = 'Canal de noticias';
-$string['feedadded'] = 'Canal de noticias agregado';
-$string['feeddeleted'] = 'Canal de noticias eliminado';
-$string['feedupdated'] = 'Canal de noticias actualizado';
+$string['feed'] = 'Canal';
+$string['feedadded'] = 'Añadido canal de noticias';
+$string['feeddeleted'] = 'Eliminado canal de noticias';
 $string['feeds'] = 'Canales de noticias';
 $string['feedsaddedit'] = 'Añadir/Editar canales';
+$string['feedsconfigurenewinstance'] = 'Haga clic aquí para configurar este bloque a fin de mostrar canales RSS.';
 $string['feedstitle'] = 'Canales RSS remotos';
-$string['findmorefeeds'] = 'Encontrar más canales rss';
-$string['nofeeds'] = 'Este sitio carece de canales RSS.';
+$string['feedupdated'] = 'Canal de noticias actualizado';
+$string['findmorefeeds'] = 'Encontrar más canales RSS';
+$string['managefeeds'] = 'Gestionar todos mis canales';
+$string['nofeeds'] = 'No hay canales RSS definidos para este sitio.';
 $string['pickfeed'] = 'Seleccionar un canal de noticias';
 $string['remotenewsfeed'] = 'Canal de noticias remoto';
 $string['seeallfeeds'] = 'Ver todos los canales';
 $string['shownumentrieslabel'] = 'Número máximo de entradas a mostrar por bloque.';
-$string['submitters'] = '¿Quién puede definir los canales rss nuevos? Los canales estarán disponibles para cualquier miembro del sitio para agregar sus propias páginas';
+$string['submitters'] = '¿Quién puede definir canales RSS de noticias? Los canales definidos estarán disponibles en cualquier página de su sitio.';
 $string['timeout'] = 'blockrsstimeout';
-$string['timeoutdesc'] = 'Tiempo en minutos de permanencia en caché de un canal RSS';
-$string['updatefeed'] = 'Actualizar canal URL de noticias:';
+$string['timeoutdesc'] = 'Tiempo de permanencia de un canal de noticias en el caché (en minutos)';
+$string['updatefeed'] = 'Actualizar una URL de canal de noticias:';
 $string['validatefeed'] = 'Validar canal';
+
 ?>
---
+++ lang/es/quiz_analysis.php
@@ -0,0 +1,31 @@
+<?PHP // $Id$ 
+      // quiz_analysis.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005042100)
+
+
+$string['analysisoptions'] = 'Opciones de análisis';
+$string['analysistitle'] = 'Tabla de análisis de ítems';
+$string['attemptsall'] = 'todos los intentos';
+$string['attemptselection'] = 'Intentos a analizar por usuario';
+$string['attemptsfirst'] = 'primer intento';
+$string['attemptshighest'] = 'calificación más alta';
+$string['attemptslast'] = 'último intento';
+$string['dicsindextitle'] = 'Índice<br/>Disc.';
+$string['disccoefftitle'] = 'Coef.<br/>Disc.';
+$string['downloadooo'] = 'Descargar en formato OpenOffice';
+$string['facilitytitle'] = '%% Correct<br>Facility';
+$string['lowmarkslimit'] = 'No analizar si la puntuación es inferior a:';
+$string['pagesize'] = 'Preguntas por página:';
+$string['qcounttitle'] = 'Nº Pr.';
+$string['qidtitle'] = 'Pr.#';
+$string['qnametitle'] = 'Nombre de la pregunta';
+$string['qtexttitle'] = 'Texto de la pregunta';
+$string['qtypetitle'] = 'Tipo de Pr.';
+$string['quizreportdir'] = 'informes_cuest.';
+$string['rcounttitle'] = 'Nº<br>R.';
+$string['reportanalysis'] = 'Informe análisis de ítems';
+$string['responsestitle'] = 'Texto de la respuesta';
+$string['rfractiontitle'] = 'crédito<br>parcial';
+$string['rpercenttitle'] = '%%R.';
+$string['stddevtitle'] = 'DT';
+
+?>
