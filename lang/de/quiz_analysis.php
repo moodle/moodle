42d74f47f923f54aa212ba364b76e7709c80730a
gustav_delius <gustav_delius>
2005-05-23 14:56:22 +0000

    Some corrections

--- lang/de/quiz_analysis.php
+++ lang/de/quiz_analysis.php
@@ -1,9 +1,9 @@
-<?PHP // $Id$ 
+<?PHP // $Id$
       // quiz_analysis.php - created with Moodle 1.5 ALPHA (2005051500)
 
 
 $string['analysisoptions'] = 'Auswertungsoptionen';
-$string['analysistitle'] = 'Tabelle der analysierten Werte';
+$string['analysistitle'] = 'Tabelle mit Fragenanalyse';
 $string['attemptsall'] = 'alle Versuche';
 $string['attemptselection'] = 'Versuche, die pro Nutzer/in analysiert werden';
 $string['attemptsfirst'] = 'erster Versuch';
@@ -12,7 +12,7 @@ $string['attemptslast'] = 'letzter Versuch';
 $string['dicsindextitle'] = 'Disc.<br />Index';
 $string['disccoefftitle'] = 'Disc<br />Koeff.';
 $string['downloadooo'] = 'Download im OpenOffice-Format';
-$string['facilitytitle'] = '%% korrekte<br>Möglichkeiten';
+$string['facilitytitle'] = '%% korrekt<br>Schwierigkeit';
 $string['lowmarkslimit'] = 'Nicht auswerten wenn der Wert niedriger ist als';
 $string['pagesize'] = 'Fragen pro Seite:';
 $string['qcounttitle'] = 'Fragen gezählt';
@@ -22,9 +22,8 @@ $string['qtexttitle'] = 'Fragetext';
 $string['qtypetitle'] = 'Fragentyp';
 $string['quizreportdir'] = 'quiz_reports';
 $string['rcounttitle'] = 'Antworten <br>gezählt';
-$string['reportanalysis'] = 'Auswertungsbericht';
+$string['reportanalysis'] = 'Fragananalyse';
 $string['responsestitle'] = 'Antworttexte';
-$string['rfractiontitle'] = 'Teil-<br>Guthaben';
 $string['rpercenttitle'] = 'Antworten %%';
 
 ?>
