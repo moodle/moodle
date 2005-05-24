1b0649d55866896f63f5bcfe5ad98ad93eb2a0bc
moodler <moodler>
2005-05-24 16:55:48 +0000

    Moved some things from People block to Administration.  Each of them will probably be tweaked a little more still.

--- blocks/participants/block_participants.php
+++ blocks/participants/block_participants.php
@@ -22,33 +22,18 @@ class block_participants extends block_list {
         $this->content->icons = array();
         $this->content->footer = '';
 
-        $strgroups   = get_string('groups');
-        $strgroupmy  = get_string('groupmy');
 
-        $course = get_record('course', 'id', $this->instance->pageid);
+        if ($this->instance->pageid != SITEID || 
+            $CFG->showsiteparticipantslist > 1 || 
+            ($CFG->showsiteparticipantslist == 1 && isteacherinanycourse()) || 
+            isteacher(SITEID)) {
 
-        if ($this->instance->pageid != SITEID || $CFG->showsiteparticipantslist > 1 || ($CFG->showsiteparticipantslist == 1 && isteacherinanycourse()) || isteacher(SITEID)) {
-            $this->content->items[]='<a title="'.get_string('listofallpeople').'" href="'.$CFG->wwwroot.'/user/index.php?id='.$this->instance->pageid.'">'.get_string('participants').'</a>';
-            $this->content->icons[]='<img src="'.$CFG->pixpath.'/i/users.gif" height="16" width="16" alt="" />';
+            $this->content->items[] = '<a title="'.get_string('listofallpeople').'" href="'.
+                                      $CFG->wwwroot.'/user/index.php?id='.$this->instance->pageid.'">'.get_string('participants').'</a>';
+            $this->content->icons[] = '<img src="'.$CFG->pixpath.'/i/users.gif" height="16" width="16" alt="" />';
+
         }
 
-        if ($course->groupmode || !$course->groupmodeforce) {
-            if (isteacheredit($this->instance->pageid)) {
-                $this->content->items[]='<a title="'.$strgroups.'" href="'.$CFG->wwwroot.'/course/groups.php?id='.$this->instance->pageid.'">'.$strgroups.'</a>';
-                $this->content->icons[]='<img src="'.$CFG->pixpath.'/i/group.gif" height="16" width="16" alt="" />';
-            }
-        }
-
-        if (!empty($USER->id) and !isguest()) {
-            $fullname = fullname($USER, true);
-            $editmyprofile = '<a title="'.$fullname.'" href="'.$CFG->wwwroot.'/user/edit.php?id='.$USER->id.'&amp;course='.$this->instance->pageid.'">'.get_string('editmyprofile').'</a>';
-            if ($USER->description) {
-                $this->content->items[]= $editmyprofile;
-            } else {
-                $this->content->items[]= $editmyprofile." <blink>*</blink>";
-            }
-            $this->content->icons[]='<img src="'.$CFG->pixpath.'/i/user.gif" height="16" width="16" alt="" />';
-        }
 
         return $this->content;
     }
