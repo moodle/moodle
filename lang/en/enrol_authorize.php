75691cecf3ae8ce0894f2c99e75166dfc4cf6da1
moodler <moodler>
2005-05-25 16:26:13 +0000

    Updated the english and added a string

--- lang/en/enrol_authorize.php
+++ lang/en/enrol_authorize.php
@@ -3,9 +3,9 @@
 
 
 $string['anlogin'] = 'Authorize.net: Login name';
-$string['anpassword'] = 'Authorize.net: Password (not required)';
-$string['anreferer'] = 'Type here URL referer if you set up this in your authorize.net account. This will send a heading \"Referer: URL\" in the web request.';
-$string['antestmode'] = 'Authorize.net: Test transactions';
+$string['anpassword'] = 'Authorize.net: Password';
+$string['anreferer'] = 'Define the URL referer if you have set up this in your authorize.net account. This will send a line \"Referer: URL\" embedded in the web request.';
+$string['antestmode'] = 'Run transactions in test mode only (no money will be drawn)';
 $string['antrankey'] = 'Authorize.net: Transaction Key';
 $string['ccexpire'] = 'Expiry Date';
 $string['ccinvalid'] = 'Invalid card number';
@@ -13,6 +13,7 @@ $string['ccno'] = 'Credit Card Number';
 $string['cctype'] = 'Credit Card Type';
 $string['ccvv'] = 'CV2';
 $string['ccvvhelp'] = 'Look at the back of card (last 3 digits)';
+$string['chooseone'] = 'Fill one or both of the following two fields';
 $string['description'] = 'The Authorize.net module allows you to set up paid courses via CC providers.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
 $string['enrolname'] = 'Authorize.net Credit Card Gateway';
 $string['nameoncard'] = 'Name on Card';
