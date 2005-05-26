2f7cdfc564982c26287a22c429bcb5b5c509d6af
martignoni <martignoni>
2005-05-26 12:54:04 +0000

    Updated the wording and added a string

--- lang/fr/enrol_authorize.php
+++ lang/fr/enrol_authorize.php
@@ -1,9 +1,9 @@
 <?php // $Id$ 
 
 $string['anlogin'] = 'Authorize.net&nbsp;: nom d\'utilisateur';
-$string['anpassword'] = 'Authorize.net&nbsp;: mot de passe (facultatif)';
-$string['anreferer'] = 'Taper ici un URL référenceur si vous le configurez dans votre compte authorize.net. Ceci enverra une entête «&nbsp;Referer: URL&nbsp;» dans la requête web';
-$string['antestmode'] = 'Authorize.net&nbsp;: tester les transactions';
+$string['anpassword'] = 'Authorize.net&nbsp;: mot de passe';
+$string['anreferer'] = 'Taper ici un URL référenceur si vous avez mis en place cette fonctionnalité dans votre compte authorize.net. Ceci enverra une entête «&nbsp;Referer: URL&nbsp;» incluse dans la requête web';
+$string['antestmode'] = 'Traiter les transactions en mode test (aucun montant ne sera prélevé)';
 $string['antrankey'] = 'Authorize.net&nbsp;: clef de transaction';
 $string['ccexpire'] = 'Date d\'échéance';
 $string['ccinvalid'] = 'Numéro de carte non valable';
@@ -11,6 +11,7 @@ $string['ccno'] = 'Numéro de carte de crédit';
 $string['cctype'] = 'Type de carte de crédit';
 $string['ccvv'] = 'Code vérification';
 $string['ccvvhelp'] = 'Au verso de votre carte (les 3 derniers chiffres)';
+$string['chooseone'] = 'Veuillez remplir l\'un des deux champs ci-dessous ou tous les deux';
 $string['description'] = 'Le module Authorize.net permet de mettre en place des cours payant. Si le coût d\'un cours est nul, les étudiants peuvent s\'y inscrire sans payer. Un coût défini globalement, que vous fixez ici, est le coût par défaut pour tous les cours du site. Chaque cours peut ensuite avoir un coût spécifique fixé individuellement. S\'il est défini, le coût spécifique d\'un cours remplace le coût par défaut.';
 $string['enrolname'] = 'Paiement par carte de crédit Authorize.net';
 $string['nameoncard'] = 'Nom sur la carte';
