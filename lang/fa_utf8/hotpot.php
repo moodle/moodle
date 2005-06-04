42bdd31e6851aae1bb18306700e4691dcf93da0c
moodler <moodler>
2005-06-04 20:05:31 +0000

    file hotpot.php was added on branch MOODLE_15_STABLE on 2005-06-06 05:03:15 +0000

--- lang/fa_utf8/hotpot.php
+++
@@ -1,8 +0,0 @@
-<?PHP // $Id$ 
-      // hotpot.php - created with Moodle 1.6 development (2005060201)
-
-
-$string['modulename'] = 'Hot Potatoes Quiz';
-$string['modulenameplural'] = 'Hot Potatoes Quizzes';
-
-?>
