e666046150403e2e4ff8854b46352bc721018068
stronk7 <stronk7>
2005-06-09 23:32:13 +0000

    Now icons, comments and ratings are showed properly in entrylist format. Bug 3455 (http://moodle.org/bugs/bug.php?op=show&bugid=3455)
    Merged from MOODLE_15_STABLE

--- mod/glossary/formats/entrylist/entrylist_format.php
+++ mod/glossary/formats/entrylist/entrylist_format.php
@@ -14,11 +14,13 @@ function glossary_show_entry_entrylist($course, $cm, $glossary, $entry, $mode=''
         echo "<span class=\"concept\"><a href=\"showentry.php?courseid=$course->id\&amp;eid=$entry->id\&amp;displayformat=dictionary\" target=\"_blank\" onClick=\"return openpopup('/mod/glossary/showentry.php?courseid=$course->id\&amp;eid=$entry->id\&amp;displayformat=dictionary', 'entry', 'menubar=0,location=0,scrollbars,resizable,width=600,height=450', 0);\">";
         glossary_print_entry_concept($entry);
         echo '</a></span> ';
-
-        if ( $return = glossary_print_entry_commentslink($course, $cm, $glossary, $entry,$mode,$hook, 'html') ) {
-            echo "($return)";
+        echo '</td><td align="right">';
+        if ($printicons) {
+            glossary_print_entry_icons($course, $cm, $glossary, $entry, $mode, $hook,'print');
         }
-
+        if ($ratings) {
+            glossary_print_entry_ratings($course, $entry, $ratings);
+        }
         echo '<br />';
     } else {
         echo '<center>';
