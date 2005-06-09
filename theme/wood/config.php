ab8734e816605d7fee1ec51c768276c2fb65e099
moodler <moodler>
2005-06-09 09:23:47 +0000

    Removed moz styles

--- theme/wood/config.php
+++ theme/wood/config.php
@@ -13,7 +13,7 @@ $THEME->sheets = array('styles_color');
 ////////////////////////////////////////////////////////////////////////////////
 
 
-$THEME->standardsheets = array('styles_layout','styles_fonts','styles_color','styles_moz');  
+$THEME->standardsheets = array('styles_layout','styles_fonts','styles_color');  
 
 /// This variable can be set to an array containing
 /// filenames from the *STANDARD* theme.  If the 
