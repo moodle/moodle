7f95e892a17a3e5104ce6c90ea92774cc37e5843
havran <havran>
2005-06-10 10:47:15 +0000

    updates and minor changes from Martina Majorova

--- lang/sk/enrol_internal.php
+++ lang/sk/enrol_internal.php
@@ -1,10 +1,10 @@
 <?PHP // $Id$ 
-      // enrol_internal.php - created with Moodle 1.5 ALPHA (2005051500)
+      // enrol_internal.php - created with Moodle 1.6 development (2005060201)
 
 
 $string['description'] = 'Toto je ¹tandardný formulár na zápis do kurzu. Existujú dva hlavné spôsoby, ako sa mô¾e ¹tudent zapísa» do kurzu.
 <ul>
-<li>Uèiteµ alebo správca ich mô¾e ruène zapísa», pou¾itím odkazu v menu Administrácia kurzu v rámci toho kurzu.</li>
+<li>Uèiteµ alebo administrátor ich mô¾e ruène zapísa», pou¾itím odkazu v menu Administrácia kurzu v rámci toho kurzu.</li>
 <li>Niektoré kurzy mô¾u vy¾adova» pri vstupe heslo, tzv. \"Kµúè pre zápis do kurzu\". Ktokoµvek ho pozná, sa mô¾e sám zapísa» do kurzu.</li>
 </ul>';
 $string['enrolname'] = 'Interný zápis do kurzu';
--- lang/sk/enrol_paypal.php
+++ lang/sk/enrol_paypal.php
@@ -1,11 +1,11 @@
 <?PHP // $Id$ 
-      // enrol_paypal.php - created with Moodle 1.5 ALPHA (2005051500)
+      // enrol_paypal.php - created with Moodle 1.6 development (2005060201)
 
 
 $string['business'] = 'Emailová adresa Vá¹ho obchodného Paypal úètu';
-$string['costorkey'] = 'Prosím vyberte si jednu z metód zápisu.';
-$string['description'] = 'Modul \"Platba cez Paypal\" Vám umo¾òuje nastavova» platené kurzy. Ak je cena kurzu nastavená na nulu, nie sú záujemcovia o zapísanie do kurzu vyzývaní k ¾iadnej platbe. Mô¾ete rovnako nastavi» východziu cenu v¹etkých kurzov na Va¹ich stránkách a ceny jednotlivých kurzov potom nastavova» individuálne. Cena nastavená v kurze má prednos» pred cenou nastavenou pre celé stránky.';
+$string['costorkey'] = 'Prosím vyberte si jednu zo spôsobov zápisu do kurzu.';
+$string['description'] = 'Modul \"Platba cez Paypal\" Vám umo¾òuje nastavova» platené kurzy. Ak je cena kurzu nastavená na nulu, nie sú záujemcovia o zapísanie do kurzu vyzývaní k ¾iadnej platbe. Mô¾ete rovnako nastavi» východziu cenu v¹etkých kurzov na Va¹ich stránkách a ceny jednotlivých kurzov potom nastavova» individuálne. Cena nastavená v kurze má prednos» pred jednotnou cenou nastavenou pre v¹etky stránky.';
 $string['enrolname'] = 'Platba cez Paypal';
-$string['sendpaymentbutton'] = 'Posla» platbu cez slu¾bu Paypal';
+$string['sendpaymentbutton'] = 'Odosla» platbu cez slu¾bu Paypal';
 
 ?>
