43759f54bf27f7b3469dfe19aaa99b1544d55e3f
lonecatz <lonecatz>
2005-06-10 15:16:50 +0000

    documents converted from zh_cn_utf8

---
+++ lang/zh_cn/docs/files.php
@@ -0,0 +1,27 @@
+<?PHP // $Id$
+
+
+
+$string['-about'] = '关于Moodle';
+$string['intro.html'] = '简介';
+$string['background.html'] = '背景';
+$string['philosophy.html'] = '理念';
+$string['licence.html'] = '授权';
+$string['features.html'] = '特性';
+$string['release.html'] = '发行说明';
+$string['future.html'] = '展望';
+$string['credits.html'] = '致谢';
+$string['-installation'] = '管理';
+$string['install.html'] = '安装';
+$string['faq.html'] = '安装FAQ';
+$string['installamp.html'] = 'Apache, MySQL, PHP';
+$string['upgrade.html'] = '升级';
+$string['-usage'] = '使用Moodle';
+$string['teacher.html'] = '教师手册';
+$string['other.html'] = '其它文档';
+$string['-development'] = '开发';
+$string['developer.html'] = '开发文档';
+$string['coding.html'] = '编码准则';
+$string['cvs.html'] = '使用CVS';
+$string['translation.html'] = '翻译';
+?>
