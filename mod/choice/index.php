07bc47e0e5295961024481278efa38f51ec9e7e7
thepurpleblob <thepurpleblob>
2005-06-10 11:09:39 +0000

    require_variable() replaced with required_param()

--- mod/choice/index.php
+++ mod/choice/index.php
@@ -3,7 +3,7 @@
     require_once("../../config.php");
     require_once("lib.php");
 
-    require_variable($id);   // course
+    $id = required_param('id',0,PARAM_INT);   // course
 
     if (! $course = get_record("course", "id", $id)) {
         error("Course ID is incorrect");
