c89312d2a2337c7c28aa5d78dc4bce57b2afab12
lonecatz <lonecatz>
2005-06-10 17:28:17 +0000

    first translation

---
+++ lang/zh_cn_utf8/quiz_analysis.php
@@ -0,0 +1,16 @@
+<?PHP // $Id$ 
+      // quiz_analysis.php - created with Moodle 1.5 (2005060201)
+
+
+$string['analysisoptions'] = '分析选项';
+$string['analysistitle'] = '单项分析表';
+$string['attemptshighest'] = '最高成绩';
+$string['downloadooo'] = '以OpenOffice格式下载';
+$string['lowmarkslimit'] = '成绩少于多少时则不分析：';
+$string['pagesize'] = '每页问题数：';
+$string['qnametitle'] = '问题名称';
+$string['qtexttitle'] = '问题内容';
+$string['reportanalysis'] = '单项分析报告';
+$string['responsestitle'] = '答案内容';
+
+?>
