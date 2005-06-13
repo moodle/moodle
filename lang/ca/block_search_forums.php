f5897334fb1fb574ac8504d73425083728601fd6
moodler <moodler>
2005-06-13 06:34:20 +0000

    file block_search_forums.php was added on branch MOODLE_15_STABLE on 2005-07-07 15:45:47 +0000

--- lang/ca/block_search_forums.php
+++
@@ -1,8 +0,0 @@
-<?PHP // $Id$ 
-      // block_search_forums.php - created with Moodle 1.5 Beta (2005052300)
-
-
-$string['advancedsearch'] = 'Cerca avançada';
-$string['blocktitle'] = 'Cerca fòrums';
-
-?>
