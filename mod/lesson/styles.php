09ade8ac0567c0b64a232f769a0b5c57ac330899
urs_hunkler <urs_hunkler>
2005-06-15 19:28:12 +0000

    Merged from 1.5: corrected the CSS. It is not enough to set the outer class once at the begining of a comma separated list of CSS stylings. Every item must contain the full series of classes or ids, only the properties are following once.

--- mod/lesson/styles.php
+++ mod/lesson/styles.php
@@ -10,7 +10,9 @@
   padding-top:40px; margin-top:40px;
 }
  
-.mod-lesson .footer a:link, .footer v:link, .footer a:visited { 
+.mod-lesson .footer a:link, 
+.mod-lesson .footer v:link, 
+.mod-lesson .footer a:visited { 
 }
 
 .mod-lesson .leftmenutable {
@@ -20,12 +22,19 @@
   vertical-align:top;
 }
 
-.mod-lesson .leftmenu a:link, .leftmenu a:active, .leftmenu a:visited, .leftmenu1 a:link, .leftmenu1 a:active, .leftmenu1 a:visited{
+.mod-lesson .leftmenu a:link, 
+.mod-lesson .leftmenu a:active, 
+.mod-lesson .leftmenu a:visited, 
+.mod-lesson .leftmenu1 a:link, 
+.mod-lesson .leftmenu1 a:active, 
+.mod-lesson .leftmenu1 a:visited{
   font-size:.7em;
   vertical-align:top;
 }
 
-.mod-lesson .main a:link, .main a:active, .main a:visited{
+.mod-lesson .main a:link, 
+.mod-lesson .main a:active,
+.mod-lesson .main a:visited{
   font-size:.9em; 
   vertical-align:top;
 }
