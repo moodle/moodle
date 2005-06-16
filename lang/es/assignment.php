91180ebfdd4c4d645d0c3e796d8e1a211a28e3b1
barias <barias>
2005-06-16 21:42:59 +0000

    Minor updates in spanish translation

--- lang/es/assignment.php
+++ lang/es/assignment.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.6 development (2005052400)
+      // assignment.php - created with Moodle 1.5 Beta (2005052300)
 
 
 $string['allowresubmit'] = 'Permitir reenvío';
@@ -38,6 +38,7 @@ $string['failedupdatefeedback'] = 'Fallo al actualizar el comentario dirigido a 
 $string['feedback'] = 'Comentario';
 $string['feedbackfromteacher'] = 'Comentarios del $a';
 $string['feedbackupdated'] = 'Comentario actualizado para $a personas';
+$string['guestnoupload'] = 'Lo sentimos, los invitados no pueden realizar envíos.';
 $string['helpoffline'] = '<p>Esto resulta útil cuando la tarea se realiza fuera de Moodle, e.g., en algún lugar de internet o personalmente.</p><p>Los estudiantes pueden ver una descipción de la tarea, pero no pueden subir archivos de ninguna clase al servidor. Las calificaciones funcionan normalmente, y los estudiantes recibirán notificaciones sobre la calificación obtenida.</p>';
 $string['helponline'] = '<p>Esta clase de tareas pide a los usuarios que editen un texto, utilizando las herramientas de edición habituales. Los profesores pueden calificarlas en línea, e incluso incorporar comentarios o cambios.</p><p>(Si usted está familiarizado con versiones anteriores de Moodle, este tipo de tarea hace lo mismo que hacía el módulo Diario)</p>';
 $string['helpuploadsingle'] = '<p>Este tipo de tarea permite a los participantes subir un solo archivo de cualquier tipo.</p><p>Podría ser un documento procesado en Word, o una imagen, un sitio web comprimido, o cualquier cosa que les pida que envíen.</p>';
