770d88ced5a276196efee88f7a15904ad6cc8965
stronk7 <stronk7>
2005-06-18 09:59:39 +0000

    Some changes added (see bug 3188).    - Added correct encoding.    - Make page XHTML 1.0 Frameset compliant Credits go to sunner_sun. (http://moodle.org/bugs/bug.php?op=show&bugid=3188)

--- doc/index.php
+++ doc/index.php
@@ -36,15 +36,19 @@
     }
 
 ?>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" 
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title><?php print_string("documentation")?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php print_string("thischarset") ?>" />
 </head>
 
 <frameset rows="70,*">
-    <frame name="top" src="top.php">
+    <frame name="top" src="top.php" />
     <frameset cols="200,*">
-        <frame name="contents" src="contents.php">
-        <frame name="main" src="index.php?file=<?php echo "$file$sub"; ?>">
+        <frame name="contents" src="contents.php" />
+        <frame name="main" src="index.php?file=<?php echo "$file$sub"; ?>" />
     </frameset>
 </frameset>
+</html>
