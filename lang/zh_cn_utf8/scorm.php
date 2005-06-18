322de0fcf35c7f96852bbf623b8b44c9c24a701f
lonecatz <lonecatz>
2005-06-18 14:35:55 +0000

    some new translation

---
+++ lang/zh_cn_utf8/scorm.php
@@ -0,0 +1,28 @@
+<?PHP // $Id$ 
+      // scorm.php - created with Moodle 1.5 + (2005060201)
+
+
+$string['browse'] = '预览';
+$string['browsed'] = '已浏览';
+$string['browsemode'] = '浏览模式';
+$string['chooseapacket'] = '选择或更新一个包';
+$string['completed'] = '已完成';
+$string['gradeaverage'] = '平均成绩';
+$string['gradehighest'] = '最高成绩';
+$string['grademethod'] = '评分方法';
+$string['gradesum'] = '总成绩';
+$string['guestsno'] = '访客不能浏览scrom课程';
+$string['height'] = '高度';
+$string['incomplete'] = '不完整';
+$string['lastaccess'] = '最后访问';
+$string['popup'] = '将当前SCO在新窗口中打开';
+$string['score'] = '成绩';
+$string['syntax'] = '语法错误';
+$string['too_many_attributes'] = '标记{$a->tag}有太多的属性';
+$string['too_many_children'] = '标记{$a->tag}有太多的子标记';
+$string['totaltime'] = '时间';
+$string['validateascorm'] = '检验一个包';
+$string['validation'] = '检验结果';
+$string['width'] = '宽度';
+
+?>
