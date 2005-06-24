e80ad3b374e868d78f8b83f0a5034dbe19d7dc12
gustav_delius <gustav_delius>
2005-06-24 07:52:26 +0000

    quoted two attributes for XHTML compliance

--- admin/index.php
+++ admin/index.php
@@ -406,8 +406,8 @@
     $table->data[] = array("<font size=\"+1\"><b><a href=\"../files/index.php?id=$site->id\">".get_string("sitefiles")."</a></b></font>",
                            "<font size=\"+1\">&nbsp;</font>".get_string("adminhelpsitefiles"));
     if (file_exists("$CFG->dirroot/$CFG->admin/$CFG->dbtype")) {
-        $table->data[] = array("<font size=+1><b><a href=\"$CFG->dbtype/frame.php\">".get_string("managedatabase")."</a></b></font>",
-                               "<font size=+1>&nbsp;</font>".get_string("adminhelpmanagedatabase"));
+        $table->data[] = array("<font size=\"+1\"><b><a href=\"$CFG->dbtype/frame.php\">".get_string("managedatabase")."</a></b></font>",
+                               "<font size=\"+1\">&nbsp;</font>".get_string("adminhelpmanagedatabase"));
     }
 
     print_table($table);
