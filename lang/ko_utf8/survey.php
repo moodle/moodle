3f0b4212a80e25ee72599c51fc019dcaa562ffad
scop22 <scop22>
2005-06-25 19:11:08 +0000

    ---------------------------------------------------------------------- Major update of the Korean UTF8 language pack -  most files are now up-to-date Modified Files:  	admin.php assignment.php auth.php calendar.php choice.php  	dialogue.php editor.php error.php exercise.php forum.php  	glossary.php hotpot.php lesson.php moodle.php quiz.php  	resource.php scheduler.php scorm.php survey.php workshop.php ----------------------------------------------------------------------

--- lang/ko_utf8/survey.php
+++ lang/ko_utf8/survey.php
@@ -1,28 +1,75 @@
-<?php // $Id$ 
-      // survey.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005031000)
+<?PHP // $Id$ 
+      // survey.php - created with Moodle 1.5 ALPHA (2005051500)
 
 
 $string['actual'] = '실적';
 $string['actualclass'] = '학급 실적';
+$string['actualstudent'] = '$a 실적';
+$string['allquestions'] = '모든 질문들을 순서대로, 모든 학생들에게';
+$string['allscales'] = '모든 등급들, 모든 학생들';
+$string['alreadysubmitted'] = '너는 이미 이 조사에 응답하였다.';
+$string['analysisof'] = ' $a 의 분석';
 $string['answers'] = '대답';
+$string['attls1'] = '평가에서 나는 그것을 표현하는 사람이 아니라 그들의 주장의 질에 초점을 두겠다.';
 $string['attls10'] = '무언가를 분석할 때에는 가능한한 객관적인 입장에 서는 것이 나에게는 중요합니다.';
+$string['attls10short'] = '목적을 유지 하시오';
 $string['attls11'] = '나는 사람들에 대항하여 생각하기 보다 그들과 함께 생각하기 위해 노력합니다.';
 $string['attls11short'] = '사람들과 같이 생각하기';
+$string['attls12'] = '나는 내가 사용하는 확실한 평가논의의 표준을 가지고 있다';
+$string['attls12short'] = '평가를 위한 평가기준을 사용하시오';
 $string['attls13'] = '나는 누군가의 의견을 평가하려 하기 보다는 더 이해할 수 있도록 노력합니다.';
 $string['attls13short'] = '이해하기';
 $string['attls14'] = '나는 다른 사람들이 그들의 주장을 명백히 하는데에 도움을 주기위해 그들의 생각에서 결점을 지적해내려 노력합니다.';
 $string['attls14short'] = '취약부분 지적';
 $string['attls15'] = '나는 논쟁거리가 되는 이슈를 다룰 때 다른 사람들이 왜 그렇게 생각하며 왜 그렇게 행동하는지를 알기 위해 내 자신을 그들의 입장에 놓아보는 경향이 있다.';
 $string['attls15short'] = '그들의 입장에 서보기';
+$string['attls16'] = '나는 모든 증거에 대해 숙고하는 것에 유의하기 때문에 어떤이들은 시험삼아 그것들을 분석하는 것을 해볼 수 있다';
+$string['attls16short'] = '시험삼아 해보다';
+$string['attls17'] = '나는 논리의 사용과 문제들을 해결할 때의 관심들의 체내화에 대한 이유의 사용을 높이 평가한다';
+$string['attls17short'] = '최상의 논리 평가';
+$string['attls18'] = '나는 감정이입에 의한 내 의견과 다른 의견에 의해서 통찰력을 획득한다';
+$string['attls18short'] = '감정이입에 의한 통찰력';
+$string['attls19'] = '내가 사람들의 의견이 외계인 같을 때 나는 나 자신을 다른 사람 안에서 확장할려고 사려깊은 노력은 하고 그들이 어떻게 그러한 의견들을 가지게 됐는지 알아볼려고 노력한다';
+$string['attls19short'] = '확장을 위해 노력하시오';
+$string['attls1short'] = '논쟁의 질에 집중하시오';
+$string['attls2'] = '나는 반박하기 놀이를 좋아한다- 반대를 말하는 사람들과 논의하기';
+$string['attls20'] = '나는 무엇이 잘못되었는지 해결하는데 시간을 쓴다. 예를 들면 나는 문학적인 해석이 충분히 잘 논의되었는지를 찾을 것이다';
 $string['attls20short'] = '무엇이 잘못되었지?';
-$string['attls2short'] = '결점만 보는 사람 역활';
+$string['attls2short'] = '결점만 보는 사람 역할';
+$string['attls3'] = '나는 다른 사람들이 어디서 왔는지 어떤 경험들이 그들을 그렇게 하는 것을 느끼게 하는지 이해하는 것을 좋아한다';
 $string['attls3short'] = '출신';
+$string['attls4'] = '내 교육의 부분에서 가장 중요한 부분은 사람들이 나와 매우 다르다는 것을 배우는 것이다';
 $string['attls4short'] = '다른 사람들 이해';
+$string['attls5'] = '나는 나만의 정체성을 성취하는 가장 좋은 방법은 다른 사람들과의 다양한 상호작용하는 것이라 느낀다';
 $string['attls5short'] = '여러방법으로 상호 작용';
-$string['attls6short'] = '의견듣는거 즐기기';
+$string['attls6'] = '나는 나와 다른 배경에서 온 사람들의 의견을 듣기를 즐긴다-이것은 나에게 같은 것이라도 어떻게 다른 방식으로 이해하게 되는지를 도와준다';
+$string['attls6short'] = '의견듣는 것 즐기시오';
+$string['attls7'] = '나는 나와 의견이 맞지 않는 사람들과 논쟁하면서 나의 위치를 강화하는 것을 발견했다';
 $string['attls7short'] = '토론하며 주장하기';
+$string['attls8'] = '나는 왜 사람들이 그들이 하는 것처럼 말하고 믿는 것을 아는것에 항상 흥미를 느낀다';
 $string['attls8short'] = '사람들이 왜 하는지 알기';
+$string['attls9'] = '나는 내가 읽은 책의 저자들과 논의하면서 내 자신을 찾는다, 논리적으로 왜 그들이 틀렸는지 이해하려고 노력한다';
 $string['attls9short'] = '작가와 논쟁하기';
+$string['attlsintro'] = '이 질문사항의 목적은 생각과 배움에 관한 너의 태도들을 평가하는데 도움을 주는 것이다.
+
+옳고 그름이 없다. 우리는 단지 너의 의견에 관심이 있다.제발 너의 응답을 높은 단계의 기밀성을 가지고 다룰수 있게 확실하게 해 주어라. 그리고 너의 평가는 작용하지 않을 것이다.';
+$string['attlsm1'] = '생각과 배움을 향한 태도들';
+$string['attlsm2'] = '연결된 배움';
+$string['attlsm3'] = '분리된 배움';
+$string['attlsmintro'] = '토론에서...';
+$string['attlsname'] = 'ATTLS (20 항목 버전)';
+$string['ciq1'] = '배우는 입장으로서 교실에서 어떤점에 어느 순간에 가장 집중하게 되어는가?';
+$string['ciq1short'] = '가장 집중한';
+$string['ciq2'] = '배우는 입장으로서 교실에서 어떤점에 어느 순간에 가장 집중 하지않게 되었는가?';
+$string['ciq2short'] = '가장 먼';
+$string['ciq3'] = '그 포럼안에서 너가 찾은 가장 확실하거나 도움되는 것은 어떤 행동인가?';
+$string['ciq3short'] = '도움이 되는 순간';
+$string['ciq4'] = '그 포럼안에서 너가 찾은 가장 당황스럽거나 혼동되는 것은 어떤 행동인가?';
+$string['ciq4short'] = '혼동된 순간';
+$string['ciq5'] = '어떤 사건에 가장 놀랐습니까?';
+$string['ciq5short'] = '놀란 순간';
+$string['ciqintro'] = '이 교실에서  최근 이벤트에 대해 생각해 볼 때, 아래의 질문에 답하시오';
+$string['ciqname'] = '비판적 사건들';
 $string['clicktocontinue'] = '계혹하려면 이곳을 클릭';
 $string['clicktocontinuecheck'] = '확인과 계속하길 원하면 이곳을 클릭';
 $string['colles1'] = '흥미있는 주제에 관심을 둔다';
@@ -38,31 +85,109 @@ $string['colles14'] = '지도 교사라 나를 참여하게끔 용기를 북돋�
 $string['colles14short'] = '나에게 용기를 북돋아 준다.';
 $string['colles15'] = '지도 교사가 좋은 예시를 들어 설명해준다.';
 $string['colles15short'] = '좋은 예시를 들어준다.';
+$string['colles16'] = '그 가정교사는 비판적 자기투영의 모범이다';
+$string['colles16short'] = '가정교사는 자기투영의 모범이다';
+$string['colles17'] = '다른 학생들은 나의 참여에 용기를 얻는다';
+$string['colles17short'] = '학생들이 나에게 용기를 줍니다';
+$string['colles18'] = '다른 학생들이 나의 노력을 칭찬합니다';
+$string['colles18short'] = '학생들이 나를 칭찬합니다';
+$string['colles19'] = '다른 학생들이 나의 노력에 가치를 둡니다';
+$string['colles19short'] = '학생들이 나에게 가치를 둡니다';
+$string['colles1short'] = '흥미있는 일에 집중하시오';
+$string['colles2'] = '내가 배운것은 나의 교수 연습을 위해 중요하다';
+$string['colles20'] = '다른 학생들은 배우기 위한 노력에 공감한다.';
+$string['colles20short'] = '학생들이 열정이 있습니다';
+$string['colles21'] = '나는 다른 학생들의 이야기들을 잘 이해한다. ';
+$string['colles21short'] = '다른 학생들을 이해합니다';
+$string['colles22'] = '다른 학생들은 자신들의 이야기를 잘 이해한다. ';
+$string['colles22short'] = '다른 학생들아 나를 이해합니다';
+$string['colles23'] = '나는 선생님의 이야기를 잘 이해한다. ';
+$string['colles23short'] = '나는 선생님을 이해한다. ';
+$string['colles24'] = '선생님은 자신들의 이야기를 잘 이해한다. ';
+$string['colles24short'] = '선생님은 나를 이해한다. ';
+$string['colles2short'] = '중요한 나의 실행';
+$string['colles3'] = '나는 나의 전문적인 실행을 향상하는 법을 배운다.';
+$string['colles3short'] = '나의';
+$string['colles4'] = '내가 배우는 것은 나의 전문적인 실행으로 잘 연결해 준다. ';
+$string['colles4short'] = '나의 실행으로 연결한다. ';
+$string['colles5'] = '나는 어떻게 내가 배우는 지에 관해 비판적으로 생각한다. ';
+$string['colles5short'] = '나는 나의 학문에 비판적이다. ';
+$string['colles6'] = '나는 나만의 생각에 대해 비판적으로 생각한다. ';
+$string['colles6short'] = '나는 나만의 생각에 비판적이다. ';
+$string['colles7'] = '나는 다른 학생들의 생각에 대해 비판적으로 생각한다. ';
+$string['colles7short'] = '나는 다른 학생들에게 비판적이다. ';
+$string['colles8'] = '나는 독서시 그 생각들에 대해 비판적으로 생각한다.';
+$string['colles8short'] = '나는 독서를 비판적으로 한다. ';
+$string['colles9'] = '나는 다른 학생들에게 나의 생각들을 설명한다. ';
+$string['colles9short'] = '나는 나의 생각들을 설명한다. ';
+$string['collesaintro'] = '이 조사의 목적은 이 단원을 배우는데 있어 우리들이 온라인 송신을 잘 이해하는 것을 도와주는 데 있습니다.
+
+아래의 각각 24개의 문항은 이 단원에서의 당신의 경험을 묻고있습니다.
+
+답에는 맞고 틀림이 없으며, 오직 당신의 의견에 관심을 두고 있습니다. 당신의 응답은 완벽하게 비밀을 보장될것을 약속드리며, 이 응답들은 당신의 성적에 전혀 영향을 미치지 않을것입니다. ';
+$string['collesaname'] = '사실상';
+$string['collesapintro'] = '이 질문사항의 목적은 이 단원을 배우는데 있어 우리들이 온라인 송신을 잘 이해하는 것을 도와주는 데 있습니다.
+
+아래의 각각 24개의 문항은 이 단원에서 당신의 우선권(이상)과 실제상의 경험을 비교하는 것을 묻고있습니다.
+
+답에는 맞고 틀림이 없으며, 오직 당신의 의견에 관심을 두고 있습니다. 당신의 응답은 철저하게 비밀을 보장될것을 약속드리며, 이 응답들은 당신의 성적에 전혀 영향을 미치지 않을것입니다. ';
+$string['collesapname'] = '우선적으로 그리고 사실상';
+$string['collesm1'] = '적합성';
+$string['collesm1short'] = '적합성';
+$string['collesm2'] = '반사적인 사고';
+$string['collesm2short'] = '반사적인 사고';
+$string['collesm3'] = '쌍방향 참가';
+$string['collesm3short'] = '쌍방향 참가';
+$string['collesm4'] = '선생님의 지지';
+$string['collesm4short'] = '선생님의 지지';
+$string['collesm5'] = '동료의 지지';
+$string['collesm5short'] = '동료의 지지';
+$string['collesm6'] = '해석';
+$string['collesm6short'] = '해석';
+$string['collesmintro'] = '이번 온라인의 단원';
+$string['collespintro'] = '이 조사의 목적은 당신이 온라인 학습 경험에서 가치를 두고 있는 것은 무엇인가에 대해 이해하는 것을 도와주는데 있습니다.
+
+아래 각각의 24개의 문항은 이 단원에서의 당신의 <b>우선의</b>(이상) 경험을 묻고 있습니다. 
+
+답에는 맞고 틀림이 없으며, 오직 당신의 의견에 관심을 두고 있습니다. 당신의 응답은 철저하게 비밀을 보장될것을 약속드리며, 이 응답들은 당신의 성적에 전혀 영향을 미치지 않을것입니다. 
+
+감사합니다.';
+$string['collespname'] = 'COLLES (우선의)';
 $string['done'] = '완료';
 $string['download'] = '다운로드';
 $string['downloadexcel'] = '엑셀 스프레드 형식으로 다운로드';
-$string['downloadinfo'] = '.';
+$string['downloadinfo'] = '당신은 엑셀, SPSS 또는 다른 패키지에서의 분석법을 통해 이 조사의 원문 그대로의 데이터 완성을 다운로드 받을 수 있습니다.';
 $string['downloadtext'] = '문서파일 형태로 다운로드';
 $string['editingasurvey'] = '조사 편집';
 $string['guestsnotallowed'] = '손님은 survey 제출이 허락되지 않았습니다.';
+$string['helpsurveys'] = '조사의 다른 형태들을 도와줍니다.';
 $string['howlong'] = '이 조사를 마치는데 시간이 얼마나 걸렸습니까?';
 $string['howlongoptions'] = '1분 미만, 1-2분, 2-3분, 3-4분, 4-5분, 5-10분, 10분 초과';
 $string['ifoundthat'] = '을 발견하다.';
+$string['introtext'] = '소개글';
 $string['ipreferthat'] = '을 더 좋아하다.';
 $string['modulename'] = '조사';
 $string['modulenameplural'] = '조사';
 $string['name'] = '이름';
+$string['newsurveyresponses'] = '새 조사 응답';
 $string['nobodyyet'] = '아무도 이 조사를 아직 끝마치지 않았습니다.';
 $string['notdone'] = '아직 되지 않았음';
+$string['notes'] = '당신의 개인적 분석과 기록';
 $string['othercomments'] = '다른 코멘트가 있습니까?';
+$string['peoplecompleted'] = '$a 다음의 이 조사를 막 완성하였다.';
+$string['preferred'] = '우선권의';
+$string['preferredclass'] = '학급 우선권의';
+$string['preferredstudent'] = '$a 우선권의';
 $string['question'] = '질문';
 $string['questions'] = '질문들';
 $string['questionsnotanswered'] = '몇몇의 선택형 질문들이 답해지지 않았습니다.';
 $string['report'] = '조사 리포트';
 $string['savednotes'] = '저장되었습니다.';
 $string['scaleagree5'] = '전적으로 동의하지 않음, 조금 동의하지 않음, 중립, 조금 동의함, 전적으로 동의함. ';
+$string['scales'] = '등급';
 $string['scaletimes5'] = '전혀, 아주가끔, 가끔, 종종, 매번';
 $string['seemoredetail'] = '더 많은 정보를 보기 원하시면 이곳을 클릭하시오.';
+$string['selectedquestions'] = '모든학생들은 등급에 따라 질문을 선택하십시오.';
 $string['summary'] = '요약';
 $string['surveycompleted'] = '당신은 이미 이것을 끝마쳤습니다. 밑에 보이는 그래프는 평균 점수에 관한 당신의 결과를 요약적으로 보여주는 것 입니다. ';
 $string['surveyname'] = '조사 이름';
@@ -70,6 +195,6 @@ $string['surveysaved'] = '조사가 저장되었음';
 $string['surveytype'] = '조사 유형';
 $string['thanksforanswers'] = '조사에 응답해 주셔서 감사합니다.';
 $string['time'] = '시간';
-$string['viewsurveyresponses'] = 'View $a survey responses';
+$string['viewsurveyresponses'] = '견해 $a 조사 응답';
 
 ?>
