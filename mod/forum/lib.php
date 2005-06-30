7f65461c36cc588df983fd1173ae471ee6391f55
defacer <defacer>
2005-06-30 00:03:03 +0000

    Merging from STABLE:
    Moving <i> to CSS and small CSS improvement.

--- mod/forum/lib.php
+++ mod/forum/lib.php
@@ -2290,7 +2290,7 @@ function forum_update_post($post,&$message) {
         $data->date = userdate($post->modified);
         $editedby = get_string('editedby', 'forum', $data);
         if ($post->format == FORMAT_HTML) {
-            $post->message .= '<p>(<i><span class="edited">'.$editedby.'</span></i>)</p>';
+            $post->message .= '<p>(<span class="edited">'.$editedby.'</span>)</p>';
         } else {
             $post->message .= "\n\n(".$editedby.')';
         }
