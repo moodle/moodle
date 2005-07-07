d00727cc528152fea937bb075c67e8185cb31315
moodler <moodler>
2005-07-07 15:57:12 +0000

    Added from HEAD

---
+++ lang/km_utf8/admin.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // admin.php - created with Moodle 1.4.4 (2004083140)
+
+
+$string['adminseesallevents'] = 'រាល់អ្នកគ្រប់គ្រងមើលឃើញរាល់ព្រិត្តិការណ៏';
+$string['adminseesownevents'] = 'អ្នកគ្រប់គ្រងទាំងអស់ដូចជាអ្នកប្រើប្រាស់ដែរ';
+$string['calendarsettings'] = 'ប្រតិទិន';
+$string['change'] = 'ប្តូរ';
+$string['configlang'] = 'ជ្រើសរើសភាសាតាមលំនាដើម​សំរាប់គេហទំព័រទាំងមូល។​អ្នកប្រើប្រាស់អាចបញ្ជាន់លើវា។';
+$string['configvariables'] = 'អថេរ';
+$string['confirmation'] = 'បញ្ជាក់';
+$string['sitemaintenancemode'] = 'ទម្រង់នៃការថែរក្សា';
+$string['therewereerrors'] = 'មានការខុសឆ្គងក្នុងទិន្នន័យរបស់អ្នក';
+
+?>
---
+++ lang/km_utf8/attendance.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // attendance.php - created with Moodle 1.4.4 (2004083140)
+
+
+$string['absentlong'] = 'អវត្តមាន';
+$string['presentlong'] = 'វត្តមាន';
+
+?>
