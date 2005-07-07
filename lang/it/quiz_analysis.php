1e41ee920eb5e5554a6ae6a8a84b842b2e07912d
moodler <moodler>
2005-07-07 16:14:38 +0000

    Adding new files from HEAD

---
+++ lang/it/quiz_analysis.php
@@ -0,0 +1,32 @@
+<?php // $Id$
+      // quiz.php - created with Moodle 1.4 development (2004080300)
+
+$string['analysisoptions'] = 'Opzioni di analisi';
+$string['attemptselection'] = 'Tentativi da analizzare per utente ';
+$string['lowmarkslimit'] = "Non analizzare se il punteggio è minore di: ";
+$string['attemptsall'] = 'tutti i tentativi';
+$string['attemptshighest'] = 'voto più alto';
+$string['attemptsfirst'] = 'primo tentativo';
+$string['attemptslast'] = 'ultimo tentativo';
+$string['pagesize'] = 'Domande per pagina: ';
+$string['analysistitle'] = 'Tabella per l\'analisi dei risultati';
+$string['reportanalysis'] = 'Analisi dei risultati';
+$string['downloadooo'] = 'Download in formato OpenOffice';
+$string['qidtitle'] = 'D#';
+$string['qnametitle'] = 'Nome domanda';
+$string['qtexttitle'] = 'Testo domanda';
+$string['qcounttitle'] = 'Conteggio dom.';
+$string['qtypetitle'] = 'Tipo dom.';
+$string['responsestitle'] = "Testo risposta";
+$string['rfractiontitle'] = 'Credito<br />parziale';
+$string['rcounttitle'] = 'Num.<br />giuste';
+$string['rpercenttitle'] = '%%<br />giuste';
+$string['facilitytitle'] = '%% Giuste<br>Facilità';
+$string['stddevtitle'] = 'Deviaz.<br />standard';
+$string['dicsindextitle'] = 'Indice<br />discrim.';
+$string['disccoefftitle'] = 'Coeff.<br />discrim.';
+$string['quizreportdir'] = 'risultati_quiz';
+
+
+
+?>
