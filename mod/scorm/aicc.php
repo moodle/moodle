7f2f2b2d1a2f07711eb898c6009c9c15d90e5d62
bobopinna <bobopinna>
2005-07-07 09:24:24 +0000

    Fixed a typo

--- mod/scorm/aicc.php
+++ mod/scorm/aicc.php
@@ -42,7 +42,7 @@
                             $userdata = $usertrack;
                         } else {
                             $userdata->status = '';
-                            $userdata->scorre_raw = '';
+                            $userdata->score_raw = '';
                         }
                         $userdata->student_id = $USER->username;
                         $userdata->student_name = $USER->lastname .', '. $USER->firstname;
