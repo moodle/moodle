f3df500ba7a5f8f4b8544d4b89871452b9a93069
papita <papita>
2005-07-08 13:32:29 +0000

    updating by paula

--- lang/pt_br/hotpot.php
+++ lang/pt_br/hotpot.php
@@ -1,12 +1,82 @@
 <?PHP // $Id$ 
-      // hotpot.php - created with Moodle 1.4.1 (2004083101)
+      // quiz.php - created with Moodle 1.2 development (2003111400)
 
+//translators:  You might want to leave the first two items 'as is' in English
+$string['modulename'] = 'Atividade Hot Potatoes ';
+$string['modulenameplural'] = 'Atividades Hot Potatoes';
 
-$string['clearall'] = 'Limpar tudo';
-$string['deleteall'] = 'Cancelar tudo';
-$string['modulename'] = 'Quiz Hot Potatoes';
-$string['modulenameplural'] = 'Quizes Hot Potatoes';
-$string['really'] = 'Você quer cancelar todos os resultados deste quiz?';
-$string['strattemptlabel'] = 'Tentativas: Pontuação, Data Hora (Duração), [Penalidades]';
+// for mod.html
+$string['outputformat'] = 'Formato de publicação';
+$string['outputformat_best'] = 'melhor';
+$string['outputformat_v6_plus'] = 'v6+';
+$string['outputformat_v6'] = 'v6';
+$string['outputformat_v5'] = 'v5';
+$string['outputformat_v4'] = 'v4';
+$string['outputformat_v3'] = 'v3';
+$string['outputformat_flash'] = 'Flash';
+$string['outputformat_mobile'] = 'portátil';
+
+$string['navigation'] = 'Navegação';
+$string['navigation_bar'] = 'Barra de navegação do Moodle';
+$string['navigation_frame'] = 'Frame de navegação do Moodle';
+$string['navigation_iframe'] = '&lt;IFRAME&gt; embutido';
+$string['navigation_buttons'] = 'Botões da atividade Hot Potatoes';
+$string['navigation_give_up'] = 'Um botão simples &quot;Desistir&quot;';
+$string['navigation_none'] = 'Nenhum';
+
+$string['giveup'] = 'Desistir';
+$string['location'] = 'Local do arquivo';
+$string['shownextquiz'] = 'Mostrar a próxima atividade';
+$string['forceplugins'] = "Forçar plugins para multimedia";
+
+$string['resultssaved'] = 'Os resultados da atividade foram gravados';
+
+// for edit.php and show.php
+$string['filetype'] = 'Tipo de arquivo';
+$string['quiztype'] = 'Tipo de atividade';
+$string['showxmlsource'] = 'Mostrar código XML';
+$string['showxmltree'] = 'Mostrar árvore XML';
+$string['showhtmlsource'] = 'Mostrar código HTML';
+$string['enterafilename'] = 'Escreva o nome do arquivo';
+
+// show.php (javascript messages, so must be double escaped. i.e. "it's" ==> 'it\\\'s' OR "it\\'s")
+$string['copytoclipboard'] = 'Copiar';
+$string['copiedtoclipboard'] = 'O conteúdo desta página foi copiado';
+
+// lib.php
+$string['noactivity'] = 'Nenhuma atividade';
+$string['inprogress'] = 'em andamento';
+$string['abandoned'] = 'abandonada';
+
+$string['correct'] = 'Certo';
+$string['ignored'] = 'Ignorado';
+$string['wrong'] = 'Errado';
+$string['score'] = 'Resultado';
+$string['weighting'] = 'Calculando o peso';
+$string['hints'] = 'Dicas'
+$string['clues'] = 'Indícios';
+$string['checks'] = 'Controles';
+$string['penalties'] = 'Penalidades';
+
+// report.php
+$string['deletedattempts'] = '$a tentativas foram canceladas';
+$string['deletedresponses'] = '$a respostas foram canceladas';
+$string['selectattempt'] = 'Selecionar uma tentativa';
+
+$string['thiscourse'] = 'este curso';
+$string['allmycourses'] = 'todos os meus cursos';
+$string['bestattempt'] = 'melhor tentativa';
+$string['allattempts'] = 'todas as tentativas';
+
+// report/*/report.php
+$string['average'] = 'Media';
+$string['questionshort'] = 'Q-$a';
+
+// report/overview/report.php
+$string['deleteabandoned'] = 'Cancelar abandonadas';
+$string['deleteabandonedcheck'] = 'tem certeza que quer cancelar todas as $a tentativas abandonadas?';
+
+// review.php
+$string['noresponses'] = 'Nenhuma informação encontrada sobre questões e respostas individuais.';
 
 ?>
