97fc2f34ec576ac54b36852f342554f5621a5736
mits <mits>
2005-07-08 01:24:56 +0000

    polished. Thanks Toshihiro KITA ( http://t-kita.net/ ), it's a great "point out".

--- lang/ja_utf8/moodle.php
+++ lang/ja_utf8/moodle.php
@@ -466,15 +466,11 @@ $string['gdnot'] = 'GD未インストール';
 $string['go'] = 'Go';
 $string['gpl'] = 'Copyright (C) 1999-2005 Martin Dougiamas (http://moodle.com)
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details:';
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details:
+
+http://www.gnu.org/copyleft/gpl.html';
 $string['grade'] = '評点';
 $string['grades'] = '評定';
 $string['group'] = 'グループ';
