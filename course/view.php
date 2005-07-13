892c082545a2c5c99d4c3ec9bc6a9571a99e2b8b
dhawes <dhawes>
2005-07-13 01:51:30 +0000

    converted some double quotes to single quote literals

--- course/view.php
+++ course/view.php
@@ -34,8 +34,8 @@
 
     //If course is hosted on an external server, redirect to corresponding
     //url with appropriate authentication attached as parameter 
-    if (file_exists($CFG->dirroot ."/course/externservercourse.php")) {
-        include $CFG->dirroot ."/course/externservercourse.php";
+    if (file_exists($CFG->dirroot .'/course/externservercourse.php')) {
+        include $CFG->dirroot .'/course/externservercourse.php';
         if (function_exists(extern_server_course)) {
             if ($extern_url = extern_server_course($course)) {
                 redirect($extern_url);
@@ -45,7 +45,7 @@
 
     require_once($CFG->dirroot.'/calendar/lib.php');    /// This is after login because it needs $USER
 
-    add_to_log($course->id, "course", "view", "view.php?id=$course->id", "$course->id");
+    add_to_log($course->id, 'course', 'view', "view.php?id=$course->id", "$course->id");
 
     $course->format = clean_param($course->format, PARAM_ALPHA);
     if (!file_exists($CFG->dirroot.'/course/format/'.$course->format.'/format.php')) {
@@ -81,7 +81,7 @@
         if (!empty($section)) {
             if (!empty($move) and confirm_sesskey()) {
                 if (!move_section($course, $section, $move)) {
-                    notify("An error occurred while moving a section");
+                    notify('An error occurred while moving a section');
                 }
             }
         }
@@ -89,10 +89,10 @@
         $USER->editing = false;
     }
 
-    $SESSION->fromdiscussion = "$CFG->wwwroot/course/view.php?id=$course->id";
+    $SESSION->fromdiscussion = $CFG->wwwroot .'/course/view.php?id='. $course->id;
 
     if ($course->id == SITEID) {      // This course is not a real course.
-        redirect("$CFG->wwwroot/");
+        redirect($CFG->wwwroot .'/');
     }
 
     $PAGE->print_header(get_string('course').': %fullname%');
@@ -103,25 +103,25 @@
 
     if (! $sections = get_all_sections($course->id)) {   // No sections found
         // Double-check to be extra sure
-        if (! $section = get_record("course_sections", "course", $course->id, "section", 0)) {
+        if (! $section = get_record('course_sections', 'course', $course->id, 'section', 0)) {
             $section->course = $course->id;   // Create a default section.
             $section->section = 0;
             $section->visible = 1;
-            $section->id = insert_record("course_sections", $section);
+            $section->id = insert_record('course_sections', $section);
         }
         if (! $sections = get_all_sections($course->id) ) {      // Try again
-            error("Error finding or creating section structures for this course");
+            error('Error finding or creating section structures for this course');
         }
     }
 
     if (empty($course->modinfo)) {       // Course cache was never made
         rebuild_course_cache($course->id);
-        if (! $course = get_record("course", "id", $course->id) ) {
+        if (! $course = get_record('course', 'id', $course->id) ) {
             error("That's an invalid course id");
         }
     }
 
-    require("$CFG->dirroot/course/format/$course->format/format.php");  // Include the actual course format
+    require($CFG->dirroot .'/course/format/'. $course->format .'/format.php');  // Include the actual course format
 
     echo '</div>';  // content wrapper end
     print_footer(NULL, $course);
