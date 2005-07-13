b6c810262550704fbb75864c0d346de3bdc4a671
dlnsk <dlnsk>
2005-07-13 08:12:12 +0000

    Added

---
+++ lang/ru/message.php
@@ -0,0 +1,64 @@
+<?PHP // $Id$ 
+      // message.php - created with Moodle 1.5.1 (2005060210)
+
+
+$string['addcontact'] = 'Добавить собеседника';
+$string['addsomecontacts'] = 'Вы можете поискать кого-либо, используя <a href=\"$a\">закладку поиска</a> в верху этой страницы. Найденого человека Вы сможете добавить себе в список собеседников на эту страницу. Также любому найденому человеку Вы можете сразу послать сообщение.';
+$string['addsomecontactsincoming'] = 'Эти сообщения получены от людей, не добавленых в Ваш список собеседников. Чтобы добавить их, нажимайте значки \"Добавить собеседника\" около их имён.';
+$string['ago'] = '$a назад';
+$string['allmine'] = 'В сообщениях и мне, и от меня';
+$string['allstudents'] = 'В сообщених между учениками курса';
+$string['allusers'] = 'Во всех сообщения всех пользователей';
+$string['beepnewmessage'] = 'Издавать звук, когда приходят новые сообщения (если мой броузер это сумеет). Может потребовать установки плагина типа QuickTime для воспроизведения звука';
+$string['blockcontact'] = 'Блокировать сообщения от этого человека';
+$string['blockedmessages'] = '$a сообшений от/к заблокированых пользователей';
+$string['blocknoncontacts'] = 'Не принимать сообщения от людей, которых я не добавлял в список собеседников';
+$string['contactlistempty'] = 'Ваш список собеседников сейчас пуст';
+$string['contacts'] = 'Собеседники';
+$string['context'] = 'смотреть в контексте';
+$string['deletemessagesdays'] = 'Хранить сообщения, дней';
+$string['discussion'] = 'Дискуссия';
+$string['emailmessages'] = 'Дублировать приходящие сообщения мне по почте, если я не открывал новых страниц сайта...';
+$string['emailtagline'] = 'Это письмо - копия сообщения, отправленного Вам \"$a\"';
+$string['emptysearchstring'] = 'Вы не указали, что именно Вы ищете';
+$string['formorethan'] = '... в течение последних';
+$string['includeblockedusers'] = 'Включая сообщения заблокированных собеседников';
+$string['incomingcontacts'] = 'Присланный список собеседников ($a)';
+$string['keywords'] = 'Слова';
+$string['keywordssearchresults'] = 'Найдено сообщений: $a';
+$string['maxmessages'] = 'Максимальное количество сообщений, отображаемое в истории дискуссии';
+$string['message'] = 'Сообщение';
+$string['messagehistory'] = 'История сообщений';
+$string['messages'] = 'Обмен сообщениями';
+$string['messaging'] = 'Обмен сообщениями';
+$string['mycontacts'] = 'Мои собеседники';
+$string['newsearch'] = 'Искать заново';
+$string['nomessages'] = 'Нет ожидающих сообщений';
+$string['nomessagesfound'] = 'Сообщений не найдено';
+$string['nosearchresults'] = 'Поиск не дал результатов';
+$string['offline'] = 'Не на сайте';
+$string['offlinecontacts'] = 'Собеседники, отсутствующие на сайте ($a)';
+$string['online'] = 'На сайте';
+$string['onlinecontacts'] = 'Собеседники, находящиеся сейчас на сайте ($a)';
+$string['onlyfromme'] = 'В сообщениях от меня';
+$string['onlymycourses'] = 'В рамках моих курсов';
+$string['onlytome'] = 'В сообщениях мне';
+$string['pagerefreshes'] = 'Эта страница обновляется автоматически, каждые $a секунд';
+$string['removecontact'] = 'Удалить собеседника из моего списка';
+$string['savemysettings'] = 'Сохранить настройки';
+$string['search'] = 'Искать';
+$string['searchforperson'] = 'Искать человека';
+$string['searchmessages'] = 'Искать сообщения';
+$string['sendmessage'] = 'Отправить сообщение';
+$string['sendmessageto'] = 'Отправить сообщение человеку $a';
+$string['settings'] = 'Настройки';
+$string['settingssaved'] = 'Ваши настройки сохранены';
+$string['showmessagewindow'] = 'Автоматически показывать окно сообщений при получении нового сообщения (если мой броузер это сумеет)';
+$string['timesent'] = 'Дата/время';
+$string['unblockcontact'] = 'Разблокировать сообщения от этого собеседника';
+$string['unreadmessages'] = 'Нечитанных сообщений: $a';
+$string['userisblockingyou'] = 'Пользователь заблокировал для себя получение ваших сообщений';
+$string['userisblockingyounoncontact'] = 'Пользователь получает сообщения только от людей, которых он добавил себе в свой список собеседников.';
+$string['userssearchresults'] = 'Найдено пользователей: $a';
+
+?>
