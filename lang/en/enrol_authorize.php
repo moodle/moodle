0b6174400cac5c3cdd252320f5a964e8ed7c6b91
ethem <ethem>
2005-07-13 19:42:51 +0000

    When using the Authorize module, you can now also use enrolment keys.

--- lang/en/enrol_authorize.php
+++ lang/en/enrol_authorize.php
@@ -14,6 +14,7 @@ $string['ccno'] = 'Credit Card Number';
 $string['cctype'] = 'Credit Card Type';
 $string['ccvv'] = 'CV2';
 $string['ccvvhelp'] = 'Look at the back of card (last 3 digits)';
+$string['choosemethod'] = 'If you know enrolment key of the cource, enter it; otherwise you must paid for this course.';
 $string['chooseone'] = 'Fill one or both of the following two fields';
 $string['description'] = 'The Authorize.net module allows you to set up paid courses via CC providers.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
 $string['enrolname'] = 'Authorize.net Credit Card Gateway';
