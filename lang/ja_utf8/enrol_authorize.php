997a29e87cd3b4b8721ddee981d1bc2f79e869a7
mits <mits>
2005-07-14 01:50:23 +0000

    translated a string for enrol_authorize.

--- lang/ja_utf8/enrol_authorize.php
+++ lang/ja_utf8/enrol_authorize.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // enrol_authorize.php - created with Moodle 1.6 development (2005052400)
+      // enrol_authorize.php - created with Moodle 1.6 development (2005060201)
 
 
 $string['anlogin'] = 'Authorize.net: ログイン名';
@@ -14,6 +14,7 @@ $string['ccno'] = 'クレジットカード番号';
 $string['cctype'] = 'クレジットカードタイプ';
 $string['ccvv'] = 'CV2';
 $string['ccvvhelp'] = 'カードの裏面 ( 3桁の数字 ) をご覧ください。';
+$string['choosemethod'] = 'コースの登録キーを知っている場合は、入力してください。コースの登録キーを知らない場合は、このコースに利用料を支払う必要があります。';
 $string['chooseone'] = '次の2つのフィールドの1つまたは両方に入力してください。';
 $string['description'] = 'Authorize.netモジュールでは、クレジットカード経由でコースの支払いを行うことができます。コースの受講料がゼロの場合、学生に対して受講登録に関する支払いは求められません。サイト全体の利用料をデフォルトとしてここで設定して、コースごとに受講料を設定することができます。コース受講料を設定した場合、コース受講料はサイト利用料に優先します。';
 $string['enrolname'] = 'Authorize.net クレジットカード・ゲイトウェイ';
