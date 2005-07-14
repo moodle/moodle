1ae1e84ec5b5d46aa634e258811b361d3a265cd6
moodler <moodler>
2005-07-14 04:16:04 +0000

    Allow Altavista in as guest too

--- lib/setup.php
+++ lib/setup.php
@@ -385,7 +385,7 @@ global $THEME;
             if (empty($_SESSION['USER']) and !empty($_SERVER['HTTP_REFERER'])) {
                 if (strpos($_SERVER['HTTP_REFERER'], 'google') !== false ) {
                     $USER = guest_user();
-                } else if (strpos($_SERVER['HTTP_REFERER'], 'babelfish.altavista.com') !== false ) {
+                } else if (strpos($_SERVER['HTTP_REFERER'], 'altavista') !== false ) {
                     $USER = guest_user();
                 }
             }
