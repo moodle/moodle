8b2f3516f23ec8f5567fa729d5b087bc4b33a78c
ethem <ethem>
2005-07-18 17:08:23 +0000

    httpsrequired string.

--- lang/en/enrol_authorize.php
+++ lang/en/enrol_authorize.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // enrol_authorize.php - created with Moodle 1.5.1 (2005060210)
+      // enrol_authorize.php - created with Moodle 1.5.2 (2005060220)
 
 
 $string['anlogin'] = 'Authorize.net: Login name';
@@ -18,7 +18,12 @@ $string['choosemethod'] = 'If you know enrolment key of the cource, enter it; ot
 $string['chooseone'] = 'Fill one or both of the following two fields';
 $string['description'] = 'The Authorize.net module allows you to set up paid courses via CC providers.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
 $string['enrolname'] = 'Authorize.net Credit Card Gateway';
-$string['logindesc'] = 'You can set up <a href=\"$a->url\">loginhttps</a> option in Variables/Security section.
+$string['httpsrequired'] = 'We are sorry to inform you that your request cannot be processed currently. This site\'s configuration couldn\'t be set up correcly.
+<br /><br />
+Please don\'t enter your credit card number unless you see a yellow lock at the bottom of the browser. It means, it simply encrypts all data sent between client and server. So the information during the transaction between 2 computers is protected and your credit card number cannot captured over the internet.';
+$string['logindesc'] = 'This option must be ON.
+<br /><br />
+You can set up <a href=\"$a->url\">loginhttps</a> option in Variables/Security section.
 <br /><br />
 Turning this on will make Moodle use a secure https connection just for the login and payment page.';
 $string['nameoncard'] = 'Name on Card';
