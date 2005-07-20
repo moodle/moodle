b967259ca539f689e5ff1db245e368dd31d92968
bobopinna <bobopinna>
2005-07-20 08:51:04 +0000

    Update Moodle release request

--- mod/scorm/version.php
+++ mod/scorm/version.php
@@ -6,7 +6,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 $module->version  = 2005071500;   // The (date) version of this module
-$module->requires = 2005060201;   // The version of Moodle that is required
+$module->requires = 2005060300;   // The version of Moodle that is required
 $module->cron     = 0;            // How often should cron check this module (seconds)?
 
 ?>
