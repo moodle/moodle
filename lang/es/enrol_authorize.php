e2bc5b1376ebbe0e3e6b23b785c62956da35cfbf
barias <barias>
2005-07-22 19:18:43 +0000

    Minor updates in spanish translation

--- lang/es/enrol_authorize.php
+++ lang/es/enrol_authorize.php
@@ -1,24 +1,33 @@
 <?PHP // $Id$ 
-      // enrol_authorize.php - created with Moodle 1.6 development (2005052400)
+      // enrol_authorize.php - created with Moodle 1.5.2 (2005060220)
 
 
-$string['anlogin'] = 'Authorize.net: Usuario';
-$string['anpassword'] = 'Authorize.net: Contraseña (no requerida)';
-$string['anreferer'] = 'Escriba aquí la referencia URL en el caso de que usted la ajuste en su cuenta authorize.net, que enviará una cabecera \"Referer: URL\" en la petición web.';
-$string['antestmode'] = 'Authorize.net:';
-$string['antrankey'] = 'Authorize.net:';
-$string['ccexpire'] = 'Fecha de expiración';
-$string['ccexpired'] = 'La tarjeta de crédito ha expirado';
-$string['ccinvalid'] = 'Número de tarjeta no válido';
-$string['ccno'] = 'Número de la tarjeta de crédito';
-$string['cctype'] = 'Tipo de la tarjeta de crédito';
-$string['ccvv'] = 'CV2';
-$string['ccvvhelp'] = 'Mire el reverso de la tarjeta (3 últimos dígitos)';
-$string['chooseone'] = 'Rellene uno o ambos de los siguientes dos campos';
-$string['description'] = 'El módulo Authorize.net le permite ajustar cursos de pago vía proveedores CC. Si el costo de cualquier curso es cero, no se pedirá a los estudiantes que paguen. Existe un costo del sitio que usted ajusta aquí por defecto para todo el sitio y además un ajuste por curso que puede efectuar para cada curso individualmente. El costo del curso pasa por alto el costo del sitio.';
-$string['enrolname'] = 'Puerta de tarjeta de crédito Authorize.net:';
-$string['nameoncard'] = 'Nombre que figura en la tarjeta';
-$string['sendpaymentbutton'] = 'Enviar pago';
-$string['zipcode'] = 'Código postal';
+$string['anlogin'] = 'Authorize.net: Login name';
+$string['anpassword'] = 'Authorize.net: Password';
+$string['anreferer'] = 'Define the URL referer if you have set up this in your authorize.net account. This will send a line \"Referer: URL\" embedded in the web request.';
+$string['antestmode'] = 'Run transactions in test mode only (no money will be drawn)';
+$string['antrankey'] = 'Authorize.net: Transaction Key';
+$string['ccexpire'] = 'Expiry Date';
+$string['ccexpired'] = 'The credit card has expired';
+$string['ccinvalid'] = 'Invalid card number';
+$string['ccno'] = 'Credit Card Number';
+$string['cctype'] = 'Credit Card Type';
+$string['ccvv'] = 'Card Verification';
+$string['ccvvhelp'] = 'Look at the back of card (last 3 digits)';
+$string['choosemethod'] = 'If you know enrolment key of the cource, enter it; otherwise you need to paid for this course.';
+$string['chooseone'] = 'Fill one or both of the following two fields';
+$string['description'] = 'The Authorize.net module allows you to set up paid courses via CC providers.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
+$string['enrolname'] = 'Authorize.net Credit Card Gateway';
+$string['httpsrequired'] = 'We are sorry to inform you that your request cannot be processed currently. This site\'s configuration couldn\'t be set up correcly.
+<br /><br />
+Please don\'t enter your credit card number unless you see a yellow lock at the bottom of the browser. It means, it simply encrypts all data sent between client and server. So the information during the transaction between 2 computers is protected and your credit card number cannot captured over the internet.';
+$string['logindesc'] = 'This option must be ON.
+<br /><br />
+You can set up <a href=\"$a->url\">loginhttps</a> option in Variables/Security section.
+<br /><br />
+Turning this on will make Moodle use a secure https connection just for the login and payment page.';
+$string['nameoncard'] = 'Name on Card';
+$string['sendpaymentbutton'] = 'Send Payment';
+$string['zipcode'] = 'Zip Code';
 
 ?>
