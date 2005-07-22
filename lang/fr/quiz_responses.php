6da74ee8397d4cad495c4f796070980214398a31
martignoni <martignoni>
2005-07-22 20:24:57 +0000

    New report plug-in

---
+++ lang/fr/quiz_responses.php
@@ -0,0 +1,8 @@
+<?php // $Id$
+
+$string['responsesoptions'] = 'Options des réponses';
+$string['responsestitle'] = 'Réponses détaillées';
+$string['pagesize'] = 'Tentatives par page: ';
+$string['reportresponses'] = 'Réponses détaillées';
+
+?>
