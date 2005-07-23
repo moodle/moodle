ec439a1547db39f2fab98de3112b648aef12852a
vthung <vthung>
2005-07-23 15:50:34 +0000

    Added quiz_responses.php

---
+++ lang/vi_utf8/quiz_responses.php
@@ -0,0 +1,7 @@
+<?php // $Id$
+
+$string['responsesoptions'] = 'Các tuỳ chọn trả lời';
+$string['responsestitle'] = 'Các câu trả lời chi tiết';
+$string['pagesize'] = 'Các lần thử mỗi trang: ';
+$string['reportresponses'] = 'Các câu trả lời chi tiết';
+?>
