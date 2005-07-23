517f62255260bad64411f20cb64d2cb9bacdf04c
dovix <dovix>
2005-07-23 20:28:53 +0000

    sync updates with non unicode

--- lang/he_utf8/assignment.php
+++ lang/he_utf8/assignment.php
@@ -1,65 +1,65 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.5 ALPHA (2005042300)
+      // assignment.php - created with Moodle 1.5.1 (2005060210)
 
 
 $string['allowresubmit'] = 'הרשה הגשה מחדש';
-$string['assignmentdetails'] = 'פרטי מטלה';
-$string['assignmentmail'] = '$a->teacher פרסם משוב על המטלה שהגשת עבור \'$a->assignment\'
+$string['assignmentdetails'] = 'פרטי תרגיל';
+$string['assignmentmail'] = '$a->teacher פרסם משוב על התרגיל שהגשת עבור \'$a->assignment\'
 
-אתה יכול לראות את זה כנספח למטלה שהגשת:
+אתה יכול לראות את זה כנספח לתרגיל שהגשת:
 
 $a->url';
-$string['assignmentmailhtml'] = '$a->teacher שלח משוב על המטלה שהגשת עבור \'<i>$a->assignment</i>\'<br /><br />
+$string['assignmentmailhtml'] = '$a->teacher שלח משוב על התרגיל שהגשת עבור \'<i>$a->assignment</i>\'<br /><br />
 
 אתה יכול לראות את זה כנספח ל<a href=\"$a->url\">מטלה שהגשת</a>.';
-$string['assignmentname'] = 'שם מטלה';
-$string['assignmenttype'] = 'סוג מטלה';
+$string['assignmentname'] = 'שם תרגיל';
+$string['assignmenttype'] = 'סוג תרגיל';
 $string['availabledate'] = 'זמינה מתאריך';
 $string['comment'] = 'הערה';
 $string['commentinline'] = 'הערה פנימית';
-$string['configmaxbytes'] = 'ברירת מחדל לגודל המירבי של כל המטלות באתר זה (כפוף להגבלות הקורס ולשאר הגדרות מקומיות)';
+$string['configmaxbytes'] = 'ברירת מחדל לגודל המירבי של כל התרגילים באתר זה (כפוף להגבלות הקורס ולשאר הגדרות מקומיות)';
 $string['description'] = 'תיאור';
 $string['duedate'] = 'תאריך הגשה';
 $string['duedateno'] = 'אין תאריך הגשה';
 $string['early'] = '$a מוקדם';
 $string['editmysubmission'] = 'ערוך את ההגשה שלי';
-$string['emailteachermail'] = 'המשתמש $a->username עדכן את הגשת המטלה עבור \'$a->assignment\'
+$string['emailteachermail'] = 'המשתמש $a->username עדכן את הגשת התרגיל עבור \'$a->assignment\'
 
 ההגשה זמינה כאן:
 
 $a->url';
-$string['emailteachermailhtml'] = 'המשתמש $a->username עדכן את הגשת המטלה עבור <i>\'$a->assignment\'</i><br /><br />
+$string['emailteachermailhtml'] = 'המשתמש $a->username עדכן את הגשת התרגיל עבור <i>\'$a->assignment\'</i><br /><br />
 היא <a href=\"$a->url\">זמינה באתר</a>.';
 $string['emailteachers'] = 'הודעת התראה למורים בדוא\"ל';
-$string['emptysubmission'] = 'לא הגשת עדיין שום מטלה';
+$string['emptysubmission'] = 'לא הגשת עדיין שום תרגיל';
 $string['existingfiledeleted'] = 'הקובץ הקיים $a נמחק';
 $string['failedupdatefeedback'] = 'כשל בעדכון משוב ההגשה למשתמש $a';
 $string['feedback'] = 'משוב';
 $string['feedbackfromteacher'] = 'משוב מ-$a';
 $string['feedbackupdated'] = 'הגשת עדכון משוב בשביל $a אנשים';
-$string['helpoffline'] = '<p>הגדרה זו שימושית כאשר המטלה מבוצעת מחוץ ל-Moodle. היא יכולה להיות במקום אחר ברשת או בהגשה פנים-אל-פנים.</p><p>תלמידים יכולים לראות את הגדרת המטלה, אבל לא יכולים להעלות קבצים או לערוך את המטלה באופן מקוון. מתן ציונים עובד כרגיל, ותלמידים יקבלו הודעה כאשר הציון שלהם נקבע.</p>';
-$string['helponline'] = '<p>סוג מטלה זה מבקש מהמשתמשים לערוך טקסט, בעזרת כלי העריכה הרגילים. המורים יכולים להעניק ציון באופן מקוון, ואפילו לבצע תיקונים או לרשום הערות בגוף הטקסט.</p>
+$string['helpoffline'] = '<p>הגדרה זו שימושית כאשר המטלה מבוצעת מחוץ ל-Moodle. היא יכולה להיות במקום אחר ברשת או בהגשה פנים-אל-פנים.</p><p>תלמידים יכולים לראות את הגדרת התרגיל, אבל לא יכולים להעלות קבצים או לערוך את התרגיל באופן מקוון. מתן ציונים עובד כרגיל, ותלמידים יקבלו הודעה כאשר הציון שלהם נקבע.</p>';
+$string['helponline'] = '<p>סוג תרגיל זה מבקש מהמשתמשים לערוך טקסט, בעזרת כלי העריכה הרגילים. המורים יכולים להעניק ציון באופן מקוון, ואפילו לבצע תיקונים או לרשום הערות בגוף הטקסט.</p>
 <p>(אם השתמשת בגרסאות קודמות של Moodle, סוג מטלה זה דומה להתנהגות של מודול ה-Journal הישן.)</p>';
-$string['helpuploadsingle'] = '<p>סוג מטלה זה מאפשר לכל משתתף להעלות קובץ בודד, מכל סוג שהוא.</p> <p>הקובץ יכול להיות מסמך שנכתב במעבד תמלילים, תמונה,
-חבילת ארכיב מכווצת, או כל סוג קובץ אחר הנדרש ע\"י המטלה.</p>';
+$string['helpuploadsingle'] = '<p>סוג תרגיל זה מאפשר לכל משתתף להעלות קובץ בודד, מכל סוג שהוא.</p> <p>הקובץ יכול להיות מסמך שנכתב במעבד תמלילים, תמונה,
+חבילת ארכיב מכווצת, או כל סוג קובץ אחר הנדרש ע\"י התרגיל.</p>';
 $string['late'] = '$a מאוחר';
-$string['maximumgrade'] = 'ציון מhירבי';
+$string['maximumgrade'] = 'ציון מירבי';
 $string['maximumsize'] = 'גודל מירבי';
-$string['modulename'] = 'מטלה';
-$string['modulenameplural'] = 'מטלות';
-$string['newsubmissions'] = 'המטלות הוגשו';
-$string['noassignments'] = 'אין עדיין מטלות';
-$string['noattempts'] = 'עדיין לא בוצעו שום פעילויות על מטלה זו';
+$string['modulename'] = 'תרגיל';
+$string['modulenameplural'] = 'תרגילים';
+$string['newsubmissions'] = 'התרגילים הוגשו';
+$string['noassignments'] = 'אין עדיין תרגילים';
+$string['noattempts'] = 'עדיין לא בוצעו שום פעילויות על תרגיל זה';
 $string['notgradedyet'] = 'עדיין לא הוערך ';
 $string['notsubmittedyet'] = 'לא הוגש עדיין';
 $string['overwritewarning'] = 'אזהרה: העלאה מחדש תחליף את ההגשה הנוכחית שלך';
-$string['preventlate'] = 'מנע שליחת מטלות באיחור';
+$string['preventlate'] = 'מנע שליחת תרגילים באיחור';
 $string['saveallfeedback'] = 'שמור את כל המשובים שלי';
 $string['submission'] = 'הגשה';
 $string['submissionfeedback'] = 'הגשת משוב';
 $string['submissions'] = 'הגשות';
 $string['submissionsaved'] = 'השינויים שלך נשמרו';
-$string['submitassignment'] = 'הגש את המטלה שלך באמצעות טופס זה';
+$string['submitassignment'] = 'הגש את התרגיל שלך באמצעות טופס זה';
 $string['submitted'] = 'הוגש';
 $string['typeoffline'] = 'פעילות לא מקוונת';
 $string['typeonline'] = 'טקסט מקוון';
@@ -72,8 +72,8 @@ $string['uploadfiletoobig'] = 'מצטערים, אבל הקובץ גדול מדי
 $string['uploadnofilefound'] = 'לא נמצא קובץ - האם אתה בטוח שבחרת קובץ להעלאה?';
 $string['uploadnotregistered'] = '\'$a\' הועלה באופן תקין אבל ההגשה לא נרשמה!';
 $string['uploadsuccess'] = '\'$a\' הועלה בהצלחה';
-$string['viewfeedback'] = 'ראה ציוני ומשובי מטלות';
-$string['viewsubmissions'] = 'ראה $a מטלות שהוגשו';
+$string['viewfeedback'] = 'ראה ציוני ומשובי תרגילים';
+$string['viewsubmissions'] = 'ראה $a תרגילים שהוגשו';
 $string['yoursubmission'] = 'ההגשה שלך';
 
 ?>
