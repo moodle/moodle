f0a4d2774b43378da60962a8a7d5749c47a7a304
ethem <ethem>
2005-07-25 15:26:02 +0000

    new files.

---
+++ lang/tr/quiz_responses.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // quiz_responses.php - created with Moodle 1.5.2 (2005072200)
+
+
+$string['pagesize'] = 'Her sayfadaki uygulama:';
+$string['reportresponses'] = 'Ayrýntýlý yanýtlar';
+$string['responsesoptions'] = 'Yanýt seçenekleri';
+$string['responsestitle'] = 'Ayrýntýlý yanýtlar';
+
+?>
