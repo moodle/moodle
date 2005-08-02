5e53ffba34ac086b13d098d14b06942062f7af01
ethem <ethem>
2005-08-02 08:27:14 +0000

    " " fix

--- enrol/authorize/db/mysql.php
+++ enrol/authorize/db/mysql.php
@@ -16,7 +16,7 @@ function authorize_upgrade($oldversion=0) {
         // Be sure, only last 4 digit is inserted.
         table_column('enrol_authorize', 'cclastfour', 'cclastfour', 'integer', '4', 'unsigned', '0', 'not null');
         table_column('enrol_authorize', 'courseid', 'courseid', 'integer', '10', 'unsigned', '0', 'not null');
-        table_column('enrol_authorize', 'userid ', 'userid ', 'integer', '10', 'unsigned', '0', 'not null');
+        table_column('enrol_authorize', 'userid', 'userid', 'integer', '10', 'unsigned', '0', 'not null');
 	// Add some indexes for speed.
         execute_sql(" ALTER TABLE `{$CFG->prefix}enrol_authorize` ADD INDEX courseid(courseid) ");
         execute_sql(" ALTER TABLE `{$CFG->prefix}enrol_authorize` ADD INDEX userid(userid) ");
