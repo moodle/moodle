3f2a955d19c39ab7ab6ae4de85d1f3c3b6e4b404
thepurpleblob <thepurpleblob>
2005-08-05 10:38:43 +0000

    Try again - adding new files for new settings screen.

---
+++ filter/mediaplugin/defaultsettings.php
@@ -0,0 +1,52 @@
+<?php
+    // check/set default config settings for media plugin
+    
+    if (!isset($CFG->filter_mediaplugin_enable_mp3)) {
+        if (isset($CFG->filter_mediaplugin_ignore_mp3)) {
+            set_config( 'filter_mediaplugin_enable_mp3', !$CFG->filter_mediaplugin_ignore_mp3 );
+        }
+        else {
+            set_config( 'filter_mediaplugin_enable_mp3', 1 );
+        }
+    }
+    if (!isset($CFG->filter_mediaplugin_enable_swf)) {
+        if (isset($CFG->filter_mediaplugin_ignore_swf)) {
+            set_config( 'filter_mediaplugin_enable_swf', !$CFG->filter_mediaplugin_ignore_swf );
+        }
+        else {
+            set_config( 'filter_mediaplugin_enable_swf', 1 );
+        }
+    }
+    if (!isset($CFG->filter_mediaplugin_enable_mov)) {
+        if (isset($CFG->filter_mediaplugin_ignore_mov)) {
+            set_config( 'filter_mediaplugin_enable_mov', !$CFG->filter_mediaplugin_ignore_mov );
+        }
+        else {
+            set_config( 'filter_mediaplugin_enable_mov', 1 );
+        }
+    }
+    if (!isset($CFG->filter_mediaplugin_enable_mpg)) {
+        if (isset($CFG->filter_mediaplugin_ignore_mpg)) {
+            set_config( 'filter_mediaplugin_enable_mpg', !$CFG->filter_mediaplugin_ignore_mpg );
+        }
+        else {
+            set_config( 'filter_mediaplugin_enable_mpg', 1 );
+        }
+    }
+    if (!isset($CFG->filter_mediaplugin_enable_wmv)) {
+        if (isset($CFG->filter_mediaplugin_ignore_wmv)) {
+            set_config( 'filter_mediaplugin_enable_wmv', !$CFG->filter_mediaplugin_ignore_wmv );
+        }
+        else {
+            set_config( 'filter_mediaplugin_enable_wmv', 1 );
+        }
+    }
+    if (!isset($CFG->filter_mediaplugin_enable_avi)) {
+        if (isset($CFG->filter_mediaplugin_ignore_avi)) {
+            set_config( 'filter_mediaplugin_enable_avi', !$CFG->filter_mediaplugin_ignore_avi );
+        }
+        else {
+            set_config( 'filter_mediaplugin_enable_avi', 1 );
+        }
+    }
+?>
