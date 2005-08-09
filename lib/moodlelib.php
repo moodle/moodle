8bc776ba4f7f016f4b32e1ee498eba39762f623a
moodler <moodler>
2005-08-09 15:14:49 +0000

    Slightly mkore reobust in the case where https was already being used for the whole site.      bug 3848

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -5805,7 +5805,7 @@ function httpsrequired() {
 
     if (!empty($CFG->loginhttps)) {
         $HTTPSPAGEREQUIRED = true;
-        $CFG->httpswwwroot = str_replace('http', 'https', $CFG->wwwroot);
+        $CFG->httpswwwroot = str_replace('http:', 'https:', $CFG->wwwroot);
     } else {
         $CFG->httpswwwroot = $CFG->wwwroot;
     }
