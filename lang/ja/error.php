af6420dacf69496321d622d7966958ec9c9d176e
mits <mits>
2005-08-12 02:41:09 +0000

    polished. translated two help files for exercise.

--- lang/ja/error.php
+++ lang/ja/error.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // error.php - created with Moodle 1.5 ALPHA (2005042400)
+      // error.php - created with Moodle 1.6 development (2005072200)
 
 
 $string['adminprimarynoedit'] = '主Adminは他から編集することはできません。';
@@ -17,8 +17,8 @@ $string['notavailable'] = '現在利用できません';
 $string['onlyeditown'] = '他の人の情報は編集できません。';
 $string['processingstops'] = 'プロセスが停止しました。残りのレコードは無視されます。';
 $string['restricteduser'] = '申し訳ございません。あなたのアカウント「 $a 」では、この処理を行うことができません。';
-$string['sessionerroruser'] = 'あなたのセッションがタイムアウトしたか、エラーが発生しました。再度ログインしてください。';
-$string['sessionipnomatch'] = '申し訳ございません。あなたのIPアドレスは、最初のログインから変更されているようです。このセキュリティー機能は、あなたがサイトにログインしている間に、クラッカーから個人情報を盗むことを防いでいます。通常のユーザは、このメッセージを見ることはありません - 詳細はシステム管理者にお問合せください。';
+$string['sessionerroruser'] = 'セッションがタイムアウトしました。再度ログインしてください。';
+$string['sessionipnomatch'] = '申し訳ございません。あなたのIPアドレスは、最初のログインから変更されているようです。このセキュリティ機能は、あなたがサイトにログインしている間に、クラッカーから個人情報を盗むことを防いでいます。通常のユーザは、このメッセージを見ることはありません - 詳細はシステム管理者にお問合せください。';
 $string['unknowncourse'] = '不明なコース名 「 $a 」';
 $string['usernotaddederror'] = 'ユーザ 「 $a 」 は追加されませんでした - 原因不明のエラー';
 $string['usernotaddedregistered'] = 'ユーザ 「 $a 」 は追加されませんでした - 登録済み';
