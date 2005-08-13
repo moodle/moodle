7e701287b6180c715417b959d1ed8f43c1df5e6c
barias <barias>
2005-08-13 10:36:15 +0000

    *** empty log message ***

---
+++ lang/es/quiz_responses.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // quiz_responses.php - created with Moodle 1.5.1 (2005060210)
+
+
+$string['pagesize'] = 'Intentos por página:';
+$string['reportresponses'] = 'Respuestas detalladas';
+$string['responsesoptions'] = 'Opciones de respuesta';
+$string['responsestitle'] = 'Respuestas detalladas';
+
+?>
