3f7f06576309a467937d251798ca63908cdd4520
vthung <vthung>
2005-08-14 15:15:13 +0000

    Appended some new strings

--- lang/vi_utf8/resource.php
+++ lang/vi_utf8/resource.php
@@ -78,5 +78,8 @@ $string['resourcetypehtml'] = 'Soạn thảo một trang web';
 $string['resourcetypelabel'] = 'Chèn một nhãn';
 $string['resourcetypetext'] = 'Soạn thảo một trang văn bản';
 $string['searchweb'] = 'Tìm kiếm trang web';
+$string['serverurl'] = 'URL của server ($a->wwwroot)';
+$string['showcourseblocks'] = 'Hiển thị các khối của cua học';
 $string['variablename'] = 'Tên biến';
+$string['vol'] = 'Vol';
 ?>
