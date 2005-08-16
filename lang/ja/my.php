8dd3aabb4e97824547277112237b384d8fcef943
mits <mits>
2005-08-16 04:31:54 +0000

    added 2 new lang files and translated some new strings.

---
+++ lang/ja/my.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // my.php - created with Moodle 1.6 development (2005072200)
+
+
+$string['mymoodle'] = 'マイ moodle';
+$string['nocourses'] = '表示するコース情報がありません。';
+$string['pinblocks'] = 'マイ moodle 固定ブロックの設定';
+$string['pinblocksexplan'] = 'あなたがここで設定した全てのブロックは、全てのMoodleユーザが「マイ moodle」画面で閲覧 ( 編集不可 ) することができます。';
+
+?>
---
+++ lang/ja_utf8/block_student_loan_calc.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // block_student_loan_calc.php - created with Moodle 1.6 development (2005072200)
+
+
+$string['amountofloan'] = 'ローン総額';
+$string['fortnightly'] = '2週間ごと';
+$string['interestrate'] = '金利';
+$string['loanterm'] = 'ローン期間 ( 年 )';
+$string['monthly'] = '月ごと';
+$string['repaymentamount'] = '返済総額';
+$string['repaymentfreq'] = '返済頻度';
+$string['studentloancalc'] = '学生ローン計算機';
+$string['weekly'] = '週ごと';
+
+?>
