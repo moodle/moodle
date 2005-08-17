69c0d07455b741bd51cb907a6c3ac29320099825
moodler <moodler>
2005-08-17 13:37:53 +0000

    file block_search_forums.php was added on branch MOODLE_15_STABLE on 2005-09-03 07:51:57 +0000

--- lang/ko/block_search_forums.php
+++
@@ -1,7 +0,0 @@
-<?PHP // $Id$
-
-
-
-$string['advancedsearch'] = '진보된 검색';
-$string['blocktitle'] = '검색 포럼';
-?>
--- lang/ko_utf8/block_search_forums.php
+++
@@ -1,8 +0,0 @@
-<?PHP // $Id$ 
-      // block_search_forums.php - created with Moodle 1.6 development (2005060201)
-
-
-$string['advancedsearch'] = '진보된 검색';
-$string['blocktitle'] = '검색 포럼';
-
-?>
