b5227d6f91cedf46bf561bc052e823900467be1c
scop22 <scop22>
2005-08-17 13:15:34 +0000

    Adding two new files to the Korean language pack (UTF version)

---
+++ lang/ko_utf8/block_search_forums.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // block_search_forums.php - created with Moodle 1.6 development (2005060201)
+
+
+$string['advancedsearch'] = '진보된 검색';
+$string['blocktitle'] = '검색 포럼';
+
+?>
