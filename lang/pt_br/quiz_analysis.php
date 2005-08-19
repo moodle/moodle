5d2e93786f1fea8d2ebef1a93e4aa7e180951365
papita <papita>
2005-08-19 16:00:51 +0000

    adding new files and updatings, by paula

---
+++ lang/pt_br/quiz_analysis.php
@@ -0,0 +1,31 @@
+<?PHP // $Id$ 
+      // quiz_analysis.php - created with Moodle 1.5 + (2005060201)
+
+
+$string['analysisoptions'] = 'Opções de análise';
+$string['analysistitle'] = 'Tabela de análise de itens';
+$string['attemptsall'] = 'todas as tentativas';
+$string['attemptselection'] = 'Tentativas a serem analizadas por usuário';
+$string['attemptsfirst'] = 'Primeira tentativa';
+$string['attemptshighest'] = 'Melhor nota';
+$string['attemptslast'] = 'última tentativa';
+$string['dicsindextitle'] = 'Disc.<br />Índice';
+$string['disccoefftitle'] = 'Disc.<br />Coef.';
+$string['downloadooo'] = 'Baixar em formato OpenOffice ';
+$string['facilitytitle'] = '%% Corretas<br>Facility';
+$string['lowmarkslimit'] = 'Não analisar se a nota for inferior a:';
+$string['pagesize'] = 'Questões por página:';
+$string['qcounttitle'] = 'Q. contagem';
+$string['qidtitle'] = 'Q#';
+$string['qnametitle'] = 'Nome da questão';
+$string['qtexttitle'] = 'Testo da questão';
+$string['qtypetitle'] = 'Q. tipo';
+$string['quizreportdir'] = 'quiz_reports';
+$string['rcounttitle'] = 'R.<br />Contagens';
+$string['reportanalysis'] = 'Relatório de análise dos itens';
+$string['responsestitle'] = 'Texto da resposta';
+$string['rfractiontitle'] = 'crédito<br />parcial';
+$string['rpercenttitle'] = 'R.%%';
+$string['stddevtitle'] = 'SD';
+
+?>
