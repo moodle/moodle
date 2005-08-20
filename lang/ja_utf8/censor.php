4398f51a506ff5017f09a9f3261928b4a15539cf
mits <mits>
2005-08-20 08:18:51 +0000

    polished.

--- lang/ja_utf8/censor.php
+++ lang/ja_utf8/censor.php
@@ -1,8 +1,8 @@
-<?php // $Id$ 
-      // censor.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+<?PHP // $Id$ 
+      // censor.php - created with Moodle 1.6 development (2005081700)
 
 
-$string['filtername'] = '単語検閲';
 $string['badwords'] = 'shit,fucked,fucker,fuck,dickhead,dick,cockhead,cock,cunt,asshole,arsehole,prick,bitch,jism,whore,slut,wanker,wank,bastard,dildo,masturbate,orgasm,penis,nigger,pussy,vagina';
+$string['filtername'] = '単語検閲';
 
 ?>
