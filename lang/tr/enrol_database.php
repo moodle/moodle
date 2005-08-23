fd0c130a21ba58f61dee48fe361853f5cfb63392
ethem <ethem>
2005-08-23 11:49:56 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant.

--- lang/tr/enrol_database.php
+++ lang/tr/enrol_database.php
@@ -1,13 +1,18 @@
 <?PHP // $Id$ 
-      // enrol_database.php - created with Moodle 1.5 ALPHA (2005042300)
+      // enrol_database.php - created with Moodle 1.6 development (2005081700)
 
 
 $string['dbhost'] = 'Veritabaný sunucusu';
-$string['dbname'] = 'Veritabaný';
-$string['dbpass'] = 'Parola';
-$string['dbtable'] = 'Tablo';
-$string['dbtype'] = 'Sunucu tipi';
-$string['dbuser'] = 'Kullanýcý adý';
+$string['dbname'] = 'Kullanýcýlacak veritabaný';
+$string['dbpass'] = 'Sunucuya eriþmek için parola';
+$string['dbtable'] = 'Veritabanýndaki tablo';
+$string['dbtype'] = 'Sunucu veritabaný tipi';
+$string['dbuser'] = 'Sunucuya eriþmek için kullanýcýadý';
+$string['description'] = 'Ders kayýtlarýný kontrol etmek için harici veritabanlarýný (herhangi bir çeþit) kullanabilirsiniz. Harici veritabanýnýn bir courseID (DersId) ve bir userID (KullaniciID) alanlarýný içerdiðini varsayýyoruz.  Bunlar yerel kurs ve kullanýcý tablolarýnýndaki alanlarla karþýlaþtýrýlacaktýr.';
 $string['enrolname'] = 'Harici Veritabaný';
+$string['localcoursefield'] = 'Uzak veritabanýndaki alanlarla eþleþtirmek için kullandýðýmýz yerel kurs tablosu alan adý (idnumber)';
+$string['localuserfield'] = 'Uzak kayýttaki kullanýcýyý eþleþtirmek için kullandýðýmýz yerel kullanýcý tablosu alan adý (idnumber)';
+$string['remotecoursefield'] = 'KursID\'yi bulmak için uzaktaki veritabanýnda hangi alaný kullanýyoruz';
+$string['remoteuserfield'] = 'KullanýcýID\'yi bulmak için uzaktaki veritabanýnda hangi alaný kullanýyoruz';
 
 ?>
