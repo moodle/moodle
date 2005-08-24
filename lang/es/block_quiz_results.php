4e902870b7469fcde6f825aeb183495435cee6ab
barias <barias>
2005-08-24 10:02:29 +0000

    Minor updates in spanish translation

--- lang/es/block_quiz_results.php
+++ lang/es/block_quiz_results.php
@@ -1,28 +1,30 @@
-<?PHP // $Id$ 
-      // block_quiz_results.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+<?php // $Id$
 
-
-$string['bestgrade'] = 'Calificación más alta:';
-$string['bestgrades'] = 'Las $a calificaciones más altas:';
-$string['bestgroupgrade'] = 'Grupo con promedio más alto:';
-$string['bestgroupgrades'] = 'Los $a grupos con el promedio más alto:';
-$string['config_format_absolute'] = 'Números absolutos';
-$string['config_format_fraction'] = 'Fracciones';
-$string['config_format_percentage'] = 'Porcentajes';
-$string['config_grade_format'] = 'Mostrar calificaciones como:';
-$string['config_no_quizzes_in_course'] = 'Este curso no contiene cuestinarios. Debe añadir al menos uno antes de poder utilizar este bloque correctamente.';
-$string['config_select_quiz'] = '¿De qué cuestionario deberá este bloque mostrar los resultados?';
-$string['config_show_best'] = '¿Cuántas calificaciones máximas se mostrarán? (0 para desactivar)';
-$string['config_show_worst'] = '¿Cuántas calificaciones míminas se mostrarán? (0 para desactivar)';
-$string['config_use_groups'] = '¿Mostrar grupos en lugar de estudiantes? (sólo si el cuestionario soporta grupos)';
-$string['configuredtoshownothing'] = 'La configuración de este bloque no permite mostrar ningún resultado. Puede optar por configurarlo u ocultarlo.';
-$string['error_emptyquizid'] = 'Hay un error con este bloque: necesita seleccionar el cuestionario cuyos resultados se mostrarán.';
-$string['error_emptyquizrecord'] = 'Hay un error con este bloque: el cuestionario seleccionado no parece existir en la base de datos.';
-$string['error_nogroupsexist'] = 'Hay un error con este bloque: está ajustado para mostrar las calificaciones de los grupos, pero el curso no tiene grupos definidos.';
-$string['formaltitle'] = 'Resultados del Cuestionario';
-$string['worstgrade'] = 'Calificación más baja:';
-$string['worstgrades'] = 'Las $a calificaciones más bajas:';
-$string['worstgroupgrade'] = 'Grupo con promedio más bajo:';
-$string['worstgroupgrades'] = 'Los $a grupos con el promedio más bajo:';
+$string['bestgrade'] = 'The highest grade:';
+$string['bestgrades'] = 'The $a highest grades:';
+$string['bestgroupgrade'] = 'The group with the highest average:';
+$string['bestgroupgrades'] = 'The $a groups with the highest average:';
+$string['worstgrade'] = 'The lowest grade:';
+$string['worstgrades'] = 'The $a lowest grades:';
+$string['worstgroupgrade'] = 'The group with the lowest average:';
+$string['worstgroupgrades'] = 'The $a groups with the lowest average:';
+$string['formaltitle'] = 'Quiz Results';
+$string['config_select_quiz'] = 'Which quiz should this block display results from?';
+$string['config_show_best'] = 'How many of the highest grades should be shown (0 to disable)?';
+$string['config_show_worst'] = 'How many of the lowest grades should be shown (0 to disable)?';
+$string['config_use_groups'] = 'Show groups instead of students (only if the quiz supports groups)?';
+$string['config_grade_format'] = 'Display grades as:';
+$string['config_name_format'] = 'Privacy level for displayed results:';
+$string['config_format_percentage'] = 'Percentages';
+$string['config_format_fraction'] = 'Fractions';
+$string['config_format_absolute'] = 'Absolute numbers';
+$string['config_names_full'] = 'Display full names';
+$string['config_names_id'] = 'Display only ID numbers';
+$string['config_names_anon'] = 'Anonymous results';
+$string['config_no_quizzes_in_course'] = 'This course does not contain any quiz activities . You must add at least one before you are able to use this block correctly.';
+$string['configuredtoshownothing'] = 'This block\'s configuration currently does not allow it to show any results. You may want to either configure it or hide it.';
+$string['error_emptyquizid'] = 'There is an error right now with this block: you need to select which quiz it should display results from.';
+$string['error_emptyquizrecord'] = 'There is an error right now with this block: the selected quiz does not seem to exist in the database.';
+$string['error_nogroupsexist'] = 'There is an error right now with this block: it is set to display grades in group mode, but the course has no defined groups.';
 
 ?>
