ca9388440ab5f6b40a1ba1d55d18a97970576067
moodler <moodler>
2005-08-30 09:36:14 +0000

    Merged bugfix 3958 into head from stable

--- lib/weblib.php
+++ lib/weblib.php
@@ -3690,6 +3690,8 @@ function print_scale_menu_helpbutton($courseid, $scale) {
 function error ($message, $link='') {
     global $CFG, $SESSION;
 
+    header('HTTP/1.0 404 Not Found');
+
     print_header(get_string('error'));
     echo '<br />';
 
@@ -3707,6 +3709,9 @@ function error ($message, $link='') {
     }
     print_continue($link);
     print_footer();
+    for ($i=0;$i<512;$i++) {  // Padding to help IE work with 404
+        echo ' ';
+    }
     die;
 }
 
