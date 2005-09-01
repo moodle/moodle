1c4541da034910f3685a76fd51c0e6382bfe9296
papita <papita>
2005-09-01 17:56:31 +0000

    changing module name into ESCOLHAS, adding new files, reconstructing index and code (index in en language pack is also incomplete!)

--- lang/pt_br/choice.php
+++ lang/pt_br/choice.php
@@ -2,30 +2,30 @@
       // choice.php - created with Moodle 1.5 + (2005060201)
 
 
-$string['addmorechoices'] = 'Adicionar outras pesquisas de opinião';
-$string['allowupdate'] = 'Permitir a atualização da pesquisa de opinião';
+$string['addmorechoices'] = 'Adicionar outras escolhas';
+$string['allowupdate'] = 'Permitir a atualização das escolhas';
 $string['answered'] = 'Respondido';
-$string['choice'] = 'Opção $a';
+$string['choice'] = 'Escolha $a';
 $string['choiceclose'] = 'Até';
-$string['choicename'] = 'Nome da pesquisa de opinião';
+$string['choicename'] = 'Nome da Escolha';
 $string['choiceopen'] = 'Abrir';
-$string['choicetext'] = 'Texto da pesquisa de opinião';
+$string['choicetext'] = 'Texto da Escolha';
 $string['displayhorizontal'] = 'Disposição Horizontal';
-$string['displaymode'] = 'Formato di visualização';
+$string['displaymode'] = 'Formato de visualização';
 $string['displayvertical'] = 'Disposição Vertical';
 $string['full'] = '(Completo)';
 $string['havetologin'] = 'Você deve fazer o acesso ao site antes de enviar a sua resposta';
 $string['limit'] = 'Limitar';
 $string['limitanswers'] = 'Limitar o número de respostas permitidas';
-$string['modulename'] = 'Pesquisa de opinião';
-$string['modulenameplural'] = 'Pesquisas de opinião';
+$string['modulename'] = 'Escolha';
+$string['modulenameplural'] = 'Escolhas';
 $string['mustchooseone'] = 'Você deve escolher uma resposta antes de salvar. ';
 $string['notanswered'] = 'Nenhuma resposta';
 $string['notopenyet'] = 'Esta atividade será acessível a partir de $a';
 $string['privacy'] = 'Privacidade dos resultados';
 $string['publish'] = 'Publicar resultados';
 $string['publishafteranswer'] = 'Mostrar os resultados ao estudante só depois que ele tiver dado a sua resposta';
-$string['publishafterclose'] = 'Mostrar resultados aos estudantes apenas após o fechamento da pesquisa de opinião';
+$string['publishafterclose'] = 'Mostrar resultados aos estudantes apenas após o fechamento do período de escolha';
 $string['publishalways'] = 'Mostrar sempre os resultados aos estudantes';
 $string['publishanonymous'] = 'Publique resultados em forma anônima, sem mostrar o nome do aluno';
 $string['publishnames'] = 'Publique resultados completos, mostrando os nomes dos alunos e os resultados';
