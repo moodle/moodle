32a50e2833a8f1db630c62b497d6bfa03c56069d
stronk7 <stronk7>
2005-09-05 22:32:06 +0000

    Avoid empty or unlinkable activity names
    Merged from MOODLE_15_STABLE

--- filter/activitynames/filter.php
+++ filter/activitynames/filter.php
@@ -31,12 +31,15 @@
                 foreach ($modinfo as $activity) {
                     //Exclude labels and hidden items
                     if ($activity->mod != "label" && $activity->visible) {
-                        $title = strip_tags(urldecode($activity->name));
-                        $title = str_replace('"', "'", $title);
-                        $href_tag_begin = "<a class=\"autolink\" title=\"$title\" href=\"$CFG->wwwroot/mod/$activity->mod/view.php?id=$activity->cm\">";
-                        $currentname = urldecode($activity->name);
-                        if ($currentname = trim($currentname)) {
-                            $activitylist[] = new filterobject($currentname, $href_tag_begin, '</a>', false, true);
+                        $title = trim(strip_tags(urldecode($activity->name)));
+                        /// Avoid empty or unlinkable activity names
+                        if (!empty($title)) {
+                            $title = str_replace('"', "'", $title);
+                            $href_tag_begin = "<a class=\"autolink\" title=\"$title\" href=\"$CFG->wwwroot/mod/$activity->mod/view.php?id=$activity->cm\">";
+                            $currentname = urldecode($activity->name);
+                            if ($currentname = trim($currentname)) {
+                                $activitylist[] = new filterobject($currentname, $href_tag_begin, '</a>', false, true);
+                            }
                         }
                     }
                 }
