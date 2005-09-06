0b44fd6f57be0a137c6f04e130d494b1e4457704
papita <papita>
2005-09-06 18:30:54 +0000

    corrections by rubens serafim

--- lang/pt_br/quiz_responses.php
+++ lang/pt_br/quiz_responses.php
@@ -2,7 +2,7 @@
       // quiz_responses.php - created with Moodle 1.5 + (2005060201)
 
 
-$string['pagesize'] = 'Tentativas po página';
+$string['pagesize'] = 'Tentativas por página';
 $string['reportresponses'] = 'Respostas detalhadas';
 $string['responsesoptions'] = 'Opções para respostas';
 $string['responsestitle'] = 'Respostas detalhadas';
