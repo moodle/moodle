fc3063690c27930271c071a22c57c6adc7d87ea9
carlesbellver <carlesbellver>
2005-09-09 12:28:45 +0000

    *** empty log message ***

---
+++ lang/ca/quiz_analysis.php
@@ -0,0 +1,25 @@
+<?PHP // $Id$ 
+      // quiz_analysis.php - created with Moodle 1.5.2 + (2005060222)
+
+
+$string['analysisoptions'] = 'Opcions d\'anàlisi';
+$string['analysistitle'] = 'Taula d\'anàlisi d\'elements';
+$string['attemptsall'] = 'tots els intents';
+$string['attemptselection'] = 'Intents a analitzar per usuari';
+$string['attemptsfirst'] = 'primer intent';
+$string['attemptshighest'] = 'qualificació més alta';
+$string['attemptslast'] = 'darrer intent';
+$string['downloadooo'] = 'Baixa en format OpenOffice.org';
+$string['facilitytitle'] = '%%Correctes<br />Dificultat';
+$string['lowmarkslimit'] = 'No analitzis si la puntuació és menor que:';
+$string['pagesize'] = 'Preguntes per pàgina:';
+$string['qidtitle'] = 'P.';
+$string['qnametitle'] = 'Títol de la pregunta';
+$string['qtexttitle'] = 'Text de la pregunta';
+$string['qtypetitle'] = 'Tipus';
+$string['reportanalysis'] = 'Informe de l\'anàlisi d\'elements';
+$string['responsestitle'] = 'Text de la resposta';
+$string['rpercenttitle'] = 'R.%%';
+$string['stddevtitle'] = 'DT';
+
+?>
