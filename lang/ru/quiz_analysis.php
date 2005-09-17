30eb86ca36d31ee4a07dae09406e1491dbcbe8b9
dlnsk <dlnsk>
2005-09-17 17:45:50 +0000

    *** empty log message ***

---
+++ lang/ru/quiz_analysis.php
@@ -0,0 +1,31 @@
+<?PHP // $Id$ 
+      // quiz_analysis.php - created with Moodle 1.5.1 (2005060210)
+
+
+$string['analysisoptions'] = 'Настройки анализа';
+$string['analysistitle'] = 'Таблица анализа вопросов';
+$string['attemptsall'] = 'все попытки';
+$string['attemptselection'] = 'Какие попытки пользователя анализировать?';
+$string['attemptsfirst'] = 'первая попытка';
+$string['attemptshighest'] = 'высшая оценка';
+$string['attemptslast'] = 'последняя попытка';
+$string['dicsindextitle'] = 'Индекс<br />дискр.';
+$string['disccoefftitle'] = 'Коэфф.<br />дискр.';
+$string['downloadooo'] = 'Загрузить в формате OpenOffice';
+$string['facilitytitle'] = '%% правильн.<br>ответов';
+$string['lowmarkslimit'] = 'Не анализировать, если оценка за попытку менее';
+$string['pagesize'] = 'Вопросов на странице';
+$string['qcounttitle'] = 'Кол-во В.';
+$string['qidtitle'] = 'В.#';
+$string['qnametitle'] = 'Название вопроса';
+$string['qtexttitle'] = 'Текст вопроса';
+$string['qtypetitle'] = 'Тип В.';
+$string['quizreportdir'] = 'quiz_reports';
+$string['rcounttitle'] = 'Число<br />ответов';
+$string['reportanalysis'] = 'Отчёт по анализу вопросов';
+$string['responsestitle'] = 'Текст ответа';
+$string['rfractiontitle'] = 'Частичная<br />оценка';
+$string['rpercenttitle'] = '%% от-<br />ветов';
+$string['stddevtitle'] = 'Станд.<br />откл.';
+
+?>
