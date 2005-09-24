ee473d735734bd47a2adc4e2a6b117e2aef96cad
mits <mits>
2005-09-24 13:12:22 +0000

    translated a new string.

--- lang/ja_utf8/error.php
+++ lang/ja_utf8/error.php
@@ -2,7 +2,7 @@
       // error.php - created with Moodle 1.6 development (2005090100)
 
 
-$string['adminprimarynoedit'] = '主Adminは他から編集することはできません。';
+$string['adminprimarynoedit'] = '主管理者は他から編集することはできません。';
 $string['confirmsesskeybad'] = '申し訳ございません、あなたのセッションキーは、このアクションを実行するための確認をできませんでした。このセキュリティ機能は、あなたのユーザ名で重要な機能が不測または悪意により実行されることを防ぎます。本当にこの機能を実行したいか確認してください。';
 $string['coursegroupunknown'] = 'コースに関連するグループ $a が指定されていません。';
 $string['errorcleaningdirectory'] = 'ディレクトリ「 $a 」のクリーニングにエラーが発生しました。';
@@ -28,6 +28,7 @@ $string['onlyeditown'] = '他の人の情報は編集できません。';
 $string['processingstops'] = 'プロセスが停止しました。残りのレコードは無視されます。';
 $string['restricteduser'] = '申し訳ございません。あなたのアカウント「 $a 」では、この処理を行うことができません。';
 $string['sessionerroruser'] = 'セッションがタイムアウトしました。再度ログインしてください。';
+$string['sessionerroruser2'] = 'あなたのログインセッションに影響を与えるサーバエラーが検知されました。再度ログインするかブラウザを再起動してください。';
 $string['sessionipnomatch'] = '申し訳ございません。あなたのIPアドレスは、最初のログインから変更されているようです。このセキュリティ機能は、あなたがサイトにログインしている間に、クラッカーから個人情報を盗むことを防いでいます。通常のユーザは、このメッセージを見ることはありません - 詳細はシステム管理者にお問合せください。';
 $string['unknowncourse'] = '不明なコース名 「 $a 」';
 $string['usernotaddederror'] = 'ユーザ 「 $a 」 は追加されませんでした - 原因不明のエラー';
