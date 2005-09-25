73cff34c90f5b5aa3f534e664ce4c7918227367c
rcantada <rcantada>
2005-09-25 07:08:36 +0000

    new strings

---
+++ lang/tl/my.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // my.php - created with Moodle 1.6 development (2005060201)
+
+
+$string['mymoodle'] = 'Aking moodle';
+$string['nocourses'] = 'Walang maipapakitang impormasyon ng kurso.';
+$string['pinblocks'] = 'Isaayos ang mga idinikit na block para sa aking moodle';
+$string['pinblocksexplan'] = 'Anumang kaayusan ng block na isaayos mo rito ay makikita (at dimaeedit) ng sinumang user ng moodle sa kanilang \'aking moodle\'na screen.';
+
+?>
