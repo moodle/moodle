54afe05f202239a8bcd24006d644549143293497
ethem <ethem>
2005-09-25 18:49:21 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant.

--- lang/tr/error.php
+++ lang/tr/error.php
@@ -1,23 +1,34 @@
 <?PHP // $Id$ 
-      // error.php - created with Moodle 1.5 ALPHA (2005043000)
+      // error.php - created with Moodle 1.6 development (2005081700)
 
 
 $string['adminprimarynoedit'] = 'Ana yönetici baþka kullanýcýlar tarafýndan düzenlenemez';
 $string['confirmsesskeybad'] = 'Üzgünüz, oturum anahtarýnýz bu eylemi gerçekleþtirmek için onaylanamadý. Bu güvenlik özelliði sizin adýnýza yapýlacak olan önemli iþlevlerin yanlýþlýkla ya da kasten kullanýlmasýný engellemek içindir. Lütfen bu iþlevi gerçekten yapmak istediðinizden emin olun.';
 $string['coursegroupunknown'] = 'Grup $a ile ilgili kurs belirtilmemiþ';
+$string['errorcleaningdirectory'] = '\"$a\" dizinini temizlerken hata oluþtu';
+$string['errorcopyingfiles'] = 'Dosyalarý kopyalarken hata oluþtu';
+$string['errorcreatingdirectory'] = '\"$a\" dizinini oluþtururken hata oluþtu';
+$string['errorcreatingfile'] = '\"$a\" dosyasýný oluþtururken hata oluþtu';
 $string['erroronline'] = '$a. satýrda hata var';
+$string['errorreadingfile'] = '\"$a\" dosyasýný okurken hata oluþtu';
+$string['errorunzippingfiles'] = 'Zipli dosyalarý açarken hata oluþtu';
 $string['fieldrequired'] = '\"$a\" doldurulmasý gerekli bir alandýr';
 $string['filenotfound'] = 'Üzgünüz, aranan dosya bulunamadý';
 $string['groupalready'] = 'Kullanýcý $a grubuna zaten kayýtlý';
 $string['groupunknown'] = '$a grubu belirtilen kursla iliþkili deðildir';
 $string['invalidfieldname'] = '\"$a\" geçerli bir alan adý deðildir';
+$string['invalidfiletype'] = '\"$a\" geçerli bir dosya tipi deðil';
+$string['invalidxmlfile'] = '\"$a\" geçerli bir XML dosyasý deðil';
 $string['missingfield'] = '\"$a\" alaný eksiktir';
 $string['modulerequirementsnotmet'] = '\"$a->modulename\" ($a->moduleversion) modülü kurulamadý. Yeni moodle sürümü gerekmektedir (þu anda kullandýðýnýz: $a->currentmoodle, gereken: $a->requiremoodle).';
+$string['nonmeaningfulcontent'] = 'Anlamsýz içerik';
 $string['notavailable'] = 'Þu anda eriþilemez';
+$string['onlyeditingteachers'] = 'Sadece eðitimciler bunu yapabilir';
 $string['onlyeditown'] = 'Sadece kendi bilgilerinizi düzenleyebilirsiniz.';
 $string['processingstops'] = 'Ýþlem burada sona erer. Kalan kayýtlar yoksayýlýr.';
 $string['restricteduser'] = 'Üzgünüz, ancak þu anki \"$a\" hesabýnýz bunu yapmak için sýnýrlandýrýlmýþtýr.';
 $string['sessionerroruser'] = 'Oturumunuz zaman aþýmýna uðradý. Lütfen tekrar giriþ yapýnýz.';
+$string['sessionerroruser2'] = 'Giriþ oturumunuzu etkileyen bir sunucu hatasý algýlandý. Lütfen yeniden giriþ yapýnýz ya da tarayýcýnýzý yeniden baþlatýnýz.';
 $string['sessionipnomatch'] = 'Üzgünüz, IP numaranýz ilk girdiðinizden farklýdýr. Bu güvenlik özelliði siteye girdiðinizde crackerlarýn sizin kimlik bilgilerinizi çalmalarýný engellemek içindir. Normal kullanýcýlarýn bu mesajý görmemeleri gerekir - lütfen yardým için site yöneticisine baþvurun.';
 $string['unknowncourse'] = '\"$a\" adlý kurs bilinmiyor';
 $string['usernotaddederror'] = '\"$a\" kullanýcýsý eklenmedi - bilinmeyen hata';
