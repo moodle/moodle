125f4a65fa33c12957a64be118ca66938c62e469
martignoni <martignoni>
2005-09-30 15:17:55 +0000

    Remaining of new text messages for HotPot v2.1

--- lang/fr/hotpot.php
+++ lang/fr/hotpot.php
@@ -73,7 +73,8 @@ $string['feedbackformmail'] = 'Formulaire de feedback';
 $string['feedbackmoodleforum'] = 'Forum Moodle';
 $string['feedbackmoodlemessaging'] = 'Message Moodle';
   	 
-  	 // lib.php (responses)$string['correct'] = 'Correct';
+  	 // lib.php (responses)
+$string['correct'] = 'Correct';
 $string['ignored'] = 'Ignoré';
 $string['wrong'] = 'Faux';
 $string['score'] = 'Score';
@@ -83,25 +84,117 @@ $string['clues'] = 'Indices';
 $string['checks'] = 'Vérifications';
 $string['penalties'] = 'Pénalités';
   	 
-  	 // report.php
-$string['deletedattempts'] = '$a tentatives ont été supprimées';
-$string['deletedresponses'] = '$a réponses ont été supprimées';
-$string['selectattempt'] = 'Veuillez choisir une tentative';
-  	 
+     // index.php
+$string['regrade'] = 'Renoter';
+$string['regradecheck'] = 'Voulez-vous vraiment renoter «&nbsp;$a&nbsp;»&nbsp;?';
+$string['regraderequired'] = 'Il est nécessaire de renoter';
+
+     // report.php
+$string['reportclick'] = 'Rapport des clics';
+
+$string['reportcontent'] = 'Contenu';
+$string['reportformat'] = 'Format';
+
+$string['reportbutton'] = 'Générer le rapport';
+
 $string['thiscourse'] = 'Ce cours';
 $string['allmycourses'] = 'Tous mes cours';
-$string['bestattempt'] = 'Meilleure tentative';
-$string['allattempts'] = 'Toutes les tentatives';
+
+$string['attemptsall'] = 'Toutes les tentatives';
+$string['attemptsbest'] = 'Meilleure tentative';
+$string['attemptsfirst'] = 'Première tentative';
+$string['attemptslast'] = 'Dernière tentative';
+
+$string['reportformathtml'] = 'HTML';
+$string['reportformatexcel'] = 'Excel';
+$string['reportformattext'] = 'Texte';
+$string['reportencoding'] = 'Encodage';
+$string['reportwrapdata'] = 'Renvoyer les données à la ligne'; // ?
+$string['reportshowlegend'] = 'Afficher la légende';
+
+$string['rawdetails'] = 'Détails bruts de la tentative';     
+     
+//$string['deletedattempts'] = '$a tentatives ont été supprimées';
+//$string['deletedresponses'] = '$a réponses ont été supprimées';
+//$string['selectattempt'] = 'Veuillez choisir une tentative';
   	 
   	 // report/*/report.php
 $string['average'] = 'Moyenne';
 $string['questionshort'] = 'Q-$a';
   	 
+     // report/default.php
+$string['reportlegend'] = 'Légende';
+  	 
   	 // report/overview/report.php
 $string['deleteabandoned'] = 'Supprimer les abandons';
 $string['deleteabandonedcheck'] = 'Voulez-vous vraiment supprimer les $a tentatives abandonées&nbsp;?';
   	 
-  	 // review.php
+ 	 // report/click/report.php
+$string['reportnottriedsymbol'] = '-';
+$string['reportcorrectsymbol'] = 'O';
+$string['reportwrongsymbol'] = 'X';
+
+$string['reportcoursename'] = 'Nom du cours';
+$string['reportsectionnumber'] = 'Section';
+$string['reportexercisenumber'] = 'Exercice';
+$string['reportexercisename'] = 'Nom ex.';
+$string['reportexercisetype'] = 'Type ex.';
+$string['reportnumberofquestions'] = "No de q.";
+
+$string['reportstudentid'] = 'Id. étudiant';
+$string['reportlogindate'] = 'Date connexion';
+$string['reportlogintime'] = 'Heure connexion';
+$string['reportlogofftime'] = 'Heure déconnexion';
+
+$string['reportattemptnumber'] = 'Tentative';
+$string['reportattemptstart'] = 'Début';
+$string['reportattemptfinish'] = 'Fin';
+
+$string['reportclicknumber'] = 'Clics';
+$string['reportclicktime'] = 'Heure du clic';
+$string['reportclicktype'] = 'Type du clic';
+
+$string['reportthisclick'] = '$a ce clic';
+$string['reportsofar'] = '$a jusqu\'ici';
+
+$string['reportquestionstried'] = "Q. tentées";
+
+$string['reportright'] = 'Juste';
+$string['reportwrong'] = 'Faux';
+$string['reportnottried'] = 'Pas tentée';
+
+$string['reportanswers'] = 'Réponses';
+$string['reportchanges'] = 'Modifications';
+$string['reportchecks'] = 'Vérifications';
+$string['reportclues'] = 'Indices';
+$string['reportevents'] = 'Événements';
+$string['reporthints'] = 'Indices';
+$string['reportshowanswer'] = 'Afficher réponses';
+$string['reportstatus'] = 'État';
+
+$string['reportrawscore'] = 'Score brut';
+$string['reportmaxscore'] = 'Score max';
+$string['reportpercentscore'] = 'Score %%';
+
+$string['reporthotpotscore'] = 'Score Hotpot';
+ 	 
+ 	 // review.php
 $string['noresponses'] = 'Aucune information n\'a été trouvée sur les questions et les réponses individuelles.';
+$string['reporttimerecorded'] = 'Réponses enregistrées';
+
+     // config.html
+$string['configshowtimes'] = 'La durée du traitement des enregistrements doit-elle être affichée dans les listes et rapports&nbsp;? Ceci n\'est vraiment que nécessaire si vous essayez de découvrir pourquoi votre serveur est lent.';
+$string['configexcelencodings'] = 'Une liste des encodages pouvant être utilisés pour enregistrer les données exportées dans un encodage spécifique. Les différents encodages doivent être séparés par des virgules.';
+
+     // error messages (lib.php)
+$string['error_nofilename'] = 'Veuillez saisir un nom de fichier';
+$string['error_notfileorfolder'] = '«&nbsp;$a&nbsp;» n\'est pas un fichier, ni un dossier';
+$string['error_nocourseorfilename'] = 'Impossible de créer l\'arborescence XML&nbsp;: le nom du cours ou du fichier est manquant';
+$string['error_couldnotopensourcefile'] = 'Impossible d\'ouvrir le fichier source «&nbsp;$a&nbsp;»';
+$string['error_couldnotopenfolder'] = 'Impossible d\'accéder au dossier «&nbsp;$a&nbsp;»';
+$string['error_couldnotopentemplate'] = 'Impossible d\'ouvrir le modèle du format «&nbsp;$a&nbsp;»';
+$string['error_noquizzesfound'] = 'Aucun test Hot Potatoes trouvé';
+$string['error_nofeedbackurlwebpage'] = 'Veuillez saisir une URL pour la page web';
+$string['error_nofeedbackurlformmail'] = 'Veuillez saisir une URL pour le script de traitement du formulaire';
 
 ?>
