38f78e158c053b1f81724ebd7d099f0a2d996166
mjollnir_ <mjollnir_>
2005-10-04 21:26:01 +0000

    Fixing sortorder typo

--- course/category.php
+++ course/category.php
@@ -138,7 +138,7 @@
                     } else {
                         // figure out a sortorder that we can use in the destination category
                         $sortorder = get_field_sql('SELECT MIN(sortorder)-1 AS min
-                                                    FROM ' . $CFG->prefix . 'course AND category=' . $destcategory->id);
+                                                    FROM ' . $CFG->prefix . 'course WHERE category=' . $destcategory->id);
                         if ($sortorder === false) {
                             // the category is empty
                             // rather than let the db default to 0
