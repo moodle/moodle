01ce0aa3a8e37b52b99b1f6c6fc5cfcec2524b24
stronk7 <stronk7>
2005-10-05 18:23:25 +0000

    Now entries containing apostrophes can be exported to main glossay and deleted without problems. Bug 4136. (http://moodle.org/bugs/bug.php?op=show&bugid=4136)

--- mod/glossary/exportentry.php
+++ mod/glossary/exportentry.php
@@ -1,6 +1,6 @@
 <?php  // $Id$
-    require_once("../../config.php");
-    require_once("lib.php");
+    require_once('../../config.php');
+    require_once('lib.php');
 
     require_variable($id);    // course module ID
     require_variable($entry);    // Entry ID
@@ -13,11 +13,11 @@
 
     $PermissionGranted = 1;
 
-    $cm = get_record("course_modules","id",$id);
+    $cm = get_record('course_modules','id',$id);
     if ( ! $cm ) {
         $PermissionGranted = 0;
     } else {
-        $mainglossary = get_record("glossary","course",$cm->course, "mainglossary",1);
+        $mainglossary = get_record('glossary','course',$cm->course, 'mainglossary',1);
         if ( ! $mainglossary ) {
             $PermissionGranted = 0;
         }
@@ -31,77 +31,79 @@
 
     if ( !isteacher($cm->course) ) {
         $PermissionGranted = 0;
-        error("You must be a teacher to use this page.");
+        error('You must be a teacher to use this page.');
     }
 
-    if (! $course = get_record("course", "id", $cm->course)) {
-        error("Course is misconfigured");
+    if (! $course = get_record('course', 'id', $cm->course)) {
+        error('Course is misconfigured');
     }
 
-    if (! $glossary = get_record("glossary", "id", $cm->instance)) {
-        error("Course module is incorrect");
+    if (! $glossary = get_record('glossary', 'id', $cm->instance)) {
+        error('Course module is incorrect');
     }
 
-    $strglossaries   = get_string("modulenameplural", "glossary");
-    $entryalreadyexist = get_string("entryalreadyexist","glossary");
-    $entryexported = get_string("entryexported","glossary");
+    $strglossaries   = get_string('modulenameplural', 'glossary');
+    $entryalreadyexist = get_string('entryalreadyexist','glossary');
+    $entryexported = get_string('entryexported','glossary');
 
-    print_header_simple(format_string($glossary->name), "",
-                 "<a href=\"index.php?id=$course->id\">$strglossaries</a> -> ".format_string($glossary->name),
-                  "", "", true, "",
+    print_header_simple(format_string($glossary->name), '',
+                 '<a href="index.php?id='.$course->id.'">'.$strglossaries.'</a> -> '.format_string($glossary->name),
+                  '', '', true, '',
                   navmenu($course, $cm));
 
     if ( $PermissionGranted ) {
-        $entry = get_record("glossary_entries", "id", $entry);
+        $entry = get_record('glossary_entries', 'id', $entry);
 
         if ( !$confirm ) {
-            echo "<center>";
-            $areyousure = get_string("areyousureexport","glossary");
-            notice_yesno ("<center><h2>$entry->concept</h2><p align=\"center\">$areyousure<br /><b>".format_string($mainglossary->name)."</b>?",
-                "exportentry.php?id=$id&amp;mode=$mode&amp;hook=$hook&amp;entry=$entry->id&amp;confirm=1",
-                "view.php?id=$cm->id&amp;mode=$mode&amp;hook=$hook" );
+            echo '<center>';
+            $areyousure = get_string('areyousureexport','glossary');
+            notice_yesno ('<center><h2>'.$entry->concept.'</h2><p align="center">'.$areyousure.'<br /><b>'.format_string($mainglossary->name).'</b>?',
+                'exportentry.php?id='.$id.'&amp;mode='.$mode.'&amp;hook='.$hook.'&amp;entry='.$entry->id.'&amp;confirm=1',
+                'view.php?id='.$cm->id.'&amp;mode='.$mode.'&amp;hook='.$hook);
 
         } else {
             if ( ! $mainglossary->allowduplicatedentries ) {
-                $dupentry = get_record("glossary_entries","glossaryid", $mainglossary->id, "$ucase(concept)",strtoupper($entry->concept));
+                $dupentry = get_record('glossary_entries','glossaryid', $mainglossary->id, $ucase.'(concept)',strtoupper(addslashes($entry->concept)));
                 if ( $dupentry ) {
                     $PermissionGranted = 0;
                 }
             }
             if ( $PermissionGranted ) {
-            
-                $entry->glossaryid       = $mainglossary->id;
-                $entry->sourceglossaryid = $glossary->id;
-                
-                if (! update_record("glossary_entries", $entry)) {
-                    error("Could not export the entry to the main glossary");
-                } else {
-                    print_simple_box_start("center", "60%");
-                    echo "<p align=\"center\"><font size=\"3\">$entryexported</font></p></font>";
 
-                    print_continue("view.php?id=$cm->id&amp;mode=entry&amp;hook=".$entry->id);
+                $dbentry = new stdClass;
+                $dbentry->id = $entry->id;
+                $dbentry->glossaryid       = $mainglossary->id;
+                $dbentry->sourceglossaryid = $glossary->id;
+                
+                if (! update_record('glossary_entries', $dbentry)) {
+                    error('Could not export the entry to the main glossary');
+                } else {
+                    print_simple_box_start('center', '60%');
+                    echo '<p align="center"><font size="3">'.$entryexported.'</font></p></font>';
+
+                    print_continue('view.php?id='.$cm->id.'&amp;mode=entry&amp;hook='.$entry->id);
                     print_simple_box_end();
 
                     print_footer();
 
-                    redirect("view.php?id=$cm->id&amp;mode=entry&amp;hook=".$entry->id);
+                    redirect('view.php?id='.$cm->id.'&amp;mode=entry&amp;hook='.$entry->id);
                     die;
                 }
             } else {
-                print_simple_box_start("center", "60%", "#FFBBBB");
-                echo "<p align=\"center\"><font size=\"3\">$entryalreadyexist</font></p></font>";
-                echo "<p align=\"center\">";
+                print_simple_box_start('center', '60%', '#FFBBBB');
+                echo '<p align="center"><font size="3">'.$entryalreadyexist.'</font></p></font>';
+                echo '<p align="center">';
 
-                print_continue("view.php?id=$cm->id&amp;mode=entry&amp;hook=".$entry->id);
+                print_continue('view.php?id='.$cm->id.'&amp;mode=entry&amp;hook='.$entry->id);
 
                 print_simple_box_end();
             }
         }
     } else {
-            print_simple_box_start("center", "60%", "#FFBBBB");
-            notice("A weird error was found while trying to export this entry. Operation cancelled.");
+            print_simple_box_start('center', '60%', '#FFBBBB');
+            notice('A weird error was found while trying to export this entry. Operation cancelled.');
 
-            print_continue("view.php?id=$cm->id&amp;mode=entry&amp;hook=".$entry->id);
+            print_continue('view.php?id='.$cm->id.'&amp;mode=entry&amp;hook='.$entry->id);
 
             print_simple_box_end();
     }
