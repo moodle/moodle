ae11b653f16bd4228ab497ceb7f497e1b45dab11
mudrd8mz <mudrd8mz>
2005-10-05 10:58:08 +0000

    Translated from scratch

---
+++ lang/cs/quiz_analysis.php
@@ -0,0 +1,31 @@
+<?PHP // $Id$ 
+      // quiz_analysis.php - created with Moodle 1.5.2 + (2005060223)
+
+
+$string['analysisoptions'] = 'Volby analýzy';
+$string['analysistitle'] = 'Tabulka polo¾kové analýzy';
+$string['attemptsall'] = 'v¹echny pokusy';
+$string['attemptselection'] = 'Pokusy k analýze podle u¾ivatele';
+$string['attemptsfirst'] = 'první pokus';
+$string['attemptshighest'] = 'nejvy¹¹í známka';
+$string['attemptslast'] = 'poslední pokus';
+$string['dicsindextitle'] = 'Diskrim.<br />index';
+$string['disccoefftitle'] = 'Diskrim.<br />koef.';
+$string['downloadooo'] = 'Stáhnout ve formátu OpenOffice';
+$string['facilitytitle'] = '%% Správných<br />mo¾nosti';
+$string['lowmarkslimit'] = 'Neprovádìt analýzu, pokud je skór men¹í ne¾:';
+$string['pagesize'] = 'Poèet úloh na stránku:';
+$string['qcounttitle'] = 'Úl. poèet';
+$string['qidtitle'] = 'Úl. è.';
+$string['qnametitle'] = 'Název úlohy';
+$string['qtexttitle'] = 'Text úlohy';
+$string['qtypetitle'] = 'Úl. typ';
+$string['quizreportdir'] = 'analyzy_testu';
+$string['rcounttitle'] = 'Od.<br />poèet';
+$string['reportanalysis'] = 'Zpráva polo¾kové analýzy';
+$string['responsestitle'] = 'Text odpovìdi';
+$string['rfractiontitle'] = 'èásteèný<br />kredit';
+$string['rpercenttitle'] = 'Od.%%';
+$string['stddevtitle'] = 'SD';
+
+?>
