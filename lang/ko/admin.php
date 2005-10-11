f73d82a1dd5d8bb9a40e95e659859c8f50b081ca
scop22 <scop22>
2005-10-11 13:39:58 +0000

    Committing EUC-KR language pack converted from UTF8 including many new help and docs files.

--- lang/ko/admin.php
+++ lang/ko/admin.php
@@ -5,41 +5,45 @@
 $string['adminseesallevents'] = '관리자가 모든 이벤트를 봄';
 $string['adminseesownevents'] = '관리자는 다른 사용자들과 같음';
 $string['backgroundcolour'] = '바탕화면 색상';
-$string['badwordsconfig'] = '콤마에 의해 분리된 나쁜 언어 목록을 입력하시오';
+$string['badwordsconfig'] = '금기 언어들을 콤마로 구분해 입력하시오';
 $string['badwordsdefault'] = '사용자가 정리한 리스트가 비어있다면 언어팩에 들어있는 기본적인 리스트가 사용될 것입니다';
 $string['badwordslist'] = '나쁜 언어 목록';
 $string['blockinstances'] = '보기';
 $string['blockmultiple'] = '다각';
-$string['cachetext'] = '텍스트 기업 메모리 ';
+$string['cachetext'] = '텍스트 기억 메모리 ';
 $string['calendarsettings'] = '일람표';
 $string['change'] = '변경';
-$string['configallowcoursethemes'] = '이것을 가능하면, 그때 과정들은 그들의 자신의 주제로 맞춰질 것이다. 과정 주제는 모든 다른 주제 선택 (위치, 사용자, 또는 회의 주제)을 무시한다';
-$string['configallowemailaddresses'] = '만약 당신이 특별한 도메인에서 모든 새로운 이메일 주소를 제한한다면, 그것들은 공간에 의해 여기에 분리될 것이다. 모든 다른 도메인들은 거부된다. eg <strong>우리대학.교육.화학.정부.물리</strong>';
+$string['configallowcoursethemes'] = '이것을 활성화하면, 각 배움터는 나름대로 독특한 테마로 설정될 수 있다. 배움터 테마는 모든 다른 테마(사이트, 사용자, 또는 세션 테마)선택보다 우선하여 적용된다';
+$string['configallowemailaddresses'] = '만약 당신이 특별한 도메인의 이메일 주소만 허용하고자 한다면, 그 목록들을 한칸씩 띄어 적어 놓아라. 그 외 모든 다른 도메인들은 거부된다. ( 예: <strong>sun.ac.kr moodle.org</strong> )';
 $string['configallowobjectembed'] = '지정된 설정값에 의해서 일반 사용자들은 멀티미디어 파일 (예:플래시,동영상,mp3)를 명령어나 태그를 사용해서 올릴수 없습니다. (비록 안전한 미디어 파일일지라도) 운영자가 허락 옵션을 켜야만 파일을 올릴수 있습니다.';
-$string['configallowunenroll'] = '만약 이것이 \"네\"로 설정된다면, 학생들은 그들 자신이 좋아하는 과정으로부터 자신들을 등록시킬수 없다. 반면에 그들이 허락되지 않으면, 이 과정은 선생님과 관리자에 의해 단독으로 운영될 것이다.';
+$string['configallowunenroll'] = '만약 이것이 \"네\"로 설정된다면, 학생들은 스스로 배움터에서 언제든지 탈퇴할 수 있다. 반면에 \"아니오\"로 설정하면 선생님과 관리자에 의해서만 학생의 배움터 탈퇴여부가 결정되게 된다.';
 $string['configallowuserblockhiding'] = '당신은 사용자들에게 이 싸이트 안에서 숨기기/보기 블록을 허락하길 원합니까? 이것의 특징은 각각의 분리된 블록을 기억하기 위해 자바스크립트와 쿠키를 사용한 것이다. 그리고 사용자들 자신의 의견에 영향을 미친다.';
-$string['configallowuserthemes'] = '이것을 가능하면, 그때 사용자들은 그들의 자신의 주제로 맞춰질 것이다. 사용자들 주제는 모든 싸이트 주제 (과정 주제가 아닌)를 무시한다.';
-$string['configallusersaresitestudents'] = '이 사이트의 첫페이지의 활동을 할때 모든 사용자는 할생으로써 간주도어야 하는가? 만약 당신이 \"네\"라고 대답한다면 어떤 승인된 사용자는 이 활동에서 학생으로 참여하도록 허락될 것이다. 만얀 당신이 \"아니요\"라고 대답한다면 오직 사용자는 활동의 첫페이지의 적어도 한 코스만 참여할 것이다. 오직 관리자와 특별히 임명된 교사들은 첫페이지에서 교사로써 행동할수 있다.';
+$string['configallowuserthemes'] = '이것을 활성화하면, 사용자들은 그들의 자신의 테마를 사용할 수 있게된다. 사용자 테마는 기존의 싸이트 테마(과정 주제가 아닌)보다 우선적으로 적용된다.';
+$string['configallusersaresitestudents'] = '이 사이트의 첫페이지의 활동에 대하여 모든 사용자는 학생으로 간주되도록 할 것인가? 만약 당신이 \"네\"라고 설정한다면 승인된 사용자 누구라도 이 활동에서 학생으로 참여하도록 허락될 것이다. 만얀 당신이 \"아니요\"라고 대답한다면 오직 사용자는 활동의 첫페이지의 적어도 한 코스만 참여할 것이다. 오직 관리자와 특별히 임명된 교사들은 첫페이지에서 교사로써 행동할수 있다.';
 $string['configautologinguests'] = '이 과정에 손님으로 들어올때 손님은 자동적으로 로그인이 되어야 할까?';
 $string['configcachetext'] = '큰 싸이트나 본문 여과를 사용하는 싸이트라면 이 구조는 정말 빠르게 움직일 것이다. 본문이 복사는 그들의 과정표를 발호돈 시간 여기로부터 보유할 것이다. 구조가너무 작은 것은 사실 천천히 진행되지만 큰 구조는 아마도 너무 길어서 새로 공급할 수 없을 것이다.(예를 들어, 새로운 링크)';
 $string['configclamactlikevirus'] = '바이러스 파일 처리하기';
 $string['configclamdonothing'] = '정상적인 파일 처리';
-$string['configclamfailureonupload'] = '만약 당신이 업로드 된 파일을 스캔 하기 위해서 배열된 꺽쇠를 가진다면 이것은 알려지지않은 이유때문에 불확실하게 또는 실패할 것이다. 어떻게 그것이 행동할수 있겠는가? 만약 당신이 바이러스같은 파일을 선택한다면 그들은 격리 구역으로 가거나 삭제 될것이다. 만약 당신이  오케이라는 파일을 선택하면 그 파일은 일반적인 desination으로 이동할 것이다. 관리자는 변환할것이고 꺽쇠는 실패한다. 만약 당신이 바이러스 파일을 선택한다면 보통 니가 불가능한 경로로 들어가기 때문에 생긴다)업로드 된 모든 파일은 격리된 구역으로 옮겨지거나 삭제 된다 이 구조를 조심해라';
-$string['configcountry'] = '만약 당신이 여기에 나라를 설정한다면, 이 나라는 새로운 사용자 보고의 디폴트 값에 의해 선택될 것입니다. 영향력을 가진 사용자가 나라를 선택하면, 이 상태로 나누십시오.';
+$string['configclamfailureonupload'] = '만약 당신이 업로드 된 파일을 스캔 하도록 지정하고, 잘 못 설정되거나 알려지지 않은 이유 때문에 오류가 발생한다면, 어떻게 될까? 이 때 만약 당신이 \'바이러스 파일 처리하기\'를 선택한다면 그들은 격리 구역으로 가거나 삭제 될것이다. 만약 당신이  \'정상적인 파일 처리\'를 선택하면 그 파일은 일반적인 저장고로 이동할 것이다. 어쨌든 관리자는 이에 대한 경고를 받을 것이다. 만약 당신이 \'바이러스 파일 처리하기\'를 선택했는데, 어떤 이유에서건 실행 오류를 만나게 된다면(대개는 불가능한 경로를 설정했기 때문에 생긴다)업로드 된 모든 파일은 격리된 구역으로 옮겨지거나 삭제 된다. 이 설정을 할 때는 매우 조심하라.';
+$string['configcountry'] = '만약 당신이 여기에 국가를 설정한다면, 설정된 국가는 새로운 사용자계정의 기본값으로 설정됩니다. 사용자들이 스스로 국가(언어)를 설정하도록 하고 싶으면, 설정하지 말고 이 상태로 놓아두십시오.';
 $string['configdbsessions'] = '만약 할수 없다면, 이 구조는 현재의 과업에서 정보를 저장하기 위해서데이타 베이스를 사용할 것이다. 이것은 특히 유용하다 큰 싸이트나 많은 서버를 포함한 사이트에서 말이다.
 대부분의 싸이트에서 이것은 아마도 불가능하다 서버 디스크가 대신 사용되는 것은,이 구조는 지금 로그 아웃될것이다 현재의 모든 사용자를 그리고 바뀔 것이다.';
 $string['configdebug'] = '이것을 연다면 지속적인 경고를 위해서 PHP 에러 보고가 증가할 것입니다.';
+$string['configdefaultallowedmodules'] = '당신의 교육과정이 범주안에 만들어질때 모듈설정을 기본값으로 허용하시겠습니까?';
 $string['configdefaultrequestedcategory'] = '기본설정 범주를 교육과정에 집어넣고 서버에 요구하면 승인될 것입니다';
 $string['configdeleteunconfirmed'] = '만약 당신이 이메일 인증제를 사용한다면, 이 기간은 사용자들로부터 받아들여진 것에 대해 반응해야 한다. 이 기간이 끝나면 예전의 확인되지 않은 보고는 삭제된다. ';
-$string['configdenyemailaddresses'] = '같은 방법으로 여기에 있는 특별한 도메인 리스트의 이메일 주소를 부정한다. 모든 다른 도메인들은 허락된다. eg<strong>hotmail.com yahoo.co.uk<strong/>';
-$string['configdigestmailtime'] = '이해되 그들을 메일로 보내기로 선택한 사람들은 메일을 매일 받을 것이다. 이것의 구조는 통제된다 낮에 그리고 보내진다.(다음은 이시간 다음에 보내진다) ';
+$string['configdenyemailaddresses'] = '같은 방법으로 여기에 있는 특별한 도메인 리스트의 이메일 주소를 부정한다. 모든 다른 도메인들은 허락된다. 예: <strong>hotmail.com yahoo.co.uk<strong/>';
+$string['configdigestmailtime'] = '미리 메일을 전송해 달라고 요청을 한 사람들에게 매일 요약을 보낼 것이다. 이 설정은 메일 발송 시간, 날짜 등을 정하는 것이다. (cron 은 여기에 지정된 시간 이후에 작동하여 이 메일을 발송할 것이다) ';
 $string['configdisplayloginfailures'] = '이것은 이전에 실패한 로그인에 대해 선택되어진 사용자들을 위해 정보를 표시할 것입니다.';
 $string['configenablecourserequests'] = '수업과정을 만드려고 하는 모든 사용자에게 허락됩니다.';
 $string['configenablerssfeeds'] = '이 스위치는 싸이트를 가로질러 RSS급송 장치를 가능하게 한다. 당신이 개인적 모듈안에서 RSS 급송 장치를 가능하게 할 필요가 있다면, 실질적으로 어떤 변화가 있는지 보아라 -관리자 환경설정 아래 있는 모듈 설정으로 가라.  ';
 $string['configenablerssfeedsdisabled'] = '이것은 이용할 수 없다. 왜냐하면 모든 싸이트에서 RSS 급송장치가 고장났기 때문이다. 그것들을 다시 가능하게 하려면 관리자 환경설정 아래 있는 다양한 설정으로 가라.';
+$string['configenablestats'] = '여기서 \'예\'를 선택하면 무들의 일정관리 기능은 여러 기록과 통계를 수합할 것이다. 사이트의 부하에 따라 시간이 좀 더 걸릴 수도 있다. 이 기능을 설정할 경우, 여러분의 배움터는 물론 사이트 전체에 관련된 여러 그래프와 통계를 볼 수 있다. ';
 $string['configerrorlevel'] = '당신이 표시하길 원했던 PHP경고의 양을 선택하라. 대부분 보통이 가장 좋은 선택이다.';
 $string['configextendedusernamechars'] = '이 설정은 학생들이 그들의 사용자 이름 안에서 어떤 캐릭터라도 허락할 수 있도록 하였다.(그들의 실제적 이름의 영향을 받지 않더라도 기록) 제한된 사용자이름에 있어 문자와 숫자를 조합한 캐릭터 만이 디폴트가 \"오류\"로 뜬다.';
-$string['configfilterall'] = '모든 string 여과장치는 머릿말, 제목,방향을 알리는 부분등등에 포함된다. 이것은 multilang 여과장치에 유용하다.이것은 나머지 로드를 창조한다.';
+$string['configfilterall'] = '여과장치는 머릿말, 제목, 주소창 등등에 포함된 모든 문자열을 검토한다. 대개 다국어 여과장치를 쓸 때에 유용하지만, 얻는 것에 비해 사이트에 추가적인 부하가 생기는 것이 문제일 수 있다.';
+$string['configfiltermatchoneperpage'] = '자동 링크 필터는 처음 텍스트에 나온 한 단어에 자동 링크를 시키고 나머지 단어들은 무시합니다.';
+$string['configfiltermatchonepertext'] = '자동 링크 필터는 한 페이지에 첫번째 단어에 자동 링크를 시키고 나머지 것들은 모두 무시합니다. 이 설정은 한 페이지에만 적용이 됩니다.';
 $string['configfilteruploadedfiles'] = '이용 가능한 이구조는 Moodle을 야기할 것이다.모든 HTML을 업로드 하기위해서 전에 그들에게 보여준 여과장치와 함께 본 파일을 업로드하기위해서도 야기한다. ';
 $string['configforcelogin'] = '일반적으로 이 싸이트의 첫페이지와 과정의 리스트는 로그인 하지 않고서도도 사람들에 의해서 읽힐수 있다. 만약 당신이 로그인 사람에게 강요하기를 원한다면 ㄱ들은 어떤것이라도 싸이트에서 해야한다 그리고 나서 너는 이 구조를 사용 가능해야만 한다.';
 $string['configforceloginforprofiles'] = '이용가능한 이 구조에 로그인 하기 위한 실제 사람들을 강요하기 위해서프로필 페이지의 사용자를 보기를 허락해라. ㅇ것은 불가능하다 그래서 열심히 하는 학생들은 읽을수 있을 것이다 각 코스에 따르느 선생님에 대해서 그러나 이럿은 또한 의미한다 웹은 그들을 볼수 있다는 거이다.';
@@ -56,7 +60,7 @@ $string['configlang'] = '전체 싸이트를 위한 디폴트 언어를 선택해라. 사용자들은 �
 $string['configlangcache'] = '언어 메뉴를 저장하라. 많은 메모리와 진행 동력이 절약 될 것이다. 만약 당신이 이것을 지정한다면. 메뉴는 당신이 언어를 더하거나 제거한 후에 짧은 시간 안에 업데이트 될 거이다.';
 $string['configlangdir'] = '대부분 언어들은 왼쪽에서 오른쪽으로 인쇄되나, 아라비아어나 헤브리아어 같은 일부 언어들은 오른족에서 왼쪽으로 인쇄된다.';
 $string['configlanglist'] = 'Moodle의 설치 안에서 당신이 가지고 있는 어떤 언어로 부터 사용자들이 선택할 수 있도록 빈칸을 남겨두는 것을 허락해라. 그러나, 당신은 원하는 언어코드의 콤마로 분리된 리스트를 넣음으로 인해 당신의 언어 메뉴를 짧게 할 수 있다.
-예를 드면: en, es_es,fr,it';
+예를 드면: en,es_es,fr,it';
 $string['configlangmenu'] = '일반적 목적의 언어 메뉴를 보여주기를 니가 원하든 그렇든 선택해라 홈페이지에서 그리고 페이지를 로그인 해라. 이 것은 영향을 주지 않는다 사용자의 능력을 좋아하는 언어를 사용해서그들의 자신의 프로필에서 찾아내서 말이다.';
 $string['configlocale'] = '사이트표시 로케일을 선택해라 이것을 영향을 줄것이다 그 포맷과 언어를 너는 이 로케일을 가질 필요가 있다 이것은 너의 번역된 시스템위헤 인스톨되어있다.(US 나 ES)만약 니가 선택할것을 모른다면 그것을 공백으로 남겨두어라.';
 $string['configloginhttps'] = 'configloginhttps를 켜는것은 Moodle이 로그인페이지을 위한 보안 http연결을 사용하게 해준다. 그리고 이후에 revert는 평소속도인 normal http URL로 돌아간다.
@@ -64,14 +68,16 @@ $string['configloginhttps'] = 'configloginhttps를 켜는것은 Moodle이 로그인페이지
 $string['configloglifetime'] = '이것은 사용자의 활동에 관한 로그를 당신이 유지하고 싶은 시간의 길이를 정해준다. 이 시간보다 길어지면 자동적으로 로그는 제거된다. 당신이 그것을 필요로 할 시에 이것은 로그를 가능한한 오래 유지할 수 있는 가장 좋은 방법이다. 그러나 당신의 서버가 busy하고 작동상의 문제를 가지고 있다면, 당신은 로그 lifetime 낮추는 것이 더 나을것이다.   ';
 $string['configlongtimenosee'] = '만약 학생들이 오랫동안 로그인을 하지않았다면, 그들은 그 코스들로부터 자동 삭제 된다. 이 변수는 그 시간 한도를 규정한다.';
 $string['configmaxbytes'] = '이것은 전체 사이트에 있을 수 있는 업로드된 파일들의 최대 사이즈를 규정한다. 이 셋팅은 PHP setting upload_max_filesize 과 Apache setting LimitRequestBody 에 의해 제한된다. 결과적으로, maxbytes는 course level 이나  module level에서 선택되어질 수 있는 사이즈의 범위를 제한한다. ';
-$string['configmaxeditingtime'] = '이것은 사용자들이 forum postings이나 journal feedback등을 재편집해야하는 시간의 양을 규정한다.
+$string['configmaxeditingtime'] = '이것은 사용자들이 포럼 게시나 저널의 답장 등을 재편집해야하는 시간의 양을 규정한다.
 일반적으로 30분이 적당하다.';
 $string['configmessaging'] = '싸이트 사용자들 간의 메세지 시스템이 잘 이루어지고 있습니까?';
+$string['configmymoodleredirect'] = '이 설정은 관리자가 아닌 사람이 로그인 할 때 강제적으로 /my 경로로 가게하고 /my 경로에 새로운 환경을 구성하도록 만듭니다. ';
 $string['confignoreplyaddress'] = '이메일은 때때로 사용자 자신에게 보내집니다. ';
 $string['confignotifyloginfailures'] = '만약 로그인 실패가 기록된다면, 이메일을 통해 통보될 것입니다. 누가 이런 통보를 보겠습니까?';
 $string['confignotifyloginthreshold'] = '로그인실패에 관한 인식이 작동한다면, 한사람 또는 한 IP주소에 의한 실패된 로그인시도의 횟수가 얼마나 많냐는 것을 인식하는 것이 가치가 있는가?';
 $string['configopentogoogle'] = '만약 니가 이 셋팅을 할 수 있단면, 구글은 너의 사이트에 Guest로 들어올 수 있게 된다. 또한, 구글서치를 통해서 들어오는 사람들도 자동적으로 Guest로서 로그인 될 것이다. 그리고 이것은 단지 Guest들이 갈수 있는 허락된 코스에 까지만 분명히 연결 되어질 수 있다.';
 $string['configpathtoclam'] = 'clam AV로 갈 수 있는 길.  /usr/bin/clamscan 이나 /usr/bin/clamdscan 처럼. clam AV를 실행하기 위해 이것이 필요하다.';
+$string['configpathtodu'] = 'du의 경로. 대개 /usr/bin/du 가 된다. 만약 이를 설정해 놓는다면, 이 경로에 있는 내용들이 다른 많은 파일들이 들어있는 경로보다 빠르게 출력될 것이다. ';
 $string['configproxyhost'] = '이서버가 인터넷에 접속하기위해 프록시 컴퓨터를 사용해야한다면, proxy hostname과 port를 이곳에 제공해라. 아니면 비워라.';
 $string['configquarantinedir'] = '만약 당신이 clam AV가 감염된 파일들을 quarantine 디렉토리로 이동시키길 원한다면, 이것을 여기에 입력시켜라.
 이것은 반드시 웹서버에 의해 기록되어질 수 있어야 한다. 
@@ -92,46 +98,51 @@ $string['configsectionoperatingsystem'] = '작동시스템';
 $string['configsectionpermissions'] = '승인';
 $string['configsectionrequestedcourse'] = '교육과정 요구';
 $string['configsectionsecurity'] = '보안';
+$string['configsectionstats'] = '통계들';
 $string['configsectionuser'] = '사용자';
-$string['configsecureforms'] = 'Moodle은 웹형태들로부터 데이터를 받을 때의 추가적인 보안의 레벨을 사용할 수 있다. 만약 이것이 되어진다면, browser\'s HTTP_REFERER variable은 현재의 form adress와 반대로 확인되어진다. 이것은 매우적은 경우이지만, 만약 사용자가  웹 트래픽으로부터 HTTP_REFERER를 벗겨내기위해 설정된 방화벽을 사용할 경우 이것은 문제들를 야기할 수 있다. 증상들은 점점 폼에 갇혀있게 된다. 만약 당신의 유저들이 로그인 페이지에 문제를 가지고 있다면 당신은 이 셋팅을 작동시키지 말아야한다. 이것이 당신의 사이트를 brute-force password 공격에 더 노출 시킬지라도. 
-만약 의심스럽다면, 이 셋팅을 \'yes\'로 놓아라. ';
+$string['configsecureforms'] = 'Moodle은 웹형태들로부터 데이터를 받을 때의 추가적인 보안 레벨을 사용할 수 있다. 만약 이것이 활성화된다면, 브라우저의 HTTP_REFERER 변수는 현재의 form adress와 반대로 확인된다. 매우 드믄 경우이지만, 만약 사용자가  웹 트래픽으로부터 HTTP_REFERER를 벗겨내기위해 설정된 방화벽을 사용할 경우 문제를 야기할 수 있다. 증상들은 점점 폼에 갇혀있게 된다. 만약 당신의 유저들이 로그인 페이지에 문제를 가지고 있다면, 사이트를 brute-force password 공격에 더 노출 시킬지라도, 이 설정을 꺼 두려고 할 것이다. 자신이 없으면, 설정된 그대로 \'yes\'로 놓아 두어라. ';
 $string['configsessioncookie'] = '이 셋팅은 Moodle세션을 위해 사용되어진 쿠기의 이름을 따왔다. 이것은 선택적이고, 같은 페이지의 웹사이트 안에서 하나 이상의 Moodle의 카피가 작동되어질 때 혼돈되어지는 쿠키를 피할 때만 유용하다. ';
 $string['configsessiontimeout'] = '이 사이트에 로그인한 사람들이 오랫동안 시간을 허비한다면, 자동적으로 로그아웃 된다. 이것은 변수는 이 시간이 얼마가 되야되는지를 규정한다.';
 $string['configshowblocksonmodpages'] = '어떤 활동 모듈은 그들의 페이지들을 차단한다. 만약 당신이 이것을 켜면, 선생들은 그 페이지들에 사이드 블락을 추가시킬 수 있을 것이다. 그렇지 않으면, 그 인터페이스 그 화면을 보여주지 않는다.';
 $string['configshowsiteparticipantslist'] = '이 사이트의 모든 선생님들과 학생들은 사이트 참가자 목록에 기록되어질 것이다. 이 사이트 참가자 목록을 볼 수 있게 허락되어진 사람은 누구인가? ';
 $string['configsitepolicy'] = '만약 당신이 모든 사용자가 반드시 봐야되는 그리고 이 사이트를 사용하기 전에 동의해야하는 사이트 정책을 가지고 있다면 이곳에 URL을 규정하라. 그렇지 않으면, 이 공간을 비워둬라. 그 URL은 어떠한곳도 지정할 수 있다-하나의 편리한 장소는 그 사이트 파일들 안에서 하나의 파일이 된다. 
- eg http://yoursite/file.php/1/policy.html';
+ 예: http://yoursite/file.php/1/policy.html';
 $string['configslasharguments'] = '파일들은 슬래쉬 아규먼트를 사용하는 스크립트를 통해서 제공되어진다. 이 방법은 파일들이 웹 브라우저나 프록시 서버등에서 훨씬 쉽게 저장되도록 한다. 불행하게도, 어떤 PHP서버들은 이 방법을 허용하지 않는다. 그래서 만약 당신이 업로드된 파일들이나 이미지들을 보는데 문제가 있다면, 이 변수를 처음 옵션으로 셋팅하시오.';
 $string['configsmtphosts'] = 'Moodle이 메일을 보낼때 사용하는 local SMTP servers들의 전체 이름을 주어라.
 만약 당신이 이곳을 빈칸으로 남겨두면 Moodle은 메일을 보내는것에 있어서 PHP default 방법을 사용할 것이다.';
 $string['configsmtpuser'] = '만약 당신이 일일이 열거된 SMTP 서버를 갖고 있다면, 이 서버는  인증,사용자 이름, 비밀번호를 여기에 칠것을 요구한다. ';
+$string['configstatsfirstrun'] = '이 곳에서는 cron이 통계를 처리한 후 얼마나 오래 기록을 유지하고 있어야 할지를 지정합니다. 만일 매우 접속이 많거나 서버를 임대해 쓰는 경우, 통계처리에 걸리는 시간도 길고 부하도 많이 걸리기 때문에, 기록을 오래 유지시키는 일은 바람직하지 않습니다.(여기에서는 1 month = 28 days로 설정했고, 그래프와 보고에서는 1 month = 1 calendar month 로 했음에 유의하라)  ';
+$string['configstatsmaxruntime'] = '통계 프로세스는 매우 부하가 많이 걸리기 때문에 언제 시작하고 얼마나 유지할지를 이 항목과 다음 항목에서 지정해 주어야 합니다. ';
+$string['configstatsruntimestart'] = '언제 자동으로 통계 프로세스를 <b>가동</b>할까요?';
 $string['configteacherassignteachers'] = '보통의 선생님들은 그들이 가르치고 있는 과정 안의 다른 선생님들의 정한 것을 따라야만 할까? 만약 아니라면, 그것은 단지 과정 창시자나 관리자만이 선생님들에게 지정할 수 있다. ';
-$string['configthemelist'] = '사용되었던 어떤 정확한 주제를 허락할 수 있도록 이 빈칸을 남겨두세요. 만약 당신이 주제 메뉴를 짧게 하길 원한다면, 당신은 조건으로 여기에 이름을 콤마로 분리된 리스트로 지정할 수 있습니다. 예를 들면: 표준화된, orangewhite';
-$string['configtimezone'] = '당신은 DEFAULT timezone여기에 셋팅할 수 있다. 이것은 단지 날짜를 표시하기 위한 DEFAULT timezone이다-각각의 사용자는 그들의 개인 신상에 있어 그들 자신의 셋팅에 의해 이것을 오버라이드 할 수 있다. 이곳의 \"서버타임\"은 서버의 작동 시스템 셋팅에 Moodle default를 만들것이다. 그러나 사용자 신상에 있는 \"서버타임\"은 이 timezone setting에 user default를 만들 것이다.
+$string['configthemelist'] = '테마를 사용하려면 이 부분을 빈칸을 남겨두세요. 만약 테마 메뉴를 단축하길 원한다면, 여기에다 콤마로 분리해서 목록 이름을 지정할 수 있습니다. (예: standard,orangewhite)';
+$string['configtimezone'] = '표준시간대를 여기에 설정합니다. 이것은 단지 날짜를 표시하기 위한 표준시간대이다-각각의 사용자는 그들의 개인 신상에 있어 그들 자신의 셋팅에 의해 이것을 오버라이드 할 수 있다. 이곳의 \"서버타임\"은 서버의 작동 시스템 셋팅에 Moodle default를 만들것이다. 그러나 사용자 신상에 있는 \"서버타임\"은 이 시간대 설정에 user default를 만들 것이다.
  ';
-$string['configunzip'] = '당신의 unzip 프로그램의 위치(유닉스만,선택사항)를 가르키세요. 
-만약 조건으로 지정한다면, 이것은 서버상에서 zip 문서를 압축된 데이터로 풀 것이다. 
-만약 당신이 이 빈칸을 비워둔다면, Moodle은 내부의 루틴을 사용할 것이다.';
-$string['configvariables'] = '변화';
-$string['configwarning'] = '이러한 설치를 수정하는 것을 조심히 하십시오-생소한 값이 문제를 일으킬 수 있습니다.';
-$string['configzip'] = '당신의 zip 프로그램의 위치(유닉스만,선택사항)를 가르키세요. 만약 조건으로 지정한다면, 이것은 서버상에서 zip 문서를 생성시킬 것이다. 만약 당신이 이 빈칸을 비워둔다면, Moodle은 내부의 루틴을 사용할 것이다.';
+$string['configunzip'] = 'unzip 프로그램의 위치(유닉스만,선택사항)를 적으세요. 
+만약 여기에 기록되면, 이것은 서버상에서 압축된 zip 문서를 푸는데 사용된다.
+만약 여기를 비워둔다면, Moodle은 내부 루틴을 사용할 것이다.';
+$string['configvariables'] = '변수';
+$string['configwarning'] = '함부로 이 설정값을 변경하지 마십시오 - 잘못된 값이 문제를 일으킬 수 있습니다.';
+$string['configzip'] = '당신의 zip 프로그램의 위치(유닉스만,선택사항)를 적으세요. 만약 지정되면, 이것은 서버상에서 zip 문서를 생성시킬 것이다. 만약 당신이 이 빈칸을 비워둔다면, Moodle은 내부 루틴을 사용할 것이다.';
 $string['confirmation'] = '확정';
-$string['cronwarning'] = '<a href=\"cron.php\">cron.php 유지 원본</a> 최소 24시간 동안 작동 되지 않습니다.<br /><a href=\"../doc/?frame=install.html⊂=cron\">설치 문서화는</a> 당신이 어떻게 이것을 자동화 할 수 있는 지를 설명해 줍니다.';
+$string['cronwarning'] = '<a href=\"cron.php\">cron.php 관리 스크립트</a>가 최소 24시간 동안 작동 되지 않습니다.<br /><a href=\"../doc/?frame=install.html⊂=cron\">설치 문서</a>는 당신이 어떻게 이것을 자동화 할 수 있는 지를 설명해 줍니다.';
 $string['density'] = '밀도';
 $string['edithelpdocs'] = '도움 문서를 편집하다';
 $string['editstrings'] = '문자열을 편집하다';
-$string['filterall'] = '필터 문자열을 거르다.';
+$string['filterall'] = '문자열을 거르다.';
+$string['filtermatchoneperpage'] = '한 페이지에 한번만 필터가 적용됩니다';
+$string['filtermatchonepertext'] = '한 텍스트에 한번만 필터가 적용됩니다';
 $string['filteruploadedfiles'] = '업로드 파일을 거르다.';
 $string['helpadminseesall'] = '모든 목록의 이벤트를 관리하거나 단지 그 이벤트를 그것들 스스로 적용됩니까?';
 $string['helpcalendarsettings'] = '다양한 일정 목록과 날짜/시간과 관련된 Moodle의 양상을 구성합니다.';
-$string['helpforcetimezone'] = '당신은 모든 사용자들이 각자 그들의 타임존을 선택 하거나 모든 이들을 위해 타임존을 밀고 나아가게끔 허락 할 수 있습니다.';
+$string['helpforcetimezone'] = '당신은 모든 사용자들이 각자 그들의 타임존을 선택하게 허용하거나, 모든 사람들에게 지정한 시간대를 쓰도록 강요할 수  있습니다.';
 $string['helpsitemaintenance'] = '업그레이드 및 다른 작업';
-$string['helpstartofweek'] = '일람표에서 주는 어느 날에 시작됩니까?';
-$string['helpupcominglookahead'] = '달력에서 앞으로 얼마나 많은 불이행으로 인해 다가오는 사건들이 보여질 것입니까?';
-$string['helpupcomingmaxevents'] = '얼마나 많은 다가오는 이벤트가 default';
-$string['helpweekenddays'] = '주중 어떤 날이 \"주말\"로 취급되어져 다른 색깔로 표시되어집니까?';
-$string['importtimezones'] = '타임존의 목록의 업데이트가 완료되었습니다.';
-$string['importtimezonescount'] = 'SA->count어귀는 Sa->source로 부터 가져왔습니다.';
+$string['helpstartofweek'] = '달력에서 한 주는 무슨 요일에 시작됩니까?';
+$string['helpupcominglookahead'] = '달력에서 다가오는 이벤트를 며칠 전에 보여지게 할까요?';
+$string['helpupcomingmaxevents'] = '얼마나 많은(최대) 이벤트를 보여지게 할까요?';
+$string['helpweekenddays'] = '주중 어느 날이 \"주말\"로 취급되어져 다른 색깔로 표시되어집니까?';
+$string['importtimezones'] = '타임존 목록의 업데이트가 완료되었습니다.';
+$string['importtimezonescount'] = '$a->source로 부터 $a->count 개를 가져왔습니다.';
 $string['importtimezonesfailed'] = '소스를 찾을 수 없습니다.(안좋은 소식)';
 $string['incompatibleblocks'] = '호환되지 않는 구역';
 $string['latexpreamble'] = '라텍스 전문';
@@ -146,22 +157,22 @@ $string['optionalmaintenancemessage'] = '임의의 정보 유지';
 $string['pathconvert'] = '전환 실행파일 경로';
 $string['pathdvips'] = 'divps 실행파일 경로';
 $string['pathlatex'] = '라텍스 실행파일 경로';
-$string['pleaseregister'] = '이 버튼을 제거하고 당신의 사이트를 등록해주세요.';
-$string['sitemaintenance'] = '이 싸이트는 중이며 현재 사용하실 수 없습니다.';
-$string['sitemaintenancemode'] = '모드 유지';
-$string['sitemaintenanceoff'] = '관리모드는 사용할 수 없고, 이 사이트는 다시 규칙적으로 작동하고 있습니다.';
-$string['sitemaintenanceon'] = '이 사이트는 현재 관리모드입니다(오직 관리자만이 이 사이트를 사용하거나 로그인 할 수 있습니다).';
-$string['sitemaintenancewarning'] = '당신은 현재 관리 모드에 있습니다(오직 관리자만 로그인 할 수 있습니다.) 일반 작업으로 되돌리시려면 <a href=\"maintenance.php\">는 </a>관리모드를 사용을 할 수 없게 합니다.';
+$string['pleaseregister'] = '이 버튼을 제거하고 싶으면 당신의 사이트를 등록해주세요.';
+$string['sitemaintenance'] = '이 싸이트는 점검 중이며 현재 사용하실 수 없습니다.';
+$string['sitemaintenancemode'] = '점검 상태';
+$string['sitemaintenanceoff'] = '점검이 끝나고, 다시 일반적인 작동 상태가 되었습니다.';
+$string['sitemaintenanceon'] = '이 사이트는 현재 점검중입니다(관리자만 이 사이트를 사용하거나 로그인 할 수 있습니다).';
+$string['sitemaintenancewarning'] = '현재 유지 점검 상태에 있습니다(관리자만 로그인 할 수 있습니다.) 일반 작업으로 되돌리시려면 <a href=\"maintenance.php\">정상 작동</a>모드를 사용하십사오.';
 $string['tabselectedtofront'] = 'tab이 있는 테이블은 줄은 현재 선택되어진 태그와 제일 앞에 위치 해 있습니다.';
-$string['therewereerrors'] = '데이터에 에러가 있었습니다.';
-$string['timezoneforced'] = '이것은 사이트 관리자에 의해 관리되고 있습니다.';
+$string['therewereerrors'] = '데이터에 오류가 있었습니다.';
+$string['timezoneforced'] = '사이트 관리자에 의해 지정되어 있습니다.';
 $string['timezoneisforcedto'] = '모든사용자를 사용하게끔 합니다.';
 $string['timezonenotforced'] = '사용자는 그들의 표준 시간대를 사용할 수 있습니다.';
 $string['upgradeforumread'] = '읽거나 읽지 않은 포럼의 게시물을 찾기 위해 Moodle 1.5에 새로운 게시물이 추가 되었습니다. <br />당신이 필요로 하는 이 기능을 사용하기 위해<a href=\"$a\">당신의 목록을 업데이트 하세요</a>.';
 $string['upgradeforumreadinfo'] = '읽거나 읽지 않은 포럼의 게시물을 찾기 위해 Moodle 1.5에 새로운 게시물이 추가 되었습니다. 당신이 필요로 하는 이 기능을 사용하기 위해 트래킹 정보를 이용하여 현존하고 있는 게시물에 있는 당신의 목록을 업데이트 하세요.';
-$string['upgradelogs'] = '활발한 기능을 위해서는 당신의 오래된 로그(경과기록)은 업그레이드가 되어야 합니다. <a href=\"$a\">More information</a>';
-$string['upgradelogsinfo'] = '최근에 바뀐 로그 기록이 저장되었습니다. 각 활동에 기초를 둔 당신의 오래된 로그를 볼 수 있습니다. 또한 당신의 오래된 고르들은 업그레이드를 필요로 합니다. 당신의 사이트에 따라 시간이 길어질 수 있습니다.(몇시간이 걸릴 수도 있습니다.) 또한 당신의 사이트 크기에 따라 약간의 세금이 붙을수 있습니다. 한번 당신이 이것을 진행시키면 꼭 끝마쳐야 합니다.(윈도우 브라우저 창을 유지시키는 것 만으로 할 수 있습니다.) 걱정마십시오 - 당신의 사이트는 당신이 로그를 업그레이드 하는 동안 다른사람이 이용하는데 불편없이 잘 운영이 될 것입니다.  ';
+$string['upgradelogs'] = '원활한 작동을 위해서 당신의 오래된 로그(경과기록)은 업그레이드가 되어야 합니다. <a href=\"$a\">추가 정보</a>';
+$string['upgradelogsinfo'] = '최근에 바뀐 로그 기록이 저장되었습니다. 이전의 활동 기록을 보려면, 오래된 기록들은 갱신되어야 합니다. 사이트에 따라 시간이 길어질 수 있으며(몇시간이 걸릴 수도 있습니다.) 데이터베이스의 규모에 따라 좀 더 시간이 걸릴 수도 있습니다. 일단 이것을 진행시키면 꼭 끝을 보아야 합니다.(윈도우 브라우저 창을 유지시키는 것 만으로 할 수 있습니다.) 걱정마십시오 - 당신의 사이트는 로그를 업그레이드 하는 동안 다른사람이 이용하는데 불편없이 잘 운영이 될 것입니다.<br /><br />이제부터 경과기록(로그)를 갱신할까요?  ';
 $string['upgradesure'] = '당신의 Moodle 파일이 변경되었습니다. 그리고 당신의 서버를 이 버전으로 자동 업그레이드할 준비가 되었습니다. 한번 이것을 시작하면 뒤로 돌아갈 수 없습니다. 확실히 이 서버를 이 버전으로 업그레이드 하시겠습니까?';
-$string['upgradingdata'] = '업그레이드 데이터';
-$string['upgradinglogs'] = '업그레이드 로그';
+$string['upgradingdata'] = '데이터 갱신';
+$string['upgradinglogs'] = '경과기록(로그) 갱신';
 ?>
---
+++ lang/ko/docs/files.php
@@ -0,0 +1,27 @@
+<?PHP // $Id$
+
+
+
+$string['-about'] = '무들에 대하여';
+$string['intro.html'] = '소개';
+$string['background.html'] = '제작 배경';
+$string['philosophy.html'] = '개발 철학';
+$string['licence.html'] = '저작권';
+$string['features.html'] = '기능';
+$string['release.html'] = 'Release Notes';
+$string['future.html'] = '향후 계획';
+$string['credits.html'] = 'Credits';
+$string['-installation'] = '관리';
+$string['install.html'] = '설치';
+$string['faq.html'] = '설치 FAQ';
+$string['installamp.html'] = 'Apache, MySQL, PHP';
+$string['upgrade.html'] = '판올림';
+$string['-usage'] = '무들의 사용';
+$string['teacher.html'] = '교사 지침서';
+$string['other.html'] = '기타 문서';
+$string['-development'] = '개발';
+$string['developer.html'] = '개발자 지침서';
+$string['coding.html'] = 'Coding Guide';
+$string['cvs.html'] = 'CVS의 이용';
+$string['translation.html'] = '번역';
+?>
