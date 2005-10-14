1347b1c10b8bc5569145b68ff0d82e410c3a341a
bobopinna <bobopinna>
2005-10-14 07:02:51 +0000

    Updated css for playscorm.php

--- mod/scorm/styles.php
+++ mod/scorm/styles.php
@@ -13,8 +13,41 @@
 .mod-scorm .scoframe {
 
 }
-.mod-scorm .fullscreen {
+.mod-scorm #scormpage {
+  position: relative;
   width: 100%;
+}
+.mod-scorm #tocbox {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 30% 
+}
+.mod-scorm #tochead {
+  text-align: center;
+  font-weight: bold;
+}
+.mod-scorm #scormbox {
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  width: 70%;
+  margin-left: 30%; 
+}
+.mod-scorm #scormtop {
+  position: relative;
+  width: 100%;
+  height: 30px;
+}
+.mod-scorm #scormbrowse {
+  position: absolute;
+  left: 5px;
+  top: 0px;
+}
+.mod-scorm #scormnav {
+  position: absolute;
+  right: 5px;
+  top: 0px;
 }
 .structlist  {
   list-style-type: none;
