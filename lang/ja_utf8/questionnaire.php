36a23a9e8f0e066ced8587ee5310f3311c81ef0a
mits <mits>
2005-10-15 21:56:47 +0000

    polished help files for questionnaire. translated new strings and a help file for questionnaire.

--- lang/ja_utf8/questionnaire.php
+++ lang/ja_utf8/questionnaire.php
@@ -1,16 +1,32 @@
-<?php // $Id$ 
-      // questionnaire.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+<?PHP // $Id$ 
+      // questionnaire.php - created with Moodle 1.6 development (2005101200)
 
 
 $string['alreadyfilled'] = 'あなたは既にこのアンケートに回答しています。ありがとうございます。';
+$string['closed'] = 'アンケートは $a に終了しました。ありがとうございます。';
+$string['closedate'] = '終了日時';
 $string['modulename'] = 'アンケート';
 $string['modulenameplural'] = 'アンケート';
 $string['mustcomplete'] = '<b>アンケート結果を保存するためには<i>今</i>このアンケートを完了させる必要があります。別の機会に回答することはできません。</b><br /><br />';
 $string['notavail'] = 'この質問は利用できません。後でお試しください。';
+$string['opendate'] = '開始日時';
 $string['qmanage'] = '調査の管理';
 $string['qmanagetitle'] = 'Survey Management phpESP';
-$string['qtype'] = 'タイプ';
+$string['qtype'] = '調査回答タイプ';
+$string['qtypeonce'] = '回答は１回限り';
+$string['qtypeunlimited'] = '何回でも回答可';
 $string['questionnaireid'] = '調査';
+$string['questiontypes'] = '質問タイプ';
+$string['realm'] = 'アンケートタイプ';
+$string['respondenteligible'] = 'アンケートの対象';
+$string['respondenteligibleall'] = '全て';
+$string['respondenteligiblestudents'] = '学生のみ';
+$string['respondenteligibleteachers'] = '教師のみ';
 $string['respondenttype'] = '記名の有無';
+$string['respondenttypeanonymous'] = '匿名';
+$string['respondenttypefullname'] = '記名';
+$string['resume'] = '回答の保存/再開';
+$string['template'] = 'テンプレート';
+$string['type'] = 'タイプ';
 
 ?>
