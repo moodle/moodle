d92a99581100bf6389e128e5983cfa2fb6fa7da2
gustav_delius <gustav_delius>
2005-10-17 07:59:47 +0000

    Fixing misprint leading to fatal error

--- mod/label/index.php
+++ mod/label/index.php
@@ -3,7 +3,7 @@
     require_once("../../config.php");
     require_once("lib.php");
 
-    $id = require_param('id',PARAM_INT);   // course
+    $id = required_param('id',PARAM_INT);   // course
 
     redirect("$CFG->wwwroot/course/view.php?id=$id");
 
