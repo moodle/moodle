9293552d223a9873b3d0f2c708ec8538ee7b891f
carlesbellver <carlesbellver>
2005-10-19 06:25:56 +0000

    *** empty log message ***

---
+++ lang/ca/block_quiz_results.php
@@ -0,0 +1,28 @@
+<?PHP // $Id$ 
+      // block_quiz_results.php - created with Moodle 1.5.2 + (2005060223)
+
+
+$string['bestgrade'] = 'La qualificació més alta:';
+$string['bestgrades'] = 'Les $a qualificacions més altes:';
+$string['bestgroupgrade'] = 'El grup amb la mitjana més alta:';
+$string['bestgroupgrades'] = 'Els $a grups amb la mitjana més alta:';
+$string['config_format_absolute'] = 'Nombres absoluts';
+$string['config_format_fraction'] = 'Fraccions';
+$string['config_format_percentage'] = 'Percentatges';
+$string['config_grade_format'] = 'Mostra les qualificacions com a:';
+$string['config_no_quizzes_in_course'] = 'Aquest curs no conté cap qüestionari. N\'heu d\'afegir almenys un per poder usar aquest bloc correctament.';
+$string['config_select_quiz'] = 'De quin qüestionari voleu mostrar els resultats en aquest bloc?';
+$string['config_show_best'] = 'Quantes de les qualificacions més altes cal mostrar (0 per inhabilitar)?';
+$string['config_show_worst'] = 'Quantes de les qualificacions més baixes cal mostrar (0 per inhabilitar)?';
+$string['config_use_groups'] = 'Voleu mostrar grups en lloc d\'estudiants? (Només si el qüestionari funciona amb grups.)';
+$string['configuredtoshownothing'] = 'La configuració actual d\'aquest qüestionari no permet mostrar cap resultat. Podeu modificar la configuració o ocultar-lo.';
+$string['error_emptyquizid'] = 'Hi ha un error en aquest bloc: heu de seleccionari de quin qüestionari voleu mostrar els resultats.';
+$string['error_emptyquizrecord'] = 'Hi ha un error en aquest bloc: el qüestionari seleccionat no sembla que existeixi en la base de dades.';
+$string['error_nogroupsexist'] = 'Hi ha un error en aquest bloc: està configurat per mostrar les qualificacions en mode de grups, però el curs no té grups definits.';
+$string['formaltitle'] = 'Resultats del qüestionari';
+$string['worstgrade'] = 'La qualificació més baixa:';
+$string['worstgrades'] = 'Les $a qualificacions més baixes:';
+$string['worstgroupgrade'] = 'El grup amb la mitjana més baixa';
+$string['worstgroupgrades'] = 'Els $a grups amb la mitjana més baixa:';
+
+?>
