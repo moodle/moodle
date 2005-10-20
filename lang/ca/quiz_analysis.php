d18e1eb6a54de63de8ea645ccc56b8e7154514c5
carlesbellver <carlesbellver>
2005-10-20 06:05:45 +0000

    *** empty log message ***

--- lang/ca/quiz_analysis.php
+++ lang/ca/quiz_analysis.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // quiz_analysis.php - created with Moodle 1.5.2 + (2005060222)
+      // quiz_analysis.php - created with Moodle 1.5.2 + (2005060223)
 
 
 $string['analysisoptions'] = 'Opcions d\'anàlisi';
@@ -9,16 +9,22 @@ $string['attemptselection'] = 'Intents a analitzar per usuari';
 $string['attemptsfirst'] = 'primer intent';
 $string['attemptshighest'] = 'qualificació més alta';
 $string['attemptslast'] = 'darrer intent';
-$string['downloadooo'] = 'Baixa en format OpenOffice.org';
+$string['dicsindextitle'] = 'Índex<br />disc.';
+$string['disccoefftitle'] = 'Coef.<br />disc.';
+$string['downloadooo'] = 'Baixa en format OpenOffice';
 $string['facilitytitle'] = '%%Correctes<br />Dificultat';
 $string['lowmarkslimit'] = 'No analitzis si la puntuació és menor que:';
 $string['pagesize'] = 'Preguntes per pàgina:';
+$string['qcounttitle'] = 'Núm R.';
 $string['qidtitle'] = 'P.';
 $string['qnametitle'] = 'Títol de la pregunta';
 $string['qtexttitle'] = 'Text de la pregunta';
 $string['qtypetitle'] = 'Tipus';
+$string['quizreportdir'] = 'quiz_reports';
+$string['rcounttitle'] = 'Núm.<br />R.';
 $string['reportanalysis'] = 'Informe de l\'anàlisi d\'elements';
 $string['responsestitle'] = 'Text de la resposta';
+$string['rfractiontitle'] = 'crèdit<br />parcial';
 $string['rpercenttitle'] = 'R.%%';
 $string['stddevtitle'] = 'DT';
 
