1ad8ec21d922fcd7933acfa39558cdea00ba0328
mits <mits>
2005-10-23 21:29:41 +0000

    translated all strings for scheduler.

--- lang/ja/scheduler.php
+++ lang/ja/scheduler.php
@@ -3,28 +3,45 @@
 
 
 $string['action'] = '操作';
-$string['addsession'] = '時間枠の追加';
-$string['addslot'] = 'いつでも面会の時間枠を追加することができます。';
-$string['cantfit'] = '$a 分の時間枠をこのセッションに割り当てることはできません。';
+$string['addsession'] = '面会予約時間枠の追加';
+$string['addslot'] = 'いつでも面会予約時間枠を追加することができます。';
+$string['cantfit'] = '$a 分の面会予約時間枠をこのセッションに割り当てることはできません。';
 $string['choice'] = '選択';
-$string['conflict'] = 'この期間には既に時間枠が登録されています。';
+$string['composeemail'] = 'メールの作成:';
+$string['conflict'] = 'この期間には既に面会予約時間枠が登録されています。';
 $string['date'] = '日付';
-$string['divide'] = '複数の時間枠に分割しますか?';
+$string['divide'] = '複数の面会予約時間枠に分割しますか?';
 $string['duration'] = '面会時間';
-$string['end'] = '終了';
+$string['end'] = '終了時間';
 $string['endtime'] = '終了時間';
-$string['markseen'] = '学生との面会後は上記テーブルの該当するチェックボックスをチェックして、「面会済み」にマークしてください。';
+$string['invitation'] = '招待';
+$string['invitationtext'] = '面会予約時間枠を選択してください:';
+$string['markseen'] = '面会後は上記テーブルの該当するチェックボックスをチェックして、「面会済み」にマークしてください。';
 $string['minutes'] = '分';
-$string['minutesperslot'] = '分/時間枠';
+$string['minutesperslot'] = '分/面会予約時間枠';
+$string['missingstudents'] = 'まだ $a 人の学生が面会予約する必要があります。';
 $string['modulename'] = 'スケジューラー';
 $string['modulenameplural'] = 'スケジューラー';
+$string['moreslots'] = 'あなたは面会予約時間枠より多い学生を持っています。面会予約時間枠を更に追加した方が良いでしょう。';
 $string['move'] = '変更';
-$string['moveslot'] = '時間枠の移動';
+$string['moveslot'] = '面会予約時間枠の移動';
+$string['noslots'] = '利用できる面会予約時間枠がありません。面会をアレンジするには、メールで教官に連絡を取ってください。';
+$string['notselected'] = 'まだ選択していません。';
 $string['reminder'] = 'リマインダー';
+$string['remindertext'] = 'これは、あなたがまだ面会予約を設定していないことに関する遅延通知です。できるだけ早く面会時間を設定してください:';
 $string['save'] = '保存';
+$string['savechoice'] = '私の面会予約を保存';
+$string['schedule'] = 'スケジュール';
+$string['scheduleappointment'] = '$a の面会スケジュール';
 $string['seen'] = '面会済み';
-$string['slots'] = '時間枠';
-$string['start'] = '開始';
+$string['slot_is_just_in_use'] = '申し訳ございません。面会予約は他の学生によって選択されています。<br />もう一度お試しください。';
+$string['slots'] = '面会予約時間枠';
+$string['slotsadded'] = '$a の面会予約時間枠が追加されました。';
+$string['start'] = '開始時間';
+$string['startpast'] = '過去に面会予約時間枠を開始することはできません。';
 $string['starttime'] = '開始時間';
+$string['welcomebackstudent'] = '下記のテーブル内の太字が、あなたの面会予約時間を示します。現在の面会予約時間を利用可能な面会時間に変更することができます。';
+$string['welcomenewstudent'] = '下記のテーブルには、全ての利用可能な面会予約時間を表示しています。ラジオボタンを選択して、忘れずに「私の面会予約を保存」をクリックしてください。後で内容を変更したい場合は、このページを再度訪問してください。';
+$string['welcomenewteacher'] = '全ての学生と面会するための面会予約時間枠を追加するには、下記のボタンをクリックしてください。';
 
 ?>
