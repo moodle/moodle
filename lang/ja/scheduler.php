e2b2d6df48dad3d778b5b58fec6bb86dfb742b7a
mits <mits>
2005-10-23 19:01:36 +0000

    translated strings for scheduler.

---
+++ lang/ja/scheduler.php
@@ -0,0 +1,30 @@
+<?PHP // $Id$ 
+      // scheduler.php - created with Moodle 1.6 development (2005101200)
+
+
+$string['action'] = '操作';
+$string['addsession'] = '時間枠の追加';
+$string['addslot'] = 'いつでも面会の時間枠を追加することができます。';
+$string['cantfit'] = '$a 分の時間枠をこのセッションに割り当てることはできません。';
+$string['choice'] = '選択';
+$string['conflict'] = 'この期間には既に時間枠が登録されています。';
+$string['date'] = '日付';
+$string['divide'] = '複数の時間枠に分割しますか?';
+$string['duration'] = '面会時間';
+$string['end'] = '終了';
+$string['endtime'] = '終了時間';
+$string['markseen'] = '学生との面会後は上記テーブルの該当するチェックボックスをチェックして、「面会済み」にマークしてください。';
+$string['minutes'] = '分';
+$string['minutesperslot'] = '分/時間枠';
+$string['modulename'] = 'スケジューラー';
+$string['modulenameplural'] = 'スケジューラー';
+$string['move'] = '変更';
+$string['moveslot'] = '時間枠の移動';
+$string['reminder'] = 'リマインダー';
+$string['save'] = '保存';
+$string['seen'] = '面会済み';
+$string['slots'] = '時間枠';
+$string['start'] = '開始';
+$string['starttime'] = '開始時間';
+
+?>
