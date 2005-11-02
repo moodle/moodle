c8487fb6df344d2d301b34a43194b322c0ec0cf4
martignoni <martignoni>
2005-11-02 13:05:05 +0000

    My moodle page should not be available to guests and offer a full login

--- lang/fr/my.php
+++ lang/fr/my.php
@@ -2,6 +2,7 @@
 
 $string['mymoodle'] = 'Mon moodle';
 $string['nocourses'] = 'Aucune information de cours à afficher.';
+$string['noguest'] = 'La page «&nbsp;Mon Moodle&nbsp;» n\'est pas disponible pour les invités';
 $string['pinblocks'] = 'Configurer les blocs fixes de la page «&nbsp;Mon moodle&nbsp;»';
 $string['pinblocksexplan'] = 'Les blocs que vous configurez ici seront visibles sur la page «&nbsp;Mon moodle&nbsp;» de chaque utilisateur. Ils ne pourront être modifiés par les utilisateurs.';
 
