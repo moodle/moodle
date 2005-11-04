4483596d794e1802bb842c937912a795d1351b1b
skodak <skodak>
2005-11-04 09:55:46 +0000

    fixed encoding of page title of framed resources

--- mod/resource/type/file/resource.class.php
+++ mod/resource/type/file/resource.class.php
@@ -346,10 +346,15 @@ function display() {
     /// Now check whether we need to display a frameset
 
     if (empty($_GET['frameset']) and !$embedded and !$inpopup and $resource->options == "frame") {
+        if (!empty($CFG->unicode)) {
+            $encoding = 'utf-8';
+        } else {
+            $encoding = get_string('thischarset');
+        }
         echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Frameset//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\">\n";
         echo "<html dir=\"ltr\">\n";
         echo '<head>';
-        echo '<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />';
+        echo '<meta http-equiv="content-type" content="text/html; charset='.$encoding.'" />';
         echo "<title>{$course->shortname}: ".strip_tags(format_string($resource->name,true))."</title></head>\n";
         echo "<frameset rows=\"$CFG->resource_framesize,*\">";
         echo "<frame src=\"view.php?id={$cm->id}&amp;type={$resource->type}&amp;frameset=top\" />";
