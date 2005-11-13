bacd847b35eecc7143e388da276c6a34774d8b01
mjollnir_ <mjollnir_>
2005-11-13 22:58:26 +0000

    Fixing last commit - mysql upgrade - table_column defaults to 0 for default rather than empty string and mysql doesn't want to allow 0 for defaults for type text columns

--- blocks/rss_client/db/mysql.php
+++ blocks/rss_client/db/mysql.php
@@ -12,8 +12,8 @@ function rss_client_upgrade($oldversion) {
 
     if ($oldversion < 2005111400) {
         // title and description should be TEXT as we don't have control over their length.
-        table_column('block_rss_client','title','title','text');
-        table_column('block_rss_client','description','description','text');
+        table_column('block_rss_client','title','title','text',10,'unsigned','');
+        table_column('block_rss_client','description','description','text',10,'unsigned','');
     }
 
     return true;
