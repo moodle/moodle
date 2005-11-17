cb776114a20b1b10f64551d311e220b7b711e1ba
moodler <moodler>
2005-11-17 06:46:20 +0000

    Tidying up (SE)

--- lang/en/lams.php
+++ lang/en/lams.php
@@ -1,33 +1,28 @@
 <?php 
 
+$string['createSequence'] = 'Create new sequence';
+$string['editSequence'] = 'Edit selected sequence';
+$string['error'] = 'Sorry, an unknown error has occured.';
+$string['introduction']  = 'Introduction';
+$string['lams'] = '-- LAMS - Learning Activity Management System --';
+$string['lamsoutline'] = 'LAMS Outline';
+$string['lesson']="learning session";
 $string['modulename'] = 'LAMS';
 $string['modulenameplural'] = 'LAMS';
-
-$string['serverurl'] = 'Enter the basic URL used to access the LAMS server. For example http://localhost:8080/lams';
-$string['serverid'] = 'Enter the server ID received from <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
-$string['serverkey'] = 'Enter the server key received from <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
-
-$string['selectExistingSequence'] = 'Select an existing sequence or create a new sequence.';
-$string['workspace'] = 'Select Workspace';
-$string['sequence'] = 'Select Sequence';
-$string['editSequence'] = 'Edit selected sequence';
-$string['createSequence'] = 'Create new sequence';
-$string['useSequence']  = 'Use selected sequence';
-$string['refreshSequenceList']  = 'Refresh sequence list';
-$string['introduction']  = 'Introduction';
-$string['visibletostudents'] = 'Show activity to students';
-
-$string['lesson']="learning session";
-
+$string['notsetup'] = 'NOT_SET_UP';
+$string['openauthor'] = 'Open LAMS Author';
 $string['openlearner'] = 'Open LAMS Learner';
 $string['openmonitor'] = 'Open LAMS Monitor';
-$string['openauthor'] = 'Open LAMS Author';
-
-$string['lams'] = '-- LAMS - Learning Activity Management System --';
-$string['error'] = 'Sorry, an unknown error has occured.';
+$string['refreshSequenceList']  = 'Refresh sequence list';
+$string['selectExistingSequence'] = 'Select an existing sequence or create a new sequence.';
+$string['sequence'] = 'Select Sequence';
+$string['serverid'] = 'Enter the server ID received from <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverkey'] = 'Enter the server key received from <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverurl'] = 'Enter the basic URL used to access the LAMS server. For example http://localhost:8080/lams';
+$string['useSequence']  = 'Use selected sequence';
+$string['visibletostudents'] = 'Show activity to students';
 $string['wikistartederror'] = 'Wiki already has entries - can\'t change.';
+$string['workspace'] = 'Select Workspace';
 $string['wrongversionrange'] = '$a is not a correct range!';
-
-$string['notsetup'] = 'NOT_SET_UP';
 
 ?>
