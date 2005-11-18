07d9001c08161faa2f0953530b89dbb2ca5c7a03
skodak <skodak>
2005-11-18 10:54:45 +0000

    xxx_param() fix

--- course/info.php
+++ course/info.php
@@ -5,8 +5,8 @@
     require_once("../config.php");
     require_once("lib.php");
 
-    require_variable($id);    // Course id
-    optional_variable($name);
+    $id   = optional_param('id', false, PARAM_INT); // Course id
+    $name = optional_param('name', false, PARAM_RAW); // Course short name
 
     if (!$id and !$name) {
         error("Must specify course id or short name");
