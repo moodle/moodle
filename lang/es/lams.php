f3aa72b411b46c5eea14a37afbbfbb51cc310ee0
barias <barias>
2005-11-18 11:02:18 +0000

    Latest spanish translation

---
+++ lang/es/lams.php
@@ -0,0 +1,29 @@
+<?PHP // $Id$ 
+      // lams.php - created with Moodle 1.5.2 (2005060220)
+
+
+$string['createSequence'] = 'Crear nueva secuencia';
+$string['editSequence'] = 'Editar secuencia seleccionada';
+$string['error'] = 'Lo sentimos, ha ocurrido un error desconocido.';
+$string['introduction'] = 'Introducción';
+$string['lams'] = '-- LAMS - Learning Activity Management System --';
+$string['lesson'] = 'sesión de aprendizaje';
+$string['modulename'] = 'LAMS';
+$string['modulenameplural'] = 'LAMS';
+$string['notsetup'] = 'NOT_SET_UP';
+$string['openauthor'] = 'Abrir Autor LAMS';
+$string['openlearner'] = 'Abrir Alumno LAMS';
+$string['openmonitor'] = 'Abrir Monitor LAMS';
+$string['refreshSequenceList'] = 'Refrescar lista de secuencias';
+$string['selectExistingSequence'] = 'Seleccionar una secuencia existente o crear una nueva secuencia.';
+$string['sequence'] = 'Seleccionar secuencia';
+$string['serverid'] = 'Escriba la ID del servidor recibida de <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverkey'] = 'Escriba la clave de servidor recibida de <a href=http://www.lamsinternational.com target=_blank>LAMS international</a>.';
+$string['serverurl'] = 'Escriba la URL básica usada para acceder al servidor LAMS. Por ejemplo, http://localhost:8080/lams';
+$string['useSequence'] = 'Usar la secuencia seleccionada';
+$string['visibletostudents'] = 'Mostrar la actividad a los estudiantes';
+$string['wikistartederror'] = 'Wiki ya tiene entradas: no puede cambiarse.';
+$string['workspace'] = 'Seleccionar Espacio de trabajo';
+$string['wrongversionrange'] = '¡$a no es un rango correcto!';
+
+?>
