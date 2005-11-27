3944e6cb7de94e7601651ab5eb7ef98867693aa2
gniske <gniske>
2005-11-27 19:35:21 +0000

    Created new language file

---
+++ lang/da/block_loancalc.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // block_loancalc.php - created with Moodle 1.6 development (2005101200)
+
+
+$string['amountofloan'] = 'Størrelsen på lånet';
+$string['fortnightly'] = 'Hver 14. dag';
+$string['interestrate'] = 'Pålydende rente';
+$string['loancalc'] = 'Beregning af lån';
+$string['loanterm'] = 'Låneperiode i år';
+$string['monthly'] = 'Månedligt';
+$string['repaymentamount'] = 'Afdrags størrelse';
+$string['repaymentfreq'] = 'Afdragsfrekvens';
+$string['weekly'] = 'Ugentligt';
+
+?>
---
+++ lang/da/block_search_forums.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // block_search_forums.php - created with Moodle 1.6 development (2005101200)
+
+
+$string['advancedsearch'] = 'Advanceret søgning';
+$string['blocktitle'] = 'Søg forums';
+
+?>
