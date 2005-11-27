85e74d9f13e8476ffbe17b889011504e89f53b00
gniske <gniske>
2005-11-27 10:24:53 +0000

    Added missing strings

--- lang/da/assignment.php
+++ lang/da/assignment.php
@@ -1,5 +1,5 @@
 <?PHP // $Id$ 
-      // assignment.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005033100)
+      // assignment.php - created with Moodle 1.6 development (2005101200)
 
 
 $string['allowresubmit'] = 'Tillad gentagelser';
@@ -13,21 +13,31 @@ $string['assignmentmailhtml'] = '$a->teacher har kikket det du har afleveret ige
 Du kan se evt. kommentarer i slutningen på <a href=\"$a->url\">det du har afleveret</a>.';
 $string['assignmentname'] = 'Opgave navn';
 $string['assignmenttype'] = 'Opgave type';
+$string['availabledate'] = 'Tilgængelig fra';
+$string['comment'] = 'Kommentar';
+$string['commentinline'] = 'Indlejret kommentar';
 $string['configmaxbytes'] = 'General maksimum filstørrelse for alle afleveringsopgaver på sitet (Underlagt kursus begrænsninger og andre lokale indstillinger)';
 $string['description'] = 'Beskrivelse';
 $string['duedate'] = 'Afleveringsdato';
 $string['duedateno'] = 'Ingen afleveringsfrist';
 $string['early'] = '$a for tidlig';
+$string['editmysubmission'] = 'Ret min aflevering';
 $string['emailteachermail'] = '$a->username har opdateret deres opgaveafleveringer for \'$a->assignment\'
 
 Det kan hentes her:
 $a->url';
 $string['emailteachermailhtml'] = '$a->username har opdateret deres opgaveaflevering for <i>\'$a->assignment\'</i><br /><br />Det kan hentes på <a href=\"$a->url\">websiden</a>.';
 $string['emailteachers'] = 'E-mail advarsel til lærere';
+$string['emptysubmission'] = 'Du har ikke afleveret noget endnu';
 $string['existingfiledeleted'] = 'Eksisterende fil er blevet slettet: $a';
 $string['failedupdatefeedback'] = 'Mislykkedes at opdatere feedback på aflevering for bruger $a';
 $string['feedback'] = 'Feedback';
+$string['feedbackfromteacher'] = 'Feedback fra $a';
 $string['feedbackupdated'] = 'Afleverings feedback opdateret for $a ';
+$string['guestnoupload'] = 'Desværre, gæster kan ikke uploade.';
+$string['helpoffline'] = '<p>Denne er brugbar når selve opgaven udføres udenfor Moodle. Det kunne være et andet sted på nettet, eller i klassen.</p><p>Eleverne kan se en beskrivelse af opgaven, men kan ikke uploade filer eller ligenene. Pointgivning fungere normalt, og elever vil få besked når deres indsats er blevet vurderet.</p>';
+$string['helponline'] = '<p>Denne opgave kræver at eleverne skriver noget tekst. Underviseren kan vurdere vurdere dem online og tilmed rette, eller kommentere i selve teksten.</p><p>(Hvis du kender tidligere versioner af Moodle, er denne opgavetype den sammen som den der førhen hed Journal)</p>';
+$string['helpuploadsingle'] = '<p>Denne type opgave tillader deltagere at uploade en enkelt fil af vilkårlig type.</p> <p>Det kan være et worddokument, et billede, et zippet website, eller hvad underviseren nu beder eleverne om at aflevere.</p>';
 $string['late'] = '$a for sent';
 $string['maximumgrade'] = 'Maksimum karakterer';
 $string['maximumsize'] = 'Maksimum størrelse';
@@ -35,15 +45,22 @@ $string['modulename'] = 'Opgave';
 $string['modulenameplural'] = 'Opgaver';
 $string['newsubmissions'] = 'Opgaver afleveret';
 $string['noassignments'] = 'Der er ingen opgaver endnu';
+$string['noattempts'] = 'Der er ikke nogen der afleveret noget til denne opgave endnu.';
 $string['notgradedyet'] = 'Ikke bedømt endnu';
 $string['notsubmittedyet'] = 'Ikke afleveret endnu';
 $string['overwritewarning'] = 'Advarsel: hvis du uploader igen, vil det erstatte den nuværende version';
+$string['pagesize'] = 'Vis antal afleveringer pr side';
+$string['preventlate'] = 'Modtag ikke opgaver afleveret for sent';
+$string['quickgrade'] = 'Hurtig vurdering tilladt';
 $string['saveallfeedback'] = 'Gem alt mit feedback';
+$string['submission'] = 'Aflevering';
 $string['submissionfeedback'] = 'Afleverings feedback';
 $string['submissions'] = 'Afleveringer';
+$string['submissionsaved'] = 'Ændringer er gemt';
 $string['submitassignment'] = 'Aflevér din opgave ved hjælp af denne formular';
 $string['submitted'] = 'Afleveret';
 $string['typeoffline'] = 'Offline aktivitet';
+$string['typeonline'] = 'Online tekst';
 $string['typeuploadsingle'] = 'Upload en enkelt fil';
 $string['uploadbadname'] = 'Dette filnavn indeholdt ugyldige tegn og kunne ikke uploades';
 $string['uploadedfiles'] = 'uploaded filer';
