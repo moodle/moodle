76ead0f2387cc31aa1e82969bcde71d4d274bfa2
ethem <ethem>
2005-11-29 09:52:17 +0000

    New strings from Ethem Evlice (ethem AT evlice DOT com) <XHTML /> compliant.

--- lang/tr/lams.php
+++ lang/tr/lams.php
@@ -2,24 +2,28 @@
       // lams.php - created with Moodle 1.6 development (2005101200)
 
 
-$string['createSequence'] = 'Yeni bir dizi oluþtur';
-$string['editSequence'] = 'Seçili diziyi düzenle';
+$string['createSequence'] = 'Yeni sahne oluþtur';
+$string['editSequence'] = 'Seçili sahneyi düzenle';
 $string['error'] = 'Üzgünüz, bilinmeyen bir hata oluþtu.';
 $string['introduction'] = 'Tanýtým';
-$string['lams'] = '-- LAMS - Learning Activity Management System --';
+$string['lams'] = '-- LAMS - Öðrenme Etkinliði Yönetim Sistemi --';
 $string['lamsoutline'] = 'LAMS Taslaðý';
 $string['lesson'] = 'öðrenme oturumu';
 $string['modulename'] = 'LAMS';
 $string['modulenameplural'] = 'LAMS';
 $string['notsetup'] = 'NOT_SET_UP';
-$string['refreshSequenceList'] = 'Dizi listesini yenile';
-$string['selectExistingSequence'] = 'Var olan bir diziyi seçin veya yeni bir dizi oluþturun.';
-$string['sequence'] = 'Dizi Seç';
-$string['serverid'] = '<a href=http://www.lamsinternational.com target=_blank>LAMS international</a>\'dan alýnan Server ID\'yi girin.';
-$string['serverkey'] = '<a href=http://www.lamsinternational.com target=_blank>LAMS international</a>\'dan alýnan Server Key\'i girin.';
+$string['openauthor'] = 'LAMS Author\'u Aç';
+$string['openlearner'] = 'LAMS Learner\'ý Aç';
+$string['openmonitor'] = 'LAMS Monitor\'u Aç';
+$string['refreshSequenceList'] = 'Sahne listesini yenile';
+$string['selectExistingSequence'] = 'Var olan bir sahneyi seçin veya yeni bir sahne oluþturun.';
+$string['sequence'] = 'Sahne Seç';
+$string['serverid'] = '<a href=http://www.lamsinternational.com target=_blank>LAMS international</a>\'dan alýnan Sunucu Kimliðini girin.';
+$string['serverkey'] = '<a href=http://www.lamsinternational.com target=_blank>LAMS international</a>\'dan alýnan Sunucu Anahtarýný girin.';
 $string['serverurl'] = 'LAMS sunucusuna eriþim için ana adresi girin. Örnek: http://localhost:8080/lams';
-$string['useSequence'] = 'Seçili diziyi göster';
+$string['useSequence'] = 'Seçili sahneyi kullan';
 $string['visibletostudents'] = 'Etkinliði öðrencilere göster';
+$string['wikistartederror'] = 'Zaten wiki kayýtlarý var - deðiþtirilemez.';
 $string['workspace'] = 'Çalýþma Alaný Seç';
 $string['wrongversionrange'] = '$a geçerli aralýkta deðil!';
 
