3b864d850eb2ae658ce98079c489aa19f6437eb1
ethem <ethem>
2005-12-02 11:45:36 +0000

    Postgres support and some indexes for speed.

--- enrol/authorize/db/mysql.php
+++ enrol/authorize/db/mysql.php
@@ -12,6 +12,16 @@ function authorize_upgrade($oldversion=0) {
         modify_database("$CFG->dirroot/enrol/authorize/db/mysql.sql");
     }
 
+    if ($oldversion < 2005071601) {
+        // Be sure, only last 4 digit is inserted.
+        table_column('enrol_authorize', 'cclastfour', 'cclastfour', 'integer', '4', 'unsigned', '0', 'not null');
+        table_column('enrol_authorize', 'courseid', 'courseid', 'integer', '10', 'unsigned', '0', 'not null');
+        table_column('enrol_authorize', 'userid', 'userid', 'integer', '10', 'unsigned', '0', 'not null');
+        // Add some indexes for speed.
+        execute_sql(" ALTER TABLE `{$CFG->prefix}enrol_authorize` ADD INDEX courseid(courseid) ");
+        execute_sql(" ALTER TABLE `{$CFG->prefix}enrol_authorize` ADD INDEX userid(userid) ");
+    }
+
     return $result;
 
 }
