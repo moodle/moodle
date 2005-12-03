2a3918625d6edf982395b9d5d1135d5cc0de02a5
ethem <ethem>
2005-12-03 11:13:11 +0000

    Authorize.net Setting section strings.

--- lang/en/enrol_authorize.php
+++ lang/en/enrol_authorize.php
@@ -1,7 +1,11 @@
 <?PHP // $Id$ 
-      // enrol_authorize.php - created with Moodle 1.6 development (2005081700)
+      // enrol_authorize.php - created with Moodle 1.6 development (2005101200)
 
 
+$string['adminauthorizeccapture'] = 'Order Review & Auto-Capture Settings';
+$string['adminauthorizeemail'] = 'Email Sending Settings';
+$string['adminauthorizesettings'] = 'Authorize.net Settings';
+$string['adminauthorizewide'] = 'Site-Wide Settings';
 $string['adminreview'] = 'Review order before capturing the credit card.';
 $string['anlogin'] = 'Authorize.net: Login name';
 $string['anpassword'] = 'Authorize.net: Password';
@@ -17,7 +21,7 @@ $string['ccvv'] = 'Card Verification';
 $string['ccvvhelp'] = 'Look at the back of card (last 3 digits)';
 $string['choosemethod'] = 'If you know enrolment key of the course, enter it; otherwise you need to paid for this course.';
 $string['chooseone'] = 'Fill one or both of the following two fields';
-$string['description'] = 'The Authorize.net module allows you to set up paid courses by merchants.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
+$string['description'] = 'The Authorize.net module allows you to set up paid courses by merchants.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.<br /><br /><b>Note:</b> If you enter an enrolment key in the course settings, then students will also have the option to enrol using a key. This is useful if you have a mixture of paying and non-paying students.';
 $string['enrolname'] = 'Authorize.net Credit Card Gateway';
 $string['httpsrequired'] = 'We are sorry to inform you that your request cannot be processed currently. This site\'s configuration couldn\'t be set up correcly.
 <br /><br />
@@ -28,7 +32,7 @@ You can set up <a href=\"$a->url\">loginhttps</a> option in Variables/Security s
 <br /><br />
 Turning this on will make Moodle use a secure https connection just for the login and payment pages.';
 $string['nameoncard'] = 'Name on Card';
-$string['reviewday'] = 'Capture the credit card automatically unless a teacher or administrator review the order within <b>$a</b> days. CRON MUST BE ENABLED.<br />(0 day = disable autocapture = teacher,admin review it manually. Transaction will be cancelled if you disable autocapture or unless you review it within 30 days.)';
+$string['reviewday'] = 'Capture the credit card automatically unless a teacher or administrator review the order within <b>$a</b> days. CRON MUST BE ENABLED.<br />(0 day means it will disable autocapture, also means teacher or admin review order manually. Transaction will be cancelled if you disable autocapture or unless you review it within 30 days.)';
 $string['reviewnotify'] = 'Your payment will be reviewed. Expect an email within a few days from your teacher.';
 $string['sendpaymentbutton'] = 'Send Payment';
 $string['zipcode'] = 'Zip Code';
