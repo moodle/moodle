50ddd5e1a111bffc75d4c5da0ccd5b42705221be
koenr <koenr>
2005-12-18 14:55:56 +0000

    New language pack: hr_utf8 by Jasmin Klindzic jasmin@ffzg.hr
    Dragan Dosen
    ddosen@ffzg.hr

---
+++ lang/hr_utf8/dialogue.php
@@ -0,0 +1,10 @@
+<?PHP // $Id$ 
+      // dialogue.php - created with Moodle 1.5.2 + (2005060220)
+
+
+$string['everybody'] = 'Svi';
+$string['modulename'] = 'Dijalog';
+$string['modulenameplural'] = 'Dijalozi';
+$string['newentry'] = 'Novi zapis';
+
+?>
---
+++ lang/hr_utf8/tex.php
@@ -0,0 +1,7 @@
+<?PHP // $Id$ 
+      // tex.php - created with Moodle 1.5.2 + (2005060220)
+
+
+$string['filtername'] = 'TeX notacija';
+
+?>
