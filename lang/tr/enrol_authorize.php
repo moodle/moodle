3a8766e6fd41ff74438cacccfb7f9ea418cac631
ethem <ethem>
2005-12-22 20:04:28 +0000

    Settlement and order status strings.

--- lang/tr/enrol_authorize.php
+++ lang/tr/enrol_authorize.php
@@ -12,6 +12,11 @@ $string['anpassword'] = 'Authorize.net: Þifre';
 $string['anreferer'] = 'Authorize.net hesabýnýzda URL referer ayarý yaptýysanýz buraya yazýnýz. Bu, web isteðinde \"Referer: URL\" baþlýðýný gönderir.';
 $string['antestmode'] = 'Ýþlemleri deneme modunda çalýþtýr (para çekilmez)';
 $string['antrankey'] = 'Authorize.net: Ýþlem Anahtarý (Transaction Key)';
+$string['authorizedpendingcapture'] = 'Onaylanmýþ / Çekilmeyi Bekliyor';
+$string['cancelled'] = 'Ýptal edilmiþ';
+$string['capture'] = 'Çek';
+$string['capturedpendingsettle'] = 'Çekilmiþ / Ödeme Bekleniyor';
+$string['capturedsettled'] = 'Çekilmiþ / Ödenmiþ';
 $string['ccexpire'] = 'Geçerlilik Tarihi';
 $string['ccexpired'] = 'Kredi kartýnýn süresi geçmiþ';
 $string['ccinvalid'] = 'Geçersiz kart numarasý';
@@ -21,8 +26,10 @@ $string['ccvv'] = 'Onay Kodu';
 $string['ccvvhelp'] = 'Kartýn arkasýna bakýnýz (son 3 rakam)';
 $string['choosemethod'] = 'Kursun kayýt anahtarýný biliyorsanýz giriniz. Diðer durumda bu kurs için ödeme yapmanýz gerekiyor.';
 $string['chooseone'] = 'Aþaðýdaki iki alandan birini veya ikisini doldurun';
+$string['cutofftime'] = 'Hesap Kesim Zamaný. Hesap kesimi en son ne zaman yapýlacak?';
 $string['description'] = 'Authorize.net modülü Kredi Kartý saðlayýcýlarýyla ücretli kurslar ayarlamanýza olanak verir. Bir kursun ücreti sýfýr ise öðrencilere ödeme yapmalarý için bir istekte bulunulmaz. Sitenin geneli için ayarlayabileceðiniz varsayýlan bir tutar vardýr ve her bir dersin ücretini tek tek de ayarlayabilirsiniz. Kurs ücreti ayarlanýrsa site genelindeki ücret yoksayýlýr..<br /><br /><b>Not:</b> Kurs ayarlarýnda kayýt anahtarýný girdiyseniz öðrenciler bu anahtara göre de kayýt olma seçeneðine sahip olabileceklerdir. Bu, öðrecilerden bazýlarýnýn ödeme yaparak bazýlarýnýn da kayýt anahtarýna göre kayýt olmasýný istiyorsanýz kullanýþlýdýr.';
 $string['enrolname'] = 'Authorize.net Kredi Kartý Saðlayýcýsý';
+$string['expired'] = 'Süresi dolmuþ';
 $string['httpsrequired'] = 'Üzgünüz, isteðinizi þu anda yerine getiremiyoruz. Bu sitenin ayarý doðru yapýlandýrýlmamýþ.
 <br /><br />
 Tarayýcýnýzýn alt tarafýnda sarý bir kilit görmüyorsanýz kredi kartý numaranýzý girmeyiniz. Bu, sizinle sunucu arasýnda gidip gelen verinin þifrelendiði anlamýna gelir. Böylece 2 bilgisayar arasýnda akan bilgi korunmuþ olur ve kredi kartý numaranýz internet üzerinden yakalanamaz.';
@@ -32,9 +39,19 @@ $string['logindesc'] = 'Bu seçenek AÇIK olmalý.
 <br /><br />
 Bu seçenek aktif ise sadece giriþ ve ödeme sayfalarý için güvenli baðlantý (https) kullanýlacaktýr.';
 $string['nameoncard'] = 'Kart Üzerindeki Ýsim';
+$string['orderid'] = 'Sipariþ ID';
+$string['paymentmanagement'] = 'Ödeme Yönetimi';
+$string['paymentpending'] = '$a->orderid numaralý ödemeniz bu kurs için onay bekliyor.';
+$string['refund'] = 'Geri Öde';
+$string['refunded'] = 'Geri ödenmiþ';
+$string['returns'] = 'Geri ödemeler';
 $string['reviewday'] = '<b>$a</b> gün içinde eðitimci veya yönetici sipariþi incelemezse kredi kartýndan otomatik olarak parayý çek. CRON ETKÝN OLMALI. <br /> (0 gün otomatik çekme aktif deðil anlamýna gelir ve ayný zamanda eðitimci veya yöneticinin sipariþi kendisi inceleyeceðini zorunlu tutar. Otomatik çekmeyi etinleþtirmezseniz veya 30 gün içinde sipariþi incelemezseniz iþlem iptal edilir.)';
 $string['reviewnotify'] = 'Ödemeniz incelenecek. Bir kaç gün içinde eðitimcinizden bir email bekleyin.';
 $string['sendpaymentbutton'] = 'Ödemeyi Yap';
+$string['settled'] = 'Ödenmiþ';
+$string['settlementdate'] = 'Hesap Kesim Tarihi';
+$string['transid'] = 'Ýþlem ID';
+$string['void'] = 'Ýptal et';
 $string['zipcode'] = 'Posta Kodu';
 
 ?>
