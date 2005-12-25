447ac17f163c355c9cd7d6a14fb7555aebb9a93d
dlnsk <dlnsk>
2005-12-25 18:39:48 +0000

    *** empty log message ***

---
+++ lang/ru/block_search_forums.php
@@ -0,0 +1,8 @@
+<?PHP // $Id$ 
+      // block_search_forums.php - created with Moodle 1.5.2 + (2005060224)
+
+
+$string['advancedsearch'] = 'Расширенный поиск';
+$string['blocktitle'] = 'Поиск по форумам';
+
+?>
