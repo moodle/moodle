2fcf2e1110b6e495e4ef68fa0aeed33b78f94d7f
scop22 <scop22>
2005-12-25 16:09:42 +0000

    Committing newly tranlsated files to Korean UTF8 language pack; also two updated files included

--- lang/ko_utf8/docs/files.php
+++ lang/ko_utf8/docs/files.php
@@ -8,14 +8,14 @@ $string['background.html'] = "제작 배경";
 $string['philosophy.html'] = "개발 철학";
 $string['licence.html'] = "저작권";
 $string['features.html'] = "기능";
-$string['release.html'] = "Release Notes";
+$string['release.html'] = "배포 이력";
 $string['future.html'] = "향후 계획";
 $string['credits.html'] = "Credits";
 
 $string['-installation'] = "관리";
 $string['install.html'] = "설치";
 $string['faq.html'] = "설치 FAQ";
-$string['installamp.html'] = "Apache, MySQL, PHP";
+$string['installamp.html'] = "A.M.P 설치";
 $string['upgrade.html'] = "판올림";
 
 $string['-usage'] = "무들의 사용";
@@ -24,7 +24,7 @@ $string['other.html'] = "기타 문서";
 
 $string['-development'] = "개발";
 $string['developer.html'] = "개발자 지침서";
-$string['coding.html'] = "Coding Guide";
+$string['coding.html'] = "코딩 지침";
 $string['cvs.html'] = "CVS의 이용";
 $string['translation.html'] = "번역";
 
