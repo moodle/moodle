b756f6f3016b0ee370eac865664de429caeb4b24
mits <mits>
2005-12-26 20:34:09 +0000

    translated new strings for Authorize.net.

--- lang/ja_utf8/enrol_authorize.php
+++ lang/ja_utf8/enrol_authorize.php
@@ -6,17 +6,36 @@ $string['adminauthorizeccapture'] = 'オーダーレビュー&オートキャプ
 $string['adminauthorizeemail'] = 'メール送信設定';
 $string['adminauthorizesettings'] = 'Authorize.net 設定';
 $string['adminauthorizewide'] = 'サイト全体の設定';
+$string['adminneworder'] = '新しい注文が入りました:
+
+注文ID: $a->orderid
+トランザクションID: $a->transid
+ユーザ: $a->user
+コース: $a->course
+金額: $a->amount
+
+オートキャプチャ有効?: $a->acstatus
+
+オートキャプチャが有効にされている場合、クレジットカードは $a->captureon 日でキャプチャされ、学生はコース登録されます。そうでない場合、$a->expireon 日で期限切れとなり、この日以降はキャプチャできなくなります。
+
+下記のリンクで、学生がコース登録するための支払いを承認/拒否することもできます:
+$a->url';
+$string['adminnewordersubject'] = '$a->course: 新しい未決注文 ( $a->orderid )';
 $string['adminreview'] = 'クレジットカード処理手続きの前に注文を検査する。';
+$string['amount'] = '金額';
 $string['anlogin'] = 'Authorize.net: ログイン名';
 $string['anpassword'] = 'Authorize.net: パスワード ( 不要 )';
 $string['anreferer'] = '必要な場合は、リファラURLを設定してください。これは、ウェブリクエストの「Referer: URL」ヘッダを送信します。';
 $string['antestmode'] = 'Authorize.net: テストトランザクション';
 $string['antrankey'] = 'Authorize.net: トランザクションキー';
 $string['authorizedpendingcapture'] = '認証完了 / キャプチャ未了';
+$string['canbecredit'] = '$a->upto に返金可能';
 $string['cancelled'] = 'キャンセル完了';
 $string['capture'] = 'キャプチャ';
 $string['capturedpendingsettle'] = '認証完了 / 確定未了';
 $string['capturedsettled'] = '認証完了 / 確定';
+$string['capturetestwarn'] = 'キャプチャに問題がありますが、テストモードではレコードは更新されません。';
+$string['captureyes'] = 'クレジットカード情報が取得され、学生がコース登録されます。本当によろしいですか?';
 $string['ccexpire'] = '有効期限';
 $string['ccexpired'] = 'クレジットカードの期限が切れています。';
 $string['ccinvalid'] = 'クレジットカードが間違っています。';
@@ -26,16 +45,20 @@ $string['ccvv'] = 'CV2';
 $string['ccvvhelp'] = 'カードの裏面 ( 3桁の数字 ) をご覧ください。';
 $string['choosemethod'] = 'コースの登録キーを知っている場合は、入力してください。コースの登録キーを知らない場合は、このコースに利用料を支払う必要があります。';
 $string['chooseone'] = '次の2つのフィールドの1つまたは両方に入力してください。';
+$string['credittestwarn'] = 'クレジットは正常に動作しているようですが、テストモードではデータベースにレコードはインサートされません。';
 $string['cutofftime'] = 'トランザクションカットオフ時間。何時に最終のトランザクションを確定のために取得しますか?';
 $string['description'] = 'Authorize.netモジュールでは、クレジットカード経由でコースの支払いを行うことができます。コースの受講料がゼロの場合、学生に対して受講登録に関する支払いは求められません。サイト全体の利用料をデフォルトとしてここで設定して、コースごとに受講料を設定することができます。コース受講料を設定した場合、コース受講料はサイト利用料に優先します。';
 $string['enrolname'] = 'Authorize.net クレジットカード・ゲイトウェイ';
 $string['expired'] = '期限切れ';
+$string['howmuch'] = 'いくらですか?';
 $string['httpsrequired'] = '申し訳ございません、あなたのリクエストは現在処理することができません。このサイトの設定は正常に行われませんでした。
 <br /><br />
 ブラウザの下部に黄色の鍵マークが表示されない場合は、あなたのクレジットカード番号を入力しないでください。これは単にクライアントとサーバ間で送信される全てのデータが暗号化されることを意味します。ですから、2台のコンピュータ間のトランザクション情報は保護され、あなたのクレジットカード番号がインターネット上で盗まれることはありません。';
-$string['logindesc'] = 'このオプションは「ON」にする必要があります。<br /><br />管理 >> 詳細設定 >> セキュリティ で <a href=\"$a->url\">loginhttps</a> が「Yes」になっていることを確認してください。 <br /><br />この設定を「Yes」にすることで、Moodleはログインおよび支払いページに関してセキュアhttps接続を使用します。';
+$string['logindesc'] = 'このオプションは「ON」にする必要があります。<br /><br />管理 >> 詳細設定 >> セキュリティ で <a href=\"$a->url\">loginhttps が「Yes」になっていること</a>を確認してください。 <br /><br />この設定を「Yes」にすることで、Moodleはログインおよび支払いページに関してセキュアhttps接続を使用します。';
 $string['nameoncard'] = 'クレジットカード名義';
-$string['orderid'] = 'オーダーID';
+$string['noreturns'] = '返金無し!';
+$string['notsettled'] = '未確定';
+$string['orderid'] = '注文ID';
 $string['paymentmanagement'] = '支払い管理';
 $string['paymentpending'] = 'このコースに関するあなたの支払いは保留中です。注文番号は、 $a->orderid です。';
 $string['refund'] = '払い戻し';
@@ -46,8 +69,14 @@ $string['reviewnotify'] = 'あなたの支払いが確認されました。数�
 $string['sendpaymentbutton'] = '支払いの送信';
 $string['settled'] = '確定済み';
 $string['settlementdate'] = '確定年月日';
+$string['subvoidyes'] = '返金済みトランザクション $a->transid はキャンセルされ、$a->amount があなたの口座に振り込まれます。本当によろしいですか?';
+$string['tested'] = 'テスト済み';
+$string['testmode'] = '[ テストモード ]';
 $string['transid'] = 'トランザクションID';
-$string['void'] = '無効';
+$string['unenrolstudent'] = '学生を登録解除しますか?';
+$string['void'] = '取り消し';
+$string['voidtestwarn'] = '取り消しに問題がありますが、テストモードではレコードは更新されません。';
+$string['voidyes'] = 'トランザクションがキャンセルされます。本当によろしいですか?';
 $string['zipcode'] = '郵便番号';
 
 ?>
