b326b3e8d72233b0f85e208aeb0a83c575f226ae
vthung <vthung>
2006-01-03 15:49:27 +0000

    Added some new strings

---
+++ lang/vi_utf8/block_glossary_random.php
@@ -0,0 +1,23 @@
+<?php  // $Id$
+$string['addentry'] = '&#272;&#432;a các l&#7901;i trích d&#7851;n yêu thích!';
+$string['askaddentry'] = 'Khi ng&#432;&#7901;i dùng b&#7893; sung các m&#7909;c m&#7899;i vào b&#7843;ng chú gi&#7843;i thu&#7853;t ng&#7919;, th&#7875; hi&#7875;n liên k&#7871;t t&#7899;i m&#7909;c này trong v&#259;n b&#7843;n';
+$string['askinvisible'] = 'Khi ng&#432;&#7901;i dùng không th&#7875; xem ho&#7863;c ch&#7881;nh s&#7917;a b&#7843;ng thu&#7853;t ng&#7919;, th&#7875; hi&#7879;n v&#259;n b&#7843;n này (không có liên k&#7871;t)'; 
+$string['askviewglossary'] = 'Khi ng&#432;&#7901;i dùng có th&#7875; xem b&#7843;ng chú gi&#7843;i thu&#7853;t ng&#7919; nh&#432;ng không &#273;&#432;a vào &#273;&#432;&#7907;c các m&#7909;c m&#7899;i, th&#7875; hi&#7879;n liên k&#7871;t v&#7899;i v&#259;n b&#7843;n này'; 
+$string['blockname'] = 'M&#7909;c ng&#7851;u nhiên trong b&#7843;ng chú gi&#7843;i thu&#7853;t ng&#7919;';
+$string['intro'] = '&#272;&#7843;m b&#7843;o r&#7857;ng b&#7841;n có ít nh&#7845;t m&#7897;t b&#7843;ng thu&#7853;t ng&#7919; v&#7899;i ít nh&#7845;t m&#7897;t m&#7909;c &#273;&#432;&#7907;c &#273;&#432;a vào khóa h&#7885;c. Sau &#273;ó b&#7841;n có th&#7875; &#273;i&#7873;u ch&#7881;nh các thi&#7871;t l&#7853;p sau';
+$string['invisible'] = '(ti&#7871;p t&#7909;c)'; 
+$string['lastmodified'] = 'M&#7909;c &#273;&#432;&#7907;c thay &#273;&#7893;i cu&#7889;i cùng';
+$string['nextone'] = 'M&#7909;c ti&#7871;p theo';
+$string['noentriesyet'] = 'Không có m&#7909;c nào trong b&#7843;ng thu&#7853;t ng&#7919; b&#7841;n ch&#7885;n.'; 
+$string['notyetconfigured'] = 'Hãy c&#7845;u hình kh&#7889;i này s&#7917; d&#7909;ng icon ch&#7881;nh s&#7917;a.'; 
+$string['notyetglossary'] = 'B&#7841;n ph&#7843;i có ít nh&#7845;t m&#7897;t b&#7843;ng thu&#7853;t ng&#7919; &#273;&#7875; ch&#7885;n.'; 
+$string['random'] = 'M&#7909;c ng&#7851;u nhiên';
+$string['refresh'] = 'Nh&#7919;ng ngày tr&#432;&#7899;c khi m&#7897;t m&#7909;c m&#7899;i &#273;&#432;&#7907;c ch&#7885;n';
+$string['select_glossary'] = 'L&#7845;y các m&#7909;c cho b&#7843;ng thu&#7853;t ng&#7919; này';
+$string['showconcept'] = 'Th&#7875; hi&#7875;n khái ni&#7879;m (ph&#7847;n &#273;&#7847;u) cho m&#7895;i m&#7909;c';
+$string['title'] = 'Tiêu &#273;&#7873;';
+$string['typehelp'] = '<b>M&#7909;c &#273;&#432;&#7907;c thay &#273;&#7893;i cu&#7889;i cùng</b> s&#7869; luôn hi&#7875;n th&#7883; m&#7909;c &#273;&#432;&#7907;c thay &#273;&#7893;i cu&#7889;i cùng, và <b>M&#7909;c ng&#7851;u nhiên</b> s&#7869; ch&#7885;n m&#7909;c m&#7899;i m&#7895;i l&#7847;n hi&#7875;n th&#7883;.  M&#7909;c <b>ti&#7871;p theo</b> s&#7869; &#273;&#432;&#7907;c l&#7845;y theo th&#7913; t&#7921;.  M&#7909;c cu&#7889;i cùng s&#7869; &#273;&#7863;c bi&#7879;t h&#7919;u ích khi m&#7897;t s&#7889; ngày &#273;&#432;&#7907;c ch&#7885;n, cho phép b&#7841;n t&#7841;o <i>Trích d&#7851;n c&#7911;a tu&#7847;n</i> ho&#7863;c m&#7897;t <i>L&#7901;i khuyên c&#7911;a ngày</i> mà m&#7885;i ng&#432;&#7901;i s&#7869; nhìn th&#7845;y.';
+$string['type'] = 'Cách m&#7897;t m&#7909;c m&#7899;i &#273;&#432;&#7907;c ch&#7885;n nh&#432; th&#7871; nào';
+$string['viewglossary'] = 'Thêm các trích d&#7851;n...'; 
+$string['whichfooter'] = 'B&#7841;n có th&#7875; hi&#7875;n th&#7883; các liên k&#7871;t v&#7899;i các ho&#7841;t &#273;&#7897;ng c&#7911;a b&#7843;ng thu&#7853;t ng&#7919; mà kh&#7889;i này g&#7855;n v&#7899;i. Kh&#7889;i s&#7869; hi&#7875;n th&#7883; các liên k&#7871;t t&#7899;i các ho&#7841;t &#273;&#7897;ng &#273;&#432;&#7907;c cho phép b&#7903;i b&#7843;ng thu&#7853;t ng&#7919; này.'; 
+?>
---
+++ lang/vi_utf8/block_html.php
@@ -0,0 +1,9 @@
+<?php // $Id$ 
+
+$string['html'] = 'HTML';
+$string['configtitle'] = 'Tiêu &#273;&#7873; c&#7911;a kh&#7889;i';
+$string['configcontent'] = 'N&#7897;i dung';
+$string['leaveblanktohide'] = '&#273;&#7875; tr&#7855;ng &#273;&#7875; gi&#7919; nguyên tiêu &#273;&#7873;';
+$string['newhtmlblock'] = '(kh&#7889;i HTML m&#7899;i)';
+
+?>
