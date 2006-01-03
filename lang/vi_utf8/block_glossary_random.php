517cc94ce0cb420fa8d185601273060cb0ce95cd
vthung <vthung>
2006-01-03 16:28:13 +0000

    Modified some strings

--- lang/vi_utf8/block_glossary_random.php
+++ lang/vi_utf8/block_glossary_random.php
@@ -17,7 +17,7 @@ $string['select_glossary'] = 'Lấy các mục từ bảng thuật ngữ này';
 $string['showconcept'] = 'Hiển thị khái niệm (tiêu đề) cho mỗi mục';
 $string['title'] = 'Tiêu đề';
 $string['typehelp'] = '<b>Lần thay đổi cuối cùng</b> sẽ luôn hiển thị mục được thay đổi cuối cùng, và <b>mục ngẫu nhiên</b> sẽ được lấy ngẫu nhiên.  Mục <b>tiếp theo</b> sẽ được lấy theo đúng thứ tự.  Mục này đặc biệt có ích khi số ngày được chọn, cho phép bạn hiển thị<i>Trích dẫn của tuần</i> hoặc một <i>Lời nhắn nhủ của ngày</i> mà mọi người nhìn thấy.';
-$string['type'] = 'Một mục mới được chọn như thế nào';
+$string['type'] = 'Mục mới được chọn như thế nào';
 $string['viewglossary'] = 'Thêm các trích dẫn...'; 
 $string['whichfooter'] = 'Bạn cũng có thể hiển thị các liên kết tới các hoạt động của bảng thuật ngữ mà khối này có liên hệ. Khối chỉ liên kết tới các hoạt động mà được phép đối với bảng thuật ngữ này.'; 
 ?>
