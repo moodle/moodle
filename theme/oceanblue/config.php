90865da6805f825cda1359d4a9406575fe557dba
mits <mits>
2006-01-19 05:53:15 +0000

    it's better to use standard css file name.

--- theme/oceanblue/config.php
+++ theme/oceanblue/config.php
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-$THEME->sheets = array('fontsize', 'gradients', 'styles_color');
+$THEME->sheets = array('styles_fonts', 'gradients', 'styles_color');
 
 /// This variable is an array containing the names of all the 
 /// stylesheet files you want included in this theme, and in what order
