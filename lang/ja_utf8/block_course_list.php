a1017a70c47ca6ffdfb7c0207c8a1e1630dc1f55
mits <mits>
2006-01-22 05:15:20 +0000

    making expression more soften, changed translation of "whether or not"  from Kanji character to Japanese syllabary characters. meaning is same.

--- lang/ja_utf8/block_course_list.php
+++ lang/ja_utf8/block_course_list.php
@@ -3,8 +3,8 @@
 
 
 $string['allcourses'] = '管理者はすべてのコースユーザを閲覧可';
-$string['blockname'] = 'コースリスト';
-$string['configadminview'] = 'コースリストブロック内で管理者に何を表示しますか?';
+$string['blockname'] = 'コース一覧';
+$string['configadminview'] = 'コース一覧ブロック内で管理者に何を表示しますか?';
 $string['owncourses'] = '管理者は自分のコースのみ閲覧可';
 
 ?>
