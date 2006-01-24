3b1cddb932bc00cb2af7f6d400a6c006322404cf
moodler <moodler>
2006-01-24 06:01:30 +0000

    adding a require_logout() function

--- login/logout.php
+++ login/logout.php
@@ -3,27 +3,7 @@
 
     require_once("../config.php");
 
-    if (isset($USER) and isset($USER->id)) {
-        add_to_log(SITEID, "user", "logout", "view.php?id=$USER->id&course=".SITEID, $USER->id, 0, $USER->id);
-
-        if ($USER->auth == 'cas' && !empty($CFG->cas_enabled)) {
-            require($CFG->dirroot.'/auth/cas/logout.php');
-        }
-    }
-
-
-    if (ini_get_bool("register_globals") and check_php_version("4.3.0")) {
-        // This method is just to try to avoid silly warnings from PHP 4.3.0
-        session_unregister("USER");
-        session_unregister("SESSION");
-    }
-
-    setcookie('MoodleSessionTest'.$CFG->sessioncookie, '', time() - 3600, '/');
-    unset($_SESSION['USER']);
-    unset($_SESSION['SESSION']);
-
-    unset($SESSION);
-    unset($USER);
+    require_logout();
 
     redirect("$CFG->wwwroot/");
 
