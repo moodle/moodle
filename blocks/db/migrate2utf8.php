052dec9a2c12dab41723d48bdfc7cfae6464b846
moodler <moodler>
2006-01-25 06:10:18 +0000

    adding more cases for serialized blocks

--- blocks/db/migrate2utf8.php
+++ blocks/db/migrate2utf8.php
@@ -31,7 +31,32 @@ function migrate2utf8_block_instance_configdata($recordid){
         update_record('block_instance',$blockinstance);
 
         return $blah;
+
+    } else if ($blocktype -> name == 'rss_client'){
+
+        ///find course
+
+        $sitelang   = $CFG->lang;
+        $courselang = get_course_lang($blockinstance->pageid);  //Non existing!
+        $userlang   = get_main_teacher_lang($blockinstance->pageid); //N.E.!!
+
+        $fromenc = get_original_encoding($sitelang, $courselang, $userlang);
+
+        $blah = unserialize(base64_decode($blockinstance->configdata));
+
+    /// We are going to use textlib facilities
+        $textlib = textlib_get_instance();
+    /// Convert the text
+        $blah->title = $textlib->convert($blah->title, $fromenc);
+
+        $blockinstance->configdata = base64_encode(serialize($blah));
+
+        update_record('block_instance',$blockinstance);
+
+        return $blah;
+
     }
+
 }
 
 function migrate2utf8_block_pinned_configdata($recordid){
