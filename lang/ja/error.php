9ae17efc1c4cc10999fd757195e78db68f8ded87
mits <mits>
2006-01-31 01:22:41 +0000

    translated new strings for component_installer class.

--- lang/ja/error.php
+++ lang/ja/error.php
@@ -3,9 +3,18 @@
 
 
 $string['adminprimarynoedit'] = '主管理者は他から編集することはできません。';
+$string['cannotcreatetempdir'] = 'tempディレクトリを作成できません。';
+$string['cannotdownloadcomponents'] = 'コンポーネットをダウンロードできません。';
+$string['cannotdownloadzipfile'] = 'ZIPファイルをダウンロードできません。';
+$string['cannotfindcomponent'] = 'コンポーネントを見つけることができません。';
+$string['cannotsavemd5file'] = 'mp5ファイルを保存できません。';
+$string['cannotsavezipfile'] = 'ZIPファイルを保存できません。';
+$string['cannotunzipfile'] = 'ZIPファイルを解凍できません。';
 $string['cmunknown'] = 'このコースモジュールがありません。';
+$string['componentisuptodate'] = 'コンポーネントは最新です。';
 $string['confirmsesskeybad'] = '申し訳ございません、あなたのセッションキーは、このアクションを実行するための確認をできませんでした。このセキュリティ機能は、あなたのユーザ名で重要な機能が不測または悪意により実行されることを防ぎます。本当にこの機能を実行したいか確認してください。';
 $string['coursegroupunknown'] = 'コースに関連するグループ $a が指定されていません。';
+$string['downloadedfilecheckfailed'] = 'ダウンロードファイルのチェックが失敗しました。';
 $string['errorcleaningdirectory'] = 'ディレクトリ「 $a 」のクリーニングにエラーが発生しました。';
 $string['errorcopyingfiles'] = 'ファイルのコピーにエラーが発生しました。';
 $string['errorcreatingdirectory'] = 'ディレクトリ「 $a 」の作成にエラーが発生しました。';
@@ -21,8 +30,10 @@ $string['groupunknown'] = 'グループ $a は指定されたコースに関連していません。';
 $string['invalidcourse'] = '無効なコース';
 $string['invalidfieldname'] = '「 $a 」 は正しいフィールド名ではありません。';
 $string['invalidfiletype'] = '「 $a 」は有効ファイルタイプではありません。';
+$string['invalidmd5'] = '無効なmp5ファイルです。';
 $string['invalidxmlfile'] = '「 $a 」は有効なXMLファイルではありません。';
 $string['missingfield'] = '「 $a 」 を入力してください。';
+$string['missingrequiredfield'] = 'いくつかの必須入力フィールドに入力されていません。';
 $string['modulemissingcode'] = 'モジュール $a が、この機能を実行するためのコードがありません。';
 $string['modulerequirementsnotmet'] = 'モジュール「 $a->modulename 」 ($a->moduleversion) をインストールできませんでした。このモジュールをインストールするためには新しいバージョンのMoodleをインストールする必要があります。(現在、あなたは $a->currentmoodle を使用しています。 $a->requiremoodle が必要です。)';
 $string['mustbeteacher'] = 'このページは教師のみ閲覧可能です。';
@@ -30,9 +41,11 @@ $string['noinstances'] = 'このコースに $a のインスタンスがありません!';
 $string['nonmeaningfulcontent'] = '意味の無いコンテンツです。';
 $string['noparticipatorycms'] = '申し訳ございません、レポートするための参加者がいるコースモジュールはありません。';
 $string['notavailable'] = '現在利用できません';
+$string['onlyadmins'] = '管理者のみ処理可能です。';
 $string['onlyeditingteachers'] = '編集権限を持った教師のみ処理可能です。';
 $string['onlyeditown'] = '他の人の情報は編集できません。';
 $string['processingstops'] = 'プロセスが停止しました。残りのレコードは無視されます。';
+$string['remotedownloadnotallowed'] = 'あなたのサーバーにコンポーネントをダウンロードすることができません ( allow_url_fopen が無効 )。<br /><br /><a href=\"$a->url\">$a->url</a> ファイルを手動でダウンロードして、サーバの「 $a->dest/ 」にコピーした後、解凍してください。';
 $string['restricteduser'] = '申し訳ございません、あなたのアカウント「 $a 」では、この処理を行うことができません。';
 $string['sessionerroruser'] = 'セッションがタイムアウトしました。再度ログインしてください。';
 $string['sessionerroruser2'] = 'あなたのログインセッションに影響を与えるサーバエラーが検知されました。再度ログインするかブラウザを再起動してください。';
@@ -44,5 +57,8 @@ $string['usernotaddedregistered'] = 'ユーザ 「 $a 」 は追加されませんでした - 登
 $string['usernotavailable'] = 'このユーザの詳細は閲覧できません。';
 $string['usernotrenamedexists'] = 'ユーザはリネームされませんでした -- 新しいユーザ名がすでに使用されています。';
 $string['usernotrenamedmissing'] = 'ユーザはリネームされませんでした -- 古いユーザ名が見つかりませんでした。';
+$string['wrongdestpath'] = '宛先パスが間違っています。';
+$string['wrongsourcebase'] = 'ソースURLベースが間違っています。';
+$string['wrongzipfilename'] = 'ZIPファイル名が間違っています。';
 
 ?>
