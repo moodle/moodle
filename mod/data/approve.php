20fda38ba9341c0d9b9ecabcab47ab73559aabd1
moodler <moodler>
2006-02-03 08:12:41 +0000

    adding functionality to allow teacher approval of record

---
+++ mod/data/approve.php
@@ -0,0 +1,30 @@
+<?php
+
+    require_once('../../config.php');
+    require_once('lib.php');
+    require_once($CFG->libdir.'/blocklib.php');
+
+    require_once('pagelib.php');
+    require_login();
+
+    if (!isteacher()) {
+        error ('you need to be a teacher to use this page!');
+    }
+
+    if (confirm_sesskey()
+        && ($recordid = required_param('recordid',PARAM_INT))
+        && ($d = required_param('d',PARAM_INT))) {
+        data_approve_record($recordid);
+    }
+
+    $page=optional_param('page','0',PARAM_INT);
+    $rid = optional_param('rid','0', PARAM_INT);
+    $search =optional_param('search','',PARAM_ALPHA);
+    $sort= optional_param('sort','',PARAM_ALPHA);
+    $order=optional_param('order','',PARAM_ALPHA);
+
+    print_heading(get_string('recordapproved','data'));
+    redirect('view.php?d='.$d.'&amp;approved=1&amp;page='.$page.'&amp;rid='.$rid.'&amp;search='.$search.'&amp;sort='.$sort.'&amp;order='.$order.'&amp;');
+
+
+?>
