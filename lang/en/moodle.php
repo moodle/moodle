919ece0f8872272de51dee576938be755235df10
moodler <moodler>
2006-02-06 12:52:12 +0000

    Slightly more documentation

--- lang/en/moodle.php
+++ lang/en/moodle.php
@@ -1,11 +1,11 @@
 <?php // $Id$
       // A stub language pack for backward compatibility with old sites not using Unicode.
       // It simply makes the en_utf8 pack appear under the old latin encoding.
+      // Do not add new strings here, please use en_utf8 for all new strings
 
 $string['thislanguage'] = 'English';
 $string['thischarset'] = 'iso-8859-1';
 $string['locale'] = 'en';
 $string['parentlang'] = 'en_utf8';
-
 
 ?>
