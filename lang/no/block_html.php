1a320e8140529224281e1b6754e60faffeeab5a6
koenr <koenr>
2006-02-08 10:02:58 +0000

    new files and strings for no moodle 1.5.3 from tormod - some XHTML issues to be solved

---
+++ lang/no/block_html.php
@@ -0,0 +1,11 @@
+<?PHP // $Id$ 
+      // block_html.php - created with Moodle 1.5.2 (2005060220)
+
+
+$string['configcontent'] = 'Innhold';
+$string['configtitle'] = 'Boksoverskrift';
+$string['html'] = 'HTML';
+$string['leaveblanktohide'] = 'la denne stå tom for å skjule tittelen';
+$string['newhtmlblock'] = 'Ny HTML-boks';
+
+?>
---
+++ lang/no/hotpot.php
@@ -0,0 +1,61 @@
+<?PHP // $Id$ 
+      // hotpot.php - created with Moodle 1.5.2 (2005060220)
+
+
+$string['abandoned'] = 'Forlatt';
+$string['allattempts'] = 'Alle forsøk';
+$string['allmycourses'] = 'Alle mine klasser';
+$string['average'] = 'Gjennomsnitt';
+$string['bestattempt'] = 'Beste forsøk';
+$string['checks'] = 'Kontroller';
+$string['clues'] = 'Tips';
+$string['copiedtoclipboard'] = 'Innholdet på denne siden har blitt kopiert til utklippstavla';
+$string['copytoclipboard'] = 'Kopier til utklippstavla';
+$string['correct'] = 'Riktig';
+$string['deleteabandoned'] = 'Slett forlatte';
+$string['deleteabandonedcheck'] = 'Vil du virkelig slette alle $a forlatte forsøk?';
+$string['deletedattempts'] = '$a forsøk ble slettet';
+$string['deletedresponses'] = '$a responses were deleted';
+$string['enterafilename'] = 'Please enter a file name';
+$string['filetype'] = 'Filtype';
+$string['forceplugins'] = 'Tving hjelpeprogrammer';
+$string['giveup'] = 'Gi opp';
+$string['hints'] = 'Hint';
+$string['ignored'] = 'Ignored';
+$string['inprogress'] = 'in progress';
+$string['location'] = 'Filplassering';
+$string['modulename'] = 'Hot Potatoes-kviss';
+$string['modulenameplural'] = 'Hot Potatoes-kvisser';
+$string['navigation'] = 'Navigasjon';
+$string['navigation_bar'] = 'Moodle navigasjonsmeny';
+$string['navigation_buttons'] = 'Hot Potatoes-kvissknapper';
+$string['navigation_frame'] = 'Moodle navigasjonsramme';
+$string['navigation_give_up'] = 'A single \"Give Up\" button';
+$string['navigation_iframe'] = 'Bakt inn som <IFRAME>';
+$string['navigation_none'] = 'None';
+$string['noactivity'] = 'Ingen aktivitet';
+$string['noresponses'] = 'No information about individual questions and responses was found.';
+$string['outputformat'] = 'Output format';
+$string['outputformat_best'] = 'best';
+$string['outputformat_flash'] = 'Flash';
+$string['outputformat_mobile'] = 'mobile';
+$string['outputformat_v3'] = 'v3';
+$string['outputformat_v4'] = 'v4';
+$string['outputformat_v5'] = 'v5';
+$string['outputformat_v6'] = 'v6';
+$string['outputformat_v6_plus'] = 'v6+';
+$string['penalties'] = 'Straffepoeng';
+$string['questionshort'] = 'Q-$a';
+$string['quiztype'] = 'Kviss-type';
+$string['resultssaved'] = 'Quiz results were saved';
+$string['score'] = 'Poeng';
+$string['selectattempt'] = 'Please select an attempt';
+$string['showhtmlsource'] = 'Vis HTML-kilde';
+$string['shownextquiz'] = 'Vis neste kviss';
+$string['showxmlsource'] = 'Vis XML-kilde';
+$string['showxmltree'] = 'Vis XML-tre';
+$string['thiscourse'] = 'Denne klassen';
+$string['weighting'] = 'Vekting';
+$string['wrong'] = 'Feil';
+
+?>
