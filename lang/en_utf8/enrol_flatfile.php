808856c6f61fc0718662a2a0d81271040913fc0a
patrickslee <patrickslee>
2006-02-13 00:36:56 +0000

    Imporved the documentation for flatfile enrolment plugin on the configuration page
    Backported from MOODLE_15_STABLE

--- lang/en_utf8/enrol_flatfile.php
+++ lang/en_utf8/enrol_flatfile.php
@@ -2,7 +2,17 @@
 
 $string['enrolname'] = 'Flat file';
 
-$string['description'] = 'This method will repeatedly check for and process a specially-formatted text file in the location that you specify.  The file can look something like this: 
+$string['description'] = 'This method will repeatedly check for and process a specially-formatted text file in the location that you specify.
+The file is a comma separated file assumed to have four or six fields per line:
+*  operation, role, idnumber(user), idnumber(course) [, starttime, endtime]
+where:
+*  operation        = add | del
+*  role             = student | teacher | teacheredit
+*  idnumber(user)   = idnumber in the user table NB not id
+*  idnumber(course) = idnumber in the course table NB not id
+*  starttime        = start time (in seconds since epoch) - optional
+*  endtime          = end time (in seconds since epoch) - optional
+It could look something like this:
 <pre>
    add, student, 5, CF101
    add, teacher, 6, CF101
