344be2522c72a725235c18e447ead0ff2ec05845
gustav_delius <gustav_delius>
2006-02-17 17:15:56 +0000

    Added link to questions bank

--- blocks/admin/block_admin.php
+++ blocks/admin/block_admin.php
@@ -145,6 +145,9 @@ class block_admin extends block_list {
                 $this->content->items[]='<a href="'.$CFG->wwwroot.'/course/import.php?id='.$this->instance->pageid.'">'.get_string('import').'</a>';
                 $this->content->icons[]='<img src="'.$CFG->pixpath.'/i/restore.gif" height="16" width="16" alt="" />';
 
+                $this->content->items[]='<a href="'.$CFG->wwwroot.'/mod/quiz/edit.php?courseid='.$this->instance->pageid.'">'.get_string('questions', 'quiz').'</a>';
+                $this->content->icons[]='<img src="'.$CFG->pixpath.'/i/questions.gif" height="16" width="16" alt="" />';
+
                 $this->content->items[]='<a href="scales.php?id='.$this->instance->pageid.'">'.get_string('scales').'</a>';
                 $this->content->icons[]='<img src="'.$CFG->pixpath.'/i/scales.gif" height="16" width="16" alt="" />';
 
