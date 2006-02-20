ea6f999dc3b1b43c317624eb37ea84873a430875
toyomoyo <toyomoyo>
2006-02-20 09:31:29 +0000

    fix for secure forms

--- mod/data/fields.php
+++ mod/data/fields.php
@@ -80,7 +80,7 @@
     switch($mode){
 
         case 'add':    ///add a new field
-            if (confirm_sesskey() and $field = data_submitted()){
+            if (confirm_sesskey() and $field = data_submitted('fields.php')){
 
                 $sql = 'SELECT * from '.$CFG->prefix.'data_fields WHERE name LIKE "'.$field->name.
                        '" AND dataid = '.$data->id;
