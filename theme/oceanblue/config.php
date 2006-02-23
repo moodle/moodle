d8c33a44a9bc8851bbef6f3338003d4b23648ad0
mits <mits>
2006-02-23 01:06:20 +0000

    polished.

--- theme/oceanblue/config.php
+++ theme/oceanblue/config.php
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-$THEME->sheets = array('styles_fonts',  'styles_layout', 'gradients', 'styles_color');
+$THEME->sheets = array('styles_fonts', 'styles_layout', 'gradients', 'styles_color');
 
 /// This variable is an array containing the names of all the 
 /// stylesheet files you want included in this theme, and in what order
