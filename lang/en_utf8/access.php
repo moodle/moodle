f6c9cf917eeacc48cd192bcf327df4a2c123d773
nfreear <nfreear>
2006-03-06 12:28:06 +0000

    Accessibility: the general accessibility, usability and navigation language file.

---
+++ lang/en_utf8/access.php
@@ -0,0 +1,30 @@
+<?php // $Id$
+      // access.php - created with Moodle 1.6 (2006030300)
+/**
+ * General accessibility, usability and navigation language file.
+ * 
+ * Most 'Alt' text and table summaries should go in the specific module language file.
+ *
+ * @copyright © 2006 The Open University
+ * @author N.D.Freear@open.ac.uk
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package  
+ */
+
+$string['access'] = 'Accessibility';
+$string['accesshelp'] = 'Accessibility help';
+$string['accesskey'] = 'Access key, $a';
+$string['accessstatement'] = 'Accessibility statement';
+$string['monthnext'] = 'Next month';
+$string['monthprevious'] = 'Previous month';
+$string['showhideblock'] = 'Show or hide block';
+$string['sitemap'] = 'Site map';
+$string['skipblock'] = 'Skip block';
+$string['skipnavigation'] = 'Skip navigation';
+$string['tabledata'] = 'Data table, $a';
+$string['tablelayout'] = 'Layout table, $a';
+$string['tocontent'] = 'Go to main content';
+$string['today'] = 'Today';
+$string['tonavigation'] = 'Go to navigation';
+
+?>
