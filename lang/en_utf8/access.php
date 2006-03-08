2eff1f87efe2ded25c6121a0af2aae1fbb94e778
nfreear <nfreear>
2006-03-08 14:12:04 +0000

    Accesibility: added 'breadcrumb' strings.

--- lang/en_utf8/access.php
+++ lang/en_utf8/access.php
@@ -5,7 +5,7 @@
  * 
  * Most 'Alt' text and table summaries should go in the specific module language file.
  *
- * @copyright © 2006 The Open University
+ * @copyright &copy; 2006 The Open University
  * @author N.D.Freear@open.ac.uk
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
  * @package  
@@ -15,8 +15,9 @@ $string['access'] = 'Accessibility';
 $string['accesshelp'] = 'Accessibility help';
 $string['accesskey'] = 'Access key, $a';
 $string['accessstatement'] = 'Accessibility statement';
+$string['breadcrumb'] = 'Breadcrumb trail';
 $string['monthnext'] = 'Next month';
-$string['monthprevious'] = 'Previous month';
+$string['monthprev'] = 'Previous month';
 $string['showhideblock'] = 'Show or hide block';
 $string['sitemap'] = 'Site map';
 $string['skipblock'] = 'Skip block';
@@ -26,5 +27,6 @@ $string['tablelayout'] = 'Layout table, $a';
 $string['tocontent'] = 'Go to main content';
 $string['today'] = 'Today';
 $string['tonavigation'] = 'Go to navigation';
+$string['youarehere'] = 'You are here';
 
 ?>
