6a2c524ca9a8c938b43a0fec562c1b35a68a4ed1
martinlanghoff <martinlanghoff>
2006-03-09 03:20:30 +0000

    [multienrol]DB upgrade script to clean up enrol field for user_students and user_teachers

--- lib/db/postgres7.php
+++ lib/db/postgres7.php
@@ -1411,6 +1411,11 @@ function main_upgrade($oldversion=0) {
         }
     }
     
+    if ($oldversion < 2005103100) { // Repair enrol field in user_students/user_teacher table
+        execute_sql("UPDATE {$CFG->prefix}user_students SET enrol='manual' WHERE enrol='' OR enrol='internal'");
+        execute_sql("UPDATE {$CFG->prefix}user_teachers SET enrol='manual' WHERE enrol=''");
+    }
+
     return $result;
 }
 
