36245854f2aa7642c2d770d59b3dfcdeeaeee0f3
moodler <moodler>
2006-03-09 07:41:18 +0000

    No direct inclusion

--- course/report/stats/mod.php
+++ course/report/stats/mod.php
@@ -1,5 +1,9 @@
 <?php
 
+    if (!defined('MOODLE_INTERNAL')) {
+        die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+    }
+
     if (!empty($CFG->enablestats)) {
         echo '<a href="'.$CFG->wwwroot.'/course/report/stats/index.php?course='.$course->id.'">'.get_string('stats').'</a>';
     }
