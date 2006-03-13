b37a5f51fc8df48216a1e83e8feb3f3120c4d088
toyomoyo <toyomoyo>
2006-03-13 06:10:45 +0000

    adding some strings

---
+++ lang/en_utf8/blog.php
@@ -0,0 +1,33 @@
+<?PHP 
+
+$string['addnewentry'] = 'Add a New Entry';
+$string['addotags'] = 'Add Official Tags';
+$string['addptags'] = 'Add User Defined Tags';
+$string['blockmenutitle'] = 'Blog Menu';
+$string['blogs'] = 'Blogs';
+$string['blogpreferences'] = 'Blog Preferences';
+$string['entrybody'] = 'Blog Entry Body';
+$string['entrybodyonlydesc'] = 'Entry Description';
+$string['entryerrornotyours'] = 'This entry is not yours';
+$string['entrytitle'] = 'Entry Title';
+$string['entryupdated'] = 'Blog Entry Updated';
+$string['noguestpost'] = 'Guest can not post blogs!';
+$string['notallowedtoedit'] = 'You are not allowed to edit this entry';
+$string['otags'] = 'Official Tags';
+$string['pagesize'] = 'Number of Blog Entries per Page';
+$string['preferences'] = 'Preferences';
+$string['ptags'] = 'User Defined Tags';
+$string['publishto'] = 'Publish To';
+$string['publishtonoone'] = 'Draft';
+$string['publishtosite'] = 'Publish To Site';
+$string['publishtoworld'] = 'Publish To World';
+$string['settingsupdatederror'] = 'An error has occured, blog preference setting could not be updated';
+$string['tag'] = 'Tag';
+$string['tagmanagement'] = 'Tags Management';
+$string['tags'] = 'Tags: ';
+$string['updateentrywithid'] = 'Updating Entry';
+$string['viewcourseentries'] = 'View My Entries';
+$string['viewmyentries'] = 'View My Entries';
+$string['viewsiteentries'] = 'View Site Entries';
+
+?>
