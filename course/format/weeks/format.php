d57be0a8b4531e1f5f299e39bbfbfc3788b6a0e6
nfreear <nfreear>
2006-03-15 10:23:09 +0000

    Accessibility: strict, replaced image border, width... with classes (OU-Bugz: 697, 745)

--- course/format/weeks/format.php
+++ course/format/weeks/format.php
@@ -93,7 +93,7 @@
         if (isediting($course->id)) {
             echo '<a title="'.$streditsummary.'" '.
                  ' href="editsection.php?id='.$thissection->id.'"><img src="'.$CFG->pixpath.'/t/edit.gif" '.
-                 ' height="11" width="11" border="0" alt="'.$streditsummary.'" /></a><br /><br />';
+                 'class="icon edit" alt="'.$streditsummary.'" /></a><br /><br />';
         }
         echo '</div>';
         
@@ -183,7 +183,7 @@
 
                 if (isediting($course->id)) {
                     echo ' <a title="'.$streditsummary.'" href="editsection.php?id='.$thissection->id.'">'.
-                         '<img src="'.$CFG->pixpath.'/t/edit.gif" border="0" height="11" width="11" alt="" /></a><br /><br />';
+                         '<img src="'.$CFG->pixpath.'/t/edit.gif" class="icon edit" alt="" /></a><br /><br />';
                 }
                 echo '</div>';
 
@@ -199,29 +199,29 @@
 
             if ($displaysection == $section) {
                 echo '<a href="view.php?id='.$course->id.'&amp;week=all#section-'.$section.'" title="'.$strshowallweeks.'">'.
-                     '<img src="'.$CFG->pixpath.'/i/all.gif" height="25" width="16" border="0" alt="'.$strshowallweeks.'" /></a><br />';
+                     '<img src="'.$CFG->pixpath.'/i/all.gif" class="icon wkall" alt="'.$strshowallweeks.'" /></a><br />';
             } else {
                 $strshowonlyweek = get_string("showonlyweek", "", $section);
                 echo '<a href="view.php?id='.$course->id.'&amp;week='.$section.'" title="'.$strshowonlyweek.'">'.
-                     '<img src="'.$CFG->pixpath.'/i/one.gif" height="16" width="16" border="0" alt="" /></a><br />';
+                     '<img src="'.$CFG->pixpath.'/i/one.gif" class="icon wkone" alt="" /></a><br />';
             }
 
             if (isediting($course->id)) {
                 if ($thissection->visible) {        // Show the hide/show eye
                     echo '<a href="view.php?id='.$course->id.'&amp;hide='.$section.'&amp;sesskey='.$USER->sesskey.'#section-'.$section.'" title="'.$strweekhide.'">'.
-                         '<img src="'.$CFG->pixpath.'/i/hide.gif" vspace="3" height="16" width="16" border="0" alt="" /></a><br />';
+                         '<img src="'.$CFG->pixpath.'/i/hide.gif" vspace="3" class="icon hide" alt="" /></a><br />';
                 } else {
                     echo '<a href="view.php?id='.$course->id.'&amp;show='.$section.'&amp;sesskey='.$USER->sesskey.'#section-'.$section.'" title="'.$strweekhide.'">'.
-                         '<img src="'.$CFG->pixpath.'/i/show.gif" vspace="3" height="16" width="16" border="0" alt="" /></a><br />';
+                         '<img src="'.$CFG->pixpath.'/i/show.gif" vspace="3" class="icon hide" alt="" /></a><br />';
                 }
                 if ($section > 1) {                       // Add a arrow to move section up
                     echo '<a href="view.php?id='.$course->id.'&amp;section='.$section.'&amp;move=-1&amp;sesskey='.$USER->sesskey.'#section-'.($section-1).'" title="'.$strmoveup.'">'.
-                         '<img src="'.$CFG->pixpath.'/t/up.gif" vspace="3" height="11" width="11" border="0" alt="" /></a><br />';
+                         '<img src="'.$CFG->pixpath.'/t/up.gif" vspace="3" class="icon up" alt="" /></a><br />';
                 }
 
                 if ($section < $course->numsections) {    // Add a arrow to move section down
                     echo '<a href="view.php?id='.$course->id.'&amp;section='.$section.'&amp;move=1&amp;sesskey='.$USER->sesskey.'#section-'.($section+1).'" title="'.$strmovedown.'">'.
-                         '<img src="'.$CFG->pixpath.'/t/down.gif" vspace="3" height="11" width="11" border="0" alt="" /></a><br />';
+                         '<img src="'.$CFG->pixpath.'/t/down.gif" vspace="3" class="icon down" alt="" /></a><br />';
                 }
             }
 
