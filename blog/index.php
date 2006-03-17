29e2dd37ea8434e270c5b140e16ac013847e3ac9
toyomoyo <toyomoyo>
2006-03-17 05:54:46 +0000

    took out rss stuff

--- blog/index.php
+++ blog/index.php
@@ -15,11 +15,6 @@ require_once('../config.php');
 require_once($CFG->dirroot .'/blog/lib.php');
 require_once($CFG->libdir .'/blocklib.php');
 
-/* rss testing code */
-require_once('rsslib.php');
-blog_rss_feeds();
-
-
 $id = optional_param('id', 0, PARAM_INT);
 $limit = optional_param('limit', 0, PARAM_INT);
 $start = optional_param('formstart', 0, PARAM_INT);
