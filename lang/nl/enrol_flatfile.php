28680c9b32c160af2d89b7580745713419e4e62d
koenr <koenr>
2006-03-28 09:20:27 +0000

    some documentation missing

--- lang/nl/enrol_flatfile.php
+++ lang/nl/enrol_flatfile.php
@@ -1,8 +1,17 @@
 <?PHP // $Id$ 
-      // enrol_flatfile.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2004093001)
+      // enrol_flatfile.php - created with Moodle 1.5.3+ (2005060230)
 
 
-$string['description'] = 'Deze methode controleert regelmatig een speciaal opgemaakt tekstbestand op een lokatie die je zelf bepaalt. Het bestand kan er uitzien als volgt:
+$string['description'] = 'Deze methode controleert regelmatig een speciaal opgemaakt tekstbestand op een lokatie die je zelf bepaalt. Het bestand is een door komma\'s gescheiden lijst met vier of zes velden per lijn:
+Actie, rol, gebruikers IDnummer, cursus IDnummer, [,starttijd, eindtijd]
+
+* actie = add|del
+* rol = student|teacher|teacheredit
+* gebruikers IDnummer = IDnummer in de user tabel NB, niet id
+* cursus IDnummer = IDnummer in de course tabel NB, niet id
+* starttime = starttijd (in seconden sinds epoch) - optioneel
+* endtime = eindtijd (in seconden sinds epoch) - optioneel
+Het bestand kan er uitzien zoals dit:
 <pre>
 add, student, 5, CF101
 add, teacher, 6, CF101
