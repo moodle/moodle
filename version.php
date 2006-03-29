353bbad47aa329682dbba6d41e2b14f65ba5cb9b
moodler <moodler>
2006-03-29 18:52:42 +0000

    Let's call it this, tomorrow.  :-)

--- version.php
+++ version.php
@@ -6,9 +6,9 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-   $version = 2006032800;  // YYYYMMDD = date
+   $version = 2006032900;  // YYYYMMDD = date
                            //       XY = increments within a single day
 
-   $release = '1.6 development';    // Human-friendly version name
+   $release = '1.6 Beta 1';    // Human-friendly version name
 
 ?>
