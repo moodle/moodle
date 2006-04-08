2d91754b3386e680fa07183e8759da356c4f15b7
urs_hunkler <urs_hunkler>
2006-04-08 12:25:06 +0000

    copied back the CSS for Item analysis and for grading

--- mod/quiz/styles.php
+++ mod/quiz/styles.php
@@ -1,3 +1,85 @@
+body#mod-quiz-report table#itemanalysis {
+  margin: 20px auto;
+}
+body#mod-quiz-report table#itemanalysis .header,
+body#mod-quiz-report table#itemanalysis .cell
+{
+  padding: 4px;
+}
+body#mod-quiz-report table#itemanalysis .header .commands {
+  display: inline;
+}
+body#mod-quiz-report table#itemanalysis td {
+  border-width: 1px;
+  border-style: solid;
+}
+body#mod-quiz-report table#itemanalysis .header {
+  text-align: left;
+}
+body#mod-quiz-report table#itemanalysis .numcol {
+  text-align: center;
+  vertical-align : middle !important;
+}
+
+body#mod-quiz-report table#itemanalysis .uncorrect {
+  color: red;
+}
+
+body#mod-quiz-report table#itemanalysis .correct {
+  color: blue;
+  font-weight : bold;
+}
+
+body#mod-quiz-report table#itemanalysis .partialcorrect {
+  color: green !important;
+}
+
+body#mod-quiz-report table#itemanalysis .qname {
+  color: green !important;
+}
+
+/* manual grading */
+body#mod-quiz-grading table#grading
+{
+  width: 80%;
+  margin: auto;
+}
+body#mod-quiz-grading table#grading
+{
+  margin: 20px auto;
+}
+body#mod-quiz-grading table#grading .header,
+body#mod-quiz-grading table#grading .cell
+{
+  padding: 4px;
+}
+body#mod-quiz-grading table#grading .header .commands 
+{
+  display: inline;
+}
+body#mod-quiz-grading table#grading .picture 
+{
+  width: 40px;
+}
+body#mod-quiz-grading table#grading td 
+{
+  border-left-width: 1px;
+  border-right-width: 1px;
+  border-left-style: solid;
+  border-right-style: solid;
+  vertical-align: bottom;
+}
+.mod-quiz .quiz-report-title {
+  text-align: center;
+  font-weight : bold;
+}
+.mod-quiz .grade {
+  font-size: small;
+  margin-top: 10pt
+}
+.mod-quiz .gradingdetails {
+  font-size: small;
+}
 #mod-quiz-attempt #page {
 	text-align: center;
 }
