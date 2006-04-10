5990290185fd7fafe26bfa4df987d4b72776cd77
urs_hunkler <urs_hunkler>
2006-04-10 17:40:28 +0000

    ~chameleon.php contains all code neccessary to add the Chameleon engine to any theme.

---
+++ theme/chameleon/ui/chameleon.php
@@ -0,0 +1,34 @@
+<?php
+
+   
+if (isset($THEME->chameleonenabled) && $THEME->chameleonenabled) {
+    $chameleon_isadmin = isadmin();
+    $chameleon_isteacher = false;
+    if (isset($course->id)) {
+        $chameleon_courseparam = '?id=' . $course->id;
+        if (!$chameleon_isadmin) {
+            $chameleon_isteacher = (isteacher($course->id) && isset($CFG->coursetheme));
+        }
+    } else {
+        $chameleon_courseparam = '';
+    }
+
+    if ($chameleon_isadmin || $chameleon_isteacher) { 
+        // either we're an admin or we're a teacher and this is being used as the course theme
+        // if we're on a page using a course theme edit that, otherwise edit the main chameleon theme
+        // $chameleon_theme = (isset($CFG->coursetheme)) ? $CFG->coursetheme : $CFG->theme;
+        $chameleon_theme = (isset($CFG->coursetheme)) ? $CFG->coursetheme : current_theme();
+?>
+
+<style type="text/css"> @import '<?php echo "$CFG->wwwroot/theme/$chameleon_theme" ?>/ui/chameleon_ui.css'; </style>
+
+<script type="text/javascript" src="<?php echo "$CFG->wwwroot/theme/$chameleon_theme/ui/css_query.js" ?>"> </script>
+<script type="text/javascript" src="<?php echo "$CFG->wwwroot/theme/$chameleon_theme/ui/sarissa.js" ?>"> </script>
+<script type="text/javascript" src="<?php echo "$CFG->wwwroot/theme/$chameleon_theme/ui/chameleon_js.php$chameleon_courseparam" ?>"> </script>
+
+<?php
+    }
+}
+?>
+
+
