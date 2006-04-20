56c5918727969a0c15d30b4a2730e7f5a8b0e81d
toyomoyo <toyomoyo>
2006-04-20 02:33:04 +0000

    fix for 5257

--- blocks/blog_tags/block_blog_tags.php
+++ blocks/blog_tags/block_blog_tags.php
@@ -70,8 +70,10 @@ class block_blog_tags extends block_base {
         $timewithin = $this->config->timewithin * 24 * 60 * 60; /// convert to seconds
         
         $sql  = 'SELECT t.*, COUNT(DISTINCT bt.id) as ct ';
-        $sql .= "FROM {$CFG->prefix}tags as t, {$CFG->prefix}blog_tag_instance as bt ";
+        $sql .= "FROM {$CFG->prefix}tags as t, {$CFG->prefix}blog_tag_instance as bt, {$CFG->prefix}post as p ";
         $sql .= 'WHERE t.id = bt.tagid ';
+        $sql .= 'AND p.id = bt.entryid ';
+        $sql .= 'AND (p.publishstate = "site" or p.publishstate="public") ';
         $sql .= "AND bt.timemodified > {$timewithin} ";
         $sql .= 'GROUP BY bt.tagid ';
         $sql .= 'ORDER BY ct DESC, t.text ASC ';
