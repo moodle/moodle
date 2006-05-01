e7226b2bc894efc3a98b6236bd22c97691a1be22
paca70 <paca70>
2006-05-01 12:56:57 +0000

    Removed comma

--- question/type/randomsamatch/db/postgres7.sql
+++ question/type/randomsamatch/db/postgres7.sql
@@ -8,7 +8,7 @@
 CREATE TABLE prefix_question_randomsamatch (
   id SERIAL PRIMARY KEY,
   question integer NOT NULL default '0',
-  choose integer NOT NULL default '4',
+  choose integer NOT NULL default '4'
 );
 
 CREATE INDEX prefix_question_randomsamatch_question_idx ON prefix_question_randomsamatch (question);
