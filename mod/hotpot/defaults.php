acfc768e3e525d53d8bd910fcfcb7b98ce356c9b
moodler <moodler>
2006-05-02 06:00:35 +0000

    COnverted from DOS format

--- mod/hotpot/defaults.php
+++ mod/hotpot/defaults.php
@@ -1,4 +1,4 @@
-<?php 
+<?php // $Id$
     if (empty($CFG->hotpot_initialdisable)) {
         if (!count_records('hotpot')) {
             set_field('modules', 'visible', 0, 'name', 'hotpot');  // Disable it by default
