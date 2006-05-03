bdeed966f6bedfdc89f8aef5f807391e28cb5dde
stronk7 <stronk7>
2006-05-03 23:48:28 +0000

    Now more quiz actions are restored properly:  -preview  -start attempt  -close attempt  -continue attempt (too long. See Bug 5419) Bug 5122 (http://moodle.org/bugs/bug.php?op=show&bugid=5122)

--- mod/quiz/restorelib.php
+++ mod/quiz/restorelib.php
@@ -659,6 +659,86 @@
                 }
             }
             break;
+        case "preview":
+            if ($log->cmid) {
+                //Get the new_id of the module (to recode the url field)
+                $mod = backup_getid($restore->backup_unique_code,$log->module,$log->info);
+                if ($mod) {
+                    $log->url = "attempt.php?id=".$log->cmid;
+                    $log->info = $mod->new_id;
+                    $status = true;
+                }
+            }
+            break;
+        case "start attempt":
+            if ($log->cmid) {
+                //Get the new_id of the module (to recode the info field)
+                $mod = backup_getid($restore->backup_unique_code,$log->module,$log->info);
+                if ($mod) {
+                    //Extract the attempt id from the url field
+                    $attid = substr(strrchr($log->url,"="),1);
+                    //Get the new_id of the attempt (to recode the url field)
+                    $att = backup_getid($restore->backup_unique_code,"quiz_attempts",$attid);
+                    if ($att) {
+                        $log->url = "review.php?id=".$log->cmid."&attempt=".$att->new_id;
+                        $log->info = $mod->new_id;
+                        $status = true;
+                    }
+                }
+            }
+            break;
+        case "close attempt":
+            if ($log->cmid) {
+                //Get the new_id of the module (to recode the info field)
+                $mod = backup_getid($restore->backup_unique_code,$log->module,$log->info);
+                if ($mod) {
+                    //Extract the attempt id from the url field
+                    $attid = substr(strrchr($log->url,"="),1);
+                    //Get the new_id of the attempt (to recode the url field)
+                    $att = backup_getid($restore->backup_unique_code,"quiz_attempts",$attid);
+                    if ($att) {
+                        $log->url = "review.php?id=".$log->cmid."&attempt=".$att->new_id;
+                        $log->info = $mod->new_id;
+                        $status = true;
+                    }
+                }
+            }
+            break;
+        case "continue attempt":
+            if ($log->cmid) {
+                //Get the new_id of the module (to recode the info field)
+                $mod = backup_getid($restore->backup_unique_code,$log->module,$log->info);
+                if ($mod) {
+                    //Extract the attempt id from the url field
+                    $attid = substr(strrchr($log->url,"="),1);
+                    //Get the new_id of the attempt (to recode the url field)
+                    $att = backup_getid($restore->backup_unique_code,"quiz_attempts",$attid);
+                    if ($att) {
+                        $log->url = "review.php?id=".$log->cmid."&attempt=".$att->new_id;
+                        $log->info = $mod->new_id;
+                        $status = true;
+                    }
+                }
+            }
+            break;
+        case "continue attemp":
+            if ($log->cmid) {
+                //Get the new_id of the module (to recode the info field)
+                $mod = backup_getid($restore->backup_unique_code,$log->module,$log->info);
+                if ($mod) {
+                    //Extract the attempt id from the url field
+                    $attid = substr(strrchr($log->url,"="),1);
+                    //Get the new_id of the attempt (to recode the url field)
+                    $att = backup_getid($restore->backup_unique_code,"quiz_attempts",$attid);
+                    if ($att) {
+                        $log->url = "review.php?id=".$log->cmid."&attempt=".$att->new_id;
+                        $log->info = $mod->new_id;
+                        $log->action = "continue attempt";  //To recover some bad actions
+                        $status = true;
+                    }
+                }
+            }
+            break;
         default:
             if (!defined('RESTORE_SILENTLY')) {
                 echo "action (".$log->module."-".$log->action.") unknown. Not restored<br />";                 //Debug
