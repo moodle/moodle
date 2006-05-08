1e4f7f125b6b52b65aa6030ac10d6e6795aea413
moodler <moodler>
2006-05-08 01:47:25 +0000

    Updated hints about stable versions

--- version.php
+++ version.php
@@ -1,13 +1,14 @@
 <?php
 
-// MOODLE VERSION INFORMATION
+// MOODLE 1.6 VERSION INFORMATION
 
 // This file defines the current version of the core Moodle code being used.
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-   $version = 2006050500;  // YYYYMMDD = date
-                           //       XY = increments within a single day
+   $version = 2006050500;  // YYYYMMDD = date of the 1.6 branch (don't change)
+                           //       X  = release number 1.6.[0,1,2,3...]
+                           //        Y = micro-increments between releases
 
    $release = '1.6 Beta 5';    // Human-friendly version name
 
