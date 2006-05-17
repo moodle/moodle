9bd76b32f3a8cb43b99f1f8009aa45016391b2d2
tjhunt <tjhunt>
2006-05-17 11:40:37 +0000

    Merge from 1.6 branch. Fix URL syntax error when slasharguments is off.

--- question/export.php
+++ question/export.php
@@ -114,12 +114,11 @@
         $quizexportdir = get_string( 'exportfilename', 'quiz' );
         $downloadextra_str = get_string( 'downloadextra','quiz' );
         if ($CFG->slasharguments) {
-          $efile = "{$CFG->wwwroot}/file.php/$course->id/$quizexportdir/$exportfilename".$file_ext;
+          $efile = "{$CFG->wwwroot}/file.php/$course->id/$quizexportdir/$exportfilename".$file_ext."?forcedownload=1";
         }
         else {
-          $efile = "{$CFG->wwwroot}/file.php?file=/$course->id/$quizexportdir/$exportfilename".$file_ext;
+          $efile = "{$CFG->wwwroot}/file.php?file=/$course->id/$quizexportdir/$exportfilename".$file_ext."&forcedownload=1";
         }
-	$efile .= "?forcedownload=1";
         echo "</p><center><a href=\"$efile\">$download_str</a></center></p>";
         echo "</p><center><font size=\"-1\">$downloadextra_str</font></center></p>";
 
