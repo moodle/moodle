6ebca3c77ee99136ec888ae7dacb79e0c987ab09
skodak <skodak>
2006-05-23 09:10:23 +0000

    removed reliance on register globals

--- theme/standard/docstyles.php
+++ theme/standard/docstyles.php
@@ -1,13 +1,11 @@
 <?PHP /*  $Id$ */
 
 /// We use PHP so we can do value substitutions into the styles
-
-    if (!isset($themename)) {
-        $themename = NULL;
-    }
-
     $nomoodlecookie = true;
+
     require_once("../../config.php");
+    $themename = optional_param('themename', NULL, PARAM_SAFEDIR);
+
     $themeurl = style_sheet_setup(filemtime("styles.php"), 300, $themename);
 
 ///
