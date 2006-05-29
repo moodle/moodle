364f1151a6d4344b66b3d76bc3946fc75dcd20bb
moodler <moodler>
2006-05-29 09:39:55 +0000

    Updated some "my moodle" strings

--- lang/en_utf8/my.php
+++ lang/en_utf8/my.php
@@ -2,9 +2,9 @@
       // moodle.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005010100)
 
 
-$string['mymoodle'] = 'My moodle';
+$string['mymoodle'] = 'Overview of my courses';
 $string['nocourses'] = 'No course information to show.';
-$string['noguest'] = 'The \'My Moodle\' page is not available to guest users';
+$string['noguest'] = 'The \'Course Overview\' page is not available to guest users';
 $string['pinblocks'] = 'Configure pinned blocks for my moodle';
-$string['pinblocksexplan'] = 'Any block settings you configure here will be visible (and non-editable) for any user of moodle on their \'my moodle\' screen.';
+$string['pinblocksexplan'] = 'Any block settings you configure here will be visible (and non-editable) for any user of moodle on their \'my moodle\' overview page.';
 ?>
