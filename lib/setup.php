baa1209e37753a9dc1f85613e7563dfded822193
moodler <moodler>
2006-05-29 07:34:55 +0000

    Merged from HEAD, better definition of $COURSE

--- lib/setup.php
+++ lib/setup.php
@@ -230,10 +230,9 @@ global $HTTPSPAGEREQUIRED;
          * @ignore
          */
         define('SITEID', 1);
-        define('COURSEID', 1);
     }
 /// And the 'default' course
-    $COURSE->id = SITEID;   // For now.  This will usually get reset later in require_login() etc.
+    $COURSE = clone($SITE);   // For now.  This will usually get reset later in require_login() etc.
 
 
 /// Set a default enrolment configuration (see bug 1598)
