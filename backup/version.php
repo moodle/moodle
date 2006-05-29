b4c8301258a2722511b4583fc18c7811320c9d8f
stronk7 <stronk7>
2006-05-29 22:49:54 +0000

    Bump or I'll forget it.

--- backup/version.php
+++ backup/version.php
@@ -5,6 +5,6 @@
 // database (backup_version) to determine whether upgrades should
 // be performed (see db/backup_*.php)
 
-$backup_version = 2006042801;   // The current version is a date (YYYYMMDDXX)
+$backup_version = 2006053000;   // The current version is a date (YYYYMMDDXX)
 
-$backup_release = "1.6beta5";  // User-friendly version number
+$backup_release = "1.6";  // User-friendly version number
