800085d9ae54f4564329699e979813f5fd0ab631
moodler <moodler>
2006-05-31 08:24:28 +0000

    Don't show stupid warnings to teachers

--- course/report/stats/mod.php
+++ course/report/stats/mod.php
@@ -4,11 +4,9 @@
         die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
     }
     
-    echo '<p style="text-align:center;">';
     if (!empty($CFG->enablestats)) {
+        echo '<p style="text-align:center;">';
         echo '<a href="'.$CFG->wwwroot.'/course/report/stats/index.php?course='.$course->id.'">'.get_string('stats').'</a>';
-    } else {
-        echo '<a href="'.$CFG->wwwroot.'/admin/config.php#configsectionstats">'.get_string('statsoff').'</a>';
+        echo '</p>';
     }
-    echo '</p>';
 ?>
