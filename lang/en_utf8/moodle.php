93e779c23690879b581f86e27dfae46cd7361db7
stronk7 <stronk7>
2006-06-06 17:02:19 +0000

    Adding two new strings for RESTRICTED tests in environmenlib.

--- lang/en_utf8/moodle.php
+++ lang/en_utf8/moodle.php
@@ -1076,6 +1076,7 @@ $string['restorecancelled'] = 'Restore canceled';
 $string['restorecoursenow'] = 'Restore this course now!';
 $string['restorefinished'] = 'Restore completed successfully';
 $string['restoreto'] = 'Restore to';
+$string['restricted'] = 'Restricted';
 $string['restrictmodules'] = 'Restrict activity modules?';
 $string['returningtosite'] = 'Returning to this web site?';
 $string['revert'] = 'Revert';
