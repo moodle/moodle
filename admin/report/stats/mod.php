0f423e9d64847892e997dca92fae5e72127666a8
vyshane <vyshane>
2006-06-06 05:31:42 +0000

    New admin stats report. This report module makes use of /course/report/stats/. Merged from 1.6.

---
+++ admin/report/stats/mod.php
@@ -0,0 +1,12 @@
+<?php  //$Id$
+
+    if (!defined('MOODLE_INTERNAL')) {
+        die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+    }
+    
+    if (!empty($CFG->enablestats)) {
+        echo '<p style="text-align:center;">';
+        echo '<a href="'.$CFG->wwwroot.'/admin/report/stats/index.php?course='.$course->id.'">'.get_string('stats').'</a>';
+        echo '</p>';
+    }
+?>
