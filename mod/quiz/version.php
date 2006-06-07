36d7c028470ddc07029acd30897cc18f39d8fa62
toyomoyo <toyomoyo>
2006-06-07 05:56:59 +0000

    fix for 5720

--- mod/quiz/version.php
+++ mod/quiz/version.php
@@ -5,7 +5,7 @@
 //  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2006042802;   // The (date) version of this module
+$module->version  = 2006060700;   // The (date) version of this module
 $module->requires = 2006022400;   // Requires this Moodle version
 $module->cron     = 0;            // How often should cron check this module (seconds)?
 
