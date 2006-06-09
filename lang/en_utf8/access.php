d65611c3fe8f0371d570456afc6301584175de28
nfreear <nfreear>
2006-06-09 14:21:25 +0000

    Fix bug 5611 (Skip block), across 4 files. Extra parameter $title in weblib.php: print_side_block, edits to moodleblock.class.php, Standard style, en_utf8 language pack.

--- lang/en_utf8/access.php
+++ lang/en_utf8/access.php
@@ -2,13 +2,13 @@
       // access.php - created with Moodle 1.6 (2006030300)
 /**
  * General accessibility, usability and navigation language file.
- * 
+ *
  * Most 'Alt' text and table summaries should go in the specific module language file.
  *
  * @copyright &copy; 2006 The Open University
  * @author N.D.Freear@open.ac.uk, and others
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
- * @package  
+ * @package
  */
 
 $string['access'] = 'Accessibility';
@@ -20,6 +20,7 @@ $string['monthnext'] = 'Next month';
 $string['monthprev'] = 'Previous month';
 $string['showhideblock'] = 'Show or hide block';
 $string['sitemap'] = 'Site map';
+$string['skipa'] = 'Skip $a';
 $string['skipblock'] = 'Skip block';
 $string['skipnavigation'] = 'Skip navigation';
 $string['tabledata'] = 'Data table, $a';
