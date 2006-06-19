72343793f6e24819d08196d69525186a19704ad9
toyomoyo <toyomoyo>
2006-06-19 03:29:15 +0000

    give IE a bit more time

--- blog/footer.php
+++ blog/footer.php
@@ -21,6 +21,17 @@ if (blocks_have_content($pageblocks, BLOCK_POS_RIGHT) || $editing) {
 
 <?php
 
+// Janne comment: Let's move this in here
+// so IE gets more time to load the
+// Page.
+
+if (isset($usehtmleditor) && $usehtmleditor) {
+    // Janne comment: there are two text fields in form
+    // so lets try to replace them both with
+    // HTMLArea editors
+    use_html_editor();
+}
+
 if (isset($course) && ($course->id)) {
     print_footer($course);
 } else {
