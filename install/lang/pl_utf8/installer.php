95e86170a2a3d04c8196f12d02f9cc28c2d31ea4
moodler <moodler>
2006-06-30 02:41:15 +0000

    Automatic installer.php lang files by installer_builder (20060630)

--- install/lang/pl_utf8/installer.php
+++ install/lang/pl_utf8/installer.php
@@ -11,26 +11,49 @@
 
 $string['admindirerror'] = 'Podany katalod admin jest nieprawidłowy';
 $string['admindirname'] = 'Katalog admin';
+$string['admindirsettingsub'] = 'Niektóre webhost’y używają / admin jako specjalne URL w celu akcesu do panela kontrolnego a także w innych celach. Niestety powoduje to konflikt ze standardową lokalizacją dla stron administratora w Moodle. Powyższy konflikt można naprawić zmieniając nazwę katalogu administratora podczas instalacji i wstawiając nową nazwę. Na przykład: 
+<br /> <br /><b>moodleadmin</b><br /> <br />
+Zostaną również poprawione linki administratora w Moodle';
+$string['bypassed'] = 'Ominięty';
+$string['cannotcreatelangdir'] = 'Nie można utworzyć katelogu do \"paczki językowej\" (lang)';
+$string['cannotcreatetempdir'] = 'Nie można utworzyć katalogu tymczasowego';
+$string['cannotdownloadcomponents'] = 'Nie można pobrać składowych (komponentów)';
+$string['cannotdownloadzipfile'] = 'Nie można pobrać spakowanego pliku (ZIP)';
+$string['cannotfindcomponent'] = 'Nie można znaleźć składowej (komponentu)';
+$string['cannotsavemd5file'] = 'Nie mozna zapisać pliku md5';
+$string['cannotsavezipfile'] = 'Nie można zapisać spakowanego pliku ZIP';
+$string['cannotunzipfile'] = 'Nie można rozpakować pliku ZIP';
 $string['caution'] = 'Ostrzeżenie';
+$string['check'] = 'Sprawdź';
+$string['chooselanguagehead'] = 'Wybierz język';
 $string['closewindow'] = 'Zamknij to okno';
+$string['compatibilitysettingshead'] = 'Sprawdzanie Twoich ustawień PHP';
+$string['componentisuptodate'] = 'Składowa (komponent) jest aktualny';
 $string['configfilenotwritten'] = 'Instalator nie mógł automatycznie utworzyć plik config.php zawierający Twoje parametry instalacyjne, prawdopodobnie dlatego że katalog Moodle nie ma prawa zapisu. Musisz ręcznie przekopiować poniższy kod do pliku config.php, który powinien znajdować się w głównym katalogu Moodle.';
 $string['configfilewritten'] = 'config.php został pomyślnie stworzony';
+$string['configurationcompletehead'] = 'Konfiguracja skończona';
 $string['continue'] = 'Kontynuuj';
 $string['database'] = 'Baza danych';
 $string['dataroot'] = 'Katalog z danymi';
 $string['datarooterror'] = 'Katalog z danymi który podałeś nie może być znaleziony lub utworzony. Popraw ścieżkę lub utwórz katalog ręcznie.';
 $string['dbconnectionerror'] = 'Nie można połączyć się z podaną bazą danych. Sprawdź ustawienia Twojej bazy danych.';
+$string['dbcreationerror'] = 'Błąd tworzenia bazy danych. Nie można stworzyć bazy danych o takiej nazwie z dostarczonymi ustawieniami';
 $string['dbhost'] = 'Serwer baz danych';
 $string['dbprefix'] = 'prefiksy tabel';
 $string['dbtype'] = 'Typ';
 $string['dirroot'] = 'Katalog Moodle';
 $string['dirrooterror'] = '\"Katalog Moodle\" wydaje się być nieprawidłowy - tam nie można znaleźć instalacji Moodle. Wartości poniżej zostaną usunięte.';
 $string['download'] = 'Pobierz';
+$string['downloadedfilecheckfailed'] = 'Pobierany plik jest uszkodzony';
+$string['downloadlanguagebutton'] = 'Pobierz &quot;$a&quot; pakiet językowy';
+$string['downloadlanguagehead'] = 'Pobierz pakiet językowy';
 $string['error'] = 'Błąd';
 $string['fail'] = 'zawieść';
 $string['fileuploads'] = 'Plik pobrany';
 $string['fileuploadserror'] = 'Powinno być włączone';
 $string['gdversion'] = 'versja biblioteki GD';
+$string['gdversionerror'] = 'Aby przeprowadzać operacje i kreować obrazy niezbędna jest biblioteka GD';
+$string['globalsquoteserror'] = 'Ustal swoje ustawienia PHP:  niedostępne register_globals i/lub udostępnione magic_quotes_gpc';
 $string['help'] = 'Pomoc';
 $string['info'] = 'Informacja';
 $string['installation'] = 'Instalacja';
@@ -38,9 +61,25 @@ $string['language'] = 'Język';
 $string['magicquotesruntime'] = 'Magic Quotes Runtime';
 $string['magicquotesruntimeerror'] = 'Powinno być wyłączone';
 $string['memorylimit'] = 'Ograniczenie pamięci';
+$string['memorylimiterror'] = '= Limit pamięci PHP jest ustawiony zbyt nisko ... może to powodować problemy w przyszłości';
+$string['memorylimithelp'] = 'Limit pamięci PHP dla Twojego serwera jest ustawiony obecnie na $a.</p>
+
+<p> Może to stworzyć sytuację, w której Moodle będzie miał w przyszłości problemy z pamięcią, zwłaszcza jeśli masz udostępnionych wiele modułów i/lub wielu użytkowników.</p>
+
+<p> Jeśli jest to możliwe, zalecamy ustawienie konfiguracji PHP z wyższym limitem, np. 16M.
+Istnieje kilka sposobów przeprowadzenia tej operacji, których możesz spróbować: </p>
+<ol>
+<li> Jeśli możesz przekompiluj PHP za pomocą <i>--enable-memory-limit</i>.
+Pozwoli to Moodle ustawić samoczynnie limit pamięci. </li>
+<li> Jeśli masz dostęp do swojego pliku php.ini, możesz w nim zmienić ustawienie <b>memory_limit</b> do wielkođci 16M. Jeśli nie posiadasz tego dostępu możesz poprosić swojego administratora aby zrobił to za Ciebie.</li>
+<li> Na niektórych serwerach PHP można stworzyć plik. htaccess w katalogu Moodle zawierający poniższą linię:
+<p><blockquote>php_value memory_limit 16M</blockquote></p>
+<p> jednakże na niektórych serwerach będzie uniemożliwiało to działanie <b> wszystkich </b> stron PHP (ujrzysz błędy na stronie), a więc będziesz zmuszony usunąć plik .htaccess. </p></li></ol>”';
+$string['mysqlextensionisnotpresentinphp'] = 'PHP nie został prawidłowo skonfigurowany z rozszerzeniem MySQL, co może zostać zakomunikowane przez MySQL. Sprawdź proszę swój plik php.ini lub przekompiluj PHP';
 $string['name'] = 'Nazwa';
 $string['next'] = 'Następne';
 $string['ok'] = 'OK.';
+$string['parentlanguage'] = '<<DO TŁUMACZY: Jeżeli Twój język ma Język nadrzędny który powinien być użyty w przypadku braku tlumaczenia, wpisz tutaj odpowiedni kod (np: nl). Pozostawienia pustego powoduje używanie języka angielskiego.';
 $string['password'] = 'Hasło';
 $string['phpversion'] = 'wersja PHP';
 $string['phpversionerror'] = 'Wersja PHP musi być ca najmniej 4.1.0';
@@ -48,6 +87,7 @@ $string['phpversionhelp'] = '<p> Moodle wymaga wersji PHP co najmniej 4.1.0. </p
 <p>Obecnie jest uruchomiona wersja $a</p>
 <p> Musisz uaktualnić wersje PHP lub przenieść na host z nowszą wersją PHP!</p>';
 $string['previous'] = 'Poprzedni';
+$string['report'] = 'Raport ';
 $string['safemode'] = 'Bezpieczny tryb';
 $string['safemodeerror'] = 'Moodle ma trudności z włączeniem bezpiecznego trybu';
 $string['sessionautostarterror'] = 'To powinno być wyłączone';
