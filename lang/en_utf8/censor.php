39c18c39d122ab017b282f66cf8a00a8003e8348
thepurpleblob <thepurpleblob>
2006-07-06 08:36:38 +0000

    Bug #4838 - removing spurious blank line at end

--- lang/en_utf8/censor.php
+++ lang/en_utf8/censor.php
@@ -4,4 +4,3 @@ $string['filtername'] = "Word Censorship";
 $string['badwords'] = 'shit,fucked,fucker,fuck,dickhead, dick,cockhead,cock,cunt,asshole,arsehole,prick,bitch, jism,whore,slut,wanker, wank,bastard,dildo,masturbate, orgasm,penis,nigger, pussy,vagina';
 
 ?>
-
