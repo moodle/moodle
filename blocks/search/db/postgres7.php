e0eb89a13ef19dd83ada6fe4e74cc2c2ef1b32a4
mchampan <mchampan>
2006-07-11 16:08:47 +0000

    Initial commit

---
+++ blocks/search/db/postgres7.php
@@ -0,0 +1,33 @@
+<?php
+//
+// This file keeps track of upgrades to Moodle's
+// blocks system.
+//
+// Sometimes, changes between versions involve
+// alterations to database structures and other
+// major things that may break installations.
+//
+// The upgrade function in this file will attempt
+// to perform all the necessary actions to upgrade
+// your older installtion to the current version.
+//
+// If there's something it cannot do itself, it
+// will tell you what you need to do.
+//
+// Versions are defined by backup_version.php
+//
+// This file is tailored to pgsql
+
+function search_upgrade($oldversion=0) {
+
+    global $CFG;
+    
+    $result = true;
+
+    if ($oldversion < 2006062500 and $result) {
+        $result = true; //Nothing to do
+    }
+
+    //Finally, return result
+    return $result;
+}
