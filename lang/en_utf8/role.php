4ed70f1b6e7b8dc41e1b795b376100810c9bdce1
moodler <moodler>
2006-08-09 05:35:55 +0000

    Some new strings

---
+++ lang/en_utf8/role.php
@@ -0,0 +1,11 @@
+<?php // $Id$ 
+      // role.php 
+
+$string['assignroles'] = 'Assign roles';
+$string['currentrole'] = 'Current role';
+$string['currentcontext'] = 'Current context';
+$string['existingusers'] = '$a existing users';
+$string['overrideroles'] = 'Override roles';
+$string['potentialusers'] = '$a potential users';
+
+?>
