c853b39f502a6c77a69d122ca9c787b56d275fff
skodak <skodak>
2006-08-09 18:36:54 +0000

    Bug #6225 - Moodle bug #4658 Breaks assignment type: Upload Single; merged from MOODLE_16_STABLE

--- mod/assignment/lib.php
+++ mod/assignment/lib.php
@@ -1494,7 +1494,7 @@ class assignment_base {
 
         $filearea = $this->file_area_name($userid);
 
-        if ( is_dir($filearea) && $basedir = $this->file_area($userid)) {
+        if ( is_dir($CFG->dataroot.'/'.$filearea) && $basedir = $this->file_area($userid)) {
             if ($files = get_directory_list($basedir)) {
                 return count($files);
             }
