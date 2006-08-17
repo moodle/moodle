1bf63d33c875b8c14174407f46ea9be4e6c9f8b5
stronk7 <stronk7>
2006-08-17 19:40:49 +0000

    Oracle automatically builds UIXs for UKs

--- lib/xmldb/classes/generators/oci8po/oci8po.class.php
+++ lib/xmldb/classes/generators/oci8po/oci8po.class.php
@@ -42,7 +42,7 @@ class XMLDBoci8po extends XMLDBgenerator {
     var $foreign_keys = false; // Does the generator build foreign keys
 
     var $primary_index = false;// Does the generator need to build one index for primary keys
-    var $unique_index = true;  // Does the generator need to build one index for unique keys
+    var $unique_index = false;  // Does the generator need to build one index for unique keys
     var $foreign_index = true; // Does the generator need to build one index for foreign keys
 
     var $sequence_extra_code = true; //Does the generator need to add extra code to generate the sequence fields
