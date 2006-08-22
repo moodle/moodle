a2b0c57b0a59018c309c39df4edaec38fe1015c2
skodak <skodak>
2006-08-22 22:25:30 +0000

    debug off level is now 5 instead of 7 to prevent E_WARNING on production sites SC#294; merged from MOODLE_16_STABLE

--- lib/setup.php
+++ lib/setup.php
@@ -221,7 +221,7 @@ global $HTTPSPAGEREQUIRED;
 
 /// Set error reporting back to normal
     if (!isset($CFG->debug)) { // empty() would override 0 or false from config.php
-        $CFG->debug = 7;
+        $CFG->debug = 5;
     }
     error_reporting($CFG->debug);
 
