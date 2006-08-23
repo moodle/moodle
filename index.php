0cfb6a52fb74ddb0532654770f79c79b8b3a2f62
moodler <moodler>
2006-08-23 08:39:15 +0000

    Adding a copyright notice, and mentioning MDL-3717 purely as a test of the new CVS integration on tracker.moodle.org

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php  // $Id$
        // index.php - the front page.
 
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//          http://moodle.org                                            //
+//                                                                       //
+// Copyright (C) 1999-2999  Martin Dougiamas  http://moodle.com          //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+
     if (!file_exists('./config.php')) {
         header('Location: install.php');
         die;
@@ -193,7 +217,6 @@
     }
 
     echo '</td>';
-
 
     // The right column
     if (blocks_have_content($pageblocks, BLOCK_POS_RIGHT) || $editing || isadmin()) {
