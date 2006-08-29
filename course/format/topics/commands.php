d63545d8e86f52e704936fbbe786a4deaa4b07cd
cap2501 <cap2501>
2006-08-29 02:34:35 +0000

    Fixed errors in refactoring ( rev4, almost there)

--- course/format/topics/commands.php
+++ course/format/topics/commands.php
@@ -15,7 +15,7 @@ $value      = optional_param('value', 0, PARAM_INT);
 $column     = optional_param('column', 0, PARAM_ALPHA);
 $id         = optional_param('id', 0, PARAM_INT);
 $summary    = optional_param('summary', '', PARAM_ALPHA);
-$sequence   = optional_param('sequence', '', PARAM_ALPHANUM);
+$sequence   = optional_param('sequence', '', PARAM_SEQUENCE);
 $visible    = optional_param('visible', 0, PARAM_INT);
 
 // Authorise the user and verify some incoming data
