15ad37459ad5947a5503c3e4cbda8f405f00a7ec
skodak <skodak>
2006-09-03 19:13:53 +0000

    fixed language string

--- admin/settings/misc.php
+++ admin/settings/misc.php
@@ -2,7 +2,7 @@
 
 // * Miscellaneous settings (still to be sorted)
 
-$ADMIN->add('misc', new admin_externalpage('stickyblocks', get_string('stickyblocks'), "$CFG->wwwroot/$CFG->admin/stickyblocks.php"));
+$ADMIN->add('misc', new admin_externalpage('stickyblocks', get_string('stickyblocks', 'admin'), "$CFG->wwwroot/$CFG->admin/stickyblocks.php"));
 
 $ADMIN->add('misc', new admin_externalpage('xmldbeditor', get_string('xmldbeditor'), "$CFG->wwwroot/$CFG->admin/xmldb/"));
 
