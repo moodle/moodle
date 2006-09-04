dd46fc5daaa3e34cb259909c83c84e0e2145aae4
moodler <moodler>
2006-09-04 08:15:48 +0000

    Just some small doc changes

--- lib/pdflib.php
+++ lib/pdflib.php
@@ -1,4 +1,4 @@
-<?php
+<?php  // $Id$
 
 ///////////////////////////////////////////////////////////////////////////
 //                                                                       //
@@ -7,7 +7,7 @@
 // Moodle - Modular Object-Oriented Dynamic Learning Environment         //
 //          http://moodle.org                                            //
 //                                                                       //
-// Copyright (C) 1999-2004  Martin Dougiamas  http://dougiamas.com       //
+// Copyright (C) 1999-2999  Martin Dougiamas  http://moodle.com          //
 //                                                                       //
 // This program is free software; you can redistribute it and/or modify  //
 // it under the terms of the GNU General Public License as published by  //
