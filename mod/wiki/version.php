0f9602085bec20dc189974bcb99b92cdc98c6398
moodler <moodler>
2006-09-12 08:22:14 +0000

    Small bump for guest role fix

--- mod/wiki/version.php
+++ mod/wiki/version.php
@@ -5,7 +5,7 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2006091200;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2006091201;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2006080900;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
