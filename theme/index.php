b88b740a802b09add1b5b3363d28f3ce394f6556
moodler <moodler>
2006-09-14 03:35:19 +0000

    Remove obsolete themes link  MDL-6518

--- theme/index.php
+++ theme/index.php
@@ -127,11 +127,7 @@
     }
     echo "</table>";
 
-    echo "<br /><div align=\"center\">";
-    $options["frame"] = "developer.html";
-    $options["sub"] = "themes";
-    print_single_button("$CFG->wwwroot/doc/index.php", $options, get_string("howtomakethemes"));
-    echo "</div>";
+
     admin_externalpage_print_footer($adminroot);
 
 
