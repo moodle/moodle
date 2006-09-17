4b7aaf4025ef1f7a0d4bbffd8265fd9f210da092
skodak <skodak>
2006-09-17 21:03:35 +0000

    added new capability 'moodle/course:managemetacourses'

--- lib/db/access.php
+++ lib/db/access.php
@@ -562,6 +562,20 @@ $moodle_capabilities = array(
         )
     ),
     
+    'moodle/course:managemetacourses' => array(
+    
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'legacy' => array(
+            'guest' => CAP_PREVENT,
+            'student' => CAP_PREVENT,
+            'teacher' => CAP_PREVENT,
+            'editingteacher' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW,
+            'admin' => CAP_ALLOW
+        )
+    ),
+    
     'moodle/course:activityvisibility' => array(
     
         'captype' => 'write',
