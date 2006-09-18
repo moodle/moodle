bd574b2a065d3b7fdfa40f5a928d99f7d971de2a
vyshane <vyshane>
2006-09-18 05:29:42 +0000

    Removed isteacher()

--- user/tabs.php
+++ user/tabs.php
@@ -34,9 +34,7 @@
         print_heading($site->fullname);
         
         if ($CFG->bloglevel >= 4) {
-            if (isteacher(SITEID) || 
-                ( $CFG->showsiteparticipants == 1
-                        && has_capability('moodle/course:viewparticipants', get_context_instance(CONTEXT_SYSTEM, SITEID)) ) {
+            if (has_capability('moodle/course:viewparticipants', get_context_instance(CONTEXT_SYSTEM, SITEID))) {
                 $toprow[] = new tabobject('participants', $CFG->wwwroot.'/user/index.php?id='.SITEID,
                     get_string('participants'));
             }
