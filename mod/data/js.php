b8fb348277bc29e3e0a1f4aec5367f0786c161f1
tomflannaghan <tomflannaghan>
2006-09-18 11:45:12 +0000

    Adds Javascript Support for the presets.

---
+++ mod/data/js.php
@@ -0,0 +1,40 @@
+<?php
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//          http://moodle.org                                            //
+//                                                                       //
+// Copyright (C) 2005 Martin Dougiamas  http://dougiamas.com             //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+    $lifetime  = 600;                                   // Seconds to cache this stylesheet
+    $nomoodlecookie = true;                             // Cookies prevent caching, so don't use them
+
+    require_once('../../config.php');
+
+    $d = optional_param('d', 0, PARAM_INT);   // database id
+
+    if ($data = get_record('data', 'id', $d)) {
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT');
+        header('Expires: ' . gmdate("D, d M Y H:i:s", time() + $lifetime) . ' GMT');
+        header('Cache-control: max_age = '. $lifetime);
+        header('Pragma: ');
+        header('Content-type: text/css');  // Correct MIME type
+
+        echo $data->jstemplate;
+    } 
