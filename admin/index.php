bd2b8ddecd70a3081d777fa5ff3cca60ec8e15c8
mjollnir_ <mjollnir_>
2006-09-22 10:00:34 +0000

    moved call to upgrade stats until after all the roles stuff has been set up

--- admin/index.php
+++ admin/index.php
@@ -247,14 +247,14 @@
                         // Upgrade to the roles system.
                         moodle_install_roles();
                     }
-                    require_once($CFG->libdir.'/statslib.php');
-                    if (!stats_upgrade_for_roles_wrapper()) {
-                        notify('Couldn\'t upgrade the stats tables to use the new roles system');
-                    }
                     if (!update_capabilities()) {
                         error('Had trouble upgrading the core capabilities for the Roles System');
                     } else {
                         set_config('rolesactive', 1);
+                    }
+                    require_once($CFG->libdir.'/statslib.php');
+                    if (!stats_upgrade_for_roles_wrapper()) {
+                        notify('Couldn\'t upgrade the stats tables to use the new roles system');
                     }
                     if (set_config("version", $version)) {
                         remove_dir($CFG->dataroot . '/cache', true); // flush cache
