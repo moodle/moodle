d249e478d4973c9ef5e7992da00639309da6f853
skodak <skodak>
2006-09-25 17:44:43 +0000

    fixed hidden misc sectionin admin tree block, sorry

--- admin/settings/top.php
+++ admin/settings/top.php
@@ -24,7 +24,7 @@ foreach(get_list_of_plugins('admin/report') as $plugin) {
     $ADMIN->add('reports', new admin_externalpage('report' . $plugin, get_string($plugin, 'admin'), "$CFG->wwwroot/$CFG->admin/report/$plugin/index.php"));
 }
 
-$ADMIN->add('root', new admin_category('misc', get_string('miscellaneous'), 999));
+$ADMIN->add('root', new admin_category('misc', get_string('miscellaneous')));
 
 
 ?>
