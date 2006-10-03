c8f1d1ca37358d9bc2c0c9273c33b0dcba53a356
moodler <moodler>
2006-10-03 01:44:15 +0000

    Added extra conditions to loops to prevent infinite loops
    ref the same fix made for daily loops already:
    
       http://moodle.cvs.sourceforge.net/moodle/moodle/lib/statslib.php?r1=1.21.2.4&r2=1.21.2.5

--- lib/statslib.php
+++ lib/statslib.php
@@ -238,7 +238,7 @@ function stats_cron_weekly () {
     
     $weeks = 0;
     mtrace("starting at $timestart");
-    while ($sunday >= $nextsunday) {
+    while ($sunday > $nextsunday && $timestart < $nextsunday) {
 
         $timesql = " (timeend > $timestart AND timeend < $nextsunday) ";
         begin_sql();
@@ -347,7 +347,7 @@ function stats_cron_monthly () {
     
     $months = 0;
     mtrace("starting from $timestart");
-    while ($monthend >= $nextmonthend) {
+    while ($monthend > $nextmonthend && $timestart < $nextmonthend) {
 
         $timesql = " (timeend > $timestart AND timeend < $nextmonthend) ";
         begin_sql();
