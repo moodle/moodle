921a34d61b0880d780a3a72a2ba62b3bdb3754b9
moodler <moodler>
2006-10-03 12:51:24 +0000

    Added copyright notice (they paid moodle.com for this development)
       http://moodle.org/mod/forum/discuss.php?d=55410

--- mod/quiz/protect_js.php
+++ mod/quiz/protect_js.php
@@ -1,4 +1,22 @@
-<?php defined('MOODLE_INTERNAL') or die('Direct access to this script is forbidden.'); ?>
+<?php defined('MOODLE_INTERNAL') or die('Direct access to this script is forbidden.'); 
+
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Copyright (C) 2004  Education Promoting Center, Yamaguchi University  //
+//                     http://www.epc.yamaguchi-u.ac.jp                  //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+?>
 
 <style media="print">body {display:none}</style>
 
