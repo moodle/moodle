718647bcb1b9b416b8280a578ab88f43fb4323f5
vyshane <vyshane>
2006-10-05 03:14:20 +0000

    Fixes for MDL-2583

--- mod/forum/rate.php
+++ mod/forum/rate.php
@@ -39,7 +39,10 @@
             $postid = (int)$postid;
             $lastpostid = $postid;
 
-            if ($oldrating = get_record("forum_ratings", "userid", $USER->id, "post", $postid)) {
+            if ($rating == FORUM_UNSET_POST_RATING) {
+                delete_records('forum_ratings', 'post', $postid, 'userid', $USER->id);
+
+            } else if ($oldrating = get_record("forum_ratings", "userid", $USER->id, "post", $postid)) {
                 if ($rating != $oldrating->rating) {
                     $oldrating->rating = $rating;
                     $oldrating->time = time();
@@ -47,7 +50,7 @@
                         error("Could not update an old rating ($postid = $rating)");
                     }
                 }
-            } else if ($rating) {
+            } else {
                 unset($newrating);
                 $newrating->userid = $USER->id;
                 $newrating->time = time();
