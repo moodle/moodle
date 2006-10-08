33f0ea4178497bb8405b8aa7498b4a02c709d1cc
skodak <skodak>
2006-10-08 08:31:12 +0000

    fixed encoding header() in messaging frames MDL-6128 - 1.6, special chars and messaging system; backported from HEAD

--- message/messages.php
+++ message/messages.php
@@ -12,6 +12,7 @@
         $direction = ' dir="ltr"';
     }
 /// Output the header
+    @header('Content-Type: text/html; charset='.$encoding);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html <?php echo $direction ?>>
