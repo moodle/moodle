0c64299b013db6f88cf729849ded3083dc9e20cc
stronk7 <stronk7>
2006-10-25 23:26:54 +0000

    First model of new upgrade.php scripts and comments in old ones. Need validation before extending them everywhere across core. MDL-7214

--- mod/glossary/db/postgres7.php
+++ mod/glossary/db/postgres7.php
@@ -1,5 +1,10 @@
 <?php  // $Id$
 
+// THIS FILE IS DEPRECATED!  PLEASE DO NOT MAKE CHANGES TO IT!
+//
+// IT IS USED ONLY FOR UPGRADES FROM BEFORE MOODLE 1.7, ALL 
+// LATER CHANGES SHOULD USE upgrade.php IN THIS DIRECTORY.
+
 function glossary_upgrade($oldversion) {
 /// This function does anything necessary to upgrade 
 /// older versions to match current functionality 
@@ -203,6 +208,8 @@ function glossary_upgrade($oldversion) {
     if ($oldversion < 2006090400) {
         table_column('glossary_comments', 'comment', 'entrycomment', 'text', '', '', '');
     }
+
+    //////  DO NOT ADD NEW THINGS HERE!!  USE upgrade.php and the lib/ddllib.php functions.
 
     return true;
 }
