ecfeb03a72617c78520ae0a2c81abca42a3c1611
stronk7 <stronk7>
2006-10-25 23:29:44 +0000

    First model of new upgrade.php scripts and comments in old ones. Need validation before extending them everywhere across core. MDL-7214
    Merged from MOODLE_17_STABLE

--- mod/glossary/db/mysql.php
+++ mod/glossary/db/mysql.php
@@ -1,6 +1,9 @@
 <?php   // $Id$
 
-
+// THIS FILE IS DEPRECATED!  PLEASE DO NOT MAKE CHANGES TO IT!
+//
+// IT IS USED ONLY FOR UPGRADES FROM BEFORE MOODLE 1.7, ALL 
+// LATER CHANGES SHOULD USE upgrade.php IN THIS DIRECTORY.
 
 function glossary_upgrade($oldversion) {
 /// This function does anything necessary to upgrade
@@ -439,6 +442,8 @@ function glossary_upgrade($oldversion) {
     if ($oldversion < 2006090400) {
          table_column('glossary_comments', 'comment', 'entrycomment', 'text', '', '', '');
     }
+
+    //////  DO NOT ADD NEW THINGS HERE!!  USE upgrade.php and the lib/ddllib.php functions.
 
     return true;
 }
