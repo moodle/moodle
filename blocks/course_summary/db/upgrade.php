d7a7184730b1e5db2c959d2ca1cc37e5c8361a23
stronk7 <stronk7>
2006-10-26 16:33:49 +0000

    New upgrade.php file added for this module. MDL-7214

---
+++ blocks/course_summary/db/upgrade.php
@@ -0,0 +1,38 @@
+<?php  //$Id$
+
+// This file keeps track of upgrades to 
+// the course_summary block
+//
+// Sometimes, changes between versions involve
+// alterations to database structures and other
+// major things that may break installations.
+//
+// The upgrade function in this file will attempt
+// to perform all the necessary actions to upgrade
+// your older installtion to the current version.
+//
+// If there's something it cannot do itself, it
+// will tell you what you need to do.
+//
+// The commands in here will all be database-neutral,
+// using the functions defined in lib/ddllib.php
+
+function xmldb_block_course_summary_upgrade($oldversion=0) {
+
+    global $CFG, $THEME, $db;
+
+    $result = true;
+
+/// And upgrade begins here. For each one, you'll need one 
+/// block of code similar to the next one. Please, delete 
+/// this comment lines once this file start handling proper
+/// upgrade code.
+
+/// if ($result && $oldversion < YYYYMMDD00) { //New version in version.php
+///     $result = result of "/lib/ddllib.php" function calls
+/// }
+
+    return $result;
+}
+
+?>
