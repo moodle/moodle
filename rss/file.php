a015506a14e814ef2612b851da73c82fa2d7320f
skodak <skodak>
2006-11-01 21:03:09 +0000

    hack for problems with concurrent use of $nomoodlecookie and capabilities in RSS feeds, see MDL-7243 - it should be replaced once we get to codes in urls; merged from MOODLE_17_STABLE

--- rss/file.php
+++ rss/file.php
@@ -21,6 +21,11 @@
 
     $lifetime = 3600;  // Seconds for files to remain in caches - 1 hour
 
+    // hack for problems with concurrent use of $nomoodlecookie and capabilities MDL-7243
+    // it should be replaced once we get to codes in urls 
+    $USER = new object();
+    $USER->id = 0;
+
     // disable moodle specific debug messages
     disable_debugging();
 
