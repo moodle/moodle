5326e5473c2648f99051a318a8e8b6d95b7f273f
nfreear <nfreear>
2006-11-07 17:29:14 +0000

    Fixed $maxbytes warning (Bug MDL-7380 "groups".)

--- group/groupui/index.php
+++ group/groupui/index.php
@@ -1,9 +1,12 @@
 <?php // $Id$
-
-/**********************************************
- * The main group management user interface
- ************************************************/
-
+/**
+ * The main group management user interface.
+ *
+ * @copyright &copy; 2006 The Open University
+ * @author J.White AT open.ac.uk 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package groups
+ */
 require_once('../../config.php');
 require_once('../lib/lib.php');
 //require_once('../../course/lib.php');
@@ -32,14 +35,14 @@ if (!$error) {
 
 	// Set the session key so we can check this later
 	$sesskey = !empty($USER->id) ? $USER->sesskey : '';
-	
-	if (!empty($CFG->gdversion) and $maxbytes) {
+
+	if (!empty($CFG->gdversion)) { //TODO: and $maxbytes)
 		$printuploadpicture = true;
 	} else {
 		$printuploadpicture = false;
 	}
-		
-	
+
+
 	$maxbytes = get_max_upload_file_size($CFG->maxbytes, $course->maxbytes);
 	$strgroups = get_string('groups');
 	$strparticipants = get_string('participants');
@@ -49,11 +52,10 @@ if (!$error) {
 	             "<a href=\"$CFG->wwwroot/course/view.php?id=$courseid\">$course->shortname</a> ".
 	             "-> <a href=\"$CFG->wwwroot/user/index.php?id=$courseid\">$strparticipants</a> ".
 	             "-> $strgroups", "", "", true, '', user_login_string($course, $USER));
-	             
-	require_once('form.html');
-	
-	print_footer($course);
 
+	require_once('form.html');
+
+	print_footer($course);
 }
 
 ?>
