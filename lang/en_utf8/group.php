99de75d7e2c1a14d85dd9dca026fa868f81dd0ef
nfreear <nfreear>
2006-11-07 14:46:08 +0000

    Initial commit for Bug MDL-7380 "groups" - Juliette White's code. NOTE, for the moment the existing code/tables will be used by default.

---
+++ lang/en_utf8/group.php
@@ -0,0 +1,85 @@
+<?php
+
+$string['groupings'] = 'Groupings';
+$string['grouping'] = 'Grouping';
+$string['groups'] = 'Groups';
+$string['group'] = 'Group';
+$string['groupsinselectedgrouping'] = 'Groups in selected grouping';
+$string['membersofselectedgroup'] = 'Members of selected group';
+
+$string['editgroupingsettings'] = 'Edit grouping settings';
+$string['editgroupingpermissions'] = 'Edit grouping permissions';
+$string['deletegrouping'] = 'Delete grouping';
+$string['creategrouping'] = 'Create grouping';
+$string['createautomaticgrouping'] = 'Create automatic grouping';
+$string['printerfriendly'] = 'Printer-friendly display of grouping';
+
+$string['editgroupsettings'] = 'Edit group settings';
+$string['deleteselectedgroup'] = 'Delete selected group';
+$string['removegroupfromselectedgrouping'] = 'Remove group from selected grouping';
+$string['creategroupinselectedgrouping'] = 'Create group in selected grouping';
+$string['addexistinggroupstogrouping'] = 'Add existing groups to grouping';
+$string['addgroupstogrouping'] = 'Add groups to grouping';
+
+$string['removeselectedusers'] = 'Remove selected users';
+$string['adduserstogroup'] = 'Add users to group';
+
+$string['groupingname'] = 'Grouping name';
+$string['defaultgroupingname'] = 'Grouping';
+$string['groupingdescription'] = 'Grouping description';
+$string['prefixforgroupnames'] = 'Prefix for group names';
+$string['defaultgroupprefix'] = 'Group ';
+$string['distributealphabetically'] = 'Distribute into groups alphabetically by last name';
+$string['usestudentsonlyfromselectedgroup'] = 'Only use students from currently selected group';
+$string['selectnumberineachgroup'] = 'Select number in each group';
+$string['numberofstudents'] = 'Number of students';
+$string['distributeevenly'] = 'Distribute leftover students evenly';
+$string['selectnumberofgroups'] = 'Select number of groups';
+$string['numberofgroups'] = 'Number of groups';
+$string['creategrouping'] = 'Create grouping';
+$string['creategroup'] = 'Create group';
+
+$string['groupname'] = 'Group name';
+$string['defaultgroupname'] = 'Group';
+$string['groupdescription'] = 'Group description';
+$string['enrolmentkey'] = 'Enrolment key';
+$string['hidepicture'] = 'Hide picture';
+$string['newpicture'] = 'New picture';
+
+$string['defaultgroupdescription'] = 'Default group description';
+
+$string['displaygrouping'] = 'Display grouping';
+$string['addgroupstogrouping'] = 'Add groups to grouping';
+$string['showusersalreadyingroup'] = 'Show users already in a group in the grouping';
+
+$string ['save'] = 'Save';
+$string['cancel'] = 'Cancel';
+
+
+$string['groupfor'] = "for group";
+$string['groupinfo'] = 'Info about selected group';
+$string['groupinfomembers'] = 'Info about selected members';
+$string['groupinfopeople'] = 'Info about selected people';
+$string['groupmembers'] = 'Group members';
+$string['groupmemberssee'] = 'See group members';
+$string['groupmembersselected'] = 'Members of selected group';
+
+
+$string['javascriptrequired'] = 'This page requires Javascript to be enabled.';
+
+$string['viewowngroup'] = 'Students may view the members of any groups to which they belong';
+$string['viewallgroupsmembers'] = 'Students may view the members of all groups';
+$string['viewallgroupsactivities'] = 'Students may view but not contribute to activities for all groups';
+$string['teachersgroupmark'] = 'Teachers may not mark activities for groups to which they do not belong';
+$string['teachersgroupview'] = 'Teachers may not view activities for groups to which they do not belong';
+$string['teachersoverride'] = 'Teachers not in any group may view and mark activities for all groups (overrides the above)';
+
+$string['groupmode'] = 'Group mode';
+$string['groupmodeforce'] = 'Force group mode';
+$string['groupmy'] = 'My group';
+$string['groupnotamember'] = 'Sorry, you are not a member of that group';
+$string['groupsnone'] = 'No groups';
+$string['groupsseparate'] = 'Separate groups';
+$string['groupsvisible'] = 'Visible groups';
+
+?>
