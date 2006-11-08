89c5cbc5256edc0c3930d54a253f3c051919568c
koenr <koenr>
2006-11-08 00:02:10 +0000

    MDL-7379 typo

--- lang/en_utf8/admin.php
+++ lang/en_utf8/admin.php
@@ -428,7 +428,7 @@ $string['timezoneisforcedto'] = 'Force all users to use';
 $string['timezonenotforced'] = 'Users can choose their own timezone';
 $string['unbookmarkthispage'] = 'unbookmark this page';
 $string['unicodeupgradenotice'] = 'In Moodle 1.6 we have migrated all languages to Unicode.  To complete the upgrade for this site, you need to convert all the data in your database to Unicode (UTF-8) using our migration script.  <a href=\"utfdbmigrate.php\">Click here to run the migration script now</a>!';
-$string['unicoderecommended'] = 'Storing all your data in Unicode (UTF=8) is recommended. New installations should be performed into databases that have their default character set as Unicode.  If you are upgrading, you should perform the UTF-8 migration process (see the Admin page).';
+$string['unicoderecommended'] = 'Storing all your data in Unicode (UTF-8) is recommended. New installations should be performed into databases that have their default character set as Unicode.  If you are upgrading, you should perform the UTF-8 migration process (see the Admin page).';
 $string['unicoderequired'] = 'It is required that you store all your data in Unicode format (UTF-8). New installations must be performed into databases that have their default character set as Unicode.  If you are upgrading, you should perform the UTF-8 migration process (see the Admin page).';
 $string['uninstall'] = 'Uninstall selected language pack';
 $string['uninstallconfirm'] = 'You are about to completely uninstall language pack $a, are you sure?';
