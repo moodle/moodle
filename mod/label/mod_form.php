630371c03ab2f847e3f1e4cc571c31bc80cf6af8
jamiesensei <jamiesensei>
2006-11-13 09:20:42 +0000

    *** empty log message ***

---
+++ mod/label/mod_form.php
@@ -0,0 +1,26 @@
+<?php // $Id$
+require_once ($CFG->libdir.'/formslib.php');
+class label_mod_form extends moodleform_mod {
+
+	function definition() {
+
+		$mform    =& $this->_form;
+		$renderer =& $mform->defaultRenderer();
+
+		$mform->addElement('htmleditor', 'content', get_string('labeltext', 'label'));
+		$mform->setType('content', PARAM_RAW);
+		$mform->addRule('content', get_string('required'), 'required', null, 'client');
+
+		$this->standard_hidden_coursemodule_elements();
+
+        $mform->addElement('modvisible', 'visible', get_string('visible'));
+
+        $buttonarray=array();
+        $buttonarray[] = &MoodleQuickForm::createElement('submit', 'submit', get_string('savechanges'));
+        $buttonarray[] = &MoodleQuickForm::createElement('submit', 'cancel', get_string('cancel'));
+        $mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
+		$renderer->addStopFieldsetElements('buttonar');
+	}
+
+}
+?>
