c5c1ec895b754074d3f04190691f6181650be6a9
moodler <moodler>
2006-12-06 02:24:18 +0000

    Non-editing teachers should NOT have course import capability by default

--- lib/db/access.php
+++ lib/db/access.php
@@ -133,7 +133,6 @@ $moodle_capabilities = array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'legacy' => array(
-            'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'coursecreator' => CAP_ALLOW,
             'admin' => CAP_ALLOW
