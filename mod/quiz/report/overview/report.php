da4c604c9d5d6cb79d3091f13be3cc0d7a9aa460
tjhunt <tjhunt>
2006-12-11 14:03:04 +0000

    MDL-7886 - typo in overview report javascript.

--- mod/quiz/report/overview/report.php
+++ mod/quiz/report/overview/report.php
@@ -442,7 +442,7 @@ class quiz_report extends quiz_default_report {
     /// Start form
 
                 echo '<div id="tablecontainer">';
-                echo '<form id="attemptsform" method="post" action="report.php" onsubmit="var menu = document.getElementById(\'menuaction\'); return (menu.options[menu.selectedIndex].value == \'delete\' ? \''.$strreallydel.'\' : true);">';
+                echo '<form id="attemptsform" method="post" action="report.php" onsubmit="var menu = document.getElementById(\'menuaction\'); return (menu.options[menu.selectedIndex].value == \'delete\' ? confirm(\''.$strreallydel.'\') : true);">';
                 echo '<input type="hidden" name="id" value="'.$cm->id.'" />';
                 echo '<input type="hidden" name="mode" value="overview" />';
 
