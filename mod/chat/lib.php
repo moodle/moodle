dd2d45a87890644f83ca5484785a44eb5de9fba6
skodak <skodak>
2006-12-30 21:10:23 +0000

    MDL-8029 obsoleted ob_end_clean() emulation removed from chat

--- mod/chat/lib.php
+++ mod/chat/lib.php
@@ -644,19 +644,6 @@ function chat_format_message($message, $courseid, $currentuser, $chat_lastrow=NU
     return chat_format_message_manually($message, $courseid, $user, $currentuser, $chat_lastrow);
 }
 
-if (!function_exists('ob_get_clean')) {
-/// Compatibility function for PHP < 4.3.0
-    function ob_get_clean() {
-        $cont = ob_get_contents();
-        if ($cont !== false) {
-            ob_end_clean();
-            return $cont;
-        } else {
-            return $cont;
-        }
-    }
-}
-
 function chat_get_view_actions() {
     return array('view','view all','report');
 }
