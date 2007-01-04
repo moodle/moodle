b68da2763356cccf893869bf897f93b5dba81c13
moodler <moodler>
2007-01-04 15:53:35 +0000

    Bending over for Strict   MDL-7861

--- iplookup/hostip/lib.php
+++ iplookup/hostip/lib.php
@@ -13,7 +13,7 @@ function iplookup_display($ip, $user=0) {
     echo 'Search results: <iframe src="http://www.hostip.info/api/get.html?ip='.$ip.'" height="60" width="300"></iframe>';
 
     echo '<object data="http://www.hostip.info/map/frame.html?ip='.$ip.'" '.
-         'type="text/html" border="0" width="610" height="330" hspace="0" vspace="0"></object>';
+         'type="text/html" border="0" width="610" height="330"></object>';
 
     close_window_button();
     print_footer('none');
