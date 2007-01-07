490d89e591e12b5dc602437b94a9b1dd8d73df5e
jamiesensei <jamiesensei>
2007-01-07 17:22:31 +0000

    removing more redundant files after qtype form formslib migration

--- question/type/random/editquestion.php
+++
@@ -1,9 +0,0 @@
-<?php // $Id$
-
-    if (empty($question->name)) {
-        $question->name = get_string("random", "quiz");
-    }
-    
-    print_heading_with_help(get_string("editingrandom", "quiz"), "random", "quiz");
-    require("$CFG->dirroot/question/type/random/editquestion.html");
-?>
