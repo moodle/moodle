cbcde5d8af7727fb7bfd19653da29a4fdc58e6fe
stronk7 <stronk7>
2007-01-08 18:33:03 +0000

    Adding initial support for mysqli databases. MDL-7967

---
+++ lib/xmldb/classes/generators/mysqli/mysqli.class.php
@@ -0,0 +1,43 @@
+<?php // $Id$
+
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//          http://moodle.com                                            //
+//                                                                       //
+// Copyright (C) 2001-3001 Martin Dougiamas        http://dougiamas.com  //
+//           (C) 2001-3001 Eloy Lafuente (stronk7) http://contiento.com  //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+/// This class generate SQL code to be used against MySQL
+/// when running over mysqli (improved). As DB is the same, this inherits
+/// everything from XMLDBmysql
+
+require_once($CFG->libdir . '/xmldb/classes/generators/mysql/mysql.class.php');
+
+class XMLDBmysqli extends XMLDBmysql {
+
+    /**
+     * Creates one new XMLDBmysql
+     */
+    function XMLDBmysqli() {
+        XMLDBmysql::XMLDBmysql();
+    }
+}
+
+?>
