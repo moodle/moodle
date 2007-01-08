82dff95cc7a0146344c3346a688aa86cd1620e98
donal72 <donal72>
2007-01-08 09:06:20 +0000

    mnet: New lang file for describing auth/mnet services

---
+++ lang/en_utf8/auth_mnet.php
@@ -0,0 +1,13 @@
+<?php // $Id$ 
+$string['sso_idp_name']                = 'SSO  (Identity Provider)';
+$string['sso_idp_description']         = 'Publish this service to allow your users to roam to the $a Moodle site without having to re-login there. '.
+                                         '<ul><li><em>Dependency</em>: You must also <strong>subscribe</strong> to the SSO (Service Provider) service on $a.</li></ul><br />'.
+                                         'Subscribe to this service to allow authenticated users from $a to access your site without having to re-login. '.
+                                         '<ul><li><em>Dependency</em>: You must also <strong>publish</strong> the SSO (Service Provider) service to $a.</li></ul><br />';
+
+$string['sso_sp_name']                 = 'SSO (Service Provider)';
+$string['sso_sp_description']          = 'Publish  this service to allow authenticated users from $a to access your site without having to re-login. '.
+                                         '<ul><li><em>Dependency</em>: You must also <strong>subscribe</strong> to the SSO (Identity Provider) service on $a.</li></ul><br />'.
+                                         'Subscribe to this service to allow your users to roam to the $a Moodle site without having to re-login there. '.
+                                         '<ul><li><em>Dependency</em>: You must also <strong>publish</strong> the SSO (Identity Provider) service to $a.</li></ul><br />';
+?>
