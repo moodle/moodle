80381122be24dd28a2d5262a6eb68392814f5cc9
skodak <skodak>
2007-01-09 00:05:56 +0000

    MDL-5162 Deprecated elements in messages block

--- blocks/messages/block_messages.php
+++ blocks/messages/block_messages.php
@@ -21,7 +21,7 @@ class block_messages extends block_base {
             return $this->content;
         }
 
-        $this->content->footer = '<a target="message" href="'.$CFG->wwwroot.'/message/index.php" onclick="return openpopup(\'/message/index.php\', \'message\', \'menubar=0,location=0,scrollbars,status,resizable,width=400,height=500\', 0);">'.get_string('messages', 'message').'</a>...';
+        $this->content->footer = '<a href="'.$CFG->wwwroot.'/message/index.php" onclick="this.target=\'message\'; return openpopup(\'/message/index.php\', \'message\', \'menubar=0,location=0,scrollbars,status,resizable,width=400,height=500\', 0);">'.get_string('messages', 'message').'</a>...';
 
         $users = get_records_sql("SELECT m.useridfrom as id, COUNT(m.useridfrom) as count,
                                          u.firstname, u.lastname, u.picture, u.lastaccess
@@ -35,16 +35,20 @@ class block_messages extends block_base {
         //Now, we have in users, the list of users to show
         //Because they are online
         if (!empty($users)) {
+            $this->content->text .= '<ul class="list">';
             foreach ($users as $user) {
-                $this->content->text .= '<div style="text-align: left; font-size: 0.75em; padding-top: 5px;">';
-                $this->content->text .= print_user_picture($user->id, $this->instance->pageid, $user->picture, 16, true).' ';
                 $timeago = format_time(time() - $user->lastaccess);
+                $this->content->text .= '<li>';
+                $this->content->text .= print_user_picture($user->id, $this->instance->pageid, $user->picture, 16, true).' ';
                 $this->content->text .= '<a href="'.$CFG->wwwroot.'/user/view.php?id='.$user->id.'&amp;course='.$this->instance->pageid.'" title="'.$timeago.'">'.fullname($user).'</a>';
                 $this->content->text .= '&nbsp;<a target="message_'.$user->id.'" href="'.$CFG->wwwroot.'/message/discussion.php?id='.$user->id.'" onclick="return openpopup(\'/message/discussion.php?id='.$user->id.'\', \'message_'.$user->id.'\', \'menubar=0,location=0,scrollbars,status,resizable,width=400,height=500\', 0);"><img class="iconsmall" src="'.$CFG->pixpath.'/t/message.gif" alt="" />&nbsp;'.$user->count.'</a>';
-                $this->content->text .= '</div>';
+                $this->content->text .= '</li>';
             }
+            $this->content->text .= '</ul>'; 
         } else {
-            $this->content->text .= "<center><font size=\"-1\">".get_string('nomessages', 'message')."</font></center>";
+            $this->content->text .= '<div class="nomessage">';
+            $this->content->text .= get_string('nomessages', 'message');
+            $this->content->text .= '</div>'; 
         }
 
         return $this->content;
