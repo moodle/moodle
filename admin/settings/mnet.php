47c0407b9733df91c90ed1020e227a52492f697d
martinlanghoff <martinlanghoff>
2007-01-17 23:49:04 +0000

    mnet: moving remote enrolment config to admin/mnet

--- admin/settings/mnet.php
+++ admin/settings/mnet.php
@@ -6,5 +6,6 @@ $ADMIN->add('mnet', new admin_externalpage('net', get_string('settings', 'mnet')
 $ADMIN->add('mnet', new admin_externalpage('ssoaccesscontrol', get_string('ssoaccesscontrol', 'mnet'), $CFG->wwwroot . '/admin/mnet/access_control.php', 'moodle/site:config'));
 
 $ADMIN->add('mnet', new admin_externalpage('mnetpeers', get_string('mnetpeers', 'mnet'), $CFG->wwwroot . '/admin/mnet/peers.php', 'moodle/site:config'));
+$ADMIN->add('mnet', new admin_externalpage('mnetenrol', get_string('mnetenrol', 'mnet'), $CFG->wwwroot . '/admin/mnet/enr_hosts.php', 'moodle/site:config'));
 $ADMIN->add('mnet', new admin_externalpage('trustedhosts', get_string('trustedhosts', 'mnet'), $CFG->wwwroot . '/admin/mnet/trustedhosts.php', 'moodle/site:config'));
 ?>
