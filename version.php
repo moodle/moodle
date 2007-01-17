aaba34cc52521097dff231928d1ac07850020d77
moodler <moodler>
2007-01-17 04:28:57 +0000

    Bumped to post-1.6.4

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
                            //         X  = release number 1.6.[0,1,2,3...]
                            //          Y = micro-increments between releases
 
-   $release = '1.6.4';    // Human-friendly version name
+   $release = '1.6.4+';    // Human-friendly version name
 
 ?>
