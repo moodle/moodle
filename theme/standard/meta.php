8b74cd23f4fdb49930350a482e97857624537f12
moodler <moodler>
2007-01-25 04:51:37 +0000

    Added some documentation and renamed thngs for clarity

--- theme/standard/meta.php
+++ theme/standard/meta.php
@@ -1,8 +1,6 @@
-
 <!--[if IE 7]>
-    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->wwwroot ?>/theme/standard/ie7.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->wwwroot ?>/theme/standard/styles_ie7.css" />
 <![endif]-->
 <!--[if IE 6]>
-    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->wwwroot ?>/theme/standard/ie6.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->wwwroot ?>/theme/standard/styles_ie6.css" />
 <![endif]-->
-
