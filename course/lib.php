56595370a3e1a2e373b88e957743540420670708
urs_hunkler <urs_hunkler>
2007-02-04 20:11:05 +0000

    'MDL-8418 - I added and changed some classes to center the table and use more standrad cell classes.'

--- course/lib.php
+++ course/lib.php
@@ -472,7 +472,8 @@ function print_log($course, $user=0, $date=0, $order="l.time ASC", $page=0, $per
 
     print_paging_bar($totalcount, $page, $perpage, "$url&amp;perpage=$perpage&amp;");
 
-    echo "<table class=\"logtable\" cellpadding=\"3\" cellspacing=\"0\" summary=\"\">\n";
+    echo '<table class="logtable genearlbox boxaligncenter" summary="">'."\n";
+    // echo "<table class=\"logtable\" cellpadding=\"3\" cellspacing=\"0\" summary=\"\">\n";
     echo "<tr>";
     if ($course->id == SITEID) {
         echo "<th class=\"c0 header\" scope=\"col\">".get_string('course')."</th>\n";
@@ -518,23 +519,23 @@ function print_log($course, $user=0, $date=0, $order="l.time ASC", $page=0, $per
 
         echo '<tr class="r'.$row.'">';
         if ($course->id == SITEID) {
-            echo "<td class=\"r$row c0\">\n";
+            echo "<td class=\"cell c0\">\n";
             echo "    <a href=\"{$CFG->wwwroot}/course/view.php?id={$log->course}\">".$courses[$log->course]."</a>\n";
             echo "</td>\n";
         }
-        echo "<td class=\"r$row c1\" align=\"right\">".userdate($log->time, '%a').
+        echo "<td class=\"cell c1\" align=\"right\">".userdate($log->time, '%a').
              ' '.userdate($log->time, $strftimedatetime)."</td>\n";
-        echo "<td class=\"r$row c2\">\n";
+        echo "<td class=\"cell c2\">\n";
         link_to_popup_window("/iplookup/index.php?ip=$log->ip&amp;user=$log->userid", 'iplookup',$log->ip, 400, 700);
         echo "</td>\n";
         $fullname = fullname($log, has_capability('moodle/site:viewfullnames', get_context_instance(CONTEXT_COURSE, $course->id)));
-        echo "<td class=\"r$row c3\">\n";
+        echo "<td class=\"cell c3\">\n";
         echo "    <a href=\"$CFG->wwwroot/user/view.php?id={$log->userid}&amp;course={$log->course}\">$fullname</a>\n";
         echo "</td>\n";
-        echo "<td class=\"r$row c4\">\n";
+        echo "<td class=\"cell c4\">\n";
         link_to_popup_window( make_log_url($log->module,$log->url), 'fromloglive',"$log->module $log->action", 400, 600);
         echo "</td>\n";;
-        echo "<td class=\"r$row c5\">{$log->info}</td>\n";
+        echo "<td class=\"cell c5\">{$log->info}</td>\n";
         echo "</tr>\n";
     }
     echo "</table>\n";
