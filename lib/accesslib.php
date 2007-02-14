b50965414e9a65688b045142db235e91209e094d
skodak <skodak>
2007-02-14 22:51:17 +0000

    MDL-8268 fixed get_user_capability_course(), patch by Mark Nielsen; backported from HEAD

--- lib/accesslib.php
+++ lib/accesslib.php
@@ -3272,7 +3272,7 @@ function get_user_capability_course($capability, $userid='') {
     $courses = get_records_select('course', '', '', 'id, id');
 
     foreach ($courses as $course) {
-        if (has_capability($capability, get_context_instance(CONTEXT_COURSE, $course->id))) {
+        if (has_capability($capability, get_context_instance(CONTEXT_COURSE, $course->id), $userid)) {
             $usercourses[] = $course;
         }
     }
