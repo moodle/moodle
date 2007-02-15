3d6d2447286cbe10646ca31c3e77302526edfdbe
toyomoyo <toyomoyo>
2007-02-15 03:21:05 +0000

    fix for MDL-5829, blocks on blog page can not be minimized

--- blog/footer.php
+++ blog/footer.php
@@ -7,7 +7,7 @@ print '<!-- End page content -->'."\n";
 
 // The right column
 if (blocks_have_content($pageblocks, BLOCK_POS_RIGHT) || $editing) {
-    echo '<td style="vertical-align: top; width: '. $preferred_width_right .'px;">';
+    echo '<td style="vertical-align: top; width: '. $preferred_width_right .'px;" id="right-column">';
     echo '<!-- Begin right side blocks -->'."\n";
     blocks_print_group($PAGE, $pageblocks, BLOCK_POS_RIGHT);
     print_spacer(1, 120, true);
