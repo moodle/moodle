82355e2f84caff7f6227eab3fba5c50434859502
stronk7 <stronk7>
2007-02-21 08:34:21 +0000

    Bump to 1.9dev (keeping sync with 18_STABLE version).

--- backup/version.php
+++ backup/version.php
@@ -6,7 +6,7 @@
 /// database (backup_version) to determine whether upgrades should
 /// be performed (see db/backup_*.php)
 
-    $backup_version = 2006082304;   // The current version is a date (YYYYMMDDXX)
-    $backup_release = "1.8dev";  // User-friendly version number
+    $backup_version = 2007022100;   // The current version is a date (YYYYMMDDXX)
+    $backup_release = "1.9dev";     // User-friendly version number
 
 ?>
