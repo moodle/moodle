ad2f4cff93cef50f4e36aea6b0a6505b5a9ae126
toyomoyo <toyomoyo>
2007-02-22 03:21:49 +0000

    fix for MDL-8577, message textarea too big to fit

--- mod/forum/post_form.php
+++ mod/forum/post_form.php
@@ -25,7 +25,7 @@ class mod_forum_post_form extends moodleform {
 		$mform->setType('subject', PARAM_TEXT);
 		$mform->addRule('subject', get_string('required'), 'required', null, 'client');
 
-		$mform->addElement('htmleditor', 'message', get_string('message', 'forum'), array('cols'=>85, 'rows'=>30));
+		$mform->addElement('htmleditor', 'message', get_string('message', 'forum'), array('cols'=>50, 'rows'=>30));
 		$mform->setType('message', PARAM_RAW);
 		$mform->addRule('message', get_string('required'), 'required', null, 'client');
         $mform->setHelpButton('message', array('reading', 'writing', 'questions', 'richtext'), false, 'editorhelpbutton');
