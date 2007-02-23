577a9f580542e78cf43d50a329d0839eb4bb8623
toyomoyo <toyomoyo>
2007-02-23 01:22:15 +0000

    fix for MDL-8585, add admin setting for $CFG->latinexcelexport

--- admin/settings/language.php
+++ admin/settings/language.php
@@ -9,9 +9,12 @@ $temp->add(new admin_setting_configcheckbox('langmenu', get_string('langmenu', '
 $temp->add(new admin_setting_configtext('langlist', get_string('langlist', 'admin'), get_string('configlanglist', 'admin'), '', PARAM_NOTAGS));
 $temp->add(new admin_setting_configcheckbox('langcache', get_string('langcache', 'admin'), get_string('configlangcache', 'admin'), 1));
 $temp->add(new admin_setting_configtext('locale', get_string('localetext', 'admin'), get_string('configlocale', 'admin'), '', PARAM_FILE));
+
+// new CFG variable for excel encoding
+$temp->add(new admin_setting_configselect('latinexcelexport', get_string('latinexcelexport', 'admin'), get_string('configlatinexcelexport', 'admin'), '0', array('0'=>'Unicode','1'=>'Latin')));
+
+
 $ADMIN->add('language', $temp);
-
-
 
 $ADMIN->add('language', new admin_externalpage('langedit', get_string('langedit', 'admin'), "$CFG->wwwroot/$CFG->admin/lang.php"));
 $ADMIN->add('language', new admin_externalpage('langimport', get_string('langpacks', 'admin'), "$CFG->wwwroot/$CFG->admin/langimport.php"));
