5f70ddb676424aadab2ef7862fd2782305d5afca
andreabix <andreabix>
2007-02-26 15:13:05 +0000

    Added string for 'Hide All courses' setting.

--- lang/en_utf8/block_course_list.php
+++ lang/en_utf8/block_course_list.php
@@ -5,6 +5,7 @@
 $string['allcourses'] = 'Admin user sees all courses';
 $string['blockname'] = 'Course List';
 $string['configadminview'] = 'What should the admin see in the course list block?';
+$string['confighideallcourseslink'] = 'Hide \"All courses\" link at the bottom of the block. Link hiding does not affects Admin\'s view';
 $string['owncourses'] = 'Admin user sees own courses';
 
 ?>
