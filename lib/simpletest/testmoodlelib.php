239c15765c69426176e55dd3be13701b88976a0b
moodler <moodler>
2007-02-28 07:58:34 +0000

    Merged from 1.8 stable

--- lib/simpletest/testmoodlelib.php
+++ lib/simpletest/testmoodlelib.php
@@ -8,7 +8,7 @@
  * @package moodlecore
  */
 
-/** */
+/** $Id */
 require_once(dirname(__FILE__) . '/../../config.php');
 
 global $CFG;
@@ -16,7 +16,31 @@ require_once($CFG->libdir . '/simpletestlib.php');
 require_once($CFG->libdir . '/moodlelib.php');
 
 class moodlelib_test extends UnitTestCase {
-
+    
+    /**
+     * An array of possible user_agent strings
+     * 
+     * @var array possible user_agent strings
+     * @TODO Complete that list using http://www.pgts.com.au/pgtsj/pgtsj0208c.html
+     */
+    var $user_agents = array(
+        'MSIE' => array(
+            '6.0' => array('Windows XP SP2' => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)'),
+            '7.0' => array('Windows XP SP2' => 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; YPC 3.0.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)')
+        ),  
+        'Firefox' => array(
+            '1.5' => array('Windows XP' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; nl; rv:1.8) Gecko/20051107 Firefox/1.5'),
+            '2.0' => array('Windows XP' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1')
+        ),
+        'Safari' => array(
+            '2.0' => array('Mac OS X' => 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en) AppleWebKit/412 (KHTML, like Gecko) Safari/412'),
+            '312' => array('Mac OS X' => 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en-us) AppleWebKit/312.1 (KHTML, like Gecko) Safari/312')
+        ),
+        'Opera' => array(
+            '9.0' => array('Windows XP' => 'Opera/9.0 (Windows NT 5.1; U; en)')
+        )
+    );
+    
     function setUp() {
     }
 
@@ -50,6 +74,28 @@ class moodlelib_test extends UnitTestCase {
         $this->assertFalse(address_in_subnet('  3.3.3.7  ', '  123.121.234.1  , 1.1.1.1/16,2.2.,3.3.3.3-6  '));
     }
     
+    /**
+     * Modifies $_SERVER['HTTP_USER_AGENT'] manually to check if check_browser_version 
+     * works as expected.
+     */
+    function test_check_browser_version()
+    {
+        $_SERVER['HTTP_USER_AGENT'] = $this->user_agents['Safari']['2.0']['Mac OS X'];
+        $this->assertTrue(check_browser_version('Safari', '312'));
+        $this->assertFalse(check_browser_version('Safari', '500'));
+        
+        $_SERVER['HTTP_USER_AGENT'] = $this->user_agents['Opera']['9.0']['Windows XP'];
+        $this->assertTrue(check_browser_version('Opera', '8.0'));
+        $this->assertFalse(check_browser_version('Opera', '10.0'));
+        
+        $_SERVER['HTTP_USER_AGENT'] = $this->user_agents['MSIE']['6.0']['Windows XP SP2'];
+        $this->assertTrue(check_browser_version('MSIE', '5.0'));
+        $this->assertFalse(check_browser_version('MSIE', '7.0'));
+        
+        $_SERVER['HTTP_USER_AGENT'] = $this->user_agents['Firefox']['2.0']['Windows XP'];
+        $this->assertTrue(check_browser_version('Firefox', '1.5'));
+        $this->assertFalse(check_browser_version('Firefox', '3.0'));        
+    }    
 }
 
 ?>
