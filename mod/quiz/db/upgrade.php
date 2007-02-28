b9ea2b7ad5c93888ffcf70a1446de9488c903fa9
tjhunt <tjhunt>
2007-02-28 17:19:04 +0000

    Add a unique index to the question_numerical_units table, to prevent duplicate units being created.

--- mod/quiz/db/upgrade.php
+++ mod/quiz/db/upgrade.php
@@ -32,6 +32,22 @@ function xmldb_quiz_upgrade($oldversion=0) {
 ///     $result = result of "/lib/ddllib.php" function calls
 /// }
 
+    if ($result && $oldversion < 2007022800) {
+    /// Ensure that there are not existing duplicate entries in the database.
+        delete_records_select('question_numerical_units', "id > (SELECT MIN(iqnu.id)
+                FROM {$CFG->prefix}question_numerical_units iqnu
+                WHERE iqnu.question = {$CFG->prefix}question_numerical_units.question AND
+                        iqnu.unit = {$CFG->prefix}question_numerical_units.unit)");
+
+    /// Define index question-unit (unique) to be added to question_numerical_units
+        $table = new XMLDBTable('question_numerical_units');
+        $index = new XMLDBIndex('question-unit');
+        $index->setAttributes(XMLDB_INDEX_UNIQUE, array('question', 'unit'));
+
+    /// Launch add index question-unit
+        $result = $result && add_index($table, $index);
+    }
+
     return $result;
 }
 
