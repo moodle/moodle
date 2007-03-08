23adf25248ce981a523b375f0471233e5b5f626f
moodler <moodler>
2007-03-08 05:15:19 +0000

    Merged ppichet changes to quiz files from 1.8

--- lang/en_utf8/qtype_calculated.php
+++ lang/en_utf8/qtype_calculated.php
@@ -1,15 +1,24 @@
 <?php
-$string['correctanswershows'] = 'Correct answer shows';
-$string['correctanswershowsformat'] = 'Format';
-$string['tolerance'] = 'Tolerance &plusmn;';
 $string['addmoreanswerblanks'] = 'Add another answer blank.';
 $string['addmoreunitblanks'] = 'Blanks for $a More Units';
 $string['answerhdr'] = 'Answer';
 $string['atleastoneanswer'] = 'You need to provide at least one answer.';
+$string['correctanswershows'] = 'Correct answer shows';
+$string['correctanswershowsformat'] = 'Format';
+$string['existingcategory1'] = 'will use an already existing shared dataset';
+$string['keptcategory1'] = 'will use the same existing shared dataset as before';
+$string['keptlocal1'] = 'will use the same existing private dataset as before';
+$string['makecopynextpage'] = 'Next Page (new question)';
+$string['mandatoryhdr']='Mandatory wild cards present in answers';
 $string['mustbenumeric'] = 'You must enter a number here.';
 $string['mustnotbenumeric'] = 'This can\'t be a number.';
-$string['youmustenteramultiplierhere'] = 'You must enter a multiplier here.';
-$string['nextpage'] = 'Next Page';
+$string['newcategory1'] = 'will use a new shared dataset';
+$string['newlocal1'] = 'will use a new private dataset';
 $string['nextitemtoadd'] = 'Next \'Item to Add\'';
-$string['makecopynextpage'] = 'Next Page (new question)';
+$string['nextpage'] = 'Next Page';
+$string['nodataset'] = 'nothing - it is not a wild card';
+$string['possiblehdr']='Possible wild cards present only in the question text';
+$string['tolerance'] = 'Tolerance &plusmn;';
+$string['usedinquestion']='Used in Question';
+$string['youmustenteramultiplierhere'] = 'You must enter a multiplier here.';
 ?>
