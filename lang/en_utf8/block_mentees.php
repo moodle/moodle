521296f5b873d0d02c8ecf03d603850dc1505b23
toyomoyo <toyomoyo>
2007-03-09 07:08:27 +0000

    merged fix for MDL-8782, adding a mentees block

---
+++ lang/en_utf8/block_mentees.php
@@ -0,0 +1,6 @@
+<?PHP 
+$string['blockname'] = 'Mentees';
+$string['configtitle'] = 'Block title';
+$string['leaveblanktohide'] = 'leave blank to hide the title';
+$string['newmenteesblock'] = '(new Mentees block)';
+?>
