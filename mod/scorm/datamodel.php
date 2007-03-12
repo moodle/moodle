6c60a57d319688f86639819c4e56eeb53f21f291
bobopinna <bobopinna>
2007-03-12 20:11:59 +0000

    fixed a typo in library call

--- mod/scorm/datamodel.php
+++ mod/scorm/datamodel.php
@@ -47,7 +47,7 @@
                 }
                 if (substr($element,0,15) == 'adl.nav.request') {
                     // SCORM 2004 Sequencing Request
-                    require_once('datamodels/scorm_13lib.php');
+                    require_once('datamodels/sequencinglib.php');
 
                     $search = array('@continue@', '@previous@', '@\{target=(\S+)\}choice@', '@exit@', '@exitAll@', '@abandon@', '@abandonAll@');
                     $replace = array('continue_', 'previous_', '\1', 'exit_', 'exitall_', 'abandon_', 'abandonall');
@@ -56,6 +56,7 @@
                     if ($action != $value) {
                         // Evaluating navigation request
                         $valid = scorm_sequencing_overall ($scoid,$USER->id,$action);
+                        $valid = 'true';
 
                         // Set valid request
                         $search = array('@continue@', '@previous@', '@\{target=(\S+)\}choice@');
