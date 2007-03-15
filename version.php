8305aeaaa8cb10b692921097b00bee1b1dc34118
toyomoyo <toyomoyo>
2007-03-15 08:30:36 +0000

    putting user/editprofile back

--- version.php
+++ version.php
@@ -6,7 +6,7 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-   $version = 2007021402;  // YYYYMMDD = date
+   $version = 2007021500;  // YYYYMMDD = date
                            //       XY = increments within a single day
 
    $release = '1.9 dev';    // Human-friendly version name
