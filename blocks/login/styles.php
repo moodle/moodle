be78d45dd2b9964fffe7dbb205f76004b262bdcc
moodler <moodler>
2007-03-20 07:35:53 +0000

    Merged login block tidy-up from stable

--- blocks/login/styles.php
+++ blocks/login/styles.php
@@ -5,3 +5,17 @@
 .block_login .loginform {
   display:inline;
 }
+
+
+.block_login .loginform .c1 input {
+  width: 6em;
+}
+
+.block_login .loginform div {
+  margin:0.3em 0.8em;
+  text-align:right;
+}
+
+.block_login .btn input {
+  text-align:center;
+}
