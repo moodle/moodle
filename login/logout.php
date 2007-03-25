466c30cf6e5e6fa4058e913031766a338cd5b7a4
skodak <skodak>
2007-03-25 20:18:15 +0000

    MDL-9028 prelogout_hook() is never executed in login/logout.php; patch by Inaki Arenaza

--- login/logout.php
+++ login/logout.php
@@ -6,12 +6,6 @@
     // can be overriden by auth plugins
     $redirect = $CFG->wwwroot.'/';
 
-    $authsequence = explode(',', $CFG->auth); // auths, in sequence
-    foreach($authsequence as $authname) {
-        $authplugin = get_auth_plugin($authname);
-        $authplugin->prelogin_hook();
-    }
-
     $sesskey = optional_param('sesskey', '__notpresent__', PARAM_RAW); // we want not null default to prevent required sesskey warning
 
     if (!confirm_sesskey($sesskey)) {
@@ -19,6 +13,12 @@
         notice_yesno(get_string('logoutconfirm'), 'logout.php', $CFG->wwwroot.'/', array('sesskey'=>sesskey()), null, 'post', 'get');
         print_footer();
         die;
+    }
+
+    $authsequence = explode(',', $CFG->auth); // auths, in sequence
+    foreach($authsequence as $authname) {
+        $authplugin = get_auth_plugin($authname);
+        $authplugin->prelogout_hook();
     }
 
     require_logout();
