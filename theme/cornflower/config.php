b24126e9a9fcff0377c121a23d66412666a39708
moodler <moodler>
2007-03-28 02:24:34 +0000

    New cornflower from Jeff Watkins  MDL-9009

--- theme/cornflower/config.php
+++ theme/cornflower/config.php
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-$THEME->sheets = array('styles_layout', 'styles_fonts', 'styles_color');
+$THEME->sheets = array('cornflower');
 
 /// This variable is an array containing the names of all the 
 /// stylesheet files you want included in this theme, and in what order
