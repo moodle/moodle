a5c3d160dc675a67b1269b56880a4cb992a080b8
stronk7 <stronk7>
2007-03-29 23:25:04 +0000

    Fixing export interface. Step3. MDL-8793
    Merged from MOODLE_18_STABLE

--- mod/glossary/export.php
+++ mod/glossary/export.php
@@ -7,7 +7,6 @@
 
     $mode= optional_param('mode', '', PARAM_ALPHA);           // term entry cat date letter search author approval
     $hook= optional_param('hook', '', PARAM_CLEAN);           // the term, entry, cat, etc... to look for based on mode
-    $l   = optional_param('l','', PARAM_ALPHANUM);
     $cat = optional_param('cat',0, PARAM_ALPHANUM);
 
     if (! $cm = get_coursemodule_from_id('glossary', $id)) {
@@ -36,31 +35,18 @@
     $strsearchindefinition = get_string("searchindefinition", "glossary");
     $strsearch = get_string("search");
     $strexportfile = get_string("exportfile", "glossary");
+    $strexportentries = get_string('exportentries', 'glossary');
 
-    $navigation = "";
-    if ($course->id != SITEID) {
-        $navigation = "<a href=\"../../course/view.php?id=$course->id\">$course->shortname</a> ->";
-        require_login($course->id);
-    }
-
-    print_header("$course->shortname: ".format_string($glossary->name), $course->fullname,
-        "$navigation <a href=\"index.php?id=$course->id\">$strglossaries</a> -> ".format_string($glossary->name),
+    print_header_simple(format_string($glossary->name), "",
+        "<a href=\"index.php?id=$course->id\">$strglossaries</a> -> " .
+        "<a href=\"view.php?id=$id\">" .format_string($glossary->name) . "</a> -> " .
+        $strexportentries,
         "", "", true, update_module_button($cm->id, $course->id, $strglossary),
         navmenu($course, $cm));
 
-    print_heading(format_string($glossary->name));
+    print_heading($strexportentries);
 
-/// Info box
-
-    if ( $glossary->intro ) {
-        print_simple_box(format_text($glossary->intro), 'center', '70%', '', 5, 'generalbox', 'intro');
-        echo '<br />';
-    }
-
-/// Tabbed browsing sections
-    $tab = GLOSSARY_EXPORT_VIEW;
-    include("tabs.php");
-
+    print_box_start('glossarydisplay generalbox');
     ?>
     <form action="exportfile.php" method="post">
     <table border="0" cellpadding="6" cellspacing="6" width="100%">
@@ -68,11 +54,9 @@
         <input type="submit" value="<?php p($strexportfile)?>" />
     </td></tr></table>
     <input type="hidden" name="id" value="<?php p($id)?>" />
-    <input type="hidden" name="l" value="<?php p($l)?>" />
     <input type="hidden" name="cat" value="<?php p($cat)?>" />
     </form>
-    <?php
-
-    glossary_print_tabbed_table_end();
-    print_footer();
+<?php
+    print_box_end();
+    print_footer($course);
 ?>
