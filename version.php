367dfbd905638173ca5c44a926007e57fbd5ce22
moodler <moodler>
2007-03-30 08:40:02 +0000

    Updating for 1.7.2 release

--- version.php
+++ version.php
@@ -6,10 +6,10 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-   $version = 2006101011;  // YYYYMMDD   = date of the 1.7 branch (don't change)
+   $version = 2006101020;  // YYYYMMDD   = date of the 1.7 branch (don't change)
                            //         X  = release number 1.7.[0,1,2,3...]
                            //          Y = micro-increments between releases
 
-   $release = '1.7.1+';       // Human-friendly version name
+   $release = '1.7.2';     // Human-friendly version name
 
 ?>
