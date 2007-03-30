696b50fff2aac139310fe59477bff0800f4f0003
tjhunt <tjhunt>
2007-03-30 10:17:20 +0000

    Small code cleanup to fix an obscure warning. If you got an error in the middle of creating a quiz attempt, you could get an attempt with no states. Then that caused an error when the attempt was deleted. Merged from MOODLE_17_STABLE.

--- lib/questionlib.php
+++ lib/questionlib.php
@@ -373,11 +373,13 @@ function delete_attempt($attemptid) {
     global $QTYPES;
 
     $states = get_records('question_states', 'attempt', $attemptid);
-    $stateslist = implode(',', array_keys($states));
-
-    // delete questiontype-specific data
-    foreach ($QTYPES as $qtype) {
-        $qtype->delete_states($stateslist);
+    if ($states) {
+        $stateslist = implode(',', array_keys($states));
+    
+        // delete question-type specific data
+        foreach ($QTYPES as $qtype) {
+            $qtype->delete_states($stateslist);
+        }
     }
 
     // delete entries from all other question tables
@@ -385,8 +387,6 @@ function delete_attempt($attemptid) {
     delete_records("question_states", "attempt", $attemptid);
     delete_records("question_sessions", "attemptid", $attemptid);
     delete_records("question_attempts", "id", $attemptid);
-
-    return;
 }
 
 /**
