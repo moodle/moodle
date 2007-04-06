5db9c35d010e835c2293b30e0c69d45bccdacbfb
Petr Skoda <info@skodak.org>
2007-04-06 17:13:31 +0000

    book - xml db merged from 17; removing utf migration code

--- version.php
+++ version.php
@@ -1,13 +1,13 @@
-<?PHP // $Id: version.php,v 1.1 2006/03/12 18:39:59 skodak Exp $
+<?PHP // $Id: version.php,v 1.2 2007/04/06 17:13:31 skodak Exp $
 
 /////////////////////////////////////////////////////////////////////////////////
 ///  Called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2006031200;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2006031000;  // Requires this Moodle version
+$module->version  = 2007040600;  // The current module version (Date: YYYYMMDDXX)
+$module->requires = 2006101020;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
-$release = "1.3alpha";                // User-friendly version number
+$release = "1.3alpha+";          // User-friendly version number
 
 ?>
