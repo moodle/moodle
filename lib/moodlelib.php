4f3835b6012e34625e9ac40aeb6a61d7dccc9ad1
stronk7 <stronk7>
2007-04-07 16:05:54 +0000

    Now $CFG->apachemaxmem is respected.
    Merged from MOODLE_16_STABLE

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -6523,6 +6523,8 @@ eval('
  */
 function moodle_request_shutdown() {
 
+    global $CFG;
+
     // initially, we are only ever called under apache 
     // but check just in case 
     if (function_exists('apache_child_terminate') && function_exists('memory_get_usage')) {
