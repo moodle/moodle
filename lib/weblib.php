28ca56c93daa7bd139ff69e3ddbc094f31a45aa9
martinlanghoff <martinlanghoff>
2007-04-12 00:45:35 +0000

    Fixed 'off by one' month bug in month select boxes generated by print_date_selector when users time zone set to GMT-13.
    Author: Matt Clarkson <mattc@catalyst.net.nz>
    Committer: Matt <mattc@catalyst.net.nz>

--- lib/weblib.php
+++ lib/weblib.php
@@ -4283,7 +4283,7 @@ function print_date_selector($day, $month, $year, $currenttime=0, $return=false)
         $days[$i] = $i;
     }
     for ($i=1; $i<=12; $i++) {
-        $months[$i] = userdate(gmmktime(12,0,0,$i,1,2000), "%B");
+        $months[$i] = userdate(gmmktime(12,0,0,$i,15,2000), "%B");
     }
     for ($i=1970; $i<=2020; $i++) {
         $years[$i] = $i;
