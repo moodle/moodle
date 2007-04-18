485726e71f728b1ddda25b68898ca4c868f3c4cb
cataluke <cataluke>
2007-04-18 02:17:00 +0000

    MDL-8569 - mod/data sorting by numerical field now works with PostgreSQL
    Author: Luke Hudson <luke@catalyst.net.nz>

--- mod/data/view.php
+++ mod/data/view.php
@@ -295,7 +295,7 @@
         $sortcontent = $sortfield->get_sort_field();
         $sortcontentfull = $sortfield->get_sort_sql('c.'.$sortcontent);
 
-        $what = ' DISTINCT r.id, r.approved, r.userid, u.firstname, u.lastname, c.'.$sortcontent.' ';
+        $what = ' DISTINCT r.id, r.approved, r.userid, u.firstname, u.lastname, c.'.$sortcontent.', '.$sortcontentfull.' AS _order ';
         $count = ' COUNT(DISTINCT c.recordid) ';
         $tables = $CFG->prefix.'data_content c,'.$CFG->prefix.'data_records r,'.$CFG->prefix.'data_content c1, '.$CFG->prefix.'user u ';
         $where =  'WHERE c.recordid = r.id 
@@ -303,7 +303,7 @@
                      AND r.dataid = '.$data->id.' 
                      AND r.userid = u.id 
                      AND c1.recordid = r.id ';
-        $sortorder = ' ORDER BY '.$sortcontentfull.' '.$order.' , r.id ASC ';
+        $sortorder = ' ORDER BY _order '.$order.' , r.id ASC ';
         if ($search) {
             $searchselect = ' AND (c1.content ' . sql_ilike() . " '%$search%') "; //Be case-insensitive
         } else {
