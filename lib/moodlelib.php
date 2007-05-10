b68ae6fca76a88b66818b40437daf4e74b63c883
skodak <skodak>
2007-05-10 08:55:10 +0000

    MDL-9764 Require_course_login() should check cm visibility in frontpage course too

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -1873,9 +1873,16 @@ function require_course_login($courseorid, $autologinguest=true, $cm=null) {
     if (!empty($CFG->forcelogin)) {
         // login required for both SITE and courses
         require_login($courseorid, $autologinguest, $cm);
+
+    } else if (!empty($cm) and !$cm->visible) {
+        // always login for hidden activities
+        require_login($courseorid, $autologinguest, $cm);
+
     } else if ((is_object($courseorid) and $courseorid->id == SITEID)
           or (!is_object($courseorid) and $courseorid == SITEID)) {
         //login for SITE not required
+        return;
+
     } else {
         // course login always required
         require_login($courseorid, $autologinguest, $cm);
