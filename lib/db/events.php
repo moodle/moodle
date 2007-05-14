ca4bcf58ec001e8a5bc65816c1d231c5ca0e34a8
moodler <moodler>
2007-05-14 09:30:56 +0000

    Event handler for new grades

---
+++ lib/db/events.php
@@ -0,0 +1,38 @@
+<?php // $Id$
+
+///////////////////////////////////////////////////////////////////////////
+// Defines core event handlers                                           //
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//          http://moodle.org                                            //
+//                                                                       //
+// Copyright (C) 1999 onwards  Martin Dougiamas  http://moodle.com       //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+
+$events = array (
+   'grade_added' => array (       // All new grades get processed immediately by the gradebook
+        'handlerfile'      => '/lib/gradelib.php',
+        'handlerfunction'  => 'grade_handler', 
+        'schedule'         => 'instant'
+    )
+);
+
+
+?>
