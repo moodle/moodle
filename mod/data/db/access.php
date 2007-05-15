52abd5d7c4f0ac54cfc0463d5276c159a02a76c0
skodak <skodak>
2007-05-15 16:57:08 +0000

    MDL-9821 incorrect permission to modify templates for non-editing teacher in db module

--- mod/data/db/access.php
+++ mod/data/db/access.php
@@ -140,7 +140,6 @@ $mod_data_capabilities = array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_MODULE,
         'legacy' => array(
-            'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'admin' => CAP_ALLOW
         )
