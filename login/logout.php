ad2c6a55d12d7586ddee14586df1bc2f11ecad0b
skodak <skodak>
2007-05-15 21:11:57 +0000

    MDL-9824 do not use sesskey check when user already logged out in logout.php

--- login/logout.php
+++ login/logout.php
@@ -8,7 +8,12 @@
 
     $sesskey = optional_param('sesskey', '__notpresent__', PARAM_RAW); // we want not null default to prevent required sesskey warning
 
-    if (!confirm_sesskey($sesskey)) {
+    if (!isloggedin()) {
+        // no confirmation, user has already logged out
+        require_logout();
+        redirect($redirect);
+
+    } else if (!confirm_sesskey($sesskey)) {
         print_header($SITE->fullname, $SITE->fullname, 'home');
         notice_yesno(get_string('logoutconfirm'), 'logout.php', $CFG->wwwroot.'/', array('sesskey'=>sesskey()), null, 'post', 'get');
         print_footer();
