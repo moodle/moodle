5d852ba111960aa7f4af5f21720adc4e71ae5d56
tjhunt <tjhunt>
2007-05-22 13:25:31 +0000

    When using DEBUG_DEVELOPER, make error(...) output a stack trace.

--- lib/weblib.php
+++ lib/weblib.php
@@ -5062,6 +5062,8 @@ function error ($message, $link='') {
     echo '<br />';
     print_simple_box($message, '', '', '', '', 'errorbox');
 
+    debugging('Stack trace:', DEBUG_DEVELOPER);
+
     // in case we are logging upgrade in admin/index.php stop it
     if (function_exists('upgrade_log_finish')) {
         upgrade_log_finish();
