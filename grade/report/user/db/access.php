90dcbeccd4b76035c9c716ebdd0875f8c010ec45
moodler <moodler>
2007-06-18 07:35:14 +0000

    Whoops, checked in wrong file before

--- grade/report/user/db/access.php
+++ grade/report/user/db/access.php
@@ -7,16 +7,17 @@ $gradereport_grader_capabilities = array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_COURSE,
         'legacy' => array(
+            'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'admin' => CAP_ALLOW
         )
     ),
 
-    'gradereport/grader:manage' => array(
-        'riskbitmask' => RISK_PERSONAL | RISK_CONFIG,
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
+    'moodle/user:viewusergrade' => array(
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_USER,
         'legacy' => array(
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
