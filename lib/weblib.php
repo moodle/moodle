12c1f0abea5bf02b084d12d0a9ada99bd26e2da2
mchurch <mchurch>
2007-06-25 20:50:17 +0000

    Fixing update_group_button and update_groups_button. They were no longer calling valid URL's.

--- lib/weblib.php
+++ lib/weblib.php
@@ -4553,10 +4553,11 @@ function update_group_button($courseid, $groupid) {
     if (has_capability('moodle/course:managegroups', get_context_instance(CONTEXT_GROUP, $groupid))) {
         $string = get_string('editgroupprofile');
 
-        return "<form $CFG->frametarget method=\"get\" action=\"$CFG->wwwroot/course/group.php\">".
+        return "<form $CFG->frametarget method=\"get\" action=\"$CFG->wwwroot/group/edit.php\">".
                '<div>'.
-               '<input type="hidden" name="id" value="'. $courseid .'" />'.
-               '<input type="hidden" name="group" value="'. $groupid .'" />'.
+               '<input type="hidden" name="courseid" value="'. $courseid .'" />'.
+               '<input type="hidden" name="id" value="'. $groupid .'" />'.
+               '<input type="hidden" name="grouping" value="-1" />'.
                '<input type="hidden" name="edit" value="on" />'.
                '<input type="submit" value="'. $string .'" /></div></form>';
     }
@@ -4583,7 +4584,7 @@ function update_groups_button($courseid) {
             $edit = 'on';
         }
 
-        return "<form $CFG->frametarget method=\"get\" action=\"$CFG->wwwroot/course/groups.php\">".
+        return "<form $CFG->frametarget method=\"get\" action=\"$CFG->wwwroot/group/index.php\">".
                '<div>'.
                "<input type=\"hidden\" name=\"id\" value=\"$courseid\" />".
                "<input type=\"hidden\" name=\"edit\" value=\"$edit\" />".
