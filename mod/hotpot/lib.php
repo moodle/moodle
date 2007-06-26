ce197abbefd05efd2333bb251cd2f93eca3f4528
gbateson <gbateson>
2007-06-26 05:02:03 +0000

    in XML source files of HP quizzes, convert relative URLs to absolute URLs in links using window.open(...)

--- mod/hotpot/lib.php
+++ mod/hotpot/lib.php
@@ -1535,6 +1535,13 @@ class hotpot_xml_quiz extends hotpot_xml_tree {
                 $this->source = preg_replace($search, $replace, $this->source);
 
             } else {
+
+                // relative URLs in <a ... onclick="window.open('...')...">...</a>
+                $search = '|'.'(?<='.'onclick=&quot;'."window.open\\(&apos;".')'."(.*?)".'(?='."&apos;\\);return false;".'&quot;)'.'|ise';
+                $replace = "hotpot_convert_url('".$this->get_baseurl()."','".$this->reference."','\\1')";
+                $this->source = preg_replace($search, $replace, $this->source);
+
+
                 if ($this->parse_xml) {
 
                     $this->filetype = 'xml';
