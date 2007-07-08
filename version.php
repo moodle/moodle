b19909d9ae812ba11397ffc991327d0bae270244
moodler <moodler>
2007-07-08 14:32:41 +0000

    Bump to 1.8.2 release

--- version.php
+++ version.php
@@ -6,10 +6,10 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-   $version = 2007021511;   // YYYYMMDD   = date of the 1.8 branch (don't change)
+   $version = 2007021520;   // YYYYMMDD   = date of the 1.8 branch (don't change)
                             //         X  = release number 1.8.[0,1,2,3...]
                             //          Y = micro-increments between releases
 
-   $release = '1.8.1+';     // Human-friendly version name
+   $release = '1.8.2';      // Human-friendly version name
 
 ?>
