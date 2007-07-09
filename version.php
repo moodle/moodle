6c8a956d90c223e45cebae966de165191182cf5f
moodler <moodler>
2007-07-09 02:39:44 +0000

    Bump for post 1.8.2

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
                             //         X  = release number 1.8.[0,1,2,3...]
                             //          Y = micro-increments between releases
 
-   $release = '1.8.2';      // Human-friendly version name
+   $release = '1.8.2+';     // Human-friendly version name
 
 ?>
