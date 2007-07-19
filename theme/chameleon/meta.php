23290529167a6c69716ad4c8addf9a56ada2809f
urs_hunkler <urs_hunkler>
2007-07-19 19:53:40 +0000

    MDL-10322 integrated the flat tabs from pre 1.8, several other enhancements

---
+++ theme/chameleon/meta.php
@@ -0,0 +1,6 @@
+<!--[if IE 7]>
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpswwwroot ?>/theme/chameleon/styles_ie7.css" />
+<![endif]-->
+<!--[if IE 6]>
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpswwwroot ?>/theme/chameleon/styles_ie6.css" />
+<![endif]-->
