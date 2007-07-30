84338bde4853905f10aeed80fff94a3ffd986259
nicolasconnault <nicolasconnault>
2007-07-30 11:51:37 +0000

    MDL-10637 Applied Eloy's suggested fix to the BOMs in the files he detected on his oh-so-aware Mac :-)

--- lib/adodb/lang/adodb-bgutf8.inc.php
+++ lib/adodb/lang/adodb-bgutf8.inc.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /*
 	Bulgarian language, v1.0, 25.03.2004, encoding by UTF-8 charset
 	contributed by Valentin Sheiretsky <valio#valio.eu.org>
@@ -6,33 +6,33 @@
 
 $ADODB_LANG_ARRAY = array (
             'LANG'                      => 'bgutf8',
-            DB_ERROR                    => 'неизвестна грешка',
-            DB_ERROR_ALREADY_EXISTS     => 'вече съществува',
-            DB_ERROR_CANNOT_CREATE      => 'не може да бъде създадена',
-            DB_ERROR_CANNOT_DELETE      => 'не може да бъде изтрита',
-            DB_ERROR_CANNOT_DROP        => 'не може да бъде унищожена',
-            DB_ERROR_CONSTRAINT         => 'нарушено условие',
-            DB_ERROR_DIVZERO            => 'деление на нула',
-            DB_ERROR_INVALID            => 'неправилно',
-            DB_ERROR_INVALID_DATE       => 'некоректна дата или час',
-            DB_ERROR_INVALID_NUMBER     => 'невалиден номер',
-            DB_ERROR_MISMATCH           => 'погрешна употреба',
-            DB_ERROR_NODBSELECTED       => 'не е избрана база данни',
-            DB_ERROR_NOSUCHFIELD        => 'несъществуващо поле',
-            DB_ERROR_NOSUCHTABLE        => 'несъществуваща таблица',
+            DB_ERROR                    => ' ',
+            DB_ERROR_ALREADY_EXISTS     => ' ',
+            DB_ERROR_CANNOT_CREATE      => '    ',
+            DB_ERROR_CANNOT_DELETE      => '    ',
+            DB_ERROR_CANNOT_DROP        => '    ',
+            DB_ERROR_CONSTRAINT         => ' ',
+            DB_ERROR_DIVZERO            => '  ',
+            DB_ERROR_INVALID            => '',
+            DB_ERROR_INVALID_DATE       => '   ',
+            DB_ERROR_INVALID_NUMBER     => ' ',
+            DB_ERROR_MISMATCH           => ' ',
+            DB_ERROR_NODBSELECTED       => '    ',
+            DB_ERROR_NOSUCHFIELD        => ' ',
+            DB_ERROR_NOSUCHTABLE        => ' ',
             DB_ERROR_NOT_CAPABLE        => 'DB backend not capable',
-            DB_ERROR_NOT_FOUND          => 'не е намерена',
-            DB_ERROR_NOT_LOCKED         => 'не е заключена',
-            DB_ERROR_SYNTAX             => 'грешен синтаксис',
-            DB_ERROR_UNSUPPORTED        => 'не се поддържа',
-            DB_ERROR_VALUE_COUNT_ON_ROW => 'некоректен брой колони в реда',
-            DB_ERROR_INVALID_DSN        => 'невалиден DSN',
-            DB_ERROR_CONNECT_FAILED     => 'връзката не може да бъде осъществена',
-            0	                        => 'няма грешки', // DB_OK
-            DB_ERROR_NEED_MORE_DATA     => 'предоставените данни са недостатъчни',
-            DB_ERROR_EXTENSION_NOT_FOUND=> 'разширението не е намерено',
-            DB_ERROR_NOSUCHDB           => 'несъществуваща база данни',
-            DB_ERROR_ACCESS_VIOLATION   => 'нямате достатъчно права'
+            DB_ERROR_NOT_FOUND          => '  ',
+            DB_ERROR_NOT_LOCKED         => '  ',
+            DB_ERROR_SYNTAX             => ' ',
+            DB_ERROR_UNSUPPORTED        => '  ',
+            DB_ERROR_VALUE_COUNT_ON_ROW => '    ',
+            DB_ERROR_INVALID_DSN        => ' DSN',
+            DB_ERROR_CONNECT_FAILED     => '     ',
+            0	                        => ' ', // DB_OK
+            DB_ERROR_NEED_MORE_DATA     => '   ',
+            DB_ERROR_EXTENSION_NOT_FOUND=> '   ',
+            DB_ERROR_NOSUCHDB           => '  ',
+            DB_ERROR_ACCESS_VIOLATION   => '  '
 );
 ?>
 		
