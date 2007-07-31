9d1e2185769482aea0f267b134315fa2bfb79416
moodler <moodler>
2007-07-31 17:02:43 +0000

    Fixing copyright years

--- grade/export/ods/grade_export_ods.php
+++ grade/export/ods/grade_export_ods.php
@@ -6,7 +6,7 @@
 // Moodle - Modular Object-Oriented Dynamic Learning Environment         //
 //          http://moodle.com                                            //
 //                                                                       //
-// Copyright (C) 2001-2003  Martin Dougiamas  http://dougiamas.com       //
+// Copyright (C) 1999 onwards  Martin Dougiamas  http://moodle.com       //
 //                                                                       //
 // This program is free software; you can redistribute it and/or modify  //
 // it under the terms of the GNU General Public License as published by  //
