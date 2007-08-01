b20ce924ee54d503f5b45265acecb3a5f62a6364
moodler <moodler>
2007-08-01 09:07:42 +0000

    Just trying some default settings here    MDL-10529

--- admin/settings/server.php
+++ admin/settings/server.php
@@ -54,8 +54,8 @@ $options['0'] = get_string('none');
 $options = array_merge($options, $charsets);
 $temp->add(new admin_setting_configselect('sitemailcharset', get_string('sitemailcharset', 'admin'), get_string('configsitemailcharset','admin'), '', $options));
 $temp->add(new admin_setting_configcheckbox('allowusermailcharset', get_string('allowusermailcharset', 'admin'), get_string('configallowusermailcharset', 'admin'), 0));
-$temp->add(new admin_setting_configtext('supportname', get_string('supportname', 'admin'), get_string('configsupportname', 'admin'), '', PARAM_NOTAGS));
-$temp->add(new admin_setting_configtext('supportemail', get_string('supportemail', 'admin'), get_string('configsupportemail', 'admin'), '', PARAM_NOTAGS));
+$temp->add(new admin_setting_configtext('supportname', get_string('supportname', 'admin'), get_string('configsupportname', 'admin'), fullname($USER), PARAM_NOTAGS));
+$temp->add(new admin_setting_configtext('supportemail', get_string('supportemail', 'admin'), get_string('configsupportemail', 'admin'), $USER->email, PARAM_NOTAGS));
 $temp->add(new admin_setting_configtext('supportpage', get_string('supportpage', 'admin'), get_string('configsupportpage', 'admin'), '', PARAM_URL));
 $ADMIN->add('server', $temp, 100);
 
