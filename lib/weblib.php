48bfa2e0c9a722224eddab070841324a8242774f
urs_hunkler <urs_hunkler>
2007-08-05 10:31:57 +0000

    Nicolas' changes to custom_corners has broken the theme. I decided to use the same method as clearfix by adding a "." and not a "&nbsp;". Some additional CSS is needed either way.

--- lib/weblib.php
+++ lib/weblib.php
@@ -5874,7 +5874,7 @@ function print_side_block_start($heading='', $attributes = array()) {
         // echo '<div class="header">'.$heading.'</div>';
         echo '<div class="header">';
         if (!empty($THEME->customcorners)) {
-            echo '<div class="bt"><div>&nbsp;</div></div>';
+            echo '<div class="bt"><div>.</div></div>';
             echo '<div class="i1"><div class="i2">';
             echo '<div class="i3">';
         }
@@ -5885,7 +5885,7 @@ function print_side_block_start($heading='', $attributes = array()) {
         echo '</div>';
     } else {
         if (!empty($THEME->customcorners)) {
-            echo '<div class="bt"><div>&nbsp;</div></div>';
+            echo '<div class="bt"><div>.</div></div>';
         }
     }
 
