4514f4bf4baf4ce1231706084946286cd0d21962
tjhunt <tjhunt>
2007-08-08 11:31:09 +0000

    MDL-10759 - Bogus get_context_instance call in quiz_print_recent_mod_activity. Merged from MOODLE_17_STABLE.

--- mod/quiz/lib.php
+++ mod/quiz/lib.php
@@ -435,7 +435,7 @@ function quiz_print_recent_mod_activity($activity, $course, $detail=false) {
 
     }
 
-    if (has_capability('mod/quiz:grade', get_context_instance(CONTEXT_MODULE, $course))) {
+    if (has_capability('mod/quiz:grade', get_context_instance(CONTEXT_MODULE, $activity->instance))) {
         $grades = "(" .  $activity->content->sumgrades . " / " . $activity->content->maxgrade . ") ";
         echo "<a href=\"$CFG->wwwroot/mod/quiz/review.php?q="
              . $activity->instance . "&amp;attempt="
