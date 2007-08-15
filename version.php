0d7b6bb3e14c610cbb66bd496a6a139ad5abddce
moodler <moodler>
2007-08-15 05:41:23 +0000

    Beta version

--- version.php
+++ version.php
@@ -6,9 +6,9 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2007081302;  // YYYYMMDD = date
+    $version = 2007081500;  // YYYYMMDD = date
                             //       XY = increments within a single day
 
-    $release = '1.9 dev';   // Human-friendly version name
+    $release = '1.9 Beta';   // Human-friendly version name
 
 ?>
