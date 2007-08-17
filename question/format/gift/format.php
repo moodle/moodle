75ca3e0d010ba56186ada9d6eb4b7fbed5f6bd19
thepurpleblob <thepurpleblob>
2007-08-17 09:46:51 +0000

    Moved check for plugin import/export to the start rather than the end

--- question/format/gift/format.php
+++ question/format/gift/format.php
@@ -222,6 +222,12 @@ class qformat_gift extends qformat_default {
         // determine QUESTION TYPE
         $question->qtype = NULL;
 
+        // give plugins first try
+        // plugins must promise not to intercept standard qtypes
+        if ($question = $this->try_importing_using_qtypes( $lines, $question, $answertext )) {
+            return $question;
+        }
+
         if ($description) {
             $question->qtype = DESCRIPTION;
         }
@@ -259,10 +265,6 @@ class qformat_gift extends qformat_default {
         }
 
         if (!isset($question->qtype)) {
-            // try for plugins
-            if ($question = $this->try_importing_using_qtypes( $lines, $question, $answertext )) {
-                return $question;
-            }
             $giftqtypenotset = get_string('giftqtypenotset','quiz');
             $this->error( $giftqtypenotset, $text );
             return false;
