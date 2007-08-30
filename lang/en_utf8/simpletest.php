ee92adc3ff779d9c19e10d5a1c2b23bd3ad35913
koenr <koenr>
2007-08-30 20:19:58 +0000

    and now with content of version 1.2

---
+++ lang/en_utf8/simpletest.php
@@ -0,0 +1,32 @@
+<?PHP // $Id$ 
+      // simpletest.php - created with Moodle 1.7 beta + (2006101003)
+
+
+$string['all'] = 'ALL';
+$string['exception'] = 'Exception';
+$string['fail'] = 'Fail';
+$string['ignorefile'] = 'Ignore tests in the file';
+$string['ignorethisfile'] = 'Re-run the tests ignoring this test file.';
+$string['moodleunittests'] = 'Moodle unit tests: $a';
+$string['notice'] = 'Notice';
+$string['onlytest'] = 'Only run tests in';
+$string['pass'] = 'Pass';
+$string['pathdoesnotexist'] = 'The path \'$a\' does not exist.';
+$string['retest'] = 'Re-run the tests';
+$string['retestonlythisfile'] = 'Re-run only this test file.';
+$string['runall'] = 'Run the tests from all the test files.';
+$string['runat'] = 'Run at $a.';
+$string['runonlyfile'] = 'Run only the tests in this file';
+$string['runonlyfolder'] = 'Run only the tests in this folder';
+$string['runtests'] = 'Run tests';
+$string['rununittests'] = 'Run the unit tests';
+$string['showpasses'] = 'Show passes as well as fails.';
+$string['showsearch'] = 'Show the search for test files.';
+$string['stacktrace'] = 'Stack trace:';
+$string['summary'] = '{$a->run}/{$a->total} test cases complete: <strong>{$a->passes}</strong> passes, <strong>{$a->fails}</strong> fails and <strong>{$a->exceptions}</strong> exceptions.';
+$string['thorough'] = 'Run a thorough test (may be slow).';
+$string['uncaughtexception'] = 'Uncaught exception [{$a->getMessage()}] in [{$a->getFile()}:{$a->getLine()}] TESTS ABORTED.';
+$string['unittests'] = 'Unit tests';
+$string['version'] = 'Using <a href=\"http://sourceforge.net/projects/simpletest/\">SimpleTest</a> version $a.';
+
+?>
