f12d645a8da29cced70ac3f219dfcd91c20469a9
stronk7 <stronk7>
2007-09-05 23:12:31 +0000

    Bump backup version!

--- backup/version.php
+++ backup/version.php
@@ -6,7 +6,7 @@
 /// database (backup_version) to determine whether upgrades should
 /// be performed (see db/backup_*.php)
 
-    $backup_version = 2007022100;   // The current version is a date (YYYYMMDDXX)
-    $backup_release = "1.9dev";     // User-friendly version number
+    $backup_version = 2007090500;   // The current version is a date (YYYYMMDDXX)
+    $backup_release = '1.9 beta +';     // User-friendly version number
 
 ?>
