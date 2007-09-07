b7a2cc89ddb95c934f4fca2e741f4e4f974f8252
diml <diml>
2007-09-07 18:56:17 +0000

    add postgress model for search

---
+++ search/db/postgres7.sql
@@ -0,0 +1,11 @@
+CREATE TABLE prefix_search_documents (
+   id SERIAL8 PRIMARY KEY,
+   docid int4 NOT NULL,
+   doctype varchar(12) NOT NULL DEFAULT 'none',
+   title varchar(100) NOT NULL default '',
+   url varchar(250) NOT NULL default '',
+   docdate timestamp NOT NULL,
+   updated timestamp NOT NULL DEFAULT NOW(),
+   courseid int4,
+   groupid int4
+);
