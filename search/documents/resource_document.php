5c8f580358639ec8712e0efc9bdbf6c358c52b5d
diml <diml>
2007-09-10 19:48:31 +0000

    Fixes wildcard exception occurence on deleting resources.

--- search/documents/resource_document.php
+++ search/documents/resource_document.php
@@ -274,7 +274,7 @@ function resource_delete($info, $itemtype) {
 */
 function resource_db_names() {
     //[primary id], [table name], [time created field name], [time modified field name], [additional where conditions for sql]
-    return array(array('id', 'resource', 'timemodified', 'timemodified', '*', " (alltext != '' AND alltext != ' ' AND alltext != '&nbsp;' AND TYPE != 'file') OR TYPE = 'file' "));
+    return array(array('id', 'resource', 'timemodified', 'timemodified', 'any', " (alltext != '' AND alltext != ' ' AND alltext != '&nbsp;' AND TYPE != 'file') OR TYPE = 'file' "));
 } //resource_db_names
 
 /**
