c92656008602a470ede2c0b45fa3f93153f768af
stronk7 <stronk7>
2007-09-13 17:51:48 +0000

    Now using one cross-db solution to be able to simulate the status boolean ins assignment submissions. MDL-8164 & MDL-10846

--- mod/assignment/lib.php
+++ mod/assignment/lib.php
@@ -796,7 +796,8 @@ class assignment_base {
 
         $select = 'SELECT u.id, u.firstname, u.lastname, u.picture,
                           s.id AS submissionid, s.grade, s.submissioncomment,
-                          s.timemodified, s.timemarked ';
+                          s.timemodified, s.timemarked,
+                          COALESCE(SIGN(SIGN(s.timemarked) + SIGN(s.timemarked - s.timemodified)), 0) AS status ';
         $sql = 'FROM '.$CFG->prefix.'user u '.
                'LEFT JOIN '.$CFG->prefix.'assignment_submissions s ON u.id = s.userid
                                                                   AND s.assignment = '.$this->assignment->id.' '.
@@ -1121,7 +1122,8 @@ class assignment_base {
 
         $select = 'SELECT u.id, u.firstname, u.lastname, u.picture,
                           s.id AS submissionid, s.grade, s.submissioncomment,
-                          s.timemodified, s.timemarked ';
+                          s.timemodified, s.timemarked,
+                          COALESCE(SIGN(SIGN(s.timemarked) + SIGN(s.timemarked - s.timemodified)), 0) AS status ';
         $sql = 'FROM '.$CFG->prefix.'user u '.
                'LEFT JOIN '.$CFG->prefix.'assignment_submissions s ON u.id = s.userid
                                                                   AND s.assignment = '.$this->assignment->id.' '.
