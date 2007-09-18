5228aa27cc5ab9aff8dfcbf8815c1736a52f2d23
urs_hunkler <urs_hunkler>
2007-09-18 17:10:32 +0000

    enhancements for messages: remove header and footer with CSS. increase user top frame height via JavaScript to view the whole user image.

--- theme/custom_corners/js/jquery.php
+++ theme/custom_corners/js/jquery.php
@@ -2,17 +2,30 @@
 
 <script type="text/javascript">
     $(document).ready(function() {
-        init();
+        script.init();
     });
 
-    var init = function() {
-        window.setTimeout(function(){$('#infowrapper').click();}, 4000);
-        $('#infowrapper').toggle(function() {
-            $('#infooverlay').animate({height: 'toggle'}, "fast");
-            $(this).animate({opacity: 0.3}, "fast");
-        }, function() {
-            $('#infooverlay').animate({height: 'toggle'}, "fast");
-            $(this).animate({opacity: 0.9}, "fast");
-        });
-    };
+    var script = {
+        corrections: function () {
+            if (top.user) {
+                top.document.getElementsByTagName('frameset')[0].rows = "117,30%,0,200";
+            }
+        },
+        
+        info: function() {
+            window.setTimeout(function(){$('#infowrapper').click();}, 4000);
+            $('#infowrapper').toggle(function() {
+                $('#infooverlay').animate({height: 'toggle'}, "fast");
+                $(this).animate({opacity: 0.3}, "fast");
+            }, function() {
+                $('#infooverlay').animate({height: 'toggle'}, "fast");
+                $(this).animate({opacity: 0.9}, "fast");
+            });
+        },
+        
+        init: function() {
+            script.corrections();
+            // script.info();
+        }
+    }
 </script>
