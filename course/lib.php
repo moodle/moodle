de9eaaac1d6041c9013c02b3109ff6d6f461aa72
skodak <skodak>
2007-09-23 13:16:53 +0000

    MDL-11414 fixed handling of log urls (problem reported by Andreas Nolden); backported from HEAD

--- course/lib.php
+++ course/lib.php
@@ -408,7 +408,7 @@ function print_log($course, $user=0, $date=0, $order="l.time ASC", $page=0, $per
 
         $log->url  = strip_tags(urldecode($log->url));   // Some XSS protection
         $log->info = strip_tags(urldecode($log->info));  // Some XSS protection
-        $log->url  = str_replace('&', '&amp;', $log->url); /// XHTML compatibility
+        $log->url  = s($log->url); /// XSS protection and XHTML compatibility - should be in link_to_popup_window() instead!!
 
         echo '<tr class="r'.$row.'">';
         if ($course->id == SITEID) {
