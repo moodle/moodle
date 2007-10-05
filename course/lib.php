eed1c7ca5b7dbbab3a3e6c427fbce6e095447bde
nicolasconnault <nicolasconnault>
2007-10-05 07:19:29 +0000

    MDL-11525 When courseid is not present in the log entry, showing "Site" instead of $log->info

--- course/lib.php
+++ course/lib.php
@@ -512,7 +512,7 @@ function print_log($course, $user=0, $date=0, $order="l.time ASC", $page=0, $per
         if ($course->id == SITEID) {
             echo "<td class=\"cell c0\">\n";
             if (empty($log->course)) {
-                echo format_string($log->info)."\n";
+                echo get_string('site') . "\n";
             } else {
                 echo "    <a href=\"{$CFG->wwwroot}/course/view.php?id={$log->course}\">". format_string($courses[$log->course])."</a>\n";
             }
