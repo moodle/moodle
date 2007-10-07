351708dcbb01d5fc4454bcd6501e2ac9d09bf35c
diml <diml>
2007-10-07 19:40:15 +0000

    string fr translations for search block

---
+++ blocks/search/lang/fr_utf8/block_search.php
@@ -0,0 +1,14 @@
+<?
+$string['blockname'] = 'Recherche globale';
+$string['configbuttonlabel'] = 'Libellé du bouton de recherche';
+$string['configenablefileindexing'] = 'Activer l\'indexation des fichiers';
+$string['configfiletypes'] = 'Types de fichiers pris en charge';
+$string['configlimitindexbody'] = 'Taille limite du corps indexé';
+$string['configpdftotextcmd'] = 'Chemin vers la commande pdftotext';
+$string['configwordtotextcmd'] = 'Chemin vers la commande doctotext';
+$string['configwordtotextenv'] = 'Réglage d\'environnement du convertisseur MSWord';
+$string['configsearchtext'] = 'Rechercher dans le texte';
+$string['go'] = 'Lancer la recherche';
+$string['bytes'] = ' octets (0 pour \"pas de limite\")';
+$string['searchmoodle'] = 'Rechercher dans le site';
+?>
