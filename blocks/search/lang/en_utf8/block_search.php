9424e7e0b54764ed64bb57dd59aede87c97dbeaa
diml <diml>
2007-10-07 19:39:42 +0000

    string en translations for search block

---
+++ blocks/search/lang/en_utf8/block_search.php
@@ -0,0 +1,14 @@
+<?
+$string['blockname'] = 'Global Search';
+$string['configbuttonlabel'] = 'Button label';
+$string['configenablefileindexing'] = 'Enable file indexing';
+$string['configfiletypes'] = 'File types handled';
+$string['configlimitindexbody'] = 'Indexed body size limitation';
+$string['configpdftotextcmd'] = 'Path to command pdftotext';
+$string['configwordtotextcmd'] = 'Path to command doctotext';
+$string['configwordtotextenv'] = 'Environment setting for the MSWord converter';
+$string['configsearchtext'] = 'Search text';
+$string['go'] = 'Go!';
+$string['bytes'] = ' bytes (0 stands for no limits)';
+$string['searchmoodle'] = 'Search Moodle';
+?>
