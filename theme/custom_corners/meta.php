ced48de808c48cf54a23b7531d05bacc6c32811c
urs_hunkler <urs_hunkler>
2007-10-12 21:42:04 +0000

    MDL-11664 :: added IE6/UE7 special CSS and because that didn't help at all places added JavaScript to correct some wrong calculated width.

--- theme/custom_corners/meta.php
+++ theme/custom_corners/meta.php
@@ -1,6 +1,18 @@
 <!--[if IE 7]>
     <link rel="stylesheet" type="text/css" href="<?php echo $CFG->themewww .'/'. current_theme() ?>/styles_ie7.css" />
+    
+	<script type="text/javascript">
+	/* <![CDATA[ */
+		var browserIE7 = true;
+	/* ]]> */
+	</script>
 <![endif]-->
 <!--[if IE 6]>
     <link rel="stylesheet" type="text/css" href="<?php echo $CFG->themewww .'/'. current_theme() ?>/styles_ie6.css" />
+    
+	<script type="text/javascript">
+	/* <![CDATA[ */
+		var browserIE6 = true;
+	/* ]]> */
+	</script>
 <![endif]-->
