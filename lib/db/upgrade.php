0597562dd41786ea1452df929037f51c302cc9ee
stronk7 <stronk7>
2007-10-14 09:54:57 +0000

    Grrr (missed to delete some lines)  MDL-11751

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -722,9 +722,6 @@ function xmldb_main_upgrade($oldversion=0) {
     /// Launch add field idnumber
         $result = $result && add_field($table, $field);
 
-    /// Launch add index idnumber
-        $result = $result && add_index($table, $index);
-
         upgrade_main_savepoint($result, 2007041100);
     }
 
