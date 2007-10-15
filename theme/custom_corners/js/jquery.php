1258810abcdc204aa7d41509ef3baeeccbfecd60
urs_hunkler <urs_hunkler>
2007-10-15 07:13:30 +0000

    removed the javascript to correct the header display :: IE7 hangs. Need to find a different solution.

--- theme/custom_corners/js/jquery.php
+++ theme/custom_corners/js/jquery.php
@@ -1,6 +1,6 @@
 <script src="<?php echo $CFG->themewww .'/'. current_theme() ?>/js/jquery-latest.pack.js" type="text/javascript"></script>
 
-<script type="text/javascript">
+<script type="text/javascript" charset="utf-8">
 /* <![CDATA[ */
     var script = {
         corrections: function () {
@@ -8,37 +8,19 @@
                 top.document.getElementsByTagName('frameset')[0].rows = "117,30%,0,200";
             }
             
+            var w;
+            
             // correct sideblock width for MSIE for columns with a calender 
             if (window.browserIE6 !== undefined || window.browserIE7 !== undefined) {
                 var calendar = $('#right-column .block_calendar_month');
                 if (calendar.length) {
-                    var w = $('#right-column').width();
+                    w = $('#right-column').width();
                     $('#right-column .sideblock').width(w);
                 }
                 calendar = $('#left-column .block_calendar_month');
                 if (calendar.length) {
                     w = $('#left-column').width();
                     $('#left-column .sideblock').width(w);
-                }
-            }
-            if (window.browserIE6 !== undefined) {
-                if ($('header-home')) {
-                    w = $('#header-home').width();
-                    $('#header-home .i2').width(w - 24);
-                }
-                if ($('header')) {
-                    w = $('#header').width();
-                    $('#header .i2').width(w - 24);
-                }
-            }
-            if (window.browserIE7 !== undefined) {
-                if ($('header-home')) {
-                    w = $('#header-home').width();
-                    $('#header-home .i2').width(w - 28);
-                }
-                if ($('header')) {
-                    w = $('#header').width();
-                    $('#header .i2').width(w - 24);
                 }
             }
         },
