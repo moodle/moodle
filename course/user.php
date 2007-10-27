5be4614e23f1b2e3e5b6dc8b35b4194e3dad8b7f
skodak <skodak>
2007-10-27 18:22:52 +0000

    MDL-11718 minor fix for grades in user profile

--- course/user.php
+++ course/user.php
@@ -69,18 +69,22 @@
 
     switch ($mode) {
         case "grade":
+            //TODO: make the report selectable somehow
             $course = get_record('course', 'id', required_param('id', PARAM_INT));
             if (!empty($course->showgrades)) {
+                require_once $CFG->libdir.'/gradelib.php';
                 require_once $CFG->dirroot.'/grade/lib.php';
                 require_once $CFG->dirroot.'/grade/report/user/lib.php';
-                $context     = get_context_instance(CONTEXT_COURSE, $id);
-                /// return tracking object
-                $gpr = new grade_plugin_return(array('type'=>'report', 'plugin'=>'user', 'courseid'=>$id, 'userid'=>$user->id));
-                // Create a report instance
-                $report = new grade_report_user($id, $gpr, $context, $user->id);
 
-                $gradetotal = 0;
-                $gradesum = 0;
+                $context = get_context_instance(CONTEXT_COURSE, $course->id);
+
+                //first make sure we have proper final grades - this must be done before constructing of the grade tree
+                grade_regrade_final_grades($course->id);
+
+                /// return tracking object
+                $gpr = new grade_plugin_return(array('type'=>'report', 'plugin'=>'user', 'courseid'=>$course->id, 'userid'=>$user->id));
+                // Create a report instance
+                $report = new grade_report_user($course->id, $gpr, $context, $user->id);
 
                 // print the page
                 print_heading(get_string('modulename', 'gradereport_user'). ' - '.fullname($report->user));
@@ -88,7 +92,6 @@
                 if ($report->fill_table()) {
                     echo $report->print_table(true);
                 }
-                // print_student_grade($user, $course);
             }
             break;
 
