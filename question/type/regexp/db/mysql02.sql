691d35efc29c1b8fd241fc2cd6e51bc64d5d8826
joseph_rezeau <joseph_rezeau>
2007-10-28 08:38:23 +0000

    removed mistakenly committed regexp plugin

--- question/type/regexp/db/mysql02.sql
+++
@@ -1,8 +0,0 @@
-
-
--- 
--- Add the usehint field to table `prefix_question_regexp`
--- 
-
-ALTER TABLE `prefix_question_regexp` ADD `usehint` TINYINT( 2 ) NULL DEFAULT '0';
--- --------------------------------------------------------
--- question/type/regexp/version.php
+++
@@ -1,6 +0,0 @@
-<?PHP // $Id$
-
-$plugin->version  = 2007012800;
-$plugin->requires = 2006032200;
-
-?>
