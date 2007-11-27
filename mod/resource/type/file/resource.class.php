98ac187b1ac367c968c2a19f423ca12553d42ada
nicolasconnault <nicolasconnault>
2007-11-27 10:31:50 +0000

    MDL-12098 Printing a closing div tag and the moodle footer

--- mod/resource/type/file/resource.class.php
+++ mod/resource/type/file/resource.class.php
@@ -609,6 +609,8 @@ class resource_file extends resource_base {
 
             if ($inpopup) {
                 echo "<div class=\"popupnotice\">(<a href=\"$fullurl\">$strdirectlink</a>)</div>";
+                echo "</div>"; // MDL-12098
+                print_footer($course); // MDL-12098
             } else {
                 print_spacer(20,20);
                 print_footer($course);
