dbb552852a278ad199bbfd16de0ce575af5f5c42
nicolasconnault <nicolasconnault>
2007-12-04 10:27:14 +0000

    MDL-12389 Removed the selectallornone element in favour of a more generic submitlink element. The checkbox controller is now added to a form using moodleform::add_checkbox_controller.

---
+++ lib/form/submitlink.php
@@ -0,0 +1,18 @@
+<?php
+global $CFG;
+require_once("$CFG->libdir/form/submit.php");
+class MoodleQuickForm_submitlink extends MoodleQuickForm_submit {
+    var $_js;
+    var $_onclick;
+
+    function toHtml() {
+        $text = $this->_attributes['value'];
+        $onmouseover = "window.status=\'" . $text . "\';";
+        $onmouseout = "window.status=\'\';"; 
+
+        return "<noscript>" . parent::toHtml() . '</noscript><script type="text/javascript">' . $this->_js . "\n" 
+             . 'document.write(\'<a href="#" onclick="' . $this->_onclick . '" onmouseover="' . $onmouseover . '" onmouseout="' . $onmouseout . '">' 
+             . $text . "</a>');\n</script>";
+    }
+}
+?>
