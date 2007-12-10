0db058ef58318280eea6f1c26d5651a1e0d81bc5
stronk7 <stronk7>
2007-12-10 09:37:34 +0000

    Backporting MDL-12407 to 18_STABLE. Fix was missing there.

--- userpix/index.php
+++ userpix/index.php
@@ -23,7 +23,7 @@
     
     foreach ($users as $user) {
         $fullname = fullname($user);
-        echo "<a href=\"$CFG->wwwroot/user/view.php?id=$user->id&amp;course=1\"".
+        echo "<a href=\"$CFG->wwwroot/user/view.php?id=$user->id&amp;course=1\" ".
              "title=\"$fullname\">";
         if ($CFG->slasharguments) {        // Use this method if possible for better caching
             echo '<img src="'. $CFG->wwwroot .'/user/pix.php/'.$user->id.'/f1.jpg"'.
