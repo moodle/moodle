075e3c715dc25d07ae8fe588b0693c73ccb14ea4
scyrma <scyrma>
2007-12-18 07:53:32 +0000

    MDL-12613 - this should fix the problem with the search block

--- mod/forum/lib.php
+++ mod/forum/lib.php
@@ -2817,7 +2817,7 @@ function forum_search_form($course, $search='') {
     $output .= '<form action="'.$CFG->wwwroot.'/mod/forum/search.php" style="display:inline">';
     $output .= '<fieldset class="invisiblefieldset">';
     $output .= helpbutton('search', get_string('search'), 'moodle', true, false, '', true);
-    $output .= '<input name="search" type="text" size="18" value="'.$search.'" alt="search" />';
+    $output .= '<input name="search" type="text" size="18" value="'.s($search).'" alt="search" />';
     $output .= '<input value="'.get_string('searchforums', 'forum').'" type="submit" />';
     $output .= '<input name="id" type="hidden" value="'.$course->id.'" />';
     $output .= '</fieldset>';
