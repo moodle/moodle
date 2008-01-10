ba4aea2b352c68f7a12803d97d38f972637033ea
poltawski <poltawski>
2008-01-10 16:10:07 +0000

    MDL-12915 - fixed require of removed file

--- lib/htmlpurifier/HTMLPurifier/AttrDef/URI/Email.php
+++ lib/htmlpurifier/HTMLPurifier/AttrDef/URI/Email.php
@@ -15,4 +15,4 @@ class HTMLPurifier_AttrDef_URI_Email extends HTMLPurifier_AttrDef
 }
 
 // sub-implementations
-require_once 'HTMLPurifier/AttrDef/URI/Email/SimpleCheck.php';
+//moodlefix require_once 'HTMLPurifier/AttrDef/URI/Email/SimpleCheck.php';
