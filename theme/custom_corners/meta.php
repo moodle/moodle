64891726571b93330912e711ce380ae0f6d2b408
urs_hunkler <urs_hunkler>
2008-01-16 17:19:03 +0000

    MDL-12536 :: changed customtheme call to fixed custom_corners call.

--- theme/custom_corners/meta.php
+++ theme/custom_corners/meta.php
@@ -1,5 +1,5 @@
 <!--[if IE 7]>
-    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->themewww .'/'. current_theme() ?>/styles_ie7.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->themewww ?>/custom_corners/styles_ie7.css" />
     
 	<script type="text/javascript">
 	/* <![CDATA[ */
@@ -8,7 +8,7 @@
 	</script>
 <![endif]-->
 <!--[if IE 6]>
-    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->themewww .'/'. current_theme() ?>/styles_ie6.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->themewww ?>/custom_corners/styles_ie6.css" />
     
 	<script type="text/javascript">
 	/* <![CDATA[ */
