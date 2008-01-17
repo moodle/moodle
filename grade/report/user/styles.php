d08bba8354fea13a4693c56f648d0951bbda6db6
nicolasconnault <nicolasconnault>
2008-01-17 15:08:09 +0000

    MDL-12122 Completed the change to a more useful user report for the admin, with a few extra bits.

--- grade/report/user/styles.php
+++ grade/report/user/styles.php
@@ -13,6 +13,11 @@
   padding-right: 5px;
 }
 
+#graded_users_selector {
+  float: right;
+  text-align: right;
+}
+
 /* this must be last if we want to override other category and course item colors */
 .grade-report-user table#user-grade .hidden,
 .grade-report-user table#user-grade .hidden a {
