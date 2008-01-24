6a581514b45d567b081c056a21886e90abda2802
nicolasconnault <nicolasconnault>
2008-01-24 14:46:01 +0000

    Fixing $Id: tag

--- lib/adodb/adodb-xmlschema03.inc.php
+++ lib/adodb/adodb-xmlschema03.inc.php
@@ -12,7 +12,7 @@
  *
  * Last Editor: $Author$
  * @author Richard Tango-Lowy & Dan Cech
- * @version $Revision$
+ * @version $Id$
  *
  * @package axmls
  * @tutorial getting_started.pkg
@@ -1309,7 +1309,7 @@ class dbQuerySet extends dbObject {
 * @tutorial getting_started.pkg
 *
 * @author Richard Tango-Lowy & Dan Cech
-* @version $Revision$
+* @version $Id$
 *
 * @package axmls
 */
