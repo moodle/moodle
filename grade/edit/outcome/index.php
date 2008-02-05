816219141b1cfb8a913a9f276906db5e1f27224d
scyrma <scyrma>
2008-02-05 08:38:44 +0000

    MDL-10639. Initial commit of an import/export system for outcomes (and associated scales).

--- grade/edit/outcome/index.php
+++ grade/edit/outcome/index.php
@@ -38,7 +38,7 @@ if ($courseid) {
     }
     require_login($course);
     $context = get_context_instance(CONTEXT_COURSE, $course->id);
-    require_capability('moodle/grade:manage', $context);
+    require_capability('moodle/grade:manageoutcomes', $context);
 
     if (empty($CFG->enableoutcomes)) {
         redirect('../../index.php?id='.$courseid);
@@ -69,6 +69,7 @@ $srtcreatenewoutcome = get_string('outcomecreate', 'grades');
 $stritems            = get_string('items', 'grades');
 $strcourses          = get_string('courses');
 $stredit             = get_string('edit');
+$strexport           = get_string('export', 'grades');
 
 switch ($action) {
     case 'delete':
@@ -115,7 +116,7 @@ if ($courseid) {
     $caneditcoursescales = $caneditsystemscales;
 }
 
-
+print('<form action="export.php" method="post">' ."\n");
 
 if ($courseid and $outcomes = grade_outcome::fetch_all_local($courseid)) {
 
@@ -157,19 +158,25 @@ if ($courseid and $outcomes = grade_outcome::fetch_all_local($courseid)) {
                         " src=\"$CFG->pixpath/t/delete.gif\" class=\"iconsmall\" alt=\"$strdelete\" /></a> ";
         }
         $line[] = $buttons;
+        
+        $buttons = '<input type="checkbox" name="export[]" value="'. $outcome->id .'">';
+        $line[] = $buttons;
+
         $data[] = $line;
     }
     $table = new object();
-    $table->head  = array($strfullname, $strshortname, $strscale, $stritems, $stredit);
-    $table->size  = array('30%', '20%', '20%', '20%', '10%');
-    $table->align = array('left', 'left', 'left', 'center', 'center');
+    $table->head  = array($strfullname, $strshortname, $strscale, $stritems, $stredit, $strexport);
+    $table->size  = array('30%', '18%', '18%', '18%', '8%', '8%' );
+    $table->align = array('left', 'left', 'left', 'center', 'center', 'center');
     $table->width = '90%';
     $table->data  = $data;
     print_table($table);
 }
 
+
 if ($outcomes = grade_outcome::fetch_all_global()) {
-    print_heading($strstandardoutcome);
+    
+    print_heading($strstandardoutcome); 
     $data = array();
     foreach($outcomes as $outcome) {
         $line = array();
@@ -210,12 +217,16 @@ if ($outcomes = grade_outcome::fetch_all_global()) {
                         " src=\"$CFG->pixpath/t/delete.gif\" class=\"iconsmall\" alt=\"$strdelete\" /></a> ";
         }
         $line[] = $buttons;
+
+        $buttons = '<input type="checkbox" name="export[]" value="'. $outcome->id .'">';
+        $line[] = $buttons;
+
         $data[] = $line;
     }
     $table = new object();
-    $table->head  = array($strfullname, $strshortname, $strscale, $strcourses, $stritems, $stredit);
-    $table->size  = array('30%', '20%', '20%', '10%', '10%', '10%');
-    $table->align = array('left', 'left', 'left', 'center', 'center', 'center');
+    $table->head  = array($strfullname, $strshortname, $strscale, $strcourses, $stritems, $stredit, $strexport);
+    $table->size  = array('30%', '19%', '19%', '8%', '8%', '8%', '8%');
+    $table->align = array('left', 'left', 'left', 'center', 'center', 'center', 'center');
     $table->width = '90%';
     $table->data  = $data;
     print_table($table);
@@ -223,7 +234,42 @@ if ($outcomes = grade_outcome::fetch_all_global()) {
 
 
 echo '<div class="buttons">';
+echo "<input type=\"hidden\" name=\"sesskey\" value=\"$USER->sesskey\" />";
+print('<input type="submit" value="'. get_string('exportselectedoutcomes', 'grades') .'" name="export_outcomes"></form>');
 print_single_button('edit.php', array('courseid'=>$courseid), $srtcreatenewoutcome);
+echo '</div>';
+
+echo '<div>'; 
+$upload_max_filesize = get_max_upload_file_size($CFG->maxbytes);
+$filesize = display_size($upload_max_filesize);
+
+$strimportoutcomes = get_string('importoutcomes', 'grades');
+$struploadthisfile = get_string('uploadthisfile');
+$strimportcustom = get_string('importcustom', 'grades');
+$strimportstandard = get_string('importstandard', 'grades');
+$strmaxsize = get_string("maxsize", "", $filesize);
+
+require_once($CFG->dirroot.'/lib/uploadlib.php');
+
+echo '<div>';
+echo '<form enctype="multipart/form-data" method="post" action="import.php">';
+echo '<input type="hidden" name="action" value="upload" />';
+echo '<input type="hidden" name="id" value="'. $courseid .'" />';
+echo '<input type="hidden" name="sesskey" value="'. $USER->sesskey .'" />';
+echo '<table class="generalbox boxaligncenter" width="50%" cellspacing="1" cellpadding="5">';
+if ($courseid && has_capability('moodle/grade:manage', get_context_instance(CONTEXT_SYSTEM))) {
+    echo '<tr><td><ul style="list-style-type:none;">';
+    echo '<li><label><input type="radio" name="scope" value="local" checked="checked">'. $strimportcustom .'</label>';
+    echo '<li><label><input type="radio" name="scope" value="global">'. $strimportstandard .'</label>';
+    echo '</ul></td></tr>';
+}
+echo '<tr><td><p>'. $strimportoutcomes .'('. $strmaxsize .')</p></td></tr>';
+echo '<tr><td>'. 
+    upload_print_form_fragment(1,array('userfile'),null,false,null,$upload_max_filesize,0,true) .
+    '<input type="submit" name="save" value="'. $struploadthisfile .'" /></td></tr>';
+echo '</table>';
+echo '</div>';
+echo '</form>';
 echo '</div>';
 
 if ($courseid) {
