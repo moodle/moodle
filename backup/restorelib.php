d916897125e1552458a217664abfa777f8da8b5e
stronk7 <stronk7>
2008-02-05 20:28:29 +0000

    Some old backups can contain empties in user->picture, fix them before insert.

--- backup/restorelib.php
+++ backup/restorelib.php
@@ -2321,7 +2321,8 @@
                     // relink the descriptions
                     $user->description = stripslashes($user->description);
 
-                    if (!empty($CFG->disableuserimages)) {
+                /// Disable pictures based on global setting or existing empty value (old backups can contain wrong empties)
+                    if (!empty($CFG->disableuserimages) || empty($user->picture)) {
                         $user->picture = 0;
                     }
 
