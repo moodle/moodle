de6f158f2b2d5b316a5e9bd5a2d6a8dcce347631
nicolasconnault <nicolasconnault>
2008-02-06 10:25:45 +0000

    MDL-6826 Removed the optional_param lines at the top, and set the submit buttons to PARAM_RAW. Also changed !empty() to isset(); Merging from 1.9

--- course/moodleform_mod.php
+++ course/moodleform_mod.php
@@ -324,6 +324,7 @@ class moodleform_mod extends moodleform {
         }
 
         $mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
+        $mform->setType('buttonar', PARAM_RAW);
         $mform->closeHeaderBefore('buttonar');
     }
 }
