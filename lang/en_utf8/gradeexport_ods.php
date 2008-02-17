1f30b89fd87bf8ae8c29e6f53076eb7a6ddb4aba
skodak <skodak>
2008-02-17 19:47:25 +0000

    MDL-11811 OpenDocument is correct name for file format

--- lang/en_utf8/gradeexport_ods.php
+++ lang/en_utf8/gradeexport_ods.php
@@ -1,6 +1,6 @@
 <?PHP // $Id$
 
-$string['modulename'] = 'OpenOffice spreadsheet';
+$string['modulename'] = 'OpenDocument spreadsheet';
 $string['ods:view'] = 'Use Openoffice grade export';
 $string['ods:publish'] = 'Publish ODS grade export';
 
