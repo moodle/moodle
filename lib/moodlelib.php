cee5f24cc365b98001d95d5d3494d9a8685e6846
skodak <skodak>
2008-02-18 23:38:37 +0000

    MDL-12382 allow admins to access any course - including courses where they have guest role and course does not allow guests; merged from MOODLE_19_STABLE

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -1752,7 +1752,7 @@ function require_login($courseorid=0, $autologinguest=true, $cm=null) {
         if (has_capability('moodle/legacy:guest', $context, NULL, false)) {
             if (has_capability('moodle/site:doanything', get_context_instance(CONTEXT_SYSTEM))) {
                 // administrators must be able to access any course - even if somebody gives them guest access
-                return true;
+                return;
             }
 
             switch ($COURSE->guest) {    /// Check course policy about guest access
