2dc20107b98c44bd47f278ffc13229c140e74c80
urs_hunkler <urs_hunkler>
2008-02-19 07:51:29 +0000

    Added h1 - h4 fontsizes. Disabled the Chameleon engine by default. Wrote a short hint about the intearactive editing in the README.

--- theme/chameleon/config.php
+++ theme/chameleon/config.php
@@ -127,10 +127,9 @@ $THEME->makenavmenulist = false;
 
 $THEME->chameleonenabled = false;
 
-/// By setting this to false it disables editing of the stylsheets
-/// this saves the overhead of loading chameleon on each page 
-/// viewed by a user who can edit pages. it is recommended to 
-/// set this to false once you're satisfied with your theme.
+/// By setting this to true it enables editing of the stylsheets.
+/// It is recommended to set this to false again once you're 
+/// satisfied with your theme.
 
 $THEME->chameleonteachereditenabled = true;
 
