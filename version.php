0e9e5927226c68504da999f6b731ab29ef235a69
scyrma <scyrma>
2008-02-21 01:13:26 +0000

    MDL-13553 - merge from 1.9

--- version.php
+++ version.php
@@ -6,8 +6,9 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2007101508.03;  // YYYYMMDD = date
-                            //       XY = increments within a single day
+    $version = 2007101508.04;  // YYYYMMDD   = date of the 1.9 branch (don't change)
+                            //         X  = release number 1.9.[0,1,2,3...]
+                            //          Y = micro-increments between releases
 
     $release = '2.0 dev';   // Human-friendly version name
 
