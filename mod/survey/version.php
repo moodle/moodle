aaa252dbc1884d418db960491d0fcf92ef76e8aa
moodler <moodler>
2008-03-03 12:02:12 +0000

    Bumoping version of module too

--- mod/survey/version.php
+++ mod/survey/version.php
@@ -5,7 +5,7 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2007020200;
+$module->version  = 2007101509;
 $module->requires = 2007101509;  // Requires this Moodle version
 $module->cron     = 0;
 
