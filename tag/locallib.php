31d4d33fd619fc00dde0c32fe976cf69ecf661b8
scyrma <scyrma>
2008-03-05 01:46:11 +0000

    MDL-13769 - Fix for postgresql

--- tag/locallib.php
+++ tag/locallib.php
@@ -23,7 +23,7 @@ function tag_print_cloud($nr_of_tags=150, $return=false) {
 
     $tagcloud = get_records_sql('SELECT tg.rawname, tg.id, tg.name, tg.tagtype, COUNT(ti.id) AS count, tg.flag '.
         'FROM '. $CFG->prefix .'tag_instance ti INNER JOIN '. $CFG->prefix .'tag tg ON tg.id = ti.tagid '.
-        'GROUP BY tg.id, tg.rawname, tg.name, tg.flag '.
+        'GROUP BY tg.id, tg.rawname, tg.name, tg.flag, tg.tagtype '.
         'ORDER BY count DESC, tg.name ASC', 0, $nr_of_tags);
 
     $totaltags  = count($tagcloud);
