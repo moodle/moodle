e4c6b6b4704ee0b12bc65feac01fa65b8835d46b
poltawski <poltawski>
2008-03-20 12:19:26 +0000

    MDL-13994 - lang typo, thanks to Charles Kelly

--- lang/en_utf8/auth.php
+++ lang/en_utf8/auth.php
@@ -29,7 +29,7 @@ $string['auth_remove_suspend'] ='Suspend internal';
 $string['auth_remove_delete'] ='Full delete internal';
 
 // nologin plugin
-$string['auth_nologindescription'] = 'Auxiliary plugin that prevents user to login into system and also discards any mail send to user. Can be used to <em>suspend</em> user accounts.';
+$string['auth_nologindescription'] = 'Auxiliary plugin that prevents user to login into system and also discards any mail sent to the user. Can be used to <em>suspend</em> user accounts.';
 $string['auth_nologintitle'] = 'No login';
 
 // CAS plugin
