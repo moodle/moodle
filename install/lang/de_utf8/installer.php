c4826181c843790fa588e57987e07d07df43c70b
moodlerobot <moodlerobot>
2008-03-25 03:22:08 +0000

    Automatic installer.php lang files by installer_builder (20080325)

--- install/lang/de_utf8/installer.php
+++ install/lang/de_utf8/installer.php
@@ -387,5 +387,6 @@ $string['wrongsourcebase'] = 'Falsche URL-Quelle';
 $string['wrongzipfilename'] = 'Falscher ZIP-Dateiname';
 $string['wwwroot'] = 'Web-Adresse';
 $string['wwwrooterror'] = 'Diese Web-Adresse scheint nicht gültig zu sein. Moodle ist nicht unter dieser Adresse installiert.';
+$string['xmlrpcrecommended'] = 'Die Installation der optionalen xmlrpc Erweiterung ist für die Moodle-Network Funktionen nützlich.';
 $string['yourchoice'] = 'Ihre Wahl :';
 ?>
