e4d75c6f2e63e959ac3978bba8e21fbf5a4a1b19
dongsheng <dongsheng>
2008-03-26 02:26:25 +0000

    MDL-12531 add institution to lockable fileds, thanks for your patch, Lgor Plisco

--- admin/auth_config.php
+++ admin/auth_config.php
@@ -50,7 +50,7 @@ if ($frm = data_submitted() and confirm_sesskey()) {
     $frm = get_config("auth/$auth");
 }
 
-$user_fields = array("firstname", "lastname", "email", "phone1", "phone2", "department", "address", "city", "country", "description", "idnumber", "lang");
+$user_fields = array("firstname", "lastname", "email", "phone1", "phone2", "institution", "department", "address", "city", "country", "description", "idnumber", "lang");
 
 /// Get the auth title (from core or own auth lang files)
     $authtitle = $authplugin->get_title();
