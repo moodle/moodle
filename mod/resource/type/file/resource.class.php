c691bebc561a460bcc944886b0357377e6019f3d
stronk7 <stronk7>
2008-04-01 22:17:58 +0000

    Force download checkbox is not initialized properly, credit goes to skodak. MDL-14010 ; merged from 19_STABLE

--- mod/resource/type/file/resource.class.php
+++ mod/resource/type/file/resource.class.php
@@ -131,7 +131,10 @@ class resource_file extends resource_base {
         global $RESOURCE_WINDOW_OPTIONS;
         $alloptions = $RESOURCE_WINDOW_OPTIONS;
 
-        if ($resource->windowpopup) {
+        if ($resource->forcedownload) {
+            $resource->options = 'forcedownload';
+
+        } else if ($resource->windowpopup) {
             $optionlist = array();
             foreach ($alloptions as $option) {
                 $optionlist[] = $option."=".$resource->$option;
@@ -160,10 +163,6 @@ class resource_file extends resource_base {
             }
             unset($resource->$parsename);
             unset($resource->$parametername);
-        }
-
-        if ($resource->forcedownload) {
-            $resource->options = 'forcedownload';
         }
 
         $resource->alltext = implode(',', $optionlist);
@@ -664,7 +663,10 @@ class resource_file extends resource_base {
 
     function setup_preprocessing(&$defaults){
 
-        if (!isset($defaults['popup'])) {
+        if (isset($defaults['options']) and $defaults['options'] === 'forcedownload') {
+            $defaults['forcedownload'] = 1;
+
+        } else if (!isset($defaults['popup'])) {
             // use form defaults
 
         } else if (!empty($defaults['popup'])) {
