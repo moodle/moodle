a0a264068f8eef19d16264f9311eeda621b96c59
skodak <skodak>
2008-04-01 20:24:00 +0000

    MDL-13792 reverting previous commits - the use of urlencode() is not correct

--- mod/assignment/type/uploadsingle/assignment.class.php
+++ mod/assignment/type/uploadsingle/assignment.class.php
@@ -23,9 +23,9 @@ class assignment_uploadsingle extends assignment_base {
                     $icon = mimeinfo('icon', $file);
                     
                     if ($CFG->slasharguments) {
-                        $ffurl = "$CFG->wwwroot/file.php/". urlencode($filearea) .'/'. urlencode($file);
+                        $ffurl = "$CFG->wwwroot/file.php/$filearea/$file";
                     } else {
-                        $ffurl = "$CFG->wwwroot/file.php?file=/". urlencode($filearea) .'/'. urlencode($file);
+                        $ffurl = "$CFG->wwwroot/file.php?file=/$filearea/$file";
                     }
                     //died right here
                     //require_once($ffurl);                
