7c68f667ba24853801e5b898e41de81ce0b7af8a
wildgirl <wildgirl>
2008-04-10 14:18:40 +0000

    MDL-14307 lang string typo fixes

--- lang/en_utf8/debug.php
+++ lang/en_utf8/debug.php
@@ -8,11 +8,11 @@ $string['cannotupdaterelease'] = 'ERROR: Could not update release version in dat
 $string['cannotsetupsite'] = 'Serious Error! Could not set up the site!';
 $string['configmoodle'] = 'Moodle has not been configured yet. You need to edit config.php first.';
 $string['dbnotinsert'] = 'Database error - Cannot insert ($a)';
-$string['dbnotupdate'] = 'Database error - Cannot update ($a)'
+$string['dbnotupdate'] = 'Database error - Cannot update ($a)';
 $string['dbnotsupport'] = 'Error: Your database ($a) is not yet fully supported by Moodle or install.xml is not present. See the lib/db directory.';
 $string['dbnotsetup'] = 'Error: Main databases NOT set up successfully';
-$string['doesnotworkwitholdversion'] = 'This script does not work with this old verion of moodle';
-$string['erroroccur'] = 'An error has occured during this process';
+$string['doesnotworkwitholdversion'] = 'This script does not work with this old version of Moodle';
+$string['erroroccur'] = 'An error has occurred during this process';
 $string['fixsetting'] = 'Please fix your settings in config.php: <p>You have:</p> <p>\$CFG->dirroot = \"$a[0]\";</p> <p>but it should be:</p> <p>\$CFG->dirroot = \"$a[1]\"</p>';
 $string['notables'] = 'No Tables!';
 $string['phpvaroff'] = 'The PHP server variable \'$a[0]\' should be Off - $a[1]';
