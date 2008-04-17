69ef0b666c6b14daa6414ac35e5f74c3f5bfa678
dongsheng <dongsheng>
2008-04-17 05:57:37 +0000

    MDL-11511, make logic more clear

--- mod/data/lib.php
+++ mod/data/lib.php
@@ -955,16 +955,18 @@ function data_print_template($template, $records, $data, $search='',$page=0, $re
         $patterns[]='##edit##';
         $patterns[]='##delete##';
         $isteacher = has_capability('mod/data:manageentries', $context);
-        $isowner = data_isowner($record->id);
+        $isowner   = data_isowner($record->id);
+        $canedit   = $isteacher or ($isowner && has_capability('mod/data:editownentries', $context));
+        $candelete = $isteacher or ($isowner && has_capability('mod/data:deleteownentries', $context));
 
-        if ($isteacher or ($isowner && has_capability('mod/data:editownentries', $context)) ) {
+        if ($canedit) {
             $replacement[] = '<a href="'.$CFG->wwwroot.'/mod/data/edit.php?d='
                              .$data->id.'&amp;rid='.$record->id.'&amp;sesskey='.sesskey().'"><img src="'.$CFG->pixpath.'/t/edit.gif" class="iconsmall" alt="'.get_string('edit').'" title="'.get_string('edit').'" /></a>';
         }else{
             $replacement[] = '';
         }
 
-        if($isteacher or ($isowner && has_capability('mod/data:deleteownentries', $context)) ) {
+        if($candelete) {
             $replacement[] = '<a href="'.$CFG->wwwroot.'/mod/data/view.php?d='
                              .$data->id.'&amp;delete='.$record->id.'&amp;sesskey='.sesskey().'"><img src="'.$CFG->pixpath.'/t/delete.gif" class="iconsmall" alt="'.get_string('delete').'" title="'.get_string('delete').'" /></a>';
         }else{
