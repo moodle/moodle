de09e4e4455bb70a5149ee6465a7d65f6f179e2c
stronk7 <stronk7>
2008-04-21 18:12:01 +0000

    Replace custom casting code by proper sql_cast_char2int() core function. MDL-12893 ; backported from HEAD

--- mod/data/field/date/field.class.php
+++ mod/data/field/date/field.class.php
@@ -83,7 +83,8 @@ class data_field_date extends data_field_base {
     }
 
     function get_sort_sql($fieldname) {
-        return 'CAST('.$fieldname.' AS unsigned)';
+
+         return sql_cast_char2int($fieldname, true);
     }
 
 
