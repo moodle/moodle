97344ae331ff427b6f5fda783af2a82b1224b1b9
tjhunt <tjhunt>
2008-04-23 14:31:21 +0000

    Improve PHPdocs on a function.

--- mod/quiz/locallib.php
+++ mod/quiz/locallib.php
@@ -171,6 +171,8 @@ function quiz_load_attempt($attemptid) {
 
 /**
  * Delete a quiz attempt.
+ * @param mixed $attempt an integer attempt id or an attempt object (row of the quiz_attempts table).
+ * @param object $quiz the quiz object.
  */
 function quiz_delete_attempt($attempt, $quiz) {
     if (is_numeric($attempt)) {
