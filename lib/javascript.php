7987cbb64c0dee67a6c6d746b127813156049cca
jerome <jerome>
2008-05-02 02:52:48 +0000

    MDL-14439 IE and Opera fire the onchange when ever you move into a dropdwown list with the keyboard - this changes fix the problem. Merged from 19.

--- lib/javascript.php
+++ lib/javascript.php
@@ -23,6 +23,7 @@
 <script type="text/javascript" src="<?php echo $CFG->httpswwwroot ?>/lib/overlib.js"></script>
 <script type="text/javascript" src="<?php echo $CFG->httpswwwroot ?>/lib/cookies.js"></script>
 <script type="text/javascript" src="<?php echo $CFG->httpswwwroot ?>/lib/ufo.js"></script>
+<script type="text/javascript" src="<?php echo $CFG->httpswwwroot ?>/lib/dropdown.js"></script>  
 
 <script type="text/javascript" defer="defer">
 
