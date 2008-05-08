44eb3b6e9eb0c5174dadb07afeddf5afaa743b26
jamiesensei <jamiesensei>
2008-05-08 09:53:31 +0000

    For the overview report : MDL-14199 "Option to only show / export final grade" small improvement to UI. Include highlighted message in the form about what the current grading method is.
    Merged from 1.9 branch.

--- mod/quiz/report/overview/report.php
+++ mod/quiz/report/overview/report.php
@@ -66,7 +66,7 @@ class quiz_report extends quiz_default_report {
 
         $reporturl = new moodle_url($CFG->wwwroot.'/mod/quiz/report.php', $pageoptions);
         $qmsubselect = quiz_report_qm_filter_subselect($quiz);
-        $mform = new mod_quiz_report_overview_settings($reporturl, compact('qmsubselect'));
+        $mform = new mod_quiz_report_overview_settings($reporturl, compact('qmsubselect', 'quiz'));
         if ($fromform = $mform->get_data()){
             $attemptsmode = $fromform->attemptsmode;
             if ($qmsubselect){
