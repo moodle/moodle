18129bbfeed0bf82d78c013c793b8ff708d6bb9d
jamiesensei <jamiesensei>
2008-05-08 09:31:05 +0000

    MDL-14198 "Make it clear which student attempt gives the final grade, if the scoring method is first, last or highest score" adding a message about the highlighting above the table.

--- lang/en_utf8/quiz_overview.php
+++ lang/en_utf8/quiz_overview.php
@@ -3,11 +3,14 @@
 
 
 $string['allattempts'] = 'Show all attempts';
+$string['allattemptscontributetograde'] = 'All attempts contribute to final grade for user.';
 $string['allstudents'] = 'Show all $a';
 $string['attemptsonly'] = 'Show $a with attempts only';
 $string['attemptsprepage'] = 'Attempts shown per page';
 $string['deleteselected'] = 'Delete selected attempts';
+$string['highlightinggraded'] = 'The user attempt that contributes to final grade is highlighted.';
 $string['noattemptsonly'] = 'Show $a with no attempts only';
+$string['onlyoneattemptallowed'] = 'Only one attempt per user allowed on this quiz.';
 $string['optallattempts'] = 'all attempts';
 $string['optallstudents'] = 'all \'$a\'';
 $string['optattemptsonly'] = '\'$a\' with attempts only';
@@ -22,5 +25,7 @@ $string['preferencespage'] = 'Preferences just for this page';
 $string['preferencessave'] = 'Save preferences';
 $string['preferencesuser'] = 'Your preferences for this report';
 $string['show'] = 'Show';
+$string['showinggraded'] = 'Showing only the attempt graded for each user.';
+$string['showinggradedandungraded'] = 'Showing graded and ungraded attempts for each user. The one attempt for each user that is graded is highlighted. The grading method for this quiz is $a.';
 $string['showdetailedmarks'] = 'Show marks for each question';
 ?>
