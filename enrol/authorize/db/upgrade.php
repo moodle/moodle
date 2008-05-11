369c0a79ff9fe52160e39849a8817fb3330b3521
skodak <skodak>
2008-05-11 18:40:39 +0000

    MDL-14451 removed pre 1.9 upgrade code

--- enrol/authorize/db/upgrade.php
+++ enrol/authorize/db/upgrade.php
@@ -23,41 +23,7 @@ function xmldb_enrol_authorize_upgrade($oldversion=0) {
 
     $result = true;
 
-    if ($result && $oldversion < 2006111700) {
-        $table = new XMLDBTable('enrol_authorize');
-        if (!field_exists($table, new XMLDBField('refundinfo'))) {
-            $field = new XMLDBField('cclastfour');
-            $field->setAttributes(XMLDB_TYPE_INTEGER, '4', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'paymentmethod');
-            $result = $result && rename_field($table, $field, 'refundinfo');
-        }
-    }
-
-    if ($result && $oldversion < 2006112900) {
-        if (isset($CFG->an_login)) {
-            if (empty($CFG->an_login)) {
-                unset_config('an_login');
-            }
-            else {
-                $result = $result && set_config('an_login', rc4encrypt($CFG->an_login), 'enrol/authorize') && unset_config('an_login');
-            }
-        }
-        if (isset($CFG->an_tran_key)) {
-            if (empty($CFG->an_tran_key)) {
-                unset_config('an_tran_key');
-            }
-            else {
-                $result = $result && set_config('an_tran_key', rc4encrypt($CFG->an_tran_key), 'enrol/authorize') && unset_config('an_tran_key');
-            }
-        }
-        if (isset($CFG->an_password)) {
-            if (empty($CFG->an_password)) {
-                unset_config('an_password');
-            }
-            else {
-                $result = $result && set_config('an_password', rc4encrypt($CFG->an_password), 'enrol/authorize') && unset_config('an_password');
-            }
-        }
-    }
+//===== 1.9.0 upgrade line ======//
 
     if ($result && $oldversion < 2008020500 && is_enabled_enrol('authorize')) {
         require_once($CFG->dirroot.'/enrol/authorize/localfuncs.php');
