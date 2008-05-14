6c619d4349b79b93f67123e9baed0b7d3ce848f4
moodlerobot <moodlerobot>
2008-05-14 01:58:47 +0000

    Updated the HEAD build version to 20080514

--- version.php
+++ version.php
@@ -9,6 +9,6 @@
     $version = 2008051201;  // YYYYMMDD   = date of the last version bump
                             //         XX = daily increments
 
-    $release = '2.0 dev (Build: 20080513)';  // Human-friendly version name
+    $release = '2.0 dev (Build: 20080514)';  // Human-friendly version name
 
 ?>
