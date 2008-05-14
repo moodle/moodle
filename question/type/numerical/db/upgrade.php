67516980580f69b413c7dd60a5375e0ed5d8f3dd
skodak <skodak>
2008-05-14 23:35:00 +0000

    MDL-14451 removed pre 1.9 upgrade code

--- question/type/numerical/db/upgrade.php
+++ question/type/numerical/db/upgrade.php
@@ -19,16 +19,11 @@
 
 function xmldb_qtype_numerical_upgrade($oldversion=0) {
 
-    global $CFG, $THEME, $db;
+    global $CFG, $THEME, $DB;
 
     $result = true;
 
-    // In numerical questions, we are changing the 'match anything' answer
-    // from the empty string to *, to be like short answer questions.
-    if ($result && $oldversion < 2006121500) {
-        $result = set_field_select('question_answers', 'answer', '*',
-            sql_compare_text('answer') . " = '" . sql_empty() . "' AND question IN (SELECT id FROM {$CFG->prefix}question WHERE qtype = '" . NUMERICAL . "')");
-    }
+//===== 1.9.0 upgrade line ======//
 
     return $result;
 }
