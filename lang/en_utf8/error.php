fc02bc8672b038673227a74bab93b66454056127
mits <mits>
2008-05-14 11:08:56 +0000

    Adding a missing string for print_error('errortoomanylogins'); in update_login_count() of lib/moodlelib.php.

--- lang/en_utf8/error.php
+++ lang/en_utf8/error.php
@@ -112,6 +112,7 @@ $string['errorcreatingdirectory'] = 'Error creating directory \"$a\"';
 $string['errorcreatingfile'] = 'Error creating file \"$a\"';
 $string['erroronline'] = 'Error on line $a';
 $string['errorreadingfile'] = 'Error reading file \"$a\"';
+$string['errorwhenconfirming'] = 'You are not confirmed yet because an error occurred.  If you clicked on a link in an email to get here, make sure that the line in your email wasn\'t broken or wrapped. You may have to use cut and paste to reconstruct the link properly.';
 $string['errorunzippingfiles'] = 'Error unzipping files';
 $string['failtoloadblocks'] = 'One or more blocks are registered in the database, but they all failed to load!';
 $string['fieldrequired'] = '\"$a\" is a required field';
