327744b1dbba65956aafa0035d0d9bf90427ecb4
stronk7 <stronk7>
2008-05-16 02:32:18 +0000

    Allow adodb to find tables when working without prefix

--- lib/dml/adodb_moodle_database.php
+++ lib/dml/adodb_moodle_database.php
@@ -60,7 +60,7 @@ abstract class adodb_moodle_database extends moodle_database {
         $tables = array();
         foreach ($metatables as $table) {
             $table = strtolower($table);
-            if (strpos($table, $this->prefix) === 0) {
+            if (strpos($table, $this->prefix) === 0 || empty($this->prefix)) {
                 $tablename = substr($table, strlen($this->prefix));
                 $tables[$tablename] = $tablename;
             }
