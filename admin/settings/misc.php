e4b13f6a397060791d0b91c6e33dd057fa5d0688
skodak <skodak>
2008-05-25 11:35:51 +0000

    MDL-14978 removed upgradeforumsread script

--- admin/settings/misc.php
+++ admin/settings/misc.php
@@ -21,7 +21,6 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
 
     // hidden scripts linked from elsewhere
     $ADMIN->add('misc', new admin_externalpage('oacleanup', 'Online Assignment Cleanup', $CFG->wwwroot.'/'.$CFG->admin.'/oacleanup.php', 'moodle/site:config', true));
-    $ADMIN->add('misc', new admin_externalpage('upgradeforumread', 'Upgrade forum', $CFG->wwwroot.'/'.$CFG->admin.'/upgradeforumread.php', 'moodle/site:config', true));
     $ADMIN->add('misc', new admin_externalpage('upgradelogs', 'Upgrade logs', $CFG->wwwroot.'/'.$CFG->admin.'/upgradelogs.php', 'moodle/site:config', true));
     $ADMIN->add('misc', new admin_externalpage('multilangupgrade', get_string('multilangupgrade', 'admin'), $CFG->wwwroot.'/'.$CFG->admin.'/multilangupgrade.php', 'moodle/site:config', !empty($CFG->filter_multilang_converted)));
 
