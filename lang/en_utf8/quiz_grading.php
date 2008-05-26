686e60204712dd98d08d584d0e01ef3edb9b54f3
jamiesensei <jamiesensei>
2008-05-26 12:47:42 +0000

    MDL-12824 "Essay type of quiz questions needs to color mark which students have been graded and which have NOT"
    As well as highlighting graded questions with a new CSS class highlightgraded which is a yellow bg in the standard theme, have also added the option to grade all ungraded attempts for a question.

--- lang/en_utf8/quiz_grading.php
+++ lang/en_utf8/quiz_grading.php
@@ -5,6 +5,12 @@
 $string['essayonly'] = 'The following questions need to be graded manually';
 $string['grading'] = 'Manual grading';
 $string['gradeall'] = 'Grade all $a attempts';
+$string['graded'] = '(graded)';
+$string['gradeungraded'] = 'Grade all $a ungraded attempts';
+$string['gradinguser'] = 'Attempts for $a';
+$string['gradingall'] = 'All $a attempts on this question.';
+$string['gradingattempt'] = 'Attempt number $a->attempt for $a->fullname.';
 $string['gradingnotallowed'] = 'You do not have permission to manually grade responses in this quiz';
-$string['questiontitle'] = 'Question $a->number : \"$a->name\" ($a->gradedattempts / $a->totalattempts attempts graded).';
+$string['gradingungraded'] = '$a ungraded attempts';
+$string['questiontitle'] = 'Question $a->number : \"$a->name\" ({$a->openspan}$a->gradedattempts{$a->closespan} / $a->totalattempts attempts {$a->openspan}graded{$a->closespan}).';
 ?>
