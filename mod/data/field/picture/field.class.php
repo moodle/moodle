2858af42407619fac47f45dc209bda66c834a68f
robertall <robertall>
2008-05-30 02:03:39 +0000

    [MDL-8407] Add new function text_export_supported() or export_text_value() to those "mod/data/field/*/field.class.php" files which need them. The existing code formatting has been cleaned up, too.  Merged from MOODLE_19_STABLE.

--- mod/data/field/picture/field.class.php
+++ mod/data/field/picture/field.class.php
@@ -1,4 +1,4 @@
-<?php // $Id$
+<?php  // $Id$
 ///////////////////////////////////////////////////////////////////////////
 //                                                                       //
 // NOTICE OF COPYRIGHT                                                   //
@@ -22,12 +22,11 @@
 //                                                                       //
 ///////////////////////////////////////////////////////////////////////////
 
-require_once($CFG->dirroot.'/mod/data/field/file/field.class.php'); // Base class is 'file'
+require_once($CFG->dirroot.'/mod/data/field/file/field.class.php');
+// Base class is 'file'
 
 class data_field_picture extends data_field_file {
-
     var $type = 'picture';
-
     var $previewwidth  = 50;
     var $previewheight = 50;
 
@@ -35,28 +34,23 @@ class data_field_picture extends data_field_file {
         parent::data_field_base($field, $data);
     }
 
-    function display_add_field($recordid=0){
+    function display_add_field($recordid=0) {
         global $CFG;
-
         $filepath = '';
         $filename = '';
         $description = '';
-
-        if ($recordid){
+        if ($recordid) {
             if ($content = get_record('data_content', 'fieldid', $this->field->id, 'recordid', $recordid)) {
                 $filename = $content->content;
                 $description = $content->content1;
             }
-
             $path = $this->data->course.'/'.$CFG->moddata.'/data/'.$this->data->id.'/'.$this->field->id.'/'.$recordid;
-
             if ($CFG->slasharguments) {
                 $filepath = $CFG->wwwroot.'/file.php/'.$path.'/'.$filename;
             } else {
                 $filepath = $CFG->wwwroot.'/file.php?file=/'.$path.'/'.$filename;
             }
         }
-
         $str = '<div title="'.s($this->field->description).'">';
         $str .= '<fieldset><legend><span class="accesshide">'.$this->field->name.'</span></legend>';
         $str .= '<input type="hidden" name ="field_'.$this->field->id.'_file" id="field_'.$this->field->id.'_file"  value="fakevalue" />';
@@ -64,7 +58,7 @@ class data_field_picture extends data_field_file {
         $str .= '<label for="field_'.$this->field->id.'_filename">'.get_string('alttext','data') .'</label>&nbsp;<input type="text" name="field_'
                 .$this->field->id.'_filename" id="field_'.$this->field->id.'_filename" value="'.s($description).'" /><br />';
         $str .= '<input type="hidden" name="MAX_FILE_SIZE" value="'.s($this->field->param3).'" />';
-        if ($filepath){
+        if ($filepath) {
             $str .= '<img width="'.s($this->previewwidth).'" height="'.s($this->previewheight).'" src="'.$filepath.'" alt="" />';
         }
         $str .= '</fieldset>';
@@ -73,37 +67,33 @@ class data_field_picture extends data_field_file {
     }
 
     function display_search_field($value = '') {
-        return '<input type="text" size="16" name="f_'.$this->field->id.'" value="'.$value.'" />';   
+        return '<input type="text" size="16" name="f_'.$this->field->id.'" value="'.$value.'" />';
     }
 
     function parse_search_field() {
         return optional_param('f_'.$this->field->id, '', PARAM_NOTAGS);
     }
-    
+
     function generate_sql($tablealias, $value) {
-        return " ({$tablealias}.fieldid = {$this->field->id} AND {$tablealias}.content LIKE '%{$value}%') "; 
+        return " ({$tablealias}.fieldid = {$this->field->id} AND {$tablealias}.content LIKE '%{$value}%') ";
     }
 
     function display_browse_field($recordid, $template) {
         global $CFG;
-
         if ($content = get_record('data_content', 'fieldid', $this->field->id, 'recordid', $recordid)){
-            if (isset($content->content)){
+            if (isset($content->content)) {
                 $contents[0] = $content->content;
                 $contents[1] = $content->content1;
             }
-
-            if (empty($contents[0])) {   // Nothing to show
+            if (empty($contents[0])) {
+                // Nothing to show
                 return '';
             }
-
             $alt = empty($contents[1])? '':$contents[1];
             $title = empty($contents[1])? '':$contents[1];
             $src = $contents[0];
-
             $path = $this->data->course.'/'.$CFG->moddata.'/data/'.$this->data->id.'/'.$this->field->id.'/'.$recordid;
             $thumbnaillocation = $CFG->dataroot .'/'.$this->data->course.'/'.$CFG->moddata.'/data/'.$this->data->id.'/'.$this->field->id.'/'.$recordid.'/thumb/'.$src;
-
             if ($CFG->slasharguments) {
                 $source = $CFG->wwwroot.'/file.php/'.$path.'/'.$src;
                 $thumbnailsource = file_exists($thumbnaillocation) ? $CFG->wwwroot.'/file.php/'.$path.'/thumb/'.$src : $source;
@@ -128,10 +118,8 @@ class data_field_picture extends data_field_file {
     }
 
     function update_field() {
-
         // Get the old field data so that we can check whether the thumbnail dimensions have changed
         $oldfield = get_record('data_fields', 'id', $this->field->id);
-
         if (!update_record('data_fields', $this->field)) {
             notify('updating of new field failed!');
             return false;
@@ -139,16 +127,17 @@ class data_field_picture extends data_field_file {
 
         // Have the thumbnail dimensions changed?
         if ($oldfield && ($oldfield->param4 != $this->field->param4 || $oldfield->param5 != $this->field->param5)) {
-
             // Check through all existing records and update the thumbnail
             if ($contents = get_records('data_content', 'fieldid', $this->field->id)) {
                 if (count($contents) > 20) {
                     notify(get_string('resizingimages', 'data'), 'notifysuccess');
-                    echo "\n\n";   // To make sure that ob_flush() has the desired effect
+                    echo "\n\n";
+                    // To make sure that ob_flush() has the desired effect
                     ob_flush();
                 }
                 foreach ($contents as $content) {
-                    @set_time_limit(300);     // Might be slow!
+                    @set_time_limit(300);
+                    // Might be slow!
                     $this->update_thumbnail($content);
                 }
             }
@@ -158,22 +147,17 @@ class data_field_picture extends data_field_file {
 
     function update_content($recordid, $value, $name) {
         parent::update_content($recordid, $value, $name);
-
         $content = get_record('data_content','fieldid', $this->field->id, 'recordid', $recordid);
-
-        $this->update_thumbnail($content); // Regenerate the thumbnail
+        $this->update_thumbnail($content);
+        // Regenerate the thumbnail
     }
 
-    /**
-    * (Re)generate thumbnail image according to the dimensions specified in the field settings.
-    * If thumbnail width and height are BOTH not specified then no thumbnail is generated, and
-    * additionally an attempted delete of the existing thumbnail takes place.
-    */
     function update_thumbnail($content) {
+        // (Re)generate thumbnail image according to the dimensions specified in the field settings.
+        // If thumbnail width and height are BOTH not specified then no thumbnail is generated, and
+        // additionally an attempted delete of the existing thumbnail takes place.
         global $CFG;
-
         require_once($CFG->libdir . '/gdlib.php');
-
         $datalocation = $CFG->dataroot .'/'.$this->data->course.'/'.$CFG->moddata.'/data/'.
                         $this->data->id.'/'.$this->field->id.'/'.$content->recordid;
         $originalfile = $datalocation.'/'.$content->content;
@@ -181,18 +165,18 @@ class data_field_picture extends data_field_file {
             return;
         }
         if (!file_exists($datalocation.'/thumb')) {
-             mkdir($datalocation.'/thumb', 0777);
+            mkdir($datalocation.'/thumb', 0777);
+            // robertall: Why hardcode 0777??
         }
         $thumbnaillocation = $datalocation.'/thumb/'.$content->content;
         $imageinfo = GetImageSize($originalfile);
         $image->width  = $imageinfo[0];
         $image->height = $imageinfo[1];
         $image->type   = $imageinfo[2];
-
-        if (!$image->width || !$image->height) {  // Should not happen
+        if (!$image->width || !$image->height) {
+            // Should not happen
             return;
         }
-
         switch ($image->type) {
             case 1:
                 if (function_exists('ImageCreateFromGIF')) {
@@ -216,42 +200,40 @@ class data_field_picture extends data_field_file {
                 }
                 break;
         }
-
-        // fix for MDL-7270
         $thumbwidth  = isset($this->field->param4)?$this->field->param4:'';
         $thumbheight = isset($this->field->param5)?$this->field->param5:'';
-
-        if ($thumbwidth || $thumbheight) { // Only if either width OR height specified do we want a thumbnail
-
+        if ($thumbwidth || $thumbheight) {
+            // Only if either width OR height specified do we want a thumbnail
             $wcrop = $image->width;
             $hcrop = $image->height;
-
             if ($thumbwidth && !$thumbheight) {
                 $thumbheight = $image->height * $thumbwidth / $image->width;
             } else if($thumbheight && !$thumbwidth) {
                 $thumbwidth = $image->width * $thumbheight / $image->height;
-            } else { // BOTH are set - may need to crop if aspect ratio differs
+            } else {
+                // BOTH are set - may need to crop if aspect ratio differs
                 $hratio = $image->height / $thumbheight;
                 $wratio = $image->width  / $thumbwidth;
-                if ($wratio > $hratio) { // Crop the width
+                if ($wratio > $hratio) {
+                    // Crop the width
                     $wcrop = intval($thumbwidth * $hratio);
-                } elseif($hratio > $wratio) { // Crop the height
+                } elseif($hratio > $wratio) {
+                    //Crop the height
                     $hcrop = intval($thumbheight * $wratio);
                 }
             }
 
             // At this point both $thumbwidth and $thumbheight are set, and $wcrop and $hcrop
+
             if (function_exists('ImageCreateTrueColor') and $CFG->gdversion >= 2) {
                 $im1 = ImageCreateTrueColor($thumbwidth,$thumbheight);
             } else {
                 $im1 = ImageCreate($thumbwidth,$thumbheight);
             }
-            
-            // Prevent alpha blending for PNG images
             if ($image->type == 3) {
+                // Prevent alpha blending for PNG images
                 imagealphablending($im1, false);
             }
-            
             $cx = $image->width  / 2;
             $cy = $image->height / 2;
 
@@ -261,28 +243,36 @@ class data_field_picture extends data_field_file {
 
             ImageCopyBicubic($im1, $im, 0, 0, $cx-$halfwidth, $cy-$halfheight,
                              $thumbwidth, $thumbheight, $halfwidth*2, $halfheight*2);
-            
-            // Save alpha transparency for PNG images
+
             if ($image->type == 3) {
+                // Save alpha transparency for PNG images
                 imagesavealpha($im1, true);
             }
-            
             if (function_exists('ImageJpeg') && $image->type != 3) {
-                @touch($thumbnaillocation);  // Helps in Safe mode
+                @touch($thumbnaillocation);
+                // Helps in Safe mode
                 if (ImageJpeg($im1, $thumbnaillocation, 90)) {
                     @chmod($thumbnaillocation, 0666);
+                    // robertall: Why hardcode 0666??
                 }
             } elseif (function_exists('ImagePng') && $image->type == 3) {
-                @touch($thumbnaillocation);  // Helps in Safe mode
+                @touch($thumbnaillocation);
+                // Helps in Safe mode
                 if (ImagePng($im1, $thumbnaillocation, 9)) {
                     @chmod($thumbnaillocation, 0666);
-                } 
+                    // robertall: Why hardcode 0666??
+                }
             }
-
-        } else { // Try and remove the thumbnail - we don't want thumbnailing active
+        } else {
+            // Try to remove the thumbnail - we don't want thumbnailing active
             @unlink($thumbnaillocation);
         }
     }
+
+    function text_export_supported() {
+        return false;
+    }
+
 }
 
 ?>
--- mod/data/field/url/field.class.php
+++ mod/data/field/url/field.class.php
@@ -23,67 +23,62 @@
 ///////////////////////////////////////////////////////////////////////////
 
 class data_field_url extends data_field_base {
-
     var $type = 'url';
 
     function data_field_text($field=0, $data=0) {
         parent::data_field_base($field, $data);
     }
 
-    function display_add_field($recordid=0){
+    function display_add_field($recordid=0) {
         global $CFG;
-
         $url = '';
         $text = '';
-
-        if ($recordid){
+        if ($recordid) {
             if ($content = get_record('data_content', 'fieldid', $this->field->id, 'recordid', $recordid)) {
                 $url  = $content->content;
                 $text = $content->content1;
             }
         }
-        $url  = empty($url) ?  'http://' : $url;
-
+        $url = empty($url) ? 'http://' : $url;
         $str = '<div title="'.s($this->field->description).'">';
         if (!empty($this->field->param1) and empty($this->field->param2)) {
             $str .= '<table><tr><td align="right">';
             $str .= get_string('url','data').':</td><td><input type="text" name="field_'.$this->field->id.'_0" id="field_'.$this->field->id.'_0" value="'.$url.'" size="60" /></td></tr>';
             $str .= '<tr><td align="right">'.get_string('text','data').':</td><td><input type="text" name="field_'.$this->field->id.'_1" id="field_'.$this->field->id.'_1" value="'.s($text).'" size="60" /></td></tr>';
             $str .= '</table>';
-        } else {   // Just the URL field
+        } else {
+            // Just the URL field
             $str .= '<input type="text" name="field_'.$this->field->id.'_0" id="field_'.$this->field->id.'_0" value="'.s($url).'" size="60" />';
         }
         $str .= '</div>';
-
         return $str;
     }
 
     function display_search_field($value = '') {
-        return '<input type="text" size="16" name="f_'.$this->field->id.'" value="'.$value.'" />';   
+        return '<input type="text" size="16" name="f_'.$this->field->id.'" value="'.$value.'" />';
     }
-    
+
     function parse_search_field() {
         return optional_param('f_'.$this->field->id, '', PARAM_NOTAGS);
     }
-    
+
     function generate_sql($tablealias, $value) {
-        return " ({$tablealias}.fieldid = {$this->field->id} AND {$tablealias}.content LIKE '%{$value}%') "; 
+        return " ({$tablealias}.fieldid = {$this->field->id} AND {$tablealias}.content LIKE '%{$value}%') ";
     }
 
     function display_browse_field($recordid, $template) {
-        if ($content = get_record('data_content', 'fieldid', $this->field->id, 'recordid', $recordid)){
+        if ($content = get_record('data_content', 'fieldid', $this->field->id, 'recordid', $recordid)) {
             $url = empty($content->content)? '':$content->content;
             $text = empty($content->content1)? '':$content->content1;
-
             if (empty($url) or ($url == 'http://')) {
                 return '';
             }
-
-            if (!empty($this->field->param2)) { // param2 forces the text to something
+            if (!empty($this->field->param2)) {
+                // param2 forces the text to something
                 $text = $this->field->param2;
             }
-
-            if ($this->field->param1) { // param1 defines whether we want to autolink the url.
+            if ($this->field->param1) {
+                // param1 defines whether we want to autolink the url.
                 if (!empty($text)) {
                     $str = '<a href="'.$url.'">'.$text.'</a>';
                 } else {
@@ -101,13 +96,14 @@ class data_field_url extends data_field_base {
         $content = new object;
         $content->fieldid = $this->field->id;
         $content->recordid = $recordid;
-
         $names = explode('_', $name);
-        switch ($names[2]){
-            case 0:    // update link
+        switch ($names[2]) {
+            case 0:
+                // update link
                 $content->content = clean_param($value, PARAM_URL);
                 break;
-            case 1:    // add text
+            case 1:
+                // add text
                 $content->content1 = clean_param($value, PARAM_NOTAGS);
                 break;
             default:
@@ -122,13 +118,18 @@ class data_field_url extends data_field_base {
         }
     }
 
-    function notemptyfield($value, $name){
+    function notemptyfield($value, $name) {
         $names = explode('_',$name);
-        $value = clean_param($value, PARAM_URL);    //clean first
-        if ($names[2] == '0'){
+        $value = clean_param($value, PARAM_URL);
+        //clean first
+        if ($names[2] == '0') {
             return ($value!='http://' && !empty($value));
         }
         return false;
+    }
+
+    function export_text_value($record) {
+        return $record->content . " " . $record->content1;
     }
 
 }
