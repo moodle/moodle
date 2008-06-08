9306b4f5c535817f962287fbb20abedc5f427e08
iarenaza <iarenaza>
2008-06-08 15:28:08 +0000

    MDL-12783 IMS idon't unenroll students who were enrolled by other plug-ins
    Credit goes to Scott Krajewski for the bug report and patch!

--- enrol/imsenterprise/enrol.php
+++ enrol/imsenterprise/enrol.php
@@ -804,7 +804,7 @@ function process_membership_tag($tagcontents){
                 }elseif($CFG->enrol_imsunenrol){
                     // Unenrol
 
-                    if (! role_unassign($moodleroleid, $memberstoreobj->userid, 0, $rolecontext)) {
+                    if (! role_unassign($moodleroleid, $memberstoreobj->userid, 0, $rolecontext, 'imsenterprise')) {
                         $this->log_line("Error unenrolling $memberstoreobj->userid from role $moodleroleid in course");
                     }else{
                         $membersuntally++;
