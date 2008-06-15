28800f02690d7fd77d4c25b62e67741241bd91c1
dongsheng <dongsheng>
2008-06-15 09:19:10 +0000

    "MDL-14129,fix print_error"

--- userpix/upgrade.php
+++ userpix/upgrade.php
@@ -10,7 +10,7 @@
     require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM));
 
     if (!$users = $DB->get_records("user", array("picture"=>"1"), "lastaccess DESC", "id,firstname,lastname")) {
-        print_error("no users!");
+        print_error('nousers');
     }
 
     $title = get_string("users");
