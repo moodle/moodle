69c6d6ca42d74c1f82740f6c502fd67113f15659
pichetp <pichetp>
2008-06-19 01:55:49 +0000

    MDL-14625 more tolerant to missing subquestions merged from HEAD

--- lang/en_utf8/qtype_multianswer.php
+++ lang/en_utf8/qtype_multianswer.php
@@ -1,3 +1,11 @@
 <?php
+$string['correctanswer'] = 'Correct Answer';
+$string['correctanswerandfeedback'] = 'Correct Answer and Feedback';
 $string['decodeverifyquestiontext'] = 'Decode and Verify the Question Text';
+$string['nooptionsforsubquestion'] = 'Unable to get options for question part # $a->sub (question->id={$a->id})';
+$string['noquestions'] = 'The Cloze(multianswer) question \"<strong>$a</strong>\" does not contain any question ';
+$string['questionnotfound'] = 'Unable to find question of question part #$a';
+$string['qtypenotrecognized'] = 'questiontype $a not recognized';
+$string['questionsmissing'] = 'No valid questions, create at least one question ';
+$string['unknownquestiontypeofsubquestion'] = 'Unknown question type: $a->type of question part # $a->sub';
 ?>
