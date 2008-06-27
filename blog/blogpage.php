8fd3d3850916092e3c55a2049bfb3434f9c0a611
moodler <moodler>
2008-06-27 03:31:27 +0000

    MDL-15413 Added some code to stop access to this page directly

--- blog/blogpage.php
+++ blog/blogpage.php
@@ -1,5 +1,9 @@
 <?php  // $Id$
 
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+}
+
 /**
 * Definition of blog page type.
  */
