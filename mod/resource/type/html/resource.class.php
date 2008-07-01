559c9187f8e667983f0a9a6ec99d8d465cbc2137
skodak <skodak>
2008-07-01 22:26:38 +0000

    MDL-15387 fixing regression spotted by John Beedell; merged from MOODLE_19_STABLE

--- mod/resource/type/html/resource.class.php
+++ mod/resource/type/html/resource.class.php
@@ -50,6 +50,11 @@ function display() {
     $formatoptions = new object();
     $formatoptions->noclean = true;
 
+    /// Set up some shorthand variables
+    $cm = $this->cm;
+    $course = $this->course;
+    $resource = $this->resource;
+
     // fix for MDL-9021, thanks Etienne Roz
     // fix for MDL-15387, thanks to John Beedell
     add_to_log($course->id, "resource", "view", "view.php?id={$cm->id}", $resource->id, $cm->id);
@@ -67,11 +72,6 @@ function display() {
 
         /// Set up generic stuff first, including checking for access
         parent::display();
-
-        /// Set up some shorthand variables
-        $cm = $this->cm;
-        $course = $this->course;
-        $resource = $this->resource;
 
         $pagetitle = strip_tags($course->shortname.': '.format_string($resource->name));
         $inpopup = optional_param('inpopup', '', PARAM_BOOL);
