303fe9f4847c2245484f79276c3b9423539d7c28
dongsheng <dongsheng>
2008-07-02 05:02:46 +0000

    "MDL-15501, add appropriate div and h2 tags to the header div, merged from MOODLE_19_STABLE"

--- lib/blocklib.php
+++ lib/blocklib.php
@@ -964,7 +964,9 @@ function blocks_print_adminblock(&$page, &$pageblocks) {
     $missingblocks = blocks_get_missing($page, $pageblocks);
 
     if (!empty($missingblocks)) {
-        $strblocks = get_string('blocks');
+        $strblocks = '<div class="title"><h2>'
+        $strblocks .= get_string('blocks');
+        $strblocks .= '</h2>';
         $stradd    = get_string('add');
         foreach ($missingblocks as $blockid) {
             $block = blocks_get_record($blockid);
