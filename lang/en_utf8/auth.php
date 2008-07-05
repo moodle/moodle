e7f720c2c7469bcfbeb3e872c7526cca6b0beb26
nicolasconnault <nicolasconnault>
2008-07-05 14:06:58 +0000

    MDL-13811 Completed the required changes. Also added a "Cancel email change" link.

--- lang/en_utf8/auth.php
+++ lang/en_utf8/auth.php
@@ -1,4 +1,4 @@
-<?php // $Id$ 
+<?php // $Id$
       // auth.php - created with Moodle 1.5 UNSTABLE DEVELOPMENT (2005010100)
 
 
@@ -113,13 +113,27 @@ $string['auth_dbdebugauthdbhelp'] = 'Debug ADOdb connection to external database
 $string['auth_dbchangepasswordurl_key'] = 'Password-change URL';
 
 // Email plugin
+$string['auth_emailchangecancel'] = 'Cancel email change';
+$string['auth_emailchangepending'] = 'Change pending. Open the link sent to you at $a->preference_newemail.';
 $string['auth_emaildescription'] = 'Email confirmation is the default authentication method.  When the user signs up, choosing their own new username and password, a confirmation email is sent to the user\'s email address.  This email contains a secure link to a page where the user can confirm their account. Future logins just check the username and password against the stored values in the Moodle database.';
+$string['auth_emailnowexists'] = 'The email address you tried to assign to your profile has been assigned to someone else since your original request. Your request for change of email address is hereby cancelled, but you may try again with a different address.';
 $string['auth_emailtitle'] = 'Email-based self-registration';
 $string['auth_emailnoinsert'] = 'Could not add your record to the database!';
 $string['auth_emailnoemail'] = 'Tried to send you an email but failed!';
 $string['auth_emailrecaptcha'] = 'Adds a visual/audio confirmation form element to the signup page for email self-registering users. This protects your site against spammers and contributes to a worthwhile cause. See http://recaptcha.net/learnmore.html for more details.';
 $string['auth_emailrecaptcha_key'] = 'Enable reCAPTCHA element';
 $string['auth_emailsettings'] = 'Settings';
+$string['auth_emailupdatemessage'] = 'Dear $a->fullname,
+
+You have requested a change of your email address for your user account at $a->site. Please open the following URL in your browser in order to confirm this change.
+
+$a->url';
+$string['auth_emailupdatetitle'] = 'Confirmation of email update at $a->site';
+$string['auth_invalidnewemailkey'] = 'Error: if you are trying to confirm a change of email address, you may have made a mistake in copying the URL we sent you by email. Please copy the address and try again.';
+$string['auth_emailupdatesuccess'] = 'Your email address was successfully updated to $a->email.';
+$string['auth_outofnewemailupdateattempts'] = 'You have run out of allowed attempts to update your email address. Your update request has been cancelled.';
+$string['auth_emailupdate'] = 'Email address update';
+$string['auth_changingemailaddress'] = 'You have requested a change of email address, from $a->oldemail to $a->newemail. For security reasons, we are sending you an email message at the new address to confirm that it belongs to you. Your email address will be updated as soon as you open the URL sent to you in that message.';
 
 // FirstClass plugin
 $string['auth_fccreators'] = 'List of groups whose members are allowed to create new courses. Separate multiple groups with \';\'. Names must be spelled exactly as on FirstClass server. System is case-sensitive.';
@@ -194,11 +208,11 @@ $string['auth_ldap_user_type'] = 'Select how users are stored in LDAP. This sett
 $string['auth_ldap_version'] = 'The version of the LDAP protocol your server is using.';
 $string['auth_ldapdescription'] = 'This method provides authentication against an external LDAP server.
 
-                                  If the given username and password are valid, Moodle creates a new user 
+                                  If the given username and password are valid, Moodle creates a new user
 
-                                  entry in its database. This module can read user attributes from LDAP and prefill 
+                                  entry in its database. This module can read user attributes from LDAP and prefill
 
-                                  wanted fields in Moodle.  For following logins only the username and 
+                                  wanted fields in Moodle.  For following logins only the username and
 
                                   password are checked.';
 $string['auth_ldap_ldap_encoding_key'] = 'LDAP encoding';
@@ -323,7 +337,7 @@ $string['auth_shib_changepasswordurl'] = 'Password-change URL';
 $string['auth_shibboleth_login_long'] = 'Login to Moodle via Shibboleth';
 $string['auth_shibboleth_select_organization'] = 'For authentication via Shibboleth, please select your organization from the drop down list:';
 $string['auth_shibboleth_contact_administrator'] = 'In case you are not associated with the given organizations and you need access to a course on this server, please contact the';
-$string['auth_shibboleth_select_member'] = 'I\'m a member of ...'; 
+$string['auth_shibboleth_select_member'] = 'I\'m a member of ...';
 $string['auth_shibboleth_errormsg'] ='Please select the organization you are member of!';
 $string['auth_shib_no_organizations_warning'] ='If you want to use the integrated WAYF service, you must provide a coma-separated list of Identity Provider entityIDs, their names and optionally a session initiator.';
 
---
+++ user/emailupdate.php
@@ -0,0 +1,58 @@
+<?php // $Id$
+require_once('../config.php');
+require_once($CFG->libdir.'/adminlib.php');
+
+$key = required_param('key', PARAM_ALPHANUM);
+$id  = required_param('id', PARAM_INT);
+
+if (!$user = get_record('user', 'id', $id)) {
+    error("Unknown user ID");
+}
+
+$preferences = get_user_preferences(null, null, $id);
+$a = new stdClass();
+$a->fullname = fullname($user, true);
+$stremailupdate = get_string('auth_emailupdate', 'auth', $a);
+print_header(format_string($SITE->fullname) . ": $stremailupdate", format_string($SITE->fullname) . ": $stremailupdate");
+
+$cancel_email_update = false;
+
+if (empty($preferences['newemailattemptsleft'])) {
+    redirect("$CFG->wwwroot/user/view.php?id=$user->id");
+
+} elseif ($preferences['newemailattemptsleft'] < 1) {
+    $cancel_email_update = true;
+    $stroutofattempts = get_string('auth_outofnewemailupdateattempts', 'auth');
+    print_box($stroutofattempts, 'center');
+
+} elseif ($key == $preferences['newemailkey']) {
+    $user->email = $preferences['newemail'];
+
+    // Detect duplicate before saving
+    if (get_record('user', 'email', addslashes($user->email))) {
+        $stremailnowexists = get_string('auth_emailnowexists', 'auth');
+        print_box($stremailnowexists, 'center');
+        $cancel_email_update = true;
+        print_continue("$CFG->wwwroot/user/view.php?id=$user->id");
+    } else {
+        // update user email
+        if (!set_field('user', 'email', addslashes($user->email), 'id', $user->id)) {
+            error('Error updating user record');
+
+        } else {
+            events_trigger('user_updated', $user);
+            $stremailupdatesuccess = get_string('auth_emailupdatesuccess', 'auth', $user);
+            print_box($stremailupdatesuccess, 'center');
+            print_continue("$CFG->wwwroot/user/view.php?id=$user->id");
+
+            $cancel_email_update = true;
+        }
+    }
+
+} else {
+    $preferences['newemailattemptsleft']--;
+    set_user_preference('newemailattemptsleft', $preferences['newemailattemptsleft'], $user->id);
+    $strinvalidkey = get_string('auth_invalidnewemailkey', 'auth');
+    print_box($strinvalidkey, 'center');
+}
+
