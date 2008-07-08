8f829910d6440bd954f01053a977d75f7e713f6f
thepurpleblob <thepurpleblob>
2008-07-08 08:45:26 +0000

    MDL-15304 Remove the item number from the created label name. It doesn't work for new labels.
    Merged from STABLE_19

--- mod/label/lib.php
+++ mod/label/lib.php
@@ -15,7 +15,7 @@ function get_label_name($label) {
 
     if (empty($name)) {
         // arbitrary name
-        $name = "label{$label->instance}";
+        $name = "label";
     }
 
     return $name;
