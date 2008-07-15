6a983a636f8a987c8f822885c58ce24b950d5a71
ikawhero <ikawhero>
2008-07-15 03:24:20 +0000

    MDL-14299 New checkbox profile field type. Credit to Brian King.

---
+++ user/profile/field/checkbox/define.class.php
@@ -0,0 +1,13 @@
+<?php  //$Id:
+
+class profile_define_checkbox extends profile_define_base {
+
+    function define_form_specific(&$form) {
+        /// select whether or not this should be checked by default
+        $form->addElement('selectyesno', 'defaultdata', get_string('profiledefaultchecked', 'profilefield_checkbox'));
+        $form->setDefault('defaultdata', 0); // defaults to 'no'
+        $form->setType('defaultdata', PARAM_BOOL);
+    }
+}
+
+?>
