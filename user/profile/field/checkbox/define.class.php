8750134a46edad73edebdb098c824d8c49ce819b
ikawhero <ikawhero>
2008-07-15 04:03:33 +0000

    Merged from stable. Change language string file.

--- user/profile/field/checkbox/define.class.php
+++ user/profile/field/checkbox/define.class.php
@@ -1,10 +1,10 @@
-<?php  //$Id:
+<?php  //$Id
 
 class profile_define_checkbox extends profile_define_base {
 
     function define_form_specific(&$form) {
         /// select whether or not this should be checked by default
-        $form->addElement('selectyesno', 'defaultdata', get_string('profiledefaultchecked', 'profilefield_checkbox'));
+        $form->addElement('selectyesno', 'defaultdata', get_string('profiledefaultchecked', 'admin'));
         $form->setDefault('defaultdata', 0); // defaults to 'no'
         $form->setType('defaultdata', PARAM_BOOL);
     }
