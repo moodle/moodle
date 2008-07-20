5a4825fa76f6aea0234c1edb840f642977bc1b8c
moodler <moodler>
2008-07-20 08:22:52 +0000

    Added lines theme (beta) from Urs

---
+++ theme/lines/meta.php
@@ -0,0 +1,8 @@
+<?php require_js(array('yui_yahoo','yui_dom','yui_event','yui_animation')); ?>
+
+<!--[if IE 7]>
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/lines/styles_ie7.css" />
+<![endif]-->
+<!--[if IE 6]>
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/lines/styles_ie6.css" />
+<![endif]-->
