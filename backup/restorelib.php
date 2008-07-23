47ec4f9c6f734b9d578916c9da6ab75ad3c78a72
stronk7 <stronk7>
2008-07-23 22:46:10 +0000

    Course page blocks are lost when restoring 1.3 and 1.4 backups. MDL-15803

--- backup/restorelib.php
+++ backup/restorelib.php
@@ -830,7 +830,7 @@ define('RESTORE_GROUPS_GROUPINGS', 3);
                 // Looks like it's from Moodle < 1.3. Let's give the course default blocks...
                 $newpage = page_create_object(PAGE_COURSE_VIEW, $restore->course_id);
                 blocks_repopulate_page($newpage);
-            } else if (!empty($CFG->showblocksonmodpages)) {
+            } else {
                 // We just have a blockinfo field, this is a legacy 1.4 or 1.3 backup
                 $blockrecords = get_records_select('block', '', '', 'name, id');
                 $temp_blocks_l = array();
