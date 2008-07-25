162f96353bbf33bcdb02646fbee406c3db1e3008
jamiesensei <jamiesensei>
2008-07-25 12:15:07 +0000

    MDL-14208 Cache results of calculations, "recompute now" functionality -- forgot one field for questions stats.

--- mod/quiz/report/statistics/db/upgrade.php
+++ mod/quiz/report/statistics/db/upgrade.php
@@ -14,6 +14,18 @@ function xmldb_quizreport_statistics_upgrade($oldversion=0) {
         //register cron to run every 5 hours.
         $result = $result && $DB->set_field('quiz_report', 'cron', HOURSECS*5, array('name'=>'statistics'));
     }
+    if ($result && $oldversion < 2008072500) {
+
+    /// Define field s to be added to quiz_question_statistics
+        $table = new xmldb_table('quiz_question_statistics');
+        $field = new xmldb_field('s', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'subquestion');
+
+    /// Conditionally launch add field s
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+    }
     return $result;
 }
 
