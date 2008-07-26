a6017978a2369f2c2d8813a86658545d16f8e4fc
skodak <skodak>
2008-07-26 11:35:23 +0000

    MDL-13353 Course page does not validate when a lable containing <p> tags is hidden

--- course/lib.php
+++ course/lib.php
@@ -1348,11 +1348,11 @@ function print_section($course, $section, $mods, $modnamesused, $absolute=false,
 
             if ($mod->modname == "label") {
                 if (!$mod->visible) {
-                    echo "<span class=\"dimmed_text\">";
+                    echo "<div class=\"dimmed_text\">";
                 }
                 echo format_text($extra, FORMAT_HTML, $labelformatoptions);
                 if (!$mod->visible) {
-                    echo "</span>";
+                    echo "</div>";
                 }
                 if (!empty($CFG->enablegroupings) && !empty($mod->groupingid) && has_capability('moodle/course:managegroups', get_context_instance(CONTEXT_COURSE, $course->id))) {
                     if (!isset($groupings)) {
