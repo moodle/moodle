be8dce4263d3193c039a3385292c2249d804d6cb
danmarsden <danmarsden>
2008-07-27 20:22:07 +0000

    MDL-15747 typo merged from HEAD

--- mod/scorm/locallib.php
+++ mod/scorm/locallib.php
@@ -834,7 +834,7 @@ function scorm_check_package($data) {
         } else if ($reference[0] == '#') {
             require_once($repositoryconfigfile);
             if ($CFG->repositoryactivate) {
-                $referencefield = $reference.'/imsmanfest.xml';
+                $referencefield = $reference.'/imsmanifest.xml';
                 $reference = $CFG->repository.substr($reference,1).'/imsmanifest.xml';
             } else {
                 $validation = null;
