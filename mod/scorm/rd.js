58afb04bc73227a5a16cf3ab3e9a6794b97fee95
piers <piers>
2008-07-28 00:44:49 +0000

    MDL-9378 New visualisation for SCORM - back ported to 1.9.

---
+++ mod/scorm/rd.js
@@ -0,0 +1,77 @@
+<!--
+function scorm_get_element_style(obj, prop, cssProp) {
+    var ret = '';
+    
+    if (obj.currentStyle) {
+        ret = obj.currentStyle[prop];
+    } else if (document.defaultView && document.defaultView.getComputedStyle) {
+        var compStyle = document.defaultView.getComputedStyle(obj, null);
+        ret = compStyle.getPropertyValue(cssProp);
+    }
+    
+    if (ret == 'auto') ret = '0';
+    return ret;
+}
+
+function scorm_resize () {
+    var winwidth = 0, winheight = 0;
+    if( typeof( window.innerWidth ) == 'number' ) {
+        //Non-IE
+        winwidth = window.innerWidth;
+        winheight = window.innerHeight;
+    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+        //IE 6+ in 'standards compliant mode'
+        winwidth = document.documentElement.clientWidth;
+        winheight = document.documentElement.clientHeight;
+    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+        //IE 4 compatible
+        winwidth = document.body.clientWidth;
+        winheight = document.body.clientHeight;
+    }
+                              
+    var header = document.getElementById('header');   
+    var content = document.getElementById('content');
+    var headerheight = 0;
+    if (content) {
+        headerheight = content.offsetTop;
+    }
+    
+    var footer = document.getElementById('footer');
+    var imsnavbar = document.getElementById('tochead');
+    var scormtop = document.getElementById('scormtop');
+    var footerheight = 0;
+    var imsnavheight = 0;
+    var scormtopheight = 0;
+    if (footer) {
+        footerheight = footer.offsetHeight + parseInt(scorm_get_element_style(footer, 'marginTop', 'margin-top')) + parseInt(scorm_get_element_style(footer, 'marginBottom', 'margin-bottom'));
+    }
+    if (imsnavbar) {
+        imsnavheight = imsnavbar.offsetHeight;
+    }
+    if (scormtop) {
+        scormtopheight = scormtop.offsetHeight;
+    }
+
+    var topmargin = parseInt(scorm_get_element_style(document.getElementsByTagName('body')[0], 'marginTop', 'margin-top'));
+    var bottommargin = parseInt(scorm_get_element_style(document.getElementsByTagName('body')[0], 'marginBottom', 'margin-bottom'));
+    
+    var totalheight = headerheight + 
+                        footerheight + 
+                        scormtopheight+
+                        topmargin +
+                        bottommargin+10; // +10 to save a minor vertical scroll always present!
+                        
+    var totalheighttoc = totalheight+imsnavheight;
+    var toctree = document.getElementById('toctree');
+    if (toctree != null){
+        var toctreeHeight = toctree.offsetHeight;
+        document.getElementById('toctree').style.height = (winheight - totalheighttoc) + 'px';
+        var scoframe2 = document.getElementById('scoframe1');
+        document.getElementById('scormobject').style.height = (winheight - totalheight) + 'px';
+    }else{
+        var scoframe2 = document.getElementById('scoframe1');
+        document.getElementById('scormobject').style.height = (winheight - totalheight) + 'px';
+    }
+
+}
+-->
