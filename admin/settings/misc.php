1e908abea49b7084b580d422205f3b9c55f21d2f
skodak <skodak>
2008-07-31 21:48:29 +0000

    MDL-15901 completion disabled by default

--- admin/settings/misc.php
+++ admin/settings/misc.php
@@ -15,7 +15,7 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
 
     // Completion system
     require_once($CFG->libdir.'/completionlib.php');
-    $temp->add(new admin_setting_configcheckbox('enablecompletion',get_string('enablecompletion','completion'),get_string('configenablecompletion','completion'),COMPLETION_ENABLED));
+    $temp->add(new admin_setting_configcheckbox('enablecompletion',get_string('enablecompletion','completion'),get_string('configenablecompletion','completion'),COMPLETION_DISABLED));
     $temp->add(new admin_setting_pickroles('progresstrackedroles',get_string('progresstrackedroles','completion'),get_string('configprogresstrackedroles','completion')));
 
     $ADMIN->add('misc', $temp);
