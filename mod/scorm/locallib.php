1f3ef341bd763257c01dc0ea95ce53e9ca6f78c7
piers <piers>
2008-08-03 22:20:20 +0000

    MDL-14457 SCORM module creating extra windows when opening module in a new window. Backport to 1.8 (thanks Jo Matthews).

--- mod/scorm/locallib.php
+++ mod/scorm/locallib.php
@@ -564,7 +564,7 @@ function scorm_view_display ($user, $scorm, $action, $cm, $boxwidth='') {
 
 ?>
             <div class="scorm-center">
-               <form id="theform" method="post" action="<?php echo $CFG->wwwroot ?>/mod/scorm/player.php?scoid=<?php echo $sco->id ?>&id=<?php echo $cm->id ?>"<?php echo $scorm->popup == 1?' target="newwin"':'' ?>>
+               <form id="theform" method="post" action="<?php echo $CFG->wwwroot ?>/mod/scorm/player.php?scoid=<?php echo $sco->id ?>&id=<?php echo $cm->id ?>">
               <?php
                   if ($scorm->hidebrowse == 0) {
                       print_string('mode','scorm');
