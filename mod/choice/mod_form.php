959f4c300cd457a50a99a94efdb1607c74478c98
danmarsden <danmarsden>
2008-08-11 04:48:43 +0000

    MDL-15328 - hide grade category drop down.

--- mod/choice/mod_form.php
+++ mod/choice/mod_form.php
@@ -95,6 +95,7 @@ class mod_choice_mod_form extends moodleform_mod {
         $features->groups = true;
         $features->groupings = true;
         $features->groupmembersonly = true;
+        $features->gradecat = false;
         $this->standard_coursemodule_elements($features);
 //-------------------------------------------------------------------------------
         $this->add_action_buttons();
