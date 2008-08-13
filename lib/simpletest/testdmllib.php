9ef749413f58f2510f3582eafe8d42e3b1d55983
tjhunt <tjhunt>
2008-08-13 06:21:49 +0000

    Remove broken unit test.

--- lib/simpletest/testdmllib.php
+++ lib/simpletest/testdmllib.php
@@ -328,17 +328,6 @@ class datalib_test extends prefix_changing_test_case {
         $obj->numberfield = 123;
         $this->assertFalse(insert_record('nonexistant_table', $obj), 'Insert into nonexistant table');
 
-        // Insert bad data - error should be printed - mysql not tested
-        if ($CFG->dbfamily != 'mysql') {
-            $obj = new stdClass;
-            $obj->textfield = 'new entry';
-            $obj->numberfield = 'not a number';
-            ob_start();
-            $this->assertFalse(insert_record($this->table, $obj), 'Insert bad data - should fail.');
-            $result = ob_get_contents();
-            ob_end_clean();
-            $this->assert(new TextExpectation('ERROR:'), $result, 'Insert bad data - error should have been printed. This is known not to work on MySQL.');
-        }
     }
 }
 
