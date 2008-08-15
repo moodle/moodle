7373d3dedab86760110345d16906dda5b79f53ec
dongsheng <dongsheng>
2008-08-15 03:23:42 +0000

    MDL-13766, move lang files

---
+++ lang/en_utf8/repository_flickr.php
@@ -0,0 +1,9 @@
+<?php
+$string['configplugin'] = 'Flickr configureation';
+$string['repositoryname'] = 'Flickr';
+$string['repositorydesc'] = 'Repository for flickr.com';
+$string['notitle'] = 'notitle';
+$string['username'] = 'Flickr Account:';
+$string['remember'] = 'Remember me';
+$string['nullphotolist'] = 'There is no photo belong to this account.';
+$string['invalidemail'] = 'Invalid Flickr email address.';
---
+++ lang/en_utf8/repository_local.php
@@ -0,0 +1,7 @@
+<?php // $Id$
+$string['configplugin'] = 'Configuration for local repository';
+$string['repositoryname'] = 'Local Moodle';
+$string['repositorydesc'] = 'Repository for local Moodle';
+$string['notitle'] = 'notitle';
+$string['remember'] = 'Remember me';
+$string['emptyfilelist'] = 'There is no files.';
