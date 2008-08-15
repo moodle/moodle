d85b412960b3b028647d3e473ce53c35e5a16aee
dongsheng <dongsheng>
2008-08-15 03:27:30 +0000

    MDL-13766, remove old lang file in plugin folder

--- repository/local/lang/en_utf8/repository_local.php
+++
@@ -1,6 +0,0 @@
-<?php // $Id$
-$string['repositoryname'] = 'Local Moodle';
-$string['repositorydesc'] = 'Repository for local Moodle';
-$string['notitle'] = 'notitle';
-$string['remember'] = 'Remember me';
-$string['emptyfilelist'] = 'There is no files.';
