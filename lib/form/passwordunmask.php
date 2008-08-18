12ffbe6f651af480eb1fb252a66f08381f8a8c04
skodak <skodak>
2008-08-18 20:20:42 +0000

    MDL-16108 force autocomplete off for unmaskable password fields; merged from MOODLE_18_STABLE

--- lib/form/passwordunmask.php
+++ lib/form/passwordunmask.php
@@ -23,6 +23,7 @@ class MoodleQuickForm_passwordunmask extends MoodleQuickForm_password {
             $unmaskjs = '<script type="text/javascript">
 //<![CDATA[
 document.write(\'<div class="unmask"><input id="'.$id.'unmask" value="1" type="checkbox" onclick="unmaskPassword(\\\''.$id.'\\\')"/><label for="'.$id.'unmask">'.addslashes_js($unmask).'<\/label><\/div>\');
+document.getElementById("'.$this->getAttribute('id').'").setAttribute("autocomplete", "off");
 //]]>
 </script>';
             return $this->_getTabs() . '<input' . $this->_getAttrString($this->_attributes) . ' />'.$unmaskjs;
