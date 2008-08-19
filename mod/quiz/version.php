2d843ded376ab44b76b0194e969dc7cb55e19f2b
skodak <skodak>
2008-08-19 21:39:05 +0000

    MDL-15819 bumping version,  needed for updated risks of quiz caps

--- mod/quiz/version.php
+++ mod/quiz/version.php
@@ -5,7 +5,7 @@
 //  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2007101509;   // The (date) version of this module
+$module->version  = 2007101510;   // The (date) version of this module
 $module->requires = 2007101509;   // Requires this Moodle version
 $module->cron     = 0;            // How often should cron check this module (seconds)?
 
