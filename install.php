0022d112abbec2fb93c12a73387fdbf5d422f1c2
stronk7 <stronk7>
2008-08-24 16:39:06 +0000

    Remove hack not needed anymore. Now enviroment check handles this properly. MDL-16008 & MDL-16188

--- install.php
+++ install.php
@@ -159,7 +159,6 @@ require_once($CFG->dirroot.'/version.php');
 /// Set version and release
 $INSTALL['version'] = $version;
 $INSTALL['release'] = $release;
-$CFG->release = $release; // needed for environment checks
 
 /// list all supported drivers - unsupported must be installed manually ;-)
 $supported = array (
