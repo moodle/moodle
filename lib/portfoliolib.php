31ca68c4c389da6efb4cec44e14219f58c8372df
mjollnir_ <mjollnir_>
2008-08-30 16:39:08 +0000

    MDL-14591 - fixing bug in last commit that would have broken for 'push' portfolio transfers

--- lib/portfoliolib.php
+++ lib/portfoliolib.php
@@ -2404,8 +2404,8 @@ function portfolio_handle_event($eventdata) {
     $exporter = portfolio_exporter::rewaken_object($eventdata);
     $exporter->process_stage_package();
     $exporter->process_stage_send();
-    $exporter->process_stage_cleanup();
     $exporter->save();
+    $exporter->process_stage_cleanup();
     return true;
 }
 
