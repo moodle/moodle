4f2b9a4feda097af7599dc22633163fd116b906f
jerome <jerome>
2008-09-03 09:53:35 +0000

    MDL-15402: change repository_cache_expire common option default to 120 (minutes)

--- admin/settings/plugins.php
+++ admin/settings/plugins.php
@@ -186,10 +186,10 @@ if ($hassiteconfig) {
     $temp->add(new admin_setting_heading('managerepositories', get_string('activerepository', 'repository'), ''));
     $temp->add(new admin_setting_managerepository());
     $temp->add(new admin_setting_heading('managerepositoriescommonheading', get_string('commonsettings', 'admin'), ''));
-    $temp->add(new admin_setting_configtext('repository_cache_expire', get_string('cacheexpire', 'repository'), get_string('configcacheexpire', 'repository'), 0));
+    $temp->add(new admin_setting_configtext('repository_cache_expire', get_string('cacheexpire', 'repository'), get_string('configcacheexpire', 'repository'), 120));
     $ADMIN->add('repositorysettings', $temp);
-    $ADMIN->add('repositorysettings', new admin_externalpage('repositorynew', 
-        get_string('createrepository', 'repository'), $url, 'moodle/site:config', true), 
+    $ADMIN->add('repositorysettings', new admin_externalpage('repositorynew',
+        get_string('createrepository', 'repository'), $url, 'moodle/site:config', true),
         '', $url);
     $ADMIN->add('repositorysettings', new admin_externalpage('repositorydelete',
         get_string('deleterepository', 'repository'), $url, 'moodle/site:config', true),
