5c43e8b742cc411e32d9f6438738eda84f56a0ea
nicolasconnault <nicolasconnault>
2008-09-04 11:10:40 +0000

    MDL-16341 Added language pack

---
+++ lang/en_utf8/portfolio_flickr.php
@@ -0,0 +1,8 @@
+<?php
+
+$string['apikey'] = 'API key (you need to get this from http://flickr.com/services/api/keys/apply)';
+$string['err_noapikey'] = 'There is no API Key configured for this plugin.  You can get one of these from http://flickr.com/services/api/keys/apply';
+$string['noauthtoken'] = 'Could not retrieve an authentication token for use in this session';
+$string['pluginname'] = 'Flickr online photo management system';
+$string['sharedsecret'] = 'Your shared secret string (will be provided with your generated API key)';
+?>
