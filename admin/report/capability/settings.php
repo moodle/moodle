89ee3c7937e2878ec40b93f5029d282d5961a6a6
tjhunt <tjhunt>
2008-09-09 09:35:54 +0000

    MDL-16348 - followup - prevent debug output when logged in as a non-admin. Thanks to Mat for ponting this out.

--- admin/report/capability/settings.php
+++ admin/report/capability/settings.php
@@ -1,3 +1,5 @@
 <?php  // $Id$
-$ADMIN->add('roles', new admin_externalpage('reportcapability', get_string('capability', 'report_capability'), "$CFG->wwwroot/$CFG->admin/report/capability/index.php",'moodle/role:manage'));
+if ($hassiteconfig) {
+    $ADMIN->add('roles', new admin_externalpage('reportcapability', get_string('capability', 'report_capability'), "$CFG->wwwroot/$CFG->admin/report/capability/index.php",'moodle/role:manage'));
+}
 ?>
--- admin/report/simpletest/settings.php
+++ admin/report/simpletest/settings.php
@@ -1,3 +1,6 @@
 <?php  //$Id$
-$ADMIN->add('development', new admin_externalpage('reportsimpletest', get_string('simpletest', 'admin'), "$CFG->wwwroot/$CFG->admin/report/simpletest/index.php",'moodle/site:config'));
-$ADMIN->add('development', new admin_externalpage('reportdbtest', get_string('dbtest', 'admin'), "$CFG->wwwroot/$CFG->admin/report/simpletest/dbtest.php",'moodle/site:config'));
+if ($hassiteconfig) {
+    $ADMIN->add('development', new admin_externalpage('reportsimpletest', get_string('simpletest', 'admin'), "$CFG->wwwroot/$CFG->admin/report/simpletest/index.php",'moodle/site:config'));
+    $ADMIN->add('development', new admin_externalpage('reportdbtest', get_string('dbtest', 'admin'), "$CFG->wwwroot/$CFG->admin/report/simpletest/dbtest.php",'moodle/site:config'));
+}
+?>
