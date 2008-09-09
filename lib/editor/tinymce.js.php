86a246e9296c68678795c4414cfcd8a6f0bef2da
scyrma <scyrma>
2008-09-09 05:29:07 +0000

    MDL-16319: set a css sheet for the content of the editor - fixing the issue with the default font size. This commit also reactivates the media plugin.

--- lib/editor/tinymce.js.php
+++ lib/editor/tinymce.js.php
@@ -137,13 +137,16 @@ EOF;
 $strtime = get_string('strftimetime');
 $strdate = get_string('strftimedaydate');
 
+// $contentcss should be customizable, but default to this.
+$contentcss = $CFG->themewww .'/'. current_theme() .'/styles.php';
+
 $output = <<<EOF
     tinyMCE.init({
         mode: "textareas",
         relative_urls: false,
         editor_selector: "form-textarea-simple",
         document_base_url: "$CFG->httpswwwroot",
-        content_css: "$CFG->httpswwwroot/lib/editor/tinymce/examples/css/content.css",
+        content_css: "$contentcss",
         theme: "simple",
         skin: "o2k7",
         skin_variant: "silver",
@@ -159,6 +162,7 @@ $output = <<<EOF
         relative_urls: false,
         editor_selector: "form-textarea-advanced",
         document_base_url: "$CFG->httpswwwroot",
+        content_css: "$contentcss",
         theme: "advanced",
         skin: "o2k7",
         skin_variant: "silver",
@@ -167,7 +171,7 @@ $output = <<<EOF
         entity_encoding: "raw",
         language: "$editorlanguage",
         directionality: "$directionality",
-        plugins: "safari,spellchecker,table,style,layer,advhr,advimage,advlink,emoticons,inlinepopups,,searchreplace,paste,standardmenu,directionality,fullscreen,moodlenolink,dragmath,nonbreaking,contextmenu,insertdatetime,save,iespell,preview,print,noneditable,visualchars,xhtmlxtras,template,pagebreak",
+        plugins: "safari,spellchecker,table,style,layer,advhr,advimage,advlink,emoticons,inlinepopups,media,searchreplace,paste,standardmenu,directionality,fullscreen,moodlenolink,dragmath,nonbreaking,contextmenu,insertdatetime,save,iespell,preview,print,noneditable,visualchars,xhtmlxtras,template,pagebreak",
         plugin_insertdate_dateFormat : "$strdate",
         plugin_insertdate_timeFormat : "$strtime",
         theme_advanced_font_sizes: "1,2,3,4,5,6,7",
@@ -177,7 +181,7 @@ $output = <<<EOF
         theme_advanced_buttons1_add: "|,undo,redo,|,search,replace,spellchecker,|,fullscreen",
         theme_advanced_buttons2: "bold,italic,underline,strikethrough,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,|,cite,abbr,acronym",
         theme_advanced_buttons2_add: "|,selectall,cleanup,removeformat,pastetext,pasteword,|,forecolor,backcolor,|,ltr,rtl",
-        theme_advanced_buttons3: "bullist,numlist,outdent,indent,|,link,unlink,moodlenolink,anchor,|,insertdate,inserttime,|,emoticons,image,,dragmath,advhr,nonbreaking,charmap",
+        theme_advanced_buttons3: "bullist,numlist,outdent,indent,|,link,unlink,moodlenolink,anchor,|,insertdate,inserttime,|,emoticons,image,media,dragmath,advhr,nonbreaking,charmap",
         theme_advanced_buttons3_add: "|,table,insertlayer,styleprops,visualchars,|,code,preview",
         theme_advanced_fonts: "Trebuchet=Trebuchet MS,Verdana,Arial,Helvetica,sans-serif;Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;Georgia=georgia,times new roman,times,serif;Tahoma=tahoma,arial,helvetica,sans-serif;Times New Roman=times new roman,times,serif;Verdana=verdana,arial,helvetica,sans-serif;Impact=impact;Wingdings=wingdings", 
         theme_advanced_resize_horizontal: true,
