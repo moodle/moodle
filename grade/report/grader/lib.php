0acbdf846c58f6e410b54c78da105e40bede9c1f
tjhunt <tjhunt>
2008-09-09 02:48:54 +0000

    XHTML Strict!

--- grade/report/grader/lib.php
+++ grade/report/grader/lib.php
@@ -715,7 +715,7 @@ class grade_report_grader extends grade_report {
 
             if ($showuseridnumber) {
                 $studentshtml .= '<th class="header c'.$columncount++.' useridnumber" onclick="set_row(this.parentNode.rowIndex);">'.
-                        $user->idnumber.'</a></th>';
+                        $user->idnumber.'</th>';
             }
 
             foreach ($this->gtree->get_items() as $itemid=>$unused) {
