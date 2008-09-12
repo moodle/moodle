91769761515303359a709a47defb8172a95db8d9
jamiesensei <jamiesensei>
2008-09-12 08:33:40 +0000

    MDL-16463 "Long group names can make drop down box too wide"  added an if condition to test the length of the group name. If it is more than 20 characters then we use the string 'this group' instead of the group name.

--- lang/en_utf8/quiz_overview.php
+++ lang/en_utf8/quiz_overview.php
@@ -50,4 +50,5 @@ $string['showinggraded'] = 'Showing only the attempt graded for each user.';
 $string['showinggradedandungraded'] = 'Showing graded and ungraded attempts for each user. The one attempt for each user that is graded is highlighted. The grading method for this quiz is $a.';
 $string['showdetailedmarks'] = 'Show / download marks for each question';
 $string['studentingroup'] = '\'$a->coursestudent\' in group \'$a->groupname\'';
+$string['studentingrouplong'] = '\'$a->coursestudent\' in this group';
 ?>
