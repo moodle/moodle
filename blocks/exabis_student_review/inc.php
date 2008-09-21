582bab05c0b9abfee53ad8db3393b73db1dd70e8
arborrow <arborrow>
2008-09-21 04:28:23 +0000

    CONTRIB-716 - removing files intended for CONTRIB from 19STABLE

--- blocks/exabis_student_review/inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__)."/../../config.php");
-require_once($CFG->dirroot . '/blocks/exabis_student_review/lib/lib.php');
--- blocks/exabis_student_review/styles.php
+++
@@ -1,8 +0,0 @@
-#exabis_student_review {
-}
-
-#exabis_student_review .singlebutton {
-	text-align: center;
-	margin: 1em 0em;
-	width: 95%;
-}
