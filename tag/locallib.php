1cb1f9850e1285cdd2418d53ab1667fe7ea2b179
scyrma <scyrma>
2008-09-22 09:09:11 +0000

    MDL-16602: always display teachers (merge from 1.9)

--- tag/locallib.php
+++ tag/locallib.php
@@ -330,7 +330,7 @@ function tag_print_user_box($user, $return=false) {
     $usercontext = get_context_instance(CONTEXT_USER, $user->id);
     $profilelink = '';
 
-    if ( has_capability('moodle/user:viewdetails', $usercontext) ) {
+    if ( has_capability('moodle/user:viewdetails', $usercontext) || isteacherinanycourse($user->id) ) {
         $profilelink = $CFG->wwwroot .'/user/view.php?id='. $user->id;
     }
 
