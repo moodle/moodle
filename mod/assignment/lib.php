92b2bd00f84eaf0634a2ac120e9bddcf2c95b161
jerome <jerome>
2008-09-23 07:47:43 +0000

    MDL-12297: improvement for fix "assignment view submission" link into "Profil/Activty Report/all logs, merged from 19

--- mod/assignment/lib.php
+++ mod/assignment/lib.php
@@ -1048,8 +1048,7 @@ class assignment_base {
         $cm         = $this->cm;
 
         $tabindex = 1; //tabindex for quick grading tabbing; Not working for dropdowns yet
-
-        add_to_log($course->id, 'assignment', 'view submission', 'submissions.php?id='.$this->assignment->cmidnumber, $this->assignment->id, $this->cm->id);
+        add_to_log($course->id, 'assignment', 'view submission', 'submissions.php?id='.$this->cm->id, $this->assignment->id, $this->cm->id);
         $navigation = build_navigation($this->strsubmissions, $this->cm);
         print_header_simple(format_string($this->assignment->name,true), "", $navigation,
                 '', '', true, update_module_button($cm->id, $course->id, $this->strassignment), navmenu($course, $cm));
