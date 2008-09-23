306a52882b5f595b6e733dad3c0fb164789c2ef6
skodak <skodak>
2008-09-23 20:28:50 +0000

    MDL-16629 removing unfinished multiple editors code

--- lib/editor/htmlEditor.class.php
+++ lib/editor/htmlEditor.class.php
@@ -49,45 +49,6 @@ class htmlEditor {
                 $configured['tinymce'] = true;
                 break;
 
-            case 'fckeditor':
-                $configuration[] = $CFG->httpswwwroot ."/lib/editor/fckeditor/fckeditor.js";
-                $configuration[] = $CFG->httpswwwroot ."/lib/editor/fckeditor.js.php?course=". $courseid;
-                $configured['fckeditor'] = true;
-                break;
-
-//            case 'xinha':
-//                $configuration = <<<EOF
-//<script type="text/javascript">
-//    _editor_url  = "{$CFG->wwwroot}/lib/editor/xinha/"
-//    _editor_lang = "en";
-//    _editor_skin = "blue-look";
-//</script>
-//<script type="text/javascript" src="{$CFG->wwwroot}/lib/editor/xinha/XinhaCore.js"></script>
-//<script type="text/javascript" src="{$CFG->wwwroot}/lib/editor/xinha.js.php"></script>
-//EOF;
-//                break;
-//
-//            case 'yuirte':
-//                $configuration = <<<EOF
-//<!-- Skin CSS file -->
-//<link rel="stylesheet" type="text/css" href="{$CFG->wwwroot}/lib/editor/yui/build/assets/skins/sam/skin.css">
-//
-//<!-- Utility Dependencies -->
-//<script type="text/javascript" src="{$CFG->wwwroot}/lib/editor/yui/build/yahoo-dom-event/yahoo-dom-event.js"></script>
-//<script type="text/javascript" src="{$CFG->wwwroot}/lib/editor/yui/build/element/element-beta-min.js"></script>
-//
-//<!-- Needed for Menus, Buttons and Overlays used in the Toolbar -->
-//<script src="{$CFG->wwwroot}/lib/editor/yui/build/container/container_core-min.js"></script>
-//<script src="{$CFG->wwwroot}/lib/editor/yui/build/menu/menu-min.js"></script>
-//<script src="{$CFG->wwwroot}/lib/editor/yui/build/button/button-min.js"></script>
-//
-//<!-- Source file for Rich Text Editor-->
-//<script src="{$CFG->wwwroot}/lib/editor/yui/build/editor/editor-beta-min.js"></script>
-//
-//<script type="text/javascript" src="{$CFG->wwwroot}/lib/editor/yuirte.js.php"></script>
-//EOF;
-//                break;
-
             default:
                 $configured[$editor] = false;
                 break;
