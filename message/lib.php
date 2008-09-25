d6b4fed0a1a1fee8589ac5c5e91cee4b5f8f4c4a
stronk7 <stronk7>
2008-09-25 22:42:58 +0000

    Protect message settings with sesskey. MDL-16688 ; merged from 19_STABLE

--- message/lib.php
+++ message/lib.php
@@ -205,7 +205,7 @@ function message_print_search() {
 function message_print_settings() {
     global $USER;
 
-    if ($frm = data_submitted()) {
+    if ($frm = data_submitted() and confirm_sesskey()) {
 
         $pref = array();
         $pref['message_showmessagewindow'] = (isset($frm->showmessagewindow)) ? '1' : '0';
