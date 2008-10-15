2079bf04723f052de757d5037819def8189a73f7
moodler <moodler>
2008-10-15 05:43:18 +0000

    Bumped the version up to 1.9.3

--- version.php
+++ version.php
@@ -6,10 +6,10 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2007101527;  // YYYYMMDD      = date of the 1.9 branch (don't change)
+    $version = 2007101530;  // YYYYMMDD      = date of the 1.9 branch (don't change)
                             //         X     = release number 1.9.[0,1,2,3...]
                             //          Y.YY = micro-increments between releases
 
-    $release = '1.9.2+ (Build: 20081015)';     // Human-friendly version name
+    $release = '1.9.3 (Build: 20081015)';     // Human-friendly version name
 
 ?>
