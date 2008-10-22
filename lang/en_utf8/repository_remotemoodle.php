795834fee85881a202b4cbac6661ecf390d34f8f
jerome <jerome>
2008-10-22 04:13:21 +0000

    MDL-15352: first version of remote moodle plugin

---
+++ lang/en_utf8/repository_remotemoodle.php
@@ -0,0 +1,8 @@
+<?php // $Id$
+$string['configplugin'] = 'Configuration for remote Moodle file repository';
+$string['repositoryname'] = 'Remote Moodle Files';
+$string['repositorydesc'] = 'Repository on the remote Moodle server';
+$string['notitle'] = 'notitle';
+$string['peer'] = 'Peer';
+$string['remember'] = 'Remember me';
+$string['emptyfilelist'] = 'There are no files to show';
