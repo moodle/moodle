d5942d6a6fbcbb1aa4fccaf58d33391cb07cc28f
peterbulmer <peterbulmer>
2008-10-30 01:39:09 +0000

    MDL-17038 formatting correction
    Author: Peter Bulmer <peter.bulmer@catalyst.net.nz>

--- auth/mnet/auth.php
+++ auth/mnet/auth.php
@@ -797,7 +797,9 @@ class auth_plugin_mnet extends auth_plugin_base {
 
             $mnethostlogs = get_records_sql($mnethostlogssql);
 
-            if (false == $mnethostlogs) continue;
+            if ($mnethostlogs == false) {
+                continue;
+            }
 
             $processedlogs = array();
 
