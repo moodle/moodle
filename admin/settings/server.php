b50f80dd6fad30cafd45f579757263deabea2ead
skodak <skodak>
2008-11-01 17:54:04 +0000

    CONTRIB-849 phpPgAdmin integration hook; merged from MOODLE_19_STABLE

--- admin/settings/server.php
+++ admin/settings/server.php
@@ -254,8 +254,16 @@ $temp->add(new admin_setting_configselect('memcachedpconn', get_string('memcache
                                                  '1' => get_string('yes'))));
 $ADMIN->add('server', $temp);
 
-if (file_exists("$CFG->dirroot/$CFG->admin/mysql/frame.php")) {
-    $ADMIN->add('server', new admin_externalpage('database', get_string('managedatabase'), "$CFG->wwwroot/$CFG->admin/mysql/frame.php"));
+$dbfamily = $DB->get_dbfamily();
+
+if ($dbfamily === 'mysql') {
+    if (file_exists("$CFG->dirroot/$CFG->admin/mysql/frame.php")) {
+        $ADMIN->add('server', new admin_externalpage('database', get_string('managedatabase'), "$CFG->wwwroot/$CFG->admin/mysql/frame.php"));
+    }
+} else if ($dbfamily === 'postgres') {
+    if (file_exists("$CFG->dirroot/$CFG->admin/pgsql/frame.php")) {
+        $ADMIN->add('server', new admin_externalpage('database', get_string('managedatabase'), "$CFG->wwwroot/$CFG->admin/pgsql/frame.php"));
+    }
 }
 
 } // end of speedup
