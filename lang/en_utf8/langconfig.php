e026d8af8f3654bb852ed568f74f8bd0b3b21687
stronk7 <stronk7>
2008-11-04 16:26:08 +0000

    MDLSITE-548 - langconfig: Adding thousands separator.

--- lang/en_utf8/langconfig.php
+++ lang/en_utf8/langconfig.php
@@ -27,5 +27,6 @@ $string['strftimetime'] = '%%I:%%M %%p';
 $string['thischarset'] = 'UTF-8';
 $string['thisdirection'] = 'ltr';
 $string['thislanguage'] = 'English';
+$string['thousandssep'] = ','; // thousands separator, for some languages it is '.' or ' '
 
 ?>
