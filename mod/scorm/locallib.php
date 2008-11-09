7f7946fdd7a816ee5ba580f9082eaa8c1e8ed2d6
piers <piers>
2008-11-09 22:27:18 +0000

    MDL-17178 - fix grade update so that it happens both when the lesson status, and the score changes.

--- mod/scorm/locallib.php
+++ mod/scorm/locallib.php
@@ -275,16 +275,12 @@ function scorm_insert_track($userid,$scormid,$scoid,$attempt,$element,$value,$fo
         $id = $DB->insert_record('scorm_scoes_track',$track);
     }
 
-    // MDL-9552, update the gradebook everything raw score is sent
-    // Scoring by learning objects also needs to be included in the gradebook update
     if (strstr($element, '.score.raw') ||
         (($element == 'cmi.core.lesson_status' || $element == 'cmi.completion_status') && ($track->value == 'completed' || $track->value == 'passed'))) {
         $scorm = $DB->get_record('scorm', array('id' => $scormid));
         $grademethod = $scorm->grademethod % 10;
-        if (strstr($element, '.score.raw') || $grademethod == GRADESCOES) {
-            include_once('lib.php');
-            scorm_update_grades($scorm, $userid);
-        }
+        include_once('lib.php');
+        scorm_update_grades($scorm, $userid);
     }
 
     return $id;
