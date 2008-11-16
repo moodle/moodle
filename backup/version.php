d3a94cbbe40bc9f15407932f645068e816c235bd
stronk7 <stronk7>
2008-11-16 23:38:27 +0000

    Mini-bump version

--- backup/version.php
+++ backup/version.php
@@ -6,7 +6,7 @@
 /// database (backup_version) to determine whether upgrades should
 /// be performed (see db/backup_*.php)
 
-    $backup_version = 2008030300;   // The current version is a date (YYYYMMDDXX)
-    $backup_release = '1.9';     // User-friendly version number
+    $backup_version = 2008030301;   // The current version is a date (YYYYMMDDXX)
+    $backup_release = '1.9.3';     // User-friendly version number
 
 ?>
