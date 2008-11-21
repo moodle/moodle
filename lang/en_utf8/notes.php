9280eac53065f2c0e663ae3a20f9c6ccb8048e83
mudrd8mz <mudrd8mz>
2008-11-21 22:46:31 +0000

    MDL-17356 Use Unix style end of lines

--- lang/en_utf8/notes.php
+++ lang/en_utf8/notes.php
@@ -1,1 +1,28 @@
-<?PHP // $Id$      // note.php $string['note'] = 'Note';$string['notes'] = 'Notes';$string['sitenotes'] = 'Site notes';$string['coursenotes'] = 'Course notes';$string['personalnotes'] = 'Personal notes';$string['created'] = 'created';$string['nonotes'] = 'There are no notes of this type yet';$string['notesnotvisible'] = 'You are not allowed to view the notes.';$string['addnewnote'] = 'Add a new note';    $string['addnewnoteselect'] = 'Select users to write notes about';$string['groupaddnewnote'] = 'Add a common note';$string['deleteconfirm'] = 'Delete this note?';$string['content'] = 'Content';$string['nocontent'] = 'Note content can not be empty';$string['nouser'] = 'You must select a user';$string['unknown'] = 'unknown';$string['bynameondate'] = 'by $a->name - $a->date';$string['publishstate'] = 'Status';$string['personal'] = 'personal';$string['course'] = 'course';$string['site'] = 'site';$string['editnote'] = 'Edit note';$string['deletenotes'] = 'Delete all notes';?>
+<?PHP // $Id$
+      // note.php 
+
+$string['addnewnote'] = 'Add a new note';    
+$string['addnewnoteselect'] = 'Select users to write notes about';
+$string['bynameondate'] = 'by $a->name - $a->date';
+$string['content'] = 'Content';
+$string['course'] = 'course';
+$string['coursenotes'] = 'Course notes';
+$string['created'] = 'created';
+$string['deleteconfirm'] = 'Delete this note?';
+$string['deletenotes'] = 'Delete all notes';
+$string['editnote'] = 'Edit note';
+$string['groupaddnewnote'] = 'Add a common note';
+$string['nocontent'] = 'Note content can not be empty';
+$string['nonotes'] = 'There are no notes of this type yet';
+$string['note'] = 'Note';
+$string['notes'] = 'Notes';
+$string['notesnotvisible'] = 'You are not allowed to view the notes.';
+$string['nouser'] = 'You must select a user';
+$string['personal'] = 'personal';
+$string['personalnotes'] = 'Personal notes';
+$string['publishstate'] = 'Status';
+$string['site'] = 'site';
+$string['sitenotes'] = 'Site notes';
+$string['unknown'] = 'unknown';
+
+?>
