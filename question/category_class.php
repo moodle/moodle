874be4b0f049cb3711d1fdd7b10ecd3c2f71c642
tjhunt <tjhunt>
2008-11-27 07:30:11 +0000

    question bank: MDL-17078 Error after deleting a question category.

--- question/category_class.php
+++ question/category_class.php
@@ -318,7 +318,7 @@ class question_category_object {
     /**
      * Deletes an existing question category
      *
-     * @param    int deletecat  id of category to delete
+     * @param int deletecat  id of category to delete
      */
     function delete_category($categoryid) {
         global $CFG;
@@ -332,10 +332,7 @@ class question_category_object {
         }
 
         /// Finally delete the category itself
-        if (delete_records("question_categories", "id", $category->id)) {
-            notify(get_string("categorydeleted", "quiz", format_string($category->name)), 'notifysuccess');
-            redirect($this->pageurl->out());//always redirect after successful action
-        }
+        delete_records("question_categories", "id", $category->id);
     }
     function move_questions_and_delete_category($oldcat, $newcat){
         question_can_delete_cat($oldcat);
