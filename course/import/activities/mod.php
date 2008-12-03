2e9cb9d29afd3a1acd510db5643b0aa597041be1
samulik <samulik>
2008-12-03 16:31:01 +0000

    course import: MDL-17497 PHP was running out of memory if a category had a lot of courses. Now get_courses only fetches course id and fullname.

--- course/import/activities/mod.php
+++ course/import/activities/mod.php
@@ -33,7 +33,7 @@
     }
 
     if (!empty($creator)) {
-        $cat_courses = get_courses($course->category);
+        $cat_courses = get_courses($course->category, $sort="c.sortorder ASC", $fields="c.id, c.fullname");
     } else {
         $cat_courses = array();
     }
