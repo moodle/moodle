36641dd264ea890ae73006b67d6ce1b17cd98eed
skodak <skodak>
2008-12-06 20:43:45 +0000

    MDL-17372 fixed regression in 3rd party admin reports; merged from MOODLE_19_STABLE

--- admin/settings/plugins.php
+++ admin/settings/plugins.php
@@ -280,6 +280,7 @@ foreach (get_list_of_plugins($CFG->admin.'/report') as $plugin) {
         continue;
     }
     // old style 3rd party plugin without settings.php
-    $ADMIN->add('reports', new admin_externalpage('report'.$plugin, $plugin, $index_path, 'moodle/site:viewreports'));
+    $www_path = "$CFG->dirroot/$CFG->admin/report/$plugin/index.php";
+    $ADMIN->add('reports', new admin_externalpage('report'.$plugin, $plugin, $www_path, 'moodle/site:viewreports'));
 }
 
