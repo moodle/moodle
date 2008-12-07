4ebc71d9411510ce75b990710e949cb76e96cffc
stronk7 <stronk7>
2008-12-07 19:00:44 +0000

    MDL-17545 activity names filter: Improve handling when name contains htmlentities() chars. Backported from HEAD.

--- filter/activitynames/filter.php
+++ filter/activitynames/filter.php
@@ -41,13 +41,15 @@
                     //Exclude labels and hidden items
                     if ($activity->mod != "label" && $activity->visible) {
                         $title = trim(strip_tags(urldecode($activity->name)));
+                        $currentname = trim(urldecode($activity->name));
+                        $entitisedname  = s($currentname);
                         /// Avoid empty or unlinkable activity names
                         if (!empty($title)) {
                             $title = str_replace('"', "'", $title);
                             $href_tag_begin = "<a class=\"autolink\" title=\"$title\" href=\"$CFG->wwwroot/mod/$activity->mod/view.php?id=$activity->cm\" $CFG->frametarget>";
-                            $currentname = urldecode($activity->name);
-                            if ($currentname = trim($currentname)) {
-                                $activitylist[] = new filterobject($currentname, $href_tag_begin, '</a>', false, true);
+                            $activitylist[] = new filterobject($currentname, $href_tag_begin, '</a>', false, true);
+                            if ($currentname != $entitisedname) { /// If name has some entity (&amp; &quot; &lt; &gt;) add that filter too. MDL-17545
+                                $activitylist[] = new filterobject($entitisedname, $href_tag_begin, '</a>', false, true);
                             }
                         }
                     }
