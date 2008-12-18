968788be74b39c9c00f1b656b035018351a317dd
moodler <moodler>
2008-12-18 06:44:51 +0000

    Cleaning up the main version file.

--- version.php
+++ version.php
@@ -6,9 +6,9 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2007101532.11;  // YYYYMMDD          = date of the 1.9 branch (don't change)
-                                        //                   X       = release number 1.9.[0,1,2,3...]
-                                        //                     Y.YY = micro-increments between releases
+    $version = 2007101533;  // YYYYMMDD      = date of the 1.9 branch (don't change)
+                            //         X     = release number 1.9.[0,1,2,3...]
+                            //          Y.YY = micro-increments between releases
 
     $release = '1.9.3+ (Build: 20081218)';     // Human-friendly version name
 
