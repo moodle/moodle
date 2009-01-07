bc4c9800109ba09b50d33c8f18aafe66363ee4d1
skodak <skodak>
2009-01-07 19:56:30 +0000

    MDL-17797 fixed cap definition - thanks Andrea Bicciolo; merged from MOODLE_19_STABLE

--- lib/db/access.php
+++ lib/db/access.php
@@ -1085,7 +1085,7 @@ $moodle_capabilities = array(
     ),
 
     'moodle/site:mnetlogintoremote' => array(
-        'riskbitmask' => RISK_PERSONAL | RISK_XSS,
+
         'captype' => 'read',
         'contextlevel' => CONTEXT_SYSTEM,
         'legacy' => array(
