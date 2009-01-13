d6bc965bb861e4d31d7cf8f838bde2976cfa7ea7
skodak <skodak>
2009-01-13 21:13:16 +0000

    MDL-17878 basic UI for looking at config log

---
+++ admin/report/configlog/settings.php
@@ -0,0 +1,3 @@
+<?php  //$Id$
+
+$ADMIN->add('reports', new admin_externalpage('reportconfiglog', get_string('configlog', 'report_configlog'), "$CFG->wwwroot/$CFG->admin/report/configlog/index.php"));
---
+++ lang/en_utf8/report_configlog.php
@@ -0,0 +1,11 @@
+<?php  // $Id$
+
+$string['configlog'] = 'Config changes';
+
+$string['timemodified'] = 'Date';
+$string['plugin'] = 'Plugin';
+$string['setting'] = 'Setting';
+$string['value'] = 'New value';
+$string['oldvalue'] = 'Original value';
+
+?>
