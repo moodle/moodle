d9e233af760febbad6e6b823a7a71cf68f82a6e5
skodak <skodak>
2009-01-14 16:26:00 +0000

    MDL-12886 adding killswitch for webservices

--- webservice/rest/testclient/config_rest.php
+++ webservice/rest/testclient/config_rest.php
@@ -12,6 +12,11 @@
 
 require_once("../../../config.php");
 require_once ('lib.php');
+
+if (empty($CFG->enablewebservices)) {
+    die;
+}
+
 $CFG->serverurl = $CFG->wwwroot.'/webservice/rest/server.php';
 if (!function_exists('curl_init')) die ('CURL library was not found!');
 
