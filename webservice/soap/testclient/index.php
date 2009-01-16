eb9f6a08a5779af9d354398522fc36569d9fdeeb
jerome <jerome>
2009-01-16 01:17:55 +0000

    web service MDL-12886 few more SOAP tests

--- webservice/soap/testclient/index.php
+++ webservice/soap/testclient/index.php
@@ -22,6 +22,12 @@ $client = new SoapClient("../moodle.wsdl",array(
 try {
     var_dump($client->tmp_get_users("admin"));
     printLastRequestResponse($client);
+    var_dump($client->tmp_create_user("mockuser6","firstname6","lastname6","mockuser6@mockuser6.com", "password6"));
+    printLastRequestResponse($client);
+    var_dump($client->tmp_update_user("mockuser6",1,"mockuser6b","firstname6b"));
+    printLastRequestResponse($client);
+    var_dump($client->tmp_delete_user("mockuser6b",1));
+    printLastRequestResponse($client);
 } catch (SoapFault $exception) {
     echo $exception;
 }
