2a02fa4abe2375a69089094201c2477ed8c04a71
Jonathan Newman <jonathan.newman@catalyst.net.nz>
2009-01-20 23:53:34 +0000

    cvsimport fixups

---
+++ lang/en_utf8/qtype_missingtype.php
@@ -0,0 +1,5 @@
+<?php // $Id: qtype_missingtype.php,v 1.1 2008-09-09 08:43:39 tjhunt Exp $ 
+
+$string['missingtype'] = 'Missing type';
+$string['warningmissingtype'] = '<b>This question is of a type that has not been installed on your Moodle yet.<br />Please alert your Moodle administrator.</b>';
+?>
