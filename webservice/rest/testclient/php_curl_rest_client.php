7a3f6bd639a7b556d841de43b295cf7e31c2d5c6
pigui <pigui>
2009-01-30 11:55:10 +0000

    Modification of testclient index for create_group and add_groupmember funtions

--- webservice/rest/testclient/php_curl_rest_client.php
+++ webservice/rest/testclient/php_curl_rest_client.php
@@ -1,26 +1,35 @@
 <?php
 /**
  * Created on 10/17/2008
- * 
+ *
  * Rest Test Client
  *
  * @author David Castro Garcia
  * @author Ferran Recio Calderó
  * @author Jerome Mouneyrac
+ * @author Jordi Piguillem
  */
 
 require_once ('config_rest.php');
 
 start_interface (false);
 
-$links = array( array('getusers.php','get_users()'),
+$links = array( array('USERS'),
+				array('getusers.php','get_users()'),
                 array('createuser.php','create_user()'),
                 array('deleteuser.php','delete_user()'),
-                array('updateuser.php','update_user()'));
+                array('updateuser.php','update_user()'),
+                array('GROUPS'),
+                array('creategroup.php','create_group()'),
+                array('addgroupmember.php', 'add_groupmember()'));
 
 echo '<ul>';
 foreach ($links as $link) {
-    echo '<li><a href="'.$link[0].'">'.$link[1].'</a></li>';
+	if (sizeof($link)==2){
+    	echo '<li><a href="'.$link[0].'">'.$link[1].'</a></li>';
+	} else {
+		echo '</ul><h2>'.$link[0].'</h2><ul>';
+	}
 }
 echo '</ul>';
 
