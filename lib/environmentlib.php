51bddf22fd6230b63122b398fc77f0a7a24babc6
skodak <skodak>
2009-02-08 21:30:06 +0000

    MDL-9796 fixed error array when environment file broken

--- lib/environmentlib.php
+++ lib/environmentlib.php
@@ -471,7 +471,7 @@ function environment_check_php_extensions($version) {
         $result = new environment_results('php_extension');
         $result->setStatus(false);
         $result->setErrorCode(NO_VERSION_DATA_FOUND);
-        return $result;
+        return array($result);
     }
 
 /// Extract the php_extension part
@@ -480,7 +480,7 @@ function environment_check_php_extensions($version) {
         $result = new environment_results('php_extension');
         $result->setStatus(false);
         $result->setErrorCode(NO_PHP_EXTENSIONS_SECTION_FOUND);
-        return $result;
+        return array($result);
     }
 /// Iterate over extensions checking them and creating the needed environment_results
     foreach($data['#']['PHP_EXTENSIONS']['0']['#']['PHP_EXTENSION'] as $extension) {
