6aec578ba1ce29dc88c16b153694cb7d24c0fdaa
nicolasconnault <nicolasconnault>
2009-02-25 06:57:59 +0000

    MDL-18321 Changed default permissions for overview report

--- grade/report/overview/db/access.php
+++ grade/report/overview/db/access.php
@@ -30,8 +30,6 @@ $gradereport_overview_capabilities = array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_COURSE,
         'legacy' => array(
-            'teacher' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
             'student' => CAP_ALLOW,
             'admin' => CAP_ALLOW
         )
