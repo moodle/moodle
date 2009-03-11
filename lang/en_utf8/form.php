a292125f20e727a31baaaef4a9185eb116fae18a
tjhunt <tjhunt>
2009-03-11 05:37:22 +0000

    sort lang file.

--- lang/en_utf8/form.php
+++ lang/en_utf8/form.php
@@ -1,41 +1,39 @@
 <?PHP // $Id$
 
-$string['mustbeoverriden'] = 'Abstract form_definition() method in class $a must be overriden, please fix the code.';
-$string['somefieldsrequired'] = 'There are required fields in this form marked$a.'; 
+$string['addfields']='Add $a fields to form';
+$string['advancedelement']='Advanced element';
+$string['day'] = 'Day';
+$string['display'] = 'Display';
 $string['err_alphanumeric']='You must enter only letters or numbers here.';
 $string['err_email']='You must enter a valid email address here.';
 $string['err_lettersonly']='You must enter only letters here.';
 $string['err_maxlength']='You must enter not more than $a->format characters here.';
 $string['err_minlength']='You must enter at least $a->format characters here.';
-$string['err_nopunctuation']='You must enter no punctuation characters here.';
 $string['err_nonzero']='You must enter a number not starting with a 0 here.';
+$string['err_nopunctuation']='You must enter no punctuation characters here.';
 $string['err_numeric']='You must enter a number here.';
 $string['err_rangelength']='You must enter between {$a->format[0]} and {$a->format[1]} characters here.';
 $string['err_required']='You must supply a value here.';
+$string['general'] = 'General';
+$string['hideadvanced']='Hide Advanced';
+$string['hour'] = 'Hour';
+$string['minute'] = 'Minute';
+$string['miscellaneoussettings']='Miscellaneous Settings';
+$string['modstandardels']='Common module settings';
+$string['month'] = 'Month';
+$string['mustbeoverriden'] = 'Abstract form_definition() method in class $a must be overriden, please fix the code.';
 $string['nomethodforaddinghelpbutton'] = 'There is no method for adding a help button to form element $a->name (class $a->classname)';
 $string['nonexistentformelements'] = 'Trying to add help buttons to nonexistent form elements : $a';
-$string['requiredelement'] = 'Required field';
-$string['general'] = 'General';
 $string['optional'] = 'Optional';
-$string['unmaskpassword'] = 'Unmask';
-$string['revealpassword'] = 'Reveal';
-$string['modstandardels']='Common module settings';
-$string['miscellaneoussettings']='Miscellaneous Settings';
 $string['othersettings']='Other settings';
-$string['addfields']='Add $a fields to form';
-$string['day']='Day';
-$string['month']='Month';
-$string['year']='Year';
-$string['hour']='Hour';
-$string['minute']='Minute';
-
-$string['advancedelement']='Advanced element';
-$string['hideadvanced']='Hide Advanced';
-$string['showadvanced']='Show Advanced';
-
-$string['timing'] = 'Timing';
+$string['requiredelement'] = 'Required field';
+$string['revealpassword'] = 'Reveal';
 $string['security'] = 'Security';
-$string['display'] = 'Display';
-
 $string['selectallornone'] = 'Select all/none';
+$string['showadvanced']='Show Advanced';
+$string['somefieldsrequired'] = 'There are required fields in this form marked$a.'; 
+$string['timing'] = 'Timing';
+$string['unmaskpassword'] = 'Unmask';
+$string['year'] = 'Year';
+
 ?>
