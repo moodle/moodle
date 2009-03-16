80fe7b9ef7b19ea2a05b49d65b1c365b26d0d633
thepurpleblob <thepurpleblob>
2009-03-16 14:19:46 +0000

    MDL-18555: Adding better handling for unsupported question types.

---
+++ lang/en_utf8/qformat_xml.php
@@ -0,0 +1,4 @@
+<?php
+$string['invalidxml'] = 'Invalid xml file - string expected (use CDATA?)';
+$string['unsupportedexport'] = 'Question type $a is not supported by xml export';
+?>
--- question/format/xml/format.php
+++ question/format/xml/format.php
@@ -106,7 +106,7 @@ class qformat_xml extends qformat_default {
         }
         if ($istext) {
             if (!is_string($xml)) {
-                $this->error( 'Invalid xml file - string expected (use CDATA?)' );
+                $this->error( get_string('invalidxml','qformat_xml') );
             }
             $xml = addslashes( trim( $xml ) );
         }
@@ -1003,7 +1003,7 @@ class qformat_xml extends qformat_default {
         default:
             // try support by optional plugin
             if (!$data = $this->try_exporting_using_qtypes( $question->qtype, $question )) { 
-                error( "Unsupported question type $question->qtype" );
+                notify( get_string( 'unsupportedexport','qformat_xml',$QTYPES[$question->qtype]->menu_name() ) );
             }
             $expout .= $data;
         }
