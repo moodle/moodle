7d6b6b1a16a65e2c3e2d325638a252e2749c911c
fmarier <fmarier>
2009-03-27 03:27:19 +0000

    mod/resource: MDL-18691 allow custom resource types to specify their name in their own lang directory
    Before this change, the full name of the resource type had to be stored in the "resource" namespace (for example in /mod/resource/lang/en_utf8/resource.php).
    
    Now it can be put in the "resource_foo" namespace:
    
    e.g. in /mod/resource/type/foo/lang/en_utf8/resource_foo.php:
    
    <?php
      $string['resourcetypefoo'] = 'Link to a Foo Bar document';
    ?>

--- mod/resource/lib.php
+++ mod/resource/lib.php
@@ -597,7 +597,7 @@ function resource_get_types() {
             $type->modclass = MOD_CLASS_RESOURCE;
             $type->name = $resourcetype;
             $type->type = "resource&amp;type=$resourcetype";
-            $type->typestr = get_string("resourcetype$resourcetype", 'resource');
+            $type->typestr = resource_get_name($resourcetype);
             $types[] = $type;
         }
     }
@@ -711,6 +711,21 @@ function resource_delete_warning($course, $files) {
     } else {
         return false;
     }
+}
+
+/**
+ * Returns the full name of the given resource type.  The name can
+ * either be set at the resource type level or at the resource module
+ * level.
+ *
+ * @param string $type shortname (or directory name) of the resource type
+ */
+function resource_get_name($type) {
+    $name = get_string("resourcetype$type", "resource_$type");
+    if (substr($name, 0, 2) === '[[') {
+        $name = get_string("resourcetype$type", 'resource');
+    }
+    return $name;
 }
 
 ?>
