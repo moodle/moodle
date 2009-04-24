db523de37dbf40bc53c75444b1a52d65366e9e29
dongsheng <dongsheng>
2009-04-24 04:57:24 +0000

    "MDL-18830, add license names strings"

--- lang/en_utf8/repository_flickr_public.php
+++ lang/en_utf8/repository_flickr_public.php
@@ -1,11 +1,21 @@
 <?php // $Id$
+$string['all'] = 'All';
 $string['apikey'] = 'API key';
 $string['callbackurl'] = 'Callback URL';
 $string['configplugin'] = 'Flickr public configuration';
+// flickr licenses
+$string['by-nc-sa'] = 'Attribution-NonCommercial-ShareAlike License';
+$string['by-nc'] = 'Attribution-NonCommercial License';
+$string['by-nc-nd'] = 'Attribution-NonCommercial-NoDerivs License';
+$string['by'] = 'Attribution License';
+$string['by-sa'] = 'Attribution-ShareAlike License';
+$string['by-nd'] = 'Attribution-NoDerivs License';
+$string['creativecommonscommercial'] = 'Only creative commons commercial';
 $string['emailaddress'] = 'Email address';
 $string['fulltext'] = 'Full text';
 $string['information'] = '<div>Get <a href=\"http://www.flickr.com/services/api/keys/\">Flickr API Key</a> for your Moodle site. </div>';
 $string['invalidemail'] = 'Invalid email address for flickr';
+$string['license'] = 'License';
 $string['notitle'] = 'notitle';
 $string['nullphotolist'] = 'There are no photos in this account';
 $string['tag'] = 'Tag';
