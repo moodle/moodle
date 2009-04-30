e8c8cee99a9de0e920881a4196a6c1a06188a23a
tjhunt <tjhunt>
2009-04-30 03:50:00 +0000

    blocklib: MDL-19010 add standard boilerplate at the top of the file.

--- lib/blocklib.php
+++ lib/blocklib.php
@@ -1,6 +1,34 @@
 <?php //$Id$
 
-//This library includes all the necessary stuff to use blocks in course pages
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//          http://moodle.org                                            //
+//                                                                       //
+// Copyright (C) 1999 onwards Martin Dougiamas  http://dougiamas.com     //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+/**
+ * This library includes all the necessary stuff to use blocks on pages in Moodle.
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package pages
+ */
 
 define('BLOCK_MOVE_LEFT',   0x01);
 define('BLOCK_MOVE_RIGHT',  0x02);
