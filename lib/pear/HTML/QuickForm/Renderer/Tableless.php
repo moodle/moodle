5c89242e505f3e15e56718ef0b12581147c71d8b
skodak <skodak>
2009-04-30 10:06:11 +0000

    MDL-19030 very ugly hack - do not try to add id to labels of element group - they do not have it; merged from MOODLE_19_STABLE

--- lib/pear/HTML/QuickForm/Renderer/Tableless.php
+++ lib/pear/HTML/QuickForm/Renderer/Tableless.php
@@ -186,7 +186,7 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
             } else {
                 $id = $element->getName();
             }
-            if (!empty($id) and !$element->isFrozen()) { // moodle hack
+            if (!empty($id) and !$element->isFrozen() and !is_a($element, 'MoodleQuickForm_group')) { // moodle hack
                 $html = str_replace('<label', '<label for="' . $id . '"', $html);
                 $element_html = preg_replace('#name="' . $id . '#',
                                              'id="' . $id . '" name="' . $id . '',
