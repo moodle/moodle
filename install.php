d764d4cae4df05793946528ab3b89ca7f5a5db5b
skodak <skodak>
2009-05-21 09:58:18 +0000

    MDL-19235 updated license header in installer - completely rewritten for 2.0

--- install.php
+++ install.php
@@ -1,6 +1,27 @@
-<?php /// $Id$
-      /// install.php - helps admin user to create a config.php file
+<?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This script creates config.php file during installation.
+ *
+ * @package    moodlecore
+ * @copyright  2009 Petr Skoda (http://skodak.org)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 if (isset($_REQUEST['lang'])) {
     $lang = eregi_replace('[^A-Za-z0-9_-]', '', $_REQUEST['lang']);
