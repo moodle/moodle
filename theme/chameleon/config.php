994d4a3be12e57891e85bc1dc128fa0eac15a2cb
dongsheng <dongsheng>
2009-05-25 09:19:27 +0000

    "MDL-18971, fix forum unread post highlighting issue, thanks Eric straw's patch, merged from 1.9"

--- theme/chameleon/config.php
+++ theme/chameleon/config.php
@@ -13,7 +13,7 @@ $THEME->sheets = array('user_styles');
 ////////////////////////////////////////////////////////////////////////////////
 
 
-$THEME->standardsheets = array('styles_layout');
+$THEME->standardsheets = array('styles_layout', 'styles_color');
 
 /// This variable can be set to an array containing
 /// filenames from the *STANDARD* theme.  If the 
--- theme/orangewhitepda/config.php
+++ theme/orangewhitepda/config.php
@@ -13,7 +13,7 @@ $THEME->sheets = array('styles_pda');
 ////////////////////////////////////////////////////////////////////////////////
 
 
-$THEME->standardsheets = array('styles_layout');
+$THEME->standardsheets = array('styles_layout', 'styles_color');
 
 /// This variable can be set to an array containing
 /// filenames from the *STANDARD* theme.  If the 
