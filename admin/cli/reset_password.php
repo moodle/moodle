57295c9224da2593734225e2f1a499e66b290960
skodak <skodak>
2009-05-31 12:34:08 +0000

    MDL-15249 password reset cli script improvements

--- admin/cli/reset_password.php
+++ admin/cli/reset_password.php
@@ -25,7 +25,7 @@
  */
 
 if (isset($_SERVER['REMOTE_ADDR'])) {
-    error_log("admin/cli/upgrade.php can not be called from web server!");
+    error_log("admin/cli/reset_password.php can not be called from web server!");
     exit;
 }
 
