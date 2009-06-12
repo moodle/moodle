8cc35d2055db4a45302be6c1ecd6722aa0ec10c4
skodak <skodak>
2009-06-12 08:50:50 +0000

    MDL-14679 license header fixes

--- lib/dtllib.php
+++ lib/dtllib.php
@@ -1,7 +1,7 @@
 <?php
 
-// This file is part of Moodle - http://moodle.org/ 
-// 
+// This file is part of Moodle - http://moodle.org/
+//
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -11,49 +11,47 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 
 /**
  * DTL == Dtatabase Transfer Library
  *
  * This library includes all the required functions used to handle
  * transfer of data from one database to another.
- * 
- * @copyright 2008 onwards Andrei Bautu 
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @package   moodlecore
+ *
+ * @package    moodlecore
+ * @subpackage DTL
+ * @copyright  2008 Andrei Bautu
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
- /** Require {@link ddllib.php} */
+// Require {@link ddllib.php}
 require_once($CFG->libdir.'/ddllib.php');
-/** Require {@link database_exporter.php} */
+// Require {@link database_exporter.php}
 require_once($CFG->libdir.'/dtl/database_exporter.php');
-/** Require {@link xml_database_exporter.php} */
+// Require {@link xml_database_exporter.php}
 require_once($CFG->libdir.'/dtl/xml_database_exporter.php');
-/** Require {@link file_xml_database_exporter.php} */
+// Require {@link file_xml_database_exporter.php}
 require_once($CFG->libdir.'/dtl/file_xml_database_exporter.php');
-/** Require {@link string_xml_database_exporter.php} */
+// Require {@link string_xml_database_exporter.php}
 require_once($CFG->libdir.'/dtl/string_xml_database_exporter.php');
-/** Require {@link database_mover.php} */
+// Require {@link database_mover.php}
 require_once($CFG->libdir.'/dtl/database_mover.php');
-/** Require {@link database_importer.php} */
+// Require {@link database_importer.php}
 require_once($CFG->libdir.'/dtl/database_importer.php');
-/** Require {@link xml_database_importer.php} */
+// Require {@link xml_database_importer.php}
 require_once($CFG->libdir.'/dtl/xml_database_importer.php');
-/** Require {@link file_xml_database_importer.php} */
+// Require {@link file_xml_database_importer.php}
 require_once($CFG->libdir.'/dtl/file_xml_database_importer.php');
-/** Require {@link string_xml_database_importer.php} */
+// Require {@link string_xml_database_importer.php}
 require_once($CFG->libdir.'/dtl/string_xml_database_importer.php');
 
 /**
  * Exception class for db transfer
  * @see moodle_exception
- *
- * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @package moodlecore
  */
 class dbtransfer_exception extends moodle_exception {
     /**
