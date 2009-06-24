6847d4d82438dde2b86e73f4151b7f1e18bf9258
jerome <jerome>
2009-06-24 08:21:16 +0000

    repository MDL-16909 missing mahara language file

---
+++ lang/en_utf8/repository_mahara.php
@@ -0,0 +1,16 @@
+<?php // $Id$
+$string['configplugin'] = 'Configuration for Mahara repository';
+$string['repositoryname'] = 'Mahara';
+$string['repositorydesc'] = 'Files on a remote Moodle server';
+$string['notitle'] = 'notitle';
+$string['peer'] = 'Peer';
+$string['remember'] = 'Remember me';
+$string['emptyfilelist'] = 'There are no files to show';
+$string['remoterep_name'] = 'Mahara';
+$string['remoterep_description'] = 'Allow the service to be discovered';
+$string['connectionfailure'] = 'Failed to retrieve file listing - The host moodle has either a version older than 2.0, either its Mahara Repository service hasn\'t been activated';
+$string['failtoretrievelist'] = 'List could not be retrieved or is empty';
+$string['usernotfound'] = 'The user $a is not registered into the Mahara';
+$string['usercannotaccess'] = 'You ($a) cannot access to this file';
+$string['nopeer'] = 'Please setup some <a href=\"http://docs.moodle.org/en/admin/mnet/peers#Peer_to_Peer_Network\">Moodle peers</a> for your Mahara site.';
+$string['hostnotfound'] = 'Cannot find the Mahara site into the database - contact your system administrator';
