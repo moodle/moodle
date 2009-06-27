88931a0e4114ed44d71c733909c5b6d19ad45b9a
poltawski <poltawski>
2009-06-27 14:48:18 +0000

    Whoops, didn't mean to remove Andrei's copyright.

--- lib/dml/pdo_moodle_recordset.php
+++ lib/dml/pdo_moodle_recordset.php
@@ -21,7 +21,7 @@
  *
  * @package    moodlecore
  * @subpackage DML
- * @copyright  2008 Petr Skoda (http://skodak.org)
+ * @copyright  2008 Andrei Bautu
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
