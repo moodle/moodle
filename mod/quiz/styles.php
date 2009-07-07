8f4e377dfe394e9a306ae8d7fdddf215c77c93c1
tjhunt <tjhunt>
2009-07-07 03:49:13 +0000

    MDL-19690 - pixpath - cannot use $OUTPUT in CSS. Use $PAGE->theme instead.

--- mod/quiz/styles.php
+++ mod/quiz/styles.php
@@ -1,3 +1,3 @@
 #quiznavigation .qnbutton.flagged {
-  background-image: url('<?php echo $OUTPUT->old_icon_url('i/ne_red_mark') ?>');
+  background-image: url('<?php echo $PAGE->theme->old_icon_url('i/ne_red_mark') ?>');
 }
