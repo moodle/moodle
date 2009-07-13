088d7399396fbea3ff1a7671e8b27c1f1bb3a75f
moodlerobot <moodlerobot>
2009-07-13 00:40:21 +0000

    Updated the 18 build version to 20090713

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
                             //         X  = release number 1.8.[0,1,2,3...]
                             //          Y = micro-increments between releases
 
-   $release = '1.8.9+ (Build: 20090712)';     // Human-friendly version name
+   $release = '1.8.9+ (Build: 20090713)';     // Human-friendly version name
 
 ?>
