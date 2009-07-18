4398ce111cd1df7d2ed9c390e2adf5d5f3b13e51
diml <diml>
2009-07-18 23:01:41 +0000

    adding an identifying version file. Not handled by upgrade core API.

---
+++ search/version.php
@@ -0,0 +1,14 @@
+<?php // $Id$
+
+////////////////////////////////////////////////////////////////////////////////
+//  Code fragment to define the search version etc.
+//  This fragment is called by /admin/index.php
+//  
+//  Consider it IS NOT handled by Moodle core upgrade code. 
+//  Just for identification
+////////////////////////////////////////////////////////////////////////////////
+
+$search->version  = 2009031000;
+$search->requires = 2007101509;  // Requires this Moodle version
+
+?>
