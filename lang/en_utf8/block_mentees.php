275769aafa6a053f26ae256daefba7f8d03100b8
tjhunt <tjhunt>
2009-07-29 05:19:45 +0000

    blocks: MDL-19889 Convert mentees block to use edit_form.php - lang string.

--- lang/en_utf8/block_mentees.php
+++ lang/en_utf8/block_mentees.php
@@ -1,6 +1,7 @@
 <?PHP 
 $string['blockname'] = 'Mentees';
 $string['configtitle'] = 'Block title';
+$string['configtitleblankhides'] = 'Block title (no title if blank)';
 $string['leaveblanktohide'] = 'leave blank to hide the title';
 $string['newmenteesblock'] = '(new Mentees block)';
 ?>
