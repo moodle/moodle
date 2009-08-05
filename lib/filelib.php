d5934b35948d470d979b954165304b3a3451e84e
mudrd8mz <mudrd8mz>
2009-08-05 11:19:34 +0000

    MDL-20001 use passed $itemid instead of $data->id

--- lib/filelib.php
+++ lib/filelib.php
@@ -178,9 +178,9 @@ function file_prepare_standard_editor($data, $field, array $options, $context=nu
         $options['noclean'] = false;
     }
 
-    if (empty($data->id) or empty($context)) {
+    if (is_null($itemid) or is_null($context)) {
         $contextid = null;
-        $data->id = null;
+        $itemid = null;
         if (!isset($data->{$field})) {
             $data->{$field} = '';
         }
@@ -208,7 +208,7 @@ function file_prepare_standard_editor($data, $field, array $options, $context=nu
 
     if ($options['maxfiles'] != 0) {
         $draftid_editor = file_get_submitted_draft_itemid($field);
-        $currenttext = file_prepare_draft_area($draftid_editor, $contextid, $filearea, $data->id, $options, $data->{$field});
+        $currenttext = file_prepare_draft_area($draftid_editor, $contextid, $filearea, $itemid, $options, $data->{$field});
         $data->{$field.'_editor'} = array('text'=>$currenttext, 'format'=>$data->{$field.'format'}, 'itemid'=>$draftid_editor);
     } else {
         $data->{$field.'_editor'} = array('text'=>$data->{$field}, 'format'=>$data->{$field.'format'}, 0);
@@ -288,15 +288,15 @@ function file_prepare_standard_filemanager($data, $field, array $options, $conte
     if (!isset($options['subdirs'])) {
         $options['subdirs'] = false;
     }
-    if (empty($data->id) or empty($context)) {
-        $data->id = null;
+    if (is_null($itemid) or is_null($context)) {
+        $itemid = null;
         $contextid = null;
     } else {
         $contextid = $context->id;
     }
 
     $draftid_editor = file_get_submitted_draft_itemid($field.'_filemanager');
-    file_prepare_draft_area($draftid_editor, $contextid, $filearea, $data->id, $options);
+    file_prepare_draft_area($draftid_editor, $contextid, $filearea, $itemid, $options);
     $data->{$field.'_filemanager'} = $draftid_editor;
 
     return $data;
@@ -329,7 +329,7 @@ function file_postupdate_standard_filemanager($data, $field, array $options, $co
         $data->$field = '';
 
     } else {
-        file_save_draft_area_files($data->{$field.'_filemanager'}, $context->id, $filearea, $data->id, $options);
+        file_save_draft_area_files($data->{$field.'_filemanager'}, $context->id, $filearea, $itemid, $options);
         $fs = get_file_storage();
 
         if ($fs->get_area_files($context->id, $filearea, $itemid)) {
