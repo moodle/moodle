3097018f7aedd99fd7897f45252671d1553cc82c
nicolasconnault <nicolasconnault>
2009-08-06 01:36:17 +0000

    MDL-19809 Upgraded calls to print_headline

--- mod/glossary/lib.php
+++ mod/glossary/lib.php
@@ -296,7 +296,7 @@ function glossary_user_complete($course, $user, $mod, $glossary) {
  * @return bool
  */
 function glossary_print_recent_activity($course, $viewfullnames, $timestart) {
-    global $CFG, $USER, $DB;
+    global $CFG, $USER, $DB, $OUTPUT;
 
     //TODO: use timestamp in approved field instead of changing timemodified when approving in 2.0
 
@@ -346,7 +346,7 @@ function glossary_print_recent_activity($course, $viewfullnames, $timestart) {
     if (!$entries) {
         return false;
     }
-    print_headline(get_string('newentries', 'glossary').':');
+    echo $OUTPUT->heading(get_string('newentries', 'glossary').':');
 
     $strftimerecent = get_string('strftimerecent');
     foreach ($entries as $entry) {
