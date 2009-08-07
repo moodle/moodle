99e2c8bd7aa255f4f88fb2bb5b832cc6bfd2841b
dongsheng <dongsheng>
2009-08-07 10:09:21 +0000

    "MDL-19181, display plugin instances based on capabilities"

---
+++ repository/flickr_public/db/access.php
@@ -0,0 +1,15 @@
+<?php
+
+$repository_flickr_public_capabilities = array(
+
+    'repository/flickr:view' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'legacy' => array(
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'admin' => CAP_ALLOW
+        )
+    )
+);
