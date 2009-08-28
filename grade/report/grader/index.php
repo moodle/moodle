c14ee7225b6034340743d59cc26ae81193295d8d
nicolasconnault <nicolasconnault>
2009-08-28 06:56:08 +0000

    MDL-20179 Replaced incompatible code with something cross-browser compatible

--- grade/report/grader/index.php
+++ grade/report/grader/index.php
@@ -198,11 +198,11 @@ YAHOO.namespace("graderreport");
 
 function init() {
     // Adjust height of header c0
-    var rows = document.getElementsByClassName('heading_name_row');
-    var header_cell_region = YAHOO.util.Dom.getRegion(rows[rows.length-1].firstChild);
+    var rows = YAHOO.util.Dom.getElementsByClassName('heading_name_row');
+    var header_cell_region = YAHOO.util.Dom.getRegion();
     var height = header_cell_region.bottom - header_cell_region.top;
     YAHOO.util.Dom.setStyle('studentheader', 'height', height + 'px');
-    
+
     // attach event listener to the table for mouseover and mouseout
     var table = document.getElementById('user-grades');
     YAHOO.util.Event.on(table, 'mouseover', YAHOO.graderreport.mouseoverHandler);
