840f10cca4c48ab8863032dd33f0e3669b8837c6
gbateson <gbateson>
2009-09-01 01:03:43 +0000

    MDL-20184 more efficient and accurate detection of missing nodes in xml tree when importing Hot Potatoes XML files to Moodle question bank / quiz module

--- question/format/hotpot/format.php
+++ question/format/hotpot/format.php
@@ -549,8 +549,29 @@ class hotpot_xml_tree {
     }
     function xml_value($tags, $more_tags="[0]['#']") {
 
-        $tags = empty($tags) ? '' : "['".str_replace(",", "'][0]['#']['", $tags)."']";
-        eval('$value = &$this->xml'.$this->xml_root.$tags.$more_tags.';');
+        $value = null;
+        if (isset($this->xml) && is_array($this->xml)) {
+
+            $all_tags = $this->xml_root;
+            if ($tags) {
+                $all_tags .= "['".str_replace(",", "'][0]['#']['", $tags)."']";
+            }
+            $all_tags .= $more_tags;
+
+            $pos = strrpos($all_tags, '[');
+            if ($pos===false) {
+                $most_tags = ''; // shouldn't happen !!
+            } else {
+                $most_tags = substr($all_tags, 0, $pos);
+            }
+
+            eval('if (isset($this->xml'.$most_tags.') && is_array($this->xml'.$most_tags.') && isset($this->xml'.$all_tags.')) {'
+                    .'$value = &$this->xml'.$all_tags.';'
+                .'} else {'
+                    .'$value = null;'
+                .'}'
+            );
+        }
 
         if (is_string($value)) {
 
