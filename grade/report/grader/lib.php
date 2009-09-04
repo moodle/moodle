0a695c36bbdbbe6bb53afcc7852f50a5e61542ef
moodler <moodler>
2009-09-04 07:11:39 +0000

    grade/report/grader MDL-20222 Define a variable as empty array to avoid notice when no users in gradebook

--- grade/report/grader/lib.php
+++ grade/report/grader/lib.php
@@ -372,6 +372,7 @@ class grade_report_grader extends grade_report {
         if (empty($this->users)) {
             $this->userselect = '';
             $this->users = array();
+            $this->userselect_params = array();
         } else {
             list($usql, $params) = $DB->get_in_or_equal(array_keys($this->users), SQL_PARAMS_NAMED, 'usid0');
             $this->userselect = "AND g.userid $usql";
