90e9e580f12cb788119f5253544d02b9ab18c430
samhemelryk <samhemelryk>
2009-09-08 02:09:07 +0000

    mod-url MDL-19799 Updated print_header_simple and build_navigation to OUTPUT and PAGE equivalents

--- mod/url/index.php
+++ mod/url/index.php
@@ -44,8 +44,8 @@ $strlastmodified = get_string('lastmodified');
 $PAGE->set_url('mod/url/index.php', array('id' => $course->id));
 $PAGE->set_title($course->shortname.': '.$strurls);
 $PAGE->set_heading($course->fullname);
-$navlinks = array(array('name' => $strurls, 'link' => '', 'type' => 'activityinstance'));
-echo $OUTPUT->header(build_navigation($navlinks), navmenu($course));
+$PAGE->navbar->add($strurls);
+echo $OUTPUT->header();
 
 if (!$urls = get_all_instances_in_course('url', $course)) {
     notice(get_string('thereareno', 'moodle', $strurls), "$CFG->wwwroot/course/view.php?id=$course->id");
--- mod/url/locallib.php
+++ mod/url/locallib.php
@@ -84,7 +84,7 @@ function url_print_header($url, $cm, $course) {
     $PAGE->set_heading($course->fullname);
     $PAGE->set_activity_record($url);
     $PAGE->set_button(update_module_button($cm->id, '', get_string('modulename', 'url')));
-    echo $OUTPUT->header(build_navigation('', $cm), navmenu($course, $cm));
+    echo $OUTPUT->header();
 }
 
 /**
