bcb6b50549d9bee3aba4834649f722207dfd20f9
arborrow <arborrow>
2009-09-26 17:43:50 +0000

    MDL-20353  - fixing typo

--- user/profile/index_category_form.php
+++ user/profile/index_category_form.php
@@ -16,7 +16,7 @@ class category_form extends moodleform {
         $mform->addElement('hidden', 'id');
         $mform->setType('id', PARAM_INT);
         $mform->addElement('hidden', 'action', 'editcategory');
-        $mform->setType('iction', PARAM_ACTION);
+        $mform->setType('action', PARAM_ACTION);
 
         $mform->addElement('text', 'name', get_string('profilecategoryname', 'admin'), 'maxlength="255" size="30"');
         $mform->setType('name', PARAM_MULTILANG);
