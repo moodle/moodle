5d850ce66e45abe492103f3f0d3408d56ae50300
skodak <skodak>
2009-09-27 09:23:08 +0000

    MDL-20209 fixed regression - the email has to be synced with ext auth when change confirmed

--- user/emailupdate.php
+++ user/emailupdate.php
@@ -26,6 +26,7 @@ if (empty($preferences['newemailattemptsleft'])) {
     print_box($stroutofattempts, 'center');
 
 } elseif ($key == $preferences['newemailkey']) {
+    $olduser = clone($user);
     cancel_email_update($user->id);
     $user->email = $preferences['newemail'];
 
@@ -40,6 +41,8 @@ if (empty($preferences['newemailattemptsleft'])) {
             error('Error updating user record');
 
         } else {
+            $authplugin = get_auth_plugin($user->auth);
+            $authplugin->user_update($olduser, $user);
             events_trigger('user_updated', $user);
             $a->email = $user->email;
             $stremailupdatesuccess = get_string('auth_emailupdatesuccess', 'auth', $a);
