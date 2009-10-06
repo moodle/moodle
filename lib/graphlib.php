43fd59ea14e83bf2acdaedbf806c7df765ac52e6
tjhunt <tjhunt>
2009-10-06 17:49:49 +0000

    graphlib: NOBUG previous Moodle-specific hack had a bug when y_max was very small. This would round, e.g. 10^-5 to 0 which would break the axis.

--- lib/graphlib.php
+++ lib/graphlib.php
@@ -1156,7 +1156,9 @@ function find_range($data, $min, $max, $resolution) {
     if ($max < 0) $factor = - pow(10, (floor(log10(abs($max))) + $resolution) );
     else $factor = pow(10, (floor(log10(abs($max))) - $resolution) );
   }
-  $factor = round($factor * 1000.0) / 1000.0; // To avoid some wierd rounding errors (Moodle)
+  if ($factor > 0.1) { // To avoid some wierd rounding errors (Moodle)
+    $factor = round($factor * 1000.0) / 1000.0; // To avoid some wierd rounding errors (Moodle)
+  } // To avoid some wierd rounding errors (Moodle)
 
   $max = $factor * @ceil($max / $factor);
   $min = $factor * @floor($min / $factor);
