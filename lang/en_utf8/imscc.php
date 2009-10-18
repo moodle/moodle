62903322fc6a3ce12cd31478d3636b58528d956a
stronk7 <stronk7>
2009-10-18 21:39:28 +0000

    MDL-20591 IMS CC Import - lang file added

---
+++ lang/en_utf8/imscc.php
@@ -0,0 +1,14 @@
+<?php
+$string['enable_cc_import'] = 'Enable CC Import';
+$string['enable_cc_import_description'] = 'This setting enables the import of Common Cartridge (IMS-CC) packages using the standard restore functionality. Note it requires PHP5, DOM, XSL and LIBXML extensions to be installed in the server.';
+$string['cc_import_req_php5'] = 'ERROR: The Common Cartridge import requires PHP 5 or highter.';
+$string['cc_import_req_xsl'] = 'ERROR: The Common Cartridge import requires XSL.';
+$string['cc_import_req_dom'] = 'ERROR: The Common Cartridge import requires DOM extension.';
+$string['cc_import_req_libxml'] = 'ERROR: The Common Cartridge import requires LIBXML extension';
+$string['cc2moodle_checking_schema'] = 'CC Format! Checking Schema...';
+$string['cc2moodle_valid_schema'] = 'Schema Valid!';
+$string['cc2moodle_req_auth'] = 'ERROR: The Common Cartridge package requires authorization.';
+$string['cc2moodle_invalid_schema'] = 'The schema is not valid.';
+$string['cc2moodle_manifest_dont_load'] = 'Cannot load the XML manifest.';
+$string['checkingforimscc'] = 'Checking for IMS-CC...';
+?>
