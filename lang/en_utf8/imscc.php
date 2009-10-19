56788d24ddc7ef4dd32a6981600f7c7b03168a9a
stronk7 <stronk7>
2009-10-19 17:19:20 +0000

    MDL-20591 check for min libxml 2.6.30 version on ims-cc import

--- lang/en_utf8/imscc.php
+++ lang/en_utf8/imscc.php
@@ -1,10 +1,11 @@
 <?php
 $string['enable_cc_import'] = 'Enable CC Import';
-$string['enable_cc_import_description'] = 'This setting enables the import of Common Cartridge (IMS-CC) packages using the standard restore functionality. Note it requires PHP5, DOM, XSL and LIBXML extensions to be installed in the server.';
+$string['enable_cc_import_description'] = 'This setting enables the import of Common Cartridge (IMS-CC) packages using the standard restore functionality. Note it requires PHP5, DOM, XSL and LIBXML (2.6.30 or newer) extensions to be installed in the server.';
 $string['cc_import_req_php5'] = 'ERROR: The Common Cartridge import requires PHP 5 or highter.';
 $string['cc_import_req_xsl'] = 'ERROR: The Common Cartridge import requires XSL.';
 $string['cc_import_req_dom'] = 'ERROR: The Common Cartridge import requires DOM extension.';
-$string['cc_import_req_libxml'] = 'ERROR: The Common Cartridge import requires LIBXML extension';
+$string['cc_import_req_libxml'] = 'ERROR: The Common Cartridge import requires LIBXML extension.';
+$string['cc_import_req_libxmlminversion'] = 'ERROR: The Common Cartridge import requires LIBXML version 2.6.30 or newer.';
 $string['cc2moodle_checking_schema'] = 'CC Format! Checking Schema...';
 $string['cc2moodle_valid_schema'] = 'Schema Valid!';
 $string['cc2moodle_req_auth'] = 'ERROR: The Common Cartridge package requires authorization.';
