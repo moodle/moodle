b55d0869e9b36280d20ba9c720a06c7a41fbfaba
ashleyholman <ashleyholman>
2009-10-19 00:18:23 +0000

    MDL-20578 forum_print_overview() performance: fix unnecessary ILIKE match to make better use of log_coumodact_ix index

--- mod/forum/lib.php
+++ mod/forum/lib.php
@@ -1203,7 +1203,7 @@ function forum_print_overview($courses,&$htmlarray) {
     }
     $sql = substr($sql,0,-3); // take off the last OR
 
-    $sql .= ") AND l.module = 'forum' AND action $LIKE 'add post%' "
+    $sql .= ") AND l.module = 'forum' AND action = 'add post' "
         ." AND userid != ? GROUP BY cmid,l.course,instance";
 
     $params[] = $USER->id;
