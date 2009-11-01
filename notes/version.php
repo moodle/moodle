5777f4e3911c51b31ce495d996eaed34c64c4b9a
Petr Skoda <skodak@moodle.org>
2009-11-01 12:29:18 +0000

    MDL-14679 notes do not use version.php!

--- notes/version.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/////////////////////////////////////////////////////////////////////////////////
-///  Code fragment to define the version of the note module
-///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
-/////////////////////////////////////////////////////////////////////////////////
-
-$note_version  = 2007070700;  // The current version of note module (Date: YYYYMMDDXX)
-$module->cron     = 1800;           // Period for cron to check this module (secs)
