40e85c92685b3c701dc8621bb2e06a24e0837f7e
Petr Skoda <skodak@moodle.org>
2009-11-08 21:58:32 +0000

    MDL-12886 more external users api

--- user/externallib.php
+++ user/externallib.php
@@ -314,24 +314,24 @@ class moodle_user_external extends external_api {
                     'firstname'   => new external_value(PARAM_NOTAGS, 'The first name(s) of the user'),
                     'lastname'    => new external_value(PARAM_NOTAGS, 'The family name of the user'),
                     'email'       => new external_value(PARAM_EMAIL, 'A valid and unique email address'),
-                    'auth'        => new external_value(PARAM_SAFEDIR, 'Auth plugins include manual, ldap, imap, etc', false),
-                    'confirmed'   => new external_value(PARAM_NUMBER, 'Active user: 1 if confirmed, 0 otherwise', false),
-                    'idnumber'    => new external_value(PARAM_RAW, 'An arbitrary ID code number perhaps from the institution', false),
-                    'emailstop'   => new external_value(PARAM_NUMBER, 'Email is blocked: 1 is blocked and 0 otherwise', false),
-                    'lang'        => new external_value(PARAM_SAFEDIR, 'Language code such as "en_utf8", must exist on server', false),
-                    'theme'       => new external_value(PARAM_SAFEDIR, 'Theme name such as "standard", must exist on server', false),
-                    'timezone'    => new external_value(PARAM_ALPHANUMEXT, 'Timezone code such as Australia/Perth, or 99 for default', false),
-                    'mailformat'  => new external_value(PARAM_INTEGER, 'Mail format code is 0 for plain text, 1 for HTML etc', false),
-                    'description' => new external_value(PARAM_TEXT, 'User profile description, as HTML', false),
-                    'city'        => new external_value(PARAM_NOTAGS, 'Home city of the user', false),
-                    'country'     => new external_value(PARAM_ALPHA, 'Home country code of the user, such as AU or CZ', false),
+                    'auth'        => new external_value(PARAM_SAFEDIR, 'Auth plugins include manual, ldap, imap, etc'),
+                    'confirmed'   => new external_value(PARAM_NUMBER, 'Active user: 1 if confirmed, 0 otherwise'),
+                    'idnumber'    => new external_value(PARAM_RAW, 'An arbitrary ID code number perhaps from the institution'),
+                    'emailstop'   => new external_value(PARAM_NUMBER, 'Email is blocked: 1 is blocked and 0 otherwise'),
+                    'lang'        => new external_value(PARAM_SAFEDIR, 'Language code such as "en_utf8", must exist on server'),
+                    'theme'       => new external_value(PARAM_SAFEDIR, 'Theme name such as "standard", must exist on server'),
+                    'timezone'    => new external_value(PARAM_ALPHANUMEXT, 'Timezone code such as Australia/Perth, or 99 for default'),
+                    'mailformat'  => new external_value(PARAM_INTEGER, 'Mail format code is 0 for plain text, 1 for HTML etc'),
+                    'description' => new external_value(PARAM_TEXT, 'User profile description, as HTML'),
+                    'city'        => new external_value(PARAM_NOTAGS, 'Home city of the user'),
+                    'country'     => new external_value(PARAM_ALPHA, 'Home country code of the user, such as AU or CZ'),
                     'customfields' => new external_multiple_structure(
                         new external_single_structure(
                             array(
                                 'type'  => new external_value(PARAM_ALPHANUMEXT, 'The name of the custom field'),
                                 'value' => new external_value(PARAM_RAW, 'The value of the custom field')
                             )
-                        ), 'User custom fields', false)
+                        ), 'User custom fields')
                 )
             )
         );
