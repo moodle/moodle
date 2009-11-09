bf38af3f55885b5892f684c486bb0e8174ef61ef
Gordon Bateson <gordon@kanazawa-gu.ac.jp>
2009-11-09 01:29:57 +0000

    ensure user has_capability('mod/hotpot:attempt', $module) when viewing a HotPot or submitting results

--- mod/hotpot/view.php
+++ mod/hotpot/view.php
@@ -39,6 +39,7 @@
         }
         require_login($course, true, $cm);
         $context = get_context_instance(CONTEXT_MODULE, $cm->id);
+        require_capability('mod/hotpot:attempt', $context);
     }
     // set nextpage (for error messages)
     $nextpage = "$CFG->wwwroot/course/view.php?id=$course->id";
