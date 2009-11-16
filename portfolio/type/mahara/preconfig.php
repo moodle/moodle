557b2a3b81e2ec4f2e7ed8596995d4333136f7cd
Penelope Leach <penny@catalyst.net.nz>
2009-11-16 12:57:36 +0000

    portfolio - MDL-19356 -  Mahara portfolio plugin: removed session use
    since we can construct whatever return url we want.

--- portfolio/type/mahara/preconfig.php
+++ portfolio/type/mahara/preconfig.php
@@ -10,22 +10,24 @@ require_once($CFG->dirroot . '/mnet/lib.php');
 
 require_login();
 
-if (!$landed = optional_param('landed', false, PARAM_BOOL)) {
-    $id = required_param('id', PARAM_INT);
+$id     = required_param('id', PARAM_INT);              // id of current export
+$landed = optional_param('landed', false, PARAM_BOOL);  // this is the parameter we get back after we've jumped to mahara
+
+if (!$landed) {
     $exporter = portfolio_exporter::rewaken_object($id);
     $exporter->verify_rewaken();
 
     $mnetauth = get_auth_plugin('mnet');
-    if (!$url = $mnetauth->start_jump_session($exporter->get('instance')->get_config('mnethostid'), '/portfolio/type/mahara/preconfig.php?landed=1', true)) {
+    if (!$url = $mnetauth->start_jump_session($exporter->get('instance')->get_config('mnethostid'), '/portfolio/type/mahara/preconfig.php?landed=1&id=' . $id, true)) {
         throw new porfolio_exception('failedtojump', 'portfolio_mahara');
     }
     redirect($url);
 } else {
     // now we have the sso session set up, start sending intent stuff and then redirect back to portfolio/add.php when we're done
-    $exporter = portfolio_exporter::rewaken_object($SESSION->portfolioexport);
+    $exporter = portfolio_exporter::rewaken_object($id);
     $exporter->verify_rewaken();
 
     $exporter->get('instance')->send_intent();
-    redirect($CFG->wwwroot . '/portfolio/add.php?postcontrol=1&id=' . $exporter->get('id'));
+    redirect($CFG->wwwroot . '/portfolio/add.php?postcontrol=1&id=' . $id);
 }
 
