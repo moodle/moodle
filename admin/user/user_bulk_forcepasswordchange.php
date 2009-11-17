9d4b3666bb052269179aec434bedb8c6ad616ae7
Petr Skoda <skodak@moodle.org>
2009-11-17 10:17:04 +0000

    MDL-19608 allow forced password change for everybody including admin

--- admin/user/user_bulk_forcepasswordchange.php
+++ admin/user/user_bulk_forcepasswordchange.php
@@ -29,12 +29,7 @@ if ($confirm and confirm_sesskey()) {
         $in = implode(',', $users);
         if ($rs = get_recordset_select('user', "id IN ($in)")) {
             while ($user = rs_fetch_next_record($rs)) {
-                if ($primaryadmin->id != $user->id and $USER->id != $user->id
-                    and set_user_preference('auth_forcepasswordchange', 1, $user->id)) {
-                    unset($SESSION->bulk_users[$user->id]);
-                } else {
-                    notify(get_string('forcepasswordchangenot', '', fullname($user, true)));
-                }
+                set_user_preference('auth_forcepasswordchange', 1, $user->id);
             }
             rs_close($rs);
         }
