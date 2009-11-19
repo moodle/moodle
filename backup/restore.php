4d93b2059f881095a431653f5329f75a558f07d4
Petr Skoda <skodak@moodle.org>
2009-11-19 18:48:52 +0000

    MDL-20901 fixed input validationand removed annoying redirect which must not be clicked during course import

--- backup/restore.php
+++ backup/restore.php
@@ -154,6 +154,7 @@
         if (!empty($SESSION->restore->importing)) {
             // set up all the config stuff and skip asking the user about it.
             restore_setup_for_check($SESSION->restore,$backup_unique_code);
+            require_sesskey();
             include_once("restore_execute.html");
         } else {
             include_once("restore_form.html");
@@ -169,6 +170,7 @@
         }
         //Unset this for the future
         unset($SESSION->cancontinue);
+        require_sesskey();
         include_once("restore_execute.html");
     }
     print_simple_box_end();
