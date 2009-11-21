93f78d063090b326bba89d6e287e657bbf4c7a3d
Valery Fremaux <valery.fremaux@club-internet.fr>
2009-11-21 13:52:31 +0000

    fix last http://tracker.moodle.org/browse/MDL-20917 shorten errors.

--- search/documents/physical_ppt.php
+++ search/documents/physical_ppt.php
@@ -87,7 +87,7 @@ function get_text_for_indexing_ppt(&$resource, $directfile = ''){
     // fclose($logppt);
     
     if (!empty($CFG->block_search_limit_index_body)){
-        $indextext = shorten($text, $CFG->block_search_limit_index_body);
+        $indextext = shorten_text($text, $CFG->block_search_limit_index_body);
     }
 
     $indextext = mb_convert_encoding($indextext, 'UTF-8', 'auto');
