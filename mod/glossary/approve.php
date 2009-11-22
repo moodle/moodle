cf6e1ae752baa4816f2d528b1f33a50c1928cb93
Petr Skoda <skodak@moodle.org>
2009-11-22 12:43:10 +0000

    MDL-20930 fixed input validation

--- mod/glossary/approve.php
+++ mod/glossary/approve.php
@@ -3,38 +3,39 @@
     require_once("../../config.php");
     require_once("lib.php");
 
-    $id  = required_param('id', PARAM_INT);     // Course Module ID
-    $eid = optional_param('eid', 0,  PARAM_INT);    // Entry ID
+    $eid = required_param('eid', PARAM_INT);    // Entry ID
 
-    $mode = optional_param('mode','approval', PARAM_ALPHA);
-    $hook = optional_param('hook','ALL', PARAM_CLEAN);
+    $mode = optional_param('mode', 'approval', PARAM_ALPHA);
+    $hook = optional_param('hook', 'ALL', PARAM_CLEAN);
 
-    if (! $cm = get_coursemodule_from_id('glossary', $id)) {
-        error("Course Module ID was incorrect");
+    if (!$entry = get_record('glossary_entries', 'id', $eid)) {
+        error('Entry is incorrect');
+    }
+    if (!$glossary = get_record('glossary', 'id', $entry->glossaryid)) {
+        error('Incorrect glossary');
+    }
+    if (!$cm = get_coursemodule_from_instance('glossary', $glossary->id)) {
+        error('Course Module ID was incorrect');
+    }
+    if (!$course = get_record('course', 'id', $cm->course)) {
+        error('Course is misconfigured');
     }
 
-    if (! $course = get_record("course", "id", $cm->course)) {
-        error("Course is misconfigured");
-    }
-
-    if (! $glossary = get_record("glossary", "id", $cm->instance)) {
-        error("Course module is incorrect");
-    }
-
-    require_login($course->id, false, $cm);
+    require_login($course, false, $cm);
 
     $context = get_context_instance(CONTEXT_MODULE, $cm->id);
     require_capability('mod/glossary:approve', $context);
 
-    $newentry->id = $eid;
-    $newentry->approved     = 1;
-    $newentry->timemodified = time(); // wee need this date here to speed up recent activity, TODO: use timestamp in approved field instead in 2.0
-
-    if (! update_record("glossary_entries", $newentry)) {
-        error("Could not update your glossary");
-    } else {
-        add_to_log($course->id, "glossary", "approve entry", "showentry.php?id=$cm->id&amp;eid=$eid", "$eid",$cm->id);
+    if (!$entry->approved and confirm_sesskey()) {
+        $newentry = new object();
+        $newentry->id           = $entry->id;
+        $newentry->approved     = 1;
+        $newentry->timemodified = time(); // wee need this date here to speed up recent activity, TODO: use timestamp in approved field instead in 2.0
+        if (update_record("glossary_entries", $newentry)) {
+            add_to_log($course->id, "glossary", "approve entry", "showentry.php?id=$cm->id&amp;eid=$eid", "$eid", $cm->id);
+        }
     }
-    redirect("view.php?id=$cm->id&amp;mode=$mode&amp;hook=$hook",get_string("entryapproved","glossary"),1);
+
+    redirect("view.php?id=$cm->id&amp;mode=$mode&amp;hook=$hook");
     die;
 ?>
