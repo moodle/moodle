182951c6ad0ed59f8157c5d96955e9525a5a5764
Martin Dougiamas <martin@moodle.com>
2009-11-24 08:51:01 +0000

    MDL-20841 Fixed some naughty hash comments :)

--- lib/weblib.php
+++ lib/weblib.php
@@ -2482,8 +2482,8 @@ function print_header ($title='', $heading='', $navigation='', $focus='',
 
 /// Perform some browser environment checks
     if (!empty($CFG->excludeoldflashclients) && empty($SESSION->flashversion)) { // Do a single check per session for the flash version
-        # Unfortunately we can't use require_js here and keep it all clean in 1.9 ...
-        # require_js(array('yui_yahoo', 'yui_event', 'yui_connection', $CFG->httpswwwroot."/lib/swfobject/swfobject.js"));
+        // Unfortunately we can't use require_js here and keep it all clean in 1.9 ...
+        // require_js(array('yui_yahoo', 'yui_event', 'yui_connection', $CFG->httpswwwroot."/lib/swfobject/swfobject.js"));
         $meta .= '<script type="text/javascript"  src="'.$CFG->wwwroot.'/lib/yui/yahoo/yahoo-min.js"></script>';
         $meta .= '<script type="text/javascript"  src="'.$CFG->wwwroot.'/lib/yui/event/event-min.js"></script>';
         $meta .= '<script type="text/javascript"  src="'.$CFG->wwwroot.'/lib/yui/connection/connection-min.js"></script>';
