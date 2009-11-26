a2a5c0875ee54fc75193179414bf5c96bcd0ff98
Sam Hemelryk <sam@moodle.com>
2009-11-26 06:48:43 +0000

    upgrade MDL-20978 Added site's URL into the message subject sent out with recent upgrades

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -3207,7 +3207,7 @@ function xmldb_main_upgrade($oldversion=0) {
 
     if ($result && $oldversion < 2007101561.02) {
         $messagesubject = s($SITE->shortname).': '.get_string('upgrade197noticesubject', 'admin');
-        $message  = '<p>'.s($SITE->fullname).':</p>'.get_string('upgrade197notice', 'admin');
+        $message  = '<p>'.s($SITE->fullname).' ('.s($CFG->wwwroot).'):</p>'.get_string('upgrade197notice', 'admin');
         if (empty($CFG->passwordmainsalt)) {
             $docspath = $CFG->docroot.'/'.str_replace('_utf8', '', current_language()).'/report/security/report_security_check_passwordsaltmain';
             $message .= "\n".get_string('upgrade197salt', 'admin', $docspath);
