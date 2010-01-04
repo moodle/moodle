669314615fdb61ad6bbe190d4656f0bf7cbee184
David Mudrak <david.mudrak@gmail.com>
2010-01-04 18:21:04 +0000

    Initial copy of form/accumulative code

---
+++ mod/workshop/form/comments/lang/en_utf8/workshopform_accumulative.php
@@ -0,0 +1,35 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English strings for Workshop subplugin - "Accumulative" grading strategy
+ *
+ * @package   mod-workshop
+ * @copyright 2009 David Mudrak <david.mudrak@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addmoredimensions'] = 'Blanks for $a more aspects';
+$string['dimensioncomment'] = 'Comment';
+$string['dimensiondescription'] = 'Description';
+$string['dimensiongrade'] = 'Grade';
+$string['dimensionmaxgrade'] = 'Best possible grade / Scale to use';
+$string['dimensionnumber'] = 'Aspect $a';
+$string['dimensionweight'] = 'Weight';
+$string['pluginname'] = 'Accumulative grading';
