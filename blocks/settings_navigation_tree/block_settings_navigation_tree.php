9af2b86b951760c4cb7c9a8ac898cda6333a97e5
Sam Hemelryk <sam@moodle.com>
2010-01-28 06:29:29 +0000

    navigation MDL-21329 Fixed regression with docked preferences thanks Jerome for spotting

--- blocks/settings_navigation_tree/block_settings_navigation_tree.php
+++ blocks/settings_navigation_tree/block_settings_navigation_tree.php
@@ -83,7 +83,7 @@ class block_settings_navigation_tree extends block_tree {
         $this->page->requires->js_module(array('name'=>'blocks_navigation', 'fullpath'=>'/blocks/global_navigation_tree/navigation.js', 'requires'=>array('blocks_dock', 'io', 'node', 'dom', 'event-custom')));
         $arguments = array($this->instance->id, array('instance'=>$this->instance->id, 'candock'=>$this->instance_can_be_docked()));
         $this->page->requires->js_object_init("M.blocks.navigation.treecollection[".$this->instance->id."]", 'M.blocks.navigation.classes.tree', $arguments, array('blocks_navigation'));
-        user_preference_allow_ajax_update('M.docked_block_instance_'.$this->instance->id, PARAM_INT);
+        user_preference_allow_ajax_update('docked_block_instance_'.$this->instance->id, PARAM_INT);
     }
 
     /**
@@ -116,6 +116,12 @@ class block_settings_navigation_tree extends block_tree {
             $url = $this->page->url;
             $url->remove_params(array('dock'));
             redirect($url);
+        }
+
+        if (!$this->page->settingsnav->contains_active_node() && !$this->page->navigation->contains_active_node()) {
+            if (!$this->page->settingsnav->reiterate_active_nodes()) {
+                $this->page->settingsnav->reiterate_active_nodes(URL_MATCH_BASE);
+            }
         }
 
         // Grab the children from settings nav, we have more than one root node
