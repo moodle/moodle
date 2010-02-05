80df574414befa4a8be7b488693e5a8afb632ce4
Petr Skoda <skodak@moodle.org>
2010-02-05 08:31:28 +0000

    MDL-21503 fixed unintended use of global Y

--- course/completion.js
+++ course/completion.js
@@ -10,10 +10,84 @@ M.core_completion.init = function(Y) {
         return;
     }
 
+    var handle_success = function(id, o, args) {
+        Y.one('#completion_dynamic_change').set('value', 1);
+
+        if (o.responseText != 'OK') {
+            alert('An error occurred when attempting to save your tick mark.\n\n('+o.responseText+'.)'); //TODO: localize
+
+        } else {
+            var current = args.state.get('value');
+
+            if (current == 1) {
+                args.state.set('value', 0);
+                args.image.set('src', M.util.image_url('i/completion-manual-y', 'moodle'));
+                args.image.set('alt', mstr.completion['completion-alt-manual-y']);
+                args.image.set('title', mstr.completion['completion-title-manual-y']);
+            } else {
+                args.state.set('value', 1);
+                args.image.set('src', M.util.image_url('i/completion-manual-n', 'moodle'));
+                args.image.set('alt', mstr.completion['completion-alt-manual-n']);
+                args.image.set('title', mstr.completion['completion-title-manual-n']);
+            }
+        }
+
+        args.ajax.remove();
+    }
+
+    var handle_failure = function(id, o, args) {
+        alert('An error occurred when attempting to save your tick mark.\n\n('+o.responseText+'.)'); //TODO: localize
+        args.ajax.remove();
+    }
+
+    var toggle = function(e) {
+        e.preventDefault();
+
+        var form = e.target;
+        var cmid = 0;
+        var completionstate = 0;
+        var state = null;
+        var image = null;
+
+        var inputs = Y.Node.getDOMNode(form).getElementsByTagName('input');
+        for (var i=0; i<inputs.length; i++) {
+            switch (inputs[i].name) {
+                 case 'id':
+                     cmid = inputs[i].value;
+                     break;
+                  case 'completionstate':
+                     completionstate = inputs[i].value;
+                     state = Y.one(inputs[i]);
+                     break;
+            }
+            if (inputs[i].type == 'image') {
+                image = Y.one(inputs[i]);
+            }
+        }
+
+        // start spinning the ajax indicator
+        var ajax = Y.Node.create('<div class="ajaxworking" />');
+        form.append(ajax);
+
+        var cfg = {
+            method: "POST",
+            data: 'id='+cmid+'&completionstate='+completionstate+'&fromajax=1&sesskey='+M.cfg.sesskey,
+            on: {
+                success: handle_success,
+                failure: handle_failure
+            },
+            arguments: {state: state, image: image, ajax: ajax}
+        };
+
+        Y.use('io', function(Y) {
+            Y.io(M.cfg.wwwroot+'/course/togglecompletion.php', cfg);
+        });
+    }
+
     // register submit handlers on manual tick completion forms
     Y.all('form.togglecompletion').each(function(form) {
         if (!form.hasClass('preventjs')) {
-            Y.on('submit', M.core_completion.toggle, form);
+            Y.on('submit', toggle, form);
         }
     });
 
@@ -22,80 +96,6 @@ M.core_completion.init = function(Y) {
     if (help && !(Y.one('form.togglecompletion') || Y.one('.autocompletion'))) {
         help.setStyle('display', 'none');
     }
-}
-
-M.core_completion.toggle = function(e) {
-    e.preventDefault();
-
-    var form = e.target;
-    var cmid = 0;
-    var completionstate = 0;
-    var state = null;
-    var image = null;
-
-    var inputs = Y.Node.getDOMNode(form).getElementsByTagName('input');
-    for (var i=0; i<inputs.length; i++) {
-        switch (inputs[i].name) {
-             case 'id':
-                 cmid = inputs[i].value;
-                 break;
-              case 'completionstate':
-                 completionstate = inputs[i].value;
-                 state = Y.one(inputs[i]);
-                 break;
-        }
-        if (inputs[i].type == 'image') {
-            image = Y.one(inputs[i]);
-        }
-    }
-
-    // start spinning the ajax indicator
-    var ajax = Y.Node.create('<div class="ajaxworking" />');
-    form.append(ajax);
-
-    var cfg = {
-        method: "POST",
-        data: 'id='+cmid+'&completionstate='+completionstate+'&fromajax=1&sesskey='+M.cfg.sesskey,
-        on: {
-            success: M.core_completion.handle_success,
-            failure: M.core_completion.handle_failure
-        },
-        arguments: {state: state, image: image, ajax: ajax}
-    };
-
-    Y.use('io', function(Y) {
-        Y.io(M.cfg.wwwroot+'/course/togglecompletion.php', cfg);
-    });
-}
-
-M.core_completion.handle_success = function(id, o, args) {
-    Y.one('#completion_dynamic_change').set('value', 1);
-
-    if (o.responseText != 'OK') {
-        alert('An error occurred when attempting to save your tick mark.\n\n('+o.responseText+'.)'); //TODO: localize
-
-    } else {
-        var current = args.state.get('value');
-
-        if (current == 1) {
-            args.state.set('value', 0);
-            args.image.set('src', M.util.image_url('i/completion-manual-y', 'moodle'));
-            args.image.set('alt', mstr.completion['completion-alt-manual-y']);
-            args.image.set('title', mstr.completion['completion-title-manual-y']);
-        } else {
-            args.state.set('value', 1);
-            args.image.set('src', M.util.image_url('i/completion-manual-n', 'moodle'));
-            args.image.set('alt', mstr.completion['completion-alt-manual-n']);
-            args.image.set('title', mstr.completion['completion-title-manual-n']);
-        }
-    }
-
-    args.ajax.remove();
-}
-
-M.core_completion.handle_failure = function(id, o, args) {
-    alert('An error occurred when attempting to save your tick mark.\n\n('+o.responseText+'.)'); //TODO: localize
-    args.ajax.remove();
 }
 
 
