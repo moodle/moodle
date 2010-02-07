cd2bbdabb738533472e198896062fd4e655092a5
Petr Skoda <skodak@moodle.org>
2010-02-07 13:18:03 +0000

    MDL-21400 main version bump - initialised jsrev counter

--- version.php
+++ version.php
@@ -6,7 +6,7 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2010020301;  // YYYYMMDD   = date of the last version bump
+    $version = 2010020700;  // YYYYMMDD   = date of the last version bump
                             //         XX = daily increments
 
     $release = '2.0 dev (Build: 20100207)';  // Human-friendly version name
