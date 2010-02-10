fddef9d6b7fa89502dda9d9d80b588fe4f79ea7c
Eloy Lafuente <stronk7@moodle.org>
2010-02-10 16:46:00 +0000

    MDL-7647 confirmation email - usernames with slashes causing confirmation problems

--- login/confirm.php
+++ login/confirm.php
@@ -19,7 +19,7 @@
     if (!empty($data) || (!empty($p) && !empty($s))) {
 
         if (!empty($data)) {
-            $dataelements = explode('/',$data);
+            $dataelements = explode('/',$data, 2); // Stop after 1st slash. Rest is username. MDL-7647
             $usersecret = $dataelements[0];
             $username   = $dataelements[1];
         } else {
