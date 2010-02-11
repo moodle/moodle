dfdec574d3cda17d6b891c7bb9275450a50ebf26
Gordon Bateson <gordon@kanazawa-gu.ac.jp>
2010-02-11 02:52:22 +0000

    fix student access to HotPots in tail of a HotPot quiz chain

--- mod/hotpot/attempt.php
+++ mod/hotpot/attempt.php
@@ -24,7 +24,7 @@
     // make sure this user is enrolled in this course and can access this HotPot
     require_login($course);
     $context = get_context_instance(CONTEXT_MODULE, $cm->id);
-    require_capability('mod/hotpot:attempt', $context);
+    require_capability('mod/hotpot:attempt', $context, $USER->id);
 
     $next_url = "$CFG->wwwroot/course/view.php?id=$course->id";
     $time = time();
--- mod/hotpot/view.php
+++ mod/hotpot/view.php
@@ -37,7 +37,7 @@
         // make sure this user is enrolled in this course and can access this HotPot
         require_login($course);
         $context = get_context_instance(CONTEXT_MODULE, $cm->id);
-        require_capability('mod/hotpot:attempt', $context);
+        require_capability('mod/hotpot:attempt', $context, $USER->id);
     }
     // set nextpage (for error messages)
     $nextpage = "$CFG->wwwroot/course/view.php?id=$course->id";
