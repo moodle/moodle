c676fe67173a201cd61f20ec4974e95d61a1864f
Petr Skoda <skodak@moodle.org>
2010-03-10 08:47:15 +0000

    MDL-21789 fixed buggy handling of 0.00 in s()

--- lib/weblib.php
+++ lib/weblib.php
@@ -114,7 +114,7 @@ $ALLOWED_PROTOCOLS = array('http', 'https', 'ftp', 'news', 'mailto', 'rtsp', 'te
  */
 function s($var, $obsolete = false) {
 
-    if ($var == '0') {  // for integer 0, boolean false, string '0'
+    if ($var === '0' or $var === false or $var === 0) {
         return '0';
     }
 
