3afe38a532043d227828de9ef0df1e79a5c33a81
Dan Poltawski <dan.poltawski@lancaster.ac.uk>
2010-03-11 16:25:45 +0000

    lib/blocklib.php MDL-21805 Moving blocks up/down causes blocks to disappear
    If two blocks of the same type were next to each other the weight would be
    set the same and block disappear..
    
    Thanks to my colleague Andrew Nicols for the patch.
    
    (This patch was commited on a train at 90mph near Coventry, UK)

--- lib/blocklib.php
+++ lib/blocklib.php
@@ -580,7 +580,7 @@ function blocks_execute_action($page, &$pageblocks, $blockaction, $instanceorid,
                 if (!empty($pageblocks[$instance->position][$instance->weight - 1])) {
                     //define instance's position in the array
                     foreach ($pageblocks[$instance->position] as $instancekeysindex => $index ){
-                        if ($pageblocks[$instance->position][$instancekeysindex]->blockid == $instance->blockid){
+                        if ($pageblocks[$instance->position][$instancekeysindex]->id == $instance->id){
                             $instanceindex = $instancekeysindex;
                         }
                     }
@@ -622,7 +622,7 @@ function blocks_execute_action($page, &$pageblocks, $blockaction, $instanceorid,
                 if (!empty($pageblocks[$instance->position][$instance->weight + 1])) {
                     //define instance's position in the array
                     foreach ($pageblocks[$instance->position] as $instancekeysindex => $index ){
-                        if ($pageblocks[$instance->position][$instancekeysindex]->blockid == $instance->blockid){
+                        if ($pageblocks[$instance->position][$instancekeysindex]->id == $instance->id){
                             $instanceindex = $instancekeysindex;
                         }
                     }
