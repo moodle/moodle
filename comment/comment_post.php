88342307b0463e9728569740cb1f14262619dd7d
Dongsheng Cai <unoter@gmail.com>
2010-03-15 08:03:18 +0000

    "MDL-21786, remove old comment lib file"

--- comment/comment_post.php
+++ comment/comment_post.php
@@ -19,7 +19,7 @@
  * Handling all ajax request for comments API
  */
 require_once('../config.php');
-require_once($CFG->libdir.'/comment/lib.php');
+require_once($CFG->dirroot . '/comment/lib.php');
 
 $courseid  = optional_param('courseid',  SITEID, PARAM_INT);
 $contextid = optional_param('contextid', SYSCONTEXTID, PARAM_INT);
