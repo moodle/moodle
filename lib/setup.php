3d732d8487653b68c73546b63e0a457115d0f86e
Eloy Lafuente <stronk7@moodle.org>
2010-03-23 11:14:26 +0000

    NOBUG: Now editorlib is using PEAR (forms) stuff. Move the include path definition before including all the Moodle stuff.

--- lib/setup.php
+++ lib/setup.php
@@ -255,6 +255,13 @@ if (!isset($CFG->prefix)) {   // Just in case it isn't defined in config.php
     $CFG->prefix = '';
 }
 
+//point pear include path to moodles lib/pear so that includes and requires will search there for files before anywhere else
+//the problem is that we need specific version of quickforms and hacked excel files :-(
+ini_set('include_path', $CFG->libdir.'/pear' . PATH_SEPARATOR . ini_get('include_path'));
+//point zend include path to moodles lib/zend so that includes and requires will search there for files before anywhere else
+//please note zend library is supposed to be used only from web service protocol classes, it may be removed in future
+ini_set('include_path', $CFG->libdir.'/zend' . PATH_SEPARATOR . ini_get('include_path'));
+
 // Load up standard libraries
 require_once($CFG->libdir .'/textlib.class.php');   // Functions to handle multibyte strings
 require_once($CFG->libdir .'/filterlib.php');       // Functions for filtering test as it is output
@@ -274,13 +281,6 @@ require_once($CFG->libdir .'/grouplib.php');        // Groups functions
 require_once($CFG->libdir .'/sessionlib.php');      // All session and cookie related stuff
 require_once($CFG->libdir .'/editorlib.php');       // All text editor related functions and classes
 require_once($CFG->libdir .'/messagelib.php');      // Messagelib functions
-
-//point pear include path to moodles lib/pear so that includes and requires will search there for files before anywhere else
-//the problem is that we need specific version of quickforms and hacked excel files :-(
-ini_set('include_path', $CFG->libdir.'/pear' . PATH_SEPARATOR . ini_get('include_path'));
-//point zend include path to moodles lib/zend so that includes and requires will search there for files before anywhere else
-//please note zend library is supposed to be used only from web service protocol classes, it may be removed in future
-ini_set('include_path', $CFG->libdir.'/zend' . PATH_SEPARATOR . ini_get('include_path'));
 
 // make sure PHP is not severly misconfigured
 setup_validate_php_configuration();
