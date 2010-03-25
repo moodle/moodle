83d629cebcf7d1bf0d678ad6eb9006821e3c6fee
Sam Hemelryk <sam@moodle.com>
2010-03-25 07:46:13 +0000

    themes MDL-21862 Add support into standard theme to resize block regions if there are no blocks due to docking.

--- theme/standard/javascript/navigation.js
+++ theme/standard/javascript/navigation.js
@@ -5,6 +5,15 @@ function customise_dock_for_theme() {
     }
     // Throw a lightbox for the navigation boxes
     M.core_dock.cfg.panel.modal = true;
+    /**
+     * This function converts the title of each docked block to make it vertical.
+     *
+     * In the case of browsers that support svg (scalable vector graphics) an SVG is
+     * generated to rotate the text. In the case of IE we have to settle for making
+     * it letter over letter.
+     *
+     * @param {Y.Node} node
+     */
     M.core_dock.genericblock.prototype.fix_title_orientation = function(node) {
         if (YAHOO.env.ua.ie > 0) {
             if (YAHOO.env.ua.ie > 7) {
@@ -38,6 +47,38 @@ function customise_dock_for_theme() {
         div.appendChild(svg);
 
         return div;
+    }
+    /**
+     * This function makes sure that if all blocks from one side or the other are docked
+     * the gap that is left behind if closed up. This is done of course by using classes.
+     */
+    M.core_dock.genericblock.prototype.resize_block_space = function() {
+        var blockregions = {
+            pre: {
+                hasblocks : (M.core_dock.Y.one('#region-pre') && M.core_dock.Y.one('#region-pre').all('.sideblock').size() > 0),
+                c : 'side-pre-only'
+            },
+            post: {
+                hasblocks : (M.core_dock.Y.one('#region-post') && M.core_dock.Y.one('#region-post').all('.sideblock').size() > 0),
+                c : 'side-post-only'
+            },
+            noblocksc:'content-only'
+        }
+        
+        if (blockregions.pre.hasblocks && blockregions.post.hasblocks) {
+            // No classes required both regions have blocks
+            M.core_dock.Y.one(document.body).removeClass(blockregions.pre.c).removeClass(blockregions.post.c).removeClass(blockregions.noblocksc);
+        } else if (blockregions.pre.hasblocks) {
+            // side-pre-only required: remove any other classes
+            M.core_dock.Y.one(document.body).addClass(blockregions.pre.c).removeClass(blockregions.post.c).removeClass(blockregions.noblocksc);
+        } else if (blockregions.post.hasblocks) {
+            // side-post-only required: remove any other classes
+            M.core_dock.Y.one(document.body).removeClass(blockregions.pre.c).addClass(blockregions.post.c).removeClass(blockregions.noblocksc);
+        } else {
+            // All blocks have been docked: add noblocks remove side-xxx-only's if set
+            M.core_dock.Y.one(document.body).removeClass(blockregions.pre.c).removeClass(blockregions.post.c).addClass(blockregions.noblocksc);
+        }
+        return '200px';
     }
     return true;
 }
