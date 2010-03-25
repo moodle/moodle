91ee8a67d67651ff3e999cf47dc0765fb1c29115
David Mudrak <david.mudrak@gmail.com>
2010-03-25 12:05:55 +0000

    config-dist.php: adding more settings and warnings for developers

--- config-dist.php
+++ config-dist.php
@@ -355,33 +355,40 @@ $CFG->admin = 'admin';
 // 8. SETTINGS FOR DEVELOPMENT SERVERS - not intended for production use!!!
 //=========================================================================
 //
+// Force a debugging mode regardless the settings in the site administration
+// @error_reporting(1023);  // NOT FOR PRODUCTION SERVERS!
+// @ini_set('display_errors', '1'); // NOT FOR PRODUCTION SERVERS!
+// $CFG->debug = 38911;  // DEBUG_DEVELOPER // NOT FOR PRODUCTION SERVERS!
+// $CFG->debugdisplay = true;   // NOT FOR PRODUCTION SERVERS!
+//
+// Prevent theme caching
+// $CFG->themerev = -1; // NOT FOR PRODUCTION SERVERS!
+//
 // When working with production data on test servers, no emails should ever be send to real users
-// $CFG->noemailever = true;
+// $CFG->noemailever = true;    // NOT FOR PRODUCTION SERVERS!
 //
 // Divert all outgoing emails to this address to test and debug emailing features
-// $CFG->divertallemailsto = 'root@localhost.local';
+// $CFG->divertallemailsto = 'root@localhost.local'; // NOT FOR PRODUCTION SERVERS!
 //
 // Specify prefix for fake unit test tables. If not specified only tests
 // that do not need fake tables will be executed.
-// $CFG->unittestprefix = 'tst_';
+// $CFG->unittestprefix = 'tst_';   // NOT FOR PRODUCTION SERVERS!
 //
 // special magic evil developer only wanting to edit the xmldb files manually
 // AND don't use the XMLDBEditor nor the prev/next stuff at all (Mahara and others)
 // Uncomment these if you're lazy like Penny
-// $CFG->xmldbdisablecommentchecking = true;
-// $CFG->xmldbdisablenextprevchecking = true;
+// $CFG->xmldbdisablecommentchecking = true;    // NOT FOR PRODUCTION SERVERS!
+// $CFG->xmldbdisablenextprevchecking = true;   // NOT FOR PRODUCTION SERVERS!
 //
 // Special magic - evil developer only wanting to edit xmldb files manually
 // AND allowing the XMLDBEditor to recostruct the prev/next elements every
 // time one file is loaded and saved (Moodle).
 // Uncomment this if you're lazy like Petr
-// $CFG->xmldbreconstructprevnext = true;
-//
+// $CFG->xmldbreconstructprevnext = true;   // NOT FOR PRODUCTION SERVERS!
 //
 // Since 2.0 sql queries are not shown during upgrade by default.
 // Please note that this setting may produce very long upgrade page on large sites.
-// $CFG->upgradeshowsql = true;
-
+// $CFG->upgradeshowsql = true; // NOT FOR PRODUCTION SERVERS!
 
 
 //=========================================================================
