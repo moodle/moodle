e68d4a11fbfc730911b9d0a44be085c190b9e98d
Penny Leach <penny@liip.ch>
2010-03-29 11:32:01 +0000

    portfolio MDL-21884 rename LEAP2A to Leap2A in accordance with new community wisdom

--- lang/en_utf8/portfolio_format_leap2a.php
+++ lang/en_utf8/portfolio_format_leap2a.php
@@ -1,10 +1,10 @@
 <?php
 
-$string['entryalreadyexists'] = 'You tried to add a LEAP2A entry with an id ($a) that already exists in this feed';
-$string['feedtitle'] = 'LEAP2A export from Moodle for $a';
+$string['entryalreadyexists'] = 'You tried to add a Leap2A entry with an id ($a) that already exists in this feed';
+$string['feedtitle'] = 'Leap2A export from Moodle for $a';
 $string['invalidentryfield'] = 'You tried to set an entry field that didn\'t exist ($a) or you can\'t set directly';
 $string['invalidentryid'] = 'You tried to access an entry by an id that didn\'t exist ($a)';
-$string['missingfield'] = 'Required LEAP2A entry field $a missing';
-$string['nonexistantlink'] = 'A LEAP2A entry ($a->from) tried to link to a non existing entry ($a->to) with rel $a->rel';
+$string['missingfield'] = 'Required Leap2A entry field $a missing';
+$string['nonexistantlink'] = 'A Leap2A entry ($a->from) tried to link to a non existing entry ($a->to) with rel $a->rel';
 $string['overwritingselection'] = 'Overwriting the original type of an entry ($a) to selection in make_selection';
-$string['selflink'] = 'A LEAP2A entry ($a->id) tried to link to itself with rel $a->rel';
+$string['selflink'] = 'A Leap2A entry ($a->id) tried to link to itself with rel $a->rel';
