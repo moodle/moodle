0abd36bf69b8ecaa71c534ad2ea12dc27877cc2b
Patrick Malley <ptrkmkl@newschoollearning.com>
2010-04-11 18:35:26 +0000

    Adding a new test theme called Boxxie. It is the new name given to the Greenie theme I created for 1.9. This is my first add commit, so please contact me if I screw something up.

---
+++ theme/boxxie/config.php
@@ -0,0 +1,36 @@
+<?php
+
+$THEME->name = 'boxxie';
+
+////////////////////////////////////////////////////
+// Name of the theme. Most likely the name of
+// the directory in which this file resides. 
+////////////////////////////////////////////////////
+
+
+$THEME->parents = array('base');
+
+/////////////////////////////////////////////////////
+// Which existing theme(s) in the /theme/ directory
+// do you want this theme to extend. A theme can 
+// extend any number of themes. Rather than 
+// creating an entirely new theme and copying all 
+// of the CSS, you can simply create a new theme, 
+// extend the theme you like and just add the 
+// changes you want to your theme.
+////////////////////////////////////////////////////
+
+
+$THEME->sheets = array('screen');
+
+////////////////////////////////////////////////////
+// Name of the stylesheet(s) you've including in 
+// this theme's /styles/ directory.
+////////////////////////////////////////////////////
+
+
+$THEME->enable_dock = false;
+
+////////////////////////////////////////////////////
+// Do you want to use the new navigation dock?
+////////////////////////////////////////////////////
