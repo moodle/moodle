87fe6ecec70193047d4f2f8ad1818d85eba36e83
Petr Skoda <skodak@moodle.org>
2010-04-11 11:15:28 +0000

    MDL-22054 converting block name strings to pluginname
    AMOS START
     MOV [blockname,block_course_list],[pluginname,block_course_list]
    AMOS END

--- blocks/course_list/lang/en/block_course_list.php
+++ blocks/course_list/lang/en/block_course_list.php
@@ -25,8 +25,8 @@
 
 $string['adminview'] = 'Admin view';
 $string['allcourses'] = 'Admin user sees all courses';
-$string['blockname'] = 'Course List';
 $string['configadminview'] = 'What should the admin see in the course list block?';
 $string['confighideallcourseslink'] = 'Hide "All courses" link at the bottom of the block. Link hiding does not affects Admin\'s view';
 $string['hideallcourseslink'] = 'Hide All courses link';
 $string['owncourses'] = 'Admin user sees own courses';
+$string['pluginname'] = 'Course List';
