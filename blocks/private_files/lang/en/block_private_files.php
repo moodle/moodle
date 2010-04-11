99f3c138f7d6540d1a833c764a34a1f599e68f84
Petr Skoda <skodak@moodle.org>
2010-04-11 11:34:51 +0000

    MDL-22054 converting block name strings to pluginname
    AMOS START
     MOV [blockname,block_private_files],[pluginname,block_private_files]
    AMOS END

--- blocks/private_files/lang/en/block_private_files.php
+++ blocks/private_files/lang/en/block_private_files.php
@@ -23,5 +23,5 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['blockname'] = 'User private files';
+$string['pluginname'] = 'User private files';
 $string['privatefiles'] = 'Private files';
