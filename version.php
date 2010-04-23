059e689c0f1478079a5368b00c63324faaacd16e
Petr Skoda <skodak@moodle.org>
2010-04-23 09:06:31 +0000

    MDL-21781 cohort main version bump

--- version.php
+++ version.php
@@ -6,7 +6,7 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2010042301;  // YYYYMMDD   = date of the last version bump
+    $version = 2010042303;  // YYYYMMDD   = date of the last version bump
                             //         XX = daily increments
 
     $release = '2.0 dev (Build: 20100423)';  // Human-friendly version name
