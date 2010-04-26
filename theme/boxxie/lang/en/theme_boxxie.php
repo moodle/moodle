9f6e1e71edbdad53b803aa0cb2e5eda826183d00
Martin Dougiamas <martin@moodle.com>
2010-04-26 02:56:12 +0000

    Just adding a small readme placeholder for Patrick

--- theme/boxxie/lang/en/theme_boxxie.php
+++ theme/boxxie/lang/en/theme_boxxie.php
@@ -26,3 +26,4 @@
 $string['pluginname'] = 'Boxxie';
 $string['region-side-post'] = 'Right';
 $string['region-side-pre'] = 'Left';
+$string['choosereadme'] = 'Boxxie, a Moodle 2.0 theme by Patrick Malley (<a href="http://newschoollearning.com/">newschoollearning.com</a>)';
