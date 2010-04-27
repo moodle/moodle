b50ffc21d5ff325b8eb46d35908adc5de7840497
Patrick Malley <ptrkmkl@newschoollearning.com>
2010-04-27 15:14:09 +0000

    Adding new master theme called Canvas

---
+++ theme/canvas/config.php
@@ -0,0 +1,216 @@
+<?php
+
+$THEME->name = 'canvas';
+
+////////////////////////////////////////////////////
+// Name of the theme. Most likely the name of
+// the directory in which this file resides. 
+////////////////////////////////////////////////////
+
+$THEME->parents = array('base');
+
+/////////////////////////////////////////////////////
+// Which existing theme(s) in the /theme/ directory
+// do you want this theme to extend. A theme can 
+// extend any number of themes. Rather than 
+// creating an entirely new theme and copying all 
+// of the CSS, you can simply create a new theme, 
+// extend the theme you like and just add the 
+// changes you want to your theme.
+////////////////////////////////////////////////////
+
+$THEME->parents_exclude_sheets = array('base'=>array('pagelayout'));
+
+////////////////////////////////////////////////////
+// An array of stylesheets not to inherit from the
+// themes parents
+////////////////////////////////////////////////////
+
+$THEME->sheets = array('pagelayout', 'text', 'course', 'core');
+
+////////////////////////////////////////////////////
+// Name of the stylesheet(s) you've including in 
+// this theme's /styles/ directory.
+////////////////////////////////////////////////////
+
+$THEME->layouts = array(
+    'base' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'general' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'course' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post'
+    ),
+    'coursecategory' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'incourse' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'frontpage' => array(
+        'theme' => 'canvas',
+        'file' => 'frontpage.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'admin' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    'mydashboard' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+        'options' => array('langmenu'=>true),
+    ),
+    'mypublic' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'login' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('langmenu'=>true),
+    ),
+    'popup' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true),
+    ),
+    'frametop' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true),
+    ),
+    'maintenance' => array(
+        'theme' => 'canvas',
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true, 'nonavbar'=>true),
+    ),
+    'embedded' => array(
+    	'theme' => 'canvas',
+        'file' => 'embedded.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true, 'nonavbar'=>true),
+    ),
+    
+);
+
+///////////////////////////////////////////////////////////////
+// These are all of the possible layouts in Moodle. The
+// simplest way to do this is to keep the theme and file
+// variables the same for every layout. Including them
+// all in this way allows some flexibility down the road
+// if you want to add a different layout template to a
+// specific page.
+///////////////////////////////////////////////////////////////
+
+
+// $THEME->enable_dock = false;
+
+////////////////////////////////////////////////////
+// Do you want to use the new navigation dock?
+////////////////////////////////////////////////////
+
+
+// $THEME->editor_sheets;
+
+////////////////////////////////////////////////////
+// An array of stylesheets to include within the 
+// body of the editor.
+////////////////////////////////////////////////////
+
+// $THEME->csspostprocess
+	
+////////////////////////////////////////////////////
+// Allows the user to provide the name of a function 
+// that all CSS should be passed to before being 
+// delivered.
+////////////////////////////////////////////////////
+
+// $THEME->filter_mediaplugin_colors
+
+////////////////////////////////////////////////////
+// Used to control the colours used in the small 
+// media player for the filters
+////////////////////////////////////////////////////
+
+// $THEME->javascripts	
+
+////////////////////////////////////////////////////
+// An array containing the names of JavaScript files
+// located in /javascript/ to include in the theme. 
+// (gets included in the head)
+////////////////////////////////////////////////////
+
+// $THEME->javascripts_footer	
+
+////////////////////////////////////////////////////
+// As above but will be included in the page footer.
+////////////////////////////////////////////////////
+
+// $THEME->larrow	
+
+////////////////////////////////////////////////////
+// Overrides the left arrow image used throughout 
+// Moodle
+////////////////////////////////////////////////////
+
+// $THEME->rarrow	
+
+////////////////////////////////////////////////////
+// Overrides the right arrow image used throughout Moodle
+////////////////////////////////////////////////////
+
+// $THEME->parents_exclude_javascripts
+
+////////////////////////////////////////////////////
+// An array of JavaScript files NOT to inherit from
+// the themes parents
+////////////////////////////////////////////////////
+
+// $THEME->plugins_exclude_sheets
+
+////////////////////////////////////////////////////
+// An array of plugin sheets to ignore and not 
+// include.
+////////////////////////////////////////////////////
+
+// $THEME->renderfactory
+
+////////////////////////////////////////////////////
+// Sets a custom render factory to use with the 
+// theme, used when working with custom renderers.
+////////////////////////////////////////////////////
+
+// $THEME->resource_mp3player_colors
+
+////////////////////////////////////////////////////
+// Controls the colours for the MP3 player 	
+////////////////////////////////////////////////////
---
+++ theme/canvas/layout/frontpage.php
@@ -0,0 +1,102 @@
+<?php
+
+$hassidepre = $PAGE->blocks->region_has_content('side-pre', $OUTPUT);
+$hassidepost = $PAGE->blocks->region_has_content('side-post', $OUTPUT);
+
+$bodyclasses = array();
+if ($hassidepre && !$hassidepost) {
+    $bodyclasses[] = 'side-pre-only';
+} else if ($hassidepost && !$hassidepre) {
+    $bodyclasses[] = 'side-post-only';
+} else if (!$hassidepost && !$hassidepre) {
+    $bodyclasses[] = 'content-only';
+}
+
+echo $OUTPUT->doctype() ?>
+<html <?php echo $OUTPUT->htmlattributes() ?>>
+<head>
+    <title><?php echo $PAGE->title ?></title>
+    <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
+    <meta name="description" content="<?php echo strip_tags(format_text($SITE->summary, FORMAT_HTML)) ?>" />
+    <?php echo $OUTPUT->standard_head_html() ?>
+</head>
+<body id="<?php echo $PAGE->bodyid ?>" class="<?php echo $PAGE->bodyclasses.' '.join(' ', $bodyclasses) ?>">
+<?php echo $OUTPUT->standard_top_of_body_html() ?>
+
+<div id="page">
+
+<!-- START OF HEADER -->
+   
+    <div id="page-header" class="clearfix">
+		<div id="page-header-wrapper">
+	        <h1 class="headermain"><?php echo $PAGE->heading ?></h1>
+    	    <div class="headermenu">
+        		<?php
+	        	    echo $OUTPUT->login_info();
+    	        	echo $OUTPUT->lang_menu();
+	        	    echo $PAGE->headingmenu;
+		        ?>	    
+	    	</div>
+	    </div>
+    </div>
+        
+<!-- END OF HEADER -->
+
+<!-- START OF CONTENT -->
+
+<div id="page-content-wrapper">
+    <div id="page-content">
+        <div id="region-main-box">
+            <div id="region-post-box">
+            
+                <div id="region-main-wrap">
+                    <div id="region-main">
+                        <div class="region-content">
+                            <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
+                        </div>
+                    </div>
+                </div>
+                
+                <?php if ($hassidepre) { ?>
+                <div id="region-pre">
+                    <div class="region-content">
+                        <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
+                    </div>
+                </div>
+                <?php } ?>
+                
+                <?php if ($hassidepost) { ?>
+                <div id="region-post">
+                    <div class="region-content">
+                        <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                    </div>
+                </div>
+                <?php } ?>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- END OF CONTENT -->
+
+<!-- START OF FOOTER -->
+
+    <div id="page-footer">
+        <p class="helplink">
+        <?php echo page_doc_link(get_string('moodledocslink')) ?>
+        </p>
+
+        <?php
+        echo $OUTPUT->login_info();
+        echo $OUTPUT->home_link();
+        echo $OUTPUT->standard_footer_html();
+        ?>
+    </div>
+
+<!-- END OF FOOTER -->
+
+</div>
+<?php echo $OUTPUT->standard_end_of_body_html() ?>
+</body>
+</html>
