81623fad7a22bbd915070aa12f891259317985ac
Sam Hemelryk <sam@moodle.com>
2010-05-02 03:14:33 +0000

    backup MDL-22142 P16 - Fixed display of hidden fixed settings

--- backup/util/ui/backup_moodleform.class.php
+++ backup/util/ui/backup_moodleform.class.php
@@ -110,10 +110,7 @@ abstract class backup_moodleform extends moodleform {
      * @return bool
      */
     function add_setting(backup_setting $setting, backup_task $task=null) {
-        if ($setting->get_visibility() != backup_setting::VISIBLE) {
-            return false;
-        }
-        if ($setting->get_status() == backup_setting::NOT_LOCKED) {
+        if ($setting->get_status() == backup_setting::NOT_LOCKED && $setting->get_visibility() != backup_setting::VISIBLE) {
             // First add the formatting for this setting
             $this->add_html_formatting($setting);
             // The call the add method with the get_element_properties array
@@ -200,18 +197,18 @@ abstract class backup_moodleform extends moodleform {
      */
     function add_fixed_setting(backup_setting $setting) {
         global $OUTPUT;
-        $this->add_html_formatting($setting);
 
-        $mform = $this->_form;
         $settingui = $setting->get_ui();
-        if ($setting->get_status() != backup_setting::NOT_LOCKED) {
-            $mform->addElement('static', 'static_'.$settingui->get_name(), $settingui->get_label(),$settingui->get_static_value().' '.$OUTPUT->pix_icon('i/unlock', get_string('locked', 'backup'), 'moodle', array('class'=>'smallicon lockedicon')));
-        } else {
-            $mform->addElement('static','static_'. $settingui->get_name(), $settingui->get_label(), $settingui->get_static_value());
+        if ($setting->get_visibility() == backup_setting::VISIBLE) {
+            $this->add_html_formatting($setting);
+            if ($setting->get_status() != backup_setting::NOT_LOCKED) {
+                $this->_form->addElement('static', 'static_'.$settingui->get_name(), $settingui->get_label(),$settingui->get_static_value().' '.$OUTPUT->pix_icon('i/unlock', get_string('locked', 'backup'), 'moodle', array('class'=>'smallicon lockedicon')));
+            } else {
+                $this->_form->addElement('static','static_'. $settingui->get_name(), $settingui->get_label(), $settingui->get_static_value());
+            }
+            $this->_form->addElement('html', html_writer::end_tag('div'));
         }
-        $mform->addElement('hidden', $settingui->get_name(), $settingui->get_value());
-
-        $this->_form->addElement('html', html_writer::end_tag('div'));
+        $this->_form->addElement('hidden', $settingui->get_name(), $settingui->get_value());
     }
     /**
      * Adds dependencies to the form recursively
