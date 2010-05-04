a2b7cd643e05ca0306bbc24b4d07b2c49b7d75c2
Helen Foster <helen@moodle.org>
2010-05-04 13:43:19 +0000

    MDL-21695 rewording help strings thanks to OU sam

--- lang/en/condition.php
+++ lang/en/condition.php
@@ -33,7 +33,7 @@ $string['availableuntil_help'] = 'The activity disappears after this date. The a
 $string['badavailabledates'] = 'Invalid dates. If you set both dates, the \'available from\' date should be before the \'until\' date.';
 $string['completion_complete'] = 'must be marked complete';
 $string['completioncondition'] = 'Activity completion condition';
-$string['completioncondition_help'] = 'Not available until the user has completed another activity in a particular way';
+$string['completioncondition_help'] = 'This setting determines any activity completion conditions which must be met in order for the activity to be available. Note that completion tracking must first be set before an activity completion condition can be set.  Multiple activity completion conditions may be set if desired.  The activity will only become available when ALL activity completion conditions are met.';
 $string['completion_fail'] = 'must be complete with fail grade';
 $string['completion_incomplete'] = 'must not be marked complete';
 $string['completion_pass'] = 'must be complete with pass grade';
@@ -41,25 +41,7 @@ $string['configenableavailability'] = 'When enabled, this lets you set condition
 $string['enableavailability'] = 'Enable conditional availability';
 $string['grade_atleast'] = 'must be at least';
 $string['gradecondition'] = 'Grade condition';
-$string['gradecondition_help'] = 'You can specify a condition on any grade in the course: the full course grade, the grade for any activity, or a custom grade that you create manually.
-You can enter either a minimum value (&ge;), a maximum value (&lt;), both, or 
-neither. The activity will only appear if the student has a value for the 
-specified grade, and if it falls within any specified number range.
-You can add more than one grade condition. All conditions must be met in order
-for the activity to appear.
-<ul>
-<li>The range numbers can be fractional (with up to five decimal places) if 
-    necessary. </li>
-<li>Be careful with the maximum value; if the maximum is 7, a student who 
-    scores exactly 7 will not see the activity. You could set it to 7.01 if
-    you really wanted to include 7.</li>
-<li>If creating several different activities that appear according to grade 
-    ranges, use the same number for the maximum of one activity, and the minimum
-    of the next. For example, you might create one activity with a maximum of 7
-    and another with a minimum of 7. The first would appear to everyone scoring
-    between 0 and 6.99999, and the second would appear to everyone scoring 7.00000
-    to 10. This guarantees that everyone with a grade will see one or other.</li>
-</ul>';
+$string['gradecondition_help'] = 'This setting determines any grade conditions which must be met in order for the activity to be available. Multiple grade conditions may be set if desired. The activity will only become available when ALL grade conditions are met.';
 $string['grade_upto'] = 'and less than';
 $string['none'] = '(none)';
 $string['notavailableyet'] = 'Not available yet';
