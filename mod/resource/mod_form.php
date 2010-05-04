b01c3e8492e72667f78dad9b593f06a347aaa45a
Dongsheng Cai <unoter@gmail.com>
2010-05-04 09:49:11 +0000

    MDL-22281, fixed moodle form

--- mod/resource/mod_form.php
+++ mod/resource/mod_form.php
@@ -56,14 +56,21 @@ class mod_resource_mod_form extends moodleform_mod {
 
         //-------------------------------------------------------
         $mform->addElement('header', 'contentsection', get_string('contentheader', 'resource'));
+
+        $filemanager_options = array();
+        $filemanager_options['accepted_types'] = '*';
+
+        // 3 == FILE_EXTERNAL & FILE_INTERNAL
+        // These two constant names are defined in repository/lib.php
+        $filemanager_options['return_types'] = 3;
+        $filemanager_options['maxbytes'] = 0;
+        $filemanager_options['maxfiles'] = -1;
         $mainfile = 'mainfile';
+        $filemanager_options['mainfile'] = $mainfile;
 
-        $options = array('mainfile'=>$mainfile, 'subdirs'=>0, 'maxbytes'=>0, 'maxfiles'=>-1, 'filetypes'=>'*', 'returnvalue'=>'ref_id');
-
-        $mform->addElement('hidden', $mainfile, '', array('id'=>$mainfile.'-id')); // TODO: what is this?
-        $mform->setType('mainfile', PARAM_PATH);
-        $mform->addElement('filemanager', 'files', get_string('selectfiles'), null, $options);
-        $mform->addElement('static', '', get_string('selectedfile'), '<div id="'.$mainfile.'-label"></div>');
+        $mform->addElement('filemanager', 'files', get_string('selectfiles'), null, $filemanager_options);
+        $mform->addElement('text', $mainfile, get_string('selectedfile'));
+        $mform->setType($mainfile, PARAM_PATH);
 
         //-------------------------------------------------------
         $mform->addElement('header', 'optionssection', get_string('optionsheader', 'resource'));
