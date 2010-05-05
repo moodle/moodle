e0ad0e7397a6048b8a72c7c89c8261ebe7a7561c
Dan Marsden <Dan@digitalclay.co.nz>
2010-05-05 23:48:00 +0000

    SCORM Grades MDL-22332 redirect to report page correctly from gradebook - thanks to Valerian for report/fix.

---
+++ mod/scorm/grade.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once("../../config.php");
+
+    $id   = required_param('id', PARAM_INT);          // Course module ID
+
+    if (! $cm = get_coursemodule_from_id('scorm', $id)) {
+        print_error('invalidcoursemodule');
+    }
+
+    if (! $scorm = get_record('scorm', 'id', $cm->instance)) {
+        print_error('invalidcoursemodule');
+    }
+
+    if (! $course = get_record('course', 'id', $scorm->course)) {
+        print_error('coursemisconf');
+    }
+
+    require_login($course->id, false, $cm);
+
+    if (has_capability('mod/scorm:viewreport', get_context_instance(CONTEXT_MODULE, $cm->id))) {
+        redirect('report.php?id='.$cm->id);
+    } else {
+        redirect('view.php?id='.$cm->id);
+    }
