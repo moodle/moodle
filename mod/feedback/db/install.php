82b90a59c1f6e87c3f74620c066641217aa64650
Martin Dougiamas <martin@moodle.com>
2010-05-16 16:05:20 +0000

    feedback - just fixing some PHP comments

--- mod/feedback/db/install.php
+++ mod/feedback/db/install.php
@@ -1,14 +1,9 @@
 <?php
 
-// This file replaces:
-//   * STATEMENTS section in db/install.xml
-//   * lib.php/modulename_install() post installation hook
-//   * partially defaults.php
-
 function xmldb_feedback_install() {
     global $DB;
 
-/// Disable it by default
+/// Disable this module by default (because it's not technically part of Moodle 2.0)
     $DB->set_field('modules', 'visible', 0, array('name'=>'feedback'));
 
 /// Install logging support
