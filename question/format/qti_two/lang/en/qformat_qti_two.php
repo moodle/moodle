eed47b019a01cf0da1e216e9050b893bbff4b87d
Helen Foster <helen@moodle.org>
2010-05-16 22:36:44 +0000

    MDL-21695 adding help and link strings

---
+++ question/format/qti_two/lang/en/qformat_qti_two.php
@@ -0,0 +1,28 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qformat_qti_two', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   qformat_qti_two
+ * @copyright 2010 Helen Foster
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['qti_two'] = 'IMS QTI 2.0 format';
+$string['qti_two_help'] = 'This format enables questions to be exported in the standard IMS QTI format (version 2.0) via a zip file.';
+$string['qti_two_link'] = 'qformat/qti/two';
