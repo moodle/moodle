aedf58841f4b62b125d7ce7f30856fafbec2257f
Helen Foster <helen@moodle.org>
2010-05-18 07:50:24 +0000

    MDL-21695 adding help and link strings

---
+++ question/format/missingword/lang/en/qformat_missingword.php
@@ -0,0 +1,28 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qformat_missingword', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   qformat_missingword
+ * @copyright 2010 Helen Foster
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['missingword'] = 'Missing word format';
+$string['missingword_help'] = 'Missing word format enables questions to be imported via text file.';
+$string['missingword_link'] = 'Missing word format';
