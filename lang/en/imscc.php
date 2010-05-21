ff24ae3b366c733b6a6901a154abfa6d28ab8f62
Petr Skoda <skodak@moodle.org>
2010-05-21 18:00:13 +0000

    another typo

--- lang/en/imscc.php
+++ lang/en/imscc.php
@@ -26,7 +26,7 @@
 $string['cc_import_req_dom'] = 'ERROR: The Common Cartridge import requires DOM extension.';
 $string['cc_import_req_libxml'] = 'ERROR: The Common Cartridge import requires LIBXML extension.';
 $string['cc_import_req_libxmlminversion'] = 'ERROR: The Common Cartridge import requires LIBXML version 2.6.30 or newer.';
-$string['cc_import_req_php5'] = 'ERROR: The Common Cartridge import requires PHP 5 or highter.';
+$string['cc_import_req_php5'] = 'ERROR: The Common Cartridge import requires PHP 5 or higher.';
 $string['cc_import_req_xsl'] = 'ERROR: The Common Cartridge import requires XSL.';
 $string['cc2moodle_checking_schema'] = 'CC Format! Checking Schema...';
 $string['cc2moodle_invalid_schema'] = 'The schema is not valid.';
