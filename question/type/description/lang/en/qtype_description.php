c72fc30cde2d8cb8d07ca59a67553bb972fc1825
Helen Foster <helen@moodle.org>
2010-05-24 13:52:38 +0000

    MDL-21695 adding help string

--- question/type/description/lang/en/qtype_description.php
+++ question/type/description/lang/en/qtype_description.php
@@ -24,6 +24,9 @@
  */
 
 $string['addingdescription'] = 'Adding a Description';
+$string['addingdescription_help'] = 'A description is not really a question type. It simply enables text to be displayed without requiring any answers, similar to a label on the course page.
+
+The question text is displayed both during the attempt and on the review page. Any general feedback is displayed on the review page only.';
 $string['description'] = 'Description';
-$string['descriptionsummary'] = 'This is not actually a question. Instead it is a way to add some instructions, rubric or other content to the quiz. This is similar to the way that Labels can be used to add content to the course page.';
+$string['descriptionsummary'] = 'This is not actually a question. Instead it is a way to add some instructions, rubric or other content to the quiz. This is similar to the way that labels can be used to add content to the course page.';
 $string['editingdescription'] = 'Editing a Description';
