5a2c8d077f2f80d520ef9e7e7df4c55280dc6ec7
Helen Foster <helen@moodle.org>
2010-05-25 15:34:33 +0000

    MDL-21695 adding help and link strings

--- question/type/randomsamatch/lang/en/qtype_randomsamatch.php
+++ question/type/randomsamatch/lang/en/qtype_randomsamatch.php
@@ -28,4 +28,6 @@ $string['editingrandomsamatch'] = 'Editing a Random short-answer matching questi
 $string['nosaincategory'] = 'There are no short answer questions in the category that you chose \'{$a->catname}\'. Choose a different category, make some questions in this category.';
 $string['notenoughsaincategory'] = 'There is/are only {$a->nosaquestions} short answer questions in the category that you chose \'{$a->catname}\'. Choose a different category, make some more questions in this category or reduce the amount of questions you\'ve selected.';
 $string['randomsamatch'] = 'Random short-answer matching';
-$string['randomsamatchsummary'] = 'Like a Matching question, but created randomly from the shortanswer questions in a particular category.';
+$string['randomsamatch_help'] = 'From the student perspective, this looks just like a matching question. The difference is that the list of names or statements (questions) for matching are drawn randomly from the short answer questions in the current category. There should be sufficient unused short answer questions in the category, otherwise an error message will be displayed.';
+$string['randomsamatch_link'] = 'question/type/randomsamatch';
+$string['randomsamatchsummary'] = 'Like a Matching question, but created randomly from the short answer questions in a particular category.';
