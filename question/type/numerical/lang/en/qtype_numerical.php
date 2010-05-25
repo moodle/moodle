c96e53b75ee8c0579486bdb2d41756ecaf1d5b0e
Helen Foster <helen@moodle.org>
2010-05-25 13:08:55 +0000

    MDL-21695 adding help and link strings

--- question/type/numerical/lang/en/qtype_numerical.php
+++ question/type/numerical/lang/en/qtype_numerical.php
@@ -41,31 +41,22 @@ $string['noneditableunittext'] = 'NON editable text of Unit No1' ;
 $string['notenoughanswers'] = 'You must enter at least one answer.';
 $string['nounitdisplay'] = 'No unit grading' ;
 $string['numerical'] = 'Numerical';
-$string['numerical_help'] = 'From the student perspective, a numerical question looks just
-like a short-answer question.
-<p>
-The difference is that numerical answers are allowed
-to have an accepted error. This allows a continuous range of answers
-to be set.
-</p>
-For example, if the answer is 30 with an accepted error of 5,
-then any number between 25 and 35 will be accepted as correct.
-Like with short answer questions, different answers, or the same
-answer with different precisions can be given.'; 
+$string['numerical_help'] = 'From the student perspective, a numerical question looks just like a short-answer question. The difference is that numerical answers are allowed to have an accepted error. This allows a fixed range of answers to be evaluated as one answer. For example, if the answer is 10 with an accepted error of 2, then any number between 8 and 12 will be accepted as correct. '; 
+$string['numerical_link'] = 'question/type/numerical';
 $string['numericalsummary'] = 'Allows a numerical response, possibly with units, that is graded by comparing against various model answers, possibly with tolerances.';
 $string['numericalinstructions'] = 'Instructions'; 
 $string['numericalinstructions_help'] = 'Specific instructions related to the question as
 
-* examples of number formats
-
-* complex units. 
-
-'; 
+* Examples of number formats
+* Complex units'; 
 $string['numericalmultiplier'] = 'Multiplier'; 
-$string['numericalmultiplier_help'] = 'The Correct numerical response will be multiplied by the multiplier related to a specific unit to obtain the Correct response in this unit.<br/>
- <p> The first unit (Unit1) has a default multiplier of 1. </p>
- <p> So, if your Correct numerical response is 5500 and you set W as unit at Unit1 which has 1 as default multiplier, the Correct response is 5500 W.</p> 
- <p> If you add the unit kW with a multiplier of 0.001, this will add a Correct response of 5.5 kW. </p><p>This means that the answers 5500W or 5.5kW would be marked correct.</p><p> Note that the accepted error is also multiplied, so an allowed error of 100W would become an error of 0.1kW.</p>'; 
+$string['numericalmultiplier_help'] = 'The multiplier is the factor by which the correct numerical response will be multiplied.
+
+The first unit (Unit 1) has a default multiplier of 1. Thus if the correct numerical response is 5500 and you set W as unit at Unit 1 which has 1 as default multiplier, the correct response is 5500 W.
+
+If you add the unit kW with a multiplier of 0.001, this will add a correct response of 5.5 kW. This means that the answers 5500W or 5.5kW would be marked correct.
+
+Note that the accepted error is also multiplied, so an allowed error of 100W would become an error of 0.1kW.'; 
 $string['onlynumerical'] = 'Only NUMERICAL ANSWER will be graded' ;
 $string['rightexample'] = 'RIGHT as 1.00cm' ;
 $string['selectunits'] = 'Select units' ; 
