62ca4e15474a45137d0f61acee22f51434ae8dd2
Helen Foster <helen@moodle.org>
2010-05-26 13:25:41 +0000

    MDL-21695 changing link string

--- question/type/match/lang/en/qtype_match.php
+++ question/type/match/lang/en/qtype_match.php
@@ -24,12 +24,12 @@
  */
 
 $string['addingmatch'] = 'Adding a Matching question';
-$string['addingmatch_link'] = 'question/type/match';
 $string['addmoreqblanks'] = '{no} More Sets of Blanks';
 $string['editingmatch'] = 'Editing a Matching question';
 $string['filloutthreeqsandtwoas'] = 'You must provide at least two questions and three answers. You can provide extra wrong answers by giving an answer with a blank question. Entries where both the question and the answer are blank will be ignored.';
 $string['match'] = 'Matching';
 $string['match_help'] = 'Matching questions require the respondent to correctly match a list of names or statements (questions) to another list of names or statements (answers).';
+$string['match_link'] = 'question/type/match';
 $string['matchsummary'] = 'The answer to each of a number of sub-question must be selected from a list of possibilities.';
 $string['nomatchinganswerforq'] = 'You must specify an answer for this question.';
 $string['notenoughqsandas'] = 'You must supply at least {$a->q} questions and {$a->a} answers.';
