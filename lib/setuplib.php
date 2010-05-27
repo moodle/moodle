a8f810ecefb498e47550eed51dcfcf17d25183c0
Sam Hemelryk <sam@moodle.com>
2010-05-27 04:09:44 +0000

    course-section MDL-22262 Bumped major DB change version

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -782,7 +782,7 @@ function get_real_size($size=0) {
  */
 function redirect_if_major_upgrade_required() {
     global $CFG;
-    $lastmajordbchanges = 2010050404;
+    $lastmajordbchanges = 2010052700;
     if (empty($CFG->version) or (int)$CFG->version < $lastmajordbchanges or
             during_initial_install() or !empty($CFG->adminsetuppending)) {
         try {
