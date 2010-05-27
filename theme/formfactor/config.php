1470ded5234881a1026985d424f48ea53c23ac4d
Patrick Malley <ptrkmkl@newschoollearning.com>
2010-05-27 02:34:13 +0000

    Moved a misplaced element

--- theme/formfactor/config.php
+++ theme/formfactor/config.php
@@ -36,7 +36,7 @@ $THEME->enable_dock = false;
 ////////////////////////////////////////////////////
 
 
-$THEME->editor_sheets = array('editor');
+// $THEME->editor_sheets = array('editor');
 
 ////////////////////////////////////////////////////
 // An array of stylesheets to include within the 
