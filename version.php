6ab0ce20cc4367826ea7191836fe59efa2ae2a90
Martin Dougiamas <martin@moodle.com>
2010-06-08 05:24:12 +0000

    Bumping to 1.9.9

--- version.php
+++ version.php
@@ -6,10 +6,10 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2007101580.00; // YYYYMMDD      = date of the 1.9 branch (don't change)
+    $version = 2007101590.00; // YYYYMMDD      = date of the 1.9 branch (don't change)
                               //         X     = release number 1.9.[0,1,2,3,4,5...]
                               //          Y.YY = micro-increments between releases
 
-    $release = '1.9.8+ (Build: 20100608)';     // Human-friendly version name
+    $release = '1.9.9 (Build: 20100608)';     // Human-friendly version name
 
 ?>
