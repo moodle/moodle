157d25fb0d95b03636daa314a957b32b144fadcb
Martin Dougiamas <martin@moodle.com>
2010-06-11 03:14:27 +0000

    MDL-18483 Added an edit icon to the forum intro to make it easier to edit

--- course/format/social/format.php
+++ course/format/social/format.php
@@ -8,15 +8,25 @@
 
     if ($forum = forum_get_course_forum($course->id, 'social')) {
 
+        $cm = get_coursemodule_from_instance('forum', $forum->id);
+        $context = get_context_instance(CONTEXT_MODULE, $cm->id);
+
     /// Print forum intro above posts  MDL-18483
         if (trim($forum->intro) != '') {
             $options = new stdclass;
             $options->para = false;
-            echo $OUTPUT->box(format_text($forum->intro, FORMAT_MOODLE, $options), 'generalbox', 'intro');
+            $introcontent = format_text($forum->intro, FORMAT_MOODLE, $options);
+
+            if ($PAGE->user_is_editing() && has_capability('moodle/course:update', $context)) {
+                $streditsummary  = get_string('editsummary');
+                $introcontent .= '<div class="editinglink"><a title="'.$streditsummary.'" '.
+                                 '   href="modedit.php?update='.$cm->id.'&amp;sesskey='.sesskey().'">'.
+                                 '<img src="'.$OUTPUT->pix_url('t/edit') . '" '.
+                                 ' class="icon edit" alt="'.$streditsummary.'" /></a></div>';
+            }
+            echo $OUTPUT->box($introcontent, 'generalbox', 'intro');
         }
 
-        $cm = get_coursemodule_from_instance('forum', $forum->id);
-        $context = get_context_instance(CONTEXT_MODULE, $cm->id);
         echo '<div class="subscribelink">', forum_get_subscribe_link($forum, $context), '</div>';
         forum_print_latest_discussions($course, $forum, 10, 'plain', '', false);
 
