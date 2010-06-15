db24996c1763da185b2ecd3bee6aa318bc6efd7b
Martin Dougiamas <martin@moodle.com>
2010-06-15 08:24:37 +0000

    MDL-19168 Added missing capability string

--- repository/dropbox/lang/en/repository_dropbox.php
+++ repository/dropbox/lang/en/repository_dropbox.php
@@ -31,3 +31,4 @@ $string['repositoryname'] = 'Dropbox';
 $string['apikey'] = 'Dropbox API Key';
 $string['secret'] = 'Dropbox Secret';
 $string['instruction'] = 'You can get your API Key and secret from <a href="http://www.dropbox.com/developers/apps">Dropbox developers</a>';
+$string['dropbox:view'] = 'View a Dropbox folder';
