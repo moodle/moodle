639ac85fecf8f2d12bc61a9e59ba918562cab103
Rossiani Wijaya <rossianiwijaya@gmail.com>
2010-06-23 09:24:11 +0000

    MDL-20448 - Add 'skip' links to create new account on login page.

--- lang/en/moodle.php
+++ lang/en/moodle.php
@@ -1818,3 +1818,4 @@ $string['yourself'] = 'yourself';
 $string['yourteacher'] = 'your {$a}';
 $string['yourwordforx'] = 'Your word for \'{$a}\'';
 $string['zippingbackup'] = 'Zipping backup';
+$string['tocreatenewaccount'] = 'Skip to create new account';
