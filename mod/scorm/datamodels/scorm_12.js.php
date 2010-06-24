8d31692538b36f407a6daf5cb3ee810ff7f1ad9f
Dan Marsden <Dan@digitalclay.co.nz>
2010-06-24 02:25:25 +0000

    SCORM MDL-17891 fix loss of session data - thanks to Ron and Matteo for report/fixes missed fix for LMSCommit

--- mod/scorm/datamodels/scorm_12.js.php
+++ mod/scorm/datamodels/scorm_12.js.php
@@ -414,7 +414,21 @@ function SCORMapi1_2() {
                         //echo 'alert("Data Commited");';
                     }
                 ?>
-                return "true";
+                <?php
+                    if (scorm_debugging($scorm)) {
+                        echo 'LogAPICall("LMSCommit", "AJAXResult", result, 0);';
+                    }
+                ?>
+                result = ('true' == result) ? 'true' : 'false';
+                errorCode = (result ==' true')? '0' : '101';
+                <?php 
+                    if (scorm_debugging($scorm)) {
+                        //echo 'alert("Finished SCORM 1.2");';
+                        echo 'LogAPICall("LMSCommit", "result", result, 0);';
+                        echo 'LogAPICall("LMSCommit", param, "", 0);';
+                    }
+                ?>
+                return result;
             } else {
                 errorCode = "301";
             }
