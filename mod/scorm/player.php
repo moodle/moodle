f30f961fa1f4ac91e958e3c7f0288bae3e25161e
Dan Marsden <Dan@digitalclay.co.nz>
2010-06-24 02:02:35 +0000

    SCORM MDL-21333 modify way link appears if popup does not load to prevent SCORM from loading and not being able to communicate with API - thanks to Matteo Scaramuccia for report/fix merged from HEAD

--- mod/scorm/player.php
+++ mod/scorm/player.php
@@ -325,8 +325,8 @@
                     </noscript>
 <?php            
             //Added incase javascript popups are blocked
-            $link = '<a href="'.$CFG->wwwroot.'/mod/scorm/loadSCO.php?id='.$cm->id.$scoidstr.$modestr.'" target="new">'.get_string('popupblockedlinkname','scorm').'</a>';
-            print_simple_box(get_string('popupblocked','scorm',$link),'center');         
+            $link = '<a href="'.$CFG->wwwroot.'/mod/scorm/loadSCO.php?id='.$cm->id.$scoidstr.$modestr.'" target="_blank">'.get_string('popupblockedlinkname','scorm').'</a>';
+            print_simple_box(get_string('popupblocked','scorm',$link),'center','','',5,'generalbox','altpopuplink');
         }
     } else {
         print_simple_box(get_string('noprerequisites','scorm'),'center');
