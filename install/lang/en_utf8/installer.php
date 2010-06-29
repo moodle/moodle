2244cc3a54637c841870f1e38fa0e4626c919995
Moodle Robot <martin@moodle.com>
2010-06-29 00:37:01 +0000

    Automatic installer.php lang files by installer_builder (20100629)

--- install/lang/en_utf8/installer.php
+++ install/lang/en_utf8/installer.php
@@ -313,6 +313,5 @@ $string['wrongsourcebase'] = 'Wrong source URL base';
 $string['wrongzipfilename'] = 'Wrong ZIP file name';
 $string['wwwroot'] = 'Web address';
 $string['wwwrooterror'] = 'The \'Web Address\' does not appear to be valid - this Moodle installation doesn\'t appear to be there. The value below has been reset.';
-$string['xmlrpcrecommended'] = 'Installing the optional xmlrpc extension is useful for Moodle Networking functionality.';
 $string['ziprequired'] = 'The Zip PHP extension is now required by Moodle, info-ZIP binaries or PclZip library are not used anymore.';
 ?>
