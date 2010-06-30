c0210b914474a3ccc174c31049f9baa55dd06fab
Martin Dougiamas <martin@moodle.com>
2010-06-30 09:37:50 +0000

    Bumped to Preview Release 4

--- version.php
+++ version.php
@@ -6,8 +6,8 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2010062101;  // YYYYMMDD   = date of the last version bump
+    $version = 2010063000;  // YYYYMMDD   = date of the last version bump
                             //         XX = daily increments
 
-    $release = '2.0 Preview 3 (Build: 20100630)';  // Human-friendly version name
+    $release = '2.0 Preview 4 (Build: 20100630)';  // Human-friendly version name
 
