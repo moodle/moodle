09ae4cad1a5d3c38d794044ce01901a47faeec05
Petr Skoda <skodak@moodle.org>
2010-07-04 20:52:09 +0000

    MDL-11113 ooops, fixing messed up commit

---
+++ lib/editor/tinymce/tiny_mce/3.3.8/plugins/spellchecker/config.php
@@ -0,0 +1,22 @@
+<?php
+// General settings
+$config['general.engine'] = get_config('editor_tinymce', 'spellengine') ? get_config('editor_tinymce', 'spellengine') : 'GoogleSpell';
+//$config['general.engine'] = 'PSpell';
+//$config['general.engine'] = 'PSpellShell';
+//$config['general.remote_rpc_url'] = 'http://some.other.site/some/url/rpc.php';
+
+// PSpell settings
+$config['PSpell.mode'] = PSPELL_FAST;
+$config['PSpell.spelling'] = "";
+$config['PSpell.jargon'] = "";
+$config['PSpell.encoding'] = "";
+
+// PSpellShell settings
+$config['PSpellShell.mode'] = PSPELL_FAST;
+$config['PSpellShell.aspell'] = $CFG->aspellpath;
+$config['PSpellShell.tmp'] = '/tmp';
+
+// Windows PSpellShell settings
+$config['PSpellShell.aspell'] = $CFG->aspellpath;
+//$config['PSpellShell.tmp'] = 'c:/temp';
+?>
