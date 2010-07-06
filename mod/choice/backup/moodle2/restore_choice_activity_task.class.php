3f9d86cd69d5c2cddce0318f6ce04e883198237a
Eloy Lafuente <stronk7@moodle.org>
2010-07-06 22:38:53 +0000

    MDL-21432 backup - choice restore (missing steps). Enables activity settings

---
+++ mod/choice/backup/moodle2/restore_choice_activity_task.class.php
@@ -0,0 +1,55 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package moodlecore
+ * @subpackage backup-moodle2
+ * @copyright 2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * choice restore task that provides all the settings and steps to perform one
+ * complete restore of the activity
+ */
+class restore_choice_activity_task extends restore_activity_task {
+
+    /**
+     * Define (add) particular settings this activity can have
+     */
+    protected function define_my_settings() {
+        // No particular settings for this activity
+    }
+
+    /**
+     * Define (add) particular steps this activity can have
+     */
+    protected function define_my_steps() {
+        // TODO: Add choice structure step here
+    }
+
+    /**
+     * Code the transformations to perform in the actvity in
+     * order to get encoded transformed back to working links
+     */
+    static public function decode_content_links($content) {
+
+        // TODO: Decode CHOICEINDEX & CHOICEVIEWBYID
+
+        return $content;
+    }
+}
