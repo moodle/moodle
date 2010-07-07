5a4c4d4d9b0c7e2a96ba3d761385271798cff7ed
Helen Foster <helen@moodle.org>
2010-07-07 14:21:56 +0000

    MDL-22997 adding help strings

--- enrol/guest/lang/en/enrol_guest.php
+++ enrol/guest/lang/en/enrol_guest.php
@@ -25,12 +25,14 @@
 
 $string['guest:config'] = 'Configure guest access instances';
 $string['password'] = 'Password';
+$string['password_help'] = 'A password allows guest access to the course to be restricted to only those who know the password. Guests will be required to supply the password each time they access the course.';
 $string['passwordinvalid'] = 'Incorrect access password, please try again';
 $string['passwordinvalidhint'] = 'That access password was incorrect, please try again<br />
 (Here\'s a hint - it starts with \'{$a}\')';
 $string['pluginname'] = 'Guest access';
 $string['pluginname_desc'] = 'Guest access plugin is only granting temporary access to courses, it is not actually enrolling users.';
-$string['requirepassword'] = 'Require guest access passsword';
+$string['pluginname_help'] = 'Guest access is "read-only" access to the course, meaning guests cannot post in a forum, edit a wiki, add glossary or database activity entries or otherwise mess up the course.';
+$string['requirepassword'] = 'Require guest access password';
 $string['requirepassword_desc'] = 'Require access password in new courses and prevent removing of access password from existing courses.';
 $string['showhint'] = 'Show hint';
 $string['showhint_desc'] = 'Show first letter of the guest access password.';
--- enrol/self/lang/en/enrol_self.php
+++ enrol/self/lang/en/enrol_self.php
@@ -33,7 +33,15 @@ $string['enrolperiod_desc'] = 'Default length of the enrolment period (in second
 $string['enrolstartdate'] = 'Start date';
 $string['groupkey'] = 'Use group enrolment keys';
 $string['groupkey_desc'] = 'Use group enrolment keys by default.';
+$string['groupkey_help'] = 'In addition to restricting access to the course to only those who know the key, use of a group enrolment key means users are automatically added to the group when they enrol in the course.
+
+To use a group enrolment key, an enrolment key must be specified in the course settings as well as the group enrolment key in the group settings.';
 $string['password'] = 'Enrolment key';
+$string['password_help'] = 'An enrolment key enables access to the course to be restricted to only those who know the key.
+
+If the field is left blank, any user may enrol in the course.
+
+If an enrolment key is specified, any user attempting to enrol in the course will be required to supply the key. Note that a user only needs to supply the enrolment key ONCE, when they enrol in the course.';
 $string['passwordinvalid'] = 'Incorrect enrolment key, please try again';
 $string['passwordinvalidhint'] = 'That enrolment key was incorrect, please try again<br />
 (Here\'s a hint - it starts with \'{$a}\')';
@@ -52,6 +60,7 @@ $string['showhint'] = 'Show hint';
 $string['showhint_desc'] = 'Show first letter of the guest access key.';
 $string['status'] = 'Allow self enrolments';
 $string['status_desc'] = 'Allow users to self enrol into course by default.';
+$string['status_help'] = 'This setting determines whether a user can enrol (and unenrol) themselves from the course.';
 $string['unenrolselfconfirm'] = 'Do you really want to unenrol yourself from course "{$a}"?';
 $string['usepasswordpolicy'] = 'Use password policy';
 $string['usepasswordpolicy_desc'] = 'Use standard password policy for enrolment keys.';
