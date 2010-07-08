119fff55dd5c2f3b47ee81f357a3d141cd1b6d5d
Dan Marsden <Dan@digitalclay.co.nz>
2010-07-08 09:48:12 +0000

    SCORM MDL-22423 don't include empty raw scores from assets when calculating grade merged from HEAD

--- mod/scorm/locallib.php
+++ mod/scorm/locallib.php
@@ -451,7 +451,7 @@ function scorm_grade_user_attempt($scorm, $userid, $attempt=1, $time=false) {
             if (($userdata->status == 'completed') || ($userdata->status == 'passed')) {
                 $attemptscore->scoes++;
             }
-            if (isset($userdata->score_raw)) {
+            if (!empty($userdata->score_raw) || ($scorm->type=='sco' && isset($userdata->score_raw))) {
                 $attemptscore->values++;
                 $attemptscore->sum += $userdata->score_raw;
                 $attemptscore->max = ($userdata->score_raw > $attemptscore->max)?$userdata->score_raw:$attemptscore->max;
