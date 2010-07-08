0047a64f68ef8a7f293d0359b336df9a7d247e50
David Mudrak <david.mudrak@gmail.com>
2010-07-08 14:08:27 +0000

    NOMDL workshop: rubric grid layout fixed after recent addition of assessment setting form

--- mod/workshop/form/assessment_form.php
+++ mod/workshop/form/assessment_form.php
@@ -63,7 +63,7 @@ class workshop_assessment_form extends moodleform {
         // add the data common for all subplugins
         $mform->addElement('hidden', 'strategy', $this->workshop->strategy);
         if (!empty($this->options['editableweight']) and !$mform->isFrozen()) {
-            $mform->addElement('header', 'assessmentsettings', get_string('assessmentsettings', 'workshop'));
+            $mform->addElement('header', 'assessmentsettings', get_string('assessmentweight', 'workshop'));
             $mform->addElement('select', 'weight',
                     get_string('assessmentweight', 'workshop'), workshop::available_assessment_weights_list());
             $mform->setDefault('weight', 1);
