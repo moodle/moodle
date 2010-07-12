000ebd5c135a296b72390595bcfff418c698952c
David Mudrak <david.mudrak@gmail.com>
2010-07-12 10:14:08 +0000

    MDL-19235 Basic phpdocs and copyright statements for core_tag subsystem

--- tag/lib.php
+++ tag/lib.php
@@ -1,5 +1,20 @@
 <?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Moodle tag library
  *
@@ -28,10 +43,11 @@
  *
  * Tag set will create tags that need to be created.
  *
- * @licence http://www.gnu.org/copyleft/gpl.html GNU Public License
- * @package moodlecore
+ * @package    core
  * @subpackage tag
  * @see http://www.php.net/manual/en/function.urlencode.php
+ * @copyright  2007 Luiz Cruz <luiz.laydner@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 define('TAG_RETURN_ARRAY', 0);
