82195aaefe5ace633c25eeda3285502592193456
David Mudrak <david.mudrak@gmail.com>
2010-07-17 22:17:10 +0000

    MDL-22787 Instances of enrol_mnet can be added to the course

--- enrol/mnet/lang/en/enrol_mnet.php
+++ enrol/mnet/lang/en/enrol_mnet.php
@@ -24,7 +24,15 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['error_multiplehost'] = 'Some instance of MNet enrolment plugin already exists for this host. Only one instance per host and/or one instance for \'All hosts\' is allowed.';
+$string['instancename'] = 'Enrolment method name';
+$string['instancename_help'] = 'You can optionally rename this instance of the MNet enrolment method. If you leave this field empty, the default instance name will be used, containing the name of the remote host and the assigned role for their users.';
 $string['mnet_enrol_description'] = 'Publish this service to allow administrators at {$a} to enrol their students in courses you have created on your server.<br/><ul><li><em>Dependency</em>: You must also <strong>publish</strong> the SSO (Service Provider) service to {$a}.</li><li><em>Dependency</em>: You must also <strong>subscribe</strong> to the SSO (Identity Provider) service on {$a}.</li></ul><br/>Subscribe to this service to be able to enrol your students in courses  on {$a}.<br/><ul><li><em>Dependency</em>: You must also <strong>subscribe</strong> to the SSO (Service Provider) service on {$a}.</li><li><em>Dependency</em>: You must also <strong>publish</strong> the SSO (Identity Provider) service to {$a}.</li></ul><br/>';
 $string['mnet_enrol_name'] = 'Moodle Networked Enrolment';
 $string['pluginname'] = 'MNet remote enrolments';
 $string['pluginname_desc'] = 'Allows remote MNet host to enrol their users into our courses.';
+$string['remotesubscriber'] = 'Remote host';
+$string['remotesubscriber_help'] = 'Select \'All hosts\' to open this course for all MNet peers we are offering the remote enrolment service to. Or choose a single host to make this course available for their users only.';
+$string['remotesubscribersall'] = 'All hosts';
+$string['roleforremoteusers'] = 'Role for their users';
+$string['roleforremoteusers_help'] = 'What role will the remote users from the selected host get.';
--- enrol/mnet/lib.php
+++ enrol/mnet/lib.php
@@ -31,4 +31,113 @@ defined('MOODLE_INTERNAL') || die;
  */
 class enrol_mnet_plugin extends enrol_plugin {
 
+    /** @var caches the result of {@link get_remote_subscribers()} */
+    protected $cachesubscribers = null;
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Enrolment plugin API - methods required by Moodle core
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Returns localised name of enrol instance
+     *
+     * @param object|null $instance enrol_mnet instance
+     * @return string
+     */
+    public function get_instance_name($instance) {
+        global $DB;
+
+        if (empty($instance)) {
+            $enrol = $this->get_name();
+            return get_string('pluginname', 'enrol_'.$enrol);
+
+        } else if (empty($instance->name)) {
+            $enrol = $this->get_name();
+            if ($role = $DB->get_record('role', array('id'=>$instance->roleid))) {
+                $role = role_get_name($role, get_context_instance(CONTEXT_COURSE, $instance->courseid));
+            } else {
+                $role = get_string('error');
+            }
+            if (empty($instance->customint1)) {
+                $host = get_string('remotesubscribersall', 'enrol_mnet');
+            } else {
+                $host = $DB->get_field('mnet_host', 'name', array('id'=>$instance->customint1));
+            }
+            return get_string('pluginname', 'enrol_'.$enrol) . ' (' . format_string($host) . ' - ' . $role .')';
+
+        } else {
+            return format_string($instance->name);
+        }
+    }
+
+    /**
+     * Returns link to page which may be used to add new instance of enrolment plugin into the course
+     *
+     * The link is returned only if there are some MNet peers that we publish enrolment service to.
+     *
+     * @param int $courseid id of the course to add the instance to
+     * @return moodle_url|null page url or null if instance can not be created
+     */
+    public function get_candidate_link($courseid) {
+        global $DB;
+
+        if (!$this->mnet_available()) {
+            return null;
+        }
+        $coursecontext = get_context_instance(CONTEXT_COURSE, $courseid);
+        if (!has_capability('moodle/course:enrolconfig', $coursecontext)) {
+            return null;
+        }
+        $subscribers = $this->get_remote_subscribers();
+        if (empty($subscribers)) {
+            return null;
+        }
+
+        return new moodle_url('/enrol/mnet/addinstance.php', array('id'=>$courseid));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Internal methods used by the plugin itself only
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Returns a list of remote servers that can enrol their users into our courses
+     *
+     * We must publish MNet service 'mnet_enrol' for the peers to allow them to enrol
+     * their users into our courses.
+     *
+     * @return array
+     */
+    public function get_remote_subscribers() {
+        global $DB;
+
+        if (is_null($this->cachesubscribers)) {
+            $sql = "SELECT DISTINCT h.id, h.name AS hostname, h.wwwroot AS hosturl,
+                           a.display_name AS appname
+                      FROM {mnet_host} h
+                      JOIN {mnet_host2service} hs ON h.id = hs.hostid
+                      JOIN {mnet_service} s ON hs.serviceid = s.id
+                      JOIN {mnet_application} a ON h.applicationid = a.id
+                     WHERE s.name = 'mnet_enrol'
+                           AND hs.publish = 1";
+            $this->cachesubscribers = $DB->get_records_sql($sql);
+        }
+
+        return $this->cachesubscribers;
+    }
+
+    /**
+     * Is MNet networking enabled at this server?
+     *
+     * @return bool
+     */
+    protected function mnet_available() {
+        global $CFG;
+
+        if (empty($CFG->mnet_dispatcher_mode) || $CFG->mnet_dispatcher_mode !== 'strict') {
+            return false;
+        }
+        return true;
+    }
+
 }
