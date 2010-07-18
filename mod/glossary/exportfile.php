b548e69cefbd4a11acebb3101357e2863591fcb3
Petr Skoda <skodak@moodle.org>
2010-07-18 08:43:25 +0000

    fixed missing inlcude

--- mod/glossary/exportfile.php
+++ mod/glossary/exportfile.php
@@ -4,6 +4,7 @@
 define('NO_DEBUG_DISPLAY', true);
 
 require_once("../../config.php");
+require_once("$CFG->libdir/filelib.php");
 require_once("lib.php");
 
 $id = required_param('id', PARAM_INT);      // Course Module ID
