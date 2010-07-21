ece2c44fe7cb3fc2b8a32262889fab5f52ea9718
Petr Skoda <skodak@moodle.org>
2010-07-21 07:23:39 +0000

    MDL-23197 fixing stupid typo, thanks Joseph

--- mod/lesson/pagetypes/multichoice.php
+++ mod/lesson/pagetypes/multichoice.php
@@ -233,7 +233,7 @@ class lesson_page_type_multichoice extends lesson_page {
             }
             $result->newpageid = $answer->jumpto;
             $result->response  = format_text($answer->response, $answer->responseformat, $formattextdefoptions);
-            $result->userresponse = foramt_text($answer->answer, $answer->answerformat, $formattextdefoptions);
+            $result->userresponse = format_text($answer->answer, $answer->answerformat, $formattextdefoptions);
         }
         return $result;
     }
