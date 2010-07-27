aae0d2fad0a6c7dd394e0f7d50e30c023d8885e3
Eloy Lafuente <stronk7@moodle.org>
2010-07-27 23:15:09 +0000

    MDL-21432 backup - need to acces gradelib API on restore

--- backup/util/includes/restore_includes.php
+++ backup/util/includes/restore_includes.php
@@ -88,3 +88,4 @@ require_once($CFG->dirroot . '/backup/util/ui/restore_ui_components.php');
 
 // And some moodle stuff too
 require_once ($CFG->dirroot . '/tag/lib.php');
+require_once ($CFG->dirroot . '/lib/gradelib.php');
