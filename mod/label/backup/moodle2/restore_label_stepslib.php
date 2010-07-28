8508a59fff2a08a314c2db7d03387a521333a44e
Dan Marsden <Dan@digitalclay.co.nz>
2010-07-28 01:17:29 +0000

    MDL-23479 Label Restore code

---
+++ mod/label/backup/moodle2/restore_label_stepslib.php
@@ -0,0 +1,56 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package moodlecore
+ * @subpackage backup-moodle2
+ * @copyright 2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Define all the restore steps that will be used by the restore_url_activity_task
+ */
+
+/**
+ * Structure step to restore one label activity
+ */
+class restore_label_activity_structure_step extends restore_activity_structure_step {
+
+    protected function define_structure() {
+
+        $paths = array();
+        $paths[] = new restore_path_element('label', '/activity/label');
+
+        // Return the paths wrapped into standard activity structure
+        return $this->prepare_activity_structure($paths);
+    }
+
+    protected function process_label($data) {
+        global $DB;
+
+        $data = (object)$data;
+        $oldid = $data->id;
+        $data->course = $this->get_courseid();
+
+        // insert the url record
+        $newitemid = $DB->insert_record('label', $data);
+        // inmediately after inserting "activity" record, call this
+        $this->apply_activity_instance($newitemid);
+    }
+
+}
