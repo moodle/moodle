c1a22a6eff18c480a5fcf71eaff74830be235658
Martin Dougiamas <martin@moodle.com>
2010-07-28 13:04:54 +0000

    Bump for today

--- version.php
+++ version.php
@@ -6,7 +6,7 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2010072700;  // YYYYMMDD   = date of the last version bump
+    $version = 2010072800;  // YYYYMMDD   = date of the last version bump
                             //         XX = daily increments
 
     $release = '2.0 Preview 4+ (Build: 20100728)';  // Human-friendly version name
