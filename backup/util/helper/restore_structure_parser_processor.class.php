2e45c72a2019252f20cc135f522b2ec334e11cbe
Dan Marsden <Dan@digitalclay.co.nz>
2010-07-29 03:41:16 +0000

    MDL-23479 missing global

--- backup/util/helper/restore_structure_parser_processor.class.php
+++ backup/util/helper/restore_structure_parser_processor.class.php
@@ -48,6 +48,7 @@ class restore_structure_parser_processor extends grouped_parser_processor {
      * Provide NULL and legacy file.php uses decoding
      */
     public function process_cdata($cdata) {
+        global $CFG;
         if ($cdata === '$@NULL@$') {  // Some cases we know we can skip complete processing
             return null;
         } else if ($cdata === '') {
