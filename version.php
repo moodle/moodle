0f86b560e8323b3e1f497b30a6ad8b3a34e1905d
Moodle Robot <martin@moodle.com>
2010-07-31 00:52:52 +0000

    Updated the 18 build version to 20100731

--- version.php
+++ version.php
@@ -13,6 +13,6 @@
                               // Do not use more than two decimal points as we have
                               // hit the float limit
 
-   $release = '1.8.13+ (Build: 20100730)';     // Human-friendly version name
+   $release = '1.8.13+ (Build: 20100731)';     // Human-friendly version name
 
 ?>
