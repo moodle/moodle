c65171b738596d41caf3d5235777952351bca0be
Petr Skoda <skodak@moodle.org>
2010-07-31 19:09:47 +0000

    MDL-23502 ohlala, sorting lang strings and adding new unenrolself string

--- enrol/paypal/lang/en/enrol_paypal.php
+++ enrol/paypal/lang/en/enrol_paypal.php
@@ -24,27 +24,28 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['defaultrole'] = 'Default role assignment';
-$string['defaultrole_desc'] = 'Select role which should be assigned to users during PayPal enrolments';
-$string['status'] = 'Allow PayPal enrolments';
-$string['status_desc'] = 'Allow users to use PayPal to enrol into a course by default.';
-$string['enrolperiod'] = 'Enrolment period';
-$string['enrolperiod_desc'] = 'Default length of the enrolment period (in seconds).'; //TODO: fixme
-$string['costorkey'] = 'Please choose one of the following methods of enrolment.';
-$string['pluginname_desc'] = 'The PayPal module allows you to set up paid courses.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
-$string['pluginname'] = 'PayPal';
-$string['paypalaccepted'] = 'PayPal payments accepted';
-$string['sendpaymentbutton'] = 'Send payment via PayPal';
-$string['cost'] = 'Enrol Cost';
-$string['currency'] = 'Currency';
+$string['assignrole'] = 'Assign role';
 $string['businessemail'] = 'PayPal business email';
 $string['businessemail_desc'] = 'The email address of your business PayPal account';
-$string['mailstudents'] = 'Notify students';
-$string['mailteachers'] = 'Notify teachers';
-$string['mailadmins'] = 'Notify admin';
-$string['assignrole'] = 'Assign role';
-$string['enrolstartdate'] = 'Start date';
+$string['cost'] = 'Enrol Cost';
+$string['costerror'] = 'The enrolment cost is not numeric';
+$string['costorkey'] = 'Please choose one of the following methods of enrolment.';
+$string['currency'] = 'Currency';
+$string['defaultrole'] = 'Default role assignment';
+$string['defaultrole_desc'] = 'Select role which should be assigned to users during PayPal enrolments';
 $string['enrolenddate'] = 'End date';
 $string['enrolenddaterror'] = 'Enrolment end date cannot be earlier than start date';
-$string['costerror'] = 'The enrolment cost is not numeric';
+$string['enrolperiod'] = 'Enrolment period';
+$string['enrolperiod_desc'] = 'Default length of the enrolment period (in seconds).'; //TODO: fixme
+$string['enrolstartdate'] = 'Start date';
+$string['mailadmins'] = 'Notify admin';
+$string['mailstudents'] = 'Notify students';
+$string['mailteachers'] = 'Notify teachers';
 $string['nocost'] = 'There is no cost associated with enrolling in this course!';
+$string['paypalaccepted'] = 'PayPal payments accepted';
+$string['pluginname'] = 'PayPal';
+$string['pluginname_desc'] = 'The PayPal module allows you to set up paid courses.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
+$string['sendpaymentbutton'] = 'Send payment via PayPal';
+$string['status'] = 'Allow PayPal enrolments';
+$string['status_desc'] = 'Allow users to use PayPal to enrol into a course by default.';
+$string['unenrolselfconfirm'] = 'Do you really want to unenrol yourself from course "{$a}"?';
