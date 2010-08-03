9deebbdade724cb7a957ed37b11f6a0102552507
Andrea Bicciolo <a.bicciolo@mtouch.it>
2010-08-03 16:31:02 +0000

    Fix for MDL-23342

--- theme/formal_white/config.php
+++ theme/formal_white/config.php
@@ -1,30 +1,62 @@
 <?php
 
-////////////////////////////////////////////////////////////////////////////////
-/// This file contains a few configuration variables that control
-/// how Moodle uses this theme.
-////////////////////////////////////////////////////////////////////////////////
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-// The name of our theme
+/**
+ * Configuration for Moodle's nonzero theme.
+ *
+ * DO NOT MODIFY THIS THEME!
+ * COPY IT FIRST, THEN RENAME THE COPY AND MODIFY IT INSTEAD.
+ *
+ * For full information about creating Moodle themes, see:
+ *  http://docs.moodle.org/en/Development:Themes_2.0
+ *
+ * @package   moodlecore
+ * @copyright Mediatouch 2000 (http://mediatouch.it/)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $THEME->name = 'formal_white';
+////////////////////////////////////////////////////
+// Name of the theme. Most likely the name of
+// the directory in which this file resides.
+////////////////////////////////////////////////////
 
+$THEME->parents = array('canvas','base');
+/////////////////////////////////////////////////////
+// Which existing theme(s) in the /theme/ directory
+// do you want this theme to extend. A theme can
+// extend any number of themes. Rather than
+// creating an entirely new theme and copying all
+// of the CSS, you can simply create a new theme,
+// extend the theme you like and just add the
+// changes you want to your theme.
+////////////////////////////////////////////////////
 
 $THEME->sheets = array('dock','base','general','fw_corners','formalwhite','settings');
-$layoutpage = 'general.php';
-/// This variable is an array containing the names of all the
-/// stylesheet files you want included in this theme, and in what order
-////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+// Name of the stylesheet(s) you've including in
+// this theme's /styles/ directory.
+////////////////////////////////////////////////////
 
-$THEME->parents = array('base');  // TODO: new themes can not be based on standardold, instead use 'base' as the base
-/// This variable can be set to the name of a parent theme
-/// which you want to have included before the current theme.
-/// This can make it easy to make modifications to another
-/// theme without having to actually change the files
-/// If this variable is empty or false then a parent theme
-/// is not used.
-////////////////////////////////////////////////////////////////////////////////
-
-$THEME->parents_exclude_sheets = array('base'=>array('styles_moz'));
+$THEME->parents_exclude_sheets = array(
+		'canvas'=>array(
+			'tabs',
+		),
+);
 
 $THEME->resource_mp3player_colors =
  'bgColour=000000&btnColour=ffffff&btnBorderColour=cccccc&iconColour=000000&'.
@@ -45,70 +77,70 @@ $THEME->editor_sheets = array('styles_tinymce');
 $THEME->layouts = array(
     // Most pages - if we encounter an unknown or a missing page type, this one is used.
     'base' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array()
     ),
     'standard' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array('side-pre'),
         'defaultregion' => 'side-pre'
     ),
     // Course page
     'course' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-post'
     ),
     // Course page
     'coursecategory' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-post'
     ),
     'incourse' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-post'
     ),
     'frontpage' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-pre'
     ),
     'admin' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array('side-pre'),
         'defaultregion' => 'side-pre'
     ),
     'mydashboard' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-post'
     ),
     'mypublic' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-post'
     ),
     'login' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array()
     ),
     // Pages that appear in pop-up windows - no navigation, no blocks, no header.
     'popup' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array(),
         'options' => array('nofooter'=>true, 'nonavbar'=>true, 'noblocks'=>true),
     ),
     // No blocks and minimal footer - used for legacy frame layouts only!
     'frametop' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array(),
         'options' => array('nofooter', 'noblocks'=>true),
     ),
     // Embeded pages, like iframe embeded in moodleform
     'embedded' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array(),
         'options' => array('nofooter'=>true, 'nonavbar'=>true, 'noblocks'=>true),
     ),
@@ -116,7 +148,7 @@ $THEME->layouts = array(
     // This must not have any blocks, and it is good idea if it does not have links to
     // other places - for example there should not be a home link in the footer...
     'maintenance' => array(
-        'file' => $layoutpage,
+        'file' => 'general.php',
         'regions' => array(),
         'options' => array('nofooter'=>true, 'nonavbar'=>true, 'noblocks'=>true),
     )
