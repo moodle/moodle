b5e1ffe45616449759298ab0d824b8ee5c056a5e
Aparup Banerjee <nebgor@gmail.com>
2010-08-06 03:49:11 +0000

    assignment MDL-23684 renamed a variable causing confusion

--- mod/assignment/lib.php
+++ mod/assignment/lib.php
@@ -1796,12 +1796,12 @@ class assignment_base {
     /**
      * Count the files uploaded by a given user
      *
-     * @param $userid int The user id
+     * @param $itemid int The submission's id as the file's itemid.
      * @return int
      */
-    function count_user_files($userid) {
+    function count_user_files($itemid) {
         $fs = get_file_storage();
-        $files = $fs->get_area_files($this->context->id, 'mod_assignment', 'submission', $userid, "id", false);
+        $files = $fs->get_area_files($this->context->id, 'mod_assignment', 'submission', $itemid, "id", false);
         return count($files);
     }
 
