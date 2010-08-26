492a55e7f2df2cb17835be5ee59eeda764eb2173
Petr Skoda <skodak@moodle.org>
2010-08-26 17:19:09 +0000

    MDL-23949 fixed use of invalid $USER before linked to session

--- lib/sessionlib.php
+++ lib/sessionlib.php
@@ -758,13 +758,12 @@ function session_gc() {
  * @return string
  */
 function sesskey() {
-    global $USER;
-
-    if (empty($USER->sesskey)) {
-        $USER->sesskey = random_string(10);
+    // note: do not use $USER because it may not be initialised yet
+    if (empty($_SESSION['USER']->sesskey)) {
+        $_SESSION['USER']->sesskey = random_string(10);
     }
 
-    return $USER->sesskey;
+    return $_SESSION['USER']->sesskey;
 }
 
 
