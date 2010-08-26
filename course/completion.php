70f25ccee41202cfe972209ebab71561ec9ccf4e
Aaron Barnes <aaronb@catalyst.net.nz>
2010-08-26 23:05:43 +0000

    course completion: MDL-2631 Only update passing grade if grade criteria enabled

--- course/completion.php
+++ course/completion.php
@@ -143,11 +143,13 @@ if ($form->is_cancelled()){
     $aggregation->insert();
 
     // Update course total passing grade
-    if ($grade_item = grade_category::fetch_course_category($course->id)->grade_item) {
-        $grade_item->gradepass = $data->criteria_grade_value;
-        if (method_exists($grade_item, 'update')) {
-            $grade_item->update('course/completion.php');
-        }
+    if (!empty($data->criteria_grade)) {
+    	if ($grade_item = grade_category::fetch_course_category($course->id)->grade_item) {
+        	$grade_item->gradepass = $data->criteria_grade_value;
+        	if (method_exists($grade_item, 'update')) {
+            	$grade_item->update('course/completion.php');
+        	}
+    	}
     }
 
     redirect($CFG->wwwroot."/course/view.php?id=$course->id", get_string('changessaved'));
