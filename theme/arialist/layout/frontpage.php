6ec548345ece3daa06adb86a12dd5dd9ad7e6c5f
Sam Hemelryk <sam@moodle.com>
2010-08-27 08:18:08 +0000

    theme-arialist MDL-23188 Added support for the custom menu

--- theme/arialist/layout/frontpage.php
+++ theme/arialist/layout/frontpage.php
@@ -6,11 +6,17 @@ $hasfooter = (empty($PAGE->layout_options['nofooter']));
 $hassidepost = $PAGE->blocks->region_has_content('side-post', $OUTPUT);
 $showsidepost = ($hassidepost && !$PAGE->blocks->region_completely_docked('side-post', $OUTPUT));
 
+$custommenu = $OUTPUT->custom_menu();
+$hascustommenu = (empty($PAGE->layout_options['nocustommenu']) && !empty($custommenu));
+
 $bodyclasses = array();
 if ($showsidepost) {
     $bodyclasses[] = 'side-post-only';
 } else if (!$showsidepost) {
     $bodyclasses[] = 'content-only';
+}
+if ($hascustommenu) {
+    $bodyclasses[] = 'has-custom-menu';
 }
 
 if (!empty($PAGE->theme->settings->tagline)) {
@@ -19,7 +25,7 @@ if (!empty($PAGE->theme->settings->tagline)) {
     $tagline = '<!-- There was no custom tagline set -->';
 }
 if (!empty($PAGE->theme->settings->logo)) { 
-	$logourl = $PAGE->theme->settings->logo;
+    $logourl = $PAGE->theme->settings->logo;
 }
 
 echo $OUTPUT->doctype() ?>
@@ -34,80 +40,83 @@ echo $OUTPUT->doctype() ?>
 <body id="<?php echo $PAGE->bodyid ?>" class="<?php echo $PAGE->bodyclasses.' '.join(' ', $bodyclasses) ?>">
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
 
+<?php if ($hascustommenu) { ?>
+<div id="custommenu"><?php echo $custommenu; ?></div>
+<?php } ?>
+
 <div id="page">
 
-	<div id="wrapper" class="clearfix">
+    <div id="wrapper" class="clearfix">
 
 <!-- START OF HEADER -->
 
-	    <div id="page-header">
-			<div id="page-header-wrapper" class="wrapper clearfix">
-	    	    
-	    	    <div id="header-left">
-					<?php if (!empty($PAGE->theme->settings->logo)) { ?>
-                    	<a href="<?php echo $CFG->wwwroot; ?>" title="Home"><img id="logo" src="<?php echo $logourl; ?>" alt="Logo" /></a>
-					<?php } else { ?>
-			    	    <h1 class="headermain"><a href="<?php echo $CFG->wwwroot; ?>" title="Home"><?php echo $PAGE->heading ?></a></h1>
-			        	<div class="tagline"><?php echo $tagline; ?></div>
-					<?php } ?>
-		        </div>
-    	    	<div class="headermenu">
-        			<?php
-	        	    	echo $OUTPUT->login_info();
-	    	        	echo $OUTPUT->lang_menu();
-		        	    echo $PAGE->headingmenu;
-			        ?>
-	    		</div>
-	    		
-	    	</div>
-	    </div>
+        <div id="page-header">
+            <div id="page-header-wrapper" class="wrapper clearfix">
+
+                <div id="header-left">
+                    <?php if (!empty($PAGE->theme->settings->logo)) { ?>
+                        <a href="<?php echo $CFG->wwwroot; ?>" title="Home"><img id="logo" src="<?php echo $logourl; ?>" alt="Logo" /></a>
+                    <?php } else { ?>
+                        <h1 class="headermain"><a href="<?php echo $CFG->wwwroot; ?>" title="Home"><?php echo $PAGE->heading ?></a></h1>
+                        <div class="tagline"><?php echo $tagline; ?></div>
+                    <?php } ?>
+                </div>
+                <div class="headermenu">
+                    <?php
+                        echo $OUTPUT->login_info();
+                        echo $OUTPUT->lang_menu();
+                        echo $PAGE->headingmenu;
+                    ?>
+                </div>
+            </div>
+        </div>
         
 <!-- END OF HEADER -->
 
 <!-- START OF CONTENT -->
 
-		<div id="page-content-wrapper" class="wrapper clearfix">
-		    <div id="page-content">
-    		    <div id="region-main-box">
-        		    <div id="region-post-box">
+        <div id="page-content-wrapper" class="wrapper clearfix">
+            <div id="page-content">
+                <div id="region-main-box">
+                    <div id="region-post-box">
             
-            		    <div id="region-main-wrap">
-                		    <div id="region-main">
-                    		    <div class="region-content">
-                        		    <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
-		                        </div>
-    		                </div>
-        		        </div>
+                        <div id="region-main-wrap">
+                            <div id="region-main">
+                                <div class="region-content">
+                                    <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
+                                </div>
+                            </div>
+                        </div>
                 
-    	    	        <?php if ($hassidepost) { ?>
-        	    	    <div id="region-post" class="block-region">
-            	    	    <div class="region-content">
-                	    	    <?php echo $OUTPUT->blocks_for_region('side-post') ?>
-	                    	</div>
-		                </div>
-    		            <?php } ?>
-    		            
-            		</div>    
-        	    </div>
-	        </div>
-    	</div>
+                        <?php if ($hassidepost) { ?>
+                        <div id="region-post" class="block-region">
+                            <div class="region-content">
+                                <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                            </div>
+                        </div>
+                        <?php } ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
 
 <!-- END OF CONTENT -->
 
-	</div> <!-- END #wrapper -->
+    </div> <!-- END #wrapper -->
 
 <!-- START OF FOOTER -->
     <div id="page-footer" class="wrapper clearfix">
-   	    <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
+           <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
         <?php
-   		    echo $OUTPUT->login_info();
-       		echo $OUTPUT->home_link();
-	        echo $OUTPUT->standard_footer_html();
-   	    ?>
-   	</div>
+               echo $OUTPUT->login_info();
+               echo $OUTPUT->home_link();
+            echo $OUTPUT->standard_footer_html();
+           ?>
+       </div>
 
 <!-- END OF FOOTER -->
-	
+
 </div> <!-- END #page -->
 
 <?php echo $OUTPUT->standard_end_of_body_html() ?>
