9f5c39b5278f240f8519400587400b3d739cba55
Sam Hemelryk <sam@moodle.com>
2010-08-30 06:57:05 +0000

    blocks MDL-24002 Split block renderering into several smaller methods, one for each area of a block.

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -763,36 +763,84 @@ class core_renderer extends renderer_base {
 
         $output .= html_writer::start_tag('div', $bc->attributes);
 
-        $controlshtml = $this->block_controls($bc->controls);
+        $output .= $this->block_header($bc);
+        $output .= $this->block_content($bc);
+
+        $output .= html_writer::end_tag('div');
+
+        $output .= $this->block_annotation($bc);
+
+        $output .= $skipdest;
+
+        $this->init_block_hider_js($bc);
+        return $output;
+    }
+
+    /**
+     * Produces a header for a block
+     * 
+     * @param block_contents $bc
+     * @return string
+     */
+    protected function block_header(block_contents $bc) {
 
         $title = '';
         if ($bc->title) {
             $title = html_writer::tag('h2', $bc->title, null);
         }
 
+        $controlshtml = $this->block_controls($bc->controls);
+
+        $output = '';
         if ($title || $controlshtml) {
             $output .= html_writer::tag('div', html_writer::tag('div', html_writer::tag('div', '', array('class'=>'block_action')). $title . $controlshtml, array('class' => 'title')), array('class' => 'header'));
         }
+        return $output;
+    }
 
-        $output .= html_writer::start_tag('div', array('class' => 'content'));
-        if (!$title && !$controlshtml) {
+    /**
+     * Produces the content area for a block
+     *
+     * @param block_contents $bc
+     * @return string
+     */
+    protected function block_content(block_contents $bc) {
+        $output = html_writer::start_tag('div', array('class' => 'content'));
+        if (!$bc->title && !$this->block_controls($bc->controls)) {
             $output .= html_writer::tag('div', '', array('class'=>'block_action notitle'));
         }
         $output .= $bc->content;
+        $output .= $this->block_footer($bc);
+        $output .= html_writer::end_tag('div');
+        
+        return $output;
+    }
 
+    /**
+     * Produces the footer for a block
+     *
+     * @param block_contents $bc
+     * @return string
+     */
+    protected function block_footer(block_contents $bc) {
+        $output = '';
         if ($bc->footer) {
             $output .= html_writer::tag('div', $bc->footer, array('class' => 'footer'));
         }
+        return $output;
+    }
 
-        $output .= html_writer::end_tag('div');
-        $output .= html_writer::end_tag('div');
-
+    /**
+     * Produces the annotation for a block
+     *
+     * @param block_contents $bc
+     * @return string
+     */
+    protected function block_annotation(block_contents $bc) {
+        $output = '';
         if ($bc->annotation) {
             $output .= html_writer::tag('div', $bc->annotation, array('class' => 'blockannotation'));
         }
-        $output .= $skipdest;
-
-        $this->init_block_hider_js($bc);
         return $output;
     }
 
