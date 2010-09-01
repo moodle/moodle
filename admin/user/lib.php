c30bcaae3f1180aa853bb880bea53e7da1f1bc39
Petr Skoda <skodak@moodle.org>
2010-09-01 07:32:21 +0000

    MDL-23927 ensure CFG in scope before using, credit goes to Myles Carrick

--- admin/user/lib.php
+++ admin/user/lib.php
@@ -7,7 +7,7 @@ if (!defined('MAX_BULK_USERS')) {
 }
 
 function add_selection_all($ufiltering) {
-    global $SESSION, $DB;
+    global $SESSION, $DB, $CFG;
 
     list($sqlwhere, $params) = $ufiltering->get_sql_filter("id<>:exguest AND deleted <> 1", array('exguest'=>$CFG->siteguest));
 
