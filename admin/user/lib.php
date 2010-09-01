39bc8cd0135a0d092b0265b473883ed5f907cef1
Martin Dougiamas <martin@moodle.com>
2010-09-01 02:57:09 +0000

    bulk user editing MDL-24029 Fixed missing CFG global

--- admin/user/lib.php
+++ admin/user/lib.php
@@ -22,7 +22,7 @@ function add_selection_all($ufiltering) {
 }
 
 function get_selection_data($ufiltering) {
-    global $SESSION, $DB;
+    global $SESSION, $DB, $CFG;
 
     // get the SQL filter
     list($sqlwhere, $params) = $ufiltering->get_sql_filter("id<>:exguest AND deleted <> 1", array('exguest'=>$CFG->siteguest));
