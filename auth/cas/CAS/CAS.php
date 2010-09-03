29c3c0a42def54017b1f1f3097a626cb62ccf483
Inaki <iarenuno@eteo.mondragon.edu>
2010-09-03 16:51:11 +0000

    MDL-23864 auth/cas Coding error: Undefined index REQUEST_URI in auth/cas/CAS/CAS.php
    Patch our local copy (and record the change) while upstream fixes it (see
    https://issues.jasig.org/browse/PHPCAS-81 )

--- auth/cas/CAS/CAS.php
+++ auth/cas/CAS/CAS.php
@@ -32,7 +32,7 @@
 //
 // hack by Vangelis Haniotakis to handle the absence of $_SERVER['REQUEST_URI'] in IIS
 //
-if (!$_SERVER['REQUEST_URI']) {
+if (!isset($_SERVER['REQUEST_URI'])) {
 	$_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'] . '?' . $_SERVER['QUERY_STRING'];
 }
 
