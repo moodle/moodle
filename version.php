077127100409f1baf4311d598314ccf19bc0447a
Moodle Robot <martin@moodle.com>
2010-09-12 00:50:20 +0000

    Updated the HEAD build version to 20100912

--- version.php
+++ version.php
@@ -32,5 +32,5 @@ defined('MOODLE_INTERNAL') || die();
 $version = 2010090501;  // YYYYMMDD   = date of the last version bump
                         //         XX = daily increments
 
-$release = '2.0 Preview 4+ (Build: 20100911)';  // Human-friendly version name
+$release = '2.0 Preview 4+ (Build: 20100912)';  // Human-friendly version name
 
