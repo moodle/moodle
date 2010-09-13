b4a3af713f8b8d893e3081e96280749bc983148f
Sam Hemelryk <sam@moodle.com>
2010-09-13 09:50:15 +0000

    lesson MDL-24084 Fixed double formating of lesson page content on expanded view

--- mod/lesson/renderer.php
+++ mod/lesson/renderer.php
@@ -284,7 +284,7 @@ class mod_lesson_renderer extends plugin_renderer_base {
             $cell = new html_table_cell();
             $cell->colspan = 2;
             $cell->style = 'text-align:center';
-            $cell->text = format_text($page->contents, $page->contentsformat, $options);
+            $cell->text = $page->contents;
             $pagetable->data[] = new html_table_row(array($cell));
 
             $cell = new html_table_cell();
