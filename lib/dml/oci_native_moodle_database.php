d87b5a5da0c7873c9ad46e885d805994d6b9f0e2
Petr Skoda <skodak@moodle.org>
2010-09-14 21:13:33 +0000

    MDL-17491 fixed notice typo

--- lib/dml/oci_native_moodle_database.php
+++ lib/dml/oci_native_moodle_database.php
@@ -168,7 +168,7 @@ class oci_native_moodle_database extends moodle_database {
         unset($this->dboptions['dbsocket']);
 
         // NOTE: use of ', ", / and \ is very problematic, even native oracle tools seem to have
-        //       problems with these, so just forget them and od not report problems into tracker...
+        //       problems with these, so just forget them and do not report problems into tracker...
 
         if (empty($this->dbhost)) {
             // old style full address (TNS)
