18ac654eabebfbb5da52873b7fd97720b555ee20
Petr Skoda <skodak@moodle.org>
2010-09-18 13:11:19 +0000

    fixed incorrect page id

--- mod/lesson/pagetypes/essay.php
+++ mod/lesson/pagetypes/essay.php
@@ -218,7 +218,7 @@ class lesson_page_type_essay extends lesson_page {
     }
     public function is_unanswered($nretakes) {
         global $DB, $USER;
-        if (!$DB->count_records("lesson_attempts", array('pageid'=>$thispage->id, 'userid'=>$USER->id, 'retry'=>$nretakes))) {
+        if (!$DB->count_records("lesson_attempts", array('pageid'=>$this->properties->id, 'userid'=>$USER->id, 'retry'=>$nretakes))) {
             return true;
         }
         return false;
