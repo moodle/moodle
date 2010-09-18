9d8de3463389ba6e506c80d4325c9e064e54cfec
Petr Skoda <skodak@moodle.org>
2010-09-18 12:37:06 +0000

    unterminated JS statement

--- mod/data/data.js
+++ mod/data/data.js
@@ -51,7 +51,7 @@ M.data_filepicker.callback = function(params) {
 M.data_filepicker.init = function(Y, options) {
     options.formcallback = M.data_filepicker.callback;
     if (!M.core_filepicker.instances[options.client_id]) {
-        M.core_filepicker.init(Y, options); 
+        M.core_filepicker.init(Y, options);
     }
     Y.on('click', function(e, client_id) {
         e.preventDefault();
@@ -73,7 +73,7 @@ M.data_urlpicker = {};
 M.data_urlpicker.init = function(Y, options) {
     options.formcallback = M.data_urlpicker.callback;
     if (!M.core_filepicker.instances[options.client_id]) {
-        M.core_filepicker.init(Y, options); 
+        M.core_filepicker.init(Y, options);
     }
     Y.on('click', function(e, client_id) {
         e.preventDefault();
@@ -84,7 +84,7 @@ M.data_urlpicker.init = function(Y, options) {
 
 M.data_urlpicker.callback = function (params) {
     document.getElementById('field_url_'+params.client_id).value = params.url;
-}
+};
 
 M.data_imagepicker = {};
 
@@ -99,7 +99,7 @@ M.data_imagepicker.callback = function(params) {
 M.data_imagepicker.init = function(Y, options) {
     options.formcallback = M.data_imagepicker.callback;
     if (!M.core_filepicker.instances[options.client_id]) {
-        M.core_filepicker.init(Y, options); 
+        M.core_filepicker.init(Y, options);
     }
     Y.on('click', function(e, client_id) {
         e.preventDefault();
