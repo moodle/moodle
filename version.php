a0eb4a653aa7724f0592400e55928ba996ad524d
Martin Dougiamas <martin@moodle.com>
2010-09-21 17:24:13 +0000

    Bumping the version number now so the packages start showing it. Will actually tag it properly tomorrow.

--- version.php
+++ version.php
@@ -29,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version = 2010092000;  // YYYYMMDD   = date of the last version bump
+$version = 2010092100;  // YYYYMMDD   = date of the last version bump
                         //         XX = daily increments
 
-$release = '2.0 Preview 4+ (Build: 20100921)';  // Human-friendly version name
+$release = '2.0 RC1 (Build: 20100921)';  // Human-friendly version name
 
