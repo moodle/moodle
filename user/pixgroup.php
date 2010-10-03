2b54304b8454a0db5f250fcb240efc0af92760c6
Petr Skoda <skodak@moodle.org>
2010-10-03 18:27:21 +0000

    MDL-24500 group pix BC - just empty image to prevent broken links

---
+++ user/pixgroup.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * BC group image location
+ *
+ * @package    core
+ * @subpackage file
+ * @copyright  2010 Petr Skoda (http://skodak.org)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('NO_DEBUG_DISPLAY', true);
+define('NOMOODLECOOKIE', 1);
+
+require('../config.php');
+
+$PAGE->set_url('/user/pixgroup.php');
+$PAGE->set_context(null);
+
+redirect($OUTPUT->pix_url('spacer'));
