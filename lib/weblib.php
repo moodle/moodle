6663b9d1e3043011b43564212690d0aaf0c7bfb0
Petr Skoda <skodak@moodle.org>
2010-10-08 09:22:56 +0000

    MDL-24565 fix invalid XML unicode characters when uses KSES filtering - credit goes to Jenny Gray

--- lib/weblib.php
+++ lib/weblib.php
@@ -1298,6 +1298,7 @@ function wikify_links($string) {
 function fix_non_standard_entities($string) {
     $text = preg_replace('/&#0*([0-9]+);?/', '&#$1;', $string);
     $text = preg_replace('/&#x0*([0-9a-fA-F]+);?/', '&#x$1;', $text);
+    $text = preg_replace('/\p{Cc}/u', ' ', $text);
     return $text;
 }
 
