605d358c3525c7e9a5b029835bd52ef974987962
Petr Skoda <skodak@moodle.org>
2010-10-14 19:33:59 +0000

    MDL-14627 fixed file_browser access control

--- mod/folder/lib.php
+++ mod/folder/lib.php
@@ -244,6 +244,9 @@ function folder_get_file_info($browser, $areas, $course, $cm, $context, $fileare
 
 
     if ($filearea === 'content') {
+        if (!has_capability('mod/folder:view', $context)) {
+            return NULL;
+        }
         $fs = get_file_storage();
 
         $filepath = is_null($filepath) ? '/' : $filepath;
