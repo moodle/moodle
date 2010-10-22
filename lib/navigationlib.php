00c11469c21fc86f44adbc06786786557320b22d
Sam Hemelryk <sam@moodle.com>
2010-10-22 03:00:01 +0000

    navigation MDL-24745 Removed unneeded capability check when adding a users forum posts link to the navigation.

--- lib/navigationlib.php
+++ lib/navigationlib.php
@@ -1617,11 +1617,11 @@ class global_navigation extends navigation_node {
         }
 
         // Add nodes for forum posts and discussions if the user can view either or both
-        if ($iscurrentuser || has_capability('moodle/user:readuserposts', $usercontext)) {
-            $forumtab = $usernode->add(get_string('forumposts', 'forum'));
-            $forumtab->add(get_string('posts', 'forum'), new moodle_url('/mod/forum/user.php', $baseargs));
-            $forumtab->add(get_string('discussions', 'forum'), new moodle_url('/mod/forum/user.php', array_merge($baseargs, array('mode'=>'discussions'))));
-        }
+        // There are no capability checks here as the content of the page is based
+        // purely on the forums the current user has access too.
+        $forumtab = $usernode->add(get_string('forumposts', 'forum'));
+        $forumtab->add(get_string('posts', 'forum'), new moodle_url('/mod/forum/user.php', $baseargs));
+        $forumtab->add(get_string('discussions', 'forum'), new moodle_url('/mod/forum/user.php', array_merge($baseargs, array('mode'=>'discussions'))));
 
         // Add blog nodes
         if (!empty($CFG->bloglevel)) {
