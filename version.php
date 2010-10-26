018f4f996dbb951fa80f10da0c77d620852fc654
Andrew Davis <andrew@affinitysoftware.net>
2010-10-26 08:45:51 +0000

    message MDL-24771 moved the upgrade code

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version = 2010102600;  // YYYYMMDD   = date of the last version bump
+$version = 2010102601;  // YYYYMMDD   = date of the last version bump
                         //         XX = daily increments
 
 $release = '2.0 RC1 (Build: 20101026)';  // Human-friendly version name
