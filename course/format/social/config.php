0d804f2fbf27ffeae1a7e9aff117b41e2072c2d4
Sam Hemelryk <sam@moodle.com>
2010-10-26 05:28:57 +0000

    course-format-social MDL-24254 Removed admin from the default blocks

--- course/format/social/config.php
+++ course/format/social/config.php
@@ -6,6 +6,6 @@
 // social format.
 //
 // The default blocks layout for this course format:
-    $format['defaultblocks'] = ':search_forums,calendar_upcoming,social_activities,recent_activity,admin,course_list';
+    $format['defaultblocks'] = ':search_forums,calendar_upcoming,social_activities,recent_activity,course_list';
 
 
