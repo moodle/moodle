b1d2d048b0f5aa7da9cd06e55385fef3fee6eb7f
Sam Hemelryk <sam@moodle.com>
2010-11-03 02:26:16 +0000

    theme-anomaly MDL-25028 Removed unused function is anomaly's lib.php file.

--- theme/anomaly/lib.php
+++ theme/anomaly/lib.php
@@ -1,20 +1,8 @@
 <?php
 
 /**
- * This is a slight variatoin on the standard_renderer_factory that uses
- * custom_corners_core_renderer instead of core_renderer.
+ * Functions needed by the anomaly theme should be put here.
  *
- * This generates the slightly different HTML that the custom_corners theme expects.
- *
- * @copyright 2009 Sam Hemelryk
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * Any functions that get created here should ALWAYS contain the theme name
+ * to reduce complications for other theme designers who may be copying this theme.
  */
-function theme_enable_block_region($region) {
-    global $PAGE, $OUTPUT;
-    return (
-        empty($PAGE->layout_options['noblocks']) &&
-        array_key_exists($PAGE->pagelayout, $PAGE->theme->layouts) &&
-        in_array($region, $PAGE->theme->layouts[$PAGE->pagelayout]['regions']) &&
-        $PAGE->blocks->region_has_content($region, $OUTPUT)
-    );
-}
