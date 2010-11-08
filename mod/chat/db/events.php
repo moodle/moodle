ae671b6c67ab9fb0f1b0c5bfc4b3a9b99e1519a2
Dongsheng Cai <unoter@gmail.com>
2010-11-08 06:03:00 +0000

    MDL-25069 CHAT added events definition file

---
+++ mod/chat/db/events.php
@@ -0,0 +1,34 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * User logout event handler definition.
+ *
+ * @package   mod_chat
+ * @copyright 2010 Dongsheng Cai <dongsheng@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/* List of handlers */
+$handlers = array (
+    'user_logout' => array (
+        'handlerfile'      => '/mod/chat/lib.php',
+        'handlerfunction'  => 'chat_user_logout',
+        'schedule'         => 'instant',
+        'internal'         => 1,
+    ),
+);
