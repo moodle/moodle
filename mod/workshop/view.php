e7ff48d2849e6f30b9abb0954bc44fa72ea8352d
David Mudrak <david.mudrak@gmail.com>
2010-11-12 11:54:53 +0000

    Fixed workshop view logging
    add_to_log() used to log whole workshop name instead of just id. Thanks
    to Eloy for spotting that.

--- mod/workshop/view.php
+++ mod/workshop/view.php
@@ -46,9 +46,9 @@ if ($id) {
 
 require_login($course, true, $cm);
 require_capability('mod/workshop:view', $PAGE->context);
-add_to_log($course->id, 'workshop', 'view', 'view.php?id=' . $cm->id, $workshop->name, $cm->id);
 
 $workshop = new workshop($workshop, $cm, $course);
+$workshop->log('view');
 
 if (!is_null($editmode) && $PAGE->user_allowed_editing()) {
     $USER->editing = $editmode;
