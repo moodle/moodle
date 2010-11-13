50efefae154b926d40d22add39318261a94737b6
Martin Dougiamas <martin@moodle.com>
2010-11-13 03:47:16 +0000

    Bumping to RC2

--- version.php
+++ version.php
@@ -29,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version = 2010111002;  // YYYYMMDD   = date of the last version bump
+$version = 2010111300;  // YYYYMMDD   = date of the last version bump
                         //         XX = daily increments
 
-$release = '2.0 RC1 (Build: 20101113)';  // Human-friendly version name
+$release = '2.0 RC2 (Build: 20101113)';  // Human-friendly version name
 
