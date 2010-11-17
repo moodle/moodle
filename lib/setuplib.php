2c0b6ab223d4eed44c87de9de5dd8d01a95a35e4
Petr Skoda <skodak@moodle.org>
2010-11-17 03:52:05 +0000

    MDL-23610 increasing version number to trigger environment test

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -933,7 +933,7 @@ function get_real_size($size = 0) {
  */
 function redirect_if_major_upgrade_required() {
     global $CFG;
-    $lastmajordbchanges = 2010070300;
+    $lastmajordbchanges = 2010111700;
     if (empty($CFG->version) or (int)$CFG->version < $lastmajordbchanges or
             during_initial_install() or !empty($CFG->adminsetuppending)) {
         try {
