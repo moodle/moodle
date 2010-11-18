d744fc392a16621f68f140b875c87facd1105cd4
Tim Hunt <T.J.Hunt@open.ac.uk>
2010-11-18 22:38:50 +0000

    quiz NOBUG improve a comment.

--- mod/quiz/accessrules.php
+++ mod/quiz/accessrules.php
@@ -731,7 +731,8 @@ class time_limit_access_rule extends quiz_access_rule_base {
 }
 
 /**
- * A rule implementing the ipaddress check against the ->submet setting.
+ * A rule for ensuring that the quiz is opened in a popup, with some JavaScript
+ * to prevent copying and pasting, etc.
  */
 class securewindow_access_rule extends quiz_access_rule_base {
     /**
