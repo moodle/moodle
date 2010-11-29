af842c4309decb0b68249c77169889410c889af5
Helen Foster <helen@moodle.org>
2010-11-29 16:03:59 +0000

    MDL-21250 capital letters removal

--- mod/chat/lang/en/chat.php
+++ mod/chat/lang/en/chat.php
@@ -24,7 +24,7 @@
  */
 
 $string['ajax'] = 'Version using Ajax';
-$string['autoscroll'] = 'Auto Scroll';
+$string['autoscroll'] = 'Auto scroll';
 $string['beep'] = 'beep';
 $string['cantlogin'] = 'Could not log in to chat room!!';
 $string['configmethod'] = 'The ajax chat method provide an ajax based chat interface, it contacts server regularly for update. The normal chat method involves the clients regularly contacting the server for updates. It requires no configuration and works everywhere, but it can create a large load on the server with many chatters.  Using a server daemon requires shell access to Unix, but it results in a fast scalable chat environment.';
