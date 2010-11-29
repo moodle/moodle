33ea845ab2ae4fde22190e5a7f3e46d9b00b4e2e
Dongsheng Cai <unoter@gmail.com>
2010-11-29 08:25:03 +0000

    "MDL-25406 WIKI, fixed wid parameter"

--- mod/wiki/view.php
+++ mod/wiki/view.php
@@ -98,7 +98,7 @@ if ($id) {
 
     // Getting first page. If it does not exists, redirecting to create page
     if (!$page = wiki_get_first_page($subwiki->id, $wiki)) {
-        $params = array('swid' => $wiki->id, 'title' => $wiki->firstpagetitle);
+        $params = array('wid'=>$wiki->id, 'uid'=>$userid ,'gid'=>$currentgroup, 'title'=>$wiki->firstpagetitle);
         $url = new moodle_url('/mod/wiki/create.php', $params);
         redirect($url);
     }
