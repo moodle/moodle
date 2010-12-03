8fcc4de423d17347b2a2a21eeef56000030636a7
Martin Dougiamas <martin@moodle.com>
2010-12-03 07:11:02 +0000

    Bumping to final 1.8 release,  1.8.14

--- version.php
+++ version.php
@@ -6,13 +6,13 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-   $version = 2007021599.18;  // YYYYMMDD      = date of the 1.8 branch (don't change)
+   $version = 2007021599.19;  // YYYYMMDD      = date of the 1.8 branch (don't change)
                               //         99    = we reached a .10 release! (don't change)
                               //          .XX  = incrementing number.
 
                               // Do not use more than two decimal points as we have
                               // hit the float limit
 
-   $release = '1.8.13+ (Build: 20101203)';     // Human-friendly version name
+   $release = '1.8.14 (Build: 20101203)';     // Human-friendly version name
 
 ?>
