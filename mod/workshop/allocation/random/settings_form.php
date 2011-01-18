32ea207a4878dba3e10874cab72651c8510b5f62
David Mudrak <david@moodle.com>
2011-01-18 13:11:02 +0100

    MDL-26298 Workshop: fixed trivial typos in phpDoc blocks

--- mod/workshop/allocation/random/settings_form.php
+++ mod/workshop/allocation/random/settings_form.php
@@ -31,7 +31,7 @@ require_once($CFG->dirroot.'/lib/formslib.php');
 /**
  * Allocator settings form
  *
- * This is used by {@see workshop_random_allocator::ui()} to set up allocation paramters.
+ * This is used by {@see workshop_random_allocator::ui()} to set up allocation parameters.
  *
  * @copyright 2009 David Mudrak <david.mudrak@gmail.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
