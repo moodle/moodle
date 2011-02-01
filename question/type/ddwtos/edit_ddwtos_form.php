6c5435420f3fe97560363bbdedcbc7f829743df7
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-02-01 23:44:40 +0000

    MDL-47494 ddwtos: ddwtos and gapselect qtypes now very nearly work.
    Todo rendering questions with files, and ddwtos javascript.

--- question/type/ddwtos/edit_ddwtos_form.php
+++ question/type/ddwtos/edit_ddwtos_form.php
@@ -34,36 +34,21 @@ require_once($CFG->dirroot . '/question/type/gapselect/edit_form_base.php');
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class qtype_ddwtos_edit_form extends qtype_gapselect_edit_form_base {
-
     function qtype() {
         return 'ddwtos';
     }
 
-    protected function default_values_from_feedback_field($feedback, $key){
-        $feedback = unserialize($feedback);
-        $draggroup = $feedback->draggroup;
-        $infinite = $feedback->infinite;
-
-        $default_values = array();
-        $default_values['choices['.$key.'][draggroup]'] = $draggroup;
-        $default_values['choices['.$key.'][infinite]'] = $infinite;
-        return $default_values;
+    protected function data_preprocessing_choice($question, $answer, $key) {
+        $question = parent::data_preprocessing_choice($question, $answer, $key);
+        $options = unserialize($answer->feedback);
+        $question->choices[$key]['choicegroup'] = $options->draggroup;
+        $question->choices[$key]['infinite'] = $options->infinite;
+        return $question;
     }
 
-    protected function repeated_options(){
-        $repeatedoptions = array();
-        $repeatedoptions['draggroup']['default'] = '1';
-        $repeatedoptions['infinite']['default'] = 0;
-        return $repeatedoptions;
-    }
-
-    protected function choice_group(&$mform, $grouparray){
-        $options = array();
-        for ($i = 1; $i <= 8; $i += 1) {
-            $options[$i] = $i;
-        }
-        $grouparray[] =& $mform->createElement('select', 'draggroup', get_string('group', 'qtype_ddwtos'), $options);
-        $grouparray[] =& $mform->createElement('checkbox', 'infinite', ' ', get_string('infinite', 'qtype_ddwtos'), null, array('size'=>1, 'class'=>'tweakcss'));
+    protected function choice_group($mform) {
+        $grouparray = parent::choice_group($mform);
+        $grouparray[] = $mform->createElement('checkbox', 'infinite', ' ', get_string('infinite', 'qtype_ddwtos'), null, array('size'=>1, 'class'=>'tweakcss'));
         return $grouparray;
     }
 }
