f7d843d1679c1bb34dce2d6a55568e143aedd5f5
Petr Skoda <commits@skodak.org>
2011-02-09 12:26:58 +0100

    weekly release

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version = 2011020200.01;  // YYYYMMDD   = date of the last version bump
+$version = 2011020900;  // YYYYMMDD   = date of the last version bump
                         //         XX = daily increments
 
-$release = '2.0.1+ (Build: 20110204)';  // Human-friendly version name
+$release = '2.0.1+ (Build: 20110209)';  // Human-friendly version name
