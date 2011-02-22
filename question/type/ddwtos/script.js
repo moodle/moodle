cb30b30f97e4b77f8db85023c1367002efa85d0e
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-02-22 19:59:12 +0000

    MDL-47494 ddwtos: Add @package and GPL boiler-plate to files in /question.

--- question/type/ddwtos/script.js
+++ question/type/ddwtos/script.js
@@ -1,11 +1,27 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * JavaScript objects, functions as well as usage of some YUI library for
  * enabling drag and drop interaction for dran-anddrop words into sentences
  * (ddwtos)
  *
- * @package qtype_ddwtos
- * @copyright 2009 The Open University
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    qtype
+ * @subpackage ddwtos
+ * @copyright  2009 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 //global variables
