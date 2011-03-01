2a921ee985f8040991e7c1282c333c50c6179b74
Petr Skoda <commits@skodak.org>
2011-03-01 12:11:31 +0100

    MDL-26047 fix string letter case and whitespace

--- theme/nimble/lang/en/theme_nimble.php
+++ theme/nimble/lang/en/theme_nimble.php
@@ -29,28 +29,28 @@ $string['region-side-pre'] = 'Left';
 $string['choosereadme'] = '
 <div class="clearfix">
 	<div class="theme_screenshot">
-		
+
 		<h2>Nimble</h2>
 		<img src="nimble/pix/screenshot.png" />
-		
+
 		<h3>Theme Discussion Forum:</h3>
 		<p><a href="http://moodle.org/mod/forum/view.php?id=46">http://moodle.org/mod/forum/view.php?id=46</a></p>
-		
+
 		<h3>Theme Credits</h3>
 		<p><a href="http://docs.moodle.org/en/Theme_credits">http://docs.moodle.org/en/Theme_credits</a></p>
-		
+
 		<h3>Theme Documentation:</h3>
 		<p><a href="http://docs.moodle.org/en/Themes">http://docs.moodle.org/en/Themes</a></p><h3>Report a bug:</h3><p><a href="http://tracker.moodle.org">http://tracker.moodle.org</a></p>
 	</div>
 
 	<div class="theme_description">
-		
+
 		<h2>About</h2>
 		<p>Nimble is a two-column, fluid-width theme coded for Moodle 2.0. It makes use of custom menus that appear above the site title on every page. It also includes a basic settings page allowing you to change link color, add a tagline next the site name on the front page, and add text to the footer.</p>
-		
+
 		<h2>Parents</h2>
 		<p>This theme is built upon both Base and Canvas, two parent themes included in the Moodle core. If you wish to modify aspects of this theme beyond the settings offered, we advise creating a new  theme using this theme, Base and Canvas all as parents so updates to any of those themes in the core will find their way into your new theme.</p>
-		
+
 		<h2>Credits</h2>
 		<p>This design was created for Moodle by Patrick Malley (patrick@newschoollearning.com) with help from John Stabinger (john@newschoollearning.com).
 		<h2>License</h2>
@@ -61,15 +61,15 @@ $string['choosereadme'] = '
 $string['tagline'] = 'Tagline';
 $string['taglinedesc'] = 'Set a tagline to appear under the site name on the front page.';
 
-$string['footerline'] = 'footerline';
+$string['footerline'] = 'Footerline';
 $string['footerlinedesc'] = 'Add some text (copyright info, etc.) to the footer.';
 
-$string['backgroundcolor'] = 'Header Background';
+$string['backgroundcolor'] = 'Header background';
 $string['backgroundcolordesc'] = 'Change the background color of the header.';
 
-$string['linkcolor'] = 'Link Color';
+$string['linkcolor'] = 'Link color';
 $string['linkcolordesc'] = 'Set the color of links in the theme, use html hex code.';
 
-$string['linkhover'] = 'Menu Hover Color';
+$string['linkhover'] = 'Menu hover color';
 $string['linkhoverdesc'] = 'Set the color of the menu on hover in the theme, use html hex code.';
 
