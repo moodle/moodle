209442ce2445c973c8589fdf564a7bea34c47900
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-03-05 19:51:38 +0000

    MDL-26517 HTML tags visible in the response history table and quiz reports.

--- question/type/questiontype.php
+++ question/type/questiontype.php
@@ -1348,13 +1348,15 @@ class default_questiontype {
         }
         return $toreturn;
     }
+
     /**
      * @param string response is a response.
      * @return formatted response
      */
-    function format_response($response, $format){
-        return s($response);
+    function format_response($response, $format) {
+        return $this->format_text($response, $format);
     }
+
     /**
     * Renders the question for printing and returns the LaTeX source produced
     *
