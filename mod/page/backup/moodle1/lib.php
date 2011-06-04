48fcecffb6468350ae6562a2a5d8717b8e24f4f0
David Mudrak <david@moodle.com>
2011-06-04 14:43:04 +0200

    MDL-27448 Page module conversion fixed and improved
    The embedded files are now converted into the proper file area on the
    conversion. The previous usage of $CFG->wwwroot/file.php was wrong for two
    reasons: (1) the moodle.xml does not (should not) contain these links but
    $@FILEPHP@$ placeholder and (2) even if it did, we could not compare to
    our wwwroot but to the original wwwroot.
    
    Also fixed the actual list of fields being written into page.xml - the
    previous version included legacy fields (like type, reference and
    friends).

--- mod/page/backup/moodle1/lib.php
+++ mod/page/backup/moodle1/lib.php
@@ -29,27 +29,16 @@ defined('MOODLE_INTERNAL') || die();
 /**
  * Page conversion handler. This resource handler is called by moodle1_mod_resource_handler
  */
-class moodle1_mod_page_handler extends moodle1_mod_handler {
-    /** @var array in-memory cache for the course module information  */
-    protected $currentcminfo = null;
+class moodle1_mod_page_handler extends moodle1_resource_successor_handler {
+
     /** @var moodle1_file_manager instance */
     protected $fileman = null;
-
-    /**
-     * Declare the paths in moodle.xml we are able to convert
-     *
-     * @return array of {@link convert_path} instances
-     */
-    public function get_paths() {
-        return array();
-    }
 
     /**
      * Converts /MOODLE_BACKUP/COURSE/MODULES/MOD/RESOURCE data
      * Called by moodle1_mod_resource_handler::process_resource()
      */
     public function process_resource($data) {
-        global $CFG;
 
         // get the course module id and context id
         $instanceid = $data['id'];
@@ -57,32 +46,36 @@ class moodle1_mod_page_handler extends moodle1_mod_handler {
         $moduleid   = $cminfo['id'];
         $contextid  = $this->converter->get_contextid(CONTEXT_MODULE, $moduleid);
 
+        // just in case there is some rubbish
         $data['contentformat'] = (int)$data['reference'];
         if ($data['contentformat'] < 0 || $data['contentformat'] > 4) {
             $data['contentformat'] = FORMAT_MOODLE;
         }
 
-        $data['content']      = $data['alltext'];
-        $data['revision']     = 1;
-        $data['timemodified'] = time();
+        // we can't use recipes in resource successors so add and rename fields manually
+        $data['content']         = $data['alltext'];
+        $data['revision']        = 1;
+        $data['timemodified']    = time();
+        $data['legacyfiles']     = RESOURCELIB_LEGACYFILES_ACTIVE;
+        $data['legacyfileslast'] = null;
 
-        // convert links to old course files
-        $originalcourseinfo = $this->converter->get_stash('original_course_info');
-        if (!empty($originalcourseinfo) && array_key_exists('original_course_id', $originalcourseinfo)) {
-            $courseid = $originalcourseinfo['original_course_id'];
-
-            $usedfiles = array("$CFG->wwwroot/file.php/$courseid/", "$CFG->wwwroot/file.php?file=/$courseid/");
-            $data['content'] = str_ireplace($usedfiles, '@@PLUGINFILE@@/', $data['content']);
-            if (strpos($data['content'], '@@PLUGINFILE@@/') === false) {
-                $data['legacyfiles'] = RESOURCELIB_LEGACYFILES_NO;
-            } else {
-                $data['legacyfiles'] = RESOURCELIB_LEGACYFILES_ACTIVE;
+        // convert embedded course files
+        if (is_null($this->fileman)) {
+            $this->fileman = $this->converter->get_file_manager(null, 'mod_page', 'content');
+        }
+        $this->fileman->reset_fileids();
+        $files = moodle1_converter::find_referenced_files($data['content']);
+        if (!empty($files)) {
+            $this->fileman->contextid = $contextid;
+            $this->fileman->itemid = $instanceid;
+            foreach ($files as $file) {
+                $this->fileman->migrate_file('course_files'.$file);
             }
-        } else {
-            $data['legacyfiles'] = RESOURCELIB_LEGACYFILES_NO;
+            $data['content'] = moodle1_converter::rewrite_filephp_usage($data['content'], $files);
         }
 
-        $options = array('printheading'=>0, 'printintro'=>0);
+        // populate display and displayoptions fields
+        $options = array('printheading' => 0, 'printintro' => 0);
         if ($data['popup']) {
             $data['display'] = RESOURCELIB_DISPLAY_POPUP;
             if ($data['popup']) {
@@ -100,26 +93,29 @@ class moodle1_mod_page_handler extends moodle1_mod_handler {
         }
         $data['displayoptions'] = serialize($options);
 
-        // prepare file manager for migrating the folder
-        $this->fileman = $this->converter->get_file_manager($contextid, 'mod_page', 'content', 0);
-        $this->fileman->migrate_directory('moddata/page/'.$data['id']);
+        unset($data['alltext']);
+        unset($data['type']);
+        unset($data['reference']);
+        unset($data['popup']);
+        unset($data['options']);
 
-        // we now have all information needed to start writing into the file
+        // write page.xml
         $this->open_xml_writer("activities/page_{$moduleid}/page.xml");
         $this->xmlwriter->begin_tag('activity', array('id' => $instanceid, 'moduleid' => $moduleid,
             'modulename' => 'page', 'contextid' => $contextid));
-        $this->xmlwriter->begin_tag('page', array('id' => $instanceid));
-
-        unset($data['id']); // we already write it as attribute, do not repeat it as child element
-        foreach ($data as $field => $value) {
-            $this->xmlwriter->full_tag($field, $value);
-        }
-    }
-
-    public function on_resource_end($data) {
-        // close page.xml
-        $this->xmlwriter->end_tag('page');
+        $this->write_xml('page', $data, array('/page/id'));
         $this->xmlwriter->end_tag('activity');
+        $this->close_xml_writer();
+
+        // write inforef.xml for migrated resource file.
+        $this->open_xml_writer("activities/page_{$moduleid}/inforef.xml");
+        $this->xmlwriter->begin_tag('inforef');
+        $this->xmlwriter->begin_tag('fileref');
+        foreach ($this->fileman->get_fileids() as $fileid) {
+            $this->write_xml('file', array('id' => $fileid));
+        }
+        $this->xmlwriter->end_tag('fileref');
+        $this->xmlwriter->end_tag('inforef');
         $this->close_xml_writer();
     }
 }
