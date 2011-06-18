88ef9d0b2b6c4c85f3610805963455844bfd0c55
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-06-18 13:29:39 +0100

    MDL-27916 Question state was described wrongly in adaptive and immediate feedback behaviours.

--- question/behaviour/adaptive/lang/en/qbehaviour_adaptive.php
+++ question/behaviour/adaptive/lang/en/qbehaviour_adaptive.php
@@ -26,4 +26,5 @@
 $string['gradingdetails'] = 'Marks for this submission: {$a->raw}/{$a->max}.';
 $string['gradingdetailsadjustment'] = 'With previous penalties this gives <strong>{$a->cur}/{$a->max}</strong>.';
 $string['gradingdetailspenalty'] = 'This submission attracted a penalty of {$a}.';
+$string['notcomplete'] = 'Not complete';
 $string['pluginname'] = 'Adaptive mode';
--- question/behaviour/immediatefeedback/lang/en/qbehaviour_immediatefeedback.php
+++ question/behaviour/immediatefeedback/lang/en/qbehaviour_immediatefeedback.php
@@ -23,4 +23,5 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['notcomplete'] = 'Not complete';
 $string['pluginname'] = 'Immediate feedback';
