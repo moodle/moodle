2e214b26f381cb6c8b0b208f8ed0fea478ab882d
Dan Poltawski <dan.poltawski@luns.net.uk>
2011-07-03 22:50:44 +0100

    MDL-26626 course competion block - fix hardcoded string

--- blocks/completionstatus/lang/en/block_completionstatus.php
+++ blocks/completionstatus/lang/en/block_completionstatus.php
@@ -3,5 +3,6 @@
 $string['completionprogressdetails'] = 'Completion progress details';
 $string['completionstatus'] = 'Course completion status';
 $string['criteriagroup'] = 'Criteria group';
+$string['firstofsecond'] = '{$a->first} of {$a->second}';
 $string['pluginname'] = 'Course completion status';
 $string['requirement'] = 'Requirement';
