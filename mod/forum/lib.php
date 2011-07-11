9bb6e09468f131d4c27e5af7489c46c6c2e5a8dd
Rossiani Wijaya <rwijaya@moodle.com>
2011-07-11 17:00:33 +0800

    MDL-27741 forum subscribe link: Removed 'no' string in social course format to display a subscribe link if user is not enrolled

--- mod/forum/lib.php
+++ mod/forum/lib.php
@@ -4605,7 +4605,7 @@ function forum_get_subscribe_link($forum, $context, $messages = array(), $cantac
         return $messages['cantaccessgroup'];
     } else {
         if (!is_enrolled($context, $USER, '', true)) {
-            return get_string('no');
+            return '';
         }
         if (is_null($subscribed_forums)) {
             $subscribed = forum_is_subscribed($USER->id, $forum);
