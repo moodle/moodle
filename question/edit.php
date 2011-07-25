26a4f319a8f057845cca3082abc7022bcadd8348
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-07-25 08:50:06 +0100

    MDL-28437 Question bank pages should all use General type: admin.

--- question/edit.php
+++ question/edit.php
@@ -50,7 +50,6 @@ if (($cmid = optional_param('cmid', 0, PARAM_INT)) !== 0) {
     $url->param('cmid', $cmid);
 }
 $PAGE->set_url($url);
-$PAGE->set_pagelayout('standard');
 
 list($thispageurl, $contexts, $cmid, $cm, $module, $pagevars) =
         question_edit_setup('questions', '/question/edit.php');
