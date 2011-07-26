6f0579691995fddc476c6788736ea86e99e85230
Petr Skoda <commits@skodak.org>
2011-07-26 11:13:59 +0200

    weekly release 2.2dev

--- version.php
+++ version.php
@@ -31,10 +31,10 @@ defined('MOODLE_INTERNAL') || die();
 
 
 
-$version  = 2011071900.00;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2011072600.00;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
-$release  = '2.2dev (Build: 20110719)'; // Human-friendly version name
+$release  = '2.2dev (Build: 20110726)'; // Human-friendly version name
 
 $maturity = MATURITY_ALPHA;             // this version's maturity level
