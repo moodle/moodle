040aa0880b254f5a8eabb7931afcd76b04ec9d5d
Dan Marsden <dan@danmarsden.com>
2011-08-01 10:27:39 +1200

    MDL-28548 allow backup to contain info from multiple plagiarism plugins.

--- backup/moodle2/backup_stepslib.php
+++ backup/moodle2/backup_stepslib.php
@@ -320,7 +320,7 @@ class backup_module_structure_step extends backup_structure_step {
         $this->add_plugin_structure('format', $module, false);
 
         // attach plagiarism plugin structure to $module element, only one allowed
-        $this->add_plugin_structure('plagiarism', $module, false);
+        $this->add_plugin_structure('plagiarism', $module, true);
 
         // Define the tree
         $module->add_child($availinfo);
@@ -422,7 +422,7 @@ class backup_course_structure_step extends backup_structure_step {
         $this->add_plugin_structure('coursereport', $course, true);
 
         // attach plagiarism plugin structure to $course element, only one allowed
-        $this->add_plugin_structure('plagiarism', $course, false);
+        $this->add_plugin_structure('plagiarism', $course, true);
 
         // Build the tree
 
