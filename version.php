1f6c3ff2f4859a0687f352d10b0005dda54f3757
Sam Hemelryk <sam@moodle.com>
2011-08-01 16:53:14 +0800

    Moodle 1.9.13 release

--- version.php
+++ version.php
@@ -6,10 +6,10 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2007101591.03; // YYYYMMDD      = date of the 1.9 branch (don't change)
+    $version = 2007101591.04; // YYYYMMDD      = date of the 1.9 branch (don't change)
                               //         X     = release number 1.9.[0,1,2,3,4,5...]
                               //          Y.YY = micro-increments between releases
 
-    $release = '1.9.12+ (Build: 20110726)';     // Human-friendly version name
+    $release = '1.9.13 (Build: 20110801)';     // Human-friendly version name
 
 ?>
