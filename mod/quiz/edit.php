3e1d47f2cc91887b8a0fb32e470b0620d7f41238
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-08-04 13:53:02 +0100

    MDL-28604 Question bank display forgets sort order

--- mod/quiz/edit.php
+++ mod/quiz/edit.php
@@ -453,8 +453,7 @@ echo '<div id="module" class="module">';
 echo '<div class="bd">';
 $questionbank->display('editq',
         $pagevars['qpage'],
-        $pagevars['qperpage'], $pagevars['qsortorder'],
-        $pagevars['qsortorderdecoded'],
+        $pagevars['qperpage'],
         $pagevars['cat'], $pagevars['recurse'], $pagevars['showhidden'],
         $pagevars['showquestiontext']);
 echo '</div>';
