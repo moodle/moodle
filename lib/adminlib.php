1824a69d7af76fc014e3352744d94a81c12c1593
David Mudrak <david@moodle.com>
2011-08-16 14:11:02 +0200

    MDL-28723 Make sure that the arrays used by print_plugin_tables() are correctly initialized
    This covers a very rare and unexpected case when no mods or no blocks
    are installed. Credit goes to Aparup Banerjee for spotting this.

--- lib/adminlib.php
+++ lib/adminlib.php
@@ -4942,13 +4942,17 @@ function print_plugin_tables() {
     $installed_mods = get_records_list('modules', '', '', '', 'name');
     $installed_blocks = get_records_list('block', '', '', '', 'name');
 
+    $plugins_installed['mod'] = array();
     foreach($installed_mods as $mod) {
         $plugins_installed['mod'][] = $mod->name;
     }
 
+    $plugins_installed['blocks'] = array();
     foreach($installed_blocks as $block) {
         $plugins_installed['blocks'][] = $block->name;
     }
+
+    $plugins_installed['filter'] = array();
 
     $plugins_ondisk = array();
     $plugins_ondisk['mod'] = get_list_of_plugins('mod', 'db');
