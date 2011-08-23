e832010c51ab7238b04e4b0f93ee5179cb5c5968
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-08-23 17:56:49 +0200

    MDL-28690 metacourses - add some more alias prefixes to be 100% in the safe side

--- admin/cron.php
+++ admin/cron.php
@@ -245,8 +245,8 @@
                                         FROM {$CFG->prefix}user_lastaccess la
                                         JOIN {$CFG->prefix}course c
                                           ON c.id = la.courseid
-                                       WHERE courseid != ".SITEID."
-                                         AND timeaccess < $cuttime
+                                       WHERE la.courseid != ".SITEID."
+                                         AND la.timeaccess < $cuttime
                                          AND c.metacourse = 0 ");
             while ($assign = rs_fetch_next_record($rs)) {
                 if ($context = get_context_instance(CONTEXT_COURSE, $assign->courseid)) {
