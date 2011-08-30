9ec4220c84f5d4bf5b0dbfe2c6b88b4abfcd7711
Sam Hemelryk <sam@moodle.com>
2011-08-30 16:43:36 +1200

    MDL-29188 multilang Reviewed the uses of section name and ensured things were being formatted consistently

--- course/format/topics/format.php
+++ course/format/topics/format.php
@@ -96,7 +96,7 @@ if ($thissection->summary or $thissection->sequence or $PAGE->user_is_editing())
     echo '<div class="right side" >&nbsp;</div>';
     echo '<div class="content">';
     if (!is_null($thissection->name)) {
-        echo $OUTPUT->heading($thissection->name, 3, 'sectionname');
+        echo $OUTPUT->heading(format_string($thissection->name, true, array('context' => $context)), 3, 'sectionname');
     }
     echo '<div class="summary">';
 
@@ -219,7 +219,7 @@ while ($section <= $course->numsections) {
             echo get_string('notavailable');
         } else {
             if (!is_null($thissection->name)) {
-                echo $OUTPUT->heading($thissection->name, 3, 'sectionname');
+                echo $OUTPUT->heading(format_string($thissection->name, true, array('context' => $context)), 3, 'sectionname');
             }
             echo '<div class="summary">';
             if ($thissection->summary) {
