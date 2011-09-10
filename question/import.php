b6b5023144f748c515fd5e2fedd38ce3e6cf5608
Petr Skoda <commits@skodak.org>
2011-09-10 10:55:32 +0200

    MDL-28701 some more tempdirs updated

--- question/import.php
+++ question/import.php
@@ -83,7 +83,7 @@ if ($form = $import_form->get_data()) {
     // or one from the filesarea.
     $realfilename = $import_form->get_new_filename('newfile');
 
-    $importfile = "{$CFG->dataroot}/temp/questionimport/{$realfilename}";
+    $importfile = "{$CFG->tempdir}/questionimport/{$realfilename}";
     make_temp_directory('questionimport');
     if (!$result = $import_form->save_file('newfile', $importfile, true)) {
         throw new moodle_exception('uploadproblem');
