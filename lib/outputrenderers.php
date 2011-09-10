1ad8143a30ce8bd8ac750a35ba12851c465f3e6d
Petr Skoda <commits@skodak.org>
2011-09-11 00:07:28 +0200

    MDL-28595 explain the need to install into new db if something fails during the initial install

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -2066,6 +2066,10 @@ EOD;
         $message = '<p class="errormessage">' . $message . '</p>'.
                 '<p class="errorcode"><a href="' . $moreinfourl . '">' .
                 get_string('moreinformation') . '</a></p>';
+        if (empty($CFG->rolesactive)) {
+            $message .= '<p class="errormessage">' . get_string('installproblem', 'error') . '</p>';
+            //It is usually not possible to recover from errors triggered during installation, you may need to create a new database or use a different database prefix for new installation.
+        }
         $output .= $this->box($message, 'errorbox');
 
         if (debugging('', DEBUG_DEVELOPER)) {
