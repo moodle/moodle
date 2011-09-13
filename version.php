3815ecf471f92059db29e21809a3c8c918c25c9b
Henning Bostelmann <henning.bostelmann@york.ac.uk>
2011-09-13 01:11:19 +0100

    MDL-29350 Prevent duplication of groupings when copying activities
    This patch also includes a database upgrade to correct data produced as a
    result of this bug.

--- version.php
+++ version.php
@@ -30,8 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-
-$version  = 2011070102.07;              // 20110701      = branching date YYYYMMDD - do not modify!
+$version  = 2011070102.08;              // 20110701      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
