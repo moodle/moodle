ce6d6b175b0147862dfe9ab611d793cb930e26d3
Jamie Pratt <me@jamiep.org>
2011-09-15 12:11:45 +0700

    MDL-47494 ddimageortext: NOBUG optional_param_array is not available in MOODLE 2.1 but must be used in MOODLE 2.2 on.

--- question/type/ddimageortext/edit_ddimageortext_form.php
+++ question/type/ddimageortext/edit_ddimageortext_form.php
@@ -259,7 +259,7 @@ class qtype_ddimageortext_edit_form extends question_edit_form {
 
         //initialise file picker for dragimages
         list(, $imagerepeats) = $this->get_drag_image_repeats();
-        $draftitemids = optional_param_array('dragitem', array(), PARAM_INT);
+        $draftitemids = optional_param('dragitem', array(), PARAM_INT);
         for ($imageindex = 0; $imageindex < $imagerepeats; $imageindex++) {
             $draftitemid = isset($draftitemids[$imageindex]) ? $draftitemids[$imageindex] :0;
             //numbers not allowed in filearea name
