fdd6b2fa57ca34ca554626db292b85b1f3f155ca
Andreas Grabs <moodle@grabs-edv.de>
2011-10-03 18:46:22 +0200

    MDL-29621 - XHTML not welformed when creating Feedback questions

--- mod/feedback/item/numeric/numeric_form.php
+++ mod/feedback/item/numeric/numeric_form.php
@@ -17,12 +17,24 @@ class feedback_numeric_form extends feedback_item_form {
         $mform->addElement('header', 'general', get_string($this->type, 'feedback'));
         $mform->addElement('checkbox', 'required', get_string('required', 'feedback'));
 
-        $mform->addElement('text', 'name', get_string('item_name', 'feedback'), array('size="'.FEEDBACK_ITEM_NAME_TEXTBOX_SIZE.'"','maxlength="255"'));
-        $mform->addElement('text', 'label', get_string('item_label', 'feedback'), array('size="'.FEEDBACK_ITEM_LABEL_TEXTBOX_SIZE.'"','maxlength="255"'));
+        $mform->addElement('text',
+                            'name',
+                            get_string('item_name', 'feedback'),
+                            array('size'=>FEEDBACK_ITEM_NAME_TEXTBOX_SIZE, 'maxlength'=>255));
+        $mform->addElement('text',
+                            'label',
+                            get_string('item_label', 'feedback'),
+                            array('size'=>FEEDBACK_ITEM_LABEL_TEXTBOX_SIZE, 'maxlength'=>255));
 
-        $mform->addElement('text', 'rangefrom', get_string('numeric_range_from', 'feedback'), array('size="10"','maxlength="10"'));
+        $mform->addElement('text',
+                            'rangefrom',
+                            get_string('numeric_range_from', 'feedback'),
+                            array('size'=>10, 'maxlength'=>10));
 
-        $mform->addElement('text', 'rangeto', get_string('numeric_range_to', 'feedback'), array('size="10"','maxlength="10"'));
+        $mform->addElement('text',
+                            'rangeto',
+                            get_string('numeric_range_to', 'feedback'),
+                            array('size'=>10,'maxlength'=>10));
 
         parent::definition();
         $this->set_data($item);
