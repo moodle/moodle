b2617f0b993175ec7d7560faceca416311031205
Petr Skoda <commits@skodak.org>
2011-10-08 10:00:22 +0200

    future versions of mod_book are going to require at least Moodle 2.1.0

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $module->version   = 2011032000; // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2010120700; // Requires this Moodle version
+$module->requires  = 2011070101; // Requires this Moodle version
 $module->cron      = 0;          // Period for cron to check this module (secs)
 $module->component = 'mod_book'; // Full name of the plugin (used for diagnostics)
 
