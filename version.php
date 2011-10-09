fe9820e8a76a4811660b4a829b3ad281c557d3e3
Petr Skoda <commits@skodak.org>
2011-10-09 16:27:42 +0200

    fix required Moodle version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $module->version   = 2011100901; // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2011070100; // Requires this Moodle version
+$module->requires  = 2011070101; // Requires this Moodle version
 $module->cron      = 0;          // Period for cron to check this module (secs)
 $module->component = 'mod_book'; // Full name of the plugin (used for diagnostics)
 
