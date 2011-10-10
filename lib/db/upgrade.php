71e1a1f820100d36ef3d9d475f405e3e5b31626b
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-10-10 23:34:27 +0200

    MDL-20306 Add course_category idnumber field: version bump and fix missing add_field() call

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -6778,16 +6778,18 @@ FROM
         upgrade_main_savepoint(true, 2011092800.03);
     }
 
-    if ($oldversion < 2011100700.01) {
+    if ($oldversion < 2011100700.02) {
         // Define field idnumber to be added to course_categories
         $table = new xmldb_table('course_categories');
         $field = new xmldb_field('idnumber', XMLDB_TYPE_CHAR, '100', null, null, null, null, 'name');
 
         // Conditionally launch add field idnumber
         if (!$dbman->field_exists($table,$field)) {
+            $dbman->add_field($table, $field);
+        }
 
         // Main savepoint reached
-        upgrade_main_savepoint(true, 2011100700.01);
+        upgrade_main_savepoint(true, 2011100700.02);
     }
 
     return true;
--- version.php
+++ version.php
@@ -31,7 +31,7 @@ defined('MOODLE_INTERNAL') || die();
 
 
 
-$version  = 2011100700.01;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2011100700.02;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
