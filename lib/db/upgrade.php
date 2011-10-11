6282381d05a62b032c20d25b7b29d8ead41cfbbd
sam marshall <s.marshall@open.ac.uk>
2011-10-11 14:50:01 +0100

    MDL-27242 Conditional availability dates should support time as well as day
    Change also adds minor feature to date-time selector so you can specify the time it uses as default when the value is 0 (disabled).
    
    Credit: This feature was developed collaboratively by Charles Fulton, Neill Magill, and me.

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -6792,6 +6792,24 @@ FROM
         upgrade_main_savepoint(true, 2011100700.02);
     }
 
+    if ($oldversion < 2011101200.01) {
+        // The conditional availability date system used to rely on dates being
+        // set to 23:59:59 for the end date, but now that exact times are
+        // supported, it uses midnight on the following day.
+        
+        // The query is restricted on 'time mod 10 = 9' in order that
+        // it is safe to run this upgrade twice if something goes wrong.
+        $DB->execute('UPDATE {course_modules} SET availableuntil = availableuntil + 1 ' .
+                'WHERE availableuntil > 0 AND ' . $DB->sql_modulo('availableuntil', 10) . ' = 9');
+        
+        // Because availableuntil is stored in modinfo, we need to clear modinfo
+        // for all courses.
+        rebuild_course_cache(0, true);
+
+        // Main savepoint reached
+        upgrade_main_savepoint(true, 2011101200.01);
+    }
+    
     return true;
 }
 
