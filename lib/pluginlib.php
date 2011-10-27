9c867cfef845c33175305102e21a3c74d7f8ccc6
Iñaki Arenaza <iarenaza@mondragon.edu>
2011-10-27 12:09:23 +0200

    MDL-29667 Plugins overview shows incorrect availability for auth & enrol plugins
    We store the enabled plugins as array values but later look them up as array
    keys. Just flip the array when we build it.

--- lib/pluginlib.php
+++ lib/pluginlib.php
@@ -1462,7 +1462,7 @@ class plugintype_auth extends plugintype_base implements plugin_information {
         }
 
         if (is_null($enabled)) {
-            $enabled = explode(',', $CFG->auth);
+            $enabled = array_flip(explode(',', $CFG->auth));
         }
 
         return isset($enabled[$this->name]);
@@ -1499,7 +1499,7 @@ class plugintype_enrol extends plugintype_base implements plugin_information {
         static $enabled = null;
 
         if (is_null($enabled)) {
-            $enabled = explode(',', $CFG->enrol_plugins_enabled);
+            $enabled = array_flip(explode(',', $CFG->enrol_plugins_enabled));
         }
 
         return isset($enabled[$this->name]);
