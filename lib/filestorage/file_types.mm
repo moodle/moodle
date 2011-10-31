5fd928d633360bc9d77ccd35e126b01bff5100d8
Rossiani Wijaya <rwijaya@moodle.com>
2011-10-31 15:15:32 +0800

    MDL-29955: filestorage/file_types: add support for .f4v, .m4v and .webm

--- lib/filestorage/file_types.mm
+++ lib/filestorage/file_types.mm
@@ -36,6 +36,9 @@
 <node ID="_Freemind_Link_810187762" TEXT=".wmv"/>
 <node ID="_Freemind_Link_763870397" TEXT=".asf"/>
 <node TEXT=".ogv"/>
+<node TEXT=".f4v"/>
+<node TEXT=".m4v"/>
+<node TEXT=".webm"/>
 </node>
 <node ID="_Freemind_Link_1019644700" TEXT="non_web_video">
 <node ID="_Freemind_Link_190589975" TEXT=".avi"/>
