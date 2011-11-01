313e048981abb5a55b06980f13285565a0a71f2f
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-11-01 20:09:10 +0100

    MDL-29283 import TCPDF to 5.9.133 - clean whitespace

--- lib/pdflib.php
+++ lib/pdflib.php
@@ -124,15 +124,15 @@ class pdf extends TCPDF {
     }
 
     /**
-   	 * Send the document to a given destination: string, local file or browser.
-   	 * In the last case, the plug-in may be used (if present) or a download ("Save as" dialog box) may be forced.<br />
-   	 * The method first calls Close() if necessary to terminate the document.
-   	 * @param $name (string) The name of the file when saved. Note that special characters are removed and blanks characters are replaced with the underscore character.
-   	 * @param $dest (string) Destination where to send the document. It can take one of the following values:<ul><li>I: send the file inline to the browser (default). The plug-in is used if available. The name given by name is used when one selects the "Save as" option on the link generating the PDF.</li><li>D: send to the browser and force a file download with the name given by name.</li><li>F: save to a local server file with the name given by name.</li><li>S: return the document as a string (name is ignored).</li><li>FI: equivalent to F + I option</li><li>FD: equivalent to F + D option</li><li>E: return the document as base64 mime multi-part email attachment (RFC 2045)</li></ul>
-   	 * @public
-   	 * @since 1.0
-   	 * @see Close()
-   	 */
+     * Send the document to a given destination: string, local file or browser.
+     * In the last case, the plug-in may be used (if present) or a download ("Save as" dialog box) may be forced.<br />
+     * The method first calls Close() if necessary to terminate the document.
+     * @param $name (string) The name of the file when saved. Note that special characters are removed and blanks characters are replaced with the underscore character.
+     * @param $dest (string) Destination where to send the document. It can take one of the following values:<ul><li>I: send the file inline to the browser (default). The plug-in is used if available. The name given by name is used when one selects the "Save as" option on the link generating the PDF.</li><li>D: send to the browser and force a file download with the name given by name.</li><li>F: save to a local server file with the name given by name.</li><li>S: return the document as a string (name is ignored).</li><li>FI: equivalent to F + I option</li><li>FD: equivalent to F + D option</li><li>E: return the document as base64 mime multi-part email attachment (RFC 2045)</li></ul>
+     * @public
+     * @since 1.0
+     * @see Close()
+     */
     public function Output($name='doc.pdf', $dest='I') {
         $olddebug = error_reporting(0);
         $result  = parent::output($name, $dest);
