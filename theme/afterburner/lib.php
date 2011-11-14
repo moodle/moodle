f1c0d7bdcee9906efcf290c94e7ae243fb066bc8
Mary Evans <lazydaisy@visible-expression.co.uk>
2011-11-14 01:29:21 +0000

    MDL-30243 Add custom settings to theme_afterburner

---
+++ theme/afterburner/lib.php
@@ -0,0 +1,48 @@
+<?php
+
+function afterburner_process_css($css, $theme) {
+
+	// Set the background image for the logo
+	if (!empty($theme->settings->logo)) {
+		$logo = $theme->settings->logo;
+	} else {
+		$logo = null;
+	}
+	$css = afterburner_set_logo($css, $logo);
+
+    // Set custom CSS
+    if (!empty($theme->settings->customcss)) {
+        $customcss = $theme->settings->customcss;
+    } else {
+        $customcss = null;
+    }
+    $css = afterburner_set_customcss($css, $customcss);
+
+    return $css;
+}
+
+function afterburner_set_logo($css, $logo) {
+	global $OUTPUT;
+	$tag = '[[setting:logo]]';
+	$replacement = $logo;
+	if (is_null($replacement)) {
+ 		$replacement = $OUTPUT->pix_url('images/logo','theme');
+ 	}
+
+	$css = str_replace($tag, $replacement, $css);
+
+	return $css;
+
+}
+
+function afterburner_set_customcss($css, $customcss) {
+    $tag = '[[setting:customcss]]';
+    $replacement = $customcss;
+    if (is_null($replacement)) {
+        $replacement = '';
+    }
+
+	$css = str_replace($tag, $replacement, $css);
+
+	return $css;
+}
