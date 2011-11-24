4d33fbef43290ae7ab1d9d886f7336bed7b9673e
Mary Evans <lazydaisy@visible-expression.co.uk>
2011-11-24 09:31:32 +0000

    MDL-30243 - corrected admin tree to add custom css settings

--- theme/afterburner/settings.php
+++ theme/afterburner/settings.php
@@ -4,27 +4,28 @@ defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
 
-// Logo file setting
-$name = 'theme_afterburner/logo';
-$title = get_string('logo','theme_afterburner');
-$description = get_string('logodesc', 'theme_afterburner');
-$default = '';
-$setting = new admin_setting_configtext($name, $title, $description, $default, PARAM_URL);
-$settings->add($setting);
+	// Logo file setting
+	$name = 'theme_afterburner/logo';
+	$title = get_string('logo','theme_afterburner');
+	$description = get_string('logodesc', 'theme_afterburner');
+	$default = '';
+	$setting = new admin_setting_configtext($name, $title, $description, $default, PARAM_URL);
+	$settings->add($setting);
 
-// Foot note setting
-$name = 'theme_afterburner/footnote';
-$title = get_string('footnote','theme_afterburner');
-$description = get_string('footnotedesc', 'theme_afterburner');
-$default = '';
-$setting = new admin_setting_confightmleditor($name, $title, $description, $default);
-$settings->add($setting);
+	// Foot note setting
+	$name = 'theme_afterburner/footnote';
+	$title = get_string('footnote','theme_afterburner');
+	$description = get_string('footnotedesc', 'theme_afterburner');
+	$default = '';
+	$setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+	$settings->add($setting);
+
+	// Custom CSS file
+	$name = 'theme_afterburner/customcss';
+	$title = get_string('customcss','theme_afterburner');
+	$description = get_string('customcssdesc', 'theme_afterburner');
+	$default = '';
+	$setting = new admin_setting_configtextarea($name, $title, $description, $default);
+	$settings->add($setting);
+
 }
-
-// Custom CSS file
-$name = 'theme_afterburner/customcss';
-$title = get_string('customcss','theme_afterburner');
-$description = get_string('customcssdesc', 'theme_afterburner');
-$default = '';
-$setting = new admin_setting_configtextarea($name, $title, $description, $default);
-$settings->add($setting);
