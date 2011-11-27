d11d1e5bc010215d3aec8d65528ce7001c1d0dea
Mary Evans <lazydaisy@visible-expression.co.uk>
2011-11-27 00:46:24 +0000

    MDL-26240 Fixes for Anomaly theme inconsistency

--- theme/anomaly/config.php
+++ theme/anomaly/config.php
@@ -7,7 +7,7 @@
 
 $THEME->name = 'anomaly';
 
-$THEME->sheets = array('base', 'general', 'browser');
+$THEME->sheets = array('base', 'general', 'browser','dock');
 /// This variable is an array containing the names of all the
 /// stylesheet files you want included in this theme, and in what order
 ////////////////////////////////////////////////////////////////////////////////
