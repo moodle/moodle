e5a3ba18f43f38d95078e8a8522c8d68c6ca95a3
Andrew Davis <andrew@moodle.com>
2011-11-29 22:05:09 +0800

    MDL-30260 messaging: backporting changes to prevent errors if $userto->emailto isn't supplied when sending messages

--- mod/quiz/locallib.php
+++ mod/quiz/locallib.php
@@ -1089,7 +1089,7 @@ function quiz_send_notification_emails($course, $quiz, $attempt, $context, $cm) 
     }
 
     // check for notifications required
-    $notifyfields = 'u.id, u.username, u.firstname, u.lastname, u.email, u.lang, u.timezone, u.mailformat, u.maildisplay';
+    $notifyfields = 'u.id, u.username, u.firstname, u.lastname, u.email, u.emailstop, u.lang, u.timezone, u.mailformat, u.maildisplay';
     $groups = groups_get_all_groups($course->id, $USER->id);
     if (is_array($groups) && count($groups) > 0) {
         $groups = array_keys($groups);
