9ecbb6e494a478bddaa30b0f3e80d4bd306a5571
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-12-01 00:14:42 +0100

    MDL-30007 standardise mnet service version files

--- mnet/service/enrol/version.php
+++ mnet/service/enrol/version.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -22,4 +21,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2010071700;
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2011112900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2011112900;        // Requires this Moodle version
+$plugin->component = 'mnetservice_enrol'; // Full name of the plugin (used for diagnostics)
