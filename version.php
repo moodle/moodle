5f874ee80b837051e07ee301925bef9d9c39026d
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-12-11 22:14:02 +0100

    MDL-28180 backup - take rid of dupes in course_completion_aggr_methd

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$version  = 2011070103.03;              // 20110701      = branching date YYYYMMDD - do not modify!
+$version  = 2011070103.04;              // 20110701      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
