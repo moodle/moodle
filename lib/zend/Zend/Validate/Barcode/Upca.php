dcaea2b744f097f1108839fd96c16c40dc75b6c2
Aparup Banerjee <aparup@moodle.com>
2011-12-12 14:06:26 +0800

    MDL-30603 synced and renamed incorrect ZF 1.10.6 file and name

--- lib/zend/Zend/Validate/Barcode/Upca.php
+++ lib/zend/Zend/Validate/Barcode/Upca.php
@@ -16,7 +16,7 @@
  * @package    Zend_Validate
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id$
+ * @version    $Id: Upca.php 20096 2010-01-06 02:05:09Z bkarwin $
  */
 
 /**
--- lib/zend/readme_moodle.txt
+++ lib/zend/readme_moodle.txt
@@ -7,4 +7,5 @@ Do not use outside of our /webservice/* or mnet !!
 Changes:
 * lots of files removed
 * small fix to error reporting in reflection (MDL-21460, ZF-8980)
+* synced and renamed file to version in ZF 1.10.6 (MDL-30603, ZF-11080)
 
