54e44c14e9a10d6df606f361659570dac74947b7
Justin Filip <jfilip@gmail.com>
2011-12-14 11:37:45 -0500

    MDL-30741 Adding a new test to check for incorrect parsing of complicate HTML with a PRE tag.

--- lib/simpletest/testweblib.php
+++ lib/simpletest/testweblib.php
@@ -144,6 +144,27 @@ class web_test extends UnitTestCase {
         $this->assertIdentical('0', html_to_text('0'));
     }
 
+    public function test_html_to_text_pre_parsing_problem() {
+        $strorig = 'Consider the following function:<br /><pre><span style="color: rgb(153, 51, 102);">void FillMeUp(char* in_string) {'.
+                   '<br />  int i = 0;<br />  while (in_string[i] != \'\0\') {<br />    in_string[i] = \'X\';<br />    i++;<br />  }<br />'.
+                   '}</span></pre>What would happen if a non-terminated string were input to this function?<br /><br />';
+
+        $strconv = 'Consider the following function:
+
+void FillMeUp(char* in_string) {
+ int i = 0;
+ while (in_string[i] != \'\0\') {
+ in_string[i] = \'X\';
+ i++;
+ }
+}
+What would happen if a non-terminated string were input to this function?
+
+';
+
+        $this->assertIdentical($strconv, html_to_text($strorig));
+    }
+
     public function test_clean_text() {
         $text = "lala <applet>xx</applet>";
         $this->assertEqual($text, clean_text($text, FORMAT_PLAIN));
