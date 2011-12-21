5831b4c70cfbd0852b496fe925c7e12d7c1b84cb
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-12-21 12:17:22 +0100

    MDL-20245 grade: fix version numbers
    This commit puts the 22_STABLE and master main
    versions back to "on-sync" mode (very first
    weeks after major release).
    
    Conflicts:
    
    	lib/db/upgrade.php
    	version.php

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$version  = 2011120500.01;              // 20111205      = branching date YYYYMMDD - do not modify!
+$version  = 2011120500.03;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
