589f57835ff93e1f06236690b2976ad668b32e7d
Tim Hunt <T.J.Hunt@open.ac.uk>
2012-01-11 16:21:47 +0000

    MDL-31095 quiz editing: only set quiz->grade to 0 if really necessary.
    It is only necessary when there are (non-preview) attempts. However,
    after making this change we have to ensure users cannot start attempts
    when ->grade > 0 and ->sumgrades = 0.
    
    Display ->grade on the order and paging tab of edit.php, so it is clear
    what is going on when you are using that tab.
    
    Finally, improve the error message a student gets if they try to start a
    quiz with no questions.

--- mod/quiz/startattempt.php
+++ mod/quiz/startattempt.php
@@ -54,9 +54,13 @@ require_login($quizobj->get_courseid(), false, $quizobj->get_cm());
 require_sesskey();
 $PAGE->set_pagelayout('base');
 
-// if no questions have been set up yet redirect to edit.php
-if (!$quizobj->has_questions() && $quizobj->has_capability('mod/quiz:manage')) {
-    redirect($quizobj->edit_url());
+// if no questions have been set up yet redirect to edit.php or display an error.
+if (!$quizobj->has_questions()) {
+    if ($quizobj->has_capability('mod/quiz:manage')) {
+        redirect($quizobj->edit_url());
+    } else {
+        print_error('cannotstartnoquestions', 'quiz', $quizobj->view_url());
+    }
 }
 
 // Create an object to manage all the other (non-roles) access rules.
