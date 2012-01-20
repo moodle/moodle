5d2dce09b3d76ab05df8cc717d714656041a41e0
M Kassaei <m.kassaei@open.ac.uk>
2012-01-20 11:59:14 +0000

    MDL-31306 question preview: disable 'Fill correct' for qtypes that can't

--- question/type/questionbase.php
+++ question/type/questionbase.php
@@ -262,9 +262,10 @@ abstract class question_definition {
     /**
      * What data would need to be submitted to get this question correct.
      * If there is more than one correct answer, this method should just
-     * return one possibility.
+     * return one possibility. If it is not possible to compute a correct
+     * response, this method should return null.
      *
-     * @return array parameter name => value.
+     * @return array|null parameter name => value.
      */
     public abstract function get_correct_response();
 
