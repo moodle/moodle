2d3eed0fbaa82d57ed7a1580c84b86fd3d2492d5
Gerard (Gerry) Caulfield <gerry@moodle.com>
2012-02-10 06:21:20 +0800

    MDL-25996 Updating languaged editing interface terms to make them user friendly

--- admin/report/customlang/lang/en/report_customlang.php
+++ admin/report/customlang/lang/en/report_customlang.php
@@ -26,36 +26,36 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['checkin'] = 'Check in strings into disk';
-$string['checkout'] = 'Check out strings into the translator';
-$string['checkoutdone'] = 'Strings checked out successfully into the translator';
-$string['checkoutinprogress'] = 'Checking out strings into the translator';
-$string['confirmcheckin'] = 'You are about to check in modified strings into your local language pack. This will export the customized strings from the translator into the data directory and Moodle will start using the modified strings. Press \'Continue\' button to proceed check in.';
+$string['checkin'] = 'Save strings to language pack';
+$string['checkout'] = 'Open language pack for editing';
+$string['checkoutdone'] = 'Language pack loaded';
+$string['checkoutinprogress'] = 'Loading language pack';
+$string['confirmcheckin'] = 'You are about to save modifications to your local language pack. This will export the customised strings from the translator into you Moodle data directory and Moodle will start using the modified strings. Press \'Continue\' to proceed with saving.';
 $string['customlang:edit'] = 'Edit local translation';
 $string['customlang:view'] = 'View local translation';
 $string['filter'] = 'Filter strings';
 $string['filtercomponent'] = 'Show strings of these components';
-$string['filtercustomized'] = 'Customized only';
+$string['filtercustomized'] = 'Customised only';
 $string['filtermodified'] = 'Modified only';
 $string['filteronlyhelps'] = 'Help only';
 $string['filtershowstrings'] = 'Show strings';
 $string['filterstringid'] = 'String identifier';
 $string['filtersubstring'] = 'Only strings containing';
 $string['headingcomponent'] = 'Component';
-$string['headinglocal'] = 'Local customization';
+$string['headinglocal'] = 'Local customisation';
 $string['headingstandard'] = 'Standard text';
 $string['headingstringid'] = 'String';
-$string['markinguptodate'] = 'Marking the customization as up-to-date';
-$string['markinguptodate_help'] = 'The customized translation may get outdated if either the English original or the master translation has modified since the string was customized on your site. Review the customized translation. If you find it up-to-date, click the checkbox. Edit it otherwise.';
+$string['markinguptodate'] = 'Marking the customisation as up-to-date';
+$string['markinguptodate_help'] = 'The customised translation may get outdated if either the English original or the master translation has modified since the string was customised on your site. Review the customised translation. If you find it up-to-date, click the checkbox. Edit it otherwise.';
 $string['markuptodate'] = 'mark as up-to-date';
-$string['modifiedno'] = 'There are no modified strings to check in.';
-$string['modifiednum'] = 'There are {$a} modified strings. You must check in them into disk to store them permanently.';
+$string['modifiedno'] = 'There are no modified strings to save.';
+$string['modifiednum'] = 'There are {$a} modified strings. Do you wish to save these changes to your local language pack?';
 $string['nostringsfound'] = 'No strings found, please modify the filter settings';
 $string['placeholder'] = 'Placeholders';
 $string['placeholder_help'] = 'Placeholders are special statements like `{$a}` or `{$a->something}` within the string. They are replaced with a value when the string is actually printed.
 
 It is important to copy them exactly as they are in the original string. Do not translate them nor change their left-to-right orientation.';
 $string['placeholderwarning'] = 'string contains a placeholder';
-$string['pluginname'] = 'Language customization';
-$string['savecheckin'] = 'Save and check in strings into files';
-$string['savecontinue'] = 'Save and continue editing';
+$string['pluginname'] = 'Language customisation';
+$string['savecheckin'] = 'Save changes to the language pack';
+$string['savecontinue'] = 'Apply changes and continue editing';
