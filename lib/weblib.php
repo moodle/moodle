5dad45912c56a1b4feb94f6535304f34d829118a
Dan Poltawski <dan@moodle.com>
2012-02-26 18:39:40 +0800

    MDL-29592 weblib: prevent warnings when $USER not set

--- lib/weblib.php
+++ lib/weblib.php
@@ -2721,7 +2721,7 @@ function debugging($message = '', $level = DEBUG_NORMAL, $backtrace = null) {
     global $CFG, $USER, $UNITTEST;
 
     $forcedebug = false;
-    if (!empty($CFG->debugusers)) {
+    if (!empty($CFG->debugusers) && $USER) {
         $debugusers = explode(',', $CFG->debugusers);
         $forcedebug = in_array($USER->id, $debugusers);
     }
