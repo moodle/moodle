bd8dc9ba0aee376bf1829efed5b1bb1943911fc2
Rajesh Taneja <rajesh@moodle.com>
2012-03-01 11:27:24 +0800

    MDL-31654 users: removed hardcoding and added docblock on top of tim's patch

--- admin/tool/uploaduser/locallib.php
+++ admin/tool/uploaduser/locallib.php
@@ -363,3 +363,30 @@ function uu_allowed_roles_cache() {
     }
     return $rolecache;
 }
+
+/**
+ * Pre process custom profile data, and update it with corrected value
+ *
+ * @param stdClass $data user profile data
+ * @return stdClass pre-processed custom profile data
+ */
+function uu_pre_process_custom_profile_data($data) {
+    global $CFG, $DB;
+    // find custom profile fields and check if data needs to converted.
+    foreach ($data as $key => $value) {
+        if (preg_match('/^profile_field_/', $key)) {
+            $shortname = str_replace('profile_field_', '', $key);
+            if ($fields = $DB->get_records('user_info_field', array('shortname' => $shortname))) {
+                foreach ($fields as $field) {
+                    require_once($CFG->dirroot.'/user/profile/field/'.$field->datatype.'/field.class.php');
+                    $newfield = 'profile_field_'.$field->datatype;
+                    $formfield = new $newfield($field->id, $data->id);
+                    if (method_exists($formfield, 'convert_external_data')) {
+                        $data->$key = $formfield->convert_external_data($value);
+                    }
+                }
+            }
+        }
+    }
+    return $data;
+}
