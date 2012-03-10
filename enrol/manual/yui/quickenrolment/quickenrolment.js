9be109005b600abdbb25187d5d33c9bc79efa045
Petr Skoda <commits@skodak.org>
2012-03-10 09:59:25 +0100

    MDL-27457 fix JS error on enrol when grade history disabled

--- enrol/manual/yui/quickenrolment/quickenrolment.js
+++ enrol/manual/yui/quickenrolment/quickenrolment.js
@@ -379,7 +379,11 @@ YUI.add('moodle-enrol_manual-quickenrolment', function(Y) {
             params['role'] = this.get(UEP.BASE).one('.'+CSS.ENROLMENTOPTION+'.'+CSS.ROLE+' select').get('value');
             params['startdate'] = this.get(UEP.BASE).one('.'+CSS.ENROLMENTOPTION+'.'+CSS.STARTDATE+' select').get('value');
             params['duration'] = this.get(UEP.BASE).one('.'+CSS.ENROLMENTOPTION+'.'+CSS.DURATION+' select').get('value');
-            params['recovergrades'] = this.get(UEP.BASE).one('#'+CSS.RECOVERGRADES).get('checked')?1:0;
+            if (this.get(UEP.DISABLEGRADEHISTORY) != true) {
+                params['recovergrades'] = this.get(UEP.BASE).one('#'+CSS.RECOVERGRADES).get('checked')?1:0;
+            } else {
+                params['recovergrades'] = 0;
+            }
 
             Y.io(M.cfg.wwwroot+this.get(UEP.AJAXURL), {
                 method:'POST',
