436650b075f2aec23e6d4759b8b957176f1efcfd
Petr Skoda <commits@skodak.org>
2012-03-11 15:51:49 +0100

    MDL-31985 convert all existing LOBs

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -204,6 +204,14 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2012030100.02);
     }
 
+    if ($oldversion < 2012030900.01) {
+        // migrate all texts and binaries to big size - it should be safe to interrupt this and continue later
+        upgrade_mysql_fix_lob_columns();
+
+        // Main savepoint reached
+        upgrade_main_savepoint(true, 2012030900.01);
+    }
+
 
     return true;
 }
