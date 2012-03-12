180bf7d49437e23d050cf0a3ed3ab1fd1ca3f825
Mary Evans <lazydaisy@visible-expression.co.uk>
2012-03-12 10:37:35 +0000

    MDL-30298 report_progress: adds outer wrapper with class no-overflow to report table so as to enable horizontal scroll bar

--- report/progress/index.php
+++ report/progress/index.php
@@ -279,6 +279,7 @@ if (!$csv) {
         exit;
     }
 
+    print '<div id="completion-progress-wrapper" class="no-overflow">';
     print '<table id="completion-progress" class="generaltable flexible boxaligncenter" style="text-align:left"><tr style="vertical-align:top">';
 
     // User heading / sort option
@@ -413,6 +414,7 @@ if ($csv) {
     exit;
 }
 print '</table>';
+print '</div>';
 print $pagingbar;
 
 print '<ul class="progress-actions"><li><a href="index.php?course='.$course->id.
