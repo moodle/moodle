133e7f3dd8f70480d5b1a76ad74606c6d00a9c1f
Dan Marsden <dan@danmarsden.com>
2012-04-11 20:46:53 +1200

    MDL-32388 SCORM: Fix chrome pop-ups

--- mod/scorm/player.js
+++ mod/scorm/player.js
@@ -1,4 +1,12 @@
 function scorm_openpopup(url,name,options,width,height) {
+    if (width<=100) {
+        width = Math.round(screen.availWidth * width / 100);
+    }
+    if (height<=100) {
+        height = Math.round(screen.availHeight * height / 100);
+    }
+    options += ",width="+width+",height="+height;
+
     windowobj = window.open(url,name,options);
     if (!windowobj) {
         return;
@@ -7,13 +15,6 @@ function scorm_openpopup(url,name,options,width,height) {
         // Fullscreen
         windowobj.moveTo(0,0);
     }
-    if (width<=100) {
-        width = Math.round(screen.availWidth * width / 100);
-    }
-    if (height<=100) {
-        height = Math.round(screen.availHeight * height / 100);
-    }
-    windowobj.resizeTo(width,height);
     windowobj.focus();
     return windowobj;
 }
