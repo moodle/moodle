d180d19a830c49b2c57460c011a4a737b1ace563
Nadav Kavalerchik <nadavkav@gmail.com>
2012-04-17 14:00:23 +0300

    MDL-32479 - RTL Theme fixes for Moodle 2 (theme/base)
    Support for blocks columns switch when in rtl mode:
    theme/base/layout/frontpage.php
    theme/base/layout/general.php
    
    Right align course completion checkbox to the right side of the activities and resources, when in RTL mode
    theme/base/style/course.css
    
    Right align DOCK bar, when in RTL mode
    theme/base/style/dock.css
    
    First-name and Last-name Initials (filter) bar (on every users list) letters spacing (usability issue, NOT RTL issue)
    theme/base/style/core.css
    
    Conflicts:
    
    	theme/base/style/core.css
    	theme/base/style/course.css

--- theme/base/layout/general.php
+++ theme/base/layout/general.php
@@ -15,9 +15,17 @@ $hascustommenu = (empty($PAGE->layout_options['nocustommenu']) && !empty($custom
 
 $bodyclasses = array();
 if ($showsidepre && !$showsidepost) {
-    $bodyclasses[] = 'side-pre-only';
+    if (!right_to_left()) {
+        $bodyclasses[] = 'side-pre-only';
+    }else{
+        $bodyclasses[] = 'side-post-only';
+    }
 } else if ($showsidepost && !$showsidepre) {
-    $bodyclasses[] = 'side-post-only';
+    if (!right_to_left()) {
+        $bodyclasses[] = 'side-post-only';
+    }else{
+        $bodyclasses[] = 'side-pre-only';
+    }
 } else if (!$showsidepost && !$showsidepre) {
     $bodyclasses[] = 'content-only';
 }
@@ -73,21 +81,33 @@ echo $OUTPUT->doctype() ?>
                     </div>
                 </div>
 
-                <?php if ($hassidepre) { ?>
+                <?php if ($hassidepre OR (right_to_left() AND $hassidepost)) { ?>
                 <div id="region-pre" class="block-region">
                     <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
+                            <?php
+                        if (!right_to_left()) {
+                            echo $OUTPUT->blocks_for_region('side-pre');
+                        } elseif ($hassidepost) {
+                            echo $OUTPUT->blocks_for_region('side-post');
+                    } ?>
+
                     </div>
                 </div>
                 <?php } ?>
 
-                <?php if ($hassidepost) { ?>
+                <?php if ($hassidepost OR (right_to_left() AND $hassidepre)) { ?>
                 <div id="region-post" class="block-region">
                     <div class="region-content">
-                        <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                           <?php
+                       if (!right_to_left()) {
+                           echo $OUTPUT->blocks_for_region('side-post');
+                       } elseif ($hassidepre) {
+                           echo $OUTPUT->blocks_for_region('side-pre');
+                    } ?>
                     </div>
                 </div>
                 <?php } ?>
+
             </div>
         </div>
     </div>
