98cef39d280c914ac8df74e5c7f049e84375bf26
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2012-05-01 01:06:50 +0200

    MDL-32709 book: Add some missing MOODLE_INTERNALs

--- mod/book/tool/print/db/access.php
+++ mod/book/tool/print/db/access.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die;
+
 $capabilities = array(
     'booktool/print:print' => array(
         'captype' => 'read',
