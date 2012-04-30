46cb5d9aced60a4867fb88ef5f846b1db8b1a283
Petr Skoda <commits@skodak.org>
2012-04-30 08:35:16 +0200

    MDL-32589 remove bogus webproxy whitelisting
    Thanks Dan Poltawski!

--- lib/phpunit/bootstrap.php
+++ lib/phpunit/bootstrap.php
@@ -159,7 +159,7 @@ $CFG->dboptions = isset($CFG->phpunit_dboptions) ? $CFG->phpunit_dboptions : $CF
 
 $allowed = array('wwwroot', 'dataroot', 'dirroot', 'admin', 'directorypermissions', 'filepermissions',
                  'dbtype', 'dblibrary', 'dbhost', 'dbname', 'dbuser', 'dbpass', 'prefix', 'dboptions',
-                 'webproxy', 'proxyhost', 'proxyport', 'proxytype', 'proxyuser', 'proxypassword', 'proxybypass', // keep proxy settings from config.php
+                 'proxyhost', 'proxyport', 'proxytype', 'proxyuser', 'proxypassword', 'proxybypass', // keep proxy settings from config.php
                 );
 $productioncfg = (array)$CFG;
 $CFG = new stdClass();
