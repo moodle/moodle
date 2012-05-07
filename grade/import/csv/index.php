280b223d84e3b00a4871ece315c8563967ba5bbf
Charles Fulton <mackensen@gmail.com>
2012-05-06 20:14:55 -0700

    MDL-26277 grade import: validate file pointer before reading

--- grade/import/csv/index.php
+++ grade/import/csv/index.php
@@ -125,7 +125,9 @@ if ($formdata = $mform->get_data()) {
     fwrite($fp,$text);
     fclose($fp);
 
-    $fp = fopen($filename, "r");
+    if (!$fp = fopen($filename, "r")) {
+    	print_error('cannotopenfile');
+    }
 
     // --- get header (field names) ---
     $header = explode($csv_delimiter, fgets($fp, GRADE_CSV_LINE_LENGTH));
