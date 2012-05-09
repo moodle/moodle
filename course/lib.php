af2ce7806b6d66796737edb6cc5ff3b36932c117
Mark Nelson <mark@moodle.com.au>
2012-05-10 00:49:26 +0800

    MDL-29538 core_condition: delete the course modules records in the course_module_avail_fields table when deleted

--- course/lib.php
+++ course/lib.php
@@ -2929,6 +2929,7 @@ function delete_course_module($id) {
     // very quick on an empty table)
     $DB->delete_records('course_modules_completion', array('coursemoduleid' => $cm->id));
     $DB->delete_records('course_modules_availability', array('coursemoduleid'=> $cm->id));
+    $DB->delete_records('course_modules_avail_fields', array('coursemoduleid'=> $cm->id));
     $DB->delete_records('course_completion_criteria', array('moduleinstance' => $cm->id,
                                                             'criteriatype' => COMPLETION_CRITERIA_TYPE_ACTIVITY));
 
