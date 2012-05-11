2a22be64ee8129912c44f28b977c05143231736f
Colin Chambers <c.chambers@open.ac.uk>
2012-05-11 11:23:18 +0100

    MDL-32750 Question: make the html editor tools collapsible

--- lib/editor/tinymce/lib.php
+++ lib/editor/tinymce/lib.php
@@ -106,6 +106,7 @@ class tinymce_texteditor extends texteditor {
         if ($fpoptions) {
             $PAGE->requires->js_init_call('M.editor_tinymce.init_filepicker', array($elementid, $fpoptions), true);
         }
+        $this->initialise_collapse_js();
     }
 
     protected function get_init_params($elementid, array $options=null) {
@@ -273,5 +274,28 @@ class tinymce_texteditor extends texteditor {
     public function get_tinymce_base_url() {
         global $CFG;
         return new moodle_url("$CFG->httpswwwroot/lib/editor/tinymce/tiny_mce/$this->version/");
+    }
+
+    /**
+     * Initialise javascript form elements
+     * @return void
+     */
+    public function initialise_collapse_js() {
+        global $CFG, $PAGE, $OUTPUT;
+
+        // This method is called for every editor instance. Ensure it's only run once.
+        // Static is a clunky solution but the best we could find to keep everything simple and encapsulated.
+        static $is_initialised;
+
+        if ($is_initialised) {
+            return;
+        }
+
+        // Initialise language strings.
+        $PAGE->requires->strings_for_js(array('hideeditortoolbar', 'showeditortoolbar'),
+                'form');
+
+        $PAGE->requires->yui_module('moodle-editor_tinymce-collapse', 'M.editor_collapse.init');
+        $is_initialised = true;
     }
 }
