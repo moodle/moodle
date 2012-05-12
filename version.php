628d7c0f9b150c06378ea4000b34aea96758f65d
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2012-05-12 03:19:00 +0200

    Moodle release 1.9.18

--- version.php
+++ version.php
@@ -6,10 +6,10 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2007101591.15; // YYYYMMDD      = date of the 1.9 branch (don't change)
+    $version = 2007101591.16; // YYYYMMDD      = date of the 1.9 branch (don't change)
                               //         X     = release number 1.9.[0,1,2,3,4,5...]
                               //          Y.YY = micro-increments between releases
 
-    $release = '1.9.17+ (Build: 20120511)';     // Human-friendly version name
+    $release = '1.9.18 (Build: 20120514)';     // Human-friendly version name
 
 ?>
