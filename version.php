c56ff4a8f7706fa66cb114d01738da93874859bc
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2012-05-19 12:20:58 +0200

    weekly release 1.9.18+

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
                               //         X     = release number 1.9.[0,1,2,3,4,5...]
                               //          Y.YY = micro-increments between releases
 
-    $release = '1.9.18 (Build: 20120514)';     // Human-friendly version name
+    $release = '1.9.18+ (Build: 20120519)';     // Human-friendly version name
 
 ?>
