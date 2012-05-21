319887f86eafc273a60212216d20f4c14cf7e53a
Kordan <kordan@mclink.it>
2012-05-21 09:26:52 +0200

    MDL-33055 theme_formal_white: changed "general" to "standard" in $theme->layouts array index

--- theme/formal_white/config.php
+++ theme/formal_white/config.php
@@ -94,7 +94,7 @@ $THEME->layouts = array(
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-post',
     ),
-    'general' => array(
+    'standard' => array(
         'file' => 'general.php',
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-post',
