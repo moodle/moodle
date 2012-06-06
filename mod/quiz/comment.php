e78e39cdbb8b88f541625121a61e60cf21b4f069
Tim Hunt <T.J.Hunt@open.ac.uk>
2012-06-06 20:21:33 +0100

    MDL-33548 quiz manual grading: check the grade is in range.
    To do this nicely, I refactored some code out of the quiz manual grading
    report.

--- mod/quiz/comment.php
+++ mod/quiz/comment.php
@@ -55,7 +55,7 @@ echo $OUTPUT->heading(format_string($attemptobj->get_question_name($slot)));
 
 // Process any data that was submitted.
 if (data_submitted() && confirm_sesskey()) {
-    if (optional_param('submit', false, PARAM_BOOL)) {
+    if (optional_param('submit', false, PARAM_BOOL) && question_behaviour::is_manual_grade_in_range($attemptobj->get_uniqueid(), $slot)) {
         $transaction = $DB->start_delegated_transaction();
         $attemptobj->process_all_actions(time());
         $transaction->allow_commit();
