9f012f896867cafe15ced7c09edcc0712016444c
Petr Skoda <commits@skodak.org>
2012-06-07 11:04:35 +0200

    MDL-33583 really keep all automated backup files when told to do so

--- backup/util/helper/backup_cron_helper.class.php
+++ backup/util/helper/backup_cron_helper.class.php
@@ -459,6 +459,11 @@ abstract class backup_cron_automated_helper {
         $storage =  $config->backup_auto_storage;
         $dir =      $config->backup_auto_destination;
 
+        if ($keep == 0) {
+            // means keep all backup files
+            return true;
+        }
+
         $backupword = str_replace(' ', '_', moodle_strtolower(get_string('backupfilename')));
         $backupword = trim(clean_filename($backupword), '_');
 
