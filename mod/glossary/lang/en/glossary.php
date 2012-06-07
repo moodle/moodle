7ccec1e2cf4c35bc6a9f0117e687a39523a0c39b
Andrew Robert Nicols <andrew.nicols@luns.net.uk>
2012-06-07 09:17:40 +0100

    MDL-33535 Correct typo in glossary documentation link

--- mod/glossary/lang/en/glossary.php
+++ mod/glossary/lang/en/glossary.php
@@ -224,7 +224,7 @@ Glossaries have many uses, such as
 * A ‘handy tips’ resource of best practice in a practical subject
 * A sharing area of useful videos, images or sound files
 * A revision resource of facts to remember';
-$string['modulename_link'] = 'mod/glosssary/view';
+$string['modulename_link'] = 'mod/glossary/view';
 $string['modulenameplural'] = 'Glossaries';
 $string['newentries'] = 'New glossary entries';
 $string['newglossary'] = 'New glossary';
