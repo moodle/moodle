2029b1d80e745d0c2b64beafc7475e7af916e233
Eugene Shwab <chillofoctober@gmail.com>
2012-06-08 16:06:38 +0400

    MDL-47494 gapselect: Translate to russian language

---
+++ question/type/gapselect/lang/ru/qtype_gapselect.php
@@ -0,0 +1,42 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://www.moodle.vsu.ru
+ *
+ * @package    qtype
+ * @subpackage gapselect
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addmorechoiceblanks'] = 'Шаблоны для ещё {no} вариантов';
+$string['answer'] = 'Ответ';
+$string['choices'] = 'Варианты выбора';
+$string['choicex'] = 'Вариант {no}';
+$string['correctansweris'] = 'Верный ответ: {$a}';
+$string['errorblankchoice'] = 'Пожалуйста, проверьте варианты выбора: вариант выбора {$a} пуст.';
+$string['errormissingchoice'] = 'Пожалуйста, проверьте текст вопроса: {$a} не найден среди вариантов выбора! Только номера ответов, которые существуют в вариантах выбора могут быть использованы как метки-заполнители.';
+$string['errornoslots'] = 'Текст вопроса должен содержать метки-заполнители, например [[1]], для обозначения местонахождения пропущенных слов.';
+$string['errorquestiontextblank'] = 'Вы должны ввести какой-либо текст.';
+$string['group'] = 'Группа';
+$string['pleaseputananswerineachbox'] = 'Пожалуйста, добавьте ответ в каждую ячейку';
+$string['pluginname'] = 'Выбрать пропущенные слова';
+$string['pluginnameadding'] = 'Добавить вопрос на выборку пропущенных слов';
+$string['pluginnameediting'] = 'Редактировать вопрос на выборку пропущенных слов';
+$string['pluginnamesummary'] = 'Пропущенные слова в тексте заполняются с помощью выпадающего меню.';
+$string['pluginname_help'] = 'Напишите текст, например "[[1]] перепрыгивает через [[2]]", затем введите возможные слова для меток 1 и 2 внизу';
+$string['shuffle'] = 'Перемешать';
