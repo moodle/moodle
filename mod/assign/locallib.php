502be9a069e47d87157a418f2c1325b77d86303f
Dan Poltawski <dan@moodle.com>
2012-06-12 13:41:54 +0800

    MDL-33572 - assign: fixed syntax probblem found immediately after integration
    Grr. shouldn't have escaped our review.

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -900,7 +900,7 @@ class assign {
                 } else {
                     $displaygrade = format_float($grade);
                 }
-                $o = '<input type="text" name="quickgrade_' . $userid . '" value="' . $displaygrade . '" size="6" maxlength
+                $o = '<input type="text" name="quickgrade_' . $userid . '" value="' . $displaygrade . '" size="6" maxlength="10" class="quickgrade"/>';
                 $o .= '&nbsp;/&nbsp;' . format_float($this->get_instance()->grade,2);
                 $o .= '<input type="hidden" name="grademodified_' . $userid . '" value="' . $modified . '"/>';
                 return $o;
