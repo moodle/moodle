0ba48ee89909e2775023a2ea457aacc2c52be915
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2012-06-14 19:51:35 +0200

    MDL-32279 glossary filter: take rid of unused courseid variable in yui module.

--- filter/glossary/yui/autolinker/autolinker.js
+++ filter/glossary/yui/autolinker/autolinker.js
@@ -5,7 +5,6 @@ YUI.add('moodle-filter_glossary-autolinker', function(Y) {
         POPUPNAME = 'name',
         POPUPOPTIONS = 'options',
         TITLE = 'title',
-        COURSEID = 'courseid',
         WIDTH = 'width',
         HEIGHT = 'height',
         MENUBAR = 'menubar',
@@ -122,8 +121,7 @@ YUI.add('moodle-filter_glossary-autolinker', function(Y) {
             status : {value : true},
             directories : {value : false},
             fullscreen : {value : false},
-            dependent : {value : true},
-            courseid : {value : 1}
+            dependent : {value : true}
         }
     });
 
