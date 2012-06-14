813f6964ca7d32c31e2ac4908e303fa91f66604a
Dan Poltawski <dan@moodle.com>
2012-06-14 10:28:34 +0800

    MDL-33400 questionlib - minor whitespace/equality imporvement

--- lib/questionlib.php
+++ lib/questionlib.php
@@ -1858,8 +1858,8 @@ function question_pluginfile($course, $context, $component, $filearea, $args, $f
                 $args, $forcedownload, $options);
         }
 
-        //okay, we're here so lets check for function without 'mod_'.
-        if(strpos($module, 'mod_') == 0) {
+        // Okay, we're here so lets check for function without 'mod_'.
+        if (strpos($module, 'mod_') === 0) {
             $filefunctionold  = substr($module, 3) . '_question_pluginfile';
             if (function_exists($filefunctionold)) {
                 $filefunctionold($course, $context, $component, $filearea, $qubaid, $slot,
