d21a98a633189cede6dde9fe210e8243dadf23f9
Dan Poltawski <dan@moodle.com>
2012-06-14 12:12:39 +0800

    MDL-27156 - version bump for new capability

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$version  = 2012061200.00;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2012061400.00;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
