c48797ac2686fada6be156df952c504b194c2cef
Jamie Pratt <me@jamiep.org>
2012-06-21 14:07:42 +0700

    MDL-47494 gapselect: NOBUG added extra options param for file access

--- question/type/gapselect/lib.php
+++ question/type/gapselect/lib.php
@@ -30,9 +30,9 @@ defined('MOODLE_INTERNAL') || die();
 /**
  * Checks file access for gapselect questions.
  */
-function qtype_gapselect_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload) {
+function qtype_gapselect_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options=array()) {
     global $CFG;
     require_once($CFG->libdir . '/questionlib.php');
-    question_pluginfile($course, $context, 'qtype_ddmarker', $filearea, $args, $forcedownload);
+    question_pluginfile($course, $context, 'qtype_ddmarker', $filearea, $args, $forcedownload, $options);
 }
 
