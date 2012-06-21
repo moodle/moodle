e0d90c54c040fc8d7e42a39aadf644f5fae93c36
Dan Marsden <dan@danmarsden.com>
2012-06-21 19:24:58 +1200

    MDL-33938 SCORM use consistant naming for maxattempt setting - match setting name with db field name to help when matching default settings

--- mod/scorm/db/upgrade.php
+++ mod/scorm/db/upgrade.php
@@ -60,6 +60,19 @@ function xmldb_scorm_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2012032101, 'scorm');
     }
 
+    //rename config var from maxattempts to maxattempt
+    if ($oldversion < 2012061701) {
+        $maxattempts = get_config('scorm', 'maxattempts');
+        $maxattempts_adv = get_config('scorm', 'maxattempts_adv');
+        set_config('maxattempt', $maxattempts, 'scorm');
+        set_config('maxattempt_adv', $maxattempts_adv, 'scorm');
+
+        unset_config('maxattempts', 'scorm'); //remove old setting.
+        unset_config('maxattempts_adv', 'scorm'); //remove old setting.
+        upgrade_mod_savepoint(true, 2012061701, 'scorm');
+    }
+
+
     return true;
 }
 
