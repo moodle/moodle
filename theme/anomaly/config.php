b27464f61769aeb50680346b64f4b5633c2f3db0
Mary Evans <lazydaisy@visible-expression.co.uk>
2012-06-26 03:40:37 +0100

    MDL-33987_M23 theme_anomaly: amended some css values to style dock better also removed exclude parent theme stylesheet as it refered to a none existant file in base theme.

--- theme/anomaly/config.php
+++ theme/anomaly/config.php
@@ -21,8 +21,6 @@ $THEME->parents = array('base');  // TODO: new themes can not be based on standa
 /// is not used.
 ////////////////////////////////////////////////////////////////////////////////
 
-$THEME->parents_exclude_sheets = array('base'=>array('styles_moz'));
-
 $THEME->layouts = array(
     // Most pages - if we encounter an unknown or a missing page type, this one is used.
     'base' => array(
