93fd7cb10cc9dca86b03443a9ec8661be754eecf
Petr Škoda <commits@skodak.org>
2012-06-30 13:57:55 +0200

    MDL-34121 drop unused backup tables that were supposed to be dropped long ago
    The backup/restore leftovers that referenced these will be dropped in 2.4

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$version  = 2012062500.00;              // 2012062500    = branching date YYYYMMDD - do not modify!
+$version  = 2012062500.01;              // 2012062500    = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
