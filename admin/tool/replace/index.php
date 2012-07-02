4387f1382119fe0827f73d7b3cd2d79d2a2bcb0a
Rajesh Taneja <rajesh@moodle.com>
2012-07-02 15:15:58 +0800

    MDL-33943 tool_replace: Hardcoded strings replaced with lang strings

--- admin/tool/replace/index.php
+++ admin/tool/replace/index.php
@@ -38,25 +38,29 @@ $sure    = optional_param('sure', 0, PARAM_BOOL);
 ###################################################################
 echo $OUTPUT->header();
 
-echo $OUTPUT->heading('Search and replace text throughout the whole database');
+echo $OUTPUT->heading(get_string('pageheader', 'tool_replace'));
 
 if ($DB->get_dbfamily() !== 'mysql' and $DB->get_dbfamily() !== 'postgres') {
     //TODO: add $DB->text_replace() to DML drivers
-    echo $OUTPUT->notification('Sorry, this feature is implemented only for MySQL and PostgreSQL databases.');
+    echo $OUTPUT->notification(get_string('notimplemented', 'tool_replace'));
     echo $OUTPUT->footer();
     die;
 }
 
 if (!data_submitted() or !$search or !$replace or !confirm_sesskey() or !$sure) {   /// Print a form
-    echo $OUTPUT->notification('This script is not supported, always make complete backup before proceeding!<br />This operation can not be reverted!');
+    echo $OUTPUT->notification(get_string('notsupported', 'tool_replace'));
 
     echo $OUTPUT->box_start();
     echo '<div class="mdl-align">';
     echo '<form action="index.php" method="post"><div>';
     echo '<input type="hidden" name="sesskey" value="'.sesskey().'" />';
-    echo '<div><label for="search">Search whole database for: </label><input id="search" type="text" name="search" size="40" /> (usually previous server URL)</div>';
-    echo '<div><label for="replace">Replace with this string: </label><input type="text" id="replace" name="replace" size="40" /> (usually new server URL)</div>';
-    echo '<div><label for="sure">I understand the risks of this operation: </label><input type="checkbox" id="sure" name="sure" value="1" /></div>';
+    echo '<div><label for="search">'.get_string('searchwholedb', 'tool_replace').
+            ' </label><input id="search" type="text" name="search" size="40" /> ('.
+            get_string('searchwholedbhelp', 'tool_replace').')</div>';
+    echo '<div><label for="replace">'.get_string('replacewith', 'tool_replace').
+            ' </label><input type="text" id="replace" name="replace" size="40" /> ('.
+            get_string('replacewithhelp', 'tool_replace').')</div>';
+    echo '<div><label for="sure">'.get_string('disclaimer', 'tool_replace').' </label><input type="checkbox" id="sure" name="sure" value="1" /></div>';
     echo '<div class="buttons"><input type="submit" class="singlebutton" value="Yes, do it now" /></div>';
     echo '</div></form>';
     echo '</div>';
@@ -70,9 +74,9 @@ db_replace($search, $replace);
 echo $OUTPUT->box_end();
 
 /// Rebuild course cache which might be incorrect now
-echo $OUTPUT->notification('Rebuilding course cache...', 'notifysuccess');
+echo $OUTPUT->notification(get_string('notifyrebuilding', 'tool_replace'), 'notifysuccess');
 rebuild_course_cache();
-echo $OUTPUT->notification('...finished', 'notifysuccess');
+echo $OUTPUT->notification(get_string('notifyfinished', 'tool_replace'), 'notifysuccess');
 
 echo $OUTPUT->continue_button(new moodle_url('/admin/index.php'));
 
