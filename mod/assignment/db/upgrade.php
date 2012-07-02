d0c33d06a3418c237a44c6d48f12c04de0467de9
Rossiani Wijaya <rwijaya@moodle.com>
2012-07-02 14:58:33 +0800

    MDL-31987 Assignment module: fixed upgrade version

--- mod/assignment/db/upgrade.php
+++ mod/assignment/db/upgrade.php
@@ -157,7 +157,7 @@ function xmldb_assignment_upgrade($oldversion) {
     // Moodle v2.1.0 release upgrade line
     // Put any upgrade step following this
 
-    if ($oldversion < 2012062800) {
+    if ($oldversion < 2010102601) {
         // Fixed/updated numfiles field in assignment_submissions table to count the actual
         // number of files has been uploaded when sendformarking is disabled
         upgrade_set_timeout(600); // increase excution time for in large sites
@@ -190,7 +190,7 @@ function xmldb_assignment_upgrade($oldversion) {
         }
         $submissions->close();
         // assignment savepoint reached
-        upgrade_mod_savepoint(true, 2012062800, 'assignment');
+        upgrade_mod_savepoint(true, 2010102601, 'assignment');
     }
 
     return true;
