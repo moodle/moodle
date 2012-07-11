ed047dabc90d67e002402c73068774d7696ce359
Petr Škoda <commits@skodak.org>
2012-07-11 15:54:03 +0200

    MDL-34271 cleanup mysql engine hack before adding similar collation hack

--- lib/ddl/mysql_sql_generator.php
+++ lib/ddl/mysql_sql_generator.php
@@ -117,23 +117,16 @@ class mysql_sql_generator extends sql_generator {
      * by any of its comments, indexes and sequence creation SQL statements.
      */
     public function getCreateTableSQL($xmldb_table) {
-        // first find out if want some special db engine
-        $engine = null;
-        if (method_exists($this->mdb, 'get_dbengine')) {
-            $engine = $this->mdb->get_dbengine();
-        }
-
+        // First find out if want some special db engine.
+        $engine = $this->mdb->get_dbengine();
         $sqlarr = parent::getCreateTableSQL($xmldb_table);
 
-        if (!$engine) {
-            // we rely on database defaults
-            return $sqlarr;
-        }
-
-        // let's inject the engine into SQL
+        // Let's inject the extra MySQL tweaks.
         foreach ($sqlarr as $i=>$sql) {
             if (strpos($sql, 'CREATE TABLE ') === 0) {
-                $sqlarr[$i] .= " ENGINE = $engine";
+                if ($engine) {
+                    $sqlarr[$i] .= " ENGINE = $engine";
+                }
             }
         }
 
