c81f332876e79ab71db930ced89fdd7732554f30
Marina Glancy <marina@moodle.com>
2012-07-13 16:02:21 +0800

    MDL-34221 disabledIf support for filemanager and filepicker elements

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -2050,7 +2050,7 @@ $icon_progress
 </div>
 <div id="filepicker-wrapper-{$client_id}" class="mdl-left" style="display:none">
     <div>
-        <input type="button" id="filepicker-button-{$client_id}" value="{$straddfile}"{$buttonname}/>
+        <input type="button" class="fp-btn-choose" id="filepicker-button-{$client_id}" value="{$straddfile}"{$buttonname}/>
         <span> $maxsize </span>
     </div>
 EOD;
