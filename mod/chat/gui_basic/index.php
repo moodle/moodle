fef7025c5e3bb5b130d2f91fe1f3547ac827727b
Andrew Davis <andrew@moodle.com>
2012-07-17 10:15:14 +0800

    MDL-32665 mod_chat: disabled popup notifications in the chat windows

--- mod/chat/gui_basic/index.php
+++ mod/chat/gui_basic/index.php
@@ -44,6 +44,7 @@ $context = get_context_instance(CONTEXT_MODULE, $cm->id);
 require_login($course, false, $cm);
 require_capability('mod/chat:chat', $context);
 $PAGE->set_pagelayout('base');
+$PAGE->set_popup_notification_allowed(false); // No popup notifications in the chat window
 
 /// Check to see if groups are being used here
  if ($groupmode = groups_get_activity_groupmode($cm)) {   // Groups are being used
