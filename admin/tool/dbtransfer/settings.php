c8e5d9c9eb19e88604df84fa4c2cf6d7a227d426
Petr Škoda <commits@skodak.org>
2012-07-22 11:01:01 +0200

    MDL-34441 show db transfer in experimental section
    DB export and import is not ready yet, sorry.

--- admin/tool/dbtransfer/settings.php
+++ admin/tool/dbtransfer/settings.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($hassiteconfig) {
-    // DB transfer related pages
-    $ADMIN->add('experimental', new admin_externalpage('tooldbtransfer', get_string('dbtransfer', 'tool_dbtransfer'), $CFG->wwwroot.'/'.$CFG->admin.'/tool/dbtransfer/index.php', 'moodle/site:config', true));
+    $ADMIN->add('experimental', new admin_externalpage('tooldbtransfer', get_string('dbtransfer', 'tool_dbtransfer'), $CFG->wwwroot.'/'.$CFG->admin.'/tool/dbtransfer/index.php', 'moodle/site:config', false));
+    // DB export/import is not ready yet - keep it hidden for now.
     $ADMIN->add('experimental', new admin_externalpage('tooldbexport', get_string('dbexport', 'tool_dbtransfer'), $CFG->wwwroot.'/'.$CFG->admin.'/tool/dbtransfer/dbexport.php', 'moodle/site:config', true));
 }
