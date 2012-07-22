9e8b02e658f25740c4c75a167c0c8868969ea170
Petr Škoda <commits@skodak.org>
2012-07-22 21:01:31 +0200

    MDL-34441  bump up version to trigger cache purging

--- admin/tool/dbtransfer/version.php
+++ admin/tool/dbtransfer/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012061700; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012062200; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012061700; // Requires this Moodle version
 $plugin->component = 'tool_dbtransfer'; // Full name of the plugin (used for diagnostics)
