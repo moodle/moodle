6fd7b70f4b6ed5f1ef0e88412c2e23fc5745b132
Ankit Agarwal <ankit@moodle.com>
2012-07-23 14:22:34 +0800

    MDL-34470 url: Replace all instances of get_context_instance() with context_xxx::instance() in mod/url

--- mod/url/view.php
+++ mod/url/view.php
@@ -44,7 +44,7 @@ if ($u) {  // Two ways to specify the module
 $course = $DB->get_record('course', array('id'=>$cm->course), '*', MUST_EXIST);
 
 require_course_login($course, true, $cm);
-$context = get_context_instance(CONTEXT_MODULE, $cm->id);
+$context = context_module::instance($cm->id);
 require_capability('mod/url:view', $context);
 
 add_to_log($course->id, 'url', 'view', 'view.php?id='.$cm->id, $url->id, $cm->id);
