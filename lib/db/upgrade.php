b97532dd8e5164c00bebbba50c0fabfdb4eb9bb4
Petr Škoda <commits@skodak.org>
2012-07-27 10:49:16 +0200

    MDL-34600 fix question table to match install.xml
    The oldquestiontextformat was used during previous upgrade and is not necessary any more.
    Next time we should add these temporary upgrade columns to install.xml files.

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -937,5 +937,18 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2012062501.06);
     }
 
+    if ($oldversion < 2012062501.08) {
+        // Drop obsolete question upgrade field that should have been added to the install.xml.
+        $table = new xmldb_table('question');
+        $field = new xmldb_field('oldquestiontextformat', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0');
+
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        upgrade_main_savepoint(true, 2012062501.08);
+    }
+
+    
     return true;
 }
