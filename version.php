41499f832b4adcf7e72cd4bbd9b939efcd3adac0
Ankit Agarwal <ankit@moodle.com>
2012-07-27 11:50:37 +0800

    MDL-32827 calendar: Bumping the version

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$version  = 2012062501.05;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2012062501.06;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
