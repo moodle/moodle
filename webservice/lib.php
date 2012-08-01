b093fd5e5a691b2b6f074bab39b91489a7a88cc8
Petr Škoda <commits@skodak.org>
2012-08-01 08:30:28 +0200

    MDL-34368 fix another validuntil condition

--- webservice/lib.php
+++ webservice/lib.php
@@ -1612,7 +1612,7 @@ abstract class webservice_base_server extends webservice_server {
                   FROM {external_services} s
                   JOIN {external_services_functions} sf ON (sf.externalserviceid = s.id AND s.restrictedusers = 1 AND sf.functionname = :name2)
                   JOIN {external_services_users} su ON (su.externalserviceid = s.id AND su.userid = :userid)
-                 WHERE s.enabled = 1 AND su.validuntil IS NULL OR su.validuntil < :now $wscond2";
+                 WHERE s.enabled = 1 AND (su.validuntil IS NULL OR su.validuntil < :now) $wscond2";
         $params = array_merge($params, array('userid'=>$USER->id, 'name1'=>$function->name, 'name2'=>$function->name, 'now'=>time()));
 
         $rs = $DB->get_recordset_sql($sql, $params);
