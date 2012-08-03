64171aac704279f038afa5e04b3558b3a6ec22f3
Rajesh Taneja <rajesh@moodle.com>
2012-08-03 11:47:44 +0800

    MDL-30792 Files API: Cleaner approach to get maxbytes size in filepicker

--- repository/repository_ajax.php
+++ repository/repository_ajax.php
@@ -87,11 +87,8 @@ $coursemaxbytes = 0;
 if (!empty($course)) {
    $coursemaxbytes = $course->maxbytes;
 }
-$moodle_maxbytes = get_max_upload_file_size($CFG->maxbytes, $coursemaxbytes);
-// to prevent maxbytes greater than moodle maxbytes setting
-if (($maxbytes <= 0) || ($maxbytes >= $moodle_maxbytes)) {
-    $maxbytes = $moodle_maxbytes;
-}
+// Make sure maxbytes passed is within site filesize limits.
+$maxbytes = get_max_upload_file_size($CFG->maxbytes, $coursemaxbytes, $maxbytes);
 
 /// Wait as long as it takes for this script to finish
 set_time_limit(0);
