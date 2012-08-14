d190fcd9d95772c9ba287a16b5159719d6022c77
Frederic Massart <fred@moodle.com>
2012-08-14 11:25:06 +0800

    MDL-28531 Unit Tests: Fixed tests to work with core update

--- lib/tests/moodlelib_test.php
+++ lib/tests/moodlelib_test.php
@@ -1549,9 +1549,9 @@ class moodlelib_testcase extends advanced_testcase {
                 'hour' => '10',
                 'minutes' => '00',
                 'seconds' => '00',
-                'timezone' => '0.0', //no dst offset
-                'applydst' => false,
-                'expectedoutput' => '1309528800'
+                'timezone' => '0.0',
+                'applydst' => false, //no dst offset
+                'expectedoutput' => '1309514400' // 6pm at UTC+0
             ),
             array(
                 'usertimezone' => 'America/Moncton',
