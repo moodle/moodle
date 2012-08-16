1f8e5648fb7d4cb5cb8b3a39d6b5514073b494d7
Tim Hunt <T.J.Hunt@open.ac.uk>
2012-08-16 16:38:16 +0200

    MDL-47494 ddimageortext: Fix required version.

--- question/type/ddimageortext/version.php
+++ question/type/ddimageortext/version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 
 $plugin->version   = 2012081600;
-$plugin->requires  = 2011120500;
+$plugin->requires  = 2012062500;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_ddimageortext';
 $plugin->maturity  = MATURITY_STABLE;
