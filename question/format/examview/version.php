06d9aa79b8d1036af52e03ce19cc62f10e88bbf0
Dan Poltawski <dan@moodle.com>
2012-08-21 11:06:23 +0800

    MDL-34808 qformat_examview - increment version

--- question/format/examview/version.php
+++ question/format/examview/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qformat_examview';
-$plugin->version   = 2011102700;
+$plugin->version   = 2011102701;
 
 $plugin->requires  = 2011102700;
 
