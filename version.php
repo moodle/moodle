c62ccd747c182e5d1c5f341ec7e96f877b40855d
Andrew Davis <andrew@moodle.com>
2012-08-28 10:41:02 +0800

    MDL-26504 blog: added upgrade code to remove orphan external blog posts
    Conflicts:
    	lib/db/upgrade.php
    	version.php

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$version  = 2012062501.12;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2012062501.13;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
