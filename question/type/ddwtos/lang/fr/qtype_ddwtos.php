227b7cb8cd2df2befe11d4a9112d8d5b8a7122c7
Nicolas Dunand <nico@kolya.ch>
2012-08-30 14:42:52 +0200

    MDL-47494 ddwtos: Added French translation

---
+++ question/type/ddwtos/lang/fr/qtype_ddwtos.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package    qtype_ddwtos
+ * @copyright  2011 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addmorechoiceblanks'] = 'Vides pour {no} choix supplémentaires';
+$string['answer'] = 'Réponse';
+$string['correctansweris'] = 'La réponse correcte est : {$a}';
+$string['infinite'] = 'Infini';
+$string['pleaseputananswerineachbox'] = 'Veuillez entrer une réponse dans chaque boîte.';
+$string['pluginname'] = 'Glisser-déposer sur texte';
+$string['pluginname_help'] = 'Entrez une question du type "Le [[1]] saute par-dessus le [[2]]", puis entrez les différents mots possibles pour les intervalles 1 et 2.';
+$string['pluginname_link'] = 'question/type/ddwtos';
+$string['pluginnameadding'] = 'Ajouter un glisser-déposer sur texte';
+$string['pluginnameediting'] = 'Editer un glisser-déposer sur texte';
+$string['pluginnamesummary'] = 'Glissez et déposez des mots pour combler les trous d\'un texte.';
