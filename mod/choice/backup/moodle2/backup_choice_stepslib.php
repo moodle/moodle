cfa50637771cbfd7ab452df406cfab14a2241dd4
Dan Marsden <dan@danmarsden.com>
2012-09-04 11:01:06 +1200

    MDL-35208 mod_choice: respect order of options when running backup

--- mod/choice/backup/moodle2/backup_choice_stepslib.php
+++ mod/choice/backup/moodle2/backup_choice_stepslib.php
@@ -65,8 +65,9 @@ class backup_choice_activity_structure_step extends backup_activity_structure_st
 
         $option->set_source_sql('
             SELECT *
-              FROM {choice_options}
-             WHERE choiceid = ?',
+            FROM {choice_options}
+            WHERE choiceid = ?
+            ORDER BY id',
             array(backup::VAR_PARENTID));
 
         // All the rest of elements only happen if we are including user info
