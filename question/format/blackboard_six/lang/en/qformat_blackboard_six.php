1c3d93d3ca40af4d7043bc97a2bc422bb2175787
David Mudrák <david@moodle.com>
2012-09-05 14:05:36 +0200

    MDL-35272 Fix typo in qformat_blackboard_six.php
    Credit goes to German Valero for spotting and reporting this.

--- question/format/blackboard_six/lang/en/qformat_blackboard_six.php
+++ question/format/blackboard_six/lang/en/qformat_blackboard_six.php
@@ -25,10 +25,10 @@
 $string['defaultname'] = 'Imported question {$a}';
 $string['errormanifest'] = 'Error while parsing the IMS manifest document';
 $string['importnotext'] = 'Missing question text in XML file';
-$string['filenothandled'] = 'This archive contains reference to a file material {$a} wich is not currently handled by import';
+$string['filenothandled'] = 'This archive contains reference to a file material {$a} which is not currently handled by import';
 $string['imagenotfound'] = 'Image file with path {$a} was not found in the import.';
 $string['notenoughtsubans'] = 'Unable to import matching question \'{$a}\' because a matching question must comprise at least two questions and three answers.';
 $string['pluginname'] = 'Blackboard V6+';
 $string['pluginname_help'] = 'Blackboard V6+ format enables questions saved in all Blackboard export formats to be imported via a dat or zip file. For zip files, images import is supported.';
-$string['unhandledpresblock'] = 'Unhandled presentation bloc';
+$string['unhandledpresblock'] = 'Unhandled presentation block';
 $string['unknownorunhandledtype'] = 'Unknown or unhandled question type: {$a}';
