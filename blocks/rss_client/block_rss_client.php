35cc173d46ff4be0c06eb0b414a46398496e1128
Charles Fulton <mackensen@gmail.com>
2012-09-10 12:31:39 -0700

    MDL-32230 block_rss_client: remove extra cleaning step

--- blocks/rss_client/block_rss_client.php
+++ blocks/rss_client/block_rss_client.php
@@ -239,7 +239,7 @@
 
         $r = html_writer::start_tag('li');
             $r.= html_writer::start_tag('div',array('class'=>'link'));
-                $r.= html_writer::link(clean_param($link,PARAM_URL), s($title), array('onclick'=>'this.target="_blank"'));
+                $r.= html_writer::link($link, s($title), array('onclick'=>'this.target="_blank"'));
             $r.= html_writer::end_tag('div');
 
             if($this->config->display_description && !empty($description)){
