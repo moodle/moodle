6b43759eff5de266f21f681f1d1a1dae6fb13da2
Rex Lorenzo <rex@oid.ucla.edu>
2012-09-11 12:27:17 -0700

    MDL-35374 - Cannot delete blocks off frontpage

--- lib/blocklib.php
+++ lib/blocklib.php
@@ -1158,7 +1158,7 @@ class block_manager {
             $PAGE->set_title($blocktitle . ': ' . $strdeletecheck);
             $PAGE->set_heading($site->fullname);
             echo $OUTPUT->header();
-            $confirmurl = new moodle_url("$deletepage->url?", array('sesskey' => sesskey(), 'bui_deleteid' => $block->instance->id, 'bui_confirm' => 1));
+            $confirmurl = new moodle_url($deletepage->url, array('sesskey' => sesskey(), 'bui_deleteid' => $block->instance->id, 'bui_confirm' => 1));
             $cancelurl = new moodle_url($deletepage->url);
             $yesbutton = new single_button($confirmurl, get_string('yes'));
             $nobutton = new single_button($cancelurl, get_string('no'));
