3b3fc5cc76ac3cfe0bdff13478a03ba96ce7f121
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2012-09-19 17:24:38 +0200

    MDL-35517 course: Handle non-existing $course->maxbytes on course creation.

--- course/edit_form.php
+++ course/edit_form.php
@@ -156,7 +156,11 @@ class course_edit_form extends moodleform {
         $mform->addHelpButton('showreports', 'showreports');
         $mform->setDefault('showreports', $courseconfig->showreports);
 
-        $choices = get_max_upload_sizes($CFG->maxbytes, 0, 0, $course->maxbytes);
+        // Handle non-existing $course->maxbytes on course creation.
+        $coursemaxbytes = !isset($course->maxbytes) ? null : $course->maxbytes;
+
+        // Let's prepare the maxbytes popup.
+        $choices = get_max_upload_sizes($CFG->maxbytes, 0, 0, $coursemaxbytes);
         $mform->addElement('select', 'maxbytes', get_string('maximumupload'), $choices);
         $mform->addHelpButton('maxbytes', 'maximumupload');
         $mform->setDefault('maxbytes', $courseconfig->maxbytes);
