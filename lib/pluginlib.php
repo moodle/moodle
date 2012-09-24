c5bc14690f36ee88872d442972197e8dd224dc9d
Dan Poltawski <dan@moodle.com>
2012-09-24 11:26:30 +0800

    Revert "MDL-35442 Add settings and uninstall link for local plugins"
    This doesn't work on 2.2.
    
    This reverts commit 7fa1f180d62fee83b0627d8b113a0788c9f5cc5c.

--- lib/pluginlib.php
+++ lib/pluginlib.php
@@ -1689,22 +1689,3 @@ class plugintype_report extends plugintype_base implements plugin_information {
         return new moodle_url('/admin/reports.php', array('delete' => $this->name, 'sesskey' => sesskey()));
     }
 }
-
-
-/**
- * Class for local plugins
- */
-class plugininfo_local extends plugininfo_base {
-
-    public function get_uninstall_url() {
-        return new moodle_url('/admin/localplugins.php', array('delete' => $this->name, 'sesskey' => sesskey()));
-    }
-
-    public function get_settings_url() {
-        if (file_exists($this->full_path('settings.php'))) {
-            return new moodle_url('/admin/settings.php', array('section' => 'local_' . $this->name));
-        } else {
-            return parent::get_settings_url();
-        }
-    }
-}
