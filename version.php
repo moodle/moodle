b95e21b8c5a75d4cbe31c6c902bf5cd25485b9f4
Dan Poltawski <dan@moodle.com>
2012-09-26 12:08:11 +0800

    MDL-15471 - oops messed up version number update

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$version  = 2012092100.06;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2012092600.00;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
