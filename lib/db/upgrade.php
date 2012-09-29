bac188fa712f28e4370a1c1c26647cefb746b080
Frederic Massart <fred@moodle.com>
2012-09-29 08:33:49 +0200

    MDL-35622 files: Add support for SMART Notebook MIME types
    - The MIME type for Notebook files is: application/x-smarttech-notebook
    - The MIME type for Gallery files is also: application/x-smarttech-notebook
    Refs: http://exchange-forum.smarttech.com/forums/p/5152/11945.aspx#11945
    
    Original author: Matteo Scaramuccia <moodle@matteoscaramuccia.com>
    Fixed by: Paul Holden <pholden@greenhead.ac.uk>
    
    Conflicts:
    
    	lib/db/upgrade.php
    	version.php

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -1524,5 +1524,23 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2012120300.04);
     }
 
+    if ($oldversion < 2012120301.07) {
+        // Fixing possible wrong MIME types for SMART Notebook files.
+        $extensions = array('%.gallery', '%.galleryitem', '%.gallerycollection', '%.nbk', '%.notebook', '%.xbk');
+        $select = $DB->sql_like('filename', '?', false);
+        foreach ($extensions as $extension) {
+            $DB->set_field_select(
+                'files',
+                'mimetype',
+                'application/x-smarttech-notebook',
+                $select,
+                array($extension)
+            );
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2012120301.07);
+    }
+
     return true;
 }
