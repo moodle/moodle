d0c05602f569706516dd5439e7d3b0c2f271b506
Marina Glancy <marina@moodle.com>
2012-10-02 14:16:54 +0800

    MDL-35263 removed unit test test_format_display_course_content()
    since the function global_navigation::format_display_course_content() is removed

--- lib/tests/navigationlib_test.php
+++ lib/tests/navigationlib_test.php
@@ -354,11 +354,6 @@ class global_navigation_testcase extends basic_testcase {
         $res3 = $section2->add('resource 3', null, navigation_node::TYPE_RESOURCE, null, 'res3');
     }
 
-    public function test_format_display_course_content() {
-        $this->assertTrue($this->node->exposed_format_display_course_content('topic'));
-        $this->assertFalse($this->node->exposed_format_display_course_content('scorm'));
-        $this->assertTrue($this->node->exposed_format_display_course_content('dummy'));
-    }
     public function test_module_extends_navigation() {
         $this->assertTrue($this->node->exposed_module_extends_navigation('data'));
         $this->assertFalse($this->node->exposed_module_extends_navigation('test1'));
