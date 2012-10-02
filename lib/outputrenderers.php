bbb553e077501dbb2ecddb676c1e285def796c31
Frederic Massart <fred@moodle.com>
2012-10-02 14:28:55 +0800

    MDL-28235 form: Help button popup close accessibility improved
    AMOS BEGIN
     CPY [close,editor],[close,form]
    AMOS END

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -1733,6 +1733,7 @@ class core_renderer extends renderer_base {
         $output = html_writer::tag('a', $output, $attributes);
 
         $this->page->requires->js_init_call('M.util.help_icon.add', array(array('id'=>$id, 'url'=>$url->out(false))));
+        $this->page->requires->string_for_js('close', 'form');
 
         // and finally span
         return html_writer::tag('span', $output, array('class' => 'helplink'));
@@ -1798,6 +1799,7 @@ class core_renderer extends renderer_base {
         $output = html_writer::tag('a', $output, $attributes);
 
         $this->page->requires->js_init_call('M.util.help_icon.add', array(array('id'=>$id, 'url'=>$url->out(false))));
+        $this->page->requires->string_for_js('close', 'form');
 
         // and finally span
         return html_writer::tag('span', $output, array('class' => 'helplink'));
