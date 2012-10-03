91813c1b4bf195e91933c780a716f6c2104d5c0e
Davo Smith <git@davosmith.co.uk>
2012-10-03 08:46:55 +0100

    MDL-35288 drag and drop upload - adjust error message text when attempting to upload a folder

--- repository/upload/lang/en/repository_upload.php
+++ repository/upload/lang/en/repository_upload.php
@@ -34,4 +34,4 @@ $string['upload_error_no_file'] = 'No file was uploaded.';
 $string['upload_error_no_tmp_dir'] = 'PHP is missing a temporary folder.';
 $string['upload_error_cant_write'] = 'Failed to write file to disk.';
 $string['upload_error_extension'] = 'A PHP extension stopped the file upload.';
-$string['upload_error_invalid_file'] = 'The file \'{$a}\' has no data in it - did you try to upload a folder?';
+$string['upload_error_invalid_file'] = 'The file \'{$a}\' is either empty or a folder. To upload folders zip them first.';
