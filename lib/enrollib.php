a1cedcc987373bc3cb6e9414478db3a620bb8f54
Petr Škoda <commits@skodak.org>
2012-10-06 19:22:07 +0200

    MDL-35839 fix invalid enrol instance delete cleanup

--- lib/enrollib.php
+++ lib/enrollib.php
@@ -1627,8 +1627,8 @@ abstract class enrol_plugin {
         $participants->close();
 
         // now clean up all remainders that were not removed correctly
-        $DB->delete_records('groups_members', array('itemid'=>$instance->id, 'component'=>$name));
-        $DB->delete_records('role_assignments', array('itemid'=>$instance->id, 'component'=>$name));
+        $DB->delete_records('groups_members', array('itemid'=>$instance->id, 'component'=>'enrol_'.$name));
+        $DB->delete_records('role_assignments', array('itemid'=>$instance->id, 'component'=>'enrol_'.$name));
         $DB->delete_records('user_enrolments', array('enrolid'=>$instance->id));
 
         // finally drop the enrol row
