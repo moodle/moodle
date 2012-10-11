c67f1a342cdde56bb58051527d94c46fa8536a27
Matteo Scaramuccia <moodle@matteoscaramuccia.com>
2012-10-11 22:34:30 +0200

    MDL-35990 Repository Library should not include the main config.php file

--- repository/lib.php
+++ repository/lib.php
@@ -23,7 +23,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(dirname(dirname(__FILE__)) . '/config.php');
 require_once($CFG->libdir . '/filelib.php');
 require_once($CFG->libdir . '/formslib.php');
 
