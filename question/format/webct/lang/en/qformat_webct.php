398dc6a3abf80764f0a65305473f47de73ff9bb9
Jean-Michel Vedrine <vedrine@vedrine.org>
2012-10-28 15:36:04 +0100

    MDL-30001 WEBCT Import does not work for 2.0

--- question/format/webct/lang/en/qformat_webct.php
+++ question/format/webct/lang/en/qformat_webct.php
@@ -25,11 +25,10 @@
 $string['errorsdetected'] = '{$a} error(s) detected';
 $string['missinganswer'] = 'Too few :ANSWER, :Lx, :Rx statements for question line {$a}. You must define at last 2 possible answers';
 $string['missingquestion'] = 'Missing question label after line {$a}';
-$string['paragraphquestion'] = 'Paragraph question';
 $string['pluginname'] = 'WebCT format';
 $string['pluginname_help'] = 'WebCT format enables multiple-choice and short answer questions saved in WebCT\'s text-based format to be imported.';
 $string['pluginname_link'] = 'qformat/webct';
 $string['questionnametoolong'] = 'Question name too long at line {$a} (255 char. max). It has been truncated.';
-$string['unknowntype'] = 'Unknown type';
+$string['unknowntype'] = 'Unknown question type after line {$a}';
 $string['warningsdetected'] = '{$a} warning(s) detected';
 $string['wronggrade'] = 'Wrong grade (after line {$a}) :';
