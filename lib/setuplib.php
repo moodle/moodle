825876620b5f084eb51613abc4ae89e497ea0ddf
Dan Poltawski <dan@moodle.com>
2012-11-05 14:25:24 +0800

    MDL-35768 - increment redirect_if_major_upgrade_required

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -1126,7 +1126,7 @@ function disable_output_buffering() {
  */
 function redirect_if_major_upgrade_required() {
     global $CFG;
-    $lastmajordbchanges = 2012051700;
+    $lastmajordbchanges = 2012110201;
     if (empty($CFG->version) or (int)$CFG->version < $lastmajordbchanges or
             during_initial_install() or !empty($CFG->adminsetuppending)) {
         try {
