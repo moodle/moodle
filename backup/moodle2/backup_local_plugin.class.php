c7b05a1d1bbc718f56fa726eff072f3e367986b7
Mark Nielsen <mark@moodlerooms.com>
2012-11-06 15:37:08 -0800

    MDL-17444: Backup hooks for local plugin (CR)
    Code review changes
    * Fixed PHPDoc
    * Fixed missing MOODLE_INTERNAL check

--- backup/moodle2/backup_local_plugin.class.php
+++ backup/moodle2/backup_local_plugin.class.php
@@ -16,15 +16,19 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package    core
- * @subpackage backup-moodle2
- * @copyright  2011 David Mudrak <david@moodle.com>
+ * Defines backup_local_plugin class
+ *
+ * @package    core_backup
+ * @subpackage moodle2
+ * @category   backup
+ * @copyright  2011 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Base class for local plugins
+ * Class extending standard backup_plugin in order to implement some
+ * helper methods related with the local plugins
  */
 abstract class backup_local_plugin extends backup_plugin {}
--- backup/moodle2/restore_local_plugin.class.php
+++ backup/moodle2/restore_local_plugin.class.php
@@ -16,15 +16,19 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package    moodlecore
- * @subpackage backup-moodle2
+ * Defines restore_local_plugin class
+ *
+ * @package    core_backup
+ * @subpackage moodle2
+ * @category   backup
  * @copyright  2011 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 /**
  * Class extending standard restore_plugin in order to implement some
  * helper methods related with local plugins
- *
  */
 abstract class restore_local_plugin extends restore_plugin {}
