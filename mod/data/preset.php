590e3ef0ee72ef245e4c79b5ccc9ed173bd17f59
Adrian Greeve <adrian@moodle.com>
2012-11-08 17:38:57 +0800

    MDL-36453 - mod_data: Presets now load in a Windows environment. Thanks to Michael de Raadt for this patch.

--- mod/data/preset.php
+++ mod/data/preset.php
@@ -216,7 +216,7 @@ if (optional_param('sesskey', false, PARAM_BOOL) && confirm_sesskey()) {
         } else if ($action == 'finishimport') {
             $overwritesettings = optional_param('overwritesettings', false, PARAM_BOOL);
             if (!$fullname) {
-                $presetdir = $CFG->tempdir.'/forms/'.required_param('directory', PARAM_ALPHANUMEXT);
+                $presetdir = $CFG->tempdir.'/forms/'.required_param('directory', PARAM_FILE);
                 if (!file_exists($presetdir) || !is_dir($presetdir)) {
                     print_error('cannotimport');
                 }
