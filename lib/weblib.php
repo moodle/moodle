791922735fa7ebdb3baf11dda5e6e31885b3b27a
Petr Škoda <commits@skodak.org>
2012-11-10 11:47:09 +0100

    MDL-36487 do not use IE=edge header on pages that have custom X-UA-Compatible
    Thanks Dan Poltawski for spotting this regression!

--- lib/weblib.php
+++ lib/weblib.php
@@ -1848,7 +1848,10 @@ function send_headers($contenttype, $cacheable = true) {
     @header('Content-Type: ' . $contenttype);
     @header('Content-Script-Type: text/javascript');
     @header('Content-Style-Type: text/css');
-    @header('X-UA-Compatible: IE=edge');
+
+    if (empty($CFG->additionalhtmlhead) or stripos($CFG->additionalhtmlhead, 'X-UA-Compatible') === false) {
+        @header('X-UA-Compatible: IE=edge');
+    }
 
     if ($cacheable) {
         // Allow caching on "back" (but not on normal clicks)
