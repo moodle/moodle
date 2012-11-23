eb48b61abb414734d2c92da38e55d0016951b6cd
Dan Poltawski <dan@moodle.com>
2012-11-23 15:21:00 +0800

    On demand release 2.4beta+

--- version.php
+++ version.php
@@ -30,11 +30,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$version  = 2012112100.00;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2012112300.00;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
-$release  = '2.4beta+ (Build: 20121121)'; // Human-friendly version name
+$release  = '2.4beta+ (Build: 20121123)'; // Human-friendly version name
 
 $branch   = '24';                       // this version's branch
 $maturity = MATURITY_BETA;             // this version's maturity level
