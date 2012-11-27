47acfc3db95f3483e5fbb7e9916c45dfa9309504
Mark Nelson <markn@moodle.com>
2012-11-27 15:58:30 +0800

    MDL-36865 filepicker: ensuring the dnd arrow displays in the center of the filepicker for all supported browsers

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -2070,9 +2070,9 @@ EOD;
             $html .= <<<EOD
     <div id="file_info_{$client_id}" class="mdl-left filepicker-filelist" style="position: relative">
     <div class="filepicker-filename">
-        <div class="filepicker-container">$currentfile<span class="dndupload-message">$strdndenabled <br/><span class="dndupload-arrow"></span></span></div>
+        <div class="filepicker-container">$currentfile<div class="dndupload-message">$strdndenabled <br/><div class="dndupload-arrow"></div></div></div>
     </div>
-    <div><div class="dndupload-target">{$strdroptoupload}<br/><span class="dndupload-arrow"></span></div></div>
+    <div><div class="dndupload-target">{$strdroptoupload}<br/><div class="dndupload-arrow"></div></div></div>
     </div>
 EOD;
         }
