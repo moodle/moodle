beae3ea2b9f9b238a96f9d728e550fbdfb4ab6a9
Dylan Vassallo <dylanvassallo@gmail.com>
2012-11-27 17:00:00 -0800

    Add the db/access.php file required for contributed blocks

---
+++ blocks/roster/db/access.php
@@ -0,0 +1,27 @@
+<?php
+
+$capabilities = array(
+
+    'block/roster:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/roster:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
