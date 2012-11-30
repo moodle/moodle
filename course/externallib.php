d872b3dc1639aa89af7bf308ee13163f77a6ce3c
Jerome Mouneyrac <jerome@mouneyrac.com>
2012-11-30 14:22:16 +0800

    MDL-30961 get_course_contents: returned activity name should not be check against PARAM_TEXT, it could be anything

--- course/externallib.php
+++ course/externallib.php
@@ -212,7 +212,7 @@ class core_course_external extends external_api {
                                 array(
                                     'id' => new external_value(PARAM_INT, 'activity id'),
                                     'url' => new external_value(PARAM_URL, 'activity url', VALUE_OPTIONAL),
-                                    'name' => new external_value(PARAM_TEXT, 'activity module name'),
+                                    'name' => new external_value(PARAM_RAW, 'activity module name'),
                                     'description' => new external_value(PARAM_RAW, 'activity description', VALUE_OPTIONAL),
                                     'visible' => new external_value(PARAM_INT, 'is the module visible', VALUE_OPTIONAL),
                                     'modicon' => new external_value(PARAM_URL, 'activity icon url'),
