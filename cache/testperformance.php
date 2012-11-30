f0ce225c128c186d9a6a941cc251f911b57f2afc
Sam Hemelryk <sam@moodle.com>
2012-11-30 14:36:57 +1300

    MDL-36938 cache: test performance script checks requirements now

--- cache/testperformance.php
+++ cache/testperformance.php
@@ -61,7 +61,7 @@ foreach (get_plugin_list_with_file('cachestore', 'lib.php', true) as $plugin => 
     $class = 'cachestore_'.$plugin;
     $plugin = get_string('pluginname', 'cachestore_'.$plugin);
 
-    if (!class_exists($class) || !method_exists($class, 'initialise_test_instance')) {
+    if (!class_exists($class) || !method_exists($class, 'initialise_test_instance') || !$class::are_requirements_met()) {
         $applicationtable->data[] = array($plugin, $strinvalidplugin, '-', '-', '-', '-');
         $sessiontable->data[] = array($plugin, $strinvalidplugin, '-', '-', '-', '-');
         $requesttable->data[] = array($plugin, $strinvalidplugin, '-', '-', '-', '-');
