03953061caf31059622849a260b6ced26ac8aaa8
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2012-12-03 21:11:33 +0100

    NOBUG: Bump backup relese for 2.5dev

--- backup/backup.class.php
+++ backup/backup.class.php
@@ -110,7 +110,7 @@ abstract class backup implements checksumable {
 
     // Version (to keep CFG->backup_version (and release) updated automatically)
     const VERSION = 2012112900;
-    const RELEASE = '2.4';
+    const RELEASE = '2.5';
 }
 
 /*
