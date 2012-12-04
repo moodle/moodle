257a5d1299a99c54dae3ee1f085cfbb2835de966
Jason Fowler <phalacee@gmail.com>
2012-12-04 11:29:01 +0800

    MDL-35884 - Course - Remove text from ALT attribute for activity chooser

--- course/lib.php
+++ course/lib.php
@@ -1830,7 +1830,7 @@ function print_section_add_menus($course, $section, $modnames = null, $vertical=
         // The module chooser link
         $modchooser = html_writer::start_tag('div', array('class' => 'mdl-right'));
         $modchooser.= html_writer::start_tag('div', array('class' => 'section-modchooser'));
-        $icon = $OUTPUT->pix_icon('t/add', $straddeither);
+        $icon = $OUTPUT->pix_icon('t/add', '');
         $span = html_writer::tag('span', $straddeither, array('class' => 'section-modchooser-text'));
         $modchooser .= html_writer::tag('span', $icon . $span, array('class' => 'section-modchooser-link'));
         $modchooser.= html_writer::end_tag('div');
