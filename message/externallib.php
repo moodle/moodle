d6731600c89157d8e9ac46be93bf84fdc818ec56
Frederic Massart <fred@moodle.com>
2012-12-13 16:25:36 +0800

    MDL-30070 message: Web Services for contact list

--- message/externallib.php
+++ message/externallib.php
@@ -185,6 +185,389 @@ class core_message_external extends external_api {
         );
     }
 
+    /**
+     * Create contacts parameters description.
+     *
+     * @return external_function_parameters
+     * @since 2.5
+     */
+    public static function create_contacts_parameters() {
+        return new external_function_parameters(
+            array(
+                'userids' => new external_multiple_structure(
+                    new external_value(PARAM_INT, 'User ID'),
+                    'List of user IDs'
+                )
+            )
+        );
+    }
+
+    /**
+     * Create contacts.
+     *
+     * @param array $userids array of user IDs.
+     * @return external_description
+     * @since 2.5
+     */
+    public static function create_contacts($userids) {
+        $params = array('userids' => $userids);
+        $params = self::validate_parameters(self::create_contacts_parameters(), $params);
+
+        $warnings = array();
+        foreach ($params['userids'] as $id) {
+            if (!message_add_contact($id)) {
+                $warnings[] = array(
+                    'item' => 'user',
+                    'itemid' => $id,
+                    'warningcode' => 'contactnotcreated',
+                    'message' => 'The contact could not be created'
+                );
+            }
+        }
+        return $warnings;
+    }
+
+    /**
+     * Create contacts return description.
+     *
+     * @return external_description
+     * @since 2.5
+     */
+    public static function create_contacts_returns() {
+        return new external_warnings();
+    }
+
+    /**
+     * Delete contacts parameters description.
+     *
+     * @return external_function_parameters
+     * @since 2.5
+     */
+    public static function delete_contacts_parameters() {
+        return new external_function_parameters(
+            array(
+                'userids' => new external_multiple_structure(
+                    new external_value(PARAM_INT, 'User ID'),
+                    'List of user IDs'
+                )
+            )
+        );
+    }
+
+    /**
+     * Delete contacts.
+     *
+     * @param array $userids array of user IDs.
+     * @return null
+     * @since 2.5
+     */
+    public static function delete_contacts($userids) {
+        $params = array('userids' => $userids);
+        $params = self::validate_parameters(self::delete_contacts_parameters(), $params);
+
+        foreach ($params['userids'] as $id) {
+            message_remove_contact($id);
+        }
+
+        return null;
+    }
+
+    /**
+     * Delete contacts return description.
+     *
+     * @return external_description
+     * @since 2.5
+     */
+    public static function delete_contacts_returns() {
+        return null;
+    }
+
+    /**
+     * Block contacts parameters description.
+     *
+     * @return external_function_parameters
+     * @since 2.5
+     */
+    public static function block_contacts_parameters() {
+        return new external_function_parameters(
+            array(
+                'userids' => new external_multiple_structure(
+                    new external_value(PARAM_INT, 'User ID'),
+                    'List of user IDs'
+                )
+            )
+        );
+    }
+
+    /**
+     * Block contacts.
+     *
+     * @param array $userids array of user IDs.
+     * @return external_description
+     * @since 2.5
+     */
+    public static function block_contacts($userids) {
+        $params = array('userids' => $userids);
+        $params = self::validate_parameters(self::block_contacts_parameters(), $params);
+
+        $warnings = array();
+        foreach ($params['userids'] as $id) {
+            if (!message_block_contact($id)) {
+                $warnings[] = array(
+                    'item' => 'user',
+                    'itemid' => $id,
+                    'warningcode' => 'contactnotblocked',
+                    'message' => 'The contact could not be blocked'
+                );
+            }
+        }
+        return $warnings;
+    }
+
+    /**
+     * Block contacts return description.
+     *
+     * @return external_description
+     * @since 2.5
+     */
+    public static function block_contacts_returns() {
+        return new external_warnings();
+    }
+
+    /**
+     * Unblock contacts parameters description.
+     *
+     * @return external_function_parameters
+     * @since 2.5
+     */
+    public static function unblock_contacts_parameters() {
+        return new external_function_parameters(
+            array(
+                'userids' => new external_multiple_structure(
+                    new external_value(PARAM_INT, 'User ID'),
+                    'List of user IDs'
+                )
+            )
+        );
+    }
+
+    /**
+     * Unblock contacts.
+     *
+     * @param array $userids array of user IDs.
+     * @return null
+     * @since 2.5
+     */
+    public static function unblock_contacts($userids) {
+        $params = array('userids' => $userids);
+        $params = self::validate_parameters(self::unblock_contacts_parameters(), $params);
+
+        foreach ($params['userids'] as $id) {
+            message_unblock_contact($id);
+        }
+
+        return null;
+    }
+
+    /**
+     * Unblock contacts return description.
+     *
+     * @return external_description
+     * @since 2.5
+     */
+    public static function unblock_contacts_returns() {
+        return null;
+    }
+
+    /**
+     * Get contacts parameters description.
+     *
+     * @return external_function_parameters
+     * @since 2.5
+     */
+    public static function get_contacts_parameters() {
+        return new external_function_parameters(array());
+    }
+
+    /**
+     * Get contacts.
+     *
+     * @param array $userids array of user IDs.
+     * @return external_description
+     * @since 2.5
+     */
+    public static function get_contacts() {
+        global $CFG;
+        require_once($CFG->dirroot . '/user/lib.php');
+
+        list($online, $offline, $strangers) = message_get_contacts();
+        $allcontacts = array('online' => $online, 'offline' => $offline, 'strangers' => $strangers);
+        foreach ($allcontacts as $mode => $contacts) {
+            foreach ($contacts as $key => $contact) {
+                $newcontact = array(
+                    'id' => $contact->id,
+                    'fullname' => fullname($contact),
+                    'unread' => $contact->messagecount
+                );
+
+                // Try to get the user picture, but sometimes this method can return null.
+                $userdetails = user_get_user_details($contact, null, array('profileimageurl', 'profileimageurlsmall'));
+                if (!empty($userdetails)) {
+                    $newcontact['profileimageurl'] = $userdetails['profileimageurl'];
+                    $newcontact['profileimageurlsmall'] = $userdetails['profileimageurlsmall'];
+                }
+
+                $allcontacts[$mode][$key] = $newcontact;
+            }
+        }
+        return $allcontacts;
+    }
+
+    /**
+     * Get contacts return description.
+     *
+     * @return external_description
+     * @since 2.5
+     */
+    public static function get_contacts_returns() {
+        return new external_single_structure(
+            array(
+                'online' => new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'id' => new external_value(PARAM_INT, 'User ID'),
+                            'fullname' => new external_value(PARAM_NOTAGS, 'User full name'),
+                            'profileimageurl' => new external_value(PARAM_URL, 'User picture URL', VALUE_OPTIONAL),
+                            'profileimageurlsmall' => new external_value(PARAM_URL, 'Small user picture URL', VALUE_OPTIONAL),
+                            'unread' => new external_value(PARAM_INT, 'Unread message count')
+                        )
+                    ),
+                    'List of online contacts'
+                ),
+                'offline' => new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'id' => new external_value(PARAM_INT, 'User ID'),
+                            'fullname' => new external_value(PARAM_NOTAGS, 'User full name'),
+                            'profileimageurl' => new external_value(PARAM_URL, 'User picture URL', VALUE_OPTIONAL),
+                            'profileimageurlsmall' => new external_value(PARAM_URL, 'Small user picture URL', VALUE_OPTIONAL),
+                            'unread' => new external_value(PARAM_INT, 'Unread message count')
+                        )
+                    ),
+                    'List of offline contacts'
+                ),
+                'strangers' => new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'id' => new external_value(PARAM_INT, 'User ID'),
+                            'fullname' => new external_value(PARAM_NOTAGS, 'User full name'),
+                            'profileimageurl' => new external_value(PARAM_URL, 'User picture URL', VALUE_OPTIONAL),
+                            'profileimageurlsmall' => new external_value(PARAM_URL, 'Small user picture URL', VALUE_OPTIONAL),
+                            'unread' => new external_value(PARAM_INT, 'Unread message count')
+                        )
+                    ),
+                    'List of users that are not in the user\'s contact list but have sent a message'
+                )
+            )
+        );
+    }
+
+    /**
+     * Search contacts parameters description.
+     *
+     * @return external_function_parameters
+     * @since 2.5
+     */
+    public static function search_contacts_parameters() {
+        return new external_function_parameters(
+            array(
+                'searchtext' => new external_value(PARAM_CLEAN, 'String the user\'s fullname has to match to be found'),
+                'onlymycourses' => new external_value(PARAM_BOOL, 'Limit search to the user\'s courses',
+                    VALUE_DEFAULT, false)
+            )
+        );
+    }
+
+    /**
+     * Search contacts.
+     *
+     * @param string $searchtext query string.
+     * @param bool $onlymycourses limit the search to the user's courses only.
+     * @return external_description
+     * @since 2.5
+     */
+    public static function search_contacts($searchtext, $onlymycourses = false) {
+        global $CFG, $USER;
+        require_once($CFG->libdir . '/enrollib.php');
+
+        $params = array('searchtext' => $searchtext, 'onlymycourses' => $onlymycourses);
+        $params = self::validate_parameters(self::search_contacts_parameters(), $params);
+
+        // Extra validation, we do not allow empty queries.
+        if ($params['searchtext'] === '') {
+            throw new moodle_exception('querystringcannotbeempty');
+        }
+
+        $courseids = array();
+        if ($params['onlymycourses']) {
+            $mycourses = enrol_get_my_courses(array('id'));
+            foreach ($mycourses as $mycourse) {
+                $courseids[] = $mycourse->id;
+            }
+        } else {
+            $courseids[] = SITEID;
+        }
+
+        // Retrieving the users matching the query.
+        $users = message_search_users($courseids, $params['searchtext']);
+        $results = array();
+        foreach ($users as $user) {
+            $results[$user->id] = $user;
+        }
+
+        // Reorganising information.
+        foreach ($results as &$user) {
+            $newuser = array(
+                'id' => $user->id,
+                'fullname' => fullname($user)
+            );
+
+            // Avoid undefined property notice as phone not specified.
+            $user->phone1 = null;
+            $user->phone2 = null;
+
+            // Try to get the user picture, but sometimes this method can return null.
+            $userdetails = user_get_user_details($user, null, array('profileimageurl', 'profileimageurlsmall'));
+            if (!empty($userdetails)) {
+                $newuser['profileimageurl'] = $userdetails['profileimageurl'];
+                $newuser['profileimageurlsmall'] = $userdetails['profileimageurlsmall'];
+            }
+
+            $user = $newuser;
+        }
+
+        return $results;
+    }
+
+    /**
+     * Search contacts return description.
+     *
+     * @return external_description
+     * @since 2.5
+     */
+    public static function search_contacts_returns() {
+        return new external_multiple_structure(
+            new external_single_structure(
+                array(
+                    'id' => new external_value(PARAM_INT, 'User ID'),
+                    'fullname' => new external_value(PARAM_NOTAGS, 'User full name'),
+                    'profileimageurl' => new external_value(PARAM_URL, 'User picture URL', VALUE_OPTIONAL),
+                    'profileimageurlsmall' => new external_value(PARAM_URL, 'Small user picture URL', VALUE_OPTIONAL)
+                )
+            ),
+            'List of contacts'
+        );
+    }
 }
 
 /**
