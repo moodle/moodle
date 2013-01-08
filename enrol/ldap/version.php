6d0a62bc3740c826927ce17a417cdc6449507b2a
Dan Poltawski <dan@moodle.com>
2013-01-08 16:42:47 +0800

    MDL-3941 - version bump for TLS support in ldap

--- enrol/ldap/version.php
+++ enrol/ldap/version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012112900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013010800;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'enrol_ldap';      // Full name of the plugin (used for diagnostics)
