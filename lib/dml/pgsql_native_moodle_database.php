d90030f1c7e40c0d018e8f76afa052446312d3dd
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-01-10 11:42:22 +0100

    MDL-37420 rework pg schema (partial backport of 92b00c3)

--- lib/dml/pgsql_native_moodle_database.php
+++ lib/dml/pgsql_native_moodle_database.php
@@ -176,7 +176,7 @@ class pgsql_native_moodle_database extends moodle_database {
 
         $sql = '';
         // Select schema if specified, otherwise the first one wins.
-        if (isset($this->dboptions['dbschema'])) {
+        if (!empty($this->dboptions['dbschema'])) {
             $sql .= "SET search_path = '".$this->dboptions['dbschema']."'; ";
         }
 
