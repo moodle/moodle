d7f4c9c3f538e9a6ab97c6a18eb4e2d1b31155f5
Tim Hunt <T.J.Hunt@open.ac.uk>
2013-01-11 12:51:53 +0000

    MDL-37485 plugins overview: require_login before _capability.

--- admin/plugins.php
+++ admin/plugins.php
@@ -28,8 +28,8 @@ require_once(dirname(dirname(__FILE__)) . '/config.php');
 require_once($CFG->libdir . '/adminlib.php');
 require_once($CFG->libdir . '/pluginlib.php');
 
-require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM));
 admin_externalpage_setup('pluginsoverview');
+require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM));
 
 $fetchremote = optional_param('fetchremote', false, PARAM_BOOL);
 
