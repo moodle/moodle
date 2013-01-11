237f06b3d00a187fa778808d549dc6bd3db8fbfa
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-01-11 22:16:54 +0100

    weekly release 1.9.19+

--- version.php
+++ version.php
@@ -6,10 +6,10 @@
 // This is compared against the values stored in the database to determine
 // whether upgrades should be performed (see lib/db/*.php)
 
-    $version = 2007101592.03; // YYYYMMDD      = date of the 1.9 branch (don't change)
+    $version = 2007101592.04; // YYYYMMDD      = date of the 1.9 branch (don't change)
                               //         X     = release number 1.9.[0,1,2,3,4,5...]
                               //          Y.YY = micro-increments between releases
 
-    $release = '1.9.19+ (Build: 20130111)';     // Human-friendly version name
+    $release = '1.9.19+ (Build: 20130114)';     // Human-friendly version name
 
 ?>
