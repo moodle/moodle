e5ca31fa4da71607e75c59f977f849c685713af3
Jonathon Fowler <fowlerj@usq.edu.au>
2013-01-17 12:19:44 +1000

    MDL-38388 qformat_blackboard_six: fix question image import
    This works on the expectation that the returned array from readdata()
    is opaque and will be fed directly into readquestions() since
    filebase is different per file and needs to be set up appropriately
    before each parse.

--- question/format/blackboard_six/tests/blackboardformatpool_test.php
+++ question/format/blackboard_six/tests/blackboardformatpool_test.php
@@ -41,8 +41,10 @@ require_once($CFG->dirroot . '/question/engine/tests/helpers.php');
 class qformat_blackboard_six_pool_test extends question_testcase {
 
     public function make_test_xml() {
-        $xml = file_get_contents(__DIR__ . '/fixtures/sample_blackboard_pool.dat');
-        return array(0=>$xml);
+        $xmlfile = new qformat_blackboard_six_file();
+        $xmlfile->filetype = 2;
+        $xmlfile->text = file_get_contents(__DIR__ . '/fixtures/sample_blackboard_pool.dat');
+        return array(0=>$xmlfile);
     }
 
     public function test_import_match() {
@@ -50,7 +52,6 @@ class qformat_blackboard_six_pool_test extends question_testcase {
         $xml = $this->make_test_xml();
 
         $importer = new qformat_blackboard_six();
-        $importer->set_filetype(2);
         $questions = $importer->readquestions($xml);
 
         $q = $questions[4];
@@ -87,7 +88,6 @@ class qformat_blackboard_six_pool_test extends question_testcase {
         $xml = $this->make_test_xml();
 
         $importer = new qformat_blackboard_six();
-        $importer->set_filetype(2);
         $questions = $importer->readquestions($xml);
         $q = $questions[1];
 
@@ -147,7 +147,6 @@ class qformat_blackboard_six_pool_test extends question_testcase {
         $xml = $this->make_test_xml();
 
         $importer = new qformat_blackboard_six();
-        $importer->set_filetype(2);
         $questions = $importer->readquestions($xml);
         $q = $questions[2];
 
@@ -221,7 +220,6 @@ class qformat_blackboard_six_pool_test extends question_testcase {
         $xml = $this->make_test_xml();
 
         $importer = new qformat_blackboard_six();
-        $importer->set_filetype(2);
         $questions = $importer->readquestions($xml);
         $q = $questions[0];
 
@@ -251,7 +249,6 @@ class qformat_blackboard_six_pool_test extends question_testcase {
         $xml = $this->make_test_xml();
 
         $importer = new qformat_blackboard_six();
-        $importer->set_filetype(2);
         $questions = $importer->readquestions($xml);
         $q = $questions[3];
 
@@ -286,7 +283,6 @@ class qformat_blackboard_six_pool_test extends question_testcase {
         $xml = $this->make_test_xml();
 
         $importer = new qformat_blackboard_six();
-        $importer->set_filetype(2);
         $questions = $importer->readquestions($xml);
         $q = $questions[5];
 
