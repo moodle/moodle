03612a7b0030fb9c3f1c4a4d933710610220fecb
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-01-30 00:28:35 +0100

    MDL-32420 anomaly theme: bump version

--- theme/anomaly/version.php
+++ theme/anomaly/version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012112900; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2013013000; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900; // Requires this Moodle version
 $plugin->component = 'theme_anomaly'; // Full name of the plugin (used for diagnostics)
