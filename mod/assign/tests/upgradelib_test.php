9c986ee01ddc3720afb4214493bbf07d9c0218d9
Damyon Wiese <damyon@moodle.com>
2013-02-20 12:50:31 +0800

    MDL-38115 Assignment Unit tests : speedup (round 2)

--- mod/assign/tests/upgradelib_test.php
+++ mod/assign/tests/upgradelib_test.php
@@ -30,6 +30,7 @@ global $CFG;
 require_once($CFG->dirroot . '/mod/assign/locallib.php');
 require_once($CFG->dirroot . '/mod/assign/upgradelib.php');
 require_once($CFG->dirroot . '/mod/assignment/lib.php');
+require_once($CFG->dirroot . '/mod/assign/tests/base_test.php');
 
 /**
  * Unit tests for (some of) mod/assign/upgradelib.php.
@@ -37,78 +38,7 @@ require_once($CFG->dirroot . '/mod/assignment/lib.php');
  * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class mod_assign_upgradelib_testcase extends advanced_testcase {
-
-    /** @var stdClass $course New course created to hold the assignments */
-    protected $course = null;
-
-    /** @var array $teachers List of 5 default teachers in the course*/
-    protected $teachers = null;
-
-    /** @var array $editingteachers List of 5 default editing teachers in the course*/
-    protected $editingteachers = null;
-
-    /** @var array $students List of 100 default students in the course*/
-    protected $students = null;
-
-    /** @var array $groups List of 10 groups in the course */
-    protected $groups = null;
-
-    /**
-     * Setup function - we will create a course and add users to it.
-     */
-    protected function setUp() {
-        global $DB, $CFG;
-
-        $this->resetAfterTest(true);
-
-        $this->course = $this->getDataGenerator()->create_course();
-        $this->teachers = array();
-        for ($i = 0; $i < 5; $i++) {
-            array_push($this->teachers, $this->getDataGenerator()->create_user());
-        }
-
-        $this->editingteachers = array();
-        for ($i = 0; $i < 5; $i++) {
-            array_push($this->editingteachers, $this->getDataGenerator()->create_user());
-        }
-
-        $this->students = array();
-        for ($i = 0; $i < 100; $i++) {
-            array_push($this->students, $this->getDataGenerator()->create_user());
-        }
-
-        $this->groups = array();
-        for ($i = 0; $i < 10; $i++) {
-            array_push($this->groups, $this->getDataGenerator()->create_group(array('courseid'=>$this->course->id)));
-        }
-
-        $teacherrole = $DB->get_record('role', array('shortname'=>'teacher'));
-        foreach ($this->teachers as $i => $teacher) {
-            $this->getDataGenerator()->enrol_user($teacher->id,
-                                                  $this->course->id,
-                                                  $teacherrole->id);
-            groups_add_member($this->groups[$i % 10], $teacher);
-        }
-
-        $editingteacherrole = $DB->get_record('role', array('shortname'=>'editingteacher'));
-        foreach ($this->editingteachers as $i => $editingteacher) {
-            $this->getDataGenerator()->enrol_user($editingteacher->id,
-                                                  $this->course->id,
-                                                  $editingteacherrole->id);
-            groups_add_member($this->groups[$i % 10], $editingteacher);
-        }
-
-        $studentrole = $DB->get_record('role', array('shortname'=>'student'));
-        foreach ($this->students as $i => $student) {
-            $this->getDataGenerator()->enrol_user($student->id,
-                                                  $this->course->id,
-                                                  $studentrole->id);
-            if ($i < 80) {
-                groups_add_member($this->groups[$i % 10], $student);
-            }
-        }
-    }
+class mod_assign_upgradelib_testcase extends mod_assign_base_testcase {
 
     public function test_upgrade_upload_assignment() {
         global $DB;
