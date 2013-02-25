65403fcf15491c4773706f340271e473c96a5762
Damyon Wiese <damyon@moodle.com>
2013-02-25 13:56:45 +0800

    MDL-28692 Base theme - Version bump for css change

--- theme/base/version.php
+++ theme/base/version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012061700; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2012061701; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012061700; // Requires this Moodle version
 $plugin->component = 'theme_base'; // Full name of the plugin (used for diagnostics)
