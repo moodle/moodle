f490036abc414976b88a6f5a886b3b19e25f13ba
kkrueger61 <krueger@life-online.de>
2013-02-25 15:55:41 +0100

    allow change passwords for loggedinas

---
+++ login/overwrite_password_form.php
@@ -0,0 +1,105 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Change password form definition.
+ *
+ * @package    core
+ * @subpackage auth
+ * @copyright  2006 Petr Skoda {@link http://skodak.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once $CFG->libdir.'/formslib.php';
+
+class login_overwrite_password_form extends moodleform {
+
+    function definition() {
+        global $USER, $CFG;
+
+        $mform = $this->_form;
+
+        $mform->addElement('header', '', get_string('changepassword'), '');
+
+        // visible elements
+        $mform->addElement('static', 'username', get_string('username'), $USER->username);
+
+        if (!empty($CFG->passwordpolicy)){
+            $mform->addElement('static', 'passwordpolicyinfo', '', print_password_policy());
+        }
+
+        $mform->addElement('password', 'newpassword1', get_string('newpassword'));
+        $mform->addRule('newpassword1', get_string('required'), 'required', null, 'client');
+        $mform->setType('newpassword1', PARAM_RAW);
+
+        $mform->addElement('password', 'newpassword2', get_string('newpassword').' ('.get_String('again').')');
+        $mform->addRule('newpassword2', get_string('required'), 'required', null, 'client');
+        $mform->setType('newpassword2', PARAM_RAW);
+
+
+        // hidden optional params
+        $mform->addElement('hidden', 'id', 0);
+        $mform->setType('id', PARAM_INT);
+
+        // buttons
+        if (get_user_preferences('auth_forcepasswordchange')) {
+            $this->add_action_buttons(false);
+        } else {
+            $this->add_action_buttons(true);
+        }
+    }
+
+/// perform extra password change validation
+    function validation($data, $files) {
+        global $USER;
+        $errors = parent::validation($data, $files);
+
+        update_login_count();
+
+        // ignore submitted username
+          //KK we cannot look for the real username
+//        if (!$user = authenticate_user_login($USER->username, $data['password'])) {
+//            $errors['password'] = get_string('invalidlogin');
+//            return $errors;
+//        }
+
+        reset_login_count();
+
+        if ($data['newpassword1'] <> $data['newpassword2']) {
+            $errors['newpassword1'] = get_string('passwordsdiffer');
+            $errors['newpassword2'] = get_string('passwordsdiffer');
+            return $errors;
+        }
+
+        if ($data['password'] == $data['newpassword1']){
+            $errors['newpassword1'] = get_string('mustchangepassword');
+            $errors['newpassword2'] = get_string('mustchangepassword');
+            return $errors;
+        }
+
+        $errmsg = '';//prevents eclipse warnings
+        if (!check_password_policy($data['newpassword1'], $errmsg)) {
+            $errors['newpassword1'] = $errmsg;
+            $errors['newpassword2'] = $errmsg;
+            return $errors;
+        }
+
+        return $errors;
+    }
+}
