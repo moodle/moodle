5dac7777dc88ace042fe47c1f70ed0b6c8423445
kkrueger61 <krueger@life-online.de>
2013-02-26 14:32:30 +0100

    shorten text in online assignment

--- mod/assign/submission/onlinetext/locallib.php
+++ mod/assign/submission/onlinetext/locallib.php
@@ -233,7 +233,7 @@ class assign_submission_onlinetext extends assign_submission_plugin {
                                                              'onlinetext',
                                                              'assignsubmission_onlinetext');
 
-            $shorttext = shorten_text($text, 140);
+            $shorttext = shorten_text($text, 1000);
             $plagiarismlinks = '';
             if (!empty($CFG->enableplagiarism)) {
                 require_once($CFG->libdir . '/plagiarismlib.php');
