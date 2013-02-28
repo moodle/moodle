4e516138e9b71ae13799c726bbf33f09d92ff28b
Damyon Wiese <damyon@moodle.com>
2013-02-28 10:51:06 +0800

    Revert "MDL-35875 - Users - Fixing RTL support"
    This reverts commit fbb31b0f905c8859cc4f559f4f8148769367ece6.
    This reverts commit c111b9f3aeea98542213ff530f59291fd45d1245.
    This reverts commit 8afb9b7c4f2bfbb336b3e3af580cb14d32b0e917.
    
    This issue has failed testing and integration will be delayed.

--- user/profile/lib.php
+++ user/profile/lib.php
@@ -452,8 +452,7 @@ function profile_display_fields($userid) {
                     $newfield = 'profile_field_'.$field->datatype;
                     $formfield = new $newfield($field->id, $userid);
                     if ($formfield->is_visible() and !$formfield->is_empty()) {
-                        echo html_writer::tag('dt', format_string($formfield->field->name));
-                        echo html_writer::tag('dd', $formfield->display_data());
+                        print_row(format_string($formfield->field->name.':'), $formfield->display_data());
                     }
                 }
             }
