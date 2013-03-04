d2be039c6ef0aa55d3834dfee6be1ef77b427c91
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-03-04 23:44:39 +0100

    MDL-32975 mini-bump to get navsortmycoursessort defined.

--- version.php
+++ version.php
@@ -31,7 +31,7 @@ defined('MOODLE_INTERNAL') || die();
 
 
 
-$version  = 2013030400.00;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2013030400.01;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
