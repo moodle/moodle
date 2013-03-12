043f1005560699b6387d2f2e23187bcb2935685d
Rajesh Taneja <rajesh@moodle.com>
2013-03-12 10:00:55 +0800

    MDL-38332 Administration: Updated update.txt and php doc for get_users_listing

--- lib/datalib.php
+++ lib/datalib.php
@@ -357,7 +357,7 @@ function get_users($get=true, $search='', $confirmed=false, array $exceptions=nu
 
 
 /**
- * @todo Finish documenting this function
+ * Return filtered (if provided) list of users in site, except guest and deleted users.
  *
  * @param string $sort An SQL field to sort by
  * @param string $dir The sort direction ASC|DESC
@@ -368,7 +368,7 @@ function get_users($get=true, $search='', $confirmed=false, array $exceptions=nu
  * @param string $lastinitial Users whose last name starts with $lastinitial
  * @param string $extraselect An additional SQL select statement to append to the query
  * @param array $extraparams Additional parameters to use for the above $extraselect
- * @param object $extracontext If specified, will include user 'extra fields'
+ * @param stdClass $extracontext If specified, will include user 'extra fields'
  *   as appropriate for current user and given context
  * @return array Array of {@link $USER} records
  */
