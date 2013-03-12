279d86943bdf921677f4f925a46091b579cbabdf
David Monllao <davidm@moodle.com>
2013-03-12 15:47:30 +0800

    MDL-38437 behat: Step definition to add a discussion

---
+++ mod/forum/tests/behat/behat_mod_forum.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Steps definitions related with the forum activity.
+ *
+ * @package    mod_forum
+ * @category   test
+ * @copyright  2013 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../../lib/behat/behat_base.php');
+
+use Behat\Behat\Context\Step\Given as Given,
+    Behat\Gherkin\Node\TableNode as TableNode;
+/**
+ * Forum-related steps definitions.
+ *
+ * @package    mod_forum
+ * @category   test
+ * @copyright  2013 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_mod_forum extends behat_base {
+
+    /**
+     * Adds a discussion to the forum specified by it's name with the provided table data (usually Subject and Message). The step begins from the forum's course page.
+     *
+     * @Given /^I add a new discussion to "(?P<forum_name_string>(?:[^"]|\\")*)" forum with:$/
+     * @param string $forumname
+     * @param TableNode $table
+     */
+    public function i_add_a_forum_discussion_to_forum_with($forumname, TableNode $table) {
+
+        // Escaping $forumname as it has been stripped automatically by the transformer.
+        return array(
+            new Given('I follow "' . $this->escape($forumname) . '"'),
+            new Given('I press "Add a new discussion topic"'),
+            new Given('I fill the moodle form with:', $table),
+            new Given('I press "Post to forum"'),
+            new Given('I wait "5" seconds')
+        );
+    }
+
+}
