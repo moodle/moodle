6b2510f4c76a3835710f246bdd02b40c31eb3a97
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-03-14 00:24:07 +0100

    Revert "Merge branch 'MDL-34818_M24' of git://github.com/lazydaisy/moodle into MOODLE_24_STABLE"
    This reverts commit 6a34b93223441c8b68a302123539f9b47fd9324b, reversing
    changes made to c9b9abc09071229231a69791bf33338573e4d92e.

--- theme/mymobile/javascript/custom.js
+++ theme/mymobile/javascript/custom.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
         });
 
         //calendar and other links that need to be external
-        $('.maincalendar .filters a, li.activity.scorm a, div.files a, #page-user-filesPAGE li div a, .maincalendar .bottom a, .section li.url.modtype_url a, .resourcecontent .resourcemediaplugin a, #underfooter .noajax a, .block_mnet_hosts .content a, .block_private_files .content a, a.portfolio-add-link, #attempts td a, .settingsul li a, .foldertree li a').attr("data-ajax", "false");
+        $('.maincalendar .filters a, li.activity.scorm a, div.files a, #page-user-filesPAGE li div a, .maincalendar .bottom a, .section li.url.modtype_url a, .resourcecontent .resourcemediaplugin a, #underfooter .noajax a, .block_mnet_hosts .content a, .block_private_files .content a, a.portfolio-add-link, #attempts td a').attr("data-ajax", "false");
 
         //add blank to open in window for some
         $('#page-mod-url-viewPAGE div.urlworkaround a, #page-mod-resource-viewPAGE div.resourceworkaround a, .mediaplugin a.mediafallbacklink, #page-mod-resource-viewPAGE .resourcemp3 a, .foldertree li a').attr("target", "_blank").attr("data-role", "button").attr("data-icon", "plus");
