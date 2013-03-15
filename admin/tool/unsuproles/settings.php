56aee7c2f6f93f3bce0b68b9f459039ba65bca5d
David Mudrák <david@moodle.com>
2013-03-15 01:00:29 +0100

    MDL-38497 Fix invalid $ADMIN->add() calls
    As the admin_category::add() method now checks for the third parameter,
    couple of typos were detected in the code. Additional parameters passed
    to the add() method were probably a mistake, a relict or a typo.
    
    Note that the typo in admin/tool/unsuproles/settings.php had actually
    significant impact on the functionality as the array with required
    capabilities was not actually passed to the admin_externalpage
    constructor as intended.

--- admin/tool/unsuproles/settings.php
+++ admin/tool/unsuproles/settings.php
@@ -26,5 +26,5 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($hassiteconfig) {
-    $ADMIN->add('roles', new admin_externalpage('toolunsuproles', get_string('pluginname', 'tool_unsuproles'), "$CFG->wwwroot/$CFG->admin/tool/unsuproles/index.php"), array('moodle/site:config', 'moodle/role:assign'));
+    $ADMIN->add('roles', new admin_externalpage('toolunsuproles', get_string('pluginname', 'tool_unsuproles'), "$CFG->wwwroot/$CFG->admin/tool/unsuproles/index.php", array('moodle/site:config', 'moodle/role:assign')));
 }
