5c5317aedc314467bea4ad6e7d787fb01a9ae277
Frederic Massart <fred@moodle.com>
2013-03-21 16:01:50 +0800

    MDL-38612 url: Reformat module settings form

--- mod/url/mod_form.php
+++ mod/url/mod_form.php
@@ -51,6 +51,8 @@ class mod_url_mod_form extends moodleform_mod {
         $mform->addElement('header', 'content', get_string('contentheader', 'url'));
         $mform->addElement('url', 'externalurl', get_string('externalurl', 'url'), array('size'=>'60'), array('usefilepicker'=>true));
         $mform->addRule('externalurl', null, 'required', null, 'client');
+        $mform->setExpanded('content');
+
         //-------------------------------------------------------
         $mform->addElement('header', 'optionssection', get_string('optionsheader', 'url'));
 
@@ -110,7 +112,6 @@ class mod_url_mod_form extends moodleform_mod {
         //-------------------------------------------------------
         $mform->addElement('header', 'parameterssection', get_string('parametersheader', 'url'));
         $mform->addElement('static', 'parametersinfo', '', get_string('parametersheader_help', 'url'));
-        $mform->setAdvanced('parametersinfo');
 
         if (empty($this->current->parameters)) {
             $parcount = 5;
@@ -129,7 +130,6 @@ class mod_url_mod_form extends moodleform_mod {
                 $mform->createElement('selectgroups', $variable, '', $options),
             );
             $mform->addGroup($group, $pargroup, get_string('parameterinfo', 'url'), ' ', false);
-            $mform->setAdvanced($pargroup);
         }
 
         //-------------------------------------------------------
