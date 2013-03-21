d86db37c44f065b48abf5294e46c2e7f3c3c5618
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-03-21 17:04:50 +0100

    MDL-38632 folder: observe show_expanded on course page

--- mod/folder/lib.php
+++ mod/folder/lib.php
@@ -426,7 +426,7 @@ function folder_dndupload_handle($uploadinfo) {
 function folder_get_coursemodule_info($cm) {
     global $DB;
     if (!($folder = $DB->get_record('folder', array('id' => $cm->instance),
-            'id, name, display, intro, introformat'))) {
+            'id, name, display, show_expanded, intro, introformat'))) {
         return NULL;
     }
     $cminfo = new cached_cm_info();
@@ -434,6 +434,7 @@ function folder_get_coursemodule_info($cm) {
     if ($folder->display == FOLDER_DISPLAY_INLINE) {
         // prepare folder object to store in customdata
         $fdata = new stdClass();
+        $fdata->show_expanded = $folder->show_expanded;
         if ($cm->showdescription && strlen(trim($folder->intro))) {
             $fdata->intro = $folder->intro;
             if ($folder->introformat != FORMAT_MOODLE) {
