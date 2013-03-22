f0e4cf5e31269515a007fcfd50c5dfcc85c9ff91
David Monllao <davidm@moodle.com>
2013-03-22 16:14:23 +0800

    MDL-38619 behat: Step definition to add enrolment methods to courses

---
+++ cohort/tests/behat/behat_cohort.php
@@ -0,0 +1,80 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Cohorts steps definitions.
+ *
+ * @package    core
+ * @category   test
+ * @copyright  2013 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../lib/behat/behat_base.php');
+
+use Behat\Behat\Context\Step\Given as Given;
+
+/**
+ * Steps definitions for cohort actions.
+ *
+ * @package    core
+ * @category   test
+ * @copyright  2013 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_cohort extends behat_base {
+
+    /**
+     * Adds the user to the specified cohort.
+     *
+     * @Given /^I add "(?P<user_username_string>(?:[^"]|\\")*)" user to "(?P<cohort_idnumber_string>(?:[^"]|\\")*)" cohort$/
+     * @param string $username
+     * @param string $cohortidnumber
+     */
+    public function i_add_user_to_cohort($username, $cohortidnumber) {
+        global $DB;
+
+        // The user was created by the data generator, executed by the same PHP process that is
+        // running this step, not by any Selenium action.
+        $userid = $DB->get_field('user', 'id', array('username' => $username));
+
+        $steps = array(
+            new Given('I click on "Assign" "link" in the "//table[@id=\'cohorts\']//tr[contains(., \'' . $cohortidnumber . '\')]" "xpath_element"'),
+            new Given('I select "' . $userid . '" from "Potential users"'),
+            new Given('I press "Add"'),
+            new Given('I press "Back to cohorts"')
+        );
+
+        // If we are not in the cohorts management we should move there before anything else.
+        if (!$this->getSession()->getPage()->find('css', 'input#cohort_search_q')) {
+            $steps = array_merge(
+                array(
+                    new Given('I am on homepage'),
+                    new Given('I expand "Front page settings" node'),
+                    new Given('I expand "Site administration" node'),
+                    new Given('I expand "Users" node'),
+                    new Given('I expand "Accounts" node'),
+                    new Given('I follow "Cohorts"')
+                ),
+                $steps
+            );
+        }
+
+        return $steps;
+    }
+}
---
+++ enrol/tests/behat/behat_enrol.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Enrolment steps definitions.
+ *
+ * @package    core_enrol
+ * @category   test
+ * @copyright  2013 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../lib/behat/behat_base.php');
+
+use Behat\Behat\Context\Step\Given as Given,
+    Behat\Gherkin\Node\TableNode as TableNode;
+
+/**
+ * Steps definitions for general enrolment actions.
+ *
+ * @package    core_enrol
+ * @category   test
+ * @copyright  2013 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_enrol extends behat_base {
+
+    /**
+     * Adds the specified enrolment method to the current course filling the form with the provided data.
+     *
+     * @Given /^I add "(?P<enrolment_method_name_string>(?:[^"]|\\")*)" enrolment method with:$/
+     * @param string $enrolmethod
+     * @param TableNode $table
+     */
+    public function i_add_enrolment_method_with($enrolmethod, TableNode $table) {
+
+        return array(
+            new Given('I expand "Users" node'),
+            new Given('I follow "Enrolment methods"'),
+            new Given('I select "' . $enrolmethod . '" from "Add method"'),
+            new Given('I fill the moodle form with:', $table),
+            new Given('I press "Add method"')
+        );
+    }
+
+}
