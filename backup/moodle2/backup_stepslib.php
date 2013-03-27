1b55bdb64413426e3cb35b52202275e86f1d03d9
Tony Levi <tony.levi@netspot.com.au>
2013-03-27 11:21:36 +1030

    MDL-38734: Automated backup failure causing all course to fail
    Conflicts:
    	backup/moodle2/backup_stepslib.php

--- backup/moodle2/backup_stepslib.php
+++ backup/moodle2/backup_stepslib.php
@@ -39,6 +39,7 @@ class create_and_clean_temp_stuff extends backup_execution_step {
         backup_helper::check_and_create_backup_dir($this->get_backupid());// Create backup temp dir
         backup_helper::clear_backup_dir($this->get_backupid(), $progress);           // Empty temp dir, just in case
         backup_helper::delete_old_backup_dirs(time() - (4 * 60 * 60), $progress);    // Delete > 4 hours temp dirs
+        backup_controller_dbops::drop_backup_ids_temp_table($this->get_backupid()); // Drop ids temp table
         backup_controller_dbops::create_backup_ids_temp_table($this->get_backupid()); // Create ids temp table
         $progress->end_progress();
     }
