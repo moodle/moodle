8cea78f500932ac389eb51946051c17fe3edbc02
Rajesh Taneja <rajesh@moodle.com>
2013-04-02 16:07:15 +0800

    MDL-35883 Accessibility: Added title for login as user link in header and footer for accessibility

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -529,7 +529,9 @@ class core_renderer extends renderer_base {
         if (session_is_loggedinas()) {
             $realuser = session_get_realuser();
             $fullname = fullname($realuser, true);
-            $realuserinfo = " [<a href=\"$CFG->wwwroot/course/loginas.php?id=$course->id&amp;sesskey=".sesskey()."\">$fullname</a>] ";
+            $loginastitle = get_string('loginas');
+            $realuserinfo = " [<a href=\"$CFG->wwwroot/course/loginas.php?id=$course->id&amp;sesskey=".sesskey()."\"";
+            $realuserinfo .= "title =\"".$loginastitle."\">$fullname</a>] ";
         } else {
             $realuserinfo = '';
         }
