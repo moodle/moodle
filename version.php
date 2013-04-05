69ea3d7dd3d12bf3b4300b5e9bdd550cfcf034de
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-04-05 14:29:54 +0200

    NOBUG: Bump to 2.5beta+
    we don't package tagged commits in master, so let's
    bump to beta+ and get the packaging done.

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2013040500.00;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2013040500.01;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
-$release  = '2.5beta (Build: 20130405)'; // Human-friendly version name
+$release  = '2.5beta+ (Build: 20130405)';// Human-friendly version name
 
 $branch   = '25';                       // this version's branch
 $maturity = MATURITY_BETA;             // this version's maturity level
