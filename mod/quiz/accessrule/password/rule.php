6b7a020b238e6fa85b80523277fe05ed06ec2769
Jayesh Anandani <jayeshanandani@gmail.com>
2013-04-11 16:25:34 +0530

    MDL-39029 quiz password: autofocus feature added

--- mod/quiz/accessrule/password/rule.php
+++ mod/quiz/accessrule/password/rule.php
@@ -64,7 +64,7 @@ class quizaccess_password extends quiz_access_rule_base {
         // Don't use the 'proper' field name of 'password' since that get's
         // Firefox's password auto-complete over-excited.
         $mform->addElement('password', 'quizpassword',
-                get_string('quizpassword', 'quizaccess_password'));
+                get_string('quizpassword', 'quizaccess_password'), array('autofocus' => 'true'));
     }
 
     public function validate_preflight_check($data, $files, $errors, $attemptid) {
