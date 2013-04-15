01b3704f1b17725907942fb085aa2d5411432d6d
Stephen Bourget <sbourget@goffstown.k12.nh.us>
2013-04-15 17:59:25 -0400

    MDL-39140 mod_folder: Fix missing call to setType()

--- mod/folder/edit_form.php
+++ mod/folder/edit_form.php
@@ -36,6 +36,7 @@ class mod_folder_edit_form extends moodleform {
         $options = $this->_customdata['options'];
 
         $mform->addElement('hidden', 'id', $data->id);
+        $mform->setType('id', PARAM_INT);
         $mform->addElement('filemanager', 'files_filemanager', get_string('files'), null, $options);
         $submit_string = get_string('savechanges');
         $this->add_action_buttons(true, $submit_string);
