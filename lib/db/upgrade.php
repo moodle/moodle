d750d41e66583ce2b10decc5c6f7bde9cecf724d
Mike Churchward <mike@remote-learner.net>
2013-04-17 10:48:32 -0400

    MDL-34933 core_message: Adding index to improve database access on message_working table.

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -1990,5 +1990,19 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2013040300.01);
     }
 
+    if ($oldversion < 2013041100.01) {
+        // Adding index to unreadmessageid field of message_working table (MDL-34933)
+        $table = new xmldb_table('message_working');
+        $index = new xmldb_index('unreadmessageid_idx', XMLDB_INDEX_NOTUNIQUE, array('unreadmessageid'));
+
+        // Conditionally launch add index unreadmessageid
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2013041100.01);
+    }
+
     return true;
 }
