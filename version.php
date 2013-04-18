3a8c4380c06d632a897714c6251be949dd5cc657
Dan Poltawski <dan@moodle.com>
2013-04-18 12:51:46 +0100

    on demand release 2.5beta+
    Special 'Spoons curry' UK Edition ;-)

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2013041800.00;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2013041801.00;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
-$release  = '2.5beta+ (Build: 20130411)';// Human-friendly version name
+$release  = '2.5beta+ (Build: 20130418)';// Human-friendly version name
 
 $branch   = '25';                       // this version's branch
 $maturity = MATURITY_BETA;              // this version's maturity level
