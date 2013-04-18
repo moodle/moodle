1d0c0c4a23ddcabc02f24c0462a96c7add343873
Damyon Wiese <damyon@moodle.com>
2013-04-18 16:29:38 +0800

    MDL-38592: Add major version redirect for settings used frontpage.

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -1165,7 +1165,7 @@ function disable_output_buffering() {
  */
 function redirect_if_major_upgrade_required() {
     global $CFG;
-    $lastmajordbchanges = 2013022500.01;
+    $lastmajordbchanges = 2013041800.00;
     if (empty($CFG->version) or (float)$CFG->version < $lastmajordbchanges or
             during_initial_install() or !empty($CFG->adminsetuppending)) {
         try {
