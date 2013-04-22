37eadb417b92abc0b92538300aec89386bb6be24
Jean-Michel Vedrine <vedrine@vedrine.org>
2013-04-22 13:40:29 +0200

    MDL-37784 Adaptive no penalty display wrong penalty info Adaptive Mode tells students they have been penalized even when set to no penalties

--- question/behaviour/adaptivenopenalty/renderer.php
+++ question/behaviour/adaptivenopenalty/renderer.php
@@ -38,9 +38,11 @@ require_once(dirname(__FILE__) . '/../adaptive/renderer.php');
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class qbehaviour_adaptivenopenalty_renderer extends qbehaviour_adaptive_renderer {
-    protected function penalty_info(qbehaviour_adaptive_mark_details $details, question_display_options $options) {
-        return '';
+    protected function grading_details(qbehaviour_adaptive_mark_details $details, question_display_options $options) {
+        $mark = $details->get_formatted_marks($options->markdp);
+        return get_string('gradingdetails', 'qbehaviour_adaptive', $mark);
     }
+
     protected function disregarded_info() {
         return '';
     }
