487bc1b6273a7202735c09a42bc1b5aa631de17f
Jerome Mouneyrac <jerome@mouneyrac.com>
2013-04-26 15:13:03 +0800

    MDL-38440 WebService get_contents doesn't return the full text of labels

---
+++ mod/label/tests/generator/lib.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * mod_label data generator
+ *
+ * @package    mod_label
+ * @category   test
+ * @copyright  2013 Jerome Mouneyrac
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Label module data generator class
+ *
+ * @package    mod_label
+ * @category   test
+ * @copyright  2013 Jerome Mouneyrac
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_label_generator extends testing_module_generator {
+
+    /**
+     * Create new label module instance
+     * @param array|stdClass $record
+     * @param array $options
+     * @return stdClass activity record with extra cmid field
+     */
+    public function create_instance($record = null, array $options = null) {
+        global $CFG;
+        require_once("$CFG->dirroot/mod/label/lib.php");
+
+        $this->instancecount++;
+        $i = $this->instancecount;
+
+        $record = (object)(array)$record;
+        $options = (array)$options;
+
+        if (empty($record->course)) {
+            throw new coding_exception('module generator requires $record->course');
+        }
+        if (!isset($record->name)) {
+            $record->name = get_string('pluginname', 'label').' '.$i;
+        }
+        if (!isset($record->intro)) {
+            $record->intro = 'Test label '.$i;
+        }
+        if (!isset($record->introformat)) {
+            $record->introformat = FORMAT_MOODLE;
+        }
+
+        $record->coursemodule = $this->precreate_course_module($record->course, $options);
+        $id = label_add_instance($record, null);
+        return $this->post_add_instance($id, $record->coursemodule);
+    }
+}
