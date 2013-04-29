e1aaddef666ec83bf49601b794dcc231e1844c74
Ankit Agarwal <ankit@moodle.com>
2013-04-29 13:21:43 +0800

    MDL-39365 scorm reports: Return correct track count

--- mod/scorm/report/reportlib.php
+++ mod/scorm/report/reportlib.php
@@ -69,14 +69,14 @@ function get_scorm_question_count($scormid) {
     $rs = $DB->get_recordset_select("scorm_scoes_track", $select, $params, 'element');
     $keywords = array("cmi.interactions_", ".id");
     if ($rs->valid()) {
-        // Done as interactions start at 0 (do only if we have something to report).
-        $count++;
         foreach ($rs as $record) {
             $num = trim(str_ireplace($keywords, '', $record->element));
             if (is_numeric($num) && $num > $count) {
                 $count = $num;
             }
         }
+        // Done as interactions start at 0 (do only if we have something to report).
+        $count++;
     }
     $rs->close(); // closing recordset
     return $count;
