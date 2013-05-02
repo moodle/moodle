0852bbaed52866fbc35c99071756d9bd882189f9
Frederic Massart <fred@moodle.com>
2013-05-02 18:14:58 +0800

    MDL-39477 repository: Introducing data generators for repositories

--- repository/recent/tests/generator/lib.php
+++ repository/recent/tests/generator/lib.php
@@ -15,19 +15,36 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Adds data generator support
+ * Recent repository data generator
  *
- * @package    core
+ * @package    repository_recent
  * @category   test
- * @copyright  2012 Petr Skoda {@link http://skodak.org}
+ * @copyright  2013 Frédéric Massart
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// NOTE: MOODLE_INTERNAL is not verified here because we load this before setup.php!
+/**
+ * Recent repository data generator class
+ *
+ * @package    repository_recent
+ * @category   test
+ * @copyright  2013 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class repository_recent_generator extends testing_repository_generator {
 
-require_once(__DIR__.'/data_generator.php');
-require_once(__DIR__.'/component_generator_base.php');
-require_once(__DIR__.'/module_generator.php');
-require_once(__DIR__.'/block_generator.php');
-require_once(__DIR__.'/repository_generator.php');
+    /**
+     * Fill in type record defaults.
+     *
+     * @param array $record
+     * @return array
+     */
+    protected function prepare_type_record(array $record) {
+        $record = parent::prepare_type_record($record);
+        if (!isset($record['recentfilesnumber'])) {
+            $record['recentfilesnumber'] = '';
+        }
+        return $record;
+    }
 
+}
--- repository/url/tests/generator/lib.php
+++ repository/url/tests/generator/lib.php
@@ -15,19 +15,21 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Adds data generator support
+ * URL repository data generator
  *
- * @package    core
+ * @package    repository_url
  * @category   test
- * @copyright  2012 Petr Skoda {@link http://skodak.org}
+ * @copyright  2013 Frédéric Massart
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// NOTE: MOODLE_INTERNAL is not verified here because we load this before setup.php!
-
-require_once(__DIR__.'/data_generator.php');
-require_once(__DIR__.'/component_generator_base.php');
-require_once(__DIR__.'/module_generator.php');
-require_once(__DIR__.'/block_generator.php');
-require_once(__DIR__.'/repository_generator.php');
-
+/**
+ * URL repository data generator class
+ *
+ * @package    repository_url
+ * @category   test
+ * @copyright  2013 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class repository_url_generator extends testing_repository_generator {
+}
