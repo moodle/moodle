e5c6694de80f0a96c78e51ab9a3eb17414e359c4
Frederic Massart <fred@moodle.com>
2013-05-02 18:14:58 +0800

    MDL-39477 repository: Introducing data generators for repositories

--- repository/boxnet/tests/generator/lib.php
+++ repository/boxnet/tests/generator/lib.php
@@ -15,19 +15,36 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Adds data generator support
+ * Box.net repository data generator
  *
- * @package    core
+ * @package    repository_boxnet
  * @category   test
- * @copyright  2012 Petr Skoda {@link http://skodak.org}
+ * @copyright  2013 Frédéric Massart
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// NOTE: MOODLE_INTERNAL is not verified here because we load this before setup.php!
+/**
+ * Box.net repository data generator class
+ *
+ * @package    repository_boxnet
+ * @category   test
+ * @copyright  2013 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class repository_boxnet_generator extends testing_repository_generator {
 
-require_once(__DIR__.'/data_generator.php');
-require_once(__DIR__.'/component_generator_base.php');
-require_once(__DIR__.'/module_generator.php');
-require_once(__DIR__.'/block_generator.php');
-require_once(__DIR__.'/repository_generator.php');
+    /**
+     * Fill in type record defaults.
+     *
+     * @param array $record
+     * @return array
+     */
+    protected function prepare_type_record(array $record) {
+        $record = parent::prepare_type_record($record);
+        if (!isset($record['api_key'])) {
+            $record['api_key'] = 'api_key';
+        }
+        return $record;
+    }
 
+}
