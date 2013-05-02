ed70c74be5f0f17169dea553b8471f81ae780301
David Mudrák <david@moodle.com>
2013-05-02 13:44:47 +0200

    MDL-39442 Do not use native rename() when moving folders
    The native rename() function does not support moving folders
    cross-device. See https://bugs.php.net/bug.php?id=54097 for details. So
    instead of trying to move the whole tree, the new installer's method
    moves files recursively one by one.
    
    This is consistent with what mdeploy.php already does.

--- admin/tool/installaddon/version.php
+++ admin/tool/installaddon/version.php
@@ -26,4 +26,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component  = 'tool_installaddon';
 $plugin->version    = 2013031400;
 $plugin->requires   = 2013031400;
-$plugin->maturity   = MATURITY_BETA;
+$plugin->maturity   = MATURITY_STABLE;
