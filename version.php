57af11ee2d63170f82978a07d94b0e5b4ca2c149
Damyon Wiese <damyon@moodle.com>
2013-05-24 13:21:39 +0800

    weekly release 2.2.10+

--- version.php
+++ version.php
@@ -30,10 +30,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$version  = 2011120510.00;              // 20111205      = branching date YYYYMMDD - do not modify!
+$version  = 2011120510.01;              // 20111205      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
-$release  = '2.2.10 (Build: 20130513)'; // Human-friendly version name
+$release  = '2.2.10+ (Build: 20130524)'; // Human-friendly version name
 
 $maturity = MATURITY_STABLE;            // this version's maturity level
