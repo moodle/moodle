df7662560becc51f06d50d48f77f37faa681e104
Jason Fowler <phalacee@gmail.com>
2013-05-28 15:25:08 +0800

    MDL-38347 - Calendar - Fixing arrow output on Android devices

--- lib/outputlib.php
+++ lib/outputlib.php
@@ -533,7 +533,10 @@ class theme_config {
                 $this->rarrow = '&#x25B6;';
                 $this->larrow = '&#x25C0;';
             }
-            elseif (false !== strpos($uagent, 'Konqueror')) {
+            elseif ((false !== strpos($uagent, 'Konqueror')) 
+                || (false !== strpos($uagent, 'Android')))  {
+                // The fonts on Android don't include the characters required for this to work as expected.
+                // So we use the same ones Konqueror uses.
                 $this->rarrow = '&rarr;';
                 $this->larrow = '&larr;';
             }
