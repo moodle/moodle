17e37c57e42cc4b73d9788da45681e1ff3a7e189
Raymond Wijaya <raymond.wijaya@netspot.com.au>
2013-06-07 15:02:47 +0930

    MDL-40093 mod/assign: Remove count of drafts in offline assignment's grading summary screen

--- mod/assign/renderer.php
+++ mod/assign/renderer.php
@@ -246,11 +246,11 @@ class mod_assign_renderer extends plugin_renderer_base {
         $this->add_table_row_tuple($t, get_string('numberofparticipants', 'assign'),
                                    $summary->participantcount);
 
-        // drafts
-        if ($summary->submissiondraftsenabled) {
+        // Drafts count and dont show drafts count when using offline assignment.
+        if ($summary->submissiondraftsenabled && $summary->submissionsenabled) {
             $this->add_table_row_tuple($t, get_string('numberofdraftsubmissions', 'assign'),
                                        $summary->submissiondraftscount);
-       }
+        }
 
         // submitted for grading
         if ($summary->submissionsenabled) {
