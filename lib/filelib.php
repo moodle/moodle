41340f0461176fbc1aa81171042a3ce9166f706f
Sam Chaffee <sam@moodlerooms.com>
2013-06-20 15:54:03 -0400

    MDL-40258: Fixed invalid json string error in file-picker

--- lib/filelib.php
+++ lib/filelib.php
@@ -1878,7 +1878,7 @@ function file_get_typegroup($element, $groups) {
         }
         $result = array_merge($result, $cached[$element][$group]);
     }
-    return array_unique($result);
+    return array_values(array_unique($result));
 }
 
 /**
