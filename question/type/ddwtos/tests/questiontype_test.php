1a3cd05df7cf290dee944e8e69258a79c901f356
Tim Hunt <T.J.Hunt@open.ac.uk>
2013-07-01 15:15:52 +0100

    MDL-47494 ddwtos: Fix broken unit tests.

--- question/type/ddwtos/tests/questiontype_test.php
+++ question/type/ddwtos/tests/questiontype_test.php
@@ -217,13 +217,13 @@ class qtype_ddwtos_test extends question_testcase {
 
         $expectedq->shuffleanswers = 1;
         $expectedq->correctfeedback = array('text' => '<p>Your answer is correct.</p>',
-                'format' => FORMAT_MOODLE, 'files' => array());
+                'format' => FORMAT_MOODLE);
         $expectedq->partiallycorrectfeedback = array(
                 'text' => '<p>Your answer is partially correct.</p>',
-                'format' => FORMAT_MOODLE, 'files' => array());
+                'format' => FORMAT_MOODLE);
         $expectedq->shownumcorrect = true;
         $expectedq->incorrectfeedback = array('text' => '<p>Your answer is incorrect.</p>',
-                'format' => FORMAT_MOODLE, 'files' => array());
+                'format' => FORMAT_MOODLE);
 
         $expectedq->choices = array(
             array('answer' => 'Alpha', 'choicegroup' => 1, 'infinite' => false),
@@ -232,9 +232,9 @@ class qtype_ddwtos_test extends question_testcase {
         );
 
         $expectedq->hint = array(
-                array('text' => 'Try again.', 'format' => FORMAT_MOODLE, 'files' => array()),
+                array('text' => 'Try again.', 'format' => FORMAT_MOODLE),
                 array('text' => 'These are the first three letters of the Greek alphabet.',
-                        'format' => FORMAT_MOODLE, 'files' => array()));
+                        'format' => FORMAT_MOODLE));
         $expectedq->hintshownumcorrect = array(true, true);
         $expectedq->hintclearwrong = array(false, true);
 
@@ -390,13 +390,13 @@ class qtype_ddwtos_test extends question_testcase {
 
         $expectedq->shuffleanswers = 0;
         $expectedq->correctfeedback = array('text' => 'Your answer is correct.',
-                'format' => FORMAT_HTML, 'files' => array());
+                'format' => FORMAT_HTML);
         $expectedq->partiallycorrectfeedback = array(
                 'text' => 'Your answer is partially correct.',
-                'format' => FORMAT_HTML, 'files' => array());
+                'format' => FORMAT_HTML);
         $expectedq->shownumcorrect = true;
         $expectedq->incorrectfeedback = array('text' => 'Your answer is incorrect.',
-                'format' => FORMAT_HTML, 'files' => array());
+                'format' => FORMAT_HTML);
 
         $expectedq->choices = array(
             array('answer' => array('text' => 'hydrogen',        'format' => FORMAT_PLAIN),
@@ -423,9 +423,9 @@ class qtype_ddwtos_test extends question_testcase {
 
         $expectedq->hint = array(array('text' => 'You may wish to read Section 9 of ' .
                 '<em class="italic">Discovering Science</em> Block 8.',
-                    'format' => FORMAT_HTML, 'files' => array()),
+                    'format' => FORMAT_HTML),
                 array('text' => 'Any incorrect choices will be removed before your final try.',
-                    'format' => FORMAT_HTML, 'files' => array()),
+                    'format' => FORMAT_HTML),
         );
         $expectedq->hintshownumcorrect = array(true, true);
         $expectedq->hintclearwrong = array(false, true);
