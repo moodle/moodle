c06a812ed65c8d9aea9701211fd9f91844eca247
Dan Poltawski <dan@moodle.com>
2013-07-05 14:11:47 +0800

    MDL-28466 Remove enrol_authorize - this code is dead
    It does not work and its not maintained.

--- enrol/authorize/authorize.js
+++
@@ -1,5 +0,0 @@
-function authorize_jump_to_mypayments(e, args) {
-    var locationtogo = M.cfg.wwwroot + '/enrol/authorize/index.php?status=' + args.status;
-    locationtogo += '&user=' + (this.checked ? args.userid : '0');
-    top.location.href = locationtogo;
-}
