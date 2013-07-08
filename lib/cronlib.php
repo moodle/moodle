ddc6c118c6f532b52a572c99ed6df9461c4530ff
Petr Škoda <commits@skodak.org>
2013-07-08 11:21:42 +0200

    MDL-40558 skip admins and guest when deleting incomplete users

--- lib/cronlib.php
+++ lib/cronlib.php
@@ -98,6 +98,9 @@ function cron_run() {
                                                   AND (lastname = '' OR firstname = '' OR email = '')",
                                           array($cuttime));
             foreach ($rs as $user) {
+                if (isguestuser($user) or is_siteadmin($user)) {
+                    continue;
+                }
                 delete_user($user);
                 mtrace(" Deleted not fully setup user $user->username ($user->id)");
             }
