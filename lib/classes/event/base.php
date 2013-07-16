c61a3a5c9477483e2cfda33e1150d28f0ada223b
Petr Škoda <commits@skodak.org>
2013-07-16 07:51:14 +0200

    MDL-39846 improve docs

--- lib/classes/event/base.php
+++ lib/classes/event/base.php
@@ -198,7 +198,7 @@ abstract class base implements \IteratorAggregate {
             }
         }
 
-        // Let developers validate their custom data (such as $this->data['other']).
+        // Let developers validate their custom data (such as $this->data['other'], contextlevel, etc.).
         $event->validate_data();
 
         return $event;
@@ -220,7 +220,7 @@ abstract class base implements \IteratorAggregate {
     protected abstract function init();
 
     /**
-     * Let developers validate their custom data (such as $this->data['other']).
+     * Let developers validate their custom data (such as $this->data['other'], contextlevel, etc.).
      *
      * Throw \coding_exception or debugging() notice in case of any problems.
      */
