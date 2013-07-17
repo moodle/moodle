cbc2444e8ab98a7cfc7650d73b5409f318599fc1
Marina Glancy <marina@moodle.com>
2013-07-17 15:24:05 +1000

    MDL-40648 Added useful functions find_button() and count_button_rows()

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2013071900.00;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2013071900.01;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
