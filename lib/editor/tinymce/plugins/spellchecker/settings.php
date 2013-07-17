6139b462d7ac47fa42e3219d6d3c8db1b94d2f52
Petr Škoda <commits@skodak.org>
2013-07-17 21:10:16 +0200

    MDL-38867 remove Google spell checking option

--- lib/editor/tinymce/plugins/spellchecker/settings.php
+++ lib/editor/tinymce/plugins/spellchecker/settings.php
@@ -26,11 +26,11 @@ defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
     $options = array(
+        ''=>get_string('none'),
         'PSpell'=>'PSpell',
-        'GoogleSpell'=>'Google Spell',
         'PSpellShell'=>'PSpellShell');
     $settings->add(new admin_setting_configselect('tinymce_spellchecker/spellengine',
-        get_string('spellengine', 'admin'), '', 'GoogleSpell', $options));
+        get_string('spellengine', 'admin'), '', '', $options));
     $settings->add(new admin_setting_configtextarea('tinymce_spellchecker/spelllanguagelist',
         get_string('spelllanguagelist', 'admin'), '',
         '+English=en,Danish=da,Dutch=nl,Finnish=fi,French=fr,German=de,Italian=it,Polish=pl,' .
