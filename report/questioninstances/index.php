7dcb9b0616a33a1fca5a04fe5117a2f2669a82de
Tim Hunt <T.J.Hunt@open.ac.uk>
2013-07-19 15:15:18 +0100

    MDL-40785 question instances report: ignore subquestions
    We should not count subquestions of cloze questions (parent != 0) but we
    do want to count random questions (parent = id).

--- report/questioninstances/index.php
+++ report/questioninstances/index.php
@@ -94,6 +94,7 @@ if ($requestedqtype) {
             JOIN {question_categories} qc ON q.category = qc.id
             JOIN {context} con ON con.id = qc.contextid
             $sqlqtypetest
+            AND (q.parent = 0 OR q.parent = q.id)
             GROUP BY qc.contextid, $ctxgroupby
             ORDER BY numquestions DESC, numhidden ASC, con.contextlevel ASC, con.id ASC", $params);
 
