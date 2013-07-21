36c1ac9b7a0e7c8312812c3f50b9f325746a8e26
Andrew Nicols <andrew@nicols.co.uk>
2013-07-21 22:52:35 +0100

    MDL-40811 JavaScript: Create a course-util module to reduce code duplication

---
+++ course/yui/build/moodle-course-util-base/moodle-course-util-base-debug.js
@@ -0,0 +1,21 @@
+YUI.add('moodle-course-util-base', function (Y, NAME) {
+
+/**
+ * The Moodle.core_course.util classes provide course-related utility functions.
+ *
+ * @module moodle-course-util
+ * @main
+ */
+
+Y.namespace('Moodle.core_course.util');
+
+/**
+ * A collection of general utility functions for use in course.
+ *
+ * @class Moodle.core_course.util
+ * @static
+ */
+
+
+
+}, '@VERSION@');
---
+++ course/yui/src/util/js/section.js
@@ -0,0 +1,40 @@
+/**
+ * A collection of utility classes for use with course sections.
+ *
+ * @module moodle-course-util
+ * @submodule moodle-course-util-section
+ */
+
+Y.namespace('Moodle.core_course.util.section');
+
+/**
+ * A collection of utility classes for use with course sections.
+ *
+ * @class Moodle.core_course.util.section
+ * @static
+ */
+Y.Moodle.core_course.util.section = {
+    CONSTANTS: {
+        SECTIONIDPREFIX : 'section-'
+    },
+
+    /**
+     * Determines the section ID for the provided section.
+     *
+     * @method getId
+     * @param section {Node} The section to find an ID for.
+     * @return {Number|false} The ID of the section in question or false if no ID was found.
+     */
+    getId: function(section) {
+        // We perform a simple substitution operation to get the ID.
+        var id = section.get('id').replace(
+                this.CONSTANTS.SECTIONIDPREFIX, '');
+
+        // Attempt to validate the ID.
+        id = parseInt(id, 10);
+        if (typeof id === 'number' && isFinite(id)) {
+            return id;
+        }
+        return false;
+    }
+};
