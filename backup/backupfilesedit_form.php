ed31b623bcdd45d0e1d0b8267663e3995bc7df21
Mark Nelson <markn@moodle.com>
2013-07-22 17:42:42 +0800

    MDL-40816 backup: added missing setType calls to the manage backups form

--- backup/backupfilesedit_form.php
+++ backup/backupfilesedit_form.php
@@ -28,12 +28,24 @@ class backup_files_edit_form extends moodleform {
         $mform =& $this->_form;
         $contextid = $this->_customdata['contextid'];
         $options = array('subdirs'=>0, 'maxfiles'=>-1, 'accepted_types'=>'*', 'return_types'=>FILE_INTERNAL | FILE_REFERENCE);
+
         $mform->addElement('filemanager', 'files_filemanager', get_string('files'), null, $options);
+
         $mform->addElement('hidden', 'contextid', $this->_customdata['contextid']);
+        $mform->setType('contextid', PARAM_INT);
+
         $mform->addElement('hidden', 'currentcontext', $this->_customdata['currentcontext']);
+        $mform->setType('currentcontext', PARAM_INT);
+
         $mform->addElement('hidden', 'filearea', $this->_customdata['filearea']);
+        $mform->setType('filearea', PARAM_AREA);
+
         $mform->addElement('hidden', 'component', $this->_customdata['component']);
+        $mform->setType('component', PARAM_COMPONENT);
+
         $mform->addElement('hidden', 'returnurl', $this->_customdata['returnurl']);
+        $mform->setType('returnurl', PARAM_URL);
+
         $this->add_action_buttons(true, get_string('savechanges'));
         $this->set_data($this->_customdata['data']);
     }
