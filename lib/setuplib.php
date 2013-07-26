7a8ef1453eb3c0f60d5cdfd36716e5e1f5f67ea3
Petr Škoda <commits@skodak.org>
2013-07-26 20:27:36 +0200

    MDL-40891 work around make_writable_directory() race condition
    You will still get a "PHP Warning:  mkdir(): File exists" but that should not cause any problems on production sites.

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -1260,10 +1260,15 @@ function make_writable_directory($dir, $exceptiononerror = true) {
 
     if (!file_exists($dir)) {
         if (!mkdir($dir, $CFG->directorypermissions, true)) {
-            if ($exceptiononerror) {
-                throw new invalid_dataroot_permissions($dir.' can not be created, check permissions.');
-            } else {
-                return false;
+            clearstatcache();
+            // There might be a race condition when creating directory.
+            if (!is_dir($dir)) {
+                if ($exceptiononerror) {
+                    throw new invalid_dataroot_permissions($dir.' can not be created, check permissions.');
+                } else {
+                    debugging('Can not create directory: '.$dir, DEBUG_DEVELOPER);
+                    return false;
+                }
             }
         }
     }
