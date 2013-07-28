b33bf039f34f8888ff9be857818dd95306674ce6
Petr Škoda <commits@skodak.org>
2013-07-28 15:06:23 +0200

    MDL-39915 cleanup core_weblib_testcase

--- lib/tests/weblib_test.php
+++ lib/tests/weblib_test.php
@@ -33,43 +33,43 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-class web_testcase extends advanced_testcase {
+class core_weblib_testcase extends advanced_testcase {
 
-    function test_format_string() {
+    public function test_format_string() {
         global $CFG;
 
-        // Ampersands
-        $this->assertEquals(format_string("& &&&&& &&"), "&amp; &amp;&amp;&amp;&amp;&amp; &amp;&amp;");
-        $this->assertEquals(format_string("ANother & &&&&& Category"), "ANother &amp; &amp;&amp;&amp;&amp;&amp; Category");
-        $this->assertEquals(format_string("ANother & &&&&& Category", true), "ANother &amp; &amp;&amp;&amp;&amp;&amp; Category");
-        $this->assertEquals(format_string("Nick's Test Site & Other things", true), "Nick's Test Site &amp; Other things");
+        // Ampersands.
+        $this->assertSame("&amp; &amp;&amp;&amp;&amp;&amp; &amp;&amp;", format_string("& &&&&& &&"));
+        $this->assertSame("ANother &amp; &amp;&amp;&amp;&amp;&amp; Category", format_string("ANother & &&&&& Category"));
+        $this->assertSame("ANother &amp; &amp;&amp;&amp;&amp;&amp; Category", format_string("ANother & &&&&& Category", true));
+        $this->assertSame("Nick's Test Site &amp; Other things", format_string("Nick's Test Site & Other things", true));
 
-        // String entities
-        $this->assertEquals(format_string("&quot;"), "&quot;");
+        // String entities.
+        $this->assertSame("&quot;", format_string("&quot;"));
 
-        // Digital entities
-        $this->assertEquals(format_string("&11234;"), "&11234;");
+        // Digital entities.
+        $this->assertSame("&11234;", format_string("&11234;"));
 
-        // Unicode entities
-        $this->assertEquals(format_string("&#4475;"), "&#4475;");
+        // Unicode entities.
+        $this->assertSame("&#4475;", format_string("&#4475;"));
 
-        // < and > signs
+        // < and > signs.
         $originalformatstringstriptags = $CFG->formatstringstriptags;
 
         $CFG->formatstringstriptags = false;
-        $this->assertEquals(format_string('x < 1'), 'x &lt; 1');
-        $this->assertEquals(format_string('x > 1'), 'x &gt; 1');
-        $this->assertEquals(format_string('x < 1 and x > 0'), 'x &lt; 1 and x &gt; 0');
+        $this->assertSame('x &lt; 1', format_string('x < 1'));
+        $this->assertSame('x &gt; 1', format_string('x > 1'));
+        $this->assertSame('x &lt; 1 and x &gt; 0', format_string('x < 1 and x > 0'));
 
         $CFG->formatstringstriptags = true;
-        $this->assertEquals(format_string('x < 1'), 'x &lt; 1');
-        $this->assertEquals(format_string('x > 1'), 'x &gt; 1');
-        $this->assertEquals(format_string('x < 1 and x > 0'), 'x &lt; 1 and x &gt; 0');
+        $this->assertSame('x &lt; 1', format_string('x < 1'));
+        $this->assertSame('x &gt; 1', format_string('x > 1'));
+        $this->assertSame('x &lt; 1 and x &gt; 0', format_string('x < 1 and x > 0'));
 
         $CFG->formatstringstriptags = $originalformatstringstriptags;
     }
 
-    function test_s() {
+    public function test_s() {
         // Special cases.
         $this->assertSame('0', s(0));
         $this->assertSame('0', s('0'));
@@ -77,103 +77,103 @@ class web_testcase extends advanced_testcase {
         $this->assertSame('', s(null));
 
         // Normal cases.
-        $this->assertEquals('This Breaks &quot; Strict', s('This Breaks " Strict'));
-        $this->assertEquals('This Breaks &lt;a&gt;&quot; Strict&lt;/a&gt;', s('This Breaks <a>" Strict</a>'));
+        $this->assertSame('This Breaks &quot; Strict', s('This Breaks " Strict'));
+        $this->assertSame('This Breaks &lt;a&gt;&quot; Strict&lt;/a&gt;', s('This Breaks <a>" Strict</a>'));
 
         // Unicode characters.
-        $this->assertEquals('Café', s('Café'));
-        $this->assertEquals('一, 二, 三', s('一, 二, 三'));
+        $this->assertSame('Café', s('Café'));
+        $this->assertSame('一, 二, 三', s('一, 二, 三'));
 
         // Don't escape already-escaped numeric entities. (Note, this behaviour
         // may not be desirable. Perhaps we should remove these tests and that
         // functionality, but we can only do that if we understand why it was added.)
-        $this->assertEquals('An entity: &#x09ff;.', s('An entity: &#x09ff;.'));
-        $this->assertEquals('An entity: &#1073;.', s('An entity: &#1073;.'));
-        $this->assertEquals('An entity: &amp;amp;.', s('An entity: &amp;.'));
-        $this->assertEquals('Not an entity: &amp;amp;#x09ff;.', s('Not an entity: &amp;#x09ff;.'));
+        $this->assertSame('An entity: &#x09ff;.', s('An entity: &#x09ff;.'));
+        $this->assertSame('An entity: &#1073;.', s('An entity: &#1073;.'));
+        $this->assertSame('An entity: &amp;amp;.', s('An entity: &amp;.'));
+        $this->assertSame('Not an entity: &amp;amp;#x09ff;.', s('Not an entity: &amp;#x09ff;.'));
     }
 
-    function test_format_text_email() {
-        $this->assertEquals("This is a TEST",
-            format_text_email('<p>This is a <strong>test</strong></p>',FORMAT_HTML));
-        $this->assertEquals("This is a TEST",
-            format_text_email('<p class="frogs">This is a <strong class=\'fishes\'>test</strong></p>',FORMAT_HTML));
-        $this->assertEquals('& so is this',
-            format_text_email('&amp; so is this',FORMAT_HTML));
-        $this->assertEquals('Two bullets: '.textlib::code2utf8(8226).' *',
-            format_text_email('Two bullets: &#x2022; &#8226;',FORMAT_HTML));
-        $this->assertEquals(textlib::code2utf8(0x7fd2).textlib::code2utf8(0x7fd2),
-            format_text_email('&#x7fd2;&#x7FD2;',FORMAT_HTML));
+    public function test_format_text_email() {
+        $this->assertSame("This is a TEST",
+            format_text_email('<p>This is a <strong>test</strong></p>', FORMAT_HTML));
+        $this->assertSame("This is a TEST",
+            format_text_email('<p class="frogs">This is a <strong class=\'fishes\'>test</strong></p>', FORMAT_HTML));
+        $this->assertSame('& so is this',
+            format_text_email('&amp; so is this', FORMAT_HTML));
+        $this->assertSame('Two bullets: '.core_text::code2utf8(8226).' *',
+            format_text_email('Two bullets: &#x2022; &#8226;', FORMAT_HTML));
+        $this->assertSame(core_text::code2utf8(0x7fd2).core_text::code2utf8(0x7fd2),
+            format_text_email('&#x7fd2;&#x7FD2;', FORMAT_HTML));
     }
 
-    function test_obfuscate_email() {
+    public function test_obfuscate_email() {
         $email = 'some.user@example.com';
         $obfuscated = obfuscate_email($email);
         $this->assertNotSame($email, $obfuscated);
-        $back = textlib::entities_to_utf8(urldecode($email), true);
+        $back = core_text::entities_to_utf8(urldecode($email), true);
         $this->assertSame($email, $back);
     }
 
-    function test_obfuscate_text() {
+    public function test_obfuscate_text() {
         $text = 'Žluťoučký koníček 32131';
         $obfuscated = obfuscate_text($text);
         $this->assertNotSame($text, $obfuscated);
-        $back = textlib::entities_to_utf8($obfuscated, true);
+        $back = core_text::entities_to_utf8($obfuscated, true);
         $this->assertSame($text, $back);
     }
 
-    function test_highlight() {
-        $this->assertEquals(highlight('good', 'This is good'), 'This is <span class="highlight">good</span>');
-        $this->assertEquals(highlight('SpaN', 'span'), '<span class="highlight">span</span>');
-        $this->assertEquals(highlight('span', 'SpaN'), '<span class="highlight">SpaN</span>');
-        $this->assertEquals(highlight('span', '<span>span</span>'), '<span><span class="highlight">span</span></span>');
-        $this->assertEquals(highlight('good is', 'He is good'), 'He <span class="highlight">is</span> <span class="highlight">good</span>');
-        $this->assertEquals(highlight('+good', 'This is good'), 'This is <span class="highlight">good</span>');
-        $this->assertEquals(highlight('-good', 'This is good'), 'This is good');
-        $this->assertEquals(highlight('+good', 'This is goodness'), 'This is goodness');
-        $this->assertEquals(highlight('good', 'This is goodness'), 'This is <span class="highlight">good</span>ness');
+    public function test_highlight() {
+        $this->assertSame('This is <span class="highlight">good</span>', highlight('good', 'This is good'));
+        $this->assertSame('<span class="highlight">span</span>', highlight('SpaN', 'span'));
+        $this->assertSame('<span class="highlight">SpaN</span>', highlight('span', 'SpaN'));
+        $this->assertSame('<span><span class="highlight">span</span></span>', highlight('span', '<span>span</span>'));
+        $this->assertSame('He <span class="highlight">is</span> <span class="highlight">good</span>', highlight('good is', 'He is good'));
+        $this->assertSame('This is <span class="highlight">good</span>', highlight('+good', 'This is good'));
+        $this->assertSame('This is good', highlight('-good', 'This is good'));
+        $this->assertSame('This is goodness', highlight('+good', 'This is goodness'));
+        $this->assertSame('This is <span class="highlight">good</span>ness', highlight('good', 'This is goodness'));
     }
 
-    function test_replace_ampersands() {
-        $this->assertEquals(replace_ampersands_not_followed_by_entity("This & that &nbsp;"), "This &amp; that &nbsp;");
-        $this->assertEquals(replace_ampersands_not_followed_by_entity("This &nbsp that &nbsp;"), "This &amp;nbsp that &nbsp;");
+    public function test_replace_ampersands() {
+        $this->assertSame("This &amp; that &nbsp;", replace_ampersands_not_followed_by_entity("This & that &nbsp;"));
+        $this->assertSame("This &amp;nbsp that &nbsp;", replace_ampersands_not_followed_by_entity("This &nbsp that &nbsp;"));
     }
 
-    function test_strip_links() {
-        $this->assertEquals(strip_links('this is a <a href="http://someaddress.com/query">link</a>'), 'this is a link');
+    public function test_strip_links() {
+        $this->assertSame('this is a link', strip_links('this is a <a href="http://someaddress.com/query">link</a>'));
     }
 
-    function test_wikify_links() {
-        $this->assertEquals(wikify_links('this is a <a href="http://someaddress.com/query">link</a>'), 'this is a link [ http://someaddress.com/query ]');
+    public function test_wikify_links() {
+        $this->assertSame('this is a link [ http://someaddress.com/query ]', wikify_links('this is a <a href="http://someaddress.com/query">link</a>'));
     }
 
     /**
      * Test basic moodle_url construction.
      */
-    function test_moodle_url_constructor() {
+    public function test_moodle_url_constructor() {
         global $CFG;
 
         $url = new moodle_url('/index.php');
-        $this->assertEquals($CFG->wwwroot.'/index.php', $url->out());
+        $this->assertSame($CFG->wwwroot.'/index.php', $url->out());
 
         $url = new moodle_url('/index.php', array());
-        $this->assertEquals($CFG->wwwroot.'/index.php', $url->out());
+        $this->assertSame($CFG->wwwroot.'/index.php', $url->out());
 
         $url = new moodle_url('/index.php', array('id' => 2));
-        $this->assertEquals($CFG->wwwroot.'/index.php?id=2', $url->out());
+        $this->assertSame($CFG->wwwroot.'/index.php?id=2', $url->out());
 
         $url = new moodle_url('/index.php', array('id' => 'two'));
-        $this->assertEquals($CFG->wwwroot.'/index.php?id=two', $url->out());
+        $this->assertSame($CFG->wwwroot.'/index.php?id=two', $url->out());
 
         $url = new moodle_url('/index.php', array('id' => 1, 'cid' => '2'));
-        $this->assertEquals($CFG->wwwroot.'/index.php?id=1&amp;cid=2', $url->out());
-        $this->assertEquals($CFG->wwwroot.'/index.php?id=1&cid=2', $url->out(false));
+        $this->assertSame($CFG->wwwroot.'/index.php?id=1&amp;cid=2', $url->out());
+        $this->assertSame($CFG->wwwroot.'/index.php?id=1&cid=2', $url->out(false));
 
         $url = new moodle_url('/index.php', null, 'test');
-        $this->assertEquals($CFG->wwwroot.'/index.php#test', $url->out());
+        $this->assertSame($CFG->wwwroot.'/index.php#test', $url->out());
 
         $url = new moodle_url('/index.php', array('id' => 2), 'test');
-        $this->assertEquals($CFG->wwwroot.'/index.php?id=2#test', $url->out());
+        $this->assertSame($CFG->wwwroot.'/index.php?id=2#test', $url->out());
     }
 
     /**
@@ -181,66 +181,66 @@ class web_testcase extends advanced_testcase {
      */
     public function test_moodle_url_get_path() {
         $url = new moodle_url('http://www.example.org:447/my/file/is/here.txt?really=1');
-        $this->assertEquals('/my/file/is/here.txt', $url->get_path());
+        $this->assertSame('/my/file/is/here.txt', $url->get_path());
 
         $url = new moodle_url('http://www.example.org/');
-        $this->assertEquals('/', $url->get_path());
+        $this->assertSame('/', $url->get_path());
 
         $url = new moodle_url('http://www.example.org/pluginfile.php/slash/arguments');
-        $this->assertEquals('/pluginfile.php/slash/arguments', $url->get_path());
-        $this->assertEquals('/pluginfile.php', $url->get_path(false));
+        $this->assertSame('/pluginfile.php/slash/arguments', $url->get_path());
+        $this->assertSame('/pluginfile.php', $url->get_path(false));
     }
 
-    function test_moodle_url_round_trip() {
+    public function test_moodle_url_round_trip() {
         $strurl = 'http://moodle.org/course/view.php?id=5';
         $url = new moodle_url($strurl);
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
 
         $strurl = 'http://moodle.org/user/index.php?contextid=53&sifirst=M&silast=D';
         $url = new moodle_url($strurl);
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
     }
 
     /**
      * Test Moodle URL objects created with a param with empty value.
      */
-    function test_moodle_url_empty_param_values() {
+    public function test_moodle_url_empty_param_values() {
         $strurl = 'http://moodle.org/course/view.php?id=0';
         $url = new moodle_url($strurl, array('id' => 0));
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
 
         $strurl = 'http://moodle.org/course/view.php?id';
         $url = new moodle_url($strurl, array('id' => false));
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
 
         $strurl = 'http://moodle.org/course/view.php?id';
         $url = new moodle_url($strurl, array('id' => null));
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
 
         $strurl = 'http://moodle.org/course/view.php?id';
         $url = new moodle_url($strurl, array('id' => ''));
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
 
         $strurl = 'http://moodle.org/course/view.php?id';
         $url = new moodle_url($strurl);
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
     }
 
-    function test_moodle_url_round_trip_array_params() {
+    public function test_moodle_url_round_trip_array_params() {
         $strurl = 'http://example.com/?a%5B1%5D=1&a%5B2%5D=2';
         $url = new moodle_url($strurl);
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
 
         $url = new moodle_url('http://example.com/?a[1]=1&a[2]=2');
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
 
-        // For un-keyed array params, we expect 0..n keys to be returned
+        // For un-keyed array params, we expect 0..n keys to be returned.
         $strurl = 'http://example.com/?a%5B0%5D=0&a%5B1%5D=1';
         $url = new moodle_url('http://example.com/?a[]=0&a[]=1');
-        $this->assertEquals($strurl, $url->out(false));
+        $this->assertSame($strurl, $url->out(false));
     }
 
-    function test_compare_url() {
+    public function test_compare_url() {
         $url1 = new moodle_url('index.php', array('var1' => 1, 'var2' => 2));
         $url2 = new moodle_url('index2.php', array('var1' => 1, 'var2' => 2, 'var3' => 3));
 
@@ -267,31 +267,31 @@ class web_testcase extends advanced_testcase {
         $this->assertTrue($url1->compare($url2, URL_MATCH_EXACT));
     }
 
-    function test_out_as_local_url() {
+    public function test_out_as_local_url() {
         global $CFG;
         // Test http url.
         $url1 = new moodle_url('/lib/tests/weblib_test.php');
-        $this->assertEquals('/lib/tests/weblib_test.php', $url1->out_as_local_url());
+        $this->assertSame('/lib/tests/weblib_test.php', $url1->out_as_local_url());
 
         // Test https url.
         $httpswwwroot = str_replace("http://", "https://", $CFG->wwwroot);
         $url2 = new moodle_url($httpswwwroot.'/login/profile.php');
-        $this->assertEquals('/login/profile.php', $url2->out_as_local_url());
+        $this->assertSame('/login/profile.php', $url2->out_as_local_url());
 
         // Test http url matching wwwroot.
         $url3 = new moodle_url($CFG->wwwroot);
-        $this->assertEquals('', $url3->out_as_local_url());
+        $this->assertSame('', $url3->out_as_local_url());
 
         // Test http url matching wwwroot ending with slash (/).
         $url3 = new moodle_url($CFG->wwwroot.'/');
-        $this->assertEquals('/', $url3->out_as_local_url());
+        $this->assertSame('/', $url3->out_as_local_url());
     }
 
     /**
      * @expectedException coding_exception
      * @return void
      */
-    function test_out_as_local_url_error() {
+    public function test_out_as_local_url_error() {
         $url2 = new moodle_url('http://www.google.com/lib/tests/weblib_test.php');
         $url2->out_as_local_url();
     }
@@ -314,7 +314,6 @@ class web_testcase extends advanced_testcase {
      * Try get local url from external https url and you should get error
      *
      * @expectedException coding_exception
-     * @return void
      */
     public function test_https_out_as_local_url_error() {
         $url4 = new moodle_url('https://www.google.com/lib/tests/weblib_test.php');
@@ -323,10 +322,10 @@ class web_testcase extends advanced_testcase {
 
     public function test_clean_text() {
         $text = "lala <applet>xx</applet>";
-        $this->assertEquals($text, clean_text($text, FORMAT_PLAIN));
-        $this->assertEquals('lala xx', clean_text($text, FORMAT_MARKDOWN));
-        $this->assertEquals('lala xx', clean_text($text, FORMAT_MOODLE));
-        $this->assertEquals('lala xx', clean_text($text, FORMAT_HTML));
+        $this->assertSame($text, clean_text($text, FORMAT_PLAIN));
+        $this->assertSame('lala xx', clean_text($text, FORMAT_MARKDOWN));
+        $this->assertSame('lala xx', clean_text($text, FORMAT_MOODLE));
+        $this->assertSame('lala xx', clean_text($text, FORMAT_HTML));
     }
 
     public function test_qualified_me() {
@@ -336,10 +335,10 @@ class web_testcase extends advanced_testcase {
         $PAGE = new moodle_page();
 
         $FULLME = $CFG->wwwroot.'/course/view.php?id=1&xx=yy';
-        $this->assertEquals($FULLME, qualified_me());
+        $this->assertSame($FULLME, qualified_me());
 
         $PAGE->set_url('/course/view.php', array('id'=>1));
-        $this->assertEquals($CFG->wwwroot.'/course/view.php?id=1', qualified_me());
+        $this->assertSame($CFG->wwwroot.'/course/view.php?id=1', qualified_me());
     }
 
     public function test_null_progres_trace() {
