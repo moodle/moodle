cbf4b0e6f68e45a2bd6f225be4a30c077c12f8da
Frederic Massart <fred@moodle.com>
2013-08-01 16:16:36 +0800

    MDL-39954 mod_workshop: Log method can return array of parameters

---
+++ mod/workshop/classes/event/assessable_uploaded.php
@@ -0,0 +1,130 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * mod_workshop assessable uploaded event.
+ *
+ * @package    mod_workshop
+ * @copyright  2013 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_workshop\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * mod_workshop assessable uploaded event class.
+ *
+ * @package    mod_workshop
+ * @copyright  2013 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class assessable_uploaded extends \core\event\assessable_uploaded {
+
+    /**
+     * Legacy log data.
+     *
+     * @var array
+     */
+    protected $legacylogdata = null;
+
+    /**
+     * Returns localised description of what happened.
+     *
+     * @return \lang_string
+     */
+    public function get_description() {
+        return new \lang_string('event_assessable_uploaded_desc', 'mod_workshop', $this->get_data());
+    }
+
+    /**
+     * Legacy event data if get_legacy_eventname() is not empty.
+     *
+     * @return stdClass
+     */
+    protected function get_legacy_eventdata() {
+        $eventdata = new \stdClass();
+        $eventdata->modulename   = 'workshop';
+        $eventdata->cmid         = $this->context->instanceid;
+        $eventdata->itemid       = $this->objectid;
+        $eventdata->courseid     = $this->courseid;
+        $eventdata->userid       = $this->userid;
+        $eventdata->content      = $this->other['content'];
+        if ($this->other['pathnamehashes']) {
+            $eventdata->pathnamehashes = $this->other['pathnamehashes'];
+        }
+        return $eventdata;
+    }
+
+    /**
+     * Return the legacy event name.
+     *
+     * @return string
+     */
+    protected function get_legacy_eventname() {
+        return 'assessable_content_uploaded';
+    }
+
+    /**
+     * Return the legacy log data.
+     *
+     * @return array
+     */
+    protected function get_legacy_logdata() {
+        return $this->legacylogdata;
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return \lang_string
+     */
+    public static function get_name() {
+        return new \lang_string('event_assessable_uploaded', 'mod_workshop');
+    }
+
+    /**
+     * Get URL related to the action.
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new \moodle_url('/mod/workshop/submission.php',
+            array('cmid' => $this->context->instanceid, 'id' => $this->objectid));
+    }
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        parent::init();
+        $this->data['objecttable'] = 'workshop_submissions';
+    }
+
+    /**
+     * Set the legacy log data.
+     *
+     * @param array $legacylogdata
+     * @return void
+     */
+    public function set_legacy_logdata($legacylogdata) {
+        $this->legacylogdata = $legacylogdata;
+    }
+
+}
