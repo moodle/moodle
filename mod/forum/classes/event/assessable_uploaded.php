62b3b99784c83a66a7de879bd20fc542b2d4783d
Frederic Massart <fred@moodle.com>
2013-08-01 11:40:02 +0800

    MDL-39954 mod_forum: Replacing old-style events_trigger

---
+++ mod/forum/classes/event/assessable_uploaded.php
@@ -0,0 +1,118 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * mod_forum assessable uploaded event.
+ *
+ * @package    mod_forum
+ * @copyright  2013 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_forum\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * mod_forum assessable uploaded event class.
+ *
+ * @package    mod_forum
+ * @copyright  2013 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class assessable_uploaded extends \core\event\assessable_uploaded {
+
+    /**
+     * Returns localised description of what happened.
+     *
+     * @return \lang_string
+     */
+    public function get_description() {
+        return new \lang_string('event_assessable_uploaded_desc', 'mod_forum', $this->get_data());
+    }
+
+    /**
+     * Legacy event data if get_legacy_eventname() is not empty.
+     *
+     * @return stdClass
+     */
+    protected function get_legacy_eventdata() {
+        $eventdata = new \stdClass();
+        $eventdata->modulename   = 'forum';
+        $eventdata->name         = $this->other['triggeredfrom'];
+        $eventdata->cmid         = $this->context->instanceid;
+        $eventdata->itemid       = $this->objectid;
+        $eventdata->courseid     = $this->courseid;
+        $eventdata->userid       = $this->userid;
+        $eventdata->content      = $this->other['content'];
+        if ($this->other['pathnamehashes']) {
+            $eventdata->pathnamehashes = $this->other['pathnamehashes'];
+        }
+        return $eventdata;
+    }
+
+    /**
+     * Return the legacy event name.
+     *
+     * @return string
+     */
+    protected function get_legacy_eventname() {
+        return 'assessable_content_uploaded';
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return \lang_string
+     */
+    public static function get_name() {
+        return new \lang_string('event_assessable_uploaded', 'mod_forum');
+    }
+
+    /**
+     * Get URL related to the action.
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new \moodle_url('/mod/forum/discuss.php', array('d' => $this->other['discussionid'], 'parent' => $this->objectid));
+    }
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        parent::init();
+        $this->data['objecttable'] = 'forum_posts';
+    }
+
+    /**
+     * Custom validation.
+     *
+     * @throws coding_exception
+     * @return void
+     */
+    protected function validate_data() {
+        parent::validate_data();
+        if (!isset($this->other['discussionid'])) {
+            throw new coding_exception('discussionid must be set in $other.');
+        } else if (!isset($this->other['triggeredfrom'])) {
+            throw new coding_exception('triggeredfrom must be set in $other');
+        }
+    }
+}
