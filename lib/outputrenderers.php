4c2892c654aa68cee3a663dc718d6dc4ba4fc112
Sam Hemelryk <sam@moodle.com>
2013-08-01 16:52:18 +1200

    MDL-40842 output: fixed fatal error display bug - thanks Marina for spotting it!

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -2527,7 +2527,7 @@ EOD;
             $message .= '<p class="errormessage">' . get_string('installproblem', 'error') . '</p>';
             //It is usually not possible to recover from errors triggered during installation, you may need to create a new database or use a different database prefix for new installation.
         }
-        $output .= $this->box($message, 'errorbox', array('data-rel' => 'fatalerror'));
+        $output .= $this->box($message, 'errorbox', null, array('data-rel' => 'fatalerror'));
 
         if (debugging('', DEBUG_DEVELOPER)) {
             if (!empty($debuginfo)) {
