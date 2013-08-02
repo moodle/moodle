84028810f40ed12d6bf59699adb40fda49323a47
sam marshall <s.marshall@open.ac.uk>
2013-08-02 14:37:56 +0100

    MDL-41004 Resource: Does not have generator

---
+++ mod/resource/tests/generator_test.php
@@ -0,0 +1,75 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * PHPUnit data generator tests.
+ *
+ * @package mod_resource
+ * @category phpunit
+ * @copyright 2013 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * PHPUnit data generator testcase.
+ *
+ * @package mod_resource
+ * @category phpunit
+ * @copyright 2013 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_resource_generator_testcase extends advanced_testcase {
+    public function test_generator() {
+        global $DB, $SITE;
+
+        $this->resetAfterTest(true);
+
+        // Must be a non-guest user to create resources.
+        $this->setAdminUser();
+
+        // There are 0 resources initially.
+        $this->assertEquals(0, $DB->count_records('resource'));
+
+        // Create the generator object and do standard checks.
+        $generator = $this->getDataGenerator()->get_plugin_generator('mod_resource');
+        $this->assertInstanceOf('mod_resource_generator', $generator);
+        $this->assertEquals('resource', $generator->get_modulename());
+
+        // Create three instances in the site course.
+        $generator->create_instance(array('course' => $SITE->id));
+        $generator->create_instance(array('course' => $SITE->id));
+        $resource = $generator->create_instance(array('course' => $SITE->id));
+        $this->assertEquals(3, $DB->count_records('resource'));
+
+        // Check the course-module is correct.
+        $cm = get_coursemodule_from_instance('resource', $resource->id);
+        $this->assertEquals($resource->id, $cm->instance);
+        $this->assertEquals('resource', $cm->modname);
+        $this->assertEquals($SITE->id, $cm->course);
+
+        // Check the context is correct.
+        $context = context_module::instance($cm->id);
+        $this->assertEquals($resource->cmid, $context->instanceid);
+
+        // Check that generated resource module contains a file.
+        $fs = get_file_storage();
+        $files = $fs->get_area_files($context->id, 'mod_resource', 'content', false, '', false);
+        $this->assertEquals(1, count($files));
+    }
+}
