54bc6d1955f47ab38420454409da8ff97fd7fcfe
Rajesh Taneja <rajesh@moodle.com>
2013-08-26 15:20:57 +0800

    MDL-41418 Administration: Page context should be set before setting page title

--- error/index.php
+++ error/index.php
@@ -49,9 +49,9 @@
     header("Status: 404 Not Found");
 
     $PAGE->set_url('/error/');
+    $PAGE->set_context(context_system::instance());
     $PAGE->set_title($site->fullname .':Error');
     $PAGE->set_heading($site->fullname .': Error 404');
-    $PAGE->set_context(context_system::instance());
     $PAGE->navbar->add('Error 404 - File not Found');
     echo $OUTPUT->header();
     echo $OUTPUT->box(get_string('pagenotexist', 'error'). '<br />'.s($requesturi), 'generalbox boxaligncenter');
