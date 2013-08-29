2bcfd98eab96cb7fc43befe8c204885f854b96a4
Ankit Agarwal <ankit@moodle.com>
2013-08-29 14:54:26 +0800

    MDL-41485 calendar: Replace value of action buttons on managesubscription page with int

--- calendar/lib.php
+++ calendar/lib.php
@@ -111,6 +111,16 @@ define('CALENDAR_IMPORT_EVENT_UPDATED',  1);
 define('CALENDAR_IMPORT_EVENT_INSERTED', 2);
 
 /**
+ * CALENDAR_SUBSCRIPTION_UPDATE - Used to represent update action for subscriptions in various forms.
+ */
+define('CALENDAR_SUBSCRIPTION_UPDATE', 1);
+
+/**
+ * CALENDAR_SUBSCRIPTION_REMOVE - Used to represent remove action for subscriptions in various forms.
+ */
+define('CALENDAR_SUBSCRIPTION_REMOVE', 2);
+
+/**
  * Return the days of the week
  *
  * @return array array of days
@@ -2851,11 +2861,9 @@ function calendar_process_subscription_row($subscriptionid, $pollinterval, $acti
     // Fetch the subscription from the database making sure it exists.
     $sub = $DB->get_record('event_subscriptions', array('id' => $subscriptionid), '*', MUST_EXIST);
 
-    $strupdate = get_string('update');
-    $strremove = get_string('remove');
     // Update or remove the subscription, based on action.
     switch ($action) {
-        case $strupdate:
+        case CALENDAR_SUBSCRIPTION_UPDATE:
             // Skip updating file subscriptions.
             if (empty($sub->url)) {
                 break;
@@ -2866,7 +2874,7 @@ function calendar_process_subscription_row($subscriptionid, $pollinterval, $acti
             // Update the events.
             return "<p>".get_string('subscriptionupdated', 'calendar', $sub->name)."</p>" . calendar_update_subscription_events($subscriptionid);
 
-        case $strremove:
+        case CALENDAR_SUBSCRIPTION_REMOVE:
             calendar_delete_subscription($subscriptionid);
             return get_string('subscriptionremoved', 'calendar', $sub->name);
             break;
