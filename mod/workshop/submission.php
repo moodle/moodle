146893d62f4da09a9440d6a8220ab248dd473bd6
Adrian Greeve <adrian@moodle.com>
2013-09-03 11:44:30 +0800

    MDL-41556 - workshop: undefined constant FILE_INTERNAL message fixed.

--- mod/workshop/submission.php
+++ mod/workshop/submission.php
@@ -26,6 +26,7 @@
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/locallib.php');
+require_once($CFG->dirroot . '/repository/lib.php');
 
 $cmid   = required_param('cmid', PARAM_INT);            // course module id
 $id     = optional_param('id', 0, PARAM_INT);           // submission id
