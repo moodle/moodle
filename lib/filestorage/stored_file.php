137885b7e9d90d8709ade3c727f6f08631bd8966
sam marshall <s.marshall@open.ac.uk>
2013-09-10 16:34:44 +0100

    MDL-38194 Files: Allow download of large backup files
    The PHP readfile function does not work for files more than 2GB.
    This commit provides a wrapper that can be used so that files less
    than 2GB are sent with readfile, and files larger than 2GB are
    sent manually.
    
    The change applies to all uses of pluginfile.php, although in
    reality, backup files are probably the only ones likely to be more
    than two gigabytes.

--- lib/filestorage/stored_file.php
+++ lib/filestorage/stored_file.php
@@ -409,7 +409,7 @@ class stored_file {
                 throw new file_exception('storedfilecannotread', '', $path);
             }
         }
-        readfile($path);
+        readfile_allow_large($path, $this->get_filesize());
     }
 
     /**
