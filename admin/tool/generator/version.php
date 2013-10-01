1702f40046c841964b10e1acead3ae5bd5063129
Damyon Wiese <damyon@moodle.com>
2013-10-01 15:06:25 +0800

    MDL-41853 tool_generator: Fix version bump for backport

--- admin/tool/generator/version.php
+++ admin/tool/generator/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013080700;
+$plugin->version = 2013051402.04;
 $plugin->requires = 2013050100;
 $plugin->component = 'tool_generator';
