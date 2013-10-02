26422a66ad1f004cee7c92b74c2766429f7c6dbe
Frederic Massart <fred@moodle.com>
2013-10-02 13:10:45 +0800

    MDL-40050 webservice: Moving new events test file to right location

--- webservice/tests/events_test.php
+++ webservice/tests/events_test.php
@@ -33,7 +33,7 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright  2013 Frédéric Massart
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class webservice_events_testcase extends advanced_testcase {
+class core_webservice_events_testcase extends advanced_testcase {
 
     public function setUp() {
         $this->resetAfterTest();
