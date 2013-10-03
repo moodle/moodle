983c46a8a766b5e976b35b059f56ac518210a3ab
rwijaya <rwijaya@moodle.com>
2013-10-03 15:47:47 +0800

    MDL-40829 frontpage course heading: add padding to coursename and fix info ajax

--- course/yui/src/categoryexpander/js/categoryexpander.js
+++ course/yui/src/categoryexpander/js/categoryexpander.js
@@ -32,7 +32,7 @@ var CSS = {
         COLLAPSEEXPAND: '.collapseexpand',
         COURSEBOX: '.coursebox',
         COURSEBOXLISTENLINK: '.coursebox .moreinfo',
-        COURSEBOXSPINNERLOCATION: '.categoryname a',
+        COURSEBOXSPINNERLOCATION: '.coursename a',
         COURSECATEGORYTREE: '.course_category_tree',
         PARENTWITHCHILDREN: '.category'
     },
