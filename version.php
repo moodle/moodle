f63fc5c41a5b3f93918949f0e3824eb15e1342e5
Matteo Scaramuccia <moodle@matteoscaramuccia.com>
2013-10-06 16:45:16 +0200

    MDL-40226 Files: Added the correct MIME Type for JNLP files. JNLP stands for Java Network Launch Protocol, used to launch Java Web Start applications. More details in: http://docs.oracle.com/javase/tutorial/deployment/webstart/deploying.html

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2012120306.04;              // 20121203      = branching date YYYYMMDD - do not modify!
+$version  = 2012120306.05;              // 20121203      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
