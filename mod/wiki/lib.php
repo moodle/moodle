6d3d9bd927d5386b1dc4fbbcb7db5559efa7a5cd
Adrian Greeve <adrian@moodle.com>
2013-10-10 13:18:11 +0800

    MDL-42238 - mod_wiki: Alternate name sql update to wiki_print_recent_activity

--- mod/wiki/lib.php
+++ mod/wiki/lib.php
@@ -279,10 +279,12 @@ function wiki_supports($feature) {
 function wiki_print_recent_activity($course, $viewfullnames, $timestart) {
     global $CFG, $DB, $OUTPUT;
 
-    $sql = "SELECT p.*, w.id as wikiid, sw.groupid
+    $usernamefields = get_all_user_name_fields(true, 'u');
+    $sql = "SELECT p.*, w.id as wikiid, sw.groupid, $usernamefields
             FROM {wiki_pages} p
                 JOIN {wiki_subwikis} sw ON sw.id = p.subwikiid
                 JOIN {wiki} w ON w.id = sw.wikiid
+                JOIN {user} u ON u.id = sw.userid
             WHERE p.timemodified > ? AND w.course = ?
             ORDER BY p.timemodified ASC";
     if (!$pages = $DB->get_records_sql($sql, array($timestart, $course->id))) {
