96e991631dff6d39cc4ea295d9c070dffd9a6e83
Ankit Agarwal <ankit@moodle.com>
2013-10-10 17:16:00 +0800

    MDL-42241 tool_uploadcourse: Fix incorrect urls

--- admin/tool/uploadcourse/settings.php
+++ admin/tool/uploadcourse/settings.php
@@ -26,5 +26,5 @@ defined('MOODLE_INTERNAL') || die();
 
 if ($hassiteconfig) {
     $ADMIN->add('courses', new admin_externalpage('tooluploadcourse',
-        get_string('uploadcourses', 'tool_uploadcourse'), "/admin/tool/uploadcourse/index.php"));
+        get_string('uploadcourses', 'tool_uploadcourse'), "$CFG->wwwroot/$CFG->admin/tool/uploadcourse/index.php"));
 }
