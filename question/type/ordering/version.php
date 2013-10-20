db585fc3ea6cbe237cb3f8b1c500e729905d6e3f
Gordon Bateson <gorodnbateson@gmail.com>
2013-10-20 20:49:57 +0900

    MDL-79863 qtype_ordering: Moodle.2x qtype/ordering standardize version.php and add readme.txt

--- question/type/ordering/version.php
+++ question/type/ordering/version.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,16 +16,20 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version information for the multiple choice question type.
+ * Version information for the ORDERING question type
  *
  * @package    qtype
- * @subpackage multichoice
- * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @subpackage ordering
+ * @copyright  2010 Thomas Robb
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+// prevent direct access to this script
 defined('MOODLE_INTERNAL') || die();
 
+$plugin->cron      = 0;
 $plugin->component = 'qtype_ordering';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->version  = 2013062800;
+$plugin->maturity  = MATURITY_STABLE; // ALPHA=50, BETA=100, RC=150, STABLE=200
+$plugin->release   = '2013-06-28.00';
+$plugin->version   = 2013062800;
+$plugin->requires  = 2010112400; // Moodle 2.0
