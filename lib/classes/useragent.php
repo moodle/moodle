a6989aa6e93ead31d9bde98c85a847c2e6e88175
Michael de Raadt <michaeld@moodle.com>
2013-10-23 14:52:08 +0800

    MDL-42491 libraries: add exception for IE11 (and hopefully later versions)

--- lib/classes/useragent.php
+++ lib/classes/useragent.php
@@ -499,6 +499,9 @@ class core_useragent {
         // See: http://www.useragentstring.com/pages/Internet%20Explorer/.
         if (preg_match("/MSIE ([0-9\.]+)/", $useragent, $match)) {
             $browser = $match[1];
+        // See: http://msdn.microsoft.com/en-us/library/ie/bg182625%28v=vs.85%29.aspx for IE11+ useragent details.
+        } else if (preg_match("/Trident\/[0-9\.]+/", $useragent) && preg_match("/rv:([0-9\.]+)/", $useragent, $match)) {
+            $browser = $match[1];
         } else {
             return false;
         }
