8e2b2206f4c47eed8825dc02767bd255500c69fe
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-10-25 13:03:22 +0200

    weekly release 2.3.9+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2012062509.04;              // YYYYMMDD      = weekly release date of this DEV branch
+$version  = 2012062509.05;              // YYYYMMDD      = weekly release date of this DEV branch
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
-$release  = '2.3.9+ (Build: 20131018)';  // Human-friendly version name
+$release  = '2.3.9+ (Build: 20131025)';  // Human-friendly version name
 
 $branch   = '23';                       // this version's branch
 $maturity = MATURITY_STABLE;            // this version's maturity level
