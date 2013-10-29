54a0a16b53f94e8d2cd7eacc293f48c2913ed0e9
Marina Glancy <marina@moodle.com>
2013-10-29 16:56:20 +1100

    MDL-41337 files: Fixed bug with accidental delete of the root directory

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2012120306.08;              // 20121203      = branching date YYYYMMDD - do not modify!
+$version  = 2012120306.09;              // 20121203      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
