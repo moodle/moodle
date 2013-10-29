c3a40deab7c88d7d6460c6c929c470cade875c35
David Monllao <davidm@moodle.com>
2013-10-29 17:16:32 +0800

    MDL-42627 behat: New steps definitions class for deprecated steps

--- lib/behat/classes/behat_config_manager.php
+++ lib/behat/classes/behat_config_manager.php
@@ -93,6 +93,11 @@ class behat_config_manager {
             }
         }
 
+        // We don't want the deprecated steps definitions here.
+        if (!$testsrunner) {
+            unset($stepsdefinitions['behat_deprecated']);
+        }
+
         // Behat config file specifing the main context class,
         // the required Behat extensions and Moodle test wwwroot.
         $contents = self::get_config_file_contents($features, $stepsdefinitions);
---
+++ lib/tests/behat/behat_deprecated.php
@@ -0,0 +1,67 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Steps definitions that will be deprecated in the next releases.
+ *
+ * @package    core
+ * @category   test
+ * @copyright  2013 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../lib/behat/behat_base.php');
+
+/**
+ * Deprecated behat step definitions.
+ *
+ * @package    core
+ * @category   test
+ * @copyright  2013 David Monllaó
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_deprecated extends behat_base {
+
+    /**
+     * Throws an exception if $CFG->behat_usedeprecated is not allowed.
+     *
+     * @throws Exception
+     * @param string|array $alternatives Alternative/s to the requested step
+     * @return void
+     */
+    protected function deprecated_message($alternatives) {
+        global $CFG;
+
+        // We do nothing if it is enabled.
+        if (!empty($CFG->behat_usedeprecated)) {
+            return;
+        }
+
+        if (is_scalar($alternatives)) {
+            $alternatives = array($alternatives);
+        }
+
+        $message = 'Deprecated step, rather than using this step you can:';
+        foreach ($alternatives as $alternative) {
+            $message .= PHP_EOL . '- ' . $alternative;
+        }
+        $message .= PHP_EOL . '- Set $CFG->behat_usedeprecated in config.php to allow the use of deprecated steps if you don\'t have any other option';
+        throw new Exception($message);
+    }
+
+}
