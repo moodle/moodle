d583059471bb57d19eddf31850c8b9bb6f867f69
Rajesh Taneja <rajesh@moodle.com>
2013-10-30 14:56:27 +0800

    MDL-42403 course: Show bluk move to category only when something selected

--- course/yui/src/management/js/console.js
+++ course/yui/src/management/js/console.js
@@ -250,6 +250,21 @@ Console.prototype = {
         if (!listing) {
             return false;
         }
+
+        // Disable category bulk actions as nothing will be selected on initialise.
+        var menumovecatto = listing.one('#menumovecategoriesto');
+        if (menumovecatto) {
+            menumovecatto.setAttribute('disabled', true);
+        }
+        var menuresortcategoriesby = listing.one('#menuresortcategoriesby');
+        if (menuresortcategoriesby) {
+            menuresortcategoriesby.setAttribute('disabled', true);
+        }
+        var menuresortcoursesby = listing.one('#menuresortcoursesby');
+        if (menuresortcoursesby) {
+            menuresortcoursesby.setAttribute('disabled', true);
+        }
+
         listing.all('.listitem[data-id]').each(function(node){
             this.set('categories', new Category({
                 node : node,
@@ -259,6 +274,8 @@ Console.prototype = {
         }, this);
         if (!this.categoriesinit) {
             this.get('categorylisting').delegate('click', this.handleCategoryDelegation, 'a[data-action]', this);
+            this.get('categorylisting').delegate('click', this.handleCategoryDelegation, 'input[name="bcat[]"]', this);
+            this.get('categorylisting').delegate('click', this.handleBulkSortByaction, '#menuselectsortby', this);
             this.categoriesinit = true;
             Y.log(count+' categories being managed', 'info', 'moodle-course-management');
         } else {
@@ -279,6 +296,13 @@ Console.prototype = {
         if (!listing) {
             return false;
         }
+
+        // Disable course move to bulk action as nothing will be selected on initialise.
+        var menumovecoursesto = listing.one('#menumovecoursesto');
+        if (menumovecoursesto) {
+            menumovecoursesto.setAttribute('disabled', true);
+        }
+
         listing.all('.listitem[data-id]').each(function(node){
             this.registerCourse(new Course({
                 node : node,
@@ -288,6 +312,7 @@ Console.prototype = {
             count++;
         }, this);
         listing.delegate('click', this.handleCourseDelegation, 'a[data-action]', this);
+        listing.delegate('click', this.handleCourseDelegation, 'input[name="bc[]"]', this);
         Y.log(count+' courses being managed', 'info', 'moodle-course-management');
     },
 
@@ -337,6 +362,118 @@ Console.prototype = {
             category.handle(action, e);
         } else {
             Y.log('Could not find category to delegate to.', 'error', 'moodle-course-management');
+        }
+    },
+
+    /**
+     * Check if any course is selected.
+     *
+     * @method isCourseSelected
+     * @param {Node} checkboxnode Checkbox node on which action happened.
+     * @return bool
+     */
+    isCourseSelected : function(checkboxnode) {
+        var selected = false;
+
+        // If any course selected then show move to category select box.
+        if (checkboxnode && checkboxnode.get('checked')) {
+            selected = true;
+        } else {
+            var i,
+                course,
+                courses = this.get('courses'),
+                length = courses.length;
+            for (i = 0; i < length; i++) {
+                if (courses.hasOwnProperty(i)) {
+                    course = courses[i];
+                    if (course.get('node').one('input[name="bc[]"]').get('checked')) {
+                        selected = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return selected;
+    },
+
+    /**
+     * Check if any category is selected.
+     *
+     * @method isCategorySelected
+     * @param {Node} checkboxnode Checkbox node on which action happened.
+     * @return bool
+     */
+    isCategorySelected : function(checkboxnode) {
+        var selected = false;
+
+        // If any category selected then show move to category select box.
+        if (checkboxnode && checkboxnode.get('checked')) {
+            selected = true;
+        } else {
+            var i,
+                category,
+                categories = this.get('categories'),
+                length = categories.length;
+            for (i = 0; i < length; i++) {
+                if (categories.hasOwnProperty(i)) {
+                    category = categories[i];
+                    if (category.get('node').one('input[name="bcat[]"]').get('checked')) {
+                        selected = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return selected;
+    },
+
+    /**
+     * Handle bulk sort action.
+     *
+     * @method handleBulkSortByaction
+     * @protected
+     * @param {EventFacade} e
+     */
+    handleBulkSortByaction : function(e) {
+        var sortcategoryby = this.get('categorylisting').one('#menuresortcategoriesby'),
+            sortcourseby = this.get('categorylisting').one('#menuresortcoursesby'),
+            sortbybutton = this.get('categorylisting').one('input[name="bulksort"]');
+            sortby = e;
+
+        if (!sortby) {
+            sortby = this.get('categorylisting').one('#menuselectsortby');
+        } else {
+            if (e && e.currentTarget) {
+                sortby = e.currentTarget;
+            }
+        }
+
+        // If no sortby select found then return as we can't do anything.
+        if (!sortby) {
+            return;
+        }
+
+        if ((this.get('categories').length <= 1) || (!this.isCategorySelected() &&
+                (sortby.get("options").item(sortby.get('selectedIndex')).getAttribute('value') === 'selectedcategories'))) {
+            if (sortcategoryby) {
+                sortcategoryby.setAttribute('disabled', true);
+            }
+            if (sortcourseby) {
+                sortcourseby.setAttribute('disabled', true);
+            }
+            if (sortbybutton) {
+                sortbybutton.setAttribute('disabled', true);
+            }
+        } else {
+            if (sortcategoryby) {
+                sortcategoryby.removeAttribute('disabled');
+            }
+            if (sortcourseby) {
+                sortcourseby.removeAttribute('disabled');
+            }
+            if (sortbybutton) {
+                sortbybutton.removeAttribute('disabled');
+            }
         }
     },
 
