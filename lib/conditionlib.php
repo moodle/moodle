2aed63d053d0c8b8515c6dd62ec11048dc7ba98c
Dan Poltawski <dan@moodle.com>
2013-11-04 22:01:18 +0800

    MDL-42533 conditionlib: Add clarifying comments

--- lib/conditionlib.php
+++ lib/conditionlib.php
@@ -830,6 +830,7 @@ abstract class condition_info_base {
                 $a = new stdclass;
                 // Display the fieldname into current lang.
                 if (is_numeric($field)) {
+                    // Is a custom profile field (will use multilang).
                     $translatedfieldname = $details->fieldname;
                 } else {
                     $translatedfieldname = get_user_field_name($details->fieldname);
@@ -1067,6 +1068,7 @@ abstract class condition_info_base {
                     $available = false;
                     // Display the fieldname into current lang.
                     if (is_numeric($field)) {
+                        // Is a custom profile field (will use multilang).
                         $translatedfieldname = $details->fieldname;
                     } else {
                         $translatedfieldname = get_user_field_name($details->fieldname);
