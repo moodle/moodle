2bdd5ff4aa9fefbb167f8c4370b4a464e1712a80
Damyon Wiese <damyon@moodle.com>
2013-11-08 14:26:49 +0800

    MDL-36224 LTI: Fix up docs, add plugininfo class and create subplugin folder.

--- mod/lti/classes/plugininfo/ltisource.php
+++ mod/lti/classes/plugininfo/ltisource.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,15 +15,19 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * LTI subplugin types declaration
+ * LTI source plugin info.
  *
- * @package    mod_lti
- * @copyright  2012 Mark Nielsen
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   mod_lti
+ * @copyright 2013 Damyon Wiese
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+namespace mod_lti\plugininfo;
+
+use core\plugininfo\base, core_plugin_manager, moodle_url;
 
 defined('MOODLE_INTERNAL') || die();
 
-$subplugins = array(
-    'ltisource' => 'mod/lti/source',
-);
+
+class ltisource extends base {
+    // 
+}
--- mod/lti/source/readme.txt
+++ mod/lti/source/readme.txt
@@ -1,5 +1,3 @@
-<?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,15 +14,14 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * LTI subplugin types declaration
+ * Readme file for local customisations
  *
  * @package    mod_lti
  * @copyright  2012 Mark Nielsen
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
-$subplugins = array(
-    'ltisource' => 'mod/lti/source',
-);
+LTI Source plugins directory
+==============================
+This directory contains LTI source plugins which are plugins that can register custom handlers
+to process lti messages.
