7444cf322ee1c863dad7e52ec2b5e8ea7266b067
Jerome Mouneyrac <jerome@mouneyrac.com>
2013-11-11 11:39:27 +0800

    MDL-42081 EditPDF: Add default stamp images

---
+++ mod/assign/feedback/editpdf/db/upgrade.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Upgrade code for the feedback_editpdf module.
+ *
+ * @package   assignfeedback_editpdf
+ * @copyright 2013 Jerome Mouneyrac
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * EditPDF upgrade code
+ * @param int $oldversion
+ * @return bool
+ */
+function xmldb_assignfeedback_editpdf_upgrade($oldversion) {
+    global $CFG;
+
+    if ($oldversion < 2013110800) {
+
+        // Check that no stamps where uploaded.
+        $fs = get_file_storage();
+        $stamps = $fs->get_area_files(context_system::instance()->id, 'assignfeedback_editpdf',
+            'stamps', 0, "filename", false);
+
+        // Add default stamps.
+        if (empty($stamps)) {
+            // List of default stamps.
+            $defaultstamps = array('smile.png', 'sad.png', 'tick.png', 'cross.png');
+
+            // Stamp file object.
+            $filerecord = new stdClass;
+            $filerecord->component = 'assignfeedback_editpdf';
+            $filerecord->contextid = context_system::instance()->id;
+            $filerecord->userid    = get_admin()->id;
+            $filerecord->filearea  = 'stamps';
+            $filerecord->filepath  = '/';
+            $filerecord->itemid    = 0;
+
+            // Add all default stamps.
+            foreach ($defaultstamps as $stamp) {
+                $filerecord->filename = $stamp;
+                $fs->create_file_from_pathname($filerecord,
+                    $CFG->dirroot . '/mod/assign/feedback/editpdf/pix/' . $filerecord->filename);
+            }
+        }
+
+        upgrade_plugin_savepoint(true, 2013110800, 'assignfeedback', 'editpdf');
+    }
+
+    return true;
+}
