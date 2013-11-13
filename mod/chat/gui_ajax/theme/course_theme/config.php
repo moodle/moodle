96c124fca0f40e8c18e89701fd33c9e32dd1d208
Aparup Banerjee <aparup@moodle.com>
2013-11-13 17:07:39 +0800

    MDL-42711 added RTL styles to chat.less .

--- mod/chat/gui_ajax/theme/course_theme/config.php
+++ mod/chat/gui_ajax/theme/course_theme/config.php
@@ -3,14 +3,14 @@ $chattheme_cfg = new stdClass();
 $chattheme_cfg->avatar = false;
 $chattheme_cfg->align  = false;
 $chattheme_cfg->event_message = <<<TEMPLATE
-<div class="chat-event">
+<div class="chat-event course-theme">
 <span class="time">___time___</span>
 <a target='_blank' href="___senderprofile___">___sender___</a>
 <span class="event">___event___</span>
 </div>
 TEMPLATE;
 $chattheme_cfg->user_message = <<<TEMPLATE
-<div class='chat-message'>
+<div class='chat-message course-theme'>
     <div class="chat-message-meta">
         <span class="time">___time___</span>
         <span class="user"><a href="___senderprofile___" target="_blank">___sender___</a></span>
