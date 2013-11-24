35897a5223e33a8bd158a49ed22ce0096670fe06
Matteo Scaramuccia <moodle@matteoscaramuccia.com>
2013-11-24 09:21:14 +0100

    MDL-43039 Libraries "The session partition is full." false positive.

--- lib/classes/session/file.php
+++ lib/classes/session/file.php
@@ -65,7 +65,8 @@ class file extends handler {
         // Need to disable debugging since disk_free_space()
         // will fail on very large partitions (see MDL-19222).
         $freespace = @disk_free_space($this->sessiondir);
-        if (!($freespace > 2048) and $freespace !== false) {
+        // MDL-43039: disk_free_space() returns null if disabled.
+        if (!($freespace > 2048) and ($freespace !== false) and ($freespace !== null)) {
             throw new exception('sessiondiskfull', 'error');
         }
 
