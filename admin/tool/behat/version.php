f4ce42df465a5e3412972b9fe7e5cf306ce4118e
Dan Poltawski <dan@moodle.com>
2013-11-27 11:34:16 +0800

    MDL-42942 behat: increment version

--- admin/tool/behat/version.php
+++ admin/tool/behat/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013050101;
+$plugin->version   = 2013050102;
 $plugin->requires  = 2013050100; // Requires Moodle 2.5.
 $plugin->component = 'tool_behat';
