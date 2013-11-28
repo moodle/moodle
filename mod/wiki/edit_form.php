23353087ca50a9fb890201e17c05c83cebd7fc04
Michael Dikih <michael.dikih@netspot.com.au>
2013-11-28 16:56:03 +1030

    MDL-43109: mod/wiki - When editing pages populate the default value for the tags input.

--- mod/wiki/edit_form.php
+++ mod/wiki/edit_form.php
@@ -101,10 +101,8 @@ class mod_wiki_edit_form extends moodleform {
         $mform->setType('contentformat', PARAM_ALPHANUMEXT);
 
         if (!empty($CFG->usetags)) {
-            $tags = !isset($this->_customdata['tags'])?"":$this->_customdata['tags'];
             $mform->addElement('header', 'tagshdr', get_string('tags', 'tag'));
             $mform->addElement('tags', 'tags', get_string('tags'));
-            $mform->setDefault('tags', $tags);
             $mform->setType('tags', PARAM_TEXT);
         }
 
