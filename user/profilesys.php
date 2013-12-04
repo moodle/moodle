bc7ac306695b43501a7521d2c0f2b98855e7b84e
Rajesh Taneja <rajesh@moodle.com>
2013-12-04 14:50:35 +0800

    MDL-43028 navigation: fixed section for profilepage
    section for profile page is profilepage and not mypage

--- user/profilesys.php
+++ user/profilesys.php
@@ -40,7 +40,7 @@ require_login();
 $header = "$SITE->shortname: ".get_string('publicprofile')." (".get_string('myprofile', 'admin').")";
 
 $PAGE->set_blocks_editing_capability('moodle/my:configsyspages');
-admin_externalpage_setup('mypage', '', null, '', array('pagelayout' => 'mydashboard'));
+admin_externalpage_setup('profilepage', '', null, '', array('pagelayout' => 'mydashboard'));
 
 // Override pagetype to show blocks properly.
 $PAGE->set_pagetype('user-profile');
