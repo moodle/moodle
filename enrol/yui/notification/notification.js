c0c1b7ac979e3a7a300d4351a4a7405aba4d3206
Andrew Nicols <andrew@nicols.co.uk>
2013-12-05 09:42:31 +0800

    MDL-43212 JavaScript: Remove deprecated moodle-enrol-notification

--- enrol/yui/notification/notification.js
+++
@@ -1,7 +0,0 @@
-/**
- * This module provides backwards compatability and should be removed
- * entirely in Moodle 2.5
- */
-YUI.add('moodle-enrol-notification', function(Y) {
-    console.log("You are using a deprecated name. Please update your YUI module to use moodle-core-notification instead of moodle-enrol-notification");
-}, '@VERSION@', {requires:['base','node','overlay','event-key', 'moodle-core-notification']});
