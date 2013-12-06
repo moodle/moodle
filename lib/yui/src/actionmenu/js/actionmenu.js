jishnugopim/moodle-1

MDL-42796 ActionMenu: Move event handlers to showMenu

lib/yui/src/actionmenu/js/actionmenu.js
@@ -149,23 +149,6 @@ ACTIONMENU.prototype = {
             return;
         }
         this.showMenu(e, menu);
-        // Close the menu if the user presses escape.
-        this.events.push(BODY.on('key', this.hideMenu, 'esc', this));
-        // Close the menu if the user clicks outside the menu.
-        this.events.push(BODY.on('click', this.hideIfOutside, this));
-        // Close the menu if the user focuses outside the menu.
-        this.events.push(BODY.delegate('focus', this.hideIfOutside, '*', this));
-
-        // Check tabbing.
-        this.events.push(menu.delegate('key', this.checkFocus, 'down:9', SELECTOR.MENUCHILD, this));
-
-        // Close the menu after a button was pushed.
-        this.events.push(menu.delegate('click', function(e) {
-            if (e.currentTarget.test(SELECTOR.KEEPOPEN)) {
-                return;
-            }
-            this.hideMenu();
-        }, SELECTOR.MENUCONTENTCHILD, this));
     },
 
     /**
@@ -240,6 +223,24 @@ ACTIONMENU.prototype = {
                 menuchild.focus();
             }
         }
+
+        // Close the menu if the user presses escape.
+        this.events.push(BODY.on('key', this.hideMenu, 'esc', this));
+        // Close the menu if the user clicks outside the menu.
+        this.events.push(BODY.on('click', this.hideIfOutside, this));
+        // Close the menu if the user focuses outside the menu.
+        this.events.push(BODY.delegate('focus', this.hideIfOutside, '*', this));
+
+        // Check tabbing.
+        this.events.push(menu.delegate('key', this.checkFocus, 'down:9', SELECTOR.MENUCHILD, this));
+
+        // Close the menu after a button was pushed.
+        this.events.push(menu.delegate('click', function(e) {
+            if (e.currentTarget.test(SELECTOR.KEEPOPEN)) {
+                return;
+            }
+            this.hideMenu();
+        }, SELECTOR.MENUCONTENTCHILD, this));
 
         return true;
     },

moodle/moodle

lib/yui/src/actionmenu/js/actionmenu.js
@@ -149,23 +149,6 @@ ACTIONMENU.prototype = {
             return;
         }
         this.showMenu(e, menu);
-        // Close the menu if the user presses escape.
-        this.events.push(BODY.on('key', this.hideMenu, 'esc', this));
-        // Close the menu if the user clicks outside the menu.
-        this.events.push(BODY.on('click', this.hideIfOutside, this));
-        // Close the menu if the user focuses outside the menu.
-        this.events.push(BODY.delegate('focus', this.hideIfOutside, '*', this));
-
-        // Check tabbing.
-        this.events.push(menu.delegate('key', this.checkFocus, 'down:9', SELECTOR.MENUCHILD, this));
-
-        // Close the menu after a button was pushed.
-        this.events.push(menu.delegate('click', function(e) {
-            if (e.currentTarget.test(SELECTOR.KEEPOPEN)) {
-                return;
-            }
-            this.hideMenu();
-        }, SELECTOR.MENUCHILD, this));
     },
 
     /**
@@ -240,6 +223,24 @@ ACTIONMENU.prototype = {
                 menuchild.focus();
             }
         }
+
+        // Close the menu if the user presses escape.
+        this.events.push(BODY.on('key', this.hideMenu, 'esc', this));
+        // Close the menu if the user clicks outside the menu.
+        this.events.push(BODY.on('click', this.hideIfOutside, this));
+        // Close the menu if the user focuses outside the menu.
+        this.events.push(BODY.delegate('focus', this.hideIfOutside, '*', this));
+
+        // Check tabbing.
+        this.events.push(menu.delegate('key', this.checkFocus, 'down:9', SELECTOR.MENUCHILD, this));
+
+        // Close the menu after a button was pushed.
+        this.events.push(menu.delegate('click', function(e) {
+            if (e.currentTarget.test(SELECTOR.KEEPOPEN)) {
+                return;
+            }
+            this.hideMenu();
+        }, SELECTOR.MENUCHILD, this));
 
         return true;
     },