be6128fcd52656589f51f644968894c7148012d5
Petr Škoda <commits@skodak.org>
2013-12-09 09:11:02 +0800

    MDL-36721 do not store passwords in config logs

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2013111800.07;              // 20131118      = branching date YYYYMMDD - do not modify!
+$version  = 2013111800.08;              // 20131118      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
