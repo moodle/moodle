9b378f3dc590f7f22c32c1c4414e4d640d376e4b
Damyon Wiese <damyon@moodle.com>
2013-12-09 10:24:17 +0800

    MDL-40096 assign: Prevent js error because uniqid() is not unique.
    Use html_writer::random_id instead because it incorporates a static counter.

--- mod/assign/renderer.php
+++ mod/assign/renderer.php
@@ -57,7 +57,7 @@ class mod_assign_renderer extends plugin_renderer_base {
      * @return string
      */
     public function render_assign_files(assign_files $tree) {
-        $this->htmlid = 'assign_files_tree_'.uniqid();
+        $this->htmlid = html_writer::random_id('assign_files_tree');
         $this->page->requires->js_init_call('M.mod_assign.init_tree', array(true, $this->htmlid));
         $html = '<div id="'.$this->htmlid.'">';
         $html .= $this->htmllize_tree($tree, $tree->dir);
