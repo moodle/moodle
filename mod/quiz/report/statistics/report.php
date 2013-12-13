7cd9c85868bd9e6b5eef15c364d424286511e59e
James Pratt <me@jamiep.org>
2013-12-13 17:15:41 +0700

    MDL-43369 : quiz statistics - average across attempts
    where item seen and not across all attempts for subqs.

--- mod/quiz/report/statistics/report.php
+++ mod/quiz/report/statistics/report.php
@@ -676,7 +676,6 @@ class quiz_statistics_report extends quiz_default_report {
             return $this->get_emtpy_stats($questions, $firstattempts->countrecs,
                     $allattempts->countrecs);
         }
-        $summarksavg = $usingattempts->total / $usingattempts->countrecs;
 
         $quizstats = new stdClass();
         $quizstats->allattempts = $useallattempts;
@@ -750,7 +749,7 @@ class quiz_statistics_report extends quiz_default_report {
             }
         }
 
-        $qstats = new quiz_statistics_question_stats($questions, $s, $summarksavg);
+        $qstats = new quiz_statistics_question_stats($questions);
         $qstats->load_step_data($quizid, $currentgroup, $groupstudents, $useallattempts);
         $qstats->compute_statistics();
 
