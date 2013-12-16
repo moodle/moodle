545b310c8482e92bee21e6c94b572d473860659b
Frederic Massart <fmcell@gmail.com>
2013-12-16 07:03:12 +0100

    MDL-42212 mod_lesson: Use autoloading for mod_lesson_file_info

--- mod/lesson/lib.php
+++ mod/lesson/lib.php
@@ -947,7 +947,6 @@ function lesson_get_file_info($browser, $areas, $course, $cm, $context, $fileare
     }
 
     if (is_null($itemid)) {
-        require_once(__DIR__ . '/locallib.php');
         return new mod_lesson_file_info($browser, $course, $cm, $context, $areas, $filearea);
     }
 
