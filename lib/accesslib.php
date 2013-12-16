f06d89b2b53ab513677d55de1423c129a96f180b
Petr Škoda <commits@skodak.org>
2013-12-16 09:44:04 +0800

    MDL-43367 stop returning of bogus context children when path unknown
    This also prevents deletion of system context.

--- lib/accesslib.php
+++ lib/accesslib.php
@@ -5270,6 +5270,10 @@ abstract class context extends stdClass implements IteratorAggregate {
     public function delete() {
         global $DB;
 
+        if ($this->_contextlevel <= CONTEXT_SYSTEM) {
+            throw new coding_exception('Cannot delete system context');
+        }
+
         // double check the context still exists
         if (!$DB->record_exists('context', array('id'=>$this->_id))) {
             context::cache_remove($this);
@@ -5364,6 +5368,11 @@ abstract class context extends stdClass implements IteratorAggregate {
      */
     public function get_child_contexts() {
         global $DB;
+
+        if (empty($this->_path) or empty($this->_depth)) {
+            debugging('Can not find child contexts of context '.$this->_id.' try rebuilding of context paths');
+            return array();
+        }
 
         $sql = "SELECT ctx.*
                   FROM {context} ctx
@@ -6336,6 +6345,11 @@ class context_coursecat extends context {
      */
     public function get_child_contexts() {
         global $DB;
+
+        if (empty($this->_path) or empty($this->_depth)) {
+            debugging('Can not find child contexts of context '.$this->_id.' try rebuilding of context paths');
+            return array();
+        }
 
         $sql = "SELECT ctx.*
                   FROM {context} ctx
