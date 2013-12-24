e71a5333a32e85374b89b7eebd0326a4d17ea2c7
Sam Hemelryk <sam@moodle.com>
2013-12-24 17:17:49 +1300

    weekly release 2.6+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2013111800.06;              // 20131118      = branching date YYYYMMDD - do not modify!
+$version  = 2013111800.07;              // 20131118      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '2.6+ (Build: 20131220)'; // Human-friendly version name
+$release  = '2.6+ (Build: 20131224)'; // Human-friendly version name
 
 $branch   = '26';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
