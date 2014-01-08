2a203eee8d394a902f39414548a0c65403a82267
Sam Hemelryk <sam@moodle.com>
2014-01-09 11:50:50 +1300

    MDL-38923 theme_bootstrapbase: tidied up dock implementation
    Also:
    * Changed to a CSS based compacting solution
    * Converted dock colours to variables
    * RTL fixes for the dock

--- theme/bootstrapbase/config.php
+++ theme/bootstrapbase/config.php
@@ -154,7 +154,7 @@ $THEME->layouts = array(
 $THEME->javascripts = array(
 );
 $THEME->javascripts_footer = array(
-    'moodlebootstrap', 'dockmod'
+    'moodlebootstrap', 'dock'
 );
 
 if (core_useragent::is_ie() && !core_useragent::check_ie_version('9.0')) {
