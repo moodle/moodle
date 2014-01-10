007865217221064bdd9f800bb425f531657acd61
Marina Glancy <marina@moodle.com>
2014-01-10 11:59:43 +0800

    MDL-43333 blocks: move activity icon in social and main menu

--- blocks/social_activities/block_social_activities.php
+++ blocks/social_activities/block_social_activities.php
@@ -74,6 +74,8 @@ class block_social_activities extends block_list {
             $strmovefull = strip_tags(get_string('movefull', '', "'$USER->activitycopyname'"));
             $strcancel= get_string('cancel');
             $stractivityclipboard = $USER->activitycopyname;
+        } else {
+            $strmove = get_string('move');
         }
         $editbuttons = '';
 
@@ -91,6 +93,14 @@ class block_social_activities extends block_list {
                 }
                 if (!$ismoving) {
                     $actions = course_get_cm_edit_actions($mod, -1);
+
+                    // Prepend list of actions with the 'move' action.
+                    $actions = array('move' => new action_menu_link_primary(
+                        new moodle_url('/course/mod.php', array('sesskey' => sesskey(), 'copy' => $mod->id)),
+                        new pix_icon('t/move', $strmove, 'moodle', array('class' => 'iconsmall', 'title' => '')),
+                        $strmove
+                    )) + $actions;
+
                     $editbuttons = html_writer::tag('div',
                         $courserenderer->course_section_cm_edit_actions($actions, $mod, array('donotenhance' => true)),
                         array('class' => 'buttons')
