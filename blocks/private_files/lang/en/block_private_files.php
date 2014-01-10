4e5f3996b2fd7a3863ef1b5eba5009a6472c569c
Frederic Massart <fmcell@gmail.com>
2014-01-10 13:03:31 +0100

    MDL-40139 block_privatefiles: Remove unused edit form

--- blocks/private_files/lang/en/block_private_files.php
+++ blocks/private_files/lang/en/block_private_files.php
@@ -23,7 +23,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['managemyfiles'] = 'Manage my files';
 $string['pluginname'] = 'My private files';
 $string['privatefiles'] = 'Private files';
 $string['private_files:addinstance'] = 'Add a new private files block';
