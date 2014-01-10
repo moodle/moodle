8c5dd5e5eb4716aa83d6ac1d92be66f3212352b6
Dan Marsden <dan@danmarsden.com>
2014-01-10 21:04:54 +1300

    MDL-42792 SCORM: Prevent JS error when network connection fails. - return false to SCORM package so that issue can be detected and reported.

--- mod/scorm/request.js
+++ mod/scorm/request.js
@@ -41,7 +41,11 @@ function DoRequest(httpReq,url,param) {
     //popupwin(url+"\n"+param);
     httpReq.open("POST", url,false);
     httpReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    httpReq.send(param);
+    try {
+        httpReq.send(param);
+    } catch (e) {
+        return false;
+    }
     if (httpReq.status == 200) {
         //popupwin(url+"\n"+param+"\n"+httpReq.responseText);
         return httpReq.responseText;
