4468527db9fdeb191909a8f5814711630bfc02db
Dan Poltawski <dan@moodle.com>
2014-01-14 14:53:03 +0800

    MDL-41062 gradebook: remove sortorder duplicates
    * Upgrade function to remove duplicates from the grade item duplicates
      column. Duplicates were causing sorting to fail in some cases.
    * Add some unit tests which simulate sort order duplicate data and
      verify that they have been removed.

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2013051404.01;              // 20130514      = branching date YYYYMMDD - do not modify!
+$version  = 2013051404.02;              // 20130514      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches
                                         //           .XX = incremental changes
 
