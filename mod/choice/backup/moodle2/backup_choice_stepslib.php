799770cbd71f9d399ff6e41263ab0b9588fc50f6
Tony Butler <tonyjbutler@gmail.com>
2014-01-20 12:10:34 +0000

    MDL-43770 mod_choice: Add option to include inactive users' responses

--- mod/choice/backup/moodle2/backup_choice_stepslib.php
+++ mod/choice/backup/moodle2/backup_choice_stepslib.php
@@ -41,7 +41,7 @@ class backup_choice_activity_structure_step extends backup_activity_structure_st
             'name', 'intro', 'introformat', 'publish',
             'showresults', 'display', 'allowupdate', 'showunanswered',
             'limitanswers', 'timeopen', 'timeclose', 'timemodified',
-            'completionsubmit', 'showpreview'));
+            'completionsubmit', 'showpreview', 'includeinactive'));
 
         $options = new backup_nested_element('options');
 
