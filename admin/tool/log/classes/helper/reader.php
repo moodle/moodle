f7b2b1fb7ac29862adb3b0e3f59019523f6e40c8
Ankit Agarwal <ankit@moodle.com>
2014-01-21 11:49:11 +0800

    MDL-43759 logging: Add helper traits for stores and add support for buffering

---
+++ admin/tool/log/classes/helper/reader.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Reader helper trait.
+ *
+ * @package    tool_log
+ * @copyright  2014 onwards Ankit Agarwal
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_log\helper;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Reader helper trait.
+ * \tool_log\helper\store must be included before using this trait.
+ *
+ * @package    tool_log
+ * @copyright  2014 onwards Ankit Agarwal
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+trait reader {
+    /**
+     * Default get name api.
+     *
+     * @return string name of the store.
+     */
+    public function get_name() {
+        if (get_string_manager()->string_exists('pluginname', $this->component)) {
+            return get_string('pluginname', $this->component);
+        }
+        return $this->store;
+    }
+
+    /**
+     * Default get description method.
+     *
+     * @return string description of the store.
+     */
+    public function get_description() {
+        if (get_string_manager()->string_exists('pluginname_desc', $this->component)) {
+            return get_string('pluginname_desc', $this->component);
+        }
+        return $this->store;
+    }
+
+    /**
+     * If the current user can access current store or not.
+     *
+     * @param \context $context.
+     *
+     * @return bool
+     */
+    public function can_access(\context $context) {
+        return has_capability('logstore/' . $this->store . ':read', $context);
+    }
+}
---
+++ admin/tool/log/classes/helper/writer.php
@@ -0,0 +1,83 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Helper trait writer
+ *
+ * @package    tool_log
+ * @copyright  2014 onwards Ankit Agarwal
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_log\helper;
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Helper trait writer. Adds buffer support for the store.
+ * \tool_log\helper\store must be included before using this trait.
+ * 
+ * @package    tool_log
+ * @copyright  2014 onwards Ankit Agarwal
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+trait writer {
+
+    /** @var array $buffer buffer of events.*/
+    protected $buffer = array();
+
+    /** @var array $buffer buffer size of events.*/
+    protected $buffersize;
+
+    /** @var int $count Counter.*/
+    protected $count = 0;
+
+    /**
+     * Write event in the store with buffering. Insert_events() must be
+     * defined. override in stores if the store doesn't support buffering.
+     *
+     * @param \core\event\base $event
+     *
+     * @return void
+     */
+    public function write(\core\event\base $event) {
+        $this->buffer[] = $event;
+        $this->count++;
+
+        if (!isset($this->buffersize)) {
+            $this->buffersize = $this->get_config('buffersize', 50);
+        }
+
+        if ($this->count >= $this->buffersize) {
+            $events = $this->buffer;
+            $this->insert_events($events);
+            $this->count = 0;
+            $this->buffer = array();
+        }
+    }
+
+    /**
+     * Push any remaining events to the database. Insert_events() must be
+     * defined. override in stores if the store doesn't support buffering.
+     *
+     */
+    public function dispose() {
+        $events = $this->buffer;
+        $this->insert_events($events);
+        $this->count = 0;
+        $this->buffer = array();
+    }
+}
