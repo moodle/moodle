482746821c216c85177036d4dc1f5218ee0ffd07
Derick Turner <derick@e-learndesign.co.uk>
2014-01-23 11:13:43 +0000

    IOMAD: Training event does not have standard capability.  Added standard Moodle 2.6 addinstance capability and bumped the version.  Closes #3

--- mod/trainingevent/db/access.php
+++ mod/trainingevent/db/access.php
@@ -25,6 +25,18 @@
 
 $capabilities = array(
 
+    'mod/trainingevent:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
+
     'mod/trainingevent:invite' => array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
