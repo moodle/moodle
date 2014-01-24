64ab5badbfbc156679c9b77bbd51eb6f07f22b68
Oleg Sychev <oasychev@gmail.com>
2014-01-25 02:41:01 +0300

    MDL-42456: add Mercurial vcs support to plugin_external_source

--- lib/pluginlib.php
+++ lib/pluginlib.php
@@ -395,6 +395,10 @@ class plugin_manager {
             return 'svn';
         }
 
+        if (is_dir($pluginroot.'/.hg')) {
+            return 'mercurial';
+        }
+
         return false;
     }
 
@@ -1983,6 +1987,10 @@ class available_update_deployer {
 
         if (is_dir($pluginroot.'/.svn')) {
             return 'svn';
+        }
+
+        if (is_dir($pluginroot.'/.hg')) {
+            return 'mercurial';
         }
 
         return false;
