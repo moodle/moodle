f4f0aa27d43527c15070d00bc96be879876ccc38
Frederic Massart <fred@moodle.com>
2014-01-30 12:02:09 +0800

    MDL-29409 repository_alfresco: Drop support for URL/link
    The current solution has two major issues. Firstly, it is using the
    session key in the file URL, allowing anyone with the link to steal
    the identify of the poster. Secondly, the links are not presistent
    and become broken as soon as the server is restarted. Let's not
    support this any more until a proper solution is found in MDL-26454.

---
+++ repository/alfresco/db/upgrade.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Upgrade.
+ *
+ * @package    repository_alfresco
+ * @copyright  2014 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Upgrade function.
+ *
+ * @param int $oldversion the version we are upgrading from.
+ * @return bool result
+ */
+function xmldb_repository_alfresco_upgrade($oldversion) {
+    global $CFG, $DB;
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2014020301) {
+        require_once($CFG->dirroot . '/repository/lib.php');
+        require_once($CFG->dirroot . '/repository/alfresco/db/upgradelib.php');
+
+        $params = array();
+        $params['context'] = array();
+        $params['onlyvisible'] = false;
+        $params['type'] = 'alfresco';
+        $instances = repository::get_instances($params);
+
+        // Notify the admin about the migration process if they are using the repo.
+        if (!empty($instances)) {
+            repository_alfresco_admin_security_key_notice();
+        }
+
+        upgrade_plugin_savepoint(true, 2014020301, 'repository', 'alfresco');
+    }
+
+    return true;
+}
