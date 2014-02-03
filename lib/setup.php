aea2973734bc51e7306330e35fa828824aa61d8d
Andrew Nicols <andrew@nicols.co.uk>
2014-02-03 11:59:26 +0800

    MDL-43952 Javascript: Support invalidation of browser caches for YUI patching
    This will also force files loaded from the CDN to instead come from the
    local distribution since those patches will not be included in the CDN
    release.
    
    Please see the documentation at http://docs.moodle.org/dev/YUI/Patching for
    important on using these changes.

--- lib/setup.php
+++ lib/setup.php
@@ -349,6 +349,15 @@ if (!defined('AJAX_SCRIPT')) {
 $CFG->yui2version = '2.9.0';
 $CFG->yui3version = '3.13.0';
 
+// Patching the upstream YUI release.
+// For important information on patching YUI modules, please see http://docs.moodle.org/dev/YUI/Patching.
+// If we need to patch a YUI modules between official YUI releases, the yuipatchlevel will need to be manually
+// incremented here. The module will also need to be listed in the yuipatchedmodules.
+// When upgrading to a subsequent version of YUI, these should be reset back to 0 and an empty array.
+$CFG->yuipatchlevel = 0;
+$CFG->yuipatchedmodules = array(
+);
+
 // Store settings from config.php in array in $CFG - we can use it later to detect problems and overrides.
 if (!isset($CFG->config_php_settings)) {
     $CFG->config_php_settings = (array)$CFG;
