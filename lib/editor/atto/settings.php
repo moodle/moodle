bed1abbc89d643243ce6a49e5f5dbd0c8c85e8dc
Andrew Davis <andrew@moodle.com>
2014-02-04 10:41:46 +0800

    MDL-43864 editor_atto: added a prevent autolink button to atto

--- lib/editor/atto/settings.php
+++ lib/editor/atto/settings.php
@@ -35,7 +35,7 @@ if ($ADMIN->fulltree) {
     $default = 'collapse = collapse
 style1 = title, bold, italic
 list = unorderedlist, orderedlist
-links = link, unlink
+links = link, unlink, noautolink
 files = image, media, managefiles
 style2 = underline, strike, subscript, superscript
 align = align
