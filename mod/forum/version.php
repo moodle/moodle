a52c04d549a09acdc2b71c7d5174d75e317c0a9e
Dan Poltawski <dan@moodle.com>
2014-02-12 15:54:41 +0800

    MDL-40062 mod_forum: add event unit tests
    And bump forum version for new events.

--- mod/forum/version.php
+++ mod/forum/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013110500;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2014021700;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500;       // Requires this Moodle version
 $plugin->component = 'mod_forum';      // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 60;
