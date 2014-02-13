777017e7845a6aaae72eed9fe6a34d20e423bcbb
Tim Hunt <T.J.Hunt@open.ac.uk>
2014-02-13 12:26:32 +0000

    MDL-44118 new environment check are all quiz attempts upgraded.
    In Moodle 2.1, there was a major DB upgrade relating to questions, and
    it was possible to delay some of that upgrade. Now, those DB tables are
    changing again, and the time has come to insist that all the updata has
    been upgraded (or deleted).

--- question/engine/upgrade/upgradelib.php
+++ question/engine/upgrade/upgradelib.php
@@ -621,3 +621,32 @@ class question_deleted_question_attempt_updater extends question_qtype_attempt_u
         $data['upgradedfromdeletedquestion'] = $state->answer;
     }
 }
+
+/**
+ * This check verifies that all quiz attempts were upgraded since following
+ * the question engine upgrade in Moodle 2.1.
+ *
+ * @param environment_results object to update, if relevant.
+ * @return environment_results updated results object, or null if this test is not relevant.
+ */
+function quiz_attempts_upgraded(environment_results $result) {
+    global $DB;
+
+    $dbman = $DB->get_manager();
+    $table = new xmldb_table('quiz_attempts');
+    $field = new xmldb_field('needsupgradetonewqe');
+
+    if (!$dbman->table_exists($table) || !$dbman->field_exists($table, $field)) {
+        // DB already upgraded. This test is no longer relevant.
+        return null;
+    }
+
+    if (!$DB->record_exists('quiz_attempts', array('needsupgradetonewqe' => 1))) {
+        // No 1s present in that column means there are no problems.
+        return null;
+    }
+
+    // Only display anything if the admins need to be aware of the problem.
+    $result->setStatus(false);
+    return $result;
+}
