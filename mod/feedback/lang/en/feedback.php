9714baa1bb55959b4b5b99e3f6bc502db7d09e9b
Sam Hemelryk <sam@moodle.com>
2014-02-14 17:01:49 +1300

    MDL-44155 mod_feedback: unified @package use

--- mod/feedback/lang/en/feedback.php
+++ mod/feedback/lang/en/feedback.php
@@ -17,7 +17,7 @@
 /**
  * Strings for component 'feedback', language 'en', branch 'MOODLE_20_STABLE'
  *
- * @package   feedback
+ * @package mod_feedback
  * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
