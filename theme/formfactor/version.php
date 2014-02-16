485f8eb3e0d48ba5bd54ff83dcc3d64904a46a9e
Sam Hemelryk <sam@moodle.com>
2014-02-17 11:18:50 +1300

    MDL-44203 theme_formfactor: unified @package use and improved coding style

--- theme/formfactor/version.php
+++ theme/formfactor/version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013110500; // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2013110500; // Requires this Moodle version
-$plugin->component = 'theme_formfactor'; // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2013110500; // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2013110500; // Requires this Moodle version.
+$plugin->component = 'theme_formfactor'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = array(
     'theme_canvas'  => 2013110500,
 );
