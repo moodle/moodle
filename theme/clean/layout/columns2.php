e11558bca3aaffff6e01be5172bb0d804a176961
Mary Evans <lazydaisy@visible-expression.co.uk>
2014-02-19 22:03:38 +0000

    MDL-44269 theme_clean: Change location of navbar (breadcumb) to below logo.

--- theme/clean/layout/columns2.php
+++ theme/clean/layout/columns2.php
@@ -54,11 +54,11 @@ echo $OUTPUT->doctype() ?>
 <div id="page" class="container-fluid">
 
     <header id="page-header" class="clearfix">
+        <?php echo $html->heading; ?>
         <div id="page-navbar" class="clearfix">
             <nav class="breadcrumb-nav"><?php echo $OUTPUT->navbar(); ?></nav>
             <div class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></div>
         </div>
-        <?php echo $html->heading; ?>
         <div id="course-header">
             <?php echo $OUTPUT->course_header(); ?>
         </div>
