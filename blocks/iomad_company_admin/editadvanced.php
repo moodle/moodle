e35c1d60695ece73a8f4e6d635008db07fd3ad23
Derick Turner <derick@e-learndesign.co.uk>
2014-02-20 11:40:28 +0000

    IOMAD: method to force HTTPS changed in 2.6.

--- blocks/iomad_company_admin/editadvanced.php
+++ blocks/iomad_company_admin/editadvanced.php
@@ -31,11 +31,10 @@ require_once($CFG->dirroot.'/user/profile/lib.php');
 require_once('lib.php');
 require_once($CFG->dirroot.'/local/email/lib.php');
 
-httpsrequired();
-
 $id = optional_param('id', $USER->id, PARAM_INT);    // User id; -1 if creating new user.
 
 require_login();
+$PAGE->https_required();
 
 $url = new moodle_url('/blocks/iomad_company_admin/editadvanced.php');
 if ($id !== $USER->id) {
