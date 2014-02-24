221c125cb35226774538e9f05908c620c90c2d2f
Damyon Wiese <damyon@moodle.com>
2014-02-24 15:04:33 +0800

    MDL-44319 phpdocs mod_page: Remove @subpackage tags and add @category

--- mod/page/backup/moodle2/backup_page_activity_task.class.php
+++ mod/page/backup/moodle2/backup_page_activity_task.class.php
@@ -18,10 +18,10 @@
 /**
  * Defines backup_page_activity_task class
  *
- * @package     mod_page
- * @category    backup
- * @copyright   2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   mod_page
+ * @category  backup
+ * @copyright 2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
