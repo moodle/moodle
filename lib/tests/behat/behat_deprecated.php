ced0f98322e43837bfc2ce99e107a67dd3ed370d
David Monllao <davidm@moodle.com>
2014-03-12 17:00:20 +0800

    MDL-44236 behat: Fixing wrong old step call

--- lib/tests/behat/behat_deprecated.php
+++ lib/tests/behat/behat_deprecated.php
@@ -511,7 +511,7 @@ class behat_deprecated extends behat_base {
      * @param TableNode $data
      */
     public function the_following_exists($elementname, TableNode $data) {
-        $alternative = 'the following "' . $this->escape($elementname) . '" exist';
+        $alternative = 'the following "' . $this->escape($elementname) . '" exist:';
         $this->deprecated_message($alternative);
         return new Given($alternative, $data);
     }
