d58a8932b81dfd230e25bb9a1c26e06895afbdfa
Petr Škoda <commits@skodak.org>
2014-03-18 14:03:43 +0800

    MDL-44107 deprecate can_view() in feedback events, to be replaced by is_anonymous()
    See MDL-44661 for more info.

--- mod/feedback/classes/event/course_module_viewed.php
+++ mod/feedback/classes/event/course_module_viewed.php
@@ -53,6 +53,8 @@ class course_module_viewed extends \core\event\course_module_viewed {
     /**
      * Define whether a user can view the event or not. Make sure no one except admin can see details of an anonymous response.
      *
+     * @deprecated since 2.7
+     *
      * @param int|\stdClass $userorid ID of the user.
      * @return bool True if the user can view the event, false otherwise.
      */
--- mod/feedback/classes/event/response_deleted.php
+++ mod/feedback/classes/event/response_deleted.php
@@ -84,6 +84,8 @@ class response_deleted extends \core\event\base {
     /**
      * Define whether a user can view the event or not. Make sure no one except admin can see details of an anonymous response.
      *
+     * @deprecated since 2.7
+     *
      * @param int|\stdClass $userorid ID of the user.
      * @return bool True if the user can view the event, false otherwise.
      */
