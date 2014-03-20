b8604eb33bbb489563155958ddcb64f8949ac519
Marina Glancy <marina@moodle.com>
2014-03-20 16:00:14 +0800

    weekly release 2.6.2+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2013111802.01;              // 20131118      = branching date YYYYMMDD - do not modify!
+$version  = 2013111802.02;              // 20131118      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '2.6.2+ (Build: 20140314)'; // Human-friendly version name
+$release  = '2.6.2+ (Build: 20140320)'; // Human-friendly version name
 
 $branch   = '26';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
