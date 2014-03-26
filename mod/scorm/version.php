8fb32f86ddb3746872760ab33befe58fd2924a2c
Dan Marsden <dan@danmarsden.com>
2014-03-26 21:38:25 +1300

    MDL-44753 SCORM: add check during SCORM restore to make sure valid launch sco is set

--- mod/scorm/version.php
+++ mod/scorm/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2013110503;       // The current module version (Date: YYYYMMDDXX)
+$module->version   = 2013110504;       // The current module version (Date: YYYYMMDDXX)
 $module->requires  = 2013110500;    // Requires this Moodle version
 $module->component = 'mod_scorm'; // Full name of the plugin (used for diagnostics)
 $module->cron      = 300;
