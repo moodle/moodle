ankitagarwal/moodle-travis

MDL-44596 logging: Add callbacks to all reports that support log stores

report/loglive/lib.php
@@ -43,3 +43,17 @@ function report_loglive_extend_navigation_course($navigation, $course, $context)
         $navigation->add(get_string('pluginname', 'report_loglive'), $action, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/report', ''));
     }
 }
+
+/**
+ * Callback to verify if the given instance of store is supported by this report or not.
+ *
+ * @param string $instance store instance.
+ *
+ * @return bool returns true if the store is supported by the report, false otherwise.
+ */
+function report_loglive_supports_store($instance) {
+    if ($instance instanceof \core\log\sql_select_reader) {
+        return true;
+    }
+    return false;
+}

report/loglive/tests/lib_test.php
@@ -1,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for report library functions.
+ *
+ * @package    report_loglive
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class report_loglive_lib_testcase
+ *
+ * @package    report_loglive
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+class report_loglive_lib_testcase extends advanced_testcase {
+
+    /**
+     * Test report_log_supports_store.
+     */
+    public function test_report_participation_supports_store() {
+        $logmanager = get_log_manager();
+        $allstores = \core_component::get_plugin_list_with_class('logstore', 'log\store');
+
+        $supportedstores = array(
+            'logstore_database' => '\logstore_legacy\log\database',
+            'logstore_legacy' => '\logstore_legacy\log\store',
+            'logstore_standard' => '\logstore_standard\log\store'
+        );
+
+        // Make sure all supported stores are installed.
+        $expectedstores = array_keys(array_intersect($allstores, $supportedstores));
+        $stores = $logmanager->get_supported_stores('loglive');
+        $stores = array_keys($stores);
+        foreach ($expectedstores as $expectedstore) {
+            $this->assertContains($expectedstore, $stores);
+        }
+    }
+}

report/outline/lib.php
@@ -105,3 +105,17 @@ function report_outline_page_type_list($pagetype, $parentcontext, $currentcontex
     );
     return $array;
 }
+
+/**
+ * Callback to verify if the given instance of store is supported by this report or not.
+ *
+ * @param string $instance store instance.
+ *
+ * @return bool returns true if the store is supported by the report, false otherwise.
+ */
+function report_outline_supports_store($instance) {
+    if ($instance instanceof \core\log\sql_internal_reader || $instance instanceof \logstore_legacy\log\store) {
+        return true;
+    }
+    return false;
+}

report/outline/tests/lib_test.php
@@ -1,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for report library functions.
+ *
+ * @package    report_outline
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class report_outline_lib_testcase
+ *
+ * @package    report_outline
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+class report_outline_lib_testcase extends advanced_testcase {
+
+    /**
+     * Test report_log_supports_store.
+     */
+    public function test_report_participation_supports_store() {
+        $logmanager = get_log_manager();
+        $allstores = \core_component::get_plugin_list_with_class('logstore', 'log\store');
+
+        $supportedstores = array(
+            'logstore_legacy' => '\logstore_legacy\log\store',
+            'logstore_standard' => '\logstore_standard\log\store'
+        );
+
+        // Make sure all supported stores are installed.
+        $expectedstores = array_keys(array_intersect($allstores, $supportedstores));
+        $stores = $logmanager->get_supported_stores('outline');
+        $stores = array_keys($stores);
+        foreach ($expectedstores as $expectedstore) {
+            $this->assertContains($expectedstore, $stores);
+        }
+    }
+}

report/participation/lib.php
@@ -56,3 +56,17 @@ function report_participation_page_type_list($pagetype, $parentcontext, $current
     );
     return $array;
 }
+
+/**
+ * Callback to verify if the given instance of store is supported by this report or not.
+ *
+ * @param string $instance store instance.
+ *
+ * @return bool returns true if the store is supported by the report, false otherwise.
+ */
+function report_participation_supports_store($instance) {
+    if ($instance instanceof \core\log\sql_internal_reader || $instance instanceof \logstore_legacy\log\store) {
+        return true;
+    }
+    return false;
+}

report/participation/tests/lib_test.php
@@ -1,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for report library functions.
+ *
+ * @package    report_participation
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class report_participation_lib_testcase
+ *
+ * @package    report_participation
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+class report_participation_lib_testcase extends advanced_testcase {
+
+    /**
+     * Test report_log_supports_store.
+     */
+    public function test_report_participation_supports_store() {
+        $logmanager = get_log_manager();
+        $allstores = \core_component::get_plugin_list_with_class('logstore', 'log\store');
+
+        $supportedstores = array(
+            'logstore_legacy' => '\logstore_legacy\log\store',
+            'logstore_standard' => '\logstore_standard\log\store'
+        );
+
+        // Make sure all supported stores are installed.
+        $expectedstores = array_keys(array_intersect($allstores, $supportedstores));
+        $stores = $logmanager->get_supported_stores('participation');
+        $stores = array_keys($stores);
+        foreach ($expectedstores as $expectedstore) {
+            $this->assertContains($expectedstore, $stores);
+        }
+    }
+}

report/stats/lib.php
@@ -113,3 +113,17 @@ function report_stats_page_type_list($pagetype, $parentcontext, $currentcontext)
     );
     return $array;
 }
+
+/**
+ * Callback to verify if the given instance of store is supported by this report or not.
+ *
+ * @param string $instance store instance.
+ *
+ * @return bool returns true if the store is supported by the report, false otherwise.
+ */
+function report_stats_supports_store($instance) {
+    if ($instance instanceof \core\log\sql_internal_reader || $instance instanceof \logstore_legacy\log\store) {
+        return true;
+    }
+    return false;
+}

report/stats/tests/lib_test.php
@@ -1,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for report library functions.
+ *
+ * @package    report_stats
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class report_stats_lib_testcase
+ *
+ * @package    report_stats
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+class report_stats_lib_testcase extends advanced_testcase {
+
+    /**
+     * Test report_log_supports_store.
+     */
+    public function test_report_participation_supports_store() {
+        $logmanager = get_log_manager();
+        $allstores = \core_component::get_plugin_list_with_class('logstore', 'log\store');
+
+        $supportedstores = array(
+            'logstore_legacy' => '\logstore_legacy\log\store',
+            'logstore_standard' => '\logstore_standard\log\store'
+        );
+
+        // Make sure all supported stores are installed.
+        $expectedstores = array_keys(array_intersect($allstores, $supportedstores));
+        $stores = $logmanager->get_supported_stores('stats');
+        $stores = array_keys($stores);
+        foreach ($expectedstores as $expectedstore) {
+            $this->assertContains($expectedstore, $stores);
+        }
+    }
+}

moodle/moodle

report/loglive/lib.php
@@ -43,3 +43,17 @@ function report_loglive_extend_navigation_course($navigation, $course, $context)
         $navigation->add(get_string('pluginname', 'report_loglive'), $action, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/report', ''));
     }
 }
+
+/**
+ * Callback to verify if the given instance of store is supported by this report or not.
+ *
+ * @param string $instance store instance.
+ *
+ * @return bool returns true if the store is supported by the report, false otherwise.
+ */
+function report_loglive_supports_logstore($instance) {
+    if ($instance instanceof \core\log\sql_select_reader) {
+        return true;
+    }
+    return false;
+}

report/loglive/tests/lib_test.php
@@ -1,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for report library functions.
+ *
+ * @package    report_loglive
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class report_loglive_lib_testcase
+ *
+ * @package    report_loglive
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+class report_loglive_lib_testcase extends advanced_testcase {
+
+    /**
+     * Test report_log_supports_logstore.
+     */
+    public function test_report_participation_supports_logstore() {
+        $logmanager = get_log_manager();
+        $allstores = \core_component::get_plugin_list_with_class('logstore', 'log\store');
+
+        $supportedstores = array(
+            'logstore_database' => '\logstore_legacy\log\database',
+            'logstore_legacy' => '\logstore_legacy\log\store',
+            'logstore_standard' => '\logstore_standard\log\store'
+        );
+
+        // Make sure all supported stores are installed.
+        $expectedstores = array_keys(array_intersect($allstores, $supportedstores));
+        $stores = $logmanager->get_supported_logstores('loglive');
+        $stores = array_keys($stores);
+        foreach ($expectedstores as $expectedstore) {
+            $this->assertContains($expectedstore, $stores);
+        }
+    }
+}

report/outline/lib.php
@@ -105,3 +105,17 @@ function report_outline_page_type_list($pagetype, $parentcontext, $currentcontex
     );
     return $array;
 }
+
+/**
+ * Callback to verify if the given instance of store is supported by this report or not.
+ *
+ * @param string $instance store instance.
+ *
+ * @return bool returns true if the store is supported by the report, false otherwise.
+ */
+function report_outline_supports_logstore($instance) {
+    if ($instance instanceof \core\log\sql_internal_reader || $instance instanceof \logstore_legacy\log\store) {
+        return true;
+    }
+    return false;
+}

report/outline/tests/lib_test.php
@@ -1,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for report library functions.
+ *
+ * @package    report_outline
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class report_outline_lib_testcase
+ *
+ * @package    report_outline
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+class report_outline_lib_testcase extends advanced_testcase {
+
+    /**
+     * Test report_log_supports_logstore.
+     */
+    public function test_report_participation_supports_logstore() {
+        $logmanager = get_log_manager();
+        $allstores = \core_component::get_plugin_list_with_class('logstore', 'log\store');
+
+        $supportedstores = array(
+            'logstore_legacy' => '\logstore_legacy\log\store',
+            'logstore_standard' => '\logstore_standard\log\store'
+        );
+
+        // Make sure all supported stores are installed.
+        $expectedstores = array_keys(array_intersect($allstores, $supportedstores));
+        $stores = $logmanager->get_supported_logstores('outline');
+        $stores = array_keys($stores);
+        foreach ($expectedstores as $expectedstore) {
+            $this->assertContains($expectedstore, $stores);
+        }
+    }
+}

report/participation/lib.php
@@ -56,3 +56,17 @@ function report_participation_page_type_list($pagetype, $parentcontext, $current
     );
     return $array;
 }
+
+/**
+ * Callback to verify if the given instance of store is supported by this report or not.
+ *
+ * @param string $instance store instance.
+ *
+ * @return bool returns true if the store is supported by the report, false otherwise.
+ */
+function report_participation_supports_logstore($instance) {
+    if ($instance instanceof \core\log\sql_internal_reader || $instance instanceof \logstore_legacy\log\store) {
+        return true;
+    }
+    return false;
+}

report/participation/tests/lib_test.php
@@ -1,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for report library functions.
+ *
+ * @package    report_participation
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class report_participation_lib_testcase
+ *
+ * @package    report_participation
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+class report_participation_lib_testcase extends advanced_testcase {
+
+    /**
+     * Test report_log_supports_logstore.
+     */
+    public function test_report_participation_supports_logstore() {
+        $logmanager = get_log_manager();
+        $allstores = \core_component::get_plugin_list_with_class('logstore', 'log\store');
+
+        $supportedstores = array(
+            'logstore_legacy' => '\logstore_legacy\log\store',
+            'logstore_standard' => '\logstore_standard\log\store'
+        );
+
+        // Make sure all supported stores are installed.
+        $expectedstores = array_keys(array_intersect($allstores, $supportedstores));
+        $stores = $logmanager->get_supported_logstores('participation');
+        $stores = array_keys($stores);
+        foreach ($expectedstores as $expectedstore) {
+            $this->assertContains($expectedstore, $stores);
+        }
+    }
+}

report/stats/lib.php
@@ -113,3 +113,17 @@ function report_stats_page_type_list($pagetype, $parentcontext, $currentcontext)
     );
     return $array;
 }
+
+/**
+ * Callback to verify if the given instance of store is supported by this report or not.
+ *
+ * @param string $instance store instance.
+ *
+ * @return bool returns true if the store is supported by the report, false otherwise.
+ */
+function report_stats_supports_logstore($instance) {
+    if ($instance instanceof \core\log\sql_internal_reader || $instance instanceof \logstore_legacy\log\store) {
+        return true;
+    }
+    return false;
+}

report/stats/tests/lib_test.php
@@ -1,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for report library functions.
+ *
+ * @package    report_stats
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class report_stats_lib_testcase
+ *
+ * @package    report_stats
+ * @copyright  2014 onwards Ankit agarwal <ankit.agrr@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+class report_stats_lib_testcase extends advanced_testcase {
+
+    /**
+     * Test report_log_supports_logstore.
+     */
+    public function test_report_participation_supports_logstore() {
+        $logmanager = get_log_manager();
+        $allstores = \core_component::get_plugin_list_with_class('logstore', 'log\store');
+
+        $supportedstores = array(
+            'logstore_legacy' => '\logstore_legacy\log\store',
+            'logstore_standard' => '\logstore_standard\log\store'
+        );
+
+        // Make sure all supported stores are installed.
+        $expectedstores = array_keys(array_intersect($allstores, $supportedstores));
+        $stores = $logmanager->get_supported_logstores('stats');
+        $stores = array_keys($stores);
+        foreach ($expectedstores as $expectedstore) {
+            $this->assertContains($expectedstore, $stores);
+        }
+    }
+}