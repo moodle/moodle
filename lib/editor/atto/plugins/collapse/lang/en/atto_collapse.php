13f432976a95c1bb700802affaa676e2cf1e1499
Andrew Davis <andrew@moodle.com>
2014-03-31 10:25:39 +0800

    MDL-44756 editor_atto: changed a string

--- lib/editor/atto/plugins/collapse/lang/en/atto_collapse.php
+++ lib/editor/atto/plugins/collapse/lang/en/atto_collapse.php
@@ -26,5 +26,5 @@ $string['pluginname'] = 'Show/hide advanced buttons';
 $string['showmore'] = 'Show more buttons';
 $string['showfewer'] = 'Show fewer buttons';
 $string['settings'] = 'Collapse toolbar settings';
-$string['showgroups'] = 'Show (n) groups when collapsed.';
+$string['showgroups'] = 'Show first (n) groups when collapsed.';
 $string['showgroups_desc'] = 'When the toolbar is collapsed (it is by default) only this many groups will be displayed at once.';
