df1323369784c4143523dd52abd8402d14e958d9
Ankit Agarwal <ankit@moodle.com>
2014-04-03 13:57:52 +0800

    MDL-41123 report_completion: Add events to completion report
    Replace add_to_log with user_report_viewed event
    add a report_viewed event

--- report/completion/lang/en/report_completion.php
+++ report/completion/lang/en/report_completion.php
@@ -32,3 +32,5 @@ $string['page-report-completion-x'] = 'Any completion report';
 $string['page-report-completion-index'] = 'Course completion report';
 $string['page-report-completion-user'] = 'User course completion report';
 $string['pluginname'] = 'Course completion';
+$string['eventreportviewed'] = 'Completion report viewed';
+$string['eventuserreportviewed'] = 'Completion user report viewed';
