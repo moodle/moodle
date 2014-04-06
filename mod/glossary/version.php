86286bdb7ed15b05d0fdabfaa4558237d5a6fcf7
Petr Škoda <commits@skodak.org>
2014-04-06 20:36:56 +0800

    MDL-44366 bump up glossary and filter version

--- mod/glossary/version.php
+++ mod/glossary/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013110500;       // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2013110500;    // Requires this Moodle version
+$plugin->version   = 2014040600;       // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2014040300;    // Requires this Moodle version
 $plugin->component = 'mod_glossary';   // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
