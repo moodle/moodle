570affa81b7cff713d4798c74a60f80c39b6450f
Andrew Nicols <andrew@nicols.co.uk>
2014-04-15 10:24:32 +0800

    MDL-43277 block_navigation: Trim from the left for long names

--- blocks/navigation/block_navigation.php
+++ blocks/navigation/block_navigation.php
@@ -163,7 +163,7 @@ class block_navigation extends block_base {
             redirect($url);
         }
 
-        $trimmode = self::TRIM_LEFT;
+        $trimmode = self::TRIM_RIGHT;
         $trimlength = 50;
 
         if (!empty($this->config->trimmode)) {
