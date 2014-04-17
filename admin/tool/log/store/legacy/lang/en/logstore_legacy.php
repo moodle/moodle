da223c11a55c5990285e395032fc53f29de9e39e
Simey Lameze <lameze@gmail.com>
2014-04-17 11:46:25 +0800

    MDL-44990 logging: Capabilites removed from legacy and standard log.

--- admin/tool/log/store/legacy/lang/en/logstore_legacy.php
+++ admin/tool/log/store/legacy/lang/en/logstore_legacy.php
@@ -23,7 +23,6 @@
  */
 
 $string['event_legacy_logged'] = 'Legacy event logged';
-$string['legacy:read'] = 'Read logs';
 $string['loglegacy'] = 'Log legacy data';
 $string['loglegacy_help'] = 'This plugin records log data to the legacy log table (mdl_log). This functionality has been replaced by newer, richer and more efficient logging plugins, so you should only run this plugin if you have old custom reports that directly query the old log table. Writing to the legacy logs will increase load, so it is recommended that you disable this plugin for performance reasons when it is not needed.';
 $string['pluginname'] = 'Legacy log';
