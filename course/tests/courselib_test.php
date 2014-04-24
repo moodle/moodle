be9fb211916973637c1b611bdc1ff4eaa3c33d08
Frederic Massart <fred@moodle.com>
2014-04-24 15:01:08 +0800

    MDL-45229 event: Validate sectionnum in course_section_updated

--- course/tests/courselib_test.php
+++ course/tests/courselib_test.php
@@ -1759,7 +1759,10 @@ class core_course_courselib_testcase extends advanced_testcase {
                 array(
                     'objectid' => $section->id,
                     'courseid' => $course->id,
-                    'context' => context_course::instance($course->id)
+                    'context' => context_course::instance($course->id),
+                    'other' => array(
+                        'sectionnum' => $section->section
+                    )
                 )
             );
         $event->add_record_snapshot('course_sections', $section);
@@ -1776,6 +1779,7 @@ class core_course_courselib_testcase extends advanced_testcase {
         $this->assertEquals($section->id, $event->objectid);
         $this->assertEquals($course->id, $event->courseid);
         $this->assertEquals($coursecontext->id, $event->contextid);
+        $this->assertEquals($section->section, $event->other['sectionnum']);
         $expecteddesc = 'Course ' . $event->courseid . ' section ' . $event->other['sectionnum'] . ' updated by user ' . $event->userid;
         $this->assertEquals($expecteddesc, $event->get_description());
         $url = new moodle_url('/course/editsection.php', array('id' => $event->objectid));
