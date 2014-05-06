f15fa7a281096f4c15a0b867b90e0f2a98e2aecd
Helen Foster <helen@moodle.org>
2014-05-07 00:16:51 +0200

    MDL-45410 Merge English strings from the en_fix language pack

--- lib/editor/atto/plugins/equation/lang/en/atto_equation.php
+++ lib/editor/atto/plugins/equation/lang/en/atto_equation.php
@@ -24,13 +24,13 @@
 
 $string['editequation'] = 'Edit equation using <a href="{$a}" target="_blank">TeX</a>';
 $string['librarygroup1'] = 'Operators';
-$string['librarygroup1_desc'] = 'List of tex commands to list on the operators tab.';
+$string['librarygroup1_desc'] = 'TeX commands listed on the operators tab.';
 $string['librarygroup2'] = 'Arrows';
-$string['librarygroup2_desc'] = 'List of tex commands to list on the arrows tab.';
+$string['librarygroup2_desc'] = 'TeX commands listed on the arrows tab.';
 $string['librarygroup3'] = 'Greek Symbols';
-$string['librarygroup3_desc'] = 'List of tex commands to list on the greek symbols tab.';
+$string['librarygroup3_desc'] = 'TeX commands listed on the Greek symbols tab.';
 $string['librarygroup4'] = 'Advanced';
-$string['librarygroup4_desc'] = 'List of tex commands to list on the advanced tab.';
+$string['librarygroup4_desc'] = 'TeX commands listed on the advanced tab.';
 $string['pluginname'] = 'Equation editor';
 $string['preview'] = 'Equation preview';
 $string['cursorinfo'] = 'An arrow indicates the position that new elements from the element library will be inserted.';
