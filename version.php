5fd16a3761e9c315e9900cf50912bb2f674a01bc
Jason Fowler <phalacee@gmail.com>
2014-05-07 13:38:46 +0800

    MDL-45418 Files: Adding upgrade steps for publisher docs
    Conflicts:
    	lib/db/upgrade.php
    	lib/db/upgradelib.php
    	version.php

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2013111803.04;              // 20131118      = branching date YYYYMMDD - do not modify!
+$version  = 2013111803.05;              // 20131118      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
