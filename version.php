8c80b452dc74f0c9cea38b4c58d215eb2dea5981
Damyon Wiese <damyon@moodle.com>
2014-05-09 11:07:26 +0800

    on-demand release 2.7rc2+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2014050800.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2014050900.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '2.7rc2 (Build: 20140508)'; // Human-friendly version name
+$release  = '2.7rc2+ (Build: 20140509)'; // Human-friendly version name
 
 $branch   = '27';                       // This version's branch.
 $maturity = MATURITY_RC;             // This version's maturity level.
