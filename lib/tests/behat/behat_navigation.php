9fb8de4cd12ecc7bad4474e90e40ec4c188a24ab
Sam Hemelryk <sam@moodle.com>
2014-05-13 11:18:47 +1200

    MDL-44711 navigation: better handling of course expansion exception

--- lib/tests/behat/behat_navigation.php
+++ lib/tests/behat/behat_navigation.php
@@ -41,11 +41,103 @@ use Behat\Behat\Context\Step\Given as Given,
 class behat_navigation extends behat_base {
 
     /**
+     * Helper function to get a navigation nodes text element given its text from within the navigation block.
+     *
+     * This function finds the node with the given text from within the navigation block.
+     * It checks to make sure the node is visible, and then returns it.
+     *
+     * @param string $text
+     * @param bool $branch Set this true if you're only interested in the node if its a branch.
+     * @param null|bool $collapsed Set this to true or false if you want the node to either be collapsed or not.
+     *    If its left as null then we don't worry about it.
+     * @param null|string|Exception|false $exception The exception to throw if the node is not found.
+     * @return \Behat\Mink\Element\NodeElement
+     */
+    protected function get_node_text_node($text, $branch = false, $collapsed = null, $exception = null) {
+        if ($exception === null) {
+            $exception = new ExpectationException('The "' . $text . '" node could not be found', $this->getSession());
+        } else if (is_string($exception)) {
+            $exception = new ExpectationException($exception, $this->getSession());
+        }
+
+        $nodetextliteral = $this->getSession()->getSelectorsHandler()->xpathLiteral($text);
+        $hasblocktree = "[contains(concat(' ', normalize-space(@class), ' '), ' block_tree ')]";
+        $hasbranch = "[contains(concat(' ', normalize-space(@class), ' '), ' branch ')]";
+        $hascollapsed = "li[contains(concat(' ', normalize-space(@class), ' '), ' collapsed ') or @data-exandable='1']";
+        $notcollapsed = "[not(contains(concat(' ', normalize-space(@class), ' '), ' collapsed '))]";
+        $match = "[normalize-space(.)={$nodetextliteral}]";
+
+        // Avoid problems with quotes.
+        $isbranch = ($branch) ? $hasbranch : '';
+        if ($collapsed === true) {
+            $iscollapsed = $hascollapsed;
+        } else if ($collapsed === false) {
+            $iscollapsed = $notcollapsed;
+        } else {
+            $iscollapsed = 'li';
+        }
+
+        $xpath  = "//ul{$hasblocktree}//li{$notcollapsed}/ul/{$iscollapsed}/p{$isbranch}/a{$match}|";
+        $xpath .= "//ul{$hasblocktree}//li{$notcollapsed}/ul/{$iscollapsed}/p{$isbranch}/span{$match}";
+
+        $node = $this->find('xpath', $xpath, $exception);
+        $this->ensure_node_is_visible($node);
+        return $node;
+    }
+
+    /**
+     * Returns true if the navigation node with the given text is expandable.
+     *
+     * @Given /^navigation node "([^"]*)" should be expandable$/
+     *
+     * @throws ExpectationException
+     * @param string $nodetext
+     * @return bool
+     */
+    public function navigation_node_should_be_expandable($nodetext) {
+        if (!$this->running_javascript()) {
+            // Nodes are only expandable when JavaScript is enabled.
+            return false;
+        }
+
+        $node = $this->get_node_text_node($nodetext, true);
+        $node = $node->getParent();
+        if ($node->hasAttribute('data-expandable') && $node->getAttribute('data-expandable')) {
+            return true;
+        }
+        throw new ExpectationException('The "' . $nodetext . '" node is not expandable', $this->getSession());
+    }
+
+    /**
+     * Returns true if the navigation node with the given text is not expandable.
+     *
+     * @Given /^navigation node "([^"]*)" should not be expandable$/
+     *
+     * @throws ExpectationException
+     * @param string $nodetext
+     * @return bool
+     */
+    public function navigation_node_should_not_be_expandable($nodetext) {
+        if (!$this->running_javascript()) {
+            // Nodes are only expandable when JavaScript is enabled.
+            return false;
+        }
+
+        $node = $this->get_node_text_node($nodetext);
+        $node = $node->getParent();
+        if ($node->hasAttribute('data-expandable') && $node->getAttribute('data-expandable')) {
+            throw new ExpectationException('The "' . $nodetext . '" node is expandable', $this->getSession());
+        }
+        return true;
+    }
+
+    /**
      * Expands the selected node of the navigation tree that matches the text.
      * @Given /^I expand "(?P<nodetext_string>(?:[^"]|\\")*)" node$/
      *
      * @throws ExpectationException
      * @param string $nodetext
+     * @return bool|void
      */
     public function i_expand_node($nodetext) {
 
@@ -60,23 +152,12 @@ class behat_navigation extends behat_base {
             return true;
         }
 
-        // Avoid problems with quotes.
-        $nodetextliteral = $this->getSession()->getSelectorsHandler()->xpathLiteral($nodetext);
-
-        $xpath = "//ul[contains(concat(' ', normalize-space(@class), ' '), ' block_tree ')]" .
-            "/child::li[contains(concat(' ', normalize-space(@class), ' '), ' collapsed ')]" .
-            "/child::p[contains(concat(' ', normalize-space(@class), ' '), ' branch ')]" .
-            "/child::span[normalize-space(.)=$nodetextliteral]" .
-            "|" .
-            "//ul[contains(concat(' ', normalize-space(@class), ' '), ' block_tree ')]" .
-            "/descendant::li[not(contains(concat(' ', normalize-space(@class), ' '), ' collapsed '))]" .
-            "/descendant::li[contains(concat(' ', normalize-space(@class), ' '), ' collapsed ')]" .
-            "/child::p[contains(concat(' ', normalize-space(@class), ' '), ' branch ')]" .
-            "/child::span[normalize-space(.)=$nodetextliteral]";
-
-        $exception = new ExpectationException('The "' . $nodetext . '" node can not be expanded', $this->getSession());
-        $node = $this->find('xpath', $xpath, $exception);
-        $this->ensure_node_is_visible($node);
+        $node = $this->get_node_text_node($nodetext, true, true, 'The "' . $nodetext . '" node can not be expanded');
+        // Check if the node is a link AND a branch.
+        if (strtolower($node->getTagName()) === 'a') {
+            // We just want to expand the node, we don't want to follow it.
+            $node = $node->getParent();
+        }
         $node->click();
     }
 
@@ -86,6 +167,7 @@ class behat_navigation extends behat_base {
      * @Given /^I collapse "(?P<nodetext_string>(?:[^"]|\\")*)" node$/
      * @throws ExpectationException
      * @param string $nodetext
+     * @return bool|void
      */
     public function i_collapse_node($nodetext) {
 
@@ -94,21 +176,12 @@ class behat_navigation extends behat_base {
             return true;
         }
 
-        // Avoid problems with quotes.
-        $nodetextliteral = $this->getSession()->getSelectorsHandler()->xpathLiteral($nodetext);
-
-        $xpath = "//ul[contains(concat(' ', normalize-space(@class), ' '), ' block_tree ')]" .
-            "/child::li[not(contains(concat(' ', normalize-space(@class), ' '), ' collapsed '))]" .
-            "/child::p[contains(concat(' ', normalize-space(@class), ' '), ' branch ')]" .
-            "/child::span[normalize-space(.)=$nodetextliteral]" .
-            "|" .
-            "//ul[contains(concat(' ', normalize-space(@class), ' '), ' block_tree ')]" .
-            "/descendant::li[not(contains(concat(' ', normalize-space(@class), ' '), ' collapsed '))]" .
-            "/child::p[contains(concat(' ', normalize-space(@class), ' '), ' branch ')]" .
-            "/child::span[normalize-space(.)=$nodetextliteral]";
-
-        $exception = new ExpectationException('The "' . $nodetext . '" node can not be collapsed', $this->getSession());
-        $node = $this->find('xpath', $xpath, $exception);
+        $node = $this->get_node_text_node($nodetext, true, false, 'The "' . $nodetext . '" node can not be collapsed');
+        // Check if the node is a link AND a branch.
+        if (strtolower($node->getTagName()) === 'a') {
+            // We just want to expand the node, we don't want to follow it.
+            $node = $node->getParent();
+        }
         $node->click();
     }
 
