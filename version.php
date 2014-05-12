c5d0fcbf1fb356700c759fc264ebe773fbcbf696
Daniel Neis Araujo <danielneis@gmail.com>
2014-05-12 12:13:34 -0300

    MDL-32543 (backport) Make sure the action and url fields of log table have correct sizes

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2013111803.00;              // 20131118      = branching date YYYYMMDD - do not modify!
+$version  = 2013111803.01;              // 20131118      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
