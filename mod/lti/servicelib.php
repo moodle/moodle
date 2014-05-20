045a69fd3e61a18512da27112d091cc068576e2f
Sam Chaffee <sam@moodlerooms.com>
2014-05-20 10:44:46 -0600

    MDL-37168 mod_lti: Fix LTI activity quick edit title

--- mod/lti/servicelib.php
+++ mod/lti/servicelib.php
@@ -26,6 +26,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 require_once($CFG->dirroot.'/mod/lti/OAuthBody.php');
+require_once($CFG->dirroot.'/mod/lti/locallib.php');
 
 // TODO: Switch to core oauthlib once implemented - MDL-30149.
 use moodle\mod\lti as lti;
