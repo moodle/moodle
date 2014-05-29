09fdd221ca3186391873d2a5eb282658142eeeb7
Graham Bowman <gbowman85@gmail.com>
2014-05-29 15:28:03 +0100

    MDL-45729 assign: conditional submission summary
    Only show the submission summary if user has assign:submit capability.
    
    Amended to fix some whitespace and the commit message.

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -3731,7 +3731,9 @@ class assign {
                                                               $gradingcontrollerpreview,
                                                               $instance->attemptreopenmethod,
                                                               $instance->maxattempts);
-            $o .= $this->get_renderer()->render($submissionstatus);
+            if (has_capability('mod/assign:submit', $this->get_context())) {
+                $o .= $this->get_renderer()->render($submissionstatus);
+            }
 
             require_once($CFG->libdir.'/gradelib.php');
             require_once($CFG->dirroot.'/grade/grading/lib.php');
