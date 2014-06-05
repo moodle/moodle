d6bc2106875fb8e14651e13ff4ef8ba9456e927d
Jenny Gray <j.m.gray@open.ac.uk>
2014-06-05 15:47:30 +0100

    MDL-45846 Libraries custom user agent device detection unit test added

--- lib/classes/useragent.php
+++ lib/classes/useragent.php
@@ -122,6 +122,11 @@ class core_useragent {
         if (!empty($CFG->devicedetectregex)) {
             $this->devicetypecustoms = json_decode($CFG->devicedetectregex, true);
         }
+        if ($this->devicetypecustoms === null) {
+            // This shouldn't happen unless you're hardcoding the config value.
+            debugging('Config devicedetectregex is not valid JSON object');
+            $this->devicetypecustoms = array();
+        }
         if ($forceuseragent !== null) {
             $this->useragent = $forceuseragent;
         } else if (!empty($_SERVER['HTTP_USER_AGENT'])) {
