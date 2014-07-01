005402f503c7080235b04a9f4d95864483b447f6
Adrian Greeve <adrian@moodle.com>
2014-07-01 13:46:25 +0800

    MDL-46201 events: Sanitization of username before returning the description.

--- lib/classes/event/user_login_failed.php
+++ lib/classes/event/user_login_failed.php
@@ -68,7 +68,9 @@ class user_login_failed extends base {
      * @return string
      */
     public function get_description() {
-        return "Login failed for the username '{$this->other['username']}' for the reason with id '{$this->other['reason']}'.";
+        // Note that username could be any random user input.
+        $username = s($this->other['username']);
+        return "Login failed for the username '{$username}' for the reason with id '{$this->other['reason']}'.";
     }
 
     /**
