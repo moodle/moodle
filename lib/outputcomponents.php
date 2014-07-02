956ec418c6afe828f166b7b658063d734839c4a7
Brian Barnes <brian.barnes@totaralms.com>
2014-07-02 16:37:01 +1200

    MDL-46226 core: pix_icon no longer sets title to the alt text

--- lib/outputcomponents.php
+++ lib/outputcomponents.php
@@ -510,9 +510,8 @@ class pix_icon implements renderable {
         if (empty($this->attributes['class'])) {
             $this->attributes['class'] = 'smallicon';
         }
-        if (!isset($this->attributes['title'])) {
-            $this->attributes['title'] = $this->attributes['alt'];
-        } else if (empty($this->attributes['title'])) {
+
+        if (empty($this->attributes['title'])) {
             // Remove the title attribute if empty, we probably want to use the parent node's title
             // and some browsers might overwrite it with an empty title.
             unset($this->attributes['title']);
