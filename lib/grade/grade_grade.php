5232d3f2c9eeafa53c48adcf6fba3f5a59007672
Damyon Wiese <damyon@moodle.com>
2014-08-04 15:03:26 +0800

    MDL-35984 grade: Recalculate min/max for sum of grades when hidden grades are involved.

--- lib/grade/grade_grade.php
+++ lib/grade/grade_grade.php
@@ -572,7 +572,10 @@ class grade_grade extends grade_object {
      *
      * @param array $grade_grades all course grades of one user, & used for better internal caching
      * @param array $grade_items array of grade items, & used for better internal caching
-     * @return array
+     * @return array This is an array of 3 arrays:
+     *      unknown => list of item ids that may be affected by hiding (with the calculated grade as the value)
+     *      altered => list of item ids that are definitely affected by hiding (with the calculated grade as the value)
+     *      alteredgrademax => for each item in altered or unknown, the new value of the grademax
      */
     public static function get_hiding_affected(&$grade_grades, &$grade_items) {
         global $CFG;
@@ -585,6 +588,8 @@ class grade_grade extends grade_object {
         $todo = array();
         $unknown = array();  // can not find altered
         $altered = array();  // altered grades
+        $alteredgrademax = array();  // Altered grade max values.
+        $alteredgrademin = array();  // Altered grade min values.
 
         $hiddenfound = false;
         foreach($grade_grades as $itemid=>$unused) {
@@ -604,7 +609,10 @@ class grade_grade extends grade_object {
             }
         }
         if (!$hiddenfound) {
-            return array('unknown'=>array(), 'altered'=>array());
+            return array('unknown' => array(),
+                         'altered' => array(),
+                         'alteredgrademax' => array(),
+                         'alteredgrademin' => array());
         }
 
         $max = count($todo);
@@ -646,6 +654,7 @@ class grade_grade extends grade_object {
                                 if (array_key_exists($itemid, $altered)) {
                                     //nulling an altered precursor
                                     $values[$itemid] = $altered[$itemid];
+                                    unset($values[$itemid]);
                                 } elseif (empty($values[$itemid])) {
                                     $values[$itemid] = $grade_grades[$itemid]->finalgrade;
                                 }
@@ -686,12 +695,18 @@ class grade_grade extends grade_object {
                                 continue;
                             }
 
-                            $agg_grade = $grade_category->aggregate_values($values, $grade_items);
+                            $adjustedgrade = $grade_category->aggregate_values_and_adjust_bounds($values, $grade_items);
 
                             // recalculate the rawgrade back to requested range
-                            $finalgrade = grade_grade::standardise_score($agg_grade, 0, 1, $grade_items[$do]->grademin, $grade_items[$do]->grademax);
+                            $finalgrade = grade_grade::standardise_score($adjustedgrade['grade'],
+                                                                         0,
+                                                                         1,
+                                                                         $adjustedgrade['grademin'],
+                                                                         $adjustedgrade['grademax']);
 
                             $finalgrade = $grade_items[$do]->bounded_grade($finalgrade);
+                            $alteredgrademin[$do] = $adjustedgrade['grademin'];
+                            $alteredgrademax[$do] = $adjustedgrade['grademax'];
 
                             $altered[$do] = $finalgrade;
                             unset($todo[$key]);
@@ -706,7 +721,10 @@ class grade_grade extends grade_object {
             }
         }
 
-        return array('unknown'=>$unknown, 'altered'=>$altered);
+        return array('unknown' => $unknown,
+                     'altered' => $altered,
+                     'alteredgrademax' => $alteredgrademax,
+                     'alteredgrademin' => $alteredgrademin);
     }
 
     /**
