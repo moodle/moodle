3ce36f6ac1c87655e1bda8e047751a281dbc4962
Tim Hunt <T.J.Hunt@open.ac.uk>
2014-08-08 13:56:23 +0100

    MDL-45642 questions: add required index to question_attempts.
    It is important to be able to tell which behaviours are in use, and
    without this index, it requires a complete scan of this table, which can
    be very big, to verify that a behaviour is unused.

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -3679,5 +3679,20 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2014051201.03);
     }
 
+    if ($oldversion < 2014051201.06) {
+
+        // Define index behaviour (not unique) to be added to question_attempts.
+        $table = new xmldb_table('question_attempts');
+        $index = new xmldb_index('behaviour', XMLDB_INDEX_NOTUNIQUE, array('behaviour'));
+
+        // Conditionally launch add index behaviour.
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2014051201.06);
+    }
+
     return true;
 }
--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2014051201.05;              // 20140512      = branching date YYYYMMDD - do not modify!
+$version  = 2014051201.06;              // 20140512      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
