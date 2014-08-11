fa268b7ed6e8ef3cbe81fe9a05b464e7e9950b15
Damyon Wiese <damyon@moodle.com>
2014-08-11 11:37:35 +0800

    MDL-46748 MathJax filter: Make misconfigured mathjax url less "fatal" for Atto.

--- filter/mathjaxloader/yui/src/loader/js/loader.js
+++ filter/mathjaxloader/yui/src/loader/js/loader.js
@@ -74,11 +74,13 @@ M.filter_mathjaxloader = M.filter_mathjaxloader || {
     _setLocale: function() {
         if (!this._configured) {
             var lang = this._lang;
-            MathJax.Hub.Queue(function () {
-                MathJax.Localization.setLocale(lang);
-            });
-            MathJax.Hub.Configured();
-            this._configured = true;
+            if (typeof MathJax !== "undefined") {
+                MathJax.Hub.Queue(function () {
+                    MathJax.Localization.setLocale(lang);
+                });
+                MathJax.Hub.Configured();
+                this._configured = true;
+            }
         }
     },
 
@@ -93,7 +95,9 @@ M.filter_mathjaxloader = M.filter_mathjaxloader || {
             Y.use('mathjax', function() {
                 self._setLocale();
                 Y.all('.filter_mathjaxloader_equation').each(function(node) {
-                    MathJax.Hub.Queue(["Typeset", MathJax.Hub, node.getDOMNode()]);
+                    if (typeof MathJax !== "undefined") {
+                        MathJax.Hub.Queue(["Typeset", MathJax.Hub, node.getDOMNode()]);
+                    }
                 });
             });
         }
@@ -110,7 +114,9 @@ M.filter_mathjaxloader = M.filter_mathjaxloader || {
             self._setLocale();
             event.nodes.each(function (node) {
                 node.all('.filter_mathjaxloader_equation').each(function(node) {
-                    MathJax.Hub.Queue(["Typeset", MathJax.Hub, node.getDOMNode()]);
+                    if (typeof MathJax !== "undefined") {
+                        MathJax.Hub.Queue(["Typeset", MathJax.Hub, node.getDOMNode()]);
+                    }
                 });
             });
         });
