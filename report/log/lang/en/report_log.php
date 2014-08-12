ae18cf2461c252f0c351d15a153c9a74881125a4
Adrian Greeve <adrian@moodle.com>
2014-08-12 14:13:35 +0800

    MDL-39888 mnet: Added a notification that logs may not be displayed in the log report.
    In Moodle 2.7+ we are using the new logging system and if legacy logs are not enabled
    (on the client system) then no logs will be displayed.

--- report/log/lang/en/report_log.php
+++ report/log/lang/en/report_log.php
@@ -35,6 +35,7 @@ $string['log:view'] = 'View course logs';
 $string['log:viewtoday'] = 'View today\'s logs';
 $string['page'] = 'Page {$a}';
 $string['logsformat'] = 'Logs format';
+$string['mnetlogwarning'] = 'If your MNet clients are running Moodle 2.7 or higher, then you may not receive any logs.';
 $string['nologreaderenabled'] = 'No log reader enabled';
 $string['page-report-log-x'] = 'Any log report';
 $string['page-report-log-index'] = 'Course log report';
