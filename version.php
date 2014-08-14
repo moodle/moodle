e1eb18080666a4485f7edb7628238c1da69a2e62
Marina Glancy <marina@moodle.com>
2014-08-14 14:52:07 +0800

    weekly release 2.8dev

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2014080801.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2014081400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '2.8dev (Build: 20140807)'; // Human-friendly version name
+$release  = '2.8dev (Build: 20140814)'; // Human-friendly version name
 
 $branch   = '28';                       // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
