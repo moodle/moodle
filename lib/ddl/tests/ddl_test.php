1b775e96c1387be29612181cc5709640eb68a3ce
Rajesh Taneja <rajesh@moodle.com>
2014-08-15 16:37:12 +0800

    MDL-46327 phpunit: Fixed oracle fail

--- lib/ddl/tests/ddl_test.php
+++ lib/ddl/tests/ddl_test.php
@@ -1608,6 +1608,11 @@ class core_ddl_testcase extends database_driver_testcase {
                   JOIN {test_temp} t ON t.name = n.name";
         $records = $DB->get_records_sql($sql);
         $this->assertCount(1, $records);
+
+        // Drop temp table.
+        $dbman->drop_temp_table($table2);
+        $this->assertFalse($dbman->table_exists('test_temp'));
+        $this->assertDebuggingCalled();
     }
 
     public function test_concurrent_temp_tables() {
