55c051f3526dd329202001d093f121f7ec62c6ff
Marina Glancy <marina@moodle.com>
2014-09-03 15:45:47 +0800

    MDL-47080 theme: call format_string on site shortname

--- theme/bootstrapbase/layout/popup.php
+++ theme/bootstrapbase/layout/popup.php
@@ -48,7 +48,9 @@ echo $OUTPUT->doctype() ?>
 <header role="banner" class="navbar navbar-fixed-top moodle-has-zindex <?php echo $hideclass; ?>">
     <nav role="navigation" class="navbar-inner">
         <div class="container-fluid">
-            <a class="brand" href="<?php echo $CFG->wwwroot;?>"><?php echo $SITE->shortname; ?></a>
+            <a class="brand" href="<?php echo $CFG->wwwroot;?>"><?php echo
+                format_string($SITE->shortname, true, array('context' => context_course::instance(SITEID)));
+                ?></a>
             <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
