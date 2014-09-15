952f37486dad009c99a0d6dbd484f7625cabd588
Damyon Wiese <damyon@moodle.com>
2014-09-15 16:49:23 +0800

    MDL-44447 Grade: Whitespace fix

--- grade/export/lib.php
+++ grade/export/lib.php
@@ -249,7 +249,7 @@ abstract class grade_export {
      */
     public function format_column_name($grade_item, $feedback=false, $gradedisplayname = null) {
         $column = new stdClass();
-        
+
         if ($grade_item->itemtype == 'mod') {
             $column->name = get_string('modulename', $grade_item->itemmodule).get_string('labelsep', 'langconfig').$grade_item->get_name();
         } else {
