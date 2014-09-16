e6d8ca248d9116677891574c47441f843bd83e53
Tim Hunt <T.J.Hunt@open.ac.uk>
2014-09-16 08:49:45 +0100

    MDL-46088 quiz secure window: further Atto JS fixes.

--- mod/quiz/module.js
+++ mod/quiz/module.js
@@ -246,12 +246,17 @@ M.mod_quiz.secure_window = {
         setTimeout(M.mod_quiz.secure_window.clear_status, 10);
     },
 
-    /**
-     * Prevent the selection event without showing an alert.
-     *
-     * @method prevent_selection
-     * @param {EventFacade} e
-     */
+    is_content_editable: function(n) {
+        if (n.test('[contenteditable=true]')) {
+            return true;
+        }
+        n = n.get('parentNode');
+        if (n === null) {
+            return false;
+        }
+        return M.mod_quiz.is_content_editable(n);
+    },
+
     prevent_selection: function(e) {
         return false;
     },
@@ -266,7 +271,7 @@ M.mod_quiz.secure_window = {
             // Left click on a button or similar. No worries.
             return;
         }
-        if (e.button == 1 && e.target.test('[contenteditable=true]')) {
+        if (e.button == 1 && M.mod_quiz.is_content_editable(e.target)) {
             // Left click in Atto or similar.
             return;
         }
