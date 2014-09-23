6b356ae385e7e9dfd8c8c185b5c0ae44fea16912
Jonathan Harker <jonathan@catalyst.net.nz>
2014-09-23 12:52:47 +1200

    MDL-47316 mod_forum: Fix course overview performance regression
     make more efficient use of mod_info cache by processing courses
     sequentially.
    
    Change-Id: I03b808c8a7efc42879ec2689fbd2a9fe7a8f1103

--- mod/forum/lib.php
+++ mod/forum/lib.php
@@ -1457,7 +1457,8 @@ function forum_print_overview($courses,&$htmlarray) {
                 .'JOIN {forum_posts} p ON p.discussion = d.id '
                 ."WHERE ($coursessql) "
                 .'AND p.userid != ? '
-                .'GROUP BY d.id, d.forum, f.course, d.groupid';
+                .'GROUP BY d.id, d.forum, f.course, d.groupid '
+                .'ORDER BY f.course, d.forum';
 
     // Avoid warnings.
     if (!$discussions = $DB->get_records_sql($sql, $params)) {
