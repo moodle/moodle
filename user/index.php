6e9aeadd75026f33ff92d9af735d8399f7e24cf3
Dmitry Nefedov <corzina@ya.ru>
2014-09-27 08:31:20 +0800

    MDL-47408 user: Add enableblogs checking before adding blog's link.

--- user/index.php
+++ user/index.php
@@ -691,7 +691,7 @@ if ($mode === MODE_USERDETAILS) {    // Print simple listing.
 
                 $links = array();
 
-                if ($CFG->bloglevel > 0) {
+                if ($CFG->enableblogs && $CFG->bloglevel > 0) {
                     $links[] = html_writer::link(new moodle_url('/blog/index.php?userid='.$user->id), get_string('blogs', 'blog'));
                 }
 
