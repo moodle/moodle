aaabecb3d8d14fcc233f29ae1c717998bc62d86a
Ben Kelada <ben.kelada@gmail.com>
2014-10-14 17:25:01 +1100

    MDL-46500 filepicker: prevent error when element does not exist

--- mod/data/edit.php
+++ mod/data/edit.php
@@ -290,10 +290,12 @@ if ($data->addtemplate){
     ///then we generate strings to replace
     foreach ($possiblefields as $eachfield){
         $field = data_get_field($eachfield, $data);
-        $patterns[]="[[".$field->field->name."]]";
-        $replacements[] = $field->display_add_field($rid);
-        $patterns[]="[[".$field->field->name."#id]]";
-        $replacements[] = 'field_'.$field->field->id;
+        if (strpos($data->addtemplate, "[[".$field->field->name."]]") !== false) {
+            $patterns[] = "[[".$field->field->name."]]";
+            $replacements[] = $field->display_add_field($rid);
+            $patterns[] = "[[".$field->field->name."#id]]";
+            $replacements[] = 'field_'.$field->field->id;
+        }
     }
     $newtext = str_ireplace($patterns, $replacements, $data->{$mode});
 
