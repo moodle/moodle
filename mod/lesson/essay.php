5a9575e2a187efe2d8f583c1a189a396e6a1e6b1
Jean-Michel Vedrine <vedrine@vedrine.org>
2014-10-18 20:00:33 +0200

    MDL-29873 Lesson: Essays not shown on grade page with SQL server

--- mod/lesson/essay.php
+++ mod/lesson/essay.php
@@ -170,7 +170,7 @@ switch ($mode) {
 
         $pages = $lesson->load_all_pages();
         foreach ($pages as $key=>$page) {
-            if ($page->qtype !== LESSON_PAGE_ESSAY) {
+            if ($page->qtype != LESSON_PAGE_ESSAY) {
                 unset($pages[$key]);
             }
         }
@@ -255,7 +255,7 @@ switch ($mode) {
         // Get lesson pages that are essay
         $pages = $lesson->load_all_pages();
         foreach ($pages as $key=>$page) {
-            if ($page->qtype !== LESSON_PAGE_ESSAY) {
+            if ($page->qtype != LESSON_PAGE_ESSAY) {
                 unset($pages[$key]);
             }
         }
