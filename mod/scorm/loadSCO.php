a53bf3a03108197e7bf49c325b337a2cff30ba39
Ray Morris <Ray.Morris@teex.tamu.edu>
2014-10-24 16:45:32 -0500

    MDL-47882 SCORM: invalid HTML in loadSCO.php

--- mod/scorm/loadSCO.php
+++ mod/scorm/loadSCO.php
@@ -229,7 +229,7 @@ echo html_writer::tag('title', 'LoadSCO');
             document.body.innerHTML = "<p><?php echo get_string('activityloading', 'scorm');?>" +
                                         "<span id='countdown'><?php echo $delayseconds ?></span> " +
                                         "<?php echo get_string('numseconds', 'moodle', '');?>. &nbsp; " +
-                                        "<img src='<?php echo $OUTPUT->pix_url('wait', 'scorm') ?>'><p>";
+                                        "<img src='<?php echo $OUTPUT->pix_url('wait', 'scorm') ?>'></p>";
             var e = document.getElementById("countdown");
             var cSeconds = parseInt(e.innerHTML);
             var timer = setInterval(function() {
