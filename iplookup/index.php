eb46bf2f4e80f4421255f6aee00b73448ba582a7
Frederic Massart <fred@moodle.com>
2014-10-28 12:04:08 +0800

    MDL-47321 iplookup: Prevent guest users from using iplookup

--- iplookup/index.php
+++ iplookup/index.php
@@ -27,7 +27,11 @@
 require('../config.php');
 require_once('lib.php');
 
-require_login();
+require_login(0, false);
+if (isguestuser()) {
+    // Guest users cannot perform lookups.
+    throw new require_login_exception('Guests are not allowed here.');
+}
 
 $ip   = optional_param('ip', getremoteaddr(), PARAM_HOST);
 $user = optional_param('user', 0, PARAM_INT);
