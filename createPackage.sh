ee9ddad252601b7ff9d20ce598580b585dcfaeb1
Gonen Radai <gonen.r@gmail.com>
2014-10-29 17:31:55 +0200

    added script to easily make package for moodle.org

---
+++ createPackage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BRANCH=`git branch | grep '*' |awk -F _ '{print $2}'`
+VER=`cat local/kaltura/version.php |grep '>version' | awk '{print $3}' | awk -F ";" '{print $1}'`
+
+echo "Generaating package for Kaltura_Video_Package_moodle"$BRANCH"_"$VER".zip\n"
+
+FILENAME="Kaltura_Video_Package_moodle"$BRANCH"_"$VER".zip"
+
+zip -r $FILENAME lib filter mod local
