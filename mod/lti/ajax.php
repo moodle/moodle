52555c36989b6704550ed0b3c6e832f5e7e150b7
Jetha Chan <jetha@moodle.com>
2014-11-12 15:03:32 +0800

    MDL-47920 mod_lti: add capability checks, http headers

--- mod/lti/ajax.php
+++ mod/lti/ajax.php
@@ -24,11 +24,13 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @author     Chris Scribner
  */
+define('AJAX_SCRIPT', true);
 
 require_once(dirname(__FILE__) . "/../../config.php");
 require_once($CFG->dirroot . '/mod/lti/locallib.php');
 
 $courseid = required_param('course', PARAM_INT);
+$context = context_course::instance($courseid);
 
 require_login($courseid, false);
 
@@ -41,15 +43,18 @@ switch ($action) {
         $toolurl = required_param('toolurl', PARAM_RAW);
         $toolid = optional_param('toolid', 0, PARAM_INT);
 
-        if(empty($toolid) && !empty($toolurl)){
+        require_capability('moodle/course:manageactivities', $context);
+        require_capability('mod/lti:addinstance', $context);
+
+        if (empty($toolid) && !empty($toolurl)) {
             $tool = lti_get_tool_by_url_match($toolurl, $courseid);
 
             if(!empty($tool)){
                 $toolid = $tool->id;
 
                 $response->toolid = $tool->id;
-                $response->toolname = htmlspecialchars($tool->name);
-                $response->tooldomain = htmlspecialchars($tool->tooldomain);
+                $response->toolname = s($tool->name);
+                $response->tooldomain = s($tool->tooldomain);
             }
         } else {
             $response->toolid = $toolid;
@@ -66,14 +71,19 @@ switch ($action) {
             ';
 
             $privacyconfigs = $DB->get_records_sql($query, array('typeid' => $toolid));
-            foreach($privacyconfigs as $config){
+            $success = count($privacyconfigs) > 0;
+            foreach ($privacyconfigs as $config) {
                 $configname = $config->name;
                 $response->$configname = $config->value;
             }
+            if (!$success) {
+                $response->error = s(get_string('tool_config_not_found', 'mod_lti'));
+            }
         }
+
         break;
 }
-
+echo $OUTPUT->header();
 echo json_encode($response);
 
 die;
