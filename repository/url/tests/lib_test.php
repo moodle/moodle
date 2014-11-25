dbe6a4d1882159920b80b49d660a33d54d3bd453
John Okely <john@moodle.com>
2014-11-25 10:03:21 +0800

    MDL-41846 repository_url: Convert spaces to %20 in input url

---
+++ repository/url/tests/lib_test.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for the URL repository.
+ *
+ * @package   repository_url
+ * @copyright 2014 John Okely
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+global $CFG;
+require_once($CFG->dirroot . '/repository/url/lib.php');
+
+
+/**
+ * URL repository test case.
+ *
+ * @copyright 2014 John Okely
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class repository_url_lib_testcase extends advanced_testcase {
+
+    /**
+     * Check that the url escaper performs as expected
+     */
+    public function test_escape_url() {
+        $this->resetAfterTest();
+
+        $repoid = $this->getDataGenerator()->create_repository('url')->id;
+
+        $testdata = array(
+                'http://example.com/test_file.png' => 'http://example.com/test_file.png',
+                'http://example.com/test%20file.png' => 'http://example.com/test%20file.png',
+                'http://example.com/test file.png' => 'http://example.com/test%20file.png',
+                'http://example.com/test file.png?query=string+test&more=string+tests' =>
+                    'http://example.com/test%20file.png?query=string+test&more=string+tests'
+            );
+
+        foreach ($testdata as $input => $expected) {
+            // The constructor uses a optional_param, so we need to hack $_GET.
+            $_GET['file'] = $input;
+            $repository = new repository_url($repoid);
+            $this->assertSame($expected, $repository->file_url);
+        }
+
+        unset($_GET['file']);
+    }
+
+}
