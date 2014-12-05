81fd67f566210d55b9a0250b6996bd7623daa6f3
Frederic Massart <fred@moodle.com>
2014-12-05 15:31:42 +0800

    MDL-48496 block_rss_client: Do not display curl error messages

--- blocks/rss_client/lang/en/block_rss_client.php
+++ blocks/rss_client/lang/en/block_rss_client.php
@@ -33,6 +33,7 @@ $string['clientshowchannellinklabel'] = 'Should a link to the original site (cha
 $string['clientshowimagelabel'] = 'Show channel image if available :';
 $string['configblock'] = 'Configure this block';
 $string['couldnotfindfeed'] = 'Could not find feed with id';
+$string['couldnotfindloadrssfeed'] = 'Could not find or load the RSS feed.';
 $string['customtitlelabel'] = 'Custom title (leave blank to use title supplied by feed):';
 $string['deletefeedconfirm'] = 'Are you sure you want to delete this feed?';
 $string['disabledrssfeeds'] = 'RSS feeds are disabled';
