6edc717d0d22f216175bf0419da2aa479c4dda53
Zachary Durber <zdurber@gmail.com>
2014-12-09 15:47:20 +0800

    MDL-47694 mod_assign: Grades require a grade to show as graded
    Special thanks to Howard Miller.

--- mod/assign/lib.php
+++ mod/assign/lib.php
@@ -414,7 +414,8 @@ function assign_print_overview($courses, &$htmlarray) {
                                                   g.attemptnumber = smx.maxattempt
                                               WHERE
                                                   ( g.timemodified is NULL OR
-                                                  s.timemodified > g.timemodified ) AND
+                                                  s.timemodified > g.timemodified OR
+                                                  g.grade IS NULL ) AND
                                                   s.timemodified IS NOT NULL AND
                                                   s.status = ? AND
                                                   s.attemptnumber = smx.maxattempt AND
