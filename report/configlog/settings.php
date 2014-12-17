17f084e1c555b8f7a35794c05bcc4d9222340f79
Marco Rougeth <marco@rougeth.com>
2014-12-17 14:42:54 -0200

    Removed duplicated code

--- report/configlog/settings.php
+++ report/configlog/settings.php
@@ -25,8 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-defined('MOODLE_INTERNAL') || die;
-
 $ADMIN->add('reports', new admin_externalpage('reportconfiglog', get_string('configlog', 'report_configlog'), "$CFG->wwwroot/report/configlog/index.php"));
 
 // no report settings
