de17153b4c9c6b7c3f48ce7a3201630d06803602
Mark Nelson <markn@moodle.com>
2014-12-17 23:29:25 -0800

    MDL-48549 mod_assign: added upgrade to clean the tables

--- mod/assign/db/upgrade.php
+++ mod/assign/db/upgrade.php
@@ -496,5 +496,37 @@ function xmldb_assign_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2014051201, 'assign');
     }
 
+    if ($oldversion < 2014051202) {
+        // Delete any entries from the assign_user_flags and assign_user_mapping that are no longer required.
+        if ($DB->get_dbfamily() === 'mysql') {
+            $sql1 = "DELETE {assign_user_flags}
+                       FROM {assign_user_flags}
+                  LEFT JOIN {assign}
+                         ON {assign_user_flags}.assignment = {assign}.id
+                      WHERE {assign}.id IS NULL";
+
+            $sql2 = "DELETE {assign_user_mapping}
+                       FROM {assign_user_mapping}
+                  LEFT JOIN {assign}
+                         ON {assign_user_mapping}.assignment = {assign}.id
+                      WHERE {assign}.id IS NULL";
+        } else {
+            $sql1 = "DELETE FROM {assign_user_flags}
+                WHERE NOT EXISTS (
+                          SELECT 'x' FROM {assign}
+                           WHERE {assign_user_flags}.assignment = {assign}.id)";
+
+            $sql2 = "DELETE FROM {assign_user_mapping}
+                WHERE NOT EXISTS (
+                          SELECT 'x' FROM {assign}
+                           WHERE {assign_user_mapping}.assignment = {assign}.id)";
+        }
+
+        $DB->execute($sql1);
+        $DB->execute($sql2);
+
+        upgrade_mod_savepoint(true, 2014051202, 'assign');
+    }
+
     return true;
 }
