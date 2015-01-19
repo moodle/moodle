9245fade30c1dcccc65b5577886e22852f1fee0e
gonenradai <gonen.r@gmail.com>
2015-01-19 17:15:26 +0200

    advanced minor version of release

--- lib/editor/atto/plugins/kalturamedia/version.php
+++ lib/editor/atto/plugins/kalturamedia/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2014121807;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release = 'Kaltura release 4.0.01';
+$plugin->release = 'Kaltura release 4.0.02';
 $plugin->requires  = 2014050800;        // Requires this Moodle version.
 $plugin->component = 'atto_kalturamedia';  // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = array(
