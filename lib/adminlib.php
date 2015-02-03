8298719c51b387bbb887722de1c7f014e015b819
Andrew Nicols <andrew@nicols.co.uk>
2015-02-03 13:21:40 +0800

    MDL-48484 admin: Improve coding style

--- lib/adminlib.php
+++ lib/adminlib.php
@@ -3231,18 +3231,20 @@ class admin_setting_configtime extends admin_setting {
             $defaultinfo = NULL;
         }
 
-        $return = '<div class="form-time defaultsnext">'.
-            '<label class="accesshide" for="' . $this->get_id().'h">' . get_string('hours') . '</label>' .
-            '<select id="'.$this->get_id().'h" name="'.$this->get_full_name().'[h]">';
+        $return  = '<div class="form-time defaultsnext">';
+        $return .= '<label class="accesshide" for="' . $this->get_id() . 'h">' . get_string('hours') . '</label>';
+        $return .= '<select id="' . $this->get_id() . 'h" name="' . $this->get_full_name() . '[h]">';
         for ($i = 0; $i < 24; $i++) {
-            $return .= '<option value="'.$i.'"'.($i == $data['h'] ? ' selected="selected"' : '').'>'.$i.'</option>';
+            $return .= '<option value="' . $i . '"' . ($i == $data['h'] ? ' selected="selected"' : '') . '>' . $i . '</option>';
         }
-        $return .= '</select>:<label class="accesshide" for="' . $this->get_id().'m">' . get_string('minutes');
-        $return .= '</label><select id="'.$this->get_id().'m" name="'.$this->get_full_name().'[m]">';
+        $return .= '</select>:';
+        $return .= '<label class="accesshide" for="' . $this->get_id() . 'm">' . get_string('minutes') . '</label>';
+        $return .= '<select id="' . $this->get_id() . 'm" name="' . $this->get_full_name() . '[m]">';
         for ($i = 0; $i < 60; $i += 5) {
-            $return .= '<option value="'.$i.'"'.($i == $data['m'] ? ' selected="selected"' : '').'>'.$i.'</option>';
+            $return .= '<option value="' . $i . '"' . ($i == $data['m'] ? ' selected="selected"' : '') . '>' . $i . '</option>';
         }
-        $return .= '</select></div>';
+        $return .= '</select>';
+        $return .= '</div>';
         return format_admin_setting($this, $this->visiblename, $return, $this->description, false, '', $defaultinfo, $query);
     }
 
