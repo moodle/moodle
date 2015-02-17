343b11a3fae05bbbb55d0bd8c5c155bca7a9aa0c
Jetha Chan <jetha@moodle.com>
2015-02-17 17:47:10 +0800

    MDL-49224 course: allow setting fullname and summary in course generator

--- admin/tool/generator/classes/course_backend.php
+++ admin/tool/generator/classes/course_backend.php
@@ -84,6 +84,21 @@ class tool_generator_course_backend extends tool_generator_backend {
     private $shortname;
 
     /**
+     * @var string Course fullname.
+     */
+    private $fullname = "";
+
+    /**
+     * @var string Course summary.
+     */
+    private $summary = "";
+
+    /**
+     * @var string Course summary format, defaults to FORMAT_HTML.
+     */
+    private $summaryformat = FORMAT_HTML;
+
+    /**
      * @var testing_data_generator Data generator
      */
     protected $generator;
@@ -107,10 +122,33 @@ class tool_generator_course_backend extends tool_generator_backend {
      * @param int|bool $filesizelimit The max number of bytes for a generated file
      * @param bool $progress True if progress information should be displayed
      */
-    public function __construct($shortname, $size, $fixeddataset = false, $filesizelimit = false, $progress = true) {
+    public function __construct(
+        $shortname,
+        $size,
+        $fixeddataset = false,
+        $filesizelimit = false,
+        $progress = true,
+        $fullname = null,
+        $summary = null,
+        $summaryformat = FORMAT_HTML) {
 
         // Set parameters.
         $this->shortname = $shortname;
+        if (is_null($fullname)) {
+            $this->fullname = get_string(
+                'fullname',
+                'tool_generator',
+                array(
+                    'size' => get_string('shortsize_' . $size, 'tool_generator')
+                )
+            );
+        } else {
+            $this->fullname = $fullname;
+        }
+        if (!is_null($summary)) {
+            $this->summary = $summary;
+            $this->summaryformat = $summaryformat;
+        }
 
         parent::__construct($size, $fixeddataset, $filesizelimit, $progress);
     }
@@ -208,10 +246,16 @@ class tool_generator_course_backend extends tool_generator_backend {
      */
     private function create_course() {
         $this->log('createcourse', $this->shortname);
-        $courserecord = array('shortname' => $this->shortname,
-                'fullname' => get_string('fullname', 'tool_generator',
-                    array('size' => get_string('shortsize_' . $this->size, 'tool_generator'))),
-                'numsections' => self::$paramsections[$this->size]);
+        $courserecord = array(
+            'shortname' => $this->shortname,
+            'fullname' => $this->fullname,
+            'numsections' => self::$paramsections[$this->size]
+        );
+        if (strlen($this->summary) > 0) {
+            $courserecord['summary'] = $this->summary;
+            $courserecord['summary_format'] = $this->summaryformat;
+        }
+
         return $this->generator->create_course($courserecord, array('createsections' => true));
     }
 
