b907bc26f6c76b67bec8e14b8d166582f3b30cb9
Helen Foster <helen@moodle.org>
2015-02-23 21:15:18 +0100

    MDL-49241 lang: Merge English strings from the en_fix language pack

--- portfolio/picasa/lang/en/portfolio_picasa.php
+++ portfolio/picasa/lang/en/portfolio_picasa.php
@@ -23,13 +23,13 @@
  */
 
 $string['clientid'] = 'Client ID';
-$string['noauthtoken'] = 'An authentication token has not been recieved from google. Please ensure you are allowing moodle to access your google account';
+$string['noauthtoken'] = 'An authentication token has not been received from Google. Please ensure you are allowing Moodle to access your Google account.';
 $string['nooauthcredentials'] = 'OAuth credentials required.';
 $string['nooauthcredentials_help'] = 'To use the Picasa portfolio plugin you must configure OAuth credentials in the portfolio settings.';
-$string['oauthinfo'] = '<p>To use this plugin, you must register your site with Google, as described in the documentation <a href="{$a->docsurl}">Google OAuth 2.0 setup</a>.</p><p>As part of the registration process, you will need to enter the following URL as \'Authorized Redirect URIs\':</p><p>{$a->callbackurl}</p>Once registered, you will be provided with a client ID and secret which can be used to configure all Google Drive and Picasa plugins.</p>';
+$string['oauthinfo'] = '<p>To use this plugin, you must register your site with Google, as described in the documentation <a href="{$a->docsurl}">Google OAuth 2.0 setup</a>.</p><p>As part of the registration process, you will need to enter the following URL as \'Authorized Redirect URIs\':</p><p>{$a->callbackurl}</p><p>Once registered, you will be provided with a client ID and secret which can be used to configure all Google Drive and Picasa plugins.</p>';
 $string['oauth2upgrade_message_subject'] = 'Important information regarding Picasa portfolio plugin';
 $string['oauth2upgrade_message_content'] = 'As part of the upgrade to Moodle 2.3, the Picasa portfolio plugin has been disabled. To re-enable it, your Moodle site needs to be registered with Google, as described in the documentation {$a->docsurl}, in order to obtain a client ID and secret. The client ID and secret can then be used to configure all Google Drive and Picasa plugins.';
 $string['oauth2upgrade_message_small'] = 'This plugin has been disabled, as it requires configuration as described in the documentation Google OAuth 2.0 setup.';
 $string['pluginname'] = 'Picasa';
-$string['sendfailed'] = 'The file {$a} failed to transfer to picasa';
+$string['sendfailed'] = 'The file {$a} failed to transfer to Picasa';
 $string['secret'] = 'Secret';
