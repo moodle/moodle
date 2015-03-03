2924ba1c73f9ed3d525987807f9d289b3eb38154
David Monllao <davidm@moodle.com>
2015-03-03 12:38:46 +0800

    MDL-49204 core_message: Checking current user

--- message/index.php
+++ message/index.php
@@ -114,9 +114,7 @@ $user2realuser = !empty($user2) && core_user::is_real_user($user2->id);
 $showactionlinks = $showactionlinks && $user2realuser;
 $systemcontext = context_system::instance();
 
-// Is the user involved in the conversation?
-// Do they have the ability to read other user's conversations?
-if (!message_current_user_is_involved($user1, $user2) && !has_capability('moodle/site:readallmessages', $systemcontext)) {
+if ($currentuser === false && !has_capability('moodle/site:readallmessages', $systemcontext)) {
     print_error('accessdenied','admin');
 }
 
