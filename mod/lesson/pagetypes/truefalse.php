ffd413d43695ae7f02a78fcf43ee79f4281923db
Jean-Michel Vedrine <vedrine@vedrine.org>
2015-03-08 11:06:53 +0100

    MDL-39262 mod_lesson: disable shortforms in lesson question display
    Conflicts:
    	mod/lesson/pagetypes/shortanswer.php

--- mod/lesson/pagetypes/truefalse.php
+++ mod/lesson/pagetypes/truefalse.php
@@ -358,6 +358,9 @@ class lesson_display_answer_form_truefalse extends moodleform {
             $attempt->answerid = null;
         }
 
+        // Disable shortforms.
+        $mform->setDisableShortforms();
+
         $mform->addElement('header', 'pageheader');
 
         $mform->addElement('html', $OUTPUT->container($contents, 'contents'));
