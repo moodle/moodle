4aee9404329becfa5e6f380dee7c6cbf734b0be5
Tim Hunt <T.J.Hunt@open.ac.uk>
2015-03-18 13:56:54 +0000

    MDL-47494 ddimageortext: Bump version for 1.7 release.

--- question/type/ddimageortext/version.php
+++ question/type/ddimageortext/version.php
@@ -24,12 +24,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014111200;
+$plugin->version   = 2015031800;
 $plugin->requires  = 2013101800;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_ddimageortext';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.6 for Moodle 2.6+';
+$plugin->release   = '1.7 for Moodle 2.6+';
 
 $plugin->dependencies = array(
     'qtype_gapselect' => 2014111200,
