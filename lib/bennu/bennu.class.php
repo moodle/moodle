b7dffc34f5251886fd208348b43c805d501244eb
Lars Anderson <lsa@its.aau.dk>
2015-03-25 15:29:44 +0100

    MDL-49561 core_calendar: Fix ical export for events with no duration

--- lib/bennu/bennu.class.php
+++ lib/bennu/bennu.class.php
@@ -20,6 +20,14 @@ class Bennu {
         return gmstrftime('%Y%m%dT%H%M%SZ', $t);
     }
 
+    static function timestamp_to_date($t = NULL) {
+        if($t === NULL) {
+            $t = time();
+        }
+        return strftime('%Y%m%d', $t);
+    }
+
+
     static function generate_guid() {
         // Implemented as per the Network Working Group draft on UUIDs and GUIDs
     
