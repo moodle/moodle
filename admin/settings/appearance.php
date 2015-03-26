e5cfbeea769059e6f73ab8210b2be02ea2312112
Adrian Greeve <adrian@moodle.com>
2015-03-26 13:48:12 +0800

    MDL-49630 admin: defaulthomepage changed to dashboard.
    Part of MDL-45774.

--- admin/settings/appearance.php
+++ admin/settings/appearance.php
@@ -140,7 +140,8 @@ mypreferences,moodle|/user/preferences.php|preferences',
         HOMEPAGE_MY => new lang_string('mymoodle', 'admin'),
         HOMEPAGE_USER => new lang_string('userpreference', 'admin')
     );
-    $temp->add(new admin_setting_configselect('defaulthomepage', new lang_string('defaulthomepage', 'admin'), new lang_string('configdefaulthomepage', 'admin'), HOMEPAGE_SITE, $choices));
+    $temp->add(new admin_setting_configselect('defaulthomepage', new lang_string('defaulthomepage', 'admin'),
+            new lang_string('configdefaulthomepage', 'admin'), HOMEPAGE_MY, $choices));
     $temp->add(new admin_setting_configcheckbox('allowguestmymoodle', new lang_string('allowguestmymoodle', 'admin'), new lang_string('configallowguestmymoodle', 'admin'), 1));
     $temp->add(new admin_setting_configcheckbox('navshowfullcoursenames', new lang_string('navshowfullcoursenames', 'admin'), new lang_string('navshowfullcoursenames_help', 'admin'), 0));
     $temp->add(new admin_setting_configcheckbox('navshowcategories', new lang_string('navshowcategories', 'admin'), new lang_string('confignavshowcategories', 'admin'), 1));
