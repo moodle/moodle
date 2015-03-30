d1b238381f31eab3be7b2e132871eb956c7cb456
Juan Leyva <juanleyvadelgado@gmail.com>
2015-03-30 14:10:01 +0200

    MDL-49499 user: Move events triggering to new API function
    New function in profile/lib: profile_viewed()

--- user/profile/lib.php
+++ user/profile/lib.php
@@ -621,4 +621,33 @@ function profile_load_custom_fields($user) {
     $user->profile = (array)profile_user_record($user->id);
 }
 
+/**
+ * Trigger a user profile viewed event.
+ *
+ * @param stdClass  $user user  object
+ * @param stdClass  $context  context object (course or user)
+ * @param stdClass  $course course  object
+ * @since Moodle 2.9
+ */
+function profile_view($user, $context, $course = null) {
+
+    $eventdata = array(
+        'objectid' => $user->id,
+        'relateduserid' => $user->id,
+        'context' => $context
+    );
+
+    if (!empty($course)) {
+        $eventdata['courseid'] = $course->id;
+        $eventdata['other'] = array(
+            'courseid' => $course->id,
+            'courseshortname' => $course->shortname,
+            'coursefullname' => $course->fullname
+        );
+    }
+
+    $event = \core\event\user_profile_viewed::create($eventdata);
+    $event->add_record_snapshot('user', $user);
+    $event->trigger();
+}
 
