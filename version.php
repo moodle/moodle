d302ba231ff20d744be953f92d4c687703c36332
Dan Poltawski <dan@moodle.com>
2015-04-17 10:54:51 +0100

    Moodle release 2.9beta

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2015041400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2015041700.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '2.9dev (Build: 20150414)'; // Human-friendly version name
+$release  = '2.9beta (Build: 20150417)'; // Human-friendly version name
 
 $branch   = '29';                       // This version's branch.
-$maturity = MATURITY_ALPHA;             // This version's maturity level.
+$maturity = MATURITY_BETA;             // This version's maturity level.
