d88484dc24aa9564b082042d17e79c6c783f43a3
Derick Turner <derick@e-learndesign.co.uk>
2015-04-23 13:06:05 +0100

    IOMAD: link to template edit list doesn't work as the requires path for local_lib.php is wrong.  Fixes #224

--- local/email/template_list.php
+++ local/email/template_list.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once( '../../local_lib.php');
+require_once( 'local_lib.php');
 require_once($CFG->dirroot . '/local/iomad/lib/blockpage.php');
 require_once($CFG->dirroot . '/blocks/iomad_company_admin/lib.php');
 require_once( 'config.php');
