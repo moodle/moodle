5563c0bcc163ec18a9990c02f8b6650031c3ca83
Mary Evans <lazydaisy@visible-expression.co.uk>
2015-06-01 20:11:14 +0100

    MDL-49659 theme: Removed home link and limited login info in secure.php
    Credits: Thanks go to Andreas Hruska [https://github.com/ahruska] for the original patch for this issue.

--- theme/bootstrapbase/layout/secure.php
+++ theme/bootstrapbase/layout/secure.php
@@ -51,9 +51,9 @@ echo $OUTPUT->doctype() ?>
 <header role="banner" class="navbar navbar-fixed-top moodle-has-zindex">
     <nav role="navigation" class="navbar-inner">
         <div class="container-fluid">
-            <a class="brand" href="<?php echo $CFG->wwwroot;?>"><?php echo
+            <span class="brand"><?php echo
                 format_string($SITE->shortname, true, array('context' => context_course::instance(SITEID)));
-                ?></a>
+                ?></span>
             <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -62,6 +62,7 @@ echo $OUTPUT->doctype() ?>
             <div class="nav-collapse collapse">
                 <ul class="nav pull-right">
                     <li><?php echo $OUTPUT->page_heading_menu(); ?></li>
+                    <li class="navbar-text"><?php echo $OUTPUT->login_info(false) ?></li>
                 </ul>
             </div>
         </div>
