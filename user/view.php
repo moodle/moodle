bffb1c15b34ca99e448182b4a151ab3a803f44e0
Bas Brands <bas@sonsbeekmedia.nl>
2015-06-24 08:36:52 +0200

    MDL-50653 user: forgotten closing div tag on the page
    The /user/view.php was missing a closing div for the <div class="description">
    causing the page grid to break on any Bootstrap 3 based theme.

--- user/view.php
+++ user/view.php
@@ -207,8 +207,8 @@ if ($user->deleted) {
 // Trigger a user profile viewed event.
 profile_view($user, $coursecontext, $course);
 
-echo '<div class="description">';
 if ($user->description && !isset($hiddenfields['description'])) {
+    echo '<div class="description">';
     if (!empty($CFG->profilesforenrolledusersonly) && !$DB->record_exists('role_assignments', array('userid' => $id))) {
         echo get_string('profilenotshown', 'moodle');
     } else {
@@ -221,6 +221,7 @@ if ($user->description && !isset($hiddenfields['description'])) {
         $options = array('overflowdiv' => true);
         echo format_text($user->description, $user->descriptionformat, $options);
     }
+    echo '</div>'; // Description class.
 }
 
 // Render custom blocks.
