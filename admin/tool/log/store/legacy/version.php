25717253d6a05ad3ef4a296cd5cffd327b123431
Dan Poltawski <dan@moodle.com>
2015-07-07 15:09:24 +0100

    MDL-50466 logstore: bump version to load new task definitions

--- admin/tool/log/store/legacy/version.php
+++ admin/tool/log/store/legacy/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015051100; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2015051101; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2015050500; // Requires this Moodle version.
 $plugin->component = 'logstore_legacy'; // Full name of the plugin (used for diagnostics).
