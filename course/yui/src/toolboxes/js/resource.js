70fb7ac4b35667675cdcdf5e08f6ea37cd10fed6
Neill Magill <neill.magill@nottingham.ac.uk>
2015-08-04 16:31:54 +0100

    MDL-50971 ActionMenu: Action menus cause page to jump when closing
    This patch changes the behavior of open action menus when using a mouse.
    
    Before this patch when clicking on the page with an open action menu the focus would be
    returned to the action menu's call button. This could cause the page to jump when a
    mouse user did not close an action menu, scrolled and then tried to perform another
    action on the page.
    
    After this patch when clicking the focus will not be returned to the action menu's call button.
    The behavior of the script will not be changed when using the keyboard.

--- course/yui/src/toolboxes/js/resource.js
+++ course/yui/src/toolboxes/js/resource.js
@@ -294,7 +294,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
             };
             this.send_request(data);
             if (M.core.actionmenu && M.core.actionmenu.instance) {
-                M.core.actionmenu.instance.hideMenu();
+                M.core.actionmenu.instance.hideMenu(ev);
             }
 
         }, this);
@@ -541,7 +541,7 @@ Y.extend(RESOURCETOOLBOX, TOOLBOX, {
 
         this.send_request(data, null, function(response) {
             if (M.core.actionmenu && M.core.actionmenu.instance) {
-                M.core.actionmenu.instance.hideMenu();
+                M.core.actionmenu.instance.hideMenu(ev);
             }
 
             // Try to retrieve the existing string from the server
