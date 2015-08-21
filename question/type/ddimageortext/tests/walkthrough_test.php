4a53044a10afdce33e066e2da98f541df94e2d73
Tim Hunt <T.J.Hunt@open.ac.uk>
2015-08-21 12:35:36 +0100

    MDL-47494 ddimageortext: Fix unit tests for 3.0 compatibility.

--- question/type/ddimageortext/tests/walkthrough_test.php
+++ question/type/ddimageortext/tests/walkthrough_test.php
@@ -132,8 +132,6 @@ class qtype_ddimageortext_walkthrough_test extends qbehaviour_walkthrough_test_b
             $this->get_contains_hidden_expectation(
                 $this->quba->get_field_prefix($this->slot) . 'p4', 1),
             $this->get_contains_try_again_button_expectation(true),
-            new question_pattern_expectation('/' .
-                preg_quote(get_string('notcomplete', 'qbehaviour_interactive'), '/') . '/'),
             $this->get_contains_hint_expectation('This is the first hint'));
 
         // Do try again.
@@ -474,8 +472,6 @@ class qtype_ddimageortext_walkthrough_test extends qbehaviour_walkthrough_test_b
             $this->get_contains_submit_button_expectation(false),
             $this->get_contains_try_again_button_expectation(true),
             $this->get_does_not_contain_correctness_expectation(),
-            new question_pattern_expectation('/' .
-                preg_quote(get_string('notcomplete', 'qbehaviour_interactive'), '/') . '/'),
             $this->get_contains_hint_expectation('This is the first hint'),
             $this->get_contains_num_parts_correct(2),
             $this->get_contains_standard_partiallycorrect_combined_feedback_expectation(),
@@ -537,8 +533,6 @@ class qtype_ddimageortext_walkthrough_test extends qbehaviour_walkthrough_test_b
             $this->get_contains_submit_button_expectation(false),
             $this->get_contains_try_again_button_expectation(true),
             $this->get_does_not_contain_correctness_expectation(),
-            new question_pattern_expectation('/' .
-                preg_quote(get_string('notcomplete', 'qbehaviour_interactive'), '/') . '/'),
             $this->get_contains_hint_expectation('This is the second hint'),
             $this->get_contains_num_parts_correct(2),
             $this->get_contains_standard_partiallycorrect_combined_feedback_expectation(),
