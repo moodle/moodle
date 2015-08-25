40a154551fcdf0b9ea906f4d1313df29754f1fa1
Adrian Greeve <adrian@moodle.com>
2015-08-25 12:57:43 +0800

    MDL-48371 editors: Option for removing managefiles
    We want to remove the managefiles button from the
    editors in the wiki so that students don't delete
    each others files.

--- lib/editor/atto/lib.php
+++ lib/editor/atto/lib.php
@@ -103,6 +103,11 @@ class atto_texteditor extends texteditor {
                     continue;
                 }
 
+                // Remove manage files if requested.
+                if ($plugin == 'managefiles' && isset($options['enable_filemanagement']) && !$options['enable_filemanagement']) {
+                    continue;
+                }
+
                 $jsplugin = array();
                 $jsplugin['name'] = $plugin;
                 $jsplugin['params'] = array();
