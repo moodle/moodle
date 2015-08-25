560c7d6afa48d1dd31c926592bf26ba87c972231
Andrew Davis <andrew@moodle.com>
2015-08-26 07:00:03 +0800

    MDL-51204 coding style fixes in /lang

--- lang/en/plagiarism.php
+++ lang/en/plagiarism.php
@@ -24,7 +24,7 @@
 
 $string['availableplugins'] = 'Available plugins';
 $string['configplagiarismplugins'] = 'Please choose the plagiarism plugin you would like to configure';
-$string['enableplagiarism'] ='Enable plagiarism plugins';
+$string['enableplagiarism'] = 'Enable plagiarism plugins';
 $string['configenableplagiarism'] = 'This will allow administrators to configure plagiarism plugins (if installed)';
 $string['manageplagiarism'] = 'Manage plagiarism plugins';
 $string['nopluginsinstalled'] = 'No plagiarism plugins are installed.';
