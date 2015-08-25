78de2e86e8506222cf49b1cc6dc58467750ae83d
Adrian Greeve <adrian@moodle.com>
2015-08-25 12:57:43 +0800

    MDL-48371 editors: Option for removing managefiles
    We want to remove the managefiles button from the
    editors in the wiki so that students don't delete
    each others files.

--- mod/wiki/pagelib.php
+++ mod/wiki/pagelib.php
@@ -381,7 +381,12 @@ class page_wiki_edit extends page_wiki {
     function __construct($wiki, $subwiki, $cm) {
         global $CFG, $PAGE;
         parent::__construct($wiki, $subwiki, $cm);
-        self::$attachmentoptions = array('subdirs' => false, 'maxfiles' => - 1, 'maxbytes' => $CFG->maxbytes, 'accepted_types' => '*');
+        $showfilemanager = false;
+        if (has_capability('mod/wiki:managefiles', context_module::instance($cm->id))) {
+            $showfilemanager = true;
+        }
+        self::$attachmentoptions = array('subdirs' => false, 'maxfiles' => - 1, 'maxbytes' => $CFG->maxbytes,
+                'accepted_types' => '*', 'enable_filemanagement' => $showfilemanager);
         $PAGE->requires->js_init_call('M.mod_wiki.renew_lock', null, true);
     }
 
