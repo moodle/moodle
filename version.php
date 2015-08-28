f563d2066d4fa76af87d29e0725bc2783783ec50
Dan Poltawski <dan@moodle.com>
2015-08-28 07:38:15 +0100

    weekly release 2.9.1+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2015051101.08;              // 20150511      = branching date YYYYMMDD - do not modify!
+$version  = 2015051101.09;              // 20150511      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '2.9.1+ (Build: 20150820)'; // Human-friendly version name
+$release  = '2.9.1+ (Build: 20150828)'; // Human-friendly version name
 
 $branch   = '29';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
