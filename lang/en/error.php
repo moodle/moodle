0e5bcf0a0b1118ab208f026eed7478e96ae65e6a
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2015-09-04 02:05:29 +0200

    MDL-51245 mssql: Diagnose missing READ_COMMITTED_SNAPSHOT mode
    Without it, transactions are executed in standard READ_COMMITED
    mode, without snapshoting/row versioning, leading to update conflicts
    under high concurrency.

--- lang/en/error.php
+++ lang/en/error.php
@@ -387,6 +387,7 @@ $string['movecatcontentstoroot'] = 'Moving the category content to root is not a
 $string['movecategorynotpossible'] = 'You cannot move category \'{$a}\' into the selected category.';
 $string['movecategoryownparent'] = 'You cannot make category \'{$a}\' a parent of itself.';
 $string['movecategoryparentconflict'] = 'You cannot make category \'{$a}\' a subcategory of one of its own subcategories.';
+$string['mssqlrcsmodemissing'] = 'The database is not using the expected READ_COMMITTED_SNAPSHOT mode which can lead to wrong results, especially under high concurrency scenarios. Please enable it for correct behaviour. You can find more information in the <a href="https://docs.moodle.org/en/Installing_MSSQL_for_PHP#Configuration">Moodle Docs</a>.';
 $string['multiplerecordsfound'] = 'Multiple records found, only one record expected.';
 $string['multiplerestorenotallow'] = 'Multiple restore execution not allowed!';
 $string['mustbeloggedin'] = 'You must be logged in to do this';
