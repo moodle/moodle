00a51457f4fe4195c4a3b2ffaacc0e9fc5b609e9
Pau Ferrer Ocaña <crazyserver@gmail.com>
2015-09-23 17:02:39 +0200

    MDL-50805 Messages: Add index to message_read to speed up message crons

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -4455,5 +4455,20 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2015051101.07);
     }
 
+    if ($oldversion < 2015051102.03) {
+
+        // Define index notification (not unique) to be added to message_read.
+        $table = new xmldb_table('message_read');
+        $index = new xmldb_index('notificationtimeread', XMLDB_INDEX_NOTUNIQUE, array('notification', 'timeread'));
+
+        // Conditionally launch add index notification.
+        if (!$dbman->index_exists($table, $index)) {
+            $dbman->add_index($table, $index);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2015051102.03);
+    }
+
     return true;
 }
