d5ded9e72d594e8f591dbb3d42c9fc20b56bfac6
Juan Leyva <juanleyvadelgado@gmail.com>
2015-09-30 14:17:08 +0200

    MDL-51615 mod_lti: New Web Service mod_lti_get_tool_launch_data

---
+++ mod/lti/classes/external.php
@@ -0,0 +1,125 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * External tool module external API
+ *
+ * @package    mod_lti
+ * @category   external
+ * @copyright  2015 Juan Leyva <juan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @since      Moodle 3.0
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->libdir . '/externallib.php');
+require_once($CFG->dirroot . '/mod/lti/lib.php');
+require_once($CFG->dirroot . '/mod/lti/locallib.php');
+
+/**
+ * External tool module external functions
+ *
+ * @package    mod_lti
+ * @category   external
+ * @copyright  2015 Juan Leyva <juan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @since      Moodle 3.0
+ */
+class mod_lti_external extends external_api {
+
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     * @since Moodle 3.0
+     */
+    public static function get_tool_launch_data_parameters() {
+        return new external_function_parameters(
+            array(
+                'toolid' => new external_value(PARAM_INT, 'external tool instance id')
+            )
+        );
+    }
+
+    /**
+     * Trigger the course module viewed event.
+     *
+     * @param int $toolid the external tool instance id
+     * @return array of warnings and launch data
+     * @since Moodle 3.0
+     * @throws moodle_exception
+     */
+    public static function get_tool_launch_data($toolid) {
+        global $DB, $CFG;
+        require_once($CFG->dirroot . '/mod/lti/lib.php');
+
+        $params = self::validate_parameters(self::get_tool_launch_data_parameters(),
+                                            array(
+                                                'toolid' => $toolid
+                                            ));
+        $warnings = array();
+
+        // Request and permission validation.
+        $lti = $DB->get_record('lti', array('id' => $params['toolid']), '*', MUST_EXIST);
+        list($course, $cm) = get_course_and_cm_from_instance($lti, 'lti');
+
+        $context = context_module::instance($cm->id);
+        self::validate_context($context);
+
+        require_capability('mod/lti:view', $context);
+
+        $lti->cmid = $cm->id;
+        list($endpoint, $parms) = lti_get_launch_data($lti);
+
+        $parameters = array();
+        foreach ($parms as $name => $value) {
+            $parameters[] = array(
+                'name' => $name,
+                'value' => $value
+            );
+        }
+
+        $result = array();
+        $result['endpoint'] = $endpoint;
+        $result['parameters'] = $parameters;
+        $result['warnings'] = $warnings;
+        return $result;
+    }
+
+    /**
+     * Returns description of method result value
+     *
+     * @return external_description
+     * @since Moodle 3.0
+     */
+    public static function get_tool_launch_data_returns() {
+        return new external_single_structure(
+            array(
+                'endpoint' => new external_value(PARAM_RAW, 'Endpoint URL'), // Using PARAM_RAW as is defined in the module.
+                'parameters' => new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'name' => new external_value(PARAM_NOTAGS, 'Parameter name'),
+                            'value' => new external_value(PARAM_RAW, 'Parameter value')
+                        )
+                    )
+                ),
+                'warnings' => new external_warnings()
+            )
+        );
+    }
+}
