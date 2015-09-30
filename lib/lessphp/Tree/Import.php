df098760820aefbd50afbe3570a529659d7316ec
Damyon Wiese <damyon@moodle.com>
2015-09-30 12:37:41 +0800

    MDL-51530 libraries: Upgrade less.php to 1.7.0.9

--- lib/lessphp/Tree/Import.php
+++ lib/lessphp/Tree/Import.php
@@ -232,6 +232,14 @@ class Less_Tree_Import extends Less_Tree{
 						return array( $full_path, $uri );
 					}
 				}elseif( !empty($rootpath) ){
+
+
+					if( $rooturi ){
+						if( strpos($evald_path,$rooturi) === 0 ){
+							$evald_path = substr( $evald_path, strlen($rooturi) );
+						}
+					}
+
 					$path = rtrim($rootpath,'/\\').'/'.ltrim($evald_path,'/\\');
 
 					if( file_exists($path) ){
--- lib/lessphp/moodle_readme.txt
+++ lib/lessphp/moodle_readme.txt
@@ -8,5 +8,7 @@ All the files from the folder lib/Less are copied in
 this directory. Only exception made for the directory
 '.easymin' which is not included.
 
+Also copy the license file from the project root.
+
 Licensed under the Apache license 2.0.
 
