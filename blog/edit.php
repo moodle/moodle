cefa583e71faefeaff97175761185a5587c478dd
Jun Pataleta <jun@moodle.com>
2015-10-12 15:47:57 -0500

    MDL-51395 blog: Fix output of strings for blog comments

--- blog/edit.php
+++ blog/edit.php
@@ -26,6 +26,7 @@
 require_once(dirname(dirname(__FILE__)).'/config.php');
 require_once('lib.php');
 require_once('locallib.php');
+require_once($CFG->dirroot .'/comment/lib.php');
 
 $action   = required_param('action', PARAM_ALPHA);
 $id       = optional_param('entryid', 0, PARAM_INT);
@@ -115,6 +116,9 @@ $output = $PAGE->get_renderer('blog');
 $strblogs = get_string('blogs', 'blog');
 
 if ($action === 'delete') {
+    // Init comment JS strings.
+    comment::init();
+
     if (empty($entry->id)) {
         print_error('wrongentryid', 'blog');
     }
