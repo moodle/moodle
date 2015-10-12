81e3251512b79a8a8368626eb3b786d196249bf9
Adrian Greeve <adrian@moodle.com>
2015-10-12 16:43:22 +0800

    MDL-51552 gradebook: Single view bulk insert fix.
    Single view now doesn't overwrite non-empty grades.
    
    A thank you to Sam Anderson and Robert Russo for providing a patch.

--- grade/report/singleview/classes/local/screen/grade.php
+++ grade/report/singleview/classes/local/screen/grade.php
@@ -333,19 +333,13 @@ class grade extends tablelike implements selectable_items, filterable_items {
 
                     $data->$field = empty($grade) ? $null : $grade->finalgrade;
                     $data->{"old$field"} = $data->$field;
-
-                    preg_match('/_(\d+)_(\d+)/', $field, $oldoverride);
-                    $oldoverride = 'oldoverride' . $oldoverride[0];
-                    if (empty($data->$oldoverride)) {
-                        $data->$field = (!isset($grade->rawgrade)) ? $null : $grade->rawgrade;
-                    }
                 }
             }
 
             foreach ($data as $varname => $value) {
                 if (preg_match('/^oldoverride_(\d+)_(\d+)/', $varname, $matches)) {
-                    // If we've selected override or overriding all grades.
-                    if (!empty($data->$matches[0]) || $filter == 'all') {
+                    // If we've selected overriding all grades.
+                    if ($filter == 'all') {
                         $override = "override_{$matches[1]}_{$matches[2]}";
                         $data->$override = '1';
                     }
