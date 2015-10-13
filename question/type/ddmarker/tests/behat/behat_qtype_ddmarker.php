e3b03886d08e5f460aeb0cf2c4da92a35df3773e
Dan Poltawski <dan@moodle.com>
2015-10-13 09:53:38 +0100

    MDL-47494 copyright: missed fixes from last commit
    With thanks to Tim!

--- question/type/ddmarker/tests/behat/behat_qtype_ddmarker.php
+++ question/type/ddmarker/tests/behat/behat_qtype_ddmarker.php
@@ -1,18 +1,18 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
 //
-// Stack is free software: you can redistribute it and/or modify
+// Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Stack is distributed in the hope that it will be useful,
+// Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Stack.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Behat steps definitions for drag and drop markers.
