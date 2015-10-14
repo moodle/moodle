a56ffba6689a9b421ffafbcdfd1ec6d64446cab5
Juan Leyva <juanleyvadelgado@gmail.com>
2015-10-14 14:11:50 +0200

    MDL-51772 webservice: Export add_user_private_files as Web Service

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2015051102.04;              // 20150511      = branching date YYYYMMDD - do not modify!
+$version  = 2015051102.05;              // 20150511      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
