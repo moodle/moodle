d41fa94a69bebeca69a4cd5332bb9569cfd87b99
David Monllao <davidm@moodle.com>
2015-10-22 10:08:13 +0800

    MDL-51684 badges: Make sure 'moodle/badges:viewbadges' is respected
    Credit to Yuliya Bozhko.

--- badges/view.php
+++ badges/view.php
@@ -75,6 +75,8 @@ if ($type == BADGE_TYPE_SITE) {
     $PAGE->set_heading($title);
 }
 
+require_capability('moodle/badges:viewbadges', $PAGE->context);
+
 $PAGE->set_title($title);
 $output = $PAGE->get_renderer('core', 'badges');
 
