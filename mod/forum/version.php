ba7fcebb793dc02504fa39a38e388403c9d71e9b
Andrew Nicols <andrew@nicols.co.uk>
2015-10-28 11:55:16 +0800

    MDL-51939 mod_forum: Correct empty groupinfo check

--- mod/forum/version.php
+++ mod/forum/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014111001;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2014111002;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014110400;       // Requires this Moodle version
 $plugin->component = 'mod_forum';      // Full name of the plugin (used for diagnostics)
