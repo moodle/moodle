da6ecfd8c17b8c43941257cdd8b0af542b2b0dab
Helen Foster <helen@moodle.org>
2015-11-02 22:12:06 +0100

    MDL-52018 lang: Merge English strings from the en_fix language pack

--- question/type/ddwtos/lang/en/qtype_ddwtos.php
+++ question/type/ddwtos/lang/en/qtype_ddwtos.php
@@ -25,11 +25,11 @@
 $string['addmorechoiceblanks'] = 'Blanks for {no} more choices';
 $string['answer'] = 'Answer';
 $string['correctansweris'] = 'The correct answer is: {$a}';
-$string['infinite'] = 'Infinite';
+$string['infinite'] = 'Unlimited';
 $string['pleaseputananswerineachbox'] = 'Please put an answer in each box.';
 $string['pluginname'] = 'Drag and drop into text';
-$string['pluginname_help'] = 'Type in some question text like "The [[1]] jumped over the [[2]]", then enter the possible words to go in gaps 1 and 2 underneath.';
+$string['pluginname_help'] = 'Drag and drop into text questions require the respondent to drag words or phrases into the correct gaps in the question text. [[1]], [[2]], [[3]], ... are used as placeholders in the question text, with the correct answers specified as choice answers 1, 2, 3, ... respectively. Choice answers may be grouped so that all answers in a particular group are coloured the same as the gaps for them in the question text. Choice answers marked as unlimited may be used in more than one gap.';
 $string['pluginname_link'] = 'question/type/ddwtos';
 $string['pluginnameadding'] = 'Adding a drag and drop into text';
 $string['pluginnameediting'] = 'Editing a drag and drop into text';
-$string['pluginnamesummary'] = 'Missing words in some text are filled in using drag-and-drop.';
+$string['pluginnamesummary'] = 'Missing words in the question text are filled in using drag and drop.';
