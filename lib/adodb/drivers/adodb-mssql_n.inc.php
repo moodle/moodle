73bb7c09905ea2840c1ad8c46feee17245e248b4
Marina Glancy <marina@moodle.com>
2015-12-08 11:52:40 +0800

    MDL-52286 core: update ADODB library to 5.21

--- lib/adodb/drivers/adodb-mssql_n.inc.php
+++ lib/adodb/drivers/adodb-mssql_n.inc.php
@@ -64,107 +64,191 @@ class ADODB_mssql_n extends ADODB_mssql {
 		return ADODB_mssql::_query($sql,$inputarr);
 	}
 
-    /**
+         /**
      * This function will intercept all the literals used in the SQL, prepending the "N" char to them
      * in order to allow mssql to store properly data sent in the correct UCS-2 encoding (by freeTDS
      * and ODBTP) keeping SQL compatibility at ADOdb level (instead of hacking every project to add
      * the "N" notation when working against MSSQL.
      *
-     * Note that this hack only must be used if ALL the char-based columns in your DB are of type nchar,
-     * nvarchar and ntext
+     * The orginal note indicated that this hack should only be used if ALL the char-based columns 
+     * in your DB are of type nchar, nvarchar and ntext, but testing seems to indicate that SQL server
+     * doesn't seem to care if the statement is used against char etc fields.
+     *
+     * @todo This function should raise an ADOdb error if one of the transformations fail
+     * 
+     * @param mixed $inboundData Either a string containing an SQL statement
+     *                           or an array with resources from prepared statements
+     *
+     * @return mixed
      */
-    function _appendN($sql) {
+    function _appendN($inboundData) {
 
-        $result = $sql;
+        $inboundIsArray  = false;
+       
+        if (is_array($inboundData))
+        {
+            $inboundIsArray = true;
+            $inboundArray   = $inboundData;
+        } else
+            $inboundArray = (array)$inboundData;
+        
+        /*
+         * All changes will be placed here
+         */
+        $outboundArray = $inboundArray;
+        
+        foreach($inboundArray as $inboundKey=>$inboundValue)
+        {
+        
+            if (is_resource($inboundValue))
+            {
+                /*
+                * Prepared statement resource
+                */
+                if ($this->debug)
+                    ADOConnection::outp("{$this->databaseType} index $inboundKey value is resource, continue");
 
-    /// Check we have some single quote in the query. Exit ok.
-        if (strpos($sql, SINGLEQUOTE) === false) {
-            return $sql;
-        }
-
-    /// Check we haven't an odd number of single quotes (this can cause problems below
-    /// and should be considered one wrong SQL). Exit with debug info.
-        if ((substr_count($sql, SINGLEQUOTE) & 1)) {
-            if ($this->debug) {
-                ADOConnection::outp("{$this->databaseType} internal transformation: not converted. Wrong number of quotes (odd)");
+                continue;
             }
-            return $sql;
-        }
-
-    /// Check we haven't any backslash + single quote combination. It should mean wrong
-    /// backslashes use (bad magic_quotes_sybase?). Exit with debug info.
-        $regexp = '/(\\\\' . SINGLEQUOTE . '[^' . SINGLEQUOTE . '])/';
-        if (preg_match($regexp, $sql)) {
-            if ($this->debug) {
-                ADOConnection::outp("{$this->databaseType} internal transformation: not converted. Found bad use of backslash + single quote");
+           
+            if (strpos($inboundValue, SINGLEQUOTE) === false)
+            {
+                /*
+                * Check we have something to manipulate
+                */
+                if ($this->debug)
+                    ADOConnection::outp("{$this->databaseType} index $inboundKey value $inboundValue has no single quotes, continue");
+                continue;
             }
-            return $sql;
-        }
 
-    /// Remove pairs of single-quotes
-        $pairs = array();
-        $regexp = '/(' . SINGLEQUOTE . SINGLEQUOTE . ')/';
-        preg_match_all($regexp, $result, $list_of_pairs);
-        if ($list_of_pairs) {
-            foreach (array_unique($list_of_pairs[0]) as $key=>$value) {
-                $pairs['<@#@#@PAIR-'.$key.'@#@#@>'] = $value;
+            /*
+            * Check we haven't an odd number of single quotes (this can cause problems below
+            * and should be considered one wrong SQL). Exit with debug info.
+            */
+            if ((substr_count($inboundValue, SINGLEQUOTE) & 1)) 
+            {
+                if ($this->debug)
+                    ADOConnection::outp("{$this->databaseType} internal transformation: not converted. Wrong number of quotes (odd)");
+               
+                break;
             }
-            if (!empty($pairs)) {
-                $result = str_replace($pairs, array_keys($pairs), $result);
-            }
-        }
 
-    /// Remove the rest of literals present in the query
-        $literals = array();
-        $regexp = '/(N?' . SINGLEQUOTE . '.*?' . SINGLEQUOTE . ')/is';
-        preg_match_all($regexp, $result, $list_of_literals);
-        if ($list_of_literals) {
-            foreach (array_unique($list_of_literals[0]) as $key=>$value) {
-                $literals['<#@#@#LITERAL-'.$key.'#@#@#>'] = $value;
+            /*
+            * Check we haven't any backslash + single quote combination. It should mean wrong
+            *  backslashes use (bad magic_quotes_sybase?). Exit with debug info.
+            */
+            $regexp = '/(\\\\' . SINGLEQUOTE . '[^' . SINGLEQUOTE . '])/';
+            if (preg_match($regexp, $inboundValue))
+            {
+                if ($this->debug) 
+                    ADOConnection::outp("{$this->databaseType} internal transformation: not converted. Found bad use of backslash + single quote");
+                
+                break;
             }
-            if (!empty($literals)) {
-                $result = str_replace($literals, array_keys($literals), $result);
+
+            /*
+            * Remove pairs of single-quotes
+            */
+            $pairs = array();
+            $regexp = '/(' . SINGLEQUOTE . SINGLEQUOTE . ')/';
+            preg_match_all($regexp, $inboundValue, $list_of_pairs);
+            
+            if ($list_of_pairs)
+            {
+                foreach (array_unique($list_of_pairs[0]) as $key=>$value)
+                    $pairs['<@#@#@PAIR-'.$key.'@#@#@>'] = $value;
+                
+                
+                if (!empty($pairs))
+                    $inboundValue = str_replace($pairs, array_keys($pairs), $inboundValue);
+                
             }
-        }
 
+            /*
+            * Remove the rest of literals present in the query
+            */
+            $literals = array();
+            $regexp = '/(N?' . SINGLEQUOTE . '.*?' . SINGLEQUOTE . ')/is';
+            preg_match_all($regexp, $inboundValue, $list_of_literals);
+           
+           if ($list_of_literals)
+           {
+                foreach (array_unique($list_of_literals[0]) as $key=>$value)
+                    $literals['<#@#@#LITERAL-'.$key.'#@#@#>'] = $value;
+                
+               
+                if (!empty($literals))
+                    $inboundValue = str_replace($literals, array_keys($literals), $inboundValue);
+            }
 
-    /// Analyse literals to prepend the N char to them if their contents aren't numeric
-        if (!empty($literals)) {
-            foreach ($literals as $key=>$value) {
-                if (!is_numeric(trim($value, SINGLEQUOTE))) {
-                /// Non numeric string, prepend our dear N
-                    $literals[$key] = 'N' . trim($value, 'N'); //Trimming potentially existing previous "N"
+            /*
+            * Analyse literals to prepend the N char to them if their contents aren't numeric
+            */
+            if (!empty($literals))
+            {
+                foreach ($literals as $key=>$value) {
+                    if (!is_numeric(trim($value, SINGLEQUOTE)))
+                        /*
+                        * Non numeric string, prepend our dear N, whilst 
+                        * Trimming potentially existing previous "N"
+                        */
+                        $literals[$key] = 'N' . trim($value, 'N'); 
+                    
                 }
             }
+
+            /*
+            * Re-apply literals to the text
+            */
+            if (!empty($literals))
+                $inboundValue = str_replace(array_keys($literals), $literals, $inboundValue);
+            
+
+            /*
+            * Any pairs followed by N' must be switched to N' followed by those pairs
+            * (or strings beginning with single quotes will fail)
+            */
+            $inboundValue = preg_replace("/((<@#@#@PAIR-(\d+)@#@#@>)+)N'/", "N'$1", $inboundValue);
+
+            /*
+            * Re-apply pairs of single-quotes to the text
+            */
+            if (!empty($pairs))
+                $inboundValue = str_replace(array_keys($pairs), $pairs, $inboundValue);
+            
+
+            /*
+            * Print transformation if debug = on
+            */
+            if (strcmp($inboundValue,$inboundArray[$inboundKey]) <> 0 && $this->debug)
+                ADOConnection::outp("{$this->databaseType} internal transformation: {$inboundArray[$inboundKey]} to {$inboundValue}");
+            
+            if (strcmp($inboundValue,$inboundArray[$inboundKey]) <> 0)
+                /*
+                * Place the transformed value into the outbound array
+                */
+                $outboundArray[$inboundKey] = $inboundValue;
         }
-
-    /// Re-apply literals to the text
-        if (!empty($literals)) {
-            $result = str_replace(array_keys($literals), $literals, $result);
-        }
-
-    /// Any pairs followed by N' must be switched to N' followed by those pairs
-    /// (or strings beginning with single quotes will fail)
-        $result = preg_replace("/((<@#@#@PAIR-(\d+)@#@#@>)+)N'/", "N'$1", $result);
-
-    /// Re-apply pairs of single-quotes to the text
-        if (!empty($pairs)) {
-            $result = str_replace(array_keys($pairs), $pairs, $result);
-        }
-
-    /// Print transformation if debug = on
-        if ($result != $sql && $this->debug) {
-            ADOConnection::outp("{$this->databaseType} internal transformation:<br>{$sql}<br>to<br>{$result}");
-        }
-
-        return $result;
+        
+        /*
+         * Any transformations are in the $outboundArray
+         */
+        if ($inboundIsArray)
+            return $outboundArray;
+        
+        /*
+         * We passed a string in originally
+         */
+        return $outboundArray[0];
+        
     }
+
 }
 
 class ADORecordset_mssql_n extends ADORecordset_mssql {
 	var $databaseType = "mssql_n";
-	function ADORecordset_mssql_n($id,$mode=false)
+	function __construct($id,$mode=false)
 	{
-		$this->ADORecordset_mssql($id,$mode);
+		parent::__construct($id,$mode);
 	}
 }
