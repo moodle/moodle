692ef1010b4a17afcc46071410c0c66d183ddc63
Amanda Doughty <amanda.doughty.1@city.ac.uk>
2015-12-13 20:43:26 +0000

    MDL-52455 quiz: doesn't include all user fields required by messaging

--- mod/quiz/locallib.php
+++ mod/quiz/locallib.php
@@ -1550,7 +1550,8 @@ function quiz_send_notification_messages($course, $quiz, $attempt, $context, $cm
     }
 
     // Check for notifications required.
-    $notifyfields = 'u.id, u.username, u.idnumber, u.email, u.emailstop, u.lang, u.timezone, u.mailformat, u.maildisplay, ';
+    $notifyfields = 'u.id, u.username, u.idnumber, u.email, u.emailstop, u.lang,
+            u.timezone, u.mailformat, u.maildisplay, u.auth, u.suspended, u.deleted, ';
     $notifyfields .= get_all_user_name_fields(true, 'u');
     $groups = groups_get_all_groups($course->id, $submitter->id, $cm->groupingid);
     if (is_array($groups) && count($groups) > 0) {
