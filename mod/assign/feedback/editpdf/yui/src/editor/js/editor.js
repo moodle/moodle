0d5a260ecd37c15fff56ea8116867a77d9b500c7
Davo Smith <git@davosmith.co.uk>
2016-01-04 11:51:08 +0000

    MDL-52290 assignfeedback_editpdf: show number of pages in page select

--- mod/assign/feedback/editpdf/yui/src/editor/js/editor.js
+++ mod/assign/feedback/editpdf/yui/src/editor/js/editor.js
@@ -1035,6 +1035,7 @@ EDITOR.prototype = {
     setup_navigation : function() {
         var pageselect,
             i,
+            strinfo,
             option,
             previousbutton,
             nextbutton;
@@ -1046,7 +1047,8 @@ EDITOR.prototype = {
             for (i = 0; i < this.pages.length; i++) {
                 option = Y.Node.create('<option/>');
                 option.setAttribute('value', i);
-                option.setHTML(M.util.get_string('pagenumber', 'assignfeedback_editpdf', i+1));
+                strinfo = {page: i+1, total: this.pages.length};
+                option.setHTML(M.util.get_string('pagexofy', 'assignfeedback_editpdf', strinfo));
                 pageselect.append(option);
             }
         }
