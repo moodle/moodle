60d47c217090b0f4a9a75bfc5c43a04d06ef83b2
Dan Poltawski <dan@moodle.com>
2016-01-12 10:47:30 +0000

    MDL-52656 mod_forum: remove $SSSION test hack

--- mod/forum/tests/lib_test.php
+++ mod/forum/tests/lib_test.php
@@ -2144,7 +2144,6 @@ class mod_forum_lib_testcase extends advanced_testcase {
     }
 
     public function test_print_overview_groups() {
-        global $SESSION;
         $this->resetAfterTest();
         $course1 = self::getDataGenerator()->create_course();
         $group1 = $this->getDataGenerator()->create_group(array('courseid' => $course1->id));
@@ -2193,10 +2192,6 @@ class mod_forum_lib_testcase extends advanced_testcase {
         // There should be one entry for a forum in course1.
         $this->assertCount(1, $results[$course1->id]);
         $this->assertArrayHasKey('forum', $results[$course1->id]);
-
-        // As viewer2 (different group to post).
-        // Temporary hack to workaround for MDL-52656.
-        $SESSION->currentgroup = null;
 
         $this->setUser($viewer2->id);
         $results = array();
@@ -2264,7 +2259,6 @@ class mod_forum_lib_testcase extends advanced_testcase {
      * @dataProvider print_overview_timed_provider
      */
     public function test_print_overview_timed_groups($config, $hasresult) {
-        global $SESSION;
         $this->resetAfterTest();
         $course1 = self::getDataGenerator()->create_course();
         $group1 = $this->getDataGenerator()->create_group(array('courseid' => $course1->id));
@@ -2324,10 +2318,6 @@ class mod_forum_lib_testcase extends advanced_testcase {
             // There should be no entries for any course.
             $this->assertCount(0, $results);
         }
-
-        // As viewer2 (different group to post).
-        // Temporary hack to workaround for MDL-52656.
-        $SESSION->currentgroup = null;
 
         $this->setUser($viewer2->id);
         $results = array();
