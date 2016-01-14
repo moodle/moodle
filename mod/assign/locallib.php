3eb931d3650f92853837dffbb5fc3a7120936f11
Greg Faller <greg.faller@flinders.edu.au>
2016-01-14 11:41:15 +1030

    MDL-50083 mod_assign: unlock submission when adding attempt

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -7200,6 +7200,10 @@ class assign {
         }
 
         $this->update_submission($newsubmission, $userid, false, $this->get_instance()->teamsubmission);
+        $flags = $this->get_user_flags($userid, false);
+        if ($flags->locked) {
+            $this->process_unlock_submission($userid);
+        }
         return true;
     }
 
