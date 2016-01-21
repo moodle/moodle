c10119a059ce5cd1f9b6c468ac63df7b4a803a23
Rushikesh <rushikeshnalla9@gmail.com>
2016-01-21 19:24:47 +0530

    MDL-25864 Blog: Confirmation dialog before unregistering

--- blog/external_blogs.php
+++ blog/external_blogs.php
@@ -94,14 +94,16 @@ if (!empty($blogs)) {
         $editurl = new moodle_url('/blog/external_blog_edit.php', array('id' => $blog->id));
         $editicon = $OUTPUT->action_icon($editurl, new pix_icon('t/edit', get_string('editexternalblog', 'blog')));
 
-        $deletelink = new moodle_url('/blog/external_blogs.php', array('delete' => $blog->id, 'sesskey'=>sesskey()));
-        $deleteicon = $OUTPUT->action_icon($deletelink, new pix_icon('t/delete', get_string('deleteexternalblog', 'blog')));
+        $deletelink = new moodle_url('/blog/external_blogs.php', array('delete' => $blog->id, 'sesskey' => sesskey()));
+        $action = new confirm_action(get_string('externalblogdeleteconfirm', 'blog'));
+        $deleteicon = $OUTPUT->action_icon($deletelink, new pix_icon('t/delete', get_string('deleteexternalblog', 'blog')),
+                                           $action);
 
         $table->data[] = new html_table_row(array($blog->name,
                                                   $blog->url,
                                                   userdate($blog->timefetched),
                                                   $validicon,
-                                                  $editicon . '&nbsp'. $deleteicon));
+                                                  $editicon . $deleteicon));
     }
     echo html_writer::table($table);
 }
