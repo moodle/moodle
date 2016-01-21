aa0b28c5e570c6c276d2421550fdcb9ab17703c6
Andrew Nicols <andrew@nicols.co.uk>
2016-01-21 13:43:41 +0800

    weekly release 2.9.4+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2015051104.02;              // 20150511      = branching date YYYYMMDD - do not modify!
+$version  = 2015051104.03;              // 20150511      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '2.9.4+ (Build: 20160114)'; // Human-friendly version name
+$release  = '2.9.4+ (Build: 20160121)'; // Human-friendly version name
 
 $branch   = '29';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
