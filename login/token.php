51d2434cfede4d1623c00f3f3a4534988ac053ec
Juan Leyva <juanleyvadelgado@gmail.com>
2016-01-27 10:26:04 +0100

    MDL-52902 webservice: Send CORS header before params validation

--- login/token.php
+++ login/token.php
@@ -27,12 +27,13 @@ define('NO_MOODLE_COOKIES', true);
 
 require_once(dirname(dirname(__FILE__)) . '/config.php');
 
+// Allow CORS requests.
+header('Access-Control-Allow-Origin: *');
+
 $username = required_param('username', PARAM_USERNAME);
 $password = required_param('password', PARAM_RAW);
 $serviceshortname  = required_param('service',  PARAM_ALPHANUMEXT);
 
-// Allow CORS requests.
-header('Access-Control-Allow-Origin: *');
 echo $OUTPUT->header();
 
 if (!$CFG->enablewebservices) {
