5a906c572782aad8ebc728925414c9687129d058
Dan Poltawski <dan@moodle.com>
2016-02-02 12:00:24 +0000

    MDL-50115 grunt: add missing jshint configuration
    The gruntfile isn't a normal browser environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/* jshint node: true, browser: false */
 
 /**
  * @copyright  2014 Andrew Nicols
