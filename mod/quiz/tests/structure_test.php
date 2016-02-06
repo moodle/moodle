93ea68f50d5ea7a75c2a6c4f1e26e15dc4d56c00
Tim Hunt <T.J.Hunt@open.ac.uk>
2016-02-06 10:13:28 +0000

    MDL-52879 quiz editing: allow removal of the last question in a quiz
    Even though you are not normally allowed to remove the last question
    in a section.

--- mod/quiz/tests/structure_test.php
+++ mod/quiz/tests/structure_test.php
@@ -153,10 +153,10 @@ class mod_quiz_structure_testcase extends advanced_testcase {
             } else {
                 list($name, $page, $qtype) = $item;
                 $question = $structure->get_question_in_slot($slot);
-                $this->assertEquals($slot,  $question->slot);
                 $this->assertEquals($name,  $question->name);
-                $this->assertEquals($qtype, $question->qtype);
-                $this->assertEquals($page,  $question->page);
+                $this->assertEquals($slot,  $question->slot,  'Slot number wrong for question ' . $name);
+                $this->assertEquals($qtype, $question->qtype, 'Question type wrong for question ' . $name);
+                $this->assertEquals($page,  $question->page,  'Page number wrong for question ' . $name);
 
                 $slot += 1;
             }
@@ -624,6 +624,29 @@ class mod_quiz_structure_testcase extends advanced_testcase {
         $structure->remove_slot(3);
     }
 
+    public function test_can_remove_last_question_in_a_quiz() {
+        $quizobj = $this->create_test_quiz(array(
+                'Heading 1',
+                array('TF1', 1, 'truefalse'),
+            ));
+        $structure = \mod_quiz\structure::create_for_quiz($quizobj);
+
+        $structure->remove_slot(1);
+
+        $questiongenerator = $this->getDataGenerator()->get_plugin_generator('core_question');
+        $cat = $questiongenerator->create_question_category();
+        $q = $questiongenerator->create_question('truefalse', null,
+                array('name' => 'TF2', 'category' => $cat->id));
+
+        quiz_add_quiz_question($q->id, $quizobj->get_quiz(), 0);
+        $structure = \mod_quiz\structure::create_for_quiz($quizobj);
+
+        $this->assert_quiz_layout(array(
+                'Heading 1',
+                array('TF2', 1, 'truefalse'),
+        ), $structure);
+    }
+
     public function test_add_question_updates_headings() {
         $quizobj = $this->create_test_quiz(array(
                 array('TF1', 1, 'truefalse'),
@@ -648,7 +671,7 @@ class mod_quiz_structure_testcase extends advanced_testcase {
         ), $structure);
     }
 
-    public function test_add_question_and_end_does_not_update_headings() {
+    public function test_add_question_at_end_does_not_update_headings() {
         $quizobj = $this->create_test_quiz(array(
                 array('TF1', 1, 'truefalse'),
                 'Heading 2',
