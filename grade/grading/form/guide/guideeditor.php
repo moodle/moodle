d94df7afa8974c29c21c78fd955d7a7fd33329f0
David Monllao <davidm@moodle.com>
2016-03-08 15:31:17 +0800

    MDL-41648 gradingforms: Remove unnecessary attr

--- grade/grading/form/guide/guideeditor.php
+++ grade/grading/form/guide/guideeditor.php
@@ -137,7 +137,7 @@ class moodlequickform_guideeditor extends HTML_QuickForm_input {
             $html .= $renderer->display_regrade_confirmation($this->getName(), $this->regradeconfirmation, $data['regrade']);
         }
         if ($this->validationerrors) {
-            $html .= html_writer::div($renderer->notification($this->validationerrors), '', array('role' => 'alert'));
+            $html .= html_writer::div($renderer->notification($this->validationerrors));
         }
         $html .= $renderer->display_guide($data['criteria'], $data['comments'], $data['options'], $mode, $this->getName());
         return $html;
