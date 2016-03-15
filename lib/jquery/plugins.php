f0c0c1ea4b80fa9065be4b7d5b31eb896f656aea
Simey Lameze <simey@moodle.com>
2016-03-15 09:05:55 +0800

    MDL-53454 libraries: upgrade jquery to 1.12.1 version

--- lib/jquery/plugins.php
+++ lib/jquery/plugins.php
@@ -37,7 +37,7 @@
  */
 
 $plugins = array(
-    'jquery'  => array('files' => array('jquery-1.11.3.min.js')),
+    'jquery'  => array('files' => array('jquery-1.12.1.min.js')),
     'migrate' => array('files' => array('jquery-migrate-1.2.1.min.js')),
     'ui'      => array('files' => array('ui-1.11.4/jquery-ui.min.js')),
     'ui-css'  => array('files' => array('ui-1.11.4/theme/smoothness/jquery-ui.min.css')),
