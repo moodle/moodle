1f8ab584e933115e8f8f1fd488290d310dd141f9
Adrian Greeve <adrian@moodle.com>
2016-03-15 10:58:14 +0800

    MDL-52752 filters: Update MathJax to version 2.6

--- filter/mathjaxloader/version.php
+++ filter/mathjaxloader/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015111600;
+$plugin->version  = 2015111601;
 $plugin->requires = 2015111000;  // Requires this Moodle version.
 $plugin->component= 'filter_mathjaxloader';
