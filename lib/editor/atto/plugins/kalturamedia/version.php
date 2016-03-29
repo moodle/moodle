357a7daa6af248ceee890446674db8f1b3af54ae
Muli Dayan <muli.dayan@kaltura.com>
2016-03-29 15:08:16 +0300

    KMS-10965 increment version

--- lib/editor/atto/plugins/kalturamedia/version.php
+++ lib/editor/atto/plugins/kalturamedia/version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015120930;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release = 'Kaltura release 4.0.08';
+$plugin->version   = 2016032930;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = 'Kaltura release 4.0.09';
 $plugin->requires  = 2014050800;        // Requires this Moodle version.
 $plugin->component = 'atto_kalturamedia';  // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = array(
-    'local_kaltura' => 2015120930
+    'local_kaltura' => 2016032930
 );
--- local/kaltura/version.php
+++ local/kaltura/version.php
@@ -25,8 +25,8 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-$plugin->version = 2015120930;
+$plugin->version = 2016032930;
 $plugin->component = 'local_kaltura';
-$plugin->release = 'Kaltura release 4.0.08';
+$plugin->release = 'Kaltura release 4.0.09';
 $plugin->requires = 2014050800;
 $plugin->maturity = MATURITY_STABLE;
