62e20cfed624ca645edfb6fd78ffe14e769e0f93
Dani Palou <dani@moodle.com>
2016-04-04 15:57:05 +0200

    MDL-53703 wiki: New WS mod_wiki_get_subwiki_files

--- mod/wiki/db/services.php
+++ mod/wiki/db/services.php
@@ -71,6 +71,15 @@ $functions = array(
         'services'      => array(MOODLE_OFFICIAL_MOBILE_SERVICE)
     ),
 
+    'mod_wiki_get_subwiki_files' => array(
+        'classname'     => 'mod_wiki_external',
+        'methodname'    => 'get_subwiki_files',
+        'description'   => 'Returns the list of files for a specific subwiki.',
+        'type'          => 'read',
+        'capabilities'  => 'mod/wiki:viewpage',
+        'services'      => array(MOODLE_OFFICIAL_MOBILE_SERVICE)
+    ),
+
     'mod_wiki_get_page_contents' => array(
         'classname'     => 'mod_wiki_external',
         'methodname'    => 'get_page_contents',
