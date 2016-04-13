e9024a144395a0375f44816f7e4d4677a03ee505
Eric Merrill <merrill@oakland.edu>
2016-04-12 22:26:35 -0400

    MDL-53795 logging: Add DB index for use when backing up logs

--- admin/tool/log/store/standard/version.php
+++ admin/tool/log/store/standard/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015111600; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2015111601; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2015111000; // Requires this Moodle version.
 $plugin->component = 'logstore_standard'; // Full name of the plugin (used for diagnostics).
