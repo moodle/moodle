8f62c066ad067cd5a572d29a1f7876fa47f57a18
Skylar Kelty <S.Kelty@kent.ac.uk>
2016-04-26 08:35:00 +0100

    MDL-53947 memcached: Fix memcached session warning on PHP7

--- lib/classes/session/memcached.php
+++ lib/classes/session/memcached.php
@@ -136,7 +136,12 @@ class memcached extends handler {
         ini_set('memcached.sess_locking', '1'); // Locking is required!
 
         // Try to configure lock and expire timeouts - ignored if memcached is before version 2.2.0.
-        ini_set('memcached.sess_lock_max_wait', $this->acquiretimeout);
+        if (version_compare($version, '3.0.0-dev') >= 0) {
+            ini_set('memcached.sess_lock_wait_max', $this->acquiretimeout * 1000);
+        } else {
+            ini_set('memcached.sess_lock_max_wait', $this->acquiretimeout);
+        }
+
         ini_set('memcached.sess_lock_expire', $this->lockexpire);
     }
 
