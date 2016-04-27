76c684dc65ba44ee4bca8982e31e1a169849e635
Marina Glancy <marina@moodle.com>
2016-04-27 15:55:30 +0800

    MDL-53904 course: apply default completion when approving request

--- course/lib.php
+++ course/lib.php
@@ -3107,6 +3107,7 @@ class course_request {
         $data->visible            = $courseconfig->visible;
         $data->visibleold         = $data->visible;
         $data->lang               = $courseconfig->lang;
+        $data->enablecompletion   = $courseconfig->enablecompletion;
 
         $course = create_course($data);
         $context = context_course::instance($course->id, MUST_EXIST);
