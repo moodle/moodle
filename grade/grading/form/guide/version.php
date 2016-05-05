6cb33703ba7376109e5363445f95ab162eb3ff5f
Jun Pataleta <jun@moodle.com>
2016-05-05 09:49:16 +0800

    MDL-54055 gradingform_guide: Skip insertion of empty comments
    We should not be adding empty entries for the frequently-used comments.

--- grade/grading/form/guide/version.php
+++ grade/grading/form/guide/version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'gradingform_guide';
-$plugin->version    = 2015111600;
+$plugin->version    = 2015111601;
 $plugin->requires   = 2015111000;
 $plugin->maturity   = MATURITY_STABLE;
