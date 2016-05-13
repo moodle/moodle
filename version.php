6f8adf29fd4d5eeb79f9e8c0533ea61a8b8be863
David Monllao <davidm@moodle.com>
2016-05-13 18:16:30 +0800

    NOBUG: ensure build number doesn't go backwards
    (Due to reverts happening in MDL-45390 and MDL-52815)

--- version.php
+++ version.php
@@ -33,7 +33,7 @@ $version  = 2016051300.00;              // YYYYMMDD      = weekly release date o
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.1beta+ (Build: 20160510)'; // Human-friendly version name
+$release  = '3.1beta+ (Build: 20160513)'; // Human-friendly version name
 
 $branch   = '31';                       // This version's branch.
 $maturity = MATURITY_BETA;             // This version's maturity level.
