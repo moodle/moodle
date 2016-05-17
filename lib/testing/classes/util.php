13708875d399151f483bd4d1b4ef7fad4eb09dc6
Rajesh Taneja <rajesh@moodle.com>
2016-05-17 13:11:01 +0800

    MDL-54573 testing: don't uppercase dbtype

--- lib/testing/classes/util.php
+++ lib/testing/classes/util.php
@@ -1106,6 +1106,7 @@ abstract class testing_util {
         $dbtype = $CFG->dbtype;
         $dbinfo = $DB->get_server_info();
         $dbversion = $dbinfo['version'];
+        $env['dbtype'] = $dbtype;
         $env['dbversion'] = $dbversion;
 
         // OS details.
