30a9fe133afbdf2ccf3c2217770f60162ab8c32d
Ryan Wyllie <ryan@moodle.com>
2016-05-20 08:15:20 +0000

    MDL-54165 mod_assign: Collapsible review and grade panel

--- mod/assign/amd/src/grading_actions.js
+++ mod/assign/amd/src/grading_actions.js
@@ -23,7 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @since      3.1
  */
-define(['jquery'], function($) {
+define(['jquery', 'mod_assign/grading_events'], function($, GradingEvents) {
 
     /**
      * GradingActions class.
@@ -35,11 +35,7 @@ define(['jquery'], function($) {
         this._regionSelector = selector;
         this._region = $(selector);
 
-        $(document).on('user-changed', this._showActionsForm.bind(this));
-
-        this._region.find('[name="savechanges"]').on('click', this._trigger.bind(this, 'save-changes'));
-        this._region.find('[name="resetbutton"]').on('click', this._trigger.bind(this, 'reset'));
-        this._region.find('form').on('submit', function(e) { e.preventDefault(); });
+        this.registerEventListeners();
     };
 
     /** @type {String} Selector for the page region containing the user navigation. */
@@ -83,6 +79,166 @@ define(['jquery'], function($) {
      */
     GradingActions.prototype._trigger = function(action) {
         $(document).trigger(action);
+    };
+
+    /**
+     * Get the review panel element.
+     *
+     * @method getReviewPanelElement
+     * @return {jQuery}
+     */
+    GradingActions.prototype.getReviewPanelElement = function() {
+        return $('[data-region="review-panel"]');
+    };
+
+    /**
+     * Check if the page has a review panel.
+     *
+     * @method hasReviewPanelElement
+     * @return {bool}
+     */
+    GradingActions.prototype.hasReviewPanelElement = function() {
+        return this.getReviewPanelElement().length > 0;
+    };
+
+    /**
+     * Get the collapse grade panel button.
+     *
+     * @method getCollapseGradePanelButton
+     * @return {jQuery}
+     */
+    GradingActions.prototype.getCollapseGradePanelButton = function() {
+        return $('[data-region="grade-actions"] .collapse-grade-panel');
+    };
+
+    /**
+     * Get the collapse review panel button.
+     *
+     * @method getCollapseReviewPanelButton
+     * @return {jQuery}
+     */
+    GradingActions.prototype.getCollapseReviewPanelButton = function() {
+        return $('[data-region="grade-actions"] .collapse-review-panel');
+    };
+
+    /**
+     * Get the expand all panels button.
+     *
+     * @method getExpandAllPanelsButton
+     * @return {jQuery}
+     */
+    GradingActions.prototype.getExpandAllPanelsButton = function() {
+        return $('[data-region="grade-actions"] .collapse-none');
+    };
+
+    /**
+     * Remove the active state from all layout buttons.
+     *
+     * @method resetLayoutButtons
+     */
+    GradingActions.prototype.resetLayoutButtons = function() {
+        this.getCollapseGradePanelButton().removeClass('active');
+        this.getCollapseReviewPanelButton().removeClass('active');
+        this.getExpandAllPanelsButton().removeClass('active');
+    };
+
+    /**
+     * Hide the review panel.
+     *
+     * @method collapseReviewPanel
+     */
+    GradingActions.prototype.collapseReviewPanel = function() {
+        $(document).trigger(GradingEvents.COLLAPSE_REVIEW_PANEL);
+        $(document).trigger(GradingEvents.EXPAND_GRADE_PANEL);
+        this.resetLayoutButtons();
+        this.getCollapseReviewPanelButton().addClass('active');
+    };
+
+    /**
+     * Show/Hide the grade panel.
+     *
+     * @method collapseGradePanel
+     */
+    GradingActions.prototype.collapseGradePanel = function() {
+        $(document).trigger(GradingEvents.COLLAPSE_GRADE_PANEL);
+        $(document).trigger(GradingEvents.EXPAND_REVIEW_PANEL);
+        this.resetLayoutButtons();
+        this.getCollapseGradePanelButton().addClass('active');
+    };
+
+    /**
+     * Return the layout to default.
+     *
+     * @method expandAllPanels
+     */
+    GradingActions.prototype.expandAllPanels = function() {
+        $(document).trigger(GradingEvents.EXPAND_GRADE_PANEL);
+        $(document).trigger(GradingEvents.EXPAND_REVIEW_PANEL);
+        this.resetLayoutButtons();
+        this.getExpandAllPanelsButton().addClass('active');
+    };
+
+    /**
+     * Register event listeners for the grade panel.
+     *
+     * @method registerEventListeners
+     */
+    GradingActions.prototype.registerEventListeners = function() {
+        // Don't need layout controls if there is no review panel.
+        if (this.hasReviewPanelElement()) {
+            var collapseReviewPanelButton = this.getCollapseReviewPanelButton();
+            collapseReviewPanelButton.click(function(e) {
+                this.collapseReviewPanel();
+                e.preventDefault();
+            }.bind(this));
+
+            collapseReviewPanelButton.keydown(function(e) {
+                if (!e.metaKey && !e.shiftKey && !e.altKey && !e.ctrlKey) {
+                    if (e.keyCode === 13 || e.keyCode === 32) {
+                        this.collapseReviewPanel();
+                        e.preventDefault();
+                    }
+                }
+            }.bind(this));
+
+            var collapseGradePanelButton = this.getCollapseGradePanelButton();
+            collapseGradePanelButton.click(function(e) {
+                this.collapseGradePanel();
+                e.preventDefault();
+            }.bind(this));
+
+            collapseGradePanelButton.keydown(function(e) {
+                if (!e.metaKey && !e.shiftKey && !e.altKey && !e.ctrlKey) {
+                    if (e.keyCode === 13 || e.keyCode === 32) {
+                        this.collapseGradePanel();
+                        e.preventDefault();
+                    }
+                }
+            }.bind(this));
+
+            var expandAllPanelsButton = this.getExpandAllPanelsButton();
+            expandAllPanelsButton.click(function(e) {
+                this.expandAllPanels();
+                e.preventDefault();
+            }.bind(this));
+
+            expandAllPanelsButton.keydown(function(e) {
+                if (!e.metaKey && !e.shiftKey && !e.altKey && !e.ctrlKey) {
+                    if (e.keyCode === 13 || e.keyCode === 32) {
+                        this.expandAllPanels();
+                        e.preventDefault();
+                    }
+                }
+            }.bind(this));
+        }
+
+        $(document).on('user-changed', this._showActionsForm.bind(this));
+
+        this._region.find('[name="savechanges"]').on('click', this._trigger.bind(this, 'save-changes'));
+        this._region.find('[name="resetbutton"]').on('click', this._trigger.bind(this, 'reset'));
+        this._region.find('form').on('submit', function(e) {
+            e.preventDefault();
+        });
     };
 
     return GradingActions;
--- mod/assign/amd/src/grading_review_panel.js
+++ mod/assign/amd/src/grading_review_panel.js
@@ -23,7 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @since      3.1
  */
-define(['jquery'], function($) {
+define(['jquery', 'mod_assign/grading_events'], function($, GradingEvents) {
 
     /**
      * GradingReviewPanel class.
@@ -32,7 +32,8 @@ define(['jquery'], function($) {
      * @param {String} selector The selector for the page region containing the user navigation.
      */
     var GradingReviewPanel = function() {
-        this._region = $('[data-region="review-panel"]');
+        this._region = $('[data-region="review-panel-content"]');
+        this.registerEventListeners();
     };
 
     /** @type {JQuery} JQuery node for the page region containing the user navigation. */
@@ -56,6 +57,110 @@ define(['jquery'], function($) {
             return this._region[0];
         }
         return false;
+    };
+
+    /**
+     * Get the toggle review panel button.
+     *
+     * @method getTogglePanelButton
+     * @return {jQuery}
+     */
+    GradingReviewPanel.prototype.getTogglePanelButton = function() {
+        return this.getPanelElement().find('[data-region="review-panel-toggle"]');
+    };
+
+    /**
+     * Get the review panel element.
+     *
+     * @method getPanelElement
+     * @return {jQuery}
+     */
+    GradingReviewPanel.prototype.getPanelElement = function() {
+        return $('[data-region="review-panel"]');
+    };
+
+    /**
+     * Get the review panel content element.
+     *
+     * @method getPanelContentElement
+     * @return {jQuery}
+     */
+    GradingReviewPanel.prototype.getPanelContentElement = function() {
+        return $('[data-region="review-panel-content"]');
+    };
+
+    /**
+     * Show/Hide the review panel.
+     *
+     * @method togglePanel
+     */
+    GradingReviewPanel.prototype.togglePanel = function() {
+        if (this.getPanelElement().hasClass('collapsed')) {
+            $(document).trigger(GradingEvents.EXPAND_REVIEW_PANEL);
+        } else {
+            $(document).trigger(GradingEvents.COLLAPSE_REVIEW_PANEL);
+        }
+    };
+
+    /**
+     * Hide the review panel.
+     *
+     * @method collapsePanel
+     */
+    GradingReviewPanel.prototype.collapsePanel = function() {
+        this.getPanelElement().addClass('collapsed').removeClass('grade-panel-collapsed');
+        this.getPanelContentElement().attr('aria-hidden', true);
+    };
+
+    /**
+     * Show the review panel.
+     *
+     * @method expandPanel
+     */
+    GradingReviewPanel.prototype.expandPanel = function() {
+        this.getPanelElement().removeClass('collapsed');
+        this.getPanelContentElement().removeAttr('aria-hidden');
+    };
+
+    /**
+     * Register event listeners for the review panel.
+     *
+     * @method registerEventListeners
+     */
+    GradingReviewPanel.prototype.registerEventListeners = function() {
+        var toggleReviewPanelButton = this.getTogglePanelButton();
+        toggleReviewPanelButton.click(function(e) {
+            this.togglePanel();
+            e.preventDefault();
+        }.bind(this));
+
+        toggleReviewPanelButton.keydown(function(e) {
+            if (!e.metaKey && !e.shiftKey && !e.altKey && !e.ctrlKey) {
+                if (e.keyCode === 13 || e.keyCode === 32) {
+                    this.togglePanel();
+                    e.preventDefault();
+                }
+            }
+        }.bind(this));
+
+        var docElement = $(document);
+        docElement.on(GradingEvents.COLLAPSE_REVIEW_PANEL, function() {
+            this.collapsePanel();
+        }.bind(this));
+
+        // Need special styling when grade panel is collapsed.
+        docElement.on(GradingEvents.COLLAPSE_GRADE_PANEL, function() {
+            this.expandPanel();
+            this.getPanelElement().addClass('grade-panel-collapsed');
+        }.bind(this));
+
+        docElement.on(GradingEvents.EXPAND_REVIEW_PANEL, function() {
+            this.expandPanel();
+        }.bind(this));
+
+        docElement.on(GradingEvents.EXPAND_GRADE_PANEL, function() {
+            this.getPanelElement().removeClass('grade-panel-collapsed');
+        }.bind(this));
     };
 
     return GradingReviewPanel;
