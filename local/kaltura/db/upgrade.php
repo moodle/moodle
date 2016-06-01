ea5ffa5762c673de54fff44175c0710df8e80145
Muli Dayan <muli.dayan@kaltura.com>
2016-06-01 14:55:48 +0300

    bump build number - 2016060130

--- local/kaltura/db/upgrade.php
+++ local/kaltura/db/upgrade.php
@@ -59,13 +59,13 @@ function xmldb_local_kaltura_upgrade($oldversion) {
         }
 
         // Kaltura savepoint reached.
-        upgrade_plugin_savepoint(true, 2016032930, 'local', 'kaltura');
+        upgrade_plugin_savepoint(true, 2016060130, 'local', 'kaltura');
         $savePointDone = true;
     }
 
-    if (!$savePointDone && $oldversion < 2016032930) {
+    if (!$savePointDone && $oldversion < 2016060130) {
         // Kaltura savepoint reached.
-        upgrade_plugin_savepoint(true, 2016032930, 'local', 'kaltura');
+        upgrade_plugin_savepoint(true, 2016060130, 'local', 'kaltura');
     }
     return true;
 }
--- local/kaltura/version.php
+++ local/kaltura/version.php
@@ -25,7 +25,7 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-$plugin->version = 2016032930;
+$plugin->version = 2016060130;
 $plugin->component = 'local_kaltura';
 $plugin->release = 'Kaltura release 4.0.09';
 $plugin->requires = 2014050800;
