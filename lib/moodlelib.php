89bacb163f390b61eebbbe99f6dffb099e1f782e
Frederic Massart <fred@moodle.com>
2016-06-07 16:58:20 +0800

    Revert "MDL-44626 files: Show the PHP max upload size instead of 'unlimited'"
    This reverts commit 6f0768618a38c11eca90a97c0dff31ec8ce434d1.

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -6305,7 +6305,7 @@ function get_user_max_upload_file_size($context, $sitebytes = 0, $coursebytes = 
     }
 
     if (has_capability('moodle/course:ignorefilesizelimits', $context, $user)) {
-        return get_max_upload_file_size(USER_CAN_IGNORE_FILE_SIZE_LIMITS);
+        return USER_CAN_IGNORE_FILE_SIZE_LIMITS;
     }
 
     return get_max_upload_file_size($sitebytes, $coursebytes, $modulebytes);
