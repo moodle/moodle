0002056f7a6cb8056bea056f5bc8e698b64095b5
Juan Leyva <juanleyvadelgado@gmail.com>
2016-06-16 11:25:11 +0100

    MDL-54857 tool_mobile: New WS get_site_public_settings

--- admin/tool/mobile/version.php
+++ admin/tool/mobile/version.php
@@ -23,6 +23,6 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2016052300; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016052301; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016051900; // Requires this Moodle version.
 $plugin->component = 'tool_mobile'; // Full name of the plugin (used for diagnostics).
