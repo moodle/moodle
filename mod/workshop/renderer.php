080c1e798b874190e66462a861c340206d78de6b
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2016-07-06 11:30:40 +0200

    MDL-54934 workshop: ensure "Current phase" is always separated
    Apparently some readers can join 2 adjacent spans completely, simply
    adding a whitespace between them prevents that, ensuring separation.,

--- mod/workshop/renderer.php
+++ mod/workshop/renderer.php
@@ -290,7 +290,7 @@ class mod_workshop_renderer extends plugin_renderer_base {
         foreach ($plan->phases as $phasecode => $phase) {
             $title = html_writer::tag('span', $phase->title);
             if ($phase->active) {
-                $title .= html_writer::tag('span', get_string('userplancurrentphase', 'workshop'),
+                $title .= ' ' . html_writer::tag('span', get_string('userplancurrentphase', 'workshop'),
                     array('class' => 'accesshide'));
             }
             $actions = '';
