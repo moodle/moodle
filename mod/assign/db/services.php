39e110d14530389f0395f8ae828c729c509c6589
Juan Leyva <juanleyvadelgado@gmail.com>
2016-07-08 13:29:39 +0100

    MDL-55163 mod_assign: Fix mod_assign_copy_previous_attempts
    - Not exposed via WS
    - Have a bug when calling the api function

--- mod/assign/db/services.php
+++ mod/assign/db/services.php
@@ -25,6 +25,15 @@
 
 $functions = array(
 
+        'mod_assign_copy_previous_attempt' => array(
+            'classname'     => 'mod_assign_external',
+            'methodname'    => 'copy_previous_attempt',
+            'classpath'     => 'mod/assign/externallib.php',
+            'description'   => 'Copy a students previous attempt to a new attempt.',
+            'type'          => 'write',
+            'capabilities'  => 'mod/assign:view, mod/assign:submit'
+        ),
+
         'mod_assign_get_grades' => array(
                 'classname'   => 'mod_assign_external',
                 'methodname'  => 'get_grades',
