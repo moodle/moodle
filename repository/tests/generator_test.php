f1f94da49673659025e4ef852f6e16b640215e37
Andrew Nicols <andrew@nicols.co.uk>
2016-07-11 14:50:09 +0800

    MDL-49533 repository: Remove the Alfresco plugin
    The SDK that this plugin was designed around is not compatible with recent
    versions of Alfresco so the decisio has been made to move the repository to
    the plugins database.

--- repository/tests/generator_test.php
+++ repository/tests/generator_test.php
@@ -45,7 +45,7 @@ class core_repository_generator_testcase extends advanced_testcase {
         $this->resetAfterTest(true);
 
         // All the repository types.
-        $all = array('alfresco', 'boxnet', 'coursefiles', 'dropbox', 'equella', 'filesystem', 'flickr',
+        $all = array('boxnet', 'coursefiles', 'dropbox', 'equella', 'filesystem', 'flickr',
             'flickr_public', 'googledocs', 'local', 'merlot', 'picasa', 'recent', 's3', 'upload', 'url',
             'user', 'webdav', 'wikimedia', 'youtube');
 
