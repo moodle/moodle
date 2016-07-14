c7ae332304f37223eda7be0f02fbd2b7040afde4
Juan Leyva <juanleyvadelgado@gmail.com>
2016-07-14 09:10:07 +0100

    MDL-55220 forum: Add string filtering in WS
    The following external functions now filter the discussion and post
    name and subject.
    - get_forum_discussions_paginated
    - get_forum_discussion_posts

--- mod/forum/externallib.php
+++ mod/forum/externallib.php
@@ -485,6 +485,7 @@ class mod_forum_external extends external_api {
             $userpicture->size = 1; // Size f1.
             $post->userpictureurl = $userpicture->get_url($PAGE)->out(false);
 
+            $post->subject = external_format_string($post->subject, $modcontext->id);
             // Rewrite embedded images URLs.
             list($post->message, $post->messageformat) =
                 external_format_text($post->message, $post->messageformat, $modcontext->id, 'mod_forum', 'post', $post->id);
@@ -717,6 +718,8 @@ class mod_forum_external extends external_api {
                 $userpicture->size = 1; // Size f1.
                 $discussion->usermodifiedpictureurl = $userpicture->get_url($PAGE)->out(false);
 
+                $discussion->name = external_format_string($discussion->name, $modcontext->id);
+                $discussion->subject = external_format_string($discussion->subject, $modcontext->id);
                 // Rewrite embedded images URLs.
                 list($discussion->message, $discussion->messageformat) =
                     external_format_text($discussion->message, $discussion->messageformat,
