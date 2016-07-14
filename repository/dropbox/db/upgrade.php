066963cd183df684303f626061004c13124e2fe3
Andrew Nicols <andrew@nicols.co.uk>
2016-07-14 13:12:40 +0800

    MDL-52051 repository_dropbox: Migrate to v2 API

--- repository/dropbox/db/upgrade.php
+++ repository/dropbox/db/upgrade.php
@@ -21,8 +21,6 @@ defined('MOODLE_INTERNAL') || die();
  * @return bool result
  */
 function xmldb_repository_dropbox_upgrade($oldversion) {
-    global $CFG;
-
     // Moodle v2.8.0 release upgrade line.
     // Put any upgrade step following this.
 
@@ -35,5 +33,9 @@ function xmldb_repository_dropbox_upgrade($oldversion) {
     // Moodle v3.1.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2016091300) {
+        unset_config('legacyapi', 'dropbox');
+        upgrade_plugin_savepoint(true, 2016091300, 'repository', 'dropbox');
+    }
     return true;
 }
