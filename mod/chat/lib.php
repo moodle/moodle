530d553580a98a8bc0480383b701f5ffeab3720c
Stephen Bourget <steve.bourget@sau19.org>
2016-07-15 16:20:32 -0400

    MDL-55252 mod_chat: prevent direct script access to lib files.

--- mod/chat/lib.php
+++ mod/chat/lib.php
@@ -22,6 +22,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 require_once($CFG->dirroot.'/calendar/lib.php');
 
 // The HTML head for the message window to start with (<!-- nix --> is used to get some browsers starting with output.
