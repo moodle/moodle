321c986c8691c52e2d8ab6c59a30c3782caeabc9
Frederic Massart <fred@moodle.com>
2016-07-20 16:45:59 +0800

    MDL-55285 core: Include library scssphp in core
    Part of MDL-55071

---
+++ lib/scssphp/Formatter/OutputBlock.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * SCSSPHP
+ *
+ * @copyright 2012-2015 Leaf Corcoran
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * @link http://leafo.github.io/scssphp
+ */
+
+namespace Leafo\ScssPhp\Formatter;
+
+/**
+ * Output block
+ *
+ * @author Anthon Pang <anthon.pang@gmail.com>
+ */
+class OutputBlock
+{
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var integer
+     */
+    public $depth;
+
+    /**
+     * @var array
+     */
+    public $selectors;
+
+    /**
+     * @var array
+     */
+    public $lines;
+
+    /**
+     * @var array
+     */
+    public $children;
+
+    /**
+     * @var \Leafo\ScssPhp\Formatter\OutputBlock
+     */
+    public $parent;
+}
