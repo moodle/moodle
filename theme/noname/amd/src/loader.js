600e283fb2abd2cac1fa60b2df701a8eddb07db4
Damyon Wiese <damyon@moodle.com>
2016-07-25 16:22:00 +0800

    MDL-55403 theme_noname: Add bootstrap4 js to moodle
    Part of MDL-55071

---
+++ theme/noname/amd/src/loader.js
@@ -0,0 +1,52 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Template renderer for Moodle. Load and render Moodle templates with Mustache.
+ *
+ * @module     core/templates
+ * @package    core
+ * @class      templates
+ * @copyright  2015 Damyon Wiese <damyon@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @since      2.9
+ */
+define(['jquery', './tether'], function(jQuery, Tether) {
+
+    var originalJQuery = window.jQuery;
+    var originalTether = window.Tether;
+
+    window.jQuery = jQuery;
+    window.Tether = Tether;
+
+    require(['theme_noname/util',
+            'theme_noname/alert',
+            'theme_noname/button',
+            'theme_noname/carousel',
+            'theme_noname/collapse',
+            'theme_noname/dropdown',
+            'theme_noname/modal',
+            'theme_noname/scrollspy',
+            'theme_noname/tab',
+            'theme_noname/tooltip',
+            'theme_noname/popover'],
+            function(another) {
+        jQuery('[data-toggle="popover"]').popover();
+        window.jQuery = originalJQuery;
+        window.Tether = originalTether;
+    });
+
+    return {};
+});
--- theme/noname/readme_moodle.txt
+++ theme/noname/readme_moodle.txt
@@ -2,6 +2,8 @@ Description of Twitter bootstrap import into Moodle
 
 Twitter bootstrap
 -----------------
+
+Sass:
 This theme uses the original unmodified version 4.0.0-alpha-3 Twitter bootstrap sass files.
 The bootstrap repository is available on:
 
@@ -11,4 +13,18 @@ To update to the latest release of twitter bootstrap:
 * remove all files from scss/bootstrap,
 * download the new scss files and store them in scss/bootstrap
 * update ./thirdpartylibs.xml
+
+Javascript:
+
+This theme uses the transpiled javascript from bootstrap4 as amd modules.
+
+To update the javascript files:
+Checkout the latest branch of bootstrap to a folder, in that folder run:
+
+> mkdir "out"
+> npm install babel-cli babel-preset-es2015
+> ./node_modules/babel-cli/bin/babel.js --presets es2015 -d out/ js/src/
+
+Copy the transpiled files from out/ into the amd/src/ folder for the theme.
+Run grunt to re-compile the JS files.
 
