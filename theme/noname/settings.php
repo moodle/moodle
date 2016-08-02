399da4fe7dcb6a2550aebe2933b91722088599fa
Frederic Massart <fred@moodle.com>
2016-08-02 16:30:37 +0800

    MDL-55384 theme_noname: Admin settings to customise the theme
    Part of MDL-55071

--- theme/noname/settings.php
+++ theme/noname/settings.php
@@ -23,12 +23,48 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
+    $settings = new theme_noname_admin_settingspage_tabs('themesettingnoname', get_string('configtitle', 'theme_noname'));
+    $page = new admin_settingpage('theme_noname_general', get_string('generalsettings', 'theme_noname'));
 
-    $settings = new theme_noname_admin_settingspage_tabs('theme_noname', 'CONFIG TITLE');
-    $temp = new admin_settingpage('theme_noname_general',  'GENERAL SETTINGS');
+    // Preset.
+    $name = 'theme_noname/preset';
+    $title = get_string('preset', 'theme_noname');
+    $description = get_string('preset_desc', 'theme_noname');
+    $choices = [
+        // A file named 'preset-' . key . '.scss' is expected.
+        'default' => get_string('presetdefault', 'theme_noname')
+    ];
+    $default = 'default';
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
 
-    $settings->add($temp);
-    $temp = new admin_settingpage('theme_noname_advanced',  'ADVANCED');
+    // Variable $body-color.
+    $name = 'theme_noname/brandcolor';
+    $title = get_string('brandcolor', 'theme_noname');
+    $description = get_string('brandcolor_desc', 'theme_noname');
+    $default = '#373A3C';   // Straight from bootstrap variables.
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, $default, null, false);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
 
-    $settings->add($temp);
+    // Must add the page after definiting all the settings!
+    $settings->add($page);
+
+    // Advanced settings.
+    $page = new admin_settingpage('theme_noname_advanced', get_string('advancedsettings', 'theme_noname'));
+
+    // Raw SCSS for before the content.
+    $setting = new theme_noname_admin_setting_scss_variables('theme_noname/scss_variables',
+        get_string('scssvariables', 'theme_noname'), get_string('scssvariables_desc', 'theme_noname'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Raw SCSS for after the content.
+    $setting = new admin_setting_configtextarea('theme_noname/scss', get_string('rawscss', 'theme_noname'),
+        get_string('rawscss_desc', 'theme_noname'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    $settings->add($page);
 }
