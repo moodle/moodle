27b922c96d4754e6fd2d647cbadceb0cbc04de58
Ryan Wyllie <ryan@moodle.com>
2016-08-02 15:09:58 +0800

    MDL-55384 theme_noname: Support tabs in theme settings
    Part of MDL-55071

---
+++ theme/noname/settings.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   theme_noname
+ * @copyright 2016 Ryan Wyllie
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($ADMIN->fulltree) {
+
+    $settings = new theme_noname_admin_settingspage_tabs('theme_noname', 'CONFIG TITLE');
+    $temp = new admin_settingpage('theme_noname_general',  'GENERAL SETTINGS');
+
+    $settings->add($temp);
+    $temp = new admin_settingpage('theme_noname_advanced',  'ADVANCED');
+
+    $settings->add($temp);
+}
