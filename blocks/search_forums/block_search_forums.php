392c54a799f80214f47eb06b1663eb470f4562bd
Frederic Massart <fred@moodle.com>
2016-08-05 12:35:15 +0800

    MDL-55474 block_search_forums: Convert search form to templates
    Part of MDL-55071

--- blocks/search_forums/block_search_forums.php
+++ blocks/search_forums/block_search_forums.php
@@ -42,21 +42,9 @@ class block_search_forums extends block_base {
             return $this->content;
         }
 
-        $advancedsearch = get_string('advancedsearch', 'block_search_forums');
-
-        $strsearch  = get_string('search');
-        $strgo      = get_string('go');
-
-        $this->content->text  = '<div class="searchform">';
-        $this->content->text .= '<form action="'.$CFG->wwwroot.'/mod/forum/search.php" style="display:inline"><fieldset class="invisiblefieldset">';
-        $this->content->text .= '<legend class="accesshide">'.$strsearch.'</legend>';
-        $this->content->text .= '<input name="id" type="hidden" value="'.$this->page->course->id.'" />';  // course
-        $this->content->text .= '<label class="accesshide" for="searchform_search">'.$strsearch.'</label>'.
-                                '<input id="searchform_search" name="search" type="text" size="16" />';
-        $this->content->text .= '<button id="searchform_button" type="submit" title="'.$strsearch.'">'.$strgo.'</button><br />';
-        $this->content->text .= '<a href="'.$CFG->wwwroot.'/mod/forum/search.php?id='.$this->page->course->id.'">'.$advancedsearch.'</a>';
-        $this->content->text .= $OUTPUT->help_icon('search');
-        $this->content->text .= '</fieldset></form></div>';
+        $output = $this->page->get_renderer('block_search_forums');
+        $searchform = new \block_search_forums\output\search_form($this->page->course->id);
+        $this->content->text = $output->render($searchform);
 
         return $this->content;
     }
