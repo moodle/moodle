f3d38863fe499fdac1bc13707430980611e671d8
David Monllao <davidm@moodle.com>
2016-08-09 15:51:09 +0800

    MDL-55390 search: is_search_area unit tests
    Added extra checking for search manager get_search_area.

--- search/classes/manager.php
+++ search/classes/manager.php
@@ -207,7 +207,8 @@ class manager {
         }
 
         $classname = static::get_area_classname($areaid);
-        if (class_exists($classname)) {
+
+        if (class_exists($classname) && static::is_search_area($classname)) {
             return new $classname();
         }
 
@@ -730,12 +731,12 @@ class manager {
     /**
      * Checks whether a classname is of an actual search area.
      *
-     * @param string $searchareaname
+     * @param string $classname
      * @return bool
      */
-    protected static function is_search_area($searchareaname) {
-        if (is_subclass_of($searchareaname, 'core_search\base')) {
-            return (new \ReflectionClass($searchareaname))->isInstantiable();
+    protected static function is_search_area($classname) {
+        if (is_subclass_of($classname, 'core_search\base')) {
+            return (new \ReflectionClass($classname))->isInstantiable();
         }
 
         return false;
