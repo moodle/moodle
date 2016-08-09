5fa90114d7aa48fb121e82a5d80eb3113471e338
Andrew Nicols <andrew@nicols.co.uk>
2016-08-09 14:41:22 +0800

    MDL-55329 behat: Correct typo

--- lib/tests/behat/behat_deprecated.php
+++ lib/tests/behat/behat_deprecated.php
@@ -29,7 +29,7 @@ require_once(__DIR__ . '/../../../lib/behat/behat_base.php');
 
 use Behat\Mink\Exception\ElementNotFoundException as ElementNotFoundException,
     Behat\Gherkin\Node\TableNode as TableNode,
-    Behat\Gherkin\Node\PyStringNode as PyStringNode,
+    Behat\Gherkin\Node\PyStringNode as PyStringNode;
 
 /**
  * Deprecated behat step definitions.
