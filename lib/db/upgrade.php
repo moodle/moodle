bc48debf36e6eafa4cefef6ef8e840bbbe248df8
Marina Glancy <marina@moodle.com>
2016-08-11 16:30:51 +0800

    MDL-29332 question: calculated variables may differ in case only

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -4700,5 +4700,20 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2015111605.07);
     }
 
+    if ($oldversion < 2015111606.01) {
+
+        // Define index attemptstepid-name (unique) to be dropped from question_attempt_step_data.
+        $table = new xmldb_table('question_attempt_step_data');
+        $index = new xmldb_index('attemptstepid-name', XMLDB_INDEX_UNIQUE, array('attemptstepid', 'name'));
+
+        // Conditionally launch drop index attemptstepid-name.
+        if ($dbman->index_exists($table, $index)) {
+            $dbman->drop_index($table, $index);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2015111606.01);
+    }
+
     return true;
 }
