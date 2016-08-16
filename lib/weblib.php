d3558c34b862ba9d1a398496d1cbdb3d4e8e7d37
Frederic Massart <fred@moodle.com>
2016-08-16 13:56:50 +0800

    MDL-55564 output: Use template for password autofill hack
    Part of MDL-55071

--- lib/weblib.php
+++ lib/weblib.php
@@ -3740,5 +3740,6 @@ function get_formatted_help_string($identifier, $component, $ajax = false, $a = 
  * @return string HTML to prevent password autofill
  */
 function prevent_form_autofill_password() {
-    return '<div class="hide"><input type="text" class="ignoredirty" /><input type="password" class="ignoredirty" /></div>';
+    global $OUTPUT;
+    return $OUTPUT->render_from_template('core/prevent_form_autofill_password', []);
 }
