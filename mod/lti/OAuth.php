cdd3165f404f699ad6973fa02ae8e0bddb2adc77
Bishop Bettini <bishop.bettini@gmail.com>
2016-08-26 12:08:29 -0400

    Fixes silly typo

--- mod/lti/OAuth.php
+++ mod/lti/OAuth.php
@@ -278,7 +278,7 @@ class OAuthRequest {
             // Deal with magic_quotes
             // http://www.php.net/manual/en/security.magicquotes.disabling.php
             if (get_magic_quotes_gpc()) {
-                $outpost = array();
+                $ourpost = array();
                 foreach ($_POST as $k => $v) {
                     $v = stripslashes($v);
                     $ourpost[$k] = $v;
