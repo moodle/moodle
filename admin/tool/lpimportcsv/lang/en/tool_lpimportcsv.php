5e4fffa013631ff73447f4da86769b092908bd59
Damyon Wiese <damyon@moodle.com>
2016-08-30 10:59:05 +0800

    MDL-55767 tool_lpimportcsv: import/export competency fmwks

---
+++ admin/tool/lpimportcsv/lang/en/tool_lpimportcsv.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_lpimportcsv', language 'en'
+ *
+ * @package    tool_lpimportcsv
+ * @copyright  2015 Damyon Wiese
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['competencyscale'] = 'Competency Scale: {$a}';
+$string['competencyscaledescription'] = 'Competency scale created by import';
+$string['confirmcolumnmappings'] = 'Confirm the columns mappings';
+$string['confirm'] = 'Confirm';
+$string['csvdelimiter'] = 'CSV delimiter';
+$string['csvdelimiter_help'] = 'CSV delimiter of the CSV file.';
+$string['description'] = 'Description';
+$string['descriptionformat'] = 'Description format';
+$string['encoding'] = 'Encoding';
+$string['encoding_help'] = 'Encoding of the CSV file.';
+$string['export'] = 'Export';
+$string['exportid'] = 'Exported id (optional)';
+$string['exportnavlink'] = 'Export competency framework';
+$string['idnumber'] = 'Id number';
+$string['importfile'] = 'CSV framework description file';
+$string['import'] = 'Import';
+$string['invalidimportfile'] = 'File format is invalid.';
+$string['isframework'] = 'Is framework';
+$string['noframeworks'] = 'No competency frameworks have been created yet';
+$string['parentidnumber'] = 'Parent id number';
+$string['pluginname'] = 'Import competency framework';
+$string['relatedidnumbers'] = 'Cross referenced competency id numbers';
+$string['ruleconfig'] = 'Rule config (optional)';
+$string['ruleoutcome'] = 'Rule outcome (optional)';
+$string['ruletype'] = 'Rule type (optional)';
+$string['scaleconfiguration'] = 'Scale configuration';
+$string['scalevalues'] = 'Scale values';
+$string['shortname'] = 'Shortname';
+$string['taxonomy'] = 'Taxonomy';
