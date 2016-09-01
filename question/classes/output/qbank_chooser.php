891750bcfad73cf310b9c9097b8d2360b407f59a
Frederic Massart <fred@moodle.com>
2016-09-01 17:30:46 +0800

    MDL-55797 core_question: Make static getter for chooser
    Part of MDL-55071

--- question/classes/output/qbank_chooser.php
+++ question/classes/output/qbank_chooser.php
@@ -25,9 +25,11 @@
 namespace core_question\output;
 defined('MOODLE_INTERNAL') || die();
 
+use context_course;
 use core\output\chooser_section;
 use lang_string;
 use moodle_url;
+use question_bank;
 
 
 /**
@@ -41,6 +43,12 @@ class qbank_chooser extends \core\output\chooser {
 
     /**
      * Constructor.
+     *
+     * @param array $real The real question types.
+     * @param array $fake The fake question types.
+     * @param stdClass $course The course.
+     * @param array $hiddenparams Hidden parameters.
+     * @param context $context The relevant context.
      */
     public function __construct($real, $fake, $course, $hiddenparams, $context) {
         $sections = [];
@@ -64,6 +72,32 @@ class qbank_chooser extends \core\output\chooser {
         foreach ($hiddenparams as $k => $v) {
             $this->add_param($k, $v);
         }
+    }
+
+    /**
+     * Get an instance of the question bank chooser.
+     *
+     * @param stdClass $course The course.
+     * @param array $hiddenparams Hidden parameters.
+     * @param array|null $allowedqtypes Allowed question types.
+     * @return qbank_chooser
+     */
+    public static function get($course, $hiddenparams, array $allowedqtypes = null) {
+        $realqtypes = array();
+        $fakeqtypes = array();
+
+        foreach (question_bank::get_creatable_qtypes() as $qtypename => $qtype) {
+            if ($allowedqtypes && !in_array($qtypename, $allowedqtypes)) {
+                continue;
+            }
+            if ($qtype->is_real_question_type()) {
+                $realqtypes[] = $qtype;
+            } else {
+                $fakeqtypes[] = $qtype;
+            }
+        }
+
+        return new static($realqtypes, $fakeqtypes, $course, $hiddenparams, context_course::instance($course->id));
     }
 
 }
