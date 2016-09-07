c874d9aa81bcf6bbab3e2267b33f5072c7e5dc26
Juan Leyva <juanleyvadelgado@gmail.com>
2016-09-07 14:53:37 +0100

    MDL-55415 course: Fix unit tests after fixing beat issues

--- course/tests/externallib_test.php
+++ course/tests/externallib_test.php
@@ -1834,14 +1834,22 @@ class core_course_externallib_testcase extends externallib_advanced_testcase {
                 $adminoptions->{$option['name']} = $option['available'];
             }
             if ($course['id'] == SITEID) {
-                $this->assertCount(6, $course['options']);
+                $this->assertCount(15, $course['options']);
                 $this->assertFalse($adminoptions->update);
                 $this->assertFalse($adminoptions->filters);
                 $this->assertFalse($adminoptions->reports);
                 $this->assertFalse($adminoptions->backup);
                 $this->assertFalse($adminoptions->restore);
                 $this->assertFalse($adminoptions->files);
-                $this->assertTrue(!isset($adminoptions->tags));
+                $this->assertFalse(!isset($adminoptions->tags));
+                $this->assertFalse($adminoptions->gradebook);
+                $this->assertFalse($adminoptions->outcomes);
+                $this->assertFalse($adminoptions->badges);
+                $this->assertFalse($adminoptions->import);
+                $this->assertFalse($adminoptions->publish);
+                $this->assertFalse($adminoptions->reset);
+                $this->assertFalse($adminoptions->roles);
+                $this->assertFalse($adminoptions->grades);
             } else {
                 $this->assertCount(15, $course['options']);
                 $this->assertFalse($adminoptions->update);
