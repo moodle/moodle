3c215fa3d2e5368d0ac73a55c2e08a8805e8afc9
David Mudrák <david@moodle.com>
2016-09-14 12:14:56 +0200

    MDL-55957 workshop: Fix the embedded files serving
    There was a bug with serving the files from the areas instructauthors,
    instructreviewers and conclusion. These three areas should not use the
    itemid in the plugininfo URLs. But they did use 0 as the itemid which
    broke the file previews when browsing via server files repository.
    
    The first part of the patch fixes all relevant calls to
    file_rewrite_pluginfile_urls() so that null is now properly used instead
    of zero.
    
    The second part of the fix is that we no longer delete the first $args
    element in the workshop_pluginfile() function - the itemid is not
    supposed to appear there now.
    
    The last part of the patch is that instead of repeating the same code
    block copy&pasted for each file area in workshop_pluginfile(), we now
    have a single block covering them all.

--- mod/workshop/exsubmission.php
+++ mod/workshop/exsubmission.php
@@ -187,7 +187,7 @@ echo $output->heading(format_string($workshop->name), 2);
 // while reading the submitted answer
 if (trim($workshop->instructauthors)) {
     $instructions = file_rewrite_pluginfile_urls($workshop->instructauthors, 'pluginfile.php', $PAGE->context->id,
-        'mod_workshop', 'instructauthors', 0, workshop::instruction_editors_options($PAGE->context));
+        'mod_workshop', 'instructauthors', null, workshop::instruction_editors_options($PAGE->context));
     print_collapsible_region_start('', 'workshop-viewlet-instructauthors', get_string('instructauthors', 'workshop'));
     echo $output->box(format_text($instructions, $workshop->instructauthorsformat, array('overflowdiv'=>true)), array('generalbox', 'instructions'));
     print_collapsible_region_end();
