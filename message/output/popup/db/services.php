53522c6e38a3e5757c91316878490f3303f74def
Juan Leyva <juanleyvadelgado@gmail.com>
2016-09-20 14:35:26 +0100

    MDL-55393 message: Add some external functions to mobile service

--- message/output/popup/db/services.php
+++ message/output/popup/db/services.php
@@ -31,6 +31,7 @@ $functions = array(
         'description' => 'Retrieve a list of popup notifications for a user',
         'type' => 'read',
         'ajax' => true,
+        'services' => array(MOODLE_OFFICIAL_MOBILE_SERVICE),
     ),
     'message_popup_get_unread_popup_notification_count' => array(
         'classname' => 'message_popup_external',
@@ -39,5 +40,6 @@ $functions = array(
         'description' => 'Retrieve the count of unread popup notifications for a given user',
         'type' => 'read',
         'ajax' => true,
+        'services' => array(MOODLE_OFFICIAL_MOBILE_SERVICE),
     ),
 );
