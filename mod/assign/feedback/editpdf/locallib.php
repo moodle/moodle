30cdddb017894966c02fa4672bcb630390cee323
Juan Leyva <juanleyvadelgado@gmail.com>
2016-09-21 12:59:32 +0100

    MDL-55129 mod_assign: Allow plugins to handle configs for external
    Until now in the get_assignments WS all the settings for assignment
    plugins (submission and feedback) are returned, this was bad because:
    
    - It was returning settings for plugins that may not be enabled
    - It was returning settings that would be a security risk (a third
    party plugin including a password or credentials for connecting a
    remote site)
    - Some values was returned "raw" without further processing, for
    example, maxsubmissionsizebytes returned 0 when using the plugin
    default value instead a valid size
    - Assignment plugins implemented an API for communication with the
    external API, see for example: get_external_parameters, it didn’t make
    sense had an API for the received parameters and not the returned data.

--- mod/assign/feedback/editpdf/locallib.php
+++ mod/assign/feedback/editpdf/locallib.php
@@ -375,4 +375,14 @@ class assign_feedback_editpdf extends assign_feedback_plugin {
     public function supports_review_panel() {
         return true;
     }
+
+    /**
+     * Return the plugin configs for external functions.
+     *
+     * @return array the list of settings
+     * @since Moodle 3.2
+     */
+    public function get_config_for_external() {
+        return (array) $this->get_config();
+    }
 }
