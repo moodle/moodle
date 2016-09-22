dd99ee8d1b9170bd5d6e2501bfb3c533fadb5504
Dan Poltawski <dan@moodle.com>
2016-09-22 12:52:03 +0100

    MDL-55071 theme_boost: prevent phpcs complaints
    The commenting rule is a bit weird, but we can get round it easily.

--- theme/boost/layout/embedded.php
+++ theme/boost/layout/embedded.php
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * An embedded layout for the boost theme.
  *
@@ -23,6 +21,9 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright 2016 Damyon Wiese
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
+
 $templatecontext = [
     'sitename' => format_string($SITE->shortname, true, array('context' => context_course::instance(SITEID))),
     'output' => $OUTPUT
