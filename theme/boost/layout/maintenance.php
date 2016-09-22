d5d0a8028d4bdf23cc5f6002338ce015df02c338
Dan Poltawski <dan@moodle.com>
2016-09-22 12:52:03 +0100

    MDL-55071 theme_boost: prevent phpcs complaints
    The commenting rule is a bit weird, but we can get round it easily.

--- theme/boost/layout/maintenance.php
+++ theme/boost/layout/maintenance.php
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * A maintenance layout for the boost theme.
  *
@@ -23,6 +21,9 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright 2016 Damyon Wiese
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
+
 $templatecontext = [
     // We cannot pass the context to format_string, this layout can be used during
     // installation. At that stage database tables do not exist yet.
