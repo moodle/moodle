82b945dbbef467ecdf532074424f2042b61e8c10
Frédéric Massart <fred@moodle.com>
2016-09-23 12:40:48 +0200

    MDL-56095 output: Make the progress bar templatable
    The order of inclusion of weblib and outputlib had to change
    as the progress_bar (weblib) uses interfaces defined in outputlib.

--- lib/weblib.php
+++ lib/weblib.php
@@ -3182,7 +3182,7 @@ function is_in_popup() {
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @package core
  */
-class progress_bar {
+class progress_bar implements renderable, templatable {
     /** @var string html id */
     private $html_id;
     /** @var int total width */
@@ -3223,26 +3223,15 @@ class progress_bar {
      * @return void Echo's output
      */
     public function create() {
-        global $PAGE;
+        global $OUTPUT;
 
         $this->time_start = microtime(true);
         if (CLI_SCRIPT) {
             return; // Temporary solution for cli scripts.
         }
 
-        $PAGE->requires->string_for_js('secondsleft', 'moodle');
-
-        $htmlcode = <<<EOT
-        <div class="progressbar_container" style="width: {$this->width}px;" id="{$this->html_id}">
-            <h2></h2>
-            <div class="progress progress-striped active">
-                <div class="bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">&nbsp;</div>
-            </div>
-            <p></p>
-        </div>
-EOT;
         flush();
-        echo $htmlcode;
+        echo $OUTPUT->render($this);
         flush();
     }
 
@@ -3276,15 +3265,17 @@ EOT;
             // No significant change, no need to update anything.
             return;
         }
+
+        $estimatemsg = null;
         if (is_numeric($estimate)) {
-            $estimate = get_string('secondsleft', 'moodle', round($estimate, 2));
+            $estimatemsg = get_string('secondsleft', 'moodle', round($estimate, 2));
         }
 
         $this->percent = round($percent, 2);
         $this->lastupdate = microtime(true);
 
         echo html_writer::script(js_writer::function_call('updateProgressBar',
-            array($this->html_id, $this->percent, $msg, $estimate)));
+            array($this->html_id, $this->percent, $msg, $estimatemsg)));
         flush();
     }
 
@@ -3342,6 +3333,19 @@ EOT;
         $this->percent    = 0;
         $this->lastupdate = 0;
         $this->time_start = 0;
+    }
+
+    /**
+     * Export for template.
+     *
+     * @param  renderer_base $output The renderer.
+     * @return array
+     */
+    public function export_for_template(renderer_base $output) {
+        return [
+            'id' => $this->html_id,
+            'width' => $this->width,
+        ];
     }
 }
 
