931bba6054cf2d9c536b1d59bedacb8b41dc7933
David Mudrák <david@moodle.com>
2016-10-11 20:21:30 +0200

    MDL-54945 workshop: Introduce workshop::check_group_membership() method
    This is just a refactored code block present at two different places. We
    will need the same logic in the portfolio caller class soit made sense
    to implement it as a new method, rather than make another copy&paste.
    
    As a side effect, the logic is now properly unit tested.

--- mod/workshop/submission.php
+++ mod/workshop/submission.php
@@ -107,20 +107,7 @@ if ($submission->id and !$workshop->modifying_submission_allowed($USER->id)) {
     $editable = false;
 }
 
-if ($canviewall) {
-    // check this flag against the group membership yet
-    if (groups_get_activity_groupmode($workshop->cm) == SEPARATEGROUPS) {
-        // user must have accessallgroups or share at least one group with the submission author
-        if (!has_capability('moodle/site:accessallgroups', $workshop->context)) {
-            $usersgroups = groups_get_activity_allowed_groups($workshop->cm);
-            $authorsgroups = groups_get_all_groups($workshop->course->id, $submission->authorid, $workshop->cm->groupingid, 'g.id');
-            $sharedgroups = array_intersect_key($usersgroups, $authorsgroups);
-            if (empty($sharedgroups)) {
-                $canviewall = false;
-            }
-        }
-    }
-}
+$canviewall = $canviewall && $workshop->check_group_membership($submission->authorid);
 
 if ($editable and $workshop->useexamples and $workshop->examplesmode == workshop::EXAMPLES_BEFORE_SUBMISSION
         and !has_capability('mod/workshop:manageexamples', $workshop->context)) {
