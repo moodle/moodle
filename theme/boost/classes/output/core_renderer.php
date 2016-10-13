e226461dabbb23145a8a494871efc9132d908b21
Damyon Wiese <damyon@moodle.com>
2016-10-13 11:38:17 +0800

    MDL-55074 navigation: Add editing links back to nav
    In theme boost - don't support edit_buttons in the page.

--- theme/boost/classes/output/core_renderer.php
+++ theme/boost/classes/output/core_renderer.php
@@ -116,6 +116,14 @@ class core_renderer extends \core_renderer {
     }
 
     /**
+     * We don't like these...
+     *
+     */
+    public function edit_button(moodle_url $url) {
+        return '';
+    }
+
+    /**
      * Override to inject the logo.
      *
      * @param array $headerinfo The header info.
