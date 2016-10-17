19f8cd61b0ddb3b98de9ee276c9cd5f545522f2c
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2016-10-17 14:30:47 +0100

    MDL-53366 antivirus_clamav: Remove "Quarantine directory" setting.

--- lib/antivirus/clamav/settings.php
+++ lib/antivirus/clamav/settings.php
@@ -27,8 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configexecutable('antivirus_clamav/pathtoclam',
             new lang_string('pathtoclam', 'antivirus_clamav'), new lang_string('configpathtoclam', 'antivirus_clamav'), ''));
-    $settings->add(new admin_setting_configdirectory('antivirus_clamav/quarantinedir',
-            new lang_string('quarantinedir', 'antivirus_clamav'), new lang_string('configquarantinedir', 'antivirus_clamav'), ''));
     $options = array(
         'donothing' => new lang_string('configclamdonothing', 'antivirus_clamav'),
         'actlikevirus' => new lang_string('configclamactlikevirus', 'antivirus_clamav'),
