8fbdc47ab8c708c0b4d915a14b64c48ec769754b
Stephen Bourget <steve.bourget@sau19.org>
2016-10-17 16:19:28 -0400

    MDL-56022 Assign: Make folders structure in the downloaded zip optional
    Special thanks to Matt McDermott for the contributing the Javascript
    code to fix this issue.

--- mod/assign/submission/file/locallib.php
+++ mod/assign/submission/file/locallib.php
@@ -304,7 +304,12 @@ class assign_submission_file extends assign_submission_plugin {
                                      false);
 
         foreach ($files as $file) {
-            $result[$file->get_filepath().$file->get_filename()] = $file;
+            // Do we return the full folder path or just the file name?
+            if (isset($submission->exportfullpath) && $submission->exportfullpath == false) {
+                $result[$file->get_filename()] = $file;
+            } else {
+                $result[$file->get_filepath().$file->get_filename()] = $file;
+            }
         }
         return $result;
     }
