057e1a1f6d5eca18a02d46b4884e3948bf9eeb70
Damyon Wiese <damyon@moodle.com>
2016-10-31 22:49:39 +0800

    MDL-56576 admin: Cannot edit blocks on search page
    Admin search page was processing the blocks delete request before the blocks
    could process it.
    
    Added a hidden field to the search results form so it can be sure the form submission
    was for saving admin settings.

--- admin/search.php
+++ admin/search.php
@@ -19,7 +19,7 @@ $errormsg  = '';
 $focus = '';
 
 // now we'll deal with the case that the admin has submitted the form with changed settings
-if ($data = data_submitted() and confirm_sesskey()) {
+if ($data = data_submitted() and confirm_sesskey() and isset($data->action) and $data->action == 'save-settings') {
     require_capability('moodle/site:config', $context);
     if (admin_write_settings($data)) {
         redirect($PAGE->url, get_string('changessaved'), null, \core\output\notification::NOTIFY_SUCCESS);
