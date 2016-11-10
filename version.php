a788beadce7da64cccbb80cf722efa60129bc6e2
Dan Poltawski <dan@moodle.com>
2016-11-10 13:48:16 +0000

    MDL-55777 version: bump

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2015111606.13;              // 20151116      = branching date YYYYMMDD - do not modify!
+$version  = 2015111606.14;              // 20151116      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
