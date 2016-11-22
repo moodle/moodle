862816eec38295ba45efc40d11f4d89dccb06c44
Frederic Massart <fred@moodle.com>
2016-11-22 16:58:43 +0800

    MDL-56116 theme_boost: Improve developer documentation related to themes

--- theme/boost/classes/autoprefixer.php
+++ theme/boost/classes/autoprefixer.php
@@ -17,6 +17,10 @@
 /**
  * Autoprefixer.
  *
+ * This autoprefixer has been developed to satisfy the basic needs of the
+ * theme Boost when working with Bootstrap 4 alpha. We do not recommend
+ * that this tool is shared, nor used outside of this theme.
+ *
  * @package    theme_boost
  * @copyright  2016 Frédéric Massart - FMCorz.net
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
