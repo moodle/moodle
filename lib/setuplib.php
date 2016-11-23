d5c2829b06b65f74dc945dd40c20c72e7bf5537d
David Monllao <davidm@moodle.com>
2016-11-23 11:50:26 +0800

    MDL-56902 version: Triggering a major upgrade

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -1391,7 +1391,7 @@ function disable_output_buffering() {
  */
 function redirect_if_major_upgrade_required() {
     global $CFG;
-    $lastmajordbchanges = 2016112200.01;
+    $lastmajordbchanges = 2016112200.03;
     if (empty($CFG->version) or (float)$CFG->version < $lastmajordbchanges or
             during_initial_install() or !empty($CFG->adminsetuppending)) {
         try {
