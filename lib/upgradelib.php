a0f554f012bd5c80b52a4f2bb295d0e22f791f49
Vladimir Zhirov <vvzh@users.noreply.github.com>
2016-11-26 23:42:54 +0400

    MDL-57163 installation: fixed "Call to undefined function curl_version"

--- lib/upgradelib.php
+++ lib/upgradelib.php
@@ -2453,6 +2453,12 @@ function upgrade_install_plugins(array $installable, $confirmed, $heading='', $c
  */
 function check_libcurl_version(environment_results $result) {
 
+    if (!function_exists('curl_version')) {
+        $result->setInfo('cURL PHP extension is not installed');
+        $result->setStatus(false);
+        return $result;
+    }
+
     // Supported version and version number.
     $supportedversion = 0x071304;
     $supportedversionstring = "7.19.4";
