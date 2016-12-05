ebcff7e25611e358c270c915e37da7874400176a
Marina Glancy <marina@moodle.com>
2016-12-05 11:12:30 +0800

    MDL-56614 behat: new steps for navigation without blocks

--- grade/grading/tests/behat/behat_grading.php
+++ grade/grading/tests/behat/behat_grading.php
@@ -49,13 +49,8 @@ class behat_grading extends behat_base {
 
         $this->execute('behat_general::click_link', $this->escape($activityname));
 
-        $this->execute('behat_general::i_click_on_in_the',
-            array(
-                get_string('gradingmanagement', 'grading'),
-                'link',
-                'Administration',
-                'block'
-            ));
+        $this->execute('behat_navigation::i_navigate_to_in_current_page_administration',
+            get_string('gradingmanagement', 'grading'));
     }
 
     /**
@@ -92,13 +87,8 @@ class behat_grading extends behat_base {
 
         $this->execute('behat_general::click_link', $this->escape($activityname));
 
-        $this->execute('behat_general::i_click_on_in_the',
-            array(
-                get_string('viewgrading', 'mod_assign'),
-                'link',
-                'Administration',
-                'block'
-            ));
+        $this->execute('behat_navigation::i_navigate_to_in_current_page_administration',
+            get_string('viewgrading', 'mod_assign'));
 
         $this->execute('behat_general::i_click_on_in_the',
                        array(
@@ -169,13 +159,8 @@ class behat_grading extends behat_base {
         $this->execute('behat_forms::press_button', 'Ok');
         $this->execute('behat_general::i_click_on', array($this->escape(get_string('editsettings')), 'link'));
         $this->execute('behat_forms::press_button', get_string('cancel'));
-        $this->execute('behat_general::i_click_on_in_the',
-            array(
-                get_string('viewgrading', 'mod_assign'),
-                'link',
-                'Administration',
-                'block'
-            ));
+        $this->execute('behat_navigation::i_navigate_to_in_current_page_administration',
+            get_string('viewgrading', 'mod_assign'));
     }
 
     /**
