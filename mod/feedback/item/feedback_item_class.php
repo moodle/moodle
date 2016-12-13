e02a11ad0a97a96b72fd8938e51abec4e1356901
Marina Glancy <marina@moodle.com>
2016-12-13 14:49:46 +0800

    MDL-56912 feedback: empty multichoice questions do not submit

--- mod/feedback/item/feedback_item_class.php
+++ mod/feedback/item/feedback_item_class.php
@@ -181,6 +181,7 @@ abstract class feedback_item_base {
      * If it is important which mode the form is in, use $form->get_mode()
      *
      * Each item type must add a single form element with the name $item->typ.'_'.$item->id
+     * This element must always be present in form data even if nothing is selected (i.e. use advcheckbox and not checkbox).
      * To add an element use either:
      * $form->add_form_element() - adds a single element to the form
      * $form->add_form_group_element() - adds a group element to the form
