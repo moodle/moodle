7d9ee66a2f3cbbe16066be5ab3c27346ef6464ba
Ryan Wyllie <ryan@moodle.com>
2016-12-21 05:17:25 +0000

    MDL-57370 message: change indexes on message tables

--- message/output/popup/version.php
+++ message/output/popup/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016120500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2016122100;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016112900;        // Requires this Moodle version
 $plugin->component = 'message_popup';  // Full name of the plugin (used for diagnostics)
--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2016120501.04;              // 20161205      = branching date YYYYMMDD - do not modify!
+$version  = 2017020200.02;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
