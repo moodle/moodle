90914d995fd4879463c64767b37e225e9beb3791
Howard Miller <howardsmiller@gmail.com>
2016-12-31 16:19:28 +0000

    IOMAD: bootstrap themes are now broken, so removing for now

--- theme/iomadbootstrap/javascript/moodlebootstrap.js
+++
@@ -1,5 +0,0 @@
-require(['core/first'], function() {
-    require(['theme_iomadbootstrap/bootstrap', 'core/log'], function(b, log) {
-        log.debug('iomadbootstrap JavaScript initialised');
-    });
-});
