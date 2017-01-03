7440c7b2986ce0234277e945cb05be9ff52250dc
Juan Leyva <juanleyvadelgado@gmail.com>
2017-01-03 11:59:51 +0100

    MDL-50542 mod_label: New WS mod_label_get_labels_by_courses

--- mod/label/db/services.php
+++ mod/label/db/services.php
@@ -15,16 +15,26 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Label module version info
+ * Label external functions and service definitions.
  *
- * @package mod_label
- * @copyright  2003 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @package    mod_label
+ * @category   external
+ * @copyright  2017 Juan Leyva <juan@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @since      Moodle 3.3
  */
 
-defined('MOODLE_INTERNAL') || die();
+defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016120501;       // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2016112900;    // Requires this Moodle version
-$plugin->component = 'mod_label'; // Full name of the plugin (used for diagnostics)
-$plugin->cron      = 0;
+$functions = array(
+
+    'mod_label_get_labels_by_courses' => array(
+        'classname'     => 'mod_label_external',
+        'methodname'    => 'get_labels_by_courses',
+        'description'   => 'Returns a list of labels in a provided list of courses, if no list is provided all labels that the user
+                            can view will be returned.',
+        'type'          => 'read',
+        'capabilities'  => 'mod/label:view',
+        'services'      => array(MOODLE_OFFICIAL_MOBILE_SERVICE),
+    ),
+);
