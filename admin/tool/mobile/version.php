63d142e2cdd3d40cc5f6512542bd094558fe2cf5
Juan Leyva <juanleyvadelgado@gmail.com>
2017-01-11 15:04:58 +0100

    MDL-57410 tool_mobile: New setting for adding menu items in the app

--- admin/tool/mobile/version.php
+++ admin/tool/mobile/version.php
@@ -23,7 +23,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2016120503; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016120504; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'tool_mobile'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = array(
