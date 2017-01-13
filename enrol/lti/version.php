f52bdbb885c8291f394c7edce1883a8abfe47d82
John Okely <john@moodle.com>
2017-01-13 10:52:24 +0800

    MDL-57613 enrol_lti: Allow longer nonces

--- enrol/lti/version.php
+++ enrol/lti/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016120500; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2016120501; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2016112900; // Requires this Moodle version (3.1)
 $plugin->component = 'enrol_lti'; // Full name of the plugin (used for diagnostics).
