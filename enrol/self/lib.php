49053808a26b16cfddd90bea3c836685ef146191
Marina Glancy <marina@moodle.com>
2017-01-27 14:24:10 +0800

    MDL-57782 enrol_self: fix check when email needs to be sent
    Thanks to Leandro for providing a fix

--- enrol/self/lib.php
+++ enrol/self/lib.php
@@ -173,7 +173,7 @@ class enrol_self_plugin extends enrol_plugin {
             }
         }
         // Send welcome message.
-        if ($instance->customint4 !== ENROL_DO_NOT_SEND_EMAIL) {
+        if ($instance->customint4 != ENROL_DO_NOT_SEND_EMAIL) {
             $this->email_welcome_message($instance, $USER);
         }
     }
