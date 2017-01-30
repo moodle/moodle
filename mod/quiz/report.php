ef8ceb2fc25550ee77c2c14102fa6c3a692b7edb
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-01-30 13:37:19 +1100

    MDL-57809 weblib: Added missing NO_OUTPUT_BUFFERING directive.

--- mod/quiz/report.php
+++ mod/quiz/report.php
@@ -22,6 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+define('NO_OUTPUT_BUFFERING', true);
 
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->dirroot . '/mod/quiz/locallib.php');
