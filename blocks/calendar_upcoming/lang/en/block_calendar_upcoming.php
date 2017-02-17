6fead54ef6256abf31d955b8e9f4762f71fe4cec
Samuel Richardson <samuel.richardson@prismaustralia.com>
2017-02-17 22:41:27 +1100

    rename lang string

--- blocks/calendar_upcoming/lang/en/block_calendar_upcoming.php
+++ blocks/calendar_upcoming/lang/en/block_calendar_upcoming.php
@@ -24,4 +24,4 @@
 
 $string['calendar_upcoming:addinstance'] = 'Add a new upcoming events block';
 $string['calendar_upcoming:myaddinstance'] = 'Add a new upcoming events block to Dashboard';
-$string['pluginname'] = 'Upcoming events';
+$string['pluginname'] = 'Reminders';
