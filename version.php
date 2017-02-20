bf313ce20217f40d6b54e7828be1bcfa420c97d2
Ryan Wyllie <ryan@moodle.com>
2017-02-20 02:56:04 +0000

    MDL-57502 core: version bump for action event webservice
    Part of MDL-55611 epic.

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2017040300.07;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2017040300.08;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
