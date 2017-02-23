d0209e611f2cc59ca04cbc45824ed77a4d1af6ab
David Monllao <david.monllao@gmail.com>
2017-02-23 08:07:49 +0100

    weekly release 3.2.1+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2016120501.08;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2016120501.09;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.2.1+ (Build: 20170216)'; // Human-friendly version name
+$release  = '3.2.1+ (Build: 20170223)'; // Human-friendly version name
 
 $branch   = '32';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
