d247a63dfa49661c00499be26b352009ef3d4ef0
Damyon Wiese <damyon@moodle.com>
2017-03-05 14:28:33 +0800

    MDL-58128 oauth2: Add a scheduled task for refresh
    We need to make sure our refresh tokens do not expire. We run a scheduled
    task to update the refresh token once per hour.
    
    Part of MDL-58220

--- lib/db/tasks.php
+++ lib/db/tasks.php
@@ -347,4 +347,13 @@ $tasks = array(
         'dayofweek' => '*',
         'month' => '*'
     ),
+    array(
+        'classname' => 'core\oauth2\refresh_system_tokens_task',
+        'blocking' => 0,
+        'minute' => 'R',
+        'hour' => '*',
+        'day' => '*',
+        'dayofweek' => '*',
+        'month' => '*'
+    ),
 );
