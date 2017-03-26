095c66a13f1e68373d4db0218775b1343b6dba5b
Howard Miller <howardsmiller@gmail.com>
2017-03-26 22:04:03 +0100

    IOMAD: adding webservice test clients to iomad_company_admin block

---
+++ blocks/iomad_company_admin/wsclients/create_company.php
@@ -0,0 +1,40 @@
+<?php
+// This file is NOT a part of Moodle - http://moodle.org/
+//
+// This client for Moodle 2 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+
+/**
+ * XML-RPC client for Moodle 2
+ *
+ * @authorr Jerome Mouneyrac
+ */
+
+/// SETUP - NEED TO BE CHANGED
+$token = 'de12b1fc61ed026d4038eaef7b17deac';
+$domainname = 'http://localhost/iomad32';
+$functionname = 'block_iomad_company_admin_create_companies';
+
+/// PARAMETERS
+$company1 = new stdClass;
+$company1->name = 'Sproatly Sprockets';
+$company1->shortname = 'sproatly';
+$company1->city = 'Glasgow';
+$company1->country = 'UK';
+
+$params = array(
+    $company1,
+);
+
+/// XML-RPC CALL
+header('Content-Type: text/plain');
+$serverurl = $domainname . '/webservice/xmlrpc/server.php'. '?wstoken=' . $token;
+require_once('./curl.php');
+$curl = new curl;
+$post = xmlrpc_encode_request($functionname, array($params));
+//var_dump($post); die;
+$resp = xmlrpc_decode($curl->post($serverurl, $post));
+print_r($resp);
