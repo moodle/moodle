e25b109131656cd2fa8af89704d3f3bde9a019bf
Howard Miller <howardsmiller@gmail.com>
2017-03-28 13:52:53 +0100

    IOMAD: setup config for wsclient tests

--- blocks/iomad_company_admin/wsclients/config.php
+++ blocks/iomad_company_admin/wsclients/config.php
@@ -2,4 +2,4 @@
 
 /// SETUP - NEED TO BE CHANGED
 $token = '471673db5d6b00d1cc305175196aacd7';
-$domainname = 'http://pc164-239.cent.gla.ac.uk/iomad32';
+$domainname = 'http://your.site.com/iomad32';
