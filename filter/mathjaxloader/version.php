d77585c67225efdf5b1293716663d1f692e4197f
Damyon Wiese <damyon@moodle.com>
2017-04-03 10:32:31 +0800

    MDL-58461 filter_mathjaxloader: Change CDN url
    The MathJax CDN url is going away - we need to switch to another mirror.

--- filter/mathjaxloader/version.php
+++ filter/mathjaxloader/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016052300;
+$plugin->version  = 2016052301;
 $plugin->requires = 2016051900;  // Requires this Moodle version.
 $plugin->component= 'filter_mathjaxloader';
