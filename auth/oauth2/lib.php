340659cc298fd7bc64f680f82c53386c60175325
Damyon Wiese <damyon@moodle.com>
2017-04-06 16:06:12 +0800

    MDL-58220 auth_oauth2: Linked logins only 4U
    You can only manage your own linked logins - never anyone elses.

--- auth/oauth2/lib.php
+++ auth/oauth2/lib.php
@@ -38,9 +38,10 @@ function auth_oauth2_extend_navigation_user_settings(navigation_node $useraccoun
                                                      context_user $context,
                                                      stdClass $course,
                                                      context_course $coursecontext) {
+    global $USER;
 
     if (!\core\session\manager::is_loggedinas()) {
-        if (has_capability('auth/oauth2:managelinkedlogins', $context)) {
+        if (has_capability('auth/oauth2:managelinkedlogins', $context) && $user->id == $USER->id) {
 
             $parent = $useraccount->parent->find('useraccount', navigation_node::TYPE_CONTAINER);
             $parent->add(get_string('linkedlogins', 'auth_oauth2'), new moodle_url('/auth/oauth2/linkedlogins.php'));
