9e452e35d0a9c72c72a256659b7af557f9bbe6ce
Ryan Wyllie <ryan@moodle.com>
2017-05-08 02:13:14 +0000

    MDL-58822 backup: backup and restore user override events

--- mod/assign/backup/moodle2/restore_assign_stepslib.php
+++ mod/assign/backup/moodle2/restore_assign_stepslib.php
@@ -65,8 +65,9 @@ class restore_assign_activity_structure_step extends restore_activity_structure_
             $userflag = new restore_path_element('assign_userflag',
                                                    '/activity/assign/userflags/userflag');
             $paths[] = $userflag;
-            $paths[] = new restore_path_element('assign_override', '/activity/assign/overrides/override');
         }
+
+        $paths[] = new restore_path_element('assign_override', '/activity/assign/overrides/override');
         $paths[] = new restore_path_element('assign_plugin_config',
                                             '/activity/assign/plugin_configs/plugin_config');
 
