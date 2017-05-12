49fa410dd198f2827bb84007de087236e4e92392
Dan Poltawski <dan@moodle.com>
2017-05-12 10:02:41 +0100

    Moodle release 3.3rc3

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2017050900.01;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2017051200.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.3rc2 (Build: 20170509)'; // Human-friendly version name
+$release  = '3.3rc3 (Build: 20170512)'; // Human-friendly version name
 
 $branch   = '33';                       // This version's branch.
 $maturity = MATURITY_RC;             // This version's maturity level.
