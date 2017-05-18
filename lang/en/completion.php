7b4eb6082f706093ae50440e829af810e3f69005
Dan Poltawski <dan@moodle.com>
2017-05-18 09:54:11 +0100

    MDL-58966 completion: improve dashboard string

--- lang/en/completion.php
+++ lang/en/completion.php
@@ -83,7 +83,7 @@ $string['completionenabled'] = 'Enabled, control via completion and activity set
 $string['completionexpected'] = 'Expect completed on';
 $string['completionexpected_help'] = 'This setting specifies the date when the activity is expected to be completed.';
 $string['completionexpecteddesc'] = 'Completion expected on {$a}';
-$string['completionexpectedfor'] = 'Expected completion for \'{$a->modulename}\' activity \'{$a->instancename}\'';
+$string['completionexpectedfor'] = '{$a->instancename} should be completed';
 $string['completionicons'] = 'Completion tick boxes';
 $string['completionicons_help'] = 'A tick next to an activity name may be used to indicate when the activity is complete.
 
