746702c88d1234f18632f5299174640b4f71bf88
Matteo Scaramuccia <moodle@matteoscaramuccia.com>
2017-06-04 16:07:14 +0200

    MDL-59109 scorm: Save autocommit setting in the backup.

--- mod/scorm/backup/moodle2/backup_scorm_stepslib.php
+++ mod/scorm/backup/moodle2/backup_scorm_stepslib.php
@@ -46,7 +46,8 @@ class backup_scorm_activity_structure_step extends backup_activity_structure_ste
             'auto', 'popup', 'options', 'width',
             'height', 'timeopen', 'timeclose', 'timemodified',
             'completionstatusrequired', 'completionscorerequired',
-            'completionstatusallscos', 'displayactivityname'));
+            'completionstatusallscos', 'displayactivityname',
+            'autocommit'));
 
         $scoes = new backup_nested_element('scoes');
 
