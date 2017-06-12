481220cb4c3d50f689fb5289f4e8355964d731f1
Mark Nelson <markn@moodle.com>
2017-06-12 11:50:14 +0800

    MDL-59140 core: upgrade 'navcourselimit' if set to old default value

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2865,5 +2865,15 @@ function xmldb_main_upgrade($oldversion) {
     // Automatically generated Moodle v3.3.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2017051500.06) {
+        // Check if the value of 'navcourselimit' is set to the old default value, if so, change it to the new default.
+        if ($CFG->navcourselimit == 20) {
+            set_config('navcourselimit', 10);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2017051500.06);
+    }
+
     return true;
 }
