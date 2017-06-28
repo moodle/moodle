6b7a32f33c8fca6843b3d91e5fc0aea37bcb2253
Marina Glancy <marina@moodle.com>
2017-06-28 12:12:48 +0800

    MDL-56579 media_videojs: use new config element for selecting file types

--- media/player/videojs/settings.php
+++ media/player/videojs/settings.php
@@ -26,15 +26,15 @@ defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
 
-    $settings->add(new admin_setting_configtext('media_videojs/videoextensions',
+    $settings->add(new admin_setting_filetypes('media_videojs/videoextensions',
         new lang_string('videoextensions', 'media_videojs'),
         new lang_string('configvideoextensions', 'media_videojs'),
-        '.mov, .mp4, .m4v, .ogv, .webm, .flv, .f4v'));
+        'html_video,.f4v,.flv'));
 
-    $settings->add(new admin_setting_configtext('media_videojs/audioextensions',
+    $settings->add(new admin_setting_filetypes('media_videojs/audioextensions',
         new lang_string('audioextensions', 'media_videojs'),
         new lang_string('configaudioextensions', 'media_videojs'),
-        '.aac, .flac, .mp3, .m4a, .oga, .ogg, .wav'));
+        'html_audio'));
 
     $settings->add(new admin_setting_configcheckbox('media_videojs/useflash',
         new lang_string('useflash', 'media_videojs'),
