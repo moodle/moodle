2109b3298472c6f25ee38d90e747958a9bb4bdc1
Adrian Greeve <adrian@moodle.com>
2017-06-29 09:07:50 +0800

    MDL-59287 core_completion: Update to unit tests.

--- course/tests/courselib_test.php
+++ course/tests/courselib_test.php
@@ -3214,6 +3214,8 @@ class core_course_courselib_testcase extends advanced_testcase {
 
         $this->resetAfterTest(true);
 
+        $this->setAdminUser();
+
         $CFG->enablecompletion = true;
 
         $this->setTimezone('UTC');
