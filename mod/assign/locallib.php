1288cf286e85a68c8fe9e0008217ca4362e6b1a8
trevor <trevor@closethedistance.com>
2017-07-07 15:45:21 -0400

    Modified filename in locallib.php of assign module

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -2934,10 +2934,14 @@ class assign {
 
                 if ($this->is_blind_marking()) {
                     $prefix = str_replace('_', ' ', $groupname . get_string('participant', 'assign'));
-                    $prefix = clean_filename($prefix . '_' . $this->get_uniqueid_for_user($userid));
+                    $prefix = clean_filename($prefix . '-' . $this->get_uniqueid_for_user($userid));
                 } else {
-                    $prefix = str_replace('_', ' ', $groupname . $student->lastname . '_' . $student->firstname);
-                    $prefix = clean_filename($prefix . '_' . $this->get_uniqueid_for_user($userid));
+                    if ( $groupname ) {
+                        $prefix = $groupname . '-' . $student->lastname . '_' . $student->firstname);
+                    } else {
+                        $prefix = $student->lastname . '_' . $student->firstname);
+                    }
+                    $prefix = clean_filename($prefix . '-' . $this->get_uniqueid_for_user($userid));
                 }
 
                 if ($submission) {
