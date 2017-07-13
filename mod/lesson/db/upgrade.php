63cf03265b46f21bc3c028a86a983dded3037a67
Adrian Greeve <adrian@moodle.com>
2017-07-13 12:30:35 +0800

    MDL-57649 mod_lesson: Delete answer files correctly.
    The files associated with answers and responses were
    not being deleted due to the fact that the wrong itemid
    was being used.

--- mod/lesson/db/upgrade.php
+++ mod/lesson/db/upgrade.php
@@ -141,5 +141,24 @@ function xmldb_lesson_upgrade($oldversion) {
     // Automatically generated Moodle v3.3.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2017051501) {
+
+        // Delete orphaned lesson answer and response files.
+        $sql = "SELECT DISTINCT f.*
+                  FROM {files} f
+             LEFT JOIN {lesson_answers} la ON f.itemid = la.id
+                 WHERE component = :component
+                   AND la.id IS NULL";
+
+        $orphanedfiles = $DB->get_recordset_sql($sql, array('component' => 'mod_lesson'));
+        $fs = get_file_storage();
+        foreach ($orphanedfiles as $file) {
+            $fs->delete_area_files($file->contextid, $file->component, $file->filearea, $file->itemid);
+        }
+        $orphanedfiles->close();
+
+        upgrade_mod_savepoint(true, 2017051501, 'lesson');
+    }
+
     return true;
 }
