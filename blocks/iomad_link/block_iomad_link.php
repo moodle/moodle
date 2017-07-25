350784bf6d1ab84f33a1b0a5e2258765d91ba4aa
Derick Turner <derick@e-learndesign.co.uk>
2017-07-25 11:11:33 +0100

    IOMAD: Convert iomad_link block to use standard theme image URLS. closes #586

--- blocks/iomad_link/block_iomad_link.php
+++ blocks/iomad_link/block_iomad_link.php
@@ -42,7 +42,7 @@ class block_iomad_link extends block_base {
         $strlink = get_string('link', 'block_iomad_link');
         $this->content = new stdClass;
         $this->content->text = "<center><a href=\"{$CFG->wwwroot}/local/iomad_dashboard/index.php\">
-                                <img src=\"{$CFG->wwwroot}/blocks/iomad_link/image/iomad_logo.png\" /></a></center>";
+                                <img src='" . $OUTPUT->pix_url('iomad_logo', 'block_iomad_link') ."' /></a></center>";
         $this->content->text .= "<center><b><a href=\"{$CFG->wwwroot}/local/iomad_dashboard/index.php\">$strlink</a></b></center>";
         $this->content->footer = '';
 
