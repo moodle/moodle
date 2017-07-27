9064d66de9dbf17acd783acd885b29ad2e02b342
Derick Turner <derick@e-learndesign.co.uk>
2017-07-27 23:09:02 +0100

    IOMAD: sync for department and company information not working on all DB types closes #589

--- local/iomad/classes/task/cron_task.php
+++ local/iomad/classes/task/cron_task.php
@@ -22,19 +22,44 @@ class cron_task extends \core\task\scheduled_task {
         // Are we copying Company to institution?
         if (!empty($CFG->iomad_sync_institution)) {
             mtrace("Copying company shortnames to user institution fields\n");
-            $DB->execute("UPDATE {user} u
-                          JOIN {company_users} cu ON cu.userid = u.id
-                          JOIN {company} c ON cu.companyid = c.id
-                          SET u.institution = c.shortname where u.institution != c.shortname");
+            // Get the users where it's wrong.
+            $users = $DB->get_records_sql("SELECT u.*, c.id as companyid
+                                           FROM {user} u
+                                           JOIN {company_users} cu ON cu.userid = u.id
+                                           JOIN {company} c ON cu.companyid = c.id
+                                           WHERE u.institution != c.shortname
+                                           AND c.parentid = 0");
+            // Get all of the companies.
+            $companies = $DB->get_records('company', array(), '', 'id,shortname');
+            foreach ($users as $user) {
+mtrace("setting user id " . $user->id . " institution to " .  $companies[$user->companyid]->shortname);
+                $user->institution = $companies[$user->companyid]->shortname;
+                $DB->update_record('user', $user);
+            }
+            $companies = array();
+            $users = array();
         }
 
         // Are we copying department to department?
         if (!empty($CFG->iomad_sync_department)) {
             mtrace("Copying company department name to user department fields\n");
-            $DB->execute("UPDATE {user} u
-                          JOIN {company_users} cu ON cu.userid = u.id
-                          JOIN {department} d ON cu.departmentid = d.id
-                          SET u.department = d.name where u.departname != d.name");
+            // Get the users where it's wrong.
+            $users = $DB->get_records_sql("SELECT u.*, d.id as departmentid
+                                           FROM {user} u
+                                           JOIN {company_users} cu ON cu.userid = u.id
+                                           JOIN {company} c ON cu.companyid = c.id
+                                           JOIN {department} d ON cu.departmentid = d.id
+                                           WHERE u.department != d.name
+                                           AND c.parentid = 0");
+            // Get all of the companies.
+            $departments = $DB->get_records('department', array(), '', 'id,name');
+            foreach ($users as $user) {
+mtrace("setting user id " . $user->id . " department to " .  $departments[$user->departmentid]->name);
+                $user->department = $departments[$user->departmentid]->name;
+                $DB->update_record('user', $user);
+            }
+            $companies = array();
+            $users = array();
         }
     }
 }
