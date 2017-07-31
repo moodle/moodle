b5977a3fdd40a9a3c709826487238825d19b65ab
Howard Miller <howardsmiller@gmail.com>
2017-07-31 13:21:46 +0100

    IOMAD: logos now correctly displayed in iomadboost theme

---
+++ theme/iomadboost/classes/output/core_renderer.php
@@ -0,0 +1,205 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace theme_iomadboost\output;
+
+use coding_exception;
+use html_writer;
+use tabobject;
+use tabtree;
+use custom_menu_item;
+use custom_menu;
+use block_contents;
+use navigation_node;
+use action_link;
+use stdClass;
+use moodle_url;
+use preferences_groups;
+use action_menu;
+use help_icon;
+use single_button;
+use paging_bar;
+use context_course;
+use pix_icon;
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Renderers to align Moodle's HTML with that expected by Bootstrap
+ *
+ * @package    theme_boost
+ * @copyright  2012 Bas Brands, www.basbrands.nl
+ * @copyright  2017 Howard Miller
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class core_renderer extends \theme_boost\output\core_renderer {
+
+    /**
+     * Override to inject the logo.
+     *
+     * @param array $headerinfo The header info.
+     * @param int $headinglevel What level the 'h' tag will be.
+     * @return string HTML for the header bar.
+     */
+    public function context_header($headerinfo = null, $headinglevel = 1) {
+        global $SITE;
+
+        // get appropriate logo
+        if (!$src = $this->get_iomad_logo(null, 150)) {
+            $src = $this->get_logo_url(null, 150);
+        }
+
+        if ($this->should_display_main_logo($headinglevel)) {
+            $sitename = format_string($SITE->fullname, true, array('context' => context_course::instance(SITEID)));
+            return html_writer::div(html_writer::empty_tag('img', [
+                'src' => $src, 'alt' => $sitename]), 'logo');
+        }
+
+        return parent::context_header($headerinfo, $headinglevel);
+    }
+
+    /**
+     * Get the Iomad logo for the current user
+     * @return string logo url or false;
+     */
+    protected function get_iomad_logo($maxwidth = 100, $maxheight = 100) {
+        global $CFG; $DB;
+
+        $fs = get_file_storage();
+
+        $clientlogo = '';
+        $companyid = \iomad::is_company_user();
+        if ($companyid) {
+            $context = \context_system::instance();
+            $files = $fs->get_area_files($context->id, 'theme_iomad', 'companylogo', $companyid );
+            if ($files) {
+                foreach ($files as $file) {
+                    $filename = $file->get_filename();
+                    $filepath = ((int) $maxwidth . 'x' . (int) $maxheight) . '/';
+                    if ($filename != '.') {
+                        $clientlogo = $CFG->wwwroot . "/pluginfile.php/{$context->id}/theme_iomad/companylogo/$companyid/$filename";
+                        return $clientlogo;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the compact logo URL.
+     *
+     * @return string
+     */
+    public function get_compact_logo_url($maxwidth = 100, $maxheight = 100) {
+        global $CFG;
+
+        if ($url = $this->get_iomad_logo($maxwidth, $maxheight)) {
+            return $url;
+        } else {
+
+            // If that didn't work... try the original version
+            return parent::get_compact_logo_url($maxwidth, $maxheight);
+        }
+    }
+
+    /**
+     * Whether we should display the main logo.
+     *
+     * @return bool
+     */
+    public function should_display_main_logo($headinglevel = 1) {
+        global $PAGE;
+
+        // Only render the logo if we're on the front page or login page and the we have a logo.
+        $logo = $this->get_logo_url();
+        if ($headinglevel == 1 && !empty($logo)) {
+            if ($PAGE->pagelayout == 'frontpage' || $PAGE->pagelayout == 'login') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Whether we should display the logo in the navbar.
+     *
+     * We will when there are no main logos, and we have compact logo.
+     *
+     * @return bool
+     */
+    public function should_display_navbar_logo() {
+        return true;
+        //$logo = $this->get_compact_logo_url();
+        //return !empty($logo) && !$this->should_display_main_logo();
+    }
+
+    /**
+     * Take a node in the nav tree and make an action menu out of it.
+     * The links are injected in the action menu.
+     *
+     * @param action_menu $menu
+     * @param navigation_node $node
+     * @param boolean $indent
+     * @param boolean $onlytopleafnodes
+     * @return boolean nodesskipped - True if nodes were skipped in building the menu
+     */
+    private function build_action_menu_from_navigation(action_menu $menu,
+                                                       navigation_node $node,
+                                                       $indent = false,
+                                                       $onlytopleafnodes = false) {
+        $skipped = false;
+        // Build an action menu based on the visible nodes from this navigation tree.
+        foreach ($node->children as $menuitem) {
+            if ($menuitem->display) {
+                if ($onlytopleafnodes && $menuitem->children->count()) {
+                    $skipped = true;
+                    continue;
+                }
+                if ($menuitem->action) {
+                    if ($menuitem->action instanceof action_link) {
+                        $link = $menuitem->action;
+                        // Give preference to setting icon over action icon.
+                        if (!empty($menuitem->icon)) {
+                            $link->icon = $menuitem->icon;
+                        }
+                    } else {
+                        $link = new action_link($menuitem->action, $menuitem->text, null, null, $menuitem->icon);
+                    }
+                } else {
+                    if ($onlytopleafnodes) {
+                        $skipped = true;
+                        continue;
+                    }
+                    $link = new action_link(new moodle_url('#'), $menuitem->text, null, ['disabled' => true], $menuitem->icon);
+                }
+                if ($indent) {
+                    $link->add_class('m-l-1');
+                }
+                if (!empty($menuitem->classes)) {
+                    $link->add_class(implode(" ", $menuitem->classes));
+                }
+
+                $menu->add_secondary_action($link);
+                $skipped = $skipped || $this->build_action_menu_from_navigation($menu, $menuitem, true);
+            }
+        }
+        return $skipped;
+    }
+}
