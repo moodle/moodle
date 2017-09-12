5189f57f38561eb96c81d2c7060e9b49cd55b6f4
Marina Glancy <marina@moodle.com>
2017-09-12 15:19:11 +0800

    MDL-59495 block_community: search courses on moodle.net only
    Remove support for alternative hubs and hub selector

--- blocks/community/locallib.php
+++ blocks/community/locallib.php
@@ -74,66 +74,6 @@ class block_community_manager {
     }
 
     /**
-     * Download the community course backup and save it in file API
-     * @param integer $courseid
-     * @param string $huburl
-     * @return array 'privatefile' the file name saved in private area
-     *               'tmpfile' the file name saved in the moodledata temp dir (for restore)
-     */
-    public function block_community_download_course_backup($course) {
-        global $CFG, $USER;
-        require_once($CFG->libdir . "/filelib.php");
-        require_once($CFG->dirroot. "/course/publish/lib.php");
-
-        $params['courseid'] = $course->id;
-        $params['filetype'] = HUB_BACKUP_FILE_TYPE;
-
-        make_temp_directory('backup');
-
-        $filename = md5(time() . '-' . $course->id . '-'. $USER->id . '-'. random_string(20));
-
-        $url  = new moodle_url($course->huburl.'/local/hub/webservice/download.php', $params);
-        $path = $CFG->tempdir.'/backup/'.$filename.".mbz";
-        $fp = fopen($path, 'w');
-        $curlurl = $course->huburl.'/local/hub/webservice/download.php?filetype='
-                .HUB_BACKUP_FILE_TYPE.'&courseid='.$course->id;
-
-        //send an identification token if the site is registered on the hub
-        require_once($CFG->dirroot . '/' . $CFG->admin . '/registration/lib.php');
-        $registrationmanager = new registration_manager();
-        $registeredhub = $registrationmanager->get_registeredhub($course->huburl);
-        if (!empty($registeredhub)) {
-            $token = $registeredhub->token;
-            $curlurl .= '&token='.$token;
-        }
-
-        $ch = curl_init($curlurl);
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        fclose($fp);
-
-        $fs = get_file_storage();
-        $record = new stdClass();
-        $record->contextid = context_user::instance($USER->id)->id;
-        $record->component = 'user';
-        $record->filearea = 'private';
-        $record->itemid = 0;
-        $record->filename = urlencode($course->fullname)."_".time().".mbz";
-        $record->filepath = '/downloaded_backup/';
-        if (!$fs->file_exists($record->contextid, $record->component,
-                $record->filearea, 0, $record->filepath, $record->filename)) {
-            $fs->create_file_from_pathname($record,
-                    $CFG->tempdir.'/backup/'.$filename.".mbz");
-        }
-
-        $filenames = array();
-        $filenames['privatefile'] = $record->filename;
-        $filenames['tmpfile'] = $filename;
-        return $filenames;
-    }
-
-    /**
      * Delete a community course
      * @param integer $communityid
      * @param integer $userid
