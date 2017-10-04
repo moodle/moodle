5d1ad088eaa1aa831f27250c4649773c6b0d727b
Christian Lawson-Perfect <christianperfect@gmail.com>
2017-10-04 11:27:03 +0100

    MDL-52653 SCORM: fix scorm_check_mode on v1.3
    SCORM 2004 uses the element 'cmi.completion_status' to store the
    completion status of a SCO, not 'cmi.core.lesson_status'.
    
    This amends scorm_check_mode so that the right element is looked up.
    
    When scorm_version_check returns false, SCORM 1.2 is assumed.

--- mod/scorm/lib.php
+++ mod/scorm/lib.php
@@ -1449,12 +1449,23 @@ function scorm_check_mode($scorm, &$newattempt, &$attempt, $userid, &$mode) {
     }
     // Check if the scorm module is incomplete (used to validate user request to start a new attempt).
     $incomplete = true;
+    $completionelements = array(
+        SCORM_12 => 'cmi.core.lesson_status',
+        SCORM_13 => 'cmi.completion_status',
+        SCORM_AICC => 'cmi.core.lesson_status'
+    );
+    $scormversion = scorm_version_check($scorm->version);
+    if($scormversion===false) {
+        $scormversion = SCORM_12;
+    }
+    $completionelement = $completionelements[$scormversion];
+
     $sql = "SELECT sc.id, t.value
               FROM {scorm_scoes} sc
          LEFT JOIN {scorm_scoes_track} t ON sc.scorm = t.scormid AND sc.id = t.scoid
-                   AND t.element = 'cmi.core.lesson_status' AND t.userid = ? AND t.attempt = ?
+                   AND t.element = ? AND t.userid = ? AND t.attempt = ?
              WHERE sc.scormtype = 'sco' AND sc.scorm = ?";
-    $tracks = $DB->get_recordset_sql($sql, array($userid, $attempt, $scorm->id));
+    $tracks = $DB->get_recordset_sql($sql, array($completionelement, $userid, $attempt, $scorm->id));
 
     foreach ($tracks as $track) {
         if (($track->value == 'completed') || ($track->value == 'passed') || ($track->value == 'failed')) {
