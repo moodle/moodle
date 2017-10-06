d8e9a23c48455d9e6a5b544f7782fe22bd1101a6
Jake Dallimore <jake@moodle.com>
2017-10-06 14:34:29 +0800

    weekly release 3.4dev

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2017100500.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2017100600.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.4dev (Build: 20170928)'; // Human-friendly version name
+$release  = '3.4dev (Build: 20171006)'; // Human-friendly version name
 
 $branch   = '34';                       // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
