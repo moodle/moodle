d12e40978415661d39c53f5e95a8f0efa4fe0c9e
David Monllao <davidm@moodle.com>
2017-10-25 11:14:22 +0200

    Moodle release 3.4beta

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2017102100.01;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2017102500.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.4dev+ (Build: 20171021)'; // Human-friendly version name
+$release  = '3.4beta (Build: 20171025)'; // Human-friendly version name
 
 $branch   = '34';                       // This version's branch.
-$maturity = MATURITY_ALPHA;             // This version's maturity level.
+$maturity = MATURITY_BETA;             // This version's maturity level.
