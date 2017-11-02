20b2565c38deeddd5bc5bd3df5866d58f19d9339
David Monllao <davidm@moodle.com>
2017-11-02 16:03:39 +0100

    MDL-60667 mlbackend_python: Bump to non-linear neural net version

--- lib/mlbackend/python/classes/processor.php
+++ lib/mlbackend/python/classes/processor.php
@@ -38,7 +38,7 @@ class processor implements  \core_analytics\classifier, \core_analytics\regresso
     /**
      * The required version of the python package that performs all calculations.
      */
-    const REQUIRED_PIP_PACKAGE_VERSION = '0.0.4';
+    const REQUIRED_PIP_PACKAGE_VERSION = '0.0.5';
 
     /**
      * The path to the Python bin.
