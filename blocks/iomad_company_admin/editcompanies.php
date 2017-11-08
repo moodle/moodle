ea435c4ff6cefcdb2c1db65bcb3d4a0cdbae07e7
Derick Turner <derick@e-learndesign.co.uk>
2017-11-08 13:19:39 +0000

    IOMAD: Manage companies page only shows 30 companies. closes #753

--- blocks/iomad_company_admin/editcompanies.php
+++ blocks/iomad_company_admin/editcompanies.php
@@ -314,7 +314,7 @@ if (!empty($companylist)) {
 } else {
     $companies = array();
 }
-$companycount = count($companies);
+$companycount = count($companyrecords);
 
 if ($companycount == 1) {
     echo $OUTPUT->heading(get_string('companycount', 'block_iomad_company_admin', $companycount));
