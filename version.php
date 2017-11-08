ccf7578373718da40162b6c64e86be77d8f6f333
David Monllao <davidm@moodle.com>
2017-11-08 09:47:39 +0100

    Moodle release 3.4rc2

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2017110300.01;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2017110800.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.4rc1 (Build: 20171103)'; // Human-friendly version name
+$release  = '3.4rc2 (Build: 20171108)'; // Human-friendly version name
 
 $branch   = '34';                       // This version's branch.
 $maturity = MATURITY_RC;             // This version's maturity level.
