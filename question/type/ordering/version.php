63eb9ac1865e1ffaaf9b0098d8aa334f2324e67f
Gordon Bateson <gordonbateson@gmail.com>
2017-11-22 08:39:12 +0900

    MDL-79863 qtype_ordering: qtype_ordering fix issue in Moodle 3.4 in which sorted order is not returned to server for processing

--- question/type/ordering/version.php
+++ question/type/ordering/version.php
@@ -29,5 +29,5 @@ $plugin->cron      = 0;
 $plugin->component = 'qtype_ordering';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->version   = 2017080164;
-$plugin->release   = '2017-08-01 (64)';
+$plugin->version   = 2017112265;
+$plugin->release   = '2017-11-22 (65)';
