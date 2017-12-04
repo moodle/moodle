7391e4a4596d1fdc8df13bed5d5629d3560c47e4
Luca Bösch <luca.boesch@bfh.ch>
2017-12-04 08:47:42 +0100

    MDL-44660 grade: Typos @package core_grade instead of core_grades

--- grade/tests/report_graderlib_test.php
+++ grade/tests/report_graderlib_test.php
@@ -17,7 +17,7 @@
 /**
  * Unit tests for grade/report/user/lib.php.
  *
- * @package  core_grade
+ * @package  core_grades
  * @category phpunit
  * @copyright 2012 Andrew Davis
  * @license  http://www.gnu.org/copyleft/gpl.html GNU Public License
