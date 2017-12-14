124f314d223ebbe09584c287d44fcdaee496f04e
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2017-12-14 16:16:04 +1100

    MDL-19667 report: Add a new capability to view user outline report

--- report/outline/db/access.php
+++ report/outline/db/access.php
@@ -37,6 +37,17 @@ $capabilities = array(
         ),
 
         'clonepermissionsfrom' => 'coursereport/outline:view',
+    ),
+
+    'report/outline:viewuserreport' => array(
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
     )
 );
 
