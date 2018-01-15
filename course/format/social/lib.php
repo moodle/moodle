ef184ad6e07a29c1f7da3ba4365a6884a2eedba2
Juan Leyva <juanleyvadelgado@gmail.com>
2018-01-15 11:08:55 +0100

    MDL-61198 course: New function for format to support external config
    New function to return the course format settings for external clients
    via Web Services.
    Some settings (like private keys/tokens) should be not returned if the
    user hasn’t appropriate permissions.

--- course/format/social/lib.php
+++ course/format/social/lib.php
@@ -120,4 +120,15 @@ class format_social extends format_base {
     public function allow_stealth_module_visibility($cm, $section) {
         return true;
     }
+
+    /**
+     * Return the plugin configs for external functions.
+     *
+     * @return array the list of configuration settings
+     * @since Moodle 3.5
+     */
+    public function get_config_for_external() {
+        // Return everything (nothing to hide).
+        return $this->get_format_options();
+    }
 }
