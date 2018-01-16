489c938b85a4e10963f4f7071d104fbe6438ad63
Gordon Bateson <gordonbateson@gmail.com>
2018-01-17 08:12:54 +0900

    MDL-79863 qtype_ordering: qtype/ordering add AMD folder and files cor compatability with Moodle >= 2.9. Many thanks to Jakob Ackermann (github.com/jacac)

---
+++ question/type/ordering/amd/src/ordering.js
@@ -0,0 +1,17 @@
+define(['jquery', 'jqueryui', 'qtype_ordering/jquery.ui.touch-punch-improved'], function($) {
+
+    return {
+        init: function (sortableid, responseid, ablockid, axis) {
+            $('#' + sortableid).sortable({
+                axis       : axis,
+                containment: $('#'+sortableid),
+                opacity    : 0.6,
+                update     : function () {
+                    var ItemsOrder = $(this).sortable('toArray').toString();
+                    $('#' + responseid).attr('value', ItemsOrder);
+                }
+            });
+        }
+    };
+
+});
