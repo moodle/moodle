6b6fd4b0b7e8983a96463763f925398b5af35082
Luke Carrier <luke@carrier.im>
2018-01-18 11:54:28 +0000

    MDL-61238 cohort: require_once all libraries

--- cohort/edit.php
+++ cohort/edit.php
@@ -23,9 +23,9 @@
  */
 
 require('../config.php');
-require($CFG->dirroot.'/course/lib.php');
-require($CFG->dirroot.'/cohort/lib.php');
-require($CFG->dirroot.'/cohort/edit_form.php');
+require_once($CFG->dirroot.'/course/lib.php');
+require_once($CFG->dirroot.'/cohort/lib.php');
+require_once($CFG->dirroot.'/cohort/edit_form.php');
 
 $id        = optional_param('id', 0, PARAM_INT);
 $contextid = optional_param('contextid', 0, PARAM_INT);
