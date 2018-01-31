749bdfe00dc095ae8f7915fd234b5f208b6eac37
David Mudrák <david@moodle.com>
2018-01-31 23:05:22 +0100

    MDL-61242 repository_equella: Fix missing class repository
    The call to the class 'repository' introduced in the commit d05c2ac7cc78
    didn't fully work and threw: "Exception - Class 'repository' not found".
    This fixes the missing library inclusion.

--- repository/equella/callback.php
+++ repository/equella/callback.php
@@ -23,6 +23,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 require(__DIR__.'/../../config.php');
+require_once($CFG->dirroot . '/repository/lib.php');
+
 $json = required_param('tlelinks', PARAM_RAW);
 
 require_login();
