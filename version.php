e8e497f4d474eba7c73b0fe8e573af94cb47e4e6
Adrian Greeve <adrian@moodle.com>
2018-01-31 18:57:06 +0800

    MDL-61268 blocks: Fix corrupt configdata in block instances.
    Old configuration data in the block instances is base64 encoded with
    the deprecated object class instead of stdClass.

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2017111301.07;              // 20171113      = branching date YYYYMMDD - do not modify!
+$version  = 2017111301.08;              // 20171113      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
