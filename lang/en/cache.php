1ae5e631016f4cd05b3b40ac02a3320102782226
Mihail Geshoski <mihail@moodle.com>
2018-03-08 15:02:16 +0800

    MDL-61423 authentication: Add digital minor verification upon signup

--- lang/en/cache.php
+++ lang/en/cache.php
@@ -62,6 +62,7 @@ $string['cachedef_navigation_expandcourse'] = 'Navigation expandable courses';
 $string['cachedef_observers'] = 'Event observers';
 $string['cachedef_plugin_functions'] = 'Plugins available callbacks';
 $string['cachedef_plugin_manager'] = 'Plugin info manager';
+$string['cachedef_presignup'] = 'Pre sign-up data for particular unregistered user';
 $string['cachedef_postprocessedcss'] = 'Post processed CSS';
 $string['cachedef_tagindexbuilder'] = 'Search results for tagged items';
 $string['cachedef_questiondata'] = 'Question definitions';
