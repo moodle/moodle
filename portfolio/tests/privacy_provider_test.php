e6f06ce99487957e180be8055d3cd339cf39824a
Jake Dallimore <jake@moodle.com>
2018-03-16 12:03:34 +0800

    MDL-61663 core_portfolio: add subsystem privacy provider
    Added provider for portfolio plugins, and legacy polyfill for plugins.

---
+++ portfolio/tests/privacy_provider_test.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Privacy provider tests.
+ *
+ * @package    core_portfolio
+ * @copyright  2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Privacy provider tests class.
+ *
+ * @copyright  2018 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class portfolio_privacy_provider_test extends \core_privacy\tests\provider_testcase {
+
+    /**
+     *  Verify that a collection of metadata is returned for this component and that it just links to the plugintype 'portfolio'.
+     */
+    public function test_get_metadata() {
+        $collection = new \core_privacy\local\metadata\collection('core_portfolio');
+        $collection = \core_portfolio\privacy\provider::get_metadata($collection);
+        $this->assertNotEmpty($collection);
+        $items = $collection->get_collection();
+        $this->assertEquals(1, count($items));
+        $this->assertInstanceOf(\core_privacy\local\metadata\types\plugintype_link::class, $items[0]);
+    }
+}
