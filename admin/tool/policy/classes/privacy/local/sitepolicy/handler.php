6109f2348bfe3e174e310e7b3c5e408137db6916
Sara Arjona <sara@moodle.com>
2018-04-05 14:04:07 +0800

    MDL-61864 tool_policy: display policies to guests and users

---
+++ admin/tool/policy/classes/privacy/local/sitepolicy/handler.php
@@ -0,0 +1,118 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Site policy handler class.
+ *
+ * @package    tool_policy
+ * @copyright  2018 Sara Arjona <sara@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_policy\privacy\local\sitepolicy;
+
+defined('MOODLE_INTERNAL') || die();
+
+use tool_policy\api;
+use tool_policy\policy_version;
+
+/**
+ * Class implementation for a site policy handler.
+ *
+ * @package    tool_policy
+ * @copyright  2018 Sara Arjona <sara@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class handler extends \core_privacy\local\sitepolicy\handler {
+
+    /**
+     * Returns URL to redirect user to when user needs to agree to site policy
+     *
+     * This is a regular interactive page for web users. It should have normal Moodle header/footers, it should
+     * allow user to view policies and accept them.
+     *
+     * @param bool $forguests
+     * @return moodle_url|null (returns null if site policy is not defined)
+     */
+    public static function get_redirect_url($forguests = false) {
+        // There is no redirect for guests, policies are shown in the popup, only return redirect url for the logged in users.
+        if (!$forguests && api::get_current_versions_ids(policy_version::AUDIENCE_LOGGEDIN)) {
+            return new \moodle_url('/admin/tool/policy/index.php');
+        }
+        return null;
+    }
+
+    /**
+     * Returns URL of the site policy that needs to be displayed to the user (inside iframe or to use in WS such as mobile app)
+     *
+     * This page should not have any header/footer, it does not also have any buttons/checkboxes. The caller needs to implement
+     * the "Accept" button and call {@link self::accept()} on completion.
+     *
+     * @param bool $forguests
+     * @return moodle_url|null
+     */
+    public static function get_embed_url($forguests = false) {
+        if (api::get_current_versions_ids($forguests ? policy_version::AUDIENCE_GUESTS : policy_version::AUDIENCE_LOGGEDIN)) {
+            return new \moodle_url('/admin/tool/policy/viewall.php');
+        }
+        return null;
+    }
+
+    /**
+     * Accept site policy for the current user
+     *
+     * @return bool - false if sitepolicy not defined, user is not logged in or user has already agreed to site policy;
+     *     true - if we have successfully marked the user as agreed to the site policy
+     */
+    public static function accept() {
+        global $USER, $DB;
+        if (!isloggedin()) {
+            return false;
+        }
+        if ($USER->policyagreed) {
+            return false;
+        }
+
+        if (!isguestuser()) {
+            // Accepts all policies with a current version for logged users on behalf of the current user.
+            if (!$versions = api::get_current_versions_ids(policy_version::AUDIENCE_LOGGEDIN)) {
+                return false;
+            }
+            api::accept_policies(array_values($versions));
+        }
+
+        if (!isguestuser()) {
+            // For the guests agreement in stored in session only, for other users - in DB.
+            $DB->set_field('user', 'policyagreed', 1, array('id' => $USER->id));
+        }
+        $USER->policyagreed = 1;
+        return true;
+    }
+
+    /**
+     * Adds "Agree to site policy" checkbox to the signup form.
+     *
+     * @param \MoodleQuickForm $mform
+     */
+    public static function signup_form($mform) {
+        if (static::is_defined()) {
+            // This plugin displays policies to the user who is signing up before the signup form is shown.
+            // By the time user has access to signup form they have already agreed to the policies.
+            $mform->addElement('hidden', 'policyagreed', 1);
+            $mform->setType('policyagreed', PARAM_INT);
+        }
+    }
+}
---
+++ admin/tool/policy/view.php
@@ -0,0 +1,58 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * View current document policy version.
+ *
+ * Script parameters:
+ *  versionid=<int> Policy version id, defaults to the current one.
+ *  policyid=<int> Policy document id, defaults to the one matching the version.
+ *  returnurl=<local url> URL to continue to after reading the policy document.
+ *  behalfid=<id> The user id to view the policy version as (such as child's id).
+ *  manage=<bool> View the policy as a part of the management UI (managedocs.php).
+ *
+ * @package     tool_policy
+ * @copyright   2018 Sara Arjona (sara@moodle.com)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use tool_policy\api;
+use tool_policy\output\page_viewdoc;
+
+// Do not check for the site policies in require_login() to avoid the redirect loop.
+define('NO_SITEPOLICY_CHECK', true);
+
+// @codingStandardsIgnoreLine See the {@link page_viewdoc} for the access control checks.
+require(__DIR__.'/../../../config.php');
+
+$versionid = optional_param('versionid', null, PARAM_INT);
+$policyid = $versionid ? optional_param('policyid', null, PARAM_INT) : required_param('policyid', PARAM_INT);
+$returnurl = optional_param('returnurl', null, PARAM_LOCALURL);
+$behalfid = optional_param('behalfid', null, PARAM_INT);
+$manage = optional_param('manage', false, PARAM_BOOL);
+$numpolicy = optional_param('numpolicy', null, PARAM_INT);
+$totalpolicies = optional_param('totalpolicies', null, PARAM_INT);
+
+$PAGE->set_context(context_system::instance());
+$PAGE->set_pagelayout('standard');
+
+$viewpage = new page_viewdoc($policyid, $versionid, $returnurl, $behalfid, $manage, $numpolicy, $totalpolicies);
+
+$output = $PAGE->get_renderer('tool_policy');
+
+echo $output->header();
+echo $output->render($viewpage);
+echo $output->footer();
