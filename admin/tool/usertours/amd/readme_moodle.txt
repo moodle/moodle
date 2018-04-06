ea33609e4b63483f365c374aa643b802ebe1d73f
Bas Brands <bas@moodle.com>
2018-04-06 17:56:46 +0200

    MDL-61852 lib: move popper.js to core
            Popper is used in tool_usertours. Bootstrap 4 stable depends on popper too. Moving to /lib so both can use a single popper.js lib

--- admin/tool/usertours/amd/readme_moodle.txt
+++ admin/tool/usertours/amd/readme_moodle.txt
@@ -8,10 +8,3 @@ Flexitour Instructions
 4. Change the "popper" inclusion to "./popper"
 5. Update thirdpartylibs.xml
 6. Run `grunt amd`
-
-Popper.js Instructions
-----------------------
-1. Clone https://github.com/FezVrasta/popper.js into an unrelated directory
-2. Copy /build/popper.js to amd/src/popper.js
-3. Update thirdpartylibs.xml
-4. Run `grunt amd`
--- admin/tool/usertours/amd/src/tour.js
+++ admin/tool/usertours/amd/src/tour.js
@@ -2,7 +2,7 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
-    define(["jquery","./popper"], function (a0,b1) {
+    define(["jquery","core/popper"], function (a0,b1) {
       return (root['Tour'] = factory(a0,b1));
     });
   } else if (typeof module === 'object' && module.exports) {
