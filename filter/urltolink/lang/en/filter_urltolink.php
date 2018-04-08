e484db7c94f87f1b4077a9874cc39a6aafae378f
Andrew Nicols <andrew@nicols.co.uk>
2018-04-08 15:51:29 +0800

    MDL-61896 filter: Implement privacy API for all filters

--- filter/urltolink/lang/en/filter_urltolink.php
+++ filter/urltolink/lang/en/filter_urltolink.php
@@ -31,3 +31,4 @@ $string['embedimages_desc'] = 'Replace image urls with images in selected text f
 $string['filtername'] = 'Convert URLs into links and images';
 $string['settingformats'] = 'Apply to formats';
 $string['settingformats_desc'] = 'The filter will be applied only if the original text was inserted in one of the selected formats.';
+$string['privacy:metadata'] = 'The Convert URLs into links and images plugin does not store any personal data.';
