07dd873f639b447ccc13c52bdbe522b74ec6cbbe
Zig Tan <zig@moodle.com>
2018-04-09 09:49:18 +0800

    MDL-61881 report_configlog: Implement null privacy provider

--- report/configlog/lang/en/report_configlog.php
+++ report/configlog/lang/en/report_configlog.php
@@ -30,3 +30,4 @@ $string['pluginname'] = 'Config changes';
 $string['setting'] = 'Setting';
 $string['timemodified'] = 'Date';
 $string['value'] = 'New value';
+$string['privacy:metadata'] = 'The report_configlog plugin does not store any personal data.';
