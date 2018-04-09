184f7d3a85de095cf58eff280a5d864275dc198d
Zig Tan <zig@moodle.com>
2018-04-09 09:57:15 +0800

    MDL-61881 report_usersessions: Implement null privacy provider

--- report/usersessions/lang/en/report_usersessions.php
+++ report/usersessions/lang/en/report_usersessions.php
@@ -28,3 +28,4 @@ $string['mysessions'] = 'My active sessions';
 $string['pluginname'] = 'User sessions report';
 $string['thissession'] = 'Current session';
 $string['usersessions:manageownsessions'] = 'Manage own browser sessions';
+$string['privacy:metadata'] = 'The report_usersessions plugin does not store any personal data.';
