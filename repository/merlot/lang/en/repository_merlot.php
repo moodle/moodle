ecebec32bc73aabbb232fe2ece9b03e80d3ccbd1
Zig Tan <zig@moodle.com>
2018-04-17 10:23:14 +0800

    MDL-61568 repository_merlot: Implement privacy providers

--- repository/merlot/lang/en/repository_merlot.php
+++ repository/merlot/lang/en/repository_merlot.php
@@ -28,3 +28,7 @@ $string['licensekey'] = 'License key';
 $string['pluginname_help'] = 'Merlot.org';
 $string['pluginname'] = 'Merlot.org';
 $string['merlot:view'] = 'View the Merlot repository';
+$string['privacy:metadata:repository_merlot'] = 'The Merlot.org repository plugin does not store any personal data, but does transmit user data from Moodle to the remote system.';
+$string['privacy:metadata:repository_merlot:author'] = 'The Merlot.org repository content author.';
+$string['privacy:metadata:repository_merlot:keywords'] = 'The Merlot.org repository user search text query.';
+$string['privacy:metadata:repository_merlot:licencekey'] = 'The Merlot.org repository licence key.';
