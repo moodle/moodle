353f1bfb37595a52dee5909a4dcb9bd79773ed91
Sara Arjona <sara@moodle.com>
2018-04-23 13:09:43 +0200

    MDL-61959 scormreport_graphs: Implement privacy API

--- mod/scorm/report/graphs/classes/privacy/provider.php
+++ mod/scorm/report/graphs/classes/privacy/provider.php
@@ -15,18 +15,32 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'scorm_basic' report plugin
+ * Privacy Subsystem implementation for scormreport_graphs.
  *
  * @package    scormreport_graphs
- * @copyright  2012 Ankit Kumar Agarwal
+ * @copyright  2018 Sara Arjona <sara@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['invaliddata'] = 'Not enough data';
-$string['participants'] = 'Number of participants';
-$string['percent'] = 'Percent(%) secured';
-$string['pluginname'] = 'Graph report';
-$string['privacy:metadata'] = 'The Graph report only shows data stored in other locations.';
+namespace scormreport_graphs\privacy;
 
+defined('MOODLE_INTERNAL') || die();
 
+/**
+ * Privacy Subsystem for scormreport_graphs implementing null_provider.
+ *
+ * @copyright  2018 Sara Arjona <sara@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
 
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+}
