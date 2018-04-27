fdcb33c5c5223ef378ab57b4d49ba4defc8d2c45
David Mudrák <david@moodle.com>
2018-04-27 11:14:11 +0200

    MDL-61905 workshop: Implement privacy API in assessment allocators
    Assessment allocation methods normally do not store any personal data.
    Their duty is to create assessment records that are then exported by the
    workshop core itself.
    
    Still, some allocators (such as the Manual allocation) can store certain
    personal data such as user preferences.

--- mod/workshop/allocation/manual/lang/en/workshopallocation_manual.php
+++ mod/workshop/allocation/manual/lang/en/workshopallocation_manual.php
@@ -31,4 +31,5 @@ $string['allocationexists'] = 'The allocation already exists';
 $string['areyousuretodeallocate'] = 'Are you sure you want deallocate the selected assessment?';
 $string['areyousuretodeallocategraded'] = 'You are going to remove the assessment that has already been graded. Are you really sure you want to do it?';
 $string['pluginname'] = 'Manual allocation';
+$string['privacy:metadata:preference:perpage'] = 'Number of allocated assessments the user prefers to see on one page.';
 $string['showallparticipants'] = 'Show all participants';
