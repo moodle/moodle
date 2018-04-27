4b8d462ee6771985cded62bf8c996eaf901bef75
David Mudrák <david@moodle.com>
2018-04-27 11:14:11 +0200

    MDL-61905 workshop: Implement privacy API in assessment allocators
    Assessment allocation methods normally do not store any personal data.
    Their duty is to create assessment records that are then exported by the
    workshop core itself.
    
    Still, some allocators (such as the Manual allocation) can store certain
    personal data such as user preferences.

---
+++ mod/workshop/allocation/random/classes/privacy/provider.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Provides the class {@link workshopallocation_random\privacy\provider}
+ *
+ * @package     workshopallocation_random
+ * @category    privacy
+ * @copyright   2018 David Mudrák <david@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace workshopallocation_random\privacy;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Privacy API implementation for the Random allocation method.
+ *
+ * @copyright 2018 David Mudrák <david@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+
+    /**
+     * Explain that this plugin stores no personal data.
+     *
+     * @return string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+}
