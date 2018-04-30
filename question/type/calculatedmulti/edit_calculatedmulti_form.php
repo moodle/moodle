023c3d4f0dfbb8c4fce9d4ba81e59d86e854c630
Tim Hunt <T.J.Hunt@open.ac.uk>
2018-04-30 19:45:47 +0100

    MDL-62275 qtype_calc*: improve validation of formulae
    Many thanks to Marina Glancy for helping with this.

--- question/type/calculatedmulti/edit_calculatedmulti_form.php
+++ question/type/calculatedmulti/edit_calculatedmulti_form.php
@@ -54,10 +54,8 @@ class qtype_calculatedmulti_edit_form extends question_edit_form {
             if (isset($this->question->id)) {
                 // Remove prefix #{..}# if exists.
                 $this->initialname = $question->name;
-                $regs= array();
-                if (preg_match('~#\{([^[:space:]]*)#~', $question->name , $regs)) {
-                    $question->name = str_replace($regs[0], '', $question->name);
-                };
+                $question->name = question_bank::get_qtype('calculated')
+                        ->clean_technical_prefix_from_question_name($question->name);
             }
         }
         parent::__construct($submiturl, $question, $category, $contexts, $formeditable);
