7a5aee27ffccc9adf683c87eaffefed495f796e1
David Monllao <davidm@moodle.com>
2018-05-02 16:42:40 +0200

    MDL-62217 search: Privacy providers

--- search/tests/fixtures/mock_search_area.php
+++ search/tests/fixtures/mock_search_area.php
@@ -74,7 +74,7 @@ class mock_search_area extends \core_search\base {
         $doc->set('title', $info->title);
         $doc->set('content', $info->content);
         $doc->set('description1', $info->description1);
-        $doc->set('description1', $info->description2);
+        $doc->set('description2', $info->description2);
         $doc->set('contextid', $info->contextid);
         $doc->set('courseid', $info->courseid);
         $doc->set('userid', $info->userid);
