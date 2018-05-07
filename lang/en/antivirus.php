77518888c8445d38f11a49c6ad52f05381a82644
Andrew Nicols <andrew@nicols.co.uk>
2018-05-07 14:31:00 +0800

    MDL-62326 core_antivirus: Implement null provider

--- lang/en/antivirus.php
+++ lang/en/antivirus.php
@@ -27,4 +27,5 @@ $string['antiviruses'] = 'Antivirus plugins';
 $string['antivirussettings'] = 'Manage antivirus plugins';
 $string['configantivirusplugins'] = 'Please choose the antivirus plugins you wish to use and arrange them in order of being applied.';
 $string['emailsubject'] = '{$a} :: Antivirus notification';
+$string['privacy:metadata'] = 'The Antivirus system does not store any personal data.';
 $string['virusfounduser'] = 'The file you have uploaded, {$a->filename}, has been scanned by a virus checker and found to be infected! Your file upload was NOT successful.';
