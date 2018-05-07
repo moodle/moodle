171a764049f3fe62b3545b9807921004ab2cf86c
Luca Bösch <luca.boesch@bfh.ch>
2018-05-08 00:59:53 +0200

    MDL-42779 course: Pagination in management page preserves view action

--- course/management.php
+++ course/management.php
@@ -495,7 +495,7 @@ if ($displaycategorylisting) {
 if ($displaycourselisting) {
     echo $renderer->grid_column_start($coursesize, 'course-listing');
     if (!$issearching) {
-        echo $renderer->course_listing($category, $course, $page, $perpage);
+        echo $renderer->course_listing($category, $course, $page, $perpage, $viewmode);
     } else {
         list($courses, $coursescount, $coursestotal) =
             \core_course\management\helper::search_courses($search, $blocklist, $modulelist, $page, $perpage);
