e77114bf7037774b61baccf5a453eee87c8e2190
David Monllao <davidm@moodle.com>
2018-05-08 14:02:11 +0200

    weekly release 3.3.5+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2017051505.09;              // 20170515      = branching date YYYYMMDD - do not modify!
+$version  = 2017051505.10;              // 20170515      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.3.5+ (Build: 20180502)'; // Human-friendly version name
+$release  = '3.3.5+ (Build: 20180508)'; // Human-friendly version name
 
 $branch   = '33';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
