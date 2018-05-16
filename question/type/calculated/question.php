897a492729725c00ede17d85384c258da53d417d
Marina Glancy <marina@moodle.com>
2018-05-16 15:25:11 +0800

    MDL-62469 qtype_calculated: check remaining placeholders, see MDL-62275

--- question/type/calculated/question.php
+++ question/type/calculated/question.php
@@ -424,7 +424,13 @@ class qtype_calculated_variable_substituter {
         if ($error = qtype_calculated_find_formula_errors($expression)) {
             throw new moodle_exception('illegalformulasyntax', 'qtype_calculated', '', $error);
         }
-        return $this->calculate_raw($this->substitute_values_for_eval($expression));
+        $expression = $this->substitute_values_for_eval($expression);
+        if ($datasets = question_bank::get_qtype('calculated')->find_dataset_names($expression)) {
+            // Some placeholders were not substituted.
+            throw new moodle_exception('illegalformulasyntax', 'qtype_calculated', '',
+                '{' . reset($datasets) . '}');
+        }
+        return $this->calculate_raw($expression);
     }
 
     /**
