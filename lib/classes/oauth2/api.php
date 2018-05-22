17550776889f20a77d4131488445ee5695905d6c
Leon Stringer <leon@priorsvle.com>
2018-05-22 19:46:12 +0100

    MDL-62316 OAuth 2: Only guess image if base URL set.
    If you added an OAuth 2 service with no base URL guess_image() would try to
    parse this empty value looking for a favicon.ico.  We now check if the base
    URL is empty beforehand and skip this if so.

--- lib/classes/oauth2/api.php
+++ lib/classes/oauth2/api.php
@@ -349,7 +349,7 @@ class api {
      * @param \core\oauth2\issuer $issuer The desired OAuth issuer
      */
     protected static function guess_image($issuer) {
-        if (empty($issuer->get('image'))) {
+        if (empty($issuer->get('image')) && !empty($issuer->get('baseurl'))) {
             $baseurl = parse_url($issuer->get('baseurl'));
             $imageurl = $baseurl['scheme'] . '://' . $baseurl['host'] . '/favicon.ico';
             $issuer->set('image', $imageurl);
