5dcdf1d1ba7f4b84a84b27aeb66ceaa02541fb4f
Shamim Rezaie <shamim@moodle.com>
2018-05-30 12:17:22 +1000

    MDL-62588 enrol_paypal: Add missing instanceid field

--- enrol/paypal/version.php
+++ enrol/paypal/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017111300;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2017111301;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2017110800;        // Requires this Moodle version
 $plugin->component = 'enrol_paypal';    // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 60;
