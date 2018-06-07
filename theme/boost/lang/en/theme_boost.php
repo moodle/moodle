dfcbcfc3199284b2c960a433e71cfbe0bd953c05
Willian Mano <willianmanoaraujo@gmail.com>
2018-06-07 08:36:19 -0300

    MDL-62474 theme_boost: Fix Boost privacy provider

--- theme/boost/lang/en/theme_boost.php
+++ theme/boost/lang/en/theme_boost.php
@@ -49,3 +49,5 @@ $string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will
 $string['rawscsspre'] = 'Raw initial SCSS';
 $string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
 $string['region-side-pre'] = 'Right';
+$string['privacy:metadata:preference:draweropennav'] = 'Hide or show the left drawer menu nav.';
+$string['privacy:request:preference:draweropennav'] = 'Your preference for hide or show the left drawer menu nav';
