a8f7818beb885e0febaf60759c663ba435e1376b
Zig Tan <zig@moodle.com>
2018-06-27 12:17:39 +0800

    MDL-62533 assign: Show assign hidden status on grading summary page

--- mod/assign/renderer.php
+++ mod/assign/renderer.php
@@ -267,6 +267,10 @@ class mod_assign_renderer extends plugin_renderer_base {
         $o .= $this->output->box_start('boxaligncenter gradingsummarytable');
         $t = new html_table();
 
+        // Visibility Status.
+        $this->add_table_row_tuple($t, get_string('hiddenfromstudents'),
+            (!$summary->isvisible) ? get_string('yes') : get_string('no'));
+
         // Status.
         if ($summary->teamsubmission) {
             if ($summary->warnofungroupedusers) {
