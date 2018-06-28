fba7dcd90abd45210d782a79c6e25bb3840c7438
Brendan Cox <brendan.cox@totaralearning.com>
2018-06-28 19:58:25 +0800

    MDL-62702 core_lib: parameters in POST override other LTI values
    This fixes an exploit where you could alter the 'Content-Type' of the
    request and bypass the case-sensitive check 'strstr'. For example,
    setting it to 'application/x-www-FORM-urlencoded'. However, changing
    this to use 'stristr' was also not an acceptable approach as you
    could also bypass it by setting the value to 'multipart/form-data'.

--- lib/ltiprovider/src/OAuth/OAuthRequest.php
+++ lib/ltiprovider/src/OAuth/OAuthRequest.php
@@ -60,15 +60,6 @@ class OAuthRequest {
               $parameters = array();
           }
 
-          // It's a POST request of the proper content-type, so parse POST
-          // parameters and add those overriding any duplicates from GET
-          if ($http_method == "POST"
-              &&  isset($request_headers['Content-Type'])
-              && strstr($request_headers['Content-Type'], 'application/x-www-form-urlencoded')) {
-              $post_data = OAuthUtil::parse_parameters(file_get_contents(self::$POST_INPUT));
-              $parameters = array_merge($parameters, $post_data);
-          }
-
           // We have a Authorization-header with OAuth data. Parse the header
           // and add those overriding any duplicates from GET or POST
           if (isset($request_headers['Authorization']) && substr($request_headers['Authorization'], 0, 6) == 'OAuth ') {
@@ -76,6 +67,11 @@ class OAuthRequest {
               $parameters = array_merge($parameters, $header_parameters);
           }
 
+          // If there are parameters in $_POST, these are likely what will be used. Therefore, they should be considered
+          // the final value in the case of any duplicates from sources parsed above.
+          foreach ($_POST as $key => $value) {
+              $parameters[$key] = OAuthUtil::urldecode_rfc3986($value);
+          }
       }
 
       return new OAuthRequest($http_method, $http_url, $parameters);
