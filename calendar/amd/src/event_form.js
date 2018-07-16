e220725ad7b700383bb15bf5f624db312e9058c6
Simey Lameze <simey@moodle.com>
2018-07-16 10:10:50 +0800

    MDL-60826 calendar: lazy load groups on event form

--- calendar/amd/src/event_form.js
+++ calendar/amd/src/event_form.js
@@ -21,42 +21,12 @@
  * @copyright  2017 Ryan Wyllie <ryan@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-define(['jquery'], function($) {
+define(['jquery', 'core_calendar/repository'], function($, CalendarRepository) {
 
     var SELECTORS = {
         EVENT_GROUP_COURSE_ID: '[name="groupcourseid"]',
         EVENT_GROUP_ID: '[name="groupid"]',
-        SELECT_OPTION: 'option',
-    };
-
-    /**
-     * Parse the group id select element in the event form and pull out
-     * the course id from the value to allow us to toggle other select
-     * elements based on the course id for the group a user selects.
-     *
-     * This is a little hacky but I couldn't find a better way to pass
-     * the course id for each group id with the limitations of mforms.
-     *
-     * The group id options are rendered with a value like:
-     * "<courseid>-<groupid>"
-     * E.g.
-     * For a group with id 10 in a course with id 3 the value of the
-     * option will be 3-10.
-     *
-     * @method parseGroupSelect
-     * @param {object} formElement The root form element
-     */
-    var parseGroupSelect = function(formElement) {
-        formElement.find(SELECTORS.EVENT_GROUP_ID)
-            .find(SELECTORS.SELECT_OPTION)
-            .each(function(index, element) {
-                element = $(element);
-                var value = element.attr('value');
-                var splits = value.split('-');
-                var courseId = splits[0];
-
-                element.attr('data-course-id', courseId);
-            });
+        SELECT_OPTION: 'option'
     };
 
     /**
@@ -69,39 +39,29 @@ define(['jquery'], function($) {
      */
     var addCourseGroupSelectListeners = function(formElement) {
         var courseGroupSelect = formElement.find(SELECTORS.EVENT_GROUP_COURSE_ID);
-        var groupSelect = formElement.find(SELECTORS.EVENT_GROUP_ID);
-        var groupSelectOptions = groupSelect.find(SELECTORS.SELECT_OPTION);
-        var filterGroupSelectOptions = function() {
-            var selectedCourseId = courseGroupSelect.val();
-            var selectedIndex = null;
-            var hasGroups = false;
-            groupSelectOptions.each(function(index, element) {
-                element = $(element);
 
-                if (element.attr('data-course-id') == selectedCourseId) {
-                    element.removeClass('hidden');
-                    element.prop('disabled', false);
-                    hasGroups = true;
-                    if (selectedIndex === null || element.attr('selected')) {
-                        selectedIndex = index;
-                    }
-                } else {
-                    element.addClass('hidden');
-                    element.prop('disabled', true);
-                }
+        var loadGroupSelectOptions = function(groups) {
+            var groupSelect = formElement.find(SELECTORS.EVENT_GROUP_ID),
+                groupSelectOptions = groupSelect.find(SELECTORS.SELECT_OPTION),
+                courseGroups = $(groups);
+
+            // Let's clear all options first.
+            groupSelectOptions.remove();
+            groupSelect.prop("disabled", false);
+            courseGroups.each(function(id, group) {
+                $(groupSelect).append($("<option></option>").attr("value", group.id).text(group.name));
             });
-
-            if (hasGroups) {
-                groupSelect.prop('disabled', false);
-            } else {
-                groupSelect.prop('disabled', true);
-            }
-
-            groupSelect.prop('selectedIndex', selectedIndex);
         };
 
-        courseGroupSelect.on('change', filterGroupSelectOptions);
-        filterGroupSelectOptions();
+        // If the user choose a course in the selector do a WS request to get groups.
+        courseGroupSelect.on('change', function() {
+            var courseId = formElement.find(SELECTORS.EVENT_GROUP_COURSE_ID).val();
+            CalendarRepository.getCourseGroupsData(courseId)
+                .then(function(groups) {
+                    return loadGroupSelectOptions(groups);
+                })
+                .catch(Notification.exception);
+        });
     };
 
     /**
@@ -112,8 +72,6 @@ define(['jquery'], function($) {
      */
     var init = function(formId) {
         var formElement = $('#' + formId);
-
-        parseGroupSelect(formElement);
         addCourseGroupSelectListeners(formElement);
     };
 
