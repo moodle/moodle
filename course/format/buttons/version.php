9f5dec142532229277f5a5bf7ddb24ea1ba73368
ilya dymshits <ilya@devlion.co>
2018-07-24 13:53:02 +0300

    (update) add new course formt_buttons

---
+++ course/format/buttons/version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * format_buttons_renderer
+ *
+ * @package    format_buttons
+ * @author     Rodrigo Brandão <rodrigo_brandao@me.com>
+ * @copyright  2018 Rodrigo Brandão
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->requires = 2014111000; /* moodle 2.8 */
+$plugin->component = 'format_buttons';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '(Build: 2018062700)';
+$plugin->version = 2018062700;
