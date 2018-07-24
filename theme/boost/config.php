341070d5b3ded5d051856fefa5d133552bf73153
Michael Hawkins <michaelh@moodle.com>
2018-07-24 16:32:43 +0800

    MDL-62968 Boost: Adding editor SCSS support

--- theme/boost/config.php
+++ theme/boost/config.php
@@ -29,6 +29,7 @@ require_once(__DIR__ . '/lib.php');
 $THEME->name = 'boost';
 $THEME->sheets = [];
 $THEME->editor_sheets = [];
+$THEME->editor_scss = ['editor'];
 $THEME->usefallback = true;
 $THEME->scss = function($theme) {
     return theme_boost_get_main_scss_content($theme);
