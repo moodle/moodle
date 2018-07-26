9ab746af278e1409ebd6e460d45aa1291f19e402
Jake Dallimore <jake@moodle.com>
2018-07-26 09:56:00 +0800

    MDL-62947 mod_feedback: fix feedback so it correctly uses forms API
    Feedback code was doing the following, which is incompatible with the
    sec-patched quickforms lib:
    - appending '[0]' to non-group element names, meaning type checks in
    getCleanType() were falling back to PARAM_RAW instead of PARAM_INT,
    and meaning _findValue() was comparing elements it thought to be arrays
    with scalar submit values (worked with eval() implementation as the 0th
    element of a string was the character, but not working when patched).
    - external unit tests incorrectly testing multichoice questions in the
    process_page tests - patched lib highlighted failures.
    
    It was also doing the following:
    - actively setting null as default element values instead of 0 for
    radio buttons and dropdowns (for 'not selected'). This is incompatible
    with more recent quickforms lib (see MDL-63070).
    - creating groups using addElement instead of addGroup - not recommended
    
    This patch addresses the above.

--- mod/feedback/item/multichoice/lib.php
+++ mod/feedback/item/multichoice/lib.php
@@ -316,14 +316,20 @@ class feedback_item_multichoice extends feedback_item_base {
         $inputname = $item->typ . '_' . $item->id;
         $options = $this->get_options($item);
         $separator = !empty($info->horizontal) ? ' ' : '<br>';
-        $tmpvalue = $form->get_item_value($item);
+        $itemval = $form->get_item_value($item);
+        $tmpvalue = !is_null($itemval) ? $itemval : 0; // Used for element defaults, so must be a valid value (not null).
 
+        // Subtypes:
+        // r = radio
+        // c = checkbox
+        // d = dropdown.
         if ($info->subtype === 'd' || ($info->subtype === 'r' && $form->is_frozen())) {
             // Display as a dropdown in the complete form or a single value in the response view.
             $element = $form->add_form_element($item,
-                    ['select', $inputname.'[0]', $name, array(0 => '') + $options, array('class' => $class)],
+                    ['select', $inputname, $name, array(0 => '') + $options, array('class' => $class)],
                     false, false);
-            $form->set_element_default($inputname.'[0]', $tmpvalue);
+            $form->set_element_default($inputname, $tmpvalue);
+            $form->set_element_type($inputname, PARAM_INT);
         } else if ($info->subtype === 'c' && $form->is_frozen()) {
             // Display list of checkbox values in the response view.
             $objs = [];
@@ -354,26 +360,26 @@ class feedback_item_multichoice extends feedback_item_base {
             } else {
                 // Radio.
                 if (!array_key_exists(0, $options)) {
-                    // Always add '0' as hidden element, otherwise form submit data may not have this element.
-                    $objs[] = ['hidden', $inputname.'[0]'];
+                    // Always add a hidden element to the group to guarantee we get a value in the submit data.
+                    $objs[] = ['hidden', $inputname, 0];
                 }
                 foreach ($options as $idx => $label) {
-                    $objs[] = ['radio', $inputname.'[0]', '', $label, $idx];
+                    $objs[] = ['radio', $inputname, '', $label, $idx];
                 }
                 // Span to hold the element id. The id is used for drag and drop reordering.
                 $objs[] = ['static', '', '', html_writer::span('', '', ['id' => 'feedback_item_' . $item->id])];
                 $element = $form->add_form_group_element($item, 'group_'.$inputname, $name, $objs, $separator, $class);
-                $form->set_element_default($inputname.'[0]', $tmpvalue);
-                $form->set_element_type($inputname.'[0]', PARAM_INT);
+                $form->set_element_default($inputname, $tmpvalue);
+                $form->set_element_type($inputname, PARAM_INT);
             }
         }
 
         // Process 'required' rule.
         if ($item->required) {
             $elementname = $element->getName();
-            $form->add_validation_rule(function($values, $files) use ($elementname, $item) {
+            $form->add_validation_rule(function($values) use ($elementname, $item) {
                 $inputname = $item->typ . '_' . $item->id;
-                return empty($values[$inputname]) || !array_filter($values[$inputname]) ?
+                return empty($values[$inputname]) || (is_array($values[$inputname]) && !array_filter($values[$inputname])) ?
                     array($elementname => get_string('required')) : true;
             });
         }
@@ -385,6 +391,9 @@ class feedback_item_multichoice extends feedback_item_base {
      * @return string
      */
     public function create_value($value) {
+        // Could be an array (multichoice checkbox) or single value (multichoice radio or dropdown).
+        $value = is_array($value) ? $value : [$value];
+
         $value = array_unique(array_filter($value));
         return join(FEEDBACK_MULTICHOICE_LINE_SEP, $value);
     }
