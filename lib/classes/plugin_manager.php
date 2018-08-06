1f2972764b86eaca77f82970bf9b0cc05221cc5c
Jun Pataleta <jun@moodle.com>
2018-08-07 07:16:58 +0800

    MDL-62799 core: Add tool_dataprivacy to list of standard plugins

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1901,7 +1901,7 @@ class core_plugin_manager {
 
             'tool' => array(
                 'assignmentupgrade', 'availabilityconditions', 'behat', 'capability', 'cohortroles', 'customlang',
-                'dbtransfer', 'filetypes', 'generator', 'health', 'innodb', 'installaddon',
+                'dataprivacy', 'dbtransfer', 'filetypes', 'generator', 'health', 'innodb', 'installaddon',
                 'langimport', 'log', 'lp', 'lpimportcsv', 'lpmigrate', 'messageinbound', 'mobile', 'multilangupgrade', 'monitor',
                 'oauth2', 'phpunit', 'policy', 'profiling', 'recyclebin', 'replace', 'spamcleaner', 'task', 'templatelibrary',
                 'uploadcourse', 'uploaduser', 'unsuproles', 'usertours', 'xmldb'
