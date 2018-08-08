1ebba914a36823464267b0c77c0e435676f875b4
kiril <kiril@devlion.co>
2018-08-08 13:54:05 +0300

    Remove Elegance (delete)

--- theme/elegance/javascript/atbar.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	$('a[title="atbar"]').click(function (e) {
-		(function(){d=document;jf=d.createElement('script');jf.src=('https:'==document.location.protocol?'https://ssl.atbar.org/c':'http://c.atbar.org')+'/ATBar2/ATBar.min.user.js';jf.type='text/javascript';jf.id='ToolBar';d.getElementsByTagName('head')[0].appendChild(jf);})();
-	});
-});
-
-
-
-
--- theme/elegance/javascript/fitvid.js
+++
@@ -1,6 +0,0 @@
-/*
- FitVid will dynamically fit embedded video in Moodle
-*/
-  $(document).ready(function(){
-    $(".mediaplugin").fitVids();
-  });
