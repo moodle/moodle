f2c6d5cc1164df285be439354adbfff3963d2f44
Andrew Nicols <andrew@nicols.co.uk>
2018-08-31 10:20:57 +0800

    MDL-63495 privacy: Add support for removal of multiple users in a context
    This issue is a part of the MDL-62560 Epic.

---
+++ privacy/classes/local/request/userlist_base.php
@@ -0,0 +1,220 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Base implementation of a userlist.
+ *
+ * @package    core_privacy
+ * @copyright  2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core_privacy\local\request;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Base implementation of a userlist used to store a set of users.
+ *
+ * @copyright  2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+abstract class userlist_base implements
+        // Implement an Iterator to fetch the Context objects.
+        \Iterator,
+
+        // Implement the Countable interface to allow the number of returned results to be queried easily.
+        \Countable {
+
+    /**
+     * @var array List of user IDs.
+     *
+     * Note: this must not be updated using set_userids only as this
+     * ensures uniqueness.
+     */
+    private $userids = [];
+
+    /**
+     * @var string component the frankenstyle component name.
+     */
+    protected $component = '';
+
+    /**
+     * @var int Current position of the iterator.
+     */
+    protected $iteratorposition = 0;
+
+    /** @var \context The context that this userlist belongs to */
+    protected $context;
+
+    /**
+     * Constructor to create a new userlist.
+     *
+     * @param   \context    $context
+     * @param   string      $component
+     */
+    public function __construct(\context $context, string $component) {
+        $this->context = $context;
+        $this->set_component($component);
+    }
+
+    /**
+     * Set the userids.
+     *
+     * @param   array   $userids The list of users.
+     * @return  $this
+     */
+    protected function set_userids(array $userids) : userlist_base {
+        $this->userids = array_values(array_unique($userids));
+
+        return $this;
+    }
+
+    /**
+     * Add a set of additional userids.
+     *
+     * @param   array   $userids The list of users.
+     * @return  $this
+     */
+    protected function add_userids(array $userids) : userlist_base {
+        $this->set_userids(array_merge($this->get_userids(), $userids));
+
+        return $this;
+    }
+
+    /**
+     * Get the list of user IDs that relate to this request.
+     *
+     * @return  int[]
+     */
+    public function get_userids() : array {
+        return $this->userids;
+    }
+
+    /**
+     * Get the complete list of user objects that relate to this request.
+     *
+     * @return  \stdClass[]
+     */
+    public function get_users() : array {
+        $users = [];
+        foreach ($this->userids as $userid) {
+            if ($user = \core_user::get_user($userid)) {
+                $users[] = $user;
+            }
+        }
+
+        return $users;
+    }
+
+    /**
+     * Sets the component for this userlist.
+     *
+     * @param string $component the frankenstyle component name.
+     * @return  $this
+     */
+    protected function set_component($component) : userlist_base {
+        $this->component = $component;
+
+        return $this;
+    }
+
+    /**
+     * Get the name of the component to which this userlist belongs.
+     *
+     * @return string the component name associated with this userlist.
+     */
+    public function get_component() : string {
+        return $this->component;
+    }
+
+    /**
+     * Return the current user.
+     *
+     * @return  \user
+     */
+    public function current() {
+        $user = \core_user::get_user($this->userids[$this->iteratorposition]);
+
+        if (false === $user) {
+            // This user was not found.
+            unset($this->userids[$this->iteratorposition]);
+
+            // Check to see if there are any more users left.
+            if ($this->count()) {
+                // Move the pointer to the next record and try again.
+                $this->next();
+                $user = $this->current();
+            } else {
+                // There are no more context ids left.
+                return;
+            }
+        }
+
+        return $user;
+    }
+
+    /**
+     * Return the key of the current element.
+     *
+     * @return  mixed
+     */
+    public function key() {
+        return $this->iteratorposition;
+    }
+
+    /**
+     * Move to the next user in the list.
+     */
+    public function next() {
+        ++$this->iteratorposition;
+    }
+
+    /**
+     * Check if the current position is valid.
+     *
+     * @return  bool
+     */
+    public function valid() {
+        return isset($this->userids[$this->iteratorposition]) && $this->current();
+    }
+
+    /**
+     * Rewind to the first found user.
+     *
+     * The list of users is uniqued during the rewind.
+     * The rewind is called at the start of most iterations.
+     */
+    public function rewind() {
+        $this->iteratorposition = 0;
+    }
+
+    /**
+     * Return the number of users.
+     */
+    public function count() {
+        return count($this->userids);
+    }
+
+    /**
+     * Get the context for this userlist
+     *
+     * @return  \context
+     */
+    public function get_context() : \context {
+        return $this->context;
+    }
+}
---
+++ privacy/tests/userlist_test.php
@@ -0,0 +1,93 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit Tests for the approved userlist Class
+ *
+ * @package     core_privacy
+ * @category    test
+ * @copyright   2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+use \core_privacy\local\request\userlist;
+
+/**
+ * Tests for the \core_privacy API's approved userlist functionality.
+ *
+ * @copyright   2018 Andrew Nicols <andrew@nicols.co.uk>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class userlist_test extends advanced_testcase {
+
+    /**
+     * Ensure that valid SQL results in the relevant users being added.
+     */
+    public function test_add_from_sql() {
+        global $DB;
+
+        $sql = "SELECT c.id FROM {user} c";
+        $params = [];
+        $allusers = $DB->get_records_sql($sql, $params);
+
+        $uut = new userlist(\context_system::instance(), 'core_privacy');
+        $uut->add_from_sql('id', $sql, $params);
+
+        $this->assertCount(count($allusers), $uut);
+    }
+
+    /**
+     * Ensure that adding a single user adds that user.
+     */
+    public function test_add_user() {
+        $this->resetAfterTest();
+
+        $u1 = $this->getDataGenerator()->create_user();
+        $u2 = $this->getDataGenerator()->create_user();
+
+        $uut = new userlist(\context_system::instance(), 'core_privacy');
+        $uut->add_user($u1->id);
+
+        $this->assertCount(1, $uut);
+        $this->assertEquals($uut->current(), $u1);
+    }
+
+
+    /**
+     * Ensure that adding multiple users by ID adds those users.
+     */
+    public function test_add_users() {
+        $this->resetAfterTest();
+
+        $u1 = $this->getDataGenerator()->create_user();
+        $u2 = $this->getDataGenerator()->create_user();
+        $u3 = $this->getDataGenerator()->create_user();
+        $expected = [$u1->id, $u3->id];
+
+        $uut = new userlist(\context_system::instance(), 'core_privacy');
+        $uut->add_users([$u1->id, $u3->id]);
+
+        $this->assertCount(2, $uut);
+
+        foreach ($uut as $user) {
+            $this->assertNotFalse(array_search($user->id, $expected));
+        }
+    }
+}
