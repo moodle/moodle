f4bd5f5e1ad0f1dd27a1d384cb4cecc52d113bc8
Helen Foster <helen@moodle.org>
2018-09-04 15:12:17 +0200

    MDL-63249 lang: Merge English strings from the en_fix language pack
    Significant string changes:
    
    * completionpass_help, gradetopassnotset in mod_quiz - grade to pass
      set in quiz settings not gradebook
    * namecolumnmissing,core_cohort - fixing incorrect message about adding
      users to a cohort

--- user/profile/field/textarea/lang/en/profilefield_textarea.php
+++ user/profile/field/textarea/lang/en/profilefield_textarea.php
@@ -23,8 +23,8 @@
  */
 
 $string['pluginname'] = 'Text area';
-$string['privacy:metadata:profile_field_textarea:userid'] = 'The ID of the user which data is stored by the Text area plugin.';
-$string['privacy:metadata:profile_field_textarea:fieldid'] = 'The ID of the profile field.';
-$string['privacy:metadata:profile_field_textarea:data'] = 'The stored user data.';
-$string['privacy:metadata:profile_field_textarea:dataformat'] = 'The format of the stored user data.';
-$string['privacy:metadata:profile_field_textarea:tableexplanation'] = 'Additional user information is stored here.';
+$string['privacy:metadata:profile_field_textarea:userid'] = 'The ID of the user whose data is stored by the Text area user profile field';
+$string['privacy:metadata:profile_field_textarea:fieldid'] = 'The ID of the profile field';
+$string['privacy:metadata:profile_field_textarea:data'] = 'Text area user profile field user data';
+$string['privacy:metadata:profile_field_textarea:dataformat'] = 'The format of Text area user profile field user data';
+$string['privacy:metadata:profile_field_textarea:tableexplanation'] = 'Additional profile data';
