ac0f38b1d84e8d18b2e0175c5c50b3ed594d0a05
Shamim Rezaie <shamim@moodle.com>
2018-09-07 14:55:28 +1000

    MDL-63138 mod_folder: Check if the module is visible to the user

--- mod/folder/tests/lib_test.php
+++ mod/folder/tests/lib_test.php
@@ -118,6 +118,60 @@ class mod_folder_lib_testcase extends advanced_testcase {
         $this->assertTrue($actionevent->is_actionable());
     }
 
+    public function test_folder_core_calendar_provide_event_action_for_non_user() {
+        global $CFG;
+
+        // Create a course.
+        $course = $this->getDataGenerator()->create_course();
+
+        // Create the activity.
+        $folder = $this->getDataGenerator()->create_module('folder', array('course' => $course->id));
+
+        // Create a calendar event.
+        $event = $this->create_action_event($course->id, $folder->id,
+                \core_completion\api::COMPLETION_EVENT_TYPE_DATE_COMPLETION_EXPECTED);
+
+        // Now, log out.
+        $CFG->forcelogin = true; // We don't want to be logged in as guest, as guest users might still have some capabilities.
+        $this->setUser();
+
+        // Create an action factory.
+        $factory = new \core_calendar\action_factory();
+
+        // Decorate action event.
+        $actionevent = mod_folder_core_calendar_provide_event_action($event, $factory);
+
+        // Confirm the event is not shown at all.
+        $this->assertNull($actionevent);
+    }
+
+    public function test_folder_core_calendar_provide_event_action_in_hidden_section() {
+        // Create a course.
+        $course = $this->getDataGenerator()->create_course();
+
+        // Create a student.
+        $student = $this->getDataGenerator()->create_and_enrol($course, 'student');
+
+        // Create the activity.
+        $folder = $this->getDataGenerator()->create_module('folder', array('course' => $course->id));
+
+        // Create a calendar event.
+        $event = $this->create_action_event($course->id, $folder->id,
+                \core_completion\api::COMPLETION_EVENT_TYPE_DATE_COMPLETION_EXPECTED);
+
+        // Set sections 0 as hidden.
+        set_section_visible($course->id, 0, 0);
+
+        // Create an action factory.
+        $factory = new \core_calendar\action_factory();
+
+        // Decorate action event.
+        $actionevent = mod_folder_core_calendar_provide_event_action($event, $factory, $student->id);
+
+        // Confirm the event is not shown at all.
+        $this->assertNull($actionevent);
+    }
+
     public function test_folder_core_calendar_provide_event_action_for_user() {
         // Create a course.
         $course = $this->getDataGenerator()->create_course();
