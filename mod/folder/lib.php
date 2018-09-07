ae9af4e48659b5c1a2696de91abb0439c7bc3da4
Shamim Rezaie <shamim@moodle.com>
2018-09-07 14:55:28 +1000

    MDL-63138 mod_folder: Check if the module is visible to the user

--- mod/folder/lib.php
+++ mod/folder/lib.php
@@ -802,6 +802,11 @@ function mod_folder_core_calendar_provide_event_action(calendar_event $event,
 
     $cm = get_fast_modinfo($event->courseid, $userid)->instances['folder'][$event->instance];
 
+    if (!$cm->uservisible) {
+        // The module is not visible to the user for any reason.
+        return null;
+    }
+
     $completion = new \completion_info($cm->get_course());
 
     $completiondata = $completion->get_data($cm, false, $userid);
