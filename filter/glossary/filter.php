86543cb1db35e35c98b6503c4ead3cf0fed4a584
Tim Hunt <T.J.Hunt@open.ac.uk>
2018-09-13 18:42:14 +0100

    MDL-63403 filter_glossary: only create replacement HTML if needed

--- filter/glossary/filter.php
+++ filter/glossary/filter.php
@@ -48,11 +48,10 @@ class filter_glossary extends moodle_text_filter {
 
     /**
      * Get all the concepts for this context.
-     * @return filterobject[] the concepts, and filterobjects, with an added
-     *      ->conceptid field.
+     * @return filterobject[] the concepts, and filterobjects.
      */
     protected function get_all_concepts() {
-        global $CFG, $USER;
+        global $USER;
 
         if ($this->cache === null) {
             $this->cache = cache::make_from_params(cache_store::MODE_REQUEST, 'filter', 'glossary');
@@ -92,48 +91,16 @@ class filter_glossary extends moodle_text_filter {
 
         foreach ($allconcepts as $concepts) {
             foreach ($concepts as $concept) {
-                if ($concept->category) { // Link to a category.
-                    $title = get_string('glossarycategory', 'filter_glossary',
-                            ['glossary' => $glossaries[$concept->glossaryid], 'category' => $concept->concept]);
-                    $link = new moodle_url('/mod/glossary/view.php', array('g' => $concept->glossaryid, 'mode' => 'cat', 'hook' => $concept->id));
-                    $attributes = array(
-                        'href'  => $link,
-                        'title' => $title,
-                        'class' => 'glossary autolink category glossaryid' . $concept->glossaryid);
-                    $conceptid = 0;
-
-                } else { // Link to entry or alias
-                    $title = get_string('glossaryconcept', 'filter_glossary',
-                            ['glossary' => $glossaries[$concept->glossaryid], 'concept' => $concept->concept]);
-                    // Hardcoding dictionary format in the URL rather than defaulting
-                    // to the current glossary format which may not work in a popup.
-                    // for example "entry list" means the popup would only contain
-                    // a link that opens another popup.
-                    $link = new moodle_url('/mod/glossary/showentry.php', array('eid' => $concept->id, 'displayformat' => 'dictionary'));
-                    $attributes = array(
-                        'href'  => $link,
-                        'title' => str_replace('&amp;', '&', $title), // Undo the s() mangling.
-                        'class' => 'glossary autolink concept glossaryid' . $concept->glossaryid);
-                    $conceptid = $concept->id;
-                }
-                // This flag is optionally set by resource_pluginfile()
-                // if processing an embedded file use target to prevent getting nested Moodles.
-                if (!empty($CFG->embeddedsoforcelinktarget)) {
-                    $attributes['target'] = '_top';
-                }
-                $href_tag_begin = html_writer::start_tag('a', $attributes);
-
-                $filterobj = new filterobject($concept->concept, $href_tag_begin, '</a>',
-                        $concept->casesensitive, $concept->fullmatch);
-                $filterobj->conceptid = $conceptid;
-                $conceptlist[] = $filterobj;
+                $conceptlist[] = new filterobject($concept->concept, null, null,
+                        $concept->casesensitive, $concept->fullmatch, null,
+                        [$this, 'filterobject_prepare_replacement_callback'], [$concept, $glossaries]);
             }
         }
 
         // We sort longest first, so that when we replace the terms,
         // the longest ones are replaced first. This does the right thing
         // when you have two terms like 'Moodle' and 'Moodle 3.5'. You want the longest match.
-        usort($conceptlist, 'filter_glossary::sort_entries_by_length');
+        usort($conceptlist, [$this, 'sort_entries_by_length']);
 
         $conceptlist = filter_prepare_phrases_for_filtering($conceptlist);
 
@@ -144,6 +111,50 @@ class filter_glossary extends moodle_text_filter {
         $this->cache->set('concepts', $tocache);
 
         return $conceptlist;
+    }
+
+    /**
+     * Callback used by filterobject / filter_phrases.
+     *
+     * @param object $concept the concept that is being replaced (from get_all_concepts).
+     * @param array $glossaries the list of glossary titles (from get_all_concepts).
+     * @return array [$hreftagbegin, $hreftagend, $replacementphrase] for filterobject.
+     */
+    public function filterobject_prepare_replacement_callback($concept, $glossaries) {
+        global $CFG;
+
+        if ($concept->category) { // Link to a category.
+            $title = get_string('glossarycategory', 'filter_glossary',
+                    ['glossary' => $glossaries[$concept->glossaryid], 'category' => $concept->concept]);
+            $link = new moodle_url('/mod/glossary/view.php',
+                    ['g' => $concept->glossaryid, 'mode' => 'cat', 'hook' => $concept->id]);
+            $attributes = array(
+                    'href'  => $link,
+                    'title' => $title,
+                    'class' => 'glossary autolink category glossaryid' . $concept->glossaryid);
+
+        } else { // Link to entry or alias.
+            $title = get_string('glossaryconcept', 'filter_glossary',
+                    ['glossary' => $glossaries[$concept->glossaryid], 'concept' => $concept->concept]);
+            // Hardcoding dictionary format in the URL rather than defaulting
+            // to the current glossary format which may not work in a popup.
+            // for example "entry list" means the popup would only contain
+            // a link that opens another popup.
+            $link = new moodle_url('/mod/glossary/showentry.php',
+                    ['eid' => $concept->id, 'displayformat' => 'dictionary']);
+            $attributes = array(
+                    'href'  => $link,
+                    'title' => str_replace('&amp;', '&', $title), // Undo the s() mangling.
+                    'class' => 'glossary autolink concept glossaryid' . $concept->glossaryid);
+        }
+
+        // This flag is optionally set by resource_pluginfile()
+        // if processing an embedded file use target to prevent getting nested Moodles.
+        if (!empty($CFG->embeddedsoforcelinktarget)) {
+            $attributes['target'] = '_top';
+        }
+
+        return [html_writer::start_tag('a', $attributes), '</a>', null];
     }
 
     public function filter($text, array $options = array()) {
@@ -157,7 +168,11 @@ class filter_glossary extends moodle_text_filter {
 
         if (!empty($GLOSSARY_EXCLUDEENTRY)) {
             foreach ($conceptlist as $key => $filterobj) {
-                if (is_object($filterobj) && $filterobj->conceptid == $GLOSSARY_EXCLUDEENTRY) {
+                if (!is_object($filterobj)) {
+                    continue;
+                }
+                $concept = $filterobj->replacementcallbackdata[0];
+                if (!$concept->category && $concept->id == $GLOSSARY_EXCLUDEENTRY) {
                     unset($conceptlist[$key]);
                 }
             }
@@ -170,16 +185,13 @@ class filter_glossary extends moodle_text_filter {
         return filter_phrases($text, $conceptlist);   // Actually search for concepts!
     }
 
-    private static function sort_entries_by_length($entry0, $entry1) {
-        $len0 = strlen($entry0->phrase);
-        $len1 = strlen($entry1->phrase);
-
-        if ($len0 < $len1) {
-            return 1;
-        } else if ($len0 > $len1) {
-            return -1;
-        } else {
-            return 0;
-        }
+    /**
+     * usort helper used in get_all_concepts above.
+     * @param filterobject $filterobject0 first item to compare.
+     * @param filterobject $filterobject1 second item to compare.
+     * @return int -1, 0 or 1.
+     */
+    private function sort_entries_by_length($filterobject0, $filterobject1) {
+        return strlen($filterobject1->phrase) <=> strlen($filterobject0->phrase);
     }
 }
