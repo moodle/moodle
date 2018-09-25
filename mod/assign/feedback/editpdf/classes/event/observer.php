4015cb2d0549f81ca9be2b7daf44f0cc6726c2a1
Adam Olley <adam.olley@blackboard.com>
2018-09-25 09:52:17 +0930

    MDL-63891 assignfeedback_editpdf: integration cleanup
    * Add a note to upgrade.php to notify devs/admins of the new setting.
    * Replace update_record with set_field.
    * Add missing phpdoc.
    
    Backport of MDL-57610.

--- mod/assign/feedback/editpdf/classes/event/observer.php
+++ mod/assign/feedback/editpdf/classes/event/observer.php
@@ -48,7 +48,7 @@ class observer {
 
     /**
      * Queue the submission for processing.
-     * @param $event The submission created/updated event.
+     * @param \mod_assign\event\base $event The submission created/updated event.
      */
     protected static function queue_conversion($event) {
         global $DB;
