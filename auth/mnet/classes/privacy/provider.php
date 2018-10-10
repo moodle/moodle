bd77a9026720bb1b62bca40b2b5c214daac77dfb
Mihail Geshoski <mihail@moodle.com>
2018-10-10 16:21:34 +0800

    MDL-63593 auth_mnet: Add support for removal of context users
    This issue is part of the MDL-62560 Epic.

--- auth/mnet/classes/privacy/provider.php
+++ auth/mnet/classes/privacy/provider.php
@@ -25,11 +25,13 @@ namespace auth_mnet\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
-use \core_privacy\local\metadata\collection;
-use \core_privacy\local\request\contextlist;
-use \core_privacy\local\request\approved_contextlist;
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\contextlist;
+use core_privacy\local\request\approved_contextlist;
 use core_privacy\local\request\transform;
-use \core_privacy\local\request\writer;
+use core_privacy\local\request\writer;
+use core_privacy\local\request\userlist;
+use core_privacy\local\request\approved_userlist;
 
 /**
  * Privacy provider for the mnet authentication
@@ -39,6 +41,7 @@ use \core_privacy\local\request\writer;
  */
 class provider implements
         \core_privacy\local\metadata\provider,
+        \core_privacy\local\request\core_userlist_provider,
         \core_privacy\local\request\plugin\provider {
     /**
      * Returns meta data about this system.
@@ -147,6 +150,33 @@ class provider implements
     }
 
     /**
+     * Get the list of users within a specific context.
+     *
+     * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist) {
+        $context = $userlist->get_context();
+
+        if (!$context instanceof \context_user) {
+            return;
+        }
+
+        $params = [
+            'contextuser' => CONTEXT_USER,
+            'contextid' => $context->id
+        ];
+
+        $sql = "SELECT ctx.instanceid as userid
+                  FROM {mnet_log} ml
+                  JOIN {context} ctx
+                       ON ctx.instanceid = ml.userid
+                       AND ctx.contextlevel = :contextuser
+                 WHERE ctx.id = :contextid";
+
+        $userlist->add_from_sql('userid', $sql, $params);
+    }
+
+    /**
      * Export all user data for the specified user, in the specified contexts, using the supplied exporter instance.
      *
      * @param   approved_contextlist $contextlist The approved contexts to export information for.
@@ -214,6 +244,21 @@ class provider implements
         }
 
         $DB->delete_records('mnet_log', ['userid' => $context->instanceid]);
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param approved_userlist $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist) {
+        global $DB;
+
+        $context = $userlist->get_context();
+
+        if ($context instanceof \context_user) {
+            $DB->delete_records('mnet_log', ['userid' => $context->instanceid]);
+        }
     }
 
     /**
