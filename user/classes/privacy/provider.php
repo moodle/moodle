d4d28485c75ca6d69c5010dc734985a4a5f136e2
Mihail Geshoski <mihail@moodle.com>
2018-10-10 15:47:27 +0800

    MDL-63534 core_user: Add support for removal of context users
    This issue is part of the MDL-62560 Epic.

--- user/classes/privacy/provider.php
+++ user/classes/privacy/provider.php
@@ -31,6 +31,8 @@ use \core_privacy\local\request\transform;
 use \core_privacy\local\request\contextlist;
 use \core_privacy\local\request\approved_contextlist;
 use \core_privacy\local\request\writer;
+use core_privacy\local\request\userlist;
+use \core_privacy\local\request\approved_userlist;
 
 /**
  * Privacy class for requesting user data.
@@ -39,7 +41,10 @@ use \core_privacy\local\request\writer;
  * @copyright  2018 Adrian Greeve <adrian@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\provider, \core_privacy\local\request\subsystem\provider {
+class provider implements
+        \core_privacy\local\metadata\provider,
+        \core_privacy\local\request\core_userlist_provider,
+        \core_privacy\local\request\subsystem\provider {
 
     /**
      * Returns information about the user data stored in this component.
@@ -197,6 +202,30 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
     }
 
     /**
+     * Get the list of users within a specific context.
+     *
+     * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist) {
+        $context = $userlist->get_context();
+
+        if (!$context instanceof \context_user) {
+            return;
+        }
+
+        $params = [
+            'contextid' => $context->id,
+            'contextuser' => CONTEXT_USER,
+        ];
+
+        $sql = "SELECT instanceid as userid
+                  FROM {context}
+                 WHERE id = :contextid and contextlevel = :contextuser";
+
+        $userlist->add_from_sql('userid', $sql, $params);
+    }
+
+    /**
      * Export all user data for the specified user, in the specified contexts.
      *
      * @param approved_contextlist $contextlist The approved contexts to export information for.
@@ -221,6 +250,20 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
     public static function delete_data_for_all_users_in_context(\context $context) {
         // Only delete data for a user context.
         if ($context->contextlevel == CONTEXT_USER) {
+            static::delete_user_data($context->instanceid, $context);
+        }
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param approved_userlist $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist) {
+
+        $context = $userlist->get_context();
+
+        if ($context instanceof \context_user) {
             static::delete_user_data($context->instanceid, $context);
         }
     }
