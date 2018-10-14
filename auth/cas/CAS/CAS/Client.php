0d2d2b988606684633d4b2233e4768bb97b8efc4
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2018-10-14 12:44:47 +0200

    MDL-63422 auth_cas: review core loop / switch / case / continue
    Created https://github.com/apereo/phpCAS/pull/278 upstream, applied locally.

--- auth/cas/CAS/CAS/Client.php
+++ auth/cas/CAS/CAS/Client.php
@@ -3380,7 +3380,7 @@ class CAS_Client
                 case 'user':
                 case 'proxies':
                 case 'proxyGrantingTicket':
-                    continue;
+                    break;
                 default:
                     if (strlen(trim($attr_node->nodeValue))) {
                         phpCas :: trace(
