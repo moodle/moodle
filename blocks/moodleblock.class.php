96d9a6e430cc1b2e7fa9278402b59de09675ccd7
Juan Leyva <juanleyvadelgado@gmail.com>
2018-10-17 16:16:12 +0200

    MDL-63694 core_block: New block class method for external content
    New method added block_base::get_content_for_external().
    It will return all the block contents rendered for external functions.
    If your block is returning formatted content or provide files for download,
    you should override this method to use the external_format_text,
    external_format_string functions for formatting or
    external_util::get_area_files for files.
    See block_html as example.

--- blocks/moodleblock.class.php
+++ blocks/moodleblock.class.php
@@ -272,6 +272,39 @@ class block_base {
         return $bc;
     }
 
+
+    /**
+     * Return an object containing all the block content to be returned by external functions.
+     *
+     * If your block is returning formatted content or provide files for download, you should override this method to use the
+     * external_format_text, external_format_string functions for formatting or external_util::get_area_files for files.
+     *
+     * @param  core_renderer $output the rendered used for output
+     * @return stdClass      object containing the block title, central content, footer and linked files (if any).
+     * @since  Moodle 3.6
+     */
+    public function get_content_for_external($output) {
+        $bc = new stdClass;
+        $bc->title = null;
+        $bc->content = null;
+        $bc->contentformat = FORMAT_HTML;
+        $bc->footer = null;
+        $bc->files = [];
+
+        if ($this->instance->visible) {
+            $bc->content = $this->formatted_contents($output);
+            if (!empty($this->content->footer)) {
+                $bc->footer = $this->content->footer;
+            }
+        }
+
+        if (!$this->hide_header()) {
+            $bc->title = $this->title;
+        }
+
+        return $bc;
+    }
+
     /**
      * Convert the contents of the block to HTML.
      *
