9c0e123af0aa77baa3cddbf18679f83e6d31aa8e
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2018-11-01 00:24:54 +0100

    MDL-63826 tests: Don't rely on random array keys for testing

--- mod/assign/submission/comments/tests/privacy_test.php
+++ mod/assign/submission/comments/tests/privacy_test.php
@@ -174,9 +174,11 @@ class assignsubmission_comments_privacy_testcase extends \mod_assign\tests\mod_a
         \assignsubmission_comments\privacy\provider::get_userids_from_context($userlist);
         $userids = $userlist->get_userids();
         $this->assertCount(3, $userids);
-        // Two is the key for user 3.
-        $this->assertEquals(2, array_search($user3->id, $userids));
-        $this->assertFalse(array_search($user4->id, $userids));
+        // User 1,2 and 3 are the expected ones in the array. User 4 isn't.
+        $this->assertContains($user1->id, $userids);
+        $this->assertContains($user2->id, $userids);
+        $this->assertContains($user3->id, $userids);
+        $this->assertNotContains($user4->id, $userids);
     }
 
     /**
