5c6db8d956a85a1925d26922d13206cb4c1989b3
Bas Brands <bas@moodle.com>
2018-10-31 11:18:07 +0100

    MDL-63064 block_starredcourses: move lang strings to core_course
    AMOS BEGIN
     MOV [aria:courseimage,block_myoverview],[aria:courseimage,core_course]
     MOV [aria:coursename,block_myoverview],[aria:coursename,core_course]
     MOV [aria:favourite,block_myoverview],[aria:favourite,core_course]
     MOV [favourite,block_myoverview],[favourite,core_course]
     MOV [notfavourite,block_myoverview],[notfavourite,core_course]
    AMOS END

--- lang/en/course.php
+++ lang/en/course.php
@@ -22,6 +22,11 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['aria:courseimage'] = 'Course image:';
+$string['aria:coursename'] = 'Course name:';
+$string['aria:favourite'] = 'Course is starred';
+$string['favourite'] = 'Starred course';
+$string['notfavourite'] = 'Not starred';
 $string['privacy:perpage'] = 'The number of courses to show per page.';
 $string['privacy:completionpath'] = 'Course completion';
 $string['privacy:metadata:completionsummary'] = 'The course contains completion information about the user.';
