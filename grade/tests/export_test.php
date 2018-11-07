a1e8c6d4975458da77434ce23d031c0717b98270
Peter <peter@moodle.com>
2018-11-07 13:32:22 +0800

    MDL-63488 core_grade: Use the rewrite_pluginurls to handle files
    * Use the rewrite_plugin_urls to handled embedded files
    * Add phpunit tests for default and updated functionality of format_text

--- grade/tests/export_test.php
+++ grade/tests/export_test.php
@@ -35,7 +35,7 @@ require_once($CFG->dirroot.'/grade/export/lib.php');
 class core_grade_export_test extends advanced_testcase {
 
     /**
-     * Ensure that feedback is correct formatted.
+     * Ensure that feedback is correct formatted. Test the default implementation of format_feedback
      *
      * @dataProvider    format_feedback_provider
      * @param   string  $input The input string to test
@@ -60,6 +60,71 @@ class core_grade_export_test extends advanced_testcase {
     }
 
     /**
+     * Ensure that feedback is correctly formatted. Test augmented functionality to handle file links
+     */
+    public function test_format_feedback_with_grade() {
+        $this->resetAfterTest();
+        $dg = $this->getDataGenerator();
+        $c1 = $dg->create_course();
+        $u1 = $dg->create_user();
+        $gi1a = new grade_item($dg->create_grade_item(['courseid' => $c1->id]), false);
+        $gi1a->update_final_grade($u1->id, 1, 'test');
+        $contextid = $gi1a->get_context()->id;
+        $gradeid = $gi1a->id;
+
+        $tests = [
+            'Has server based image (HTML)' => [
+                '<p>See this reference: <img src="@@PLUGINFILE@@/test.img"></p>',
+                FORMAT_HTML,
+                "See this reference: "
+            ],
+            'Has server based image and more (HTML)' => [
+                '<p>See <img src="@@PLUGINFILE@@/test.img"> for <em>reference</em></p>',
+                FORMAT_HTML,
+                "See  for reference"
+            ],
+            'Has server based video and more (HTML)' => [
+                '<p>See <video src="@@PLUGINFILE@@/test.img">video of a duck</video> for <em>reference</em></p>',
+                FORMAT_HTML,
+                'See video of a duck for reference'
+            ],
+            'Has server based video with text and more (HTML)' => [
+                '<p>See <video src="@@PLUGINFILE@@/test.img">@@PLUGINFILE@@/test.img</video> for <em>reference</em></p>',
+                FORMAT_HTML,
+                "See https://www.example.com/moodle/pluginfile.php/$contextid/grade/feedback/$gradeid/test.img for reference"
+            ],
+            'Multiple videos (HTML)' => [
+                '<p>See <video src="@@PLUGINFILE@@/test.img">video of a duck</video> and '.
+                '<video src="http://example.com/myimage.jpg">video of a cat</video> for <em>reference</em></p>',
+                FORMAT_HTML,
+                'See video of a duck and video of a cat for reference'
+            ],
+        ];
+
+        $feedback = $this->getMockForAbstractClass(
+            \grade_export::class,
+            [],
+            '',
+            false
+        );
+
+        foreach ($tests as $key => $testdetails) {
+            $expected = $testdetails[2];
+            $input = $testdetails[0];
+            $inputformat = $testdetails[1];
+
+            $this->assertEquals(
+                $expected,
+                $feedback->format_feedback((object) [
+                    'feedback' => $input,
+                    'feedbackformat' => $inputformat,
+                ], $gi1a),
+                $key
+            );
+        }
+    }
+
+    /**
      * Data provider for the format_feedback tests.
      *
      * @return  array
@@ -79,12 +144,12 @@ class core_grade_export_test extends advanced_testcase {
             'Has image (HTML)' => [
                 '<p>See this reference: <img src="http://example.com/myimage.jpg"></p>',
                 FORMAT_HTML,
-                'See this reference:  ',
+                'See this reference: ',
             ],
             'Has image and more (HTML)' => [
                 '<p>See <img src="http://example.com/myimage.jpg"> for <em>reference</em></p>',
                 FORMAT_HTML,
-                'See for reference',
+                'See  for reference',
             ],
             'Has video and more (HTML)' => [
                 '<p>See <video src="http://example.com/myimage.jpg">video of a duck</video> for <em>reference</em></p>',
@@ -92,15 +157,17 @@ class core_grade_export_test extends advanced_testcase {
                 'See video of a duck for reference',
             ],
             'Multiple videos (HTML)' => [
-                '<p>See <video src="http://example.com/myimage.jpg">video of a duck</video> and <video src="http://example.com/myimage.jpg">video of a cat</video> for <em>reference</em></p>',
+                '<p>See <video src="http://example.com/myimage.jpg">video of a duck</video> and '.
+                '<video src="http://example.com/myimage.jpg">video of a cat</video> for <em>reference</em></p>',
                 FORMAT_HTML,
-                'See video of a duck and video of a cat for reference',
+                'See video of a duck and video of a cat for reference'
             ],
             'HTML Looking tags in PLAIN' => [
                 'The way you have written the <img thing looks pretty fun >',
                 FORMAT_PLAIN,
-                'The way you have written the <img thing looks pretty fun >',
+                'The way you have written the &lt;img thing looks pretty fun &gt;',
             ],
+
         ];
     }
 }
