77612382a275408dc5c727b71bda51dc9f4821e7
Derick Turner <derick@e-learndesign.co.uk>
2018-11-08 14:07:19 +0000

    IOMAD: bad cherry-pick broke company lib

--- local/iomad/lib/company.php
+++ local/iomad/lib/company.php
@@ -2860,6 +2860,7 @@ class company {
                     }
                 }
             }
+        }
 
         return true;
     }
@@ -2876,37 +2877,6 @@ class company {
         $userid = $event->objectid;
         $user = $DB->get_record('user', array('id' => $userid));
         $user->manager = 'no';
-
-        if ($CFG->commerce_enable_external) {
-            // Fire off the payload to the external site.
-            require_once($CFG->dirroot . '/blocks/iomad_commerce/locallib.php');
-            iomad_commerce::update_user($user);
-        }
-
-        return true;
-    }
-
-    /**
-     * Triggered via user_updated event.
-     *
-     * @param \core\event\user_updated $event
-     * @return bool true on success.
-     */
-    public static function user_updated(\core\event\user_updated $event) {
-        global $DB, $CFG;
-
-        $userid = $event->objectid;
-        $user = $DB->get_record('user', array('id' => $userid));
-        if ($DB->get_record('company_users', array('userid'=> $user->id, 'managertype' => 1))) {
-            $user->manager = true;
-            $company = self::get_company_byuserid($user->id);
-            $user->country = $company->country;
-            $user->city = $company->city;
-            $user->adress = "";
-        } else {
-            $user->manager = false;
-        }
->>>>>>> b2003418fa0... IOMAD: added web services to manage users enrolments from purchases from external e-commerce solution.  Updated iomad commerce block to send you over to this.  Added events and handlers so that if this is enabled users are managed on the external side, (created and updated along with company name changes)
 
         if ($CFG->commerce_enable_external) {
             // Fire off the payload to the external site.
@@ -2944,60 +2914,8 @@ class company {
             // Fire off the payload to the external site.
             require_once($CFG->dirroot . '/blocks/iomad_commerce/locallib.php');
             iomad_commerce::update_user($user);
->>>>>>> cf0ac477442... IOMAD: external eccommerce not getting correct parameters
         }
 
-        return true;
-    }
-
-    /**
-     * Triggered via user_created event.
-     *
-     * @param \core\event\user_created $event
-     * @return bool true on success.
-     */
-    public static function user_created(\core\event\user_created $event) {
-        global $DB, $CFG;
-
-        $userid = $event->objectid;
-        $user = $DB->get_record('user', array('id' => $userid));
-        $user->manager = false;
-
-        if ($CFG->commerce_enable_external) {
-            // Fire off the payload to the external site.
-            require_once($CFG->dirroot . '/blocks/iomad_commerce/locallib.php');
-            iomad_commerce::update_user($user);
-        }
-
-        return true;
-    }
-
-    /**
-     * Triggered via user_updated event.
-     *
-     * @param \core\event\user_updated $event
-     * @return bool true on success.
-     */
-    public static function user_updated(\core\event\user_updated $event) {
-        global $DB, $CFG;
-
-        $userid = $event->objectid;
-        $user = $DB->get_record('user', array('id' => $userid));
-        if ($DB->get_record('company_users', array('userid'=> $user->id, 'managertype' => 1))) {
-            $user->manager = true;
-            $company = self::get_company_byuserid($user->id);
-            $user->country = $company->country;
-            $user->city = $company->city;
-            $user->adress = "";
-        } else {
-            $user->manager = false;
-        }
-
-        if ($CFG->commerce_enable_external) {
-            // Fire off the payload to the external site.
-            require_once($CFG->dirroot . '/blocks/iomad_commerce/locallib.php');
-            iomad_commerce::update_user($user);
-        }
         return true;
     }
 
