a14a44ee13bd8edab7119ac21036e4c298e157d6
Andrew Nicols <andrew@nicols.co.uk>
2018-11-08 15:32:20 +0800

    MDL-63924 core_privacy: Add shared_userlist provider

--- privacy/classes/local/request/userlist_provider.php
+++ privacy/classes/local/request/userlist_provider.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * This file contains an interface to describe classes which provide user data in some form to core.
+ * This file contains an interface to describe classes which userlist support.
  *
  * @package     core_privacy
  * @copyright   2018 Andrew Nicols <andrew@nicols.co.uk>
@@ -33,19 +33,5 @@ defined('MOODLE_INTERNAL') || die();
  * @package     core_privacy
  * @copyright   2018 Andrew Nicols <andrew@nicols.co.uk>
  */
-interface core_userlist_provider extends userlist_provider {
-
-    /**
-     * Get the list of users who have data within a context.
-     *
-     * @param   userlist    $userlist   The userlist containing the list of users who have data in this context/plugin combination.
-     */
-    public static function get_users_in_context(userlist $userlist);
-
-    /**
-     * Delete multiple users within a single context.
-     *
-     * @param   approved_userlist       $userlist The approved context and user information to delete information for.
-     */
-    public static function delete_data_for_users(approved_userlist $userlist);
+interface userlist_provider {
 }
