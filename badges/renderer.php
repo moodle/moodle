750f1588fa99adec7de47bd036bed77abd3b850c
Adrian Greeve <abgreeve@gmail.com>
2018-11-15 14:32:15 +0800

    MDL-64011 badges: Changed the date format of endorsements.

--- badges/renderer.php
+++ badges/renderer.php
@@ -1051,7 +1051,7 @@ class core_badges_renderer extends plugin_renderer_base {
             $dl[get_string('issueremail', 'badges')] = $endorsement->issueremail;
             $dl[get_string('issuerurl', 'badges')] = html_writer::link($endorsement->issuerurl, $endorsement->issuerurl,
                 array('target' => '_blank'));
-            $dl[get_string('dateawarded', 'badges')] = date('c', $endorsement->dateissued);
+            $dl[get_string('dateawarded', 'badges')] = userdate($endorsement->dateissued);
             $dl[get_string('claimid', 'badges')] = html_writer::link($endorsement->claimid, $endorsement->claimid,
             array('target' => '_blank'));
             $dl[get_string('claimcomment', 'badges')] = $endorsement->claimcomment;
