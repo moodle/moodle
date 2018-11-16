4f8c9232f14fe7ea6850cdd045d6a55f35d832aa
Mihail Geshoski <mihail@moodle.com>
2018-11-16 12:18:58 +0800

    MDL-63969 core_course: Include core_favourites subsystem in provider

--- lang/en/course.php
+++ lang/en/course.php
@@ -28,4 +28,6 @@ $string['aria:favourite'] = 'Course is starred';
 $string['favourite'] = 'Starred course';
 $string['privacy:perpage'] = 'The number of courses to show per page.';
 $string['privacy:completionpath'] = 'Course completion';
+$string['privacy:favouritespath'] = 'Course starred information';
 $string['privacy:metadata:completionsummary'] = 'The course contains completion information about the user.';
+$string['privacy:metadata:favouritessummary'] = 'The course contains information relating to the course being starred by the user.';
