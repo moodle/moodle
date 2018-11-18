fe5c789b1044f4ff2516798e30cc152dd7974b05
Gordon Bateson <gordonbateson@gmail.com>
2018-11-18 10:51:49 +0900

    MDL-79863 qtype_ordering: qtype/ordering add legacy_polyfill to privacy/provider.php

--- question/type/ordering/classes/privacy/provider.php
+++ question/type/ordering/classes/privacy/provider.php
@@ -37,13 +37,17 @@ defined('MOODLE_INTERNAL') || die();
  */
 class provider implements \core_privacy\local\metadata\null_provider {
 
+    // This polyfill allows the provider to work on both old (pre-7)
+    // and new PHP versions. Thanks to Tim Hunt for this suggestion.
+    use \core_privacy\local\legacy_polyfill;
+
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
      *
      * @return  string
      */
-    public static function get_reason() : string {
+    public static function _get_reason() {
         return 'privacy:metadata';
     }
 }
