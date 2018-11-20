0225ad42eab9f20ac4348dd85ccde8fdbfc206fb
David Monllao <davidm@moodle.com>
2018-11-20 12:55:28 +0100

    on-demand release 3.6beta+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2018112000.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2018112000.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.6beta (Build: 20181118)'; // Human-friendly version name
+$release  = '3.6beta+ (Build: 20181120)'; // Human-friendly version name
 
 $branch   = '36';                       // This version's branch.
 $maturity = MATURITY_BETA;             // This version's maturity level.
