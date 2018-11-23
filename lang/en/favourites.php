414da8c0be98321810c8022529cba294e7d7e493
Helen Foster <helen@moodle.org>
2018-11-23 14:39:54 +0100

    MDL-64186 lang: Import fixed English strings (en_fix)
    Significant string change:
    
    * togglemessagemenu, core_message - messages menu -> messaging drawer
      (new functionality)

--- lang/en/favourites.php
+++ lang/en/favourites.php
@@ -20,11 +20,11 @@
  * @copyright 2018 Jake Dallimore <jrhdallimore@gmail.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-$string['privacy:metadata:favourite'] = 'Stores references to items which have been marked as favourites.';
-$string['privacy:metadata:favourite:component'] = 'The component to which the favourite belongs to. E.g. core_user.';
-$string['privacy:metadata:favourite:itemid'] = 'The identifier for the item being marked as a favourite.';
-$string['privacy:metadata:favourite:itemtype'] = 'The type of the favourite item. E.g. course.';
-$string['privacy:metadata:favourite:ordering'] = 'A number used to order the favourites of the same type.';
-$string['privacy:metadata:favourite:timecreated'] = 'The time at which the item was marked as a favourite.';
-$string['privacy:metadata:favourite:timemodified'] = 'The time at which favourite was last modified.';
-$string['privacy:metadata:favourite:userid'] = 'The user who created the favourite.';
+$string['privacy:metadata:favourite'] = 'Stores references to items which have been starred.';
+$string['privacy:metadata:favourite:component'] = 'The component which the starred item belongs to e.g. core_user';
+$string['privacy:metadata:favourite:itemid'] = 'The identifier for the item being starred';
+$string['privacy:metadata:favourite:itemtype'] = 'The starred item type e.g. course';
+$string['privacy:metadata:favourite:ordering'] = 'A number used to order starred items of the same type';
+$string['privacy:metadata:favourite:timecreated'] = 'The time when the item was starred';
+$string['privacy:metadata:favourite:timemodified'] = 'The time when the starred item was last modified';
+$string['privacy:metadata:favourite:userid'] = 'The ID of the user who starred the item';
