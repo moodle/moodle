5471eb17586d76845adb02e58a6d7ca12404cea9
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2018-12-02 18:21:52 +0100

    MDL-64299 backup: Bump release to 3.7

--- backup/backup.class.php
+++ backup/backup.class.php
@@ -140,7 +140,7 @@ abstract class backup implements checksumable {
     /**
      * Usually same than major release zero version, mainly for informative/historic purposes.
      */
-    const RELEASE = '3.6';
+    const RELEASE = '3.7';
 
     /**
      * Cipher to be used in backup and restore operations.
