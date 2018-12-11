442f142522409f9f933e33986c7aa427338111da
Bas Brands <bas@moodle.com>
2018-12-11 18:45:02 +0100

    MDL-64038 core_message: add default group image
    add default group image to improve the alignment
    of group names in the message drawer group messages section

--- message/tests/api_test.php
+++ message/tests/api_test.php
@@ -1686,8 +1686,7 @@ class core_message_api_testcase extends core_message_messagelib_testcase {
         $conversations = \core_message\api::get_conversations($user3->id);
         $this->assertEquals(2, $conversations[0]->membercount);
         $this->assertEquals($course1->shortname, $conversations[0]->subname);
-        $groupimageurl = get_group_picture_url($group2, $group2->courseid, true);
-        $this->assertEquals($groupimageurl, $conversations[0]->imageurl);
+        $this->assertEquals('https://www.example.com/moodle/theme/image.php/_s/boost/core/1/g/g1', $conversations[0]->imageurl);
 
         // Now, disable the conversation linked to the group and verify it's no longer returned.
         $DB->set_field('message_conversations', 'enabled', 0, ['id' => $conversations[0]->id]);
