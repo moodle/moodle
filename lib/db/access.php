cfde0b8d38061f370a5c21523c2188aca5a7160d
Michael Hawkins <michaelh@moodle.com>
2018-12-20 10:10:09 +0800

    MDL-64395 groups: Added XSS Risk to the manage groups capability

--- lib/db/access.php
+++ lib/db/access.php
@@ -1103,6 +1103,7 @@ $capabilities = array(
     ),
 
     'moodle/course:managegroups' => array(
+        'riskbitmask' => RISK_XSS,
 
         'captype' => 'write',
         'contextlevel' => CONTEXT_COURSE,
--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2017111306.06;              // 20171113      = branching date YYYYMMDD - do not modify!
+$version  = 2017111306.07;              // 20171113      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
