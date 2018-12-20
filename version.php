38a1b4f203c706a3b374d343270ba9202cc17d93
Adrian Greeve <abgreeve@gmail.com>
2018-12-20 11:09:10 +0100

    weekly release 3.7dev

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2018121400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2018122000.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.7dev (Build: 20181214)'; // Human-friendly version name
+$release  = '3.7dev (Build: 20181220)'; // Human-friendly version name
 
 $branch   = '37';                       // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
