ed72175e45573d7a739ef40f15d5bd70574b6f11
Helen Foster <helen@moodle.org>
2019-01-07 20:21:09 +0100

    MDL-64509 lang: Import fixed English strings (en_fix)

--- cache/stores/memcached/lang/en/cachestore_memcached.php
+++ cache/stores/memcached/lang/en/cachestore_memcached.php
@@ -77,7 +77,7 @@ servername:port:weight
 If *Enable clustered servers* is enabled below, there must be only one server listed here. This would usually be a name that always resolves to the local machine, like 127.0.0.1 or localhost.';
 $string['serversclusterinvalid'] = 'Exactly one server is required when clustering is enabled.';
 $string['setservers'] = 'Set Servers';
-$string['setservers_help'] = 'This is the list of servers that will updated when data is modified in the cache. Generally the fully qualified name of each server in the pool.
+$string['setservers_help'] = 'This is the list of servers that will be updated when data is modified in the cache. Generally the fully qualified name of each server in the pool.
 It **must** include the server listed in *Servers* above, even if by a different hostname.
 Servers should be defined one per line and consist of a server address and optionally a port.
 If no port is provided then the default port (11211) is used.
