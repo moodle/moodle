65d70aa81b294d18a0b934f657274588b080710a
Helen Foster <helen@moodle.org>
2019-01-07 14:40:34 +0100

    MDL-64509 lang: Import fixed English strings (en_fix)
    Significant string change: enablerunnow_desc,tool_task - including
    mention of pathtophp requirement

--- blocks/course_list/lang/en/block_course_list.php
+++ blocks/course_list/lang/en/block_course_list.php
@@ -24,7 +24,7 @@
 
 $string['adminview'] = 'Admin view';
 $string['allcourses'] = 'Admin user sees all courses';
-$string['configadminview'] = 'What should the admin see in the course list block?';
+$string['configadminview'] = 'Whether to display all courses in the Courses block, or only courses that the admin is enrolled in.';
 $string['confighideallcourseslink'] = 'Remove the \'All courses\' link under the list of courses. (This setting does not affect the admin view.)';
 $string['course_list:addinstance'] = 'Add a new courses block';
 $string['course_list:myaddinstance'] = 'Add a new courses block to Dashboard';
