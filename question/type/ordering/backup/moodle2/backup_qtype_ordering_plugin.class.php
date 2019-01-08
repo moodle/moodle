912e5646e9a04de4e57aa6e973c6eee0a1492bbf
Gordon Bateson <gordonbateson@gmail.com>
2019-01-08 12:51:45 +0900

    MDL-79863 qtype_ordering: qtype/ordering add showgrading field to backup file

--- question/type/ordering/backup/moodle2/backup_qtype_ordering_plugin.class.php
+++ question/type/ordering/backup/moodle2/backup_qtype_ordering_plugin.class.php
@@ -51,7 +51,7 @@ class backup_qtype_ordering_plugin extends backup_qtype_plugin {
         $this->add_question_question_answers($pluginwrapper);
 
         // Now create the qtype own structures.
-        $fields = array('layouttype', 'selecttype', 'selectcount', 'gradingtype',
+        $fields = array('layouttype', 'selecttype', 'selectcount', 'gradingtype', 'showgrading',
                         'correctfeedback', 'correctfeedbackformat',
                         'incorrectfeedback', 'incorrectfeedbackformat',
                         'partiallycorrectfeedback', 'partiallycorrectfeedbackformat');
--- question/type/ordering/version.php
+++ question/type/ordering/version.php
@@ -29,5 +29,5 @@ $plugin->cron      = 0;
 $plugin->component = 'qtype_ordering';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400; // Moodle 2.0
-$plugin->version   = 2018122682;
-$plugin->release   = '2018-12-26 (82)';
+$plugin->version   = 2019010883;
+$plugin->release   = '2019-01-08 (83)';
