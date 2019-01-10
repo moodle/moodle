4c5b60a0f9e2efc2128f41dc0c8b5d06ea50cc48
Sara Arjona <sara@moodle.com>
2019-01-10 17:51:25 +0100

    weekly release 3.7dev

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2018122000.02;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2019011100.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.7dev (Build: 20181220)'; // Human-friendly version name
+$release  = '3.7dev (Build: 20190111)'; // Human-friendly version name
 
 $branch   = '37';                       // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
