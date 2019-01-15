cfac07f2833a53ae13cc0e23bfe2a26c70b79564
Luca Bösch <luca.boesch@bfh.ch>
2019-01-15 01:09:31 +0100

    MDL-64577 grade: style "Add ID numbers" button.

--- grade/edit/tree/calculation.php
+++ grade/edit/tree/calculation.php
@@ -139,7 +139,7 @@ echo '
         </div>
     </fieldset>
     <div class="fitem" style="text-align: center;">
-        <input id="id_addidnumbers" type="submit" value="'.get_string('addidnumbers', 'grades').'" name="addidnumbers" />
+        <input id="id_addidnumbers" type="submit" class="btn btn-secondary" value="' . get_string('addidnumbers', 'grades') . '" name="addidnumbers" />
     </div>
 </form>';
 
