62b942a5100746e0f36e6e5509f7df0715eab5d9
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2019-01-30 21:23:15 +0100

    MDL-64528 core_completion: new cap version bump

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2019012400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2019013000.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
