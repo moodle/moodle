6699b20eb957d7cda7a27041048ef2d10661027d
David Monllaó <davidm@moodle.com>
2019-02-12 09:48:58 +0100

    MDL-63706 tasks: Fix typo in pathtophp

--- admin/tool/task/lang/en/tool_task.php
+++ admin/tool/task/lang/en/tool_task.php
@@ -59,4 +59,3 @@ $string['taskscheduleminute_help'] = 'Minute field for task schedule. The field 
 $string['taskschedulemonth'] = 'Month';
 $string['taskschedulemonth_help'] = 'Month field for task schedule. The field uses the same format as unix cron. Some examples are:<br/><ul><li><strong>*</strong> Every month</li><li><strong>*/2</strong> Every second month</li><li><strong>1</strong> Every January</li><li><strong>1,5</strong> Every January and May</li></ul>';
 $string['privacy:metadata'] = 'The Scheduled task configuration plugin does not store any personal data.';
-
