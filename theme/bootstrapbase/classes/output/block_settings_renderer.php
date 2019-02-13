6a80186ebbd345fb76ae213812dfaa4d99ba20ff
Mathew May <mathewm@hotmail.co.nz>
2019-02-13 14:20:23 +0800

    MDL-58428 renderer: Move renderer override from blocks

--- theme/bootstrapbase/classes/output/block_settings_renderer.php
+++ theme/bootstrapbase/classes/output/block_settings_renderer.php
@@ -17,12 +17,12 @@
 /**
  * Overriden block settings renderer.
  *
- * @package    theme_boost
+ * @package    theme_bootstrapbase
  * @copyright  2016 Frédéric Massart - FMCorz.net
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace theme_boost\output;
+namespace theme_bootstrapbase\output;
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot . '/blocks/settings/renderer.php');
@@ -32,19 +32,21 @@ use moodle_url;
 /**
  * Overriden block settings renderer.
  *
- * @package    theme_boost
+ * @package    theme_bootstrapbase
  * @copyright  2016 Frédéric Massart - FMCorz.net
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class block_settings_renderer extends \block_settings_renderer {
 
     public function search_form(moodle_url $formtarget, $searchvalue) {
-        $data = [
-            'action' => $formtarget->out(false),
-            'label' => get_string('searchinsettings', 'admin'),
-            'searchvalue' => $searchvalue
-        ];
-        return $this->render_from_template('block_settings/search_form', $data);
+        $content = html_writer::start_tag('form', array('class'=>'adminsearchform', 'method'=>'get', 'action'=>$formtarget, 'role' => 'search'));
+        $content .= html_writer::start_tag('div');
+        $content .= html_writer::tag('label', s(get_string('searchinsettings', 'admin')), array('for'=>'adminsearchquery', 'class'=>'accesshide'));
+        $content .= html_writer::empty_tag('input', array('id'=>'adminsearchquery', 'type'=>'text', 'name'=>'query', 'value'=>s($searchvalue)));
+        $content .= html_writer::empty_tag('input', array('type'=>'submit', 'value'=>s(get_string('search'))));
+        $content .= html_writer::end_tag('div');
+        $content .= html_writer::end_tag('form');
+        return $content;
     }
 
 }
