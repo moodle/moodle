571b08b80081eeb15d5e19e1c83623d7d2483856
Derick Turner <derick@e-learndesign.co.uk>
2019-02-26 14:08:38 +0000

    IOMAD: user percentage was incorrect on course completion report

--- local/report_completion/report_course_completion_user_table.php
+++ local/report_completion/report_course_completion_user_table.php
@@ -201,7 +201,7 @@ class local_report_course_completion_user_table extends table_sql {
         } else {
             $total = $DB->count_records('course_completion_criteria', array('course' => $row->courseid));
             if ($total != 0) {
-                $usercount = $DB->count_records('course_completion_crit_compl', array('course' => $row->courseid, 'userid' => $row->id));
+                $usercount = $DB->count_records('course_completion_crit_compl', array('course' => $row->courseid, 'userid' => $row->userid));
                 $progress = round($usercount * 100 / $total, 0);
             } else {
                 $progress = -1;
