75dd3dda9efb29504e8ebbc27b01c8fa5f28738d
Derick Turner <derick@e-learndesign.co.uk>
2019-03-04 15:00:14 +0000

    IOMAD: Typo on company_course_create_form.php closes #1085

--- blocks/iomad_company_admin/company_course_create_form.php
+++ blocks/iomad_company_admin/company_course_create_form.php
@@ -264,9 +264,9 @@ if ($mform->is_cancelled()) {
 
     if (isset($data->submitandviewbutton)) {
         // We are going to the course instead.
-        redirect(new moodle_url('/course/view.php', array('id' => $course->id), get_string('coursecreatedok', 'block_iomad_company_admin'), null, \core\output\notification::NOTIFY_SUCCESS));
+        redirect(new moodle_url('/course/view.php', array('id' => $course->id)), get_string('coursecreatedok', 'block_iomad_company_admin'), null, \core\output\notification::NOTIFY_SUCCESS);
     } else {
-        redirect($companylist, get_string('coursecreatedok', 'block_iomad_company_admin'), null, \core\output\notification::NOTIFY_SUCCESS));
+        redirect($companylist, get_string('coursecreatedok', 'block_iomad_company_admin'), null, \core\output\notification::NOTIFY_SUCCESS);
     }
 } else {
 
