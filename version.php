4d3a32dde97e964ad4512c35d15a1784c69c247e
Sara Arjona <sara@moodle.com>
2019-03-08 11:03:15 +0100

    Moodle release 3.6.3

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2018120302.12;              // 20181203      = branching date YYYYMMDD - do not modify!
+$version  = 2018120303.00;              // 20181203      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.6.2+ (Build: 20190307)'; // Human-friendly version name
+$release  = '3.6.3 (Build: 20190311)'; // Human-friendly version name
 
 $branch   = '36';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
