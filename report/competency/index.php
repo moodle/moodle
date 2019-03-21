27737ae6660e92e2fe9a5b57c65ef0ebd4ba503b
Damyon Wiese <damyon@moodle.com>
2019-03-21 12:38:19 +0800

    MDL-52828 competencies: Filter by activity
    On the competency breakdown report, allow filter by competencies linked to an activity.

--- report/competency/index.php
+++ report/competency/index.php
@@ -31,6 +31,11 @@ $course = $DB->get_record('course', $params, '*', MUST_EXIST);
 require_login($course);
 $context = context_course::instance($course->id);
 $currentuser = optional_param('user', null, PARAM_INT);
+$currentmodule = optional_param('mod', null, PARAM_INT);
+if ($currentmodule > 0) {
+    $cm = get_coursemodule_from_id('', $currentmodule, 0, false, MUST_EXIST);
+    $context = context_module::instance($cm->id);
+}
 
 // Fetch current active group.
 $groupmode = groups_get_course_groupmode($course);
@@ -54,6 +59,7 @@ if (empty($currentuser)) {
 $urlparams = array('id' => $id);
 $navurl = new moodle_url('/report/competency/index.php', $urlparams);
 $urlparams['user'] = $currentuser;
+$urlparams['mod'] = $currentmodule;
 $url = new moodle_url('/report/competency/index.php', $urlparams);
 
 $title = get_string('pluginname', 'report_competency');
@@ -69,7 +75,7 @@ $output = $PAGE->get_renderer('report_competency');
 
 echo $output->header();
 $baseurl = new moodle_url('/report/competency/index.php');
-$nav = new \report_competency\output\user_course_navigation($currentuser, $course->id, $baseurl);
+$nav = new \report_competency\output\user_course_navigation($currentuser, $course->id, $baseurl, $currentmodule);
 echo $output->render($nav);
 if ($currentuser > 0) {
     $user = core_user::get_user($currentuser);
@@ -79,12 +85,16 @@ if ($currentuser > 0) {
         'user' => $user,
         'usercontext' => $usercontext
     );
+    if ($currentmodule > 0) {
+        $title = get_string('filtermodule', 'report_competency', format_string($cm->name));
+    }
     echo $output->context_header($userheading, 3);
 }
+echo $output->container('', 'clearfix');
 echo $output->heading($title, 3);
 
 if ($currentuser > 0) {
-    $page = new \report_competency\output\report($course->id, $currentuser);
+    $page = new \report_competency\output\report($course->id, $currentuser, $currentmodule);
     echo $output->render($page);
 } else {
     echo $output->container('', 'clearfix');
