97c85a001cba0e67ade67081d8d7bc7c6d00a732
Mark Nelson <markn@moodle.com>
2019-03-25 10:31:26 +0800

    MDL-64017 message_email: add new database table
    This table will be responsible for storing the messages
    that need to be emailed, but have not yet. A task will
    run that will email these and then remove them from the
    table.

--- message/output/email/version.php
+++ message/output/email/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018120300;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2019032500;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018112800;        // Requires this Moodle version
 $plugin->component = 'message_email';  // Full name of the plugin (used for diagnostics)
