a87f5b29344a1096fd2251044d4576af4364857a
Shamim Rezaie <shamim@moodle.com>
2019-04-03 04:05:24 +1100

    MDL-65142 dataformat_pdf: Added privacy implementation

--- dataformat/pdf/classes/privacy/provider.php
+++ dataformat/pdf/classes/privacy/provider.php
@@ -15,14 +15,32 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * pdf dataformat lang strings.
+ * Privacy provider implementation for dataformat_pdf.
  *
  * @package    dataformat_pdf
  * @copyright  2019 Shamim Rezaie <shamim@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['dataformat'] = 'Portable Document Format (.pdf)';
-$string['privacy:metadata'] = 'The PDF data format plugin does not store any personal data.';
-$string['shortname'] = 'PDF';
+namespace dataformat_pdf\privacy;
 
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Privacy provider implementation for dataformat_pdf.
+ *
+ * @copyright  2019 Shamim Rezaie <shamim@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+}
