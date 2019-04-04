e6725b8cbb2f9508070593b543abd765c7637422
Neill Magill <neill.magill@nottingham.ac.uk>
2019-04-04 11:33:31 +0100

    MDL-64336 assign: Add missing PHP doc

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -2124,6 +2124,7 @@ class assign {
      *
      * @param int $currentgroup
      * @param bool $idsonly
+     * @param bool $tablesort
      * @return array List of user records
      */
     public function list_participants($currentgroup, $idsonly, $tablesort = false) {
