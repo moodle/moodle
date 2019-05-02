902c8f58b24bb0c09bee8516d0dbf2340c99df53
Ryan Wyllie <ryan@moodle.com>
2019-05-02 10:54:27 +0800

    MDL-65464 forum: disable buttons when saving in page reply

--- mod/forum/amd/src/inpage_reply.js
+++ mod/forum/amd/src/inpage_reply.js
@@ -14,13 +14,9 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * This module is the highest level module for the calendar. It is
- * responsible for initialising all of the components required for
- * the calendar to run. It also coordinates the interaction between
- * components by listening for and responding to different events
- * triggered within the calendar UI.
+ * This module handles the in page replying to forum posts.
  *
- * @module     mod_forum/posts_list
+ * @module     mod_forum/inpage_reply
  * @package    mod_forum
  * @copyright  2019 Peter Dias
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -46,18 +42,57 @@ define([
         FLAT_NEWEST_FIRST: -1
     };
 
+    /**
+     * Show the loading icon for the submit button.
+     *
+     * @param {Object} button The submit button element
+     */
+    var showSubmitButtonLoadingIcon = function(button) {
+        var textContainer = button.find(Selectors.post.inpageSubmitBtnText);
+        var loadingIconContainer = button.find(Selectors.post.loadingIconContainer);
+        var width = button.outerWidth();
+        // Fix the width so that the button size doesn't change when we show the loading icon.
+        button.css('width', width);
+        textContainer.addClass('hidden');
+        loadingIconContainer.removeClass('hidden');
+    };
+
+    /**
+     * Hide the loading icon for the submit button.
+     *
+     * @param {Object} button The submit button element
+     */
+    var hideSubmitButtonLoadingIcon = function(button) {
+        var textContainer = button.find(Selectors.post.inpageSubmitBtnText);
+        var loadingIconContainer = button.find(Selectors.post.loadingIconContainer);
+        // Reset the width back to it's default.
+        button.css('width', '');
+        textContainer.removeClass('hidden');
+        loadingIconContainer.addClass('hidden');
+    };
+
+    /**
+     * Register the event listeners for the submit button of the in page reply.
+     *
+     * @param {Object} root The discussion container element.
+     */
     var registerEventListeners = function(root) {
         root.on('click', Selectors.post.inpageSubmitBtn, function(e) {
             e.preventDefault();
-            var form = $(e.currentTarget).parents(Selectors.post.inpageReplyForm).get(0);
+            var submitButton = $(e.currentTarget);
+            var allButtons = submitButton.parent().find(Selectors.post.inpageReplyButton);
+            var form = submitButton.parents(Selectors.post.inpageReplyForm).get(0);
             var message = form.elements.post.value.trim();
             var postid = form.elements.reply.value;
             var subject = form.elements.subject.value;
-            var currentRoot = $(e.currentTarget).parents(Selectors.post.forumContent);
+            var currentRoot = submitButton.parents(Selectors.post.forumContent);
             var mode = parseInt(root.find(Selectors.post.modeSelect).get(0).value);
             var newid;
 
             if (message.length) {
+                showSubmitButtonLoadingIcon(submitButton);
+                allButtons.prop('disabled', true);
+
                 Repository.addDiscussionPost(postid, subject, message)
                     .then(function(context) {
                         var message = context.messages.reduce(function(carry, message) {
@@ -105,13 +140,19 @@ define([
                         }
                     })
                     .then(function() {
+                        hideSubmitButtonLoadingIcon(submitButton);
+                        allButtons.prop('disabled', false);
                         return currentRoot.find(Selectors.post.inpageReplyContent).hide();
                     })
                     .then(function() {
                         location.href = "#p" + newid;
                         return;
                     })
-                    .fail(Notification.exception);
+                    .catch(function(error) {
+                        hideSubmitButtonLoadingIcon(submitButton);
+                        allButtons.prop('disabled', false);
+                        return Notification.exception(error);
+                    });
             }
         });
     };
@@ -119,7 +160,6 @@ define([
     return {
         init: function(root) {
             registerEventListeners(root);
-
         }
     };
 });
