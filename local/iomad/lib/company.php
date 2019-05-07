bef607c84d692b3f88a7dcb9e35308db41b3e3d0
Derick Turner <derick@e-learndesign.co.uk>
2019-05-07 11:40:09 +0100

    IOMAD: add company to external WS call if user doesn't have it set already

--- local/iomad/lib/company.php
+++ local/iomad/lib/company.php
@@ -3398,6 +3398,12 @@ class company {
         $user->company = $company->name;
 
         if ($CFG->commerce_enable_external && !empty($CFG->commerce_externalshop_url)) {
+            if (empty($user->company)) {
+                if ($company = self::by_userid($userid)) {
+                    $user->company = $company->get_name();
+                }
+            }
+
             // Fire off the payload to the external site.
             require_once($CFG->dirroot . '/blocks/iomad_commerce/locallib.php');
             iomad_commerce::update_user($user);
