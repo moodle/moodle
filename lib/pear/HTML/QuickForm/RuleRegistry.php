1453ce1208c84ea43d50720bdd9c02899307f5cb
Shamim Rezaie <shamim@moodle.com>
2019-05-09 12:22:30 +0800

    MDL-65506 forms: get the correct value for advcheckbox fields in js

--- lib/pear/HTML/QuickForm/RuleRegistry.php
+++ lib/pear/HTML/QuickForm/RuleRegistry.php
@@ -302,15 +302,13 @@ class HTML_QuickForm_RuleRegistry
                     "    }\n";
             }
 
+        } elseif ($element->getType() == 'advcheckbox') {
+            $value = "  value{$jsIndex} = frm.elements['$elementName'][1].checked ?" .
+                    " frm.elements['$elementName'][1].value : frm.elements['$elementName'][0].value;\n";
+            $tmp_reset .= $reset ? "    field[1].checked = field[1].defaultChecked;\n" : '';
         } elseif ($element->getType() == 'checkbox') {
-            if (is_a($element, 'html_quickform_advcheckbox')) {
-                $value = "  value{$jsIndex} = frm.elements['$elementName'][1].checked? frm.elements['$elementName'][1].value: frm.elements['$elementName'][0].value;\n";
-                $tmp_reset .= $reset ? "    field[1].checked = field[1].defaultChecked;\n" : '';
-            } else {
-                $value = "  value{$jsIndex} = frm.elements['$elementName'].checked? '1': '';\n";
-                $tmp_reset .= $reset ? "    field.checked = field.defaultChecked;\n" : '';
-            }
-
+            $value = "  value{$jsIndex} = frm.elements['$elementName'].checked? '1': '';\n";
+            $tmp_reset .= $reset ? "    field.checked = field.defaultChecked;\n" : '';
         } elseif ($element->getType() == 'radio') {
             $value = "  value{$jsIndex} = '';\n" .
                      // Fix for bug #5644
