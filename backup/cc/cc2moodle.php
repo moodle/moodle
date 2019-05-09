a2b727d7bc9526c89ee12e38212c4560f1727d3c
Stephen Bourget <steve.bourget@sau19.org>
2019-05-08 20:47:29 -0400

    MDL-69900 core_backup: Allow IMSCC v1.0 restore to finish

--- backup/cc/cc2moodle.php
+++ backup/cc/cc2moodle.php
@@ -794,7 +794,7 @@ class cc2moodle {
         }
     }
 
-    protected function critical_error ($text) {
+    protected static function critical_error ($text) {
 
         $path_to_log = static::log_file();
 
