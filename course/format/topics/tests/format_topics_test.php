14f74896752026413bd5d9caf56592ab51bcfcb5
David Mudrák <david@moodle.com>
2019-05-10 10:26:26 +0200

    MDL-65537 lang: Use fixed strings in tests, too

--- course/format/topics/tests/format_topics_test.php
+++ course/format/topics/tests/format_topics_test.php
@@ -182,7 +182,7 @@ class format_topics_testcase extends advanced_testcase {
             $tmpl = component_callback('format_weeks', 'inplace_editable', array('sectionname', $section->id, 'New name'));
             $this->fail('Exception expected');
         } catch (moodle_exception $e) {
-            $this->assertEquals(1, preg_match('/^Can not find data record in database/', $e->getMessage()));
+            $this->assertEquals(1, preg_match('/^Can\'t find data record in database/', $e->getMessage()));
         }
     }
 
