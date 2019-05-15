2ddd7605d87a68848a770a0c543c8dfce1900b5b
Mathew May <mathewm@hotmail.co.nz>
2019-05-15 11:21:27 +0800

    MDL-65606 mod_data: Add approvalstatusclass to template

--- mod/data/version.php
+++ mod/data/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019052000;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2019052001;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019051100;       // Requires this Moodle version
 $plugin->component = 'mod_data';       // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
