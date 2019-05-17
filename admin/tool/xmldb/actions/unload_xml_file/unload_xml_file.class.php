ec2f7c0fe9552dc8051e92370e13b2775a119f68
Damyon Wiese <damyon@moodle.com>
2019-05-17 13:41:45 +0800

    MDL-53689 xmldb: Require a sesskey to load xml
    Loading and unloading xml files is not protected from XSS with a sesskey.

--- admin/tool/xmldb/actions/unload_xml_file/unload_xml_file.class.php
+++ admin/tool/xmldb/actions/unload_xml_file/unload_xml_file.class.php
@@ -35,9 +35,6 @@ class unload_xml_file extends XMLDBAction {
     function init() {
         parent::init();
 
-        // Set own custom attributes
-        $this->sesskey_protected = false; // This action doesn't need sesskey protection
-
         // Get needed strings
         $this->loadStrings(array(
             // 'key' => 'module',
