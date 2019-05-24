ec5d9e968a785a3972f3c0872b2f391c8b0dd874
Shamim Rezaie <shamim@moodle.com>
2019-05-25 03:54:58 +1000

    MDL-64523 core_access: Improve the title of the delete role links

--- admin/roles/classes/permissions_table.php
+++ admin/roles/classes/permissions_table.php
@@ -96,7 +96,7 @@ class core_role_permissions_table extends core_role_capability_table_base {
                                   "linkclass" => "preventlink", "adminurl" => $adminurl->out(), "icon" => "", "iconalt" => "");
                 if (isset($overridableroles[$id]) and ($allowoverrides or ($allowsafeoverrides and is_safe_capability($capability)))) {
                     $templatecontext['icon'] = 't/delete';
-                    $templatecontext['iconalt'] = get_string('delete');
+                    $templatecontext['iconalt'] = get_string('deletexrole', 'core_role', $name);
                 }
                 $neededroles[$id] = $renderer->render_from_template('core/permissionmanager_role', $templatecontext);
             }
@@ -109,7 +109,7 @@ class core_role_permissions_table extends core_role_capability_table_base {
                                 "icon" => "", "iconalt" => "");
                 if (isset($overridableroles[$id]) and prohibit_is_removable($id, $context, $capability->name)) {
                     $templatecontext['icon'] = 't/delete';
-                    $templatecontext['iconalt'] = get_string('delete');
+                    $templatecontext['iconalt'] = get_string('deletexrole', 'core_role', $name);
                 }
                 $forbiddenroles[$id] = $renderer->render_from_template('core/permissionmanager_role', $templatecontext);
             }
