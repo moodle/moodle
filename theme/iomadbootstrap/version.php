72cdfe7b1df95c31bb4c87db009cf82711294f99
Derick Turner <derick@e-learndesign.co.uk>
2019-05-27 12:26:00 +0100

    IOMAD: Forgot themes for release version info #1130

--- theme/iomadbootstrap/version.php
+++ theme/iomadbootstrap/version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2016111800;
 $plugin->requires  = 2016052300;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '3.1.0';
+$plugin->release  = '3.6.4+ (Build: 20190527)'; // Human-friendly version name
 $plugin->component = 'theme_iomadbootstrap';
