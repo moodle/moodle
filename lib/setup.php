af1a436b2b53a1e36df66e3795ce227cc08d76de
Andrew Nicols <andrew@nicols.co.uk>
2019-06-18 12:12:03 +0800

    MDL-60978 testing: Support ability to run phpunit in isolated process

--- lib/setup.php
+++ lib/setup.php
@@ -620,8 +620,12 @@ setup_validate_php_configuration();
 setup_DB();
 
 if (PHPUNIT_TEST and !PHPUNIT_UTIL) {
-    // make sure tests do not run in parallel
-    test_lock::acquire('phpunit');
+    // Make sure tests do not run in parallel.
+    $suffix = '';
+    if (phpunit_util::is_in_isolated_process()) {
+        $suffix = '.isolated';
+    }
+    test_lock::acquire('phpunit', $suffix);
     $dbhash = null;
     try {
         if ($dbhash = $DB->get_field('config', 'value', array('name'=>'phpunittest'))) {
