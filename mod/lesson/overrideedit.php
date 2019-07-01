ccca3fc6e455dbc9d90af87fb1046cd39ee7749e
Shamim Rezaie <shamim@moodle.com>
2019-07-02 03:40:42 +1000

    MDL-61115: mod_lesson: Check if the teacher can access the override

--- mod/lesson/overrideedit.php
+++ mod/lesson/overrideedit.php
@@ -76,6 +76,16 @@ require_capability('mod/lesson:manageoverrides', $context);
 if ($overrideid) {
     // Editing an override.
     $data = clone $override;
+
+    if ($override->groupid) {
+        if (!groups_group_visible($override->groupid, $course, $cm)) {
+            print_error('invalidoverrideid', 'lesson');
+        }
+    } else {
+        if (!groups_user_groups_visible($course, $override->userid, $cm)) {
+            print_error('invalidoverrideid', 'lesson');
+        }
+    }
 } else {
     // Creating a new override.
     $data = new stdClass();
