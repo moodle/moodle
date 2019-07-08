9789f683ed61f706d7a02990709a1cf8e8b29f8f
Noy Cohen <noy.cohen@kaltura.com>
2019-07-08 13:26:45 +0300

    version bump - 4.2.1

--- blocks/kalturamediagallery/version.php
+++ blocks/kalturamediagallery/version.php
@@ -14,11 +14,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019041636;
-$plugin->release = 'Kaltura release 4.2.0';
+$plugin->version   = 2019070837;
+$plugin->release = 'Kaltura release 4.2.1';
 $plugin->requires  = 2018120300;
 $plugin->component = 'block_kalturamediagallery';
 $plugin->dependencies = array(
-    'local_kaltura' => 2019041636,
-    'local_kalturamediagallery' => 2019041636
+    'local_kaltura' => 2019070837,
+    'local_kalturamediagallery' => 2019070837
 );
--- lib/editor/atto/plugins/kalturamedia/version.php
+++ lib/editor/atto/plugins/kalturamedia/version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019041636;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release = 'Kaltura release 4.2.0';
+$plugin->version   = 2019070837;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = 'Kaltura release 4.2.1';
 $plugin->requires  = 2018120300;        // Requires this Moodle version.
 $plugin->component = 'atto_kalturamedia';  // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = array(
-    'local_kaltura' => 2019041636
+    'local_kaltura' => 2019070837
 );
