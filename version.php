50cc80fd3b2ece78b3347fb677bda8a12a751dd7
David Balch <david.balch@conted.ox.ac.uk>
2019-07-23 11:35:08 +0100

    MDL-66213 roles: Add capability for viewing anonymous events.

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2019122000.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2019122000.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '3.9dev (Build: 20191220)'; // Human-friendly version name
