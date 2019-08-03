52dfd995d1d4def5c471b7a220b2d504c0913bb0
nayabbukhari <bec_dir@yahoo.com>
2019-08-03 11:51:48 +0300

    initial Clamav

--- lib/antivirus/clamav/version.php
+++ lib/antivirus/clamav/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019052000;          // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019052000.008;          // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019051100;          // Requires this Moodle version.
 $plugin->component = 'antivirus_clamav';  // Full name of the plugin (used for diagnostics).
