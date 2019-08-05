8ea1e39f4c7ed53a0ae85a91ce1640215073f53f
Bas Brands <bas@moodle.com>
2019-08-05 13:37:14 +0200

    MDL-55751 themes: remove the css chunker
    The css chunker is a workaround a browser bug for old IE versions

--- theme/styles_debug.php
+++ theme/styles_debug.php
@@ -36,7 +36,6 @@ $type      = optional_param('type', '', PARAM_SAFEDIR);
 $subtype   = optional_param('subtype', '', PARAM_SAFEDIR);
 $sheet     = optional_param('sheet', '', PARAM_SAFEDIR);
 $usesvg    = optional_param('svg', 1, PARAM_BOOL);
-$chunk     = optional_param('chunk', null, PARAM_INT);
 $rtl       = optional_param('rtl', false, PARAM_BOOL);
 
 if (file_exists("$CFG->dirroot/theme/$themename/config.php")) {
@@ -56,9 +55,6 @@ if ($type === 'editor') {
     css_send_uncached_css($csscontent);
 }
 
-$chunkurl = new moodle_url('/theme/styles_debug.php', array('theme' => $themename,
-    'type' => $type, 'subtype' => $subtype, 'sheet' => $sheet, 'usesvg' => $usesvg, 'rtl' => $rtl));
-
 // We need some kind of caching here because otherwise the page navigation becomes
 // way too slow in theme designer mode. Feel free to create full cache definition later...
 $key = "$type $subtype $sheet $usesvg $rtl";
@@ -66,27 +62,11 @@ $cache = cache::make_from_params(cache_store::MODE_APPLICATION, 'core', 'themede
 if ($content = $cache->get($key)) {
     if ($content['created'] > time() - THEME_DESIGNER_CACHE_LIFETIME) {
         $csscontent = $content['data'];
-
-        // We need to chunk the content.
-        if ($chunk !== null) {
-            $chunks = css_chunk_by_selector_count($csscontent, $chunkurl->out(false));
-            $csscontent = ($chunk === 0) ? end($chunks) : $chunks[$chunk - 1];
-        }
-
         css_send_uncached_css($csscontent);
     }
 }
 
 $csscontent = $theme->get_css_content_debug($type, $subtype, $sheet);
 $cache->set($key, array('data' => $csscontent, 'created' => time()));
-
-// We need to chunk the content.
-if ($chunk !== null) {
-    // The chunks are ordered so that the last chunk is the one containing the @import, and so
-    // the first one to be included. All the other chunks are set in the array before that one.
-    // See {@link css_chunk_by_selector_count()} for more details.
-    $chunks = css_chunk_by_selector_count($csscontent, $chunkurl->out(false));
-    $csscontent = ($chunk === 0) ? end($chunks) : $chunks[$chunk - 1];
-}
 
 css_send_uncached_css($csscontent);
