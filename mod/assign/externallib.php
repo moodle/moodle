a25615ff66273995f0ab13904ffd0e02cc84f334
Juan Leyva <juanleyvadelgado@gmail.com>
2019-08-16 10:37:12 +0100

    MDL-66113 assign: Apply user overrides in get_submission_status WS

--- mod/assign/externallib.php
+++ mod/assign/externallib.php
@@ -2330,6 +2330,8 @@ class mod_assign_external extends external_api {
             throw new required_capability_exception($context, 'mod/assign:viewgrades', 'nopermission', '');
         }
 
+        $assign->update_effective_access($user->id);
+
         $gradingsummary = $lastattempt = $feedback = $previousattempts = null;
 
         // Get the renderable since it contais all the info we need.
