f3e79377a3a5a16984255dd5cc7b8cc93acf10b5
Mike Churchward <mike.churchward@poetgroup.org>
2019-08-28 09:55:49 -0400

    Adding mobile support to mycourses block.

--- blocks/mycourses/version.php
+++ blocks/mycourses/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015051500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015051502;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'block_mycourses'; // Full name of the plugin (used for diagnostics)
