dff565b32cb0e2b3823ab2e10df9223dc8ec022a
Jun Pataleta <jun@moodle.com>
2019-08-28 16:06:58 +0800

    MDL-66481 mod_forum: Move "Mark as read" into the discussion menu

--- mod/forum/lang/en/forum.php
+++ mod/forum/lang/en/forum.php
@@ -353,6 +353,7 @@ $string['mailnow'] = 'Send forum post notifications with no editing-time delay';
 $string['manydiscussions'] = 'Discussions per page';
 $string['managesubscriptionsoff'] = 'Finish managing subscriptions';
 $string['managesubscriptionson'] = 'Manage subscribers';
+$string['markasread'] = 'Mark as read';
 $string['markalldread'] = 'Mark all posts in this discussion read.';
 $string['markallread'] = 'Mark all posts in this forum read.';
 $string['markasreadonnotification'] = 'When sending forum post notifications';
