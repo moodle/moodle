5a1fd6e85481c36020e0e7eeda6ebcebee21971d
Simey Lameze <simey@moodle.com>
2019-08-29 10:57:23 +0800

    MDL-66551 core_user: delete_user() should remove users calendar subs

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -4244,6 +4244,9 @@ function delete_user(stdClass $user) {
 
     // Now do a brute force cleanup.
 
+    // Remove user's calendar subscriptions.
+    $DB->delete_records('event_subscriptions', ['userid' => $user->id]);
+
     // Remove from all cohorts.
     $DB->delete_records('cohort_members', array('userid' => $user->id));
 
