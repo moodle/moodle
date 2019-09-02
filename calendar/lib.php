4f4d28b56ba400b3893697cbb3a7c344b98234ba
David Matamoros <davidmc@moodle.com>
2019-09-02 14:59:30 +0200

    MDL-66354 core_calendar: Notices in error.log on calendar events

--- calendar/lib.php
+++ calendar/lib.php
@@ -3661,6 +3661,9 @@ function calendar_get_filter_types() {
         return [
             'eventtype' => $type,
             'name' => get_string("eventtype{$type}", "calendar"),
+            'icon' => true,
+            'key' => 'i/'.$type.'event',
+            'component' => 'core'
         ];
     }, $types);
 }
