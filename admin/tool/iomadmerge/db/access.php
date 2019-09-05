f47d1504546ffd3d1325dc11b4bcdb30a092c574
Derick Turner <derick@e-learndesign.co.uk>
2019-09-05 12:07:05 +0100

    IOMAD: converted mergeuser tool to iomad.

---
+++ admin/tool/iomadmerge/db/access.php
@@ -0,0 +1,15 @@
+<?php
+
+$capabilities = array(
+    'tool/iomadmerge:iomadmerge' => array(
+        'riskbitmask' => RISK_DATALOSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'manager' => CAP_ALLOW,
+        ),
+        'clonepermissionsfrom' => 'moodle/user:delete'
+    ),
+ );
+
+
