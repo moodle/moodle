35b62d00b8d3f3093df9811d1a1bc58429701b30
Sara Arjona <sara@moodle.com>
2019-09-06 13:54:48 +0200

    MDL-66609 core_h5p: Player and embed.php implementation

--- lang/en/h5p.php
+++ lang/en/h5p.php
@@ -20,5 +20,5 @@
  * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 $string['privacy:metadata'] = 'H5P subsystem does not store any personal data.';
+$string['h5pfilenotfound'] = 'H5P file not found';
