2aaac3f67093e81912ba9971dc7e40a56d5c22a4
Nathan Nguyen <nathannguyen@catalyst-au.net>
2019-09-12 12:10:13 +1000

    MDL-46978 core_grades: preserve category total visibility

--- lib/grade/grade_item.php
+++ lib/grade/grade_item.php
@@ -681,9 +681,7 @@ class grade_item extends grade_object {
             if ($category_array && array_key_exists($this->categoryid, $category_array)) {
                 $category = $category_array[$this->categoryid];
                 //call set_hidden on the category regardless of whether it is hidden as its parent might be hidden
-                //if($category->is_hidden()) {
-                    $category->set_hidden($hidden, false);
-                //}
+                $category->set_hidden($hidden, false);
             }
         }
     }
