4897a4a16651a6d7f27de3c731161466d32adf32
Jun Pataleta <jun@moodle.com>
2019-09-27 11:12:20 +0800

    weekly release 3.6.6+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2018120306.03;              // 20181203      = branching date YYYYMMDD - do not modify!
+$version  = 2018120306.04;              // 20181203      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.6.6+ (Build: 20190920)'; // Human-friendly version name
+$release  = '3.6.6+ (Build: 20190927)'; // Human-friendly version name
 
 $branch   = '36';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
