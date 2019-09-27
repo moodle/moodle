e1fb8e4aec22ac1dd9ff5d8154a6b64396d237cb
Mihail Geshoski <mihail@moodle.com>
2019-09-27 13:22:52 +0800

    MDL-64745 block_online_users: Option to disable users to set visibility

--- blocks/online_users/settings.php
+++ blocks/online_users/settings.php
@@ -27,5 +27,9 @@ defined('MOODLE_INTERNAL') || die;
 if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configtext('block_online_users_timetosee', get_string('timetosee', 'block_online_users'),
                    get_string('configtimetosee', 'block_online_users'), 5, PARAM_INT));
+
+    $settings->add(new admin_setting_configcheckbox('block_online_users_onlinestatushiding',
+            get_string('onlinestatushiding', 'block_online_users'),
+            get_string('onlinestatushiding_desc', 'block_online_users'), 1));
 }
 
