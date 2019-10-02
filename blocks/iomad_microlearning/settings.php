f4c659d0a315ba4009bcd4f1b1b27de019f84d75
Derick Turner <derick@e-learndesign.co.uk>
2019-10-02 11:45:15 +0100

    IOMAD: microlearning lang identifier was wrong in settings

--- blocks/iomad_microlearning/settings.php
+++ blocks/iomad_microlearning/settings.php
@@ -42,8 +42,8 @@ if ($ADMIN->fulltree) {
     $settings = new admin_settingpage('block_iomad_microlearning', get_string('pluginname', 'block_iomad_microlearning'));
 
     $settings->add(new admin_setting_configtext('microlearninglinkexpires',
-                                                get_string('microllinkexpires', 'block_iomad_microlearning'),
-                                                get_string('microllinkexpires_help', 'block_iomad_microlearning'),
+                                                get_string('microlearninglinkexpires', 'block_iomad_microlearning'),
+                                                get_string('microlearninglinkexpires_help', 'block_iomad_microlearning'),
                                                 '30',
                                                 PARAM_INT));
 
