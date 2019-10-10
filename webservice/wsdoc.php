5779862ee7b7749d99bd86a9dd6cfd0f53ce9dd9
Luca Bösch <luca.boesch@bfh.ch>
2019-10-10 10:04:05 +0200

    MDL-66847 webservice: Correct breadcrumb + title for ws documentation.

--- webservice/wsdoc.php
+++ webservice/wsdoc.php
@@ -35,17 +35,18 @@ $tokenid = required_param('id', PARAM_INT);
 // PAGE settings
 $PAGE->set_context($usercontext);
 $PAGE->set_url('/user/wsdoc.php');
-$PAGE->set_title(get_string('documentation', 'webservice'));
-$PAGE->set_heading(get_string('documentation', 'webservice'));
+$PAGE->set_title(get_string('wsdocumentation', 'webservice'));
+$PAGE->set_heading(get_string('wsdocumentation', 'webservice'));
 $PAGE->set_pagelayout('standard');
 
 // nav bar
 $PAGE->navbar->ignore_active(true);
-$PAGE->navbar->add(get_string('usercurrentsettings'));
+$PAGE->navbar->add(get_string('preferences'), new moodle_url('/user/preferences.php'));
+$PAGE->navbar->add(get_string('useraccount'));
 $PAGE->navbar->add(get_string('securitykeys', 'webservice'),
         new moodle_url('/user/managetoken.php', 
                 array('id' => $tokenid, 'sesskey' => sesskey())));
-$PAGE->navbar->add(get_string('documentation', 'webservice'));
+$PAGE->navbar->add(get_string('wsdocumentation', 'webservice'));
 
 // check web service are enabled
 if (empty($CFG->enablewsdocumentation)) {
