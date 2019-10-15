80d7651534de2fd8ebfb0cfec528ac1b1e06b06d
Jun Pataleta <jun@moodle.com>
2019-10-15 14:50:06 +0800

    MDL-66790 quizreport: Prevent sorting for checkbox column

--- mod/quiz/report/responses/report.php
+++ mod/quiz/report/responses/report.php
@@ -141,8 +141,9 @@ class quiz_responses_report extends quiz_attempts_report {
             $headers = array();
 
             if (!$table->is_downloading() && $options->checkboxcolumn) {
-                $columns[] = 'checkbox';
-                $headers[] = $table->checkbox_col_header();
+                $columnname = 'checkbox';
+                $columns[] = $columnname;
+                $headers[] = $table->checkbox_col_header($columnname);
             }
 
             $this->add_user_columns($table, $columns, $headers);
