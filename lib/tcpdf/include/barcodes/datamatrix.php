71af7accffaf171bd66349d0b5a135f32f2dbd77
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2019-10-20 02:14:51 +0200

    MDL-66966 tcpdf: Some more curly braces to fix
    This is https://git.io/JeKfT

--- lib/tcpdf/include/barcodes/datamatrix.php
+++ lib/tcpdf/include/barcodes/datamatrix.php
@@ -629,7 +629,7 @@ class Datamatrix {
 					if ($numch[ENC_C40] == $numch[ENC_X12]) {
 						$k = ($pos + $charscount + 1);
 						while ($k < $data_length) {
-							$tmpchr = ord($data{$k});
+							$tmpchr = ord($data[$k]);
 							if ($this->isCharMode($tmpchr, ENC_X12)) {
 								return ENC_X12;
 							} elseif (!($this->isCharMode($tmpchr, ENC_X12) OR $this->isCharMode($tmpchr, ENC_C40))) {
