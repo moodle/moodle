5da7a7fc31a8e6235330b8d34a3ecf9ce7e35b79
Sara Arjona <sara@moodle.com>
2019-10-21 15:34:02 +0200

    MDL-66609 core_h5p: Change core files when upgrading lib
    This commit contains the changes required when upgrading the H5P
    PHP library.

--- lib/h5p/h5p.classes.php
+++ lib/h5p/h5p.classes.php
@@ -758,11 +758,14 @@ class H5PValidator {
       unlink($tmpPath);
       return FALSE;
     }
+    // Moodle: the extension mbstring is optional.
+    /*
     if (!extension_loaded('mbstring')) {
       $this->h5pF->setErrorMessage($this->h5pF->t('The mbstring PHP extension is not loaded. H5P need this to function properly'), 'mbstring-unsupported');
       unlink($tmpPath);
       return FALSE;
     }
+    */
 
     // Create a temporary dir to extract package in.
     $tmpDir = $this->h5pF->getUploadedH5pFolderPath();
@@ -809,7 +812,7 @@ class H5PValidator {
       }
       $totalSize += $fileStat['size'];
 
-      $fileName = mb_strtolower($fileStat['name']);
+      $fileName = \core_text::strtolower($fileStat['name']);
       if (preg_match('/(^[\._]|\/[\._])/', $fileName) !== 0) {
         continue; // Skip any file or folder starting with a . or _
       }
@@ -2432,7 +2435,7 @@ class H5PCore {
     // Using content dependencies
     foreach ($dependencies as $dependency) {
       if (isset($dependency['path']) === FALSE) {
-        $dependency['path'] = 'libraries/' . H5PCore::libraryToString($dependency, TRUE);
+        $dependency['path'] = $this->getDependencyPath($dependency);
         $dependency['preloadedJs'] = explode(',', $dependency['preloadedJs']);
         $dependency['preloadedCss'] = explode(',', $dependency['preloadedCss']);
       }
@@ -2450,6 +2453,16 @@ class H5PCore {
     }
 
     return $files;
+  }
+
+  /**
+   * Get the path to the dependency.
+   *
+   * @param stdClass $dependency
+   * @return string
+   */
+  protected function getDependencyPath(array $dependency): string {
+    return H5PCore::libraryToString($dependency, TRUE);
   }
 
   private static function getDependenciesHash(&$dependencies) {
@@ -3303,12 +3316,15 @@ class H5PCore {
       $setup->disable_hub = TRUE;
     }
 
+    // Moodle: the extension mbstring is optional.
+    /*
     if (!extension_loaded('mbstring')) {
       $setup->errors[] = $this->h5pF->t(
         'The mbstring PHP extension is not loaded. H5P needs this to function properly'
       );
       $setup->disable_hub = TRUE;
     }
+    */
 
     // Check php version >= 5.2
     $php_version = explode('.', phpversion());
@@ -3656,12 +3672,13 @@ class H5PContentValidator {
 
     // Check if string is within allowed length
     if (isset($semantics->maxLength)) {
+      // Moodle: the extension mbstring is optional.
+      /*
       if (!extension_loaded('mbstring')) {
         $this->h5pF->setErrorMessage($this->h5pF->t('The mbstring PHP extension is not loaded. H5P need this to function properly'), 'mbstring-unsupported');
       }
-      else {
-        $text = mb_substr($text, 0, $semantics->maxLength);
-      }
+      */
+      $text = \core_text::substr($text, 0, $semantics->maxLength);
     }
 
     // Check if string is according to optional regexp in semantics
@@ -3711,11 +3728,14 @@ class H5PContentValidator {
         // file name, 2. testing against a returned error array that could
         // never be more than 1 element long anyway, 3. recreating the regex
         // for every file.
+        // Moodle: the extension mbstring is optional.
+        /*
         if (!extension_loaded('mbstring')) {
           $this->h5pF->setErrorMessage($this->h5pF->t('The mbstring PHP extension is not loaded. H5P need this to function properly'), 'mbstring-unsupported');
           $valid = FALSE;
         }
-        else if (!preg_match($wl_regex, mb_strtolower($file))) {
+        */
+        if (!preg_match($wl_regex, \core_text::strtolower($file))) {
           $this->h5pF->setErrorMessage($this->h5pF->t('File "%filename" not allowed. Only files with the following extensions are allowed: %files-allowed.', array('%filename' => $file, '%files-allowed' => $whitelist)), 'not-in-whitelist');
           $valid = FALSE;
         }
