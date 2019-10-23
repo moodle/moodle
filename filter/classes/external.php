f6f931728a32125297f9575faa3e87cc53a4a8c4
Juan Leyva <juanleyvadelgado@gmail.com>
2019-10-23 16:58:34 +0200

    MDL-66876 core_filter: Fix context in Web Service warning

--- filter/classes/external.php
+++ filter/classes/external.php
@@ -85,7 +85,7 @@ class external extends external_api {
             } catch (Exception $e) {
                 $warnings[] = array(
                     'item' => 'context',
-                    'itemid' => $context['instanceid'],
+                    'itemid' => $contextinfo['instanceid'],
                     'warningcode' => $e->getCode(),
                     'message' => $e->getMessage(),
                 );
