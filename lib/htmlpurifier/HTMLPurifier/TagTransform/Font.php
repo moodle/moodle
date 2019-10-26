9fbe33a6a29ea00348f74630100b93fabab601e7
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2019-10-26 11:34:03 +0200

    MDL-67030 htmlpurifier: php74 fixup for htmlpurifier v4.10.0
    This simply applies this pull request:
    
    https://github.com/ezyang/htmlpurifier/pull/224
    
    straight to our library. Once there is some new htmlpurifier
    release available (>v4.11.0) we'll be back upstream because
    the fix will be included.

--- lib/htmlpurifier/HTMLPurifier/TagTransform/Font.php
+++ lib/htmlpurifier/HTMLPurifier/TagTransform/Font.php
@@ -75,7 +75,7 @@ class HTMLPurifier_TagTransform_Font extends HTMLPurifier_TagTransform
         if (isset($attr['size'])) {
             // normalize large numbers
             if ($attr['size'] !== '') {
-                if ($attr['size']{0} == '+' || $attr['size']{0} == '-') {
+                if ($attr['size'][0] == '+' || $attr['size'][0] == '-') {
                     $size = (int)$attr['size'];
                     if ($size < -2) {
                         $attr['size'] = '-2';
