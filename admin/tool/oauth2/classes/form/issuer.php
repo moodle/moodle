034729a5cef1ecc17815ffb7e5dd1501af2694b0
Jun Pataleta <jun@moodle.com>
2019-10-30 17:00:36 +0800

    MDL-66598 tool_oauth2: Make account confirmation required by default
    * Only Facebook, Google, and Microsoft issuers can optionally offer to
    require account confirmation via email. We will require email
    confirmation for the rest of the issuers.

--- admin/tool/oauth2/classes/form/issuer.php
+++ admin/tool/oauth2/classes/form/issuer.php
@@ -46,6 +46,9 @@ class issuer extends persistent {
     /** @var string $type */
     protected $type;
 
+    /** @var boolean $showrequireconfirm Whether to show the require confirmation email checkbox or not. */
+    protected $showrequireconfirm;
+
     /**
      * Constructor.
      *
@@ -71,6 +74,7 @@ class issuer extends persistent {
         if (array_key_exists('type', $customdata)) {
             $this->type = $customdata['type'];
         }
+        $this->showrequireconfirm = !empty($customdata['showrequireconfirm']);
         parent::__construct($action, $customdata, $method, $target, $attributes, $editable, $ajaxformdata);
     }
 
@@ -157,9 +161,11 @@ class issuer extends persistent {
         $mform->addElement('checkbox', 'showonloginpage', get_string('issuershowonloginpage', 'tool_oauth2'));
         $mform->addHelpButton('showonloginpage', 'issuershowonloginpage', 'tool_oauth2');
 
-        // Require confirmation email for new accounts.
-        $mform->addElement('advcheckbox', 'requireconfirmation', get_string('issuerrequireconfirmation', 'tool_oauth2'));
-        $mform->addHelpButton('requireconfirmation', 'issuerrequireconfirmation', 'tool_oauth2');
+        if ($this->showrequireconfirm) {
+            // Require confirmation email for new accounts.
+            $mform->addElement('advcheckbox', 'requireconfirmation', get_string('issuerrequireconfirmation', 'tool_oauth2'));
+            $mform->addHelpButton('requireconfirmation', 'issuerrequireconfirmation', 'tool_oauth2');
+        }
 
         $mform->addElement('hidden', 'sortorder');
         $mform->setType('sortorder', PARAM_INT);
