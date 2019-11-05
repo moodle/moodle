0d97e6103f519d7d4ad8fd723eef6c02d2a45735
Amaia Anabitarte <amaia@moodle.com>
2019-11-05 16:18:43 +0100

    MDL-66405 core_h5p: New behat steps and selectors for H5P tools

--- filter/displayh5p/filter.php
+++ filter/displayh5p/filter.php
@@ -146,8 +146,8 @@ class filter_displayh5p extends moodle_text_filter {
             $sourceurl .= $urlmodifier;
         }
 
-        $h5piframesrc = $sourceurl.
-            '" class="h5p-iframe" style="height:230px; width: 100%; border: 0;" allowfullscreen="allowfullscreen">';
+        $h5piframesrc = $sourceurl . '" class="h5p-iframe" name="h5pcontent"' .
+            ' style="height:230px; width: 100%; border: 0;" allowfullscreen="allowfullscreen">';
 
         // We want to request the resizing script only once.
         if (self::$loadresizerjs) {
--- lib/behat/classes/partial_named_selector.php
+++ lib/behat/classes/partial_named_selector.php
@@ -78,6 +78,7 @@ class behat_partial_named_selector extends \Behat\Mink\Selector\PartialNamedSele
         'group_message_header' => 'group_message_header',
         'group_message' => 'group_message',
         'autocomplete' => 'autocomplete',
+        'iframe' => 'iframe',
     );
 
     /**
@@ -121,6 +122,7 @@ class behat_partial_named_selector extends \Behat\Mink\Selector\PartialNamedSele
         'autocomplete_selection' => 'autocomplete_selection',
         'autocomplete_suggestions' => 'autocomplete_suggestions',
         'autocomplete' => 'autocomplete',
+        'iframe' => 'iframe',
     );
 
     /**
@@ -229,6 +231,9 @@ XPATH
 XPATH
         , 'autocomplete' => <<<XPATH
 .//descendant::input[@id = //label[contains(normalize-space(string(.)), %locator%)]/@for]/ancestor::*[@data-fieldtype = 'autocomplete']
+XPATH
+        , 'iframe' => <<<XPATH
+.//iframe[contains(concat(' ', normalize-space(@class), ' '), %locator% )]
 XPATH
     );
 
