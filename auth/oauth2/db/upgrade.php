116d8541d2be3a4f9109d63ba671a690637f9eb2
Jun Pataleta <jun@moodle.com>
2019-11-05 16:12:26 +0800

    MDL-66598 auth_oauth2: OAuth2 upgrade step
    This basically does two things:
    1. Updates all non-Facebook/Google/Microsoft issuers and sets their
    'requireconfirmation' field to 1 so that future OAuth2 logins on these
    issuers will always have an email confirmation sent to them.
    2. Deletes the linked logins of users with non-Facebook/Google/Microsoft
    OAuth2 issuers.

--- auth/oauth2/db/upgrade.php
+++ auth/oauth2/db/upgrade.php
@@ -50,5 +50,39 @@ function xmldb_auth_oauth2_upgrade($oldversion) {
     // Automatically generated Moodle v3.6.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2018120301) {
+        // Fetch Facebook, Google, and Microsoft issuers. We use the URL field to determine the issuer type as it's the only
+        // field that contains the keyword that can somewhat let us reliably determine the issuer type.
+        $likefacebook = $DB->sql_like('oe.url', ':facebook');
+        $likegoogle = $DB->sql_like('oe.url', ':google');
+        $likemicrosoft = $DB->sql_like('oe.url', ':microsoft');
+
+        $params = [
+            'facebook' => '%facebook%',
+            'google' => '%google%',
+            'microsoft' => '%microsoft%',
+        ];
+
+        // We're querying from the oauth2_endpoint table because the base URLs of FB and Microsoft can be empty in the issuer table.
+        $subsql = "
+            SELECT DISTINCT oe.issuerid
+                       FROM {oauth2_endpoint} oe
+                      WHERE $likefacebook
+                            OR $likegoogle
+                            OR $likemicrosoft";
+
+        // Update non-Facebook/Google/Microsoft issuers and set requireconfirmation to 1.
+        $updatesql = "
+            UPDATE {oauth2_issuer}
+               SET requireconfirmation = 1
+             WHERE id NOT IN ({$subsql})";
+        $DB->execute($updatesql, $params);
+
+        // Delete linked logins for non-Facebook/Google/Microsoft issuers. They can easily re-link their logins anyway.
+        $DB->delete_records_select('auth_oauth2_linked_login', "issuerid NOT IN ($subsql)", $params);
+
+        upgrade_plugin_savepoint(true, 2018120301, 'auth', 'oauth2');
+    }
+
     return true;
 }
