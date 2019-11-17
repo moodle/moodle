91286bb1381319a3776a510844d95eaa825670f6
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2019-11-18 00:06:13 +0100

    MDL-66966 tcpdf: Silence non-existing K_PATH_CACHE
    I was fixing this adding a extra dir_exists() check that
    is better than the silence operator but that was that already
    there was a PR using @. So went to it.
    
    This is https://git.io/JeKft

--- lib/tcpdf/tcpdf.php
+++ lib/tcpdf/tcpdf.php
@@ -7788,7 +7788,7 @@ class TCPDF {
 		if ($destroyall AND !$preserve_objcopy) {
 			self::$cleaned_ids[$this->file_id] = true;
 			// remove all temporary files
-			if ($handle = opendir(K_PATH_CACHE)) {
+			if ($handle = @opendir(K_PATH_CACHE)) {
 				while ( false !== ( $file_name = readdir( $handle ) ) ) {
 					if (strpos($file_name, '__tcpdf_'.$this->file_id.'_') === 0) {
 						unlink(K_PATH_CACHE.$file_name);
