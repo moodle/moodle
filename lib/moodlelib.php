031f3a1d4b310fe871405b197f02f3cd45d0d32d
Shamim Rezaie <shamim@moodle.com>
2019-11-27 20:49:38 +1100

    MDL-67151 core: format_float can auto detect number of decimal points

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -8630,9 +8630,10 @@ function generate_password($maxlen=10) {
  * then it will display '5.4' instead of '5.400' or '5' instead of '5.000'.
  *
  * @param float $float The float to print
- * @param int $decimalpoints The number of decimal places to print.
+ * @param int $decimalpoints The number of decimal places to print. -1 is a special value for auto detect (full precision).
  * @param bool $localized use localized decimal separator
- * @param bool $stripzeros If true, removes final zeros after decimal point
+ * @param bool $stripzeros If true, removes final zeros after decimal point. It will be ignored and the trailing zeros after
+ *                         the decimal point are always striped if $decimalpoints is -1.
  * @return string locale float
  */
 function format_float($float, $decimalpoints=1, $localized=true, $stripzeros=false) {
@@ -8644,6 +8645,13 @@ function format_float($float, $decimalpoints=1, $localized=true, $stripzeros=fal
     } else {
         $separator = '.';
     }
+    if ($decimalpoints == -1) {
+        // The following counts the number of decimals.
+        // It is safe as both floatval() and round() functions have same behaviour when non-numeric values are provided.
+        $floatval = floatval($float);
+        for ($decimalpoints = 0; $floatval != round($float, $decimalpoints); $decimalpoints++);
+    }
+
     $result = number_format($float, $decimalpoints, $separator, '');
     if ($stripzeros) {
         // Remove zeros and final dot if not needed.
