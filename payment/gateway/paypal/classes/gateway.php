e1cab8e18e29604bbedc4074cf450203d037d912
Shamim Rezaie <shamim@moodle.com>
2019-12-16 23:14:51 +1100

    MDL-69166 pg_paypal: A new payment gateway is born

---
+++ payment/gateway/paypal/classes/gateway.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Contains class for PayPal payment gateway.
+ *
+ * @package    pg_paypal
+ * @copyright  2019 Shamim Rezaie <shamim@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace pg_paypal;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The gateway class for PayPal payment gateway.
+ *
+ * @copyright  2019 Shamim Rezaie <shamim@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class gateway extends \core_payment\gateway {
+    public static function get_supported_currencies(): array {
+        // See https://developer.paypal.com/docs/api/reference/currency-codes/,
+        // 3-character ISO-4217: https://en.wikipedia.org/wiki/ISO_4217#Active_codes.
+        return [
+            'AUD', 'BRL', 'CAD', 'CHF', 'CZK', 'DKK', 'EUR', 'GBP', 'HKD', 'HUF', 'ILS', 'INR', 'JPY',
+            'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'TWD', 'USD'
+        ];
+    }
+}
