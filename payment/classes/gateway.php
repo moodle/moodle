e52362fc5cfe14a0aed404d60c7008bb119d73b9
Shamim Rezaie <shamim@moodle.com>
2019-12-17 01:21:47 +1100

    MDL-69166 core_payment: basic gateway skeleton

---
+++ payment/classes/gateway.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Contains base class for payment gateways.
+ *
+ * @package    core_payment
+ * @copyright  2019 Shamim Rezaie <shamim@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core_payment;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Base class for payment gateways.
+ *
+ * @copyright  2019 Shamim Rezaie <shamim@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+abstract class gateway {
+    /**
+     * Returns the list of currencies that the payment gateway supports.
+     *
+     * @return string[] An array of the currency codes in the three-character ISO-4217 format
+     */
+    public abstract static function get_supported_currencies(): array;
+}
