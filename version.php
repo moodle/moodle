b2c0237ae8b7257f29708238de1440815a3e6d3d
Jake Dallimore <jake@moodle.com>
2020-01-03 11:25:02 +0800

    weekly release 3.7.3+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2019052003.07;              // 20190520      = branching date YYYYMMDD - do not modify!
+$version  = 2019052003.08;              // 20190520      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.7.3+ (Build: 20191220)'; // Human-friendly version name
+$release  = '3.7.3+ (Build: 20200103)'; // Human-friendly version name
 
 $branch   = '37';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
