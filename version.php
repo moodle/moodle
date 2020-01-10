86a19e4910cf035f7e0feedf514f2354ede267eb
Andrew Nicols <andrew@nicols.co.uk>
2020-01-10 13:03:39 +0800

    Moodle release 3.7.4

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2019052003.09;              // 20190520      = branching date YYYYMMDD - do not modify!
+$version  = 2019052004.00;              // 20190520      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.7.3+ (Build: 20200109)'; // Human-friendly version name
+$release  = '3.7.4 (Build: 20200113)'; // Human-friendly version name
 
 $branch   = '37';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
