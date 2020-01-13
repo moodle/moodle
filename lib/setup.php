f9a80888bcd2bd96a8345e0566302fdc202a49c8
Marina Glancy <marina@moodle.com>
2020-01-13 16:41:55 +0100

    MDL-67680 core: Catch PHP exceptions in the after_config callback

--- lib/setup.php
+++ lib/setup.php
@@ -1047,7 +1047,7 @@ foreach ($pluginswithfunction as $plugins) {
     foreach ($plugins as $function) {
         try {
             $function();
-        } catch (Exception $e) {
+        } catch (Throwable $e) {
             debugging("Exception calling '$function'", DEBUG_DEVELOPER, $e->getTrace());
         }
     }
