858f41a1131f5bf863af0fb5f877824b72862e5e
Simey Lameze <simey@moodle.com>
2020-01-16 09:38:16 +0800

    MDL-67703 calendar: change manage subscription form to hideIf

--- calendar/classes/local/event/forms/managesubscriptions.php
+++ calendar/classes/local/event/forms/managesubscriptions.php
@@ -78,9 +78,9 @@ class managesubscriptions extends \moodleform {
         $mform->addElement('filepicker', 'importfile', get_string('importfromfile', 'calendar'), null, array('accepted_types' => '.ics'));
 
         // Disable appropriate elements depending on import from value.
-        $mform->disabledIf('pollinterval', 'importfrom', 'eq', CALENDAR_IMPORT_FROM_FILE);
-        $mform->disabledIf('url',  'importfrom', 'eq', CALENDAR_IMPORT_FROM_FILE);
-        $mform->disabledIf('importfile', 'importfrom', 'eq', CALENDAR_IMPORT_FROM_URL);
+        $mform->hideIf('pollinterval', 'importfrom', 'eq', CALENDAR_IMPORT_FROM_FILE);
+        $mform->hideIf('url',  'importfrom', 'eq', CALENDAR_IMPORT_FROM_FILE);
+        $mform->hideIf('importfile', 'importfrom', 'eq', CALENDAR_IMPORT_FROM_URL);
 
         // Add the select elements for the available event types.
         $this->add_event_type_elements($mform, $eventtypes);
