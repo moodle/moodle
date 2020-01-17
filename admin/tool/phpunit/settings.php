f070dd126f7a2d63144186b73c2313badc7a2a4c
Brendan Heywood <brendan@catalyst-au.net>
2020-01-17 22:25:27 +1100

    MDL-67726 tool_phpunit: Removed webrunner tool

--- admin/tool/phpunit/settings.php
+++ admin/tool/phpunit/settings.php
@@ -27,6 +27,4 @@ defined('MOODLE_INTERNAL') || die;
 
 if ($hassiteconfig) {
     $ADMIN->add('development', new admin_externalpage('toolphpunit', get_string('pluginname', 'tool_phpunit'), "$CFG->wwwroot/$CFG->admin/tool/phpunit/index.php"));
-    $ADMIN->add('development', new admin_externalpage('toolphpunitwebrunner', get_string('pluginname', 'tool_phpunit'), "$CFG->wwwroot/$CFG->admin/tool/phpunit/webrunner.php",
-        'moodle/site:config', true));
 }
