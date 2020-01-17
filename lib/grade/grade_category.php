8d21ca65d4b592980b7214e934879571de66af81
Martin Gauk <gauk@math.tu-berlin.de>
2020-01-17 12:13:41 +0000

    MDL-51694 core_grades: use transactions when deleting

--- lib/grade/grade_category.php
+++ lib/grade/grade_category.php
@@ -285,6 +285,9 @@ class grade_category extends grade_object {
      * @return bool success
      */
     public function delete($source=null) {
+        global $DB;
+
+        $transaction = $DB->start_delegated_transaction();
         $grade_item = $this->load_grade_item();
 
         if ($this->is_course_category()) {
@@ -334,7 +337,10 @@ class grade_category extends grade_object {
         $grade_item->delete($source);
 
         // delete category itself
-        return parent::delete($source);
+        $success = parent::delete($source);
+
+        $transaction->allow_commit();
+        return $success;
     }
 
     /**
