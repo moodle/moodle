b6be1e982218bbb244eabe3921aec52dcf4c56b4
David Mudrák <david@moodle.com>
2020-01-22 23:00:58 +0100

    MDL-67748 user: Add a new core_user_search_identity external function
    The purpose of this external function is to provide data for
    asynchronous user selectors and similar widgets. It allows to search
    users matching the given query in their name or other available identity
    fields.

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2021031200.00;              // 20201109      = branching date YYYYMMDD - do not modify!
+$version  = 2021031200.01;              // 20201109      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '3.11dev (Build: 20210312)';// Human-friendly version name
