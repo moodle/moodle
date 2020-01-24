a470b60534d46b67428925fc2e94b0e0c9564038
Adrian Greeve <abgreeve@gmail.com>
2020-01-24 10:17:14 +0800

    weekly release 3.6.8+

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2018120308.00;              // 20181203      = branching date YYYYMMDD - do not modify!
+$version  = 2018120308.01;              // 20181203      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.6.8 (Build: 20200113)'; // Human-friendly version name
+$release  = '3.6.8+ (Build: 20200124)'; // Human-friendly version name
 
 $branch   = '36';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
