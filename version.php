4e41ace0bebfd90908b4d3f0cb9ac9800fb1e696
Adrian Greeve <abgreeve@gmail.com>
2020-01-24 10:16:48 +0800

    weekly release 3.9dev

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020011700.02;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2020012400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.9dev (Build: 20200117)'; // Human-friendly version name
+$release  = '3.9dev (Build: 20200124)'; // Human-friendly version name
 $branch   = '39';                       // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
