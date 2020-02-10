54cb5666c183906cb6d769b562407ed7ebbfc505
Paul Holden <paulh@moodle.com>
2020-02-10 08:19:49 +0000

    MDL-64417 message_popup: remove old notifications during cleanup task.
    Add upgrade step to remove existing orphaned records.

--- message/output/popup/version.php
+++ message/output/popup/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020012300;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2020020600;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019111200;        // Requires this Moodle version
 $plugin->component = 'message_popup';  // Full name of the plugin (used for diagnostics)
