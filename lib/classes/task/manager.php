e63040d02b858cc151e475cae1ffd214be85c812
Mikhail Golenkov <mikhailgolenkov@catalyst-au.net>
2020-02-17 14:54:08 +1100

    MDL-67980 tasks: Sort ad-hoc tasks by nextruntime.

--- lib/classes/task/manager.php
+++ lib/classes/task/manager.php
@@ -561,7 +561,7 @@ class manager {
 
         $where = '(nextruntime IS NULL OR nextruntime < :timestart1)';
         $params = array('timestart1' => $timestart);
-        $records = $DB->get_records_select('task_adhoc', $where, $params);
+        $records = $DB->get_records_select('task_adhoc', $where, $params, 'nextruntime ASC, id ASC');
 
         $records = self::ensure_adhoc_task_qos($records);
 
