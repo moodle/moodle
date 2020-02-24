8ad4255fbe725ea72239a227e563522d975c2fa3
Bas Brands <bas@moodle.com>
2020-02-24 14:39:31 +0100

    MDL-67863 theme_boost: change keyboard tab order
    Move navdrawer up in the dom for a more logical tab order
    Hide navdrawer links for tab / screenreader when navdrawer is closed

--- theme/boost/amd/src/drawer.js
+++ theme/boost/amd/src/drawer.js
@@ -128,8 +128,9 @@ define(['jquery', 'core/custom_interaction_events', 'core/log', 'core/pubsub'],
             // Close.
             body.removeClass('drawer-open-' + side);
             trigger.attr('aria-expanded', 'false');
-            drawer.attr('aria-hidden', 'true');
-            drawer.addClass('closed');
+            drawer.addClass('closed').delay(500).queue(function() {
+                $(this).attr('aria-hidden', 'true').dequeue();
+            });
             if (!small) {
                 M.util.set_user_preference(preference, 'false');
             }
