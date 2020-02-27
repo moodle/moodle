b34f2c75c9c7e2368afa65359337af0876a68192
Michael Hawkins <michaelh@moodle.com>
2020-02-27 13:16:00 +0800

    MDL-68061 gradereport_history: Restrict per page config value to int

--- grade/report/history/settings.php
+++ grade/report/history/settings.php
@@ -30,7 +30,8 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configtext('grade_report_historyperpage',
         new lang_string('historyperpage', 'gradereport_history'),
         new lang_string('historyperpage_help', 'gradereport_history'),
-        50
+        50,
+        PARAM_INT
     ));
 
 }
