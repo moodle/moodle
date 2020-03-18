fb56f5190ca2724d626b94677324506b742042bb
Sara Arjona <sara@moodle.com>
2020-03-18 14:25:59 +0100

    MDL-68116 atto_h5p: deprecate unused strings

--- lib/editor/atto/plugins/h5p/lang/en/atto_h5p.php
+++ lib/editor/atto/plugins/h5p/lang/en/atto_h5p.php
@@ -25,18 +25,20 @@
 $string['browserepositories'] = 'Browse repositories...';
 $string['copyrightbutton'] = 'Copyright button';
 $string['downloadbutton'] = 'Allow download';
-$string['either'] = 'Either';
 $string['embedbutton'] = 'Embed button';
-$string['enterurl'] = 'URL or embed code';
 $string['h5p:addembed'] = 'Add embedded H5P';
 $string['h5pfile'] = 'H5P file upload';
 $string['h5pfileorurl'] = 'H5P URL or file upload';
 $string['h5poptions'] = 'H5P options';
-$string['h5pproperties'] = 'H5P properties';
 $string['h5purl'] = 'H5P URL';
 $string['invalidh5purl'] = 'Invalid URL';
 $string['instructions'] = 'You can insert H5P content by <strong>either</strong> entering a URL <strong>or</strong> by uploading an H5P file.';
 $string['noh5pcontent'] = 'No H5P content added';
 $string['pluginname'] = 'Insert H5P';
 $string['privacy:metadata'] = 'The atto_h5p plugin does not store any personal data.';
+
+// Deprecated since Moodle 3.9.
+$string['either'] = 'Either';
+$string['enterurl'] = 'URL or embed code';
+$string['h5pproperties'] = 'H5P properties';
 $string['or'] = 'or';
