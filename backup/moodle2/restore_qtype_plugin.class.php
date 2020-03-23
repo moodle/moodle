d83bf6e0ff5d7e141600335887f629964c4b8b71
Ilya Tregubov <ilyatregubov@catalyst-au.net>
2020-03-24 09:36:57 +1100

    MDL-68203 restore: Decode quiz links when restore into existing course
                       or duplicating.

--- backup/moodle2/restore_qtype_plugin.class.php
+++ backup/moodle2/restore_qtype_plugin.class.php
@@ -175,6 +175,20 @@ abstract class restore_qtype_plugin extends restore_plugin {
                 }
             }
 
+            $rules = restore_course_task::define_decode_rules();
+            $rulesactivity = restore_quiz_activity_task::define_decode_rules();
+            $rules = array_merge($rules, $rulesactivity);
+
+            $decoder = $this->task->get_decoder();
+            foreach ($rules as $rule) {
+                $decoder->add_rule($rule);
+            }
+
+            $contentdecoded = $decoder->decode_content($data->answertext);
+            if ($contentdecoded) {
+                $data->answertext = $contentdecoded;
+            }
+
             if (!isset($this->questionanswercache[$data->answertext])) {
                 // If we haven't found the matching answer, something has gone really wrong, the question in the DB
                 // is missing answers, throw an exception.
