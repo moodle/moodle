2a9e5ba70a0ac9b0e98e359f82692cfd14dc3f90
Simey Lameze <simey@moodle.com>
2020-05-05 09:31:18 +0800

    MDL-68563 filter_urltolink: enable filter uppon install
    Every major system that people use these days turns URLs into links automatically.
    
    This filter was turned off by default a decade ago because of performance concerns
    that do not apply any more, specially on fresh new installations.
    
    In order to improve user experience on text editing features, the urltolink filter
    will be now enabled by default in new installations.

--- filter/urltolink/version.php
+++ filter/urltolink/version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019111800;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2020051200;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019111200;        // Requires this Moodle version
 $plugin->component = 'filter_urltolink'; // Full name of the plugin (used for diagnostics)
