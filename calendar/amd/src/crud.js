88f1a81bd6965d4f9d77b7834f768b66f0ec16e2
Andrew Nicols <andrew@nicols.co.uk>
2020-05-18 08:06:58 +0800

    MDL-68728 calendar: Pending promises for event summary

--- calendar/amd/src/crud.js
+++ calendar/amd/src/crud.js
@@ -239,7 +239,9 @@ function(
      * @returns {Promise}
      */
     function registerEditListeners(root, eventFormModalPromise) {
-        eventFormModalPromise
+        var pendingPromise = new Pending('core_calendar/crud:registerEditListeners');
+
+        return eventFormModalPromise
         .then(function(modal) {
             // When something within the calendar tells us the user wants
             // to edit an event then show the event form modal.
@@ -251,11 +253,14 @@ function(
 
                 e.stopImmediatePropagation();
             });
-            return;
+            return modal;
         })
-        .fail(Notification.exception);
+        .then(function(modal) {
+            pendingPromise.resolve();
 
-        return eventFormModalPromise;
+            return modal;
+        })
+        .catch(Notification.exception);
     }
 
     return {
