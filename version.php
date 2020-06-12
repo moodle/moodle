8dbc6ebbe3ab02f19268cb5fabdfacc0976133d1
Victor Deniz Falcon <victor@moodle.com>
2020-06-12 12:32:06 +0100

    Moodle release 3.9rc3

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020061200.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2020061200.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.9rc2 (Build: 20200612)'; // Human-friendly version name
+$release  = '3.9rc3 (Build: 20200612)'; // Human-friendly version name
 $branch   = '39';                       // This version's branch.
 $maturity = MATURITY_RC;             // This version's maturity level.
