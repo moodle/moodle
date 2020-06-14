a30eb7d16ccfddafc21f4e702cd26afa5d2fb378
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2020-06-14 13:08:09 +0200

    MDL-69044 upgrade: add 3.9.0 separation line to all upgrade scripts

--- admin/tool/dataprivacy/db/upgrade.php
+++ admin/tool/dataprivacy/db/upgrade.php
@@ -337,5 +337,8 @@ function xmldb_tool_dataprivacy_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2019121700, 'tool', 'dataprivacy');
     }
 
+    // Automatically generated Moodle v3.9.0 release upgrade line.
+    // Put any upgrade step following this.
+
     return true;
 }
