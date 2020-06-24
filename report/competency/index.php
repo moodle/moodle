a66d07b15ce676be97b97d7544d1a49544a91ab9
Bas Brands <bas@moodle.com>
2020-06-24 09:06:21 +0200

    MDL-69122 report_competency: fix clearfix
    - a misplaced clearfix caused part of the recontrols to be
    inaccessible

--- report/competency/index.php
+++ report/competency/index.php
@@ -76,7 +76,7 @@ $output = $PAGE->get_renderer('report_competency');
 echo $output->header();
 $baseurl = new moodle_url('/report/competency/index.php');
 $nav = new \report_competency\output\user_course_navigation($currentuser, $course->id, $baseurl, $currentmodule);
-echo $output->render($nav);
+$top = $output->render($nav);
 if ($currentuser > 0) {
     $user = core_user::get_user($currentuser);
     $usercontext = context_user::instance($currentuser);
@@ -88,9 +88,9 @@ if ($currentuser > 0) {
     if ($currentmodule > 0) {
         $title = get_string('filtermodule', 'report_competency', format_string($cm->name));
     }
-    echo $output->context_header($userheading, 3);
+    $top .= $output->context_header($userheading, 3);
 }
-echo $output->container('', 'clearfix');
+echo $output->container($top, 'clearfix');
 echo $output->heading($title, 3);
 
 if ($currentuser > 0) {
