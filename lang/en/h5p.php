6bfddeea21a4ae2af20319ab6c20caf0a4d4ed15
cescobedo <carlos.escobedo@gmail.com>
2020-06-25 15:09:09 +0200

    MDL-67172 core_h5p: Prevent synchronous access to save the same h5p
    Backport MDL-67095.
    When deploying a new H5P resource we need to ensure that
    we don't start deploying the same H5P in another thread
    (e.g. for another session/user).

--- lang/en/h5p.php
+++ lang/en/h5p.php
@@ -123,6 +123,7 @@ $string['licenseV3'] = 'Version 3';
 $string['licensee'] = 'Licensee';
 $string['licenseextras'] = 'Licence extras';
 $string['licenseversion'] = 'Licence version';
+$string['lockh5pdeploy'] = 'This H5P content cannot be accessed because it is being deployed. Please try again later.';
 $string['missingcontentfolder'] = 'A valid content folder is missing';
 $string['missingcoreversion'] = 'The system was unable to install the {$a->%component} component from the package, as it requires a newer version of the H5P plugin. This site is currently running version {$a->%current}, whereas the required version is {$a->%required} or higher. Please upgrade and then try again.';
 $string['missingdependency'] = 'Missing dependency {$a->@dep} required by {$a->@lib}.';
