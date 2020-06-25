d61fa24239d542377cbb253a087c05613e128d61
Andrew Nicols <andrew@nicols.co.uk>
2020-06-25 14:38:26 +0800

    MDL-69138 behat: Add "I visit :localurl" step

--- lib/tests/behat/behat_general.php
+++ lib/tests/behat/behat_general.php
@@ -1937,4 +1937,16 @@ EOF;
         $value = [\WebDriver\Key::ENTER];
         $this->getSession()->getDriver()->getWebDriverSession()->activeElement()->postValue(['value' => $value]);
     }
+
+    /**
+     * Visit a local URL relative to the behat root.
+     *
+     * @When I visit :localurl
+     *
+     * @param string|moodle_url $localurl The URL relative to the behat_wwwroot to visit.
+     */
+    public function i_visit($localurl): void {
+        $localurl = new moodle_url($localurl);
+        $this->getSession()->visit($this->locate_path($localurl->out_as_local_url(false)));
+    }
 }
