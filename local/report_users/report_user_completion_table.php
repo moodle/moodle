cca173e6e2d95a95f7c2ec30680ba92fe9f1eb28
Derick Turner <derick@e-learndesign.scot>
2020-07-02 19:58:12 +0100

    IOMAD: lost the clear buttons for finished users. #1396

--- local/report_users/report_user_completion_table.php
+++ local/report_users/report_user_completion_table.php
@@ -182,7 +182,11 @@ class local_report_user_completion_table extends table_sql {
                         $delaction .= '<a class="btn btn-danger" href="'.$clearlink.'">' . get_string('clear', 'local_report_users') . '</a>';
                     }
                 } else {
-                    if ($DB->get_record('companylicense_users', array('userid' => $row->userid, 'licensecourseid' => $row->courseid, 'licenseid' => $row->licenseid, 'issuedate' => $row->licenseallocated, 'isusing' => 1))) {
+                    if (!empty($row->timecompleted)) {
+                        if (has_capability('local/report_users:clearentries', context_system::instance())) {
+                            $delaction .= '<a class="btn btn-danger" href="'.$clearlink.'">' . get_string('clear', 'local_report_users') . '</a>';
+                        }
+                    } else if ($DB->get_record('companylicense_users', array('userid' => $row->userid, 'licensecourseid' => $row->courseid, 'licenseid' => $row->licenseid, 'issuedate' => $row->licenseallocated, 'isusing' => 1))) {
                         if (has_capability('local/report_users:deleteentries', context_system::instance())) {
                             $delaction .= '<a class="btn btn-danger" href="'.$dellink.'">' . get_string('delete') . '</a>';
                         }
