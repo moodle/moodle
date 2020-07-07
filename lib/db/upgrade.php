e63b6b8665972a2342fb5f77361c1944873adfa1
Peter Burnett <peterburnett@catalyst-au.net>
2020-07-07 15:17:08 +1000

    MDL-69170 task: Added timecreated field to adhoc task table

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2539,5 +2539,20 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2020072300.01);
     }
 
+    if ($oldversion < 2020073000.01) {
+
+        // Define field timecreated to be added to task_adhoc.
+        $table = new xmldb_table('task_adhoc');
+        $field = new xmldb_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'blocking');
+
+        // Conditionally launch add field timecreated.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2020073000.01);
+    }
+
     return true;
 }
