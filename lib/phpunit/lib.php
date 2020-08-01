bdc3ad00c989b93c75b99d90bb68a082bb9081e8
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2020-08-01 18:15:28 +0200

    MDL-67673 phpunit: Bump to PHPUnit 8.5 and remove DBunit
    This makes everything to break, but it's what we'll be fixing
    in the next commits, heh. :-)
    
    Generated with php72 (min version supported), following the
    instructions @ https://docs.moodle.org/dev/Composer

--- lib/phpunit/lib.php
+++ lib/phpunit/lib.php
@@ -33,7 +33,6 @@ require_once(__DIR__.'/classes/phpmailer_sink.php');
 require_once(__DIR__.'/classes/base_testcase.php');
 require_once(__DIR__.'/classes/basic_testcase.php');
 require_once(__DIR__.'/classes/database_driver_testcase.php');
-require_once(__DIR__.'/classes/arraydataset.php');
 require_once(__DIR__.'/classes/advanced_testcase.php');
 require_once(__DIR__.'/classes/hint_resultprinter.php'); // Loaded here because phpunit.xml does not support relative links for printerFile.
 require_once(__DIR__.'/classes/constraint_object_is_equal_with_exceptions.php');
