8c274e82f426f9f637b8d899a89da0eea150b0d6
Andrew Nicols <andrew@nicols.co.uk>
2020-08-12 08:02:42 +0800

    MDL-68974 calendar: Use page context for block check

--- calendar/lib.php
+++ calendar/lib.php
@@ -1274,9 +1274,12 @@ class calendar_information {
      * @param string|null $view preference view options (eg: day, month, upcoming)
      */
     public function add_sidecalendar_blocks(core_calendar_renderer $renderer, $showfilters=false, $view=null) {
-        if (!has_capability('moodle/block:view', $this->context) ) {
+        global $PAGE;
+
+        if (!has_capability('moodle/block:view', $PAGE->context) ) {
             return;
         }
+
         if ($showfilters) {
             $filters = new block_contents();
             $filters->content = $renderer->event_filter();
