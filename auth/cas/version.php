db361e09e7932c5604d61e8cb38644f02b8d0ae1
Paul Holden <paulh@moodle.com>
2020-08-17 20:52:51 +0100

    MDL-69492 auth_cas: normalise member distinguished name config.

--- auth/cas/version.php
+++ auth/cas/version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020061500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2020081700;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020060900;        // Requires this Moodle version
 $plugin->component = 'auth_cas';        // Full name of the plugin (used for diagnostics)
 
