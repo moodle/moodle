fb9670a9b273c2aed13a5b2c5c2fbe4403aa7dc4
Simey Lameze <simey@moodle.com>
2020-08-20 09:17:42 +0800

    MDL-68683 competencies: fix query to delete stale files

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -3930,15 +3930,14 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2019111804.01);
     }
 
-    if ($oldversion < 2019111804.07) {
+    if ($oldversion < 2019111804.08) {
         // Delete all user evidence files from users that have been deleted.
         $sql = "SELECT DISTINCT f.*
                   FROM {files} f
              LEFT JOIN {context} c ON f.contextid = c.id
-             LEFT JOIN {user} u ON c.instanceid = u.id
                  WHERE f.component = :component
                    AND f.filearea = :filearea
-                   AND u.deleted = 1";
+                   AND c.id IS NULL";
         $stalefiles = $DB->get_records_sql($sql, ['component' => 'core_competency', 'filearea' => 'userevidence']);
 
         $fs = get_file_storage();
@@ -3946,7 +3945,7 @@ function xmldb_main_upgrade($oldversion) {
             $fs->get_file_instance($stalefile)->delete();
         }
 
-        upgrade_main_savepoint(true, 2019111804.07);
+        upgrade_main_savepoint(true, 2019111804.08);
     }
 
     return true;
