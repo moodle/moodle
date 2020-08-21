e290345cd65c5eccde4dd70b9373d48d33d54954
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2020-08-21 20:43:20 +0200

    weekly release 3.9.1+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020061501.09;              // 20200615      = branching date YYYYMMDD - do not modify!
+$version  = 2020061501.10;              // 20200615      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.9.1+ (Build: 20200814)'; // Human-friendly version name
+$release  = '3.9.1+ (Build: 20200822)'; // Human-friendly version name
 $branch   = '39';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
