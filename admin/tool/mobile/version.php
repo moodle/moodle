94d05a5bfb7400d7e2c8f6ebb6a8c243899af4de
Andrew Nicols <andrew@nicols.co.uk>
2020-09-09 12:43:55 +0800

    MDL-68797 tool_mobile: Version bump

--- admin/tool/mobile/version.php
+++ admin/tool/mobile/version.php
@@ -23,7 +23,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2020061500; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020061501; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020060900; // Requires this Moodle version.
 $plugin->component = 'tool_mobile'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = array(
