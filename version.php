fe1031244b3e53ac4b42d10becb933bb9579b7e8
Jake Dallimore <jake@moodle.com>
2020-09-16 10:50:48 +0800

    MDL-59510 core_oauth2: add oauth2_refresh_token table

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020100200.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2020100200.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '3.10dev+ (Build: 20201002)';// Human-friendly version name
