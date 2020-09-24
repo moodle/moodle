a348c5f420db82e12b6e367ef19ccbe1fad665c3
Víctor Déniz <victor@moodle.com>
2020-09-24 16:27:32 +0100

    weekly release 3.9.2+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020061502.01;              // 20200615      = branching date YYYYMMDD - do not modify!
+$version  = 2020061502.02;              // 20200615      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.9.2+ (Build: 20200918)'; // Human-friendly version name
+$release  = '3.9.2+ (Build: 20200924)'; // Human-friendly version name
 $branch   = '39';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
