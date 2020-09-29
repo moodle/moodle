1efbb9d0ba39cd7aae9c5d519ff56b2b1e968c49
Derick Turner <derick@e-learndesign.co.uk>
2020-09-29 14:37:47 +0100

    IOMAD: Add create user button when no users found in editusers page

--- blocks/iomad_company_admin/editusers.php
+++ blocks/iomad_company_admin/editusers.php
@@ -641,6 +641,8 @@ flush();
 if (!$users) {
     $match = array();
     echo $output->heading(get_string('nousersfound'));
+    echo $output->single_button(new moodle_url($CFG->wwwroot . '/blocks/iomad_company_admin/company_user_create_form.php'),
+                                get_string('createuser', 'block_iomad_company_admin'));
 
     $table = null;
 
