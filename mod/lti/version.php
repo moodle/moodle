6d79bed31b54a6b049678b07f8111de7958ec4b3
Claude Vervoort <claude.vervoort@cengage.com>
2020-10-01 15:12:15 -0400

    MDL-57756 mod_lti: params change to text type

--- mod/lti/version.php
+++ mod/lti/version.php
@@ -48,7 +48,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2019111800;    // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2019111801;    // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019111200;    // Requires this Moodle version.
 $plugin->component = 'mod_lti';     // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
