cc1c4de8dc626354badfa8c35a977042b5c619ee
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2020-10-07 20:41:44 +0200

    MDL-67673 phpunit: Remove the rerun hint on failed test cases
    That custom printer was using some hacks to be able to intercept
    configuration switches, reflection and other tricks to be able to
    print the:
    
    To re-run:
     vendor/bin/phpunit --verbose "core_setuplib_testcase" lib/tests/setuplib_test.php
    
    line on every failed/exceptional/skipped test. After some internal
    discussion it was agreed that the normal phpunit output:
    
    1) core_setuplib_testcase::test_localcachedir
    Time is lower that allowed start value
    Failed asserting that 1601976686 is equal to 1601976687 or is greater than 1601976687.
    
    /var/www/html/lib/phpunit/classes/advanced_testcase.php:446
    /var/www/html/lib/tests/setuplib_test.php:170
    /var/www/html/lib/phpunit/classes/advanced_testcase.php:80
    
    has already all the information at hand about how to rerun a test:
    
    - vendor/bin/phpunit lib/tests/setuplib_test.php
    - vendor/bin/phpunit --filter core_setuplib_testcase::test_localcachedir
    - vendor/bin/phpunit --filter ::test_localcachedir
    - vendor/bin/phpunit --testsuite componentname_testsuite
    - vendor/bin/phpunit --config <<compoenent directory>>
    - use --cache-result to get failed tests rerun with ease.
    - ...
    
    So better, let's use standard phpunit output and done. Also, note that,
    with the upgrade to phpunit 8.5, the printer was not working correctly
    any more, causing some switches, like --verbose ... to be ignored. Sure
    it could have been fixed but, as commented above, no real need for all
    that "parapheranlia" to print the rerun information.

--- lib/phpunit/lib.php
+++ lib/phpunit/lib.php
@@ -35,7 +35,6 @@ require_once(__DIR__.'/classes/base_testcase.php');
 require_once(__DIR__.'/classes/basic_testcase.php');
 require_once(__DIR__.'/classes/database_driver_testcase.php');
 require_once(__DIR__.'/classes/advanced_testcase.php');
-require_once(__DIR__.'/classes/hint_resultprinter.php'); // Loaded here because phpunit.xml does not support relative links for printerFile.
 require_once(__DIR__.'/classes/constraint_object_is_equal_with_exceptions.php');
 require_once(__DIR__.'/../testing/classes/test_lock.php');
 require_once(__DIR__.'/../testing/classes/tests_finder.php');
