8e3031aed0d424abdacee73a677d2e6ae9a2ebf7
Peter Dias <peter@moodle.com>
2020-10-07 10:55:25 +0800

    MDL-50955 core: Reset lessons completion criteria if grade type is none

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2021052500.41;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2021052500.42;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '4.0dev (Build: 20201107)'; // Human-friendly version name
