8d70a113073442eec3503f9b4fd3a4e28ed5d66c
Brendan Heywood <brendan@catalyst-au.net>
2020-10-10 19:24:38 +1100

    MDL-69910 calendar: Fix export exception when both params are sent

--- calendar/export_execute.php
+++ calendar/export_execute.php
@@ -18,7 +18,7 @@ if (empty($CFG->enablecalendarexport)) {
 $checkuserid = !empty($userid) && $user = $DB->get_record('user', array('id' => $userid), 'id,password');
 //allowing for fallback check of old url - MDL-27542
 $checkusername = !empty($username) && $user = $DB->get_record('user', array('username' => $username), 'id,password');
-if (!$checkuserid && !$checkusername) {
+if ((!$checkuserid && !$checkusername) || !$user) {
     //No such user
     die('Invalid authentication');
 }
