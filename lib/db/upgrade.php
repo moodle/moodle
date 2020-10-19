4dcca1b084a7f568ee7ae11f9e56b6dc951c6ed9
sam marshall <s.marshall@open.ac.uk>
2020-10-19 10:14:09 +0100

    MDL-69687 Course: remove_course_contents orphaned completion data
    The remove_course_contents function did not delete data from
    course_module_completion table in most cases, resulting in large
    quantities of orphaned data.

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2782,5 +2782,16 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2021052500.24);
     }
 
+    if ($oldversion < 2021052500.26) {
+        // Delete orphaned course_modules_completion rows; these were not deleted properly
+        // by remove_course_contents function.
+        $DB->delete_records_subquery('course_modules_completion', 'id', 'id',
+               "SELECT cmc.id
+                  FROM {course_modules_completion} cmc
+             LEFT JOIN {course_modules} cm ON cm.id = cmc.coursemoduleid
+                 WHERE cm.id IS NULL");
+        upgrade_main_savepoint(true, 2021052500.26);
+    }
+
     return true;
 }
