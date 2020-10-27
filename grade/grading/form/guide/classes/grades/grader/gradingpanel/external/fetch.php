38aec5552100f07ad177615145634c8d926b2fcb
Juan Segarra Montesinos <juan.segarra@uji.es>
2020-10-27 19:06:49 +0100

    MDL-69797 gradingform_guide: Implement authorization into fetch method

--- grade/grading/form/guide/classes/grades/grader/gradingpanel/external/fetch.php
+++ grade/grading/form/guide/classes/grades/grader/gradingpanel/external/fetch.php
@@ -133,7 +133,12 @@ class fetch extends external_api {
         }
 
         // Fetch the actual data.
-        $gradeduser = core_user::get_user($gradeduserid);
+        $gradeduser = core_user::get_user($gradeduserid, '*', MUST_EXIST);
+
+        // One can access its own grades. Others just if they're graders.
+        if ($gradeduserid != $USER->id) {
+            $gradeitem->require_user_can_grade($gradeduser, $USER);
+        }
 
         return self::get_fetch_data($gradeitem, $gradeduser);
     }
