51c9ea51946615dd4d5ec3e8de9876bb45231712
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2020-11-03 19:38:27 +0100

    MDL-70089 versions: main version bump

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020110300.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2020110400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '3.10beta+ (Build: 20201103)';// Human-friendly version name
