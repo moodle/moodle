e4cda88e7651478b0972d670ec106cd4d37303e8
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2020-11-06 22:56:08 +0100

    Moodle release 3.10rc2

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020110500.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2020110700.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.10rc1 (Build: 20201105)';// Human-friendly version name
+$release  = '3.10rc2 (Build: 20201107)';// Human-friendly version name
 $branch   = '310';                      // This version's branch.
 $maturity = MATURITY_RC;             // This version's maturity level.
