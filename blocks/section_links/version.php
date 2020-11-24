53663f2ea126b18709711d2126ea501f3b9c6956
Mikhail Golenkov <mikhailgolenkov@catalyst-au.net>
2020-11-24 11:33:27 +1100

    MDL-69773 block_section_links: Add an option to display section name

--- blocks/section_links/version.php
+++ blocks/section_links/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021052500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2021052501;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021052500;        // Requires this Moodle version
 $plugin->component = 'block_section_links'; // Full name of the plugin (used for diagnostics)
