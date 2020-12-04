8b8c96ff3c7e4134a899f6e156a65671cab53797
Iñigo Zendegi <izendegi@mondragon.edu>
2020-12-04 12:55:38 +0100

    MDL-69097 mod_forum: Apply filters to whole forum grading

--- mod/forum/view.php
+++ mod/forum/view.php
@@ -179,9 +179,9 @@ switch ($forum->get_type()) {
                 $gradeobj = (object) [
                     'contextid' => $forum->get_context()->id,
                     'cmid' => $cmid,
-                    'name' => $forum->get_name(),
+                    'name' => format_string($forum->get_name()),
                     'courseid' => $course->id,
-                    'coursename' => $course->shortname,
+                    'coursename' => format_string($course->shortname),
                     'experimentaldisplaymode' => $displaymode == FORUM_MODE_NESTED_V2,
                     'groupid' => $groupid,
                     'gradingcomponent' => $forumgradeitem->get_grading_component_name(),
@@ -196,9 +196,9 @@ switch ($forum->get_type()) {
                 $gradeobj = (object) [
                     'contextid' => $forum->get_context()->id,
                     'cmid' => $cmid,
-                    'name' => $forum->get_name(),
+                    'name' => format_string($forum->get_name()),
                     'courseid' => $course->id,
-                    'coursename' => $course->shortname,
+                    'coursename' => format_string($course->shortname),
                     'groupid' => $groupid,
                     'userid' => $USER->id,
                     'gradingcomponent' => $forumgradeitem->get_grading_component_name(),
