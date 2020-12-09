93025be2e7cbca8a098f3926b57b60d3be3cf1b8
Paul Holden <paulh@moodle.com>
2020-12-09 14:23:10 +0000

    MDL-70794 reportbuilder: management and further utility classes.
    We define the base classes and APIs for reports, that can contain
    columns and filters instances themselves.

---
+++ reportbuilder/classes/local/helpers/format.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+declare(strict_types=1);
+
+namespace core_reportbuilder\local\helpers;
+
+use stdClass;
+
+/**
+ * Class containing helper methods for format columns data as callbacks.
+ *
+ * @package     core_reportbuilder
+ * @copyright   2021 Sara Arjona <sara@moodle.com> based on Alberto Lara Hernández <albertolara@moodle.com> code.
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class format {
+
+    /**
+     * Returns formatted date.
+     *
+     * @param int $value Unix timestamp
+     * @param stdClass $row
+     * @param string|null $format Format string for strftime
+     * @return string
+     */
+    public static function userdate(int $value, stdClass $row, ?string $format = null): string {
+        return $value ? userdate($value, $format) : '';
+    }
+
+    /**
+     * Returns yes/no string depending on the given value
+     *
+     * @param bool $value
+     * @return string
+     */
+    public static function boolean_as_text(bool $value): string {
+        return $value ? get_string('yes') : get_string('no');
+    }
+}
