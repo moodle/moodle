bbf242efb086307a0af9367d6caea08649b261d1
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2021-01-16 16:58:14 +0100

    Moodle release 3.5.16

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2018051715.08;              // 20180517      = branching date YYYYMMDD - do not modify!
+$version  = 2018051716.00;              // 20180517      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.5.15+ (Build: 20210115)'; // Human-friendly version name
+$release  = '3.5.16 (Build: 20210118)'; // Human-friendly version name
 
 $branch   = '35';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
