db6d34630fb993c859d3eb6979fb6faf77599c41
Jake Dallimore <jake@moodle.com>
2021-01-22 11:30:51 +0800

    weekly release 3.9.4+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020061504.00;              // 20200615      = branching date YYYYMMDD - do not modify!
+$version  = 2020061504.01;              // 20200615      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.9.4 (Build: 20210118)'; // Human-friendly version name
+$release  = '3.9.4+ (Build: 20210122)'; // Human-friendly version name
 $branch   = '39';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
