0420ec757a8a89a5288edefcacb3af0bec237a12
Ilya Tregubov <ilya@moodle.com>
2021-01-26 14:21:33 +0200

    MDL-70676 workshop: Allow empty grades.

--- mod/workshop/db/upgrade.php
+++ mod/workshop/db/upgrade.php
@@ -51,5 +51,21 @@ function xmldb_workshop_upgrade($oldversion) {
     // Automatically generated Moodle v3.9.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2021052501) {
+
+        $table = new xmldb_table('workshop_grades');
+        $field = new xmldb_field('grade');
+
+        if ($dbman->field_exists($table, $field) && ($field->getNotNull() == XMLDB_NOTNULL)) {
+            $field = new xmldb_field('grade', XMLDB_TYPE_NUMBER, '10, 5', null, false, false,
+                null, null);
+            $dbman->change_field_notnull($table, $field);
+        }
+
+        // Main savepoint reached.
+        upgrade_mod_savepoint(true, 2021052501, 'workshop');
+
+    }
+
     return true;
 }
