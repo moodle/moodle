fa644137d4b527a77e5bd53c39c2f80c77564817
Derick Turner <derick@e-learndesign.co.uk>
2021-01-26 17:54:19 +0000

    IOMAD: left in debug for #1531

--- blocks/iomad_company_admin/uploaduser.php
+++ blocks/iomad_company_admin/uploaduser.php
@@ -804,7 +804,6 @@ if ($mform->is_cancelled()) {
                 }
 
             } else {
-echo "Got a new user - stopping";die;
                 // Save the user to the database.
                 $user->confirmed = 1;
                 $user->timemodified = time();
