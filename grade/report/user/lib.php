aead4ba45a4ecd20a2d99f9f1e9d73913aaa436f
Luca Bösch <luca.boesch@bfh.ch>
2021-02-13 14:31:33 +0100

    MDL-70874 gradereport_user: Add colors and icon distinguishing pass/fail

--- grade/report/user/lib.php
+++ grade/report/user/lib.php
@@ -387,7 +387,7 @@ class grade_report_user extends grade_report {
      * @param $element - An array containing the table data for the current row.
      */
     private function fill_table_recursive(&$element) {
-        global $DB, $CFG;
+        global $DB, $CFG, $OUTPUT;
 
         $type = $element['type'];
         $depth = $element['depth'];
@@ -577,10 +577,23 @@ class grade_report_user extends grade_report {
                                                                                 true);
                         }
                     } else {
-                        $data['grade']['class'] = $class;
-                        $data['grade']['content'] = grade_format_gradevalue($gradeval,
-                                                                            $grade_grade->grade_item,
-                                                                            true);
+                        $gradestatusclass = '';
+                        $gradepassicon = '';
+                        $ispassinggrade = $grade_grade->is_passed($grade_grade->grade_item);
+                        if (!is_null($ispassinggrade)) {
+                            $gradestatusclass = $ispassinggrade ? 'gradepass' : 'gradefail';
+                            if ($ispassinggrade) {
+                                $gradepassicon = $OUTPUT->pix_icon('i/valid', get_string('pass', 'grades'), null,
+                                    array('class' => 'inline'));
+                            } else {
+                                $gradepassicon = $OUTPUT->pix_icon('i/invalid', get_string('fail', 'grades'), null,
+                                    array('class' => 'inline'));
+                            }
+                        }
+
+                        $data['grade']['class'] = "{$class} {$gradestatusclass}";
+                        $data['grade']['content'] = $gradepassicon . grade_format_gradevalue($gradeval,
+                            $grade_grade->grade_item, true);
                         $gradeitemdata['graderaw'] = $gradeval;
                     }
                     $data['grade']['headers'] = "$header_cat $header_row grade";
