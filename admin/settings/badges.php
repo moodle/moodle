a306fd7a18ca8ab778773e99f016f0771d6d5b21
Sara Arjona <sara@moodle.com>
2021-02-16 10:45:25 +0100

    MDL-70911 core_badges: Move "External backpack" to badges settings
    Setting "External backpack connection" has been moved from "Backpack
    settings" to "Badges settings" in order to make easier to find and
    configure it.
    Some extra tests have been added to cover existing behaviour and
    confirm is still working as expected.

--- admin/settings/badges.php
+++ admin/settings/badges.php
@@ -71,6 +71,10 @@ if (($hassiteconfig || has_any_capability(array(
             new lang_string('allowcoursebadges', 'badges'),
             new lang_string('allowcoursebadges_desc', 'badges'), 1));
 
+    $globalsettings->add(new admin_setting_configcheckbox('badges_allowexternalbackpack',
+            new lang_string('allowexternalbackpack', 'badges'),
+            new lang_string('allowexternalbackpack_desc', 'badges'), 1));
+
     $ADMIN->add('badges', $globalsettings);
 
     $ADMIN->add('badges',
@@ -99,10 +103,6 @@ if (($hassiteconfig || has_any_capability(array(
     );
     $backpacksettings = new admin_settingpage('backpacksettings', new lang_string('backpacksettings', 'badges'),
             array('moodle/badges:manageglobalsettings'), empty($CFG->enablebadges));
-
-    $backpacksettings->add(new admin_setting_configcheckbox('badges_allowexternalbackpack',
-            new lang_string('allowexternalbackpack', 'badges'),
-            new lang_string('allowexternalbackpack_desc', 'badges'), 1));
 
     $backpacksettings->add(new admin_setting_configselect('badges_site_backpack',
             new lang_string('sitebackpack', 'badges'),
