f807863ba3192aaa87bcff4614d85874dda61d7f
Simey Lameze <simey@moodle.com>
2021-02-19 16:11:29 +0800

    MDL-70820 mod_data: custom completion implementation

---
+++ mod/data/classes/completion/custom_completion.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+declare(strict_types=1);
+
+namespace mod_data\completion;
+
+use core_completion\activity_custom_completion;
+
+/**
+ * Activity custom completion subclass for the data activity.
+ *
+ * Class for defining mod_data's custom completion rules and fetching the completion statuses
+ * of the custom completion rules for a given data instance and a user.
+ *
+ * @package mod_data
+ * @copyright Simey Lameze <simey@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class custom_completion extends activity_custom_completion {
+
+    /**
+     * Fetches the completion state for a given completion rule.
+     *
+     * @param string $rule The completion rule.
+     * @return int The completion state.
+     */
+    public function get_state(string $rule): int {
+        global $DB;
+
+        $this->validate_rule($rule);
+
+        $userentries = $DB->count_records('data_records', ['dataid' => $this->cm->instance, 'userid' => $this->userid]);
+        $completionentries = $this->cm->customdata['customcompletionrules']['completionentries'];
+
+        return ($completionentries <= $userentries) ? COMPLETION_COMPLETE : COMPLETION_INCOMPLETE;
+    }
+
+    /**
+     * Fetch the list of custom completion rules that this module defines.
+     *
+     * @return array
+     */
+    public static function get_defined_custom_rules(): array {
+        return ['completionentries'];
+    }
+
+    /**
+     * Returns an associative array of the descriptions of custom completion rules.
+     *
+     * @return array
+     */
+    public function get_custom_rule_descriptions(): array {
+        $entries = $this->cm->customdata['customcompletionrules']['completionentries'] ?? 0;
+        return [
+            'completionentries' => get_string('completiondetail:entries', 'data', $entries),
+        ];
+    }
+}
