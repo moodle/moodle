681c70017c40f13d4208700cb9e6246a255ee969
Marina Glancy <marina@moodle.com>
2021-02-22 16:17:21 +0100

    MDL-70901 core: @ no longer masks errors in PHP 8.0

--- backup/util/loggers/file_logger.class.php
+++ backup/util/loggers/file_logger.class.php
@@ -97,7 +97,7 @@ class file_logger extends base_logger {
         } else {
             $content = $prefix . str_repeat('&nbsp;&nbsp;', $depth) . htmlentities($message, ENT_QUOTES, 'UTF-8') . '<br/>' . PHP_EOL;
         }
-        if (false === fwrite($this->fhandle, $content)) {
+        if (!is_resource($this->fhandle) || (false === fwrite($this->fhandle, $content))) {
             throw new base_logger_exception('error_writing_file', $this->fullpath);
         }
         return true;
