01403b2f0d54c7c6081d8f54628a55761fe58724
Mahmoud Kassaei <mk4359@open.ac.uk>
2021-02-22 08:25:11 +0000

    MDL-70895 Questions: Default options when creating a question

--- question/type/questiontypebase.php
+++ question/type/questiontypebase.php
@@ -296,6 +296,45 @@ class question_type {
     }
 
     /**
+     * Return default value for a given form element either from user_preferences table or $default.
+     *
+     * @param string $name the name of the form element.
+     * @param mixed $default default value.
+     * @return string|null default value for a given  form element.
+     */
+    public function get_default_value(string $name, $default): ?string {
+        return get_user_preferences($this->plugin_name() . '_' . $name, $default ?? '0');
+    }
+
+    /**
+     * Save the default value for a given form element in user_preferences table.
+     *
+     * @param string $name the name of the value to set.
+     * @param string $value the setting value.
+     */
+    public function set_default_value(string $name, string $value): void {
+        set_user_preference($this->plugin_name() . '_' . $name, $value);
+    }
+
+    /**
+     * Save question defaults when creating new questions.
+     *
+     * @param stdClass $fromform data from the form.
+     */
+    public function save_defaults_for_new_questions(stdClass $fromform): void {
+        // Some question types may not make use of the certain form elements, so
+        // we need to do a check on the following generic form elements. For instance,
+        // 'defaultmark' is not use in qtype_multianswer and 'penalty' in not used in
+        // qtype_essay and qtype_recordrtc.
+        if (isset($fromform->defaultmark)) {
+            $this->set_default_value('defaultmark', $fromform->defaultmark);
+        }
+        if (isset($fromform->penalty)) {
+            $this->set_default_value('penalty', $fromform->penalty);
+        }
+    }
+
+    /**
      * Saves (creates or updates) a question.
      *
      * Given some question info and some data about the answers
--- question/type/shortanswer/tests/behat/add.feature
+++ question/type/shortanswer/tests/behat/add.feature
@@ -18,17 +18,24 @@ Feature: Test creating a Short answer question
     And I am on "Course 1" course homepage
     And I navigate to "Question bank" in current page administration
 
+  @javascript
   Scenario: Create a Short answer question
-    When I add a "Short answer" question filling the form with:
+    Given I add a "Short answer" question filling the form with:
       | Question name        | shortanswer-001                           |
       | Question text        | What is the national langauge in France?  |
       | General feedback     | The national langauge in France is French |
       | Default mark         | 1                                         |
-      | Case sensitivity     | No, case is unimportant                   |
+      | Case sensitivity     | Yes, case must match                      |
       | id_answer_0          | French                                    |
       | id_fraction_0        | 100%                                      |
       | id_feedback_0        | Well done. French is correct.             |
       | id_answer_1          | *                                         |
       | id_fraction_1        | None                                      |
       | id_feedback_1        | Your answer is incorrect.                 |
-    Then I should see "shortanswer-001"
+    And I should see "shortanswer-001"
+    # Checking that the next new question form displays user preferences settings.
+    When I press "Create a new question ..."
+    And I set the field "Short answer" to "1"
+    And I click on "Add" "button" in the "Choose a question type to add" "dialogue"
+    Then the following fields match these values:
+      | Case sensitivity | Yes, case must match |
