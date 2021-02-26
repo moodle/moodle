4652b66ee4c94816eea1cf97f8a452329b618cd2
Jun Pataleta <jun@moodle.com>
2021-02-26 13:42:46 +0800

    weekly release 3.11dev

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2021021900.00;              // 20201109      = branching date YYYYMMDD - do not modify!
+$version  = 2021022600.00;              // 20201109      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.11dev (Build: 20210219)';// Human-friendly version name
+$release  = '3.11dev (Build: 20210226)';// Human-friendly version name
 $branch   = '311';                      // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
