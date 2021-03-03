2deaee321ed42120f156688a19771c265ef1283c
Ilya Tregubov <ilya@moodle.com>
2021-03-03 11:11:23 +0200

    MDL-71026 user: Fix incorrect get_string call.

--- user/editadvanced.php
+++ user/editadvanced.php
@@ -204,7 +204,7 @@ if ($userform->is_cancelled()) {
         if (!$authplugin->is_internal() and $authplugin->can_change_password() and !empty($usernew->newpassword)) {
             if (!$authplugin->user_update_password($usernew, $usernew->newpassword)) {
                 // Do not stop here, we need to finish user creation.
-                debugging(get_string('cannotupdatepasswordonextauth', '', '', $usernew->auth), DEBUG_NONE);
+                debugging(get_string('cannotupdatepasswordonextauth', 'error', $usernew->auth), DEBUG_NONE);
             }
         }
         $usercreated = true;
