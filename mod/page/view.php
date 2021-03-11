4c0b62f6d6c9a9669527a56169c5d6c62994222f
Michael Hawkins <michaelh@moodle.com>
2021-03-11 11:52:34 +0800

    MDL-71161 mod_page: Display activity completion information

--- mod/page/view.php
+++ mod/page/view.php
@@ -72,6 +72,12 @@ if (!isset($options['printheading']) || !empty($options['printheading'])) {
     echo $OUTPUT->heading(format_string($page->name), 2);
 }
 
+// Display any activity information (eg completion requirements / dates).
+$cminfo = cm_info::create($cm);
+$completiondetails = \core_completion\cm_completion_details::get_instance($cminfo, $USER->id);
+$activitydates = \core\activity_dates::get_dates_for_module($cminfo, $USER->id);
+echo $OUTPUT->activity_information($cminfo, $completiondetails, $activitydates);
+
 if (!empty($options['printintro'])) {
     if (trim(strip_tags($page->intro))) {
         echo $OUTPUT->box_start('mod_introbox', 'pageintro');
