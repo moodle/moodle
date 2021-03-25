16a9e8ada7cae1a2bbfd597ae8eb934e303d4088
Jun Pataleta <jun@moodle.com>
2021-03-25 16:35:46 +0800

    MDL-71161 mod_h5pactivity: Display activity information output component

--- mod/h5pactivity/view.php
+++ mod/h5pactivity/view.php
@@ -70,6 +70,11 @@ $PAGE->set_context($context);
 echo $OUTPUT->header();
 echo $OUTPUT->heading(format_string($moduleinstance->name));
 
+// Render the activity information.
+$completiondetails = \core_completion\cm_completion_details::get_instance($cm, $USER->id);
+$activitydates = \core\activity_dates::get_dates_for_module($cm, $USER->id);
+echo $OUTPUT->activity_information($cm, $completiondetails, $activitydates);
+
 $instance = $manager->get_instance();
 if (!empty($instance->intro)) {
     echo $OUTPUT->box(format_module_intro('h5pactivity', $instance, $cm->id), 'generalbox', 'intro');
