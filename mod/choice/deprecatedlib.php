17c3c4dc7576cb5776203b0f497dfa5489667a39
Jun Pataleta <jun@moodle.com>
2021-03-30 09:52:59 +0800

    MDL-71144 mod_choice: Deprecate choice_get_completion_state()

---
+++ mod/choice/deprecatedlib.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * List of deprecated mod_choice functions.
+ *
+ * @package   mod_choice
+ * @copyright 2021 Jun Pataleta
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Obtains the automatic completion state for this choice based on any conditions
+ * in forum settings.
+ *
+ * @deprecated since Moodle 3.11
+ * @todo MDL-71196 Final deprecation in Moodle 4.3
+ * @see \mod_choice\completion\custom_completion
+ * @param object $course Course
+ * @param object $cm Course-module
+ * @param int $userid User ID
+ * @param bool $type Type of comparison (or/and; can be used as return value if no conditions)
+ * @return bool True if completed, false if not, $type if conditions not set.
+ */
+function choice_get_completion_state($course, $cm, $userid, $type) {
+    global $DB;
+
+    // No need to call debugging here. Deprecation debugging notice already being called in \completion_info::internal_get_state().
+
+    // Get choice details.
+    $choice = $DB->get_record('choice', array('id' => $cm->instance), '*',
+        MUST_EXIST);
+
+    // If completion option is enabled, evaluate it and return true/false.
+    if ($choice->completionsubmit) {
+        return $DB->record_exists('choice_answers', array(
+            'choiceid' => $choice->id, 'userid' => $userid));
+    } else {
+        // Completion option is not enabled so just return $type.
+        return $type;
+    }
+}
