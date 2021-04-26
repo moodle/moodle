70ac3b091f918c49c47b7aa6eccea8d61b626063
Shamim Rezaie <shamim@moodle.com>
2021-04-26 12:05:45 +1000

    MDL-71163 mod_forum: Remove duplicate due date info

--- mod/forum/classes/local/renderers/discussion_list.php
+++ mod/forum/classes/local/renderers/discussion_list.php
@@ -337,22 +337,11 @@ class discussion_list {
     private function get_notifications(stdClass $user, ?int $groupid) : array {
         $notifications = $this->notifications;
         $forum = $this->forum;
-        $renderer = $this->renderer;
         $capabilitymanager = $this->capabilitymanager;
 
         if ($forum->is_cutoff_date_reached()) {
             $notifications[] = (new notification(
                     get_string('cutoffdatereached', 'forum'),
-                    notification::NOTIFY_INFO
-            ))->set_show_closebutton();
-        } else if ($forum->is_due_date_reached()) {
-            $notifications[] = (new notification(
-                    get_string('thisforumisdue', 'forum', userdate($forum->get_due_date())),
-                    notification::NOTIFY_INFO
-            ))->set_show_closebutton();
-        } else if ($forum->has_due_date()) {
-            $notifications[] = (new notification(
-                    get_string('thisforumhasduedate', 'forum', userdate($forum->get_due_date())),
                     notification::NOTIFY_INFO
             ))->set_show_closebutton();
         }
