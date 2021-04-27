5b315fa5958e45487ad7af25e5608395e0257b8b
Sara Arjona <sara@moodle.com>
2021-04-27 15:15:26 +0200

    on-demand release 3.11dev+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2021042400.00;              // 20201109      = branching date YYYYMMDD - do not modify!
+$version  = 2021042700.00;              // 20201109      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.11dev+ (Build: 20210423)';// Human-friendly version name
+$release  = '3.11dev+ (Build: 20210427)';// Human-friendly version name
 $branch   = '311';                      // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
