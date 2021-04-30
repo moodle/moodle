6f48b4ab7104a4a966a60436902d61bd3284599b
Víctor Déniz <victor@moodle.com>
2021-04-30 12:54:10 +0100

    weekly release 3.11dev+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2021042700.00;              // 20201109      = branching date YYYYMMDD - do not modify!
+$version  = 2021043000.00;              // 20201109      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.11dev+ (Build: 20210427)';// Human-friendly version name
+$release  = '3.11dev+ (Build: 20210430)';// Human-friendly version name
 $branch   = '311';                      // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
