486ba9af352b4e74ea8492ae116e7ac14c45955f
Sara Arjona <sara@moodle.com>
2021-05-14 11:06:23 +0200

    weekly release 3.10.4+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020110904.01;              // 20201109      = branching date YYYYMMDD - do not modify!
+$version  = 2020110904.02;              // 20201109      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.10.4+ (Build: 20210511)';// Human-friendly version name
+$release  = '3.10.4+ (Build: 20210514)';// Human-friendly version name
 $branch   = '310';                      // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
