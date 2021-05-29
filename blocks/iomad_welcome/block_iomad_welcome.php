c19d41fefe62fabbf41bb1988dccf8493544e7dd
registerme1963 <45921124+registerme1963@users.noreply.github.com>
2021-05-29 09:08:44 +0530

    Add files via upload

---
+++ blocks/iomad_welcome/block_iomad_welcome.php
@@ -0,0 +1,64 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   block_iomad_welcome
+ * @copyright 2021 Derick Turner
+ * @author    Derick Turner
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_iomad_welcome extends block_base {
+    public function init() {
+        $this->title = get_string('pluginname', 'block_iomad_welcome');
+    }
+
+    public function hide_header() {
+        return false;
+    }
+
+    public function applicable_formats() {
+        return array('my' => true);
+    }
+
+    public function get_content() {
+        global $USER, $CFG, $DB, $OUTPUT;
+
+        // Empty by default;
+        $this->content = new stdClass;
+        $this->content->footer = '';
+        $this->content->text = '';
+
+        // Only display if you have the correct capability.
+        if (!iomad::has_capability('block/iomad_welcome:view', context_system::instance())) {
+            return;
+        }
+
+        // Only display until companies have been created
+        if ($DB->record_exists('company', array())) {
+            return;
+        }
+
+        $message = get_string('message', 'block_iomad_welcome');
+        $dashboardlink = new moodle_url('/my');
+        $dashboardtext = get_string('dashboardtext', 'block_iomad_welcome');
+        $this->content->text = '<p><center>' . $message . '</center></p>';
+        $this->content->text .= '<p><center><a href="' . $dashboardlink . '">' . $dashboardtext . '</a></center></p>';
+        $this->content->footer = '';
+
+        return $this->content;
+    }
+}
---
+++ blocks/iomad_welcome/lang/en/block_iomad_welcome.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   block_iomad_welcome
+ * @copyright 2021 Derick Turner
+ * @author    Derick Turner
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['dashboardtext'] = 'Click here to start using IOMAD';
+$string['iomad_welcome:addinstance'] = 'Add a new Iomad Welcome block';
+$string['iomad_welcome:myaddinstance'] = 'Add a new Iomad Welcome block to the users dashboard';
+$string['iomad_welcome:view'] = 'View Iomad Welcome';
+$string['message'] = 'Welcome to <b>IOMAD</b> from E-Learn Design Limited. Iomad is controlled from its own Dashboard. Follow the link below. You will start by creating your first company.';
+$string['pluginname'] = 'Iomad - Welcome';
+$string['privacy:metadata'] = 'The Iomad Welcome block only shows data stored in other locations.';
