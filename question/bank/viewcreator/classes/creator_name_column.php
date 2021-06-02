b0067846efe294735ae19c018412e55340531966
Marc-Alexandre Ghaly <marc-alexandreghaly@catalyst-ca.net>
2021-06-02 09:51:18 -0400

    MDL-71613 qbank_viewcreator: Add View creator to core
    This implementation will introduce a qbank plugin "viewcreator"
    which will view the question creator/modifier column in
    the question bank view by replacing the core classes. Having
    this plugin will give users the flexibility of enabling
    or disabling the question creator and modifier column
    in the question bank view.
    
    Co-Authored-By: Marc-Alexandre Ghaly <marc-alexandreghaly@catalyst-ca.net>

--- question/bank/viewcreator/classes/creator_name_column.php
+++ question/bank/viewcreator/classes/creator_name_column.php
@@ -14,50 +14,48 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * A column type for the name of the question creator.
- *
- * @package    core_question
- * @copyright  2009 Tim Hunt
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+namespace qbank_viewcreator;
 
-namespace core_question\bank;
-defined('MOODLE_INTERNAL') || die();
-
+use core_question\local\bank\column_base;
 
 /**
  * A column type for the name of the question creator.
  *
+ * @package   qbank_viewcreator
  * @copyright 2009 Tim Hunt
+ * @author    2021 Ghaly Marc-Alexandre <marc-alexandreghaly@catalyst-ca.net>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @todo MDL-72004 delete the class and add it to lib/db/renameclasses.php pointing to the plugin
  */
 class creator_name_column extends column_base {
-    public function get_name() {
+
+    public function get_name(): string {
         return 'creatorname';
     }
 
-    protected function get_title() {
+    protected function get_title(): string {
         return get_string('createdby', 'question');
     }
 
-    protected function display_content($question, $rowclasses) {
+    protected function display_content($question, $rowclasses): void {
+        global $PAGE;
+        $displaydata = [];
+
         if (!empty($question->creatorfirstname) && !empty($question->creatorlastname)) {
             $u = new \stdClass();
             $u = username_load_fields_from_object($u, $question, 'creator');
-            $date = userdate($question->timecreated, get_string('strftimedatetime', 'langconfig'));
-            echo fullname($u) . '<br>' . \html_writer::tag('span', $date, array('class' => 'date'));
+            $displaydata['date'] = userdate($question->timecreated, get_string('strftimedatetime', 'langconfig'));
+            $displaydata['creator'] = fullname($u);
+            echo $PAGE->get_renderer('qbank_viewcreator')->render_creator_name($displaydata);
         }
     }
 
-    public function get_extra_joins() {
-        return array('uc' => 'LEFT JOIN {user} uc ON uc.id = q.createdby');
+    public function get_extra_joins(): array {
+        return ['uc' => 'LEFT JOIN {user} uc ON uc.id = q.createdby'];
     }
 
-    public function get_required_fields() {
+    public function get_required_fields(): array {
         $allnames = \core_user\fields::get_name_fields();
-        $requiredfields = array();
+        $requiredfields = [];
         foreach ($allnames as $allname) {
             $requiredfields[] = 'uc.' . $allname . ' AS creator' . $allname;
         }
@@ -65,11 +63,12 @@ class creator_name_column extends column_base {
         return $requiredfields;
     }
 
-    public function is_sortable() {
-        return array(
-            'firstname' => array('field' => 'uc.firstname', 'title' => get_string('firstname')),
-            'lastname' => array('field' => 'uc.lastname', 'title' => get_string('lastname')),
-            'timecreated' => array('field' => 'q.timecreated', 'title' => get_string('date'))
-        );
+    public function is_sortable(): array {
+        return [
+            'firstname' => ['field' => 'uc.firstname', 'title' => get_string('firstname')],
+            'lastname' => ['field' => 'uc.lastname', 'title' => get_string('lastname')],
+            'timecreated' => ['field' => 'q.timecreated', 'title' => get_string('date')]
+        ];
     }
+
 }
