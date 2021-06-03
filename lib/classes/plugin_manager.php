38ec8028587603ed84bff9ac617b1905a8cb3ce8
Safat Shahin <safatshahin@catalyst-au.net>
2021-06-03 16:17:37 +1000

    MDL-71615 qbank_deletequestion: Add Delete question plugin to core
    This implementation will introduce a qbank plugin
    "deletequestion" which will implement the delete question
    action in the question bank view by replacing the core
    class. Having this plugin will give users the
    flexibility of enabling or disabling this action.

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1939,7 +1939,7 @@ class core_plugin_manager {
             ),
 
             'qbank' => [
-                ''
+                'deletequestion',
             ],
 
             'qbehaviour' => array(
