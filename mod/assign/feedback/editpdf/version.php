f3cd860698b235a051854f549636479ee534201a
Mikhail Golenkov <mikhailgolenkov@catalyst-au.net>
2021-06-04 15:18:59 +1000

    MDL-71471 assign: Remove submission from queue when converted in web

--- mod/assign/feedback/editpdf/version.php
+++ mod/assign/feedback/editpdf/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021052500;
+$plugin->version   = 2021060400;
 $plugin->requires  = 2021052500;
 $plugin->component = 'assignfeedback_editpdf';
