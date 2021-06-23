3e62c6c272a0499b0828456a20b7d4369cdfde64
Daniel Neis Araujo <danielneis@gmail.com>
2021-06-23 11:49:36 -0300

    MDL-71999 enrol: enrol_imsenterprise_allow_group_member_remove global

--- enrol/imsenterprise/lib.php
+++ enrol/imsenterprise/lib.php
@@ -851,20 +851,6 @@ class enrol_imsenterprise_plugin extends enrol_plugin {
     }
 
     /**
-     * Called whenever anybody tries (from the normal interface) to remove a group
-     * member which is registered as being created by this component. (Not called
-     * when deleting an entire group or course at once.)
-     * @param int $itemid Item ID that was stored in the group_members entry
-     * @param int $groupid Group ID
-     * @param int $userid User ID being removed from group
-     * @return bool True if the remove is permitted, false to give an error
-     */
-    public function enrol_imsenterprise_allow_group_member_remove($itemid, $groupid, $userid) {
-        return false;
-    }
-
-
-    /**
      * Get the default category id (often known as 'Miscellaneous'),
      * statically cached to avoid multiple DB lookups on big imports.
      *
@@ -991,4 +977,17 @@ class enrol_imsenterprise_plugin extends enrol_plugin {
         $context = context_course::instance($instance->courseid);
         return has_capability('enrol/imsenterprise:config', $context);
     }
+}
+
+/**
+ * Called whenever anybody tries (from the normal interface) to remove a group
+ * member which is registered as being created by this component. (Not called
+ * when deleting an entire group or course at once.)
+ * @param int $itemid Item ID that was stored in the group_members entry
+ * @param int $groupid Group ID
+ * @param int $userid User ID being removed from group
+ * @return bool True if the remove is permitted, false to give an error
+ */
+function enrol_imsenterprise_allow_group_member_remove($itemid, $groupid, $userid) {
+    return false;
 }
