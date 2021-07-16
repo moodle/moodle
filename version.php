2e1212eeae2bcab3c89362d31a7471e2240f6cb5
abgreeve <adrian@moodle.com>
2021-07-16 10:16:41 +0800

    weekly release 3.10.5+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020110905.00;              // 20201109      = branching date YYYYMMDD - do not modify!
+$version  = 2020110905.01;              // 20201109      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.10.5 (Build: 20210712)';// Human-friendly version name
+$release  = '3.10.5+ (Build: 20210716)';// Human-friendly version name
 $branch   = '310';                      // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
