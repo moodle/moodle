d06da2d1a0420359b507c4c31906eac576ff0f7e
Mihail Geshoski <mihail@moodle.com>
2021-08-03 15:51:12 +0800

    MDL-71912 core_grading: Update behat step definitions

--- grade/grading/tests/behat/behat_grading.php
+++ grade/grading/tests/behat/behat_grading.php
@@ -87,8 +87,7 @@ class behat_grading extends behat_base {
 
         $this->execute('behat_navigation::go_to_breadcrumb_location', $this->escape($activityname));
 
-        $this->execute('behat_navigation::i_navigate_to_in_current_page_administration',
-            get_string('viewgrading', 'mod_assign'));
+        $this->execute('behat_general::click_link', get_string('viewgrading', 'mod_assign'));
 
         $this->execute('behat_general::i_click_on_in_the',
                        array(
@@ -158,8 +157,7 @@ class behat_grading extends behat_base {
         $this->execute('behat_forms::press_button', get_string('savechanges'));
         $this->execute('behat_general::i_click_on', array($this->escape(get_string('editsettings')), 'link'));
         $this->execute('behat_forms::press_button', get_string('cancel'));
-        $this->execute('behat_navigation::i_navigate_to_in_current_page_administration',
-            get_string('viewgrading', 'mod_assign'));
+        $this->execute('behat_general::click_link', get_string('viewgrading', 'mod_assign'));
     }
 
     /**
