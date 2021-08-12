b24bef6b97f9cc749df92f00f98d03d5e3a023e0
james-cnz <5689414+james-cnz@users.noreply.github.com>
2021-08-12 17:44:17 +1200

    MDL-69637 course: Avoid unnecessary aria labels

--- course/format/weeks/format.js
+++ course/format/weeks/format.js
@@ -86,8 +86,6 @@ M.course.format.process_sections = function(Y, sectionlist, response, sectionfro
             stridx = str.lastIndexOf(' ');
             newstr = str.substr(0, stridx +1) + i;
             ele.setAttribute('title', newstr);
-            // Update the aria-label for the section.
-            sectionlist.item(i).setAttribute('aria-label', content.get('innerText').trim());
 
             // Remove the current class as section has been moved.
             sectionlist.item(i).removeClass('current');
