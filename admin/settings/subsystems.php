6498d743c32fabada44eaa226673bfe4a791b1b2
Sara Arjona <sara@moodle.com>
2021-08-12 12:00:45 +0200

    MDL-72119 admin: Move Enable competencies to Advanced features
    The "Enable competencies" setting has been moved from "Competencies
    settings" page to "Advanced features".
    Besides, the behaviour of the "Competencies" section has been reviewed
    to completely hide it when competencies are disabled.

--- admin/settings/subsystems.php
+++ admin/settings/subsystems.php
@@ -52,6 +52,12 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
     $optionalsubsystems->add(new admin_setting_configcheckbox('enableanalytics', new lang_string('enableanalytics', 'admin'),
         new lang_string('configenableanalytics', 'admin'), 1, 1, 0));
 
+    $optionalsubsystems->add(new admin_setting_configcheckbox('core_competency/enabled',
+        new lang_string('enablecompetencies', 'core_competency'),
+        new lang_string('enablecompetencies_desc', 'core_competency'),
+        1)
+    );
+
     $fullunicodesupport = true;
     if ($DB->get_dbfamily() == 'mysql') {
         $collation = $DB->get_dbcollation();
