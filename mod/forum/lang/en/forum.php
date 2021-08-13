6bd3db4fb59159a819f7a6bf24bcf78cb7949dc0
Jun Pataleta <jun@moodle.com>
2021-08-13 18:14:47 +0800

    MDL-69855 mod_forum: Deprecate postmailinfolink lang string
    With the info text and reply link text being split, this lang string
    is no longer being used and should be deprecated.

--- mod/forum/lang/en/forum.php
+++ mod/forum/lang/en/forum.php
@@ -515,9 +515,6 @@ $string['postbymailsuccess_html'] = 'Your reply <a href="{$a->discussionurl}">{$
 $string['postbyuser'] = '{$a->post} by {$a->user}';
 $string['postincontext'] = 'See this post in context';
 $string['postmailinfo'] = 'This is a copy of a message posted in {$a}.';
-$string['postmailinfolink'] = 'This is a copy of a message posted in {$a->coursename}.
-
-To reply click on this link: {$a->replylink}';
 $string['postmailnow'] = '<p>This post will be mailed out immediately to all forum subscribers.</p>';
 $string['postmailreply'] = 'To reply click on this link: {$a}';
 $string['postmailsubject'] = '{$a->courseshortname}: {$a->subject}';
@@ -777,6 +774,11 @@ $string['nousersmatch'] = 'No user(s) found for given criteria';
 $string['showmoreusers'] = 'Show more users';
 $string['viewconversation'] = 'View discussion';
 $string['viewgrades'] = 'View grades';
+
+// Deprecated since Moodle 4.0.
+$string['postmailinfolink'] = 'This is a copy of a message posted in {$a->coursename}.
+
+To reply click on this link: {$a->replylink}';
 
 // Deprecated since Moodle 3.8.
 $string['cannotdeletediscussioninsinglediscussion'] = 'You cannot delete the first post in a single discussion';
