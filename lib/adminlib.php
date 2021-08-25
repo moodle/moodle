617ff7fb83ddfaf855c24c3a4b3982572c62808c
Andrew Nicols <andrew@nicols.co.uk>
2021-08-25 14:17:37 +0800

    MDL-72163 admin: Manually include some interfaces
    Where the autolodaer is not registered (ABORT_AFTER_CONFIG and
    installation), some adminlib classes, traits, and interfaces need to be
    manually loaded.

--- lib/adminlib.php
+++ lib/adminlib.php
@@ -111,6 +111,11 @@ require_once($CFG->libdir.'/ddllib.php');
 require_once($CFG->libdir.'/xmlize.php');
 require_once($CFG->libdir.'/messagelib.php');
 
+// Add classes, traits, and interfaces which should be autoloaded.
+// The autoloader is configured late in setup.php, after ABORT_AFTER_CONFIG.
+// This is also required where the setup system is not included at all.
+require_once($CFG->dirroot.'/admin/classes/local/settings/linkable_settings_page.php');
+
 define('INSECURE_DATAROOT_WARNING', 1);
 define('INSECURE_DATAROOT_ERROR', 2);
 
