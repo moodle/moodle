5a165c86a23fe3054e85b2f048c7148ce443d370
Ferran Recio <ferran@moodle.com>
2021-08-31 19:06:46 +0200

    MDL-71795 courseformat: add cache to course index

--- course/format/amd/src/local/courseeditor/courseeditor.js
+++ course/format/amd/src/local/courseeditor/courseeditor.js
@@ -18,6 +18,7 @@ import notification from 'core/notification';
 import Exporter from 'core_courseformat/local/courseeditor/exporter';
 import log from 'core/log';
 import ajax from 'core/ajax';
+import * as Storage from 'core/sessionstorage';
 
 /**
  * Main course editor module.
@@ -31,6 +32,19 @@ import ajax from 'core/ajax';
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 export default class extends Reactive {
+
+    /**
+     * The current state cache key
+     *
+     * The state cache is considered dirty if the state changes from the last page or
+     * if the page has editing mode on.
+     *
+     * @attribute stateKey
+     * @type number|null
+     * @default 1
+     * @package
+     */
+    stateKey = 1;
 
     /**
      * Set up the course editor when the page is ready.
@@ -62,6 +76,20 @@ export default class extends Reactive {
         }
 
         this.setInitialState(stateData);
+
+        // In editing mode, the session cache is considered dirty always.
+        if (this.isEditing) {
+            this.stateKey = null;
+        } else {
+            // Check if the last state is the same as the cached one.
+            const newState = JSON.stringify(stateData);
+            const previousState = Storage.get(`course/${courseId}/staticState`);
+            if (previousState !== newState) {
+                Storage.set(`course/${courseId}/staticState`, newState);
+                Storage.set(`course/${courseId}/stateKey`, Date.now());
+            }
+            this.stateKey = Storage.get(`course/${courseId}/stateKey`);
+        }
     }
 
     /**
@@ -126,6 +154,56 @@ export default class extends Reactive {
      */
     get supportComponents() {
         return this._supportscomponents ?? false;
+    }
+
+    /**
+     * Get a value from the course editor static storage if any.
+     *
+     * The course editor static storage uses the sessionStorage to store values from the
+     * components. This is used to prevent unnecesary template loadings on every page. However,
+     * the storage does not work if no sessionStorage can be used (in debug mode for example),
+     * if the page is in editing mode or if the initial state change from the last page.
+     *
+     * @param {string} key the key to get
+     * @return {boolean|string} the storage value or false if cannot be loaded
+     */
+    getStorageValue(key) {
+        if (this.isEditing || !this.stateKey) {
+            return false;
+        }
+        const dataJson = Storage.get(`course/${this.courseId}/${key}`);
+        if (!dataJson) {
+            return false;
+        }
+        // Check the stateKey.
+        try {
+            const data = JSON.parse(dataJson);
+            if (data?.stateKey !== this.stateKey) {
+                return false;
+            }
+            return data.value;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    /**
+     * Stores a value into the course editor static storage if available
+     *
+     * @param {String} key the key to store
+     * @param {*} value the value to store (must be compatible with JSON,stringify)
+     * @returns {boolean} true if the value is stored
+     */
+    setStorageValue(key, value) {
+        // Values cannot be stored on edit mode.
+        if (this.isEditing) {
+            return false;
+        }
+        const data = {
+            stateKey: this.stateKey,
+            value,
+        };
+        return Storage.set(`course/${this.courseId}/${key}`, JSON.stringify(data));
     }
 
     /**
