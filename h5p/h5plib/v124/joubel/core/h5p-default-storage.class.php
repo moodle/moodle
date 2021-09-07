ae3066690cf3470449e52927fa46acb8fe3e1e32
Sara Arjona <sara@moodle.com>
2021-09-07 17:04:52 +0200

    MDL-71970 h5plib_v124: Add namespace to H5P core library
    The joubel/core is a third-party library. A namespace has been added to
    avoid collision with other plugins using it (such as mod_hvp).
    That way, they will be able to have a different version without
    side effects.

--- h5p/h5plib/v124/joubel/core/h5p-default-storage.class.php
+++ h5p/h5plib/v124/joubel/core/h5p-default-storage.class.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Moodle;
 /**
  * File info?
  */
@@ -9,13 +10,13 @@
  * operations using PHP's standard file operation functions.
  *
  * Some implementations of H5P that doesn't use the standard file system will
- * want to create their own implementation of the \H5P\FileStorage interface.
+ * want to create their own implementation of the H5PFileStorage interface.
  *
  * @package    H5P
  * @copyright  2016 Joubel AS
  * @license    MIT
  */
-class H5PDefaultStorage implements \H5PFileStorage {
+class H5PDefaultStorage implements H5PFileStorage {
   private $path, $alteditorpath;
 
   /**
@@ -39,10 +40,10 @@ class H5PDefaultStorage implements \H5PFileStorage {
    *  Library properties
    */
   public function saveLibrary($library) {
-    $dest = $this->path . '/libraries/' . \H5PCore::libraryToString($library, TRUE);
+    $dest = $this->path . '/libraries/' . H5PCore::libraryToString($library, TRUE);
 
     // Make sure destination dir doesn't exist
-    \H5PCore::deleteFileTree($dest);
+    H5PCore::deleteFileTree($dest);
 
     // Move library folder
     self::copyFileTree($library['uploadDirectory'], $dest);
@@ -60,7 +61,7 @@ class H5PDefaultStorage implements \H5PFileStorage {
     $dest = "{$this->path}/content/{$content['id']}";
 
     // Remove any old content
-    \H5PCore::deleteFileTree($dest);
+    H5PCore::deleteFileTree($dest);
 
     self::copyFileTree($source, $dest);
   }
@@ -72,7 +73,7 @@ class H5PDefaultStorage implements \H5PFileStorage {
    *  Content properties
    */
   public function deleteContent($content) {
-    \H5PCore::deleteFileTree("{$this->path}/content/{$content['id']}");
+    H5PCore::deleteFileTree("{$this->path}/content/{$content['id']}");
   }
 
   /**
@@ -133,7 +134,7 @@ class H5PDefaultStorage implements \H5PFileStorage {
    *  Folder that library resides in
    */
   public function exportLibrary($library, $target, $developmentPath=NULL) {
-    $folder = \H5PCore::libraryToString($library, TRUE);
+    $folder = H5PCore::libraryToString($library, TRUE);
     $srcPath = ($developmentPath === NULL ? "/libraries/{$folder}" : $developmentPath);
     self::copyFileTree("{$this->path}{$srcPath}", "{$target}/{$folder}");
   }
