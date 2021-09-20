341e3079fc7e31faf4585e0923a70313b7cb6b4a
Nathan Nguyen <nathannguyen@catalyst-au.net>
2021-09-20 15:01:32 +1000

    MDL-72075 qbank_statistics: Add question statistics to core
    This implementation will add the statistics plugin to core.
    Statistics plugin shows the overall report for a question using
    a couple columns in the base view.

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1958,6 +1958,7 @@ class core_plugin_manager {
                 'importquestions',
                 'managecategories',
                 'previewquestion',
+                'statistics',
                 'tagquestion',
                 'viewcreator',
                 'viewquestionname',
