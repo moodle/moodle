d5059795c9c73eda304eb82400a22d667f3a7a7a
Joshua Ebarvia <jemate18@gmail.com>
2021-09-28 12:29:01 +0930

    MDL-67966 mod_workshop: Use condition to verify form is using editor
    This adds a condition to check if Online text is used
    for the submission. The functions file_postupdate_standard_editor
    and file_postupdate_standard_filemanager needs the property
    content_editor of the formdata.

--- mod/workshop/exsubmission.php
+++ mod/workshop/exsubmission.php
@@ -157,10 +157,13 @@ if ($edit and $canmanage) {
         }
 
         // Save and relink embedded images and save attachments.
-        $formdata = file_postupdate_standard_editor($formdata, 'content', $workshop->submission_content_options(),
-            $workshop->context, 'mod_workshop', 'submission_content', $example->id);
-        $formdata = file_postupdate_standard_filemanager($formdata, 'attachment', $workshop->submission_attachment_options(),
-            $workshop->context, 'mod_workshop', 'submission_attachment', $example->id);
+        // To be used when Online text is allowed as a submission type.
+        if (!empty($formdata->content_editor)) {
+            $formdata = file_postupdate_standard_editor($formdata, 'content', $workshop->submission_content_options(),
+                $workshop->context, 'mod_workshop', 'submission_content', $example->id);
+            $formdata = file_postupdate_standard_filemanager($formdata, 'attachment', $workshop->submission_attachment_options(),
+                $workshop->context, 'mod_workshop', 'submission_attachment', $example->id);
+        }
 
         if (empty($formdata->attachment)) {
             // explicit cast to zero integer
