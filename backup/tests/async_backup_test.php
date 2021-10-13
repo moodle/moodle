28d6333f4753e2b89e53ab1a5aeeccf407279302
Peter Burnett <peterburnett@catalyst-au.net>
2021-10-14 08:49:49 +1000

    MDL-68773 backup: Allow completed async backup + restore tasks to clear

--- backup/tests/async_backup_test.php
+++ backup/tests/async_backup_test.php
@@ -41,7 +41,7 @@ class core_backup_async_backup_testcase extends \core_privacy\tests\provider_tes
      * Tests the asynchronous backup.
      */
     public function test_async_backup() {
-        global $DB, $CFG, $USER;
+        global $CFG, $DB, $USER;
 
         $this->resetAfterTest(true);
         $this->setAdminUser();
@@ -79,8 +79,6 @@ class core_backup_async_backup_testcase extends \core_privacy\tests\provider_tes
         $DB->set_field('course_sections', 'availability', $availability, array(
                 'course' => $course->id, 'section' => 1));
 
-        // Start backup process.
-
         // Make the backup controller for an async backup.
         $bc = new backup_controller(backup::TYPE_1COURSE, $course->id, backup::FORMAT_MOODLE,
                 backup::INTERACTIVE_YES, backup::MODE_ASYNC, $USER->id);
@@ -97,7 +95,7 @@ class core_backup_async_backup_testcase extends \core_privacy\tests\provider_tes
         // Create the adhoc task.
         $asynctask = new \core\task\asynchronous_backup_task();
         $asynctask->set_blocking(false);
-        $asynctask->set_custom_data(array('backupid' => $backupid));
+        $asynctask->set_custom_data(['backupid' => $backupid]);
         \core\task\manager::queue_adhoc_task($asynctask);
 
         // We are expecting trace output during this test.
@@ -115,5 +113,85 @@ class core_backup_async_backup_testcase extends \core_privacy\tests\provider_tes
         // Check backup was created successfully.
         $this->assertEquals(backup::STATUS_FINISHED_OK, $postbackuprec->status);
         $this->assertEquals(1.0, $postbackuprec->progress);
+    }
+
+    /**
+     * Tests the asynchronous backup will resolve in duplicate cases.
+     */
+    public function test_complete_async_backup() {
+        global $CFG, $DB, $USER;
+
+        $this->resetAfterTest(true);
+        $this->setAdminUser();
+        $CFG->enableavailability = true;
+        $CFG->enablecompletion = true;
+
+        // Create a course with some availability data set.
+        $generator = $this->getDataGenerator();
+        $course = $generator->create_course(
+                array('format' => 'topics', 'numsections' => 3,
+                        'enablecompletion' => COMPLETION_ENABLED),
+                array('createsections' => true));
+        $forum = $generator->create_module('forum', array(
+                'course' => $course->id));
+        $forum2 = $generator->create_module('forum', array(
+                'course' => $course->id, 'completion' => COMPLETION_TRACKING_MANUAL));
+
+        // We need a grade, easiest is to add an assignment.
+        $assignrow = $generator->create_module('assign', array(
+                'course' => $course->id));
+        $assign = new assign(context_module::instance($assignrow->cmid), false, false);
+        $item = $assign->get_grade_item();
+
+        // Make a test grouping as well.
+        $grouping = $generator->create_grouping(array('courseid' => $course->id,
+                'name' => 'Grouping!'));
+
+        $availability = '{"op":"|","show":false,"c":[' .
+                '{"type":"completion","cm":' . $forum2->cmid .',"e":1},' .
+                '{"type":"grade","id":' . $item->id . ',"min":4,"max":94},' .
+                '{"type":"grouping","id":' . $grouping->id . '}' .
+                ']}';
+        $DB->set_field('course_modules', 'availability', $availability, array(
+                'id' => $forum->cmid));
+        $DB->set_field('course_sections', 'availability', $availability, array(
+                'course' => $course->id, 'section' => 1));
+
+        // Make the backup controller for an async backup.
+        $bc = new backup_controller(backup::TYPE_1COURSE, $course->id, backup::FORMAT_MOODLE,
+                backup::INTERACTIVE_YES, backup::MODE_ASYNC, $USER->id);
+        $bc->finish_ui();
+        $backupid = $bc->get_backupid();
+        $bc->destroy();
+
+        // Now hack the record to remove the controller and set the status fields to complete.
+        // This emulates a duplicate run for an already finished controller.
+        $id = $DB->get_field('backup_controllers', 'id', ['backupid' => $backupid]);
+        $data = [
+            'id' => $id,
+            'controller' => '',
+            'progress' => 1.0,
+            'status' => backup::STATUS_FINISHED_OK
+        ];
+        $DB->update_record('backup_controllers', $data);
+
+        // Now queue an adhoc task and check it handles and completes gracefully.
+        $asynctask = new \core\task\asynchronous_backup_task();
+        $asynctask->set_blocking(false);
+        $asynctask->set_custom_data(array('backupid' => $backupid));
+        \core\task\manager::queue_adhoc_task($asynctask);
+
+        // We are expecting a specific message output during this test.
+        $this->expectOutputRegex('/invalid controller/');
+
+        // Execute adhoc task.
+        $now = time();
+        $task = \core\task\manager::get_next_adhoc_task($now);
+        $this->assertInstanceOf('\\core\\task\\asynchronous_backup_task', $task);
+        $task->execute();
+        \core\task\manager::adhoc_task_complete($task);
+
+        // Check the task record is removed.
+        $this->assertEquals(0, $DB->count_records('task_adhoc'));
     }
 }
