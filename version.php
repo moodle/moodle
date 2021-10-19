8885e22a0b56f503b685313d0ae3e581ecf0665b
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2021-10-19 12:52:08 +0200

    on-demand release 4.0dev+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2021101500.01;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2021101900.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.0dev+ (Build: 20211015)'; // Human-friendly version name
+$release  = '4.0dev+ (Build: 20211019)'; // Human-friendly version name
 $branch   = '400';                      // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
