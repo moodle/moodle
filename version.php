d135a1200a87a62cad7afa2e51889b845f72e4cb
Ilya Tregubov <ilya@moodle.com>
2021-11-05 19:18:58 +0200

    on-demand release 4.0dev+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2021110200.01;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2021110600.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.0dev+ (Build: 20211102)'; // Human-friendly version name
+$release  = '4.0dev+ (Build: 20211106)'; // Human-friendly version name
 $branch   = '400';                      // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
