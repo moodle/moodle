5efd4ea6c61c1a8545facfcb9240c138e88370a5
paulsev <severovpg@gmail.com>
2021-11-06 19:26:56 +0300

    Test commit

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
  * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
+// test comment
 if (!file_exists('./config.php')) {
     header('Location: install.php');
     die;
