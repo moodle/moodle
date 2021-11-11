4124e8b4bb2f5001a74736e9592e8aef0480834f
PraiseSatan <blackjeffrey1@gmail.com>
2021-11-12 07:29:21 +1100

    MDL-69496 quiz: Only validate completion if unlocked
    Quiz completion settings are only validated if they are unlocked.
    Undefined behaviour in the forms API results in the completion
    settings 'require passing grade' and 'completion attempts exhausted'
    not being sent to the process options function if completion
    settings are locked. This resulted in the completion attempts
    exhausted setting being disabled whenever the quiz was saved
    as it did not detect the require passing grade setting.
    This changes the behaviour so that validation is only performed
    if the completion settings are unlocked.
    A behat test is included to ensure the setting does not change.

--- mod/quiz/lib.php
+++ mod/quiz/lib.php
@@ -1128,14 +1128,17 @@ function quiz_process_options($quiz) {
     $quiz->reviewoverallfeedback &= ~mod_quiz_display_options::DURING;
 
     // Ensure that disabled checkboxes in completion settings are set to 0.
-    if (empty($quiz->completionusegrade)) {
-        $quiz->completionpass = 0;
-    }
-    if (empty($quiz->completionpass)) {
-        $quiz->completionattemptsexhausted = 0;
-    }
-    if (empty($quiz->completionminattemptsenabled)) {
-        $quiz->completionminattempts = 0;
+    // But only if the completion settinsg are unlocked.
+    if (!empty($quiz->completionunlocked)) {
+        if (empty($quiz->completionusegrade)) {
+            $quiz->completionpass = 0;
+        }
+        if (empty($quiz->completionpass)) {
+            $quiz->completionattemptsexhausted = 0;
+        }
+        if (empty($quiz->completionminattemptsenabled)) {
+            $quiz->completionminattempts = 0;
+        }
     }
 }
 
