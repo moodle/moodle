5e42a09a9208ab9871e874ea48a3b159439e55c3
David Matamoros <davidmc@moodle.com>
2021-12-21 16:35:44 +0100

    MDL-73436 tool_componentlibrary: Document dynamic tabs component

---
+++ admin/tool/componentlibrary/classes/local/examples/dynamictabs/tab2.php
@@ -0,0 +1,85 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle Component Library
+ *
+ * A sample dynamic tab page.
+ *
+ * @package    tool_componentlibrary
+ * @copyright  2021 David Matamoros <davidmc@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+declare(strict_types=1);
+
+namespace tool_componentlibrary\local\examples\dynamictabs;
+
+use core\output\dynamic_tabs\base;
+use renderer_base;
+use stdClass;
+
+/**
+ * Example tab class
+ *
+ * @package     tool_componentlibrary
+ * @copyright   2021 David Matamoros <davidmc@moodle.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tab2 extends base {
+
+    /**
+     * Export this for use in a mustache template context.
+     *
+     * @param renderer_base $output
+     * @return stdClass
+     */
+    public function export_for_template(renderer_base $output) {
+        $content = (object)[];
+        // Define any content you might want to pass to the dynamic tab.
+        $content->currenttime = userdate(time(), get_string('strftimedatetimeaccurate'));
+
+        return $content;
+    }
+
+    /**
+     * The label to be displayed on the tab
+     *
+     * @return string
+     */
+    public function get_tab_label(): string {
+        return 'Tab 2';
+    }
+
+    /**
+     * Check permission of the current user to access this tab
+     *
+     * @return bool
+     */
+    public function is_available(): bool {
+        // Define the correct permissions here.
+        return true;
+    }
+
+    /**
+     * Template to use to display tab contents
+     *
+     * @return string
+     */
+    public function get_template(): string {
+        return 'tool_componentlibrary/examples/dynamictabs/tab2';
+    }
+}
