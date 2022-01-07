ec3bfb8292d9c252a6529f2d5efee0fa79fa62b3
Chinh Vo Thuong <vothuongchinh@gmail.com>
2022-01-07 17:51:13 +0700

    Update version.php

--- lib/editor/atto/plugins/fullscreen/version.php
+++ lib/editor/atto/plugins/fullscreen/version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015122011;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2014051200;        // Requires this Moodle version.
+$plugin->version   = 2021052500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2021052500;        // Requires this Moodle version.
 $plugin->component = 'atto_fullscreen';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE; // Maturity level.
 $plugin->release = '1.0.1';
