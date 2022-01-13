c653a49fe3549e2e767a807f85ce4125ed803245
Paul Holden <paulh@moodle.com>
2022-01-13 23:00:57 +0000

    MDL-72972 tool_uploaduser: observe capability to delete users.

--- admin/tool/uploaduser/user_form.php
+++ admin/tool/uploaduser/user_form.php
@@ -147,6 +147,11 @@ class admin_uploaduser_form2 extends moodleform {
 
         $mform->addElement('selectyesno', 'uuallowdeletes', get_string('allowdeletes', 'tool_uploaduser'));
         $mform->setDefault('uuallowdeletes', 0);
+        // Ensure user is able to perform user deletion.
+        if (!has_capability('moodle/user:delete', context_system::instance())) {
+            $mform->hardFreeze('uuallowdeletes');
+            $mform->setConstant('uuallowdeletes', 0);
+        }
         $mform->hideIf('uuallowdeletes', 'uutype', 'eq', UU_USER_ADDNEW);
         $mform->hideIf('uuallowdeletes', 'uutype', 'eq', UU_USER_ADDINC);
 
