catalyst/moodle-MDL-70329

MDL-71696 core_question: Implement question status change
This commit implements the status change pop up for a question
in the base view. This feature update will allow the change of
status of a question without creating a new version.
This commit also implements the status as a char rather than
an int value.

question/bank/editquestion/db/services.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component qbank_editquestion, language 'en'.
+ * External services definition for qbank_editquestion.
  *
  * @package    qbank_editquestion
  * @copyright  2021 Catalyst IT Australia Pty Ltd
@@ -23,17 +23,13 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Edit questions';
-$string['privacy:metadata'] = 'The Edit questions question bank plugin does not store any user data.';
+defined('MOODLE_INTERNAL') || die();
 
-// Question status.
-$string['questionstatus'] = 'Status';
-$string['questionstatusready'] = 'Ready';
-$string['questionstatushidden'] = 'Hidden';
-$string['questionstatusdraft'] = 'Draft';
-$string['questionstatusunknown'] = 'Unknown';
-$string['questionstatusheader'] = 'Change question status';
-
-// Edit form.
-$string['versioninfo'] = 'Authors, version control and usage';
-$string['status'] = 'Question status';
+$functions = [
+    'qbank_editquestion_set_status' => [
+        'classname' => 'qbank_editquestion\external\submit_status',
+        'description' => 'Update the question status.',
+        'type' => 'write',
+        'ajax' => true,
+    ],
+];

moodle/moodle

question/bank/editquestion/db/services.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version information for qbank_editquestion.
+ * External services definition for qbank_editquestion.
  *
  * @package    qbank_editquestion
  * @copyright  2021 Catalyst IT Australia Pty Ltd
@@ -25,7 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'qbank_editquestion';
-$plugin->version   = 2021110800;
-$plugin->requires  = 2021052500;
-$plugin->maturity  = MATURITY_STABLE;
+$functions = [
+    'qbank_editquestion_set_status' => [
+        'classname' => 'qbank_editquestion\external\update_question_version_status',
+        'description' => 'Update the question status.',
+        'type' => 'write',
+        'ajax' => true,
+    ],
+];