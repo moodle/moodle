31751c2f328060eb8ed3d22ff0a6af0dae886589
Marina Glancy <marina@moodle.com>
2022-02-04 17:13:08 +0100

    MDL-73669 behat: allow multiple files with deprecated steps

--- lib/tests/behat/behat_deprecated.php
+++ lib/tests/behat/behat_deprecated.php
@@ -14,72 +14,26 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Steps definitions that will be deprecated in the next releases.
- *
- * @package    core
- * @category   test
- * @copyright  2013 David Monllaó
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 // NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
 
-require_once(__DIR__ . '/../../../lib/behat/behat_base.php');
+require_once(__DIR__ . '/../../../lib/behat/behat_deprecated_base.php');
 
 use Behat\Mink\Exception\ElementNotFoundException as ElementNotFoundException,
-    Behat\Gherkin\Node\TableNode as TableNode,
-    Behat\Gherkin\Node\PyStringNode as PyStringNode;
+    Behat\Gherkin\Node\TableNode as TableNode;
 
 /**
- * Deprecated behat step definitions.
+ * Steps definitions that are now deprecated and will be removed in the next releases.
+ *
+ * This file only contains the steps that previously were in the behat_*.php files in the SAME DIRECTORY.
+ * When deprecating steps from other components or plugins, create a behat_COMPONENT_deprecated.php
+ * file in the same directory where the steps were defined.
  *
  * @package    core
  * @category   test
  * @copyright  2013 David Monllaó
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class behat_deprecated extends behat_base {
-
-    /**
-     * Throws an exception if $CFG->behat_usedeprecated is not allowed.
-     *
-     * @throws Exception
-     * @param string|array $alternatives Alternative/s to the requested step
-     * @param bool $throwexception If set to true we always throw exception, irrespective of behat_usedeprecated setting.
-     * @return void
-     */
-    protected function deprecated_message($alternatives, $throwexception = false) {
-        global $CFG;
-
-        // We do nothing if it is enabled.
-        if (!empty($CFG->behat_usedeprecated) && !$throwexception) {
-            return;
-        }
-
-        if (is_scalar($alternatives)) {
-            $alternatives = array($alternatives);
-        }
-
-        // Show an appropriate message based on the throwexception flag.
-        if ($throwexception) {
-            $message = 'This step has been removed. Rather than using this step you can:';
-        } else {
-            $message = 'Deprecated step, rather than using this step you can:';
-        }
-
-        // Add all alternatives to the message.
-        foreach ($alternatives as $alternative) {
-            $message .= PHP_EOL . '- ' . $alternative;
-        }
-
-        if (!$throwexception) {
-            $message .= PHP_EOL . '- Set $CFG->behat_usedeprecated in config.php to allow the use of deprecated steps
-                    if you don\'t have any other option';
-        }
-
-        throw new Exception($message);
-    }
+class behat_deprecated extends behat_deprecated_base {
 
     /**
      * Clicks link with specified id|title|alt|text in the flat navigation drawer.
