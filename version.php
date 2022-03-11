7c52eaaf180ba34534c3b4106fe18ac7a02f0f20
Ilya Tregubov <ilya@moodle.com>
2022-03-11 16:19:54 +0600

    weekly release 3.9.12+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020061512.05;              // 20200615      = branching date YYYYMMDD - do not modify!
+$version  = 2020061512.06;              // 20200615      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.9.12+ (Build: 20220226)'; // Human-friendly version name
+$release  = '3.9.12+ (Build: 20220311)'; // Human-friendly version name
 $branch   = '39';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
