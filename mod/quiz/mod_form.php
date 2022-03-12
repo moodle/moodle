039953f01ab5b745b01c5230f68d9a035c9e5f88
Tim Hunt <T.J.Hunt@open.ac.uk>
2022-03-12 16:21:53 +0000

    MDL-74184 quiz: prevent repagination of quizzes with attepts
    There was a loophole to this rule on the settings form, which is now fixed.

--- mod/quiz/mod_form.php
+++ mod/quiz/mod_form.php
@@ -146,7 +146,7 @@ class mod_quiz_mod_form extends moodleform_mod {
                 get_string('newpage', 'quiz'), quiz_questions_per_page_options(), array('id' => 'id_questionsperpage'));
         $mform->setDefault('questionsperpage', $quizconfig->questionsperpage);
 
-        if (!empty($this->_cm)) {
+        if (!empty($this->_cm) && !quiz_has_attempts($this->_cm->instance)) {
             $pagegroup[] = $mform->createElement('checkbox', 'repaginatenow', '',
                     get_string('repaginatenow', 'quiz'), array('id' => 'id_repaginatenow'));
         }
