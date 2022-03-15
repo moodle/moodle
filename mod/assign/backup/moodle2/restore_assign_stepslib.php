4fe474fad4bde9e5937c9d81b79cef12e6d4c8a2
Simey Lameze <simey@moodle.com>
2022-03-15 17:39:04 +0800

    MDL-74088 assign: backup and restore support for time limit fields

--- mod/assign/backup/moodle2/restore_assign_stepslib.php
+++ mod/assign/backup/moodle2/restore_assign_stepslib.php
@@ -111,10 +111,6 @@ class restore_assign_activity_structure_step extends restore_activity_structure_
         } else {
             $data->teamsubmissiongroupingid = 0;
         }
-
-        if (!isset($data->timelimit)) {
-            $data->timelimit = 0;
-        }
         if (!isset($data->cutoffdate)) {
             $data->cutoffdate = 0;
         }
