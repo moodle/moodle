9164c896c4e0b5e5d0d68c8d7f0368287129ef5d
Mihail Geshoski <mihail@moodle.com>
2022-03-21 23:07:19 +0800

    MDL-74189 tool_moodlenet: Add upgrade step to remove irrelevant data
    Upgrade step to identify all existing cases where users have linked
    their MoodleNet profile on the site and remove the related data from
    the database. Due to some recent changes on the MoodleNet platform,
    this data is now irrelevant and can no longer be used to authenticate
    on the MoodleNet platform.

--- admin/tool/moodlenet/db/upgrade.php
+++ admin/tool/moodlenet/db/upgrade.php
@@ -123,5 +123,24 @@ function xmldb_tool_moodlenet_upgrade(int $oldversion) {
         upgrade_plugin_savepoint(true, 2020061503, 'tool', 'moodlenet');
     }
 
+    if ($oldversion < 2020061504) {
+
+        $selectsql = "moodlenetprofile IS NOT NULL AND moodlenetprofile != ''";
+
+        // If there are any users with MoodleNet profile set.
+        if ($DB->count_records_select('user', $selectsql)) {
+            // Remove the value set for the MoodleNet profile as this format can no longer be used to authenticate
+            // MoodleNet users.
+            $DB->set_field_select('user', 'moodlenetprofile', '', $selectsql);
+
+            // Use an adhoc task to send a notification to admin stating that the user data related to the linked
+            // MoodleNet profiles has been removed.
+            $notificationtask = new tool_moodlenet\task\send_mnet_profiles_data_removed_notification();
+            core\task\manager::queue_adhoc_task($notificationtask);
+        }
+
+        upgrade_plugin_savepoint(true, 2020061504, 'tool', 'moodlenet');
+    }
+
     return true;
 }
