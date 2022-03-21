c0462feec8a6e5c2c47a5c26742a7c6865ed26a1
Mihail Geshoski <mihail@moodle.com>
2022-03-21 23:07:19 +0800

    MDL-74189 tool_moodlenet: Add upgrade step to remove irrelevant data
    Upgrade step to identify all existing cases where users have linked
    their MoodleNet profile on the site and remove the related data from
    the database. Due to some recent changes on the MoodleNet platform,
    this data is now irrelevant and can no longer be used to authenticate
    on the MoodleNet platform.

--- admin/tool/moodlenet/version.php
+++ admin/tool/moodlenet/version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'tool_moodlenet';
-$plugin->version    = 2021051700;
+$plugin->version    = 2021051701;
 $plugin->requires   = 2021051100;
 $plugin->maturity   = MATURITY_ALPHA;
