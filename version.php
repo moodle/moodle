34ce1463ccc657914f33c3ba34234f177038836a
Sara Arjona <sara@moodle.com>
2022-03-22 12:55:08 +0100

    on-demand release 4.0beta+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2022031800.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2022032200.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.0beta+ (Build: 20220318)'; // Human-friendly version name
+$release  = '4.0beta+ (Build: 20220322)'; // Human-friendly version name
 $branch   = '400';                     // This version's branch.
 $maturity = MATURITY_BETA;             // This version's maturity level.
