86f08a393f180864c474cdad23175d17bee7c1de
Jake Dallimore <jake@moodle.com>
2022-03-24 11:00:00 +0800

    MDL-74221 enrol_lti: db changes supporting draft registrations
    Summary:
    - Allows nulls for a number of fields meaning largely empty drafts can
    be stored.
    - Adds 'uniqueid', an id unique to each issuer which is intended
    to be used as a parameter in both the initiate login and dynamic
    registration urls provided to platforms, as a means to identify specific
    registrations for the issuer, in the absense of client_id (optional in
    the 1.3 spec).
    - Add 'status', used to denote draft or active registrations.

--- enrol/lti/version.php
+++ enrol/lti/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021052517; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2022031402; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2021052500; // Requires this Moodle version.
 $plugin->component = 'enrol_lti'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
