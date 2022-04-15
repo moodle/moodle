4ca5665aa0ef1adf53b0272889bc0bacbd8f6e47
Ilya Tregubov <ilya@moodle.com>
2022-04-15 16:03:34 +0600

    Moodle release 4.0rc3

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2022041300.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2022041500.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.0rc2 (Build: 20220412)'; // Human-friendly version name
+$release  = '4.0rc3 (Build: 20220415)'; // Human-friendly version name
 $branch   = '400';                     // This version's branch.
 $maturity = MATURITY_RC;             // This version's maturity level.
