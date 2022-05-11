96e2caeaa2bb9b4895a7166c1a3c345086a811ff
sam marshall <s.marshall@open.ac.uk>
2022-05-11 15:10:43 +0100

    MDL-74733 core_navigation: Secondary nav in plugins, change namespace
    Secondary nav previously had to be in my_plugin\local\views\secondary; this
    location continues to work but is deprecated. The new location is
    my_plugin\navigation\views\secondary.

--- lib/pagelib.php
+++ lib/pagelib.php
@@ -867,13 +867,22 @@ class moodle_page {
 
     /**
      * Returns the secondary navigation object
+     *
+     * @todo MDL-74939 Remove support for old 'local\views\secondary' class location
      * @return secondary
      */
     protected function magic_get_secondarynav() {
         if ($this->_secondarynav === null) {
             $class = 'core\navigation\views\secondary';
             // Try and load a custom class first.
-            if (class_exists("mod_{$this->activityname}\\local\\views\\secondary")) {
+            if (class_exists("mod_{$this->activityname}\\navigation\\views\\secondary")) {
+                $class = "mod_{$this->activityname}\\navigation\\views\\secondary";
+            } else if (class_exists("mod_{$this->activityname}\\local\\views\\secondary")) {
+                // For backwards compatibility, support the old location for this class (it was in a
+                // 'local' namespace which shouldn't be used for core APIs).
+                debugging("The class mod_{$this->activityname}}\\local\\views\\secondary uses a deprecated " .
+                        "namespace. Please move it to mod_{$this->activityname}\\navigation\\views\\secondary.",
+                        DEBUG_DEVELOPER);
                 $class = "mod_{$this->activityname}\\local\\views\\secondary";
             }
 
