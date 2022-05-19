ab42a7156341a86958681a4b7ff56989949625c5
anisa kusumadewi <kusumadewi@itc.rwth-aachen.de>
2022-05-19 16:01:57 +0200

    MDL-75766 core_h5p: Added index for pathnamehash to h5p table

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2022041904.13;              // 20220419      = branching date YYYYMMDD - do not modify!
+$version  = 2022041904.14;              // 20220419      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '4.0.4+ (Build: 20221104)'; // Human-friendly version name
