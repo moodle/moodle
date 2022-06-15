31b45356959c87996f35e1c1eb3fb438146417bf
Ferran Recio <ferran@moodle.com>
2022-06-15 12:45:31 +0200

    MDL-74716 core_my: Fix My course user tour for admins

--- admin/tool/usertours/version.php
+++ admin/tool/usertours/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022041900;            // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2022061600;            // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041200;            // Requires this Moodle version.
 $plugin->component = 'tool_usertours';      // Full name of the plugin (used for diagnostics).
