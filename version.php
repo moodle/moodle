ceb41588d203dbf966f594069fd4a84ac435e2bd
Jun Pataleta <jun@moodle.com>
2022-06-24 11:59:02 +0800

    weekly release 4.1dev

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2022061700.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2022062400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.1dev (Build: 20220617)'; // Human-friendly version name
+$release  = '4.1dev (Build: 20220624)'; // Human-friendly version name
 $branch   = '401';                     // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
