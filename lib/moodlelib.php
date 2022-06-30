4fd50e8cd95b11fb250898f1841d19b2f24eee5a
Andrew Nicols <andrew@nicols.co.uk>
2022-06-30 09:49:32 +0800

    MDL-74379 core: Update date_format_string to use x-os functions

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -2354,18 +2354,15 @@ function date_format_string($date, $format, $tz = 99) {
 
     date_default_timezone_set(core_date::get_user_timezone($tz));
 
-    $datearray = getdate($date);
-
-    if (nl_langinfo(AM_STR) == nl_langinfo(PM_STR)) {
-
+    if (strftime('%p', 0) === strftime('%p', HOURSECS * 18)) {
+        $datearray = getdate($date);
         $format = str_replace([
             '%P',
             '%p',
         ], [
             $datearray['hours'] < 12 ? get_string('am', 'langconfig') : get_string('pm', 'langconfig'),
             $datearray['hours'] < 12 ? get_string('amcaps', 'langconfig') : get_string('pmcaps', 'langconfig'),
-            ],
-        $format);
+        ], $format);
     }
 
     $datestring = strftime($format, $date);
