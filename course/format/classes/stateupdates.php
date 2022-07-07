437db87b3ea12a5bc0cf910de35094c7f2eb38f0
Simey Lameze <simey@moodle.com>
2022-07-07 10:37:46 +0800

    MDL-74925 course: use the correct 'remove' action

--- course/format/classes/stateupdates.php
+++ course/format/classes/stateupdates.php
@@ -135,6 +135,15 @@ class stateupdates implements JsonSerializable {
     }
 
     /**
+     * Add track about a section removed.
+     *
+     * @param int $sectionid The affected section id.
+     */
+    public function add_section_remove(int $sectionid): void {
+        $this->add_update('section', 'remove', (object)['id' => $sectionid]);
+    }
+
+    /**
      * Add track about a course module state update.
      *
      * @param int $cmid the affected course module id
@@ -185,10 +194,19 @@ class stateupdates implements JsonSerializable {
     }
 
     /**
+     * Add track about a course module removed.
+     *
+     * @param int $cmid the affected course module id
+     */
+    public function add_cm_remove(int $cmid): void {
+        $this->add_update('cm', 'remove', (object)['id' => $cmid]);
+    }
+
+    /**
      * Add a valid update message to the update list.
      *
      * @param string $name the update name
-     * @param string $action the update action (usually update, create, delete)
+     * @param string $action the update action (usually update, create, remove)
      * @param stdClass $fields the object fields
      */
     protected function add_update(string $name, string $action, stdClass $fields): void {
