f96173b0c80ef83c2bbe086a19bae96ed5c3dbc2
Derick Turner <derick@e-learndesign.co.uk>
2022-07-12 16:07:03 +0100

    IOMAD: IOMAD 3.10+ #1659

--- blocks/iomad_company_admin/classes/forms/course_group_users_form.php
+++ blocks/iomad_company_admin/classes/forms/course_group_users_form.php
@@ -172,7 +172,7 @@ class course_group_users_form extends moodleform {
 
             $mform->addElement('html', '
                       <input name="remove" id="remove" type="submit" value="' .
-                       get_string('remove') . '&nbsp;' . $ouput->rarrow() .
+                       get_string('remove') . '&nbsp;' . $output->rarrow() .
                        '" title="'.get_string('remove') .'" /></br>');
         }
 
