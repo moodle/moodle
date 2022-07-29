0fb5b7e233846b3ea2bb8a58cd2a52ba7c27e0b9
vochinh <vothuongchinh@gmail.com>
2022-07-29 10:28:15 +0700

    Revert "update wordimport"
    This reverts commit edc83ee121fd62898c8827799035fb6caba19622.

--- lib/editor/atto/plugins/wordimport/version.php
+++ lib/editor/atto/plugins/wordimport/version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021083100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020070603;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300;        // Requires Moodle 3.1 or higher.
 $plugin->component = 'atto_wordimport';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.4.2 (Build 2021083100)'; // Human readable version information.
-$plugin->dependencies = array('booktool_wordimport' => 2021083100);
+$plugin->release   = '1.4.0 (Build 2020070603)'; // Human readable version information.
+$plugin->dependencies = array('booktool_wordimport' => 2020070601);
