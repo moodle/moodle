7f63314383ac9d45d797c6331ca62758fbfe4aff
Brendan Heywood <brendan@catalyst-au.net>
2022-08-04 21:16:18 +1000

    MDL-75375 cache: Fix file store path cleaning

--- cache/stores/file/addinstanceform.php
+++ cache/stores/file/addinstanceform.php
@@ -43,7 +43,7 @@ class cachestore_file_addinstance_form extends cachestore_addinstance_form {
         $form = $this->_form;
 
         $form->addElement('text', 'path', get_string('path', 'cachestore_file'));
-        $form->setType('path', PARAM_SAFEPATH);
+        $form->setType('path', PARAM_RAW);
         $form->addHelpButton('path', 'path', 'cachestore_file');
 
         $form->addElement('checkbox', 'autocreate', get_string('autocreate', 'cachestore_file'));
