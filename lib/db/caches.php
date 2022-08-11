ea01fb24273de4011694d91e34f86016d319686e
Mark Johnson <mark.johnson@catalyst-eu.net>
2022-08-11 09:13:41 +0100

    MDL-67020 Cache: Make local caching work for coursemodinfo

--- lib/db/caches.php
+++ lib/db/caches.php
@@ -251,6 +251,7 @@ $definitions = array(
         'mode' => cache_store::MODE_APPLICATION,
         'simplekeys' => true,
         'canuselocalstore' => true,
+        'requirelockingbeforewrite' => true
     ),
     // This is the session user selections cache.
     // It's a special cache that is used to record user selections that should persist for the lifetime of the session.
