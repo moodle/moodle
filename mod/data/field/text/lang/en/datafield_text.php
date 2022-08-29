7d2fada8b028b36882507866553c5b6762e1ba2d
Helen Foster <helen@moodle.org>
2022-08-29 20:51:47 +0200

    MDL-75553 lang: Import fixed English strings (en_fix)

--- mod/data/field/text/lang/en/datafield_text.php
+++ mod/data/field/text/lang/en/datafield_text.php
@@ -24,6 +24,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Text input';
-$string['fieldtypelabel'] = 'Text field';
-$string['privacy:metadata'] = 'The Text field component does not store any personal data, it uses tables defined in mod_data.';
+$string['pluginname'] = 'Short text';
+$string['fieldtypelabel'] = 'Short text';
+$string['privacy:metadata'] = 'The Short text field component doesn\'t store any personal data; it uses tables defined in mod_data.';
