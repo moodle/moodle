6f01131eeb199a48145c579a1ef142f9556511af
Jun Pataleta <jun@moodle.com>
2022-08-30 17:20:22 +0800

    MDL-70480 mod_assign: Apply proper upgrade savepoint version

--- mod/assign/db/upgrade.php
+++ mod/assign/db/upgrade.php
@@ -115,7 +115,7 @@ function xmldb_assign_upgrade($oldversion) {
     // Automatically generated Moodle v4.0.0 release upgrade line.
     // Put any upgrade step following this.
 
-    if ($oldversion < 2022071300) {
+    if ($oldversion < 2022041901) {
         // The most recent assign submission should always have latest = 1, we want to find all records where this is not the case.
         // Find the records with the maximum timecreated for each assign and user combination where latest is also 0.
         $sqluser = "SELECT s.id
@@ -150,7 +150,7 @@ function xmldb_assign_upgrade($oldversion) {
         }
 
         // Assignment savepoint reached.
-        upgrade_mod_savepoint(true, 2022071300, 'assign');
+        upgrade_mod_savepoint(true, 2022041901, 'assign');
     }
     return true;
 }
