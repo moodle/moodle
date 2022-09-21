1d8d5874ae14f8fabb05b5669c562a81b995280e
iankohhhh <iankoh_@outlook.com>
2022-09-21 13:29:27 +0800

    Update version of moove

--- theme/moove/version.php
+++ theme/moove/version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_moove';
 
 // This is the version of the plugin.
-$plugin->version = 2022062005;
+$plugin->version = 2022062007;
 
 // This is the named version.
 $plugin->release = '4.0.5';
