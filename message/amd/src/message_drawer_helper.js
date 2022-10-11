dc50ea7d3a4d3f080d649d419996f9a62a290126
Andrew Nicols <andrew@nicols.co.uk>
2022-10-11 11:04:13 +0800

    MDL-75936 core_message: Migrate message_drawer_helper to ESM

--- message/amd/src/message_drawer_helper.js
+++ message/amd/src/message_drawer_helper.js
@@ -20,104 +20,76 @@
  * @copyright  2018 Ryan Wyllie <ryan@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-define(
-[
-    'core/pubsub',
-    'core_message/message_drawer_events'
-],
-function(
-    PubSub,
-    MessageDrawerEvents
-) {
 
-    /** @property {boolean} Whether the drawer is ready or not */
-    var drawerMarkedReady = false;
+import {publish, subscribe} from 'core/pubsub';
+import MessageDrawerEvents from 'core_message/message_drawer_events';
 
-    /**
-     * Trigger an event to create a new conversation in the message drawer.
-     *
-     * @param {object} args
-     * @param {Number} args.userId The user id to start a conversation.
-     */
-    var createConversationWithUser = function(args) {
-        waitForDrawerToLoad().then(function () {
-            PubSub.publish(MessageDrawerEvents.CREATE_CONVERSATION_WITH_USER, args);
-            return;
-        }).catch();
-    };
+/** @property {boolean} Whether the drawer is ready or not */
+let drawerMarkedReady = false;
 
-    /**
-     * Trigger an event to hide the message drawer.
-     */
-    var hide = function() {
-        waitForDrawerToLoad().then(function () {
-            PubSub.publish(MessageDrawerEvents.HIDE);
-            return;
-        }).catch();
-    };
+/**
+ * Trigger an event to create a new conversation in the message drawer.
+ *
+ * @param {object} args
+ * @param {Number} args.userId The user id to start a conversation.
+ */
+export const createConversationWithUser = async(args) => {
+    await waitForDrawerToLoad();
+    publish(MessageDrawerEvents.CREATE_CONVERSATION_WITH_USER, args);
+};
 
-    /**
-     * Trigger an event to show the message drawer.
-     */
-    var show = function() {
-        waitForDrawerToLoad().then(function () {
-            PubSub.publish(MessageDrawerEvents.SHOW);
-            return;
-        }).catch();
-    };
+/**
+ * Trigger an event to hide the message drawer.
+ */
+export const hide = async() => {
+    await waitForDrawerToLoad();
+    publish(MessageDrawerEvents.HIDE);
+};
 
-    /**
-     * Trigger an event to show the given conversation.
-     *
-     * @param {object} args
-     * @param {int} args.conversationId Id for the conversation to show.
-     */
-    var showConversation = function(args) {
-        waitForDrawerToLoad().then(function () {
-            PubSub.publish(MessageDrawerEvents.SHOW_CONVERSATION, args);
-            return;
-        }).catch();
-    };
+/**
+ * Trigger an event to show the message drawer.
+ */
+export const show = async() => {
+    await waitForDrawerToLoad();
+    publish(MessageDrawerEvents.SHOW);
+};
 
-    /**
-     * Trigger an event to show messaging settings.
-     */
-    var showSettings = function() {
-        waitForDrawerToLoad().then(function() {
-            PubSub.publish(MessageDrawerEvents.SHOW_SETTINGS);
-            return;
-        }).catch();
-    };
+/**
+ * Trigger an event to show the given conversation.
+ *
+ * @param {object} args
+ * @param {int} args.conversationId Id for the conversation to show.
+ */
+export const showConversation = async(args) => {
+    await waitForDrawerToLoad();
+    publish(MessageDrawerEvents.SHOW_CONVERSATION, args);
+};
 
-    /**
-     * Helper to wait for the drawer to be ready before performing an action.
-     *
-     * @returns {Promise<void>}
-     */
-    var waitForDrawerToLoad = function() {
-        return new Promise(function(resolve) {
-            if (drawerMarkedReady) {
-                resolve();
-            } else {
-                PubSub.subscribe(MessageDrawerEvents.READY, resolve);
-            }
-        });
-    };
+/**
+ * Trigger an event to show messaging settings.
+ */
+export const showSettings = async() => {
+    await waitForDrawerToLoad();
+    publish(MessageDrawerEvents.SHOW_SETTINGS);
+};
 
-    /**
-     * Helper to allow the drawer to mark itself as ready.
-     */
-    var markDrawerReady = function() {
-        drawerMarkedReady = true;
-        PubSub.publish(MessageDrawerEvents.READY);
-    };
-
-    return {
-        createConversationWithUser: createConversationWithUser,
-        hide: hide,
-        show: show,
-        showConversation: showConversation,
-        showSettings: showSettings,
-        markDrawerReady: markDrawerReady,
-    };
+/**
+ * Helper to wait for the drawer to be ready before performing an action.
+ *
+ * @returns {Promise<void>}
+ */
+export const waitForDrawerToLoad = () => new Promise((resolve) => {
+    if (drawerMarkedReady) {
+        resolve();
+    } else {
+        subscribe(MessageDrawerEvents.READY, resolve);
+    }
 });
+
+/**
+ * Helper to allow the drawer to mark itself as ready.
+ */
+export const markDrawerReady = () => {
+    drawerMarkedReady = true;
+    publish(MessageDrawerEvents.READY);
+};
