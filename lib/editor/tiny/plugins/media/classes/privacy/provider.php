75ebfb703b0d577725a82fa48b634c83093b6ae9
Andrew Nicols <andrew@nicols.co.uk>
2022-10-18 09:50:54 +0800

    MDL-75259 tiny_media: Tidy up strings
    Part of MDL-75966

--- lib/editor/tiny/plugins/media/classes/privacy/provider.php
+++ lib/editor/tiny/plugins/media/classes/privacy/provider.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace tiny_media\privacy;
+
 /**
  * Privacy Subsystem implementation for the media plugin for TinyMCE.
  *
@@ -21,9 +23,6 @@
  * @copyright 2022 Andrew Lyons <andrew@nicols.co.uk>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-namespace tiny_media\privacy;
-
 class provider implements \core_privacy\local\metadata\null_provider {
     public static function get_reason(): string {
         return 'privacy:metadata';
