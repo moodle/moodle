3f17ea00e7a3dbab9f19b7faae86e8db7c80b1d1
Jake Dallimore <jake@moodle.com>
2022-10-20 11:08:39 +0800

    MDL-61880 core: don't clear issuer fields not present in form data
    In cases where we have an id, load the persistent and then set new data
    before saving. This ensures we don't delete fields which are missing in
    the form data.

--- lib/classes/oauth2/api.php
+++ lib/classes/oauth2/api.php
@@ -327,7 +327,12 @@ class api {
      */
     protected static function create_or_update_issuer($data, bool $create): issuer {
         require_capability('moodle/site:config', context_system::instance());
-        $issuer = new issuer(0, $data);
+        $issuer = new issuer($data->id ?? 0, $data);
+        if (!empty($data->id)) {
+            foreach ($data as $property => $value) {
+                $issuer->set($property, $value);
+            }
+        }
 
         // Will throw exceptions on validation failures.
         if ($create) {
