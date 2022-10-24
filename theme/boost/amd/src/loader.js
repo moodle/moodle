fce11bf6aa92de1b90a5c3389be66f0fb7f7c58b
Mihail Geshoski <mihail@moodle.com>
2022-10-25 00:26:39 +0800

    MDL-75957 core: Prevent repositioning dropdowns along the y-axis
    Disables filipping the dropdowns up or dynamically repositioning
    them along the y-axis to prevent them getting hidden behind the
    navbar or them covering the trigger element.

--- theme/boost/amd/src/loader.js
+++ theme/boost/amd/src/loader.js
@@ -104,8 +104,33 @@ enablePopovers();
 // Enable all tooltips.
 enableTooltips();
 
-// Disables flipping the dropdowns up and getting hidden behind the navbar.
-$.fn.dropdown.Constructor.Default.flip = false;
+// Disables flipping the dropdowns up or dynamically repositioning them along the Y-axis (based on the viewport)
+// to prevent the dropdowns getting hidden behind the navbar or them covering the trigger element.
+$.fn.dropdown.Constructor.Default.popperConfig = {
+    modifiers: {
+        flip: {
+            enabled: false,
+        },
+        storeTopPosition: {
+            enabled: true,
+            // eslint-disable-next-line no-unused-vars
+            fn(data, options) {
+                data.storedTop = data.offsets.popper.top;
+                return data;
+            },
+            order: 299
+        },
+        restoreTopPosition: {
+            enabled: true,
+            // eslint-disable-next-line no-unused-vars
+            fn(data, options) {
+                data.offsets.popper.top = data.storedTop;
+                return data;
+            },
+            order: 301
+        }
+    },
+};
 
 pendingPromise.resolve();
 
