1a35908e1f8a2d620ec09e74330b3dde325688c8
Jun Pataleta <jun@moodle.com>
2022-11-07 14:40:30 +0800

    MDL-50935 atto_equation: Add upgrade step to fix existing sites

--- lib/editor/atto/plugins/equation/db/upgrade.php
+++ lib/editor/atto/plugins/equation/db/upgrade.php
@@ -47,5 +47,15 @@ function xmldb_atto_equation_upgrade($oldversion) {
     // Automatically generated Moodle v4.0.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2022041901) {
+        $oldmatrix = '\left| \begin{matrix} a_1 & a_2 \\ a_3 & a_4 \end{matrix} \right|';
+        $fixedmatrix = '\left| \begin{matrix} a_1 & a_2 \\\\ a_3 & a_4 \end{matrix} \right|';
+        $config = get_config('atto_equation', 'librarygroup4');
+        $newdefault = str_replace($oldmatrix, $fixedmatrix, $config);
+        set_config('librarygroup4', $newdefault, 'atto_equation');
+        // Atto equation savepoint reached.
+        upgrade_plugin_savepoint(true, 2022041901, 'atto', 'equation');
+    }
+
     return true;
 }
