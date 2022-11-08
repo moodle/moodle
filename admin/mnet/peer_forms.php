d2dd970b19593fb1ecd11d67acd688bf0e45fb9d
Jake Dallimore <jake@moodle.com>
2022-11-08 17:01:40 +0800

    MDL-63037 mnet: fix hostexists string usage

--- admin/mnet/peer_forms.php
+++ admin/mnet/peer_forms.php
@@ -59,9 +59,8 @@ class mnet_simple_host_form extends moodleform {
             $wwwroot = 'http://'.$wwwroot;
         }
         if ($host = $DB->get_record('mnet_host', array('wwwroot' => $wwwroot))) {
-            global $CFG;
-            return array('wwwroot' => get_string('hostexists', 'mnet',
-                new moodle_url('/admin/mnet/peers.php', array('hostid' => $host->id))));
+            $str = get_string('hostexists', 'mnet', (new moodle_url('/admin/mnet/peers.php', ['hostid' => $host->id]))->out());
+            return array('wwwroot' => $str);
         }
         return array();
     }
