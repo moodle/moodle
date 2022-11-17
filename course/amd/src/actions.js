63a7c58ee345090ef7ec6d7b8939b1c0e9ed39c1
Ferran Recio <ferran@moodle.com>
2022-11-17 12:02:08 +0100

    MDL-76188 core_courseformat: migrate highlight to reactive component
    The highlight and unhighlight sections applies only to the format_topics
    plugins. With this commit all the related code is migrated to reactive
    component and format_topics extend the current course editor to add all
    the logic

--- course/amd/src/actions.js
+++ course/amd/src/actions.js
@@ -60,7 +60,7 @@ define(
         // Meanwhile, we filter the migrated actions.
         const componentActions = [
             'moveSection', 'moveCm', 'addSection', 'deleteSection', 'sectionHide', 'sectionShow',
-            'cmHide', 'cmShow', 'cmStealth',
+            'cmHide', 'cmShow', 'cmStealth', 'sectionHighlight', 'sectionUnhighlight',
         ];
 
         // The course reactive instance.
