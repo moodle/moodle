aafbdf7a6bd587b167f06590ea78e3f1b2e5b3af
Stephan Robotta <stephan.robotta@bfh.ch>
2022-12-01 13:05:28 +0100

    MDL-43195 license: upgrading cc core licenses to version 4.0
    The short name of the cc licenses are trailed with a suffix containing
    the version number (current 3.0 and 4.0). The old cc* licenses become
    the new cc-*-3.0 licenses and are disables, because the new cc*-4.0
    licenses are the current ones.

--- lang/en/license.php
+++ lang/en/license.php
@@ -21,17 +21,26 @@
  * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 // Core licenses.
 $string['allrightsreserved'] = 'All rights reserved';
-$string['cc'] = 'Creative Commons';
-$string['cc-nc'] = 'Creative Commons - No Commercial';
-$string['cc-nc-nd'] = 'Creative Commons - No Commercial NoDerivs';
-$string['cc-nc-sa'] = 'Creative Commons - No Commercial ShareAlike';
-$string['cc-nd'] = 'Creative Commons - NoDerivs';
-$string['cc-sa'] = 'Creative Commons - ShareAlike';
 $string['public'] = 'Public domain';
 $string['unknown'] = 'Licence not specified';
+
+// Old cc-* 3.0 licenses that should be disabled and replaces by the 4.0 licenses from above.
+$string['cc'] = 'Creative Commons - 3.0 International';
+$string['cc-nc'] = 'Creative Commons - NonCommercial 3.0 International';
+$string['cc-nc-nd'] = 'Creative Commons - NonCommercial-NoDerivatives 3.0 International';
+$string['cc-nc-sa'] = 'Creative Commons - NonCommercial-ShareAlike 3.0 International';
+$string['cc-nd'] = 'Creative Commons - NoDerivatives 3.0 International';
+$string['cc-sa'] = 'Creative Commons - ShareAlike 3.0 International';
+
+// The new 4.0 licenses.
+$string['cc-4.0'] = 'Creative Commons - 4.0 International';
+$string['cc-nc-4.0'] = 'Creative Commons - NonCommercial 4.0 International';
+$string['cc-nc-nd-4.0'] = 'Creative Commons - NonCommercial-NoDerivatives 4.0 International';
+$string['cc-nc-sa-4.0'] = 'Creative Commons - NonCommercial-ShareAlike 4.0 International';
+$string['cc-nd-4.0'] = 'Creative Commons - NoDerivatives 4.0 International';
+$string['cc-sa-4.0'] = 'Creative Commons - ShareAlike 4.0 International';
 
 // Error messages.
 $string['cannotdeletecore'] = 'Cannot delete a standard licence';
