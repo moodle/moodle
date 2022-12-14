d770ebd01b3f87f47eaa6c55244400401a617d3a
Ferran Recio <ferran@moodle.com>
2022-12-14 10:56:44 +0100

    MDL-76430 core_courseformat: implement fragment to course format

--- course/format/amd/src/local/content.js
+++ course/format/amd/src/local/content.js
@@ -24,11 +24,12 @@
 
 import {BaseComponent} from 'core/reactive';
 import {getCurrentCourseEditor} from 'core_courseformat/courseeditor';
+import Config from 'core/config';
 import inplaceeditable from 'core/inplace_editable';
 import Section from 'core_courseformat/local/content/section';
 import CmItem from 'core_courseformat/local/content/section/cmitem';
-// Course actions is needed for actions that are not migrated to components.
-import courseActions from 'core_course/actions';
+import Fragment from 'core/fragment';
+import Templates from 'core/templates';
 import DispatchActions from 'core_courseformat/local/content/actions';
 import * as CourseEvents from 'core_course/events';
 // The jQuery module is only used for interacting with Boostrap 4. It can we removed when MDL-71979 is integrated.
@@ -219,6 +220,7 @@ export default class Component extends BaseComponent {
             // State changes that require to reload some course modules.
             {watch: `cm.visible:updated`, handler: this._reloadCm},
             {watch: `cm.stealth:updated`, handler: this._reloadCm},
+            {watch: `cm.sectionid:updated`, handler: this._reloadCm},
             // Update section number and title.
             {watch: `section.number:updated`, handler: this._refreshSectionNumber},
             // Collapse and expand sections.
@@ -229,9 +231,6 @@ export default class Component extends BaseComponent {
             {watch: `section.cmlist:updated`, handler: this._refreshSectionCmlist},
             // Reindex sections and cms.
             {watch: `state:updated`, handler: this._indexContents},
-            // State changes thaty require to reload course modules.
-            {watch: `cm.visible:updated`, handler: this._reloadCm},
-            {watch: `cm.sectionid:updated`, handler: this._reloadCm},
         ];
     }
 
@@ -506,8 +505,18 @@ export default class Component extends BaseComponent {
         const pendingReload = new Pending(`courseformat/content:reloadCm_${element.id}`);
         const cmitem = this.getElement(this.selectors.CM, element.id);
         if (cmitem) {
-            const promise = courseActions.refreshModule(cmitem, element.id);
-            promise.then(() => {
+            const promise = Fragment.loadFragment(
+                'core_courseformat',
+                'cmitem',
+                Config.courseContextId,
+                {
+                    id: element.id,
+                    courseid: Config.courseId,
+                    sr: this.reactive.sectionReturn ?? 0,
+                }
+            );
+            promise.then((html, js) => {
+                Templates.replaceNode(cmitem, html, js);
                 this._indexContents();
                 pendingReload.resolve();
                 return;
@@ -528,8 +537,18 @@ export default class Component extends BaseComponent {
         const pendingReload = new Pending(`courseformat/content:reloadSection_${element.id}`);
         const sectionitem = this.getElement(this.selectors.SECTION, element.id);
         if (sectionitem) {
-            const promise = courseActions.refreshSection(sectionitem, element.id);
-            promise.then(() => {
+            const promise = Fragment.loadFragment(
+                'core_courseformat',
+                'section',
+                Config.courseContextId,
+                {
+                    id: element.id,
+                    courseid: Config.courseId,
+                    sr: this.reactive.sectionReturn ?? 0,
+                }
+            );
+            promise.then((html, js) => {
+                Templates.replaceNode(sectionitem, html, js);
                 this._indexContents();
                 pendingReload.resolve();
                 return;
