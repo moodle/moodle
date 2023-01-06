745080671b5a22bc991fb09e58cc24dc9a084197
Andrew Nicols <andrew@nicols.co.uk>
2023-01-06 21:20:20 +0800

    MDL-76362 question: Refactor question number unit tests

---
+++ question/tests/coverage.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Coverage information for the core_question.
+ *
+ * @copyright  2022 Andrew Nicols <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+return new class extends phpunit_coverage_info {
+    /** @var array The list of files relative to the plugin root to include in coverage generation. */
+    protected $includelistfiles = [
+        'category_class.php',
+        'category_form.php',
+        'editlib.php',
+        'export_form.php',
+        'format.php',
+        'import_form.php',
+        'lib.php',
+        'move_form.php',
+        'previewlib.php',
+        'renderer.php',
+        'upgrade.php',
+    ];
+};
