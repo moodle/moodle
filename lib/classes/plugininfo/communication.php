ec5663f1a4876f53c7490a349ffea0ab6dccdfa1
Huong Nguyen <huongnv13@gmail.com>
2023-01-10 09:32:20 +0700

    MDL-77576 core: Register a new communication plugin-type
    A new communication plugin type added to core to implement
    the communication provider features.
    
    Original implemented as MDL-76699
    
    Co-Authored-By: Huong Nguyen <huongnv13@gmail.com>

---
+++ lib/classes/plugininfo/communication.php
@@ -0,0 +1,116 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core\plugininfo;
+
+use admin_settingpage;
+use core_plugin_manager;
+use moodle_url;
+
+/**
+ * Class for communication provider.
+ *
+ * @package    core
+ * @copyright  2023 Huong Nguyen <huongnv13@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class communication extends base {
+
+    public static function get_manage_url(): moodle_url {
+        return new moodle_url('/admin/settings.php', ['section' => 'managecommunicationproviders']);
+    }
+
+    public function get_settings_section_name(): string {
+        return $this->type . '_' . $this->name;
+    }
+
+    public static function enable_plugin(string $pluginname, int $enabled): bool {
+        $haschanged = false;
+
+        $plugin = 'communication_' . $pluginname;
+        $oldvalue = get_config($plugin, 'disabled');
+        $disabled = !$enabled;
+        // Only set value if there is no config setting or if the value is different from the previous one.
+        if ($oldvalue == false && $disabled) {
+            set_config('disabled', $disabled, $plugin);
+            $haschanged = true;
+        } else if ($oldvalue != false && !$disabled) {
+            unset_config('disabled', $plugin);
+            $haschanged = true;
+        }
+
+        if ($haschanged) {
+            add_to_config_log('disabled', $oldvalue, $disabled, $plugin);
+            core_plugin_manager::reset_caches();
+        }
+
+        return $haschanged;
+    }
+
+    public static function get_enabled_plugins(): ?array {
+        $pluginmanager = core_plugin_manager::instance();
+        $plugins = $pluginmanager->get_installed_plugins('communication');
+
+        if (!$plugins) {
+            return [];
+        }
+
+        $plugins = array_keys($plugins);
+
+        // Filter to return only enabled plugins.
+        $enabled = [];
+        foreach ($plugins as $plugin) {
+            $disabled = get_config('communication_' . $plugin, 'disabled');
+            if (empty($disabled)) {
+                $enabled[$plugin] = $plugin;
+            }
+        }
+        return $enabled;
+    }
+
+    public function load_settings(\part_of_admin_tree $adminroot, $parentnodename, $hassiteconfig) {
+        global $CFG, $USER, $DB, $OUTPUT, $PAGE; // In case settings.php wants to refer to them.
+        $ADMIN = $adminroot; // May be used in settings.php.
+        $plugininfo = $this;      // Also can be used inside settings.php.
+
+        if (!$this->is_installed_and_upgraded()) {
+            return;
+        }
+
+        if (!$hassiteconfig) {
+            return;
+        }
+
+        $section = $this->get_settings_section_name();
+        $settings = null;
+        if (file_exists($this->full_path('settings.php'))) {
+            $settings = new admin_settingpage($section, $this->displayname,
+                'moodle/site:config', $this->is_enabled() === false);
+            include($this->full_path('settings.php')); // This may also set $settings to null.
+        }
+        if ($settings) {
+            $ADMIN->add($parentnodename, $settings);
+        }
+    }
+
+    public function is_uninstall_allowed(): bool {
+        if (in_array($this->name, \core_plugin_manager::standard_plugins_list('communication'))) {
+            return false;
+        }
+        return true;
+    }
+
+}
