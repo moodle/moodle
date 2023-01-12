71b3f9c39f774123dfae2adf401e889ee15b7484
Jun Pataleta <jun@moodle.com>
2023-01-12 15:17:35 +0800

    MDL-76649 mod_quiz: Use correct quizid attribute from attempt

--- mod/quiz/report/statistics/classes/task/recalculate.php
+++ mod/quiz/report/statistics/classes/task/recalculate.php
@@ -82,7 +82,7 @@ class recalculate extends \core\task\scheduled_task {
             mtrace("  Examining quiz '$attempt->quizname' ($attempt->quizid) in course " .
                     "$attempt->courseshortname ($attempt->courseid) with most recent attempt at " .
                     userdate($attempt->mostrecentattempttime, $dateformat) . ".");
-            $quizobj = quiz_settings::create($attempt->quiz);
+            $quizobj = quiz_settings::create($attempt->quizid);
             $quiz = $quizobj->get_quiz();
             // Hash code for question stats option in question bank.
             $qubaids = quiz_statistics_qubaids_condition($quiz->id, new \core\dml\sql_join(), $quiz->grademethod);
