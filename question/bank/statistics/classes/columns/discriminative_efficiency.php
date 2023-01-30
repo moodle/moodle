d1fcfd1de299a6cd1432caba4623d33a011ea390
Tim Hunt <T.J.Hunt@open.ac.uk>
2023-01-30 12:31:07 +0000

    MDL-75576 qbank_statistics: remove copy-pasted PHPdoc comments
    These methods are already correctly documented on the base class.

--- question/bank/statistics/classes/columns/discriminative_efficiency.php
+++ question/bank/statistics/classes/columns/discriminative_efficiency.php
@@ -18,6 +18,7 @@ namespace qbank_statistics\columns;
 
 use core_question\local\bank\column_base;
 use qbank_statistics\helper;
+
 /**
  * This column show the average discriminative efficiency for this question.
  *
@@ -28,11 +29,6 @@ use qbank_statistics\helper;
  */
 class discriminative_efficiency extends column_base {
 
-    /**
-     * Title for this column.
-     *
-     * @return string column title
-     */
     public function get_title(): string {
         return get_string('discriminative_efficiency', 'qbank_statistics');
     }
@@ -41,20 +37,10 @@ class discriminative_efficiency extends column_base {
         return new \help_icon('discriminative_efficiency', 'qbank_statistics');
     }
 
-    /**
-     * Column name.
-     *
-     * @return string column name
-     */
     public function get_name(): string {
         return 'discriminative_efficiency';
     }
 
-    /**
-     * Output the contents of this column.
-     * @param object $question the row from the $question table, augmented with extra information.
-     * @param string $rowclasses CSS class names that should be applied to this row of output.
-     */
     protected function display_content($question, $rowclasses) {
         global $PAGE;
         // Average discriminative efficiency per quiz.
