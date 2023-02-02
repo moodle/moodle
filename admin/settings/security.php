23318ebb5ecb6bf13ee19caeedadf99e9c451e5c
David Woloszyn <david.woloszyn@moodle.com>
2023-02-02 15:16:17 +1100

    MDL-76755 admin: Blocked hosts defaults improved

--- admin/settings/security.php
+++ admin/settings/security.php
@@ -169,7 +169,7 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
 
     // Settings elements used by the \core\files\curl_security_helper class.
     $blockedhostsdefault = [
-        '127.0.0.1',
+        '127.0.0.0/8',
         '192.168.0.0/16',
         '10.0.0.0/8',
         '172.16.0.0/12',
