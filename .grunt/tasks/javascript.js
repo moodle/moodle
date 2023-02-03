d01eba812bfea3f9e9d22c86bf2b7586eb7e2a40
Andrew Nicols <andrew@nicols.co.uk>
2023-02-03 10:54:57 +0800

    MDL-75012 js: Remove grunt-contrib-uglify
    We stopped using this when we updated our processes to support ESMs in
    MDL-62497. Removal of the dependency was missed at the time.

--- .grunt/tasks/javascript.js
+++ .grunt/tasks/javascript.js
@@ -21,7 +21,7 @@
  */
 
 /**
- * Function to generate the destination for the uglify task
+ * Function to generate the destination for the minification task
  * (e.g. build/file.min.js). This function will be passed to
  * the rename property of files array when building dynamically:
  * http://gruntjs.com/configuring-tasks#building-the-files-object-dynamically
@@ -61,7 +61,6 @@ module.exports = grunt => {
     grunt.registerTask('js', ['amd', 'yui']);
 
     // Register NPM tasks.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-rollup');
 
