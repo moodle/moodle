dfb3de1eb3de83e3aa16f9366670f248cafe34f4
Tim Hunt <T.J.Hunt@open.ac.uk>
2023-02-27 12:59:52 +0000

    MDL-74610 quiz: multiple grades - UI mockup and nav

---
+++ mod/quiz/classes/output/edit_grading_page.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_quiz\output;
+
+use mod_quiz\structure;
+use renderable;
+use renderer_base;
+use templatable;
+
+/**
+ * Represents the page where teachers can set up additional grade items.
+ *
+ * @package   mod_quiz
+ * @category  output
+ * @copyright 2023 The Open University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class edit_grading_page implements renderable, templatable {
+
+    /**
+     * Constructor.
+     *
+     * @param structure $structure
+     */
+    public function __construct(
+
+        /** @var structure structure of the quiz for which to display the grade edit page. */
+        protected readonly structure $structure,
+
+    ) {
+    }
+
+    public function export_for_template(renderer_base $output) {
+        return [];
+    }
+}
--- mod/quiz/edit.php
+++ mod/quiz/edit.php
@@ -40,6 +40,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use mod_quiz\output\edit_nav_actions;
 use mod_quiz\quiz_settings;
 
 require_once(__DIR__ . '/../../config.php');
@@ -186,7 +187,12 @@ if ($message = optional_param('message', '', PARAM_TEXT)) {
     core\notification::add($message, core\notification::SUCCESS);
 }
 
-echo $OUTPUT->header();
+$tertiarynav = new edit_nav_actions($cmid, edit_nav_actions::SUMMARY);
+
+// Do output.
+echo $output->header();
+echo $output->render($tertiarynav);
+
 // Initialise the JavaScript.
 $quizeditconfig = new stdClass();
 $quizeditconfig->url = $thispageurl->out(true, ['qbanktool' => '0']);
@@ -211,4 +217,4 @@ echo $output->edit_page($quizobj, $structure, $contexts, $thispageurl, $pagevars
 // Questions wrapper end.
 echo html_writer::end_tag('div');
 
-echo $OUTPUT->footer();
+echo $output->footer();
