02716c8ea6dbd2215c3799bfbf8c9c6a700051cb
Daniel Poggenpohl <daniel.poggenpohl@fernuni-hagen.de>
2023-03-20 08:50:41 +0100

    MDL-77657 theme: Fix get_pre_scss_code load order
    This led to problems with grandchild themes,
    depending on their direct parent's prescsscallback.

--- lib/outputlib.php
+++ lib/outputlib.php
@@ -1631,7 +1631,7 @@ class theme_config {
 
         // Getting all the candidate functions.
         $candidates = array();
-        foreach ($this->parent_configs as $parent_config) {
+        foreach (array_reverse($this->parent_configs) as $parent_config) {
             if (!isset($parent_config->prescsscallback)) {
                 continue;
             }
