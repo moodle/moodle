3faf1c198944ed312e84f1633c5d3530d293f009
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2023-03-22 19:13:11 +0100

    MDL-77714 general: Remove mentions to Travis CI in a few places
    Not critical to have, but better don't mention it. Note that all
    the cases need to stay because there may be other environments
    where the same problems are reproducible.
    
    Only exception is the CURL_SSLVERSION_TLSv1_2 constant, that
    can be safely removed because it's available since PHP 5.5.19.

--- lib/weblib.php
+++ lib/weblib.php
@@ -1394,9 +1394,9 @@ function format_text($text, $format = FORMAT_MOODLE, $options = null, $courseidd
         }
 
         // This regex is nasty and I don't like it. The correct way to solve this is by loading the HTML like so:
-        // $domdoc->loadHTML($text, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD); however it seems like the libxml
-        // version that travis uses doesn't work properly and ends up leaving <html><body>, so I'm forced to use
-        // this regex to remove those tags.
+        // $domdoc->loadHTML($text, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD); however it seems like some libxml
+        // versions don't work properly and end up leaving <html><body>, so I'm forced to use
+        // this regex to remove those tags as a preventive measure.
         $text = trim(preg_replace('~<(?:!DOCTYPE|/?(?:html|body))[^>]*>\s*~i', '', $domdoc->saveHTML($domdoc->documentElement)));
     }
 
