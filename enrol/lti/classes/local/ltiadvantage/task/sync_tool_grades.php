d9e182b5c62478aee31da3dc410b90e1cee7fadb
ishatalkin <ishatalkin@custis.ru>
2023-03-24 15:10:39 +0300

    MDL-76557 enrol_lti: sync_grades accepts status codes 200,201,202,204

--- enrol/lti/classes/local/ltiadvantage/task/sync_tool_grades.php
+++ enrol/lti/classes/local/ltiadvantage/task/sync_tool_grades.php
@@ -185,7 +185,8 @@ class sync_tool_grades extends adhoc_task {
                         continue;
                     }
 
-                    if ($response['status'] == 200) {
+                    $successresponses = [200, 201, 202, 204];
+                    if (in_array($response['status'], $successresponses)) {
                         $user->set_lastgrade(grade_floatval($grade));
                         $syncedusergrades[$user->get_id()] = $user;
                         mtrace("Success - The grade '$floatgrade' $mtracecontent was sent.");
