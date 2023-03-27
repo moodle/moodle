3ffc2ed91433247163609f70c18ec9dbb1b0b024
Simey Lameze <simey@moodle.com>
2023-03-27 09:37:37 +0800

    MDL-77733 behat: enable Axe accessibility tests by default

--- admin/tool/behat/cli/util.php
+++ admin/tool/behat/cli/util.php
@@ -54,7 +54,8 @@ list($options, $unrecognized) = cli_get_params(
         'torun'       => 0,
         'optimize-runs' => '',
         'add-core-features-to-theme' => false,
-        'axe'         => false,
+        'axe'      => '',
+        'no-axe'      => false,
     ),
     array(
         'h' => 'help',
@@ -70,7 +71,7 @@ $help = "
 Behat utilities to manage the test environment
 
 Usage:
-  php util.php [--install|--drop|--enable|--disable|--diag|--updatesteps|--axe|--help] [--parallel=value [--maxruns=value]]
+  php util.php [--install|--drop|--enable|--disable|--diag|--updatesteps|--no-axe|--help] [--parallel=value [--maxruns=value]]
 
 Options:
 --install      Installs the test environment for acceptance tests
@@ -79,7 +80,7 @@ Options:
 --disable      Disables test environment
 --diag         Get behat test environment status code
 --updatesteps  Update feature step file.
---axe          Include axe accessibility tests
+--no-axe       Disable axe accessibility tests.
 
 -j, --parallel Number of parallel behat run operation
 -m, --maxruns Max parallel processes to be executed at one time.
