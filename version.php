8400206468fc3c3a17a343babd0357af36492312
Paul Holden <paulh@moodle.com>
2023-03-31 18:11:57 +0100

    weekly release 4.0.7+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2022041907.05;              // 20220419      = branching date YYYYMMDD - do not modify!
+$version  = 2022041907.06;              // 20220419      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.0.7+ (Build: 20230328)'; // Human-friendly version name
+$release  = '4.0.7+ (Build: 20230401)'; // Human-friendly version name
 $branch   = '400';                     // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
