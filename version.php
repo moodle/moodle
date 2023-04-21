2204fd7ad7653efa2bac4334b43a81b2b91acf18
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2023-04-21 19:19:59 +0200

    Moodle release 4.2rc2

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023042000.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2023042200.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.2rc1 (Build: 20230418)'; // Human-friendly version name
+$release  = '4.2rc2 (Build: 20230422)'; // Human-friendly version name
 $branch   = '402';                     // This version's branch.
 $maturity = MATURITY_RC;             // This version's maturity level.
