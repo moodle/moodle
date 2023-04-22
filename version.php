ae4efa96ee8169a848c141ad21690165ea791552
Jun Pataleta <jun@moodle.com>
2023-04-22 17:06:54 +0800

    Moodle release 4.2

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023042200.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2023042400.00;              // 20230424      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.2rc2 (Build: 20230422)'; // Human-friendly version name
+$release  = '4.2 (Build: 20230424)'; // Human-friendly version name
 $branch   = '402';                     // This version's branch.
-$maturity = MATURITY_RC;             // This version's maturity level.
+$maturity = MATURITY_STABLE;             // This version's maturity level.
