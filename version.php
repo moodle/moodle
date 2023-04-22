4e632b72518d86f069839620cca270911e09b29a
Jun Pataleta <jun@moodle.com>
2023-04-22 17:15:01 +0800

    weekly back-to-dev release 4.3dev

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023042400.00;              // 20230424      = branching date YYYYMMDD - do not modify!
+$version  = 2023042400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.2 (Build: 20230424)'; // Human-friendly version name
-$branch   = '402';                     // This version's branch.
-$maturity = MATURITY_STABLE;             // This version's maturity level.
+$release  = '4.3dev (Build: 20230424)'; // Human-friendly version name
+$branch   = '403';                     // This version's branch.
+$maturity = MATURITY_ALPHA;             // This version's maturity level.
