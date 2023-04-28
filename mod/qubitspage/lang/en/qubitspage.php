18ab3e6f1cc4fafe4200c140ba8c620956fbb358
Kamesh V <kamesh.veerachamy@fourbends.com>
2023-04-28 23:39:13 +0530

    Submitted Qubits page custom module.

---
+++ mod/qubitspage/lang/en/qubitspage.php
@@ -0,0 +1,78 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'page', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   mod_qubitspage
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configdisplayoptions'] = 'Select all options that should be available, existing settings are not modified. Hold CTRL key to select multiple fields.';
+$string['content'] = 'Qubits Page content';
+$string['contentheader'] = 'Content';
+$string['createpage'] = 'Create a new Qubits page resource';
+$string['displayoptions'] = 'Available display options';
+$string['displayselect'] = 'Display';
+$string['displayselectexplain'] = 'Select display type.';
+$string['indicator:cognitivedepth'] = 'Qubits Page cognitive';
+$string['indicator:cognitivedepth_help'] = 'This indicator is based on the cognitive depth reached by the student in a Page resource.';
+$string['indicator:cognitivedepthdef'] = 'Qubits Page cognitive';
+$string['indicator:cognitivedepthdef_help'] = 'The participant has reached this percentage of the cognitive engagement offered by the Page resources during this analysis interval (Levels = No view, View)';
+$string['indicator:cognitivedepthdef_link'] = 'Learning_analytics_indicators#Cognitive_depth';
+$string['indicator:socialbreadth'] = 'Qubits Page social';
+$string['indicator:socialbreadth_help'] = 'This indicator is based on the social breadth reached by the student in a Page resource.';
+$string['indicator:socialbreadthdef'] = 'Qubits Page social';
+$string['indicator:socialbreadthdef_help'] = 'The participant has reached this percentage of the social engagement offered by the Page resources during this analysis interval (Levels = No participation, Participant alone)';
+$string['indicator:socialbreadthdef_link'] = 'Learning_analytics_indicators#Social_breadth';
+$string['legacyfiles'] = 'Migration of old course file';
+$string['legacyfilesactive'] = 'Active';
+$string['legacyfilesdone'] = 'Finished';
+$string['modulename'] = 'Qubits Page';
+$string['modulename_help'] = 'The Qubits page module enables a teacher to create a web page resource using the text editor. A page can display text, images, sound, video, web links and embedded code, such as Google maps.
+
+Advantages of using the Qubits page module rather than the file module include the resource being more accessible (for example to users of mobile devices) and easier to update.
+
+For large amounts of content, it\'s recommended that a book is used rather than a Qubits page.
+
+A Qubits page may be used
+
+* To present the terms and conditions of a course or a summary of the course syllabus
+* To embed several videos or sound files together with some explanatory text';
+$string['modulename_link'] = 'mod/qubitspage/view';
+$string['modulenameplural'] = 'Qubits Pages';
+$string['optionsheader'] = 'Display options';
+$string['qubitspage-mod-qubitspage-x'] = 'Any Qubits page module Qubits page';
+$string['qubitspage:addinstance'] = 'Add a new page resource';
+$string['qubitspage:view'] = 'View page content';
+$string['pluginadministration'] = 'Qubits Page module administration';
+$string['pluginname'] = 'Qubits Page';
+$string['popupheight'] = 'Pop-up height (in pixels)';
+$string['popupheightexplain'] = 'Specifies default height of popup windows.';
+$string['popupwidth'] = 'Pop-up width (in pixels)';
+$string['popupwidthexplain'] = 'Specifies default width of popup windows.';
+$string['printintro'] = 'Display page description';
+$string['printintroexplain'] = 'Display page description above content?';
+$string['printlastmodified'] = 'Display last modified date';
+$string['printlastmodifiedexplain'] = 'Display last modified date below content?';
+$string['privacy:metadata'] = 'The Qubits Page resource plugin does not store any personal data.';
+$string['search:activity'] = 'Qubits Page';
+
+// Deprecated since 4.0.
+$string['printheading'] = 'Display Qubits page name';
+$string['printheadingexplain'] = 'Display Qubits page name above content?';
