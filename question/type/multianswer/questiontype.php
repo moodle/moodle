2eb879360cbb47661dc0fea6f26c7893cca07b12
Meirza <meirza.arson@moodle.com>
2023-05-15 12:43:22 +0700

    MDL-77328 qtype_multianswer: Refactor for PHP 8.2 compatibility
    This commit addresses compatibility issues in the qtype_multianswer
    question type. The following changes were made:
    
    - The usage of the undeclared property $maxmark on subquestions in
      qtype_multianswer was identified as problematic. To resolve this,
      the existing declared property $defaultmark is now being used for
      the same purpose.
    
    By switching to the declared property $defaultmark, compatibility with
    PHP 8.2 is ensured and the issue with the undeclared property is
    resolved.

--- question/type/multianswer/questiontype.php
+++ question/type/multianswer/questiontype.php
@@ -146,7 +146,6 @@ class qtype_multianswer extends question_type {
             question_bank::get_qtype($wrapped->qtype)->get_question_options($wrapped);
             // For wrapped questions the maxgrade is always equal to the defaultmark,
             // there is no entry in the question_instances table for them.
-            $wrapped->maxmark = $wrapped->defaultmark;
             $wrapped->category = $question->categoryobject->id;
             $question->options->questions[$sequence[$wrapped->id]] = $wrapped;
         }
@@ -303,7 +302,7 @@ class qtype_multianswer extends question_type {
                 }
             }
             $question->subquestions[$key] = question_bank::make_question($subqdata);
-            $question->subquestions[$key]->maxmark = $subqdata->defaultmark;
+            $question->subquestions[$key]->defaultmark = $subqdata->defaultmark;
             if (isset($subqdata->options->layout)) {
                 $question->subquestions[$key]->layout = $subqdata->options->layout;
             }
