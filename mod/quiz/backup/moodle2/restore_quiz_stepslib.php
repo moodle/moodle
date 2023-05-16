078782e656e9e8e954162ad56467544522008e57
Mark Johnson <mark.johnson@catalyst-eu.net>
2023-05-16 15:26:22 +0100

    MDL-75696 quiz: Fix restoring pre-4.0 quizzes with random questions
    Restoring multiple quizzes from a pre-4.0 backup was broken when
    the quizzes shared a random question. This is because after the
    first quiz created a set reference in place of the random question,
    it deleted the question record so it was not there for the second
    quiz to use. This change tracks the IDs of random questions so
    they can be deleted at the end.

--- mod/quiz/backup/moodle2/restore_quiz_stepslib.php
+++ mod/quiz/backup/moodle2/restore_quiz_stepslib.php
@@ -45,6 +45,11 @@ class restore_quiz_activity_structure_step extends restore_questions_activity_st
     /** @var stdClass */
     protected $oldquizlayout;
 
+    /**
+     * @var array Track old question ids that need to be removed at the end of the restore.
+     */
+    protected $oldquestionids = [];
+
     protected function define_structure() {
 
         $paths = [];
@@ -347,8 +352,7 @@ class restore_quiz_activity_structure_step extends restore_questions_activity_st
             $filtercondition->includingsubcategories = $data->includingsubcategories ?? false;
             $questionsetreference->filtercondition = json_encode($filtercondition);
             $DB->insert_record('question_set_references', $questionsetreference);
-            // Cleanup leftover random qtype data from question table.
-            question_delete_question($question->questionid);
+            $this->oldquestionids[$question->questionid] = 1;
         } else {
             // Reference data.
             $questionreference = new \stdClass();
@@ -607,6 +611,14 @@ class restore_quiz_activity_structure_step extends restore_questions_activity_st
                     'quizid' => $this->get_new_parentid('quiz'),
                     'firstslot' => 1, 'heading' => '',
                     'shufflequestions' => $this->legacyshufflequestionsoption]);
+        }
+    }
+
+    protected function after_restore() {
+        parent::after_restore();
+        // Delete old random questions that have been converted to set references.
+        foreach (array_keys($this->oldquestionids) as $oldquestionid) {
+            question_delete_question($oldquestionid);
         }
     }
 }
