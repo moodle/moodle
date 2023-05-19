b171f57cd5755b6d37dbda201143c4b12d466541
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2023-05-19 10:54:02 +0200

    MDL-77576 matrix: Bump version to today
    All master versions need to be > than the previous
    release (4.2) versions. To avoid overlapping to happen.

--- communication/provider/matrix/version.php
+++ communication/provider/matrix/version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'communication_matrix';
-$plugin->version = 2023032900;
+$plugin->version = 2023051900;
 $plugin->requires = 2023011300;
 $plugin->maturity = MATURITY_ALPHA;
