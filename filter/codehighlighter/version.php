bccd225c390eda293cc03055bd6b2b3ac876b843
Sara Arjona <sara@moodle.com>
2023-05-22 16:35:41 +0200

    MDL-75963 tiny: Bump version to expected number
    There was as an error with the previous version number:
    "Version (2023031400) cannot be before 20230424 (YYYYMMDD)".

--- filter/codehighlighter/version.php
+++ filter/codehighlighter/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023031400;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires = 2023031000;        // Requires this Moodle version.
+$plugin->version = 2023052200;  // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires = 2023041800; // Requires this Moodle version.
 $plugin->component = 'filter_codehighlighter'; // Full name of the plugin (used for diagnostics).
