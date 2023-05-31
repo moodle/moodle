4b80d7b6e067701508317abaf5509fd38e3834c2
Gordon Bateson <gordonbateson@gmail.com>
2023-05-31 10:31:03 +0900

    MDL-79863 qtype_ordering:  * Fix Behat failure following button change in Moodle master branch

--- question/type/ordering/version.php
+++ question/type/ordering/version.php
@@ -29,5 +29,5 @@ $plugin->cron      = 0;
 $plugin->component = 'qtype_ordering';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2021051700; // Moodle 3.11.
-$plugin->version   = 2023042907;
-$plugin->release   = '2023-04-29 (07)';
+$plugin->version   = 2023053108;
+$plugin->release   = '2023-05-31 (08)';
