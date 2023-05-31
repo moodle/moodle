44bd656a98b0fd7bd766172b89f0b09d6f3355d1
Ilya Tregubov <ilya@moodle.com>
2023-05-31 17:27:57 +0800

    MDL-77635 core_grades: Use sticky footer on import grades page

--- grade/import/xml/index.php
+++ grade/import/xml/index.php
@@ -16,8 +16,10 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 require_once('../../../config.php');
-require_once('lib.php');
-require_once('grade_import_form.php');
+require_once($CFG->libdir . '/gradelib.php');
+require_once($CFG->dirroot . '/grade/lib.php');
+require_once($CFG->dirroot . '/grade/import/grade_import_form.php');
+require_once($CFG->dirroot . '/grade/import/lib.php');
 
 $id = required_param('id', PARAM_INT); // course id
 
