d73d56f07065841cf6b4a22923699f24d732bc8d
stemiwe <stefan.weber@think-modular.com>
2023-06-14 15:03:48 +0200

    MDL-78474 mod_label: include new field for name

--- mod/label/version.php
+++ mod/label/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023042400;       // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2023042401;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023041800;    // Requires this Moodle version.
 $plugin->component = 'mod_label'; // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
