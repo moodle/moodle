73fffd976532dbe79b5fd3c9f498aafc2c5f4394
Kamesh V <kamesh.veerachamy@fourbends.com>
2023-06-27 12:02:05 +0530

    Revert "Course detail static content updated"
    This reverts commit 129e78206a9161c3c45fb2ebcab14f54c0ad3aef.

--- theme/qubitsbasic/renderers.php
+++ theme/qubitsbasic/renderers.php
@@ -22,5 +22,5 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-//require_once('renderers/course_renderer.php');
+require_once('renderers/course_renderer.php');
 require_once('renderers/core_renderer.php');
