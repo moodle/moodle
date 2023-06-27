bb1f1519948c662850f4aa8892a10e720a24eede
sonia <sonia@fourbends.com>
2023-06-27 12:09:41 +0530

    Updated styles for course detail page

--- theme/qubitsbasic/config.php
+++ theme/qubitsbasic/config.php
@@ -29,7 +29,7 @@ require_once(__DIR__ . '/lib.php');
 $THEME->doctype = 'html5';
 $THEME->name = 'qubitsbasic';
 $THEME->parents = array('boost');
-$THEME->sheets = ['qubitsfonts','qubitsbasic', 'styles'];
+$THEME->sheets = ['qubitsfonts','qubitsbasic', 'styles', 'temp'];
 $THEME->iconsystem = '\\theme_qubitsbasic\\output\\icon_system_fa_custom';
 $THEME->editor_sheets = [];
 $THEME->enable_dock = false;
