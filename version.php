e77452220b268f70612b9b915179c17fc6d93173
Ilya Tregubov <ilya@moodle.com>
2023-07-14 09:25:31 +0800

    weekly release 4.3dev

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023070700.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2023071400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.3dev (Build: 20230707)'; // Human-friendly version name
+$release  = '4.3dev (Build: 20230714)'; // Human-friendly version name
 $branch   = '403';                     // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
