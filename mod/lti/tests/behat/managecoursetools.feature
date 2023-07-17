turnitin-public/moodle-dev

MDL-78575 mod_lti: Add deletion support to course tools page

mod/lti/tests/behat/managecoursetools.feature
@@ -90,3 +90,23 @@ Feature: Manage course tools
     And I should see "Test tool 20" in the "course-tools" "table"
     And I click on "2" "link" in the "page" "region"
     And I should see "Test tool 1" in the "course-tools" "table"
+
+  @javascript
+  Scenario: Delete a course tool
+    Given the following "mod_lti > course tools" exist:
+      | name      | description         | baseurl                   | course |
+      | Test tool | Example description | https://example.com/tool | C1     |
+      | Another tool | Example 123 | https://another.example.com/tool | C1     |
+    And I am on the "Course 1" course page logged in as teacher1
+    And I navigate to "LTI External tools" in current page administration
+    When I open the action menu in "Test tool" "table_row"
+    And I choose "Delete" in the open action menu
+    Then I should see "Are you sure you want to delete this course tool?"
+    And I click on "Cancel" "button" in the "Delete a course tool" "dialogue"
+    And I should see "Test tool" in the "course-tools" "table"
+    And I open the action menu in "Test tool" "table_row"
+    And I choose "Delete" in the open action menu
+    And I should see "Are you sure you want to delete this course tool?"
+    And I click on "Delete" "button" in the "Delete a course tool" "dialogue"
+    And I should see "Course tool deleted"
+    And I should not see "Test tool" in the "course-tools" "table"

mod/lti/tests/external/delete_course_tool_type_test.php
@@ -1,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_lti\external;
+
+use core_external\external_api;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/mod/lti/tests/mod_lti_testcase.php');
+
+/**
+ * PHPUnit tests for delete_course_tool_type external function.
+ *
+ * @package    mod_lti
+ * @copyright  2023 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class delete_course_tool_type_test extends \mod_lti_testcase {
+
+    /**
+     * Test delete_course_tool() for a course tool.
+     */
+    public function test_delete_course_tool() {
+        $this->resetAfterTest();
+
+        $course = $this->getDataGenerator()->create_course();
+        $editingteacher = $this->getDataGenerator()->create_and_enrol($course, 'editingteacher');
+        $this->setUser($editingteacher);
+
+        $typeid = lti_add_type(
+            (object) [
+                'state' => LTI_TOOL_STATE_CONFIGURED,
+                'course' => $course->id
+            ],
+            (object) [
+                'lti_typename' => "My course tool",
+                'lti_toolurl' => 'http://example.com',
+                'lti_ltiversion' => 'LTI-1p0'
+            ]
+        );
+
+        $data = delete_course_tool_type::execute($typeid);
+        $data = external_api::clean_returnvalue(delete_course_toolType::execute_returns(), $data);
+
+        $this->assertTrue($data);
+    }
+
+    /**
+     * Test delete_course_tool() for a site tool, which is forbidden.
+     */
+    public function test_delete_course_tool_site_tool() {
+        $this->resetAfterTest();
+
+        $course = $this->getDataGenerator()->create_course();
+        $editingteacher = $this->getDataGenerator()->create_and_enrol($course, 'editingteacher');
+        $this->setUser($editingteacher);
+
+        $type = $this->generate_tool_type(123); // Creates a site tool.
+
+        $this->expectException(\invalid_parameter_exception::class);
+        delete_course_tool_type::execute($type->id);
+    }
+}

moodle/moodle

mod/lti/tests/behat/managecoursetools.feature
@@ -96,3 +96,23 @@ Feature: Manage course tools
     And I should see "Test tool 20" in the "reportbuilder-table" "table"
     And I click on "2" "link" in the "page" "region"
     And I should see "Test tool 1" in the "reportbuilder-table" "table"
+
+  @javascript
+  Scenario: Delete a course tool
+    Given the following "mod_lti > course tools" exist:
+      | name         | description         | baseurl                          | course |
+      | Test tool    | Example description | https://example.com/tool         | C1     |
+      | Another tool | Example 123         | https://another.example.com/tool | C1     |
+    And I am on the "Course 1" course page logged in as teacher1
+    And I navigate to "LTI External tools" in current page administration
+    When I open the action menu in "Test tool" "table_row"
+    And I choose "Delete" in the open action menu
+    Then I should see "Are you sure you want to delete this course tool?"
+    And I click on "Cancel" "button" in the "Delete a course tool" "dialogue"
+    And I should see "Test tool" in the "reportbuilder-table" "table"
+    And I open the action menu in "Test tool" "table_row"
+    And I choose "Delete" in the open action menu
+    And I should see "Are you sure you want to delete this course tool?"
+    And I click on "Delete" "button" in the "Delete a course tool" "dialogue"
+    And I should see "Course tool deleted"
+    And I should not see "Test tool" in the "reportbuilder-table" "table"

mod/lti/tests/external/delete_course_tool_type_test.php
@@ -1,0 +1,82 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_lti\external;
+
+use core_external\external_api;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/mod/lti/tests/mod_lti_testcase.php');
+
+/**
+ * PHPUnit tests for delete_course_tool_type external function.
+ *
+ * @package    mod_lti
+ * @copyright  2023 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @coversDefaultClass \mod_lti\external\delete_course_tool_type
+ */
+class delete_course_tool_type_test extends \mod_lti_testcase {
+
+    /**
+     * Test delete_course_tool() for a course tool.
+     * @covers ::execute
+     */
+    public function test_delete_course_tool() {
+        $this->resetAfterTest();
+
+        $course = $this->getDataGenerator()->create_course();
+        $editingteacher = $this->getDataGenerator()->create_and_enrol($course, 'editingteacher');
+        $this->setUser($editingteacher);
+
+        $typeid = lti_add_type(
+            (object) [
+                'state' => LTI_TOOL_STATE_CONFIGURED,
+                'course' => $course->id
+            ],
+            (object) [
+                'lti_typename' => "My course tool",
+                'lti_toolurl' => 'http://example.com',
+                'lti_ltiversion' => 'LTI-1p0'
+            ]
+        );
+
+        $data = delete_course_tool_type::execute($typeid);
+        $data = external_api::clean_returnvalue(delete_course_tool_type::execute_returns(), $data);
+
+        $this->assertTrue($data);
+    }
+
+    /**
+     * Test delete_course_tool() for a site tool, which is forbidden.
+     * @covers ::execute
+     */
+    public function test_delete_course_tool_site_tool() {
+        $this->resetAfterTest();
+
+        $course = $this->getDataGenerator()->create_course();
+        $editingteacher = $this->getDataGenerator()->create_and_enrol($course, 'editingteacher');
+        $this->setUser($editingteacher);
+
+        $type = $this->generate_tool_type(123); // Creates a site tool.
+
+        $this->expectException(\invalid_parameter_exception::class);
+        delete_course_tool_type::execute($type->id);
+    }
+}