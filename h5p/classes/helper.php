1ae0262730b56e6ea4d88e32f56eb8fad0629a4f
Paul Holden <paulh@moodle.com>
2023-07-27 13:01:53 +0100

    MDL-78820 h5p: use fullname of current user as the xAPI actor name.

--- h5p/classes/helper.php
+++ h5p/classes/helper.php
@@ -333,7 +333,7 @@ class helper {
         // When there is a logged in user, her information will be passed to the player. It will be used for tracking.
         $usersettings = [];
         if (isloggedin()) {
-            $usersettings['name'] = $USER->username;
+            $usersettings['name'] = fullname($USER, has_capability('moodle/site:viewfullnames', $systemcontext));
             $usersettings['id'] = $USER->id;
         }
         $settings = array(
