aa8ab48521fe4a57c3ec923e6e82a5ac1202e9de
Paul Holden <paulh@moodle.com>
2023-08-10 08:42:05 +0100

    MDL-78971 quiz_grading: escape user identity fields on display.

--- mod/quiz/report/grading/report.php
+++ mod/quiz/report/grading/report.php
@@ -720,13 +720,15 @@ class quiz_grading_report extends report_base {
         $a = new stdClass();
         $a->attempt = $attempt->attempt;
         $a->fullname = fullname($attempt);
+
         $customfields = [];
         foreach ($this->extrauserfields as $field) {
-            if ($attempt->{s($field)}) {
-                $customfields[] = $attempt->{s($field)};
+            if (strval($attempt->{$field}) !== '') {
+                $customfields[] = s($attempt->{$field});
             }
         }
-        $a->customfields = trim(implode(', ', (array)$customfields), ' ,');
+
+        $a->customfields = implode(', ', $customfields);
 
         if ($shownames && $showcustomfields) {
             return get_string('gradingattemptwithcustomfields', 'quiz_grading', $a);
