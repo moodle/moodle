981614e48353c1370379bcc482b1f776fc96b8e4
Rossco Hellmans <rosscohellmans@catalyst-au.net>
2023-08-23 09:20:22 +1000

    MDL-79092 session: fix recentsessionlocks mutated session debugging
    In write_close() call update_recent_session_locks() before setting
    $sessionatclose so that it includes $SESSION->recentsessionlocks and
    doesn't trigger the debugging in check_mutated_closed_session().

--- lib/classes/session/manager.php
+++ lib/classes/session/manager.php
@@ -689,12 +689,6 @@ class manager {
         global $PERF, $ME, $CFG;
 
         if (self::$sessionactive) {
-            // If debugging, take a snapshot of session at close and compare on shutdown to detect any accidental mutations.
-            if (debugging()) {
-                self::$sessionatclose = (array) $_SESSION['SESSION'];
-                \core_shutdown_manager::register_function('\core\session\manager::check_mutated_closed_session');
-            }
-
             // Grab the time when session lock is released.
             $PERF->sessionlock['released'] = microtime(true);
             if (!empty($PERF->sessionlock['gained'])) {
@@ -703,6 +697,12 @@ class manager {
             $PERF->sessionlock['url'] = me();
             self::update_recent_session_locks($PERF->sessionlock);
             self::sessionlock_debugging();
+
+            // If debugging, take a snapshot of session at close and compare on shutdown to detect any accidental mutations.
+            if (debugging()) {
+                self::$sessionatclose = (array) $_SESSION['SESSION'];
+                \core_shutdown_manager::register_function('\core\session\manager::check_mutated_closed_session');
+            }
 
             $requireslock = self::$handler->requires_write_lock();
             if (!$requireslock || !self::$requireslockdebug) {
