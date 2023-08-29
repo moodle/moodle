7cb2a78626182b320ba2a781fd541193e374cd01
Jake Dallimore <jake@moodle.com>
2023-08-29 16:08:53 +0800

    MDL-78597 mod_lti: add category restriction support to lti generator
    This is already supported in lti_add_type, via the lti_coursecategories
    config value, so all that's needed is to match the categories by
    idnumber and pass through the resulting ids.

--- mod/lti/tests/generator/behat_mod_lti_generator.php
+++ mod/lti/tests/generator/behat_mod_lti_generator.php
@@ -44,6 +44,7 @@ class behat_mod_lti_generator extends behat_generator_base {
                 'singular' => 'tool type',
                 'datagenerator' => 'tool_types',
                 'required' => ['baseurl'],
+                'switchids' => ['lti_coursecategories' => 'lti_coursecategories']
             ],
             'course tools' => [
                 'singular' => 'course tool',
@@ -73,5 +74,25 @@ class behat_mod_lti_generator extends behat_generator_base {
             throw new coding_exception('The specified tool with name "' . $name . '" does not exist');
         }
         return (int) $id;
+    }
+
+    /**
+     * Handles the switchid ['lti_coursecategories' => 'lti_coursecategories'] for restricting a tool to certain categories.
+     *
+     * @param string $idnumbers a comma-separated string containing the course category id numbers, e.g. 'cata, catb, catc'.
+     * @return string a comma-separated string containing the course category ids.
+     * @throws coding_exception if one or more of the categories is unable to be matched by its idnumber.
+     */
+    protected function get_lti_coursecategories_id(string $idnumbers): string {
+        global $DB;
+        $categoryids = array_map('trim', explode(',', $idnumbers));
+
+        [$insql, $inparams] = $DB->get_in_or_equal($categoryids);
+        $ids = $DB->get_fieldset_sql("SELECT id FROM {course_categories} WHERE idnumber $insql", $inparams);
+        if (!$ids || count($ids) != count($categoryids)) {
+            throw new coding_exception("One or more course categories unable to be matched using idnumbers: $idnumbers");
+        }
+
+        return implode(',', $ids);
     }
 }
