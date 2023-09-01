77d3c5b1b05c2854f5962e83f7d75613ea253235
Marina Glancy <marina@moodle.com>
2023-09-01 10:06:57 +0100

    MDL-79088 navigation: add stoppable interface to navigation extend hook

--- lib/classes/hook/navigation/primary_extend.php
+++ lib/classes/hook/navigation/primary_extend.php
@@ -26,7 +26,11 @@ use core\navigation\views\primary;
  * @copyright  2023 Marina Glancy
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class primary_extend implements described_hook {
+class primary_extend implements described_hook,
+        \Psr\EventDispatcher\StoppableEventInterface {
+
+    /** @var bool $stopped propagation is stopped */
+    protected $stopped = false;
 
     /**
      * Creates new hook.
@@ -61,5 +65,24 @@ class primary_extend implements described_hook {
      */
     public static function get_hook_tags(): array {
         return ['navigation'];
+    }
+
+    /**
+     * Allows plugins to notify the hook dispatcher that hook propagation should be stopped
+     */
+    public function stop(): void {
+        $this->stopped = true;
+    }
+
+    /**
+     * Is propagation stopped?
+     *
+     * This will typically only be used by the Dispatcher to determine if the
+     * previous listener halted propagation.
+     * @return bool True if the Event is complete and no further listeners should be called.
+     *              False to continue calling listeners.
+     */
+    public function isPropagationStopped(): bool {
+        return $this->stopped;
     }
 }
