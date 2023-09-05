00f0613f99d2ca31f0b8e5b20267c0c3ba6873ea
Andrew Nicols <andrew@nicols.co.uk>
2023-09-05 22:27:40 +0800

    on-demand release 4.3dev+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023090200.01;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2023090500.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.3dev+ (Build: 20230902)'; // Human-friendly version name
+$release  = '4.3dev+ (Build: 20230905)'; // Human-friendly version name
 $branch   = '403';                     // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
