62a6ddd16ca123f437a0f78360fe315cfbb9e510
Derick Turner <derick@e-learndesign.co.uk>
2023-09-07 13:24:00 +0100

    IOMAD: typo

--- blocks/iomad_company_admin/user_bulk_download.php
+++ blocks/iomad_company_admin/user_bulk_download.php
@@ -206,7 +206,7 @@ function user_download_ods($userids, $fields, $includecompanyfield) {
             // Stop the script from timing out on large numbers of users.
             set_time_limit(30);
             if ($includecompanyfield || $field != "profile_field_company") {
-                if (!empty($user->field) {
+                if (!empty($user->field)) {
                     $worksheet[0]->write($row, $col, $user->$field);
                 } else {
                     $worksheet[0]->write($row, $col, '');
