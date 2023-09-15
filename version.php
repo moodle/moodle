88f41107c6cef14d65d5d285e7babfda62e5cf3e
Paul Holden <paulh@moodle.com>
2023-09-15 13:28:41 +0100

    MDL-79373 admin: ensure all current password configs are defined.
    Ensure the admin preset export tool has an accurate record of each,
    to be observed as sensitive settings when creating an export.

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023100905.08;              // 20231009      = branching date YYYYMMDD - do not modify!
+$version  = 2023100905.09;              // 20231009      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '4.3.5+ (Build: 20240802)'; // Human-friendly version name
