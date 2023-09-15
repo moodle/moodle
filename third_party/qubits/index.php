6e493bf1c871b21ee02e5048170a69671074f0ff
kameshfourbends <31654071+kameshfourbends@users.noreply.github.com>
2023-09-15 11:37:23 +0530

    Updated new build (#112)
    * Updated new build
    
    * Updated Site editors name changes, css path issue fixed

--- third_party/qubits/index.php
+++ third_party/qubits/index.php
@@ -6,31 +6,43 @@ $clsname = 'page-editor-qubits-'.$elanguage;
 $PAGE->set_pagelayout('thirdparty');
 $PAGE->add_body_class($clsname);
 echo $OUTPUT->header();
-
 ?>
 <div id="__next"></div>
-
+<?php if($elanguage=="webeditor") { ?>
+<script id="__NEXT_DATA__" type="application/json">
+	{
+	"props": {
+		"pageProps": {}
+	},
+	"page": "/CloudIDE",
+	"query": {},
+	"buildId": "iLLJ41TGYwZnM0_woJzO_",
+	"nextExport": true,
+	"autoExport": true,
+	"isFallback": false,
+	"scriptLoader": []
+	}
+</script>
+<?php
+} else { ?>
 <script id="__NEXT_DATA__" type="application/json">
 	{
 		"props": {
 			"pageProps": {},
 			"__N_SSG": true
 		},
-
-
 		"page": "/Editor/[language]",
 	"query": {
 	  "language": "<?php echo $elanguage; ?>"
 	},
 
-		"buildId": "1ZAuSTIIzMcs6Ol2DXPM_",
+		"buildId": "iLLJ41TGYwZnM0_woJzO_",
 		"isFallback": false,
 		"gsp": true,
 		"scriptLoader": []
 	}
 </script>
-<?php
-
+<?php }
 echo $OUTPUT->footer();
 
 ?>
