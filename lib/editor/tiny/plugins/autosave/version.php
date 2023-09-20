dc2e3d8eecca82f0955dad200507e378e3730705
Paul Holden <paulh@moodle.com>
2023-09-20 17:50:16 +0100

    MDL-79433 tiny_autosave: allow non-logged in user calls to webservice.
    Accounts for the editor being added to pages for non-logged in users,
    such as the "New account" page.

--- lib/editor/tiny/plugins/autosave/version.php
+++ lib/editor/tiny/plugins/autosave/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023042400;
+$plugin->version   = 2023042401;
 $plugin->requires  = 2023041800;
 $plugin->component = 'tiny_autosave';
