43d5aec47ef117867a5d72dfc14c3eec1a99f285
Jun Pataleta <jun@moodle.com>
2023-09-29 22:21:20 +0800

    on-demand release 4.3beta+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023092600.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2023092900.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.3beta (Build: 20230926)'; // Human-friendly version name
+$release  = '4.3beta+ (Build: 20230929)'; // Human-friendly version name
 $branch   = '403';                     // This version's branch.
 $maturity = MATURITY_BETA;             // This version's maturity level.
