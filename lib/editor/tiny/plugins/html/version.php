3d55b4a865b82d1cfad5aaa77e2a84104db455b1
Jun Pataleta <jun@moodle.com>
2023-10-02 17:42:27 +0800

    MDL-79551 tiny_html: Remove unnecessary $plugin->release

--- lib/editor/tiny/plugins/html/version.php
+++ lib/editor/tiny/plugins/html/version.php
@@ -25,7 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_html';
-$plugin->release = '2023050400';
 $plugin->version = 2023100900;
 $plugin->requires = 2023100400;
 $plugin->maturity = MATURITY_STABLE;
