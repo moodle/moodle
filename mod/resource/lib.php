8fa5a3008dea15c2a56f0f2ffd2bc476c35edb92
Sara Arjona <sara@moodle.com>
2023-10-04 09:16:41 +0200

    MDL-80150 resource: Reintroduce file icon
    The new activity card design, planned for Moodle 4.3, reintroduces
    the resource MIME icon that was removed in Moodle 4.0.
    This is a backport of MDL-78285.

--- mod/resource/lib.php
+++ mod/resource/lib.php
@@ -227,6 +227,7 @@ function resource_get_coursemodule_info($coursemodule) {
     if (count($files) >= 1) {
         $mainfile = reset($files);
         $resource->mainfile = $mainfile->get_filename();
+        $info->icon = file_file_icon($mainfile);
     }
 
     $display = resource_get_final_display_type($resource);
