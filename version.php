c765bb3b529e9236f335760ce04944d92a5f906a
Sara Arjona <sara@moodle.com>
2023-10-06 14:03:06 +0200

    Moodle release 4.3rc2

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023100400.03;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2023100600.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.3rc1 (Build: 20231004)'; // Human-friendly version name
+$release  = '4.3rc2 (Build: 20231006)'; // Human-friendly version name
 $branch   = '403';                     // This version's branch.
 $maturity = MATURITY_RC;             // This version's maturity level.
