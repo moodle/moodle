f10d837fcea8c54398fd6528c328443be8d1b7d2
Luca Bösch <luca.boesch@bfh.ch>
2023-11-08 10:12:48 +0100

    MDL-79931 mod_glossary: Change edit entry icon from gear to pencil.

--- mod/glossary/lib.php
+++ mod/glossary/lib.php
@@ -1289,7 +1289,7 @@ function glossary_print_entry_icons($course, $cm, $glossary, $entry, $mode='',$h
 
             $url = "edit.php?cmid=$cm->id&amp;id=$entry->id&amp;mode=$mode&amp;hook=".urlencode($hook);
             $return .= "<a class='icon' title=\"" . get_string("edit") . "\" href=\"$url\">" .
-                       $OUTPUT->pix_icon('t/edit', get_string('editentrya', 'mod_glossary', $altsuffix)) . '</a>';
+                       $OUTPUT->pix_icon('i/edit', get_string('editentrya', 'mod_glossary', $altsuffix)) . '</a>';
         } elseif ( $importedentry ) {
             $return .= "<font size=\"-1\">" . get_string("exportedentry","glossary") . "</font>";
         }
