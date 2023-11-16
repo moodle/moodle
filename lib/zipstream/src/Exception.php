6972e9c83d13b0b92f63797a6da3c53e9192427d
meirzamoodle <meirza.arson@moodle.com>
2023-11-16 09:55:36 +0700

    MDL-78256 lib: Upgrade Zipstream to 3.1.0

--- lib/zipstream/src/Exception.php
+++ lib/zipstream/src/Exception.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace ZipStream;
 
-/**
- * This class is only for inheriting
- */
 abstract class Exception extends \Exception
 {
 }
--- lib/zipstream/src/Exception/OverflowException.php
+++ lib/zipstream/src/Exception/OverflowException.php
@@ -11,6 +11,9 @@ use ZipStream\Exception;
  */
 class OverflowException extends Exception
 {
+    /**
+     * @internal
+     */
     public function __construct()
     {
         parent::__construct('File size exceeds limit of 32 bit integer. Please enable "zip64" option.');
