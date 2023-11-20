1bbc0425e3f0f5cfb94e5ff98ffd0e0aa2cb3319
Jeremy Schulz <jjbbss@outlook.de>
2023-11-20 15:33:24 +0100

    MDL-80180 admin: added readonly support for multicheckbox

--- lib/adminlib.php
+++ lib/adminlib.php
@@ -3271,6 +3271,7 @@ class admin_setting_configmulticheckbox extends admin_setting {
         $context = (object) [
             'id' => $this->get_id(),
             'name' => $this->get_full_name(),
+            'readonly' => $this->is_readonly(),
         ];
 
         $options = array();
