3aac83edc4d168be232cdba5c72f1cf7af0217a9
Paul Holden <paulh@moodle.com>
2023-11-20 17:31:07 +0000

    MDL-80192 reportbuilder: ensure filter form group elements have labels.
    This ensures compliance with the following WCAG criterion:
    
    "H71: Providing a description for groups of form controls using fieldset
    and legend elements".

--- reportbuilder/classes/form/filter.php
+++ reportbuilder/classes/form/filter.php
@@ -149,9 +149,10 @@ class filter extends dynamic_form {
         $buttons = [];
         $buttons[] = $mform->createElement('submit', 'submitbutton', get_string('apply', 'core_reportbuilder'));
         $buttons[] = $mform->createElement('submit', 'resetfilters',  get_string('resetall', 'core_reportbuilder'),
-            null, null, ['customclassoverride' => 'btn-link']);
+            null, null, ['customclassoverride' => 'btn-link ml-1']);
 
-        $mform->addGroup($buttons, 'buttonar', '', [' '], false);
+        $mform->addGroup($buttons, 'buttonar', get_string('formactions', 'core_form'), '', false)
+            ->setHiddenLabel(true);
         $mform->closeHeaderBefore('buttonar');
 
         $mform->disable_form_change_checker();
