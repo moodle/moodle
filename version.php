b1bb5671df76d740b767f80b7df33506d7e1bf14
Huong Nguyen <huongnv13@gmail.com>
2023-11-24 12:23:36 +0700

    weekly release 4.1.6+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2022112806.06;              // 20221128      = branching date YYYYMMDD - do not modify!
+$version  = 2022112806.07;              // 20221128      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.1.6+ (Build: 20231116)'; // Human-friendly version name
+$release  = '4.1.6+ (Build: 20231124)'; // Human-friendly version name
 $branch   = '401';                     // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
