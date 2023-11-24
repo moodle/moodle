cf1bcfce182079b477500a5a370a82f91ffe3729
Mathew May <mathewm@hotmail.co.nz>
2023-11-24 10:23:33 +0800

    MDL-79884 qtype_ordering: Add unit coverage and exemptions
    Part of: MDL-79863

--- question/type/ordering/lib.php
+++ question/type/ordering/lib.php
@@ -32,6 +32,8 @@
  * @param array $args extra arguments
  * @param bool $forcedownload whether or not force download
  * @param array $options additional options affecting the file serving
+ * @codeCoverageIgnore
+ * This code can not be tested currently as pluginfile() is not testable.
  */
 function qtype_ordering_pluginfile(stdClass $course, stdClass $cm, stdClass $context, string $filearea, array $args,
         bool $forcedownload, array $options = []) {
