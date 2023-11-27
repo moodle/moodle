e72871579a3293343735689c52f37d280bb369eb
Paul Holden <paulh@moodle.com>
2023-11-27 20:35:33 +0000

    MDL-80268 badges: require capability to view awarded badges.

--- badges/recipients.php
+++ badges/recipients.php
@@ -54,6 +54,8 @@ $badge = new badge($badgeid);
 $context = $badge->get_context();
 $navurl = new moodle_url('/badges/index.php', array('type' => $badge->type));
 
+require_capability('moodle/badges:viewawarded', $context);
+
 if ($badge->type == BADGE_TYPE_COURSE) {
     if (empty($CFG->badges_allowcoursebadges)) {
         print_error('coursebadgesdisabled', 'badges');
