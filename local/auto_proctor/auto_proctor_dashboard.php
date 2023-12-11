18eefb5eee8b43e72fc75f7969dc123adb8c49b3
AAS1214 <angel1214.as@gmail.com>
2023-12-12 04:13:27 +0800

    auto_proctor_dashboar file preparation for frontend

--- local/auto_proctor/auto_proctor_dashboard.php
+++ local/auto_proctor/auto_proctor_dashboard.php
@@ -31,7 +31,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Auto-Proctor Dashboard</title>
 </head>
 <body>
     <h1> Hello bitch! </h1>
