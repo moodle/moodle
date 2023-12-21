0e51c69f9c3d30a42010db7a007ccdbf618bec6c
IvanZaykov <zaykov_ivan@list.ru>
2023-12-21 18:58:55 +0300

    Переход к версии 3.9

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'ow_mysql';
+$CFG->dbname    = 'moodle_base';
+$CFG->dbuser    = 'admin';
+$CFG->dbpass    = 'admin';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_0900_ai_ci',
+);
+
+$CFG->wwwroot   = 'http://www.moodle_test.ru';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
