bca3f96344554dbc8e7df3221dd6fcfad03e74c2
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2023-12-22 13:13:21 +0200

    Updated repo

--- auth/useradditionapi/lang/en/auth_useradditionapi.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-$string['eventuseradded'] = 'User Added';
-$string['pluginname'] = 'User Addition API';
-$string['lastapiresponse'] = 'Last API Response';
--- auth/useradditionapi/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version = 2023120400;
-$plugin->requires = 2020061500;
-$plugin->component = 'auth_useradditionapi';
