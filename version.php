076379a485b83d5a4126e1dc50a8838f9ea7566c
Ilya Tregubov <ilya.a.tregubov@gmail.com>
2024-01-12 09:11:37 +0800

    weekly release 4.3.2+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023100902.02;              // 20231009      = branching date YYYYMMDD - do not modify!
+$version  = 2023100902.03;              // 20231009      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.3.2+ (Build: 20240104)'; // Human-friendly version name
+$release  = '4.3.2+ (Build: 20240112)'; // Human-friendly version name
 $branch   = '403';                     // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
