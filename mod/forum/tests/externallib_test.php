a0890debe7c0b9b3da18dcf7dac6648f8e601724
Kevin Percy <kevin.percy@moodle.com>
2024-01-18 16:17:56 +0800

    MDL-70483 forum: Final deprecation of get_forum_discussions_paginated

--- mod/forum/tests/externallib_test.php
+++ mod/forum/tests/externallib_test.php
@@ -882,208 +882,6 @@ class externallib_test extends externallib_advanced_testcase {
     }
 
     /**
-     * Test get forum discussions paginated
-     */
-    public function test_mod_forum_get_forum_discussions_paginated() {
-        global $USER, $CFG, $DB, $PAGE;
-
-        $this->resetAfterTest(true);
-
-        // Set the CFG variable to allow track forums.
-        $CFG->forum_trackreadposts = true;
-
-        // Create a user who can track forums.
-        $record = new \stdClass();
-        $record->trackforums = true;
-        $user1 = self::getDataGenerator()->create_user($record);
-        // Create a bunch of other users to post.
-        $user2 = self::getDataGenerator()->create_user();
-        $user3 = self::getDataGenerator()->create_user();
-        $user4 = self::getDataGenerator()->create_user();
-
-        // Set the first created user to the test user.
-        self::setUser($user1);
-
-        // Create courses to add the modules.
-        $course1 = self::getDataGenerator()->create_course();
-
-        // First forum with tracking off.
-        $record = new \stdClass();
-        $record->course = $course1->id;
-        $record->trackingtype = FORUM_TRACKING_OFF;
-        $forum1 = self::getDataGenerator()->create_module('forum', $record);
-
-        // Add discussions to the forums.
-        $record = new \stdClass();
-        $record->course = $course1->id;
-        $record->userid = $user1->id;
-        $record->forum = $forum1->id;
-        $discussion1 = self::getDataGenerator()->get_plugin_generator('mod_forum')->create_discussion($record);
-
-        // Add three replies to the discussion 1 from different users.
-        $record = new \stdClass();
-        $record->discussion = $discussion1->id;
-        $record->parent = $discussion1->firstpost;
-        $record->userid = $user2->id;
-        $discussion1reply1 = self::getDataGenerator()->get_plugin_generator('mod_forum')->create_post($record);
-
-        $record->parent = $discussion1reply1->id;
-        $record->userid = $user3->id;
-        $discussion1reply2 = self::getDataGenerator()->get_plugin_generator('mod_forum')->create_post($record);
-
-        $record->userid = $user4->id;
-        $discussion1reply3 = self::getDataGenerator()->get_plugin_generator('mod_forum')->create_post($record);
-
-        // Enrol the user in the first course.
-        $enrol = enrol_get_plugin('manual');
-
-        // We don't use the dataGenerator as we need to get the $instance2 to unenrol later.
-        $enrolinstances = enrol_get_instances($course1->id, true);
-        foreach ($enrolinstances as $courseenrolinstance) {
-            if ($courseenrolinstance->enrol == "manual") {
-                $instance1 = $courseenrolinstance;
-                break;
-            }
-        }
-        $enrol->enrol_user($instance1, $user1->id);
-
-        // Delete one user.
-        delete_user($user4);
-
-        // Assign capabilities to view discussions for forum 1.
-        $cm = get_coursemodule_from_id('forum', $forum1->cmid, 0, false, MUST_EXIST);
-        $context = \context_module::instance($cm->id);
-        $newrole = create_role('Role 2', 'role2', 'Role 2 description');
-        $this->assignUserCapability('mod/forum:viewdiscussion', $context->id, $newrole);
-
-        // Create what we expect to be returned when querying the forums.
-
-        $post1 = $DB->get_record('forum_posts', array('id' => $discussion1->firstpost), '*', MUST_EXIST);
-
-        // User pictures are initially empty, we should get the links once the external function is called.
-        $expecteddiscussions = array(
-                'id' => $discussion1->firstpost,
-                'name' => $discussion1->name,
-                'groupid' => (int) $discussion1->groupid,
-                'timemodified' => $discussion1reply3->created,
-                'usermodified' => (int) $discussion1reply3->userid,
-                'timestart' => (int) $discussion1->timestart,
-                'timeend' => (int) $discussion1->timeend,
-                'discussion' => $discussion1->id,
-                'parent' => 0,
-                'userid' => (int) $discussion1->userid,
-                'created' => (int) $post1->created,
-                'modified' => (int) $post1->modified,
-                'mailed' => (int) $post1->mailed,
-                'subject' => $post1->subject,
-                'message' => $post1->message,
-                'messageformat' => (int) $post1->messageformat,
-                'messagetrust' => (int) $post1->messagetrust,
-                'attachment' => $post1->attachment,
-                'totalscore' => (int) $post1->totalscore,
-                'mailnow' => (int) $post1->mailnow,
-                'userfullname' => fullname($user1),
-                'usermodifiedfullname' => fullname($user4),
-                'userpictureurl' => '',
-                'usermodifiedpictureurl' => '',
-                'numreplies' => 3,
-                'numunread' => 0,
-                'pinned' => (bool) FORUM_DISCUSSION_UNPINNED,
-                'locked' => false,
-                'canreply' => false,
-                'canlock' => false
-            );
-
-        // Call the external function passing forum id.
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum1->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
-        $expectedreturn = array(
-            'discussions' => array($expecteddiscussions),
-            'warnings' => array()
-        );
-
-        // Wait the theme to be loaded (the external_api call does that) to generate the user profiles.
-        $userpicture = new \user_picture($user1);
-        $userpicture->size = 1; // Size f1.
-        $expectedreturn['discussions'][0]['userpictureurl'] = $userpicture->get_url($PAGE)->out(false);
-
-        $userpicture = new \user_picture($user4);
-        $userpicture->size = 1; // Size f1.
-        $expectedreturn['discussions'][0]['usermodifiedpictureurl'] = $userpicture->get_url($PAGE)->out(false);
-
-        $this->assertEquals($expectedreturn, $discussions);
-
-        // Call without required view discussion capability.
-        $this->unassignUserCapability('mod/forum:viewdiscussion', $context->id, $newrole);
-        try {
-            mod_forum_external::get_forum_discussions_paginated($forum1->id);
-            $this->fail('Exception expected due to missing capability.');
-        } catch (\moodle_exception $e) {
-            $this->assertEquals('noviewdiscussionspermission', $e->errorcode);
-        }
-
-        // Unenrol user from second course.
-        $enrol->unenrol_user($instance1, $user1->id);
-
-        // Call for the second course we unenrolled the user from, make sure exception thrown.
-        try {
-            mod_forum_external::get_forum_discussions_paginated($forum1->id);
-            $this->fail('Exception expected due to being unenrolled from the course.');
-        } catch (\moodle_exception $e) {
-            $this->assertEquals('requireloginerror', $e->errorcode);
-        }
-
-        $this->setAdminUser();
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum1->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
-        $this->assertTrue($discussions['discussions'][0]['canlock']);
-    }
-
-    /**
-     * Test get forum discussions paginated (qanda forums)
-     */
-    public function test_mod_forum_get_forum_discussions_paginated_qanda() {
-
-        $this->resetAfterTest(true);
-
-        // Create courses to add the modules.
-        $course = self::getDataGenerator()->create_course();
-
-        $user1 = self::getDataGenerator()->create_user();
-        $user2 = self::getDataGenerator()->create_user();
-
-        // First forum with tracking off.
-        $record = new \stdClass();
-        $record->course = $course->id;
-        $record->type = 'qanda';
-        $forum = self::getDataGenerator()->create_module('forum', $record);
-
-        // Add discussions to the forums.
-        $discussionrecord = new \stdClass();
-        $discussionrecord->course = $course->id;
-        $discussionrecord->userid = $user2->id;
-        $discussionrecord->forum = $forum->id;
-        $discussion = self::getDataGenerator()->get_plugin_generator('mod_forum')->create_discussion($discussionrecord);
-
-        self::setAdminUser();
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
-
-        $this->assertCount(1, $discussions['discussions']);
-        $this->assertCount(0, $discussions['warnings']);
-
-        self::setUser($user1);
-        $this->getDataGenerator()->enrol_user($user1->id, $course->id);
-
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
-
-        $this->assertCount(1, $discussions['discussions']);
-        $this->assertCount(0, $discussions['warnings']);
-
-    }
-
-    /**
      * Test get forum discussions
      */
     public function test_mod_forum_get_forum_discussions() {
@@ -1716,8 +1514,8 @@ class externallib_test extends externallib_advanced_testcase {
         $createddiscussion = mod_forum_external::add_discussion($forum->id, 'the subject', 'some text here...');
         $createddiscussion = external_api::clean_returnvalue(mod_forum_external::add_discussion_returns(), $createddiscussion);
 
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
+        $discussions = mod_forum_external::get_forum_discussions($forum->id);
+        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_returns(), $discussions);
 
         $this->assertCount(1, $discussions['discussions']);
         $this->assertCount(0, $discussions['warnings']);
@@ -1731,8 +1529,8 @@ class externallib_test extends externallib_advanced_testcase {
                                                                 array('options' => array('name' => 'discussionpinned',
                                                                                          'value' => true)));
         $discussion3 = mod_forum_external::add_discussion($forum->id, 'the non pinnedsubject', 'some 3 text here...');
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
+        $discussions = mod_forum_external::get_forum_discussions($forum->id);
+        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_returns(), $discussions);
         $this->assertCount(3, $discussions['discussions']);
         $this->assertEquals($discussion2pinned['discussionid'], $discussions['discussions'][0]['discussion']);
 
@@ -1776,8 +1574,8 @@ class externallib_test extends externallib_advanced_testcase {
                                                                 $dummytext, -1, $options);
         $createddiscussion = external_api::clean_returnvalue(mod_forum_external::add_discussion_returns(), $createddiscussion);
 
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
+        $discussions = mod_forum_external::get_forum_discussions($forum->id);
+        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_returns(), $discussions);
 
         $this->assertCount(4, $discussions['discussions']);
         $this->assertCount(0, $createddiscussion['warnings']);
@@ -1860,8 +1658,8 @@ class externallib_test extends externallib_advanced_testcase {
         $discussion = mod_forum_external::add_discussion($forum->id, 'the subject', 'some text here...', $group->id);
         $discussion = external_api::clean_returnvalue(mod_forum_external::add_discussion_returns(), $discussion);
 
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
+        $discussions = mod_forum_external::get_forum_discussions($forum->id);
+        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_returns(), $discussions);
 
         $this->assertCount(1, $discussions['discussions']);
         $this->assertCount(0, $discussions['warnings']);
@@ -1872,8 +1670,8 @@ class externallib_test extends externallib_advanced_testcase {
         $discussion = mod_forum_external::add_discussion($forum->id, 'the subject', 'some text here...');
         $discussion = external_api::clean_returnvalue(mod_forum_external::add_discussion_returns(), $discussion);
 
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
+        $discussions = mod_forum_external::get_forum_discussions($forum->id);
+        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_returns(), $discussions);
 
         $this->assertCount(2, $discussions['discussions']);
         $this->assertCount(0, $discussions['warnings']);
@@ -1888,8 +1686,8 @@ class externallib_test extends externallib_advanced_testcase {
         $discussion = mod_forum_external::add_discussion($forum->id, 'the subject', 'some text here...');
         $discussion = external_api::clean_returnvalue(mod_forum_external::add_discussion_returns(), $discussion);
 
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
+        $discussions = mod_forum_external::get_forum_discussions($forum->id);
+        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_returns(), $discussions);
 
         $this->assertCount(3, $discussions['discussions']);
         $this->assertCount(0, $discussions['warnings']);
@@ -2323,8 +2121,8 @@ class externallib_test extends externallib_advanced_testcase {
         $discussionrecord->messagetrust  = trusttext_trusted($context);
         $discussion2 = self::getDataGenerator()->get_plugin_generator('mod_forum')->create_discussion($discussionrecord);
 
-        $discussions = mod_forum_external::get_forum_discussions_paginated($forum->id);
-        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_paginated_returns(), $discussions);
+        $discussions = mod_forum_external::get_forum_discussions($forum->id);
+        $discussions = external_api::clean_returnvalue(mod_forum_external::get_forum_discussions_returns(), $discussions);
 
         $this->assertCount(2, $discussions['discussions']);
         $this->assertCount(0, $discussions['warnings']);
