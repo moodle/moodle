24b5e8036533f34e3a3e9ba8624f534c6f5870aa
Paul Holden <paulh@moodle.com>
2024-01-30 14:32:25 +0000

    MDL-80692 calendar: calculate today relative to users own timezone.
    Co-authored-by: Archil Abuladze <armiworker@gmail.com>

--- calendar/classes/external/day_exporter.php
+++ calendar/classes/external/day_exporter.php
@@ -14,14 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Contains event class for displaying the day view.
- *
- * @package   core_calendar
- * @copyright 2017 Andrew Nicols <andrew@nicols.co.uk>
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 namespace core_calendar\external;
 
 defined('MOODLE_INTERNAL') || die();
@@ -29,6 +21,8 @@ defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot . '/calendar/lib.php');
 
 use core\external\exporter;
+use core_date;
+use DateTimeImmutable;
 use renderer_base;
 use moodle_url;
 
@@ -166,7 +160,10 @@ class day_exporter extends exporter {
         $timestamp = $this->data[0];
         // Need to account for user's timezone.
         $usernow = usergetdate(time());
-        $today = new \DateTimeImmutable();
+        $today = new DateTimeImmutable(
+            timezone: core_date::get_user_timezone_object(),
+        );
+
         // The start time should use the day's date but the current
         // time of the day (adjusted for user's timezone).
         $neweventstarttime = $today->setTimestamp($timestamp)->setTime(
