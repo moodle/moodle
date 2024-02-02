435ee33af1453e7ad398418a62cbd6357908306b
Tim Hunt <T.J.Hunt@open.ac.uk>
2024-02-02 10:28:10 +0000

    MDL-80808 quiz: improve message when the student can't review

--- mod/quiz/tests/quizobj_test.php
+++ mod/quiz/tests/quizobj_test.php
@@ -16,8 +16,10 @@
 
 namespace mod_quiz;
 
+use basic_testcase;
 use mod_quiz\question\display_options;
 use mod_quiz\quiz_settings;
+use stdClass;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -30,33 +32,99 @@ require_once($CFG->dirroot . '/mod/quiz/locallib.php');
  * @package    mod_quiz
  * @copyright  2008 The Open University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @covers \mod_quiz\quiz_settings
  */
-class quizobj_test extends \basic_testcase {
-    public function test_cannot_review_message() {
-        $quiz = new \stdClass();
-        $quiz->reviewattempt = 0x10010;
-        $quiz->timeclose = 0;
-        $quiz->attempts = 0;
+class quizobj_test extends basic_testcase {
+    /**
+     * Test cases for {@see test_cannot_review_message()}.
+     *
+     * @return array[]
+     */
+    public static function cannot_review_message_testcases(): array {
+        return [
+            // Review       Time close
+            // Later close  quiz attempt    When                Expected
+            // Quiz with no close date.
+            [false, false, null, null, display_options::DURING, ''],
+            [false, false, null, -60, display_options::IMMEDIATELY_AFTER, 'noreview'],
+            [false, false, null, -180, display_options::LATER_WHILE_OPEN, 'noreview'],
+            [false, false, null, -180, display_options::AFTER_CLOSE, 'noreview'],
+            [false, true, null, null, display_options::DURING, ''],
+            [false, true, null, -60, display_options::IMMEDIATELY_AFTER, 'noreview'],
+            [false, true, null, -180, display_options::LATER_WHILE_OPEN, 'noreview'],
+            [false, true, null, -180, display_options::AFTER_CLOSE, 'noreview'],
+            // Quiz with a close in the future date, review only after close.
+            [false, true, 300, null, display_options::DURING, ''],
+            [false, true, 300, -60, display_options::IMMEDIATELY_AFTER, 300],
+            [false, true, 300, -180, display_options::LATER_WHILE_OPEN, 300],
+            // Quiz with a close in the future date, review later while open, or after close.
+            [true, true, 300, null, display_options::DURING, ''],
+            [true, true, 300, -60, display_options::IMMEDIATELY_AFTER, 60],
+            [true, false, 300, -60, display_options::IMMEDIATELY_AFTER, 60],
+            // Quiz with no closer date, review later while open.
+            [true, false, 300, null, display_options::DURING, ''],
+            [true, false, 300, -60, display_options::IMMEDIATELY_AFTER, 60],
+        ];
+    }
 
-        $cm = new \stdClass();
+    /**
+     * Unit test for {@see quiz_settings::cannot_review_message()}.
+     *
+     * @dataProvider cannot_review_message_testcases
+     * @param bool $reviewlater whether the quiz allows reivew 'later while the quiz is still open'.
+     * @param bool $reviewafterclose whether the quiz allows rievew 'after the quiz is closed'.
+     * @param int|null $quizcloseoffset quiz close date, relative to now. Null means not set.
+     * @param int|null $attemptsubmitoffset quiz attempt sumbite time relative to now. Null means not submitted yet.
+     * @param int $attemptstate current state of the attempt, one of the display_options constants.
+     * @param string|int $expectation expected result: '' means '', 'noreview' means noreview lang string,
+     *      int means noreviewuntil with that time relative to now.
+     */
+    public function test_cannot_review_message(
+        bool $reviewlater,
+        bool $reviewafterclose,
+        ?int $quizcloseoffset,
+        ?int $attemptsubmitoffset,
+        int $attemptstate,
+        string|int $expectation
+    ): void {
+        $quiz = new stdClass();
+        $now = time();
+
+        $cm = new stdClass();
         $cm->id = 123;
 
-        $quizobj = new quiz_settings($quiz, $cm, new \stdClass(), false);
+        // Prepare quiz settings.
+        $quiz->reviewattempt = display_options::DURING;
+        if ($reviewlater) {
+            $quiz->reviewattempt |= display_options::LATER_WHILE_OPEN;
+        }
+        if ($reviewafterclose) {
+            $quiz->reviewattempt |= display_options::AFTER_CLOSE;
+        }
+        $quiz->attempts = 0;
 
-        $this->assertEquals('',
-            $quizobj->cannot_review_message(display_options::DURING));
-        $this->assertEquals('',
-            $quizobj->cannot_review_message(display_options::IMMEDIATELY_AFTER));
-        $this->assertEquals(get_string('noreview', 'quiz'),
-            $quizobj->cannot_review_message(display_options::LATER_WHILE_OPEN));
-        $this->assertEquals(get_string('noreview', 'quiz'),
-            $quizobj->cannot_review_message(display_options::AFTER_CLOSE));
+        if ($quizcloseoffset === null) {
+            $quiz->timeclose = 0;
+        } else {
+            $quiz->timeclose = $now + $quizcloseoffset;
+        }
+        if ($attemptsubmitoffset === null) {
+            $submittime = 0;
+        } else {
+            $submittime = $now + $attemptsubmitoffset;
+        }
 
-        $closetime = time() + 10000;
-        $quiz->timeclose = $closetime;
-        $quizobj = new quiz_settings($quiz, $cm, new \stdClass(), false);
+        $quizobj = new quiz_settings($quiz, $cm, new stdClass(), false);
 
-        $this->assertEquals(get_string('noreviewuntil', 'quiz', userdate($closetime)),
-            $quizobj->cannot_review_message(display_options::LATER_WHILE_OPEN));
+        // Prepare expected message.
+        if ($expectation === 'noreview') {
+            $expectation = get_string('noreview', 'quiz');
+        } else if (is_int($expectation)) {
+            $expectation = get_string('noreviewuntil', 'quiz', userdate($now + $expectation));
+        }
+
+        // Test.
+        $this->assertEquals($expectation,
+            $quizobj->cannot_review_message($attemptstate, false, $submittime));
     }
 }
