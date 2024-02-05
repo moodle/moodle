ef8bf40615f04b96be50247e27d0f20adfb2a8e1
AAS1214 <angel1214.as@gmail.com>
2024-02-06 01:54:34 +0800

    compatible plugin zip

--- local/auto_proctor/proctor_tools/camera_monitoring/save_cam_capture.php
+++ local/auto_proctor/proctor_tools/camera_monitoring/save_cam_capture.php
@@ -1,9 +1,32 @@
 <?php
+// This file is part of Moodle Course Rollover Plugin
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     local_auto_proctor
+ * @author      Angelica
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @var stdClass $plugin
+*/
+require_once(__DIR__ . '/../../../../config.php');
+
 $dataUri = $_POST['dataUri'];
 $filename = $_POST['filename'];
 
 // Specify the folder for saving captures
-$folderPath = '../evidences/camera_capture_evidence'; // Removed leading slash
+$folderPath = '../evidences/camera_capture_evidence/'; // Removed leading slash
 
 // Ensure the folder exists
 if (!file_exists($folderPath)) {
