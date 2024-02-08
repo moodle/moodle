4bfe3249bdcd5f3b7aae8fda93a68b2ce03f7cc8
Derick Turner <derick@e-learndesign.co.uk>
2024-02-08 17:48:04 +0000

    IOMAD: deprecated user_preference_allow_ajax_update in selectors

--- local/course_selector/lib.php
+++ local/course_selector/lib.php
@@ -652,7 +652,6 @@ abstract class course_selector_base {
                 // element with name="frog".
                 '<input type="checkbox" id="' . $name . 'id" name="' . $name . '" value="1"' .
                 $checked . ' /> ' .  '<label for="' . $name . 'id">' . $label . "</label></p>\n";
-        user_preference_allow_ajax_update($name, PARAM_BOOL);
         return $output;
     }
 
