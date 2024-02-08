4e3c61ba0e43c7612af73381d0426d6cc6ae2cb4
Ilya Tregubov <ilya.a.tregubov@gmail.com>
2024-02-08 10:54:50 +0700

    MDL-80504 forum: Fix seperate group mode

--- enrol/locallib.php
+++ enrol/locallib.php
@@ -563,26 +563,40 @@ class course_enrolment_manager {
      * @param int $page Starting at 0.
      * @param int $perpage Number of users returned per page.
      * @param bool $returnexactcount Return the exact total users using count_record or not.
+     * @param ?int $contextid Context ID we are in - we might use search on activity level and its group mode can be different from course group mode.
      * @return array with two or three elements:
      *      int totalusers Number users matching the search. (This element only exist if $returnexactcount was set to true)
      *      array users List of user objects returned by the query.
      *      boolean moreusers True if there are still more users, otherwise is False.
      */
     public function search_users(string $search = '', bool $searchanywhere = false, int $page = 0, int $perpage = 25,
-            bool $returnexactcount = false) {
+            bool $returnexactcount = false, ?int $contextid = null) {
         global $USER;
 
         [$ufields, $joins, $params, $wherecondition] = $this->get_basic_search_conditions($search, $searchanywhere);
 
-        $groupmode = groups_get_course_groupmode($this->course);
-        if ($groupmode == SEPARATEGROUPS && !has_capability('moodle/site:accessallgroups', $this->context)) {
+        if (isset($contextid)) {
+            // If contextid is set, we need to determine the group mode that should be used (module or course).
+            [$context, $course, $cm] = get_context_info_array($contextid);
+            // If cm instance is returned, then use the group mode from the module, otherwise get the course group mode.
+            $groupmode = $cm ? groups_get_activity_groupmode($cm, $course) : groups_get_course_groupmode($this->course);
+        } else {
+            // Otherwise, default to the group mode of the course.
+            $context = $this->context;
+            $groupmode = groups_get_course_groupmode($this->course);
+        }
+
+        if ($groupmode == SEPARATEGROUPS && !has_capability('moodle/site:accessallgroups', $context)) {
             $groups = groups_get_all_groups($this->course->id, $USER->id, 0, 'g.id');
             $groupids = array_column($groups, 'id');
+            if (!$groupids) {
+                return ['totalusers' => 0, 'users' => [], 'moreusers' => false];
+            }
         } else {
             $groupids = [];
         }
 
-        [$enrolledsql, $enrolledparams] = get_enrolled_sql($this->context, '', $groupids);
+        [$enrolledsql, $enrolledparams] = get_enrolled_sql($context, '', $groupids);
 
         $fields      = 'SELECT ' . $ufields;
         $countfields = 'SELECT COUNT(u.id)';
