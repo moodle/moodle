e0a482e3b3108a5f3e46c335005d58214f335ed5
Amaia Anabitarte <aanabit@yahoo.es>
2024-02-23 12:32:34 +0100

    MDL-81023 core_course: Move deprecated behat steps to course directory

--- lib/tests/behat/behat_deprecated.php
+++ lib/tests/behat/behat_deprecated.php
@@ -17,7 +17,6 @@
 // NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
 
 require_once(__DIR__ . '/../../../lib/behat/behat_deprecated_base.php');
-use Behat\Gherkin\Node\TableNode as TableNode;
 
 /**
  * Steps definitions that are now deprecated and will be removed in the next releases.
@@ -32,85 +31,4 @@ use Behat\Gherkin\Node\TableNode as TableNode;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class behat_deprecated extends behat_deprecated_base {
-    /**
-     * Opens the activity chooser and opens the activity/resource form page. Sections 0 and 1 are also allowed on frontpage.
-     *
-     * @Given /^I add a "(?P<activity_or_resource_name_string>(?:[^"]|\\")*)" to section "(?P<section_number>\d+)"$/
-     * @throws \Behat\Mink\Exception\ElementNotFoundException Thrown by behat_base::find
-     * @param string $activity
-     * @param int $section
-     * @deprecated Since Moodle 4.4
-     */
-    public function i_add_to_section($activity, $section) {
-        $this->deprecated_message([
-            'behat_course::i_add_to_course_section',
-            'behat_course::i_add_to_section_using_the_activity_chooser',
-        ]);
-
-        $this->require_javascript('Please use the \'the following "activity" exists:\' data generator instead.');
-
-        if ($this->getSession()->getPage()->find('css', 'body#page-site-index') && (int) $section <= 1) {
-            // We are on the frontpage.
-            if ($section) {
-                // Section 1 represents the contents on the frontpage.
-                $sectionxpath = "//body[@id='page-site-index']" .
-                    "/descendant::div[contains(concat(' ',normalize-space(@class),' '),' sitetopic ')]";
-            } else {
-                // Section 0 represents "Site main menu" block.
-                $sectionxpath = "//*[contains(concat(' ',normalize-space(@class),' '),' block_site_main_menu ')]";
-            }
-        } else {
-            // We are inside the course.
-            $sectionxpath = "//li[@id='section-" . $section . "']";
-        }
-
-        // Clicks add activity or resource section link.
-        $sectionnode = $this->find('xpath', $sectionxpath);
-        $this->execute('behat_general::i_click_on_in_the', [
-            "//button[@data-action='open-chooser' and not(@data-beforemod)]",
-            'xpath',
-            $sectionnode,
-            'NodeElement',
-        ]);
-
-        // Clicks the selected activity if it exists.
-        $activityliteral = behat_context_helper::escape(ucfirst($activity));
-        $activityxpath = "//div[contains(concat(' ', normalize-space(@class), ' '), ' modchooser ')]" .
-            "/descendant::div[contains(concat(' ', normalize-space(@class), ' '), ' optioninfo ')]" .
-            "/descendant::div[contains(concat(' ', normalize-space(@class), ' '), ' optionname ')]" .
-            "[normalize-space(.)=$activityliteral]" .
-            "/parent::a";
-
-        $this->execute('behat_general::i_click_on', [$activityxpath, 'xpath']);
-    }
-
-    /**
-     * Adds the selected activity/resource filling the form data with the specified field/value pairs.
-     *
-     * Sections 0 and 1 are also allowed on frontpage.
-     *
-     * @When /^I add a "(?P<activity_or_resource_name_string>(?:[^"]|\\")*)" to section "(?P<section_number>\d+)" and I fill the form with:$/
-     * @param string $activity The activity name
-     * @param int $section The section number
-     * @param TableNode $data The activity field/value data
-     * @deprecated Since Moodle 4.4
-     */
-    public function i_add_to_section_and_i_fill_the_form_with($activity, $section, TableNode $data) {
-        $this->deprecated_message(['behat_course::i_add_to_course_section_and_i_fill_the_form_with']);
-
-        // Add activity to section.
-        $this->execute(
-            "behat_course::i_add_to_section",
-            [$this->escape($activity), $this->escape($section)]
-        );
-
-        // Wait to be redirected.
-        $this->execute('behat_general::wait_until_the_page_is_ready');
-
-        // Set form fields.
-        $this->execute("behat_forms::i_set_the_following_fields_to_these_values", $data);
-
-        // Save course settings.
-        $this->execute("behat_forms::press_button", get_string('savechangesandreturntocourse'));
-    }
 }
