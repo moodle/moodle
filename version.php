b2fa19f45df7bb802371cc4285a196fb927da3ef
Sara Arjona <sara@moodle.com>
2024-03-05 11:22:44 +0100

    on-demand release 4.4dev+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024030100.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2024030500.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.4dev (Build: 20240301)'; // Human-friendly version name
+$release  = '4.4dev+ (Build: 20240305)'; // Human-friendly version name
 $branch   = '404';                     // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
