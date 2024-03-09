5e87af83966cde3489c5663eb369501fed11e83f
Adam Olley <adam.olley@openlms.net>
2024-03-10 01:04:51 +1030

    MDL-81182 factor_sms: Correct path for config.php require
    Without this, the path is incorrect and can, in some circumstances (if
    config.php is a symlink for one) - result in PHP requiring the config
    file twice.

--- admin/tool/mfa/factor/sms/editphonenumber.php
+++ admin/tool/mfa/factor/sms/editphonenumber.php
@@ -22,7 +22,7 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(__DIR__ . '../../../../../../config.php');
+require_once(__DIR__ . '/../../../../../config.php');
 
 require_login(null, false);
 if (isguestuser()) {
