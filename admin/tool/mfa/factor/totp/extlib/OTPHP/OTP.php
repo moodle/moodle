9762901cba48761ec2078b8a18c8cec82d9144b6
Daniel Ziegenberg <daniel@ziegenberg.at>
2024-03-10 04:04:37 +0100

    MDL-79678 libraries: Update OTPHP to 11.3.0
    Notable changes:
    - The OTPHP library requires now at least PHP 8.1.
    - The OTPHP library now relies on the MBString extension. This extension
      is now a required dependency.
    - The OTPHP library now utilizes a PSR-20: clock.
    
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- admin/tool/mfa/factor/totp/extlib/OTPHP/OTP.php
+++ admin/tool/mfa/factor/totp/extlib/OTPHP/OTP.php
@@ -2,42 +2,33 @@
 
 declare(strict_types=1);
 
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2014-2018 Spomky-Labs
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
- */
-
 namespace OTPHP;
 
-use Assert\Assertion;
+use Exception;
+use InvalidArgumentException;
 use ParagonIE\ConstantTime\Base32;
+use RuntimeException;
+use function assert;
+use function chr;
+use function count;
+use function is_string;
+use const STR_PAD_LEFT;
 
 abstract class OTP implements OTPInterface
 {
     use ParameterTrait;
 
-    /**
-     * OTP constructor.
-     *
-     * @param string|null $secret
-     * @param string      $digest
-     * @param int         $digits
-     */
-    protected function __construct($secret, string $digest, int $digits)
-    {
-        $this->setSecret($secret);
-        $this->setDigest($digest);
-        $this->setDigits($digits);
-    }
+    private const DEFAULT_SECRET_SIZE = 64;
 
     /**
-     * {@inheritdoc}
+     * @param non-empty-string $secret
      */
-    public function getQrCodeUri(string $uri = 'https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl={PROVISIONING_URI}', string $placeholder = '{PROVISIONING_URI}'): string
+    protected function __construct(string $secret)
+    {
+        $this->setSecret($secret);
+    }
+
+    public function getQrCodeUri(string $uri, string $placeholder): string
     {
         $provisioning_uri = urlencode($this->getProvisioningUri());
 
@@ -45,38 +36,52 @@ abstract class OTP implements OTPInterface
     }
 
     /**
-     * @param int $input
+     * @param 0|positive-int $input
+     */
+    public function at(int $input): string
+    {
+        return $this->generateOTP($input);
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    final protected static function generateSecret(): string
+    {
+        return Base32::encodeUpper(random_bytes(self::DEFAULT_SECRET_SIZE));
+    }
+
+    /**
+     * The OTP at the specified input.
      *
-     * @return string The OTP at the specified input
+     * @param 0|positive-int $input
+     *
+     * @return non-empty-string
      */
     protected function generateOTP(int $input): string
     {
-        $hash = hash_hmac($this->getDigest(), $this->intToByteString($input), $this->getDecodedSecret());
-        $hmac = [];
-        foreach (str_split($hash, 2) as $hex) {
-            $hmac[] = hexdec($hex);
-        }
-        $offset = $hmac[count($hmac) - 1] & 0xF;
-        $code = ($hmac[$offset + 0] & 0x7F) << 24 | ($hmac[$offset + 1] & 0xFF) << 16 | ($hmac[$offset + 2] & 0xFF) << 8 | ($hmac[$offset + 3] & 0xFF);
-        $otp = $code % pow(10, $this->getDigits());
+        $hash = hash_hmac($this->getDigest(), $this->intToByteString($input), $this->getDecodedSecret(), true);
+        $unpacked = unpack('C*', $hash);
+        $unpacked !== false || throw new InvalidArgumentException('Invalid data.');
+        $hmac = array_values($unpacked);
+
+        $offset = ($hmac[count($hmac) - 1] & 0xF);
+        $code = ($hmac[$offset] & 0x7F) << 24 | ($hmac[$offset + 1] & 0xFF) << 16 | ($hmac[$offset + 2] & 0xFF) << 8 | ($hmac[$offset + 3] & 0xFF);
+        $otp = $code % (10 ** $this->getDigits());
 
         return str_pad((string) $otp, $this->getDigits(), '0', STR_PAD_LEFT);
     }
 
     /**
-     * {@inheritdoc}
+     * @param array<non-empty-string, mixed> $options
      */
-    public function at(int $timestamp): string
+    protected function filterOptions(array &$options): void
     {
-        return $this->generateOTP($timestamp);
-    }
-
-    /**
-     * @param array $options
-     */
-    protected function filterOptions(array &$options)
-    {
-        foreach (['algorithm' => 'sha1', 'period' => 30, 'digits' => 6] as $key => $default) {
+        foreach ([
+            'algorithm' => 'sha1',
+            'period' => 30,
+            'digits' => 6,
+        ] as $key => $default) {
             if (isset($options[$key]) && $default === $options[$key]) {
                 unset($options[$key]);
             }
@@ -86,61 +91,60 @@ abstract class OTP implements OTPInterface
     }
 
     /**
-     * @param string $type
-     * @param array  $options
+     * @param non-empty-string $type
+     * @param array<non-empty-string, mixed> $options
      *
-     * @return string
+     * @return non-empty-string
      */
     protected function generateURI(string $type, array $options): string
     {
         $label = $this->getLabel();
-        Assertion::string($label, 'The label is not set.');
-        Assertion::false($this->hasColon($label), 'Label must not contain a colon.');
-        $options = array_merge($options, $this->getParameters());
+        is_string($label) || throw new InvalidArgumentException('The label is not set.');
+        $this->hasColon($label) === false || throw new InvalidArgumentException('Label must not contain a colon.');
+        $options = [...$options, ...$this->getParameters()];
         $this->filterOptions($options);
         $params = str_replace(['+', '%7E'], ['%20', '~'], http_build_query($options, '', '&'));
 
-        return sprintf('otpauth://%s/%s?%s', $type, rawurlencode((null !== $this->getIssuer() ? $this->getIssuer().':' : '').$label), $params);
+        return sprintf(
+            'otpauth://%s/%s?%s',
+            $type,
+            rawurlencode(($this->getIssuer() !== null ? $this->getIssuer() . ':' : '') . $label),
+            $params
+        );
     }
 
     /**
-     * @return string
-     */
-    private function getDecodedSecret(): string
-    {
-        try {
-            $secret = Base32::decodeUpper($this->getSecret());
-        } catch (\Exception $e) {
-            throw new \RuntimeException('Unable to decode the secret. Is it correctly base32 encoded?');
-        }
-
-        return $secret;
-    }
-
-    /**
-     * @param int $int
-     *
-     * @return string
-     */
-    private function intToByteString(int $int): string
-    {
-        $result = [];
-        while (0 !== $int) {
-            $result[] = chr($int & 0xFF);
-            $int >>= 8;
-        }
-
-        return str_pad(implode(array_reverse($result)), 8, "\000", STR_PAD_LEFT);
-    }
-
-    /**
-     * @param string $safe
-     * @param string $user
-     *
-     * @return bool
+     * @param non-empty-string $safe
+     * @param non-empty-string $user
      */
     protected function compareOTP(string $safe, string $user): bool
     {
         return hash_equals($safe, $user);
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    private function getDecodedSecret(): string
+    {
+        try {
+            $decoded = Base32::decodeUpper($this->getSecret());
+        } catch (Exception) {
+            throw new RuntimeException('Unable to decode the secret. Is it correctly base32 encoded?');
+        }
+        assert($decoded !== '');
+
+        return $decoded;
+    }
+
+    private function intToByteString(int $int): string
+    {
+        $result = [];
+        while ($int !== 0) {
+            $result[] = chr($int & 0xFF);
+            $int >>= 8;
+        }
+
+        return str_pad(implode('', array_reverse($result)), 8, "\000", STR_PAD_LEFT);
     }
 }
--- admin/tool/mfa/factor/totp/extlib/OTPHP/OTPInterface.php
+++ admin/tool/mfa/factor/totp/extlib/OTPHP/OTPInterface.php
@@ -1,123 +1,132 @@
 <?php
 
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2014-2018 Spomky-Labs
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
- */
+declare(strict_types=1);
 
 namespace OTPHP;
 
 interface OTPInterface
 {
-    /**
-     * @param int $timestamp
-     *
-     * @return string Return the OTP at the specified timestamp
-     */
-    public function at(int $timestamp): string;
+    public const DEFAULT_DIGITS = 6;
+
+    public const DEFAULT_DIGEST = 'sha1';
 
     /**
-     * Verify that the OTP is valid with the specified input.
-     * If no input is provided, the input is set to a default value or false is returned.
+     * Create a OTP object from an existing secret.
      *
-     * @param string   $otp
-     * @param int|null $input
-     * @param int|null $window
-     *
-     * @return bool
+     * @param non-empty-string $secret
      */
-    public function verify(string $otp, $input = null, $window = null): bool;
+    public static function createFromSecret(string $secret): self;
 
     /**
-     * @return string The secret of the OTP
+     * Create a new OTP object. A random 64 bytes secret will be generated.
+     */
+    public static function generate(): self;
+
+    /**
+     * @param non-empty-string $secret
+     */
+    public function setSecret(string $secret): void;
+
+    public function setDigits(int $digits): void;
+
+    /**
+     * @param non-empty-string $digest
+     */
+    public function setDigest(string $digest): void;
+
+    /**
+     * Generate the OTP at the specified input.
+     *
+     * @param 0|positive-int $input
+     *
+     * @return non-empty-string Return the OTP at the specified timestamp
+     */
+    public function at(int $input): string;
+
+    /**
+     * Verify that the OTP is valid with the specified input. If no input is provided, the input is set to a default
+     * value or false is returned.
+     *
+     * @param non-empty-string $otp
+     * @param null|0|positive-int $input
+     * @param null|0|positive-int $window
+     */
+    public function verify(string $otp, null|int $input = null, null|int $window = null): bool;
+
+    /**
+     * @return non-empty-string The secret of the OTP
      */
     public function getSecret(): string;
 
     /**
-     * @param string $label The label of the OTP
+     * @param non-empty-string $label The label of the OTP
      */
-    public function setLabel(string $label);
+    public function setLabel(string $label): void;
 
     /**
-     * @return string|null The label of the OTP
+     * @return non-empty-string|null The label of the OTP
      */
-    public function getLabel();
+    public function getLabel(): null|string;
 
     /**
-     * @return string|null The issuer
+     * @return non-empty-string|null The issuer
      */
-    public function getIssuer();
+    public function getIssuer(): ?string;
 
     /**
-     * @param string $issuer
-     *
-     * @throws \InvalidArgumentException
+     * @param non-empty-string $issuer
      */
-    public function setIssuer(string $issuer);
+    public function setIssuer(string $issuer): void;
 
     /**
      * @return bool If true, the issuer will be added as a parameter in the provisioning URI
      */
     public function isIssuerIncludedAsParameter(): bool;
 
-    /**
-     * @param bool $issuer_included_as_parameter
-     *
-     * @return $this
-     */
-    public function setIssuerIncludedAsParameter(bool $issuer_included_as_parameter);
+    public function setIssuerIncludedAsParameter(bool $issuer_included_as_parameter): void;
 
     /**
-     * @return int Number of digits in the OTP
+     * @return positive-int Number of digits in the OTP
      */
     public function getDigits(): int;
 
     /**
-     * @return string Digest algorithm used to calculate the OTP. Possible values are 'md5', 'sha1', 'sha256' and 'sha512'
+     * @return non-empty-string Digest algorithm used to calculate the OTP. Possible values are 'md5', 'sha1', 'sha256' and 'sha512'
      */
     public function getDigest(): string;
 
     /**
-     * @param string $parameter
-     *
-     * @return null|mixed
+     * @param non-empty-string $parameter
      */
-    public function getParameter(string $parameter);
+    public function getParameter(string $parameter): mixed;
 
     /**
-     * @param string $parameter
-     *
-     * @return bool
+     * @param non-empty-string $parameter
      */
     public function hasParameter(string $parameter): bool;
 
     /**
-     * @return array
+     * @return array<non-empty-string, mixed>
      */
     public function getParameters(): array;
 
     /**
-     * @param string $parameter
-     * @param mixed  $value
-     *
-     * @return $this
+     * @param non-empty-string $parameter
      */
-    public function setParameter(string $parameter, $value);
+    public function setParameter(string $parameter, mixed $value): void;
 
     /**
-     * @return string Get the provisioning URI
+     * Get the provisioning URI.
+     *
+     * @return non-empty-string
      */
     public function getProvisioningUri(): string;
 
     /**
-     * @param string $uri         The Uri of the QRCode generator with all parameters. By default the Googgle Chart API is used. This Uri MUST contain a placeholder that will be replaced by the method.
-     * @param string $placeholder The placeholder to be replaced in the QR Code generator URI. Default value is {PROVISIONING_URI}.
+     * Get the provisioning URI.
      *
-     * @return string Get the provisioning URI
+     * @param non-empty-string $uri         The Uri of the QRCode generator with all parameters. This Uri MUST contain a placeholder that will be replaced by the method.
+     * @param non-empty-string $placeholder the placeholder to be replaced in the QR Code generator URI
      */
-    public function getQrCodeUri(string $uri = 'https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl={PROVISIONING_URI}', string $placeholder = '{PROVISIONING_URI}'): string;
+    public function getQrCodeUri(string $uri, string $placeholder): string;
 }
