581cd56e511c5db75d878b986b6525c8aef05757
Renzi Delposo <113895134+httprenz@users.noreply.github.com>
2024-03-16 21:58:49 +0800

    Update ARCHIVES ICON side_navigation_bar

--- local/auto_proctor/ui/side_navigation_bar.php
+++ local/auto_proctor/ui/side_navigation_bar.php
@@ -192,16 +192,17 @@ $num_of_courses = count($course_ids);
                                     ?>
                                 </ul>
                             </li>
-                            <li>
-                                <a href = "<?php echo $CFG->wwwroot  . '/local/auto_proctor/ui/auto_proctor_dashboard.php?archives=1'?>">
-                                    <button type="button" class="flex items-center w-full p-2 text-base text-gray-50 transition duration-75 rounded-lg group hover:bg-gray-100 hover:text-gray-700" aria-controls="dropdown-layouts" data-collapse-toggle="dropdown-layouts">
-                                        <svg class="flex-shrink-0 w-6 h-6 text-gray-100 transition duration-75 group-hover:text-gray-900 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                            <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"></path>
-                                        </svg>
-                                        <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Archives</span>
-                                    </button>
-                                </a>
-                            </li>
+                        <li>
+                            <a href="<?php echo $CFG->wwwroot  . '/local/auto_proctor/ui/auto_proctor_dashboard.php?archives=1' ?>">
+                                <button type="button" class="flex items-center w-full p-2 text-base text-gray-50 transition duration-75 rounded-lg group hover:bg-gray-100 hover:text-gray-700" aria-controls="dropdown-layouts" data-collapse-toggle="dropdown-layouts">
+                                    <svg class="flex-shrink-0 w-6 h-6 text-gray-100 transition duration-75 group-hover:text-gray-900 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M10 12v1h4v-1m4 7H6a1 1 0 0 1-1-1V9h14v9a1 1 0 0 1-1 1ZM4 5h16a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z" />
+                                    </svg>
+
+                                    <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Archives</span>
+                                </button>
+                            </a>
+                        </li>
                         </ul>
                     </div>
                 </div>
