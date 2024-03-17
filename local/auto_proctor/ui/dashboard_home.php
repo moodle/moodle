e079ece1623db6e1b52f24d7e7d4b209b0a016eb
Renzi Delposo <113895134+httprenz@users.noreply.github.com>
2024-03-17 15:39:06 +0800

    Update dashboard_home.php

--- local/auto_proctor/ui/dashboard_home.php
+++ local/auto_proctor/ui/dashboard_home.php
@@ -304,8 +304,13 @@ $num_of_courses = count($course_ids);
         <div class="flex items-center mb-4 sm:mb-0">
             <form class="sm:pr-3" action="#" method="GET">
                 <label for="archive_search" class="sr-only">Search</label>
-                <div class="relative w-48 mt-1 sm:w-64 xl:w-96">
-                    <input type="text" name="email" id="archive_search" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " placeholder="⌕ Search">
+                <div class="relative mt-1 lg:w-48">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 px-4 py-2 pointer-events-none">
+                        <svg class="w-5 h-5 text-gray-500 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <input type="text" name="text" id="topbar-search" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 px-4 py-2  text-white " placeholder="Search">
                 </div>
             </form>
         </div>
