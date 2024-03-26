fa4a79d6d74081084828ee70563350f824f6d4fd
Sara Arjona <sara@moodle.com>
2024-03-26 12:31:16 +0100

    weekly release 4.2.6+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023042406.10;              // 20230424      = branching date YYYYMMDD - do not modify!
+$version  = 2023042406.11;              // 20230424      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.2.6+ (Build: 20240322)'; // Human-friendly version name
+$release  = '4.2.6+ (Build: 20240326)'; // Human-friendly version name
 $branch   = '402';                     // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
