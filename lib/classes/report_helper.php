b7ef78c4e6774a198c8974a5efb563356c10a2bb
Andrew Nicols <andrew@nicols.co.uk>
2024-03-27 11:51:30 +0800

    MDL-81327 report_log: Make group filtering logstore agnostic
    The original implementation of group filtering introduced in MDL-80565
    assumed that the log table existed in Moodle's own database. This is not
    the case of the database logstore, or any similar logstore implemetning
    the database \core\log\sql_reader interface.
    
    Furthermore this check was also applying the SQL when the user had the
    `accessallgroups` capability, or when the course was not in SEPARATE
    groups mode (no groupmode and/or visible groups).
    
    Co-authored: Laurent David <laurent.david@moodle.com>

--- lib/classes/report_helper.php
+++ lib/classes/report_helper.php
@@ -24,7 +24,9 @@
  */
 
 namespace core;
+use context_course;
 use moodle_url;
+use stdClass;
 
 /**
  * A helper class with static methods to help report plugins
@@ -109,5 +111,72 @@ class report_helper {
             $USER->course_last_report = [];
         }
         $USER->course_last_report[$id] = $url;
+    }
+
+    /**
+     * Retrieve the right SQL / params for the group filter depending on the filterparams, course and group settings.
+     *
+     * Addionnaly, it will return the list of users visible by the current user so
+     * it can be used to filter out records that are not visible. This is mainly
+     * because we cannot use joins as the log tables can be in two different databases.
+     *
+     * @param stdClass $filterparams
+     * @return array
+     */
+    public static function get_group_filter(stdClass $filterparams): array {
+        global $DB, $USER;
+        $useridfilter = null;
+        // First and just in case we are in separate group, just set the $useridfilter to the list
+        // of users visible by this user.
+        $courseid = $filterparams->courseid ?? SITEID;
+        $courseid = $courseid ?: SITEID; // Make sure that if courseid is set to 0 we use SITEID.
+        $course = get_course($courseid);
+        $groupmode = groups_get_course_groupmode($course);
+        $groupid = $filterparams->groupid ?? 0;
+        if ($groupmode == SEPARATEGROUPS || $groupid) {
+            $context = context_course::instance($courseid);
+            if ($groupid) {
+                $cgroups = [(int) $groupid];
+            } else {
+                $cgroups = groups_get_all_groups(
+                    $courseid,
+                    has_capability('moodle/site:accessallgroups', $context) ? 0 : $USER->id
+                );
+                $cgroups = array_keys($cgroups);
+                // If that's the case, limit the users to be in the groups only, defined by the filter.
+                if (has_capability('moodle/site:accessallgroups', $context) || empty($cgroups)) {
+                    $cgroups[] = USERSWITHOUTGROUP;
+                }
+            }
+            // If that's the case, limit the users to be in the groups only, defined by the filter.
+            [$groupmembersql, $groupmemberparams] = groups_get_members_ids_sql($cgroups, $context);
+            $groupusers = $DB->get_fieldset_sql($groupmembersql, $groupmemberparams);
+            $useridfilter = array_fill_keys($groupusers, true);
+        }
+        $joins = [];
+        $params = [];
+        if (empty($filterparams->userid)) {
+            if ($groupid) {
+                if ($thisgroupusers = groups_get_members($groupid)) {
+                    [$sql, $sqlfilterparams] = $DB->get_in_or_equal(
+                        array_keys($thisgroupusers),
+                        SQL_PARAMS_NAMED,
+                    );
+                    $joins[] = "userid {$sql}";
+                    $params = $sqlfilterparams;
+                } else {
+                    $joins[] = 'userid = 0'; // No users in groups, so we want something that will always be false.
+                }
+            }
+        } else {
+            $joins[] = "userid = :userid";
+            $params['userid'] = $filterparams->userid;
+        }
+
+        return [
+            'joins' => $joins,
+            'params' => $params,
+            'useridfilter' => $useridfilter,
+        ];
     }
 }
