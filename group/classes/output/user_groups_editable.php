9c4131f03d80a845f903e3d1710bfba09ea5d076
Stevani Andolo <stevani.andolo@moodle.com>
2024-04-11 14:39:24 +0800

    MDL-81354 core: Escape string of fullname

--- group/classes/output/user_groups_editable.php
+++ group/classes/output/user_groups_editable.php
@@ -79,6 +79,7 @@ class user_groups_editable extends \core\output\inplace_editable {
         }
 
         $fullname = fullname($user, has_capability('moodle/site:viewfullnames', $this->context));
+        $fullname = htmlspecialchars($fullname, ENT_QUOTES, 'utf-8');
         $this->edithint = get_string('editusersgroupsa', 'group', $fullname);
         $this->editlabel = get_string('editusersgroupsa', 'group', $fullname);
 
