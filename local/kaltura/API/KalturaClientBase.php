bce6ef908a7aa257d82a5e588c63900a0f7ca431
Carson Tam <carson.tam@ucsf.edu>
2024-04-11 13:28:50 -0700

    Added workaround to the PHP 8.1: Return types in PHP built-in class methods and deprecation notices, probably until PHP 9.0.

--- local/kaltura/API/KalturaClientBase.php
+++ local/kaltura/API/KalturaClientBase.php
@@ -48,20 +48,24 @@ class MultiRequestSubResult implements ArrayAccess
         return new MultiRequestSubResult($this->value . ':' . $name);
 	}
 
+	#[\ReturnTypeWillChange]
 	public function offsetExists($offset)
 	{
 		return true;
 	}
 
+	#[\ReturnTypeWillChange]
 	public function offsetGet($offset)
 	{
         return new MultiRequestSubResult($this->value . ':' . $offset);
 	}
 
+	#[\ReturnTypeWillChange]
 	public function offsetSet($offset, $value)
 	{
 	}
 
+	#[\ReturnTypeWillChange]
 	public function offsetUnset($offset)
 	{
 	}
