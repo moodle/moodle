8cf14f6e701cce6294b0c6166d1d432df4505553
Sara Arjona <sara@moodle.com>
2024-04-12 13:23:54 +0200

    Moodle release 4.4beta

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024041200.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2024041200.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.4dev+ (Build: 20240409)'; // Human-friendly version name
+$release  = '4.4beta (Build: 20240412)'; // Human-friendly version name
 $branch   = '404';                     // This version's branch.
-$maturity = MATURITY_ALPHA;             // This version's maturity level.
+$maturity = MATURITY_BETA;             // This version's maturity level.
