03b6d6313b060bb6823e8aa2242741bdfd384a1f
Anupama Sarjoshi <anupama.sarjoshi@open.ac.uk>
2024-04-22 18:10:23 +0100

    MDL-81407 qbank_columnsortorder: Fix error displaying hiddencols
    When the question custom fields were deleted, if there were any
    hidden columns in qbank Column sort order referring those fields they
    were throwing 'Custom field does not exist' exception. Changes done to
    ignore such fields and display valid hiddencols, as these references
    were breaking the qbank Column sort order and Question bank pages.

--- question/bank/customfields/classes/custom_field_column.php
+++ question/bank/customfields/classes/custom_field_column.php
@@ -44,14 +44,18 @@ class custom_field_column extends column_base {
         $this->field = $field;
     }
 
-    public static function from_column_name(view $view, string $columnname): custom_field_column {
+    public static function from_column_name(view $view, string $columnname, bool $ingoremissing = false): ?custom_field_column {
         $handler = question_handler::create();
         foreach ($handler->get_fields() as $field) {
             if ($field->get('shortname') == $columnname) {
                 return new static($view, $field);
             }
         }
-        throw new \coding_exception('Custom field ' . $columnname . ' does not exist.');
+        if ($ingoremissing) {
+            return null;
+        } else {
+            throw new \coding_exception('Custom field ' . $columnname . ' does not exist.');
+        }
     }
 
     /**
