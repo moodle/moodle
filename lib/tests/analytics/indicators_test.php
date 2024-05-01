c9d2e72943eef29358ca0b8675c1b1e416b68ccf
Paul Holden <paulh@moodle.com>
2024-05-01 15:27:08 +0100

    MDL-81282 dml: replace occurrences of large integers with new constant.
    Where calling code was previously using either the 32-bit limit, or a
    value equal to SQL_INT_MAX inside a database read/write, instead use
    our new constant.

--- lib/tests/analytics/indicators_test.php
+++ lib/tests/analytics/indicators_test.php
@@ -86,8 +86,8 @@ class indicators_test extends \advanced_testcase {
 
         // Test any access before start.
         $params = array(
-            'startdate' => 9999999998,
-            'enddate' => 9999999999
+            'startdate' => SQL_INT_MAX - 1,
+            'enddate' => SQL_INT_MAX,
         );
         // Resetting $course var.
         $course = $this->getDataGenerator()->create_course($params);
