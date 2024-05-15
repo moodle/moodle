e0b54b9cf22429fbe3bd383dfd2d6dc2b76656b8
Andrew Nicols <andrew@nicols.co.uk>
2024-05-15 23:34:03 +0800

    MDL-81914 core_competency: Remove deprecated invalid_persistent_exception

--- competency/classes/persistent.php
+++ competency/classes/persistent.php
@@ -14,25 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Abstract class for core_competency objects saved to the DB.
- *
- * @package    core_competency
- * @copyright  2015 Damyon Wiese
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 namespace core_competency;
-defined('MOODLE_INTERNAL') || die();
-
-// We need to alias the invalid_persistent_exception, because the persistent classes from
-// core_competency used to throw a \core_competency\invalid_persistent_exception. They now
-// fully inherit from \core\persistent which throws a core exception. Using class_alias
-// ensures that previous try/catch statements still work. Also note that we always need
-// need to alias, we cannot do it passively in the classloader because try/catch statements
-// do not trigger a class loading. Note that for this trick to work, all the classes
-// which were extending \core_competency\persistent still need to extend it or the alias
-// won't be effective.
-class_alias('core\\invalid_persistent_exception', 'core_competency\\invalid_persistent_exception');
 
 /**
  * Abstract class for core_competency objects saved to the DB.
