dc2a09d59a01de805fa3fa082df01c3703aba4fe
Mark Johnson <mark.johnson@catalyst-eu.net>
2024-05-21 10:48:45 +0100

    MDL-81514 groups: Add participationonly option to activity group menu
    groups_print_activity_menu() and groups_get_activity_group() now include
    an additional $participationonly parameter,
    which is true by default. This can be set false when we want the user to
    be able to select a non-participation group within
    an activity, for example if a teacher wants to filter assignment
    submissions by non-participation groups. It should never be
    used when the menu is displayed to students, as this may allow them to
    participate using non-participation groups.
    
    groups_sort_menu_options() now has a $splitparticipation parameter,
    which will split non-participation groups out into their own optgroup at
    the end of the menu.

--- public/lib/tests/grouplib_test.php
+++ public/lib/tests/grouplib_test.php
@@ -1236,14 +1236,18 @@ final class grouplib_test extends \advanced_testcase {
     /**
      * Create dummy groups array for use in menu tests
      * @param int $number
+     * @param bool $alternateparticipation If true, set participation = 1 for even groups, and 0 for odd groups.
      * @return array
      */
-    protected function make_group_list($number) {
+    protected function make_group_list($number, $alternateparticipation = false) {
         $testgroups = array();
         for ($a = 0; $a < $number; $a++) {
             $grp = new \stdClass();
             $grp->id = 100 + $a;
             $grp->name = 'test group ' . $grp->id;
+            if ($alternateparticipation) {
+                $grp->participation = ($a % 2 == 0);
+            }
             $testgroups[$grp->id] = $grp;
         }
         return $testgroups;
@@ -1300,6 +1304,82 @@ final class grouplib_test extends \advanced_testcase {
                 112 => 'test group 112',
             )),
         ), groups_sort_menu_options($this->make_group_list(13), $this->make_group_list(2)));
+    }
+
+    /**
+     * Splitting allowed groups by participation returns an optgroup for non-participation groups.
+     *
+     * @covers ::groups_sort_menu_options()
+     * @return void
+     */
+    public function test_groups_split_participation_allowed_groups_only(): void {
+        $this->assertEquals(
+            [
+                100 => 'test group 100',
+                3 => [
+                    get_string('nonparticipation', 'group') => [
+                        101 => 'test group 101',
+                    ],
+                ],
+            ],
+            groups_sort_menu_options($this->make_group_list(2, true), [], true),
+        );
+    }
+
+    /**
+     * Splitting user groups by participation returns an optgroup for non-participation groups.
+     *
+     * @covers ::groups_sort_menu_options()
+     * @return void
+     */
+    public function test_groups_split_participation_options_user_groups_only(): void {
+        $this->assertEquals(
+            [
+                100 => 'test group 100',
+                3 => [
+                        get_string('nonparticipation', 'group') => [
+                                101 => 'test group 101',
+                        ],
+                ],
+            ],
+            groups_sort_menu_options([], $this->make_group_list(2, true), true),
+        );
+    }
+
+    /**
+     * Splitting allowed and user groups by participation returns optgroups.
+     *
+     * One optgroup for user participation groups, one for other participation groups, and one for non-participation groups.
+     *
+     * @covers ::groups_sort_menu_options()
+     * @return void
+     */
+    public function test_groups_split_participation_options_user_both(): void {
+        $this->assertEquals(
+            [
+                1 => [
+                    get_string('mygroups', 'group') => [
+                        100 => 'test group 100',
+                    ],
+                ],
+                2 => [
+                    get_string('othergroups', 'group') => [
+                        102 => 'test group 102',
+                    ],
+                ],
+                3 => [
+                    get_string('nonparticipation', 'group') => [
+                        101 => 'test group 101',
+                        103 => 'test group 103',
+                    ],
+                ],
+            ],
+            groups_sort_menu_options(
+                $this->make_group_list(4, true),
+                $this->make_group_list(2, true),
+                true
+            ),
+        );
     }
 
     /**
