61c5863b1a443483f1908aa178fbd0595e38bffd
David Mudrák <david@moodle.com>
2024-06-01 16:13:00 +0200

    MDL-81982 lang: Fix strings order
    Fixes the strings order in files where CI has detected the wrong order
    of modified strings.

--- lib/editor/tiny/plugins/equation/lang/en/tiny_equation.php
+++ lib/editor/tiny/plugins/equation/lang/en/tiny_equation.php
@@ -22,9 +22,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['buttontitle'] = 'Equation editor';
 $string['cursorinfo'] = 'An arrow indicates the position that new elements from the element library will be inserted.';
 $string['editequation'] = 'Edit equation using <a href="{$a}" target="_blank">TeX</a>';
-$string['buttontitle'] = 'Equation editor';
 $string['helplinktext'] = 'Equation helper';
 $string['librarygroup1'] = 'Operators';
 $string['librarygroup1_desc'] = 'TeX commands listed on the operators tab.';
@@ -35,8 +35,8 @@ $string['librarygroup3_desc'] = 'TeX commands listed on the Greek symbols tab.';
 $string['librarygroup4'] = 'Advanced';
 $string['librarygroup4_desc'] = 'TeX commands listed on the advanced tab.';
 $string['modaltitle'] = 'Equation editor';
-$string['saveequation'] = 'Save equation';
-$string['settings'] = 'Equation editor settings';
 $string['pluginname'] = 'Equation editor';
 $string['preview'] = 'Equation preview';
 $string['privacy:metadata'] = 'The equation editor for TinyMCE does not store any personal data.';
+$string['saveequation'] = 'Save equation';
+$string['settings'] = 'Equation editor settings';
