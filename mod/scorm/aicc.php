55571fb06cbb25fdbd6b16ae33204ac06c04781d
Eric Merrill <eric.merrill@moodle.com>
2024-06-13 18:35:07 -0400

    MDL-82192 scorm: Correct field names for AICC ExitAu

--- mod/scorm/aicc.php
+++ mod/scorm/aicc.php
@@ -413,10 +413,10 @@ if (!empty($command)) {
                         } else {
                             $track = new stdClass();
                             $track->scoid = $sco->id;
-                            $track->element = scorm_get_elementid('cmi.core.total_time');
+                            $track->elementid = scorm_get_elementid('cmi.core.total_time');
                             $track->value = $scormsession->sessiontime;
                             $atobject = scorm_get_attempt($aiccuser->id, $scormsession->scormid, $attempt);
-                            $track->attempt = $atobject->id;
+                            $track->attemptid = $atobject->id;
                             $track->timemodified = time();
                             $id = $DB->insert_record('scorm_scoes_value', $track);
                         }
