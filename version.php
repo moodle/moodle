9db8d9148cabee7ecf03fc69441a17a6d9e3f7eb
David Woloszyn <david.woloszyn@moodle.com>
2024-06-20 14:53:05 +1000

    MDL-81506 core: Version bump

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024061400.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2024061400.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '4.5dev (Build: 20240614)'; // Human-friendly version name
