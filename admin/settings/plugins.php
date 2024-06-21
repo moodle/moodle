5b0701d97bc493df143f412717d83ed7d374313e
Derick Turner <derick@e-learndesign.co.uk>
2024-06-21 14:38:55 +0100

    IOMAD: getting -1 for company id isn't empty

--- admin/settings/plugins.php
+++ admin/settings/plugins.php
@@ -27,7 +27,7 @@
 // IOMAD
 require_once($CFG->dirroot . '/local/iomad/lib/company.php');
 $companyid = iomad::get_my_companyid(context_system::instance(), false);
-if (!empty($companyid)) {
+if (!empty($companyid) && $companyid > 0) {
     $postfix = "_$companyid";
 } else {
     $postfix = "";
