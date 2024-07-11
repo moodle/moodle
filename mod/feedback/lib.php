d413053822404b5047bba7cd4d8b04aab1bf603f
Sara Arjona <sara@moodle.com>
2024-07-11 15:48:21 +0200

    MDL-82211 mod_feedback: Update SVG icons and remove unused ones

--- mod/feedback/lib.php
+++ mod/feedback/lib.php
@@ -2917,8 +2917,8 @@ function feedback_can_view_analysis($feedback, $context, $courseid = false) {
  */
 function mod_feedback_get_fontawesome_icon_map() {
     return [
-        'mod_feedback:required' => 'fa-exclamation-circle',
-        'mod_feedback:notrequired' => 'fa-question-circle-o',
+        'mod_feedback:notrequired' => 'fa-circle-question',
+        'mod_feedback:required' => 'fa-circle-exclamation',
     ];
 }
 
