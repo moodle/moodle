60367220fa1277ebd475b27f7ad14f0ddd90e6e1
Nguyen Tuan Anh <anh.nguyentuan3@nashtechglobal.com>
2024-07-19 13:43:05 +0700

    MDL-82425 Admin: Remove tooltips when click toggle

--- admin/amd/src/plugin_management_table.js
+++ admin/amd/src/plugin_management_table.js
@@ -138,6 +138,10 @@ export default class {
 
             // Refocus on the link that as pressed in the first place.
             updatedRoot.querySelector(`[data-action="togglestate"][data-plugin="${stateToggle.dataset.plugin}"]`).focus();
+
+            // When clicking the toggle and it remains focused, a new tooltip will be generated.
+            // Therefore, the old tooltip should be removed.
+            this.removeTooltips();
             pendingPromise.resolve();
         }
     }
@@ -175,5 +179,17 @@ export default class {
         }
 
         pendingPromise.resolve();
+    }
+
+    /**
+     * Remove tooltips.
+     */
+    removeTooltips() {
+        const tooltips = document.querySelectorAll('[id*="tooltip"]');
+        if (tooltips.length > 0) {
+            tooltips.forEach(tooltip => {
+                tooltip.remove();
+            });
+        }
     }
 }
