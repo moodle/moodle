5e4e5bf13ebac16884074a4bb78fb7f024e5e342
Juan Leyva <juanleyvadelgado@gmail.com>
2024-07-24 16:15:57 +0200

    MDL-80017 core_grades: Fix return type for itemname

--- grade/report/user/tests/externallib_test.php
+++ grade/report/user/tests/externallib_test.php
@@ -72,7 +72,7 @@ class externallib_test extends externallib_advanced_testcase {
         groups_add_member($group1->id, $teacher->id);
         groups_add_member($group2->id, $student2->id);
 
-        $assignment = $this->getDataGenerator()->create_module('assign', ['name' => "Test assign", 'course' => $course->id]);
+        $assignment = $this->getDataGenerator()->create_module('assign', ['name' => "Test assign & grade items", 'course' => $course->id]);
         $modcontext = get_coursemodule_from_instance('assign', $assignment->id, $course->id);
         $assignment->cmidnumber = $modcontext->id;
 
