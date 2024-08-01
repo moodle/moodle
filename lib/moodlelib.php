3ab8986eb040c4feec489189efd1a9a83fe95f57
James Williams <jameswilliams@catalyst-au.net>
2024-08-01 18:53:18 +1000

    MDL-82384 core: Alter logic to only purge muc OR course caches.

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -1185,6 +1185,7 @@ function purge_all_caches() {
  *
  * @param bool[] $options Specific parts of the cache to purge. Valid options are:
  *        'muc'    Purge MUC caches?
+ *        'courses' Purge all course caches, or specific course caches (CLI only)
  *        'theme'  Purge theme cache?
  *        'lang'   Purge language string cache?
  *        'js'     Purge javascript cache?
@@ -1200,8 +1201,7 @@ function purge_caches($options = []) {
     }
     if ($options['muc']) {
         cache_helper::purge_all();
-    }
-    if ($options['courses']) {
+    } else if ($options['courses']) {
         if ($options['courses'] === true) {
             $courseids = [];
         } else {
