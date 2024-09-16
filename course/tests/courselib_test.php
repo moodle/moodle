84d831318fc65182cfc706c1c364196a9d59e7c3
sam marshall <s.marshall@open.ac.uk>
2024-09-16 10:26:54 +0100

    MDL-83163 core_course: Fresh install fails if dependency on format
    If any plugin had dependency on a course format, Moodle fresh install
    showed dependency errors (even though course format exists).

--- course/tests/courselib_test.php
+++ course/tests/courselib_test.php
@@ -7488,4 +7488,36 @@ class courselib_test extends advanced_testcase {
         $course = get_course($course->id);
         $this->assertEquals($course->fullname, $data->fullname);
     }
+
+    /**
+     * Tests get_sorted_course_formats returns all plugins in cases where plugins are installed now,
+     * installed previously but no longer exist, or not installed yet.
+     *
+     * @covers ::get_sorted_course_formats()
+     */
+    public function test_get_sorted_course_formats_installed_or_not(): void {
+        global $DB;
+
+        $this->resetAfterTest();
+
+        // By default returns all course formats.
+        $formats = get_sorted_course_formats();
+        $this->assertEquals(['singleactivity', 'social', 'topics', 'weeks'], $formats);
+
+        // If there is an extra format installed that no longer exists, include in list (at start).
+        $DB->insert_record('config_plugins', [
+            'plugin' => 'format_frogs',
+            'name' => 'version',
+            'value' => '20240916',
+        ]);
+        \core_plugin_manager::reset_caches();
+        $formats = get_sorted_course_formats();
+        $this->assertEquals(['frogs', 'singleactivity', 'social', 'topics', 'weeks'], $formats);
+
+        // If one of the formats is not installed yet, we still return it.
+        $DB->delete_records('config_plugins', ['plugin' => 'format_weeks']);
+        \core_plugin_manager::reset_caches();
+        $formats = get_sorted_course_formats();
+        $this->assertEquals(['frogs', 'singleactivity', 'social', 'topics', 'weeks'], $formats);
+    }
 }
