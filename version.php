2bf0d3b14900c28b84f6891c88f9e5e0d96bfe97
Huong Nguyen <huongnv13@gmail.com>
2024-10-04 20:48:15 +0700

    weekly release 4.3.7+

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * MOODLE VERSION INFORMATION
+ * MOODLE VERSION INFORMATION.
  *
  * This file defines the current version of the core Moodle code being used.
  * This is compared against the values stored in the database to determine
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023100907.10;              // 20231009      = branching date YYYYMMDD - do not modify!
+$version  = 2023100907.11;              // 20231009      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.3.7+ (Build: 20241001)'; // Human-friendly version name
-$branch   = '403';                     // This version's branch.
-$maturity = MATURITY_STABLE;             // This version's maturity level.
+$release  = '4.3.7+ (Build: 20241004)';    // Human-friendly version name
+$branch   = '403';                      // This version's branch.
+$maturity = MATURITY_STABLE;            // This version's maturity level.
