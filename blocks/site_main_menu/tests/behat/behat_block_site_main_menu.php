a5ba68c97f185c5d235db6c236877265ddecbf1c
ferranrecio <ferran@moodle.com>
2024-10-08 18:03:26 +0200

    MDL-82349 block_site_main_menu: fix behats

--- blocks/site_main_menu/tests/behat/behat_block_site_main_menu.php
+++ blocks/site_main_menu/tests/behat/behat_block_site_main_menu.php
@@ -27,10 +27,6 @@
 
 require_once(__DIR__ . '/../../../../lib/behat/behat_base.php');
 
-use Behat\Mink\Exception\ExpectationException as ExpectationException,
-    Behat\Mink\Exception\DriverException as DriverException,
-    Behat\Mink\Exception\ElementNotFoundException as ElementNotFoundException;
-
 /**
  * Behat steps definitions for block site main menu
  *
@@ -40,121 +36,6 @@ use Behat\Mink\Exception\ExpectationException as ExpectationException,
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class behat_block_site_main_menu extends behat_base {
-
-    /**
-     * Returns the DOM node of the activity in the site menu block
-     *
-     * @throws ElementNotFoundException Thrown by behat_base::find
-     * @param string $activityname The activity name
-     * @return NodeElement
-     */
-    protected function get_site_menu_activity_node($activityname) {
-        $activityname = behat_context_helper::escape($activityname);
-        $xpath = "//*[contains(concat(' ',normalize-space(@class),' '),' block_site_main_menu ')]//li[contains(., $activityname)]";
-
-        return $this->find('xpath', $xpath);
-    }
-
-    /**
-     * Checks that the specified activity's action menu contains an item.
-     *
-     * @Then /^"(?P<activity_name_string>(?:[^"]|\\")*)" activity in site main menu block should have "(?P<icon_name_string>(?:[^"]|\\")*)" editing icon$/
-     * @param string $activityname
-     * @param string $iconname
-     */
-    public function activity_in_site_main_menu_block_should_have_editing_icon($activityname, $iconname) {
-        $activitynode = $this->get_site_menu_activity_node($activityname);
-
-        $notfoundexception = new ExpectationException('"' . $activityname . '" doesn\'t have a "' .
-            $iconname . '" editing icon', $this->getSession());
-        $this->find('named_partial', array('link', $iconname), $notfoundexception, $activitynode);
-    }
-
-    /**
-     * Checks that the specified activity's action menu contains an item.
-     *
-     * @Then /^"(?P<activity_name_string>(?:[^"]|\\")*)" activity in site main menu block should not have "(?P<icon_name_string>(?:[^"]|\\")*)" editing icon$/
-     * @param string $activityname
-     * @param string $iconname
-     */
-    public function activity_in_site_main_menu_block_should_not_have_editing_icon($activityname, $iconname) {
-        $activitynode = $this->get_site_menu_activity_node($activityname);
-
-        try {
-            $this->find('named_partial', array('link', $iconname), false, $activitynode);
-            throw new ExpectationException('"' . $activityname . '" has a "' . $iconname .
-                '" editing icon when it should not', $this->getSession());
-        } catch (ElementNotFoundException $e) {
-            // This is good, the menu item should not be there.
-        }
-    }
-
-    /**
-     * Clicks on the specified element of the activity. You should be in the course page with editing mode turned on.
-     *
-     * @Given /^I click on "(?P<element_string>(?:[^"]|\\")*)" "(?P<selector_string>(?:[^"]|\\")*)" in the "(?P<activity_name_string>(?:[^"]|\\")*)" activity in site main menu block$/
-     * @param string $element
-     * @param string $selectortype
-     * @param string $activityname
-     */
-    public function i_click_on_in_the_activity_in_site_main_menu_block($element, $selectortype, $activityname) {
-        $element = $this->get_site_menu_activity_element($element, $selectortype, $activityname);
-        $element->click();
-    }
-
-    /**
-     * Clicks on the specified element inside the activity container.
-     *
-     * @throws ElementNotFoundException
-     * @param string $element
-     * @param string $selectortype
-     * @param string $activityname
-     * @return NodeElement
-     */
-    protected function get_site_menu_activity_element($element, $selectortype, $activityname) {
-        $activitynode = $this->get_site_menu_activity_node($activityname);
-
-        $exception = new ElementNotFoundException($this->getSession(), "'{$element}' '{$selectortype}' in '{$activityname}'");
-        return $this->find($selectortype, $element, $exception, $activitynode);
-    }
-
-    /**
-     * Checks that the specified activity is hidden.
-     *
-     * @Then /^"(?P<activity_name_string>(?:[^"]|\\")*)" activity in site main menu block should be hidden$/
-     * @param string $activityname
-     */
-    public function activity_in_site_main_menu_block_should_be_hidden($activityname) {
-        $activitynode = $this->get_site_menu_activity_node($activityname);
-        $exception = new ExpectationException('"' . $activityname . '" is not hidden', $this->getSession());
-        $this->find('named_partial', array('badge', get_string('hiddenfromstudents')), $exception, $activitynode);
-    }
-
-    /**
-     * Checks that the specified activity is hidden.
-     *
-     * @Then /^"(?P<activity_name_string>(?:[^"]|\\")*)" activity in site main menu block should be available but hidden from course page$/
-     * @param string $activityname
-     */
-    public function activity_in_site_main_menu_block_should_be_available_but_hidden_from_course_page($activityname) {
-        $activitynode = $this->get_site_menu_activity_node($activityname);
-        $exception = new ExpectationException('"' . $activityname . '" is not hidden but available', $this->getSession());
-        $this->find('named_partial', array('badge', get_string('hiddenoncoursepage')), $exception, $activitynode);
-    }
-
-    /**
-     * Opens an activity actions menu if it is not already opened.
-     *
-     * @Given /^I open "(?P<activity_name_string>(?:[^"]|\\")*)" actions menu in site main menu block$/
-     * @throws DriverException The step is not available when Javascript is disabled
-     * @param string $activityname
-     */
-    public function i_open_actions_menu_in_site_main_menu_block($activityname) {
-        $activityname = behat_context_helper::escape($activityname);
-        $xpath = "//*[contains(concat(' ',normalize-space(@class),' '),' block_site_main_menu ')]//li[contains(., $activityname)]";
-        $this->execute('behat_action_menu::i_open_the_action_menu_in', [$xpath, 'xpath_element']);
-    }
-
     /**
      * Return the list of partial named selectors.
      *
