ab5b390a77f4bb26959a11781670ecc2f56598e9
ferranrecio <ferran@moodle.com>
2024-10-09 13:43:56 +0200

    MDL-82351 block_social_activities: migrate to reactive course editor
    This commit integrate and clean the social activities block to use the
    standard course format outputs and the course editor modules. The code
    is similar to the one used in the site_main_menu block and cleanup most
    of the ancient code.

--- blocks/social_activities/block_social_activities.php
+++ blocks/social_activities/block_social_activities.php
@@ -21,8 +21,7 @@
  * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-class block_social_activities extends block_list {
+class block_social_activities extends block_base {
     function init(){
         $this->title = get_string('pluginname', 'block_social_activities');
     }
@@ -32,15 +31,12 @@ class block_social_activities extends block_list {
     }
 
     function get_content() {
-        global $USER, $CFG, $DB, $OUTPUT;
-
         if ($this->content !== NULL) {
             return $this->content;
         }
 
         $this->content = new stdClass();
-        $this->content->items = array();
-        $this->content->icons = array();
+        $this->content->text = '';
         $this->content->footer = '';
 
         if (empty($this->instance)) {
@@ -48,198 +44,24 @@ class block_social_activities extends block_list {
         }
 
         $course = $this->page->course;
+
+        course_create_sections_if_missing($course, 0);
         $format = course_get_format($course);
-        $courserenderer = $format->get_renderer($this->page);
-
-        require_once($CFG->dirroot.'/course/lib.php');
-
-        $context = context_course::instance($course->id);
-        $isediting = $this->page->user_is_editing() && has_capability('moodle/course:manageactivities', $context);
-        $modinfo = get_fast_modinfo($course);
-
-        // Output classes.
-        $cmnameclass = $format->get_output_classname('content\\cm\\cmname');
-        $controlmenuclass = $format->get_output_classname('content\\cm\\controlmenu');
-
-        $badgeattributes = [
-            'class' => 'badge rounded-pill bg-warning text-dark mt-2',
-            'data-region' => 'visibility'
-        ];
-
-        // Extra fast view mode.
-        if (!$isediting) {
-            if (!empty($modinfo->sections[0])) {
-                foreach($modinfo->sections[0] as $cmid) {
-                    $cm = $modinfo->cms[$cmid];
-                    if (!$cm->uservisible || !$cm->is_visible_on_course_page()) {
-                        continue;
-                    }
-
-                    $badges = '';
-                    if (!$cm->visible) {
-                        $badges = html_writer::tag(
-                            'span',
-                            get_string('hiddenfromstudents'),
-                            $badgeattributes
-                        );
-                    }
-
-                    if ($cm->is_stealth()) {
-                        $badges = html_writer::tag(
-                            'span',
-                            get_string('hiddenoncoursepage'),
-                            $badgeattributes
-                        );
-                    }
-
-                    if (!$cm->url) {
-                        $activitybasis = html_writer::div(
-                            $cm->get_formatted_content(['overflowdiv' => true, 'noclean' => true]),
-                            'activity-basis d-flex align-items-center'
-                        );
-                        $content = html_writer::div(
-                            $activitybasis . $badges,
-                            'contentwithoutlink activity-item activity',
-                            ['data-activityname' => $cm->name]
-                        );
-                        $this->content->items[] = $content;
-                        $this->content->icons[] = '';
-                    } else {
-                        $cmname = new $cmnameclass($format, $cm->get_section_info(), $cm);
-                        $activitybasis = html_writer::div(
-                            $courserenderer->render($cmname),
-                            'activity-basis d-flex align-items-center');
-                        $content = html_writer::div(
-                            $activitybasis . $badges,
-                            'activity-item activity',
-                            ['data-activityname' => $cm->name]
-                        );
-                        $this->content->items[] = $content;
-                    }
-                }
-            }
-            return $this->content;
-        }
-
-        // Slow & hacky editing mode.
-        $ismoving = ismoving($course->id);
+        $modinfo = $format->get_modinfo();
         $section = $modinfo->get_section_info(0);
 
-        if ($ismoving) {
-            $strmovefull = strip_tags(get_string('movefull', '', "'$USER->activitycopyname'"));
-            $strcancel= get_string('cancel');
-        } else {
-            $strmove = get_string('move');
-        }
+        $courserenderer = $format->get_renderer($this->page);
 
-        if ($ismoving) {
-            $this->content->icons[] = '&nbsp;' . $OUTPUT->pix_icon('t/move', get_string('move'));
-            $cancelurl = new moodle_url('/course/mod.php', array('cancelcopy' => 'true', 'sesskey' => sesskey()));
-            $this->content->items[] = $USER->activitycopyname . '&nbsp;(<a href="' . $cancelurl . '">' . $strcancel . '</a>)';
-        }
+        $output = new block_social_activities\output\blocksection($format, $section);
 
-        if (!empty($modinfo->sections[0])) {
-            foreach ($modinfo->sections[0] as $modnumber) {
-                $mod = $modinfo->cms[$modnumber];
-                if (!$mod->uservisible || !$mod->is_visible_on_course_page()) {
-                    continue;
-                }
-                if (!$ismoving) {
+        $this->content->text = $courserenderer->render($output);
 
-                    $controlmenu = new $controlmenuclass(
-                        $format,
-                        $mod->get_section_info(),
-                        $mod,
-                        ['disableindentation' => true]
-                    );
-
-                    $menu = $controlmenu->get_action_menu($OUTPUT);
-
-                    // Add a move primary action.
-                    $moveaction = html_writer::link(
-                        new moodle_url('/course/mod.php', ['sesskey' => sesskey(), 'copy' => $mod->id]),
-                        $OUTPUT->pix_icon('i/dragdrop', $strmove),
-                        ['class' => 'editing_move_activity']
-                    );
-
-                    $editbuttons = html_writer::tag('div',
-                        $courserenderer->render($controlmenu),
-                        ['class' => 'buttons activity-actions ms-auto']
-                    );
-                } else {
-                    $editbuttons = '';
-                    $moveaction = '';
-                }
-                if ($mod->visible || has_capability('moodle/course:viewhiddenactivities', $mod->context)) {
-                    if ($ismoving) {
-                        if ($mod->id == $USER->activitycopy) {
-                            continue;
-                        }
-                        $movingurl = new moodle_url('/course/mod.php', array('moveto' => $mod->id, 'sesskey' => sesskey()));
-                        $this->content->items[] = html_writer::link($movingurl, '', array('title' => $strmovefull,
-                            'class' => 'movehere'));
-                        $this->content->icons[] = '';
-                    }
-
-                    $badges = '';
-                    if (!$mod->visible) {
-                        $badges = html_writer::tag(
-                            'span',
-                            get_string('hiddenfromstudents'),
-                            $badgeattributes
-                        );
-                    }
-
-                    if ($mod->is_stealth()) {
-                        $badges = html_writer::tag(
-                            'span',
-                            get_string('hiddenoncoursepage'),
-                            $badgeattributes
-                        );
-                    }
-
-                    if (!$mod->url) {
-                        $activitybasis = html_writer::div(
-                            $mod->get_formatted_content(['overflowdiv' => true, 'noclean' => true]) .
-                            $editbuttons,
-                            'activity-basis d-flex align-items-center');
-                        $content = html_writer::div(
-                            $moveaction .
-                            $activitybasis .
-                            $badges,
-                            'contentwithoutlink activity-item activity',
-                            ['data-activityname' => $mod->name]
-                        );
-                        $this->content->items[] = $content;
-                        $this->content->icons[] = '';
-                    } else {
-                        $cmname = new $cmnameclass($format, $mod->get_section_info(), $mod);
-                        $activitybasis = html_writer::div(
-                            $courserenderer->render($cmname) .
-                            $editbuttons,
-                            'activity-basis d-flex align-items-center');
-                        $content = html_writer::div(
-                            $moveaction .
-                            $activitybasis .
-                            $badges,
-                            'activity-item activity',
-                            ['data-activityname' => $mod->name]
-                        );
-                        $this->content->items[] = $content;
-                    }
-                }
-            }
-        }
-
-        if ($ismoving) {
-            $movingurl = new moodle_url('/course/mod.php', array('movetosection' => $section->id, 'sesskey' => sesskey()));
-            $this->content->items[] = html_writer::link($movingurl, '', array('title' => $strmovefull, 'class' => 'movehere'));
-            $this->content->icons[] = '';
-        }
-
-        $this->content->footer = $courserenderer->course_section_add_cm_control($course,
-                0, null, array('inblock' => true));
-
+        $this->content->footer = $courserenderer->course_section_add_cm_control(
+            course: $course,
+            section: 0,
+            sectionreturn: null,
+            displayoptions: ['inblock' => true],
+        );
         return $this->content;
     }
 }
---
+++ blocks/social_activities/classes/output/blocksection.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace block_social_activities\output;
+
+use core_courseformat\base as courseformat;
+use renderable;
+use section_info;
+use templatable;
+
+/**
+ * Class blocksection
+ *
+ * @package    block_social_activities
+ * @copyright  2024 Ferran Recio <ferran@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class blocksection implements renderable, templatable {
+
+    /**
+     * The class constructor.
+     *
+     * @param courseformat $format the course format instance
+     * @param section_info $section the section to render
+     */
+    public function __construct(
+        /** @var courseformat $format the course format instance. */
+        protected courseformat $format,
+        /** @var section_info $section the section to render. */
+        protected section_info $section,
+    ) {
+    }
+
+    /**
+     * Export for template.
+     *
+     * @param \renderer_base $output
+     * @return array
+     */
+    public function export_for_template(\renderer_base $output) {
+        $format = $this->format;
+        $course = $format->get_course();
+        $section = $this->section;
+
+        $sectionoutputclass = $format->get_output_classname('content\\section\\cmlist');
+        $sectionoutput = new $sectionoutputclass($format, $section);
+
+        $cmlist = $output->render($sectionoutput);
+
+        return [
+            'siteid' => $course->id,
+            'cmlist' => $cmlist,
+            'sectionid' => $section->id,
+            'sectionname' => $format->get_section_name($section),
+            'sectionnum' => $section->sectionnum,
+            'editing' => $format->show_editor(),
+        ];
+    }
+}
