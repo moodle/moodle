6807076172b94d4b6cb4d5411c419d3753895766
Laurent David <lmedavid@gmail.com>
2024-10-10 13:26:42 +0200

    MDL-82757 mod_data: Improve group access handling
    * Improved handling of group access to ensure correct visibility of records.

--- mod/data/tests/locallib_test.php
+++ mod/data/tests/locallib_test.php
@@ -97,6 +97,101 @@ class locallib_test extends \advanced_testcase {
     }
 
     /**
+     * Confirms that search is working with groups
+     * @covers ::data_search_entries
+     */
+    public function test_data_search_entries_with_groups(): void {
+        $this->resetAfterTest();
+        $this->setAdminUser();
+        $course = $this->getDataGenerator()->create_course(['groupmode' => SEPARATEGROUPS, 'groupmodeforce' => 1]);
+        $group1 = $this->getDataGenerator()->create_group(['courseid' => $course->id]);
+        $group2 = $this->getDataGenerator()->create_group(['courseid' => $course->id]);
+        $student1 = $this->getDataGenerator()->create_and_enrol($course);
+        $student2 = $this->getDataGenerator()->create_and_enrol($course);
+        $student3 = $this->getDataGenerator()->create_and_enrol($course);
+        $teacher1 = $this->getDataGenerator()->create_and_enrol($course, 'teacher');
+        $teacher2 = $this->getDataGenerator()->create_and_enrol($course, 'teacher');
+        $teacher3 = $this->getDataGenerator()->create_and_enrol($course, 'teacher');
+        groups_add_member($group1->id, $student1->id);
+        groups_add_member($group1->id, $teacher1->id);
+        groups_add_member($group2->id, $student3->id);
+        groups_add_member($group2->id, $teacher3->id);
+
+        $record = new \stdClass();
+        $record->course = $course->id;
+        $record->name = "Mod data delete test";
+        $record->intro = "Some intro of some sort";
+        $module = $this->getDataGenerator()->create_module('data', $record);
+        $titlefield = $this->getDataGenerator()->get_plugin_generator('mod_data')->create_field(
+            (object) [
+                'name' => 'title',
+                'type' => 'text',
+                'required' => 1,
+            ],
+            $module);
+        $captionfield = $this->getDataGenerator()->get_plugin_generator('mod_data')->create_field(
+            (object) [
+                'name' => 'caption',
+                'type' => 'text',
+                'required' => 1,
+            ],
+            $module);
+        $this->getDataGenerator()->get_plugin_generator('mod_data')->create_entry($module, [
+            $titlefield->field->id => 'Entry 1 - group 1',
+            $captionfield->field->id => 'caption',
+        ],
+            $group1->id,
+            [],
+            null,
+            $student1->id
+        );
+        $this->getDataGenerator()->get_plugin_generator('mod_data')->create_entry($module, [
+            $titlefield->field->id => 'Entry 2 - group 1',
+            $captionfield->field->id => 'caption',
+        ],
+            $group1->id,
+            [],
+            null,
+            $student1->id
+        );
+        $this->getDataGenerator()->get_plugin_generator('mod_data')->create_entry($module, [
+            $titlefield->field->id => 'Entry 3 - group 2',
+            $captionfield->field->id => '',
+        ],
+            $group2->id,
+            [],
+            null,
+            $student3->id
+        );
+        $this->getDataGenerator()->get_plugin_generator('mod_data')->create_entry($module, [
+            $titlefield->field->id => 'Entry 3 - no group',
+            $captionfield->field->id => '',
+        ],
+            0,
+            [],
+            null,
+            $student2->id
+        );
+        $cm = get_coursemodule_from_id('data', $module->cmid);
+        $this->setUser($teacher1);
+        // As a non editing teacher in group 1, I should see only the entries for group 1.
+        list($records, $maxcount, $totalcount, $page, $nowperpage, $sort, $mode) =
+            data_search_entries($module, $cm, \context_course::instance($course->id), 'list', $group1->id);
+        $this->assertCount(3, $records); // Record with group 1 and record with no group.
+        // As a non editing teacher not in a group, I should see the entry from users not in a group.
+        $this->setUser($teacher3);
+        list($records, $maxcount, $totalcount, $page, $nowperpage, $sort, $mode) =
+            data_search_entries($module, $cm, \context_course::instance($course->id), 'list', $group2->id);
+        $this->assertCount(2, $records); // Record with group 2 and record with no group.
+        // As a non editing teacher not in a group, I should see the entry from users not in a group.
+        $this->setUser($teacher2);
+        list($records, $maxcount, $totalcount, $page, $nowperpage, $sort, $mode) =
+            data_search_entries($module, $cm, \context_course::instance($course->id), 'list', 0);
+        $this->assertCount(1, $records); // Just the record with no group.
+        $this->assert_record_entries_contains($records, $titlefield->field->id, 'Entry 3 - no group');
+    }
+
+    /**
      * Assert that all records contains a value for the matching field id.
      *
      * @param array $records
