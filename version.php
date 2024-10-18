08e4f06bc3c70dbfeaee10cd2c070e55d3206920
Shamim Rezaie <shamim@moodle.com>
2024-10-18 23:29:04 +1100

    weekly release 4.3.8+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2023100908.01;              // 20231009      = branching date YYYYMMDD - do not modify!
+$version  = 2023100908.02;              // 20231009      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.3.8+ (Build: 20241011)';    // Human-friendly version name
+$release  = '4.3.8+ (Build: 20241018)';    // Human-friendly version name
 $branch   = '403';                      // This version's branch.
 $maturity = MATURITY_STABLE;            // This version's maturity level.
