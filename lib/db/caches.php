1f7faa941b0644880a4183230fd6d943a415b721
ferran <ferranrecio@gmail.com>
2024-10-30 15:10:59 +0100

    MDL-83185 core_courseformat: Allow global state cache invalidation

--- lib/db/caches.php
+++ lib/db/caches.php
@@ -239,6 +239,9 @@ $definitions = array(
         'mode' => cache_store::MODE_SESSION,
         'simplekeys' => true,
         'simpledata' => true,
+        'invalidationevents' => [
+            'changesincoursestate',
+        ],
     ],
     // Course actions instances cache.
     'courseactionsinstances' => [
