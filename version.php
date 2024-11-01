bd16230b1e9cb0aa8c94206f956eda41bc5aac8b
Huong Nguyen <huongnv13@gmail.com>
2024-11-01 14:29:15 +0700

    2024/11/07 [change] config.php等の設定を変更

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024100700.03;              // 20241007      = branching date YYYYMMDD - do not modify!
+$version  = 2024100700.04;              // 20241007      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.5+ (Build: 20241025)';    // Human-friendly version name
+$release  = '4.5+ (Build: 20241101)';    // Human-friendly version name
 $branch   = '405';                      // This version's branch.
 $maturity = MATURITY_STABLE;            // This version's maturity level.
