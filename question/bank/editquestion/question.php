5e952377aa9fac0447ea2733df951d8e16cfab5b
hieuvu <hieuvu@nashtechglobal.com>
2024-11-06 14:30:08 +0700

    MDL-82273 core_question: get correct category for question bank view

--- question/bank/editquestion/question.php
+++ question/bank/editquestion/question.php
@@ -310,6 +310,14 @@ if ($mform->is_cancelled()) {
                 $returnurl->param('sesskey', sesskey());
                 $returnurl->param('cmid', $cmid);
             }
+            // Update the filter param to the updated category if the return have any.
+            if (!empty($returnurl->param('filter'))) {
+                $filter = json_decode($returnurl->param('filter'), true);
+                if (isset($filter['category']['values'])) {
+                    $filter['category']['values'][0] = $question->category;
+                    $returnurl->param('filter', json_encode($filter));
+                }
+            }
             redirect($returnurl);
         }
 
@@ -330,6 +338,14 @@ if ($mform->is_cancelled()) {
             $nexturl->param('cmid', $cmid);
         } else {
             $nexturl->param('courseid', $COURSE->id);
+        }
+        // Update the filter param to the updated category if the return url have any.
+        if (!empty($nexturl->param('filter'))) {
+            $filter = json_decode($nexturl->param('filter'), true);
+            if (isset($filter['category']['values'])) {
+                $filter['category']['values'][0] = $question->category;
+                $nexturl->param('filter', json_encode($filter));
+            }
         }
         redirect($nexturl);
     }
