626ea71299c7805401e7ef0488e8364c5bd10eab
Jun Pataleta <jun@moodle.com>
2024-11-18 19:38:40 +0800

    MDL-83747 mod_data: Add accessible name for delcheck checkboxes

--- mod/data/classes/template.php
+++ mod/data/classes/template.php
@@ -479,6 +479,8 @@ class template {
             'name' => 'delcheck[]',
             'classes' => 'recordcheckbox',
             'value' => $entry->id,
+            'label' => get_string('selectfordeletion', 'data'),
+            'labelclasses' => 'sr-only',
         ]);
         return $OUTPUT->render($checkbox);
     }
