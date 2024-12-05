5f96398a90e5998639649262f4f2d05e451da3c5
Huong Nguyen <huongnv13@gmail.com>
2024-12-05 11:20:51 +0700

    MDL-83921 competency: Bump version

--- admin/tool/lp/version.php
+++ admin/tool/lp/version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2024100700; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024100701; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100100; // Requires this Moodle version.
 $plugin->component = 'tool_lp'; // Full name of the plugin (used for diagnostics).
