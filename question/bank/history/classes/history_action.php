0beaac4d29c0a4c2ce13d49dc1f9eee66c125bfe
Khoa Nguyen <knd39@open.ac.uk>
2024-12-11 15:45:55 +0700

    MDL-83832 core_question: Links to question history may show blank page

--- question/bank/history/classes/history_action.php
+++ question/bank/history/classes/history_action.php
@@ -16,6 +16,7 @@
 
 namespace qbank_history;
 
+use core_question\local\bank\filter_condition_manager;
 use core_question\local\bank\question_action_base;
 
 /**
@@ -49,11 +50,20 @@ class history_action extends question_action_base {
         }
 
         if (question_has_capability_on($question, 'use')) {
+            $currentfilter = $this->qbank->base_url()->param('filter');
+            if ($currentfilter) {
+                $currentfilter = filter_condition_manager::update_filter_param_to_category(
+                    $currentfilter, $question->categoryid);
+            } else if (empty($question->isdummy)) {
+                $currentfilter = json_encode(filter_condition_manager::get_default_filter(
+                    $question->categoryid . ',' . $question->contextid));
+            }
+
             $url = helper::get_question_history_url(
                 $question->questionbankentryid,
                 $this->qbank->returnurl,
                 $this->qbank->cm->id,
-                $this->qbank->base_url()->param('filter'),
+                $currentfilter,
             );
             return [$url, 't/log', $this->strpreview];
         }
