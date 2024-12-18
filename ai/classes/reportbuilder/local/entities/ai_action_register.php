af7ca95b3f45cb76af3cecea293d30f0e8bcd35f
David Woloszyn <david.woloszyn@moodle.com>
2024-12-19 10:51:43 +1100

    MDL-83395 core_ai: AI usage report

---
+++ ai/classes/reportbuilder/local/entities/ai_action_register.php
@@ -0,0 +1,256 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core_ai\reportbuilder\local\entities;
+
+use core_reportbuilder\local\entities\base;
+use core_reportbuilder\local\filters\boolean_select;
+use core_reportbuilder\local\filters\date;
+use core_reportbuilder\local\filters\number;
+use core_reportbuilder\local\filters\text;
+use core_reportbuilder\local\helpers\format;
+use core_reportbuilder\local\report\column;
+use core_reportbuilder\local\report\filter;
+use lang_string;
+
+/**
+ * AI action register entity.
+ *
+ * Defines all the columns and filters that can be added to reports that use this entity.
+ *
+ * @package    core_ai
+ * @copyright  2024 David Woloszyn <david.woloszyn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class ai_action_register extends base {
+
+    #[\Override]
+    protected function get_default_tables(): array {
+        return [
+            'ai_action_register',
+        ];
+    }
+
+    #[\Override]
+    protected function get_default_entity_title(): lang_string {
+        return new lang_string('aiactionregister', 'core_ai');
+    }
+
+    #[\Override]
+    public function initialise(): base {
+        $columns = $this->get_all_columns();
+        foreach ($columns as $column) {
+            $this->add_column($column);
+        }
+
+        // All the filters defined by the entity can also be used as conditions.
+        $filters = $this->get_all_filters();
+        foreach ($filters as $filter) {
+            $this
+                ->add_filter($filter)
+                ->add_condition($filter);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Returns list of all available columns.
+     *
+     * @return column[]
+     */
+    protected function get_all_columns(): array {
+        $mainalias = $this->get_table_alias('ai_action_register');
+        $generatetextalias = 'aagt';
+        $summarisetextalias = 'aast';
+
+        // Action name column.
+        $columns[] = (new column(
+            'actionname',
+            new lang_string('action', 'core_ai'),
+            $this->get_entity_name(),
+        ))
+            ->add_joins($this->get_joins())
+            ->set_type(column::TYPE_TEXT)
+            ->add_field("{$mainalias}.actionname")
+            ->set_is_sortable(true)
+            ->add_callback(static function(string $actionname): string {
+                return get_string("action_{$actionname}", 'core_ai');
+            });
+
+        // Provider column.
+        $columns[] = (new column(
+            'provider',
+            new lang_string('provider', 'core_ai'),
+            $this->get_entity_name(),
+        ))
+            ->add_joins($this->get_joins())
+            ->set_type(column::TYPE_TEXT)
+            ->add_field("{$mainalias}.provider")
+            ->set_is_sortable(true);
+
+        // Success column.
+        $columns[] = (new column(
+            'success',
+            new lang_string('success', 'moodle'),
+            $this->get_entity_name(),
+        ))
+            ->add_joins($this->get_joins())
+            ->set_type(column::TYPE_BOOLEAN)
+            ->add_field("{$mainalias}.success")
+            ->set_is_sortable(true)
+            ->set_callback([format::class, 'boolean_as_text']);
+
+        // Time created column.
+        $columns[] = (new column(
+            'timecreated',
+            new lang_string('timegenerated', 'core_ai'),
+            $this->get_entity_name(),
+        ))
+            ->add_joins($this->get_joins())
+            ->set_type(column::TYPE_TIMESTAMP)
+            ->add_field("{$mainalias}.timecreated")
+            ->set_is_sortable(true)
+            ->add_callback([format::class, 'userdate']);
+
+        // Prompt tokens column.
+        // Only available for summarise_text and generate_text actions.
+        $columns[] = (new column(
+            'prompttokens',
+            new lang_string('prompttokens', 'core_ai'),
+            $this->get_entity_name(),
+        ))
+            ->add_joins($this->get_joins())
+            ->add_join("
+                LEFT JOIN {ai_action_generate_text} {$generatetextalias}
+                       ON {$mainalias}.actionid = {$generatetextalias}.id
+                      AND {$mainalias}.actionname = 'generate_text'")
+            ->add_join("
+                LEFT JOIN {ai_action_summarise_text} {$summarisetextalias}
+                       ON {$mainalias}.actionid = {$summarisetextalias}.id
+                      AND {$mainalias}.actionname = 'summarise_text'")
+            ->set_type(column::TYPE_INTEGER)
+            ->add_field("COALESCE({$generatetextalias}.prompttokens, {$summarisetextalias}.prompttokens)", 'prompttokens')
+            ->set_is_sortable(true)
+            ->add_callback(static function(?int $value): string {
+                return $value ?? get_string('unknownvalue', 'core_ai');
+            });
+
+        // Completion tokens column.
+        // Only available for summarise_text and generate_text actions.
+        $columns[] = (new column(
+            'completiontokens',
+            new lang_string('completiontokens', 'core_ai'),
+            $this->get_entity_name(),
+        ))
+            ->add_joins($this->get_joins())
+            ->add_join("
+                LEFT JOIN {ai_action_generate_text} {$generatetextalias}
+                       ON {$mainalias}.actionid = {$generatetextalias}.id
+                      AND {$mainalias}.actionname = 'generate_text'")
+            ->add_join("
+                LEFT JOIN {ai_action_summarise_text} {$summarisetextalias}
+                       ON {$mainalias}.actionid = {$summarisetextalias}.id
+                      AND {$mainalias}.actionname = 'summarise_text'")
+            ->set_type(column::TYPE_INTEGER)
+            ->add_field("COALESCE({$generatetextalias}.completiontoken, {$summarisetextalias}.completiontoken)", 'completiontokens')
+            ->set_is_sortable(true)
+            ->add_callback(static function(?int $value): string {
+                return $value ?? get_string('unknownvalue', 'core_ai');
+            });
+
+        return $columns;
+    }
+
+    /**
+     * Return list of all available filters.
+     *
+     * @return filter[]
+     */
+    protected function get_all_filters(): array {
+        $mainalias = $this->get_table_alias('ai_action_register');
+        $generatetextalias = 'aagt';
+        $summarisetextalias = 'aast';
+
+        // Action name filter.
+        $filters[] = (new filter(
+            text::class,
+            'actionname',
+            new lang_string('action', 'core_ai'),
+            $this->get_entity_name(),
+            "{$mainalias}.actionname",
+        ))
+            ->add_joins($this->get_joins());
+
+        // Provider filter.
+        $filters[] = (new filter(
+            text::class,
+            'provider',
+            new lang_string('provider', 'core_ai'),
+            $this->get_entity_name(),
+            "{$mainalias}.provider",
+        ))
+            ->add_joins($this->get_joins());
+
+        // Time created filter.
+        $filters[] = (new filter(
+            date::class,
+            'timecreated',
+            new lang_string('timegenerated', 'core_ai'),
+            $this->get_entity_name(),
+            "{$mainalias}.timecreated",
+        ))
+            ->add_joins($this->get_joins())
+            ->set_limited_operators([
+                date::DATE_ANY,
+                date::DATE_RANGE,
+                date::DATE_PREVIOUS,
+                date::DATE_CURRENT,
+            ]);
+
+        // Prompt tokens filter.
+        $filters[] = (new filter(
+            number::class,
+            'prompttokens',
+            new lang_string('prompttokens', 'core_ai'),
+            $this->get_entity_name(),
+            "COALESCE({$generatetextalias}.prompttokens, {$summarisetextalias}.prompttokens)",
+        ))
+            ->add_joins($this->get_joins());
+
+        // Completion tokens filter.
+        $filters[] = (new filter(
+            number::class,
+            'completiontokens',
+            new lang_string('completiontokens', 'core_ai'),
+            $this->get_entity_name(),
+            "COALESCE({$generatetextalias}.completiontoken, {$summarisetextalias}.completiontoken)",
+        ))
+            ->add_joins($this->get_joins());
+
+        // Success filter.
+        $filters[] = (new filter(
+            boolean_select::class,
+            'success',
+            new lang_string('success', 'moodle'),
+            $this->get_entity_name(),
+           "{$mainalias}.success",
+        ))
+            ->add_joins($this->get_joins());
+
+        return $filters;
+    }
+}
---
+++ ai/classes/reportbuilder/local/systemreports/usage.php
@@ -0,0 +1,117 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core_ai\reportbuilder\local\systemreports;
+
+use core_reportbuilder\system_report;
+use core_ai\reportbuilder\local\entities\ai_action_register;
+use core\reportbuilder\local\entities\context;
+use core_reportbuilder\local\entities\user;
+
+/**
+ * AI usage system report.
+ *
+ * @package    core_ai
+ * @copyright  2024 David Woloszyn <david.woloszyn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class usage extends system_report {
+
+    #[\Override]
+    protected function initialise(): void {
+        $entitymain = new ai_action_register();
+        $entitymainalias = $entitymain->get_table_alias('ai_action_register');
+
+        $this->set_main_table('ai_action_register', $entitymainalias);
+        $this->add_entity($entitymain);
+
+        // Join the 'user' entity to our main entity.
+        $entityuser = new user();
+        $entituseralias = $entityuser->get_table_alias('user');
+        $this->add_entity($entityuser->add_join(
+            "LEFT JOIN {user} {$entituseralias} ON {$entituseralias}.id = {$entitymainalias}.userid"
+        ));
+
+        // Join the 'context' entity to our main entity.
+        $entitycontext = new context();
+        $entitycontextalias = $entitycontext->get_table_alias('context');
+        $this->add_entity($entitycontext->add_join(
+            "LEFT JOIN {context} {$entitycontextalias} ON {$entitycontextalias}.id = {$entitymainalias}.contextid"
+        ));
+
+        // Now we can call our helper methods to add the content we want to include in the report.
+        $this->add_columns();
+        $this->add_filters();
+
+        // Set if report can be downloaded.
+        $this->set_downloadable(true, get_string('aiusage', 'core_ai'));
+    }
+
+    #[\Override]
+    protected function can_view(): bool {
+        return has_capability('moodle/ai:viewaiusagereport', $this->get_context());
+    }
+
+    #[\Override]
+    public static function get_name(): string {
+        return get_string('aiusage', 'core_ai');
+    }
+
+    /**
+     * Adds the columns we want to display in the report.
+     *
+     * They are all provided by the entities we previously added in the {@see initialise} method, referencing each by their
+     * unique identifier.
+     */
+    public function add_columns(): void {
+        $columns = [
+            'ai_action_register:provider',
+            'ai_action_register:actionname',
+            'ai_action_register:timecreated',
+            'ai_action_register:prompttokens',
+            'ai_action_register:completiontokens',
+            'ai_action_register:success',
+            'context:name',
+            'user:fullnamewithlink',
+        ];
+
+        $this->add_columns_from_entities($columns);
+
+        // It's possible to set a default initial sort direction for one column.
+        $this->set_initial_sort_column('ai_action_register:timecreated', SORT_DESC);
+    }
+
+    /**
+     * Adds the filters we want to display in the report.
+     *
+     * They are all provided by the entities we previously added in the {@see initialise} method, referencing each by their
+     * unique identifier.
+     */
+    protected function add_filters(): void {
+        $filters = [
+            'ai_action_register:actionname',
+            'ai_action_register:provider',
+            'ai_action_register:timecreated',
+            'ai_action_register:prompttokens',
+            'ai_action_register:completiontokens',
+            'ai_action_register:success',
+            'context:level',
+            'user:fullname',
+        ];
+
+        $this->add_filters_from_entities($filters);
+    }
+}
