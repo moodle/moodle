moodle/moodle

commit b92886ad59b3836bfb04e8b8d5d82ad6b65b59a7

MDL-70854 core: Add stored progress bars.
    - Alters existing progress_bar class to support extension
    - Adds stored_progress_bar class as child of progress_bar
    - Adds webservice to poll stored progress
    - Updates database tables
    - Bumps version
    - Adds unit/behat tests

lib/classes/output/core_renderer.php
@@ -4697,15 +4697,12 @@ EOD;
      * @param  float $percent
      * @param  string $msg Message
      * @param  string $estimate time remaining message
+     * @param  bool $error Was there an error?
      * @return string ascii fragment
      */
-    public function render_progress_bar_update(string $id, float $percent, string $msg, string $estimate): string {
-        return html_writer::script(js_writer::function_call('updateProgressBar', [
-            $id,
-            round($percent, 1),
-            $msg,
-            $estimate,
-        ]));
+    public function render_progress_bar_update(string $id, float $percent, string $msg, string $estimate,
+        bool $error = false): string {
+        return html_writer::script(js_writer::function_call('updateProgressBar', [$id, $percent, $msg, $estimate, $error]));
     }
 
     /**

lib/classes/output/progress_bar.php
@@ -34,35 +34,49 @@ use core\exception\coding_exception;
  * @category output
  */
 class progress_bar implements renderable, templatable {
-    /** @var string html id */
-    private $htmlid;
+
+    /** @var bool Can use output buffering. */
+    protected static $supportsoutputbuffering = false;
+
+    /** @var string unique id */
+    protected $idnumber;
+
     /** @var int total width */
-    private $width;
+    protected $width;
+
     /** @var int last percentage printed */
-    private $percent = 0;
+    protected $percent = 0;
+
     /** @var int time when last printed */
-    private $lastupdate = 0;
+    protected $lastupdate = 0;
+
     /** @var int when did we start printing this */
-    private $timestart = 0;
+    protected $timestart = 0;
+
+    /** @var bool Whether or not to auto render updates to the screen */
+    protected $autoupdate = true;
+
+    /** @var bool Whether or not an error has occured */
+    protected $haserrored = false;
 
     /**
      * Constructor
      *
      * Prints JS code if $autostart true.
      *
-     * @param string $htmlid The container ID.
+     * @param string $htmlid The unique ID for the progress bar or HTML container id.
      * @param int $width The suggested width.
      * @param bool $autostart Whether to start the progress bar right away.
      */
     public function __construct($htmlid = '', $width = 500, $autostart = false) {
-        if (!CLI_SCRIPT && !NO_OUTPUT_BUFFERING) {
+        if (!static::$supportsoutputbuffering && !CLI_SCRIPT && !NO_OUTPUT_BUFFERING) {
             debugging('progress_bar used in a non-CLI script without setting NO_OUTPUT_BUFFERING.', DEBUG_DEVELOPER);
         }
 
         if (!empty($htmlid)) {
-            $this->htmlid  = $htmlid;
+            $this->idnumber  = $htmlid;
         } else {
-            $this->htmlid  = 'pbar_' . uniqid();
+            $this->idnumber  = 'pbar_'.uniqid();
         }
 
         $this->width = $width;
@@ -77,7 +91,15 @@ class progress_bar implements renderable, templatable {
      * @return string id
      */
     public function get_id(): string {
-        return $this->htmlid;
+        return $this->idnumber;
+    }
+
+    /**
+     * Get the percent
+     * @return float
+     */
+    public function get_percent(): float {
+        return $this->percent;
     }
 
     /**
@@ -86,13 +108,41 @@ class progress_bar implements renderable, templatable {
      * @return void Echo's output
      */
     public function create() {
-        global $OUTPUT;
 
         $this->timestart = microtime(true);
+        $this->render();
 
+    }
+
+    /**
+     * Render the progress bar.
+     *
+     * @return void
+     */
+    public function render(): void {
         flush();
-        echo $OUTPUT->render($this);
+        echo $this->get_content();
         flush();
+    }
+
+    /**
+     * Get the content to be rendered
+     *
+     * @return string
+     */
+    public function get_content(): string {
+        global $OUTPUT;
+        return $OUTPUT->render($this);
+    }
+
+    /**
+     * Set whether or not to auto render updates to the screen
+     *
+     * @param bool $value
+     * @return void
+     */
+    public function auto_update(bool $value): void {
+        $this->autoupdate = $value;
     }
 
     /**
@@ -103,7 +153,7 @@ class progress_bar implements renderable, templatable {
      * @return void Echo's output
      * @throws coding_exception
      */
-    private function update_raw($percent, $msg) {
+    protected function update_raw($percent, $msg) {
         global $OUTPUT;
 
         if (empty($this->timestart)) {
@@ -113,28 +163,20 @@ class progress_bar implements renderable, templatable {
 
         $estimate = $this->estimate($percent);
 
-        if ($estimate === null) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
-            // Always do the first and last updates.
-        } else if ($estimate == 0) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
-            // Always do the last updates.
-        } else if ($this->lastupdate + 20 < time()) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
-            // We must update otherwise browser would time out.
-        } else if (round($this->percent, 2) === round($percent, 2)) {
-            // No significant change, no need to update anything.
+        // No significant change, no need to update anything.
+        if (round($this->percent, 2) === round($percent, 2)) {
             return;
         }
 
-        $estimatemsg = '';
-        if ($estimate != 0 && is_numeric($estimate)) {
-            // Err on the conservative side and also avoid showing 'now' as the estimate.
-            $estimatemsg = format_time(ceil($estimate));
-        }
+        $estimatemsg = $this->get_estimate_message($percent);
 
         $this->percent = $percent;
         $this->lastupdate = microtime(true);
 
-        echo $OUTPUT->render_progress_bar_update($this->htmlid, $this->percent, $msg, $estimatemsg);
-        flush();
+        if ($this->autoupdate) {
+            echo $OUTPUT->render_progress_bar_update($this->idnumber, sprintf("%.1f", $this->percent), $msg, $estimatemsg);
+            flush();
+        }
     }
 
     /**
@@ -143,7 +185,7 @@ class progress_bar implements renderable, templatable {
      * @param int $pt From 1-100.
      * @return mixed Null (unknown), or int.
      */
-    private function estimate($pt) {
+    protected function estimate($pt) {
         if ($this->lastupdate == 0) {
             return null;
         }
@@ -201,10 +243,68 @@ class progress_bar implements renderable, templatable {
      */
     public function export_for_template(renderer_base $output) {
         return [
-            'id' => $this->htmlid,
+            'id' => '',
+            'idnumber' => $this->idnumber,
             'width' => $this->width,
+            'class' => '',
+            'value' => 0,
+            'error' => 0,
         ];
     }
+
+    /**
+     * This gets the estimate message to be displayed with the progress bar.
+     *
+     * @param float $percent
+     * @return string
+     */
+    public function get_estimate_message(float $percent): string {
+        $estimate = $this->estimate($percent);
+        $estimatemsg = '';
+        if ($estimate != 0 && is_numeric($estimate)) {
+            $estimatemsg = format_time(ceil($estimate));
+        }
+
+        return $estimatemsg;
+    }
+
+    /**
+     * Set the error flag on the object
+     *
+     * @param bool $value
+     * @return void
+     */
+    protected function set_haserrored(bool $value): void {
+        $this->haserrored = $value;
+    }
+
+    /**
+     * Check if the process has errored
+     *
+     * @return bool
+     */
+    public function get_haserrored(): bool {
+        return $this->haserrored;
+    }
+
+    /**
+     * Set that the process running has errored
+     *
+     * @param string $errormsg
+     * @return void
+     */
+    public function error(string $errormsg): void {
+        global $OUTPUT;
+
+        $this->haserrored = true;
+        $this->message = $errormsg;
+
+        if ($this->autoupdate) {
+            echo $OUTPUT->render_progress_bar_update($this->idnumber, sprintf("%.1f", $this->percent), $errormsg, '', true);
+            flush();
+        }
+    }
+
 }
 
 // Alias this class to the old name.

lib/classes/output/stored_progress_bar.php
@@ -1,0 +1,365 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core\output;
+
+/**
+ * Stored progress bar class.
+ *
+ * @package    core
+ * @copyright  2023 onwards Catalyst IT {@link http://www.catalyst-eu.net/}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Conn Warwicker <conn.warwicker@catalyst-eu.net>
+ */
+class stored_progress_bar extends progress_bar {
+
+    /** @var bool Can use output buffering. */
+    protected static $supportsoutputbuffering = true;
+
+    /** @var int DB record ID */
+    protected $recordid;
+
+    /** @var string|null Message to associate with bar */
+    protected $message = null;
+
+    /** @var \core\clock Clock object */
+    protected $clock;
+
+    /**
+     * This overwrites the progress_bar::__construct method.
+     *
+     * @param string $idnumber
+     */
+    public function __construct($idnumber) {
+
+        $this->clock = \core\di::get(\core\clock::class);
+
+        // Construct from the parent.
+        parent::__construct($idnumber, 0, true);
+
+    }
+
+    /**
+     * Just set the timestart, do not render the bar immediately.
+     *
+     * @return void
+     */
+    public function create(): void {
+        $this->timestart = $this->clock->time();
+    }
+
+    /**
+     * Load the stored progress bar from the database based on its uniqued idnumber
+     *
+     * @param string $idnumber Unique ID of the bar
+     * @return stored_progress_bar|null
+     */
+    public static function get_by_idnumber(string $idnumber): ?stored_progress_bar {
+        global $DB;
+
+        $record = $DB->get_record('stored_progress', ['idnumber' => $idnumber]);
+        if ($record) {
+            return self::load($record);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Load the stored progress bar from the database, based on it's record ID
+     *
+     * @param int $id Database record ID
+     * @return stored_progress_bar|null
+     */
+    public static function get_by_id(int $id): ?stored_progress_bar {
+        global $DB;
+
+        $record = $DB->get_record('stored_progress', ['id' => $id]);
+        if ($record) {
+            return self::load($record);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Load the stored progress bar object from its record in the database.
+     *
+     * @param stdClass $record
+     * @return stored_progress_bar
+     */
+    public static function load(\stdClass $record): stored_progress_bar {
+        $progress = new stored_progress_bar($record->idnumber);
+        $progress->set_record_id($record->id);
+        $progress->set_time_started($record->timestart);
+        $progress->set_last_updated($record->lastupdate);
+        $progress->set_percent($record->percentcompleted);
+        $progress->set_message($record->message);
+        $progress->set_haserrored($record->haserrored);
+        return $progress;
+    }
+
+    /**
+     * Set the DB record ID
+     *
+     * @param int $id
+     * @return void
+     */
+    protected function set_record_id(int $id): void {
+        $this->recordid = $id;
+    }
+
+    /**
+     * Set the time we started the process.
+     *
+     * @param int $value
+     * @return void
+     */
+    protected function set_time_started(int $value): void {
+        $this->timestart = $value;
+    }
+
+    /**
+     * Set the time we started last updated the progress.
+     *
+     * @param int|null $value
+     * @return void
+     */
+    protected function set_last_updated(?int $value = null): void {
+        $this->lastupdate = $value;
+    }
+
+    /**
+     * Set the percent completed.
+     *
+     * @param float|null $value
+     * @return void
+     */
+    protected function set_percent($value = null): void {
+        $this->percent = $value;
+    }
+
+    /**
+     * Set the message.
+     *
+     * @param string|null $value
+     * @return void
+     */
+    protected function set_message(?string $value = null): void {
+        $this->message = $value;
+    }
+
+    /**
+     * Set that the process running has errored and store that against the bar
+     *
+     * @param string $errormsg
+     * @return void
+     */
+    public function error(string $errormsg): void {
+        // Update the error variables.
+        parent::error($errormsg);
+
+        // Update the record.
+        $this->update_record();
+    }
+
+    /**
+     * Get the progress bar message.
+     *
+     * @return string|null
+     */
+    public function get_message(): ?string {
+        return $this->message;
+    }
+
+    /**
+     * Get the content to display the progress bar and start polling via AJAX
+     *
+     * @return string
+     */
+    public function get_content(): string {
+        global $CFG, $PAGE, $OUTPUT;
+
+        $PAGE->requires->js_call_amd('core/stored_progress', 'init', [
+            self::get_timeout(),
+        ]);
+
+        $context = $this->export_for_template($OUTPUT);
+        return $OUTPUT->render_from_template('core/progress_bar', $context);
+    }
+
+    /**
+     * Export for template.
+     *
+     * @param  renderer_base $output The renderer.
+     * @return array
+     */
+    public function export_for_template(\renderer_base $output): array {
+        return [
+            'id' => $this->recordid,
+            'idnumber' => $this->idnumber,
+            'width' => $this->width,
+            'class' => 'stored-progress-bar',
+            'value' => $this->percent,
+            'message' => $this->message,
+            'error' => $this->haserrored,
+        ];
+    }
+
+    /**
+     * Start the recording of the progress and store in the database
+     *
+     * @return int ID of the DB record
+     */
+    public function start(): int {
+        global $OUTPUT, $DB;
+
+        // If we are running in an non-interactive CLI environment, call the progress bar renderer to avoid warnings
+        // when we do an update.
+        if (defined('STDOUT') && !stream_isatty(STDOUT)) {
+            $OUTPUT->render_progress_bar($this);
+        }
+
+        // Delete any existing records for this.
+        $this->clear_records();
+
+        // Create new progress record.
+        $this->recordid = $DB->insert_record('stored_progress', [
+            'idnumber' => $this->idnumber,
+            'timestart' => (int)$this->timestart,
+        ]);
+
+        return $this->recordid;
+    }
+
+    /**
+     * End the polling progress and delete the DB record.
+     *
+     * @return void
+     */
+    protected function clear_records(): void {
+        global $DB;
+
+        $DB->delete_records('stored_progress', [
+            'idnumber' => $this->idnumber,
+        ]);
+    }
+
+    /**
+     * Update the database record with the percentage and message
+     *
+     * @param float $percent
+     * @param string $msg
+     * @return void
+     */
+    protected function update_raw($percent, $msg): void {
+        $this->percent = $percent;
+        $this->message = $msg;
+
+        // Update the database record with the new data.
+        $this->update_record();
+
+        // Update any CLI script's progress with an ASCII progress bar.
+        $this->render_update();
+    }
+
+    /**
+     * Render an update to the CLI
+     *
+     * This will only work in CLI scripts, and not in scheduled/adhoc tasks even though they run via CLI,
+     * as they seem to use a different renderer (core_renderer instead of core_renderer_cli).
+     *
+     * We also can't check this based on "CLI_SCRIPT" const as that is true for tasks.
+     *
+     * So this will just check a flag to see if we want auto rendering of updates.
+     *
+     * @return void
+     */
+    protected function render_update(): void {
+        global $OUTPUT;
+
+        // If no output buffering, don't render it at all.
+        if (defined('NO_OUTPUT_BUFFERING') && NO_OUTPUT_BUFFERING) {
+            $this->auto_update(false);
+        }
+
+        // If we want the screen to auto update, render it.
+        if ($this->autoupdate) {
+            echo $OUTPUT->render_progress_bar_update(
+                $this->idnumber, sprintf("%.1f", $this->percent), $this->message, $this->get_estimate_message($this->percent)
+            );
+        }
+    }
+
+    /**
+     * Update the database record
+     *
+     * @throws \moodle_exception
+     * @return void
+     */
+    protected function update_record(): void {
+        global $DB;
+
+        if (is_null($this->recordid)) {
+            throw new \moodle_exception('Polling has not been started. Cannot set iteration.');
+        }
+
+        // Update time.
+        $this->lastupdate = $this->clock->time();
+
+        // Update the database record.
+        $record = new \stdClass();
+        $record->id = $this->recordid;
+        $record->lastupdate = (int)$this->lastupdate;
+        $record->percentcompleted = $this->percent;
+        $record->message = $this->message;
+        $record->haserrored = $this->haserrored;
+        $DB->update_record('stored_progress', $record);
+    }
+
+    /**
+     * We need a way to specify a unique idnumber for processes being monitored, so that
+     * firstly we don't accidentally overwrite a running process, and secondly so we can
+     * automatically load them in some cases, without having to manually code in its name.
+     *
+     * So this uses the classname of the object being monitored, along with its id.
+     *
+     * This method should be used when creating the stored_progress record to set it's idnumber.
+     *
+     * @param string $class Class name of the object being monitored, e.g. \local_something\task\my_task
+     * @param int|null $id ID of an object from database, e.g. 123
+     * @return string Converted string, e.g. local_something_task_my_task_123
+     */
+    public static function convert_to_idnumber(string $class, ?int $id = null): string {
+        $idnumber = preg_replace("/[^a-z0-9_]/", "_", ltrim($class, '\\'));
+        if (!is_null($id)) {
+            $idnumber .= '_' . $id;
+        }
+
+        return $idnumber;
+    }
+
+    /**
+     * Get the polling timeout in seconds. Default: 5.
+     *
+     * @return int
+     */
+    public static function get_timeout(): int {
+        global $CFG;
+        return $CFG->progresspollinterval ?? 5;
+    }
+
+}

.upgradenotes/MDL-70854-2024071306574741.yml
@@ -1,0 +1,5 @@
+issueNumber: MDL-70854
+notes:
+  core:
+    - message: Added stored progress bars
+      type: improved

admin/tool/task/classes/running_tasks_table.php
@@ -49,6 +49,7 @@ class running_tasks_table extends \table_sql {
             'classname'    => get_string('classname', 'tool_task'),
             'type'         => get_string('tasktype', 'admin'),
             'time'         => get_string('taskage', 'tool_task'),
+            'progress'     => get_string('progress', 'core'),
             'timestarted'  => get_string('started', 'tool_task'),
             'hostname'     => get_string('hostname', 'tool_task'),
             'pid'          => get_string('pid', 'tool_task'),
@@ -153,4 +154,27 @@ class running_tasks_table extends \table_sql {
     public function col_timestarted($row): string {
         return userdate($row->timestarted);
     }
+
+    /**
+     * Format the progress column.
+     *
+     * @param \stdClass $row
+     * @return string
+     */
+    public function col_progress($row): string {
+        // Check to see if there is a stored progress record for this task.
+        if ($row->type === 'adhoc') {
+            $idnumber = \core\output\stored_progress_bar::convert_to_idnumber($row->classname, $row->id);
+        } else {
+            $idnumber = \core\output\stored_progress_bar::convert_to_idnumber($row->classname);
+        }
+
+        $bar = \core\output\stored_progress_bar::get_by_idnumber($idnumber);
+        if ($bar) {
+            return $bar->get_content();
+        } else {
+            return '-';
+        }
+    }
+
 }

admin/tool/task/tests/behat/running_tasks.feature
@@ -38,3 +38,24 @@ Feature: See running scheduled tasks
     And I should see "2 days" in the "core\task\asynchronous_restore_task" "table_row"
     And I should see "c69335460f7f" in the "core\task\asynchronous_restore_task" "table_row"
     And I should see "1916" in the "core\task\asynchronous_restore_task" "table_row"
+
+  @javascript
+  Scenario: If a task with a stored progress bar is running, I should be able to observe the progress.
+    Given the following config values are set as admin:
+      | progresspollinterval | 1 |
+    And the following "tool_task > scheduled tasks" exist:
+      | classname                                | seconds | hostname     | pid  |
+      | \core\task\delete_unconfirmed_users_task | 120     | c69335460f7f | 1917 |
+    And the following "stored progress bars" exist:
+      | idnumber                                | percent |
+      | core_task_delete_unconfirmed_users_task | 50.00   |
+    And I navigate to "Server > Tasks > Tasks running now" in site administration
+    And I should see "2 mins" in the "Delete unconfirmed users" "table_row"
+    And I should see "c69335460f7f" in the "Delete unconfirmed users" "table_row"
+    And I should see "1917" in the "Delete unconfirmed users" "table_row"
+    And I should see "50.0%" in the "Delete unconfirmed users" "table_row"
+    When I set the stored progress bar "core_task_delete_unconfirmed_users_task" to "75.00"
+    # Wait for the progress polling.
+    And I wait "1" seconds
+    Then I should not see "50.0%" in the "Delete unconfirmed users" "table_row"
+    And I should see "75.0%" in the "Delete unconfirmed users" "table_row"

config-dist.php
@@ -773,6 +773,15 @@ $CFG->admin = 'admin';
 // Defaults to 60 minutes.
 //
 //      $CFG->enrolments_sync_interval = 3600
+//
+// Stored progress polling interval
+//
+// Stored progress bars which can be polled for updates via AJAX can be controlled by the
+// `progresspollinterval` config setting, to determine the interval (in seconds) at which the
+// polling should be done and latest update retrieved.
+// If no value is set, then it will default to 5 seconds.
+//
+// $CFG->progresspollinterval = 5;
 
 //=========================================================================
 // 7. SETTINGS FOR DEVELOPMENT SERVERS - not intended for production use!!!

lang/en/admin.php
@@ -1342,6 +1342,7 @@ $string['stickyblockscourseview'] = 'Course page';
 $string['stickyblocksduplicatenotice'] = 'If any block you add here is already present in a particular page, it will result in a duplicate.<br />Only the pinned block will be non-editable, the duplicate will still be editable.';
 $string['stickyblocksmymoodle'] = 'My Moodle';
 $string['stickyblockspagetype'] = 'Page type to configure';
+$string['storedprogressbarcleanuptask'] = 'Stored progress bar cleanup task';
 $string['strictformsrequired'] = 'Strict validation of required fields';
 $string['stripalltitletags'] = 'Remove HTML tags from all activity names';
 $string['supportandservices'] = 'Support and services';

commit a296bdfc5b68d3e2cd0fddf51b49bb1b95d2f878

MDL-84072 output: correct progress bar component percentage parameter.
Avoid mixing float/string types, where the decimal separator could
vary according to current locale (as per previous fix 5a1aef5a, which
was subsequently re-broken by b92886ad).

lib/classes/output/core_renderer.php
@@ -4797,7 +4797,13 @@ EOD;
      */
     public function render_progress_bar_update(string $id, float $percent, string $msg, string $estimate,
         bool $error = false): string {
-        return html_writer::script(js_writer::function_call('updateProgressBar', [$id, $percent, $msg, $estimate, $error]));
+        return html_writer::script(js_writer::function_call('updateProgressBar', [
+            $id,
+            round($percent, 1),
+            $msg,
+            $estimate,
+            $error,
+        ]));
     }
 
     /**

lib/classes/output/progress_bar.php
@@ -181,7 +181,7 @@ class progress_bar implements renderable, templatable {
         $this->lastupdate = microtime(true);
 
         if ($this->autoupdate) {
-            echo $OUTPUT->render_progress_bar_update($this->idnumber, sprintf("%.1f", $this->percent), $msg, $estimatemsg);
+            echo $OUTPUT->render_progress_bar_update($this->idnumber, $this->percent, $msg, $estimatemsg);
             flush();
         }
     }
@@ -307,7 +307,7 @@ class progress_bar implements renderable, templatable {
         $this->message = $errormsg;
 
         if ($this->autoupdate) {
-            echo $OUTPUT->render_progress_bar_update($this->idnumber, sprintf("%.1f", $this->percent), $errormsg, '', true);
+            echo $OUTPUT->render_progress_bar_update($this->idnumber, $this->percent, $errormsg, '', true);
             flush();
         }
     }

lib/classes/output/stored_progress_bar.php
@@ -299,7 +299,7 @@ class stored_progress_bar extends progress_bar {
         // If we want the screen to auto update, render it.
         if ($this->autoupdate) {
             echo $OUTPUT->render_progress_bar_update(
-                $this->idnumber, sprintf("%.1f", $this->percent), $this->message, $this->get_estimate_message($this->percent)
+                $this->idnumber, $this->percent, $this->message, $this->get_estimate_message($this->percent)
             );
         }
     }