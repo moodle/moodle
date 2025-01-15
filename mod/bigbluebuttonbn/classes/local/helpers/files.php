9619f598035267ca9ea4703285632819297ea312
Paul Holden <paulh@moodle.com>
2025-01-15 14:01:17 +0000

    MDL-84214 mod_bigbluebuttonbn: fix default presentation file serving.

--- mod/bigbluebuttonbn/classes/local/helpers/files.php
+++ mod/bigbluebuttonbn/classes/local/helpers/files.php
@@ -251,9 +251,12 @@ class files {
         } else {
             require_login(null, true, $cm, true, true);
         }
-        if (!has_capability('mod/bigbluebuttonbn:join', $context)) {
+
+        // If the file belongs to a course module, ensure user can join the activity.
+        if ($cm !== null && !has_capability('mod/bigbluebuttonbn:join', $context)) {
             return null;
         }
+
         return implode('/', $args);
     }
 
