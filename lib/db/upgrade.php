660f12dc7c5cefc7fb5c2733a12e120f3a6486db
Daniel Ziegenberg <daniel@ziegenberg.at>
2025-01-23 17:03:07 +0100

    MDL-84652 block_mnet_hosts: Remove block_mnet_hosts from Moodle 5.0
    Original issue: MDL-84309
    
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -1491,5 +1491,14 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2025030400.03);
     }
 
+    // Remove block_mnet_hosts.
+    if ($oldversion < 2025030400.04) {
+        if (!file_exists($CFG->dirroot . "/blocks/mnet_hosts/version.php")) {
+            uninstall_plugin('block', 'mnet_hosts');
+        }
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2025030400.04);
+    }
+
     return true;
 }
