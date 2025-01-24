70149c2483cb953e7b147f5891c6cf2ea9e401d1
Colin Beeby <colin.beeby@hee.nhs.uk>
2025-01-24 15:27:25 +0000

    fixed issue with managed instance name

--- Terraform/test/main.tf
+++ Terraform/test/main.tf
@@ -111,7 +111,7 @@ resource "azurerm_subnet_route_table_association" "subnet_route_table_associatio
 }
 
 resource "azurerm_mssql_managed_instance" "sqlmi" {
-  name = "learninghub-moodle-sql-mi"
+  name = "learninghub-moodle-sql-mi-test"
   resource_group_name = azurerm_resource_group.learningHubMoodleResourceGroup.name
   location = azurerm_resource_group.learningHubMoodleResourceGroup.location
   license_type = "BasePrice"
