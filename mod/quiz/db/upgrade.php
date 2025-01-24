61632fcd434b6d6fabd1e0b5f299cf6636956a58
Luca Bösch <luca.boesch@bfh.ch>
2025-01-24 16:08:47 +0100

    MDL-83985 mod_quiz: change name to 1333 chars

--- mod/quiz/db/upgrade.php
+++ mod/quiz/db/upgrade.php
@@ -136,5 +136,18 @@ function xmldb_quiz_upgrade($oldversion) {
     // Automatically generated Moodle v5.0.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2025041401) {
+
+        // Changing precision of field name on table quiz to (1333).
+        $table = new xmldb_table('quiz');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'course');
+
+        // Launch change of precision for field name.
+        $dbman->change_field_precision($table, $field);
+
+        // Quiz savepoint reached.
+        upgrade_mod_savepoint(true, 2025041401, 'quiz');
+    }
+
     return true;
 }
--- mod/quiz/version.php
+++ mod/quiz/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025041400;
+$plugin->version = 2025041401;
 $plugin->requires = 2025040800;
 $plugin->component = 'mod_quiz';
