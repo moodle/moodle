09c191aefdb9a197df36ce8f42e4e38f42a8c637
Paul Holden <paulh@moodle.com>
2025-01-24 10:50:09 +0000

    MDL-83537 check: confirm presence of instance action link.

--- lib/classes/output/core_renderer.php
+++ lib/classes/output/core_renderer.php
@@ -1353,7 +1353,9 @@ class core_renderer extends renderer_base {
         }
 
         // Add the action link.
-        $renderedresult .= $this->render_action_link($check->get_action_link());
+        if ($actionlink = $check->get_action_link()) {
+            $renderedresult .= $this->render_action_link($actionlink);
+        }
 
         return $renderedresult;
     }
