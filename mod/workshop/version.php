19cc7dc5df5b6232a18a05d9bec16f4f112f891e
Luca Bösch <luca.boesch@bfh.ch>
2025-01-24 16:20:14 +0100

    MDL-83985 mod_workshop: change name to 1333 chars

--- mod/workshop/version.php
+++ mod/workshop/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025041400;        // The current module version (YYYYMMDDXX).
+$plugin->version   = 2025041401;        // The current module version (YYYYMMDDXX).
 $plugin->requires  = 2025040800;        // Requires this Moodle version.
 $plugin->component = 'mod_workshop';
