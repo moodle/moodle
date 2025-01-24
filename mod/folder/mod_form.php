c3af150e1f3f7a44ed90a3d0649e15c87816a971
Luca Bösch <luca.boesch@bfh.ch>
2025-01-24 14:45:18 +0100

    MDL-83985 mod_folder: change name to 1333 chars

--- mod/folder/mod_form.php
+++ mod/folder/mod_form.php
@@ -43,7 +43,7 @@ class mod_folder_mod_form extends moodleform_mod {
             $mform->setType('name', PARAM_CLEANHTML);
         }
         $mform->addRule('name', null, 'required', null, 'client');
-        $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
+        $mform->addRule('name', get_string('maximumchars', '', 1333), 'maxlength', 1333, 'client');
         $this->standard_intro_elements();
 
         //-------------------------------------------------------
