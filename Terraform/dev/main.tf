8cb25b7674d5195464bbf3cbaae4c8babeb40dce
Colin Beeby <colin.beeby@hee.nhs.uk>
2025-02-06 14:03:18 +0000

    Added private endpoint to the redis cache

--- Terraform/dev/main.tf
+++ Terraform/dev/main.tf
@@ -259,17 +259,6 @@ resource "azurerm_mssql_managed_database" "sqldb" {
   managed_instance_id = azurerm_mssql_managed_instance.sqlmi.id
 }
 
-data "azurerm_virtual_network" "aks_vnet" {
-  name                = "aks-vnet-10306673"
-  resource_group_name = azurerm_resource_group.learningHubMoodleResourceGroup.name
-}
-
-data "azurerm_subnet" "aks_subnet" {
-  name                 = "aks-subnet"
-  virtual_network_name = data.azurerm_virtual_network.aks_vnet.name
-  resource_group_name  = data.azurerm_virtual_network.aks_vnet.resource_group_name
-}
-
 resource "azurerm_redis_cache" "moodle_cache" {
   name                = "moodle-cache"
   resource_group_name = azurerm_resource_group.learningHubMoodleResourceGroup.name
@@ -279,7 +268,4 @@ resource "azurerm_redis_cache" "moodle_cache" {
   sku_name            = "Standard"
   non_ssl_port_enabled = false
   minimum_tls_version = "1.2"
-  private_endpoint {
-    subnet_id = data.azurerm_subnet.aks_subnet.id
-  }
 }
