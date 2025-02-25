9011f8a9dba5cdc5576bcf32eaa2249151c5c918
Safat <safat.shahin@moodle.com>
2025-02-25 15:47:00 +1100

    MDL-79329 smsgateway_modica: Add Modica SMS gateway to core

---
+++ sms/gateway/modica/classes/gateway.php
@@ -0,0 +1,88 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace smsgateway_modica;
+
+use core\http_client;
+use core_sms\manager;
+use core_sms\message;
+use GuzzleHttp\Exception\GuzzleException;
+
+/**
+ * Modica SMS gateway.
+ *
+ * @see https://confluence.modicagroup.com/display/DC/Mobile+Gateway+REST+API#MobileGatewayRESTAPI-Sendingtoasingledestination
+ * @package    smsgateway_modica
+ * @copyright  2025 Safat Shahin <safat.shahin@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class gateway extends \core_sms\gateway {
+
+    /**
+     * @var string MODICA_DEFAULT_API The default api gateway for modica.
+     */
+    public const MODICA_DEFAULT_API = 'https://api.modicagroup.com/rest/gateway/messages';
+
+    #[\Override]
+    public function send(
+        message $message,
+    ): message {
+        $recipientnumber = manager::format_number(
+            phonenumber: $message->recipientnumber,
+            countrycode: $this->config->countrycode ?? null,
+        );
+        $json = json_encode(
+            [
+                'destination' => $recipientnumber,
+                'content' => $message->content,
+            ],
+            JSON_THROW_ON_ERROR,
+        );
+
+        $client = \core\di::get(http_client::class);
+        try {
+            $response = $client->post(
+                uri: $config->modica_url ?? self::MODICA_DEFAULT_API,
+                options:
+                [
+                    'auth' => [
+                        $this->config->modica_application_name,
+                        $this->config->modica_application_password,
+                    ],
+                    'headers' => ['Content-Type' => 'application/json'],
+                    'body' => $json,
+                ],
+            );
+
+            if ($response->getStatusCode() === 201) {
+                $status = \core_sms\message_status::GATEWAY_SENT;
+            } else {
+                $status = \core_sms\message_status::GATEWAY_FAILED;
+            }
+        } catch (GuzzleException $e) {
+            $status = \core_sms\message_status::GATEWAY_FAILED;
+        }
+
+        return $message->with(
+            status: $status,
+        );
+    }
+
+    #[\Override]
+    public function get_send_priority(message $message): int {
+        return 50;
+    }
+}
---
+++ sms/gateway/modica/classes/hook_listener.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace smsgateway_modica;
+
+use core_sms\hook\after_sms_gateway_form_hook;
+
+/**
+ * Hook listener for Modica sms gateway.
+ *
+ * @package    smsgateway_modica
+ * @copyright  2025 Safat Shahin <safat.shahin@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class hook_listener {
+
+    /**
+     * Hook listener for the sms gateway setup form.
+     *
+     * @param after_sms_gateway_form_hook $hook The hook to add to sms gateway setup.
+     */
+    public static function set_form_definition_for_modica_sms_gateway(after_sms_gateway_form_hook $hook): void {
+        if ($hook->plugin !== 'smsgateway_modica') {
+            return;
+        }
+
+        $mform = $hook->mform;
+
+        $mform->addElement('static', 'information', get_string('modica_information', 'smsgateway_modica'));
+
+        $mform->addElement(
+            'text',
+            'modica_url',
+            get_string('modica_url', 'smsgateway_modica'),
+            'maxlength="255" size="20"',
+        );
+        $mform->setType('modica_url', PARAM_URL);
+        $mform->addRule('modica_url', get_string('maximumchars', '', 255), 'maxlength', 255);
+        $mform->addRule('modica_url', null, 'required');
+        $mform->setDefault(
+            elementName: 'modica_url',
+            defaultValue: gateway::MODICA_DEFAULT_API,
+        );
+
+        $mform->addElement(
+            'text',
+            'modica_application_name',
+            get_string('modica_application_name', 'smsgateway_modica'),
+            'maxlength="255" size="20"',
+        );
+        $mform->setType('modica_application_name', PARAM_TEXT);
+        $mform->addRule('modica_application_name', get_string('maximumchars', '', 255), 'maxlength', 255);
+        $mform->addRule('modica_application_name', null, 'required');
+        $mform->setDefault(
+            elementName: 'modica_application_name',
+            defaultValue: '',
+        );
+
+        $mform->addElement(
+            'passwordunmask',
+            'modica_application_password',
+            get_string('modica_application_password', 'smsgateway_modica'),
+            'maxlength="255" size="20"',
+        );
+        $mform->setType('modica_application_password', PARAM_TEXT);
+        $mform->addRule('modica_application_password', get_string('maximumchars', '', 255), 'maxlength', 255);
+        $mform->addRule('modica_application_password', null, 'required');
+        $mform->setDefault(
+            elementName: 'modica_application_password',
+            defaultValue: '',
+        );
+    }
+}
