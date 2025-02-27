c6a4b5cf842a5f66c0729180322502aa84032fd0
Colin Beeby <colin.beeby@hee.nhs.uk>
2025-02-27 16:31:30 +0000

    Added fix recommendations section to Action

--- Terraform/dev/main.tf
+++ Terraform/dev/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_D4_v3"
+    vm_size    = "Standard_B4ms"
     temporary_name_for_rotation = "tmpnodepool1"
   }
   identity {
