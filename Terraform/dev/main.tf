73c394eae31958598f0b2058f3ed352a949f297b
Colin Beeby <colin.beeby@hee.nhs.uk>
2025-02-28 15:46:51 +0000

    Fixing Azure recommendations

--- Terraform/dev/main.tf
+++ Terraform/dev/main.tf
@@ -287,6 +287,12 @@ resource "azurerm_mssql_managed_instance" "sqlmi" {
   }
 }
 
+resource "azurerm_mssql_managed_instance_vulnerability_assessment" "sqlmi_va" {
+  managed_instance_id = azurerm_mssql_managed_instance.sqlmi.id
+  storage_container_path = "https://${azurerm_storage_account.storage_account.name}.blob.core.windows.net/${azurerm_storage_container.assessment_container.name}"
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+}
+
 resource "azurerm_mssql_managed_database" "sqldb" {
   name = "LearningHubMoodle"
   managed_instance_id = azurerm_mssql_managed_instance.sqlmi.id
