ae31be1e34b8c6f3cb02784eb05a2f6b23413d49
Jun Pataleta <jun@moodle.com>
2025-03-08 11:44:23 +0800

    weekly release 4.5.2+

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024100702.05;              // 20241007      = branching date YYYYMMDD - do not modify!
+$version  = 2024100702.06;              // 20241007      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.5.2+ (Build: 20250304)';    // Human-friendly version name
+$release  = '4.5.2+ (Build: 20250308)';    // Human-friendly version name
 $branch   = '405';                      // This version's branch.
 $maturity = MATURITY_STABLE;            // This version's maturity level.
