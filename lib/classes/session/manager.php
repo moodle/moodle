1501f6644b97a791aa6d640a0065bf5c6baf2dd4
Paul Holden <paulh@moodle.com>
2025-03-17 18:13:35 +0000

    MDL-84901 auth: specific session start error language string.

--- lib/classes/session/manager.php
+++ lib/classes/session/manager.php
@@ -153,7 +153,7 @@ class manager {
 
             if (!self::$handler->start()) {
                 // Could not successfully start/recover session.
-                throw new \core\session\exception(get_string('servererror'));
+                throw new \core\session\exception('sessionstarterror', 'error');
             }
 
             if ($requireslock) {
