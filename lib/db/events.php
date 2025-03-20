2a077d88b93a5e743f0d5413dc89cf11dee4c676
Dai Nguyen Trong <ngtrdai@hotmail.com>
2025-03-20 09:37:01 +0700

    MDL-83904 badges: Add namespace to badges/classes/observer.php

--- lib/db/events.php
+++ lib/db/events.php
@@ -34,47 +34,46 @@ defined('MOODLE_INTERNAL') || die();
 
 // List of legacy event handlers.
 
-$handlers = array(
+$handlers = [
     // No more old events!
-);
+];
 
 // List of events_2 observers.
 
-$observers = array(
-
-    array(
+$observers = [
+    [
         'eventname'   => '\core\event\course_module_completion_updated',
-        'callback'    => 'core_badges_observer::course_module_criteria_review',
-    ),
-    array(
+        'callback'    => '\core_badges\event\observer::course_module_criteria_review',
+    ],
+    [
         'eventname'   => '\core\event\badge_awarded',
-        'callback'    => 'core_badges_observer::badge_criteria_review',
-    ),
-    array(
+        'callback'    => '\core_badges\event\observer::badge_criteria_review',
+    ],
+    [
         'eventname'   => '\core\event\course_completed',
-        'callback'    => 'core_badges_observer::course_criteria_review',
-    ),
-    array(
+        'callback'    => '\core_badges\event\observer::course_criteria_review',
+    ],
+    [
         'eventname'   => '\core\event\user_updated',
-        'callback'    => 'core_badges_observer::profile_criteria_review',
-    ),
-    array(
+        'callback'    => '\core_badges\event\observer::profile_criteria_review',
+    ],
+    [
         'eventname'   => '\core\event\cohort_member_added',
-        'callback'    => 'core_badges_observer::cohort_criteria_review',
-    ),
-    array(
+        'callback'    => '\core_badges\event\observer::cohort_criteria_review',
+    ],
+    [
         'eventname'   => '\core\event\competency_evidence_created',
-        'callback'    => 'core_badges_observer::competency_criteria_review',
-    ),
+        'callback'    => '\core_badges\event\observer::competency_criteria_review',
+    ],
     // Competencies.
-    array(
+    [
         'eventname'   => '\core\event\course_completed',
         'callback'    => 'core_competency\api::observe_course_completed',
-    ),
-    array(
+    ],
+    [
         'eventname'   => '\core\event\course_module_completion_updated',
         'callback'    => 'core_competency\api::observe_course_module_completion_updated',
-    ),
-);
+    ],
+];
 
 // List of all events triggered by Moodle can be found using Events list report.
