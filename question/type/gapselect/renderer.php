992949915d22d2a2d3bee19e19ab5888350a3ae4
Mikel Martín <mikel@moodle.com>
2025-03-20 17:40:45 +0100

    MDL-84894 core_question: Fix question input elements layout
    After Bootstrap 5 upgrade in MDL-75669 some inline input elements inside questions
    (qtype_gapselect, qtype_match and qtype_multianswer) are not correctly displayed.
    That happened because new "form-select" class adds "display: block" instead of
    "display: inline-block" as before in Bootstrap 4.
    This commit fixes the "select" inputs in the question types adding "d-inline-block"
    class.

--- question/type/gapselect/renderer.php
+++ question/type/gapselect/renderer.php
@@ -46,7 +46,7 @@ class qtype_gapselect_renderer extends qtype_elements_embedded_in_question_text_
 
         $attributes = [
             'id' => $this->box_id($qa, 'p' . $place),
-            'class' => 'form-select place' . $place,
+            'class' => 'form-select d-inline-block place' . $place,
         ];
         $groupclass = 'group' . $group;
 
