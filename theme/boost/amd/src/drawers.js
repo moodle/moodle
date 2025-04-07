e41473d9ff5232a12a3809e915361ae04632cd06
Laurent David <lmedavid@gmail.com>
2025-04-07 09:33:42 +0200

    MDL-81702 theme_boost: Add focus lock to drawers
    * Add focus lock to drawers for small screens

--- theme/boost/amd/src/drawers.js
+++ theme/boost/amd/src/drawers.js
@@ -28,6 +28,7 @@ import {debounce} from 'core/utils';
 import {isSmall, isLarge} from 'core/pagehelpers';
 import Pending from 'core/pending';
 import {setUserPreference} from 'core_user/repository';
+import * as FocusLock from 'core/local/aria/focuslock';
 // The jQuery module is only used for interacting with Boostrap 4. It can we removed when MDL-71979 is integrated.
 import jQuery from 'jquery';
 
@@ -460,7 +461,9 @@ export default class Drawers {
                 pageWrapper.style.overflow = 'hidden';
                 return backdrop;
             })
-            .catch();
+            .catch(() => {
+                return;
+            });
         }
 
         // Show close button and header content once the drawer is fully opened.
@@ -475,6 +478,11 @@ export default class Drawers {
             if (focusOnCloseButton) {
                 closeButton.focus();
             }
+            // On small devices, the drawer must have a trap focus once the focus is inside
+            // to prevent the user from focussing on covered elements.
+            if (isSmall()) {
+                FocusLock.trapFocus(this.drawerNode);
+            }
             pendingPromise.resolve();
         }, 300);
 
@@ -488,7 +496,7 @@ export default class Drawers {
      * @param {boolean} [args.focusOnOpenButton=true] Whether to alter page focus when opening the drawer
      * @param {boolean} [args.updatePreferences=true] Whether to update the user prewference
      */
-    closeDrawer({focusOnOpenButton = true, updatePreferences = true} = {}) {
+        closeDrawer({focusOnOpenButton = true, updatePreferences = true} = {}) {
 
         const pendingPromise = new Pending('theme_boost/drawers:close');
 
@@ -531,8 +539,13 @@ export default class Drawers {
             }
             return backdrop;
         })
-        .catch();
+        .catch(() => {
+                return;
+        });
 
+        if (isSmall()) {
+            FocusLock.untrapFocus();
+        }
         // Move focus to the open drawer (or toggler) button once the drawer is hidden.
         let openButton = getDrawerOpenButton(this.drawerNode.id);
         if (openButton) {
