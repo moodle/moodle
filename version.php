e3b05240c1a7af72765a839188fdbd4742446761
Sara Arjona <sara@moodle.com>
2025-04-08 17:06:38 +0200

    Moodle release 5.0rc2

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2025040700.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2025040800.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '5.0rc1 (Build: 20250404)';    // Human-friendly version name
+$release  = '5.0rc2 (Build: 20250408)';    // Human-friendly version name
 $branch   = '500';                      // This version's branch.
 $maturity = MATURITY_RC;                // This version's maturity level.
