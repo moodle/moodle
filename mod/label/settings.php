ecadc40c0d0cf529e406e68c6b774ea0bd8a078a
ferran <ferranrecio@gmail.com>
2025-04-10 12:22:21 +0200

    MDL-83081 mod_label: remove dndmedia setting

--- mod/label/settings.php
+++ mod/label/settings.php
@@ -26,9 +26,6 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
-    $settings->add(new admin_setting_configcheckbox('label/dndmedia',
-        get_string('dndmedia', 'mod_label'), get_string('configdndmedia', 'mod_label'), 1));
-
     $settings->add(new admin_setting_configtext('label/dndresizewidth',
         get_string('dndresizewidth', 'mod_label'), get_string('configdndresizewidth', 'mod_label'), 400, PARAM_INT, 6));
 
