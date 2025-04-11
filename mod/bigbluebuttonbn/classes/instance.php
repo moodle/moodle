f591916893770b9e6dd4be46efc0e609d18946b8
Vincent Schneider <22986332+cli-ish@users.noreply.github.com>
2025-04-11 21:42:29 +0200

    MDL-84706 bigbluebuttonbn: improved authorization and mitigate CSRF risks

--- mod/bigbluebuttonbn/classes/instance.php
+++ mod/bigbluebuttonbn/classes/instance.php
@@ -1134,7 +1134,8 @@ EOF;
         return new moodle_url('/mod/bigbluebuttonbn/bbb_view.php', [
             'action' => 'logout',
             'id' => $this->get_cm()->id,
-            'courseid' => $this->get_cm()->course // Used to find the course if ever the activity is deleted
+            'courseid' => $this->get_cm()->course, // Used to find the course if ever the activity is deleted
+            'sesskey' => sesskey(),
             // while the meeting is running.
         ]);
     }
@@ -1173,6 +1174,7 @@ EOF;
             'action' => 'join',
             'id' => $this->get_cm()->id,
             'bn' => $this->instancedata->id,
+            'sesskey' => sesskey(),
         ]);
     }
 
