5d640c4b5c27853306a614c0465ceef3600fedf9
Daniel Ureña <daniel.urena@moodle.com>
2025-05-08 10:43:28 +0200

    MDL-85407 mod_data: Fix unclosed HTML in list view

--- mod/data/lib.php
+++ mod/data/lib.php
@@ -1866,7 +1866,6 @@ function data_print_preference_form($data, $perpage, $search, $sort='', $order='
     echo '<input type="submit" class="btn btn-secondary" value="' . get_string('savesettings', 'data') . '" />';
     echo '</div>';
     echo '</div>';
-    echo '<div>';
 
     echo '<br />';
     echo '<div class="' . $advancedsearchclass . '" id="data_adv_form">';
