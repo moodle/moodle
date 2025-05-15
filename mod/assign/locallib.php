a3d6763e713afe86b22c8bbffdd43e6b6638bfbb
aya-saad1 <aya.saad@moodle.com>
2025-05-15 15:20:44 +0930

    MDL-42263 assign: Force language of email sent to graders
    Co-authored-by: Jayce Birrell <jayce.birrell@moodle.com>

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -6607,6 +6607,7 @@ class assign {
         global $USER;
         $userid = core_user::is_real_user($userfrom->id) ? $userfrom->id : $USER->id;
         $uniqueid = $this->get_uniqueid_for_user($userid);
+        $oldforcelang = force_current_language($userto->lang);
         self::send_assignment_notification($userfrom,
                                            $userto,
                                            $messagetype,
@@ -6620,6 +6621,7 @@ class assign {
                                            $this->is_blind_marking(),
                                            $uniqueid,
                                            $extrainfo);
+        force_current_language($oldforcelang);
     }
 
     /**
@@ -6668,7 +6670,9 @@ class assign {
             $user = $USER;
         }
         // Prepare extra data for submission receipt notification.
+        $oldforcelang = force_current_language($user->lang);
         $extrainfo = $this->get_submission_summaries_for_messages($submission);
+        force_current_language($oldforcelang);
         if ($submission->userid == $USER->id) {
             $this->send_notification(core_user::get_noreply_user(),
                                      $user,
