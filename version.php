b4a8c83ea35a4a6d0e6657570f9d2d51afc55629
Mihail Geshoski <mihail@moodle.com>
2025-05-16 12:57:23 +0800

    weekly release 5.1dev

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2025050900.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2025051600.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '5.1dev (Build: 20250509)';    // Human-friendly version name
+$release  = '5.1dev (Build: 20250516)';    // Human-friendly version name
 $branch   = '501';                      // This version's branch.
 $maturity = MATURITY_ALPHA;             // This version's maturity level.
