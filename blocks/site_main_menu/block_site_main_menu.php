c158c3ef214e485862a54cc5d80803dc89742b57
Sara Arjona <sara@moodle.com>
2025-05-27 09:51:28 +0200

    MDL-85392 block_site_main_menu: Add required library

--- blocks/site_main_menu/block_site_main_menu.php
+++ blocks/site_main_menu/block_site_main_menu.php
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+defined('MOODLE_INTERNAL') || die();
+require_once($CFG->dirroot. '/course/format/lib.php');
+
 /**
  * Site main menu block.
  *
