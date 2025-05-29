b9b69a5e3e6a5b884c788d2e8cc6f7c1cb47be8f
Mark Johnson <mark.johnson@catalyst-eu.net>
2025-05-29 15:09:12 +0100

    MDL-85069 question: Services for question bank and category search
    This updates the core_question_search_shared_banks external function
    with additional parameters to make it applicable outside of the "Switch
    bank" dialogue in the quiz, with the corresponding changes to the
    question_banks_datasource Javascript module, and unit test coverage.
    
    It also adds a new \core_question\output\question_category_selector
    class for rendering a select list of question categories, migrating
    the logic used in qbank_managecategories to a core namespace. There
    is a fragment callback for rendering this via AJAX.

--- question/classes/external/search_shared_banks.php
+++ question/classes/external/search_shared_banks.php
@@ -16,6 +16,7 @@
 
 namespace core_question\external;
 
+use core\exception\coding_exception;
 use core_external\external_api;
 use core_external\external_function_parameters;
 use core_external\external_single_structure;
@@ -49,10 +50,55 @@ class search_shared_banks extends external_api {
     public static function execute_parameters(): external_function_parameters {
         return new external_function_parameters(
             [
-                'contextid' => new external_value(PARAM_INT, 'The current context ID.'),
+                'contextid' => new external_value(PARAM_INT, 'The current context ID for applying text filters to bank names.'),
                 'search' => new external_value(PARAM_TEXT, 'Search terms by which to filter the shared banks.', default: ''),
+                'requiredcapabilities' => new external_multiple_structure(
+                    new external_value(PARAM_TEXT, 'Capability'),
+                    'Array of abbreviated "moodle/question:" capabilities that the user must have at least one of in the context ' .
+                        'of each matching question bank. Valid options are "add", "managecategory", "flag", "config", "edit", ' .
+                        '"view", "use", "move" or "tag". For "edit", "view", "use", "move" and "tag", the -all and -mine ' .
+                        "suffixed versions of the capabilty will both be checked.",
+                    VALUE_DEFAULT,
+                    ['use'],
+                ),
             ]
         );
+    }
+
+    /**
+     * Expand a list of abbreviated capaibilities into an array of full capability strings.
+     *
+     * Each abbreviation must match a capability with the 'moodle/question:' prefix. Capabilities that have an "all" and "mine"
+     * variant will have both variants included in the returned array.
+     *
+     * These abbreviations are copied from {@see question_has_capability_on()}
+     *
+     * @param array $abbreviations Abbreviated capabilities. Must match capabilities with the 'moodle/question:' prefix.
+     * @return array The expanded capabilities
+     */
+    protected static function expand_capabilities(array $abbreviations): array {
+        $capabilitieswithallandmine = ['edit', 'view', 'use', 'move', 'tag'];
+        $prefix = 'moodle/question:';
+        $suffixes = ['all', 'mine'];
+        $capabilities = [];
+        foreach ($abbreviations as $abbreviation) {
+            if (in_array($abbreviation, $capabilitieswithallandmine)) {
+                foreach ($suffixes as $suffix) {
+                    $capability = $prefix . $abbreviation . $suffix;
+                    if (is_null(get_capability_info($capability))) {
+                        throw new coding_exception("Capability {$capability} does not exist.");
+                    }
+                    $capabilities[] = $capability;
+                }
+            } else {
+                $capability = $prefix . $abbreviation;
+                if (is_null(get_capability_info($capability))) {
+                    throw new coding_exception("Capability {$capability} does not exist.");
+                }
+                $capabilities[] = $capability;
+            }
+        }
+        return $capabilities;
     }
 
     /**
@@ -60,23 +106,29 @@ class search_shared_banks extends external_api {
      *
      * @param int $contextid Context ID of the current activity
      * @param string $search String to filter results by question bank name
+     * @param array $requiredcapabilities List of abbreviated capabilities to check, {@see self::expand_capabilities()}
      * @return array
      */
-    public static function execute(int $contextid, string $search = ''): array {
+    public static function execute(
+        int $contextid,
+        string $search = '',
+        array $requiredcapabilities = ['use'],
+    ): array {
         [
             'contextid' => $contextid,
             'search' => $search,
+            'requiredcapabilities' => $requiredcapabilities,
         ] = self::validate_parameters(self::execute_parameters(), [
             'contextid' => $contextid,
             'search' => $search,
+            'requiredcapabilities' => $requiredcapabilities,
         ]);
 
         $modulecontext = context::instance_by_id($contextid);
-        $courseid = $modulecontext->get_parent_context()->instanceid;
+        self::validate_context($modulecontext);
 
         $sharedbanks = question_bank_helper::get_activity_instances_with_shareable_questions(
-            notincourseids: [$courseid],
-            havingcap: ['moodle/question:useall', 'moodle/question:usemine'],
+            havingcap: self::expand_capabilities($requiredcapabilities),
             filtercontext: $modulecontext,
             search: $search,
             limit: self::MAX_RESULTS + 1, // Return up to 1 extra result, so we know there are more.
@@ -112,7 +164,7 @@ class search_shared_banks extends external_api {
         return new external_single_structure([
             'sharedbanks' => new external_multiple_structure(
                 new external_single_structure([
-                    'value' => new external_value(PARAM_INT, 'Module ID of the shared bank.'),
+                    'value' => new external_value(PARAM_INT, 'Course Module ID of the shared bank.'),
                     'label' => new external_value(PARAM_TEXT, 'Formatted bank name'),
                 ]),
                 'List of shared banks',
