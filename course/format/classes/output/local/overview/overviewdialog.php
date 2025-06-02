7920905430a1ba5b5da7e2e04027c357558f3127
Mikel Martín <mikel@moodle.com>
2025-06-02 10:17:17 +0200

    MDL-83895 courseformat: Add new overviewdialog for overview page
    Added new core_courseformat\output\local\overview\overviewdialog class to display
    dialog elemetns in the course overview table. It will show a combination of
    title, description and a list of items (label: value).

---
+++ course/format/classes/output/local/overview/overviewdialog.php
@@ -0,0 +1,124 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core_courseformat\output\local\overview;
+
+use core\output\local\dropdown\dialog;
+use stdClass;
+
+/**
+ * Class to render an overview dialog element.
+ *
+ * @package    core_courseformat
+ * @copyright  2025 Mikel Martín <mikel@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class overviewdialog extends dialog {
+
+    /** @var stdClass[] The list of the items. */
+    protected array $items;
+
+    /**
+     * Constructor.
+     *
+     * The definition object could contain the following keys:
+     * - classes: component CSS classes.
+     * - buttonclasses: the button CSS classes.
+     * - dialogwidth: the dropdown width.
+     * - extras: extra HTML attributes (attribute => value).
+     *
+     * @param string $buttoncontent the button content
+     * @param string $title the overview dialog content title
+     * @param string $description the overview dialog content description
+     * @param array $definition an optional array of the element definition
+     */
+    public function __construct(
+        string $buttoncontent,
+        /** @var string The title of the overview dialog content. */
+        protected string $title = '',
+        /** @var string The title of the overview dialog content. */
+        protected string $description = '',
+        array $definition = []
+    ) {
+        parent::__construct($buttoncontent, '', $definition);
+        $this->items = [];
+    }
+
+    /**
+     * Set the items to be displayed in the overview dialog.
+     *
+     * @param string $label the label of the item
+     * @param string $value the value of the item
+     * @return overviewdialog
+     */
+    public function add_item(string $label, string $value): self {
+        $this->items[] = (object) [
+            'label' => $label,
+            'value' => $value,
+        ];
+        return $this;
+    }
+
+    /**
+     * Set the title of the overview dialog content.
+     *
+     * @param string $title the title to set
+     * @return overviewdialog
+     */
+    public function set_title(string $title): self {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * Set the description of the overview dialog content.
+     *
+     * @param string $description the description to set
+     * @return overviewdialog
+     */
+    public function set_description(string $description): self {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Export this data so it can be used as the context for a mustache template.
+     *
+     * @param \renderer_base $output typically, the renderer that's calling this function
+     * @return array data context for a mustache template
+     */
+    public function export_for_template(\renderer_base $output): array {
+        $data = [
+            ...parent::export_for_template($output),
+            'title' => $this->title,
+            'items' => $this->items,
+            'hasitems' => count($this->items),
+            'description' => $this->description,
+        ];
+
+        return $data;
+    }
+
+    /**
+     * Get the name of the template to use for this templatable.
+     *
+     * @param \renderer_base $renderer The renderer requesting the template name
+     * @return string the template name
+     */
+    public function get_template_name(\renderer_base $renderer): string {
+        return 'core_courseformat/local/overview/overviewdialog';
+    }
+}
