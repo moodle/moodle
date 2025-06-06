662befa64ed6f8d70c9b265fed7ca609a32d9a16
Mihail Geshoski <mihail@moodle.com>
2025-06-06 12:26:06 +0800

    Moodle release 4.4.9

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024042208.07;              // 20240422      = branching date YYYYMMDD - do not modify!
+$version  = 2024042209.00;              // 20240422      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '4.4.8+ (Build: 20250605)';    // Human-friendly version name
+$release  = '4.4.9 (Build: 20250609)';    // Human-friendly version name
 $branch   = '404';                      // This version's branch.
 $maturity = MATURITY_STABLE;            // This version's maturity level.
