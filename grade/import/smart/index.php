9c6d844d00c30d368fd23e9b6af3608dd7486ffd
Robert Russo <rrusso@lsu.edu>
2025-06-20 19:07:54 +0000

    Remove space from index

--- grade/import/smart/index.php
+++ grade/import/smart/index.php
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-
 require_once('../../../config.php');
 require_once('forms.php');
 require_once('lib.php');
