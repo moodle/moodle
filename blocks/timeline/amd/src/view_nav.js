moodle/moodle

commit 0c586b41c1336202286dcfcb2709cc2f75679c91

MDL-75669 theme_boost: Fix BS5 related JS
- Migrate Boostrap components related JS from JQuery to vanilla JS.
- Migrate Boostrap components related Events from JQuery to vanilla JS.

blocks/timeline/amd/src/view_nav.js
@@ -26,6 +26,7 @@ import * as View from 'block_timeline/view';
 import * as Notification from 'core/notification';
 import * as Utils from 'core/utils';
 import * as UserRepository from 'core_user/repository';
+import {getFirst} from 'core/normalise';
 
 const SELECTORS = {
     TIMELINE_DAY_FILTER: '[data-region="day-filter"]',
@@ -107,9 +108,11 @@ const registerViewSelector = function(root, timelineViewRoot) {
 
     // Listen for when the user changes tab so that we can show the first set of courses
     // and load their events when they request the sort by courses view for the first time.
-    viewSelector.on('shown shown.bs.tab', function(e) {
-        View.shown(timelineViewRoot);
-        $(e.target).removeClass('active');
+    getFirst(viewSelector).querySelectorAll('[data-bs-toggle="tab"]').forEach((tab) => {
+        tab.addEventListener('shown.bs.tab', (e) => {
+            View.shown(timelineViewRoot);
+            $(e.target).removeClass('active');
+        });
     });
 
 

commit 2424374a812b3ce6bd705ee0d827b6bf694d5b47

MDL-85779 block_timeline: fix/restore sort toggle dropdown behaviour.
Changes during Bootstrap upgrade, in conjunction with both 8a8c8ab2 and
0c586b41, caused the layout to deviate from that expected elsewhere for
managing of Bootstrap dropdown elements. Thus the dynamically chosen
item was no longer being shown as selected. Fix that here.

blocks/timeline/amd/src/view_nav.js
@@ -61,7 +61,7 @@ const registerTimelineDaySelector = function(root, timelineViewRoot) {
 
             var option = $(e.target).closest(SELECTORS.TIMELINE_DAY_FILTER_OPTION);
 
-            if (option.attr('aria-selected') == 'true') {
+            if (option.attr('aria-current') == 'true') {
                 // If it's already active then we don't need to do anything.
                 return;
             }
@@ -109,8 +109,9 @@ const registerViewSelector = function(root, timelineViewRoot) {
     // Listen for when the user changes tab so that we can show the first set of courses
     // and load their events when they request the sort by courses view for the first time.
     getFirst(viewSelector).querySelectorAll('[data-bs-toggle="tab"]').forEach((tab) => {
-        tab.addEventListener('shown.bs.tab', () => {
+        tab.addEventListener('shown.bs.tab', (e) => {
             View.shown(timelineViewRoot);
+            $(e.target).removeClass('active');
         });
     });
 

blocks/timeline/templates/nav-view-selector.mustache
@@ -31,13 +31,13 @@
             {{#sorttimelinedates}}{{#str}} sortbydates, block_timeline {{/str}}{{/sorttimelinedates}}
         </span>
     </button>
-    <div id="menusortby" role="tablist" class="dropdown-menu dropdown-menu-end list-group hidden" data-show-active-item data-skip-active-class="true">
+    <div id="menusortby" role="tablist" class="dropdown-menu dropdown-menu-end list-group hidden" data-show-active-item>
         <a
             class="dropdown-item {{#sorttimelinedates}}active{{/sorttimelinedates}}"
             href="#view_dates_{{uniqid}}-{{timelineinstanceid}}"
             data-bs-toggle="tab"
             data-filtername="sortbydates"
-            {{#sorttimelinedates}}aria-selected="true"{{/sorttimelinedates}}
+            {{#sorttimelinedates}}aria-current="true"{{/sorttimelinedates}}
             aria-label="{{#str}} ariaviewselectoroption, block_timeline, {{#str}} sortbydates, block_timeline {{/str}}{{/str}}"
             aria-controls="view_dates_{{uniqid}}-{{timelineinstanceid}}"
             role="tab"
@@ -49,7 +49,7 @@
             href="#view_courses_{{uniqid}}-{{timelineinstanceid}}"
             data-bs-toggle="tab"
             data-filtername="sortbycourses"
-            {{#sorttimelinecourses}}aria-selected="true"{{/sorttimelinecourses}}
+            {{#sorttimelinecourses}}aria-current="true"{{/sorttimelinecourses}}
             aria-label="{{#str}} ariaviewselectoroption, block_timeline, {{#str}} sortbycourses, block_timeline {{/str}}{{/str}}"
             aria-controls="view_courses_{{uniqid}}-{{timelineinstanceid}}"
             role="tab"