d182af6585c7e50c3fd056d4d973ebfc227c2b31
Benjamin Abraham <benjamin.abraham@unige.ch>
2025-07-25 12:41:44 +0200

    MDL-86119 mod_folder: Remove YUI TreeView
    Remove YUI TreeView and replace it with a layout of "ul" and  "li" tags
    Using core/tree javascript module to implement ARIA navigation and collapse toggle
    Accessibility text for root folder
    Empty alt attributes
    Remove unused foldertree class from edit.php form

--- public/mod/folder/amd/src/folder.js
+++ public/mod/folder/amd/src/folder.js
@@ -17,34 +17,17 @@
  * Javascript helper function for Folder module
  *
  * @module     mod_folder/folder
- * @copyright  2009 Petr Skoda  {@link http://skodak.org}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-import YUI from 'core/yui';
+import Tree from 'core/tree';
 
-export const initTree = (id, expandAll) => {
-    YUI.use('yui2-treeview', 'node-event-simulate', function(Y) {
-        var tree = new Y.YUI2.widget.TreeView(id);
-
-        tree.subscribe("clickEvent", function() {
-            // we want normal clicking which redirects to url
-            return false;
-        });
-
-        tree.subscribe("enterKeyPressed", function(node) {
-            // We want keyboard activation to trigger a click on the first link.
-            Y.one(node.getContentEl()).one('a').simulate('click');
-            return false;
-        });
-
-        if (expandAll) {
-            tree.expandAll();
-        } else {
-            // Else just expand the top node.
-            tree.getRoot().children[0].expand();
-        }
-
-        tree.render();
-    });
+/**
+ * The init function that does the job.
+ * It changes the display of directories and files into a tree.
+ *
+ * @param {string} elementId The ID of the container element.
+ */
+export const init = (elementId) => {
+    new Tree(`#${elementId} ul.foldertree[role="tree"]`);
 };
