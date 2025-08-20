b82b297954717c51f461219d311eca55bed98b1e
ferran <ferranrecio@gmail.com>
2025-08-20 13:54:46 +0200

    MDL-84632 mod_page: deprecate index.php related event

--- public/mod/page/classes/event/course_module_instance_list_viewed.php
+++ public/mod/page/classes/event/course_module_instance_list_viewed.php
@@ -14,14 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * The mod_page instance list viewed event.
- *
- * @package    mod_page
- * @copyright  2013 Ankit Agarwal
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 namespace mod_page\event;
 defined('MOODLE_INTERNAL') || die();
 
@@ -32,7 +24,23 @@ defined('MOODLE_INTERNAL') || die();
  * @since      Moodle 2.7
  * @copyright  2013 onwards Ankit Agarwal
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @deprecated This event is deprecated and will be removed in Moodle 6.0.
+ * @todo       Remove class in 6.0 (MDL-86384)
  */
+#[\core\attribute\deprecated(
+    replacement: core\event\course_resources_list_viewed::class,
+    since: '5.1',
+    mdl: 'MDL-84632',
+)]
 class course_module_instance_list_viewed extends \core\event\course_module_instance_list_viewed {
+    #[\Override]
+    #[\core\attribute\deprecated(
+        replacement: core\event\course_resources_list_viewed::class,
+        since: '5.1',
+        mdl: 'MDL-84632',
+    )]
+    protected function init() {
+        \core\deprecation::emit_deprecation([self::class, __FUNCTION__]);
+        parent::init();
+    }
 }
-
