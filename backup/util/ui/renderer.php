1894130564b0aa05c1988aadaf1dcb24c48a9278
Luca Bösch <luca.boesch@bfh.ch>
2025-08-22 12:17:47 +0200

    MDL-86413 backup: Fix backup tables styles

--- backup/util/ui/renderer.php
+++ backup/util/ui/renderer.php
@@ -202,7 +202,7 @@ class core_backup_renderer extends plugin_renderer_base {
                         $table->head = array(get_string('module', 'backup'), get_string('title', 'backup'), get_string('userinfo', 'backup'));
                         $table->colclasses = array('modulename', 'moduletitle', 'userinfoincluded');
                         $table->align = array('left', 'left', 'center');
-                        $table->attributes = array('class' => 'activitytable generaltable');
+                        $table->attributes = ['class' => 'activitytable table generaltable'];
                         $table->data = array();
                     }
                     $name = get_string('pluginname', $activity->modulename);
@@ -690,7 +690,7 @@ class core_backup_renderer extends plugin_renderer_base {
             }
 
             $table = new html_table();
-            $table->attributes['class'] = 'backup-files-table generaltable';
+            $table->attributes['class'] = 'backup-files-table table generaltable';
             $table->head = $tablehead;
             $table->width = '100%';
             $table->data = [];
@@ -1051,7 +1051,7 @@ class core_backup_renderer extends plugin_renderer_base {
         $tablehead = array(get_string('course'), get_string('time'), get_string('status', 'backup'));
 
         $table = new html_table();
-        $table->attributes['class'] = 'backup-files-table generaltable';
+        $table->attributes['class'] = 'backup-files-table table generaltable';
         $table->head = $tablehead;
         $tabledata = array();
 
@@ -1092,7 +1092,7 @@ class core_backup_renderer extends plugin_renderer_base {
         );
 
         $table = new html_table();
-        $table->attributes['class'] = 'backup-files-table generaltable';
+        $table->attributes['class'] = 'backup-files-table table generaltable';
         $table->head = $tablehead;
 
         $tabledata = array();
