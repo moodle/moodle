a93254f43c066980439ad915f89847486c8c9d11
Huong Nguyen <huongnv13@gmail.com>
2025-08-22 11:16:31 +0700

    MDL-73845 enrol_database: Fix MySQL issue

--- enrol/database/version.php
+++ enrol/database/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024100701;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024100702;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100100;        // Requires this Moodle version.
 $plugin->component = 'enrol_database';  // Full name of the plugin (used for diagnostics)
