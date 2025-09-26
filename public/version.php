a828ba12c1796bb6f22a705ad61202be5b26fab2
cescobedo <carlos.escobedo@gmail.com>
2025-09-26 13:45:44 +0200

    Moodle release 5.1rc1

--- public/version.php
+++ public/version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2025092500.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2025092600.00;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '5.1beta (Build: 20250923)';    // Human-friendly version name
+$release  = '5.1rc1 (Build: 20250926)';    // Human-friendly version name
 $branch   = '501';                      // This version's branch.
-$maturity = MATURITY_BETA;              // This version's maturity level.
+$maturity = MATURITY_RC;                // This version's maturity level.
