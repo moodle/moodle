d3c9ccba3cd9a34460c55aad8eaa541d5d860f1a
Jayce Birrell <jayce88@live.com.au>
2025-10-02 14:39:41 +0930

    MDL-86802 core: make CURLOPT_USERPWD optional for put()
    Microsoft does not always allow an Authorization header when using PUT but the put() method sets one
    by default - https://learn.microsoft.com/en-us/graph/api/driveitem-createuploadsession?view=graph-rest-1.0#remarks.
    I've included an optional argument to prevent this, but the default
    value will maintain the pre-existing logic to avoid other regressions.

--- public/lib/filelib.php
+++ public/lib/filelib.php
@@ -4135,9 +4135,10 @@ class curl {
      * @param string $url
      * @param array $params
      * @param array $options
+     * @param bool $includeuserpwd Whether to include CURLOPT_USERPWD if not already set
      * @return ?string
      */
-    public function put($url, $params = array(), $options = array()) {
+    public function put($url, $params = [], $options = [], $includeuserpwd = true) {
         $file = '';
         $fp = false;
         if (isset($params['file'])) {
@@ -4151,7 +4152,7 @@ class curl {
             } else {
                 return null;
             }
-            if (!isset($this->options['CURLOPT_USERPWD'])) {
+            if (!isset($this->options['CURLOPT_USERPWD']) && $includeuserpwd) {
                 $this->setopt(array('CURLOPT_USERPWD' => 'anonymous: noreply@moodle.org'));
             }
         } else {
