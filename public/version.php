eb47eff0719455a7e1f7a9805bc75dad3f6b3995
Huong Nguyen <huongnv13@gmail.com>
2025-10-04 09:06:25 +0700

    Moodle release 5.1

--- public/version.php
+++ public/version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2025100300.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2025100600.00;              // 20251006      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '5.1rc2 (Build: 20251003)';    // Human-friendly version name
+$release  = '5.1 (Build: 20251006)';    // Human-friendly version name
 $branch   = '501';                      // This version's branch.
-$maturity = MATURITY_RC;                // This version's maturity level.
+$maturity = MATURITY_STABLE;            // This version's maturity level.
