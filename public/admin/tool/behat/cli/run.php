8fec48df4a9e7fdf652d36fd9b74bd4a8343b805
Luca Bösch <luca.boesch@bfh.ch>
2025-10-05 22:30:34 +0200

    MDL-86831 behat: Adopt behat paths to include public/

--- public/admin/tool/behat/cli/run.php
+++ public/admin/tool/behat/cli/run.php
@@ -89,7 +89,7 @@ Options:
 -h, --help         Print out this help
 
 Example from Moodle root directory:
-\$ php admin/tool/behat/cli/run.php --tags=\"@javascript\"
+\$ php public/admin/tool/behat/cli/run.php --tags=\"@javascript\"
 
 More info in https://moodledev.io/general/development/tools/behat/running
 ";
