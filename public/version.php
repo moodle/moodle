d33ecac29c772c0ddafdf7fd7770e43fbba31eed
Andrew Nicols <andrew@nicols.co.uk>
2025-10-17 07:21:28 +0800

    weekly on-sync release 5.2dev

--- public/version.php
+++ public/version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2025101700.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2025101700.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '5.2dev (Build: 20251017)';    // Human-friendly version name
