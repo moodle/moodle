0c146aa2612fb6d0544f200a018cb42da75db713
David Woloszyn <david.woloszyn@moodle.com>
2025-10-22 11:52:11 +1100

    MDL-86544 lib: Adjust checks on local URL params

--- public/lib/classes/param.php
+++ public/lib/classes/param.php
@@ -1067,8 +1067,10 @@ enum param: string {
                 // Absolute, and matches our wwwroot.
             } else {
                 // Relative - let's make sure there are no tricks.
-                if (validateUrlSyntax('/' . $param, 's-u-P-a-p-f+q?r?') &&
-                        !preg_match('/javascript(?:.*\/{2,})?:/i', rawurldecode($param))) {
+                if (
+                    validateUrlSyntax('/' . $param, 's-u-P-a-p-f+q?r?') &&
+                    !preg_match('/\bjavascript\b[\s\S]*?(?:\:|\/)/i', rawurldecode($param))
+                ) {
                     // Valid relative local URL.
                 } else {
                     $param = '';
