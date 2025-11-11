156c6f69ace761483291bc21fd0f2acb60a98daa
Paul Holden <paulh@moodle.com>
2025-11-11 11:29:03 +0000

    MDL-87120 course: observe activity dates display in course requests.
    Upgrade step to ensure the default value for this field in the schema
    matches the default value used in the interface itself.

--- public/lib/db/upgrade.php
+++ public/lib/db/upgrade.php
@@ -1618,5 +1618,17 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2025121200.01);
     }
 
+    if ($oldversion < 2025121900.01) {
+        // Changing the default of field showactivitydates on table course to 1.
+        $table = new xmldb_table('course');
+        $field = new xmldb_field('showactivitydates', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '1', 'originalcourseid');
+
+        // Launch change of default for field showactivitydates.
+        $dbman->change_field_default($table, $field);
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2025121900.01);
+    }
+
     return true;
 }
