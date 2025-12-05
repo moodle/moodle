00cd643c333ff6cca0eac92c67925a4c78af354b
Andi Permana <andi.thea96@gmail.com>
2025-12-05 15:13:46 +0700

    MDL-86679 core: Fix MS field mappings in upgrade script

--- public/version.php
+++ public/version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2025100600.12;              // 20251006      = branching date YYYYMMDD - do not modify!
+$version  = 2025100600.13;              // 20251006      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '5.1+ (Build: 20251205)';    // Human-friendly version name
