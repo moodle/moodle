09d881637e73778d57b5feb5e33d67a6939403e5
roileviK <roi.levi@kaltura.com>
2026-01-01 13:53:18 +0200

    KMS-32502 add custom kaltura media submission plugin to core moodle assignment

---
+++ mod/assign/submission/kalvid/version.php
@@ -0,0 +1,32 @@
+<?php
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Kaltura version script
+ *
+ * @package    assignsubmission_kalvid
+ * @copyright 2012 NetSpot {@link http://www.netspot.com.au}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2025041400;
+$plugin->component  = 'assignsubmission_kalvid';
+$plugin->release    = 'Kaltura release 4.5.2';
+$plugin->requires = 2025040800;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = array(
+    'local_kaltura' => 2025041400,
+);
