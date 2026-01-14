a11dd20bc9cd9e9aed0a4af763447ebb32b9ccfd
BryantVanOrdenAust <bvanorden@aust-mfg.com>
2026-01-14 10:27:36 -0700

    upgrade version of cource matrix

--- public/local/coursematrix/version.php
+++ public/local/coursematrix/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursematrix';
-$plugin->version = 2024011400;
+$plugin->version = 2026011401;
 $plugin->requires = 2022112800; // Moodle 4.1+.
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '1.0.0';
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = '1.1.0';
