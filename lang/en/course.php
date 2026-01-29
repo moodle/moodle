4f3ecdf095e9e88026400cbdc2cbc01832d28938
Sara Arjona <sara@moodle.com>
2026-01-29 12:09:43 +0100

    MDL-87761 course: Add warning for phasing out subsection descriptions
    The Description field within the Subsection edit form has been deprecated since Moodle 5.2 onwards (see MDL-87279).
    To ensure a smooth transition and avoid data loss for our users, the XD team have prepared a solution to notify them
    of this upcoming change in stable versions.

--- lang/en/course.php
+++ lang/en/course.php
@@ -141,6 +141,7 @@ $string['searchresults'] = 'Search results: {$a}';
 $string['sectionlink'] = 'Permalink';
 $string['showstartedcoursestask'] = 'Show courses on start date';
 $string['submitsearch'] = 'Submit search';
+$string['subsectiondescriptionwarning'] = '<strong>Subsection descriptions will be discontinued in Moodle 5.2</strong>. To add a description, use a <strong>Text and media area</strong> instead.';
 $string['studentsatriskincourse'] = 'Students at risk in {$a} course';
 $string['studentsatriskinfomessage'] = 'Hi {$a->userfirstname},
 <p>Students in the {$a->coursename} course have been identified as being at risk.</p>';
