e5269918b5d9b32065a07fd6748089a93732c6cb
Andi Permana <andi.thea96@gmail.com>
2026-02-03 23:40:46 +0700

    MDL-87352 admin: Add MoodleNet removal warnings

--- admin/renderer.php
+++ admin/renderer.php
@@ -279,6 +279,7 @@ class core_admin_renderer extends plugin_renderer_base {
      * @param bool $showfeedbackencouragement Whether the feedback encouragement content should be displayed or not.
      * @param bool $showservicesandsupport Whether the services and support content should be displayed or not.
      * @param string $xmlrpcwarning XML-RPC deprecation warning message.
+     * @param string $moodlenetwarning MoodleNet removal warning message.
      *
      * @return string HTML to output.
      */
@@ -288,7 +289,7 @@ class core_admin_renderer extends plugin_renderer_base {
             $themedesignermode = false, $devlibdir = false, $mobileconfigured = false,
             $overridetossl = false, $invalidforgottenpasswordurl = false, $croninfrequent = false,
             $showcampaigncontent = false, bool $showfeedbackencouragement = false, bool $showservicesandsupport = false,
-            $xmlrpcwarning = '') {
+            $xmlrpcwarning = '', $moodlenetwarning = '') {
 
         global $CFG;
         $output = '';
@@ -314,6 +315,7 @@ class core_admin_renderer extends plugin_renderer_base {
         $output .= $this->mobile_configuration_warning($mobileconfigured);
         $output .= $this->forgotten_password_url_warning($invalidforgottenpasswordurl);
         $output .= $this->mnet_deprecation_warning($xmlrpcwarning);
+        $output .= $this->moodlenet_removal_warning($moodlenetwarning);
         $output .= $this->userfeedback_encouragement($showfeedbackencouragement);
         $output .= $this->services_and_support_content($showservicesandsupport);
         $output .= $this->campaign_content($showcampaigncontent);
@@ -2265,6 +2267,20 @@ class core_admin_renderer extends plugin_renderer_base {
         }
 
         return $this->warning($xmlrpcwarning);
+    }
+
+    /**
+     * Display a warning about the removal of MoodleNet integration.
+     *
+     * @param string $moodlenetwarning The warning message
+     * @return string HTML to output.
+     */
+    protected function moodlenet_removal_warning($moodlenetwarning) {
+        if (empty($moodlenetwarning)) {
+            return '';
+        }
+
+        return $this->warning($moodlenetwarning);
     }
 
     /**
