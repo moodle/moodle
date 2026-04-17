508b7030ad62492255e48caf334c8d2203ddc7e4
Cameron Ball <cameronball@catalyst-au.net>
2026-04-17 19:54:32 +0800

    MDL-88477 javascript: Fix paging bar label requests

--- public/version.php
+++ public/version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2026051900.00;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2026051900.01;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '5.3dev (Build: 20260519)';    // Human-friendly version name
