ae481ba73800af576a244300857d389694a6ccbf
Paul Holden <paulh@moodle.com>
2026-04-23 21:51:27 +0100

    MDL-88538 enrol_manual: ensure selected group belongs to course.

--- public/enrol/manual/ajax.php
+++ public/enrol/manual/ajax.php
@@ -98,7 +98,7 @@ switch ($action) {
         $startdateselect = optional_param_array('startdateselect', [], PARAM_INT);
         $recovergrades = optional_param('recovergrades', 0, PARAM_INT);
         $timeend = optional_param_array('timeend', [], PARAM_INT);
-        $group = optional_param('group', null, PARAM_INT);
+        $groupid = optional_param('group', 0, PARAM_INT);
 
         if (empty($roleid)) {
             $roleid = null;
@@ -154,7 +154,7 @@ switch ($action) {
 
         $mform = new enrol_manual_enrol_users_form(null, (object)["context" => $context]);
         $userenroldata = [
-                'group' => $group,
+                'group' => $groupid,
                 'startdate' => $timestart,
                 'timeend' => $timeend,
         ];
@@ -177,8 +177,11 @@ switch ($action) {
         if ($plugin->allow_enrol($instance) && has_capability('enrol/'.$plugin->get_name().':enrol', $context)) {
             foreach ($users as $user) {
                 $plugin->enrol_user($instance, $user->id, $roleid, $timestart, $timeend, null, $recovergrades);
-                if ($group && has_capability('moodle/course:managegroups', $context)) {
-                    groups_add_member($group, $user->id);
+                if ($groupid) {
+                    $group = $DB->get_record('groups', ['id' => $groupid, 'courseid' => $course->id]);
+                    if ($group && has_capability('moodle/course:managegroups', $context)) {
+                        groups_add_member($group, $user->id);
+                    }
                 }
             }
             $outcome->count += count($users);
