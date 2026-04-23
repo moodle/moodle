630e9119c9d83d400e128e7d8adbcdb3638f9932
Paul Holden <paulh@moodle.com>
2026-04-23 18:46:49 +0100

    MDL-88533 aiplacement_courseassist: verify access in external methods.

--- ai/placement/courseassist/classes/external/summarise_text.php
+++ ai/placement/courseassist/classes/external/summarise_text.php
@@ -72,12 +72,14 @@ class summarise_text extends external_api {
             'contextid' => $contextid,
             'prompttext' => $prompttext,
         ]);
+
         // Context validation and permission check.
-        // Get the context from the passed in ID.
         $context = \context::instance_by_id($contextid);
+        self::validate_context($context);
 
         // Check the user has permission to use the AI service.
-        self::validate_context($context);
+        require_capability('aiplacement/courseassist:summarise_text', $context);
+
         if (!utils::is_course_assist_available($context)) {
             throw new \moodle_exception('nocourseassist', 'aiplacement_courseassist');
         }
