30065fb804ed562257ea7bc05c9ee75fc6d5134a
Brendan Heywood <brendan@catalyst-au.net>
2026-05-14 13:36:45 +1000

    MDL-86934 tool_dataprivacy: Remove data loss risk from requestdelete
    This capability only grants a user the ability to request a delete
    so there is no risk here. The approver of the request bears the risk.

--- public/admin/tool/dataprivacy/version.php
+++ public/admin/tool/dataprivacy/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2026042000;
+$plugin->version   = 2026042001;
 $plugin->requires  = 2026041000;
 $plugin->component = 'tool_dataprivacy';
