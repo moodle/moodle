244d94041df0b9193d99146bf3e235ba0534caa1
Rajneel Totaram <rjnlfj@yahoo.com>
2026-05-20 14:37:31 +1200

    MDL-88637 h5p: Rebuild content library dependencies
    This change forces H5P content dependencies to be rebuilt. When we upgraded to
    h5plib_v128 in MDL-88092, the existing h5p content dependencies were not refreshed.
    So when newer libraries were fetched via the h5p_get_content_types_task task,
    some h5p content failed to load since they were referencing the old dependencies.
    
    This fix will trigger a full rebuild of the dependencies.
    
    Co-authored-by: Safat <safat.shahin@moodle.com>

--- public/h5p/classes/framework.php
+++ public/h5p/classes/framework.php
@@ -1484,7 +1484,7 @@ class framework implements H5PFrameworkInterface {
     }
 
     /**
-     * Will clear filtered params for all the content that uses the specified.
+     * Will clear filtered params for all the content that uses the specified
      * libraries. This means that the content dependencies will have to be rebuilt and the parameters re-filtered.
      * Implements clearFilteredParameters().
      *
@@ -1497,10 +1497,17 @@ class framework implements H5PFrameworkInterface {
             return;
         }
 
-        list($insql, $inparams) = $DB->get_in_or_equal($libraryids);
+        [$maininsql, $maininparams] = $DB->get_in_or_equal($libraryids, SQL_PARAMS_NAMED, 'mainlib');
+        [$depinsql, $depinparams] = $DB->get_in_or_equal($libraryids, SQL_PARAMS_NAMED, 'deplib');
 
-        $DB->set_field_select('h5p', 'filtered', null,
-            "mainlibraryid $insql", $inparams);
+        $select = "mainlibraryid {$maininsql}
+            OR id IN (
+                SELECT h5pid
+                  FROM {h5p_contents_libraries}
+                 WHERE libraryid {$depinsql}
+            )";
+
+        $DB->set_field_select('h5p', 'filtered', null, $select, $maininparams + $depinparams);
     }
 
     /**
