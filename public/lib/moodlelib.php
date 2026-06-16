27f18db2f06faaeff1dd9a8980a1e609ca5dc47b
Paul Holden <paulh@moodle.com>
2026-06-16 12:24:17 +0100

    MDL-88335 auth: escape username display field for password reset.

--- public/lib/moodlelib.php
+++ public/lib/moodlelib.php
@@ -6256,7 +6256,7 @@ function send_password_change_confirmation_email($user, $resetrecord) {
     foreach ($placeholders as $field => $value) {
         $data->{$field} = $value;
     }
-    $data->username  = $user->username;
+    $data->username  = s($user->username);
     $data->sitename  = format_string($site->fullname);
     $data->link      = $CFG->wwwroot .'/login/forgot_password.php?token='. $resetrecord->token;
     $data->admin     = generate_email_signoff();
