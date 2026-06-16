141c3c8b86883a3b48063377472a25d7a3e0ae21
Alex Damsted <alexdamsted@catalyst-au.net>
2026-06-16 10:17:18 +1000

    MDL-79763 mod_assign: convert_submission adhoc task refactor

--- public/mod/assign/feedback/editpdf/classes/task/convert_submission.php
+++ public/mod/assign/feedback/editpdf/classes/task/convert_submission.php
@@ -70,7 +70,7 @@ class convert_submission extends adhoc_task {
             }
         }
 
-        $conversionrequirespolling = false;
+        $remainingattempts = $this->get_attempts_available() - 1;
         foreach ($users as $userid) {
             mtrace('Converting submission for user id ' . $userid);
 
@@ -83,39 +83,24 @@ class convert_submission extends adhoc_task {
             if (!$assign->can_view_submission($userid)) {
                 continue;
             }
-            // Note: Before MDL-71468, the scheduled task version of this
-            // task would stop attempting to poll the conversion after a
-            // configured number of attempts were made to poll it, see:
-            //
-            // mod/assign/feedback/editpdf/classes/task/convert_submissions.php@MOODLE_400_STABLE
-            //
-            // This means that currently this adhoc task, if it fails, will retry forever. But
-            // the fail-delay mechanism will ensure that it eventually only tries once per day.
-            //
-            // See MDL-75457 for details on re-implementing the conversionattemptlimit.
-            //
-            // Also note: This code must not be in a try/catch - an exception needs to be thrown to
-            // allow the task API to mark the task as failed and update its faildelay. Using
-            // manager::adhoc_task_failed in the catch block will not work, as the task API
-            // will later assume the task completed successfully (as no exception was thrown) and
-            // complete it (removing it from the adhoc task queue).
+
             $combineddocument = document_services::get_combined_pdf_for_attempt($assign, $userid, $data->submissionattempt);
             switch ($combineddocument->get_status()) {
                 case combined_document::STATUS_READY:
                 case combined_document::STATUS_READY_PARTIAL:
                 case combined_document::STATUS_PENDING_INPUT:
-                    // The document has not been converted yet or is somehow still ready.
-                    $conversionrequirespolling = true;
+                    // Unresolved cases exist where these switch cases never resolve.
+                    // Set a soft retry delay so the task tries again without error.
+                    mtrace("Conversion in-progress. Requeueing self to check again. Attempts remaining: {$remainingattempts}");
+                    $this->set_soft_retry_delay();
                     continue 2;
                 case combined_document::STATUS_FAILED:
                     // Although STATUS_FAILED indicates a "permanent error" it should be possible
                     // in some cases to fix the underlying problem, allowing the conversion to
                     // complete. So we throw an exception here, allowing the adhoc task to retry.
-                    //
-                    // Currently this can result in the task trying indefinitely (although it will
-                    // settle on trying once per day due to the faildelay exponential backoff)
-                    // however once the conversionattepmtlimit is re-implemented in MDL-75457 the
-                    // task will eventually get dropped.
+                    mtrace(
+                        "Conversion has failed. Failed task will retry with a fail-delay. Attempts remaining: {$remainingattempts}."
+                    );
                     throw new \moodle_exception('documentcombinationfailed');
             }
 
@@ -123,13 +108,7 @@ class convert_submission extends adhoc_task {
             document_services::get_page_images_for_attempt($assign, $userid, $data->submissionattempt, true);
         }
 
-        if ($conversionrequirespolling) {
-            mtrace('Conversion still in progress. Requeueing self to check again.');
-            $task = new self;
-            $task->set_custom_data($data);
-            $task->set_next_run_time(\core\di::get(\core\clock::class)->time() + MINSECS);
-            manager::queue_adhoc_task($task);
-        } else {
+        if (!$this->is_adhoc_task_delayed()) {
             mtrace('The document has been successfully converted');
         }
     }
