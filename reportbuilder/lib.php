d7a2c05d16902da53ff8ca8c12c518bb52d1d1b4
Paul Holden <paulh@moodle.com>
2026-07-02 13:13:29 +0100

    MDL-89078 reportbuilder: stricter validation of audience class name.

--- reportbuilder/lib.php
+++ reportbuilder/lib.php
@@ -24,6 +24,7 @@
 
 declare(strict_types=1);
 
+use core\exception\invalid_parameter_exception;
 use core\output\inplace_editable;
 use core_reportbuilder\form\audience;
 use core_reportbuilder\form\filter;
@@ -65,6 +66,7 @@ function core_reportbuilder_output_fragment_filters_form(array $params): string 
  *
  * @param array $params
  * @return string
+ * @throws invalid_parameter_exception
  */
 function core_reportbuilder_output_fragment_audience_form(array $params): string {
     global $PAGE;
@@ -73,7 +75,11 @@ function core_reportbuilder_output_fragment_audience_form(array $params): string
     permission::require_can_edit_report($report);
 
     // Verify current user can add the requested audience type.
-    audience_base::instance(0, (object) $params)->require_user_can_add();
+    $instance = audience_base::instance(0, (object) $params);
+    if ($instance === null) {
+        throw new invalid_parameter_exception($params['classname']);
+    }
+    $instance->require_user_can_add();
 
     $audienceform = new audience(null, null, 'post', '', [], true, [
         'reportid' => $params['reportid'],
