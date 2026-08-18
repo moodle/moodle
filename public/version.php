3c779016c2b79670589db21c642911d6a1c3239e
Michael Hawkins <michaelh@moodle.com>
2026-08-18 22:06:50 +0800

    weekly release 5.1.6+

--- public/version.php
+++ public/version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2025100606.00;              // 20251006      = branching date YYYYMMDD - do not modify!
+$version  = 2025100606.01;              // 20251006      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '5.1.6 (Build: 20260810)';    // Human-friendly version name
+$release  = '5.1.6+ (Build: 20260818)';    // Human-friendly version name
 $branch   = '501';                      // This version's branch.
 $maturity = MATURITY_STABLE;            // This version's maturity level.
